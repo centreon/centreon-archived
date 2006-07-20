792e4c766cae5da9f0da1640021f347b0ed40856
Cedrick Facon <cfacon@merethis.com>
2006-07-20 17:24:57 +0000

    bug fix ( search_type..)
    git-svn-id: http://svn.centreon.com/oreon-1.3@135 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/pagination.php
+++ www/include/common/pagination.php
@@ -19,10 +19,16 @@ For information : contact@oreon.org
 global $oreon;
 global $num;
 global $search;
+
+global $search_type_service;
+global $search_type_host;
+
+isset ($_GET["search_type_service"]) ? $search_type_service = $_GET["search_type_service"] : $search_type_service = NULL;
+isset ($_GET["search_type_host"]) ? $search_type_host = $_GET["search_type_host"] : $search_type_host = NULL;
+
 isset ($_GET["num"]) ? $num = $_GET["num"] : $num = 0;
 isset ($_GET["search"]) ? $search = $_GET["search"] : $search = NULL;
 isset ($_GET["o"]) ? $o = $_GET["o"] : $o = NULL;
-
 
 global $rows;
 global $limit;
@@ -32,8 +38,10 @@ global $limit;
 global $gopt;
 global $pagination;
 
+$url_var = "";
+$url_var .= "&search_type_service=" . $search_type_service;
+$url_var .= "&search_type_host=" . $search_type_host;
 
-$url_var = ""; 
 if(isset($_GET["order"]))
 {
 	$url_var .= "&order=".$_GET["order"];
@@ -53,10 +61,13 @@ if(isset($_GET["sort_types"]))
 	$tpl = initSmartyTpl($path, $tpl, "./include/common/");
 
 
+
 	$pageArr = array();
 	$istart = 0;
 	for($i = 5, $istart = $num; $istart && $i > 0; $i--)
 		$istart--;		
+
+
 	for($i2 = 0, $iend = $num; ( $iend <  ($rows / $limit -1)) && ( $i2 < (5 + $i)); $i2++)
 		$iend++;
 
@@ -90,7 +101,7 @@ if(isset($_GET["sort_types"]))
 	ksort($select);
 
 
-	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p);
+	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p."&search_type_service=" . $search_type_service."&search_type_host=" . $search_type_host);
 	$selLim =& $form->addElement('select', 'limit', $lang['nbr_per_page'], $select, array("onChange" => "this.form.submit('')"));
 	$selLim->setSelected($limit);
 
@@ -101,11 +112,13 @@ if(isset($_GET["sort_types"]))
 	$form->addElement('hidden', 'num');
 	$form->addElement('hidden', 'order');
 	$form->addElement('hidden', 'sort_types');
+	$form->addElement('hidden', 'search_type_service');
+	$form->addElement('hidden', 'search_type_host');
 	$tab = array ("p" => $p, "search" => $search, "num"=>$num);
 	$form->setDefaults($tab);
 
 
-		$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$form->accept($renderer);
 	
 	isset($_GET["host_name"]) ? $host_name = $_GET["host_name"] : $host_name = NULL;
