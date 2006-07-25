8b4c2fa7712c0aad6363817b1fc224c77615a8de
tmai <tmai>
2006-07-25 10:12:13 +0000

    avoid multiply insert when the script is executed many time
    git-svn-id: http://svn.centreon.com/oreon-1.3@172 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/php/update-1.3-RC2.php
+++ www/install/php/update-1.3-RC2.php
@@ -5,8 +5,14 @@ require_once ("../../DBconnect.php");
 
 $res =& $pearDB->query("SELECT service_id FROM service");
 while($res->fetchInto($service))	{
-	$rq = "INSERT INTO `extended_service_information` ( `esi_id` , `service_service_id` , `esi_notes` , `esi_notes_url` , `esi_action_url` , `esi_icon_image` , `esi_icon_image_alt` ) ";
-	$rq .= "VALUES (NULL , '".$service["service_id"]."', NULL , NULL , NULL , NULL , NULL);";
-	$pearDB->query($rq);	
+	$cmd = "SELECT esi_id FROM extended_service_information where service_service_id = ".$service["service_id"]." ";	
+	$result =& $pearDB->query($cmd);
+	if ($result->numRows() == 0){
+		$rq = "INSERT INTO `extended_service_information` ( `esi_id` , `service_service_id` , `esi_notes` , `esi_notes_url` , `esi_action_url` , `esi_icon_image` , `esi_icon_image_alt` ) ";
+		$rq .= "VALUES (NULL , '".$service["service_id"]."', NULL , NULL , NULL , NULL , NULL);";
+		$pearDB->query($rq);
+	}
+	$result->free();
 }
+$res->free();
 ?>
