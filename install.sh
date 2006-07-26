077f0237319746b691e9873c8016598cfd2498ce
Nicolas Cordier <ncordier@merethis.com>
2006-07-26 11:51:44 +0000

    ajout du reload pour nagios et du restart pour snmptrapd dans sudoers
    git-svn-id: http://svn.centreon.com/oreon-1.3@213 6bcd3966-0018-0410-8128-fd23d134de7e

--- install.sh
+++ install.sh
@@ -419,6 +419,8 @@ function config_sudo()
       echo "#Add by OREON" >> $SUDO_FILE
       echo "User_Alias      OREON= $WEB_USER" >> $SUDO_FILE
       echo "OREON   ALL = NOPASSWD: /etc/init.d/nagios restart" >> $SUDO_FILE
+      echo "OREON   ALL = NOPASSWD: /etc/init.d/nagios reload" >> $SUDO_FILE
+      echo "OREON   ALL = NOPASSWD: /etc/init.d/snmptrapd restart" >> $SUDO_FILE
       echo "" >> $SUDO_FILE
       echo_success "Configuring Sudo" "OK"
   else
