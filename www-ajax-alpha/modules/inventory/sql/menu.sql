a51bd16958514e719f7a997d60b3a055861143f9
Julien Mathis <jmathis@merethis.com>
2006-07-28 09:04:57 +0000

    bad branch
    git-svn-id: http://svn.centreon.com/oreon-1.3@249 6bcd3966-0018-0410-8128-fd23d134de7e

--- www-ajax-alpha/modules/inventory/sql/menu.sql
+++
@@ -1,4 +0,0 @@
-
-INSERT INTO `redirect_pages` VALUES (88, 320, './modules/inventory/inventory_server.php', 1);
-INSERT INTO `redirect_pages` VALUES (89, 321, './modules/inventory/inventory_network_ressource.php', 1);
-INSERT INTO `redirect_pages` VALUES (90, 322, './modules/inventory/inventory_configure.php', 1);
