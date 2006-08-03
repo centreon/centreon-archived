e0805a81b299685c94a44840ce630ab51847cc46
Romain Le Merlus <rlemerlus@merethis.com>
2006-08-03 08:08:11 +0000

    Missing DB foreign key for meta_service
    git-svn-id: http://svn.centreon.com/oreon-1.3@282 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.3.sql
+++ www/install/sql/UpdateDB-1.3.sql
@@ -18,3 +18,8 @@ ALTER TABLE `dependency_serviceChild_relation` ADD FOREIGN KEY ( `host_host_id` 
 ALTER TABLE `escalation_service_relation` ADD `host_host_id` INT NULL AFTER `service_service_id` ;
 ALTER TABLE `escalation_service_relation` ADD INDEX `host_index` ( `host_host_id` ) ;
 ALTER TABLE `escalation_service_relation` ADD FOREIGN KEY ( `host_host_id` ) REFERENCES `host` ( `host_id` ) ON DELETE CASCADE ;
+
+-- 03 06 2006 
+ALTER TABLE `meta_service_relation` ADD INDEX `host_index` ( `host_id` ) ;
+ALTER TABLE `meta_service_relation` ADD FOREIGN KEY ( `host_id` ) REFERENCES `host` ( `host_id` ) ON DELETE CASCADE ;
+ALTER TABLE `meta_service_relation` ADD FOREIGN KEY ( `meta_id` ) REFERENCES `meta_service` ( `meta_id` ) ON DELETE CASCADE ;
