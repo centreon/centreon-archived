bf422cca32194136573720bd5c20a353e8845251
Romain Le Merlus <rlemerlus@merethis.com>
2006-08-09 17:00:31 +0000

    Add ';' at the end of the line
    git-svn-id: http://svn.centreon.com/oreon-dev@336 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.3.1.sql
+++ www/install/sql/UpdateDB-1.3.1.sql
@@ -1,4 +1,5 @@
 -- 08/08/2006
 
 ALTER TABLE `giv_graphs_template` CHANGE `lower_limit` `lower_limit` FLOAT NULL DEFAULT NULL ,
-CHANGE `upper_limit` `upper_limit` FLOAT NULL DEFAULT NULL 
+CHANGE `upper_limit` `upper_limit` FLOAT NULL DEFAULT NULL ;
+
