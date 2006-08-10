ca599539c2bcb6463c984081facf8c6559af0637
Julien Mathis <jmathis@merethis.com>
2006-08-10 14:24:30 +0000

    add optgen Field for graph preferencies
    git-svn-id: http://svn.centreon.com/oreon-dev@345 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.3.1.sql
+++ www/install/sql/UpdateDB-1.3.1.sql
@@ -5,7 +5,7 @@ CHANGE `upper_limit` `upper_limit` FLOAT NULL DEFAULT NULL;
 
 -- 10/08/2006
 
-ALTER TABLE `general_opt` ADD `graph_preferencies` INT NULL AFTER `perfparse_installed` ;
+ALTER TABLE `general_opt` ADD `graph_preferencies` INT '0' AFTER `perfparse_installed` ;
 
 UPDATE `topology` SET `topology_url` = './include/views/graphs/graphSummary/graphSummary.php' WHERE `topology_page` = 402 LIMIT 1 ;
 UPDATE `topology` SET `topology_url` = './include/views/graphs/graphSummary/graphSummary.php' WHERE `topology_page` = 4 LIMIT 1 ;
