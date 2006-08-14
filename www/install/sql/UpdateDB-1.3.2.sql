4b49e9263ab7a390ccfb58b1d9a9d69169ffb2d7
wistof <wistof@free.fr>
2006-08-14 15:10:15 +0000

    - add debug_path field in general_opt
    git-svn-id: http://svn.centreon.com/oreon-dev@377 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.3.2.sql
+++ www/install/sql/UpdateDB-1.3.2.sql
@@ -1,3 +1,4 @@
 -- 13 08 06
-ALTER TABLE  `general_opt` ADD `debug_auth` enum('0','1') default NULL AFTER `ldap_auth_enable`;
+ALTER TABLE  `general_opt` ADD `debug_path` VARCHAR( 255 ) NULL AFTER `ldap_auth_enable`;
+ALTER TABLE  `general_opt` ADD `debug_auth` enum('0','1') default NULL AFTER `debug_path`;
 ALTER TABLE  `general_opt` ADD `debug_nagios_import` enum('0','1') default NULL AFTER `debug_auth`;
