457bb70b15322a5bdcffdcaf4a7733685429afd0
Romain Le Merlus <rlemerlus@merethis.com>
2006-08-21 13:01:54 +0000

    Get all the city name case, just set a number to multiple version
    git-svn-id: http://svn.centreon.com/oreon-dev@402 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/views/localisation/countries/def/load_cities.php
+++ www/include/views/localisation/countries/def/load_cities.php
@@ -1,9 +1,8 @@
 <?
 
-$local_file = "fr.txt";
-$country_id = "1";
-//$absPath_to_OreonOconf = $path = $oreon->optGen["oreon_web_path"]."oreon.conf.php";
-$absPath_to_OreonOconf = "/usr/local/oreon/www/oreon.conf.php";
+$local_file = "";
+$country_id = "";
+$absPath_to_OreonOconf = "../../../../../oreon.conf.php";
 
 function microtime_float() {
   list($usec, $sec) = explode(" ", microtime());
@@ -38,25 +37,32 @@ if (PEAR::isError($db)) {
 
 $db->setFetchMode(DB_FETCHMODE_ASSOC);
 
-$insert = false;
-$current = array();
-$cmp = array();
+$tab = array();
+$last_elem = NULL;
+$i = 2;
 if ($log_file)	{
-	while ($str = fgets($log_file))	{
-	    $tab = split("\t", $str);
-	   /* foreach ($tab as $key=>$value)	{
-	    	print "key :".$key." value:".$value."\n";
-	    }*/
-	    if ($tab[23] != "FULL_NAME_ND")	{
-			$date = explode("-", $tab[24]);
+	for ($cpt = 0; $str = fgets($log_file); $cpt++)	{
+	   	$tab = split("\t", $str);
+	    if ($cpt && $tab[9] == "P")	{
+	    	$lat = $tab[3];
+	    	$long = $tab[4];
+	    	$tab = array_reverse($tab);			
+			$date = explode("-", $tab[0]);
 			$date = mktime(0, 0, 0, $date[1], $date[2], $date[0]);
-			//print "INSERT INTO `view_city` ( `city_id` , `country_id` , `city_name` , `city_zipcode` , `city_lat` , `city_long`, `city_date`) VALUES ('', '".$country_id."', '".$tab[23]."', NULL , '".$tab[3]."', '".$tab[4]."', '".$date."')\n";
-			$db->query("INSERT INTO `view_city` ( `city_id` , `country_id` , `city_name` , `city_zipcode` , `city_lat` , `city_long`, `city_date`) VALUES ('', '".$country_id."', '".$tab[23]."', NULL , '".$tab[3]."', '".$tab[4]."', '".$date."')");
+			if ($tab[1] == $last_elem)	{
+				$tab[1] = $tab[1]." - ".$i;
+				$i++;
+			}
+			else	{
+				$last_elem = $tab[1];
+				$i = 2;
+			}
+			$db->query("INSERT INTO `view_city` ( `city_id` , `country_id` , `city_name` , `city_zipcode` , `city_lat` , `city_long`, `city_date`) VALUES ('', '".$country_id."', '".$tab[1]."', NULL , '".$lat."', '".$long."', '".$date."')");
 		}
+		unset($str);
 	}
 }
 $time_end = microtime_float();
 $now = $time_end - $time_start;
 print round($now,3) . "secondes";
-
 ?>
