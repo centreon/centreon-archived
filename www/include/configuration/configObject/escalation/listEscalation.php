006e826fae5069bccdcf30f5a1246a0f0b012066
Nicolas Cordier <ncordier@merethis.com>
2006-08-28 14:56:04 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@441 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/escalation/listEscalation.php
+++ www/include/configuration/configObject/escalation/listEscalation.php
@@ -93,7 +93,7 @@ $pagination = "maxViewConfiguration";
 		$moptions .= "<a href='oreon.php?p=".$p."&esc_id=".$esc['esc_id']."&o=c&search=".$search."&list=".$list."'><img src='img/icones/16x16/document_edit.gif' border='0' alt='".$lang['modify']."'></a>&nbsp;&nbsp;";
 		$moptions .= "<a href='oreon.php?p=".$p."&esc_id=".$esc['esc_id']."&o=d&select[".$esc['esc_id']."]=1&num=".$num."&limit=".$limit."&search=".$search."&list=".$list."' onclick=\"return confirm('".$lang['confirm_removing']."')\"><img src='img/icones/16x16/delete.gif' border='0' alt='".$lang['delete']."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-		$moptions .= "<select style='width:35; margin-bottom: 3px;' name='dupNbr[".$esc['esc_id']."]'><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>10</option></select>";
+		$moptions .= "<select style='margin-bottom: 3px;' name='dupNbr[".$esc['esc_id']."]'><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>10</option></select>";
 		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$esc["esc_name"],
