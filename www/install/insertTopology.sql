fe52d6a24254acc2d60e3d3e2e8414fddfdf19b9
Julien Mathis <jmathis@merethis.com>
2006-09-07 14:36:22 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@498 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/insertTopology.sql
+++ www/install/insertTopology.sql
@@ -101,6 +101,10 @@ INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topol
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'm_commandNotif', './img/icones/16x16/exchange.gif', '608', '60802', '20', NULL , './include/configuration/configObject/command/command.php', '&type=1', '0', '0', '1');
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'm_commandMisc', './img/icones/16x16/exchange.gif', '608', '60803', '30', NULL , './include/configuration/configObject/command/command.php', '&type=3', '0', '0', '1');
 
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`) VALUES ('', 'm_plugins', './img/icones/16x16/window_gear.gif' , '608', '60804', '40', NULL , './include/configuration/configPlugins/Plugins.php', NULL , '0', '0', '1');
+
+
+
 --
 -- Options
 --
