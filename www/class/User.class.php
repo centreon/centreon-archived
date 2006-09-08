fbf3adb8e22885cd16c8b44e393c56a553a2bb5e
Julien Mathis <jmathis@merethis.com>
2006-09-08 15:56:28 +0000

    modify LCA acces
    git-svn-id: http://svn.centreon.com/oreon-dev@530 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/User.class.php
+++ www/class/User.class.php
@@ -59,50 +59,24 @@ class User	{
   }
   
   function createLCA($pearDB = NULL)	{
-  	if(!$pearDB)
+  	$have_an_lca = 0;
+  	$num = 0;
+   	if(!$pearDB)
   		return; 
-	if ($this->admin)	{
-		$res3 =& $pearDB->query("SELECT DISTINCT host_id, host_name FROM host");
-		while ($res3->fetchInto($host))
-			$this->lcaHost[$host["host_id"]] = $host["host_name"];
-	 	$res3 =& $pearDB->query("SELECT DISTINCT hg_id, hg_name FROM hostgroup");	
-		while ($res3->fetchInto($hostGroup))
-			$this->lcaHostGroup[$hostGroup["hg_id"]] = $hostGroup["hg_name"];
-		$res3 =& $pearDB->query("SELECT sg_id, sg_name FROM servicegroup");	
-		while ($res3->fetchInto($serviceGroup))
-			$this->lcaServiceGroup[$serviceGroup["sg_id"]] = $serviceGroup["sg_name"];
-		$res3 =& $pearDB->query("SELECT topology_id FROM topology");	
+  	if ($this->admin){
+	  	$res3 =& $pearDB->query("SELECT topology_id FROM topology");	
 		while ($res3->fetchInto($topo))
 			$this->lcaTopo[$topo["topology_id"]] = $topo["topology_id"];			
 		unset($res3);
-	}
-	else	{
-		$have_an_lca = false;
-		$res1 =& $pearDB->query("SELECT contactgroup_cg_id FROM contactgroup_contact_relation WHERE contact_contact_id = '".$this->user_id."'");
-		if ($res1->numRows())	{
+  	} else {
+  		$res1 =& $pearDB->query("SELECT contactgroup_cg_id FROM contactgroup_contact_relation WHERE contact_contact_id = '".$this->user_id."'");
+		if ($num = $res1->numRows())	{
 			while($res1->fetchInto($contactGroup))	{
 			 	$res2 =& $pearDB->query("SELECT lca.lca_id, lca.lca_hg_childs FROM lca_define_contactgroup_relation ldcgr, lca_define lca WHERE ldcgr.contactgroup_cg_id = '".$contactGroup["contactgroup_cg_id"]."' AND ldcgr.lca_define_lca_id = lca.lca_id AND lca.lca_activate = '1'");	
 				 if ($res2->numRows())	{
+					$have_an_lca = 1;
 					while ($res2->fetchInto($lca))	{
-						$have_an_lca = true;
-					 	$res3 =& $pearDB->query("SELECT DISTINCT host_id, host_name FROM host, lca_define_host_relation ldr WHERE lca_define_lca_id = '".$lca["lca_id"]."' AND host_id = ldr.host_host_id");
-						while ($res3->fetchInto($host))
-							$this->lcaHost[$host["host_id"]] = $host["host_name"];
-					 	$res3 =& $pearDB->query("SELECT DISTINCT hg_id, hg_name FROM hostgroup, lca_define_hostgroup_relation WHERE lca_define_lca_id = '".$lca["lca_id"]."' AND hg_id = hostgroup_hg_id");	
-						while ($res3->fetchInto($hostGroup))	{
-							$this->lcaHostGroup[$hostGroup["hg_id"]] = $hostGroup["hg_name"];
-							# Apply the LCA to hosts contains in
-							if ($lca["lca_hg_childs"])	{
-								$res4 =& $pearDB->query("SELECT h.host_name, hgr.host_host_id FROM hostgroup_relation hgr, host h WHERE hgr.hostgroup_hg_id = '".$hostGroup["hg_id"]."' AND h.host_id = hgr.host_host_id");	
-								while ($res4->fetchInto($host))	
-									$this->lcaHost[$host["host_host_id"]] = $host["host_name"];
-							}
-						}
-					 	$res3 =& $pearDB->query("SELECT sg_id, sg_name FROM servicegroup, lca_define_servicegroup_relation WHERE lca_define_lca_id = '".$lca["lca_id"]."' AND sg_id = servicegroup_sg_id");	
-						while ($res3->fetchInto($serviceGroup))
-							$this->lcaServiceGroup[$serviceGroup["sg_id"]] = $serviceGroup["sg_name"];
-						unset($res3);
-					 	$res3 =& $pearDB->query("SELECT topology_topology_id FROM lca_define_topology_relation WHERE lca_define_lca_id = '".$lca["lca_id"]."'");	
+  						$res3 =& $pearDB->query("SELECT topology_topology_id FROM lca_define_topology_relation WHERE lca_define_lca_id = '".$lca["lca_id"]."'");	
 						while ($res3->fetchInto($topo))
 							$this->lcaTopo[$topo["topology_topology_id"]] = $topo["topology_topology_id"];
 						unset($res3);
@@ -110,57 +84,18 @@ class User	{
 				}
 			}
 		}
-		if (!$res1->numRows() || !$have_an_lca) {
-			$res3 =& $pearDB->query("SELECT DISTINCT host_id, host_name FROM host");	
-			while ($res3->fetchInto($host))
-				$this->lcaHost[$host["host_id"]] = $host["host_name"];
-		 	$res3 =& $pearDB->query("SELECT DISTINCT hg_id, hg_name FROM hostgroup");	
-			while ($res3->fetchInto($hostGroup))
-				$this->lcaHostGroup[$hostGroup["hg_id"]] = $hostGroup["hg_name"];
-			$res3 =& $pearDB->query("SELECT sg_id, sg_name FROM servicegroup");	
-			while ($res3->fetchInto($serviceGroup))
-				$this->lcaServiceGroup[$serviceGroup["sg_id"]] = $serviceGroup["sg_name"];
-			$res3 =& $pearDB->query("SELECT topology_id FROM topology");	
-			while ($res3->fetchInto($topo))
-				$this->lcaTopo[$topo["topology_id"]] = $topo["topology_id"];			
-			unset($res3);			
-		}
-		unset($res1);
+  	}
+  	if (!$num || !$have_an_lca) {
+		$res3 =& $pearDB->query("SELECT topology_id FROM topology");	
+		while ($res3->fetchInto($topo))
+			$this->lcaTopo[$topo["topology_id"]] = $topo["topology_id"];			
+		unset($res3);			
 	}
-	
-   	# Have the LCA in both format : String or Array
-   	
-  	$this->lcaHStr = NULL;
-  	$this->lcaHStrName = NULL;
-  	foreach ($this->lcaHost as $key=>$value)	{
-  		$this->lcaHStr ? $this->lcaHStr .= ", ".$key : $this->lcaHStr = $key;
-  		$this->lcaHStrName ? $this->lcaHStrName .= ", ".$value : $this->lcaHStrName = $value;
-  	}
-  	if (!$this->lcaHStr) $this->lcaHStr = '\'\'';
-  	if (!$this->lcaHStrName) $this->lcaHStrName = '\'\'';
- 
-  	$this->lcaHGStr = NULL;
-  	$this->lcaHGStrName = NULL;
-  	foreach ($this->lcaHostGroup as $key=>$value)	{
-  		$this->lcaHGStr ? $this->lcaHGStr .= ", ".$key : $this->lcaHGStr = $key;
-  		$this->lcaHGStrName ? $this->lcaHGStrName .= ", ".$value : $this->lcaHGStrName = $value;
-  	}
-  	if (!$this->lcaHGStr) $this->lcaHGStr = '\'\'';
-  	if (!$this->lcaHGStrName) $this->lcaHGStrName = '\'\'';
-
- 	$this->lcaSGStr = NULL;
- 	$this->lcaSGStrName = NULL;
-  	foreach ($this->lcaServiceGroup as $key=>$value)	{
-  		$this->lcaSGStr ? $this->lcaSGStr .= ", ".$key : $this->lcaSGStr = $key;
-  		$this->lcaSGStrName ? $this->lcaSGStrName .= ", ".$value : $this->lcaSGStrName = $value;
-  	}
-  	if (!$this->lcaSGStr) $this->lcaSGStr = '\'\'';
-  	if (!$this->lcaSGStrName) $this->lcaSGStrName = '\'\'';
-  	
   	$this->lcaTStr = NULL;
   	foreach ($this->lcaTopo as $tmp)
   		$this->lcaTStr ? $this->lcaTStr .= ", ".$tmp : $this->lcaTStr = $tmp;
   	if (!$this->lcaTStr) $this->lcaTStr = '\'\'';
+  	
   }
   
   // Get
