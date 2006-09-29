f164941ef82c831971b507051115ef74e61605f6
Cedrick Facon <cfacon@merethis.com>
2006-09-29 09:09:18 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@641 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/reporting/lang/en.php
@@ -0,0 +1,71 @@
+<?
+# MailDB
+
+$lang["mailDB_list"] = "List";
+$lang['mailDB_add'] = "Add a Contact";
+$lang['mailDB_change'] = "Modify a Contact";
+$lang['mailDB_view'] = "View a Contact";
+$lang['mailDB_infos'] = "General Informations";
+$lang["mailDB_mail"] = "Email";
+$lang["mailDB_mailTxt"] = "1 to n mails, separated by : ' ',;-";
+$lang["mailDB_diffList"] = "Diffusion List";
+$lang["mailDB_receiptType"] = "Receipt type";
+$lang["mailDB_htmlType"] = "HTML";
+$lang["mailDB_textType"] = "TEXTE";
+
+# Diffusion List
+
+$lang["diffList"] = "Diffusion List";
+$lang["diffListNbrMail"] = "Mail number";
+$lang['list_add'] = "Add a Diffusion List";
+$lang['list_change'] = "Modify a Diffusion List";
+$lang['list_view'] = "View a Diffusion List";
+$lang['list_infos'] = "General Informations";
+$lang["list_name"] = "List Name";
+$lang["list_description"] = "List Description";
+$lang["list_period"] = "Sending Period";
+$lang["list_mails"] = "Contacts linked";
+$lang["list_oreonMails"] = "Oreon Contacts linked";
+
+# Message
+
+$lang["m_send"] = "Send";
+
+# Dashboard
+$lang["m_view"] = "View";
+$lang["m_start"] = "Begin date";
+$lang["m_end"] = "End date";
+$lang["m_predefinedPeriod"] = "Predefined:";
+$lang["m_customizedPeriod"] = "Customized";
+$lang["m_selectPeriodTitle"] = "Period Selection";
+$lang["m_hostResumeTitle"] = "Host state";
+$lang["m_serviceResumeTitle"] = "Host state";
+$lang["m_hostLogTitle"] = "service today log";
+$lang["m_serviceLogTitle"] = "service today log";
+$lang["m_hostSvcAssocied"] = "State Breakdowns For Host Services";
+
+
+$lang["m_serviceTilte"] = "Service";
+$lang["m_OKTitle"] = "OK";
+$lang["m_WarningTitle"] = "Warning";
+$lang["m_UnknownTitle"] = "Unknown";
+$lang["m_CriticalTitle"] = "Critical";
+$lang["m_PendingTitle"] = "Pending";
+$lang["m_UpTitle"] = "Up";
+$lang["m_DownTitle"] = "Down";
+$lang["m_UnreachableTitle"] = "Unreachable";
+
+$lang["m_StateTitle"] = "State";
+$lang["m_TimeTitle"] = "Time";
+$lang["m_TimeTotalTitle"] = "Total Time";
+$lang["m_KnownTimeTitle"] = "Known Time";
+		
+$lang["m_DateTitle"] = "Date";
+$lang["m_EventTitle"] = "Event";
+$lang["m_HostTitle"] = "Host";
+$lang["m_InformationsTitle"] = "Info";
+
+$lang["m_to"] = " to ";
+$lang["m_duration"] = "Duration ";
+
+?>
