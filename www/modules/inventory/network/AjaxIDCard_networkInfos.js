5c1f6ca6670ab0396fe3f757c42a419a839c3d28
Cedrick Facon <cfacon@merethis.com>
2006-10-06 15:22:20 +0000

    ajax idcard network
    git-svn-id: http://svn.centreon.com/oreon-dev@679 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/modules/inventory/network/AjaxIDCard_networkInfos.js
@@ -0,0 +1,317 @@
+/**
+Oreon is developped with Apache Licence 2.0 :
+http://www.apache.org/licenses/LICENSE-2.0.txt
+Developped by : Cedrick Facon
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+// JavaScript Document
+/*
+function init(){
+		for (var i = 2; document.getElementById('tab'+i); i++) {
+			document.getElementById('tab'+i).style.display='none';
+		}
+	}
+*/
+
+function mreload(id) {
+	var _tableforajax = document.getElementById('tab' + id);
+	var _tableAjax = null;
+    var childrenNumber = _tableforajax.childNodes.length;
+
+    for (var i = 0; i < childrenNumber; i++) 
+    {
+		var element = _tableforajax.childNodes[i];
+      	var elementName = element.nodeName.toLowerCase();
+		if (elementName == 'table')
+		{
+		    var childrenNumbertable = element.childNodes.length;
+		    for (var j = 0; j < childrenNumbertable; j++) 
+		    {
+				var elementtable = element.childNodes[j];
+		  		var elementNametable = elementtable.nodeName.toLowerCase();
+		  		
+				if (elementNametable == 'tbody')
+				{
+					_tableAjax = elementtable;
+				}
+			}
+		}
+	}	
+	_tableAjax.innerHTML = "";
+	
+	if (id == 1){
+	}
+	else if (id == 2){
+			MyLoading('Loading...');
+			_f2.status.value = 1;
+			get_network(_host_id);
+	}
+	else if (id == 3){
+			MyLoading('Loading...');
+			_f3.status.value = 1;
+			get_vlan(_host_id);
+	}
+	else if (id == 4){
+	}	
+	else if (id == 5){
+	}	
+	else if (id == 6){
+	}
+	else
+	{
+		;
+	}
+}
+
+function montre(id) {
+
+	_formBasic=document.getElementById('infosBasic');		       
+	_host_id=_formBasic.host_id.value;
+
+		for (var i = 1; document.getElementById('c'+i); i++) {
+				document.getElementById('c'+i).className='b';
+		}
+		document.getElementById('c'+id).className='a';
+
+		var d = document.getElementById('tab'+id);
+		var dreload = document.getElementById('mreload'+id);
+		for (var i = 1; document.getElementById('tab'+i); i++) {
+			document.getElementById('tab'+i).style.display='none';
+			document.getElementById('mreload'+i).style.display='none';
+		}
+	if (d) {
+	d.style.display='block';
+	}
+	if (dreload) {
+	dreload.style.display='block';
+	}
+
+	if (id == 1){
+	}
+	else if (id == 2){
+		_f2=document.getElementById('f2');
+		if(_f2.status.value == '0')
+		{
+			MyLoading('Loading...');
+			_f2.status.value = 1;
+			get_network(_host_id);
+		}
+	}	
+	else if (id == 3){
+		_f3=document.getElementById('f3');
+		if(_f3.status.value == '0')
+		{
+			MyLoading('Loading...');
+			_f3.status.value = 1;
+			get_vlan(_host_id);
+		}
+	}
+	else
+	{
+		;
+	}
+}
+
+function MyHiddenDiv() {
+	_divmsg = document.getElementById('msg');
+	_divmsg.className = "cachediv";
+}
+
+function MyIsLoading(_txt) {
+	_divmsg = document.getElementById('msg');
+	_divmsg.innerHTML = '';
+	_divmsg.className = "msg_isloading";
+	var _text = document.createTextNode(_txt);
+	_divmsg.appendChild(_text);
+	setTimeout('MyHiddenDiv()', '2000');
+}
+
+function MyLoading(_txt) {
+	_divmsg = document.getElementById('msg');
+	_divmsg.innerHTML = '';
+	
+	var _text = document.createTextNode(_txt);
+	_divmsg.className = "msg_loading";
+	_divmsg.appendChild(_text);
+}
+
+var xhrIDCard = null; 
+var _adrrsearchIDCard = "./modules/inventory/IDCard_server/infosNetworkXML.php" //l'adresse   interroger pour trouver les suggestions
+	 
+function getXhrIDCard(){
+	if(window.XMLHttpRequest) // Firefox et autres
+	   xhrIDCard = new XMLHttpRequest(); 
+	else if(window.ActiveXObject){ // Internet Explorer 
+	   try {
+                xhrIDCard = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                xhrIDCard = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+	}
+	else { // XMLHttpRequest non supportÃ¯Â¿Âœ par le navigateur 
+	   alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
+	   xhrIDCard = false; 
+	} 
+}
+function get_vlan(_host_id)
+{
+	
+}
+function get_network(_host_id)
+{
+	var _tableforajax = document.getElementById('tab6');
+	var _tableAjax = null;
+    var childrenNumber = _tableforajax.childNodes.length;
+
+    for (var i = 0; i < childrenNumber; i++) 
+    {
+		var element = _tableforajax.childNodes[i];
+      	var elementName = element.nodeName.toLowerCase();
+		if (elementName == 'table')
+		{
+		    var childrenNumbertable = element.childNodes.length;
+		    for (var j = 0; j < childrenNumbertable; j++) 
+		    {
+				var elementtable = element.childNodes[j];
+		  		var elementNametable = elementtable.nodeName.toLowerCase();
+		  		
+				if (elementNametable == 'tbody')
+				{
+					_tableAjax = elementtable;
+				}
+			}
+		}
+	}
+
+	getXhrIDCard()
+	// On defini ce qu'on va faire quand on aura la reponse
+	xhrIDCard.onreadystatechange = function()
+	{	
+		// On ne fait quelque chose que si on a tout recu et que le serveur est ok
+		if(xhrIDCard.readyState == 4 && xhrIDCard.status == 200 && xhrIDCard.responseXML)
+		{		
+			reponseIDCard = xhrIDCard.responseXML.documentElement;
+
+			var _networks = reponseIDCard.getElementsByTagName("network");
+
+
+			for (var i = 0 ; i < _networks.length ; i++) {
+				var _network = _networks[i];
+
+				var _interfaceName = _network.getElementsByTagName("interfaceName")[0].firstChild.nodeValue;
+				var _Status = _network.getElementsByTagName("Status")[0].firstChild.nodeValue;
+				var _PhysAddress = _network.getElementsByTagName("PhysAddress")[0].firstChild.nodeValue;
+				var _Type = _network.getElementsByTagName("Type")[0].firstChild.nodeValue;
+				var _Trafic = _network.getElementsByTagName("Trafic")[0].firstChild.nodeValue;
+				var _ipAdress = _network.getElementsByTagName("ipAddress")[0].firstChild.nodeValue;
+				var _Speed = _network.getElementsByTagName("Speed")[0].firstChild.nodeValue;
+				var _errorPaquet = _network.getElementsByTagName("errorPaquet")[0].firstChild.nodeValue;
+
+				var _interfaceName_label = 'interfaceName_label';
+				var _Status_label = 'Status_label';
+				var _PhysAddress_label = 'PhysAddress_label';
+				var _Type_label = 'Type_label';
+				var _Trafic_label = 'Trafic_label';
+				var _ipAdress_label = 'ipAdress_label';
+				var _Speed_label = 'Speed_label';
+				var _errorPaquet_label = 'errorPaquet';
+
+
+				var _classname = _network.getElementsByTagName("class")[0].firstChild.nodeValue;
+
+				var _ligne_title = document.createElement('tr');
+				
+				var _case_interfaceName = document.createElement('td');
+				var _text_interfaceName = document.createTextNode(_interfaceName_label + ': ' + _interfaceName);
+				_case_interfaceName.appendChild(_text_interfaceName);
+				
+				_case_interfaceName.colSpan = 2;
+
+				var _case_Status = document.createElement('td');
+				var _text_Status = document.createTextNode(_Status_label + ': ' + _Status);
+				_case_Status.appendChild(_text_Status);
+				_case_Status.colSpan = 2;
+				
+				_ligne_title.appendChild(_case_interfaceName);
+				_ligne_title.appendChild(_case_Status);
+				_ligne_title.className = "ListHeader";
+
+				var _ligne_1 = document.createElement('tr');
+				var _case_PhysAddress_label = document.createElement('td');
+				var _case_PhysAddress = document.createElement('td');
+				var _case_ipAdress_label = document.createElement('td');
+				var _case_ipAdress = document.createElement('td');
+				var _text_PhysAddress_label = document.createTextNode(_PhysAddress_label);
+				var _text_PhysAddress = document.createTextNode(_PhysAddress);
+				var _text_ipAdress_label = document.createTextNode(_ipAdress_label);
+				var _text_ipAdress = document.createTextNode(_ipAdress);
+				_case_PhysAddress_label.appendChild(_text_PhysAddress_label);	
+				_case_PhysAddress.appendChild(_text_PhysAddress);	
+				_case_ipAdress_label.appendChild(_text_ipAdress_label);	
+				_case_ipAdress.appendChild(_text_ipAdress);	
+				_ligne_1.appendChild(_case_PhysAddress_label);
+				_ligne_1.appendChild(_case_PhysAddress);
+				_ligne_1.appendChild(_case_ipAdress_label);
+				_ligne_1.appendChild(_case_ipAdress);
+				_ligne_1.className = _classname;
+
+				var _ligne_2 = document.createElement('tr');
+				var _case_Type_label = document.createElement('td');
+				var _case_Type = document.createElement('td');
+				var _case_Speed_label = document.createElement('td');
+				var _case_Speed = document.createElement('td');
+				var _text_Type_label = document.createTextNode(_Type_label);	
+				var _text_Type = document.createTextNode(_Type);	
+				var _text_Speed_label = document.createTextNode(_Speed_label);	
+				var _text_Speed = document.createTextNode(_Speed);	
+
+				_case_Type_label.appendChild(_text_Type_label);	
+				_case_Type.appendChild(_text_Type);	
+				_case_Speed_label.appendChild(_text_Speed_label);	
+				_case_Speed.appendChild(_text_Speed);	
+				_ligne_2.appendChild(_case_Type_label);
+				_ligne_2.appendChild(_case_Type);
+				_ligne_2.appendChild(_case_Speed_label);
+				_ligne_2.appendChild(_case_Speed);
+				_ligne_2.className = _classname;
+
+
+				var _ligne_3 = document.createElement('tr');
+				var _case_Trafic = document.createElement('td');
+				var _case_errorPaquet = document.createElement('td');
+				var _text_Trafic = document.createTextNode(_Trafic_label + ': ' + _Trafic);	
+				var _text_errorPaquet = document.createTextNode(_errorPaquet_label + ': ' + _errorPaquet);	
+				_case_Trafic.appendChild(_text_Trafic);
+				_case_errorPaquet.appendChild(_text_errorPaquet);
+
+				_case_Trafic.colSpan = 2;
+				_case_errorPaquet.colSpan = 2;
+
+				_ligne_3.appendChild(_case_Trafic);
+				_ligne_3.appendChild(_case_errorPaquet);
+				_ligne_3.className = _classname;
+
+				_tableAjax.appendChild(_ligne_title);
+				_tableAjax.appendChild(_ligne_1);
+				_tableAjax.appendChild(_ligne_2);
+				_tableAjax.appendChild(_ligne_3);
+			}
+		}
+//		MyIsLoading("network is loaded");
+		setTimeout('MyIsLoading("network is loaded")', '800');
+	}
+	xhrIDCard.open("POST",_adrrsearchIDCard,true);
+	xhrIDCard.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	xhrIDCard.send("type=6&host_id="+_host_id);
+}
---
+++ www/modules/inventory/network/listNetwork.php
@@ -0,0 +1,158 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Julien Mathis - Romain Le Merlus
+
+This unit, called  Oreon Inventory  is developped by Merethis company for Lafarge Group,
+under the direction of Jean Baptiste Sarrodie <jean-baptiste@sarrodie.org>
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+	if (!isset ($oreon))
+		exit ();
+	$pagination = "maxViewConfiguration";		
+
+	# LCA 
+	$lcaHostByName = getLcaHostByName($pearDB);
+	$lcaHostByID = getLcaHostByID($pearDB);
+	$lcaHoststr = getLCAHostStr($lcaHostByID["LcaHost"]);
+	$lcaHostGroupstr = getLCAHGStr($lcaHostByID["LcaHostGroup"]);
+	$isRestreint = HadUserLca($pearDB);
+
+	# set limit & num
+	$res =& $pearDB->query("SELECT maxViewConfiguration FROM general_opt LIMIT 1");
+	if (PEAR::isError($pearDB)) {
+		print "Mysql Error : ".$pearDB->getMessage();
+	}
+	$gopt = array_map("myDecode", $res->fetchRow());		
+
+	!isset ($_GET["limit"]) ? $limit = $gopt["maxViewConfiguration"] : $limit = $_GET["limit"];
+	!isset($_GET["num"]) ? $num = 0 : $num = $_GET["num"];
+	isset ($_GET["search"]) ? $search = $_GET["search"] : $search = NULL;
+
+	if ($search)
+	{
+		if ($oreon->user->admin || !$isRestreint)
+			$rq = "SELECT COUNT(*) FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				  " h.host_name LIKE '%".htmlentities($search, ENT_QUOTES)."%'  " .
+				  " AND ii.type_ressources";
+		else
+			$rq = "SELECT COUNT(*) FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				  " h.host_name LIKE '%".htmlentities($search, ENT_QUOTES)."%' AND h.host_id IN (".$lcaHostByID["LcaHost"].") " .
+				  " AND ii.type_ressources";
+	}
+	else
+	{
+		if ($oreon->user->admin || !$isRestreint)
+			$rq = "SELECT COUNT(*) FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+					" im.id = ii.type_ressources";
+		else
+			$rq = "SELECT COUNT(*) FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+					" h.host_id IN (".$lcaHostByID["LcaHost"].") AND im.id = ii.type_ressources";
+	}
+	$res =& $pearDB->query($rq);
+	$tmp = & $res->fetchRow();
+	$rows = $tmp["COUNT(*)"];
+
+	# start quickSearch form
+	include_once("./include/common/quickSearch.php");
+	# end quickSearch form
+
+	# Smarty template Init
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl($path, $tpl);
+
+	# start header menu
+	$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
+	$tpl->assign("headerMenu_name", $lang['name']);
+	$tpl->assign("headerMenu_desc", $lang['description']);
+	$tpl->assign("headerMenu_address", $lang['h_address']);
+	$tpl->assign("headerMenu_status", $lang['status']);
+	$tpl->assign("headerMenu_manu", $lang['s_manufacturer']);
+	# end header menu
+
+	#Host list
+	if ($search)
+	{
+		if ($oreon->user->admin || !$isRestreint)
+			$rq = "SELECT h.host_id, h.host_name, h.host_alias, h.host_address, h.host_activate, im.alias AS manu_alias FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				  " h.host_name LIKE '%".htmlentities($search, ENT_QUOTES)."%' AND h.host_id IN (".$lcaHostByID["LcaHost"].") " .
+				  " AND ii.type_ressources ORDER BY h.host_name LIMIT ".$num * $limit.", ".$limit;
+		else
+			$rq = "SELECT h.host_id, h.host_name, h.host_alias, h.host_address, h.host_activate, im.alias AS manu_alias FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				  " h.host_name LIKE '%".htmlentities($search, ENT_QUOTES)."%' AND h.host_id IN (".$lcaHostByID["LcaHost"].") " .
+				  " AND ii.type_ressources ORDER BY h.host_name LIMIT ".$num * $limit.", ".$limit;
+	}
+	else
+	{
+		if ($oreon->user->admin || !$isRestreint)
+		$rq = "SELECT h.host_id, h.host_name, h.host_alias, h.host_address, h.host_activate, im.alias AS manu_alias FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				" im.id = ii.type_ressources  " .
+				" ORDER BY h.host_name LIMIT ".$num * $limit.", ".$limit;
+		else
+		$rq = "SELECT h.host_id, h.host_name, h.host_alias, h.host_address, h.host_activate, im.alias AS manu_alias FROM host h, inventory_index ii, inventory_manufacturer im WHERE h.host_id = ii.host_id AND " .
+				" h.host_id IN (".$lcaHostByID["LcaHost"].") AND im.id = ii.type_ressources  " .
+				" ORDER BY h.host_name LIMIT ".$num * $limit.", ".$limit;
+	}
+	$res = & $pearDB->query($rq);
+	
+	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p);
+	#Different style between each lines
+	$style = "one";
+	#Fill a tab with a mutlidimensionnal Array we put in $tpl
+
+	$elemArr = array();
+	for ($i = 0; $res->fetchInto($host); $i++) {		
+		$selectedElements =& $form->addElement('checkbox', "select[".$host['host_id']."]");	
+
+		if (!$host["host_name"])
+			$host["host_name"] = getMyHostName($host["host_template_model_htm_id"]);
+		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+						"RowMenu_select"=>$selectedElements->toHtml(),
+						"RowMenu_name"=>$host["host_name"],
+						"RowMenu_link"=>"?p=".$p."&o=o&host_id=".$host['host_id']."&search=".$search,
+						"RowMenu_desc"=>$host["host_alias"],
+						"RowMenu_address"=>$host["host_address"],
+						"RowMenu_status"=>$host["host_activate"] ? $lang["enable"] : $lang["disable"],
+						"RowMenu_manu"=>$host["manu_alias"]);
+		$style != "two" ? $style = "two" : $style = "one";
+	}
+	$tpl->assign("elemArr", $elemArr);
+
+	
+	#form select host
+	$req = "SELECT id, alias FROM inventory_manufacturer ";
+	$res = & $pearDB->query($req);
+
+	$option = array(NULL=>$lang['s_none']);
+	for ($i=0;$res->fetchInto($host);$i++) 
+    	$option[$host['id']] = $host['alias'];
+
+	$form->addElement('select', 'select_manufacturer', $lang['s_manufacturer'], $option);
+
+
+	#
+	##Apply a template definition
+	#
+	
+	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+	$form->accept($renderer);	
+	$tpl->assign('p', $p);
+	$tpl->assign('form', $renderer->toArray());
+	$tpl->display("IDCard_network/listNetwork.ihtml");
+	
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl("./", $tpl);
+	$tpl->assign('lang', $lang);
+	$tpl->display("include/common/legend.ihtml");	
+?>
