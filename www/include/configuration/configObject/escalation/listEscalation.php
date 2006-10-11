fca2e08bbe0cd66b54350c55e25207fce4ed5a44
tmai <tmai>
2006-10-11 13:16:21 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@739 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/escalation/listEscalation.php
+++ www/include/configuration/configObject/escalation/listEscalation.php
@@ -123,9 +123,4 @@ For information : contact@oreon-project.org
 	$form->accept($renderer);	
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listEscalation.ihtml");
-
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");	
 ?>
