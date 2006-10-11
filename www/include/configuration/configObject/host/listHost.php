8cf5b9c8d7d0cc222b7cab4cdbd5eb5a5b292b21
tmai <tmai>
2006-10-11 13:17:24 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@741 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -131,8 +131,4 @@ For information : contact@oreon-project.org
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listHost.ihtml");
 
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");
-
+?>
