65f1dffefa6478a85130d14481e69c8963533f21
tmai <tmai>
2006-10-11 13:47:53 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@764 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/servicegroup/listServiceGroup.php
+++ www/include/configuration/configObject/servicegroup/listServiceGroup.php
@@ -122,8 +122,4 @@ For information : contact@oreon-project.org
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listServiceGroup.ihtml");
 
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");	
 ?>
