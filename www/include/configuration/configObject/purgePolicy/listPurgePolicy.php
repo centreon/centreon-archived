1cde1dc92b32a36cda7811ade18bb4e4b71fe666
tmai <tmai>
2006-10-11 13:40:33 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@755 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/purgePolicy/listPurgePolicy.php
+++ www/include/configuration/configObject/purgePolicy/listPurgePolicy.php
@@ -104,8 +104,4 @@ For information : contact@oreon-project.org
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listPurgePolicy.ihtml");
 
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");	
 ?>
