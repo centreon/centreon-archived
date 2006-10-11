279ec4d86d6124e40c4c20f17466cbc6429062a0
tmai <tmai>
2006-10-11 13:11:13 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@733 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/command/listCommand.php
+++ www/include/configuration/configObject/command/listCommand.php
@@ -112,9 +112,5 @@ For information : contact@oreon-project.org
 	$form->accept($renderer);	
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listCommand.ihtml");
-	
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");
+
 ?>
