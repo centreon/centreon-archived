de31216e2f13340f9177cac9b813e11f8af55f00
tmai <tmai>
2006-10-12 12:36:12 +0000

    update : legend
    git-svn-id: http://svn.centreon.com/oreon-dev@792 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/modules/inventory/IDCard_server/listServer.php
+++ www/modules/inventory/IDCard_server/listServer.php
@@ -146,8 +146,4 @@ For information : contact@oreon-project.org
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("IDCard_server/listServer.ihtml");
 	
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./", $tpl);
-	$tpl->assign('lang', $lang);
-	$tpl->display("include/common/legend.ihtml");	
 ?>
