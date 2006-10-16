9022b0612bb8e6bb0d139774acb5a77bdabfa7e1
Cedrick Facon <cfacon@merethis.com>
2006-10-16 15:50:09 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@841 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/meta_service/listMetaService.php
+++ www/include/configuration/configObject/meta_service/listMetaService.php
@@ -88,7 +88,9 @@ For information : contact@oreon-project.org
 		else
 			$moptions .= "<a href='oreon.php?p=".$p."&meta_id=".$ms['meta_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='".$lang['enable']."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-		$moptions .= "<select style='width:35; margin-bottom: 3px;' name='dupNbr[".$ms['meta_id']."]'></input>";
+		
+		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$ms['meta_id']."]'></input>";
+
 		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$ms["meta_name"],
