950e6062f721cb348c2302ceeb65a458517ffc97
Cedrick Facon <cfacon@merethis.com>
2006-10-17 12:43:25 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@867 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/LCA/define/listLCA.php
+++ www/include/options/LCA/define/listLCA.php
@@ -85,7 +85,8 @@ For information : contact@oreon-project.org
 		else
 			$moptions .= "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='".$lang['enable']."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-		$moptions .= "<select style='width:35; margin-bottom: 3px;' name='dupNbr[".$lca['lca_id']."]'></input>";
+		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$lca['lca_id']."]'></input>";
+
 		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$lca["lca_name"],
