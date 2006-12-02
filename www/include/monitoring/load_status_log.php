4b92668d4553705a9c3f4e6e95d9cf5f7d4562e5
Julien Mathis <jmathis@merethis.com>
2006-12-02 09:18:11 +0000

    $pearDB  ->  change return to display error and check mistake.....
    git-svn-id: http://svn.centreon.com/oreon-dev@1179 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/load_status_log.php
+++ www/include/monitoring/load_status_log.php
@@ -30,20 +30,22 @@ For information : contact@oreon-project.org
 	if (!isset($pearDB))
 		global $pearDB;
 	
-	$res1 =& $pearDB->query("SELECT contact_admin FROM contact, session WHERE contact.contact_id = session.user_id AND session_id = '$sid'");
-	$res1->fetchInto($user);
+	$DBRESULT1 =& $pearDB->query("SELECT contact_admin FROM contact, session WHERE contact.contact_id = session.user_id AND session_id = '$sid'");
+	if (PEAR::isError($DBRESULT1))
+		print "Mysql Error : ".$DBRESULT1->getMessage()."<br>";
+	$DBRESULT1->fetchInto($user);
 	$user_admin = $user["contact_admin"];
 	
-	
 	// Read 
-	$res1 =& $pearDB->query("SELECT nagios_version, problem_sort_order, problem_sort_type FROM general_opt");
-	$res1->fetchInto($general_opt);
+	$DBRESULT1 =& $pearDB->query("SELECT nagios_version, problem_sort_order, problem_sort_type FROM general_opt");
+	if (PEAR::isError($DBRESULT1))
+		print "Mysql Error : ".$DBRESULT1->getMessage()."<br>";
+	$DBRESULT1->fetchInto($general_opt);
 	$version = $general_opt["nagios_version"];
 	
 	# Init tab
 	$tab_status_svc = array("0" => "OK", "1" => "WARNING", "2" => "CRITICAL", "3" => "UNKNOWN", "4" => "PENDING");
 	$tab_status_host = array("0" => "UP", "1" => "DOWN", "2" => "UNREACHABLE");
-	
 	
 	// Stats
 	if (is_object($oreon)){
@@ -62,8 +64,10 @@ For information : contact@oreon-project.org
 	
 	# Read File
 	if ($version == 1 || $version == 2){
-		$res1 =& $pearDB->query("SELECT status_file FROM cfg_nagios WHERE nagios_activate = '1'");
-		$res1->fetchInto($nagios_cfg);
+		$DBRESULT1 =& $pearDB->query("SELECT status_file FROM cfg_nagios WHERE nagios_activate = '1'");
+		if (PEAR::isError($DBRESULT1))
+			print "Mysql Error : ".$DBRESULT1->getMessage()."<br>";
+		$DBRESULT1->fetchInto($nagios_cfg);
 		$file = $nagios_cfg["status_file"];
 	} else
 		$file = "/srv/nagios/var/status_oreon.log";
