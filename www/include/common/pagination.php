9b73fd3e750f42b9802c9310f069476fde296b77
Julien Mathis <jmathis@merethis.com>
2006-12-02 17:02:16 +0000

    tidy white space
    git-svn-id: http://svn.centreon.com/oreon-dev@1201 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/pagination.php
+++ www/include/common/pagination.php
@@ -16,88 +16,80 @@ been previously advised of the possibility of such damages.
 For information : contact@oreon-project.org
 */
 
-global $oreon;
-global $num;
-global $search;
-
-global $search_type_service;
-global $search_type_host;
-
-isset ($_GET["search_type_service"]) ? $search_type_service = $_GET["search_type_service"] : $search_type_service = NULL;
-isset ($_GET["search_type_host"]) ? $search_type_host = $_GET["search_type_host"] : $search_type_host = NULL;
-isset ($_GET["type"]) ? $type = $_GET["type"] : $stype = NULL;
-
-isset ($_GET["num"]) ? $num = $_GET["num"] : $num = 0;
-isset ($_GET["search"]) ? $search = $_GET["search"] : $search = NULL;
-isset ($_GET["o"]) ? $o = $_GET["o"] : $o = NULL;
-
-global $rows;
-global $limit;
-global $p;
-global $lang;
-global $limit;
-global $gopt;
-global $pagination;
-
-$url_var = "";
-$url_var .= "&search_type_service=" . $search_type_service;
-$url_var .= "&search_type_host=" . $search_type_host;
-
-if(isset($_GET["order"]))
-{
-	$url_var .= "&order=".$_GET["order"];
-	$order = $_GET["order"];
-}
-if(isset($_GET["sort_types"]))
-{
-	$url_var .= "&sort_types=".$_GET["sort_types"];
-	$sort_type = $_GET["sort_types"];
-}
+	global $oreon;
 
 	if (!isset($oreon))
 		exit();
+
+
+	global $num;
+	global $search;
+	
+	global $search_type_service;
+	global $search_type_host;
+	
+	isset ($_GET["search_type_service"]) ? $search_type_service = $_GET["search_type_service"] : $search_type_service = NULL;
+	isset ($_GET["search_type_host"]) ? $search_type_host = $_GET["search_type_host"] : $search_type_host = NULL;
+	isset ($_GET["type"]) ? $type = $_GET["type"] : $stype = NULL;
+	
+	isset ($_GET["num"]) ? $num = $_GET["num"] : $num = 0;
+	isset ($_GET["search"]) ? $search = $_GET["search"] : $search = NULL;
+	isset ($_GET["o"]) ? $o = $_GET["o"] : $o = NULL;
+	
+	global $rows;
+	global $limit;
+	global $p;
+	global $lang;
+	global $limit;
+	global $gopt;
+	global $pagination;
+
+	$url_var = "";
+	$url_var .= "&search_type_service=" . $search_type_service;
+	$url_var .= "&search_type_host=" . $search_type_host;
+	
+	if(isset($_GET["order"])){
+		$url_var .= "&order=".$_GET["order"];
+		$order = $_GET["order"];
+	}
+	if(isset($_GET["sort_types"])){
+		$url_var .= "&sort_types=".$_GET["sort_types"];
+		$sort_type = $_GET["sort_types"];
+	}
+
 
 	# Smarty template Init
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl, "./include/common/");
 
-
-
 	$pageArr = array();
 	$istart = 0;
 	for($i = 5, $istart = $num; $istart && $i > 0; $i--)
 		$istart--;		
-
-
 	for($i2 = 0, $iend = $num; ( $iend <  ($rows / $limit -1)) && ( $i2 < (5 + $i)); $i2++)
 		$iend++;
-
-	for ($i = $istart; $i <= $iend; $i++)
-	{
+	for ($i = $istart; $i <= $iend; $i++){
 		$pageArr[$i] = array("url_page"=>"./oreon.php?p=".$p."&num=$i&limit=".$limit."&search=".$search."&type=".$type."&o=" . $o . $url_var,
 							"label_page"=>"<b>".($i +1)."</b>",
 							"num"=> $i);
 	}
 	if($i > 1)							
-	$tpl->assign("pageArr", $pageArr);
+		$tpl->assign("pageArr", $pageArr);
 
 	$tpl->assign("num", $num);
 	$tpl->assign("previous", $lang["previous"]);
 	$tpl->assign("next", $lang["next"]);
 
 	if(($prev = $num - 1) >= 0)
-	$tpl->assign('pagePrev', ("./oreon.php?p=".$p."&num=$prev&limit=".$limit."&search=".$search."&type=".$type."&o=" . $o .$url_var));
+		$tpl->assign('pagePrev', ("./oreon.php?p=".$p."&num=$prev&limit=".$limit."&search=".$search."&type=".$type."&o=" . $o .$url_var));
 	if(($next = $num + 1) < ($rows/$limit))
-	$tpl->assign('pageNext', ("./oreon.php?p=".$p."&num=$next&limit=".$limit."&search=".$search."&type=".$type."&o=" . $o .$url_var));
-
+		$tpl->assign('pageNext', ("./oreon.php?p=".$p."&num=$next&limit=".$limit."&search=".$search."&type=".$type."&o=" . $o .$url_var));
 	if(($rows / $limit) > 0)
 		$tpl->assign('pageNumber', ($num +1)."/".ceil($rows / $limit));
 	else
 		$tpl->assign('pageNumber', ($num)."/".ceil($rows / $limit));
 
-
 	#Select field to change the number of row on the page
-
 
 	for ($i = 10; $i <= 100; $i = $i +10)
 		$select[$i]=$i;
@@ -105,12 +97,9 @@ if(isset($_GET["sort_types"]))
 	$select[$rows]=$rows;
 	ksort($select);
 
-
 ?>
-
 <SCRIPT LANGUAGE="JavaScript">
-function setL(_this)
-{
+function setL(_this){
 	var _l = document.getElementsByName('l');
 
 	document.forms['form'].elements['limit'].value = _this;
@@ -120,17 +109,13 @@ function setL(_this)
 
 }
 </SCRIPT>
-
 <?
 
 	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p."&search_type_service=" . $search_type_service."&search_type_host=" . $search_type_host);
-
 	$selLim =& $form->addElement('select', 'l', $lang['nbr_per_page'], $select, array("onChange" => "setL(this.value);  this.form.submit('')"));
 	$selLim->setSelected($limit);
-
-
+	
 	#Element we need when we reload the page
-
 	$form->addElement('hidden', 'p');
 	$form->addElement('hidden', 'search');
 	$form->addElement('hidden', 'num');
@@ -143,6 +128,7 @@ function setL(_this)
 	$form->setDefaults($tab);
 
 
+	# Init QuickForm
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$form->accept($renderer);
 	
@@ -152,22 +138,13 @@ function setL(_this)
 	$tpl->assign("status", $status);
 
 	$tpl->assign("limite", $limite);
-
-
 	$tpl->assign("begin", $num);
 	$tpl->assign("end", $limit);
 	$tpl->assign("lang", $lang);
 	$tpl->assign("order", $_GET["order"]);
 	$tab_order = array("sort_asc" => "sort_desc", "sort_desc" => "sort_asc"); 
-
 	$tpl->assign("tab_order", $tab_order);
-	
-	
 
 	$tpl->assign('form', $renderer->toArray());
-
-
-
 	$tpl->display("pagination.ihtml");
-
 ?>
