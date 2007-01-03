3f74958660ef498ea6551417297f04a36f43e294
Cedrick Facon <cfacon@merethis.com>
2007-01-03 14:04:17 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1292 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/UpdateDB-1.3.4.sql
@@ -0,0 +1,5 @@
+-- 03/01/2007 --- 
+
+ALTER TABLE `topology` ADD `topology_style_class` VARCHAR( 255 ) NULL ;
+ALTER TABLE `topology` ADD `topology_style_id` VARCHAR( 255 ) NULL ;
+ALTER TABLE `topology` ADD `topology_OnClick` VARCHAR( 255 ) NULL ;
