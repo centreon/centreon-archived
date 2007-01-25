1fbc6d9004b3bf208b84d1f733798fb88a26a308
Julien Mathis <jmathis@merethis.com>
2007-01-25 17:31:42 +0000

    keep search in memory
    git-svn-id: http://svn.centreon.com/oreon-dev@1426 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -22,10 +22,6 @@ For information : contact@oreon-project.org
 	$DBRESULT =& $pearDB->query("SELECT maxViewConfiguration FROM general_opt LIMIT 1");
 	$gopt = array_map("myDecode", $DBRESULT->fetchRow());
 	!isset ($_GET["limit"]) ? $limit = $gopt["maxViewConfiguration"] : $limit = $_GET["limit"];
-
-	//isset ($_GET["num"]) ? $num = $_GET["num"] : $num = 0;
-	print $num;
-	isset ($_GET["search"]) ? $search = $_GET["search"] : $search = NULL;
 	
 	if ($search) {
 		if ($oreon->user->admin || !$isRestreint)
