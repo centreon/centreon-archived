8e1dd7b1fa326ab26cb953dd2fa9e0c3968c50f8
Cedrick Facon <cfacon@merethis.com>
2007-02-13 16:55:14 +0000

    use dashboard parsing now
    git-svn-id: http://svn.centreon.com/oreon-dev@1504 6bcd3966-0018-0410-8128-fd23d134de7e

--- cron/ArchiveLogInDB.php
+++ cron/ArchiveLogInDB.php
@@ -20,20 +20,10 @@ For information : contact@oreon-project.org
 	/*
 	 * Set your path here
 	 */
-	$path_oreon = '/srv/oreon/';
-	$NagiosPathArchive = "/srv/nagios/var/archives";
+	$path_oreon = '/usr/local/oreon/';
+	$NagiosPathArchive = "/usr/local/nagios/var/archives";
 
-	function getLogData($time_event, $host, $service, $status, $output, $type){
-		global $lang;
-		$tab["time"] = date($lang["header_format"], $time_event);
-		$tab["timeb"] = $time_event;
-		$tab["host"] = $host;
-		$tab["service"] = $service;
-		$tab["status"] = $status;
-		$tab["output"] = $output;
-		$tab["type"] = $type;
-		return $tab ;
-	}
+
 
 	require_once 'DB.php';	
 	include_once($path_oreon . "/www/oreon.conf.php");
@@ -121,348 +111,171 @@ For information : contact@oreon-project.org
 	}
 	krsort($tableFile2);
 
-function trim_value(&$value)
-{
-   $value = trim($value);
-}
 
 
-
-function parseFile($file,$end_time){
-	$start_time = 0;
-	$log = NULL;
-	$matches = "";
-
-	if (file_exists($file) && !($log = fopen($file, "r")))
-		echo "pel_cant_open" . $file . "<br>";
-	$tab_log = array();	
-	$tab_svc_log = array();
-	$tablist = array();
-	$tab_host = array();
-	$tab_services = array();
-	$res1 = array();	
-
-	if ($log)
-		for ($a=0, $b= 0, $i = 0; $str = fgets($log); $i++){
-			if (preg_match("/^\[([0-9]*)\] (.+)/", $str, $matches)){				
-				$time_event = $matches[1];
-				$res = preg_split("/:/", $matches[2], 2);
-			if (isset($res[1]))
-				$res1 = preg_split("/;/", $res[1]);
-			$type = $res[0];
-				array_walk($res1, 'trim_value');
-
-
-			#
-			## find the log's start time
-			#				
-			if ($i == 0)// take start time
-			$start_time = $time_event;			
-
-
-
-			
-			if (!strncmp($type, "LOG ROTATION", 12))
-			{
-				if($res1[0] == "DAILY")
-					$start_time = $end_time - (24*60*60);
-				else
-					$start_time = $time_event;
-			}						
-			#
-			## host
-			#			
-			else if (!strncmp($type, "CURRENT HOST STATE", 18) || !strncmp($type, "INITIAL HOST STATE", 18)){
-				if($time_event > $start_time && !isset($tab_host[$res1[0]])){// util si un initial apparait au millieu d'une periode
-					$tab_host[$res1[0]] = array();
-					$tab_host[$res1[0]]["current_time"] = $start_time;
-					$tab_host[$res1[0]]["current_state"] = "UNDETERMINATED";// pending
-					$tab_host[$res1[0]]["timeUP"] = 0;
-					$tab_host[$res1[0]]["timeDOWN"] = 0;
-					$tab_host[$res1[0]]["timeUNREACHABLE"] = 0;
-					$tab_host[$res1[0]]["timeNONE"] = 0;
-					$tab_host[$res1[0]]["start_time"] = $start_time;
-					$tab_host[$res1[0]]["tab_svc_log"] = array();
-
-					if(!strncmp($tab_host[$res1[0]]["current_state"], "UP", 2))
-					$tab_host[$res1[0]]["timeUP"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					elseif(!strncmp($tab_host[$res1[0]]["current_state"], "DOWN", 4))
-					$tab_host[$res1[0]]["timeDOWN"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					elseif(!strncmp($tab_host[$res1[0]]["current_state"], "UNREACHABLE", 11))
-					$tab_host[$res1[0]]["timeUNREACHABLE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					else
-					$tab_host[$res1[0]]["timeNONE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);					
-					$tab_host[$res1[0]]["current_state"] = $res1[1];
-					$tab_host[$res1[0]]["current_time"] = $time_event; //save time
-				}
-				else if($time_event > $start_time && isset($tab_host[$res1[0]])) {
-					if(!strncmp($tab_host[$res1[0]]["current_state"], "UP", 2))
-					$tab_host[$res1[0]]["timeUP"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					elseif(!strncmp($tab_host[$res1[0]]["current_state"], "DOWN", 4))
-					$tab_host[$res1[0]]["timeDOWN"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					elseif(!strncmp($tab_host[$res1[0]]["current_state"], "UNREACHABLE", 11))
-					$tab_host[$res1[0]]["timeUNREACHABLE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-					else
-					$tab_host[$res1[0]]["timeNONE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);					
-					$tab_host[$res1[0]]["current_state"] = $res1[1];
-					$tab_host[$res1[0]]["current_time"] = $time_event; //save time					
-				}
-				else {
-					$tab_host[$res1[0]] = array();
-					$tab_host[$res1[0]]["current_time"] = $time_event;
-					$tab_host[$res1[0]]["current_state"] = $res1[1];
-					$tab_host[$res1[0]]["timeUP"] = 0;
-					$tab_host[$res1[0]]["timeDOWN"] = 0;
-					$tab_host[$res1[0]]["timeUNREACHABLE"] = 0;
-					$tab_host[$res1[0]]["timeNONE"] = 0;
-					$tab_host[$res1[0]]["start_time"] = $start_time;
-					$tab_host[$res1[0]]["tab_svc_log"] = array();
-				}
-			}
-			else if (!strncmp($type, "HOST ALERT", 10) )
-			{
-				if(!isset($tab_host[$res1[0]]))
-				{
-					$tab_host[$res1[0]] = array();
-					$tab_host[$res1[0]]["current_time"] = $start_time;
-					$tab_host[$res1[0]]["current_state"] = "NONE";
-					$tab_host[$res1[0]]["timeUP"] = 0;
-					$tab_host[$res1[0]]["timeDOWN"] = 0;
-					$tab_host[$res1[0]]["timeUNREACHABLE"] = 0;
-					$tab_host[$res1[0]]["timeNONE"] = 0;
-					$tab_host[$res1[0]]["start_time"] = $start_time;
-					$tab_host[$res1[0]]["tab_svc_log"] = array();					
-
-				}						
-				if(!strncmp($tab_host[$res1[0]]["current_state"], "UP", 2))
-				$tab_host[$res1[0]]["timeUP"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-				elseif(!strncmp($tab_host[$res1[0]]["current_state"], "DOWN", 4))
-				$tab_host[$res1[0]]["timeDOWN"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-				elseif(!strncmp($tab_host[$res1[0]]["current_state"], "UNREACHABLE", 11))
-				$tab_host[$res1[0]]["timeUNREACHABLE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-				else
-				$tab_host[$res1[0]]["timeNONE"] += ($time_event-$tab_host[$res1[0]]["current_time"]);
-				
-
-				$tab_host[$res1[0]]["current_state"] = $res1[1];
-				$tab_host[$res1[0]]["current_time"] = $time_event; //save time
-			}
-				
-			#
-			## services
-			#
-			else if (!strncmp($type, "CURRENT SERVICE STATE", 21) || !strncmp($type, "INITIAL SERVICE STATE", 21))
-			{
-				if($time_event > $start_time && !isset($tab_services[$res1[1]][$res1[0]])){// util si un initial apparait au millieu d'une periode
-					$tab_services[$res1[1]][$res1[0]] = array();
-					$tab_tmp = array();
-					$tab_tmp["current_state"] = "UNDETERMINATED";
-					$tab_tmp["current_time"] = $start_time;
-					$tab_tmp["timeOK"] = 0;
-					$tab_tmp["timeWARNING"] = 0;
-					$tab_tmp["timeUNKNOWN"] = 0;
-					$tab_tmp["timeCRITICAL"] = 0;
-					$tab_tmp["timeNONE"] = 0;
-					$tab_tmp["start_time"] = $start_time;
-					$tab_tmp["service_id"] = getMyServiceID($res1[1],getMyHostID($res1[0]));
-					$tab_services[$res1[1]][$res1[0]] = $tab_tmp;
-
-					$tab_tmp = array();
-					$tab_tmp = $tab_services[$res1[1]][$res1[0]];
-					if(!strncmp($tab_tmp["current_state"], "OK", 2))
-						$tab_tmp["timeOK"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "WARNING", 7))
-						$tab_tmp["timeWARNING"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "UNKNOWN", 7))
-						$tab_tmp["timeUNKNOWN"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "CRITICAL", 8))
-						$tab_tmp["timeCRITICAL"] += ($time_event-$tab_tmp["current_time"]);
-					else
-						$tab_tmp["timeNONE"] += ($time_event-$tab_tmp["current_time"]);
-					$tab_tmp["current_time"] = $time_event; //save time
-					$tab_tmp["current_state"] = $res1[2]; //save time
-					$tab_services[$res1[1]][$res1[0]] = $tab_tmp;
-				}
-				else if($time_event > $start_time && isset($tab_services[$res1[1]][$res1[0]])) {
-					$tab_tmp = array();
-					$tab_tmp = $tab_services[$res1[1]][$res1[0]];
-					if(!strncmp($tab_tmp["current_state"], "OK", 2))
-						$tab_tmp["timeOK"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "WARNING", 7))
-						$tab_tmp["timeWARNING"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "UNKNOWN", 7))
-						$tab_tmp["timeUNKNOWN"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "CRITICAL", 8))
-						$tab_tmp["timeCRITICAL"] += ($time_event-$tab_tmp["current_time"]);
-					else
-						$tab_tmp["timeNONE"] += ($time_event-$tab_tmp["current_time"]);
-					$tab_tmp["current_time"] = $time_event; //save time
-					$tab_tmp["current_state"] = $res1[2]; //save time
-					$tab_services[$res1[1]][$res1[0]] = $tab_tmp;
-				}
-				else {				
-					$tab_services[$res1[1]][$res1[0]] = array();
-					$tab_tmp = array();
-					$tab_tmp["current_state"] = $res1[2];
-					$tab_tmp["current_time"] = $time_event;
-					$tab_tmp["timeOK"] = 0;
-					$tab_tmp["timeWARNING"] = 0;
-					$tab_tmp["timeUNKNOWN"] = 0;
-					$tab_tmp["timeCRITICAL"] = 0;
-					$tab_tmp["timeNONE"] = 0;
-					$tab_tmp["start_time"] = $start_time;
-					$tab_tmp["service_id"] = getMyServiceID($res1[1],getMyHostID($res1[0]));
-					$tab_services[$res1[1]][$res1[0]] = $tab_tmp;
-				}
-			}
-			else if (!strncmp($type, "SERVICE ALERT", 13))
-			{
-				if(isset($tab_services[$res1[1]][$res1[0]]))
-				{
-					$tab_tmp = array();
-					$tab_tmp = $tab_services[$res1[1]][$res1[0]];
-					if(!strncmp($tab_tmp["current_state"], "OK", 2))
-						$tab_tmp["timeOK"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "WARNING", 7))
-						$tab_tmp["timeWARNING"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "UNKNOWN", 7))
-						$tab_tmp["timeUNKNOWN"] += ($time_event-$tab_tmp["current_time"]);
-					elseif(!strncmp($tab_tmp["current_state"], "CRITICAL", 8))
-						$tab_tmp["timeCRITICAL"] += ($time_event-$tab_tmp["current_time"]);
-					else
-						$tab_tmp["timeNONE"] += ($time_event-$tab_tmp["current_time"]);
-					$tab_tmp["current_time"] = $time_event; //save time
-					$tab_tmp["current_state"] = $res1[2]; //save time
-					$tab_services[$res1[1]][$res1[0]] = $tab_tmp;
-				}
-			}
-		}
-	}
-	
-	$tablist["time_start"] = $start_time;
-	$tablist["tab_host"] = $tab_host;
-	$tablist["tab_services"] = $tab_services;
-	
-	return($tablist);
-}
-
-
-foreach($tableFile2 as $key => $time)
-{
-	$tab = array();
-	$tab = parseFile($key,$time);
-
-	insert_file_name_in_db($key);
-	
-	$time_start = $tab["time_start"];
-	$tab_host = $tab["tab_host"];
-	$tab_services = $tab["tab_services"];
+	#
+	## Parsing file help function
+	#
 	
 	
-	foreach($tab_host as $host => $htab)
+	function insert_in_db($tab_hosts, $tab_services, $day_current_start, $day_current_end)
 	{
-		if (isset($host_list[trim($host)]))
-		{	
-			#
-			## last host alert
-			#	
-			if(!strncmp($htab["current_state"], "UP", 2))
-				$htab["timeUP"] += ($time-$htab["current_time"]) + 1;
-			elseif(!strncmp($htab["current_state"], "DOWN", 4))
-				$htab["timeDOWN"] += ($time-$htab["current_time"]) + 1;
-			elseif(!strncmp($htab["current_state"], "UNREACHABLE", 11))
-				$htab["timeUNREACHABLE"] += ($time-$htab["current_time"]) + 1;
-			else
-				$htab["timeNONE"] += ($time-$htab["current_time"]) + 1;
-			#
-			## insert in db the host time
-			#		
-			$host_id = $host_list[trim($host)];
-			$Upsc =$htab["timeUP"];
-			$UpUnsc =$htab["timeUP"];
-			$DOWNsc =$htab["timeDOWN"];
-			$DOWNUnsc =$htab["timeDOWN"];
-			$UNREACHABLEsc = $htab["timeUNREACHABLE"];
-			$UNREACHABLEUnsc = $htab["timeUNREACHABLE"];
-			$NONEsc = $htab["timeNONE"];
-			$NONEUnsc = $htab["timeNONE"];
+		global $host_list;
+		global $service_list;
+		global $pearDB;
+		/* pensez a verifier si day existe deja dans le cas ou les log sont melangé... */
 
-			$sql = "INSERT INTO `log_archive_host` ( `log_id` , `host_id` ," .
-					" `UPTimeScheduled` , `UPTimeUnScheduled` ," .
-					" `DOWNTimeScheduled` , `DOWNTimeUnScheduled` ," .
-					" `UNREACHABLETimeScheduled` , `UNREACHABLETimeUnScheduled` ," .
-					" `UNDETERMINATETimeScheduled` , `UNDETERMINATETimeUnScheduled` ," .
-					" `date_end`, `date_start` ) VALUES" .
-				" (NULL , '$host_id'," .
-				" '$Upsc', '$UpUnsc'," .
-				" '$DOWNsc', '$DOWNUnsc'," .
-				" '$UNREACHABLEsc', '$UNREACHABLEUnsc'," .
-				" '$NONEsc', '$NONEUnsc'," .
-				" '$time', '$time_start')";
 
-			$result = $pearDB->query($sql);
-			if (PEAR::isError($res)){
-			  die($res->getMessage());}
-			  
-		}
-	}
-	foreach($tab_services as $svc => $htabsvc)
-	{
-		if (isset($service_list[trim($svc)]))
-		foreach($htabsvc as $host => $htab)
+		#
+		## Hosts in db
+		#
+		if(is_array($tab_hosts))
+		foreach($tab_hosts as $host => $htab)
 		{
 			if (isset($host_list[trim($host)]))
 			{
-			#
-			## last service alert
-			#	
-			if(!strncmp($htab["current_state"], "OK", 2))
-				$htab["timeOK"] += ($time-$htab["current_time"]) + 1;
-			elseif(!strncmp($htab["current_state"], "WARNING", 4))
-				$htab["timeWARNING"] += ($time-$htab["current_time"]) + 1;
-			elseif(!strncmp($htab["current_state"], "UNKNOWN", 11))
-				$htab["timeUNKNOWN"] += ($time-$htab["current_time"]) + 1;
-			elseif(!strncmp($htab["current_state"], "CRITICAL", 11))
-				$htab["timeCRITICAL"] += ($time-$htab["current_time"]) + 1;
-			else
-				$htab["timeNONE"] += ($time-$htab["current_time"]) + 1;
+				#
+				## last host alert
+				#
+				if(!strncmp($htab["current_state"], "UP", 2))
+					$htab["timeUP"] += ($day_current_end - $htab["current_time"]);
+				elseif(!strncmp($htab["current_state"], "DOWN", 4))
+					$htab["timeDOWN"] += ($day_current_end - $htab["current_time"]);
+				elseif(!strncmp($htab["current_state"], "UNREACHABLE", 11))
+					$htab["timeUNREACHABLE"] += ($day_current_end - $htab["current_time"]);
+				else
+					$htab["timeNONE"] += ($day_current_end - $htab["current_time"]);
 
+				/*
+				echo "insert in db => ".date("d/m/Y",$day_current_start)."\n\n";	
+				print_r($htab);
+				echo "start:" . $day_current_start."\n";
+				echo "end  :" . $day_current_end."\n";
+				echo "up=".$htab["timeUP"]."\n";
+				*/
 
-			$host_id = $host_list[trim($host)];
-			
-			$service_id = $htab["service_id"];						
-			$OKsc =$htab["timeOK"];
-			$OKUnsc =$htab["timeOK"];
-			$WARNINGsc =$htab["timeWARNING"];
-			$WARNINGUnsc =$htab["timeWARNING"];
-			$UNKNOWNsc = $htab["timeUNKNOWN"];
-			$UNKNOWNUnsc = $htab["timeUNKNOWN"];
-			$CRITICALsc = $htab["timeCRITICAL"];
-			$CRITICALUnsc = $htab["timeCRITICAL"];
-			$NONEsc = $htab["timeNONE"];
-			$NONEUnsc = $htab["timeNONE"];
+				$sql = "SELECT * FROM `log_archive_host` WHERE `date_end` = " . $day_current_end . " AND `date_start` = " . $day_current_start; 
+				$res = $pearDB->query($sql);
+				if (PEAR::isError($res)){
+				  die($res->getMessage());}
+				if($res->numRows())
+				{
+				//echo $res->numRows() . "\n";
+				}
 
-			$sql = "INSERT INTO `log_archive_service` ( `log_id` , `host_id`, `service_id` ," .
-					" `OKTimeScheduled` , `OKTimeUnScheduled` ," .
-					" `WARNINGTimeScheduled` , `WARNINGTimeUnScheduled` ," .
-					" `UNKNOWNTimeScheduled` , `UNKNOWNTimeUnScheduled` ," .
-					" `CRITICALTimeScheduled` , `CRITICALTimeUnScheduled` ," .
-					"`UNDETERMINATETimeScheduled` ,`UNDETERMINATETimeUnScheduled` ," .
-					" `date_end`, `date_start` ) VALUES" .
-				" (NULL , '$host_id', '$service_id'," .
-				" '$OKsc', '$OKUnsc'," .
-				" '$WARNINGsc', '$WARNINGUnsc'," .
-				" '$UNKNOWNsc', '$UNKNOWNUnsc'," .
-				" '$CRITICALsc', '$CRITICALUnsc'," .
-				" '$NONEsc', '$NONEUnsc'," .
-				" '$time', '$time_start')";
-
-			$result = $pearDB->query($sql);
-			if (PEAR::isError($res)){
-			  die($res->getMessage());}
-			}
-		}		
-	}	
-}
+				#
+				## insert in db the host time
+				#		
+				$host_id = $host_list[trim($host)];
+				$Upsc =$htab["timeUP"];
+				$UpUnsc =$htab["timeUP"];
+				$DOWNsc =$htab["timeDOWN"];
+				$DOWNUnsc =$htab["timeDOWN"];
+				$UNREACHABLEsc = $htab["timeUNREACHABLE"];
+				$UNREACHABLEUnsc = $htab["timeUNREACHABLE"];
+				$NONEsc = $htab["timeNONE"];
+				$NONEUnsc = $htab["timeNONE"];
 	
+				$sql = "INSERT INTO `log_archive_host` ( `log_id` , `host_id` ," .
+						" `UPTimeScheduled` , `UPTimeUnScheduled` ," .
+						" `DOWNTimeScheduled` , `DOWNTimeUnScheduled` ," .
+						" `UNREACHABLETimeScheduled` , `UNREACHABLETimeUnScheduled` ," .
+						" `UNDETERMINATETimeScheduled` , `UNDETERMINATETimeUnScheduled` ," .
+						" `date_end`, `date_start` ) VALUES" .
+					" (NULL , '$host_id'," .
+					" '$Upsc', '$UpUnsc'," .
+					" '$DOWNsc', '$DOWNUnsc'," .
+					" '$UNREACHABLEsc', '$UNREACHABLEUnsc'," .
+					" '$NONEsc', '$NONEUnsc'," .
+					" '$day_current_end', '$day_current_start')";
+	
+				$res = $pearDB->query($sql);
+				if (PEAR::isError($res)){
+				  die($res->getMessage());}
+				  
+			}
+		}
+
+		#
+		## Services in db
+		#
+		if(is_array($tab_services))
+		foreach($tab_services as $svc => $htabsvc)
+		{
+			if (isset($service_list[trim($svc)]))
+			foreach($htabsvc as $host => $htab)
+			{
+				if (isset($host_list[trim($host)]))
+				{
+					#
+					## last service alert
+					#	
+					if(!strncmp($htab["current_state"], "OK", 2))
+						$htab["timeOK"] += ($day_current_end-$htab["current_time"]) + 1;
+					elseif(!strncmp($htab["current_state"], "WARNING", 4))
+						$htab["timeWARNING"] += ($day_current_end-$htab["current_time"]) + 1;
+					elseif(!strncmp($htab["current_state"], "UNKNOWN", 11))
+						$htab["timeUNKNOWN"] += ($day_current_end-$htab["current_time"]) + 1;
+					elseif(!strncmp($htab["current_state"], "CRITICAL", 11))
+						$htab["timeCRITICAL"] += ($day_current_end-$htab["current_time"]) + 1;
+					else
+						$htab["timeNONE"] += ($day_current_end-$htab["current_time"]) + 1; // ?? calcul des none et autre pending
+
+
+		if($htab["timeOK"] < 0){
+			//echo "-->negative\n";
+			exit();
+		}
+		
+					$host_id = $host_list[trim($host)];
+					
+					$service_id = $htab["service_id"];						
+					$OKsc =$htab["timeOK"];
+					$OKUnsc =$htab["timeOK"];
+					$WARNINGsc =$htab["timeWARNING"];
+					$WARNINGUnsc =$htab["timeWARNING"];
+					$UNKNOWNsc = $htab["timeUNKNOWN"];
+					$UNKNOWNUnsc = $htab["timeUNKNOWN"];
+					$CRITICALsc = $htab["timeCRITICAL"];
+					$CRITICALUnsc = $htab["timeCRITICAL"];
+					$NONEsc = $htab["timeNONE"];
+					$NONEUnsc = $htab["timeNONE"];
+		
+					$sql = "INSERT INTO `log_archive_service` ( `log_id` , `host_id`, `service_id` ," .
+							" `OKTimeScheduled` , `OKTimeUnScheduled` ," .
+							" `WARNINGTimeScheduled` , `WARNINGTimeUnScheduled` ," .
+							" `UNKNOWNTimeScheduled` , `UNKNOWNTimeUnScheduled` ," .
+							" `CRITICALTimeScheduled` , `CRITICALTimeUnScheduled` ," .
+							"`UNDETERMINATETimeScheduled` ,`UNDETERMINATETimeUnScheduled` ," .
+							" `date_end`, `date_start` ) VALUES" .
+						" (NULL , '$host_id', '$service_id'," .
+						" '$OKsc', '$OKUnsc'," .
+						" '$WARNINGsc', '$WARNINGUnsc'," .
+						" '$UNKNOWNsc', '$UNKNOWNUnsc'," .
+						" '$CRITICALsc', '$CRITICALUnsc'," .
+						" '$NONEsc', '$NONEUnsc'," .
+						" '$day_current_end', '$day_current_start')";
+		
+					$result = $pearDB->query($sql);
+					if (PEAR::isError($res)){
+					  die($res->getMessage());}
+				}
+			}
+		}
+	}
+
+	require_once($path_oreon."www/include/reporting/dashboard/reporting-func.php");
+	require_once($path_oreon."www/include/reporting/dashboard/simple-func.php");
+
+	$tab_hosts = array();
+	$tab_services = array();
+	
+	$day_current_start = 0;
+	$day_current_end = 0;
+	
+	foreach($tableFile2 as $key => $time)
+	{
+		insert_file_name_in_db($key);
+		parseFile($key, $time, $tab_hosts, $tab_services,$day_current_start, $day_current_end, false);
+	}
 ?>
