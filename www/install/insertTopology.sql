97b17f3e8babdbeed1accefca8bd67b8538a8554
Julien Mathis <jmathis@merethis.com>
2007-02-22 10:02:47 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1538 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/insertTopology.sql
+++ www/install/insertTopology.sql
@@ -238,6 +238,9 @@ INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VAL
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 40205, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 40205, 'w', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 40205, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 20303, NULL, './include/common/javascript/changetab.js', 'initChangeTab');
+
+-- INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 40210, 'vz', './include/common/javascript/changetab.js', 'initChangeTab');
 
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 502, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES ('', 502, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
