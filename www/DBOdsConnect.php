ec2d8581e7375d74966b70c2c499bad0fdbec47f
Julien Mathis <jmathis@merethis.com>
2007-02-22 10:05:51 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1540 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/DBOdsConnect.php
@@ -0,0 +1,49 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Julien Mathis - Romain Le Merlus - Christophe Coraboeuf
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+	if (!isset($oreon))
+		exit();
+
+	// This file have to be included whenever we want to connect to the DB
+		
+	require_once("DB.php");
+	
+	// Pear connection
+	
+	$debug = 0;
+	$dsn = array(
+	    'phptype'  => 'mysql',
+	    'username' => $conf_oreon["user"],
+	    'password' => $conf_oreon["password"],
+	    'hostspec' => $conf_oreon["host"],
+	    'database' => "ODS",
+	);
+	
+	$options = array(
+	    'debug'       => 2,
+	    'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE, 
+	);
+	
+	global $pearDB0;
+	
+	$pearDBO =& DB::connect($dsn, $options);
+	if (PEAR::isError($pearDBO))
+	    die($pearDBO->getMessage());
+	    
+	$pearDBO->setFetchMode(DB_FETCHMODE_ASSOC);
+	// End of Pear connection
+?>
