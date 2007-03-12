250f9154f01fa2abfd475ff9151fab57046bb650
Cedrick Facon <cfacon@merethis.com>
2007-03-12 15:55:59 +0000

    ajax bug fix
    git-svn-id: http://svn.centreon.com/oreon-dev@1697 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/MakeXML4statusCounter.php
+++ www/include/monitoring/engine/MakeXML4statusCounter.php
@@ -17,7 +17,7 @@ For information : contact@oreon-project.org
 */
 
 	$debug = 0;
-
+	$flag_reset = 1;
 
 	## pearDB init
 	require_once 'DB.php';	
@@ -65,48 +65,83 @@ For information : contact@oreon-project.org
 ## sessionID check and refresh
 #
 
-$flag = 0;
-if(isset($_POST["sid"]) && isset($_POST["slastreload"]) && isset($_POST["smaxtime"])){
-	$res =& $pearDB->query("SELECT * FROM session WHERE session_id = '".$_POST["sid"]."'");
-	if (PEAR::isError($res))
-		print "Mysql Error : ".$res->getMessage();
-	if($session =& $res->fetchRow()){
-		$flag = $_POST["slastreload"];		
-		if(time() - $_POST["slastreload"] > ($_POST["smaxtime"] / 4)){		
-			$flag = time();
-			$sql = "UPDATE `session` SET `last_reload` = '".time()."', `ip_address` = '".$_SERVER["REMOTE_ADDR"]."' WHERE CONVERT( `session_id` USING utf8 ) = '".$_POST["sid"]."' LIMIT 1";
-			$res =& $pearDB->query($sql);
-			if (PEAR::isError($res))
-				print "Mysql Error : ".$res->getMessage();
+
+$sid = isset($_POST["sid"]) ? $_POST["sid"] : 0;
+$sid = isset($_GET["sid"]) ? $_GET["sid"] : $sid;
+
+$session_expire = isset($_POST["session_expire"]) ? $_POST["session_expire"] : 30;
+$session_expire = isset($_GET["session_expire"]) ? $_GET["session_expire"] : $session_expire;
+
+
+	$session =& $pearDB->query("UPDATE session SET " .
+			" s_nbHostsUp = 1" . 
+			" s_nbHostsDown = 1" . 
+			" s_nbHostsUnreachable = 1" . 
+			" s_nbHostsPending = 1" . 
+			" s_nbServicesOk = 1" . 
+			" s_nbServicesWarning = 1" . 
+			" s_nbServicesCritical = 1" . 
+			" s_nbServicesUnknown = 1" . 
+			" s_nbServicesPending = 1" . 
+			"WHERE session_id = 1");	
+
+
+
+function restore_session($statistic_service = 'null', $statistic_host = 'null'){
+	global $pearDB;
+	
+	if(isset($statistic_service) && !is_null($statistic_service))
+		$sql = "UPDATE session SET " .
+				" s_nbHostsUp = '".$statistic_host["UP"]."'," . 
+				" s_nbHostsDown = '".$statistic_host["DOWN"]."'," . 
+				" s_nbHostsUnreachable = '".$statistic_host["UNREACHABLE"]."'," . 
+				" s_nbHostsPending = '".$statistic_host["PENDING"]."'," . 
+				" s_nbServicesOk = '".$statistic_service["OK"]."'," . 
+				" s_nbServicesWarning = '".$statistic_service["WARNING"]."'," . 
+				" s_nbServicesCritical = '".$statistic_service["CRITICAL"]."'," . 
+				" s_nbServicesUnknown = '".$statistic_service["UNKNOWN"]."'," . 
+				" s_nbServicesPending = '".$statistic_service["PENDING"]."'" . 
+				" WHERE session_id = '".$_POST["sid"]."'";	
+
+			$DBRESULT =& $pearDB->query($sql);
+			if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+}
+
+if($sid){
+
+	$debug_session = $sid;
+	$time = time();
+
+	$res =& $pearDB->query("SELECT * FROM session WHERE session_id = '".$sid."'");
+
+
+	if($res->fetchInto($session))
+	{
+/*
+echo $time . "<br>";
+echo $session["last_reload"] . "<br>";
+echo ($time - $session["last_reload"]);
+exit(0);
+*/
+	if(($time - $session["last_reload"]) > $session_expire)
+		 {
+			$flag_reset = 1;	 	
+		 }
+		else
+		{
+			$flag_reset = 0;
 		}
 	}
 }
 
-/*
-	Session::start();	
-	if (isset($_SESSION["oreon"])) {	// already connected
-		$oreon = & $_SESSION["oreon"];
-		$pearDB->query("DELETE FROM session WHERE session_id = '".session_id()."'");
-		Session::stop();
-		Session::start();
-		
-		$debug_session = 'OK';
-	}
-*/
-
-
-
 
 	function read($version,$sid,$file){
 		global $pearDB, $flag;
+		$_POST["sid"] = $sid;
+		$_GET["sid"] = $sid;
 		
-		$MyLog = date('l dS \of F Y h:i:s A'). "\n";
-		$buffer = null;
-		$buffer  = '<?xml version="1.0"?>';
-		$buffer .= '<reponse>';
-		$buffer .= '<infos>';
-		$buffer .= '<filetime>'.filectime($file).'</filetime>';
-		$buffer .= '</infos>';
+		
 		
 		$oreon = "";
 		$search = "";
@@ -114,6 +149,7 @@ if(isset($_POST["sid"]) && isset($_POST["slastreload"]) && isset($_POST["smaxtim
 		$search_type_host = 0;
 	
 		include("../load_status_log.php");
+
 	
 		## calcul stat for resume		
 		$statistic_host = array("UP" => 0, "DOWN" => 0, "UNREACHABLE" => 0, "PENDING" => 0);
@@ -125,6 +161,17 @@ if(isset($_POST["sid"]) && isset($_POST["slastreload"]) && isset($_POST["smaxtim
 		if (isset($service_status))
 			foreach ($service_status as $s)
 				$statistic_service[$s["current_state"]]++;
+
+		restore_session($statistic_service, $statistic_host);
+		$MyLog = date('l dS \of F Y h:i:s A'). "\n";
+		$buffer = null;
+		$buffer  = '<?xml version="1.0"?>';
+		$buffer .= '<reponse>';
+		$buffer .= '<infos>';
+		$buffer .= '<filetime>'.filectime($file).'</filetime>';
+		$buffer .= '</infos>';
+
+
 	
 		$buffer .= '<stats>';
 		$buffer .= '<statistic_service_ok>'.$statistic_service["OK"].'</statistic_service_ok>';
@@ -132,45 +179,39 @@ if(isset($_POST["sid"]) && isset($_POST["slastreload"]) && isset($_POST["smaxtim
 		$buffer .= '<statistic_service_critical>'.$statistic_service["CRITICAL"].'</statistic_service_critical>';
 		$buffer .= '<statistic_service_unknown>'.$statistic_service["UNKNOWN"].'</statistic_service_unknown>';
 		$buffer .= '<statistic_service_pending>'.$statistic_service["PENDING"].'</statistic_service_pending>';
-global $debug_session;
-		$buffer .= '<statistic_host_up>'.$debug_session.'</statistic_host_up>';
-//		$buffer .= '<statistic_host_up>'.$statistic_host["UP"].'</statistic_host_up>';
+		$buffer .= '<statistic_host_up>'.$statistic_host["UP"].'</statistic_host_up>';
 		$buffer .= '<statistic_host_down>'.$statistic_host["DOWN"].'</statistic_host_down>';
 		$buffer .= '<statistic_host_unreachable>'.$statistic_host["UNREACHABLE"].'</statistic_host_unreachable>';
 		$buffer .= '<statistic_host_pending>'.$statistic_host["PENDING"].'</statistic_host_pending>';
-		$buffer .= '</stats>';	
+		$buffer .= '</stats>';
 		$buffer .= '</reponse>';
 		header('Content-Type: text/xml');
 		echo $buffer;
 
-	/*
-		global $debug;
-		if($debug)
-		{
-			$file = "logb.xml";
-			$inF = fopen($file,"w");
-			fwrite($inF,$buffer);
-			fclose($inF);
-			
-		
-			$file = "logb.txt";
-			$inF = fopen($file,"w");
-			fwrite($inF,"log:\n ".$MyLog."\n\n");
-			fwrite($inF,"sid:\n ".$sid."\n\n");
-			fwrite($inF,"uid:\n ".$uid."\n\n");
-			fwrite($inF,"admin:\n ".$IsAdmin."\n\n");
-	
-			if($oreonLCA && is_array($oreonLCA))
-				foreach($oreonLCA as $key => $h)
-				fwrite($inF,"lca h: ".$h."\n\n");	
-			
-			fwrite($inF,"log:\n----------\n\n");
-			fclose($inF);
-		}
-		*/
 	}
 	
-	if(isset($_POST["version"]) && isset($_POST["sid"])&& isset($_POST["fileStatus"])){
+	if(!$flag_reset){
+		$buffer = null;
+		$buffer  = '<?xml version="1.0"?>';
+		$buffer .= '<reponse>';
+		$buffer .= '<infos>';
+		$buffer .= '<filetime>'.$session["last_reload"].'</filetime>';
+		$buffer .= '</infos>';
+		$buffer .= '<stats>';
+		$buffer .= '<statistic_service_ok>'.$session["s_nbServicesOk"].'</statistic_service_ok>';
+		$buffer .= '<statistic_service_warning>'.$session["s_nbServicesWarning"].'</statistic_service_warning>';
+		$buffer .= '<statistic_service_critical>'.$session["s_nbServicesCritical"].'</statistic_service_critical>';
+		$buffer .= '<statistic_service_unknown>'.$session["s_nbServicesUnknown"].'</statistic_service_unknown>';
+		$buffer .= '<statistic_service_pending>'.$session["s_nbServicesPending"].'</statistic_service_pending>';
+		$buffer .= '<statistic_host_up>'.$session["s_nbHostsUp"].'</statistic_host_up>';
+		$buffer .= '<statistic_host_down>'.$session["s_nbHostsDown"].'</statistic_host_down>';
+		$buffer .= '<statistic_host_unreachable>'.$session["s_nbHostsUnreachable"].'</statistic_host_unreachable>';
+		$buffer .= '<statistic_host_pending>'.$session["s_nbHostsPending"].'</statistic_host_pending>';
+		$buffer .= '</stats>';	
+		$buffer .= '</reponse>';
+		header('Content-Type: text/xml');
+		echo $buffer;
+	} else if (isset($_POST["version"]) && isset($_POST["sid"])&& isset($_POST["fileStatus"])){
 		read($_POST["version"],$_POST["sid"],$_POST["fileStatus"]);
 	} else if (isset($_GET["version"]) && isset($_GET["sid"])&& isset($_GET["fileStatus"])) {
 		read($_GET["version"],$_GET["sid"],$_GET["fileStatus"]);
@@ -181,5 +222,6 @@ global $debug_session;
 		$buffer .= '</reponse>';	
 		header('Content-Type: text/xml');
 		echo $buffer;
+		restore_session();
 	}
 ?>
