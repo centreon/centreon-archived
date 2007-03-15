c271b53e68fe7c41d53f68b2d85ae22203191dee
Julien Mathis <jmathis@merethis.com>
2007-03-15 13:22:37 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1753 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/doc/index.php
@@ -0,0 +1,36 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Julien Mathis - Romain Le Merlus
+
+The Software is provided to you AS IS and WITH ALL FAULTS. OREON makes no representation
+and gives no warranty whatsoever, whether express or implied, and without limitation, 
+with regard to the quality, safety, contents, performance, merchantability, non-infringement
+or suitability for any particular or intended purpose of the Software found on the OREON web
+site. In no event will OREON be liable for any direct, indirect, punitive, special, incidental
+or consequential damages however they may arise and even if OREON has been previously advised 
+of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+	if (!isset($oreon))
+		exit(); 
+	
+	include_once("./include/monitoring/common-Func.php");
+	include_once("./include/monitoring/status/resume.php"); 
+
+	unset($tpl);
+	unset($path);
+
+	$path = "./include/home/";
+		
+	# Smarty template Init
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl($path, $tpl, "./");	
+		
+	$tpl->assign("session", session_id());
+	$tpl->display("home.ihtml");
+?>
+
