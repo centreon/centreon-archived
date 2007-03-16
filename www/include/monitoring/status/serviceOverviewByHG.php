37a68297d07cb1f4a0bb51585335b5909a84106d
Julien Mathis <jmathis@merethis.com>
2007-03-16 08:56:26 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1764 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/serviceOverviewByHG.php
+++ www/include/monitoring/status/serviceOverviewByHG.php
@@ -42,6 +42,7 @@ For information : contact@oreon-project.org
 			while ($DBRESULT1->fetchInto($r_h)){
 				if ($oreon->user->admin || !$isRestreint || ($isRestreint && isset($TabLca["LcaHost"][$r_h["host_name"]]))){
 					$service_data_str = NULL;	
+					$service_data_str_ack = NULL;	
 					$host_data_str = "<a href='./oreon.php?p=201&o=hd&host_name=".$r_h["host_name"]."'>" . $r_h["host_name"] . "</a>";
 					isset($host_status[$r_h["host_name"]]) && $host_status[$r_h["host_name"]]["current_state"] == "DOWN" ? $h_class[$r["hg_name"]][$r_h["host_name"]] = "list_down" : $h_class[$r["hg_name"]][$r_h["host_name"]] = $tab_color[++$counter_host % 2];
 					
@@ -51,19 +52,40 @@ For information : contact@oreon-project.org
 							|| 	(!isset($_GET["problem"]) && !isset($_GET["acknowledge"])) 
 							|| 	(!isset($_GET["problem"]) && isset($_GET["acknowledge"]) && $_GET["acknowledge"] == 1 && $service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 1)
 							|| 	(!isset($_GET["problem"]) && isset($_GET["acknowledge"]) && $_GET["acknowledge"] == 0 && $service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 0 && $service_status[$r_h["host_name"]."_".$key]["current_state"] != "OK" )){
-								$service_data_str .= 	"<span style='background:".
-														$oreon->optGen["color_".strtolower($service_status[$r_h["host_name"]."_".$key]["current_state"])].";opacity: 0.9;'>".
-														"<a href='./oreon.php?p=202&o=svcd&host_name=".$r_h["host_name"]."&service_description=".$key."'>".$key.
-														"</a></span> &nbsp;&nbsp;";
-								if (!isset($hg[$r["hg_name"]]))
-									$hg[$r["hg_name"]] = array("name" => $r["hg_name"], 'alias' => $r["hg_alias"], "host" => array());
+								if (isset($_GET["problem"])){
+									if ($service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 1)
+										$service_data_str_ack .= 	"<span style='background:".
+																$oreon->optGen["color_".strtolower($service_status[$r_h["host_name"]."_".$key]["current_state"])].";opacity: 0.9;'>".
+																"<a href='./oreon.php?p=202&o=svcd&host_name=".$r_h["host_name"]."&service_description=".$key."'>".$key.
+																"</a></span> &nbsp;&nbsp;";
+									else
+										$service_data_str .= 	"<span style='background:".
+																$oreon->optGen["color_".strtolower($service_status[$r_h["host_name"]."_".$key]["current_state"])].";opacity: 0.9;'>".
+																"<a href='./oreon.php?p=202&o=svcd&host_name=".$r_h["host_name"]."&service_description=".$key."'>".$key.
+																"</a></span> &nbsp;&nbsp;";
+								} else {
+									$service_data_str .= 	"<span style='background:".
+															$oreon->optGen["color_".strtolower($service_status[$r_h["host_name"]."_".$key]["current_state"])].";opacity: 0.9;'>".
+															"<a href='./oreon.php?p=202&o=svcd&host_name=".$r_h["host_name"]."&service_description=".$key."'>".$key.
+															"</a></span> &nbsp;&nbsp;";
+								}
+							}
+							
+							if ($host_status[$r_h["host_name"]]["current_state"]){
 								$hg[$r["hg_name"]]["host"][$cpt_host] = $r_h["host_name"];
 								$h_data[$r["hg_name"]][$r_h["host_name"]] = $host_data_str;
 								$status = "color_".strtolower($host_status[$r_h["host_name"]]["current_state"]);
 								$h_status_data[$r["hg_name"]][$r_h["host_name"]] = "<td class='ListColCenter' width='70' style='background:".$oreon->optGen[$status]."'>".$host_status[$r_h["host_name"]]["current_state"]."</td>";
-								$svc_data[$r["hg_name"]][$r_h["host_name"]] = $service_data_str;
 							}
 						}
+						if ((isset($service_data_str) || isset($service_data_str_ack)) && !isset($hg[$r["hg_name"]])){
+							$hg[$r["hg_name"]] = array("name" => $r["hg_name"], 'alias' => $r["hg_alias"], "host" => array());
+						}								
+						if (isset($service_data_str))
+							$svc_data[$r["hg_name"]][$r_h["host_name"]] = $service_data_str;
+						
+						if (isset($service_data_str_ack))
+							$svc_data_ack[$r["hg_name"]][$r_h["host_name"]] = $service_data_str_ack;
 					}
 					$cpt_host++;
 				}
@@ -77,6 +99,16 @@ For information : contact@oreon-project.org
 
 	$tpl->assign("refresh", $oreon->optGen["oreon_refresh"]);
 	$tpl->assign("p", $p);
+	foreach ($hg as $key => $value){
+		$flag = 0;
+		foreach ($value['host'] as $h){
+			if ((isset($svc_data[$key][$h]) && $svc_data[$key][$h]) || (isset($svc_data_ack[$key][$h]) && $svc_data_ack[$key][$h])){
+				$flag = 1;	
+			}
+		}
+		if (!$flag)
+			unset($hg[$key]);
+	}
 	$tpl->assign("hostgroup", $hg);
 	if (isset($h_data))
 		$tpl->assign("h_data", $h_data);
@@ -84,6 +116,8 @@ For information : contact@oreon-project.org
 		$tpl->assign("h_status_data", $h_status_data);
 	if(isset($svc_data))
 		$tpl->assign("svc_data", $svc_data);
+	if(isset($svc_data_ack))
+		$tpl->assign("svc_data_ack", $svc_data_ack);
 	if(isset($h_class))
 		$tpl->assign("h_class", $h_class);
 	$tpl->assign("lang", $lang);
--- www/include/monitoring/status/serviceSummaryByHG.php
+++ www/include/monitoring/status/serviceSummaryByHG.php
@@ -24,6 +24,7 @@ For information : contact@oreon-project.org
 	
 	$hg = array();
 	$status_hg = array();
+	$status_hg_ack = array();
 	$tab_color = array(0=>"list_one", 1=>"list_two");
 	
 	$DBRESULT =& $pearDB->query("SELECT * FROM hostgroup WHERE hg_activate = '1' ORDER BY hg_name");
@@ -40,7 +41,9 @@ For information : contact@oreon-project.org
 			$counter_host = 0;	
 			while ($DBRESULT1->fetchInto($r_h)){
 				$status_hg = array("OK" => 0, "PENDING" => 0, "WARNING" => 0, "CRITICAL" => 0, "UNKNOWN" => 0);
-				$service_data_str = NULL;	
+				$status_hg_ack = array("OK" => 0, "PENDING" => 0, "WARNING" => 0, "CRITICAL" => 0, "UNKNOWN" => 0);
+				$service_data_str = NULL;
+				$service_data_str_ack = NULL;
 				isset($host_status[$r_h["host_name"]]) && $host_status[$r_h["host_name"]]["current_state"] == "DOWN" ? $h_class[$r["hg_name"]][$r_h["host_name"]] = "list_down" : $h_class[$r["hg_name"]][$r_h["host_name"]] = $tab_color[++$counter_host % 2];
 				if ($oreon->user->admin || !$isRestreint || ($isRestreint && isset($TabLca["LcaHost"][$r_h["host_name"]]))){
 					if (isset($tab_host_service[$r_h["host_name"]])){
@@ -49,35 +52,81 @@ For information : contact@oreon-project.org
 							|| 	(!isset($_GET["problem"]) && !isset($_GET["acknowledge"])) 
 							|| 	(!isset($_GET["problem"]) && isset($_GET["acknowledge"]) && $_GET["acknowledge"] == 1 && $service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 1)
 							|| 	(!isset($_GET["problem"]) && isset($_GET["acknowledge"]) && $_GET["acknowledge"] == 0 && $service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 0 && $service_status[$r_h["host_name"]."_".$key]["current_state"] != "OK" )){
-								$status_hg[$service_status[$r_h["host_name"]. "_" .$key]["current_state"]]++;					
-								$service_data_str = "";
-								if ($status_hg["OK"] != 0)
-									$service_data_str = "<span style='background:".$oreon->optGen["color_ok"]."'>" . $status_hg["OK"] . " <a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."&status=OK'>OK</a></span> ";
-								if ($status_hg["WARNING"] != 0)
-									$service_data_str .= "<span style='background:".$oreon->optGen["color_warning"]."'>" . $status_hg["WARNING"] . " <a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."&status=WARNING'>WARNING</a></span> ";
-								if ($status_hg["CRITICAL"] != 0)
-									$service_data_str .= "<span style='background:".$oreon->optGen["color_critical"]."'>" . $status_hg["CRITICAL"] . " <a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."&status=CRITICAL'>CRITICAL</a></span> ";
-								if ($status_hg["PENDING"] != 0)
-									$service_data_str .= "<span style='background:".$oreon->optGen["color_pending"]."'>" . $status_hg["PENDING"] . " <a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."&status=PENDING'>PENDING</a></span> ";
-								if ($status_hg["UNKNOWN"] != 0)
-									$service_data_str .= "<span style='background:".$oreon->optGen["color_unknown"]."'>" . $status_hg["UNKNOWN"] . " <a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."&status=UNKNOWN'>UNKNOWN</a></span> ";
-								if (!isset($hg[$r["hg_name"]]))
-									$hg[$r["hg_name"]] = array("name" => $r["hg_name"], 'alias' => $r["hg_alias"], "host" => array());
-								$hg[$r["hg_name"]]["host"][$cpt_host] = $r_h["host_name"];
-								$host_data_str = "<a href='./oreon.php?p=201&o=hd&host_name=".$r_h["host_name"]."'>" . $r_h["host_name"] . "</a>";
-								$h_data[$r["hg_name"]][$r_h["host_name"]] = $host_data_str;
-								$status = "color_".strtolower($host_status[$r_h["host_name"]]["current_state"]);
-								$h_status_data[$r["hg_name"]][$r_h["host_name"]] = "<td class='ListColCenter' width='70' style='background:".$oreon->optGen[$status]."'><a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."'>".$host_status[$r_h["host_name"]]["current_state"]."</a></td>";
-								$svc_data[$r["hg_name"]][$r_h["host_name"]] = $service_data_str;
+								if (isset($_GET["problem"])){
+									if ($service_status[$r_h["host_name"]."_".$key]["problem_has_been_acknowledged"] == 1)
+										$status_hg_ack[$service_status[$r_h["host_name"]. "_" .$key]["current_state"]]++;
+									else
+										$status_hg[$service_status[$r_h["host_name"]. "_" .$key]["current_state"]]++;	
+								} else {
+									$status_hg[$service_status[$r_h["host_name"]. "_" .$key]["current_state"]]++;					
+								}
 							}
-						}						
+						}
+						$service_data_str = "";
+						$service_data_str_ack = "";
+						if (isset($_GET["problem"])){
+							if (isset($status_hg["OK"]) && $status_hg["OK"] != 0)
+								$service_data_str = "<span style='background:".$oreon->optGen["color_ok"]."'>" . $status_hg["OK"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_ok'>OK</a></span> ";
+							if (isset($status_hg["WARNING"]) && $status_hg["WARNING"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_warning"]."'>" . $status_hg["WARNING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_warning'>WARNING</a></span> ";
+							if (isset($status_hg["CRITICAL"]) && $status_hg["CRITICAL"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_critical"]."'>" . $status_hg["CRITICAL"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_critical'>CRITICAL</a></span> ";
+							if (isset($status_hg["PENDING"]) && $status_hg["PENDING"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_pending"]."'>" . $status_hg["PENDING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=PENDING'>PENDING</a></span> ";
+							if (isset($status_hg["UNKNOWN"]) && $status_hg["UNKNOWN"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_unknown"]."'>" . $status_hg["UNKNOWN"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_unknown'>UNKNOWN</a></span> ";
+							
+							if (isset($status_hg_ack["OK"]) && $status_hg_ack["OK"] != 0)
+								$service_data_str_ack = "<span style='background:".$oreon->optGen["color_ok"]."'>" . $status_hg_ack["OK"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_ok'>OK</a></span> ";
+							if (isset($status_hg_ack["WARNING"]) && $status_hg_ack["WARNING"] != 0)
+								$service_data_str_ack .= "<span style='background:".$oreon->optGen["color_warning"]."'>" . $status_hg_ack["WARNING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_warning'>WARNING</a></span> ";
+							if (isset($status_hg_ack["CRITICAL"]) && $status_hg_ack["CRITICAL"] != 0)
+								$service_data_str_ack .= "<span style='background:".$oreon->optGen["color_critical"]."'>" . $status_hg_ack["CRITICAL"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_critical'>CRITICAL</a></span> ";
+							if (isset($status_hg_ack["PENDING"]) && $status_hg_ack["PENDING"] != 0)
+								$service_data_str_ack .= "<span style='background:".$oreon->optGen["color_pending"]."'>" . $status_hg_ack["PENDING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=PENDING'>PENDING</a></span> ";
+							if (isset($status_hg_ack["UNKNOWN"]) && $status_hg_ack["UNKNOWN"] != 0)
+								$service_data_str_ack .= "<span style='background:".$oreon->optGen["color_unknown"]."'>" . $status_hg_ack["UNKNOWN"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_unknown'>UNKNOWN</a></span> ";
+							$svc_data[$r["hg_name"]][$r_h["host_name"]] = $service_data_str;
+							$svc_data_ack[$r["hg_name"]][$r_h["host_name"]] = $service_data_str_ack;
+						} else {
+							
+							if (isset($status_hg["OK"]) && $status_hg["OK"] != 0)
+								$service_data_str = "<span style='background:".$oreon->optGen["color_ok"]."'>" . $status_hg["OK"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_ok'>OK</a></span> ";
+							if (isset($status_hg["WARNING"]) && $status_hg["WARNING"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_warning"]."'>" . $status_hg["WARNING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_warning'>WARNING</a></span> ";
+							if (isset($status_hg["CRITICAL"]) && $status_hg["CRITICAL"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_critical"]."'>" . $status_hg["CRITICAL"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_critical'>CRITICAL</a></span> ";
+							if (isset($status_hg["PENDING"]) && $status_hg["PENDING"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_pending"]."'>" . $status_hg["PENDING"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=PENDING'>PENDING</a></span> ";
+							if (isset($status_hg["UNKNOWN"]) && $status_hg["UNKNOWN"] != 0)
+								$service_data_str .= "<span style='background:".$oreon->optGen["color_unknown"]."'>" . $status_hg["UNKNOWN"] . " <a href='./oreon.php?p=2020202&host_name=".$r_h["host_name"]."&o=svc_unknown'>UNKNOWN</a></span> ";
+							$svc_data[$r["hg_name"]][$r_h["host_name"]] = $service_data_str;
+						}
+						if (!isset($hg[$r["hg_name"]]))
+							$hg[$r["hg_name"]] = array("name" => $r["hg_name"], 'alias' => $r["hg_alias"], "host" => array());
+						$hg[$r["hg_name"]]["host"][$cpt_host] = $r_h["host_name"];
+						$host_data_str = "<a href='./oreon.php?p=201&o=hd&host_name=".$r_h["host_name"]."'>" . $r_h["host_name"] . "</a>";
+						$h_data[$r["hg_name"]][$r_h["host_name"]] = $host_data_str;
+						$status = "color_".strtolower($host_status[$r_h["host_name"]]["current_state"]);
+						$h_status_data[$r["hg_name"]][$r_h["host_name"]] = "<td class='ListColCenter' width='70' style='background:".$oreon->optGen[$status]."'><a href='./oreon.php?p=".$p."&host_name=".$r_h["host_name"]."'>".$host_status[$r_h["host_name"]]["current_state"]."</a></td>";
 					}
 				}
 				$cpt_host++;
 			}
 		}
 	}
-		
+	
+	foreach ($hg as $key => $value){
+		$flag = 0;
+		foreach ($value['host'] as $h){
+			if ((isset($svc_data[$key][$h]) && $svc_data[$key][$h]) || (isset($svc_data_ack[$key][$h]) && $svc_data_ack[$key][$h])){
+				$flag = 1;	
+			}
+		}
+		if (!$flag)
+			unset($hg[$key]);
+	}
+	
 	# Smarty template Init
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl, "/templates/");
@@ -90,6 +139,8 @@ For information : contact@oreon-project.org
 		$tpl->assign("h_status_data", $h_status_data);
 	if (isset($svc_data))
 		$tpl->assign("svc_data", $svc_data);
+	if (isset($svc_data_ack))
+		$tpl->assign("svc_data_ack", $svc_data_ack);
 	if (isset($h_class))
 		$tpl->assign("h_class", $h_class);
 	$tpl->assign("lang", $lang);
