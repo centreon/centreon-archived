9f30f26178297fc969832de9c5a293bad98af72e
Cedrick Facon <cfacon@merethis.com>
2007-03-20 12:48:05 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@1920 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/Themes/Basic/Color/pink_css.php
@@ -0,0 +1,65 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Julien Mathis - Romain Le Merlus
+
+Adapted to Pear library by Merethis company, under direction of Cedrick Facon, Romain Le Merlus, Julien Mathis
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+
+header("Content-type: text/css");
+
+$color1 = "#F2F2F2";
+$color2 = "#666666";
+$color3 = "#E3A385";
+$color4 = "#d5dfeb";
+$color5 = "#ced3ed";
+$color6 = "#BFD0E2";
+$color7 = "#AAAAAA";
+$color8 = "#D1DCEB";
+$color9 = "#FFFFFF";
+$color10 = "#AAAAEE";
+$color11 = "#592bed";
+$color12 = "#242af6";
+
+$color13 = "#5e5e5e";
+$color14 = "#E8AB5C";
+
+$menu1_bgcolor = "#9C85E3";
+$menu2_bgcolor = "#ECE5F9";
+
+$footerline2 = "#dedede";
+
+$color_list_1 = "#F7FAFF";
+$color_list_1_hover = "#FDF0D5";
+
+$color_list_2 = "#EDF4FF";
+$color_list_2_hover = "#FDF0D5";
+$color_list_3 = "#fada83";
+$color_list_3_hover = "#bada83";
+$color_list_4 = "#fdc11e";
+$color_list_4_hover = "#bdc11e";
+$color_list_up = "#B2F867";
+$color_list_up_hover = "#B2A867";
+$color_list_down = "#ffbbbb";
+$color_list_down_hover = "#dfbbbb";
+
+
+
+$bg_image_header = "../Images/bg_header_pink.gif";
+$menu1_bgimg = "../Images/menu_bg_purple.gif";
+
+include("color_css.php")
+?>
