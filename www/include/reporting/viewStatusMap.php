cd9eaf52ba5c0609e9df7ff21b07fe6db8ac095a
Romain Le Merlus <rlemerlus@merethis.com>
2007-03-23 08:41:48 +0000

    Drop in header bad comment "PHPLIB.."
    git-svn-id: http://svn.centreon.com/oreon-dev@2020 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/reporting/viewStatusMap.php
+++ www/include/reporting/viewStatusMap.php
@@ -4,8 +4,6 @@ Oreon is developped with GPL Licence 2.0 :
 http://www.gnu.org/licenses/gpl.txt
 Developped by : Julien Mathis - Romain Le Merlus - Christophe Coraboeuf - Cedrick Facon
 
-Adapted to Pear library Quickform & Template_PHPLIB by Merethis company, under direction of Cedrick Facon
-
 The Software is provided to you AS IS and WITH ALL FAULTS.
 OREON makes no representation and gives no warranty whatsoever,
 whether express or implied, and without limitation, with regard to the quality,
