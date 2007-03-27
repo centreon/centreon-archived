10b1c262e4df95c7c4e3209842f76f2a01ba0af8
Julien Mathis <jmathis@merethis.com>
2007-03-27 14:30:04 +0000

    add a read temporary file
    git-svn-id: http://svn.centreon.com/oreon-dev@2083 6bcd3966-0018-0410-8128-fd23d134de7e

--- ODS/lib/getPerfData.pm
+++ ODS/lib/getPerfData.pm
@@ -20,49 +20,65 @@
 ####################################################################
 
 sub GetPerfData(){
-	my ($line_tab, $sth2, $data);
+	my ($line_tab, $sth2, $data, $flag_drop);
 	use vars qw($con_oreon $con_ods);
-	my $debug = 1;
+	
 	CheckMySQLConnexion();
+	
 	$sth2 = $con_ods->prepare("SELECT perfdata_file FROM config");
 	if (!$sth2->execute) {writeLogFile("Error when getting perfdata file : " . $sth2->errstr . "\n");}
 	$data = $sth2->fetchrow_hashref();
-	my $PFDT = $data->{'perfdata_file'};		
+	my $PFDT = $data->{'perfdata_file'};
 	undef($sth2);
 	undef($data);
-	my $drop_data = 1;
-	my $flag_drop = 1;
+
 	while (1) {
-		if (-r $PFDT && open(PFDT, "< $PFDT")){
-			CheckMySQLConnexion();
-			$sth2 = $con_ods->prepare("SELECT auto_drop,drop_file,perfdata_file FROM config");
-			if (!$sth2->execute) {writeLogFile("Error when getting drop and perfdata properties : " . $sth2->errstr . "\n");}
-			$data = $sth2->fetchrow_hashref();
-			$PFDT = $data->{'perfdata_file'};
-			if ($data->{'auto_drop'} == 1 && defined($data->{'drop_file'})){
-				if (!open(DROP, ">> ".$data->{'drop_file'})){
-					$flag_drop = 0;
-					writeLogFile("can't write in ".$data->{'drop_file'}." : $!");
-				}
-			} else {$flag_drop = 0;}
-			undef($data);
-			while (<PFDT>){
-				if ($debug){writeLogFile($_);}
-				if ($flag_drop == 1){print DROP $_ ;}
-		    	@line_tab = split('\t');
-		    	if (defined($line_tab[5]) && ($line_tab[5] ne '' && $line_tab[5] ne "\n")){
-					CheckMySQLConnexion();
-					checkAndUpdate(@line_tab);
-				}
-				$line_tab[5] = '';
+		if (-r $PFDT){
+			if (copy($PFDT, $PFDT."_read")){
+				unlink($PFDT);
+			} else {
+				writeLogFile("Error When moving data in tmp read file : $!");
 			}
-			unlink($PFDT);
-			close(PFDT);
-			if ($flag_drop == 1){close(DROP);}
-			undef($line_tab);
+			if (open(PFDT, "< $PFDT"."_read")){
+				CheckMySQLConnexion();
+	
+				$sth2 = $con_ods->prepare("SELECT auto_drop,drop_file,perfdata_file FROM config");
+				if (!$sth2->execute) {writeLogFile("Error when getting drop and perfdata properties : " . $sth2->errstr . "\n");}
+				$data = $sth2->fetchrow_hashref();
+	
+				$PFDT = $data->{'perfdata_file'};
+				$flag_drop = 1;
+				if ($data->{'auto_drop'} == 1 && defined($data->{'drop_file'})){
+					if (!open(DROP, ">> ".$data->{'drop_file'})){
+						$flag_drop = 0;
+						writeLogFile("can't write in ".$data->{'drop_file'}." : $!");
+					}
+				} else {
+					$flag_drop = 0;
+				}
+				undef($data);
+				
+				while (<PFDT>){
+					if ($debug){writeLogFile($_);}
+					if ($flag_drop == 1){print DROP $_ ;}
+			    	@line_tab = split('\t');
+			    	if (defined($line_tab[5]) && ($line_tab[5] ne '' && $line_tab[5] ne "\n")){
+						CheckMySQLConnexion();
+						checkAndUpdate(@line_tab);
+					}
+					$line_tab[5] = '';
+				}
+				
+				unlink($PFDT."_read");
+				close(PFDT);
+				
+				if ($flag_drop == 1){close(DROP);}
+				undef($line_tab);
+			} else {
+				writeLogFile("Error When reading data in tmp read file : $!");
+			}
 		}
 		sleep(getSleepTime());
-		$flag_drop = 1;
 	}
 } 
 
