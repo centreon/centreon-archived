c0e7a3ce4e259e6542313f4e994399817bed6633
Julien Mathis <jmathis@merethis.com>
2007-03-28 15:45:53 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2102 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.sql
+++ www/install/sql/UpdateDB-1.4.sql
@@ -499,3 +499,8 @@ UPDATE `topology` SET `topology_url` = './include/inventory/inventory.php' WHERE
 UPDATE `topology` SET `topology_url` = './include/inventory/inventory.php' WHERE `topology`.`topology_page` = '70103';
 
 
+INSERT INTO `topology` ( `topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show` , `topology_style_class` , `topology_style_id` , `topology_OnClick` ) VALUES ('' , 'm_nagios', NULL, 607, NULL, NULL, 33, NULL, NULL, '0', '0', '1', NULL , NULL , NULL);
+INSERT INTO `topology` ( `topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show` , `topology_style_class` , `topology_style_id` , `topology_OnClick` ) VALUES ('' , 'm_nagios', NULL, 202, NULL, NULL, 33, NULL, NULL, '0', '0', '1', NULL , NULL , NULL);
+INSERT INTO `topology` ( `topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show` , `topology_style_class` , `topology_style_id` , `topology_OnClick` ) VALUES ('' , 'm_nagios', NULL, 203, NULL, NULL, 34, NULL, NULL, '0', '0', '1', NULL , NULL , NULL);
+
+UPDATE `topology` SET `topology_group` = '1' WHERE `topology_group` IS NULL;
