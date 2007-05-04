333439b3c56ac326007f059e482448bd318aacdd
Julien Mathis <jmathis@merethis.com>
2007-05-04 10:07:06 +0000

    remove purge policy
    git-svn-id: http://svn.centreon.com/oreon-dev@2229 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.1.sql
+++ www/install/sql/UpdateDB-1.4.1.sql
@@ -3,9 +3,11 @@ ALTER TABLE `traps` ADD `traps_status` ENUM( '-1', '0', '1', '2', '3' ) NULL DEF
 ALTER TABLE `traps` ADD `manufacturer_id` INT( 11 ) NOT NULL ;
 ALTER TABLE `traps` CHANGE `traps_comments` `traps_comments` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ;
 
+ALTER TABLE `general_opt` DROP `perfparse_installed`;
 DELETE FROM `topology` WHERE `topology`.`topology_page` = 40202 LIMIT 1;
 DELETE FROM `topology` WHERE `topology`.`topology_page` = 40201 LIMIT 1;
 DELETE FROM `topology` WHERE `topology`.`topology_page` = 40203 LIMIT 1;
 DELETE FROM `topology` WHERE `topology`.`topology_page` = 40208 LIMIT 1;
 DELETE FROM `topology` WHERE `topology`.`topology_page` IS NULL AND `topology`.`topology_parent` = 402 AND `topology`.`topology_group` = 41 LIMIT 1;
 DELETE FROM `topology` WHERE `topology`.`topology_page` IS NULL AND `topology`.`topology_parent` = 402 AND `topology`.`topology_group` = 42 LIMIT 1;
+DELETE FROM `topology` WHERE `topology`.`topology_page` = 60708 LIMIT 1;
