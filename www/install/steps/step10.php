3ca6d9ea95721da8f893fecda4d8072ab2aaf820
Julien Mathis <jmathis@merethis.com>
2007-06-13 16:58:58 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2477 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/steps/step10.php
+++ www/install/steps/step10.php
@@ -53,12 +53,12 @@ aff_header("Oreon Setup Wizard", "Oreon Configuration File", 10);	?>
     	<td align="right"><b><? echo $msg ;	?></b></td>
   	</tr>
   	<tr>
-		<td><b>Writable ODS Configuration File (oreon.pm)</b></td>
+		<td><b>Writable ODS Configuration File (conf.pm)</b></td>
 		<td align="right"><?
        	$uid = posix_getpwuid (fileowner($_SESSION["oreon_dir"]."ODS/etc/"));
 		$gid = posix_getgrgid (filegroup($_SESSION["oreon_dir"]."ODS/etc/"));
        	$perms = substr(sprintf('%o', fileperms($_SESSION["oreon_dir"]."ODS/etc/")), -3) ;
-		if((strcmp($perms,'775') == 0 )  && (strcmp($_SESSION['apache_user'], $uid['name']) == 0 ) && (strcmp($_SESSION['apache_group'], $gid['name']) == 0) ){
+		if((strcmp($perms,'755') == 0 )  && (strcmp($_SESSION['apache_user'], $uid['name']) == 0 ) && (strcmp($_SESSION['apache_group'], $gid['name']) == 0) ){
           	echo '<b><span class="go">OK</font></b>';
         	$msg =  '';
 		} else {
