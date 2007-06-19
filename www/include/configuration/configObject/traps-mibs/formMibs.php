ab60f8bb4a84f25d16f6c0307a5998141a5e12a5
Mat Sugumaran <msugumaran@merethis.com>
2007-06-19 11:58:47 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2488 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/configuration/configObject/traps-mibs/formMibs.php
@@ -0,0 +1,108 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Sugumaran Mathavarajan - Julien Mathis - Romain Le Merlus
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+	#
+	## Database retrieve information for Manufacturer
+	#
+	
+	function myDecodeMib($arg)	{
+		$arg = html_entity_decode($arg, ENT_QUOTES);
+		return($arg);
+	}
+
+	$mnftr = array(NULL=>NULL);	
+	$DBRESULT =& $pearDB->query("SELECT id, alias FROM inventory_manufacturer order by alias");
+	if (PEAR::isError($DBRESULT))
+		print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+	# Set base value
+	while($DBRESULT->fetchInto($rmnftr)){
+		$mnftr[$rmnftr["id"]] = $rmnftr["alias"];
+	}
+	$DBRESULT->free();
+	
+	##########################################################
+	# Var information to format the element
+	#
+	
+	#
+	## Form begin
+	#
+	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
+	$form->addElement('header', 'title', $lang["m_mibs_title"]);
+
+	#
+	## Manufacturer information
+	#
+	$form->addElement('select', 'mnftr', $lang["m_mibs_mnftr"], $mnftr);
+	$form->addElement('file', 'filename', $lang["m_mibs_file"]);
+
+	#
+	## Further informations
+	#
+
+	#
+	## Form Rules
+	#
+	
+	$form->applyFilter('_ALL_', 'trim');
+	$form->addRule('mnftr', $lang['ErrName'], 'required');
+	$form->addRule('filename', $lang['ErrName'], 'required');
+	$form->setRequiredNote($lang['requiredFields']);
+
+	#
+	##End of form definition
+	#
+
+	# Smarty template Init
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl($path, $tpl);
+
+	# Just watch a Command information
+	$subA =& $form->addElement('submit', 'submit', $lang["gen_butOK"]);
+	$form->addElement('header', 'status',$lang["mibs_status"]);
+	$valid = false;
+	$msg = NULL;
+	$stdout = NULL;
+	if ($form->validate())	{
+		$ret = $form->getSubmitValues();
+		$fileObj =& $form->getElement('filename');
+		if ($fileObj->isUploadedFile()) {
+			$fileObj->moveUploadedFile("/tmp/");
+			$values = $fileObj->getValue();
+			$stdout = shell_exec("export LD_LIBRARY_PATH=/usr/local/lib && snmpttconvertmib --in=/tmp/".$values["name"]." --out=/tmp/".
+								$values["name"].".conf 2>&1");
+			$msg .= "<br>".str_replace ("\n", "<br>", $stdout);
+			$msg .= "<br>Moving traps in DataBase...";	
+			$stdout = shell_exec($oreon->optGen["nagios_path_plugins"]."traps/fill_trapDB -f /tmp/".
+								$values["name"].".conf -m ".htmlentities($ret["mnftr"], ENT_QUOTES)." 2>&1");
+			shell_exec("rm /tmp/".$values["name"].".conf /tmp/".$values["name"]);
+			$msg .= "<br>".str_replace ("\n", "<br>", $stdout);
+			$msg .= "<br>Generate Traps configuration files from Nagios configuration Tab.";
+			if ($msg)
+				$tpl->assign('msg', $msg);
+		}	
+		$valid = true;
+	}
+	##Apply a template definition
+	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+	$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
+	$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
+	$form->accept($renderer);
+	$tpl->assign('form', $renderer->toArray());
+	$tpl->display("formMibs.ihtml");
+?>
---
+++ www/include/configuration/configObject/traps-mibs/mibs.php
@@ -0,0 +1,40 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Sugumaran Mathavarajan - Julien Mathis - Romain Lemerlus
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+	if (!isset ($oreon))
+		exit ();
+
+	isset($_GET["id"]) ? $mnftrG = $_GET["id"] : $mnftrG = NULL;
+	isset($_POST["id"]) ? $mnftrP = $_POST["id"] : $mnftrP = NULL;
+	$mnftrG ? $id = $mnftrG : $id = $mnftrP;
+
+	#Pear library
+	require_once "HTML/QuickForm.php";
+	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+
+	#Path to the configuration dir
+	$path = "./include/configuration/configObject/traps-mibs/";
+	//$nagios_plugins = "/etc/nagios/libexec";
+
+	#PHP functions
+	require_once "./include/common/common-Func.php";
+
+	switch ($o)	{
+		case "a" : require_once($path."formMibs.php"); break; #Show command execution
+		default : require_once($path."formMibs.php"); break;
+	}
+?>
