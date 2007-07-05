dccdc7f80fac8c21dc14b74325e85e99298fc2ab
Julien Mathis <jmathis@merethis.com>
2007-07-05 22:15:00 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2579 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.sql
+++ www/install/sql/UpdateDB-1.4.sql
@@ -1,4 +1,3 @@
-
 ALTER TABLE `topology` ADD `topology_style_class` VARCHAR( 255 ) NULL ;
 ALTER TABLE `topology` ADD `topology_style_id` VARCHAR( 255 ) NULL ;
 ALTER TABLE `topology` ADD `topology_OnClick` VARCHAR( 255 ) NULL ;
