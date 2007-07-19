e03bbf8ae7e27427050c380aab422d4d0fca5025
Romain Le Merlus <rlemerlus@merethis.com>
2007-07-19 12:20:50 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2708 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/UpdateDB-1.4.1-RC2.sql
@@ -0,0 +1,8 @@
+-- 19/07/2007
+DELETE FROM topology WHERE topology_page = '401';
+DELETE FROM topology WHERE topology_page = '40101';
+DELETE FROM topology WHERE topology_page = '40102';
+
+DROP TABLE 'view_city';
+DROP TABLE 'view_country';
+DROP TABLE 'view_map';
