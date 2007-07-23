dfaf5c6ec91317686957c8468291d92b5a991586
Julien Mathis <jmathis@merethis.com>
2007-07-23 14:37:22 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@2748 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-ODS-1.4.1-RC2.sql
+++ www/install/sql/Update-ODS-1.4.1-RC2.sql
@@ -3,7 +3,7 @@ ALTER TABLE `index_data` ADD INDEX ( `trashed` );
 ALTER TABLE `index_data` ADD INDEX ( `host_name` );
 ALTER TABLE `index_data` ADD INDEX ( `service_description` );
 
-UPDATE `config` SET `len_storage_mysql` = '260' WHERE `config`.`id` =1 LIMIT 1 ;
+UPDATE `config` SET `len_storage_mysql` = '365' ;
 ALTER TABLE `index_data` CHANGE `must_be_rebuild` `must_be_rebuild` ENUM( '0', '1', '2' ) NULL DEFAULT '0';
 
 
