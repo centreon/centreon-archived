fff7586ddca70dae4de479f691bc140e465472cf
Julien Mathis <jmathis@merethis.com>
2007-07-26 16:49:45 +0000

    license Apache -> License GPL2
    git-svn-id: http://svn.centreon.com/oreon-dev@2794 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/javascript/autoSelectCommandExample.js
+++ www/include/common/javascript/autoSelectCommandExample.js
@@ -1,7 +1,7 @@
 /**
-Oreon is developped with Apache Licence 2.0 :
-http://www.apache.org/licenses/LICENSE-2.0.txt
-Developped by : Cedrick Facon
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/gpl.txt
+Developped by : Julien Mathis - Romain Le Merlus
 
 The Software is provided to you AS IS and WITH ALL FAULTS.
 OREON makes no representation and gives no warranty whatsoever,
