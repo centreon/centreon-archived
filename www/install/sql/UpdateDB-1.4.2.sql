36961924aba4e9877efbd17396a8c9097c198c8c
Mat Sugumaran <msugumaran@merethis.com>
2007-08-20 13:43:05 +0000

    updates for traps
    git-svn-id: http://svn.centreon.com/oreon-dev@3018 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/UpdateDB-1.4.2.sql
@@ -0,0 +1,24 @@
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `service_categories`
+--
+
+CREATE TABLE `service_categories` (
+`sc_id` INT NULL AUTO_INCREMENT PRIMARY KEY ,
+`sc_name` VARCHAR( 255 ) NULL ,
+`sc_description` VARCHAR( 255 ) NULL ,
+`sc_activate` ENUM( '0', '1') NULL
+) ENGINE = innodb COMMENT = 'Services Catégories For best Reporting';
+
+
+INSERT INTO `topology` (`topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show`) VALUES ('', 'm_categories', './img/icones/16x16/certificate.gif', 602, 60209, 90, 1, './include/configuration/configObject/service_categories/serviceCategories.php', NULL, '0', '0', '1');
+
+
+INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'a', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'w', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'c', './include/common/javascript/changetab.js', 'initChangeTab');
+
+-- 20/08/2007
+ALTER TABLE `traps` DROP INDEX `traps_name`;
+ALTER TABLE `traps` ADD UNIQUE (`traps_oid`);
