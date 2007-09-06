c50646c63c5908258907f46b914a2182c81c7f3e
Julien Mathis <jmathis@merethis.com>
2007-09-06 16:39:35 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@3084 6bcd3966-0018-0410-8128-fd23d134de7e

--- ODS/lib/indentifyService.pm
+++ ODS/lib/indentifyService.pm
@@ -47,14 +47,14 @@ sub identify_service($$){
 					}
 				}
 			}
-		    $sth1 = $con_ods->prepare("SELECT id, storage_type FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
+		    $sth1 = $con_ods->prepare("SELECT id,storage_type,must_be_rebuild,locked FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
 		    if (!$sth1->execute) {writeLogFile("Error:" . $sth1->errstr . "\n");}
 	    }
 	    undef($host_id);
 	    undef($service_id);
 	    my $data = $sth1->fetchrow_hashref();
 	    undef($sth1);
-	    my @data_return = ($data->{'id'}, $data->{'storage_type'}, $data->{'must_be_rebuild'});
+	    my @data_return = ($data->{'id'}, $data->{'storage_type'}, $data->{'must_be_rebuild'}, , $data->{'locked'});
 	    undef($data);
 	    return @data_return;
 	}
@@ -63,7 +63,7 @@ sub identify_service($$){
 sub identify_hidden_service($$){
 	while (!$con_ods->ping){;}
 	if ($con_ods->ping){
-		my $sth1 = $con_ods->prepare("SELECT id, storage_type, must_be_rebuild FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
+		my $sth1 = $con_ods->prepare("SELECT id, storage_type, must_be_rebuild, locked FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
 	    if (!$sth1->execute) {writeLogFile("Error : " . $sth1->errstr . "\n");}
 	    # IF service unknown, insert it.
 	    if ($sth1->rows() == 0){
@@ -72,7 +72,7 @@ sub identify_hidden_service($$){
 				if (!$sth1->execute) {writeLogFile("Error : " . $sth1->errstr . "\n");}
 				undef($sth1);
 			}
-		    $sth1 = $con_ods->prepare("SELECT id, storage_type FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
+		    $sth1 = $con_ods->prepare("SELECT id, storage_type, must_be_rebuild, locked FROM index_data WHERE host_name = '".$_[0]."' AND service_description = '".$_[1]."'");
 		    if (!$sth1->execute) {writeLogFile("Error : " . $sth1->errstr . "\n");}
 	    }
 	    my $data = $sth1->fetchrow_hashref();
