c30f04c8b6e99785084c976b3933cf2830a98fdf
Cedrick Facon <cfacon@merethis.com>
2007-09-12 07:43:21 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@3091 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.1_to_1.4.2.sql
+++ www/install/sql/UpdateDB-1.4.1_to_1.4.2.sql
@@ -9,12 +9,21 @@ CREATE TABLE `service_categories` (
 `sc_name` VARCHAR( 255 ) NULL ,
 `sc_description` VARCHAR( 255 ) NULL ,
 `sc_activate` ENUM( '0', '1') NULL
-) ENGINE = innodb COMMENT = 'Services Catégories For best Reporting';
+) ENGINE = innodb COMMENT = 'Services Catï¿½gories For best Reporting';
 
 
 INSERT INTO `topology` (`topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show`) VALUES ('', 'm_categories', './img/icones/16x16/certificate.gif', 602, 60209, 90, 1, './include/configuration/configObject/service_categories/serviceCategories.php', NULL, '0', '0', '1');
 
-
 INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'a', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'w', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', '60703', 'c', './include/common/javascript/changetab.js', 'initChangeTab');
+
+--
+-- Reporting
+--
+
+INSERT INTO `topology` (`topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show` , `topology_style_class` , `topology_style_id` , `topology_OnClick`) VALUES ('', 'm_dashboardHostGroup', './img/icones/16x16/text_rich_colored.gif', 307, 30703, 20, 50, './include/reporting/dashboard/viewHostGroupLog.php', NULL, '0', '0', '1', NULL , NULL , NULL);
+INSERT INTO `topology` (`topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show` , `topology_style_class` , `topology_style_id` , `topology_OnClick`) VALUES ('', 'm_dashboardServiceGroup', './img/icones/16x16/text_rich_colored.gif', 307, 30704, 20, 50, './include/reporting/dashboard/viewServicesGroupLog.php', NULL, '0', '0', '1', NULL , NULL , NULL);
+
+INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', 30703, NULL, './include/common/javascript/Timeline/src/main/webapp/api/timeline-api.js', 'initTimeline');
+INSERT INTO `topology_JS` (`id_t_js` , `id_page` , `o` , `PathName_js` , `Init`) VALUES ('', 30704, NULL, './include/common/javascript/Timeline/src/main/webapp/api/timeline-api.js', 'initTimeline');
