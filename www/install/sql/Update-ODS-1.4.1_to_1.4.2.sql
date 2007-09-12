563c80ff3a369eaa5272e2baaf8b2331db1fbc0a
Julien Mathis <jmathis@merethis.com>
2007-09-12 17:43:37 +0000

    git-svn-id: http://svn.centreon.com/oreon-dev@3114 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-ODS-1.4.1_to_1.4.2.sql
+++ www/install/sql/Update-ODS-1.4.1_to_1.4.2.sql
@@ -1,4 +1,5 @@
 
+ALTER TABLE `index_data` CHANGE `must_be_rebuild` `must_be_rebuild` ENUM( '0', '1', '2' ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT '0' 
 ALTER TABLE `index_data` ADD `locked` ENUM('0' , '1') NULL DEFAULT '0' AFTER `trashed` ;
 
 ALTER TABLE `config` ADD `DailyReporting` ENUM( '0', '1' ) NULL DEFAULT '1',
@@ -9,3 +10,5 @@ ADD `Force_Reporting_Rebuild` ENUM( '0', '1' ) NULL DEFAULT '0';
 
 
 ALTER TABLE `metrics` ADD `hidden` ENUM( '0', '1' ) NULL DEFAULT '0';
+ALTER TABLE `metrics` ADD `min` INT(11) NULL DEFAULT '0' AFTER `crit`;
+ALTER TABLE `metrics` ADD `max` INT(11) NULL DEFAULT '0' AFTER `min`;
