0c57ea9e5b9e14451e5d35df59ed052c639d6d51
Julien Mathis <jmathis@merethis.com>
2007-09-21 08:06:36 +0000

    move menu entry
    git-svn-id: http://svn.centreon.com/oreon-dev@3187 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.1_to_1.4.2.sql
+++ www/install/sql/UpdateDB-1.4.1_to_1.4.2.sql
@@ -39,3 +39,6 @@ ALTER TABLE `general_opt` ADD `ndo_base_name` VARCHAR( 255 ) NOT NULL DEFAULT 'n
 --
 
 ALTER TABLE `general_opt` ADD `snmptt_unknowntrap_log_file` VARCHAR( 255 ) NULL AFTER `snmpttconvertmib_path_bin` ;
+
+
+UPDATE `centreon`.`topology` SET `topology_name` = 'menu_ODS_config',`topology_parent` = '506', `topology_page` = '50601', `topology_order` = '5' WHERE `topology`.`topology_page` = 5010110;
