5fd1f47333eb62c60abef3df7024c25cce0e7495
Cedrick Facon <cfacon@merethis.com>
2007-10-04 07:42:43 +0000

    NDO dev
    git-svn-id: http://svn.centreon.com/trunk/centreon@3316 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/MakeXML_Ndo_serviceGridBySG.php
+++ www/include/monitoring/engine/MakeXML_Ndo_serviceGridBySG.php
@@ -233,17 +233,16 @@ For information : contact@oreon-project.org
 	$tab_status_host = array("0" => "UP", "1" => "DOWN", "2" => "UNREACHABLE");
 
 
-
-
-
 	/* Get Host status */
 
 
-	$rq1 = "SELECT hg.alias, no.name1 as host_name, hgm.servicegroup_id, hgm.service_object_id, ss.current_state".
-			" FROM " .$general_opt["ndo_base_prefix"]."_servicegroups hg," .$general_opt["ndo_base_prefix"]."_servicegroup_members hgm, " .$general_opt["ndo_base_prefix"]."_servicestatus ss, " .$general_opt["ndo_base_prefix"]."_objects no".
-			" WHERE ss.service_object_id = hgm.service_object_id".
-			" AND no.object_id = hgm.service_object_id" .
-			" AND hgm.servicegroup_id = hg.servicegroup_id";
+	$rq1 = "SELECT sg.alias, no.name1 as host_name, no.name2 as service_description, sgm.servicegroup_id, sgm.service_object_id, ss.current_state".
+			" FROM " .$general_opt["ndo_base_prefix"]."_servicegroups sg," .$general_opt["ndo_base_prefix"]."_servicegroup_members sgm, " .$general_opt["ndo_base_prefix"]."_servicestatus ss, " .$general_opt["ndo_base_prefix"]."_objects no".
+			" WHERE sg.config_type = 0 " .
+			" AND ss.service_object_id = sgm.service_object_id".
+			" AND no.object_id = sgm.service_object_id" .
+			" AND sgm.servicegroup_id = sg.servicegroup_id";
+
 
 	if($o == "svcgridHG_pb" || $o == "svcOVHG_pb")
 		$rq1 .= " AND no.name1 IN (" .
@@ -275,24 +274,30 @@ For information : contact@oreon-project.org
 		print "DB Error : ".$DBRESULT_PAGINATION->getDebugInfo()."<br>";	
 	$numRows = $DBRESULT_PAGINATION->numRows();
 	/* End Pagination Rows */
+	
+
+	$rq1 .= " ORDER BY sg.alias, host_name";
+
+//	$rq1 .= " LIMIT ".($num * $limit).",".$limit;
 
 
-	$rq1 .= " ORDER BY hg.alias";
-	$rq1 .= " LIMIT ".($num * $limit).",".$limit;
 
 	$buffer .= '<reponse>';
+
 	$buffer .= '<i>';
 	$buffer .= '<numrows>'.$numRows.'</numrows>';
 	$buffer .= '<num>'.$num.'</num>';
 	$buffer .= '<limit>'.$limit.'</limit>';
 	$buffer .= '<p>'.$p.'</p>';
 
-	if($o == "svcOVHG")
+	if($o == "svcOVSG")
 		$buffer .= '<s>1</s>';
 	else
 		$buffer .= '<s>0</s>';
 	
 	$buffer .= '</i>';
+
+
 	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
 	if (PEAR::isError($DBRESULT_NDO1))
 		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br>";	
@@ -300,51 +305,54 @@ For information : contact@oreon-project.org
 	$ct = 0;
 	$flag = 0;
 
+	$sg = "";
+	$h = "";
+	$flag = 0;
 
-	$tab_final = array();
-	while($DBRESULT_NDO1->fetchInto($ndo))
+	while($DBRESULT_NDO1->fetchInto($tab))
 	{
-		$tab_svc = get_services($ndo["host_name"]);
-		$tab_final[$ndo["host_name"]]["tab_svc"] = $tab_svc;
-		$tab_final[$ndo["host_name"]]["cs"] = $ndo["current_state"];
-		$tab_final[$ndo["host_name"]]["hg_name"] = $ndo["alias"];
-	}
 
-	$hg = "";
-	foreach($tab_final as $host_name => $tab)
-	{
-		if($hg != $tab["hg_name"]){
+		if($sg != $tab["alias"]){
+			$flag = 0;
+			if($sg != "")
+				$buffer .= '</h></sg>';
 
-			if($hg != "")
-				$buffer .= '</hg>';
-
-			$hg = $tab["hg_name"];
-			$buffer .= '<hg>';
-			$buffer .= '<hgn>'. $tab["hg_name"]  .'</hgn>';
+			$sg = $tab["alias"];
+			$buffer .= '<sg class="'.$class.'">';
+			$buffer .= '<sgn>'. $tab["alias"]  .'</sgn>';
+			$buffer .= '<o>'. $ct . '</o>';
 		}
-		$buffer .= '<l class="'.$class.'">';
-		foreach ($tab["tab_svc"] as $svc => $state) {
-			$buffer .= '<svc>';
-			$buffer .= '<sn>'. $svc . '</sn>';
-			$buffer .= '<sc>'. $tab_color_service[$state] . '</sc>';
-			$buffer .= '</svc>';
+		$ct++;
+
+		if($h != $tab["host_name"]){
+			if($h != "" && $flag)
+				$buffer .= '</h>';
+			$flag = 1;
+			$h = $tab["host_name"];
+			$buffer .= '<h>';
+			$buffer .= '<hn>'. $tab["host_name"]  . '</hn>';
+			$buffer .= '<hs>'. $tab_status_host[$tab["current_state"]]  . '</hs>';
+			$buffer .= '<hc>'. $tab_color_host[$tab["current_state"]]  . '</hc>';
 		}
-		$buffer .= '<o>'. $ct++ . '</o>';
-		$buffer .= '<hn>'. $host_name  . '</hn>';
-		$buffer .= '<hs>'. $tab_status_host[$tab["cs"]]  . '</hs>';
-		$buffer .= '<hc>'. $tab_color_host[$tab["cs"]]  . '</hc>';
-		$buffer .= '</l>';
+
+
+
+		$buffer .= '<svc>';
+		$buffer .= '<sn>'. $tab["service_description"] . '</sn>';
+		$buffer .= '<sc>'. $tab_color_service[$tab["current_state"]] . '</sc>';
+		$buffer .= '</svc>';
+
+
 	}
-	if($hg != "")
-	$buffer .= '</hg>';
-	/* end */
-		
-	if(!$ct){
+	if($sg != "")
+		$buffer .= '</h></sg>';
+	
+/*
 		$buffer .= '<infos>';
 		$buffer .= 'none';
 		$buffer .= '</infos>';
 	}
-	
+*/	
 	$buffer .= '</reponse>';
 	header('Content-Type: text/xml');
 	echo $buffer;
