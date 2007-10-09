bb8fce943128b7f119aaee5fc7a2145c7e0315b8
Cedrick Facon <cfacon@merethis.com>
2007-10-09 11:42:05 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@3328 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/UpdateDB-1.4.2.sql
+++ www/install/sql/UpdateDB-1.4.2.sql
@@ -9,7 +9,7 @@ CREATE TABLE `service_categories` (
 `sc_name` VARCHAR( 255 ) NULL ,
 `sc_description` VARCHAR( 255 ) NULL ,
 `sc_activate` ENUM( '0', '1') NULL
-) ENGINE = innodb COMMENT = 'Services Catégories For best Reporting';
+) ENGINE = innodb COMMENT = 'Services Catï¿½gories For best Reporting';
 
 
 INSERT INTO `topology` (`topology_id` , `topology_name` , `topology_icone` , `topology_parent` , `topology_page` , `topology_order` , `topology_group` , `topology_url` , `topology_url_opt` , `topology_popup` , `topology_modules` , `topology_show`) VALUES ('', 'm_categories', './img/icones/16x16/certificate.gif', 602, 60209, 90, 1, './include/configuration/configObject/service_categories/serviceCategories.php', NULL, '0', '0', '1');
@@ -25,3 +25,15 @@ ALTER TABLE `traps` ADD UNIQUE (`traps_oid`);
 
 -- 24/08/2007
 ALTER TABLE `general_opt` ADD `snmptt_unknowntrap_log_file` VARCHAR( 255 ) NULL AFTER `snmpttconvertmib_path_bin` ;
+
+
+-- 09/10/2007
+-- NDO view
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '20212', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021201', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021202', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021203', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '20213', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021301', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021302', NULL , NULL , 'initM');
+INSERT INTO `centreon`.`topology_JS` (`id_t_js` ,`id_page` ,`o` ,`PathName_js` ,`Init`) VALUES (NULL , '2021303', NULL , NULL , 'initM');
