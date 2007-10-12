21fcd83f2a19560df59f13336551c73a22ee9ca1
Julien Mathis <jmathis@merethis.com>
2007-10-12 17:52:39 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@3343 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/configuration/configNdomod/configNdo2db/listNdo2db.php
@@ -0,0 +1,146 @@
+<?
+/**
+Oreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+Developped by : Julien Mathis - Romain Le Merlus
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+	
+	if (!isset($oreon))
+		exit();
+		
+	include("./include/common/autoNumLimit.php");
+	
+	if (isset($search))
+		$DBRESULT = & $pearDB->query("SELECT COUNT(*) FROM `cfg_ndo2db` WHERE description LIKE '%".htmlentities($search, ENT_QUOTES)."%'");
+	else
+		$DBRESULT = & $pearDB->query("SELECT COUNT(*) FROM `cfg_ndo2db`");
+	if (PEAR::isError($DBRESULT))
+		print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+
+	$tmp = & $DBRESULT->fetchRow();
+	$rows = $tmp["COUNT(*)"];
+
+	# nagios servers comes from DB 
+	$nagios_servers = array();
+	$DBRESULT =& $pearDB->query("SELECT * FROM nagios_server ORDER BY name");
+	if (PEAR::isError($DBRESULT))
+		print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+	while($nagios_server = $DBRESULT->fetchRow())
+		$nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
+	$DBRESULT->free();
+
+	# start quickSearch form
+	include_once("./include/common/quickSearch.php");
+	# end quickSearch form
+
+	include("./include/common/checkPagination.php");
+
+	# Smarty template Init
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl($path, $tpl);
+
+	# start header menu
+	$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
+	$tpl->assign("headerMenu_name", $lang['description']);
+	$tpl->assign("headerMenu_desc", $lang['n2db_nagios_server']);
+	$tpl->assign("headerMenu_status", $lang['status']);
+	$tpl->assign("headerMenu_options", $lang['options']);
+	# end header menu
+	#Nagios list
+	if ($search)
+		$rq = "SELECT id, description, ns_nagios_server, activate FROM cfg_ndo2db WHERE description LIKE '%".htmlentities($search, ENT_QUOTES)."%' ORDER BY description LIMIT ".$num * $limit.", ".$limit;
+	else
+		$rq = "SELECT id, description, ns_nagios_server, activate FROM cfg_ndo2db ORDER BY description LIMIT ".$num * $limit.", ".$limit;
+	$DBRESULT =& $pearDB->query($rq);
+	if (PEAR::isError($DBRESULT))
+		print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+	
+	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
+	#Different style between each lines
+	$style = "one";
+	#Fill a tab with a mutlidimensionnal Array we put in $tpl
+	$elemArr = array();
+	for ($i = 0; $config = $DBRESULT->fetchRow(); $i++) {		
+		$selectedElements =& $form->addElement('checkbox', "select[".$config['id']."]");	
+		$moptions = "<a href='oreon.php?p=".$p."&nagios_id=".$config['id']."&o=w&search=".$search."'><img src='img/icones/16x16/view.gif' border='0' alt='".$lang['view']."'></a>&nbsp;&nbsp;";
+		$moptions .= "<a href='oreon.php?p=".$p."&id=".$config['id']."&o=c&search=".$search."'><img src='img/icones/16x16/document_edit.gif' border='0' alt='".$lang['modify']."'></a>&nbsp;&nbsp;";
+		$moptions .= "<a href='oreon.php?p=".$p."&id=".$config['id']."&o=d&select[".$config['id']."]=1&num=".$num."&limit=".$limit."&search=".$search."' onclick=\"return confirm('".$lang['confirm_removing']."')\"><img src='img/icones/16x16/delete.gif' border='0' alt='".$lang['delete']."'></a>&nbsp;&nbsp;";
+		if ($config["activate"])
+			$moptions .= "<a href='oreon.php?p=".$p."&id=".$config['id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='".$lang['disable']."'></a>&nbsp;&nbsp;";
+		else
+			$moptions .= "<a href='oreon.php?p=".$p."&id=".$config['id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='".$lang['enable']."'></a>&nbsp;&nbsp;";
+		$moptions .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$config['id']."]'></input>";
+		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+						"RowMenu_select"=>$selectedElements->toHtml(),
+						"RowMenu_name"=>$config["description"],
+						"RowMenu_link"=>"?p=".$p."&o=c&id=".$config['id'],
+						"RowMenu_desc"=>substr($nagios_servers[$config["ns_nagios_server"]], 0, 40),
+						"RowMenu_status"=>$config["activate"] ? $lang['enable'] : $lang['disable'],
+						"RowMenu_options"=>$moptions);
+		$style != "two" ? $style = "two" : $style = "one";	
+	}
+	$tpl->assign("elemArr", $elemArr);
+	#Different messages we put in the template
+	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>$lang['add'], "delConfirm"=>$lang['confirm_removing']));
+
+	#
+	##Toolbar select $lang["lgd_more_actions"]
+	#
+	?>
+	<SCRIPT LANGUAGE="JavaScript">
+	function setO(_i) {
+		document.forms['form'].elements['o'].value = _i;
+	}
+	</SCRIPT>
+	<?
+	$attrs = array(
+		'onchange'=>"javascript: " .
+				"if (this.form.elements['o1'].selectedIndex == 1 && confirm('".$lang['confirm_duplication']."')) {" .
+				" 	setO(this.form.elements['o1'].value); submit();} " .
+				"else if (this.form.elements['o1'].selectedIndex == 2 && confirm('".$lang['confirm_removing']."')) {" .
+				" 	setO(this.form.elements['o1'].value); submit();} " .
+				"else if (this.form.elements['o1'].selectedIndex == 3) {" .
+				" 	setO(this.form.elements['o1'].value); submit();} " .
+				"");	  
+        $form->addElement('select', 'o1', NULL, array(NULL=>$lang["lgd_more_actions"], "m"=>$lang['dup'], "d"=>$lang['delete']/*, "mc"=>$lang['mchange']*/), $attrs);
+	$form->setDefaults(array('o1' => NULL));
+			$o1 =& $form->getElement('o1');
+		$o1->setValue(NULL);
+	
+	$attrs = array(
+		'onchange'=>"javascript: " .
+				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('".$lang['confirm_duplication']."')) {" .
+				" 	setO(this.form.elements['o2'].value); submit();} " .
+				"else if (this.form.elements['o2'].selectedIndex == 2 && confirm('".$lang['confirm_removing']."')) {" .
+				" 	setO(this.form.elements['o2'].value); submit();} " .
+				"else if (this.form.elements['o2'].selectedIndex == 3) {" .
+				" 	setO(this.form.elements['o2'].value); submit();} " .
+				"");
+    $form->addElement('select', 'o2', NULL, array(NULL=>$lang["lgd_more_actions"], "m"=>$lang['dup'], "d"=>$lang['delete']/*, "mc"=>$lang['mchange']*/), $attrs);
+	$form->setDefaults(array('o2' => NULL));
+
+		$o2 =& $form->getElement('o2');
+		$o2->setValue(NULL);
+	
+	$tpl->assign('limit', $limit);
+
+	#
+	##Apply a template definition
+	#	
+	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+	$form->accept($renderer);	
+	$tpl->assign('form', $renderer->toArray());
+	$tpl->display("listNdo2db.ihtml");
+?>
