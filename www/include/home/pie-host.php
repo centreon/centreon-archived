30e62e9165605e7fb527f1ce84117cdadac762e0
Julien Mathis <jmathis@merethis.com>
2008-01-23 18:55:18 +0000

    add new charts
    git-svn-id: http://svn.centreon.com/trunk/centreon@3819 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/pie-host.php
+++ www/include/home/pie-host.php
@@ -6,7 +6,53 @@
 	Session::start();
 	$oreon =& $_SESSION["oreon"];
 
+	$ndo_base_prefix = "nagios";
+	$oreonPath = '/srv/oreon/';
 
+	## pearDB init
+	require_once 'DB.php';	
+
+	include_once($oreonPath . "etc/centreon.conf.php");
+	include_once($oreonPath . "www/include/common/common-Func-ACL.php");
+
+	/* Connect to oreon DB */
+	
+	$dsn = array('phptype'  => 'mysql',
+			     'username' => $conf_oreon['user'],
+			     'password' => $conf_oreon['password'],
+			     'hostspec' => $conf_oreon['host'],
+			     'database' => $conf_oreon['db'],);	
+	$options = array('debug'=> 2, 'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE,);	
+
+	$pearDB =& DB::connect($dsn, $options);
+	if (PEAR::isError($pearDB)) die("Connecting problems with oreon database : " . $pearDB->getMessage());
+	$pearDB->setFetchMode(DB_FETCHMODE_ASSOC);
+	
+	## calcul stat for resume
+	$statistic_host = array("UP" => 0, "DOWN" => 0, "UNREACHABLE" => 0, "PENDING" => 0);
+
+	include_once($oreonPath . "www/DBndoConnect.php");
+
+	## calcul stat for resume
+	$statistic_host = array(0 => "UP", 1 => "DOWN", 2 => "UNREACHABLE",3 => "PENDING");
+	
+	/* Get HostNDO status */
+	$rq1 = "SELECT count(nhs.current_state) as cnt, nhs.current_state" .
+			" FROM ".$ndo_base_prefix."_hoststatus nhs, ".$ndo_base_prefix."_objects no" .
+			" WHERE no.object_id = nhs.host_object_id AND no.is_active = 1 GROUP BY nhs.current_state ORDER by nhs.current_state";
+
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br>";
+	$data = array();
+	$color = array();
+	while($DBRESULT_NDO1->fetchInto($ndo)){
+		$data[] = $ndo["cnt"];
+		$legend[] = $statistic_host[$ndo["current_state"]];
+		$color[] = $oreon->optGen["color_".strtolower($statistic_host[$ndo["current_state"]])];		
+	}
+
+/*
 	// create the dataset
 	$data = array();
 	$color = array();
@@ -18,7 +64,7 @@
 			$color[] = $oreon->optGen["color_".strtolower($key)];		
 		}			
 	}
-
+*/
 	include_once( '/usr/local/centreon/www/lib/ofc-library/open-flash-chart.php' );
 	$g = new graph();
 	$g->bg_colour = '#F3F6F6';
--- www/include/home/pie-svc.php
+++ www/include/home/pie-svc.php
@@ -6,19 +6,55 @@
 	Session::start();
 	$oreon =& $_SESSION["oreon"];
 
+	$ndo_base_prefix = "nagios";
+	$oreonPath = '/srv/oreon/';
 
-	// create the dataset
+	## pearDB init
+	require_once 'DB.php';	
+
+	include_once($oreonPath . "etc/centreon.conf.php");
+	include_once($oreonPath . "www/include/common/common-Func-ACL.php");
+
+	/* Connect to oreon DB */
+	
+	$dsn = array('phptype'  => 'mysql',
+			     'username' => $conf_oreon['user'],
+			     'password' => $conf_oreon['password'],
+			     'hostspec' => $conf_oreon['host'],
+			     'database' => $conf_oreon['db'],);	
+	$options = array('debug'=> 2, 'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE,);	
+
+	$pearDB =& DB::connect($dsn, $options);
+	if (PEAR::isError($pearDB)) die("Connecting problems with oreon database : " . $pearDB->getMessage());
+	$pearDB->setFetchMode(DB_FETCHMODE_ASSOC);
+	
+	include_once($oreonPath . "www/DBndoConnect.php");
+
+	## calcul stat for resume
+	$statistic = array(0 => "OK", 1 => "WARNING", 2 => "CRITICAL", 3 => "UNKNOWN", 4 => "PENDING");
+	
+	/* Get HostNDO status */
+	/* Get ServiceNDO status */
+	$rq1 = "SELECT count(nss.current_state) as cnt, nss.current_state" .
+			" FROM ".$ndo_base_prefix."_servicestatus nss, ".$ndo_base_prefix."_objects no" .
+			" WHERE no.object_id = nss.service_object_id".
+			" AND no.name1 not like 'OSL_Module'".
+			" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
+	
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br>";
 	$data = array();
 	$color = array();
-	
-	foreach ($oreon->status_graph_service as $key => $value){
-		if ($value != 0){
-			$data[] = $value;
-			$legend[] = $key;
-			$color[] = $oreon->optGen["color_".strtolower($key)];		
-		}			
+	while($DBRESULT_NDO1->fetchInto($ndo)){
+		$data[] = $ndo["cnt"];
+		$legend[] = $statistic[$ndo["current_state"]];
+		$color[] = $oreon->optGen["color_".strtolower($statistic[$ndo["current_state"]])];		
 	}
-
+	/*
+	 *  create the dataset
+	 */
+	
 	include_once( '/usr/local/centreon/www/lib/ofc-library/open-flash-chart.php' );
 	$g = new graph();
 	$g->bg_colour = '#F3F6F6';
@@ -30,7 +66,7 @@
 	// pass in two arrays, one of data, the other data labels
 	//
 	
-	$g->pie_values( $data, $legend );
+	$g->pie_values($data, $legend);
 	//
 	// Colours for each slice, in this case some of the colours
 	// will be re-used (3 colurs for 5 slices means the last two
