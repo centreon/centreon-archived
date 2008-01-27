f1d843c89b69bde321e2ed9beaca8eb5b5fbc1e8
Julien Mathis <jmathis@merethis.com>
2008-01-27 17:41:52 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@3841 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/reporting/dashboard/pie-charts.php
@@ -0,0 +1,66 @@
+<?php
+/**
+Centreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+	require_once ("../../../class/Session.class.php");
+	require_once ("../../../class/Oreon.class.php");
+
+	Session::start();
+	$oreon =& $_SESSION["oreon"];
+	
+
+	// -----------------------------------------------------
+	$value =& $_GET["value"];
+	
+	foreach ($value as $key => $val)	{
+		if ($val)
+			if (!isset($oreon->optGen["color_".strtolower($key)])) {
+				print $val;
+				$color[] = $oreon->optGen["color_unknown"];
+				$data[] = $val;
+				$legend[] = "Undeterminated";
+			} else {
+				$color[] = $oreon->optGen["color_".strtolower($key)];		
+				$data[] = $val;
+				$legend[] =$key;
+			}
+	}
+	include_once( '/usr/local/centreon/www/lib/ofc-library/open-flash-chart.php' );
+	$g = new graph();
+	$g->bg_colour = '#F3F6F6';
+	//
+	// PIE chart, 60% alpha
+	//
+	$g->pie(60,'#505050','#000000');
+	//
+	// pass in two arrays, one of data, the other data labels
+	//
+	
+	$g->pie_values( $data, $legend );
+	//
+	// Colours for each slice, in this case some of the colours
+	// will be re-used (3 colurs for 5 slices means the last two
+	// slices will have colours colour[0] and colour[1]):
+	//
+	
+	$g->pie_slice_colours($color);
+
+	$g->set_tool_tip( '#val#%' );
+	$g->title( $_GET["host_name"], '{font-size:18px; color: #424242}' );
+	echo $g->render();
+
+?>
