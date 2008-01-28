407e4e3b4d6fe6c88b47624d37e9b21fc9d5fcd0
Julien Mathis <jmathis@merethis.com>
2008-01-28 15:10:28 +0000

    new style on LCA
    git-svn-id: http://svn.centreon.com/trunk/centreon@3849 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/LCA/define/listLCA.php
+++ www/include/options/LCA/define/listLCA.php
@@ -71,16 +71,13 @@ For information : contact@oreon-project.org
 	#Fill a tab with a mutlidimensionnal Array we put in $tpl
 	$elemArr = array();
 	for ($i = 0; $DBRESULT->fetchInto($lca); $i++) {		
+		$moptions = "";
 		$selectedElements =& $form->addElement('checkbox', "select[".$lca['lca_id']."]");	
-		$moptions = "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=w&search=".$search."'><img src='img/icones/16x16/view.gif' border='0' alt='".$lang['view']."'></a>&nbsp;&nbsp;";
-		$moptions .= "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=c&search=".$search."'><img src='img/icones/16x16/document_edit.gif' border='0' alt='".$lang['modify']."'></a>&nbsp;&nbsp;";
-		$moptions .= "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=d&select[".$lca['lca_id']."]=1&num=".$num."&limit=".$limit."&search=".$search."' onclick=\"return confirm('".$lang['confirm_removing']."')\"><img src='img/icones/16x16/delete.gif' border='0' alt='".$lang['delete']."'></a>&nbsp;&nbsp;";
 		if ($lca["lca_activate"])
 			$moptions .= "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='".$lang['disable']."'></a>&nbsp;&nbsp;";
 		else
 			$moptions .= "<a href='oreon.php?p=".$p."&lca_id=".$lca['lca_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='".$lang['enable']."'></a>&nbsp;&nbsp;";
-		$moptions .= "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$lca['lca_id']."]'></input>";
+		$moptions .= "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$lca['lca_id']."]'></input>";
 
 		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
 						"RowMenu_select"=>$selectedElements->toHtml(),
