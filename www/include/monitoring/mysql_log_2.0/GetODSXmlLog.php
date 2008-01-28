7911c6676c073ec5c834e3f8465e38eb4ef80522
Cedrick Facon <cfacon@merethis.com>
2008-01-28 18:33:25 +0000

    2.0 version beta treeview for graph and log
    git-svn-id: http://svn.centreon.com/trunk/centreon@3852 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/monitoring/mysql_log_2.0/GetODSXmlLog.php
@@ -0,0 +1,549 @@
+<?php
+/**
+Created on 23 janv. 08
+
+Centreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+Developped by : Cedrick Facon
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+
+	function get_error($motif){
+		$buffer = null;
+		$buffer .= '<reponse>';
+		$buffer .= $motif;
+		$buffer .= '</reponse>';
+		header('Content-Type: text/xml');
+		echo $buffer;
+		exit(0);
+	}
+
+	function check_injection($g){
+		if ( eregi("(<|>|;|UNION|ALL|OR|AND|ORDER|SELECT|WHERE)", $g)) {
+			get_error('sql injection detected');
+			return 1;
+		}
+		return 0;
+	}
+
+if ( stristr($_SERVER["HTTP_ACCEPT"],"application/xhtml+xml") )
+{
+	header("Content-type: application/xhtml+xml"); }
+else
+{
+	header("Content-type: text/xml");
+}
+echo("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n");
+
+
+
+/*
+ * Start document root
+ */
+ echo "<root>";
+
+
+
+# if debug == 0 => Normal, debug == 1 => get use, debug == 2 => log in file (log.xml)
+$debugXML = 0;
+$buffer = '';
+
+$oreonPath = '../../../../';
+
+
+
+/*
+ * pearDB init
+ */ 
+require_once 'DB.php';
+
+include_once($oreonPath . "etc/centreon.conf.php");
+include_once($oreonPath . "www/DBconnect.php");
+include_once($oreonPath . "www/DBOdsConnect.php");
+
+
+/*
+ * PHP functions
+ */
+include_once($oreonPath . "www/include/common/common-Func-ACL.php");
+include_once($oreonPath . "www/include/common/common-Func.php");
+
+/*
+ * Lang file
+ */
+	if(isset($_GET["lang"]) && !check_injection($_GET["lang"])){
+		$lang_ = htmlentities($_GET["lang"]);
+	}else
+		$lang_ = "-1";
+
+	is_file ("../lang/".$lang_.".php") ? include_once ("../lang/".$lang_.".php") : include_once ("../lang/en.php");
+	is_file ("../../../lang/".$lang_.".php") ? include_once ("../../../lang/".$lang_.".php") : include_once ("../../../lang/en.php");
+
+	function getMyHostIDService($svc_id = NULL)	{
+		if (!$svc_id) return;
+		global $pearDB;
+		$DBRESULT =& $pearDB->query("SELECT host_id FROM host h, host_service_relation hs WHERE h.host_id = hs.host_host_id AND hs.service_service_id = '".$svc_id."'");
+		if (PEAR::isError($DBRESULT))
+			print "DB Error : ".$DBRESULT->getDebugInfo()."<br>";
+		if ($DBRESULT->numRows())	{
+			$row =& $DBRESULT->fetchRow();
+			return $row["host_id"];
+		}
+		return NULL;
+	}
+
+
+	if(isset($_GET["id"]) && !check_injection($_GET["id"])){
+		$openid = htmlentities($_GET["id"]);
+	}else
+		$openid = "-1";
+
+
+	$id = substr($openid, 3, strlen($openid));
+	$type = substr($openid, 0, 2);
+
+
+	if(isset($_GET["sid"]) && !check_injection($_GET["sid"])){
+		$sid = htmlentities($_GET["sid"]);
+	}else
+		$sid = "-1";
+
+	if(isset($_GET["num"]) && !check_injection($_GET["num"])){
+		$num = htmlentities($_GET["num"]);
+	}else
+		$num = "0";
+
+	if(isset($_GET["limit"]) && !check_injection($_GET["limit"])){
+		$limit = htmlentities($_GET["limit"]);
+	}else
+		$limit = "30";
+
+
+	if(isset($_GET["host"]) && !check_injection($_GET["host"])){
+		$host = htmlentities($_GET["host"]);
+	}else
+		$host = "1";
+
+	if(isset($_GET["service"]) && !check_injection($_GET["service"])){
+		$service = htmlentities($_GET["service"]);
+	}else
+		$service = "1";
+
+
+
+	if(isset($_GET["up"]) && !check_injection($_GET["up"])){
+		$up = htmlentities($_GET["up"]);
+	}else
+		$up = "1";
+	if(isset($_GET["down"]) && !check_injection($_GET["down"])){
+		$down = htmlentities($_GET["down"]);
+	}else
+		$down = "1";
+
+	if(isset($_GET["unreachable"]) && !check_injection($_GET["unreachable"])){
+		$unreachable = htmlentities($_GET["unreachable"]);
+	}else
+		$unreachable = "1";
+
+	if(isset($_GET["ok"]) && !check_injection($_GET["ok"])){
+		$ok = htmlentities($_GET["ok"]);
+	}else
+		$ok = "1";
+	if(isset($_GET["warning"]) && !check_injection($_GET["warning"])){
+		$warning = htmlentities($_GET["warning"]);
+	}else
+		$warning = "1";
+	if(isset($_GET["critical"]) && !check_injection($_GET["critical"])){
+		$critical = htmlentities($_GET["critical"]);
+	}else
+		$critical = "1";
+	if(isset($_GET["unknown"]) && !check_injection($_GET["unknown"])){
+		$unknown = htmlentities($_GET["unknown"]);
+	}else
+		$unknown = "1";
+
+
+
+
+	if(isset($_GET["notification"]) && !check_injection($_GET["notification"])){
+		$notification = htmlentities($_GET["notification"]);
+	}else
+		$notification = "0";
+		
+	if(isset($_GET["alert"]) && !check_injection($_GET["alert"])){
+		$alert = htmlentities($_GET["alert"]);
+	}else
+		$alert = "0";
+		
+	if(isset($_GET["error"]) && !check_injection($_GET["error"])){
+		$error = htmlentities($_GET["error"]);
+	}else
+		$error = "0";
+
+
+	if(isset($_GET["StartDate"]) && !check_injection($_GET["StartDate"])){
+		$StartDate = htmlentities($_GET["StartDate"]);
+	}else
+		$StartDate = "";
+	if(isset($_GET["EndDate"]) && !check_injection($_GET["EndDate"])){
+		$EndDate = htmlentities($_GET["EndDate"]);
+	}else
+		$EndDate = "";
+
+	if(isset($_GET["StartTime"]) && !check_injection($_GET["StartTime"])){
+		$StartTime = htmlentities($_GET["StartTime"]);
+	}else
+		$StartTime = "";
+	if(isset($_GET["EndTime"]) && !check_injection($_GET["EndTime"])){
+		$EndTime = htmlentities($_GET["EndTime"]);
+	}else
+		$EndTime = "";
+
+
+	if(isset($_GET["period"]) && !check_injection($_GET["period"])){
+		$auto_period = htmlentities($_GET["period"]);
+	}else
+		$auto_period = "-1";
+
+
+
+//$oreon->user->user_id
+$contact_id = '2';
+
+	if ($StartDate !=  "" && $StartTime != ""){
+		preg_match("/^([0-9]*)\/([0-9]*)\/([0-9]*)/", $StartDate, $matchesD);
+		preg_match("/^([0-9]*):([0-9]*)/", $StartTime, $matchesT);
+		$start = mktime($matchesT[1], $matchesT[2], "0", $matchesD[1], $matchesD[2], $matchesD[3], 1) ;
+	}
+	if ($EndDate !=  "" && $EndTime != ""){
+		preg_match("/^([0-9]*)\/([0-9]*)\/([0-9]*)/", $EndDate, $matchesD);
+		preg_match("/^([0-9]*):([0-9]*)/", $EndTime, $matchesT);
+		$end = mktime($matchesT[1], $matchesT[2], "0", $matchesD[1], $matchesD[2], $matchesD[3], 1) ;
+	}
+
+
+	$period = 86400;
+	if($auto_period > 0)
+		$period = $auto_period;
+
+	if (!isset($start) && !isset($end)){
+/*
+		$start = time() - ($day + 30);
+		$end = time() + 10;*/
+		$start = time() - ($period);
+		$end = time();
+	}			
+	
+	
+	$tab_type = array("1" => "HARD", "0" => "SOFT");
+	$tab_class = array("0" => "list_one", "1" => "list_two");
+	$tab_status_host = array("0" => "UP", "1" => "DOWN", "2" => "UNREACHABLE");
+	$tab_status_service = array("0" => "OK", "1" => "WARNING", "2" => "CRITICAL", "3" => "UNKNOWN");
+
+	$logs = array();	
+	if (isset($_GET["search1"]) && $_GET["search1"])
+		$sort_str1 = " AND (`host_name` LIKE '%".$_GET["search1"]."%' OR `service_description` LIKE '%".$_GET["search1"]."%' OR `output` LIKE '%".$_GET["search1"]."%' OR `notification_cmd` LIKE '%".$_GET["search1"]."%' OR `notification_contact` LIKE '%".$_GET["search1"]."%')";
+	else 
+		$sort_str1 = "";
+
+
+/*
+ * tmp
+ */
+
+$msg_type_set = array ();
+
+if($alert && $service){
+	array_push ($msg_type_set, "'0'");
+}
+if($alert && $host){
+	array_push ($msg_type_set, "'1'");
+}
+if($notification && $service){
+	array_push ($msg_type_set, "'2'");
+}
+if($notification && $host){
+	array_push ($msg_type_set, "'3'");
+}
+if($error)
+array_push ($msg_type_set, "'4'");
+
+$msg_req='';
+if( count($msg_type_set) > 0 )
+	$msg_req .= ' AND msg_type IN (' . implode(",",$msg_type_set). ') ';
+
+$msg_status_set = array ();
+
+
+if($error){
+	array_push ($msg_status_set, "'NULL'");
+}
+
+if($up && $host){
+	array_push ($msg_status_set, "'UP'");
+}
+if($down && $host){
+	array_push ($msg_status_set, "'DOWN'");
+}
+if($unreachable && $host){
+	array_push ($msg_status_set, "'UNREACHABLE'");
+}
+
+if($ok && $service){
+	array_push ($msg_status_set, "'ok'");
+}
+if($warning && $service){
+	array_push ($msg_status_set, "'warning'");
+}
+if($critical && $service){
+	array_push ($msg_status_set, "'critical'");
+}
+if($unknown && $service){
+	array_push ($msg_status_set, "'unknown'");
+}
+
+if( count($msg_status_set) > 0 )
+{
+	$msg_req .= ' AND (status IN (' . implode(",",$msg_status_set). ') ';
+
+	if($error || $notification)
+		$msg_req .= 'OR status is null';
+
+	$msg_req .=')';
+}
+
+
+	if($type == "HG"){
+		$hosts = getMyHostGroupHosts($id);
+		$tab_host_name= array();
+		foreach($hosts as $h_id)
+		{
+			$host_name = getMyHostName($h_id);
+			array_push ($tab_host_name, "'".$host_name."'");
+//			echo "<type>".$host_name."</type>";
+		}
+
+		$req = "SELECT * FROM log WHERE ctime > '$start' AND ctime <= '$end' $sort_str1 $msg_req AND (host_name in(".implode(",",$tab_host_name).") ";
+
+		if($error || $notification)
+			$req .= ' OR host_name is null';
+		$req .= ")";
+
+	}/* end HG */
+	else if($type == "HH"){
+		$host_name = getMyHostName($id);
+
+		$req = "SELECT * FROM log WHERE ctime > '$start' AND ctime <= '$end' $sort_str1 $msg_req AND (host_name like '".$host_name."' ";
+
+		if($error || $notification)
+			$req .= ' OR host_name is null';
+		$req .= ")";
+
+
+	}/* end HH */
+	else if($type == "HS"){
+		$service_description = getMyServiceName($id);
+		$host_id = getMyHostIDService($id);
+		$host_name = getMyHostName($host_id);
+
+		$req = "SELECT * FROM log WHERE ctime > '$start' AND ctime <= '$end' $sort_str1 $msg_req AND (host_name like '".$host_name."'";
+		if($error || $notification)
+			$req .= ' OR host_name is null';
+		$req .= ")";
+
+		$req .= " AND (service_description like '".$service_description."' ";
+		if($error || $notification)
+			$req .= ' OR service_description is null';
+		$req .= ") ";
+
+
+	}/* end HH */
+	else{
+		$req = "SELECT * FROM log WHERE ctime > '$start' AND ctime <= '$end' $sort_str1 $msg_req ";
+	}
+
+echo "<infos>";
+echo "<sid>".$sid."</sid>";
+echo "<id>".$id."</id>";
+echo "<opid>".$openid."</opid>";
+echo "<start>".$start."</start>";
+echo "<end>".$end."</end>";
+echo "<notification>".$notification."</notification>";
+echo "<alert>".$alert."</alert>";
+echo "<error>".$error."</error>";
+echo "<service>".$service."</service>";
+echo "<host>".$host."</host>";
+
+echo "<up>".$up."</up>";
+echo "<down>".$down."</down>";
+echo "<unreachable>".$unreachable."</unreachable>";
+
+echo "<ok>".$ok."</ok>";
+echo "<warning>".$warning."</warning>";
+echo "<critical>".$critical."</critical>";
+echo "<unknown>".$unknown."</unknown>";
+
+echo "</infos>";
+
+/*
+ * calculate size before limit for pagination 
+ */
+ $lstart = 0;
+	$DBRESULT =& $pearDBO->query($req);
+	if (PEAR::isError($DBRESULT))
+		print "Mysql Error : ".$DBRESULT->getMessage();
+	$rows = $DBRESULT->numrows();
+
+	if(($num * $limit) > $rows)
+		$num = round($rows / $limit) - 1;
+	$lstart = $num * $limit;
+
+	if ($lstart <= 0)
+		$lstart = 0;
+
+/*
+ * pagination
+ */
+	echo "<type>".$type."</type>";
+	echo "<rows>".$rows."</rows>";
+	echo "<num>".$num."</num>";
+//	echo "<msg><![CDATA[".$req."]]></msg>";
+
+
+	$page_max = ceil($rows / $limit);
+	if ($num > $page_max && $rows)
+		$num = $page_max - 1;
+	
+	if($num < 0)
+	$num = 0;
+
+	$pageArr = array();
+	$istart = 0;
+	for($i = 5, $istart = $num; $istart > 0 && $i > 0; $i--)
+		$istart--;
+	for($i2 = 0, $iend = $num; ( $iend <  ($rows / $limit -1)) && ( $i2 < (5 + $i)); $i2++)
+		$iend++;
+	for ($i = $istart; $i <= $iend; $i++){
+		$pageArr[$i] = array("url_page"=>"&num=$i&limit=".$limit, "label_page"=>($i +1),"num"=> $i);
+	}
+	foreach ($pageArr as $key => $tab) {
+		echo "<page>";
+		if($tab["num"] == $num)
+		echo "<selected>1</selected>";
+		else
+		echo "<selected>0</selected>";
+		
+		echo "<num><![CDATA[".$tab["num"]."]]></num>";
+		echo "<url_page><![CDATA[".$tab["url_page"]."]]></url_page>";
+		echo "<label_page><![CDATA[".$tab["label_page"]."]]></label_page>";
+		echo "</page>";
+	}
+
+	$num_page = 0;
+
+	if($num > 0 && $num < $rows)
+	$num_page= $num * $limit;
+
+	$req .= " ORDER BY ctime DESC,log_id DESC";
+	$req .= " LIMIT $lstart,$limit";
+
+	$DBRESULT =& $pearDBO->query($req);
+	if (PEAR::isError($DBRESULT))
+		print "Mysql Error : ".$DBRESULT->getMessage();
+	for ($cpts = 0;$DBRESULT->fetchInto($log);){
+			echo "<line>";
+			echo "<msg_type>".$log["msg_type"]."</msg_type>";
+
+			if($log["msg_type"] > 1)
+			 echo "<retry></retry>";
+			else
+			 echo "<retry>".$log["retry"]."</retry>";
+
+
+			if($log["msg_type"] == 2 || $log["msg_type"] == 3)
+				echo "<type>NOTIF</type>";
+			else
+				echo "<type>".$log["type"]."</type>";
+
+			echo "<status>".$log["status"]."</status>";
+			echo "<service_description>".$log["service_description"]."</service_description>";
+			echo "<host_name>".$log["host_name"]."</host_name>";
+			echo "<class>".$tab_class[$cpts % 2]."</class>";
+			echo "<date>".date($lang["date_format"], $log["ctime"])."</date>";
+			echo "<time>".date($lang["time_format"], $log["ctime"])."</time>";
+			echo "<output><![CDATA[".$log["output"]."]]></output>";
+			echo "</line>";
+		$cpts++;
+	}
+
+
+
+
+
+
+/*
+ * LANG
+ */
+
+$lang["notification"] = "notification";
+$lang["alert"] = "alert";
+$lang["error"] = "error";
+
+$lang["service"] = "service";
+$lang["host"] = "host";
+
+$lang["down"] = "down";
+$lang["up"] = "up";
+$lang["unreachable"] = "unreachable";
+
+$lang["ok"] = "ok";
+$lang["critical"] = "critical";
+$lang["warning"] = "warning";
+$lang["unknown"] = "unknown";
+
+$lang["typeAlert"] = "Alert";
+
+echo "<lang>";
+echo "<typeAlert>".$lang["typeAlert"]."</typeAlert>";
+echo "<notification>".$lang["notification"]."</notification>";
+echo "<alert>".$lang["alert"]."</alert>";
+echo "<error>".$lang["error"]."</error>";
+
+echo "<service>".$lang["service"]."</service>";
+echo "<host>".$lang["host"]."</host>";
+
+echo "<down>".$lang["down"]."</down>";
+echo "<up>".$lang["up"]."</up>";
+echo "<unreachable>".$lang["unreachable"]."</unreachable>";
+
+echo "<warning>".$lang["warning"]."</warning>";
+echo "<ok>".$lang["ok"]."</ok>";
+echo "<critical>".$lang["critical"]."</critical>";
+echo "<unknown>".$lang["unknown"]."</unknown>";
+
+//echo "<>".$lang[""]."</>";
+//echo "<advanced>".$lang["optionAdvanced"]."</advanced>";
+
+echo "</lang>";
+
+
+
+
+
+/*
+ * end root
+ */
+echo "</root>";
+
+
+?>
---
+++ www/include/monitoring/mysql_log_2.0/viewLog.php
@@ -0,0 +1,397 @@
+<?php
+/**
+Created on 23 janv. 08
+
+Centreon is developped with GPL Licence 2.0 :
+http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+Developped by : Cedrick Facon
+
+The Software is provided to you AS IS and WITH ALL FAULTS.
+OREON makes no representation and gives no warranty whatsoever,
+whether express or implied, and without limitation, with regard to the quality,
+safety, contents, performance, merchantability, non-infringement or suitability for
+any particular or intended purpose of the Software found on the OREON web site.
+In no event will OREON be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if OREON has
+been previously advised of the possibility of such damages.
+
+For information : contact@oreon-project.org
+*/
+	if (!isset($oreon))
+		exit();
+
+	#Path to the configuration dir
+	$path = "./include/monitoring/mysql_log_2/";
+	# Smarty template Init
+	$tpl = new Smarty();
+	$tpl = initSmartyTpl($path, $tpl);
+
+	#Pear library
+	require_once "HTML/QuickForm.php";
+	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+
+
+	$openid = '0';
+	$open_id_sub = '0';
+	if(isset($_GET["openid"])){
+	$openid = $_GET["openid"];
+	$open_id_type = substr($openid, 0, 2);
+	$open_id_sub = substr($openid, 3, strlen($openid));
+	}
+
+	if(isset($_GET["host_id"]) && $open_id_type == "HH"){
+		$_GET["host_id"] = $open_id_sub;
+	}
+	else
+		$_GET["host_id"] = null;
+
+?>
+<link href="./include/common/javascript/datePicker.css" rel="stylesheet" type="text/css"/>
+<script language='javascript' src='./include/common/javascript/tool.js'></script>
+<script>
+			var css_file = './include/common/javascript/codebase/dhtmlxtree.css';
+		    var headID = document.getElementsByTagName("head")[0];  
+		    var cssNode = document.createElement('link');
+		       cssNode.type = 'text/css';
+		       cssNode.rel = 'stylesheet';
+		       cssNode.href = css_file;
+		       cssNode.media = 'screen';headID.appendChild(cssNode);
+ 
+ 
+    		tree=new dhtmlXTreeObject("menu_20301","100%","100%","1");
+            tree.setImagePath("./img/icones/csh_vista/");
+			//tree.setImagePath("./include/common/javascript/codebase/imgs/csh_vista/");
+
+
+            //link tree to xml
+            tree.setXMLAutoLoading("./include/monitoring/mysql_log_2/GetODSXmlTree.php"); 
+            
+            //load first level of tree
+            tree.loadXML("./include/monitoring/mysql_log_2/GetODSXmlTree.php?id=1&openid=<?php echo $openid; ?>");
+
+			// system to reload page after link with new url
+			tree.attachEvent("onClick",onNodeSelect)//set function object to call on node select 
+			tree.attachEvent("onDblClick",onDblClick)//set function object to call on node select 
+			//see other available event handlers in API documentation 
+
+			tree.enableDragAndDrop(0);
+			tree.enableTreeLines(false);
+
+			function onDblClick(nodeId)
+			{
+				tree.openAllItems(nodeId);
+				return(false);
+			}
+			
+			function onNodeSelect(nodeId)
+			{
+				var logView4xml = document.getElementById('logView4xml');
+//				logView4xml.innerHTML="..W.." + nodeId;
+				logView4xml.innerHTML="Waiting XML log";
+
+				tree.openItem(nodeId);
+
+				log_4_host(nodeId,'');
+			}
+			
+			// it's fake methode for using ajax system by default
+			function mk_pagination(){;}
+			function set_header_title(){;}
+
+			var _num = 0;
+			function log_4_host_page(id, formu, num)
+			{
+				_num = num;
+				log_4_host(id, formu);
+			}
+
+			function log_4_host(id, formu)
+			{
+				// Period
+				var currentTime = new Date();
+				var period ='';
+
+				var _zero_hour = '';
+				var _zero_min = '';
+				var StartDate='';
+				var EndDate='';
+				var StartTime='';
+				var EndTime='';
+
+				if(document.formu && !document.formu.period_choice[1].checked)
+				{
+					period = document.formu.period.value;
+
+				var _zero_hour = '';
+				var _zero_min = '';
+				var StartDate='';
+				var EndDate='';
+				var StartTime='';
+				var EndTime='';
+
+				}
+				else
+				{
+					if(currentTime.getMinutes() <= 9){
+						_zero_min = '0';
+					}
+					if(currentTime.getHours() >= 12){
+						StartDate= currentTime.getMonth()+1+"/"+currentTime.getDate()+"/"+currentTime.getFullYear();
+						EndDate= currentTime.getMonth()+1+"/"+ currentTime.getDate()+"/"+currentTime.getFullYear();						
+	
+						if((currentTime.getHours()- 12) <= 12){
+							_zero_hour = '0';					
+						}
+						StartTime = _zero_hour + (currentTime.getHours() - 12) +":" + _zero_min + currentTime.getMinutes();
+						EndTime   = currentTime.getHours() + ":" + _zero_min + currentTime.getMinutes();
+					}
+					else
+					{
+						var StartDate= currentTime.getMonth()+1+"/"+(currentTime.getDate()-1)+"/"+currentTime.getFullYear();
+						var EndDate=   currentTime.getMonth()+1+"/"+ currentTime.getDate()+"/"+currentTime.getFullYear();
+						var StartTime=  (24 -(12 - currentTime.getHours()))+ ":00";
+						var EndTime= "0" + currentTime.getHours()+":" + _zero_min + currentTime.getMinutes();
+						var EndTime   = "0" + currentTime.getHours() + ":" + _zero_min + currentTime.getMinutes();
+					
+					}
+				}
+
+
+				var _notification = 1;
+				var _error = 1;
+				var _alert = 1;
+
+				// alert type
+				if(document.formu2 && !document.formu2.notification.checked)
+					_notification = 0;
+				if(document.formu2 && !document.formu2.error.checked)
+					_error = 0;
+				if(document.formu2 && !document.formu2.alert.checked)
+					_alert = 0;
+
+				var _host = 1;
+				var _service = 1;
+				if(document.formu3 && !document.formu3.host.checked)
+					_host = 0;
+				if(document.formu4 && !document.formu4.service.checked)
+					_service = 0;
+
+
+				var _up = 1;
+				var _down = 1;
+				var _unreachable = 1;
+				if(document.formu3 && !document.formu3.up.checked)
+					_up = 0;
+				if(document.formu3 && !document.formu3.down.checked)
+					_down = 0;
+				if(document.formu3 && !document.formu3.unreachable.checked)
+					_unreachable = 0;
+
+
+				var _ok = 1;		
+				if(document.formu4 && !document.formu4.ok.checked)
+					_ok = 0;
+
+				var _warning = 1;
+				if(document.formu4 && !document.formu4.warning.checked)
+					_warning = 0;
+
+				var _critical = 1;
+				if(document.formu4 && !document.formu4.critical.checked)
+					_critical = 0;
+
+				var _unknown = 1;
+				if(document.formu4 && !document.formu4.unknown.checked)
+					_unknown = 0;
+
+				if(document.formu && document.formu.StartDate.value != "")
+					StartDate = document.formu.StartDate.value;
+				if(document.formu && document.formu.EndDate.value != "")
+					EndDate = document.formu.EndDate.value;
+
+				if(document.formu && document.formu.StartTime.value != "")
+					StartTime = document.formu.StartTime.value;
+				if(document.formu && document.formu.EndTime.value != "")
+					EndTime = document.formu.EndTime.value;
+
+				tree.selectItem(id);
+				
+				var proc = new Transformation();
+				var _addrXSL = "./include/monitoring/mysql_log_2/log.xsl";
+				var _addrXML = './include/monitoring/mysql_log_2/GetODSXmlLog.php?warning='+_warning+'&unknown='+_unknown+'&critical='+_critical+'&ok='+_ok+'&unreachable='+_unreachable+'&down='+_down+'&up='+_up+'&host='+_host+'&service='+_service+'&num='+_num+'&error='+_error+'&alert='+_alert+'&notification='+_notification+'&period='+period+'&StartDate='+StartDate+'&EndDate='+EndDate+'&StartTime='+StartTime+'&EndTime='+EndTime+'&id='+id+'&sid=<?php echo $sid;?>';
+				proc.setXml(_addrXML)
+				proc.setXslt(_addrXSL)
+				proc.transform("logView4xml");
+
+				if(document.formu){					
+					document.formu.StartDate.value = StartDate;
+					document.formu.EndDate.value = EndDate;
+					document.formu.StartTime.value = StartTime;
+					document.formu.EndTime.value = EndTime;
+				}
+		}
+
+function displayTimePicker(timeFieldName, displayBelowThisObject, dtFormat)
+{
+	if (document.getElementsByName (timeFieldName).item(1))
+	  var targetDateField = document.getElementsByName (timeFieldName).item(1);
+	else
+	  var targetDateField = document.getElementsByName (timeFieldName).item(0);
+
+
+  var x = displayBelowThisObject.offsetLeft;
+  var y = displayBelowThisObject.offsetTop + displayBelowThisObject.offsetHeight ;
+ 
+  // deal with elements inside tables and such
+  var parent = displayBelowThisObject;
+  while (parent.offsetParent) {
+    parent = parent.offsetParent;
+    x += parent.offsetLeft;
+    y += parent.offsetTop ;
+  }
+drawTimePicker(timeFieldName, targetDateField, x, y);
+}
+
+function drawTimePicker(timeFieldName, targetTimeField, x, y)
+{
+ 	var timePickerDivID = timeFieldName + "_timePickerDivID";
+ 
+	var newNode = document.createElement("select");
+    newNode.setAttribute("id", timePickerDivID);
+    newNode.setAttribute("class", "tpDiv");
+    newNode.setAttribute("size", 6);
+    newNode.setAttribute("style", "visibility: hidden;");
+	newNode.onchange = function() { 
+		var pickerDiv = document.getElementById(timePickerDivID);
+		targetTimeField.value = '';
+		targetTimeField.innerHTML = '';
+		
+		targetTimeField.value = pickerDiv.options[pickerDiv.selectedIndex].value;
+		pickerDiv.style.visibility = (pickerDiv.style.visibility == "visible" ? "hidden" : "visible");
+		pickerDiv.style.display = (pickerDiv.style.display == "block" ? "none" : "block");
+
+		var pickerDiv_close = document.getElementById(timePickerDivID+"_close");
+		pickerDiv_close.style.visibility = "hidden";
+		pickerDiv_close.style.display = "block";
+
+
+		return false;
+	};
+
+	var _zero = "0";
+	for (var i=0; i < 24; i++) {
+		if(i < 10)
+			_zero = "0";
+		else
+			_zero = "";
+		
+		var k = document.createElement('option');
+		k.value= _zero + i + ":00";
+		k.innerHTML= _zero + i + ":00";
+		var currentTime = new Date()
+		if(i == currentTime.getHours())
+		k.selected = true;
+		newNode.appendChild(k);		
+
+		var k = document.createElement('option');
+		k.value= _zero + i+":30";
+		k.innerHTML= _zero + i+":30";
+		newNode.appendChild(k);
+	}
+    document.body.appendChild(newNode);
+    
+  
+	var pickerDiv = document.getElementById(timePickerDivID);
+	pickerDiv.style.position = "absolute";
+	pickerDiv.style.left = x + "px";
+	pickerDiv.style.top = y + "px";
+	pickerDiv.style.visibility = (pickerDiv.style.visibility == "visible" ? "hidden" : "visible");
+	pickerDiv.style.display = (pickerDiv.style.display == "block" ? "none" : "block");
+	pickerDiv.style.zIndex = 10000;
+
+
+	var closeButton = document.createElement("input");
+    closeButton.type= "button";
+    closeButton.value= "close";
+	closeButton.style.position = "absolute";
+	closeButton.id = timePickerDivID + "_close";
+	closeButton.style.left = x + "px";
+	closeButton.style.top = y + 78 + "px";
+	closeButton.style.width = 45 + "px";
+	closeButton.style.zIndex = 10000;
+	closeButton.style.textDecoration = "none";
+
+	closeButton.onclick = function() { 
+
+		var pickerDiv = document.getElementById(timePickerDivID);
+		pickerDiv.style.visibility = "hidden";
+		pickerDiv.style.display = "block";
+		
+//		var pickerDiv_close = document.getElementById(timePickerDivID+"_close");
+		var pickerDiv_close = this;
+		pickerDiv_close.style.visibility = "hidden";
+		pickerDiv_close.style.display = "block";
+		return false;
+	};
+
+    document.body.appendChild(closeButton);
+
+    
+}
+
+
+function grise_period(_hid, _show)
+{
+	var _radio = document.forms["formu"].elements["period_choice"]
+	var _flag = 0;
+
+       for (var i=0; i< _radio.length;i++) {
+			if (_radio[i].checked) 
+			{
+				_radio[i].checked = false;
+				//_flag = 1;
+         	}
+         	else
+			{
+				_radio[i].checked = true;
+         	}
+       }
+}
+
+log_4_host('RR_0',null);
+
+</script>
+
+
+<?php
+//<div id="graphView4xml">..</div>
+	## Form begin
+	$form = new HTML_QuickForm('Form', 'get', "?p=".$p);
+	$form->addElement('header', 'title', $lang["giv_sr_infos"]);
+
+	$periods = array(	""=>"",
+						"10800"=>$lang["giv_sr_p3h"],
+						"21600"=>$lang["giv_sr_p6h"],
+						"43200"=>$lang["giv_sr_p12h"],
+						"86400"=>$lang["giv_sr_p24h"],
+						"172800"=>$lang["giv_sr_p2d"],
+						"302400"=>$lang["giv_sr_p4d"],
+						"604800"=>$lang["giv_sr_p7d"],
+						"1209600"=>$lang["giv_sr_p14d"],
+						"2419200"=>$lang["giv_sr_p28d"],
+						"2592000"=>$lang["giv_sr_p30d"],
+						"2678400"=>$lang["giv_sr_p31d"],
+						"5184000"=>$lang["giv_sr_p2m"],
+						"10368000"=>$lang["giv_sr_p4m"],
+						"15552000"=>$lang["giv_sr_p6m"],
+						"31104000"=>$lang["giv_sr_p1y"]);
+
+	$sel =& $form->addElement('select', 'period', $lang["giv_sr_period"], $periods);
+
+	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+	$form->accept($renderer);
+	$tpl->assign('form', $renderer->toArray());
+
+	$tpl->assign('lang', $lang);
+	$tpl->display("viewLog.ihtml");
+?>
