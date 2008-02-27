4004ae68711888274bbd9c981918d1d2a274a0b1
Julien Mathis <jmathis@merethis.com>
2008-02-27 23:00:44 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@4043 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/common_ndo_func.php
+++ www/include/monitoring/engine/common_ndo_func.php
@@ -1,19 +1,19 @@
 <?php
 /**
-Centreon is developped with GPL Licence 2.0 :
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Cedrick Facon
-
-The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
-whether express or implied, and without limitation, with regard to the quality,
-safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
-been previously advised of the possibility of such damages.
-
-For information : contact@oreon-project.org
+	Centreon is developped with GPL Licence 2.0 :
+	http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+	Developped by : Cedrick Facon
+	
+	The Software is provided to you AS IS and WITH ALL FAULTS.
+	OREON makes no representation and gives no warranty whatsoever,
+	whether express or implied, and without limitation, with regard to the quality,
+	safety, contents, performance, merchantability, non-infringement or suitability for
+	any particular or intended purpose of the Software found on the OREON web site.
+	In no event will OREON be liable for any direct, indirect, punitive, special,
+	incidental or consequential damages however they may arise and even if OREON has
+	been previously advised of the possibility of such damages.
+	
+	For information : contact@oreon-project.org
 */
 
 	function get_Host_Status($host_name,$pearDBndo,$general_opt){
