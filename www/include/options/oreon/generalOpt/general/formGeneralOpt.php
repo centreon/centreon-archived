1d81fb381acdc3e424b5ed690c44b9cc81fcff40
Julien Mathis <jmathis@merethis.com>
2008-02-28 12:32:55 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@4047 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/oreon/generalOpt/general/formGeneralOpt.php
+++ www/include/options/oreon/generalOpt/general/formGeneralOpt.php
@@ -149,8 +149,6 @@ For information : contact@oreon-project.org
 	$tpl->assign("time_sec", _(" seconds "));
 	$tpl->assign("genOpt_display_options", _("Display Options"));
 	$tpl->assign("genOpt_time_zone", _("Time Zone"));
-
-	
 	$tpl->assign('valid', $valid);
 	$tpl->display("formGeneralOpt.ihtml");
 ?>
