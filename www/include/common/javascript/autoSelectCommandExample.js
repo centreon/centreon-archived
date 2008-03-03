06b4f76767fb1a79c10476b4254e4ebcf3809bba
Julien Mathis <jmathis@merethis.com>
2008-03-03 15:48:06 +0000

    normalise !
    git-svn-id: http://svn.centreon.com/trunk/centreon@4136 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/javascript/autoSelectCommandExample.js
+++ www/include/common/javascript/autoSelectCommandExample.js
@@ -18,7 +18,7 @@ For information : contact@oreon-project.org
 
 
 function set_arg(e, a) {
-var f = document.forms["Form"];
+	var f = document.forms["Form"];
 	var example    = f.elements[e];
 	var argument    = f.elements[a];
 	argument.value = example.value;
@@ -26,45 +26,34 @@ var f = document.forms["Form"];
 
 
 function setArgument(f, l, a) {
-	
 	var mlist    = f.elements[l];
 	var argument    = f.elements[a];
 	var index = mlist.selectedIndex;
 
 	if(argument.value)
-	argument.value = '';
+		argument.value = '';
 
 	if(index >= 1) {
-	   var xhr_object = null; 
+	   	var xhr_object = null; 
 	     
-	   if(window.XMLHttpRequest) // Firefox 
-	      xhr_object = new XMLHttpRequest(); 
-	   else if(window.ActiveXObject) // Internet Explorer 
-	      xhr_object = new ActiveXObject("Microsoft.XMLHTTP"); 
-	   else { // XMLHttpRequest non supporté par le navigateur 
-	      alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
-	      return; 
+		if(window.XMLHttpRequest) // Firefox 
+	      	xhr_object = new XMLHttpRequest(); 
+	   	else if(window.ActiveXObject) // Internet Explorer 
+	      	xhr_object = new ActiveXObject("Microsoft.XMLHTTP"); 
+	   	else { 
+	   		// XMLHttpRequest non supporté par le navigateur 
+	      	alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest..."); 
+	      	return; 
 	   	} 
 	 
-	   xhr_object.open("POST", "./include/common/javascript/autoSelectCommandExample.php", true);
-
-
-	   xhr_object.onreadystatechange = function() { 
-	      if(xhr_object.readyState == 4) {
-	          argument.value = xhr_object.responseText; 
-	         }
+	   	xhr_object.open("POST", "./include/common/javascript/autoSelectCommandExample.php", true);
+	   	xhr_object.onreadystatechange = function() { 
+	    	if(xhr_object.readyState == 4) {
+	          	argument.value = xhr_object.responseText; 
+	        }
 	   	}
 	 
-	   xhr_object.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
-//	   var data = "family="+escape(l1.options[index].value)+"&form="+f.name+"&select=list2"; 
-
-/*
-	   	var data = ""; 
-		var s1       = mlist.value; 
-		if( s1 != "") 
-	   		data = "s1="+s1; 
-	   	xhr_object.send(data); 
-*/
+	 	xhr_object.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
 	   	xhr_object.send("index=" + mlist.value); 	   	
 	} 
 }
