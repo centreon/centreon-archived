0486f1b8ec17a480c355f0f76d8ebaf3c74c0c04
Julien Mathis <jmathis@merethis.com>
2008-03-12 13:18:07 +0000

    Add translators in about :)
    git-svn-id: http://svn.centreon.com/trunk/centreon@4222 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/about/about.php
+++ www/include/options/about/about.php
@@ -45,11 +45,26 @@ For information : contact@oreon-project.org
 	<a href="mailto:ccoraboeuf@oreon-project.org" class="list_two">Christophe Coraboeuf (Wistof)</a><br />
 	<a href="mailto:cfacon@oreon-project.org" class="list_two">Cedrick Facon (Apo)</a><br />
 	Mathavarajan Sugumaran (MrBrown)<br />
+	Sylvestre Ho Tam Chay<br />
+	Nicolas Cordier<br />
 	Guillaume Watteeux (Watt)<br />
 	Maximilien Bersoult (leoncx)<br />
 	Gaetan Lucas de Couville (gae)<br />
 	Nathanael Guyot (Tor)<br />
 	Jean Marc Grisard (Jmou)<br />
 	Ira Janssen (Iralein)<br />
+	
+	<br />
+	<div class="list_one"><h3>
+	<b><?php echo _("Translators"); ?> :</b>
+	</h3></div><br />
+	Christoph Ziemann
+	guigui2607
+	Silvio Rodrigo Damasceno de Souza
+	Tobias Boehnert
+	Luiz Gustavo
+	Danil Makeyev
+	Duy-Huan BUI  
+
 	</ul>
 </div>
