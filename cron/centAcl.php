fc347457cc0a25098623e960934d4b42336e7f36
Julien Mathis <jmathis@merethis.com>
2008-03-15 18:16:01 +0000

    new acl
    git-svn-id: http://svn.centreon.com/trunk/centreon@4263 6bcd3966-0018-0410-8128-fd23d134de7e

--- cron/centAcl.php
+++ cron/centAcl.php
@@ -9,61 +9,91 @@
 	include_once($path."/www/include/common/common-Func.php");
 	include_once($path."/www/include/common/common-Func-ACL.php");
 	
-	
+	$tabGroups = array();
+	$DBRESULT1 =& $pearDB->query(	"SELECT DISTINCT acl_groups.acl_group_id, acl_resources.acl_res_id " .
+									"FROM acl_res_group_relations, `acl_groups`, `acl_resources` " .
+									"WHERE acl_groups.acl_group_id = acl_res_group_relations.acl_group_id " .
+									"AND acl_res_group_relations.acl_res_id = acl_resources.acl_res_id " .
+									"AND acl_groups.acl_group_activate = '1' ".			
+									"AND acl_resources.changed = '1'");
+	while ($result = $DBRESULT1->fetchRow())
+		$tabGroups[$result["acl_group_id"]] = $result["acl_res_id"];
+			
 	/*
 	 * Purge datas
 	 */
-	$DBRESULT =& $pearDBndo->query("TRUNCATE TABLE `centreon_acl`");
-	if (PEAR::isError($DBRESULT))
-		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	/*
-	 * Begin to create topology of hosts/services
-	 */
-		  	
-	$DBRESULT =& $pearDB->query("SELECT acl_group_id FROM `acl_groups` WHERE acl_group_activate = '1'");
-	while ($res = $DBRESULT->fetchRow()){
-		$group_id = $res["acl_group_id"];
-//		print "-----------$group_id-------------\n";
-		$DBRESULT2 =& $pearDB->query("SELECT acl_res_id FROM acl_res_group_relations WHERE acl_group_id = '".$group_id."'");			
-		$Host = array();
-		while ($res2 = $DBRESULT2->fetchRow()){
-//			print "###################################\n";
-			
-			$DBRESULT3 =& $pearDB->query("SELECT host_id, host_name FROM `host`, `acl_resources_host_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_host_relations.host_host_id = host.host_id AND host.host_register = '1'");
-		  	while ($h = $DBRESULT3->fetchRow()){
-				$Host[$h["host_id"]] = $h["host_name"];
-		  	}
-			$DBRESULT3 =& $pearDB->query("SELECT hg_id FROM `hostgroup`, `acl_resources_hg_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_hg_relations.hg_hg_id = hostgroup.hg_id");
-	  		while ($hostgroup = $DBRESULT3->fetchRow()){
-				$DBRESULT4 =& $pearDB->query("SELECT host_host_id, host_name FROM `hostgroup_relation`, `host` WHERE hostgroup_hg_id = '".$hostgroup["hg_id"]."' AND host.host_id = hostgroup_relation.host_host_id");
-	  			while ($host_hostgroup = $DBRESULT4->fetchRow())
-					$Host[$host_hostgroup["host_host_id"]] = $host_hostgroup["host_name"];
-	  		}
-	  		
-	  		$DBRESULT3 =& $pearDB->query("SELECT host_id FROM `host`, `acl_resources_hostex_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_hostex_relations.host_host_id = host.host_id");
-			if ($DBRESULT3->numRows())
-		  		while ($h = $DBRESULT3->fetchRow())
-					if (isset($Host[$h["host_id"]]))
-						unset($Host[$h["host_id"]]);
-
-			$strBegin = "INSERT INTO `centreon_acl` ( `host_name` , `service_description` , `group_id` ) VALUES ";
-
-			foreach ($Host as $key => $value){
-//				print "Hosts : ".$value." -> $key  ($group_id)\n";
-				$tab = getAuthorizedServicesHost($key, $group_id);
+	$strBegin = "INSERT INTO `centreon_acl` ( `host_name` , `service_description` , `group_id` ) VALUES ";
+	
+	foreach ($tabGroups as $acl_group_id => $acl_res_id){
+		$tabElem = array();
+		/*
+		 * Delete old datas for this groups
+		 */
+		$DBRESULT =& $pearDBndo->query("DELETE FROM `centreon_acl` WHERE `group_id` = '".$acl_group_id."'");
+		if (PEAR::isError($DBRESULT))
+			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		$DBRESULT =& $pearDB->query("SELECT acl_group_id FROM `acl_groups` WHERE acl_group_id = '".$acl_group_id."'");
+		while ($res = $DBRESULT->fetchRow()){
+			print "-----------$acl_group_id-------------\n";
+			$DBRESULT2 =& $pearDB->query("SELECT acl_res_id FROM acl_res_group_relations WHERE acl_group_id = '".$acl_group_id."'");			
+			$Host = array();
+			while ($res2 = $DBRESULT2->fetchRow()){
+				//print "###################################\n";
+				
+				/*
+				 * Get all Hosts 
+				 */
+				$DBRESULT3 =& $pearDB->query("SELECT host_id, host_name FROM `host`, `acl_resources_host_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_host_relations.host_host_id = host.host_id AND host.host_register = '1'");
+			  	while ($h = $DBRESULT3->fetchRow())
+					$Host[$h["host_id"]] = $h["host_name"];
+					
+			  	/*
+			  	 * Get all host in hostgroups
+			  	 */
+				$DBRESULT3 =& $pearDB->query("SELECT hg_id FROM `hostgroup`, `acl_resources_hg_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_hg_relations.hg_hg_id = hostgroup.hg_id");
+		  		while ($hostgroup = $DBRESULT3->fetchRow()){
+					$DBRESULT4 =& $pearDB->query("SELECT host_host_id, host_name FROM `hostgroup_relation`, `host` WHERE hostgroup_hg_id = '".$hostgroup["hg_id"]."' AND host.host_id = hostgroup_relation.host_host_id");
+		  			while ($host_hostgroup = $DBRESULT4->fetchRow())
+						$Host[$host_hostgroup["host_host_id"]] = $host_hostgroup["host_name"];
+		  		}
+		  		
+		  		/*
+		  		 * Get All exclude Hosts
+		  		 */
+		  		$DBRESULT3 =& $pearDB->query("SELECT host_id FROM `host`, `acl_resources_hostex_relations` WHERE acl_res_id = '".$res2["acl_res_id"]."' AND acl_resources_hostex_relations.host_host_id = host.host_id");
+				if ($DBRESULT3->numRows())
+			  		while ($h = $DBRESULT3->fetchRow())
+						if (isset($Host[$h["host_id"]]))
+							unset($Host[$h["host_id"]]);
+				
 				$str = "";
-				foreach ($tab as $desc => $id){
-					if ($str)
-						$str .= ", ";
-					$str .= "('".$value."', '".$desc."', ".$group_id.") ";
-//					print "SVC : $desc \n";
-				}
-				if ($str){
-					$DBRESULTNDO =& $pearDBndo->query($strBegin.$str);
-					if (PEAR::isError($DBRESULTNDO))
-						print "DB Error : ".$DBRESULTNDO->getDebugInfo()."<br />";
+				foreach ($Host as $key => $value){
+					$tab = getAuthorizedServicesHost($key, $acl_group_id);
+					foreach ($tab as $desc => $id){
+						if (!isset($tabElem[$value]))
+							$tabElem[$value] = array();
+						$tabElem[$value][$desc] = 1;
+					}	 
 				}
 			}
 		}
+		if (count($tabElem)){
+			foreach ($tabElem as $host => $svc_list){
+				foreach ($svc_list as $desc => $t){
+					if ($str != "")
+						$str .= ', ';
+					$str .= "('".$host."', '".$desc."', ".$acl_group_id.") ";
+				}
+			}
+			print $strBegin.$str . "\n\n";
+			$DBRESULTNDO =& $pearDBndo->query($strBegin.$str);
+			if (PEAR::isError($DBRESULTNDO))
+				print "DB Error : ".$DBRESULTNDO->getDebugInfo()."<br />";
+			else {
+				$DBRESULT3 =& $pearDB->query("UPDATE `acl_resources` SET `changed` = '0'");
+				if (PEAR::isError($DBRESULT3))
+					print "DB Error : ".$DBRESULT3->getDebugInfo()."<br />";	
+			}
+		}	
 	}
 ?>
