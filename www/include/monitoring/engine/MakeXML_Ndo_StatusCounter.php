5024d442030758951d8a08ccbeb4a60ef5d0ab74
Julien Mathis <jmathis@merethis.com>
2008-03-17 17:58:01 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@4272 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/MakeXML_Ndo_StatusCounter.php
+++ www/include/monitoring/engine/MakeXML_Ndo_StatusCounter.php
@@ -2,15 +2,15 @@
 /**
 Centreon is developped with GPL Licence 2.0 :
 http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Cedrick Facon
+Developped by : Julien Mathis - Romain Le Merlus
 
 The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
+Centreon makes no representation and gives no warranty whatsoever,
 whether express or implied, and without limitation, with regard to the quality,
 safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
+any particular or intended purpose of the Software found on the Centreon web site.
+In no event will Centreon be liable for any direct, indirect, punitive, special,
+incidental or consequential damages however they may arise and even if Centreon has
 been previously advised of the possibility of such damages.
 
 For information : contact@oreon-project.org
@@ -36,22 +36,21 @@ For information : contact@oreon-project.org
 		$_GET[$key] = $value;
 	}
 
-	## pearDB init
-	require_once 'DB.php';
+	include_once 'DB.php';
 
 	include_once($oreonPath . "etc/centreon.conf.php");
 	include_once($oreonPath . "www/include/common/common-Func-ACL.php");
 	include_once($oreonPath . "www/include/common/common-Func.php");
-
+	
 	/* Connect to oreon DB */
 
 	$dsn = array('phptype'  => 'mysql',
 			     'username' => $conf_oreon['user'],
 			     'password' => $conf_oreon['password'],
 			     'hostspec' => $conf_oreon['host'],
-			     'database' => $conf_oreon['db'],);
-	$options = array('debug'=> 2, 'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE,);
-	$pearDB =& DB::connect($dsn, $options);
+			     'database' => $conf_oreon['db']);
+
+	$pearDB =& DB::connect($dsn, array('debug'=> 2, 'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE));
 	if (PEAR::isError($pearDB)) die("Connecting problems with oreon database : " . $pearDB->getMessage());
 	$pearDB->setFetchMode(DB_FETCHMODE_ASSOC);
 
@@ -65,17 +64,15 @@ For information : contact@oreon-project.org
 	$sid = isset($_GET["sid"]) ? $_GET["sid"] : $sid;
 
 	/* security check 2/2*/
-	if(!check_injection($sid)){
+	if (!check_injection($sid)){
 		$sid = htmlentities($sid);
 		$res =& $pearDB->query("SELECT * FROM session WHERE session_id = '".$sid."'");
-		if($res->fetchInto($session)){
-			/* update session */
+		if ($res->fetchInto($session))
 			$DBRESULT2 =& $pearDB->query("UPDATE `session` SET `last_reload` = '".time()."', `ip_address` = '".$_SERVER["REMOTE_ADDR"]."' WHERE CONVERT( `session_id` USING utf8 ) = '".$sid."' LIMIT 1");
-		} else
+		else
 			get_error('bad session id');
 	} else
 		get_error('need session identifiant !');
-	/* security end 2/2 */
 
 	/*
 	 * LCA
@@ -98,11 +95,10 @@ For information : contact@oreon-project.org
 		$lcaSTR = getLCAHostStr($lca["LcaHost"]);
 	}
 	 
-
 	function restore_session($statistic_service = 'null', $statistic_host = 'null'){
 		global $pearDB;
 		if (isset($statistic_service) && !is_null($statistic_service)){
-			$sql = "UPDATE session SET " .
+			$sql = 	" UPDATE session SET " .
 					" s_nbHostsUp = '".$statistic_host["UP"]."'," .
 					" s_nbHostsDown = '".$statistic_host["DOWN"]."'," .
 					" s_nbHostsUnreachable = '".$statistic_host["UNREACHABLE"]."'," .
@@ -130,7 +126,6 @@ For information : contact@oreon-project.org
 		$statistic_host = array("UP" => 0, "DOWN" => 0, "UNREACHABLE" => 0, "PENDING" => 0);
 		$statistic_service = array("OK" => 0, "WARNING" => 0, "CRITICAL" => 0, "UNKNOWN" => 0, "PENDING" => 0);
 
-
 		$DBRESULT_OPT =& $pearDB->query("SELECT color_ok,color_warning,color_critical,color_unknown,color_pending,color_up,color_down,color_unreachable FROM general_opt");
 		if (PEAR::isError($DBRESULT_OPT))
 			print "DB Error : ".$DBRESULT_OPT->getDebugInfo()."<br />";
@@ -140,10 +135,10 @@ For information : contact@oreon-project.org
 
 		/* Get HostNDO status */
 		if (!$is_admin)
-			$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state) , ".$ndo_base_prefix."hoststatus.current_state" .
-					" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects " .
+			$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state), ".$ndo_base_prefix."hoststatus.current_state" .
+					" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects" .
 					" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 1 " .
-					" AND ".$ndo_base_prefix."objects.name1 IN (".$lcaSTR." )" .
+					" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR)" .
 					" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 					" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
 		else
@@ -152,27 +147,26 @@ For information : contact@oreon-project.org
 					" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 1 " .
 					" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 					" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
+					
 		$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
 		if (PEAR::isError($DBRESULT_NDO1))
 			print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
 		
-		$host_stat = array();
-		$host_stat[0] = 0;
-		$host_stat[1] = 0;
-		$host_stat[2] = 0;
-		$host_stat[3] = 0;
-		while($DBRESULT_NDO1->fetchInto($ndo))
+		$host_stat = array(0=>0, 1=>0, 2=>0, 3=>0);
+
+		while ($ndo = $DBRESULT_NDO1->fetchRow())
 			$host_stat[$ndo["current_state"]] = $ndo["count(nagios_hoststatus.current_state)"];
-		/* end */
 
 		/* Get ServiceNDO status */
 		if (!$is_admin)
 			$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
-					" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
+					" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, centreon_acl " .
 					" WHERE no.object_id = nss.service_object_id".
 					" AND no.name1 not like 'OSL_Module' ".
 					" AND no.name1 not like 'Meta_Module' ".
-					" AND no.name1 IN (".$lcaSTR." ) ".
+					" AND no.name1 = centreon_acl.host_name ".
+					" AND no.name2 = centreon_acl.service_description " .
+					" AND centreon_acl.group_id IN (5) ".
 					" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
 		else
 			$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
@@ -180,35 +174,18 @@ For information : contact@oreon-project.org
 					" WHERE no.object_id = nss.service_object_id".
 					" AND no.name1 not like 'OSL_Module' ".
 					" AND no.name1 not like 'Meta_Module' ".
-					" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
-			
+					" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";			
 		$DBRESULT_NDO2 =& $pearDBndo->query($rq2);
 		if (PEAR::isError($DBRESULT_NDO2))
 			print "DB Error : ".$DBRESULT_NDO2->getDebugInfo()."<br />";
 
-		$svc_stat = array();
-		$svc_stat[0] = 0;
-		$svc_stat[1] = 0;
-		$svc_stat[2] = 0;
-		$svc_stat[3] = 0;
-		$svc_stat[4] = 0;
+		$svc_stat = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
 		while($DBRESULT_NDO2->fetchInto($ndo))
 			$svc_stat[$ndo["current_state"]] = $ndo["count(nss.current_state)"];
 		/* end */
 
-		$statistic_service["OK"] = $svc_stat[0];
-		$statistic_service["WARNING"] = $svc_stat[1];
-		$statistic_service["CRITICAL"] = $svc_stat[2];
-		$statistic_service["UNKNOWN"] = $svc_stat[3];
-		$statistic_service["PENDING"] = $svc_stat[4];
-		$statistic_host["UP"] = $host_stat[0];
-		$statistic_host["DOWN"] = $host_stat[1];
-		$statistic_host["UNREACHABLE"] = $host_stat[2];
-		$statistic_host["PENDING"] = $host_stat[3];
-
 		restore_session($statistic_service, $statistic_host);
 
-		$buffer = null;
 		$buffer  = '<?phpxml version="1.0"?>';
 		$buffer .= '<reponse>';
 		$buffer .= '<infos>';
