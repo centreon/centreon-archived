012e8b032f5d23b80f3bad84fba162099f4ca195
Julien Mathis <jmathis@merethis.com>
2008-04-03 19:13:44 +0000

    move directories
    git-svn-id: http://svn.centreon.com/trunk/centreon@4440 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/views/graphs/exportData/ExportCSVServiceData.php
+++ www/include/views/graphs/exportData/ExportCSVServiceData.php
@@ -1,22 +1,21 @@
 <?php
-/**
-Centreon is developped with GPL Licence 2.0 :
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@oreon-project.org
+ */
 
-The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
-whether express or implied, and without limitation, with regard to the quality,
-safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
-been previously advised of the possibility of such damages.
-
-For information : contact@oreon-project.org
-*/
-
-	$oreonPath = '/srv/oreon/';
+	$centreon_Path = '/usr/local/centreon/';
 
 	function check_injection(){
 		if ( eregi("(<|>|;|UNION|ALL|OR|AND|ORDER|SELECT|WHERE)", $_GET["sid"])) {
@@ -31,10 +30,10 @@ For information : contact@oreon-project.org
 		exit(0);
 	}
 
-	include_once($oreonPath . "www/oreon.conf.php");
-	include_once($oreonPath . "www/DBconnect.php");
-	$oreon = 1;
-	include_once($oreonPath . "www/DBOdsConnect.php");
+	include_once("/etc/centreon/centreon.conf.php");
+	include_once($centreon_Path."www/DBconnect.php");
+	//$oreon = 1;
+	include_once($centreon_Path."www/DBOdsConnect.php");
 
 	if (isset($_GET["sid"]) && !check_injection($_GET["sid"])){
 		$sid = $_GET["sid"];
@@ -48,8 +47,8 @@ For information : contact@oreon-project.org
 		get_error('need session identifiant !');
 	/* security end 2/2 */
 
-	isset ($_GET["index"]) ? $index = $_GET["index"] : $index = NULL;
-	isset ($_POST["index"]) ? $index = $_POST["index"] : $index = $index;
+	isset($_GET["index"]) ? $index = $_GET["index"] : $index = NULL;
+	isset($_POST["index"]) ? $index = $_POST["index"] : $index = $index;
 
 	$path = "./include/views/graphs/graphODS/";
 
