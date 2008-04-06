b200018dbec2b152dedf19cdc5f6bd4344afae30
Julien Mathis <jmathis@merethis.com>
2008-04-06 22:44:06 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@4503 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/MakeXML_Ndo_service.php
+++ www/include/monitoring/engine/MakeXML_Ndo_service.php
@@ -22,13 +22,13 @@
 	include("DB.php");
 
 	include_once("/etc/centreon/centreon.conf.php");
-	include_once($centreon_path . "www/class/other.class.php");
-	include_once($centreon_path . "www/DBconnect.php");
-	include_once($centreon_path . "www/DBOdsConnect.php");
-	include_once($centreon_path . "www/DBNDOConnect.php");	
-	include_once($centreon_path . "www/include/monitoring/engine/common-Func.php");
-	include_once($centreon_path . "www/include/common/common-Func-ACL.php");
-	include_once($centreon_path . "www/include/common/common-Func.php");
+	include_once($centreon_path."www/class/other.class.php");
+	include_once($centreon_path."www/DBconnect.php");
+	include_once($centreon_path."www/DBOdsConnect.php");
+	include_once($centreon_path."www/DBNDOConnect.php");	
+	include_once($centreon_path."www/include/monitoring/engine/common-Func.php");
+	include_once($centreon_path."www/include/common/common-Func-ACL.php");
+	include_once($centreon_path."www/include/common/common-Func.php");
 
 	$ndo_base_prefix = getNDOPrefix();
 	$general_opt = getStatusColor($pearDB);
