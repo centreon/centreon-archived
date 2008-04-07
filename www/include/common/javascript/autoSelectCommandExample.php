dd4f664eedc96ddc76ed33bd50c2ab8f3e283698
Julien Mathis <jmathis@merethis.com>
2008-04-07 14:21:45 +0000

    change centreon config file path
    git-svn-id: http://svn.centreon.com/trunk/centreon@4514 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/javascript/autoSelectCommandExample.php
+++ www/include/common/javascript/autoSelectCommandExample.php
@@ -1,28 +1,25 @@
 <?php
-/**
-Centreon is developped with GPL Licence 2.0 :
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Julien Mathis - Romain Le Merlus
-
-Adapted to Pear library by Merethis company, under direction of Cedrick Facon, Romain Le Merlus, Julien Mathis
-
-The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
-whether express or implied, and without limitation, with regard to the quality,
-safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
-been previously advised of the possibility of such damages.
-
-For information : contact@oreon-project.org
-*/
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@oreon-project.org
+ */
 
 	# return argument for specific command in txt format
 	# use by ajax
 
-	require_once("../../../../etc/centreon.conf.php");
-	require_once("../../../DBconnect.php");
+	require_once("/etc/centreon/centreon.conf.php");
+	require_once($centreon_path."www/DBconnect.php");
 
 	function myDecodeService($arg)	{
 		$arg = str_replace('#BR#', "\\n", $arg);
@@ -35,12 +32,14 @@ For information : contact@oreon-project.org
 	
 	header('Content-type: text/html; charset=iso-8859-1');
 
-	if(isset($_POST["index"])){
-		$res =& $pearDB->query("SELECT command_example FROM command WHERE command_id = '". $_POST["index"] ."' ");
-		if (PEAR::isError($res))
-			print "Mysql Error : ".$res->getMessage();
-		while ($arg = $res->fetchRow())
+	if (isset($_POST["index"])){
+		$DBRESULT =& $pearDB->query("SELECT command_example FROM command WHERE command_id = '". $_POST["index"] ."' ");
+		if (PEAR::isError($DBRESULT))
+			print "Mysql Error : ".$DBRESULT->getMessage();
+		while ($arg = $DBRESULT->fetchRow())
 			echo utf8_encode(myDecodeService($arg["command_example"]));
+		unset($arg);
+		unset($DBRESULT);
 		$pearDB->disconnect();
 	}	
 ?>
