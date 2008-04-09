3b13cf3e214bca8fb0e95028b50977ea46a2f23b
Sylvestre Ho <shotamchay@merethis.com>
2008-04-09 11:52:27 +0000

    tactical overview working fine
    git-svn-id: http://svn.centreon.com/trunk/centreon@4562 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/tacticalOverview/tacticalOverview.php
+++ www/include/home/tacticalOverview/tacticalOverview.php
@@ -51,24 +51,28 @@
 	/*
 	 * Check ACL and generate ACL restrictions
 	 */
-	if (!$is_admin){
+	if (!$is_admin)
+	{
 		$lca = getLcaHostByName($pearDB);
 		$lcaSTR = getLCAHostStr($lca["LcaHost"]);
-	}
+    }
+
 	/*
 	 * Get Status Globals for hosts
 	 */
 	if (!$is_admin)
 		$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state), ".$ndo_base_prefix."hoststatus.current_state" .
 				" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects" .
-				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id".
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
 				" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR)" .
 				" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 				" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
 	else
 		$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state) , ".$ndo_base_prefix."hoststatus.current_state" .
 				" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects " .
-				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id".
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
 				" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 				" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
 				
@@ -79,32 +83,214 @@
 	$hostStatus = array(0=>0, 1=>0, 2=>0, 3=>0);
 	while ($ndo = $DBRESULT_NDO1->fetchRow())
 		$hostStatus[$ndo["current_state"]] = $ndo["count(nagios_hoststatus.current_state)"];
-		
+	
 	/*
-	 * Get Host Down Ack
-	 */
-
+	 * Get the  id's of problem hosts
+	*/
+	if (!$is_admin)
+		$rq1 = 	" SELECT ".$ndo_base_prefix."hoststatus.host_object_id, " .$ndo_base_prefix. "hoststatus.current_state, " . $ndo_base_prefix."objects.name1".
+				" FROM ".$ndo_base_prefix."servicestatus, ".$ndo_base_prefix."hoststatus, " . $ndo_base_prefix."services, " . $ndo_base_prefix. "objects" .
+				" WHERE ".$ndo_base_prefix."servicestatus.service_object_id = ".$ndo_base_prefix."services.service_object_id" . 
+				" AND ".$ndo_base_prefix."services.host_object_id = " . $ndo_base_prefix . "hoststatus.host_object_id" .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR) ".
+				" GROUP BY nagios_services.host_object_id";
+	else
+		$rq1 = 	" SELECT ".$ndo_base_prefix."services.host_object_id, " .$ndo_base_prefix. "hoststatus.current_state, " . $ndo_base_prefix."objects.name1".
+				" FROM ".$ndo_base_prefix."servicestatus, ".$ndo_base_prefix."hoststatus, " . $ndo_base_prefix."services, " . $ndo_base_prefix. "objects" .
+				" WHERE ".$ndo_base_prefix."servicestatus.service_object_id = ".$ndo_base_prefix."services.service_object_id" . 
+				" AND ".$ndo_base_prefix."services.host_object_id = " . $ndo_base_prefix . "hoststatus.host_object_id" .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" GROUP BY nagios_services.host_object_id";
+				
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	$pbCount = 0;
+	while ($ndo = $DBRESULT_NDO1->fetchRow())
+	{
+		if ($ndo["current_state"] != 0)
+		{
+			$hostPb[$pbCount] = $ndo["host_object_id"];
+			$hostPbName[$pbCount] = $ndo["name1"];
+			$pbCount++;
+		}
+	}
+	
 	/*
-	 * Get Host Down Not Actif
+	 * Get Host Ack  UP(0), DOWN(1),  UNREACHABLE(2)
 	 */
-
-
+	if (!$is_admin)
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."acknowledgements.state), ".$ndo_base_prefix."acknowledgements.state" .
+				" FROM ".$ndo_base_prefix."acknowledgements, ".$ndo_base_prefix."objects, ".$ndo_base_prefix."hoststatus" .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."acknowledgements.object_id" .
+				" AND ".$ndo_base_prefix."acknowledgements.object_id = ".$ndo_base_prefix."hoststatus.host_object_id" .
+				" AND ".$ndo_base_prefix."hoststatus.problem_has_been_acknowledged = 1 " .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" AND ".$ndo_base_prefix."acknowledgements.acknowledgement_type = 0 " .	
+				" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR)" .
+				" GROUP BY ".$ndo_base_prefix."acknowledgements.state " .
+				" ORDER by ".$ndo_base_prefix."acknowledgements.state";
+	else
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."acknowledgements.state), ".$ndo_base_prefix."acknowledgements.state" .
+				" FROM ".$ndo_base_prefix."acknowledgements, ".$ndo_base_prefix."objects, ".$ndo_base_prefix."hoststatus" .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."acknowledgements.object_id" .
+				" AND ".$ndo_base_prefix."acknowledgements.object_id = ".$ndo_base_prefix."hoststatus.host_object_id" .
+				" AND ".$ndo_base_prefix."hoststatus.problem_has_been_acknowledged = 1 " .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" AND ".$ndo_base_prefix."acknowledgements.acknowledgement_type = 0 " .
+				" GROUP BY ".$ndo_base_prefix."acknowledgements.state " .
+				" ORDER by ".$ndo_base_prefix."acknowledgements.state";
+				
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	$hostAck = array(0=>0, 1=>0, 2=>0);
+	while ($ndo = $DBRESULT_NDO1->fetchRow())
+		$hostAck[$ndo["state"]] = $ndo["count(nagios_acknowledgements.state)"];
+	 
 	/*
-	 * Get Host Unrea Ack
+	 * Get Host inactive objects
 	 */
-
-	/*
-	 * Get Host Unrea Not Actif
-	 */
-
+	if (!$is_admin)
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state), ".$ndo_base_prefix."hoststatus.current_state" .
+				" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects" .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 0 " .
+				" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR)" .
+				" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
+				" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
+	else
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."hoststatus.current_state) , ".$ndo_base_prefix."hoststatus.current_state" .
+				" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects " .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 0 " .
+				" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
+				" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
+				
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	$hostInactive = array(0=>0, 1=>0, 2=>0, 3=>0);
+	while ($ndo = $DBRESULT_NDO1->fetchRow())
+		$hostInactive[$ndo["current_state"]] = $ndo["count(nagios_hoststatus.current_state)"];
+	 
+	 
 	/*
 	 * Get Host Unrea Not Unhandled
 	 */
 	
 	/*
 	 * Get Status global for Services
+	 */		
+	if (!$is_admin)
+		$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
+				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, centreon_acl" .
+				" WHERE no.object_id = nss.service_object_id".
+				" AND no.name1 not like 'OSL_Module' ".
+				" AND no.name1 not like 'Meta_Module' ".
+				" AND no.name1 = centreon_acl.host_name ".
+				" AND no.name2 = centreon_acl.service_description " .
+				" AND centreon_acl.group_id IN (".groupsListStr(getGroupListofUser($pearDB)).") " .
+				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
+	else
+		$rq2 = 	" SELECT count(nss.current_state), nss.current_state". 
+				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
+				" WHERE no.object_id = nss.service_object_id".
+				" AND no.name1 not like 'OSL_Module' ".
+				" AND no.name1 not like 'Meta_Module' ".
+				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
+				
+	$DBRESULT_NDO2 =& $pearDBndo->query($rq2);
+	if (PEAR::isError($DBRESULT_NDO2))
+		print "DB Error : ".$DBRESULT_NDO2->getDebugInfo()."<br />";
+
+	$SvcStat = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
+
+	while($DBRESULT_NDO2->fetchInto($ndo))
+		$SvcStat[$ndo["current_state"]] = $ndo["count(nss.current_state)"];
+
+
+	/*
+	 * Get on pb host
+	*/
+	if (!$is_admin)
+		$rq2 = 	" SELECT nss.current_state,". $ndo_base_prefix ."services.host_object_id".
+				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, centreon_acl, " . $ndo_base_prefix."services" .
+				" WHERE no.object_id = nss.service_object_id".
+				" AND nss.service_object_id = ".$ndo_base_prefix."services.service_object_id".
+				" AND no.name1 not like 'OSL_Module' ".
+				" AND no.name1 not like 'Meta_Module' ".
+				" AND no.name1 = centreon_acl.host_name ".
+				" AND no.name2 = centreon_acl.service_description " .
+				" AND centreon_acl.group_id IN (".groupsListStr(getGroupListofUser($pearDB)).") " .
+				" AND no.is_active = 1" .
+				" AND nss.problem_has_been_acknowledged = 0" .
+				" AND nss.current_state > 0";
+	else
+		$rq2 = 	" SELECT nss.current_state,". $ndo_base_prefix ."services.host_object_id".
+				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, " . $ndo_base_prefix."services" .
+				" WHERE no.object_id = nss.service_object_id".
+				" AND nss.service_object_id = ".$ndo_base_prefix."services.service_object_id".
+				" AND no.name1 not like 'OSL_Module' ".
+				" AND no.name1 not like 'Meta_Module' ".
+				" AND no.is_active = 1" .
+				" AND nss.problem_has_been_acknowledged = 0" .
+				" AND nss.current_state > 0";
+	
+	$onPbHost = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
+	
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq2);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	while($ndo = $DBRESULT_NDO1->fetchRow())
+	{
+		if ($ndo["current_state"] != 0)
+			for($i=0; $i<=$pbCount; $i++)
+				if (isSet($hostPb[$i]) && ($hostPb[$i] == $ndo["host_object_id"]))
+					$onPbHost[$ndo["current_state"]]++;
+	}
+
+	
+	/*
+	 * Get ServiceAck  OK(0), WARNING(1),  CRITICAL(2), UNKNOWN(3)
 	 */
-		
+	if (!$is_admin)
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."acknowledgements.state), ".$ndo_base_prefix."acknowledgements.state" .
+				" FROM ".$ndo_base_prefix."acknowledgements, ".$ndo_base_prefix."objects, ".$ndo_base_prefix."servicestatus" .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."acknowledgements.object_id" .
+				" AND ".$ndo_base_prefix."acknowledgements.object_id = ".$ndo_base_prefix."servicestatus.service_object_id" .
+				" AND ".$ndo_base_prefix."servicestatus.problem_has_been_acknowledged = 1 " .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" AND ".$ndo_base_prefix."acknowledgements.acknowledgement_type = 1 " .	
+				" AND ".$ndo_base_prefix."objects.name1 IN ($lcaSTR)" .
+				" GROUP BY ".$ndo_base_prefix."acknowledgements.state " .
+				" ORDER by ".$ndo_base_prefix."acknowledgements.state";
+	else
+		$rq1 = 	" SELECT count(".$ndo_base_prefix."acknowledgements.state), ".$ndo_base_prefix."acknowledgements.state" .
+				" FROM ".$ndo_base_prefix."acknowledgements, ".$ndo_base_prefix."objects, ".$ndo_base_prefix."servicestatus" .
+				" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."acknowledgements.object_id" .
+				" AND ".$ndo_base_prefix."acknowledgements.object_id = ".$ndo_base_prefix."servicestatus.service_object_id" .
+				" AND ".$ndo_base_prefix."servicestatus.problem_has_been_acknowledged = 1 " .
+				" AND ".$ndo_base_prefix."objects.is_active = 1 " .
+				" AND ".$ndo_base_prefix."acknowledgements.acknowledgement_type = 1 " .
+				" GROUP BY ".$ndo_base_prefix."acknowledgements.state " .
+				" ORDER by ".$ndo_base_prefix."acknowledgements.state";
+				
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	$svcAck = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
+	while ($ndo = $DBRESULT_NDO1->fetchRow())
+		$svcAck[$ndo["state"]] = $ndo["count(nagios_acknowledgements.state)"];
+	
+	/*
+	 * Get Services  Inactive objects
+	 */
 	if (!$is_admin)
 		$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
 				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, centreon_acl " .
@@ -114,72 +300,80 @@
 				" AND no.name1 = centreon_acl.host_name ".
 				" AND no.name2 = centreon_acl.service_description " .
 				" AND centreon_acl.group_id IN (".groupsListStr(getGroupListofUser($pearDB)).") ".
-				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
+				" AND no.is_active = 0 GROUP BY nss.current_state ORDER by nss.current_state";
 	else
 		$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
 				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
 				" WHERE no.object_id = nss.service_object_id".
 				" AND no.name1 not like 'OSL_Module' ".
 				" AND no.name1 not like 'Meta_Module' ".
-				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";			
+				" AND no.is_active = 0 GROUP BY nss.current_state ORDER by nss.current_state";			
 	$DBRESULT_NDO2 =& $pearDBndo->query($rq2);
 	if (PEAR::isError($DBRESULT_NDO2))
 		print "DB Error : ".$DBRESULT_NDO2->getDebugInfo()."<br />";
 
-	$SvcStat = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
+	$svcInactive = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
 	while($DBRESULT_NDO2->fetchInto($ndo))
-		$SvcStat[$ndo["current_state"]] = $ndo["count(nss.current_state)"];
+		$svcInactive[$ndo["current_state"]] = $ndo["count(nss.current_state)"];
 
 	/*
-	 * Get Services Critical Ack
+	 * Get Undandled Services
 	 */
+	$svcUnhandled = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
 	
-	/*
-	 * Get Services Critical Disabled
-	 */
-
-	/*
-	 * Get Services Critical On host PB
-	 */
+	for ($i=0; $i<=4; $i++)
+		$svcUnhandled[$i] = $SvcStat[$i] - $svcAck[$i] - $svcInactive[$i] - $onPbHost[$i];
+	 
 	 
 	/*
-	 * Get Services Critical Un handled
-	 */
+	 * Get problem table
+	*/
+	if (!$is_admin)
+		$rq1 = 	" SELECT obj.name1, obj.name2, stat.current_state, stat.last_check, stat.output, unix_timestamp(stat.last_state_change) as last_state_change" .
+				" FROM ".$ndo_base_prefix."objects obj, ".$ndo_base_prefix."servicestatus stat" .
+				" WHERE obj.object_id = stat.service_object_id" .
+				" AND stat.current_state > 0" .
+				" AND stat.problem_has_been_acknowledged = 0" .
+				" AND obj.is_active = 1" .				
+				" AND obj.name1 IN ($lcaSTR)" .
+				" ORDER by stat.current_state DESC, obj.name1, stat.last_check";
+	else
+		$rq1 = 	" SELECT obj.name1, obj.name2, stat.current_state, stat.last_check, stat.output, unix_timestamp(stat.last_state_change) as last_state_change" .
+				" FROM ".$ndo_base_prefix."objects obj, ".$ndo_base_prefix."servicestatus stat" .
+				" WHERE obj.object_id = stat.service_object_id" .
+				" AND stat.current_state > 0" .
+				" AND stat.problem_has_been_acknowledged = 0" .
+				" AND obj.is_active = 1" .				
+				" ORDER by stat.current_state DESC, obj.name1, stat.last_check";
+				
+	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
+	if (PEAR::isError($DBRESULT_NDO1))
+		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
 	
-	/*
-	 * Get Services Warning Ack
-	 */
-	
-	/*
-	 * Get Services Warning Disabled
-	 */
+	$j = 0;	
+	while ($ndo =& $DBRESULT_NDO1->fetchRow())
+	{
+		$is_unhandled = 1;	
+		for($i=0; $i<=$pbCount && $is_unhandled; $i++)
+			if (isSet($hostPbName[$i]) && ($hostPbName[$i] == $ndo["name1"]))
+				$is_unhandled = 0;
+		if ($is_unhandled)
+		{
+			$tab_hostname[$j] = $ndo["name1"];
+			$tab_svcname[$j] = $ndo["name2"];
+			$tab_state[$j] = $ndo["current_state"];
+			$tab_last[$j] = $ndo["last_check"];
 
-	/*
-	 * Get Services Warning On host PB
-	 */
+			if ($ndo["last_state_change"] > 0 && time() > $ndo["last_state_change"])
+				$tab_duration[$j] = Duration::toString(time() - $ndo["last_state_change"]);
+			else if($ndo["last_state_change"] > 0)
+				$tab_duration[$j] = " - ";
+			$tab_output[$j] = $ndo["output"];
+			$j++;
+		}
+	}
 	 
-	/*
-	 * Get Services Warning Un handled
-	 */
-
-
-	/*
-	 * Get Services Unknown Ack
-	 */
-	
-	/*
-	 * Get Services Unknown Disabled
-	 */
-
-	/*
-	 * Get Services Unknown On host PB
-	 */
 	 
-	/*
-	 * Get Services Unknown Un handled
-	 */
-
-			
 	$path = "./include/home/tacticalOverview/";
 
 	# Smarty template Init
@@ -188,6 +382,57 @@
 	
 	$tpl->assign("color", $general_opt);
 	$tpl->assign("HostStatus", $hostStatus);
+	$tpl->assign("HostAck", $hostAck);
+	$tpl->assign("HostInact", $hostInactive);
 	$tpl->assign("ServiceStatus", $SvcStat);
+	$tpl->assign("SvcAck", $svcAck);
+	$tpl->assign("SvcInact", $svcInactive);
+	$tpl->assign("SvcOnPbHost", $onPbHost);
+	$tpl->assign("SvcUnhandled", $svcUnhandled);
+	$tpl->assign("tb_hostname", $tab_hostname);
+	$tpl->assign("tb_svcname", $tab_svcname);
+	$tpl->assign("tb_state", $tab_state);
+	$tpl->assign("tb_last", $tab_last);
+	$tpl->assign("tb_output", $tab_output);
+	$tpl->assign("tb_duration", $tab_duration);
+	
+	/*
+	 *  Strings for the host part
+	 */
+	$tpl->assign("str_hosts", _("Hosts"));
+	$tpl->assign("str_up", _("Up"));
+	$tpl->assign("str_down", _("Down"));
+	$tpl->assign("str_unreachable", _("Unreachable"));
+	
+	/*
+	 *  Strings for the service part
+	 */
+	$tpl->assign("str_services", _("Services"));
+	$tpl->assign("str_ok", _("OK"));
+	$tpl->assign("str_warning", _("Warning"));
+	$tpl->assign("str_critical", _("Critical"));
+	$tpl->assign("str_unknown", _("Unknown"));
+	$tpl->assign("str_pbhost", _("On Problem Host"));
+	$tpl->assign("str_svcunhandled", _("Unhandled"));
+	
+	/*
+	 *  Common Strings for both the host and service parts
+	 */
+ 	$tpl->assign("str_pending", _("Pending"));
+	$tpl->assign("str_disabled", _("Disabled"));
+	$tpl->assign("str_acknowledged", _("Acknowledged"));
+	
+	/*
+	 *  Strings for service problems
+	 */
+	$tpl->assign("str_unhandled", _("Unhandled Service problems"));
+	$tpl->assign("str_hostname", _("Host Name"));
+	$tpl->assign("str_servicename", _("Service Name"));
+	$tpl->assign("str_status", _("Status"));
+	$tpl->assign("str_lastcheck", _("Last Check"));
+	$tpl->assign("str_duration", _("Duration"));
+	$tpl->assign("str_output", _("Status Output"));
+	$tpl->assign("str_actions", _("Actions"));
+	
 	$tpl->display("tacticalOverview.ihtml");
  ?>
