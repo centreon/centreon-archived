72c3401ebfa9397b62ae5a5a111c555d4818f8a5
Julien Mathis <jmathis@merethis.com>
2008-04-20 20:59:23 +0000

    change header
    git-svn-id: http://svn.centreon.com/trunk/centreon@4757 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/nagiosStats/getStats.php
+++ www/include/nagiosStats/getStats.php
@@ -1,21 +1,19 @@
 <?php
-/**
-Centreon is developped with GPL Licence 2.0 :
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Julien Mathis - Romain Le Merlus
-
-The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
-whether express or implied, and without limitation, with regard to the quality,
-safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
-been previously advised of the possibility of such damages.
-
-For information : contact@oreon-project.org
-*/
-
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@oreon-project.org
+ */
 	
 	function escape_command($command) {
 		return ereg_replace("(\\\$|`)", "", $command);
@@ -70,54 +68,54 @@ For information : contact@oreon-project.org
 		$end = $_GET["end"];
 
 		 
-		$command_line = " graph - --start=".$start." --end=".$end;
+	$command_line = " graph - --start=".$start." --end=".$end;
 
-		/*
-		 * get all template infos
-		 */
-		 
-		$command_line .= " --interlaced --imgformat PNG --width=500 --height=120 --title='".$title[$_GET["key"]]."' --vertical-label='".$_GET["key"]."' --slope-mode  ";
-		$command_line .= "--rigid --alt-autoscale-max ";
-				
-		/*
-		 * Init DS template For each curv
-		 */
-		
-		$colors = array("1"=>"#19EE11", "2"=>"#82CFD8", "3"=>"#F8C706", "4"=>"#F8C706");
-		//$metrics = array("Used" => 1, "High" => 2, "Total" => 3);
-		
-		$metrics = $differentStats[$options[$_GET["key"]]];
-		$cpt = 1;
-		foreach ($metrics as $key => $value){
-			$command_line .= " DEF:v".$cpt."=/var/lib/ods/perfmon-".$_GET["ns_id"]."/".$options[$_GET["key"]].":".$value.":AVERAGE ";
-			$cpt++;
+	/*
+	 * get all template infos
+	 */
+	 
+	$command_line .= " --interlaced --imgformat PNG --width=500 --height=120 --title='".$title[$_GET["key"]]."' --vertical-label='".$_GET["key"]."' --slope-mode  ";
+	$command_line .= "--rigid --alt-autoscale-max ";
+			
+	/*
+	 * Init DS template For each curv
+	 */
+	
+	$colors = array("1"=>"#19EE11", "2"=>"#82CFD8", "3"=>"#F8C706", "4"=>"#F8C706");
+	//$metrics = array("Used" => 1, "High" => 2, "Total" => 3);
+	
+	$metrics = $differentStats[$options[$_GET["key"]]];
+	$cpt = 1;
+	foreach ($metrics as $key => $value){
+		$command_line .= " DEF:v".$cpt."=/var/lib/ods/perfmon-".$_GET["ns_id"]."/".$options[$_GET["key"]].":".$value.":AVERAGE ";
+		$cpt++;
+	}
+	$command_line .= " COMMENT:\" \\l\" ";
+	
+	
+	# Create Legende
+	$cpt = 1;
+
+	foreach ($metrics as $key => $tm){
+		//$command_line .= " AREA:v".($cpt).$colors[$cpt]."3c ";
+		$command_line .= " LINE1:v".($cpt);
+		$command_line .= $colors[$cpt].":\"";
+		$command_line .= $tm."\"";
+		$cpt++;
+	}
+
+	$command_line = "/usr/bin/rrdtool ".$command_line." 2>&1";
+	$command_line = escape_command("$command_line");
+
+	//print $command_line;
+	$fp = popen($command_line  , 'r');
+	if (isset($fp) && $fp ) {
+		$str ='';
+		while (!feof ($fp)) {
+	  		$buffer = fgets($fp, 4096);
+	 		$str = $str . $buffer ;
 		}
-		$command_line .= " COMMENT:\" \\l\" ";
-		
-		
-		# Create Legende
-		$cpt = 1;
-
-		foreach ($metrics as $key => $tm){
-			//$command_line .= " AREA:v".($cpt).$colors[$cpt]."3c ";
-			$command_line .= " LINE1:v".($cpt);
-			$command_line .= $colors[$cpt].":\"";
-			$command_line .= $tm."\"";
-			$cpt++;
-		}
-
-		$command_line = "/usr/bin/rrdtool ".$command_line." 2>&1";
-		$command_line = escape_command("$command_line");
-
-		//print $command_line;
-		$fp = popen($command_line  , 'r');
-		if (isset($fp) && $fp ) {
-			$str ='';
-			while (!feof ($fp)) {
-		  		$buffer = fgets($fp, 4096);
-		 		$str = $str . $buffer ;
-			}
-			print $str;
-		}
+		print $str;
+	}
 	
 ?>
