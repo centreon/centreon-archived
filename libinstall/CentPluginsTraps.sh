a48539a9ff30847ec59693cb37215767beb93304
Guillaume Watteeux <guillaume.watteeux@gmail.com>
2008-05-19 22:20:26 +0000

    Install Script: Plugins Traps (task #3)  * Need tests...
    git-svn-id: http://svn.centreon.com/trunk/centreon@5196 6bcd3966-0018-0410-8128-fd23d134de7e

--- libinstall/CentPluginsTraps.sh
+++ libinstall/CentPluginsTraps.sh
@@ -10,57 +10,119 @@ echo "$line"
 ## Where is nagios_pluginsdir
 locate_nagios_plugindir
 
-## Locale for sed
-locate_nagios_vardir
-locate_nagios_installdir
-locate_nagios_etcdir
-locate_rrd_perldir
-locate_centplugins_tmpdir
+## Locate centreon etc_dir
+locate_centreon_etcdir
+locate_snmp_etcdir
+locate_snmptt_bindir
 
-## check nagios user and group
-check_user_nagios
 check_group_nagios
-
+check_user_apache
 
 ## Populate temporaty source directory
 copyInTempFile 2>>$LOG_FILE
 
 ## Create temporary folder
 log "INFO" "$(gettext "Create working directory")"
-mkdir -p $TMPDIR/final/plugins/traps
-mkdir -p $TMPDIR/work/plugins/traps
+mkdir -p $TMPDIR/final/bin
+mkdir -p $TMPDIR/work/bin
+mkdir -p $TMPDIR/work/snmptrapd
+mkdir -p $TMPDIR/final/snmptrapd
+mkdir -p $TMPDIR/work/snmptt
+mkdir -p $TMPDIR/final/snmptt
 
 ## Change Macro in working dir
-for FILE in `ls $TMPDIR/src/plugins/src/check*centreon*` \
-	$TMPDIR/src/plugins/src/centreon.pm \
-	$TMPDIR/src/plugins/src/centreon.conf \
-	$TMPDIR/src/plugins/src/check_meta_service \
-	`ls $TMPDIR/src/plugins/src/check_snmp*` \
-	$TMPDIR/src/plugins/src/process-service-perfdata \
-	$TMPDIR/src/plugins/src/submit_host_check_result \
-	$TMPDIR/src/plugins/src/submit_service_check_result; do
+for FILE in  $TMPDIR/bin/centFillTrapDB \
+	$TMPDIR/bin/centGenSnmpttConfFile \
+	$TMPDIR/bin/centTrapHandler-2.x ; do
 
-	sed -e 's|@NAGIOS_VAR@|'"$NAGIOS_VAR"'|g' \
-		-e 's|@INSTALL_DIR_NAGIOS@|'"$INSTALL_DIR_NAGIOS"'|g' \
-		-e 's|@NAGIOS_ETC@|'"$NAGIOS_ETC"'|g' \
-		-e 's|@NAGIOS_PLUGINS@|'"$NAGIOS_PLUGIN"'|g' \
-		-e 's|@RRDTOOL_PERL_LIB@|'"$RRD_PERL"'|g' \
-		-e 's|@INSTALL_DIR_CENTREON@|'"$INSTALL_DIR_CENTREON"'|g' \
-		-e 's|@CENTPLUGINS_TMP@|'"$CENTPLUGINS_TMP"'|g' \
-		"$FILE" > "$TMPDIR/work/plugins/`basename $FILE`"
+	sed -e 's|@CENTREON_ETC@|'"$CENTREON_ETC"'|g' \
+		"$FILE" > "$TMPDIR/work/bin/`basename $FILE`"
 done
 
 ## Copy in final dir
-log "INFO" "$(gettext "Copying plugins in final directory")"
-cp -r $TMPDIR/work/plugins/* $TMPDIR/final/plugins >> $LOG_FILE 2>&1
+log "INFO" "$(gettext "Copying Traps binaries in final directory")"
+cp -r $TMPDIR/work/bin/* $TMPDIR/final/bin >> $LOG_FILE 2>&1
 
-## Install the plugins
-log "INFO" "$(gettext "Installing the plugins")"
-$INSTALL_DIR/cinstall -m 755 -v -p $TMPDIR/final/plugins \
-	$TMPDIR/final/plugins/* $NAGIOS_PLUGIN >> $LOG_FILE 2>&1
+## Install the plugins traps binaries
+log "INFO" "$(gettext "Installing the plugins Traps binaries")"
+$INSTALL_DIR/cinstall -m 755 -v -p $TMPDIR/final/bin \
+	$TMPDIR/final/bin/* $CENTPLUGINSTRAPS_BINDIR >> $LOG_FILE 2>&1
 
-log "INFO" "$(gettext "Install temporary directory for plugins") : $CENTPLUGINS_TMP"
-$INSTALL_DIR/cinstall -u $NAGIOS_USER -g $NAGIOS_GROUP -d 775 -v \
-	$CENTPLUGINS_TMP >> $LOG_FILE 2>&1
-echo_success "$(gettext "CentPlugins is installed")"
+# Create a SNMP config
+## Create centreon_traps directory
+$INSTALL_DIR/cinstall -u $WEB_USER -g $NAGIOS_GROUP -m 775 -v \
+	$SNMP_ETC/centreon_traps >> $LOG_FILE 2>&1
+
+# Backup snmptrapd.conf if exist
+if [ -e "$SNMP_ETC/snmptrapd.conf" ] ; then
+	log "INFO" "$(gettext "Backup") : $SNMP_ETC/snmptrapd.conf"
+	mv $SNMP_ETC/snmptrapd.conf $SNMP_ETC/snmptrapd.conf.bak-centreon
+fi
+# Backup snmptt.ini 
+if [ -e "$SNMP_ETC/centreon_traps/snmptt.ini" ] ; then
+	log "INFO" "$(gettext "Backup") : $SNMP_ETC/centreon_traps/snmptt.ini"
+	mv $SNMP_ETC/centreon_traps/snmptt.ini \
+		$SNMP_ETC/centreon_traps/snmptt.ini.bak-centreon
+fi
+# Backup snmp.conf if exist
+if [ -e "$SNMP_ETC/snmp.conf" ] ; then
+	log "INFO" "$(gettext "Backup") : $SNMP_ETC/snmp.conf"
+	mv $SNMP_ETC/snmp.conf $SNMP_ETC/snmp.conf.bak-centreon
+fi
+
+# Backup snmptt if exist
+if [ -e "$SNMPTT_BINDIR/snmptt" ] ; then
+	log "INFO" "$(gettext "Backup") : $SNMPTT_BINDIR/snmptt"
+	mv $SNMPTT_BINDIR/snmptt $SNMPTT_BINDIR/snmptt.bak-centreon
+fi
+
+# Backup snmptt if exist
+if [ -e "$SNMPTT_BINDIR/snmpttconvertmib" ] ; then
+	log "INFO" "$(gettext "Backup") : $SNMPTT_BINDIR/snmpttconvertmib"
+	mv $SNMPTT_BINDIR/snmpttconvertmib \
+		$SNMPTT_BINDIR/snmpttconvertmib.bak-centreon
+fi
+
+# Change macros on snmptrapd.conf
+sed -e 's|@SNMPTT_INI_FILE@|'"$SNMP_DIR/centreon_traps/snmptt.ini"'|g' \
+	$TMPDIR/src/snmptrapd/snmptrapd.conf > \
+	$TMPDIR/work/snmptrapd/snmptrapd.conf
+
+## Copy in final dir
+log "INFO" "$(gettext "Copying traps config in final directory")"
+cp -r $TMPDIR/work/snmptrapd/snmptrapd.conf \
+	$TMPDIR/final/snmptrapd/snmptrapd.conf >> $LOG_FILE 2>&1
+cp $TMPDIR/src/snmptt/snmptt.ini \
+	$TMPDIR/final/snmptt/snmptt.ini >> $LOG_FILE 2>&1
+cp $TMPDIR/src/snmptrapd/snmp.conf \
+	$TMPDIR/final/snmptrapd/snmp.conf >> $LOG_FILE 2>&1
+cp $TMPDIR/src/snmptt/snmptt \
+	$TMPDIR/final/snmptt/snmptt >> $LOG_FILE 2>&1
+cp $TMPDIR/src/snmptt/snmpttconvertmib \
+	$TMPDIR/final/snmptt/snmpttconvermib >> $LOG_FILE 2>&1
+
+
+## Install all config file
+$INSTALL_DIR/cinstall -v -m 644 \
+	$TMPDIR/final/snmptrapd/snmptrapd.conf \
+	$SNMP_ETC/snmptrapd.conf >> $LOG_FILE 2>&1
+
+$INSTALL_DIR/cinstall -v -m 644 \
+	$TMPDIR/final/snmptrapd/snmp.conf \
+	$SNMP_ETC/snmp.conf >> $LOG_FILE 2>&1
+
+$INSTALL_DIR/cinstall -u $WEB_USER -g $NAGIOS_GROUP -v -m 644 \
+	$TMPDIR/final/snmptt/snmptt.ini \
+	$SNMP_ETC/centreon_traps/snmptt.ini >> $LOG_FILE 2>&1
+
+$INSTALL_DIR/cinstall -v -m 755 \
+	$TMPDIR/final/snmptt/snmptt \
+	$SNMPTT_BINDIR/snmptt >> $LOG_FILE 2>&1
+
+$INSTALL_DIR/cinstall -v -m 755 \
+	$TMPDIR/final/snmptt/snmpttconvertmib \
+	$SNMPTT_BINDIR/snmpttconvertmib >> $LOG_FILE 2>&1
+
+## TODO : comment ^^ , log and echo_*
+#	: copy centreon.pm and centreon.conf if not exist
 
