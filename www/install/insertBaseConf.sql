0de1e5a3394871682ec1c8bf4e202f6d01907b4a
Julien Mathis <jmathis@merethis.com>
2008-05-26 10:21:07 +0000

    change version...
    git-svn-id: http://svn.centreon.com/trunk/centreon@5245 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `centreon`.`informations` (`key` ,`value`) VALUES ('version', '2.0-b2');
+INSERT INTO `centreon`.`informations` (`key` ,`value`) VALUES ('version', '2.0-b3');
 
 --
 -- Contenu de la table `nagios_server`
