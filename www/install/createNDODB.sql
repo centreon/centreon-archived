5ed40f3a7bd6a594381adae574d82de81c105482
Damien Duponchelle <dduponchelle@merethis.com>
2008-06-13 08:04:16 +0000

    They had errors in the SQL Syntax to create database NDOutils.
    git-svn-id: http://svn.centreon.com/trunk/centreon@5357 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/createNDODB.sql
+++ www/install/createNDODB.sql
@@ -1,5 +1,5 @@
 --
--- Base de donn�es: `centreon2_ndo`
+-- Base de données: `centreon2_ndo`
 --
 
 -- --------------------------------------------------------
@@ -855,7 +855,7 @@ CREATE TABLE IF NOT EXISTS `nagios_objects` (
   `name2` varchar(128) default NULL,
   `is_active` smallint(6) NOT NULL default '0',
   PRIMARY KEY  (`object_id`),
-  KEY `objecttype_id` (`objecttype_id`,`name1`,`name2`)
+  KEY `objecttype_id` (`objecttype_id`,`name1`,`name2`),
   KEY `name1` (`name1`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Current and historical objects of all kinds';
 
@@ -1156,7 +1156,7 @@ CREATE TABLE IF NOT EXISTS `nagios_services` (
   `icon_image` varchar(255) NOT NULL default '',
   `icon_image_alt` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`service_id`),
-  UNIQUE KEY `instance_id` (`instance_id`,`config_type`,`service_object_id`)
+  UNIQUE KEY `instance_id` (`instance_id`,`config_type`,`service_object_id`),
   KEY `host_object_id` (`host_object_id`),
   KEY `service_object_id` (`service_object_id`)  
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Service definitions';
@@ -1310,7 +1310,7 @@ CREATE TABLE IF NOT EXISTS `nagios_timedeventqueue` (
   `scheduled_time` datetime NOT NULL default '0000-00-00 00:00:00',
   `recurring_event` smallint(6) NOT NULL default '0',
   `object_id` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`timedeventqueue_id`)
+  PRIMARY KEY  (`timedeventqueue_id`),
   KEY `instance_id` (`instance_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='Current Nagios event queue';
 
