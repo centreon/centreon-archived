67e8dba8bcb2fb066759ff34fa9209e7bc8c54f0
Julien Mathis <jmathis@merethis.com>
2008-07-08 18:19:28 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@5548 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-DB-1.0-b4.sql
+++
@@ -1,9 +0,0 @@
-
-
-ALTER TABLE `host` ADD `display_name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER `host_alias` ;
-ALTER TABLE `host` ADD `initial_state` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER `host_checks_enabled` ;
-ALTER TABLE `host` ADD `flap_detection_options` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL AFTER `host_flap_detection_enabled` ;
-
-
-ALTER TABLE `service` CHANGE `service_activate` `service_activate` ENUM( '0', '1', '2' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '1' 
-ALTER TABLE `host` CHANGE `host_activate` `host_activate` ENUM( '0', '1', '2' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '1' 
