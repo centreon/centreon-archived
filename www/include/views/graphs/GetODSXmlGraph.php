b03b5da7f8ac53e45163ebee20ffe8fc95844d96
Guillaume Watteeux <guillaume.watteeux@gmail.com>
2008-07-17 07:28:20 +0000

    Remove hard path coding.
    git-svn-id: http://svn.centreon.com/trunk/centreon@5735 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/views/graphs/GetODSXmlGraph.php
+++ www/include/views/graphs/GetODSXmlGraph.php
@@ -34,8 +34,8 @@
 	 * pearDB init
 	 */ 
 	require_once 'DB.php';
-//	include_once("@CENTREON_ETC@/centreon.conf.php");
-	include_once("/etc/centreon/centreon.conf.php");
+	include_once("@CENTREON_ETC@/centreon.conf.php");
+//	include_once("/etc/centreon/centreon.conf.php");
 	include_once($centreon_path . "www/DBconnect.php");
 	include_once($centreon_path . "www/DBOdsConnect.php");
 	/*
