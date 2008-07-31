ec2d2354b493f846c7fc37554a1b23f50d9b9ffe
Julien Mathis <jmathis@merethis.com>
2008-07-31 16:10:13 +0000

    add icone for All logs
    git-svn-id: http://svn.centreon.com/trunk/centreon@5972 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-DB-2.0-b6_to_2.0-RC1.sql
+++ www/install/sql/Update-DB-2.0-b6_to_2.0-RC1.sql
@@ -6,10 +6,10 @@ INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topol
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Setup', './img/icones/16x16/press.gif', 507, 50701, 10, 1, './include/options/oreon/modules/modules.php', NULL, NULL, NULL, '1', NULL, NULL, NULL);
 
 
-UPDATE `centreon`.`topology` SET `topology_order` = '12' WHERE `topology`.`topology_page` = 502 LIMIT 1 ;
-UPDATE `centreon`.`topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 503 LIMIT 1 ;
-UPDATE `centreon`.`topology` SET `topology_order` = '14' WHERE `topology`.`topology_page` = 504 LIMIT 1 ;
-UPDATE `centreon`.`topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 505 LIMIT 1 ;
-UPDATE `centreon`.`topology` SET `topology_order` = '16' WHERE `topology`.`topology_page` = 506 LIMIT 1 ;
+UPDATE `topology` SET `topology_order` = '12' WHERE `topology`.`topology_page` = 502 LIMIT 1 ;
+UPDATE `topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 503 LIMIT 1 ;
+UPDATE `topology` SET `topology_order` = '14' WHERE `topology`.`topology_page` = 504 LIMIT 1 ;
+UPDATE `topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 505 LIMIT 1 ;
+UPDATE `topology` SET `topology_order` = '16' WHERE `topology`.`topology_page` = 506 LIMIT 1 ;
 
-
+UPDATE `topology` SET `topology_icone` = './img/icones/16x16/text_code.gif' WHERE `topology`.`topology_page` = 20301 LIMIT 1 ;
