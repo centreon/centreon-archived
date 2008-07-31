d0dd77a28c5a7b4c30d8f975cb640ecbdaf9573f
Julien Mathis <jmathis@merethis.com>
2008-07-31 14:32:16 +0000

    move menu for modules
    git-svn-id: http://svn.centreon.com/trunk/centreon@5967 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/Update-DB-2.0-b6_to_2.0-RC1.sql
@@ -0,0 +1,15 @@
+
+DELETE FROM topology WHERE topology_page = 50103;
+
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Modules', NULL, 5, 507, 10, 1, NULL, NULL, '0', '0', '1', NULL, NULL, NULL);
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Modules', NULL, 507, NULL, NULL, 1, NULL, NULL, NULL, NULL, '1', NULL, NULL, NULL);
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Setup', './img/icones/16x16/press.gif', 507, 50701, 10, 1, './include/options/oreon/modules/modules.php', NULL, NULL, NULL, '1', NULL, NULL, NULL);
+
+
+UPDATE `centreon`.`topology` SET `topology_order` = '12' WHERE `topology`.`topology_page` = 502 LIMIT 1 ;
+UPDATE `centreon`.`topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 503 LIMIT 1 ;
+UPDATE `centreon`.`topology` SET `topology_order` = '14' WHERE `topology`.`topology_page` = 504 LIMIT 1 ;
+UPDATE `centreon`.`topology` SET `topology_order` = '15' WHERE `topology`.`topology_page` = 505 LIMIT 1 ;
+UPDATE `centreon`.`topology` SET `topology_order` = '16' WHERE `topology`.`topology_page` = 506 LIMIT 1 ;
+
+
