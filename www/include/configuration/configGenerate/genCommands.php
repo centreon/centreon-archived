6560fbaef8f667bf263439a4fff5b445d382c5d3
Julien Mathis <jmathis@merethis.com>
2008-08-01 13:33:42 +0000

    feature #40 : write command example in command config file
    git-svn-id: http://svn.centreon.com/trunk/centreon@5984 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configGenerate/genCommands.php
+++ www/include/configuration/configGenerate/genCommands.php
@@ -31,26 +31,38 @@
 	$i2 = 1;
 	$str1 = NULL;
 	$str2 = NULL;
-	while($DBRESULT->fetchInto($command))	{
+	while ($command =& $DBRESULT->fetchRow())	{
 		$command["command_line"] = str_replace('#BR#', "\\n", $command["command_line"]);
 		$command["command_line"] = str_replace('#T#', "\\t", $command["command_line"]);
 		$command["command_line"] = str_replace('#R#', "\\r", $command["command_line"]);
 		$command["command_line"] = str_replace('#S#', "/", $command["command_line"]);
 		$command["command_line"] = str_replace('#BS#', "\\", $command["command_line"]);
-		# Notification Command case -> command_type == 1
 		if ($command["command_type"] == 1 || $command["command_type"] == 3)	{
+			/*
+			 * Notification Command case -> command_type == 1
+			 */
 			$ret["comment"] ? ($str1 .= "# '" . $command["command_name"] . "' command definition " . $i1 . "\n") : NULL;
 			$str1 .= "define command{\n";
-			$str1 .= print_line("command_name", $command["command_name"]);
-			$str1 .= print_line("command_line", str_replace("@MAILER@", $oreon->optGen["mailer_path_bin"], $command["command_line"]));
+			if ($command["command_name"]) 
+				$str1 .= print_line("command_name", $command["command_name"]);
+			if ($command["command_line"]) 
+				$str1 .= print_line("command_line", str_replace("@MAILER@", $oreon->optGen["mailer_path_bin"], $command["command_line"]));
+			if ($command["command_example"]) 
+				$str1 .= print_line(";command_example", $command["command_example"]);
 			$str1 .= "}\n\n";
 			$i1++;
 		} else if ($command["command_type"] == 2)	{
-		# Check Command case -> command_type == 2
+			/*
+			 * Check Command case -> command_type == 2
+			 */
 			$ret["comment"] ? ($str2 .= "# '" . $command["command_name"] . "' command definition " . $i2 . "\n") : NULL;
 			$str2 .= "define command{\n";
-			if ($command["command_name"]) $str2 .= print_line("command_name", $command["command_name"]);
-			if ($command["command_line"]) $str2 .= print_line("command_line", str_replace("@MAILER@", $oreon->optGen["mailer_path_bin"], $command["command_line"]));
+			if ($command["command_name"]) 
+				$str2 .= print_line("command_name", $command["command_name"]);
+			if ($command["command_line"]) 
+				$str2 .= print_line("command_line", str_replace("@MAILER@", $oreon->optGen["mailer_path_bin"], $command["command_line"]));
+			if ($command["command_example"]) 
+				$str2 .= print_line(";command_example", $command["command_example"]);
 			$str2 .= "}\n\n";
 			$i2++;
 		}
