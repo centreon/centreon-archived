eed7a7af772ff1ddc6c6ffcaf2d0bb93517e218d
Sylvestre Ho <shotamchay@merethis.com>
2008-08-07 16:26:27 +0000

    patch from jean loup gregoire
    git-svn-id: http://svn.centreon.com/trunk/centreon@6079 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/pie-svc.php
+++ www/include/home/pie-svc.php
@@ -22,7 +22,7 @@
 	$oreon =& $_SESSION["oreon"];
 
 	require_once("DB.php");
-	include_once("@CENTREON_ETC@/centreon.conf.php");
+	include_once("@CENTREON_ETC@/centreon.conf.php");	
 		
 	/* Connect to oreon DB */
 	$dsn = array('phptype'  => 'mysql',
@@ -46,14 +46,62 @@
 	## calcul stat for resume
 	$statistic = array(0 => "OK", 1 => "WARNING", 2 => "CRITICAL", 3 => "UNKNOWN", 4 => "PENDING");
 	
-	/* Get HostNDO status */
+        /*
+         * LCA
+         */
+
+        $sid = session_id();
+        $res1 =& $pearDB->query("SELECT user_id FROM session WHERE session_id = '$sid'");
+        $user = $res1->fetchRow();
+        $user_id = $user["user_id"];
+
+        $res2 =& $pearDB->query("SELECT contact_admin FROM contact WHERE contact_id = '".$user_id."'");
+        $admin = $res2->fetchrow();
+
+        global $is_admin;
+
+        $is_admin = 0;
+        $is_admin = $admin["contact_admin"];
+
+        if (!$is_admin){
+                $_POST["sid"] = $_GET["sid"];
+        }
+
 	/* Get ServiceNDO status */
+	if(!$is_admin){
+
+	$DBRESULT =& $pearDB->query("SELECT acl_group_id FROM acl_group_contacts_relations WHERE acl_group_contacts_relations.contact_contact_id = '$user_id'");
+        $DBRESULT_1 = $DBRESULT->fetchRow();
+        $acl_group_id = $DBRESULT_1["acl_group_id"];
+
+	$DBRESULT =& $pearDB->query("SELECT acl_res_id FROM acl_res_group_relations, acl_groups WHERE acl_res_group_relations.acl_group_id = '$acl_group_id' AND acl_groups.acl_group_activate = '1'");
+        $DBRESULT_1 = $DBRESULT->fetchRow();
+        $acl_res_id = $DBRESULT_1["acl_res_id"];
+
+        $DBRESULT =& $pearDB->query("SELECT host_host_id FROM acl_resources_host_relations WHERE acl_res_id = '$acl_res_id'");
+        while($DBRESULT_1 = $DBRESULT->fetchRow()){
+                $host_host_id = $DBRESULT_1["host_host_id"];
+		$DBRESULT_2 =& $pearDB->query("SELECT host_name from host WHERE host_id = '$host_host_id'");
+		$DBRESULT_3 = $DBRESULT_2->fetchRow();
+		$host_name = $DBRESULT_3["host_name"];
+
+			$rq1 = "SELECT count(nss.current_state) as cnt, nss.current_state" .
+							" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
+							" WHERE no.object_id = nss.service_object_id".
+							" AND no.name1 not like 'OSL_Module'".
+							" AND no.name1 = '$host_name'".
+							" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
+	}
+	}
+	else
+	{
 	$rq1 = "SELECT count(nss.current_state) as cnt, nss.current_state" .
 			" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
 			" WHERE no.object_id = nss.service_object_id".
 			" AND no.name1 not like 'OSL_Module'".
 			" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
-	
+	}
+
 	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
 	if (PEAR::isError($DBRESULT_NDO1))
 		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
