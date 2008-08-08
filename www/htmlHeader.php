1cdfc17ad940ef74960137c8bdb760d372fac56c
Julien Mathis <jmathis@merethis.com>
2008-08-08 14:17:04 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@6106 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/htmlHeader.php
+++ www/htmlHeader.php
@@ -28,7 +28,7 @@
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $mlang; ?>">
 <head>
-<title> Centreon </title>
+<title>Centreon, Revisited Experience Of Nagios</title>
 <link rel="shortcut icon" href="./img/iconOreon.ico"/>
 <link rel="stylesheet" type="text/css" href="./include/common/javascript/autocompletion.css" />
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
