9bcf3412ff7047095b0c0779255ec536d6a70629
Julien Mathis <jmathis@merethis.com>
2008-08-13 08:20:08 +0000

    bug #145
    git-svn-id: http://svn.centreon.com/trunk/centreon@6130 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/traps-mibs/formMibs.php
+++ www/include/configuration/configObject/traps-mibs/formMibs.php
@@ -14,65 +14,60 @@
  * 
  * For information : contact@centreon.com
  */
- 
-
-	#
-	## Database retrieve information for Manufacturer
-	#
+ 	
+ 	if (!isset($oreon))
+ 		exit();
+	/*
+	 * Database retrieve information for Manufacturer
+	 */
 	
 	function myDecodeMib($arg)	{
 		$arg = html_entity_decode($arg, ENT_QUOTES);
 		return($arg);
 	}
 
-	$mnftr = array(NULL=>NULL);	
+	$mnftr = array(NULL => NULL);
 	$DBRESULT =& $pearDB->query("SELECT id, alias FROM traps_vendor order by alias");
 	if (PEAR::isError($DBRESULT))
 		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	# Set base value
-	while($rmnftr =& $DBRESULT->fetchRow()){
+
+	/*
+	 * Set base value
+	 */
+	while ($rmnftr =& $DBRESULT->fetchRow()){
 		$mnftr[$rmnftr["id"]] = $rmnftr["alias"];
 	}
 	$DBRESULT->free();
 	
-	##########################################################
-	# Var information to format the element
-	#
-	
-	#
-	## Form begin
-	#
+	/*
+	 * Init Formulary
+	 */
 	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
 	$form->addElement('header', 'title', _("Load a MIB"));
 
-	#
-	## Manufacturer information
-	#
+	/*
+	 * Manufacturer information
+	 */
 	$form->addElement('select', 'mnftr', _("Vendor Name"), $mnftr);
 	$form->addElement('file', 'filename', _("File (.mib)"));
 
-	#
-	## Further informations
-	#
-
-	#
-	## Form Rules
-	#
-	
+	/*
+	 * Formulary Rules
+	 */
 	$form->applyFilter('__ALL__', 'myTrim');
 	$form->addRule('mnftr', _("Compulsory Name"), 'required');
 	$form->addRule('filename', _("Compulsory Name"), 'required');
 	$form->setRequiredNote("<font style='color: red;'>*</font>". _(" Required fields"));
 
-	#
-	##End of form definition
-	#
-
-	# Smarty template Init
+	/*
+	 * Smarty template Init
+	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
 
-	# Just watch a Command information
+	/*
+	 * Just watch a Command information
+	 */
 	$subA =& $form->addElement('submit', 'submit', _("Export"));
 	$form->addElement('header', 'status',_("Status"));
 	$valid = false;
@@ -84,11 +79,10 @@
 		if ($fileObj->isUploadedFile()) {
 			$fileObj->moveUploadedFile("/tmp/");
 			$values = $fileObj->getValue();
-			$stdout = shell_exec("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=/tmp/".$values["name"]." --out=/tmp/".
-								$values["name"].".conf 2>&1");
+			$stdout = shell_exec("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=/tmp/".$values["name"]." --out=/tmp/".$values["name"].".conf 2>&1");
 			$msg .= "<br />".str_replace ("\n", "<br />", $stdout);
 			$msg .= "<br />Moving traps in DataBase...";	
-			$stdout = shell_exec($oreon->optGen["nagios_path_plugins"]."traps/centFillTrapDB -f /tmp/".
+			$stdout = shell_exec("@CENTPLUGINSTRAPS_BINDIR@/centFillTrapDB -f /tmp/".
 								$values["name"].".conf -m ".htmlentities($ret["mnftr"], ENT_QUOTES)." 2>&1");
 			shell_exec("rm /tmp/".$values["name"].".conf /tmp/".$values["name"]);
 			$msg .= "<br />".str_replace ("\n", "<br />", $stdout);
@@ -98,7 +92,10 @@
 		}	
 		$valid = true;
 	}
-	##Apply a template definition
+	
+	/*
+	 * Apply a template definition
+	 */
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
 	$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
