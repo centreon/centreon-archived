4a6af1f95e0fe26b5a6ff90030f242467634efd7
Damien Duponchelle <dduponchelle@merethis.com>
2008-08-21 10:17:58 +0000

    Errors and Warnings in apache resolved during pie chart generation in reporting interface. Comments has been added and the code normalized. One file deleted because doesn't use.
    git-svn-id: http://svn.centreon.com/trunk/centreon@6219 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/reporting/dashboard/imagesGenerations/pie-charts.php
+++ www/include/reporting/dashboard/imagesGenerations/pie-charts.php
@@ -15,55 +15,71 @@
  * For information : contact@centreon.com
  */
 
-	include_once("@CENTREON_ETC@/centreon.conf.php");
-	
-	require_once ($centreon_path."www/class/Session.class.php");
-	require_once ($centreon_path."www/class/Oreon.class.php");
+/**
+ * This script drawing pie chart of host or service on the reporting interface.
+ *
+ * PHP version 5
+ *
+ * @package pie-charts.php
+ * @author Damien Duponchelle dduponchelle@merethis.com
+ * @version $Id: $
+ * @copyright (c) 2007-2008 Centreon
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
 
+	// Including files and dependences
+	include_once '@CENTREON_ETC@/centreon.conf.php';
+	require_once $centreon_path.'www/class/Session.class.php';
+	require_once $centreon_path.'www/class/Oreon.class.php';
+
+	// Session Variables
 	Session::start();
 	$oreon =& $_SESSION["oreon"];
-	
-
-	// -----------------------------------------------------
 	$value =& $_GET["value"];
-	
-	foreach ($value as $key => $val)	{
-		if ($val)
-			if (!isset($oreon->optGen["color_".strtolower($key)])) {
-				$color[] = $oreon->optGen["color_unknown"];
-				$data[] = $val;
-				$legend[] = "Undetermined";
-			} else {
-				$color[] = $oreon->optGen["color_".strtolower($key)];		
-				$data[] = $val;
-				$legend[] =$key;
+
+	if(isset($value) == true) {
+		foreach ($value as $key => $val) {
+			if (isset($val) == true) {
+				if (isset($oreon->optGen["color_".strtolower($key)]) == false) {
+						$color[] = $oreon->optGen["color_unknown"];
+						$data[] = $val;
+						$legend[] = "Undetermined";
+					} else {
+						$color[] = $oreon->optGen["color_".strtolower($key)];		
+						$data[] = $val;
+						$legend[] = $key;
+				}
 			}
+		}
 	}
-	include_once($centreon_path . '/www/lib/ofc-library/open-flash-chart.php' );
-	$g = new graph();
-	$g->bg_colour = '#F3F6F6';
-	//
-	// PIE chart, 60% alpha
-	//
-	$g->pie(60,'#505050','#000000');
-	//
-	// pass in two arrays, one of data, the other data labels
-	//
 	
-	$g->pie_values( $data, $legend );
-	//
+	include_once($centreon_path . '/www/lib/ofc-library/open-flash-chart.php' );
+
+	// Declaring a new pie chart with a white background
+	$g = new graph();
+	$g->bg_colour = '#F3F6F6'; // Declaring white background for the pie chart.
+	$g->pie(60,'#505050','#000000'); // Initializing properties with a grey color and 60% (alpha)
+   
+    // Insert datas on the pie chart
+    if(isset($data) == true && isset($legend) == true) {	
+	$g->pie_values($data,$legend);
+    }
+
 	// Colours for each slice, in this case some of the colours
 	// will be re-used (3 colurs for 5 slices means the last two
 	// slices will have colours colour[0] and colour[1]):
-	//
-	
 	$g->pie_slice_colours($color);
-
 	$g->set_tool_tip( '#val#%' );
-	if (isset($_GET["service_name"]) && isset($_GET["host_name"]))
+	
+	// Initializing title of pie chart.
+	if (isset($_GET["service_name"]) == true && isset($_GET["host_name"]) == true) {
+		// Title for a service displayed like "Service on Host".
 		$g->title( $_GET["service_name"] . " on " . $_GET["host_name"], '{font-size:15px; color: #424242}' );
-	else if (isset($_GET["host_name"]))
+	} else if (isset($_GET["host_name"]) == true) {
+		// Title for a host.		
 		$g->title( $_GET["host_name"], '{font-size:18px; color: #424242}' );
+	}
+	
+	// Apply the current pie chart
 	echo $g->render();
-
 ?>
