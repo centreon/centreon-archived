74424514bbf6398489749d242b1969d1972902d0
Julien Mathis <jmathis@merethis.com>
2008-08-21 15:35:59 +0000

    new version of makeXML for bubulle
    git-svn-id: http://svn.centreon.com/trunk/centreon@6237 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/engine/makeXMLForOneService.php
+++ www/include/monitoring/engine/makeXMLForOneService.php
@@ -27,7 +27,7 @@
 	include_once($centreon_path . "www/include/common/common-Func.php");
 
 	$ndo_base_prefix = getNDOPrefix();
-	
+
 	/* security check 2/2*/
 	if (isset($_GET["sid"]) && !check_injection($_GET["sid"])){
 		$sid = $_GET["sid"];
@@ -37,20 +37,18 @@
 			get_error('bad session id');
 	} else
 		get_error('need session identifiant !');
-
+		
 	/*
 	 * Get Acl Group list
 	 */
 	
 	$grouplist = getGroupListofUser($pearDB); 
-	$groupnumber = count($grouplist);
-
-	(isset($_GET["host_id"]) && !check_injection($_GET["host_id"])) ? $host_id = htmlentities($_GET["host_id"]) : $host_id = "0";
+	$groupnumber = count($grouplist);	
+	
+	(isset($_GET["svc_id"]) && !check_injection($_GET["svc_id"])) ? $svc_id = htmlentities($_GET["svc_id"]) : $svc_id = "0";
 	(isset($_GET["enable"]) && !check_injection($_GET["enable"])) ? $enable = htmlentities($_GET["enable"]) : $enable = "enable";
 	(isset($_GET["disable"]) && !check_injection($_GET["disable"])) ? $disable = htmlentities($_GET["disable"]) : $disable = "disable";
 	(isset($_GET["date_time_format_status"]) && !check_injection($_GET["date_time_format_status"])) ? $date_time_format_status = htmlentities($_GET["date_time_format_status"]) : $date_time_format_status = "d/m/Y H:i:s";
-
-	/* security end*/
 
 	function get_centreon_date($date){
 		global $date_time_format_status;
@@ -60,35 +58,7 @@
 			return "N/A";
 	}
 
-	/* LCA */
-	// check is admin
-	$res1 =& $pearDB->query("SELECT user_id FROM session WHERE session_id = '".$sid."'");
-	$user =& $res1->fetchRow();
-	$user_id = $user["user_id"];
-	$res2 =& $pearDB->query("SELECT contact_admin,contact_lang FROM contact WHERE contact_id = '".$user_id."'");
-	$admin =& $res2->fetchRow();
-	$is_admin = 0;
-	$is_admin = $admin["contact_admin"];
-	$lang_ext = $admin["contact_lang"];
-	
-	// if is admin -> lca
-	if(!$is_admin){
-		$_POST["sid"] = $sid;
-		$lca =  getLCAHostByName($pearDB);
-		$lcaSTR = getLCAHostStr($lca["LcaHost"]);
-	}
-
-	$service = array();
-	$host_status = array();
-	$service_status = array();
-	$host_services = array();
-	$metaService_status = array();
-	$tab_host_service = array();
-
-	$DBRESULT_OPT =& $pearDB->query("SELECT color_ok,color_warning,color_critical,color_unknown,color_pending,color_up,color_down,color_unreachable FROM general_opt");
-	if (PEAR::isError($DBRESULT_OPT))
-		print "DB Error : ".$DBRESULT_OPT->getDebugInfo()."<br />";
-	$general_opt =& $DBRESULT_OPT->fetchRow();
+	$general_opt = getStatusColor($pearDB);
 
 	$tab_color_service = array();
 	$tab_color_service[0] = $general_opt["color_ok"];
@@ -105,53 +75,60 @@
 	$tab_status_svc = array("0" => "OK", "1" => "WARNING", "2" => "CRITICAL", "3" => "UNKNOWN", "4" => "PENDING");
 	$tab_status_host = array("0" => "UP", "1" => "DOWN", "2" => "UNREACHABLE");
 
-	$state_type = array("1" => "HARD","0" => "SOFT");
+	$state_type = array("1" => "HARD", "0" => "SOFT");
 
-	/* Get Host status */
-	$rq1 =  " SELECT nhs.current_state," .
-			" nh.address," .
-			" no.name1 as host_name," .
-			" nhs.perfdata," .
-			" nhs.current_check_attempt," .
-			" nhs.state_type," .
-			" unix_timestamp(nhs.last_check) as last_check," .
-			" unix_timestamp(nhs.next_check) as next_check," .
-			" nhs.latency," .
-			" nhs.execution_time," .
-			" unix_timestamp(nhs.last_state_change) as last_state_change," .
-			" unix_timestamp(nhs.last_notification) as last_notification," .
-			" unix_timestamp(nhs.next_notification) as next_notification," .
-			" unix_timestamp(nhs.last_hard_state_change) as last_hard_state_change," .
-			" nhs.last_hard_state," .
-			" unix_timestamp(nhs.last_time_up) as last_time_up," .
-			" unix_timestamp(nhs.last_time_down) as last_time_down," .
-			" unix_timestamp(nhs.last_time_unreachable) as last_time_unreachable," .
-			" nhs.current_notification_number," .
-			" nhs.scheduled_downtime_depth," .
-			" nhs.output," .
-			" ROUND(nhs.percent_state_change) as percent_state_change," .
-			" nh.notifications_enabled," .
-			" nh.event_handler_enabled," .
-			" nh.icon_image_alt" .
-			" FROM ".$ndo_base_prefix."hoststatus nhs, ".$ndo_base_prefix."objects no, ".$ndo_base_prefix."hosts nh" .
-			" WHERE no.object_id = " . $host_id .
-			" AND no.object_id = nhs.host_object_id and nh.host_object_id = no.object_id " .
+	/* 
+	 * Get Host status 
+	 */
+	$rq1 = "SELECT nss.current_state," .
+			" no.name1 as hostname," .
+			" no.name2 as service_description," .
+			" unix_timestamp(nss.last_check) as last_check," .
+			" unix_timestamp(nss.next_check) as next_check," .
+			" unix_timestamp(nss.last_state_change) as last_state_change," .
+			" unix_timestamp(nss.last_notification) as last_notification," .
+			" unix_timestamp(nss.next_notification) as next_notification," .
+			" unix_timestamp(nss.last_hard_state_change) as last_hard_state_change," .
+			" nss.last_hard_state," .
+			" nss.latency," .
+			" unix_timestamp(nss.last_time_ok) as last_time_ok," .
+			" unix_timestamp(nss.last_time_critical) as last_time_critical," .
+			" unix_timestamp(nss.last_time_unknown) as last_time_unknown," .
+			" unix_timestamp(nss.last_time_warning) as last_time_warning," .
+			" nss.current_notification_number," .
+			" nss.scheduled_downtime_depth," .
+			" nss.output," .
+			" ROUND(nss.percent_state_change) as percent_state_change," .
+			" nss.notifications_enabled," .
+			" nss.perfdata," .
+			" nss.state_type," .
+			" nss.execution_time," .
+			" nss.event_handler_enabled" .
+			" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
+			" WHERE no.object_id = " . $svc_id .
+			" AND no.object_id = nss.service_object_id " .
 			" AND no.name1 not like 'OSL_Module'".
-			" AND no.is_active = 1 AND no.objecttype_id = 1 AND nh.config_type = 1";
-		if (!$is_admin && $groupnumber)
-			$rq1 .= " AND no.name1 IN (".$lcaSTR." )";
-	$buffer .= '<reponse>';
-
+			" AND no.is_active = 1 AND no.objecttype_id = 2";
+	
+	/*
+	 * Request
+	 */
+	
 	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
 	if (PEAR::isError($DBRESULT_NDO1))
 		print "DB Error : ".$DBRESULT_NDO1->getDebugInfo()."<br />";
+	
+	/*
+	 * Init Buffer
+	 */
+		
+	$buffer .= '<reponse>';
+
 	$class = "list_one";
-	$ct = 0;
-	$flag = 0;
-	$c = array("1" => "#00ff00", "0" => "#ff0000");
 	$en = array("0" => _("No"), "1" => _("Yes"));
 
-	if($ndo =& $DBRESULT_NDO1->fetchRow()){
+	if ($ndo =& $DBRESULT_NDO1->fetchRow()){
+		$buffer .= '<svc_name><![CDATA['.$ndo["service_description"].']]></svc_name>';
 		$duration = "";
 		if ($ndo["last_state_change"] > 0)
 			$duration = Duration::toString(time() - $ndo["last_state_change"]);
@@ -164,14 +141,13 @@
 		if ($ndo["next_notification"] > 0)
 			$next_notification = $ndo["next_notification"];
 			
-		$buffer .= '<hostname><![CDATA['. $ndo["host_name"]  . ']]></hostname>';
-		$buffer .= '<address><![CDATA['. $ndo["address"]  . ']]></address>';
-		$buffer .= '<current_state color="'.$tab_color_host[$ndo["current_state"]].'">'. $tab_status_host[$ndo["current_state"]]  . '</current_state>';
+		$buffer .= '<service_description><![CDATA['.$ndo["service_description"].']]></service_description>';
+		$buffer .= '<hostname><![CDATA['.$ndo["hostname"].']]></hostname>';
+		$buffer .= '<current_state color="'.$tab_color_service[$ndo["current_state"]].'">'. $tab_status_svc[$ndo["current_state"]]  . '</current_state>';
 		$buffer .= '<current_state_name><![CDATA['. html_entity_decode(_("Host Status")).']]> </current_state_name>';
 		$buffer .= '<plugin_output name="'._("Status Information").'"><![CDATA['. $ndo["output"]  . ']]></plugin_output>';
 		$buffer .= '<performance_data>'. $ndo["perfdata"]  . '</performance_data>';
 		$buffer .= '<performance_data_name><![CDATA['.html_entity_decode(_("Performance Data")).']]></performance_data_name>';
-		$buffer .= '<current_attempt name="'._("Current Attempt").'">'. $ndo["current_check_attempt"]  . '</current_attempt>';
 		$buffer .= '<state_type>'.$state_type[$ndo["state_type"]].'</state_type>';
 		$buffer .= '<state_type_name><![CDATA['.html_entity_decode(_("State Type")).']]> </state_type_name>';
 		$buffer .= '<last_check >'. get_centreon_date($ndo["last_check"])  . '</last_check>';
@@ -185,7 +161,7 @@
 		$buffer .= '<last_state_change>'. get_centreon_date($ndo["last_state_change"])  . '</last_state_change>';
 		$buffer .= '<last_state_change_name><![CDATA['._("Last State Change").']]></last_state_change_name>';
 		$buffer .= '<duration>'. $duration  . '</duration>';
-		$buffer .= '<duration_name><![CDATA['.html_entity_decode(_("Current State Duration")).']]></duration_name>';
+		$buffer .= '<duration_name><![CDATA['.html_entity_decode(_("Current State Duration")).']]></duration_name>';	
 		$buffer .= '<last_notification>'.  get_centreon_date($last_notification)  . '</last_notification>';
 		$buffer .= '<last_notification_name><![CDATA['.html_entity_decode(_("Last Notification")).']]></last_notification_name>';
 		$buffer .= '<next_notification>'.  get_centreon_date($next_notification)  . '</next_notification>';
@@ -198,21 +174,29 @@
 		$buffer .= '<is_downtime_name><![CDATA['._("In Scheduled Downtime?").']]></is_downtime_name>';
 		$buffer .= '<last_update>'. get_centreon_date( time())  . '</last_update>';
 		$buffer .= '<last_update_name><![CDATA['._("Last Update").']]></last_update_name>';
-		$buffer .= '<last_time_up name="'.html_entity_decode(_("Last up time")).'">'. get_centreon_date( $ndo["last_time_up"])  . '</last_time_up>';
-		$buffer .= '<last_time_down name="'.html_entity_decode(_("Last down time")).'">'. get_centreon_date( $ndo["last_time_down"])  . '</last_time_down>';
-		$buffer .= '<last_time_unreachable name="'.html_entity_decode(_("Last unreachable time")).'">'. get_centreon_date( $ndo["last_time_unreachable"])  . '</last_time_unreachable>';
+		$buffer .= '<last_time_ok name="'.html_entity_decode(_("Last ok time")).'">'. get_centreon_date( $ndo["last_time_ok"])  . '</last_time_ok>';
+		$buffer .= '<last_time_warning name="'.html_entity_decode(_("Last warning time")).'">'. get_centreon_date( $ndo["last_time_warning"])  . '</last_time_warning>';
+		$buffer .= '<last_time_unknown name="'.html_entity_decode(_("Last unknown time")).'">'. get_centreon_date( $ndo["last_time_unknown"])  . '</last_time_unknown>';
+		$buffer .= '<last_time_critical name="'.html_entity_decode(_("Last critical time")).'">'. get_centreon_date( $ndo["last_time_critical"])  . '</last_time_critical>';
 
-		$ct++;
-	}
-	/* end */
-
-	if (!$ct){
+	} else {
 		$buffer .= '<infos>';
 		$buffer .= 'none';
 		$buffer .= '</infos>';
 	}
 
+	/*
+	 * Translations
+	 */
+
+	$buffer .= '<tr1><![CDATA['._("Ckeck informations").']]></tr1><tr2><![CDATA['._("Notification Informations").']]></tr2><tr3><![CDATA['._("Last Status Change").']]></tr3>';
+	
+	/*
+	 * End Buffer
+	 */
+
 	$buffer .= '</reponse>';
+
 	header('Content-type: text/xml; charset=iso-8859-1');
 	header('Cache-Control: no-cache, must-revalidate');
 
