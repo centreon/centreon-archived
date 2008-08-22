826bbb01f282856fce4dfe7a5b256aa6f3e0a959
Julien Mathis <jmathis@merethis.com>
2008-08-22 14:46:01 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@6294 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/options/oreon/generalOpt/general/charset.php
@@ -0,0 +1,46 @@
+<?php 
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@centreon.com
+ */
+
+	if (!isset($oreon))
+		exit();
+
+	$arrayCharSet = array(  "ISO-8859-1", "ISO-8859-2"
+							, "ISO-8859-3", "ISO-8859-4"
+							, "ISO-8859-5", "ISO-8859-6"
+							, "ISO-8859-7", "ISO-8859-8"
+							, "ISO-8859-9", "ISO-8859-10"
+							, "ISO-8859-13", "ISO-8859-14"
+							, "ISO-8859-15", "ISO-8859-16"
+							, "ISO-2022-JP", "ISO-2022-KR"
+							, "ISO-2022-CN", "WINDOWS-1251"
+							, "CP866", "KOI8"
+							, "KOI8-E", "KOI8-r"
+							, "KOI8-U", "KOI8-ru"
+							, "ISO-10646-UCS-2"
+							, "ISO-10646-UCS-4"
+							, "UTF-7", "UTF-8"
+							, "UTF-16", "UTF-16BE"
+							, "UTF-16LE", "UTF-32"
+							, "UTF-32BE", "UTF-32LE"
+							, "euc-cn", "euc-gb"
+							, "euc-jp", "euc-kr"
+							, "EUC-TW", "gb2312"
+							, "iso-10646-ucs-2"
+							, "iso-10646-ucs-4"
+							, "shift_jis");
+?>
+
