f0f99c0beb952cefeacb389eea4926fc60997e04
Julien Mathis <jmathis@merethis.com>
2008-08-24 10:50:50 +0000

    Et la norme ???
    git-svn-id: http://svn.centreon.com/trunk/centreon@6314 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/oreon/generalOpt/reporting/DB-Func.php
+++ www/include/options/oreon/generalOpt/reporting/DB-Func.php
@@ -1,59 +1,88 @@
 <?php
-function updateReportingTimePeriodInDB() {
-	global $form, $pearDB;
-	
-	$ret = $form->getSubmitValues();
-	(isset($ret["dayList"]["report_Monday"])) ? $ret["dayList"]["report_Monday"] = 1  : $ret["dayList"]["report_Monday"] = 0;
-	(isset($ret["dayList"]["report_Tuesday"])) ? $ret["dayList"]["report_Tuesday"] = 1  : $ret["dayList"]["report_Tuesday"] = 0;
-	(isset($ret["dayList"]["report_Wednesday"])) ? $ret["dayList"]["report_Wednesday"] = 1  : $ret["dayList"]["report_Wednesday"] = 0;
-	(isset($ret["dayList"]["report_Thursday"])) ? $ret["dayList"]["report_Thursday"] = 1  : $ret["dayList"]["report_Thursday"] = 0;
-	(isset($ret["dayList"]["report_Friday"])) ? $ret["dayList"]["report_Friday"] = 1  : $ret["dayList"]["report_Friday"] = 0;
-	(isset($ret["dayList"]["report_Saturday"])) ? $ret["dayList"]["report_Saturday"] = 1  : $ret["dayList"]["report_Saturday"] = 0;
-	(isset($ret["dayList"]["report_Sunday"])) ? $ret["dayList"]["report_Sunday"] = 1  : $ret["dayList"]["report_Sunday"] = 0;
-	
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Monday"]."' WHERE cp_contact_id is null AND cp_key = 'report_Monday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Tuesday"]."' WHERE cp_contact_id  is null AND cp_key = 'report_Tuesday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value =' ".$ret["dayList"]["report_Wednesday"]."' WHERE cp_contact_id is null AND cp_key = 'report_Wednesday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Thursday"]."' WHERE cp_contact_id  is null AND cp_key = 'report_Thursday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Friday"]."' WHERE cp_contact_id is null AND cp_key = 'report_Friday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Saturday"]."' WHERE cp_contact_id is null AND cp_key = 'report_Saturday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Sunday"]."' WHERE cp_contact_id is null AND cp_key = 'report_Sunday'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_hour_start"]."' WHERE cp_contact_id is null AND cp_key = 'report_hour_start'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_minute_start"]."' WHERE cp_contact_id is null AND cp_key = 'report_minute_start'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_hour_end"]."' WHERE cp_contact_id is null AND cp_key = 'report_hour_end'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_minute_end"]."' WHERE cp_contact_id is null AND cp_key = 'report_minute_end'";
-	$DBRESULT =& $pearDB->query($query);
-	if (PEAR::isError($DBRESULT))
-			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-}
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@centreon.com
+ */
+ 
+ 	if (!isset($oreon))
+ 		exit();
+ 	
+	function updateReportingTimePeriodInDB() {
+		global $form, $pearDB;
+		
+		$ret = $form->getSubmitValues();
+		(isset($ret["dayList"]["report_Monday"])) ? $ret["dayList"]["report_Monday"] = 1  : $ret["dayList"]["report_Monday"] = 0;
+		(isset($ret["dayList"]["report_Tuesday"])) ? $ret["dayList"]["report_Tuesday"] = 1  : $ret["dayList"]["report_Tuesday"] = 0;
+		(isset($ret["dayList"]["report_Wednesday"])) ? $ret["dayList"]["report_Wednesday"] = 1  : $ret["dayList"]["report_Wednesday"] = 0;
+		(isset($ret["dayList"]["report_Thursday"])) ? $ret["dayList"]["report_Thursday"] = 1  : $ret["dayList"]["report_Thursday"] = 0;
+		(isset($ret["dayList"]["report_Friday"])) ? $ret["dayList"]["report_Friday"] = 1  : $ret["dayList"]["report_Friday"] = 0;
+		(isset($ret["dayList"]["report_Saturday"])) ? $ret["dayList"]["report_Saturday"] = 1  : $ret["dayList"]["report_Saturday"] = 0;
+		(isset($ret["dayList"]["report_Sunday"])) ? $ret["dayList"]["report_Sunday"] = 1  : $ret["dayList"]["report_Sunday"] = 0;
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Monday"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_Monday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Tuesday"]."' WHERE cp_contact_id  IS NULL AND cp_key = 'report_Tuesday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value =' ".$ret["dayList"]["report_Wednesday"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_Wednesday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Thursday"]."' WHERE cp_contact_id  IS NULL AND cp_key = 'report_Thursday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Friday"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_Friday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Saturday"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_Saturday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["dayList"]["report_Sunday"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_Sunday'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_hour_start"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_hour_start'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_minute_start"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_minute_start'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_hour_end"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_hour_end'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		
+		$query = "UPDATE `contact_param` SET cp_value = '".$ret["report_minute_end"]."' WHERE cp_contact_id IS NULL AND cp_key = 'report_minute_end'";
+		$DBRESULT =& $pearDB->query($query);
+		if (PEAR::isError($DBRESULT))
+				print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+	}
 ?>
--- www/include/options/oreon/generalOpt/reporting/form_reporting.php
+++ www/include/options/oreon/generalOpt/reporting/form_reporting.php
@@ -1,38 +1,37 @@
 <?php
-/**
-Centreon is developped with GPL Licence 2.0 :
-http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
-Developped by : Julien Mathis - Romain Le Merlus
-
-The Software is provided to you AS IS and WITH ALL FAULTS.
-OREON makes no representation and gives no warranty whatsoever,
-whether express or implied, and without limitation, with regard to the quality,
-safety, contents, performance, merchantability, non-infringement or suitability for
-any particular or intended purpose of the Software found on the OREON web site.
-In no event will OREON be liable for any direct, indirect, punitive, special,
-incidental or consequential damages however they may arise and even if OREON has
-been previously advised of the possibility of such damages.
-
-For information : contact@oreon-project.org
-*/
-
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus - Cedrick Facon 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@centreon.com
+ */
+ 
 	if (!isset($oreon))
 		exit();
 
-	require_once($path."/reporting/DB-Func.php");
-	#
-	## Database retrieve information
-	#
-	$DBRESULT =& $pearDB->query("SELECT * FROM `contact_param` WHERE cp_contact_id is null");
-	# Set base value
+	require_once $path."/reporting/DB-Func.php";
+	
+	/*
+	 * Database retrieve information
+	 */
+	$DBRESULT =& $pearDB->query("SELECT * FROM `contact_param` WHERE cp_contact_id IS NULL");
 
 	$params = array();
 	$params["dayList"] = array();
 	while ($param =& $DBRESULT->fetchRow())
 		if ($param["cp_key"] != "report_hour_start" 
-		&& $param["cp_key"] != "report_hour_end" 
-		&& $param["cp_key"] != "report_minute_start" 
-		&& $param["cp_key"] != "report_minute_end") {
+			&& $param["cp_key"] != "report_hour_end" 
+			&& $param["cp_key"] != "report_minute_start" 
+			&& $param["cp_key"] != "report_minute_end") {
 			if ($param["cp_value"] == 1){
 				$params["dayList"][$param["cp_key"]] = true;
 			} else {
@@ -41,28 +40,23 @@ For information : contact@oreon-project.org
 		} else {
 			$params[$param["cp_key"]] = $param["cp_value"];
 		}
-	#
-	## Database retrieve information for differents elements list we need on the page
-	#
-	#
-	# End of "database-retrieved" information
-	##########################################################
-	##########################################################
-	# Var information to format the element
-	#
+
+	/*
+	 * Format template
+	 */
 	$attrsText 		= array("size"=>"2", "maxlength"=>"2");
 
-	#
-	## Form begin
-	#
+	/*
+	 * Form begin
+	 */
 	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
 	$form->addElement('header', 'title', _("Modify General Options"));
 
 	$form->setDefaults($param);
 
-    #
-	## Debug information
-	#
+	/*
+	 * Debug information
+	 */
 	$form->addElement('header', 'title', _("Reporting time period"));
 
 	$form->addElement('text', 'report_hour_start', _("Start Hour"), $attrsText);
@@ -71,19 +65,19 @@ For information : contact@oreon-project.org
 	$form->addElement('text', 'report_minute_end', _("End Minute"), $attrsText);
 
 	$Opt = array();
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Monday', 	'&nbsp;', _("&nbsp;Monday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Tuesday', 	'&nbsp;', _("&nbsp;Tuesday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Wednesday', '&nbsp;', _("&nbsp;Wednesday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Thursday', 	'&nbsp;', _("&nbsp;Thursday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Friday', 	'&nbsp;', _("&nbsp;Friday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Saturday', 	'&nbsp;', _("&nbsp;Saturday"));
- 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Sunday', 	'&nbsp;', _("&nbsp;Sunday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Monday', 	'&nbsp;', _("Monday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Tuesday', 	'&nbsp;', _("Tuesday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Wednesday', '&nbsp;', _("Wednesday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Thursday', 	'&nbsp;', _("Thursday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Friday', 	'&nbsp;', _("Friday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Saturday', 	'&nbsp;', _("Saturday"));
+ 	$Opt[] = &HTML_QuickForm::createElement('checkbox', 'report_Sunday', 	'&nbsp;', _("Sunday"));
 	$form->addGroup($Opt, 'dayList', _("Days") , '&nbsp;&nbsp;');
 
 
-	#
-	## Form Rules
-	#
+	/*
+	 * Defined Rules
+	 */	
 	function slash($elem = NULL)	{
 		if ($elem)
 			return rtrim($elem, "/")."/";
@@ -92,24 +86,26 @@ For information : contact@oreon-project.org
 	$form->addElement('hidden', 'gopt_id');
 	$redirect =& $form->addElement('hidden', 'o');
 	$redirect->setValue($o);
+	$form->setDefaults($params);
 
-	#
-	##End of form definition
-	#
-
-	# Smarty template Init
+	/*
+	 * Smarty template Init
+	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path.'reporting/', $tpl);
 
-	$form->setDefaults($params);
 
 	$subC =& $form->addElement('submit', 'submitC', _("Save"));
 	$DBRESULT =& $form->addElement('reset', 'reset', _("Reset"));
 
+	/*
+	 * Check Formulary
+	 */
     $valid = false;
 	if ($form->validate())	{
-
-		# Update in DB
+		/*
+		 * Update in DB
+		 */
 		updateReportingTimePeriodInDB();
 		$o = "reporting";
    		$valid = true;
@@ -117,9 +113,10 @@ For information : contact@oreon-project.org
 	}
 
 	$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=reporting'"));
-	#
-	##Apply a template definition
-	#
+	
+	/*
+	 * Apply a template definition
+	 */
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
 	$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
