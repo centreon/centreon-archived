ccd50a7a51e2ef6aa5b3c84fdc6b016c8deb68c3
Julien Mathis <jmathis@merethis.com>
2008-08-25 13:01:23 +0000

    Update About and Footer
    git-svn-id: http://svn.centreon.com/trunk/centreon@6333 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/footer.php
+++ www/footer.php
@@ -30,8 +30,7 @@
 			<table cellpadding='0' cellspacing='0' width='100%' border='0'>
 				<tr>
 					<td align='center' class='copyRight'>
-						<?php print _("Generated in "); $time_end = microtime_float(); $now = $time_end - $time_start; print round($now,3) . _(" seconds "); ?><br />
-						Copyright &copy; 1999-2008 Nagios - <a href="http://www.nagios.org/contact/">Ethan Galstad</a> | Copyright &copy; 2004-2008 <a href="mailto:infos@centreon.com">Centreon</a><br />All Rights Reserved<br />
+						<?php print _("Generated in "); $time_end = microtime_float(); $now = $time_end - $time_start; print round($now,3) . _(" seconds "); ?> | Centreon - Copyright &copy; 2005-2008 <a href="mailto:infos@centreon.com">Merethis</a> - All Rights Reserved<br />
 					</td>
 				</tr>
 			</table>
