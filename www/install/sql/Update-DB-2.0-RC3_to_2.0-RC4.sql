ed55864b8350fab59dcb936503efb3cb57a747bd
Sylvestre Ho <shotamchay@merethis.com>
2008-08-29 16:18:08 +0000

    fixes bug (monitoring>service by SG summary)
    git-svn-id: http://svn.centreon.com/trunk/centreon@6435 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-DB-2.0-RC3_to_2.0-RC4.sql
+++ www/install/sql/Update-DB-2.0-RC3_to_2.0-RC4.sql
@@ -12,5 +12,11 @@ INSERT INTO `contact_param` (`id`, `cp_key` , `cp_value` , `cp_contact_id`) VALU
 INSERT INTO `contact_param` (`id`, `cp_key` , `cp_value` , `cp_contact_id`) VALUES (NULL, 'report_Saturday', '1', NULL);
 INSERT INTO `contact_param` (`id`, `cp_key` , `cp_value` , `cp_contact_id`) VALUES (NULL, 'report_Sunday', '1', NULL);
 
+DELETE FROM `topology_JS` WHERE `id_page` = '20213' AND `PathName_js` = './include/common/javascript/ajaxMonitoring.js';
+DELETE FROM `topology_JS` WHERE `id_page` = '2021301' AND `PathName_js` = './include/common/javascript/ajaxMonitoring.js';
+DELETE FROM `topology_JS` WHERE `id_page` = '2021302' AND `PathName_js` = './include/common/javascript/ajaxMonitoring.js';
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES(NULL, 20213, NULL, './include/common/javascript/ajaxMonitoring.js', 'initM');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES(NULL, 2021301, NULL, './include/common/javascript/ajaxMonitoring.js', 'initM');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES(NULL, 2021302, NULL, './include/common/javascript/ajaxMonitoring.js', 'initM');
 
 UPDATE `informations` SET `value` = '2.0-RC4' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.0-RC3' LIMIT 1;
