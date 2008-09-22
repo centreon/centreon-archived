92d3865cac2c7db5b3a53c0afa7d2582290f11d7
Julien Mathis <jmathis@merethis.com>
2008-09-22 15:06:43 +0000

    remove sql error.
    git-svn-id: http://svn.centreon.com/trunk/centreon@6600 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/tacticalOverview/tacticalOverview.php
+++ www/include/home/tacticalOverview/tacticalOverview.php
@@ -147,7 +147,6 @@
 				$rq1 = 	" SELECT count(DISTINCT ".$ndo_base_prefix."objects.name1), ".$ndo_base_prefix."hoststatus.current_state" .
 						" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects " .
 						" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id AND ".$ndo_base_prefix."objects.is_active = 1 " .
-						" AND ".$ndo_base_prefix."objects.name1 = centreon_acl.host_name " .
 						" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 						" ORDER by ".$ndo_base_prefix."hoststatus.current_state";						
 			$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
