d727631ec1954709631dbdfdad86303ba8921fa8
Julien Mathis <jmathis@merethis.com>
2008-09-27 17:14:36 +0000

    add class for GMT functionality
    git-svn-id: http://svn.centreon.com/trunk/centreon@6638 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/class/centreonGMT.class.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@centreon.com
+ */
+ 
+class CentreonGMT{
+
+	var $listGTM;
+	var $myGMT;
+	
+	function CentreonGMT(){
+		/*
+		 * Define Table of GMT line
+		 */
+		$this->listGTM = array();
+		
+		$this->listGTM['-12'] = -12;
+		$this->listGTM['-11'] = -11;
+		$this->listGTM['-10'] = -10;
+		$this->listGTM['-9'] = -9;
+		$this->listGTM['-8'] = -8;
+		$this->listGTM['-7'] = -7;
+		$this->listGTM['-6'] = -6;
+		$this->listGTM['-5'] = -5;
+		$this->listGTM['-4'] = -4;
+		$this->listGTM['-3'] = -3;
+		$this->listGTM['-2'] = -2;
+		$this->listGTM['-1'] = -1;
+		$this->listGTM['0'] = 0;
+		$this->listGTM['1'] = 1;
+		$this->listGTM['2'] = 2;
+		$this->listGTM['3'] = 3;
+		$this->listGTM['4'] = 4;
+		$this->listGTM['5'] = 5;
+		$this->listGTM['6'] = 6;
+		$this->listGTM['7'] = 7;
+		$this->listGTM['8'] = 8;
+		$this->listGTM['9'] = 9;
+		$this->listGTM['10'] = 10;
+		$this->listGTM['11'] = 11;
+		$this->listGTM['12'] = 12;
+	}
+	
+	function setMyGMT($value){
+		if (!isset($value))
+			$this->myGMT = $value;	
+	}
+	
+	function getGMTList() {
+		return $this->listGTM;
+	}
+	
+	function getDate($format, $date, $gmt = NULL) {
+		/*
+		 * Specify special GMT
+		 */
+		if (!isset($gmt))
+			$gmt = $this->myGMT;
+			
+		if (isset($date) && isset($gmt)) {
+			$date += $gmt * 60 * 60;
+			return date($format, $date);
+		} else {
+			return "";	
+		}
+	}
+	
+	function getDelaySecondsForRRD($gmt) {
+		$str = "";
+		if ($gmt) {
+			if ($gmt > 0)
+				$str .= "+";
+		} else {
+			return "";	
+		}
+	}
+	
+	function getMyGMTFromSession($sid = NULL){
+		global $pearDB;
+		
+		if (!isset($sid))
+			return 0;
+		
+		$DBRESULT =& $pearDB->query("SELECT `contact_location` FROM `contact`, `session` " .
+									"WHERE `session`.`user_id` = `contact`.`contact_id` " .
+									"AND `session_id` = '$sid' LIMIT 1");
+		if (PEAR::isError($DBRESULT))
+			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		$info =& $DBRESULT->fetchRow();
+		$DBRESULT->free();
+		$this->myGMT = $info["contact_location"];
+	}
+	
+}
