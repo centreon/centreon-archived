fe3915cae160866d1b4fa2560a4eda145deb53fb
Julien Mathis <jmathis@merethis.com>
2008-10-02 08:18:27 +0000

    Set location by default
    git-svn-id: http://svn.centreon.com/trunk/centreon@6674 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-DB-2.0_to_2.1.sql
+++ www/install/sql/Update-DB-2.0_to_2.1.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `contact` ADD `contact_location` INT NULL ;
 ALTER TABLE `host` ADD `host_location` INT NULL AFTER `host_snmp_version` ;
+UPDATE `contact` SET `contact_location` = '0';
+UPDATE `host` SET `host_location` = '0';
