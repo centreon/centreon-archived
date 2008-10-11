700e115a7a92a428c73e5f7da48373c11ff434ca
Julien Mathis <jmathis@merethis.com>
2008-10-11 16:42:20 +0000

    check if host is linked to a poller.
    git-svn-id: http://svn.centreon.com/trunk/centreon@6829 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -183,7 +183,7 @@
 						"RowMenu_link"=>"?p=".$p."&o=c&host_id=".$host['host_id'],
 						"RowMenu_desc"=>$host["host_alias"],
 						"RowMenu_address"=>htmlentities($host["host_address"]),
-						"RowMenu_poller"=>htmlentities($tab_relation[$host["host_id"]]),
+						"RowMenu_poller"=>htmlentities(isset($tab_relation[$host["host_id"]]) ? $tab_relation[$host["host_id"]] : ""),
 						"RowMenu_parent"=>$tplStr,
 						"RowMenu_status"=>$host["host_activate"] ? _("Enabled") : _("Disabled"),
 						"RowMenu_options"=>$moptions);
