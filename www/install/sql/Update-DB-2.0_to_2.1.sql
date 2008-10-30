780732e22c104f2c5ca426fdd0c16b151adf6589
Julien Mathis <jmathis@merethis.com>
2008-10-30 11:19:28 +0000

    update ugrade script for centreon 2.0 to 2.1
    git-svn-id: http://svn.centreon.com/trunk/centreon@6956 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/Update-DB-2.0_to_2.1.sql
+++ www/install/sql/Update-DB-2.0_to_2.1.sql
@@ -1,14 +1,11 @@
+
 ALTER TABLE `contact` ADD `contact_location` INT NULL ;
 ALTER TABLE `host` ADD `host_location` INT NULL AFTER `host_snmp_version` ;
+
 UPDATE `contact` SET `contact_location` = '0';
 UPDATE `host` SET `host_location` = '0';
+
 INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Actions Access', './img/icones/16x16/wrench.gif', 502, 50204, 25, 1, './include/options/accessLists/actionsACL/actionsConfig.php', NULL, '0', '0', '1', NULL, NULL, NULL);
-
--- --------------------------------------------------------
-
--- 
--- Structure de la table `acl_actions`
--- 
 
 CREATE TABLE `acl_actions` (
   `acl_action_id` int(11) NOT NULL auto_increment,
@@ -18,24 +15,12 @@ CREATE TABLE `acl_actions` (
   PRIMARY KEY  (`acl_action_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- --------------------------------------------------------
-
--- 
--- Structure de la table `acl_actions_rules`
--- 
-
 CREATE TABLE `acl_actions_rules` (
   `aar_id` int(11) NOT NULL auto_increment,
   `acl_action_rule_id` int(11) default NULL,
   `acl_action_name` varchar(255) default NULL,
   PRIMARY KEY  (`aar_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
--- 
--- Structure de la table `acl_group_actions_relations`
--- 
 
 CREATE TABLE `acl_group_actions_relations` (
   `agar_id` int(11) NOT NULL auto_increment,
