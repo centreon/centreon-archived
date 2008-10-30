82e9203d6b1df75cf6eb02eff133668c54a479dd
Julien Mathis <jmathis@merethis.com>
2008-10-30 10:24:39 +0000

    disable centreon.pm
    git-svn-id: http://svn.centreon.com/trunk/centreon@6947 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configGenerate/centreon_pm.php
+++ www/include/configuration/configGenerate/centreon_pm.php
@@ -14,60 +14,60 @@
  * 
  * For information : contact@centreon.com
  */
+ 
 	if (!isset($oreon))
 		exit();
-
-	if (!is_dir($nagiosCFGPath.$tab['id']."/")) {
-		mkdir($nagiosCFGPath.$tab['id']."/");
-	}
 	
 	if (!isLocalInstance($tab['id']))
 		$file = $oreon->optGen["nagios_path_plugins"]."centreon.conf" ;
 	else
 		$file = $nagiosCFGPath.$tab['id']."/centreon.conf" ;
 	
-	$handle = fopen($file, 'w');
-	$ini = readINIfile ($file, ";");
-	// We modify [GLOBAL] section
-	$ini["GLOBAL"]["DIR_OREON"] = $oreon->optGen["oreon_path"];
-	$ini["GLOBAL"]["NAGIOS_LIBEXEC"] = $oreon->optGen["nagios_path_plugins"];
-	$ini["GLOBAL"]["NAGIOS_ETC"] = $oreon->Nagioscfg["cfg_dir"];
+	if (!file_exists($file)) {
 	
-	# other section
-	$ini["NT"]["CPU"] = ".1.3.6.1.2.1.25.3.3.1.2";
-	$ini["NT"]["HD_USED"] = ".1.3.6.1.2.1.25.2.3.1.6";
-	$ini["NT"]["HD_NAME"] = ".1.3.6.1.2.1.25.2.3.1.3";
-	
-	$ini["CISCO"]["NB_CONNECT"] = ".1.3.6.1.4.1.9.9.147.1.2.2.2.1.5.40.6";
-	
-	$ini["UNIX"]["CPU_USER"] = ".1.3.6.1.4.1.2021.11.50.0";
-	$ini["UNIX"]["CPU_SYSTEM"] = ".1.3.6.1.4.1.2021.11.52.0";
-	$ini["UNIX"]["CPU_LOAD_1M"] = ".1.3.6.1.4.1.2021.10.1.3.1";
-	$ini["UNIX"]["CPU_LOAD_5M"] = ".1.3.6.1.4.1.2021.10.1.3.2";
-	$ini["UNIX"]["CPU_LOAD_15M"] = ".1.3.6.1.4.1.2021.10.1.3.3";
-	
-	$ini["DELL"]["TEMP"] = ".1.3.6.1.4.1.674.10892.1.700.20.1.6.1";
-	
-	$ini["ALTEON"]["VIRT"] = "1.3.6.1.4.1.1872.2.1.8.2.7.1.3.1";
-	$ini["ALTEON"]["FRONT"] = "1.3.6.1.4.1.1872.2.1.8.2.5.1.3.1";
-	
-	$ini["MIB2"]["SW_RUNNAME"] = ".1.3.6.1.2.1.25.4.2.1.2";
-	$ini["MIB2"]["SW_RUNINDEX"] = ".1.3.6.1.2.1.25.4.2.1.1";
-	$ini["MIB2"]["SW_RUNSTATUS"] = ".1.3.6.1.2.1.25.4.2.1.7";
-	$ini["MIB2"]["HR_STORAGE_DESCR"] = ".1.3.6.1.2.1.25.2.3.1.3";
-	$ini["MIB2"]["HR_STORAGE_ALLOCATION_UNITS"] = ".1.3.6.1.2.1.25.2.3.1.4";
-	$ini["MIB2"]["HR_STORAGE_SIZE"] = ".1.3.6.1.2.1.25.2.3.1.5";
-	$ini["MIB2"]["HR_STORAGE_USED"] = ".1.3.6.1.2.1.25.2.3.1.6";
-	$ini["MIB2"]["OBJECTID"] = ".1.3.6.1.2.1.1.1.0";
-	$ini["MIB2"]["UPTIME_WINDOWS"] = ".1.3.6.1.2.1.1.3.0";
-	$ini["MIB2"]["UPTIME_OTHER"] = ".1.3.6.1.2.1.25.1.1.0";
-	$ini["MIB2"]["IF_IN_OCTET"] = ".1.3.6.1.2.1.2.2.1.10";
-	$ini["MIB2"]["IF_OUT_OCTET"] = ".1.3.6.1.2.1.2.2.1.16";
-	$ini["MIB2"]["IF_SPEED"] = ".1.3.6.1.2.1.2.2.1.5";
-	$ini["MIB2"]["IF_DESC"] = ".1.3.6.1.2.1.2.2.1.2";
-	$ini["MIB2"]["IF_OPERSTATUS"] = ".1.3.6.1.2.1.2.2.1.8";
-	
-	// We write conf file
-	writeINIfile($file,$ini, "", "");
-	fclose($handle);
+		$handle = fopen($file, 'w');
+		$ini = readINIfile ($file, ";");
+		// We modify [GLOBAL] section
+		$ini["GLOBAL"]["DIR_OREON"] = $oreon->optGen["oreon_path"];
+		$ini["GLOBAL"]["NAGIOS_LIBEXEC"] = $oreon->optGen["nagios_path_plugins"];
+		$ini["GLOBAL"]["NAGIOS_ETC"] = $oreon->Nagioscfg["cfg_dir"];
+		
+		# other section
+		$ini["NT"]["CPU"] = ".1.3.6.1.2.1.25.3.3.1.2";
+		$ini["NT"]["HD_USED"] = ".1.3.6.1.2.1.25.2.3.1.6";
+		$ini["NT"]["HD_NAME"] = ".1.3.6.1.2.1.25.2.3.1.3";
+		
+		$ini["CISCO"]["NB_CONNECT"] = ".1.3.6.1.4.1.9.9.147.1.2.2.2.1.5.40.6";
+		
+		$ini["UNIX"]["CPU_USER"] = ".1.3.6.1.4.1.2021.11.50.0";
+		$ini["UNIX"]["CPU_SYSTEM"] = ".1.3.6.1.4.1.2021.11.52.0";
+		$ini["UNIX"]["CPU_LOAD_1M"] = ".1.3.6.1.4.1.2021.10.1.3.1";
+		$ini["UNIX"]["CPU_LOAD_5M"] = ".1.3.6.1.4.1.2021.10.1.3.2";
+		$ini["UNIX"]["CPU_LOAD_15M"] = ".1.3.6.1.4.1.2021.10.1.3.3";
+		
+		$ini["DELL"]["TEMP"] = ".1.3.6.1.4.1.674.10892.1.700.20.1.6.1";
+		
+		$ini["ALTEON"]["VIRT"] = "1.3.6.1.4.1.1872.2.1.8.2.7.1.3.1";
+		$ini["ALTEON"]["FRONT"] = "1.3.6.1.4.1.1872.2.1.8.2.5.1.3.1";
+		
+		$ini["MIB2"]["SW_RUNNAME"] = ".1.3.6.1.2.1.25.4.2.1.2";
+		$ini["MIB2"]["SW_RUNINDEX"] = ".1.3.6.1.2.1.25.4.2.1.1";
+		$ini["MIB2"]["SW_RUNSTATUS"] = ".1.3.6.1.2.1.25.4.2.1.7";
+		$ini["MIB2"]["HR_STORAGE_DESCR"] = ".1.3.6.1.2.1.25.2.3.1.3";
+		$ini["MIB2"]["HR_STORAGE_ALLOCATION_UNITS"] = ".1.3.6.1.2.1.25.2.3.1.4";
+		$ini["MIB2"]["HR_STORAGE_SIZE"] = ".1.3.6.1.2.1.25.2.3.1.5";
+		$ini["MIB2"]["HR_STORAGE_USED"] = ".1.3.6.1.2.1.25.2.3.1.6";
+		$ini["MIB2"]["OBJECTID"] = ".1.3.6.1.2.1.1.1.0";
+		$ini["MIB2"]["UPTIME_WINDOWS"] = ".1.3.6.1.2.1.1.3.0";
+		$ini["MIB2"]["UPTIME_OTHER"] = ".1.3.6.1.2.1.25.1.1.0";
+		$ini["MIB2"]["IF_IN_OCTET"] = ".1.3.6.1.2.1.2.2.1.10";
+		$ini["MIB2"]["IF_OUT_OCTET"] = ".1.3.6.1.2.1.2.2.1.16";
+		$ini["MIB2"]["IF_SPEED"] = ".1.3.6.1.2.1.2.2.1.5";
+		$ini["MIB2"]["IF_DESC"] = ".1.3.6.1.2.1.2.2.1.2";
+		$ini["MIB2"]["IF_OPERSTATUS"] = ".1.3.6.1.2.1.2.2.1.8";
+		
+		// We write conf file
+		writeINIfile($file,$ini, "", "");
+		fclose($handle);
+	}
 ?>
