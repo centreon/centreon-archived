f8235ab5a0841e3c7e595120cc4099d2c1fa2c00
Julien Mathis <jmathis@merethis.com>
2008-10-30 17:12:59 +0000

    change msg type in log table : add 2 new types
    git-svn-id: http://svn.centreon.com/trunk/centreon@6985 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/createTablesCentstorage.sql
+++ www/install/createTablesCentstorage.sql
@@ -162,7 +162,7 @@ CREATE TABLE IF NOT EXISTS `log` (
   `notification_contact` varchar(255) default NULL,
   `type` varchar(255) default NULL,
   `retry` int(255) NOT NULL,
-  `msg_type` enum('0','1','2','3','4','5','6','7','8','9') NOT NULL,
+  `msg_type` enum('0','1','2','3','4','5','6','7','8','9','10','11') NOT NULL,
   `instance` int(11) NOT NULL default '1',
   PRIMARY KEY  (`log_id`),
   KEY `host_name` (`host_name`(64)),
