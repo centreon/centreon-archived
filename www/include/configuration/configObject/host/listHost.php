bb04e199047da7ec9702bb6d3b5c4cea841490f5
Julien Mathis <jmathis@merethis.com>
2008-11-29 18:44:33 +0000

    add search field for hosts config pages
    git-svn-id: http://svn.centreon.com/trunk/centreon@7171 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -20,12 +20,18 @@
 
 	include("./include/common/autoNumLimit.php");
 
-	/*
-	 * start quickSearch form
-	 */
-	$advanced_search = 0;
-	include_once("./include/common/quickSearch.php");
-	
+	if (isset($_POST["searchH"]))
+		$search = $_POST["searchH"];
+	else if (isset($oreon->historySearch[$url]))
+		$search = $oreon->historySearch[$url];
+	else
+		$search = NULL;
+
+	if (isset($_POST["poller"]))
+		$poller = $_POST["poller"];
+	else
+		$poller = 0;
+
 	/*
 	 * Access list activation
 	 */
@@ -46,10 +52,10 @@
 	 * Launch Request
 	 */	
 	$DBRESULT =& $pearDB->query("SELECT COUNT(*) FROM host h WHERE $SearchTool $LCATool host_register = '1'");
-	
 	if (PEAR::isError($DBRESULT))
 		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";	
 	$tmp =& $DBRESULT->fetchRow();
+	$DBRESULT->free();
 	$rows = $tmp["COUNT(*)"];
 	
 	include("./include/common/checkPagination.php");
@@ -70,12 +76,7 @@
 	$tpl->assign("headerMenu_desc", _("Description"));
 	$tpl->assign("headerMenu_address", _("IP Address / DNS"));
 	$tpl->assign("headerMenu_poller", _("Poller"));
-	
-	if ($oreon->user->get_version() < 3)
-		$tpl->assign("headerMenu_parent", _("Parent Template"));
-	else
-		$tpl->assign("headerMenu_parent", _("Templates"));
-	
+	$tpl->assign("headerMenu_parent", _("Templates"));
 	$tpl->assign("headerMenu_status", _("Status"));
 	$tpl->assign("headerMenu_options", _("Options"));
 	
@@ -90,22 +91,20 @@
 	while ($relation =& $DBRESULT->fetchRow()) {
 		$nagios_server[$relation["id"]] = $relation["name"];
 	}
+	$DBRESULT->free();
 	unset($relation);
 	
 	$tab_relation = array();
+	$tab_relation_id = array();
 	$DBRESULT =& $pearDB->query("SELECT nhr.host_host_id, nhr.nagios_server_id FROM ns_host_relation nhr"); 
 	if (PEAR::isError($DBRESULT))
 		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";	
 	while ($relation =& $DBRESULT->fetchRow()) {
 		$tab_relation[$relation["host_host_id"]] = $nagios_server[$relation["nagios_server_id"]];
+		$tab_relation_id[$relation["host_host_id"]] = $relation["nagios_server_id"];
 	}
+	$DBRESULT->free();
 	
-	$DBRESULT =& $pearDB->query("SELECT host_id, host_name, host_alias, host_address, host_activate, host_template_model_htm_id FROM host h WHERE $SearchTool $LCATool host_register = '1' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit); 
-	if (PEAR::isError($DBRESULT))
-		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";	
-
-	$search = tidySearchKey($search, $advanced_search);
-
 	/*
 	 * Init Formulary
 	 */
@@ -121,73 +120,79 @@
 	/*
 	 * Fill a tab with a mutlidimensionnal Array we put in $tpl
 	 */
+	
+	/*
+	 * Select hosts 
+	 */
+	$DBRESULT =& $pearDB->query("SELECT host_id, host_name, host_alias, host_address, host_activate, host_template_model_htm_id FROM host h WHERE $SearchTool $LCATool host_register = '1' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit); 
+	if (PEAR::isError($DBRESULT))
+		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";	
+
+	$search = tidySearchKey($search, $advanced_search);
 	 
 	$elemArr = array();
 	$search = str_replace('\_', "_", $search);
 	for ($i = 0; $host =& $DBRESULT->fetchRow(); $i++) {
-
-		$selectedElements =& $form->addElement('checkbox', "select[".$host['host_id']."]");	
-		
-		if ($host["host_activate"])
-			$moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
-		else
-			$moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
-		
-		$moptions .= "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$host['host_id']."]'></input>";
-		
-		if (!$host["host_name"])
-			$host["host_name"] = getMyHostField($host['host_id'], "host_name");
-		
-		/*
-		 * TPL List
-		 */
-		 
-		$tplArr = array();
-		$tplStr = "";
-				
-		/*
-		 * Create Template topology 
-		 */		
-		if ($oreon->user->get_version() < 3){
-			$tplArr = getMyHostTemplateModels($host["host_template_model_htm_id"]);
-			if (count($tplArr))
-			{ 
-				foreach($tplArr as $key =>$value)
-					$tplStr .= "&nbsp;->&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
-			}
-		}
-		else {
-			$tplArr = getMyHostMultipleTemplateModels($host['host_id']);
-			if (count($tplArr))
-			{ 
-				$firstTpl = 1;
-				foreach($tplArr as $key =>$value) {
-					if ($firstTpl) {
-						$tplStr .= "<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
-						$firstTpl = 0;
+		if (!isset($poller) || $poller == 0 || ($poller != 0 && $poller == $tab_relation_id[$host["host_id"]])) {
+			$selectedElements =& $form->addElement('checkbox', "select[".$host['host_id']."]");	
+			
+			if ($host["host_activate"])
+				$moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
+			else
+				$moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
+			
+			$moptions .= "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$host['host_id']."]'></input>";
+			
+			if (!$host["host_name"])
+				$host["host_name"] = getMyHostField($host['host_id'], "host_name");
+			
+			/*
+			 * TPL List
+			 */
+			 
+			$tplArr = array();
+			$tplStr = "";
+					
+			/*
+			 * Create Template topology 
+			 */		
+			if ($oreon->user->get_version() < 3){
+				$tplArr = getMyHostTemplateModels($host["host_template_model_htm_id"]);
+				if (count($tplArr))	{ 
+					foreach($tplArr as $key =>$value)
+						$tplStr .= "&nbsp;->&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
+				}
+			} else {
+				$tplArr = getMyHostMultipleTemplateModels($host['host_id']);
+				if (count($tplArr)) { 
+					$firstTpl = 1;
+					foreach($tplArr as $key =>$value) {
+						if ($firstTpl) {
+							$tplStr .= "<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
+							$firstTpl = 0;
+						} else
+							$tplStr .= "&nbsp;|&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
 					}
-					else
-						$tplStr .= "&nbsp;|&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
 				}
 			}
+			
+			
+			/*
+			 * Create Array Data for template list
+			 */
+			
+			$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+							"RowMenu_select"=>$selectedElements->toHtml(),
+							"RowMenu_name"=>$host["host_name"],
+							"RowMenu_link"=>"?p=".$p."&o=c&host_id=".$host['host_id'],
+							"RowMenu_desc"=>$host["host_alias"],
+							"RowMenu_address"=>htmlentities($host["host_address"]),
+							"RowMenu_poller"=>htmlentities(isset($tab_relation[$host["host_id"]]) ? $tab_relation[$host["host_id"]] : ""),
+							"RowMenu_parent"=>$tplStr,
+							"RowMenu_status"=>$host["host_activate"] ? _("Enabled") : _("Disabled"),
+							"RowMenu_options"=>$moptions);
+			$style != "two" ? $style = "two" : $style = "one";
 		}
-		
-		
-		/*
-		 * Create Array Data for template list
-		 */
-		
-		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
-						"RowMenu_select"=>$selectedElements->toHtml(),
-						"RowMenu_name"=>$host["host_name"],
-						"RowMenu_link"=>"?p=".$p."&o=c&host_id=".$host['host_id'],
-						"RowMenu_desc"=>$host["host_alias"],
-						"RowMenu_address"=>htmlentities($host["host_address"]),
-						"RowMenu_poller"=>htmlentities(isset($tab_relation[$host["host_id"]]) ? $tab_relation[$host["host_id"]] : ""),
-						"RowMenu_parent"=>$tplStr,
-						"RowMenu_status"=>$host["host_activate"] ? _("Enabled") : _("Disabled"),
-						"RowMenu_options"=>$moptions);
-		$style != "two" ? $style = "two" : $style = "one";
 	}
 	
 	/*
@@ -273,6 +278,19 @@
 
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$form->accept($renderer);
+
+	$tpl->assign("search", $search);
+	
+	/*
+	 * create Poller Select
+	 */
+	
+	$options = "<option value='0'>All Pollers</option>";
+	foreach ($nagios_server as $key => $name)
+		$options .= "<option value='$key' ".(($poller == $key) ? 'selected' : "").">$name</option>"; 
+	 
+	$tpl->assign("poller", $options);
+	unset($options);
 
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listHost.ihtml");
