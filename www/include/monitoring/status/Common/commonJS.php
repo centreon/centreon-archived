e429b9a2587e9f78834e030e683333c8ff688083
Julien Mathis <jmathis@merethis.com>
2008-12-22 11:00:20 +0000

    merge 2.0 => 2.1 enhancement
    git-svn-id: http://svn.centreon.com/trunk/centreon@7281 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/Common/commonJS.php
+++ www/include/monitoring/status/Common/commonJS.php
@@ -1,20 +1,41 @@
 <?php
 /*
- * Centreon is developped with GPL Licence 2.0 :
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
- * Developped by : Julien Mathis - Romain Le Merlus 
+ * Copyright 2005-2009 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
  * 
- * The Software is provided to you AS IS and WITH ALL FAULTS.
- * Centreon makes no representation and gives no warranty whatsoever,
- * whether express or implied, and without limitation, with regard to the quality,
- * any particular or intended purpose of the Software found on the Centreon web site.
- * In no event will Centreon be liable for any direct, indirect, punitive, special,
- * incidental or consequential damages however they may arise and even if Centreon has
- * been previously advised of the possibility of such damages.
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation ; either version 2 of the License.
  * 
- * For information : contact@centreon.com
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ * 
+ * Linking this program statically or dynamically with other modules is making a 
+ * combined work based on this program. Thus, the terms and conditions of the GNU 
+ * General Public License cover the whole combination.
+ * 
+ * As a special exception, the copyright holders of this program give MERETHIS 
+ * permission to link this program with independent modules to produce an executable, 
+ * regardless of the license terms of these independent modules, and to copy and 
+ * distribute the resulting executable under terms of MERETHIS choice, provided that 
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions 
+ * of the license of that module. An independent module is a module which is not 
+ * derived from this program. If you modify this program, you may extend this 
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ * 
+ * For more information : contact@centreon.com
+ * 
+ * SVN : $URL
+ * SVN : $Id: makeJS_Common.php 7202 2008-12-03 21:36:14Z jmathis $
+ * 
  */
- 
+  
 	if (!isset($oreon))
 		exit();
 	
@@ -37,24 +58,23 @@
 ?>
 function getXhrC(){
 	if (window.XMLHttpRequest) // Firefox et autres
-		var xhrC = new XMLHttpRequest();
+	   var xhrC = new XMLHttpRequest();
 	else if (window.ActiveXObject){ // Internet Explorer
-		try {
-	   		var xhrC = new ActiveXObject("Msxml2.XMLHTTP");
-		} catch (e) {
-			var xhrC = new ActiveXObject("Microsoft.XMLHTTP");
-		}
+	   try {
+                var xhrC = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                var xhrC = new ActiveXObject("Microsoft.XMLHTTP");
+            }
 	} else { // XMLHttpRequest non support2 par le navigateur
-		alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest...");
-		var xhrC = false;
+	   alert("Votre navigateur ne supporte pas les objets XMLHTTPRequest...");
+	   var xhrC = false;
 	}
 	return xhrC;
 }
 
 function addORdelTab(_name){
 	var d = document.getElementsByName('next_check_case');
-	if (d[0].checked == true)
-	{
+	if (d[0].checked == true) {
 		_nc = 1;
 	} else {
 		_nc = 0;
@@ -64,7 +84,7 @@ function addORdelTab(_name){
 
 function advanced_options(id){
 	var d = document.getElementById(id);
-	if (d){
+	if (d) {
 		if (d.style.display == 'block') {
 			d.style.display='none';
 		} else {
@@ -87,7 +107,6 @@ function construct_selecteList_ndo_instance(id){
 		_select.appendChild(k);
 
 <?php
-
 	while ($nagios_server =& $DBRESULT->fetchRow())	{
 	 	$isntance_id = get_ndo_instance_id($nagios_server["name"]);
 ?>
@@ -100,22 +119,22 @@ function construct_selecteList_ndo_instance(id){
 <?php }	?>
 		_select.selectedIndex = _default_instance;
 		_select_instance.appendChild(_select);
+	
 	}
+	
 }
 
-function viewDebugInfo(_str){
+function viewDebugInfo(_str) {
 	if (_debug)	{
 		_nb = _nb + 1;
-		var mytable = document.getElementById("debugtable")
-		//add new row to end of table
-		var newrow = mytable.insertRow(0) 
-		//insert new cell to row
-		var newcell = newrow.insertCell(0) 
+		var mytable=document.getElementById("debugtable")
+		var newrow=mytable.insertRow(0) //add new row to end of table
+		var newcell=newrow.insertCell(0) //insert new cell to row
 		newcell.innerHTML='<td>line:' + _nb + ' ' + _str + '</td>';
 	}
 }
 
-function change_page(page_number){
+function change_page(page_number) {
 	viewDebugInfo('change page');
 	_num = page_number;
 	monitoring_refresh();
@@ -123,14 +142,14 @@ function change_page(page_number){
 	set_page(page_number);
 }
 
-function change_type_order(_type){
+function change_type_order(_type) {
 	if (_sort_type != _type){
 		_sort_type = _type;
 		monitoring_refresh();
 	}
 }
 
-function change_order(_odr){
+function change_order(_odr) {
 	if (_order == 'ASC'){
 		_order = 'DESC';
 	} else
@@ -139,12 +158,12 @@ function change_order(_odr){
 }
 
 
-function change_limit(l){
+function change_limit(l) {
 	_limit= l;
 	pagination_changed();
 	monitoring_refresh();
 	var _sel1 = document.getElementById('l1');
-	for (i = 0 ; _sel1[i] && _sel1[i].value != l ;i++)
+	for(i=0 ; _sel1[i] && _sel1[i].value != l ; i++)
 		;
 	_sel1.selectedIndex = i;
 	set_limit(l);
@@ -152,7 +171,7 @@ function change_limit(l){
 
 var _numRows = 0;
 
-function getVar (nomVariable){
+function getVar (nomVariable) {
 	var infos = location.href.substring(location.href.indexOf("?")+1, location.href.length)+"&";
 	if (infos.indexOf("#")!=-1)
 	infos = infos.substring(0,infos.indexOf("#"))+"&";
@@ -195,17 +214,42 @@ function mk_imgOrder(_src, _alt)	{
 }
 
 function mk_pagination(resXML){
-	
 	viewDebugInfo('mk pagination');
 
 	var flag = 0;
-	var infos = resXML.getElementsByTagName("i");
+	var infos = resXML.getElementsByTagName('i');
 
-	if (infos[0]){
-		var _nr = infos[0].getElementsByTagName("numrows")[0].firstChild.nodeValue;
-		var _nl = infos[0].getElementsByTagName("limit")[0].firstChild.nodeValue;
-		var _nn = infos[0].getElementsByTagName("num")[0].firstChild.nodeValue;
+	var _nr = infos[0].getElementsByTagName('numrows')[0].firstChild.data;
+	var _nl = infos[0].getElementsByTagName("limit")[0].firstChild.data;
+	var _nn = infos[0].getElementsByTagName("num")[0].firstChild.data;
 
+	if (_numRows != _nr){
+		_numRows = _nr;
+		flag = 1;
+	}
+	if (_num != _nn){
+		_num = _nn;
+		flag = 1;
+	}
+	if (_limit != _nl){
+		_limit = _nl;
+		flag = 1;
+	}
+	if (flag == 1){
+		pagination_changed();
+	}
+}
+
+function mk_paginationFF(resXML){
+	viewDebugInfo('mk pagination');
+
+	var flag = 0;
+	var infos = resXML.getElementsByTagName('i');
+	if (infos[0]) {
+		var _nr = infos[0].getElementsByTagName('numrows')[0].firstChild.data;
+		var _nl = infos[0].getElementsByTagName("limit")[0].firstChild.data;
+		var _nn = infos[0].getElementsByTagName("num")[0].firstChild.data;
+	
 		if (_numRows != _nr){
 			_numRows = _nr;
 			flag = 1;
@@ -225,7 +269,7 @@ function mk_pagination(resXML){
 }
 
 function pagination_changed(){
-	viewDebugInfo('begin : pagination_changed');
+	viewDebugInfo('pagination_changed');
 	
 	// compute Max Page
 	var page_max = 0;
@@ -293,42 +337,41 @@ function pagination_changed(){
 	 * Page Number
 	 */
 
-	for ($i = 1; $i <= 2; $i++) { ?>
-		var istart = 0;
-		for (i = 5, istart = _num; istart && i > 0 && istart > 0; i--)
-			istart--;
-		
-		for (i2 = 0, iend = _num; ( iend <  (_numRows / _limit -1)) && ( i2 < (5 + i)); i2++)
-			iend++;
-		
-		for (i = istart; i <= iend && page_max > 1; i++){
-			var span_space = document.createElement("span");
-			span_space.innerHTML = '&nbsp;';
-			_pagination<?php echo $i; ?>.appendChild(span_space);
+for ($i = 1; $i <= 2; $i++) { ?>
+	var istart = 0;
+	for (i = 5, istart = _num; istart && i > 0 && istart > 0; i--)
+		istart--;
 	
-			var _linkaction_num = document.createElement("a");
-	  		_linkaction_num.href = '#' ;
-	  		_linkaction_num.indice = i;
-	  		_linkaction_num.onclick=function(){change_page(this.indice)};
-			_linkaction_num.innerHTML = parseInt(i + 1);
-			_linkaction_num.className = "otherPageNumber";
-			
-			if (i == _num)
-				_linkaction_num.className = "currentPageNumber";
-			_pagination<?php echo $i; ?>.appendChild(_linkaction_num);
+	for (i2 = 0, iend = _num; ( iend <  (_numRows / _limit -1)) && ( i2 < (5 + i)); i2++)
+		iend++;
 	
-			var span_space = document.createElement("span");
-			span_space.innerHTML = '&nbsp;';
-			_pagination<?php echo $i; ?>.appendChild(span_space);
-		}
-	
-		if (_num < page_max - 1){
-			_pagination<?php echo $i; ?>.appendChild(_linkaction_right<?php echo $i; ?>);
-			_pagination<?php echo $i; ?>.appendChild(_linkaction_last<?php echo $i; ?>);
-		}
+	for (i = istart; i <= iend && page_max > 1; i++){
+		var span_space = document.createElement("span");
+		span_space.innerHTML = '&nbsp;';
+		_pagination<?php echo $i; ?>.appendChild(span_space);
+
+		var _linkaction_num = document.createElement("a");
+  		_linkaction_num.href = '#' ;
+  		_linkaction_num.indice = i;
+  		_linkaction_num.onclick=function(){change_page(this.indice)};
+		_linkaction_num.innerHTML = parseInt(i + 1);
+		_linkaction_num.className = "otherPageNumber";
 		
-	<?php 
-	} ?>
+		if (i == _num)
+			_linkaction_num.className = "currentPageNumber";
+		_pagination<?php echo $i; ?>.appendChild(_linkaction_num);
+
+		var span_space = document.createElement("span");
+		span_space.innerHTML = '&nbsp;';
+		_pagination<?php echo $i; ?>.appendChild(span_space);
+	}
+
+	if (_num < page_max - 1){
+		_pagination<?php echo $i; ?>.appendChild(_linkaction_right<?php echo $i; ?>);
+		_pagination<?php echo $i; ?>.appendChild(_linkaction_last<?php echo $i; ?>);
+	}
+<?php 
+} ?>
 	
 	var _sel1 = document.getElementById('sel1');
 	_sel1.innerHTML ='';
@@ -347,12 +390,7 @@ function pagination_changed(){
 	sel2.onchange = function() { change_limit(this.value) };
 
 	var _index = 0;
-	var _i = 10;
-	for (i = 10; i <= 1000 ; i += _i){
-		if (i >= 100)
-			_i = 100;
-		if (i >= 1000)
-			_i = 1000;
+	for (i = 10; i <= 100 ;i += 10){
 		if (i < _limit)
 			_index++;
 		var k = document.createElement('option');
@@ -362,12 +400,7 @@ function pagination_changed(){
 		k.appendChild(l);
 	}
 	var _index = 0;
-	var _i = 10;
-	for (i = 10; i <= 1000 ;i += _i){
-		if (i >= 100)
-			_i = 100;
-		if (i >= 1000)
-			_i = 1000;
+	for (i = 10; i <= 100 ;i += 10){
 		if (i < _limit)
 			_index++;
 		var k = document.createElement('option');
@@ -378,10 +411,8 @@ function pagination_changed(){
 	}
 	sel1.selectedIndex = _index;
 	_sel1.appendChild(sel1);
-	
 	sel2.selectedIndex = _index;
 	_sel2.appendChild(sel2);
-	viewDebugInfo('End : pagination_changed');
 }
 
 function escapeURI(La){
@@ -397,7 +428,7 @@ function mainLoop(){
  	_currentInputField = document.getElementById('input_search');
   	_currentInputFieldValue = document.getElementById('input_search').value;
   	if ((_currentInputFieldValue.length >= 3 || _currentInputFieldValue.length == 0) && _oldInputFieldValue!=_currentInputFieldValue){
-    	var valeur = escapeURI(_currentInputFieldValue);
+    	var valeur=escapeURI(_currentInputFieldValue);
 		_search = valeur;		
 		if (!_lock){
 			monitoring_refresh();
@@ -408,8 +439,8 @@ function mainLoop(){
 				_currentInputField.className = "search_input";
 		}
 	}
-	_oldInputFieldValue = _currentInputFieldValue;
-	setTimeout("mainLoop()", 222);
+	_oldInputFieldValue=_currentInputFieldValue;
+	setTimeout("mainLoop()",222);
 }
 
 function set_limit(limit)	{
