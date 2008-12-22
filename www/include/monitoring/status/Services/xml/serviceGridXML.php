c4bef90fc560e2b794238a107df98ff2044492b9
Julien Mathis <jmathis@merethis.com>
2008-12-22 11:00:03 +0000

    align sort icon
    git-svn-id: http://svn.centreon.com/trunk/centreon@7280 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/Services/xml/serviceGridXML.php
+++ www/include/monitoring/status/Services/xml/serviceGridXML.php
@@ -23,7 +23,7 @@
 	$debugXML = 0;
 	$buffer = '';
 
-	include_once("@CENTREON_ETC@/centreon.conf.php");
+	include_once("/etc/centreon/centreon.conf.php");
 	include_once($centreon_path . "www/class/other.class.php");
 	include_once($centreon_path . "www/DBconnect.php");
 	include_once($centreon_path . "www/DBNDOConnect.php");
@@ -55,17 +55,19 @@
 	/* 
 	 * requisit 
 	 */
-	(isset($_GET["num"]) 		&& !check_injection($_GET["num"])) ? $num = htmlentities($_GET["num"]) : get_error('num unknown');
-	(isset($_GET["limit"]) 		&& !check_injection($_GET["limit"])) ? $limit = htmlentities($_GET["limit"]) : get_error('limit unknown');
-	(isset($_GET["instance"])	/* && !check_injection($_GET["instance"])*/) ? $instance = htmlentities($_GET["instance"]) : $instance = "ALL";
-	(isset($_GET["search"]) 	&& !check_injection($_GET["search"])) ? $search = htmlentities($_GET["search"]) : $search = "";
-	(isset($_GET["sort_type"]) 	&& !check_injection($_GET["sort_type"])) ? $sort_type = htmlentities($_GET["sort_type"]) : $sort_type = "host_name";
-	(isset($_GET["order"]) 		&& !check_injection($_GET["order"])) ? $order = htmlentities($_GET["order"]) : $oreder = "ASC";
+	(isset($_GET["num"]) && !check_injection($_GET["num"])) ? $num = htmlentities($_GET["num"]) : get_error('num unknown');
+	(isset($_GET["limit"]) && !check_injection($_GET["limit"])) ? $limit = htmlentities($_GET["limit"]) : get_error('limit unknown');
+	(isset($_GET["instance"])/* && !check_injection($_GET["instance"])*/) ? $instance = htmlentities($_GET["instance"]) : $instance = "ALL";
+	(isset($_GET["search"]) && !check_injection($_GET["search"])) ? $search = htmlentities($_GET["search"]) : $search = "";
+	(isset($_GET["sort_type"]) && !check_injection($_GET["sort_type"])) ? $sort_type = htmlentities($_GET["sort_type"]) : $sort_type = "host_name";
+	(isset($_GET["order"]) && !check_injection($_GET["order"])) ? $order = htmlentities($_GET["order"]) : $oreder = "ASC";
 	(isset($_GET["date_time_format_status"]) && !check_injection($_GET["date_time_format_status"])) ? $date_time_format_status = htmlentities($_GET["date_time_format_status"]) : $date_time_format_status = "d/m/Y H:i:s";
-	(isset($_GET["o"]) 			&& !check_injection($_GET["o"])) ? $o = htmlentities($_GET["o"]) : $o = "h";
-	(isset($_GET["p"]) 			&& !check_injection($_GET["p"])) ? $p = htmlentities($_GET["p"]) : $p = "2";
+	(isset($_GET["o"]) && !check_injection($_GET["o"])) ? $o = htmlentities($_GET["o"]) : $o = "h";
+	(isset($_GET["p"]) && !check_injection($_GET["p"])) ? $p = htmlentities($_GET["p"]) : $p = "2";
 
-	// check is admin
+	/*
+	 * check is admin
+	 */
 	$is_admin = isUserAdmin($sid);
 
 	/*
@@ -75,6 +77,48 @@
 		$_POST["sid"] = $sid;
 	}
 	
+	function get_services_status($host_name){
+	
+		global $pearDBndo,$pearDB, $ndo_base_prefix, $general_opt, $o, $instance,$is_admin, $groupnumber, $grouplist;
+
+		$rq = 		" SELECT no.name1, no.name2 as service_name, nss.current_state" .
+					" FROM `".$ndo_base_prefix."servicestatus` nss, `".$ndo_base_prefix."objects` no";
+				
+		if (!$is_admin)
+			$rq .= ", centreon_acl ";
+					
+		$rq .= 		" WHERE no.object_id = nss.service_object_id".
+					" AND no.name1 not like 'OSL_Module'";
+					" AND no.name1 not like 'Meta_Module'";
+
+		if ($o == "svcgrid_pb" || $o == "svcOV_pb")
+			$rq .= 	" AND nss.current_state != 0" ;
+
+		if ($o == "svcgrid_ack_0" || $o == "svcOV_ack_0")
+			$rq .= 	" AND nss.problem_has_been_acknowledged = 0 AND nss.current_state != 0" ;
+
+		if ($o == "svcgrid_ack_1" || $o == "svcOV_ack_1")
+			$rq .= 	" AND nss.problem_has_been_acknowledged = 1" ;
+
+		$rq .=  	" AND no.object_id IN (" .
+					" SELECT nno.object_id FROM " .$ndo_base_prefix."objects nno " .
+					" WHERE nno.objecttype_id =2 AND nno.name1 = '".$host_name."')";
+
+		if ($instance != "ALL")
+			$rq .= 	" AND no.instance_id = ".$instance;
+
+		if (!$is_admin && $groupnumber)
+			$rq .= 	" AND no.name1 = centreon_acl.host_name AND no.name2 = centreon_acl.service_description AND centreon_acl.group_id IN (".groupsListStr(getGroupListofUser($pearDB)).")";
+
+		$DBRESULT =& $pearDBndo->query($rq);
+		if (PEAR::isError($DBRESULT))
+			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		$tab = array();
+		while ($svc =& $DBRESULT->fetchRow())
+			$tab[$svc["service_name"]] = $svc["current_state"];
+		return($tab);
+	}
+
 	$service = array();
 	$host_status = array();
 	$service_status = array();
@@ -101,39 +145,43 @@
 	 * Get Host status 
 	 */
 	 
-	$rq1 =	  	" SELECT DISTINCT no.object_id id, no.name1 as host_name, nhs.current_state hs, no.name2 svc_name, nss.current_state svcs" .
-				" FROM " .$ndo_base_prefix."objects no, " .$ndo_base_prefix."hoststatus nhs, " .$ndo_base_prefix."services ns, " .$ndo_base_prefix."servicestatus nss  ";
+	$rq1 =	  	" SELECT DISTINCT no.name1 as host_name, nhs.current_state" .
+				" FROM " .$ndo_base_prefix."objects no, " .$ndo_base_prefix."hoststatus nhs ";
 	if (!$is_admin)
 		$rq1 	.= ", centreon_acl ";
 	
-	$rq1 .=		" WHERE ((no.objecttype_id = '1' AND nhs.host_object_id = no.object_id) OR (no.objecttype_id = '2' AND nss.service_object_id = no.object_id))".
-				" AND no.name1 NOT LIKE 'qos_Module'".
+	$rq1 .=		" WHERE no.objecttype_id = 1 AND nhs.host_object_id = no.object_id ".
+				" AND no.name1 NOT LIKE 'OSL_Module'".
 				" AND no.name1 NOT LIKE 'Meta_Module'";
 	
 	if (!$is_admin && $groupnumber)
-		$rq1 .= " AND no.name1 = centreon_acl.host_name AND no.name2 = centreon_acl.service_description AND group_id IN (".groupsListStr($grouplist).")";
+		$rq1 .= " AND no.name1 = centreon_acl.host_name AND group_id IN (".groupsListStr($grouplist).")";
+
 	if ($o == "svcgrid_pb" || $o == "svcOV_pb" || $o == "svcgrid_ack_0" || $o == "svcOV_ack_0")
-		$rq1 .= " AND nss.current_state != 0 ";
+		$rq1 .= " AND no.name1 IN (" .
+				" SELECT nno.name1 FROM " .$ndo_base_prefix."objects nno," .$ndo_base_prefix."servicestatus nss " .
+				" WHERE nss.service_object_id = nno.object_id AND nss.current_state != 0)";
+	
 	if ($o == "svcgrid_ack_1" || $o == "svcOV_ack_1")
-		$rq1 .= "AND nss.problem_has_been_acknowledged = 1";
-	if ($o == "svcgrid_ack_0" || $o == "svcOV_ack_0")
-		$rq1 .= "AND nss.problem_has_been_acknowledged = 0";
+		$rq1 .= " AND no.name1 IN (" .
+				" SELECT nno.name1 FROM " .$ndo_base_prefix."objects nno," .$ndo_base_prefix."servicestatus nss " .
+				" WHERE nss.service_object_id = nno.object_id AND nss.problem_has_been_acknowledged = 1)";
+
 	if ($search != "")
 		$rq1 .= " AND no.name1 like '%" . $search . "%' ";
+
 	if ($instance != "ALL")
 		$rq1 .= " AND no.instance_id = ".$instance;
-	/*
-	 * Sort
-	 */
-	switch ($sort_type){
-			case 'current_state' : $rq1 .= " order by nhs.current_state ". $order.",no.name1 "; break;
-			default : $rq1 .= " order by no.name1, no.name2 ". $order; break;
+
+	switch ($sort_type) {
+		case 'current_state' : $rq1 .= " order by nhs.current_state ". $order.",no.name1 "; break;
+		default : $rq1 .= " order by no.name1 ". $order; break;
 	}
 
 	$rq_pagination = $rq1;
 
 	/*
-	 * Pagination computing
+	 * Get Pagination Rows
 	 */
 	$DBRESULT_PAGINATION =& $pearDBndo->query($rq_pagination);
 	if (PEAR::isError($DBRESULT_PAGINATION))
@@ -142,7 +190,12 @@
 
 	$rq1 .= " LIMIT ".($num * $limit).",".$limit;
 
-	$buffer .= '<reponse><i><numrows>'.$numRows.'</numrows><num>'.$num.'</num><limit>'.$limit.'</limit><p>'.$p.'</p>';
+	$buffer .= '<reponse>';
+	$buffer .= '<i>';
+	$buffer .= '<numrows>'.$numRows.'</numrows>';
+	$buffer .= '<num>'.$num.'</num>';
+	$buffer .= '<limit>'.$limit.'</limit>';
+	$buffer .= '<p>'.$p.'</p>';
 
 	preg_match("/svcOV/",$_GET["o"], $matches) ? $buffer .= '<s>1</s>' : $buffer .= '<s>0</s>';
 	$buffer .= '</i>';
@@ -153,43 +206,33 @@
 	$ct = 0;
 	$flag = 0;
 
-	$tabH = array();
 	$tab_final = array();
 	while ($ndo =& $DBRESULT_NDO1->fetchRow())	{
-		if (!$ndo["svc_name"] && !isset($tabH[$ndo["id"]])) {
-			$tab_final[$ndo["host_name"]]["cs"] = $ndo["hs"];			
-			$tab_final[$ndo["host_name"]]["tab_svc"] = array();
-			$tabH[$ndo["id"]] = 1;
-		} 
-		if ($ndo["svc_name"]) {
-			if (!isset($tab_final[$ndo["host_name"]]))
-				$tab_final[$ndo["host_name"]] = array("tab_svc" => array());
-			$tab_final[$ndo["host_name"]]["tab_svc"][$ndo["svc_name"]] = $ndo["svcs"];
+		$tab_svc = get_services_status($ndo["host_name"]);
+		if (count($tab_svc)){
+			$tab_final[$ndo["host_name"]]["tab_svc"] = $tab_svc;
+			$tab_final[$ndo["host_name"]]["cs"] = $ndo["current_state"];
 		}
 	}
 
-	foreach ($tab_final as $host_name => $tab){
-		if (count($tab["tab_svc"])) {
-			$class == "list_one" ? $class = "list_two" : $class = "list_one";
-			$buffer .= '<l class="'.$class.'">';
-			foreach ($tab["tab_svc"] as $svc => $state) {
-				$buffer .= '<svc>';
-				$buffer .= '<sn><![CDATA['. $svc . ']]></sn>';
-				$buffer .= '<sc><![CDATA['. $tab_color_service[$state] . ']]></sc>';
-				$buffer .= '</svc>';
-			}
-			$buffer .= '<o>'. $ct++ . '</o>';
-			$buffer .= '<hn><![CDATA['. $host_name  . ']]></hn>';
-			$buffer .= '<hs><![CDATA['. $tab_status_host[$tab["cs"]]  . ']]></hs>';
-			$buffer .= '<hc><![CDATA['. $tab_color_host[$tab["cs"]]  . ']]></hc>';
-			$buffer .= '</l>';		
+	foreach($tab_final as $host_name => $tab){
+		$class == "list_one" ? $class = "list_two" : $class = "list_one";
+		$buffer .= '<l class="'.$class.'">';
+		foreach ($tab["tab_svc"] as $svc => $state) {
+			$buffer .= '<svc>';
+			$buffer .= '<sn><![CDATA['. $svc . ']]></sn>';
+			$buffer .= '<sc><![CDATA['. $tab_color_service[$state] . ']]></sc>';
+			$buffer .= '</svc>';
 		}
+		$buffer .= '<o>'. $ct++ . '</o>';
+		$buffer .= '<hn><![CDATA['. $host_name  . ']]></hn>';
+		$buffer .= '<hs><![CDATA['. $tab_status_host[$tab["cs"]]  . ']]></hs>';
+		$buffer .= '<hc><![CDATA['. $tab_color_host[$tab["cs"]]  . ']]></hc>';
+		$buffer .= '</l>';
 	}
 
-	if (!$ct) {
-		$buffer .= '<infos>';
-		$buffer .= 'none';
-		$buffer .= '</infos>';
+	if (!$ct){
+		$buffer .= '<infos>none</infos>';
 	}
 
 	$buffer .= '</reponse>';
