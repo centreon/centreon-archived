81c9f578cda9681f0d0d3697b38f8bad682be583
Julien Mathis <jmathis@merethis.com>
2008-12-23 21:08:36 +0000

    add blank file
    git-svn-id: http://svn.centreon.com/trunk/centreon@7314 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/class/centreonAuth.class.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Centreon is developped with GPL Licence 2.0 :
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+ * Developped by : Julien Mathis - Romain Le Merlus 
+ * 
+ * The Software is provided to you AS IS and WITH ALL FAULTS.
+ * Centreon makes no representation and gives no warranty whatsoever,
+ * whether express or implied, and without limitation, with regard to the quality,
+ * any particular or intended purpose of the Software found on the Centreon web site.
+ * In no event will Centreon be liable for any direct, indirect, punitive, special,
+ * incidental or consequential damages however they may arise and even if Centreon has
+ * been previously advised of the possibility of such damages.
+ * 
+ * For information : contact@centreon.com
+ */
+ 
+class centreonAuth {
+
+    function centreonAuth() {
+    	
+    }
+
+}
+?>
