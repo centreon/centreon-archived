ef9b2d8e39fc775a5ba921e6e4cf84eacc5c9f1b
Sylvestre Ho <shotamchay@merethis.com>
2009-01-27 22:33:54 +0000

    replaces /etc/centreon with macro
    git-svn-id: http://svn.centreon.com/trunk/centreon@7396 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/views/graphs/GetODSXmlTree.php
+++ www/include/views/graphs/GetODSXmlTree.php
@@ -20,7 +20,7 @@
 
 	require_once 'DB.php';
 
-	include_once "/etc/centreon/centreon.conf.php";
+	include_once "@CENTREON_ETC@/centreon.conf.php";
 	include_once $centreon_path . "www/DBconnect.php";
 	include_once $centreon_path . "www/DBOdsConnect.php";
 	include_once $centreon_path . "www/DBNDOConnect.php";
