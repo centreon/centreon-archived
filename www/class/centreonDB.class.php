9f665e01e7970a99aa318f873fc2f99269943904
Sylvestre Ho <shotamchay@merethis.com>
2009-02-12 17:49:57 +0000

    classes that basically uses DB.php no need to check the return value of queries anymore, the class does it all, all we gotta do is call the constructor &  query methods and that's it =)
    git-svn-id: http://svn.centreon.com/trunk/centreon@7478 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonDB.class.php
+++ www/class/centreonDB.class.php
@@ -14,11 +14,127 @@
  * 
  * For information : contact@centreon.com
  */
- 
-class centreonDB {
 
-    function centreonDB() {
-    	
+require_once ("DB.php");
+
+class CentreonDB {		
+	private $db_type = "mysql";
+	private $retry;
+	private $privatePearDB;
+	private $dsn;
+	private $options;
+	
+	/*
+	 *  Constructor only accepts 1 parameter which can be :
+	 *  - centreon or NULL
+	 *  - centstorage
+	 *  - ndo
+	 */
+    function CentreonDB($db = "centreon", $retry = 3) {
+		include("/etc/centreon/centreon.conf.php");
+
+		$this->retry = $retry;				
+		$this->options = array('debug' => 2,'portability' => DB_PORTABILITY_ALL ^ DB_PORTABILITY_LOWERCASE);
+		switch (strtolower($db)) {
+			case "centreon" : 				
+				$this->connectToCentreon($conf_centreon);
+				$this->connect();
+				break;
+			case "centstorage" :
+				$this->connectToCentstorage($conf_centreon);
+				$this->connect();
+				break;
+			case "ndo" : 
+				$this->connectToCentreon($conf_centreon);
+				$this->connect();
+				$this->connectToNDO($conf_centreon);
+				$this->connect();
+				break;
+			case "default" : 
+				$this->connectToCentreon($conf_centreon);
+				$this->connect();
+				break;
+		}		
+    }
+    
+    /*
+     *  Get info to connect to Centreon DB
+     */
+    private function connectToCentreon($conf_centreon) {		
+		$this->dsn = array(
+	    	'phptype'  => $this->db_type,
+	    	'username' => $conf_centreon["user"],
+	    	'password' => $conf_centreon["password"],
+	    	'hostspec' => $conf_centreon["hostCentreon"],
+	    	'database' => $conf_centreon["db"],
+		);		
+    }
+    
+    /*
+     *  Get info to connect to Centstorage DB
+     */
+    private function connectToCentstorage($conf_centreon) {
+    	$this->dsn = array(
+	    	'phptype'  => $this->db_type,
+	    	'username' => $conf_centreon["user"],
+	    	'password' => $conf_centreon["password"],
+	    	'hostspec' => $conf_centreon["hostCentstorage"],
+	    	'database' => $conf_centreon["dbcstg"],
+		);
+    }
+    
+    /*
+     *  Get info to connect to NDO DB
+     */
+    private function connectToNDO($conf_centreon) {		
+		$DBRESULT =& $this->privatePearDB->query("SELECT db_name, db_prefix, db_user, db_pass, db_host FROM cfg_ndo2db LIMIT 1;");
+		if (PEAR::isError($DBRESULT))
+			print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
+		$confNDO = $DBRESULT->fetchRow();
+		unset($DBRESULT);
+		
+		$this->dsn = array(
+	    	'phptype'  => $this->db_type,
+	    	'username' => $confNDO['db_user'],
+	    	'password' => $confNDO['db_pass'],
+	    	'hostspec' => $confNDO['db_host'],
+	    	'database' => $confNDO['db_name'],
+		);
+    }
+    
+    /*
+     *  The connection is established here
+     */
+    public function connect() {    	    	    	
+    	$this->privatePearDB =& DB::connect($this->dsn, $this->options);
+		$i = 0;
+		while (PEAR::isError($this->privatePearDB) && ($i < $this->retry)) {
+			$this->privatePearDB =& DB::connect($this->dsn, $this->options);
+			$i++;
+		}
+		if ($i == $this->retry) {
+			echo "retry : $i times <br />";//log error with the number of attempts
+			echo $this->privatePearDB->getMessage() . "<br />";	
+		}
+		else	
+			$this->privatePearDB->setFetchMode(DB_FETCHMODE_ASSOC);
+    }
+    
+    /*
+     *  Disconnection
+     */
+    public function disconnect() {
+    	$this->privatePearDB->disconnect();
+    }
+    
+    /*
+     *  Query
+     */
+    public function query($query_string = NULL) {    	
+    	$DBRES = $this->privatePearDB->query($query_string);
+    	if (PEAR::isError($DBRES))
+    		$DBRES->getMessage();//log error
+    	return $DBRES;
     }
 }
 ?>
