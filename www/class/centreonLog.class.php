4ad12cd3846956d628ba31cdadf41697c0b1c4ba
Julien Mathis <jmathis@merethis.com>
2009-02-12 18:37:19 +0000

    add a class for logging error
    git-svn-id: http://svn.centreon.com/trunk/centreon@7480 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/class/centreonLog.class.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Copyright 2005-2009 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation ; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ * 
+ * Linking this program statically or dynamically with other modules is making a 
+ * combined work based on this program. Thus, the terms and conditions of the GNU 
+ * General Public License cover the whole combination.
+ * 
+ * As a special exception, the copyright holders of this program give MERETHIS 
+ * permission to link this program with independent modules to produce an executable, 
+ * regardless of the license terms of these independent modules, and to copy and 
+ * distribute the resulting executable under terms of MERETHIS choice, provided that 
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions 
+ * of the license of that module. An independent module is a module which is not 
+ * derived from this program. If you modify this program, you may extend this 
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ * 
+ * For more information : contact@centreon.com
+ * 
+ * SVN : $URL
+ * SVN : $Id 
+ * 
+ */
+ 
+class CentreonUserLog {
+
+	var $errorType;
+	var $uid;
+	var $path;
+	
+	/*
+	 * Constructor
+	 */
+	function CentreonUserLog ($uid, $pearDB) {
+		
+		$this->uid = $uid;
+		$this->errorType = array();
+		
+		/*
+		 * Get Log directory path
+		 */
+		$DBRESULT =& $pearDB->query("SELECT debug_path FROM general_opt LIMIT 1");
+		$optGen =& $DBRESULT->fetchRow();
+		$DBRESULT->free();
+		
+		/*
+		 * Init log Directory
+		 */
+		if (isset($optGen["debug_path"]) && $optGen["debug_path"] != "") {
+			$this->path = $optGen["debug_path"];
+		} else {
+			$this->path = "/usr/local/centreon/log/";
+		}
+		
+		$this->errorType[1] = $this->path."/error-login-";
+		$this->errorType[2] = $this->path."/error-sql-";
+	}
+	
+	/*
+	 * Function for writing logs
+	 */		
+	function insertLog($id, $str, $print = 0, $page = 0, $option = 0) {
+		/*
+		 * Construct alerte message
+		 */
+		$string = date("H:s")."|".$this->uid."|$page|$option|$str";
+		
+		/*
+		 * Display error on Standard exit
+		 */
+		if ($print) {
+			print $str;
+		}
+		
+		/*
+		 * print Error in log file.
+		 */
+		exec("echo \"".$string."\" >> ".$this->errorType[$id].date("Y-m-d").".log");
+	}
+	
+	function setUID($uid)  {
+		$this->uid = $uid;
+	}
+	
+}
+
+?>
