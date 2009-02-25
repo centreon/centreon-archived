a1122d0a3a1ea92cd5e3a6d9672dc98b64558589
Julien Mathis <jmathis@merethis.com>
2009-02-25 09:09:37 +0000

    add index in ndo DB
    git-svn-id: http://svn.centreon.com/trunk/centreon@7645 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/brocker/Update-NDO-2.0_to_2.1.sql
+++ www/install/sql/brocker/Update-NDO-2.0_to_2.1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `centreon_acl` ADD host_id INT(11) default NULL AFTER `id` ;
 ALTER TABLE `centreon_acl` ADD service_id INT(11) default NULL AFTER `host_name`;
+ALTER TABLE `nagios_servicestatus` ADD INDEX (`current_state`);
