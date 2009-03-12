e35a7875508084dd317edf59d072ad1b92727367
Julien Mathis <jmathis@merethis.com>
2009-03-12 11:34:12 +0000

    remove files
    git-svn-id: http://svn.centreon.com/trunk/centreon@7726 6bcd3966-0018-0410-8128-fd23d134de7e

--- bin/centreonPurge.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-/etc/init.d/centcore stop
-/etc/init.d/centstorage stop
-sleep 10
-/usr/local/centreon/bin/centreonPurge
-sleep 10
-/etc/init.d/centcore start
-/etc/init.d/centstorage start
