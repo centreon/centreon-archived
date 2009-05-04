f7af971e787b38ac07d3f08b77c4dc974cd2a7e8
Julien Mathis <jmathis@merethis.com>
2009-05-04 15:58:04 +0000

    add new class for update error
    git-svn-id: http://svn.centreon.com/trunk/centreon@7939 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/Themes/Centreon-2/color_css.php
+++ www/Themes/Centreon-2/color_css.php
@@ -37,8 +37,7 @@
  */
  
  	require_once "@CENTREON_ETC@/centreon.conf.php";
- 	//require_once "/etc/centreon/centreon.conf.php";
-	require_once $centreon_path . "www/class/centreonDB.class.php";
+ 	require_once $centreon_path . "www/class/centreonDB.class.php";
 
 	$pearDB = new CentreonDB();
  
