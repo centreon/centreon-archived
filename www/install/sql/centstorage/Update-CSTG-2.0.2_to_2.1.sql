504dee7a4955c426b23a6c20dfd6916e6ed631f8
Sylvestre Ho <shotamchay@merethis.com>
2009-05-19 14:06:32 +0000

    alter table already done in 2.0.1 -> 2.0.2 upgrade script
    git-svn-id: http://svn.centreon.com/trunk/centreon@8093 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centstorage/Update-CSTG-2.0.2_to_2.1.sql
+++ www/install/sql/centstorage/Update-CSTG-2.0.2_to_2.1.sql
@@ -31,12 +31,3 @@ CREATE TABLE IF NOT EXISTS `log_action_modification` (
   PRIMARY KEY  (`modification_id`),
   KEY `action_log_id` (`action_log_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
-
---
--- Update for reporting
---
-
-ALTER TABLE  `log_archive_last_status` ADD  `host_id` INT NULL;
-ALTER TABLE  `log_archive_last_status` ADD  `service_id` INT NULL ;
-ALTER TABLE `log_archive_last_status` DROP `id` ;
