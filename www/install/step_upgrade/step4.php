68f33a58f53e99fddcd0f66f77e8bf9a5b7f9f9b
Julien Mathis <jmathis@merethis.com>
2009-05-23 14:51:37 +0000

    move upgrade steps
    git-svn-id: http://svn.centreon.com/trunk/centreon@8179 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/step_upgrade/step4.php
+++ www/install/step_upgrade/step4.php
@@ -35,143 +35,39 @@
  * SVN : $Id$
  * 
  */
-
+ 	
 	include_once $centreon_path . "/www/class/centreonDB.class.php";
 	
 	$pearDB = new CentreonDB();
 	
 	$DBRESULT =& $pearDB->query("SELECT `value` FROM `informations` WHERE `key` = 'version'");
-	$version =& $DBRESULT->fetchRow();	
+	$version =& $DBRESULT->fetchRow();
 	
-	global $pearDB0;	
-	$pearDBO = new CentreonDB("centstorage");	
-	$pearDBndo 	= new CentreonDB("ndo");
-	
-	if (isset($_POST["goto"]) && strcmp($_POST["goto"], "Back"))
-		$_SESSION["script"] = $_POST["script"]; 
-
-	aff_header("Centreon Setup Wizard", "Updating Centreon", 4);	?>
-	<br /><br />
-	<table cellpadding="0" cellspacing="0" border="0" width="80%" class="StyleDottedHr" align="center"><?php
-	print "<tr><th align='left'>Component</th><th style='text-align: right;'>Status</th></tr>";
-	
-	/*
-	 * Update Centstorage
-	 */
-	print "<tr><td><b>Database &#146;".$conf_centreon['dbcstg']."&#146; : Upgrade</b></td>";
-	if (file_exists("./sql/centstorage/Update-CSTG-".$_SESSION["script"].".sql")) {
-		$file_sql = file("./sql/centstorage/Update-CSTG-".$_SESSION["script"].".sql");
-        $request = "";
-        if (count($file_sql)) {
-	        foreach ($file_sql as $line)
-	        	if ($line[0] != "#" && $line[0] != "-") {
-	        		$pos = strrpos($line, ";");
-                	if ($pos != false) {
-                    	$str .= $line;
-                    	$str = rtrim($str);
-                    	$str = str_replace(";", "", $str);                    
-                   	$DBRES = $pearDBO->query($str);
-                    $str = NULL;
-                } else
-                	$str .= $line;
-	        }	        	
-			if (!PEAR::isError($DBRES))
-				echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-    		else
-    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';    			    		
-        } else {
-        	echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-        }
-	} else {
-		echo '<td align="right"><b><span class="warning">PASS</span></b></td></tr>';
-	}
-
-	/*
-	 * Update NDO
-	 */	
-	$DBRESULT =& $pearDB->query("SELECT db_name, db_prefix, db_user, db_pass, db_host FROM cfg_ndo2db LIMIT 1;");
-	if (PEAR::isError($DBRESULT))
-		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
-	$confNDO = $DBRESULT->fetchRow();
-	unset($DBRESULT);	
-	
-	print "<tr><td><b>Database &#146;".$confNDO['db_name']."&#146; : Upgrade</b></td>";
-	if (file_exists("./sql/brocker/Update-NDO-".$_SESSION["script"].".sql")) {
-		$file_sql = file("./sql/brocker/Update-NDO-".$_SESSION["script"].".sql");
-        $request = "";
-        if (count($file_sql)) {
-	        foreach ($file_sql as $line)
-	        	if ($line[0] != "#" && $line[0] != "-") {
-	        		$pos = strrpos($line, ";");
-                	if ($pos != false) {
-                    	$str .= $line;
-                    	$str = rtrim($str);
-                    	$str = str_replace(";", "", $str);                    
-                   	$DBRES = $pearDBndo->query($str);
-                    $str = NULL;
-                	} else
-                		$str .= $line;
-	        	}
-			if (!PEAR::isError($DBRES))
-				echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-    		else
-    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';    			    		
-        } else {
-        	echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-        }
-	} else {
-		echo '<td align="right"><b><span class="warning">PASS</span></b></td></tr>';
-	}
-
-	/*
-	 * Update PHP
-	 */
-	print "<tr><td><b>PHP Script : Upgrade</b></td>";
-	if (file_exists("./php/Update-".$_SESSION["script"].".php")) {
-		if (include_once("./php/Update-".$_SESSION["script"].".php"))
-			echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-		else
-			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
-	} else {
-		echo '<td align="right"><b><span class="warning">PASS</span></b></td></tr>';
-	}
-
-	/*
-	 * Update Centreon
-	 */
-	print "<tr><td><b>Database &#146;".$conf_centreon['db']."&#146; : Upgrade</b></td>";
-	if (file_exists("./sql/centreon/Update-DB-".$_SESSION["script"].".sql")) {
-		$file_sql = file("./sql/centreon/Update-DB-".$_SESSION["script"].".sql");
-        $request = "";
-        if (count($file_sql)) {
-	        foreach ($file_sql as $line)
-	        	if ($line[0] != "#" && $line[0] != "-") {
-	        		$pos = strrpos($line, ";");
-                	if ($pos != false) {
-                    	$str .= $line;
-                    	$str = rtrim($str);
-                    	$str = str_replace(";", "", $str);                    
-                   	$DBRES = $pearDB->query($str);
-                    $str = NULL;
-                	} else
-                		$str .= $line;
-	        	}
-			if (!PEAR::isError($DBRES))
-				echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-    		else
-    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
-        } else {
-        	echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-        }
-	} else {
-		echo '<td align="right"><b><span class="warning">PASS</span></b></td></tr>';
-	}
-
+	aff_header("Centreon Upgrade Wizard", "Select Version", 3); ?>
+	In order for your Centreon upgrade to work properly, please select the appropriate Centreon upgrade script.<br /><br />
+	<table cellpadding="0" cellspacing="0" border="0" width="80%" class="StyleDottedHr" align="center">
+      <tr>
+        <th style="padding-left:20px;" colspan="2">Upgrade SQL Scripts</th>
+      </tr>
+	  <tr>
+        <td><b>MySQL Scripts</b></td>
+        <td align="right">
+        	<select name="script">
+        	<?php       		
+        		chdir('sql/centreon/');
+        		foreach (glob("Update-DB-".$version["value"]."_to_*.sql") as $filename) {
+					$filenameDisplayed = str_replace("Update-DB-", "", $filename);
+					$filenameDisplayed = str_replace(".sql", "", $filenameDisplayed);
+					$filenameDisplayed2 = str_replace("_", " ", $filenameDisplayed);
+					echo '<option value="'.$filenameDisplayed.'">'.$filenameDisplayed2.'</option>'; 
+				}
+        	?>
+        	</select>
+       	</td>
+      </tr>
+	</table>
+	<?php
 	aff_middle();
-	$str = "<input class='button' type='submit' name='goto' value='Back' /><input class='button' type='submit' name='goto' value='Next' id='button_next' ";
-	if (isset($return_false) && $return_false)
-		$str .= " disabled";
-	$str .= " />";
-	print $str;
+	print "<input class='button' type='submit' name='goto' value='Back' /><input class='button' type='submit' name='goto' value='Next' id='button_next' />";
 	aff_footer();
 ?>
