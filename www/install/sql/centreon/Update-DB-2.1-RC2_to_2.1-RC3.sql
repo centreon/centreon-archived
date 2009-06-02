78b0f0b7710ab478ef02a09ad89e6ff4db9c7bde
Julien Mathis <jmathis@merethis.com>
2009-06-02 16:13:55 +0000

    prepare RC3
    git-svn-id: http://svn.centreon.com/trunk/centreon@8294 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1-RC2_to_2.1-RC3.sql
+++ www/install/sql/centreon/Update-DB-2.1-RC2_to_2.1-RC3.sql
@@ -5,3 +5,5 @@ INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VAL
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'w', './include/common/javascript/changetab.js', 'initChangeTab');
+
+UPDATE `informations` SET `value` = '2.1-RC3' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.1-RC2' LIMIT 1;
