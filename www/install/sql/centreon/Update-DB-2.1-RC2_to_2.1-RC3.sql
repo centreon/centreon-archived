6996c5958dff94bf9bbb26c1d80fc2537c785c31
Julien Mathis <jmathis@merethis.com>
2009-06-02 14:22:27 +0000

    add forgotten topology_js add new js for ACL resources
    git-svn-id: http://svn.centreon.com/trunk/centreon@8292 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/centreon/Update-DB-2.1-RC2_to_2.1-RC3.sql
@@ -0,0 +1,7 @@
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 50202, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 50202, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 50202, 'w', './include/common/javascript/changetab.js', 'initChangeTab');
+
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL, 60101, 'w', './include/common/javascript/changetab.js', 'initChangeTab');
