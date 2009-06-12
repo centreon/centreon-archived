0658c0f498b1a029fe8fd8845fd2d0d6e59c3661
Sylvestre Ho <shotamchay@merethis.com>
2009-06-12 12:05:50 +0000

    undefined variable $mLang
    git-svn-id: http://svn.centreon.com/trunk/centreon@8385 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/htmlHeader.php
+++ www/htmlHeader.php
@@ -42,7 +42,7 @@
 	print "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $mlang; ?>">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $oreon->user->lang; ?>">
 <head>
 <title>Centreon - IT & Network Monitoring</title>
 <link rel="shortcut icon" href="./img/favicon.ico"/>
