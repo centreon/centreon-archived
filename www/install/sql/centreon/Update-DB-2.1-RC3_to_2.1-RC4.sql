6e221c040a7cb7528fd53def9ee9deaeafc21e66
Julien Mathis <jmathis@merethis.com>
2009-06-15 17:56:01 +0000

    scaled by default
    git-svn-id: http://svn.centreon.com/trunk/centreon@8425 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1-RC3_to_2.1-RC4.sql
+++ www/install/sql/centreon/Update-DB-2.1-RC3_to_2.1-RC4.sql
@@ -2,4 +2,6 @@ INSERT INTO `topology_JS` (id_page, o, PathName_js, Init) VALUES ('2021203', NUL
 
 ALTER TABLE cfg_nagios ADD debug_level_opt VARCHAR(200) DEFAULT '0' AFTER debug_level;
 
+UPDATE `giv_graphs_template` SET `scaled` = '1';
+
 UPDATE `informations` SET `value` = '2.1-RC4' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.1-RC3' LIMIT 1;
