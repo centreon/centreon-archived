d6ec5184e64dadb45e2d5233825084ef155bb0b7
Nikolaus Filus <nfilus@gmx.net>
2009-06-23 07:25:03 +0000

    fix #950
    git-svn-id: http://svn.centreon.com/trunk/centreon@8511 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1-RC5_to_2.1-RC6.sql
+++ www/install/sql/centreon/Update-DB-2.1-RC5_to_2.1-RC6.sql
@@ -1,1 +1,4 @@
 UPDATE `informations` SET `value` = '2.1-RC6' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.1-RC5' LIMIT 1;
+
+UPDATE `command` SET `command_line` = '$USER1$#S#process-service-perfdata  &quot;$LASTSERVICECHECK$&quot; &quot;$HOSTNAME$&quot; &quot;$SERVICEDESC$&quot; &quot;$LASTSERVICESTATE$&quot; &quot;$SERVICESTATE$&quot; &quot;$SERVICEPERFDATA$&quot;' WHERE `command_name` = 'process-service-perfdata' LIMIT 1;
+
