aa36b66dea99db026e821323e29a2358e5a8f09a
Sylvestre Ho <shotamchay@merethis.com>
2009-08-28 13:34:17 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@8947 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1-RC8_to_2.1.sql
+++ www/install/sql/centreon/Update-DB-2.1-RC8_to_2.1.sql
@@ -1,4 +1,6 @@
-UPDATE `topology` SET topology_url = 'http://trac.centreon.com/' WHERE topology_url LIKE 'http://bugs.centreon.com%'; 
+UPDATE `topology` SET topology_url = 'http://trac.centreon.com/' WHERE topology_url LIKE 'http://bugs.centreon.com%';
+
+ALTER TABLE `service` ADD `service_first_notification_delay` INT NULL AFTER `service_notifications_enabled` ;
 
 UPDATE `informations` SET `value` = '2.1' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.1-RC8' LIMIT 1;
 
