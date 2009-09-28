3b57fc538a9fbe57e02ca351514e6be918ccdc45
Julien Mathis <jmathis@merethis.com>
2009-09-28 17:46:18 +0000

    sync trunk and 2.1
    git-svn-id: http://svn.centreon.com/trunk/centreon@9114 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1-RC8_to_2.1.sql
+++ www/install/sql/centreon/Update-DB-2.1-RC8_to_2.1.sql
@@ -2,7 +2,4 @@ UPDATE `topology` SET topology_url = 'http://trac.centreon.com/' WHERE topology_
 
 UPDATE `acl_resources` SET changed = '1';
 
-ALTER TABLE `service` ADD `service_first_notification_delay` INT NULL AFTER `service_notifications_enabled` ;
-
 UPDATE `informations` SET `value` = '2.1' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.1-RC8' LIMIT 1;
-
