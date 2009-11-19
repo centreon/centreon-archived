dc4d51c2d2fbe36113cba4eae212c8b4adf112a9
Julien Mathis <jmathis@merethis.com>
2009-11-19 23:05:56 +0000

    improve source code
    git-svn-id: http://svn.centreon.com/trunk/centreon@9393 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/menu/xml/menuXML.php
+++ www/menu/xml/menuXML.php
@@ -37,10 +37,12 @@
  */
 		
 	require_once "@CENTREON_ETC@/centreon.conf.php";	
+	
+	require_once $centreon_path."/www/include/common/common-Func.php";
+	
 	require_once $centreon_path."/www/class/centreonDB.class.php";	
 	require_once $centreon_path."/www/class/centreonXML.class.php";
 	require_once $centreon_path."/www/class/centreonACL.class.php";
-	require_once $centreon_path."/www/include/common/common-Func.php";
 	require_once $centreon_path."/www/class/centreon.class.php";
 	require_once $centreon_path."/www/class/centreonSession.class.php";
 	require_once $centreon_path."/www/class/centreonLang.class.php";
