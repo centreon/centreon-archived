431a4332b367847ba6ad03fb4ebee47ee8d3b3b0
Julien Mathis <jmathis@merethis.com>
2009-11-19 18:16:15 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@9384 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/ServicesHostGroups/xml/serviceGridByHGXML.php
+++ www/include/monitoring/status/ServicesHostGroups/xml/serviceGridByHGXML.php
@@ -73,7 +73,7 @@
 	(isset($_GET["limit"]) 			&& !check_injection($_GET["limit"])) ? $limit = htmlentities($_GET["limit"]) : get_error('limit unknown');
 	(isset($_GET["instance"])		/* && !check_injection($_GET["instance"])*/) ? $instance = htmlentities($_GET["instance"]) : $instance = "ALL";
 	(isset($_GET["search"]) 		&& !check_injection($_GET["search"])) ? $search = htmlentities($_GET["search"]) : $search = "";
-	(isset($_GET["sort_type"]) 		&& !check_injection($_GET["sort_type"])) ? $sort_type = htmlentities($_GET["sort_type"]) . ", service_description" : $sort_type = "host_name, service_description";
+	(isset($_GET["sort_type"]) 		&& !check_injection($_GET["sort_type"])) ? $sort_type = htmlentities($_GET["sort_type"]) : $sort_type = "host_name";
 	(isset($_GET["order"]) 			&& !check_injection($_GET["order"])) ? $order = htmlentities($_GET["order"]) : $oreder = "ASC";
 	(isset($_GET["date_time_format_status"]) && !check_injection($_GET["date_time_format_status"])) ? $date_time_format_status = htmlentities($_GET["date_time_format_status"]) : $date_time_format_status = "d/m/Y H:i:s";
 	(isset($_GET["o"]) 				&& !check_injection($_GET["o"])) ? $o = htmlentities($_GET["o"]) : $o = "h";
