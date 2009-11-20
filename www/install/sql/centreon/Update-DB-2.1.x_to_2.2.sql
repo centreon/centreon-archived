be1a6019110223cf964f6271f75d5665eb74aed1
Julien Mathis <jmathis@merethis.com>
2009-11-20 23:18:19 +0000

    finish to implement addressx in contacts configuration page
    git-svn-id: http://svn.centreon.com/trunk/centreon@9408 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1.x_to_2.2.sql
+++ www/install/sql/centreon/Update-DB-2.1.x_to_2.2.sql
@@ -8,3 +8,10 @@ INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VAL
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES(NULL, 60301, 'mc', './include/common/javascript/changetab.js', 'initChangeTab');
 
 ALTER TABLE `host` ADD `host_retry_check_interval` INT NULL AFTER `host_check_interval` ;
+
+ALTER TABLE `contact` ADD `contact_address1` VARCHAR( 200 ) NULL AFTER `contact_pager` ,
+ADD `contact_address2` VARCHAR( 250 ) NULL AFTER `contact_address1` ,
+ADD `contact_address3` VARCHAR( 200 ) NULL AFTER `contact_address2` ,
+ADD `contact_address4` VARCHAR( 200 ) NULL AFTER `contact_address3` ,
+ADD `contact_address5` VARCHAR( 200 ) NULL AFTER `contact_address4` ,
+ADD `contact_address6` VARCHAR( 200 ) NULL AFTER `contact_address5` ;
