792195905fbea9ddfc859f6c2c8b2aa1b956ac2a
Julien Mathis <jmathis@merethis.com>
2009-12-28 11:22:55 +0000

    created by nfilus... but no headers, and why ?
    git-svn-id: http://svn.centreon.com/trunk/centreon@9631 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/external_cmd/extcmd.php
+++ www/include/monitoring/external_cmd/extcmd.php
@@ -1,3 +1,41 @@
+<?php
+/*
+ * Copyright 2005-2009 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation ; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ * 
+ * Linking this program statically or dynamically with other modules is making a 
+ * combined work based on this program. Thus, the terms and conditions of the GNU 
+ * General Public License cover the whole combination.
+ * 
+ * As a special exception, the copyright holders of this program give MERETHIS 
+ * permission to link this program with independent modules to produce an executable, 
+ * regardless of the license terms of these independent modules, and to copy and 
+ * distribute the resulting executable under terms of MERETHIS choice, provided that 
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions 
+ * of the license of that module. An independent module is a module which is not 
+ * derived from this program. If you modify this program, you may extend this 
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ * 
+ * For more information : contact@centreon.com
+ * 
+ * SVN : $URL: http://svn.centreon.com/branches/centreon-2.1/www/include/monitoring/external_cmd/cmd.php $
+ * SVN : $Id: cmd.php 8142 2009-05-20 21:09:15Z jmathis $
+ * 
+ */
+
 	/*
 	 * Write command in nagios pipe or in centcore pipe. 
 	 */
@@ -42,3 +80,5 @@
 		isset($flg) && $flg ? $ret = $flg : $ret = _("Command execution problem");
 		return $ret;
 	}
+	
+?>
