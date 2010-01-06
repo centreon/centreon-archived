6ade9885cd02bb6d69ddcb766f78545654ea21a1
Julien Mathis <jmathis@merethis.com>
2010-01-06 14:06:41 +0000

    fixe generation warinig
    git-svn-id: http://svn.centreon.com/trunk/centreon@9703 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configGenerate/genContacts.php
+++ www/include/configuration/configGenerate/genContacts.php
@@ -134,17 +134,17 @@
 			/*
 			 * ADDRESSX
 			 */
-			if ($contact["contact_address1"]) 
+			if (isset($contact["contact_address1"]) && $contact["contact_address1"]) 
 				$str .= print_line("address1", $contact["contact_address1"]);
-			if ($contact["contact_address2"]) 
+			if (isset($contact["contact_address2"]) && $contact["contact_address2"]) 
 				$str .= print_line("address2", $contact["contact_address2"]);
-			if ($contact["contact_address3"]) 
+			if (isset($contact["contact_address3"]) && $contact["contact_address3"]) 
 				$str .= print_line("address3", $contact["contact_address3"]);
-			if ($contact["contact_address4"]) 
+			if (isset($contact["contact_address4"]) && $contact["contact_address4"]) 
 				$str .= print_line("address4", $contact["contact_address4"]);
-			if ($contact["contact_address5"]) 
+			if (isset($contact["contact_address5"]) && $contact["contact_address5"]) 
 				$str .= print_line("address5", $contact["contact_address5"]);
-			if ($contact["contact_address6"]) 
+			if (isset($contact["contact_address6"]) && $contact["contact_address6"]) 
 				$str .= print_line("address6", $contact["contact_address6"]);
 			
 			$str .= "}\n\n";
