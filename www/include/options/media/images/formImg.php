443294a40d7b933c9226b702eb1803e107c5c2c8
Nikolaus Filus <nfilus@gmx.net>
2010-03-18 20:26:12 +0000

    references #1561 - add media help
    git-svn-id: http://svn.centreon.com/trunk/centreon@10240 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/media/images/formImg.php
+++ www/include/options/media/images/formImg.php
@@ -132,6 +132,15 @@
 	# Smarty template Init
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
+
+	$tpl->assign("helpattr", 'TITLE, "Help", CLOSEBTN, true, FIX, [this, 0, 5], BGCOLOR, "#ffff99", BORDERCOLOR, "orange", TITLEFONTCOLOR, "black", TITLEBGCOLOR, "orange", CLOSEBTNCOLORS, ["","black", "white", "red"], WIDTH, -300, SHADOW, true, TEXTALIGN, "justify"' );
+	# prepare help texts
+	$helptext = "";
+	include_once("help.php");
+	foreach ($help as $key => $text) { 
+		$helptext .= '<span style="display:none" id="help:'.$key.'">'.$text.'</span>'."\n";
+	}
+	$tpl->assign("helptext", $helptext);
 	
 	$valid = false;
 	if ($form->validate())	{
---
+++ www/include/options/media/images/help.php
@@ -0,0 +1,9 @@
+<?php
+$help = array();
+
+$help["img_name"] = dgettext("help", "Enter a new name for the image.");
+$help["img_dir"] = dgettext("help", "Enter an already existing or a new directory to add the uploads to it. A non-existant directory name will be created first.");
+$help["img_file"] = dgettext("help", "Select a local file to upload. You can upload jpg, png, gif and gd2 files. Multiple images can be uploaded together inside archives like zip, tar, tar.gz or tar.bz2.");
+
+?>
+
