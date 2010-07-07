7b0b0bf52783e604f5da705d6b83e49ad60bc1c6
Sylvestre Ho <shotamchay@merethis.com>
2010-07-07 07:59:01 +0000

    Enhancement #1561 context-aware help
    git-svn-id: http://svn.centreon.com/trunk/centreon@10682 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonLang.class.php
+++ www/class/centreonLang.class.php
@@ -3,73 +3,82 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL: http://svn.centreon.com/trunk/centreon/www/class/centreonLang.class.php $
  * SVN : $Id: centreonHost.class.php 8114 2009-05-20 09:51:24Z jmathis $
- * 
+ *
  */
- 
+
  /*
   *  Language management class
   */
- class CentreonLang {
- 	private $charset;
- 	private $lang;
- 	private $path;
- 	private $charsetList;
- 	
- 	/*
+ class CentreonLang
+ {
+ 	protected $_charset;
+ 	protected $_lang;
+ 	protected $_path;
+ 	protected $_charsetList;
+
+ 	/**
  	 *  Constructor
+ 	 *
+ 	 * @param string $centreon_path
+ 	 * @param Centreon $centreon
+ 	 * @return void
  	 */
- 	function CentreonLang($centreon_path, $oreon = NULL) { 						
-		if (isset($oreon)) {
-			$this->lang = $oreon->user->lang;
-			$this->charset = $oreon->user->charset;
+ 	public function CentreonLang($centreon_path, $centreon = null)
+ 	{
+		if (isset($centreon)) {
+			$this->_lang = $centreon->user->lang;
+			$this->_charset = $centreon->user->charset;
+		} else {
+			$this->_lang = "en_US";
+			$this->_charset = "UTF-8";
 		}
-		else {
-			$this->lang = "en_US";
-			$this->charset = "UTF-8";
-		}
-		$this->path = $centreon_path;
+		$this->_path = $centreon_path;
 		$this->setCharsetList();
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Sets list of charsets
+ 	 *
+ 	 *  @return void
  	 */
- 	private function setCharsetList() {
- 		$this->charsetList = array( "ISO-8859-1", 
+ 	private function setCharsetList()
+ 	{
+ 		$this->_charsetList = array(
+ 									"ISO-8859-1",
 									"ISO-8859-2",
-									"ISO-8859-3", 
+									"ISO-8859-3",
 									"ISO-8859-4",
-									"ISO-8859-5", 
+									"ISO-8859-5",
 									"ISO-8859-6",
 									"ISO-8859-7",
 									"ISO-8859-8",
@@ -108,54 +117,71 @@
 									"ISO-10646-UCS-2",
 									"ISO-10646-UCS-4",
 									"SHIFT_JIS");
-		sort($this->charsetList);
+		sort($this->_charsetList);
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Binds lang to the current Centreon page
+ 	 *
+ 	 *  @return void
  	 */
- 	public function bindLang() { 		
-		putenv("LANG=$this->lang");
-		setlocale(LC_ALL, $this->lang);
-		bindtextdomain("messages", $this->path."www/locale/");
-		bind_textdomain_codeset("messages", $this->charset); 
+ 	public function bindLang()
+ 	{
+		putenv("LANG=$this->_lang");
+		setlocale(LC_ALL, $this->_lang);
+		bindtextdomain("*", $this->_path."www/locale/");
+		bind_textdomain_codeset("*", $this->_charset);
 		textdomain("messages");
  	}
- 	 	
- 	/*
+
+ 	/**
  	 *  Lang setter
+ 	 *
+ 	 *  @param string $newLang
+ 	 *  @return void
  	 */
- 	public function setLang($newLang){
- 		$this->lang = $newLang;
+ 	public function setLang($newLang)
+ 	{
+ 		$this->_lang = $newLang;
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Returns lang that is being used
+ 	 *
+ 	 *  @return string
  	 */
- 	public function getLang() {
- 		return $this->lang;	
+ 	public function getLang()
+ 	{
+ 		return $this->_lang;
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Charset Setter
+ 	 *  @param string $newCharset
+ 	 *  @return void
  	 */
- 	public function setCharset($newCharset) {
- 		$this->charset = $newCharset;
+ 	public function setCharset($newCharset)
+ 	{
+ 		$this->_charset = $newCharset;
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Returns charset that is being used
+ 	 *
+ 	 *  @return string
  	 */
- 	public function getCharset() {
- 		return $this->charset;
+ 	public function getCharset()
+ 	{
+ 		return $this->_charset;
  	}
- 	
- 	/*
+
+ 	/**
  	 *  Returns an array with a list of charsets
+ 	 *
+ 	 *  @return array
  	 */
- 	public function getCharsetList() {
- 		return $this->charsetList;
+ 	public function getCharsetList()
+ 	{
+ 		return $this->_charsetList;
  	}
  }
- 
- ?>
