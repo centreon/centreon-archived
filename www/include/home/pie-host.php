4e351ae56b117c48a7c9f12cf962a251793ed5b6
Sylvestre Ho <shotamchay@merethis.com>
2010-07-09 15:12:51 +0000

    enhancement #1847 Global health page - More details
    git-svn-id: http://svn.centreon.com/trunk/centreon@10700 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/pie-host.php
+++ www/include/home/pie-host.php
@@ -3,56 +3,56 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
  	include_once("@CENTREON_ETC@/centreon.conf.php");
  	require_once ($centreon_path . "www/class/centreonSession.class.php");
 	require_once ($centreon_path . "www/class/centreon.class.php");
 	require_once ($centreon_path . "www/class/centreonLang.class.php");
 
 	CentreonSession::start();
-	$oreon =& $_SESSION["oreon"];
-	
+	$oreon =& $_SESSION["centreon"];
+
 	$centreonLang = new CentreonLang($centreon_path, $oreon);
-	$centreonLang->bindLang();	
-			
+	$centreonLang->bindLang();
+
 	include_once($centreon_path . "www/class/centreonDB.class.php");
-		
+
 	/* Connect to oreon DB */
-	
+
 	$pearDB = new CentreonDB();
-		
+
 	include_once($centreon_path . "www/include/common/common-Func.php");
 
 	$ndo_base_prefix = getNDOPrefix();
@@ -63,13 +63,13 @@
 	 * calcul stat for resume
 	 */
 	$statistic_host = array(0 => "UP", 1 => "DOWN", 2 => "UNREACHABLE",3 => "PENDING");
-		
+
 	/* Get HostNDO status */
 	$rq1 = 	" SELECT count(DISTINCT ".$ndo_base_prefix."objects.name1) cnt, ".$ndo_base_prefix."hoststatus.current_state" .
 			" FROM ".$ndo_base_prefix."hoststatus, ".$ndo_base_prefix."objects " .
 			" WHERE ".$ndo_base_prefix."objects.object_id = ".$ndo_base_prefix."hoststatus.host_object_id " .
 			" AND ".$ndo_base_prefix."objects.is_active = 1 " .
-			$oreon->user->access->queryBuilder("AND", $ndo_base_prefix."objects.name1", $oreon->user->access->getHostsString("NAME", $pearDBndo)) .	
+			$oreon->user->access->queryBuilder("AND", $ndo_base_prefix."objects.name1", $oreon->user->access->getHostsString("NAME", $pearDBndo)) .
 			" GROUP BY ".$ndo_base_prefix."hoststatus.current_state " .
 			" ORDER by ".$ndo_base_prefix."hoststatus.current_state";
 	$DBRESULT_NDO1 =& $pearDBndo->query($rq1);
@@ -80,11 +80,11 @@
 	while ($ndo =& $DBRESULT_NDO1->fetchRow()){
 		$data[] = $ndo["cnt"];
 		$legend[] = $statistic_host[$ndo["current_state"]];
-		$color[] = $oreon->optGen["color_".strtolower($statistic_host[$ndo["current_state"]])];	
-		$counter += $ndo["cnt"];	
+		$color[] = $oreon->optGen["color_".strtolower($statistic_host[$ndo["current_state"]])];
+		$counter += $ndo["cnt"];
 	}
 	$DBRESULT_NDO1->free();
-	
+
 	foreach ($data as $key => $value)
 	{
 		$value = round($value / $counter * 100, 2);
@@ -98,7 +98,7 @@
 
 	// PIE chart, 60% alpha
 	$g->pie(60,'#505050','#000000');
-	
+
 	// pass in two arrays, one of data, the other data labels
 	$g->pie_values( $data, $legend );
 	//
@@ -106,9 +106,10 @@
 	// will be re-used (3 colurs for 5 slices means the last two
 	// slices will have colours colour[0] and colour[1]):
 	//
-	
+
 	$g->pie_slice_colours($color);
 	$g->set_tool_tip( '#val#%' );
+	$g->title( sprintf(_('Availability: %s%%'), $data[0]), '{font-size: 18px;}' );
 	//$g->title( " ", '{font-size:18px; color: #424242}' );
 	header("Cache-Control: cache, must-revalidate");
     header("Pragma: public");
--- www/include/home/pie-svc.php
+++ www/include/home/pie-svc.php
@@ -3,65 +3,65 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
  	include_once "@CENTREON_ETC@/centreon.conf.php";
- 
+
 	require_once ($centreon_path."www/class/centreonSession.class.php");
 	require_once ($centreon_path."www/class/centreon.class.php");
 	require_once ($centreon_path."www/class/centreonLang.class.php");
-	
+
 	CentreonSession::start();
-	$oreon =& $_SESSION["oreon"];
+	$oreon =& $_SESSION["centreon"];
 	$centreonlang = new CentreonLang($centreon_path, $oreon);
 	$centreonlang->bindLang();
-	
+
 	include_once $centreon_path . "www/class/centreonDB.class.php";
-		
+
 	$pearDB = new CentreonDB();
-	
+
 	include_once $centreon_path . "www/include/common/common-Func.php";
 
 	$ndo_base_prefix = getNDOPrefix();
-			
+
 	$pearDBndo = new CentreonDB("ndo");
 
 	/*
 	 *  calcul stat for resume
 	 */
 	$statistic = array(0 => "OK", 1 => "WARNING", 2 => "CRITICAL", 3 => "UNKNOWN", 4 => "PENDING");
-	
+
     /*
 	 * LCA
 	 */
@@ -71,17 +71,17 @@
 	$user_id = $user["user_id"];
 
 	global $is_admin;
-	
+
 	$is_admin =  $oreon->user->admin;
 	$grouplistStr = $oreon->user->access->getAccessGroupsString();
-			
-	/* 
-	 * Get Service NDO status 
+
+	/*
+	 * Get Service NDO status
 	 */
 	if (!$is_admin)
 		$rq2 = 	" SELECT count(nss.current_state), nss.current_state" .
 				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no, centreon_acl " .
-				" WHERE no.object_id = nss.service_object_id".				
+				" WHERE no.object_id = nss.service_object_id".
 				" AND no.name1 NOT LIKE '_Module_%' ".
 				" AND no.name1 = centreon_acl.host_name ".
 				" AND no.name2 = centreon_acl.service_description " .
@@ -92,9 +92,9 @@
 				" FROM ".$ndo_base_prefix."servicestatus nss, ".$ndo_base_prefix."objects no" .
 				" WHERE no.object_id = nss.service_object_id".
 				" AND no.name1 NOT LIKE '_Module_%' ".
-				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";			
+				" AND no.is_active = 1 GROUP BY nss.current_state ORDER by nss.current_state";
 	$DBRESULT_NDO2 =& $pearDBndo->query($rq2);
-	
+
 	$svc_stat = array(0=>0, 1=>0, 2=>0, 3=>0, 4=>0);
 	$data = array();
 	$color = array();
@@ -107,18 +107,18 @@
 		$counter += $ndo["count(nss.current_state)"];
 	}
 	$DBRESULT_NDO2->free();
-	
+
 	/*
 	 *  create the dataset
 	 */
-	
+
 	foreach ($data as $key => $value)
 	{
 		$value = round($value / $counter * 100, 2);
 	  	$value = str_replace(",", ".", $value);
 	  	$data[$key] = $value;
 	}
-	
+
 	include_once($centreon_path.'/www/lib/ofc-library/open-flash-chart.php' );
 	$g = new graph();
 	$g->bg_colour = '#FFFFFF';
@@ -129,7 +129,7 @@
 	//
 	// pass in two arrays, one of data, the other data labels
 	//
-	
+
 	$g->pie_values($data, $legend);
 	//
 	// Colours for each slice, in this case some of the colours
@@ -137,7 +137,7 @@
 	// slices will have colours colour[0] and colour[1]):
 	//
 	$g->set_tool_tip( '#key# : #val# %' );
-	
+
 	$g->pie_slice_colours($color);
 
 	$g->set_tool_tip( '#val#%' );
