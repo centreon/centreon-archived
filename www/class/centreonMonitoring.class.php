6dab5d34bef6c8ba76a1d930ad7df13b4d24a996
Julien Mathis <jmathis@merethis.com>
2010-08-31 14:11:03 +0000

    remove little errors
    git-svn-id: http://svn.centreon.com/trunk/centreon@10803 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonMonitoring.class.php
+++ www/class/centreonMonitoring.class.php
@@ -3,52 +3,52 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
- 
+
+
 class CentreonMonitoring {
-	
+
 	var $poller;
-	
+
 	public function CentreonMonitoring() {
 		;
 	}
-	
+
 	public function setPoller($pollerId) {
 		$this->poller = $pollerId;
 	}
-	
+
 	public function getPoller() {
 		return $this->poller;
 	}
@@ -63,22 +63,22 @@ class CentreonMonitoring {
 		} else if ($o == "svcSum_ack_1") {
 			$rq .= " AND nss.problem_has_been_acknowledged = 1 AND nss.current_state != 0";
 		}
-		
+
 		$rq .= 	" AND nss.service_object_id".
 				" IN (".
 				" SELECT nno.object_id".
-				" FROM " .$objXMLBG->ndoPrefix."objects nno";		
-		
+				" FROM " .$objXMLBG->ndoPrefix."objects nno";
+
 		if (!$objXMLBG->is_admin) {
 			$rq	.=	", centreon_acl";
-		}		
+		}
 		$rq	.=	" WHERE nno.objecttype_id = 2 " .
 				" AND nno.name1 = '".$host_name."')";
 		if (!$objXMLBG->is_admin) {
 			$rq .= 	" AND nno.name1 = centreon_acl.host_name AND nno.name2 = centreon_acl.service_description AND centreon_acl.group_id IN (".$oreon->user->access->getAccessGroupsString().")";
 		}
-		
-		$DBRESULT =& $objXMLBG->DBNdo->query($rq);		
+
+		$DBRESULT =& $objXMLBG->DBNdo->query($rq);
 		$tab =& $DBRESULT->fetchRow();
 		return ($tab["nb"]);
 	}
@@ -90,10 +90,10 @@ class CentreonMonitoring {
 
 		$rq = 		" SELECT no.name1, no.name2 as service_name, nss.current_state" .
 					" FROM `".$objXMLBG->ndoPrefix."servicestatus` nss, `".$objXMLBG->ndoPrefix."objects` no";
-				
-		if (!$objXMLBGXMLBG->is_admin)
+
+		if (!$objXMLBG->is_admin)
 			$rq .= ", centreon_acl ";
-					
+
 		$rq .= 		" WHERE no.object_id = nss.service_object_id".
 					" AND no.name1 NOT LIKE '_Module_%'";
 
@@ -125,6 +125,6 @@ class CentreonMonitoring {
 		$DBRESULT->free();
 		return $tab;
 	}
-	
+
 }
 ?>
--- www/class/centreonService.class.php
+++ www/class/centreonService.class.php
@@ -3,52 +3,52 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
  /*
   *  Class that contains various methods for managing services
   */
  class CentreonService {
- 	private $local_pearDB; 	
- 	
+ 	private $local_pearDB;
+
  	/*
  	 *  Constructor
  	 */
  	function CentreonService($pearDB) {
  		$this->local_pearDB = $pearDB;
  	}
- 	
+
  	/*
  	 *  Method that returns service description from service_id
  	 */
@@ -57,16 +57,16 @@
  		$DBRES =& $this->local_pearDB->query($rq);
  		if (!$DBRES->numRows())
  			return NULL;
- 		$row =& $DBRES->fetchRow(); 		
+ 		$row =& $DBRES->fetchRow();
  		return $row['service_description'];
  	}
- 	
+
  	/*
  	 *  Method that returns the id of a service
  	 */
  	public function getServiceId($svc_desc, $host_name) {
- 		$rq = "SELECT s.service_id " . 
- 		      "FROM service s, host_service_relation hsr, host h " . 
+ 		$rq = "SELECT s.service_id " .
+ 		      "FROM service s, host_service_relation hsr, host h " .
  			  "WHERE s.service_id = hsr.service_service_id " .
  			  "AND hsr.host_host_id = h.host_id " .
  			  "AND h.host_name = '".$host_name."' " .
@@ -78,35 +78,35 @@
  		$row = $DBRES->fetchRow();
  		return $row['service_id'];
  	}
- 	 	
+
  	/*
  	 *  Returns a string that replaces on demand macros by their values
  	 */
- 	public function replaceMacroInString($svc_id, $string) { 		 		 		 		
+ 	public function replaceMacroInString($svc_id, $string) {
  		$rq = "SELECT service_register FROM service WHERE service_id = '".$svc_id."' LIMIT 1";
         $DBRES =& $this->local_pearDB->query($rq);
         if (!$DBRES->numRows())
         	return $string;
         $row =& $DBRES->fetchRow();
-        
+
         /*
          * replace if not template
          */
         if ($row['service_register']) {
-	 		if (preg_match("/$SERVICEDESC$/", $string))
-	 			$string = str_replace("\$SERVICEDESC\$", $this->getServiceDesc($svc_id), $string); 		
+	 		if (preg_match("/\$SERVICEDESC\$/", $string))
+	 			$string = str_replace("\$SERVICEDESC\$", $this->getServiceDesc($svc_id), $string);
         }
  		$matches = array();
  		$pattern = '|(\$_SERVICE[0-9a-zA-Z\_\-]+\$)|';
  		preg_match_all($pattern, $string, $matches);
- 		$i = 0; 		
- 		while (isset($matches[1][$i])) {	 			 			
- 			$rq = "SELECT svc_macro_value FROM on_demand_macro_service WHERE svc_svc_id = '".$svc_id."' AND svc_macro_name LIKE '".$matches[1][$i]."'"; 			
- 			$DBRES =& $this->local_pearDB->query($rq); 			 			
+ 		$i = 0;
+ 		while (isset($matches[1][$i])) {
+ 			$rq = "SELECT svc_macro_value FROM on_demand_macro_service WHERE svc_svc_id = '".$svc_id."' AND svc_macro_name LIKE '".$matches[1][$i]."'";
+ 			$DBRES =& $this->local_pearDB->query($rq);
 	 		while ($row =& $DBRES->fetchRow()) {
 	 			$string = str_replace($matches[1][$i], $row['svc_macro_value'], $string);
-	 		} 			 			 			
- 			$i++;		
+	 		}
+ 			$i++;
  		}
  		if ($i) {
 	 		$rq2 = "SELECT service_template_model_stm_id FROM service WHERE service_id = '".$svc_id."'";
@@ -118,5 +118,5 @@
  		return $string;
  	}
  }
- 
+
  ?>
