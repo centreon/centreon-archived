7e30486f2e7b641402665f1834d61e796ddaac78
Julien Mathis <jmathis@merethis.com>
2010-09-09 11:35:55 +0000

    remove hardcoded paths
    git-svn-id: http://svn.centreon.com/trunk/centreon@10840 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/home/pie-host.php
+++ www/include/home/pie-host.php
@@ -36,8 +36,7 @@
  *
  */
 
- 	//include_once "@CENTREON_ETC@/centreon.conf.php";
-	include_once "/etc/centreon/centreon.conf.php";
+ 	include_once "@CENTREON_ETC@/centreon.conf.php";
 
 	require_once ($centreon_path . "www/class/centreonSession.class.php");
 	require_once ($centreon_path . "www/class/centreon.class.php");
