6381b60f72b631c861613476341c6ae6c88a7940
Sylvestre Ho <shotamchay@merethis.com>
2010-10-13 16:17:56 +0000

    Enhancement #1561 CentreonToolTip::render() is called from footer now
    git-svn-id: http://svn.centreon.com/trunk/centreon@10933 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/footer.php
+++ www/footer.php
@@ -3,39 +3,39 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
  	if (!isset($centreon))
 		exit;
 
@@ -43,14 +43,14 @@
 		<table cellpadding="0" cellspacing="0" style="height:1px; width:100%;">
 			<tr><td id="footerline1"></td></tr>
 			<tr><td id="footerline2"></td></tr>
-		</table>		
-		<div id="footer">			
+		</table>
+		<div id="footer">
 			<table cellpadding='0' cellspacing='0' width='100%' border='0'>
 				<tr>
 					<td align='center' class='copyRight'><a href="http://support.centreon.com" title="Centreon Support Access" target='_blank'>Centreon Support</a> - <a href="http://www.centreon.com" title='Centreon Services Overview' target='_blank'>Centreon Services</a> | Copyright &copy; 2004-2010 <a href="http://www.merethis.com">Merethis</a><br /><?php print _("Generated in "); $time_end = microtime_float(); $now = $time_end - $time_start; print round($now,3) . " " . _("seconds"); ?></td>
 				</tr>
-			</table>			
-		</div>		
+			</table>
+		</div>
 		<img src="./img/icones/7x7/sort_asc.gif" onclick="new Effect.toggle('footer'); xhr = new XMLHttpRequest(); xhr.open('GET','./menu/userMenuPreferences.php?uid=<?php echo $centreon->user->user_id; ?>&div=footer', true);xhr.send(null);" style="position:absolute;left:5px;" title="<?php echo _("Hide Footer"); ?>" />
 	</div>
 <?php
@@ -63,15 +63,17 @@ if	(isset($_GET["mini"]) && $_GET["mini"] == 1)	{
 		new Effect.toggle('footer');
 		Effect.toggle('menu1_bgcolor');
 		Effect.toggle('QuickSearch');
+		var centreonTooltip = new CentreonToolTip();
+		centreonTooltip.render();
 	</script>
-<?php } else { 	
+<?php } else {
 	if (!$centreon->user->showDiv("footer")) { ?> <script type="text/javascript">new Effect.toggle('footer', 'blind', { duration : 0 });</script> <?php }
 }
 ?>
 </body>
 </html>
 <?php
-	
+
 	if (isset($pearDB) && is_object($pearDB))
 		$pearDB->disconnect();
 	if (isset($pearDBO) && is_object($pearDBO))
