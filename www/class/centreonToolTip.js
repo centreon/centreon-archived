8f27dde665471872505d42a128f457e8f2f63d6d
Sylvestre Ho <shotamchay@merethis.com>
2010-10-14 08:28:30 +0000

    method for setting title
    git-svn-id: http://svn.centreon.com/trunk/centreon@10937 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonToolTip.js
+++ www/class/centreonToolTip.js
@@ -2,6 +2,8 @@ function CentreonToolTip()
 {	
 	this._className = 'helpTooltip';
 	this._source = './img/icones/16x16/question_grey.gif';
+	this._title = 'Help';
+	
 	var _self = this;
 	
 	this.setClass = function(name) {
@@ -12,11 +14,15 @@ function CentreonToolTip()
 		this._source = source;
 	}
 	
+	this.setTitle = function(title) {
+		this._title = title;
+	}
+	
 	this.render = function() {
 		$$('img.' + _self._className).each(function(el){
 			el.src = _self._source;
 			el.setStyle('cursor:pointer');
-			el.setAttribute('onclick' , 'TagToTip("help:'+el.getAttribute("name")+'", TITLE, "Help", CLOSEBTN, true, FIX, [this, 0, 5], BGCOLOR, "#ffff99", BORDERCOLOR, "orange", TITLEFONTCOLOR, "black", TITLEBGCOLOR, "orange", CLOSEBTNCOLORS, ["","black", "white", "red"], WIDTH, -300, SHADOW, true, TEXTALIGN, "justify");');
+			el.setAttribute('onclick' , 'TagToTip("help:'+el.getAttribute("name")+'", TITLE, "' + _self._title + '", CLOSEBTN, true, FIX, [this, 0, 5], BGCOLOR, "#ffff99", BORDERCOLOR, "orange", TITLEFONTCOLOR, "black", TITLEBGCOLOR, "orange", CLOSEBTNCOLORS, ["","black", "white", "red"], WIDTH, -300, SHADOW, true, TEXTALIGN, "justify");');
 		});	
 	}
 }
