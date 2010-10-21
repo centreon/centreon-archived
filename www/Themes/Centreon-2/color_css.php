8b993ea8993a776aaaada78e8c90a9a7540078bb
Julien Mathis <jmathis@merethis.com>
2010-10-21 15:02:08 +0000

    fix #1984
    git-svn-id: http://svn.centreon.com/trunk/centreon@10971 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/Themes/Centreon-2/color_css.php
+++ www/Themes/Centreon-2/color_css.php
@@ -3,44 +3,44 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
  	require_once "@CENTREON_ETC@/centreon.conf.php";
  	require_once $centreon_path . "www/class/centreonDB.class.php";
 
 	$pearDB = new CentreonDB();
- 
+
  	/*
  	 * Get Options colors
  	 */
@@ -49,7 +49,7 @@
  	while ($res =& $DBRESULT->fetchRow())
  		$options[$res["key"]] = $res["value"];
  	unset($res);
- 	
+
 ?>
 
 .ListTable a:link, #ListTable a:link			{color:<?php print $color2; ?>;}
@@ -65,13 +65,13 @@
 				background-color:<?php print $color6; ?> ;
 }
 
-.ListSubHeader{	
+.ListSubHeader{
 				background-color:#A9C5F2;
 				font-weight:bold;
 				background-position:top left;
 }
 
-	
+
 .ListTable, .ListTableMedium, .ListTableSmall {border-color: <?php print $color6; ?>;}
 
 .list_lvl_2 td, .list_lvl_1 td{
@@ -81,27 +81,27 @@
 	border-right-color: <?php print $color7; ?>;
 }
 
-.list_one td, .list_two td, .list_three td, .list_four td, .list_up td, .list_down td, .line_ack td, .line_downtime td, .list_unreachable td{
+.list_one td, .list_two td, .list_three td, .list_four td, .list_up td, .list_down td, .line_ack td, .line_downtime td, .list_unreachable td, .row_disabled td {
 	border-top-color: <?php print $color8; ?>;
 	border-left-color: <?php print $color8; ?>;
 	border-bottom-color: <?php print $color8; ?>;
 	border-right-color: <?php print $color8; ?>;
 }
 
-.tab .list_one td, .tab .list_two td, .tab .list_three td, .tab .list_four td, .tab .list_up td, .tab .list_down td,.tab .line_ack td,.tab .line_downtime td,.tab .list_unreachable td{
+.tab .list_one td, .tab .list_two td, .tab .list_three td, .tab .list_four td, .tab .list_up td, .tab .list_down td,.tab .line_ack td,.tab .line_downtime td,.tab .list_unreachable td, .tab .row_disabled td {
 	border-top-color: <?php print $color8; ?>;
 	border-left-color: <?php print $color8; ?>;
 	border-bottom-color: <?php print $color8; ?>;
 	border-right-color: <?php print $color8; ?>;
 }
-	
+
 .tab .list_lvl_2 td, .tab .list_lvl_1 td{
 	border-top-color: <?php print $color7; ?>;
 	border-left-color: <?php print $color7; ?>;
 	border-bottom-color: <?php print $color7; ?>;
 	border-right-color: <?php print $color7; ?>;
 }
-	
+
 .ListHeader td{
 	border-top-color: <?php print $color9; ?>;
 	border-left-color: <?php print $color9; ?>;
@@ -111,10 +111,10 @@
 	border-top-color: <?php print $color10; ?>;
 }
 
-.list_one_fixe		{	background-color:<?php print $color_list_1; ?>;} 
+.list_one_fixe		{	background-color:<?php print $color_list_1; ?>;}
 .list_two_fixe 		{	background-color:<?php print $color_list_2; ?>;}
 
-.list_one			{	background-color:<?php print $color_list_1; ?>;} 
+.list_one			{	background-color:<?php print $color_list_1; ?>;}
 .list_one:hover 	{	background-color:<?php print $color_list_1_hover; ?>;}
 
 .list_two 			{	background-color:<?php print $color_list_2; ?>; }
@@ -148,7 +148,7 @@
 
 
 /* Menu */
-#menu1_bgimg	{	
+#menu1_bgimg	{
 	background-image: url(<?php print $menu1_bgimg; ?>);
 	background-position:top right;
 	background-repeat:repeat-x;
@@ -169,7 +169,7 @@
 input, textarea {	font-size: 10px; border:1px solid #BBBBBB;}
 
 input[type="submit"],input[type="button"],input[type="reset"]{
-	background : white;	
+	background : white;
 	color : <?php print $menu1_bgcolor; ?>;
 	border-color : <?php print $menu1_bgcolor; ?>;
 	}
