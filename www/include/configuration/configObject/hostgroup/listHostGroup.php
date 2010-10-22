d10adda9ece1deff910e7e6aa4444e54ea1e448b
Julien Mathis <jmathis@merethis.com>
2010-10-22 08:16:07 +0000

    disable hostgroup groups
    git-svn-id: http://svn.centreon.com/trunk/centreon@10976 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/hostgroup/listHostGroup.php
+++ www/include/configuration/configObject/hostgroup/listHostGroup.php
@@ -3,59 +3,59 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (!isset($oreon))
 		exit();
-		
+
 	include("./include/common/autoNumLimit.php");
-	
+
 	/*
 	 * start quickSearch form
 	 */
 	$advanced_search = 0;
 	include_once("./include/common/quickSearch.php");
-	
+
 	/*
 	 * Search
 	 */
 	$SearchTool = NULL;
-	if (isset($search) && $search)	
-		$SearchTool = " WHERE (hg_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' OR hg_alias LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%')";
-	
-	$request = "SELECT COUNT(*) FROM hostgroup $SearchTool";
-	
+	if (isset($search) && $search)
+		$SearchTool = " (hg_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' OR hg_alias LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%') AND ";
+
+	$request = "SELECT COUNT(*) FROM hostgroup WHERE $SearchTool hg_id NOT IN (SELECT hg_child_id FROM hostgroup_hg_relation)";
+
 	$DBRESULT =& $pearDB->query($request);
 	$tmp = & $DBRESULT->fetchRow();
 	$rows = $tmp["COUNT(*)"];
@@ -67,7 +67,11 @@
 	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
-	
+
+	print "CACHE:";
+	print_r($centreon->cache);
+	$centreon->cache->initHostGroupCache($pearDB);
+
 	/* Access level */
 	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r'; 
 	$tpl->assign('mode_access', $lvl_access);
@@ -84,28 +88,28 @@
 	$tpl->assign("headerMenu_hostgroupAct", _("Enabled HostGroups"));
 	$tpl->assign("headerMenu_hostgroupDeact", _("Disabled HostGroups"));
 	$tpl->assign("headerMenu_options", _("Options"));
-	
+
 	/*
 	 * Hostgroup list
 	 */
-	 
-	$rq = "SELECT hg_id, hg_name, hg_alias, hg_activate FROM hostgroup $SearchTool ORDER BY hg_name LIMIT ".$num * $limit .", $limit"; 
+
+	$rq = "SELECT hg_id, hg_name, hg_alias, hg_activate FROM hostgroup WHERE $SearchTool hg_id NOT IN (SELECT hg_child_id FROM hostgroup_hg_relation) ORDER BY hg_name LIMIT ".$num * $limit .", $limit";
 	$DBRESULT =& $pearDB->query($rq);
-	
+
 	$search = tidySearchKey($search, $advanced_search);
-	
+
 	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
 	/*
 	 * Different style between each lines
 	 */
 	$style = "one";
-	
+
 	/*
 	 * Fill a tab with a mutlidimensionnal Array we put in $tpl
 	 */
 	$elemArr = array();
 	for ($i = 0; $hg =& $DBRESULT->fetchRow(); $i++) {
-		$selectedElements =& $form->addElement('checkbox', "select[".$hg['hg_id']."]");	
+		$selectedElements =& $form->addElement('checkbox', "select[".$hg['hg_id']."]");
 		$moptions = "";
 		if ($hg["hg_activate"])
 			$moptions .= "<a href='main.php?p=".$p."&hg_id=".$hg['hg_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
@@ -113,33 +117,33 @@
 			$moptions .= "<a href='main.php?p=".$p."&hg_id=".$hg['hg_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;";
 		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$hg['hg_id']."]'></input>";
-		
-		/* 
-		 * Check Nbr of Host / hg 
+
+		/*
+		 * Check Nbr of Host / hg
 		 */
 		$nbrhostAct = array();
 		$nbrhostDeact = array();
 		$nbrhostgroupAct = array();
 		$nbrhostgroupDeact = array();
-		
+
 		$rq = "SELECT COUNT(*) as nbr FROM hostgroup_relation hgr, host WHERE hostgroup_hg_id = '".$hg['hg_id']."' AND host.host_id = hgr.host_host_id AND host.host_register = '1' AND host.host_activate = '1'";
 		$DBRESULT2 =& $pearDB->query($rq);
 		$nbrhostAct = $DBRESULT2->fetchRow();
-		
+
 		$rq = "SELECT COUNT(*) as nbr FROM hostgroup_relation hgr, host WHERE hostgroup_hg_id = '".$hg['hg_id']."' AND host.host_id = hgr.host_host_id AND host.host_register = '1' AND host.host_activate = '0'";
 		$DBRESULT2 =& $pearDB->query($rq);
 		$nbrhostDeact = $DBRESULT2->fetchRow();
-		
+
 		$rq = "SELECT COUNT(*) as nbr FROM hostgroup_hg_relation hgr, hostgroup WHERE hg_parent_id = '".$hg['hg_id']."' AND hostgroup.hg_id = hgr.hg_child_id AND hostgroup.hg_activate = '1'";
 		$DBRESULT2 =& $pearDB->query($rq);
 		$nbrhostgroupAct = $DBRESULT2->fetchRow();
-		
+
 		$rq = "SELECT COUNT(*) as nbr FROM hostgroup_hg_relation hgr, hostgroup WHERE hg_parent_id = '".$hg['hg_id']."' AND hostgroup.hg_id = hgr.hg_child_id AND hostgroup.hg_activate = '0'";
 		$DBRESULT2 =& $pearDB->query($rq);
 		$nbrhostgroupDeact = $DBRESULT2->fetchRow();
-		
-		
-		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+
+
+		$elemArr[$i] = array("MenuClass"=>"list_".$style,
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$hg["hg_name"],
 						"RowMenu_link"=>"?p=".$p."&o=c&hg_id=".$hg['hg_id'],
@@ -151,17 +155,17 @@
 						"RowMenu_hostgroupDeact"=>$nbrhostgroupDeact["nbr"],
 						"RowMenu_options"=>$moptions);
 		/*
-		 * Switch color line 
+		 * Switch color line
 		 */
-		$style != "two" ? $style = "two" : $style = "one";	
+		$style != "two" ? $style = "two" : $style = "one";
 	}
 	$tpl->assign("elemArr", $elemArr);
-	
+
 	/*
 	 * Different messages we put in the template
 	 */
 	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
-	
+
 	?>
 	<script type="text/javascript">
 	function setO(_i) {
@@ -182,7 +186,7 @@
 				"this.form.elements['o1'].selectedIndex = 0");
 	$form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs1);
 	$form->setDefaults(array('o1' => NULL));
-		
+
 	$attrs2 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
@@ -204,14 +208,14 @@
 	$o2 =& $form->getElement('o2');
 	$o2->setValue(NULL);
 	$o2->setSelected(NULL);
-	
+
 	$tpl->assign('limit', $limit);
 
 	/*
 	 * Apply a template definition
 	 */
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);	
+	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listHostGroup.ihtml");
 ?>
