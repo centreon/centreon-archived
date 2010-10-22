21b80fddd7122192ed5264828dbee761f85f59c1
Julien Mathis <jmathis@merethis.com>
2010-10-22 21:41:44 +0000

    fix #1991
    git-svn-id: http://svn.centreon.com/trunk/centreon@10977 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/session/connected_user.php
+++ www/include/options/session/connected_user.php
@@ -36,8 +36,9 @@
  * 
  */
 
-	if (!isset($oreon))
+	if (!isset($oreon)) {
 		exit();
+	}
 	
 	$path = "./include/options/session/";	
 	
@@ -52,44 +53,51 @@
 		$msg->setTimeOut("3");
 	}	
 
-	# Smarty template Init
+	/*
+	 * Smarty template Init
+	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
 
-	$res =& $pearDB->query("SELECT * FROM session");
 	$session_data = array();
-	$cpt = 0;
-	while ($r =& $res->fetchRow()){
+	$res =& $pearDB->query("SELECT session.*, contact_name, contact_admin FROM session, contact WHERE contact_id = user_id ORDER BY contact_name, contact_admin");
+	for ($cpt = 0;$r =& $res->fetchRow();$cpt++){
+
 		$session_data[$cpt] = array();
-		if ($cpt % 2)
+		if ($cpt % 2) {
 			$session_data[$cpt]["class"] = "list_one";
-		else
+		} else {
 			$session_data[$cpt]["class"] = "list_two";
-		$resUser =& $pearDB->query("SELECT contact_name, contact_admin FROM contact WHERE contact_id = '".$r["user_id"]."'");
-		$rU =& $resUser->fetchRow();	
+		} 
+		
 		$session_data[$cpt]["user_id"] = $r["user_id"];
-		$session_data[$cpt]["user_alias"] = $rU["contact_name"];
-		$session_data[$cpt]["admin"] = $rU["contact_admin"];
+		$session_data[$cpt]["user_alias"] = $r["contact_name"];
+		$session_data[$cpt]["admin"] = $r["contact_admin"];
+		$session_data[$cpt]["ip_address"] = $r["ip_address"];
+		$session_data[$cpt]["last_reload"] = date("H:i:s", $r["last_reload"]);
+		
 		$resCP =& $pearDB->query("SELECT topology_name, topology_icone, topology_page, topology_url_opt FROM topology WHERE topology_page = '".$r["current_page"]."'");
 		$rCP =& $resCP->fetchRow();
-		$session_data[$cpt]["ip_address"] = $r["ip_address"];
+		
 		$session_data[$cpt]["current_page"] = $r["current_page"].$rCP["topology_url_opt"];
 		$session_data[$cpt]["topology_name"] = _($rCP["topology_name"]);
 		if ($rCP["topology_icone"])
 			$session_data[$cpt]["topology_icone"] = "<img src='".$rCP["topology_icone"]."'>";
 		else
 			$session_data[$cpt]["topology_icone"] = "&nbsp;";
-		$session_data[$cpt]["last_reload"] = date("H:i:s", $r["last_reload"]);
 		$session_data[$cpt]["actions"] = "<a href='./main.php?p=$p&o=k&session_id=".$r["session_id"]."'><img src='./img/icones/16x16/flash.gif' border='0' alt='"._("Kick User")."' title='"._("Kick User")."'></a>";
-		$cpt++;
+
 	}
-	if (isset($msg))
+	
+	if (isset($msg)) {
 		$tpl->assign("msg", $msg);
-		
+	}
+			
 	$tpl->assign("session_data", $session_data);
 	$tpl->assign("wi_user", _("Users"));
 	$tpl->assign("wi_where", _("Position"));
 	$tpl->assign("wi_last_req", _("Last request"));
 	$tpl->assign("distant_location", _("IP Address"));
+	
 	$tpl->display("connected_user.ihtml");
 ?>
