66124a28a4e22fdebbed40b9e58155a957982674
Julien Mathis <jmathis@merethis.com>
2010-11-29 19:16:20 +0000

    add toolbar in servicegroups views
    git-svn-id: http://svn.centreon.com/trunk/centreon@11142 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/ServicesServiceGroups/serviceSummaryBySG.php
+++ www/include/monitoring/status/ServicesServiceGroups/serviceSummaryBySG.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
@@ -41,7 +41,7 @@
 
 	include("./include/common/autoNumLimit.php");
 
-	
+
 	!isset($_GET["sort_types"]) ? $sort_types = 0 : $sort_types = $_GET["sort_types"];
 	!isset($_GET["order"]) ? $order = 'ASC' : $order = $_GET["order"];
 	!isset($_GET["num"]) ? $num = 0 : $num = $_GET["num"];
@@ -51,7 +51,7 @@
 
 	# start quickSearch form
 	include_once("./include/common/quickSearch.php");
-	
+
 	$tab_class = array("0" => "list_one", "1" => "list_two");
 	$rows = 10;
 
@@ -73,6 +73,7 @@
 	$tpl->assign("mon_ip", _("IP"));
 	$tpl->assign("mon_last_check", _("Last Check"));
 	$tpl->assign("mon_duration", _("Duration"));
+	$tpl->assign('pollerStr', _('Poller'));
 	$tpl->assign("mon_status_information", _("Status information"));
 
 
@@ -95,17 +96,17 @@
 	<?php
 
 	$attrs = array(	'onchange'=>"javascript: setO(this.form.elements['o1'].value); submit();");
-    $form->addElement('select', 'o1', NULL, array(	NULL	=>	_("More actions..."), 
-													"3"		=>	_("Verification Check"), 
-													"4"		=>	_("Verification Check (Forced)"), 
-													"70" 	=> 	_("Services : Acknowledge"), 
+    $form->addElement('select', 'o1', NULL, array(	NULL	=>	_("More actions..."),
+													"3"		=>	_("Verification Check"),
+													"4"		=>	_("Verification Check (Forced)"),
+													"70" 	=> 	_("Services : Acknowledge"),
 													"71" 	=> 	_("Services : Disacknowledge"),
-													"80" 	=> 	_("Services : Enable Notification"), 
+													"80" 	=> 	_("Services : Enable Notification"),
 													"81" 	=> 	_("Services : Disable Notification"),
-													"90" 	=> 	_("Services : Enable Check"), 
+													"90" 	=> 	_("Services : Enable Check"),
 													"91" 	=> 	_("Services : Disable Check"),
 													"72" 	=> 	_("Hosts : Acknowledge"),
-													"73" 	=> 	_("Hosts : Disacknowledge"), 
+													"73" 	=> 	_("Hosts : Disacknowledge"),
 													"82" 	=> 	_("Hosts : Enable Notification"),
 													"83" 	=> 	_("Hosts : Disable Notification"),
 													"92" 	=> 	_("Hosts : Enable Check"),
@@ -116,22 +117,22 @@
 	$o1->setValue(NULL);
 
 	$attrs = array('onchange'=>"javascript: setO(this.form.elements['o2'].value); submit();");
-    $form->addElement('select', 'o2', NULL, array(	NULL	=>	_("More actions..."), 
-													"3"		=>	_("Verification Check"), 
-													"4"		=>	_("Verification Check (Forced)"), 
-													"70" 	=> 	_("Services : Acknowledge"), 
+    $form->addElement('select', 'o2', NULL, array(	NULL	=>	_("More actions..."),
+													"3"		=>	_("Verification Check"),
+													"4"		=>	_("Verification Check (Forced)"),
+													"70" 	=> 	_("Services : Acknowledge"),
 													"71" 	=> 	_("Services : Disacknowledge"),
-													"80" 	=> 	_("Services : Enable Notification"), 
+													"80" 	=> 	_("Services : Enable Notification"),
 													"81" 	=> 	_("Services : Disable Notification"),
-													"90" 	=> 	_("Services : Enable Check"), 
+													"90" 	=> 	_("Services : Enable Check"),
 													"91" 	=> 	_("Services : Disable Check"),
 													"72" 	=> 	_("Hosts : Acknowledge"),
-													"73" 	=> 	_("Hosts : Disacknowledge"), 
+													"73" 	=> 	_("Hosts : Disacknowledge"),
 													"82" 	=> 	_("Hosts : Enable Notification"),
 													"83" 	=> 	_("Hosts : Disable Notification"),
 													"92" 	=> 	_("Hosts : Enable Check"),
 													"93" 	=> 	_("Hosts : Disable Check")), $attrs);
-													
+
 	$form->setDefaults(array('o2' => NULL));
 	$o2 =& $form->getElement('o2');
 	$o2->setValue(NULL);
