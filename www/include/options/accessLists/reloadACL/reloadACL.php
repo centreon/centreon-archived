b517b18617c593cd84a090ac05c5c0e8f0359d4e
Sylvestre Ho <shotamchay@merethis.com>
2010-11-30 12:08:10 +0000

    reloadACL will force the engine to be executed
    git-svn-id: http://svn.centreon.com/trunk/centreon@11151 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/accessLists/reloadACL/reloadACL.php
+++ www/include/options/accessLists/reloadACL/reloadACL.php
@@ -3,106 +3,109 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
 		exit();
-	
-	$path = "./include/options/accessLists/reloadACL/";	
-	
-	
+
+	$path = "./include/options/accessLists/reloadACL/";
+
+
 	require_once "./include/common/common-Func.php";
 	require_once "./class/centreonMsg.class.php";
-	require_once "HTML/QuickForm.php";	
+	require_once "HTML/QuickForm.php";
 	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
-		
-	if (isset($_GET["o"]) && $_GET["o"] == "r"){		
+
+	if (isset($_GET["o"]) && $_GET["o"] == "r"){
 		$pearDB->query("UPDATE session SET update_acl = '1' WHERE session_id = '".$_GET["session_id"]."'");
 		$pearDB->query("UPDATE acl_resources SET changed = '1'");
 		$msg = new CentreonMsg();
 		$msg->setTextStyle("bold");
 		$msg->setText(_("ACL reloaded"));
-		$msg->setTimeOut("3");		
-	}	
-	else if (isset($_POST["o"]) && $_POST["o"] == "u") {
+		$msg->setTimeOut("3");
+		passthru("php " . $centreon_path . "/cron/centAcl.php");
+	} elseif (isset($_POST["o"]) && $_POST["o"] == "u") {
 		isset($_GET["select"]) ? $sel = $_GET["select"] : $sel = NULL;
 		isset($_POST["select"]) ? $sel = $_POST["select"] : $sel;
-				
+
 		$pearDB->query("UPDATE acl_resources SET changed = '1'");
 		$query = "UPDATE session SET update_acl = '1' WHERE user_id IN (";
-		$i = 0;		
+		$i = 0;
 		if (isset($sel))
 			foreach ($sel as $key => $val) {
-				if ($i) 
+				if ($i)
 					$query .= ", ";
 				$query .= "'".$key."'";
 				$i++;
 			}
-		if (!$i)
+		if (!$i) {
 			$query .= "'')";
-		else
+		} else {
 			$query .= ")";
-		
+		}
+
 		$pearDB->query($query);
 		$msg->setTextStyle("bold");
 		$msg->setText(_("ACL reloaded"));
 		$msg->setTimeOut("3");
+		passthru("php " . $centreon_path . "/cron/centAcl.php");
 	}
 
 	# Smarty template Init
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
 
-	$res =& $pearDB->query("SELECT DISTINCT * FROM session");
+	$res = $pearDB->query("SELECT DISTINCT * FROM session");
 	$session_data = array();
 	$cpt = 0;
 	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
-	
-	while ($r =& $res->fetchRow()){		
-		$resUser =& $pearDB->query("SELECT contact_name, contact_admin FROM contact WHERE contact_id = '".$r["user_id"]."'");
-		$rU =& $resUser->fetchRow();
+
+	while ($r = $res->fetchRow()){
+		$resUser = $pearDB->query("SELECT contact_name, contact_admin FROM contact WHERE contact_id = '".$r["user_id"]."'");
+		$rU = $resUser->fetchRow();
 		if ($rU["contact_admin"] != "1") {
 			$session_data[$cpt] = array();
-			if ($cpt % 2)
+			if ($cpt % 2) {
 				$session_data[$cpt]["class"] = "list_one";
-			else
-				$session_data[$cpt]["class"] = "list_two";			
+			} else {
+				$session_data[$cpt]["class"] = "list_two";
+			}
 			$session_data[$cpt]["user_id"] = $r["user_id"];
 			$session_data[$cpt]["user_alias"] = $rU["contact_name"];
 			$session_data[$cpt]["admin"] = $rU["contact_admin"];
-			$resCP =& $pearDB->query("SELECT topology_name, topology_icone, topology_page, topology_url_opt FROM topology WHERE topology_page = '".$r["current_page"]."'");
-			$rCP =& $resCP->fetchRow();
+			$resCP = $pearDB->query("SELECT topology_name, topology_icone, topology_page, topology_url_opt FROM topology WHERE topology_page = '".$r["current_page"]."'");
+			$rCP = $resCP->fetchRow();
 			$session_data[$cpt]["ip_address"] = $r["ip_address"];
 			$session_data[$cpt]["current_page"] = $r["current_page"].$rCP["topology_url_opt"];
 			$session_data[$cpt]["topology_name"] = _($rCP["topology_name"]);
@@ -112,12 +115,13 @@
 			$cpt++;
 		}
 	}
-	if (isset($msg))
+	if (isset($msg)) {
 		$tpl->assign("msg", $msg);
-		
+	}
+
 	$tpl->assign("session_data", $session_data);
 	$tpl->assign("wi_user", _("Connected users"));
-	$tpl->assign("wi_where", _("Position"));	
+	$tpl->assign("wi_where", _("Position"));
 	$tpl->assign("actions", _("Reload ACL"));
 	$tpl->assign("distant_location", _("IP Address"));
 	?>
@@ -132,23 +136,23 @@
 				"if (this.form.elements['o1'].selectedIndex == 1) {" .
 				" 	setO(this.form.elements['o1'].value); submit();} this.form.elements['o1'].selectedIndex = 0");
     $form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "u"=>_("Reload ACL")), $attrs);
-	
-	
+
+
 	$attrs = array(
 			'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1) {" .
 				" 	setO(this.form.elements['o2'].value); submit();} this.form.elements['o1'].selectedIndex = 0");
     $form->addElement('select', 'o2', NULL, array(NULL=>_("More actions..."), "u"=>_("Reload ACL")), $attrs);
-	
-	
+
+
 	$o1 =& $form->getElement('o1');
-	$o1->setValue(NULL);	
-	
+	$o1->setValue(NULL);
+
 	$o2 =& $form->getElement('o2');
 	$o2->setValue(NULL);
-	
+
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);	
+	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->assign('p', $p);
 	$tpl->display("reloadACL.ihtml");
