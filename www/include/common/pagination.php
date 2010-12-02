2d845df8d855efc286c4d97a8129d935cdf39f6b
Julien Mathis <jmathis@merethis.com>
2010-12-02 18:16:17 +0000

    add more options in pagination on order to keep selections
    git-svn-id: http://svn.centreon.com/trunk/centreon@11215 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/pagination.php
+++ www/include/common/pagination.php
@@ -3,58 +3,58 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 	global $oreon;
 
 	if (!isset($oreon))
 		exit();
 
-	global $num, $limit, $search, $url, $pearDB, $search_type_service, $search_type_host, $host_name, $hostgroup, $rows, $p, $gopt, $pagination, $poller, $template;
-	
+	global $num, $limit, $search, $url, $pearDB, $search_type_service, $search_type_host, $host_name, $hostgroup, $rows, $p, $gopt, $pagination, $poller, $template, $search_output;
+
 	isset($_GET["type"]) ? $type = $_GET["type"] : $stype = NULL;
 	isset($_GET["o"]) ? $o = $_GET["o"] : $o = NULL;
-	
+
 	if (isset($_GET["num"]))
 		$num = $_GET["num"];
 	else if (!isset($_GET["num"]) && isset($oreon->historyPage[$url]) && $oreon->historyPage[$url])
 		$num = $oreon->historyPage[$url];
 	else
 		$num = 0;
-	
+
 	$num = mysql_real_escape_string($num);
-	
-	$tab_order = array("sort_asc" => "sort_desc", "sort_desc" => "sort_asc"); 	
+
+	$tab_order = array("sort_asc" => "sort_desc", "sort_desc" => "sort_asc");
 
 	if (isset($_GET["search_type_service"])){
 		$search_type_service = $_GET["search_type_service"];
@@ -71,7 +71,7 @@
 		 $search_type_host = $oreon->search_type_host;
 	else
 		$search_type_host = NULL;
-	
+
 	if (!isset($_GET["search_type_host"]) && !isset($oreon->search_type_host) && !isset($_GET["search_type_service"]) && !isset($oreon->search_type_service)){
 		$search_type_host = 1;
 		$oreon->search_type_host = 1;
@@ -80,11 +80,22 @@
 	}
 
 	$url_var = "";
-	$url_var .= "&search_type_service=" . $search_type_service;
-	$url_var .= "&search_type_host=" . $search_type_host;
-	$url_var .= "&hostgroup=" . $hostgroup;
-	$url_var .= "&search_host=" . $host_name;
-	
+	if (isset($search_type_service)) {
+		$url_var .= "&search_type_service=" . $search_type_service;
+	}
+	if (isset($search_type_host)) {
+		$url_var .= "&search_type_host=" . $search_type_host;
+	}
+	if (isset($hostgroup)) {
+		$url_var .= "&hostgroup=" . $hostgroup;
+	}
+	if (isset($host_name)) {
+		$url_var .= "&search_host=" . $host_name;
+	}
+	if (isset($search_output) && $search_output != "") {
+		$url_var .= "&search_output=" . $search_output;
+	}
+
 	if (isset($_GET["order"])){
 		$url_var .= "&order=".$_GET["order"];
 		$order = $_GET["order"];
@@ -93,7 +104,7 @@
 		$url_var .= "&sort_types=".$_GET["sort_types"];
 		$sort_type = $_GET["sort_types"];
 	}
-	
+
 	/*
 	 * Smarty template Init
 	 */
@@ -115,34 +126,34 @@
 	if ($rows != 0) {
 
 		for ($i = $istart; $i <= $iend; $i++)
-			$pageArr[$i] = array("url_page"=>"./main.php?p=".$p."&num=$i&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o . $url_var, "label_page"=>"<b>".($i +1)."</b>","num"=> $i);		
-	
-		if ($i > 1)							
+			$pageArr[$i] = array("url_page"=>"./main.php?p=".$p."&num=$i&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o . $url_var, "label_page"=>"<b>".($i +1)."</b>","num"=> $i);
+
+		if ($i > 1)
 			$tpl->assign("pageArr", $pageArr);
-	
+
 		$tpl->assign("num", $num);
 		$tpl->assign("first", _("First page"));
 		$tpl->assign("previous", _("Previous page"));
 		$tpl->assign("next", _("Next page"));
 		$tpl->assign("last", _("Last page"));
-	
+
 		if (($prev = $num - 1) >= 0)
 			$tpl->assign('pagePrev', ("./main.php?p=".$p."&num=$prev&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
-		
+
 		if (($next = $num + 1) < ($rows/$limit))
 			$tpl->assign('pageNext', ("./main.php?p=".$p."&num=$next&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
-		
+
 		$pageNumber = ceil($rows / $limit);
 		if (($rows / $limit) > 0)
 			$tpl->assign('pageNumber', ($num +1)."/".$pageNumber);
 		else
 			$tpl->assign('pageNumber', ($num)."/".$pageNumber);
-		
+
 		if ($page_max > 5 && $num != 0)
 			$tpl->assign('firstPage', ("./main.php?p=".$p."&num=0&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
 		if ($page_max > 5 && $num != ($pageNumber-1))
 			$tpl->assign('lastPage', ("./main.php?p=".$p."&num=".($pageNumber-1)."&limit=".$limit."&template=$template&poller=".$poller."&search=".$search."&type=".$type."&o=" . $o .$url_var));
-		
+
 		/*
 		 * Select field to change the number of row on the page
 		 */
@@ -154,7 +165,7 @@
 			$select[$rows] = $rows;
 		ksort($select);
 	}
-	
+
 	?><script type="text/javascript">
 	function setL(_this){
 		var _l = document.getElementsByName('l');
@@ -167,7 +178,7 @@
 	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p."&search_type_service=" . $search_type_service."&search_type_host=" . $search_type_host);
 	$selLim =& $form->addElement('select', 'l', _("Rows"), $select, array("onChange" => "setL(this.value);  this.form.submit()"));
 	$selLim->setSelected($limit);
-	
+
 	/*
 	 * Element we need when we reload the page
 	 */
@@ -187,7 +198,7 @@
 
 	isset($_GET["host_name"]) ? $host_name = $_GET["host_name"] : $host_name = NULL;
 	isset($_GET["status"]) ? $status = $_GET["status"] : $status = NULL;
-	
+
 	$tpl->assign("host_name", $host_name);
 	$tpl->assign("status", $status);
 	$tpl->assign("limite", $limite);
