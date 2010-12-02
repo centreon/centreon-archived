5ff1e894b287e1ad51bf2e46ddc6615637c56e59
Sylvestre Ho <shotamchay@merethis.com>
2010-12-02 17:11:08 +0000

    want to try sql upgrade before php upgrade
    git-svn-id: http://svn.centreon.com/trunk/centreon@11211 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/step_upgrade/step5.php
+++ www/install/step_upgrade/step5.php
@@ -3,58 +3,58 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	include_once $centreon_path . "/www/class/centreonDB.class.php";
-	
+
 	$pearDB = new CentreonDB();
-	
+
 	$DBRESULT =& $pearDB->query("SELECT `value` FROM `informations` WHERE `key` = 'version'");
-	$version =& $DBRESULT->fetchRow();	
-	
-	global $pearDB0;	
-	$pearDBO = new CentreonDB("centstorage");	
+	$version =& $DBRESULT->fetchRow();
+
+	global $pearDB0;
+	$pearDBO = new CentreonDB("centstorage");
 	$pearDBndo 	= new CentreonDB("ndo");
-	
+
 	if (isset($_POST["goto"]) && strcmp($_POST["goto"], "Back"))
-		$_SESSION["script"] = $_POST["script"]; 
+		$_SESSION["script"] = $_POST["script"];
 
 	aff_header("Centreon Setup Wizard", "Updating Centreon", 5);	?>
 	<br /><br />
 	<table cellpadding="0" cellspacing="0" border="0" width="80%" class="StyleDottedHr" align="center"><?php
 	print "<tr><th align='left'>Component</th><th style='text-align: right;'>Status</th></tr>";
-	
+
 	/*
 	 * Update Centstorage
 	 */
@@ -70,17 +70,17 @@
                 	if ($pos != false) {
                     	$str .= $line;
                     	$str = rtrim($str);
-                    
+
                    		$DBRES = $pearDBO->query($str);
                     	$str = NULL;
                		} else
                 		$str .= $line;
 	        	}
-	        }	        	
+	        }
 			if (isset($DBRES) && !PEAR::isError($DBRES))
 				echo '<td align="right"><b><span class="go">OK</b></td></tr>';
     		else
-    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';    			    		
+    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
         } else {
         	echo '<td align="right"><b><span class="go">OK</b></td></tr>';
         }
@@ -90,13 +90,13 @@
 
 	/*
 	 * Update NDO
-	 */	
+	 */
 	$DBRESULT =& $pearDB->query("SELECT db_name, db_prefix, db_user, db_pass, db_host FROM cfg_ndo2db LIMIT 1;");
 	if (PEAR::isError($DBRESULT))
 		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
 	$confNDO = $DBRESULT->fetchRow();
-	unset($DBRESULT);	
-	
+	unset($DBRESULT);
+
 	print "<tr><td><b>Database &#146;".$confNDO['db_name']."&#146; : Upgrade</b></td>";
 	if (file_exists("./sql/brocker/Update-NDO-".$_SESSION["script"].".sql")) {
 		$file_sql = file("./sql/brocker/Update-NDO-".$_SESSION["script"].".sql");
@@ -117,25 +117,12 @@
 			if (isset($DBRES) && !PEAR::isError($DBRES))
 				echo '<td align="right"><b><span class="go">OK</b></td></tr>';
     		else
-    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';    			    		
+    			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
         } else {
         	echo '<td align="right"><b><span class="go">OK</b></td></tr>';
         }
 	} else {
 		echo '<td align="right"><b><span class="warning">PASS</span></b></td></tr>';
-	}
-
-	/*
-	 * Update PHP
-	 */
-	print "<tr><td><b>PHP Script : Upgrade</b></td>";
-	if (file_exists("./php/Update-".$_SESSION["script"].".php")) {
-		if (include_once("./php/Update-".$_SESSION["script"].".php"))
-			echo '<td align="right"><b><span class="go">OK</b></td></tr>';
-		else
-			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
-	} else {
-		echo '<td align="right"><b><span class="go">OK</span></b></td></tr>';
 	}
 
 	/*
@@ -168,6 +155,19 @@
         }
 	} else {
 		echo '<td align="right"><b><span class="go">PASS</span></b></td></tr>';
+	}
+
+	/*
+	 * Update PHP
+	 */
+	print "<tr><td><b>PHP Script : Upgrade</b></td>";
+	if (file_exists("./php/Update-".$_SESSION["script"].".php")) {
+		if (include_once("./php/Update-".$_SESSION["script"].".php"))
+			echo '<td align="right"><b><span class="go">OK</b></td></tr>';
+		else
+			echo '<td align="right"><b><span class="critical">CRITICAL</span></b></td></tr>';
+	} else {
+		echo '<td align="right"><b><span class="go">OK</span></b></td></tr>';
 	}
 
 	aff_middle();
