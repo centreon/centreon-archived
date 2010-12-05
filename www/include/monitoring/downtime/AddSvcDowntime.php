263730a632887350dbf94cc478bb43b57c0cae1d
Julien Mathis <jmathis@merethis.com>
2010-12-05 22:08:01 +0000

    add search engine on service downtime. Todo : search by hostgroup have to be enabled
    git-svn-id: http://svn.centreon.com/trunk/centreon@11237 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/downtime/AddSvcDowntime.php
+++ www/include/monitoring/downtime/AddSvcDowntime.php
@@ -3,42 +3,42 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
 		exit();
-	
+
 	include_once $centreon_path."www/class/centreonGMT.class.php";
 	include_once $centreon_path."www/class/centreonDB.class.php";
 
@@ -49,31 +49,31 @@
 	 */
 	$centreonGMT = new CentreonGMT($pearDB);
 	$centreonGMT->getMyGMTFromSession(session_id(), $pearDB);
-		
+
 	$hostStr = $oreon->user->access->getHostsString("ID", $pearDBndo);
-	
-	if ($oreon->user->access->checkAction("service_schedule_downtime")) {		
+
+	if ($oreon->user->access->checkAction("service_schedule_downtime")) {
 		isset($_GET["host_id"]) ? $cG = $_GET["host_id"] : $cG = NULL;
-		isset($_POST["host_id"]) ? $cP = $_POST["host_id"] : $cP = NULL;	
+		isset($_POST["host_id"]) ? $cP = $_POST["host_id"] : $cP = NULL;
 		$cG ? $host_id = $cG : $host_id = $cP;
-	
+
 	    $svc_description = NULL;
-		
+
 		if (isset($_GET["host_name"]) && isset($_GET["service_description"])){
-			$host_id = getMyHostID($_GET["host_name"]);			
+			$host_id = getMyHostID($_GET["host_name"]);
 			$service_id = getMyServiceID($_GET["service_description"], $host_id);
 			$host_name = $_GET["host_name"];
 			$svc_description = $_GET["service_description"];
 		} else
 			$host_name = NULL;
-			
+
 			$data = array();
 			$data = array("start" => $centreonGMT->getDate("Y/m/d G:i" , time() + 120), "end" => $centreonGMT->getDate("Y/m/d G:i", time() + 7320), "persistant" => 1);
-			if (isset($host_id)) 
-				$data["host_id"] = $host_id;			
+			if (isset($host_id))
+				$data["host_id"] = $host_id;
 			if (isset($service_id))
 				$data["service_id"] = $service_id;
-				
+
 			/*
 			 * Database retrieve information for differents elements list we need on the page
 			 */
@@ -81,61 +81,61 @@
 			$query = "SELECT host_id, host_name " .
 					"FROM `host` " .
 					"WHERE host_register = '1' " .
-					$oreon->user->access->queryBuilder("AND", "host_id", $hostStr) . 
+					$oreon->user->access->queryBuilder("AND", "host_id", $hostStr) .
 					"ORDER BY host_name";
 			$DBRESULT =& $pearDB->query($query);
-			while ($host =& $DBRESULT->fetchRow()){				
+			while ($host =& $DBRESULT->fetchRow()){
 				$hosts[$host["host_id"]]= $host["host_name"];
 			}
 			$DBRESULT->free();
-		
+
 			$services = array(NULL => NULL);
 			if (isset($host_id))
 				$services = $oreon->user->access->getHostServices($pearDBndo, $host_id);
-			
+
 			$debug = 0;
 			$attrsTextI		= array("size"=>"3");
 			$attrsText 		= array("size"=>"30");
 			$attrsTextarea 	= array("rows"=>"7", "cols"=>"100");
-			
+
 			/*
 			 * Form begin
-			 */		
+			 */
 			$form = new HTML_QuickForm('Form', 'POST', "?p=".$p);
 			$form->addElement('header', 'title', _("Add a Service downtime"));
-			
+
 			/*
 			 * Indicator basic information
-			 */			
+			 */
 			$redirect =& $form->addElement('hidden', 'o');
 			$redirect->setValue($o);
-		    
+
 		    $selHost =& $form->addElement('select', 'host_id', _("Host Name"), $hosts, array("onChange" =>"this.form.submit();"));
 			$selSv =& $form->addElement('select', 'service_id', _("Service"), $services);
 		    $form->addElement('checkbox', 'persistant', _("Fixed"));
 			$form->addElement('textarea', 'comment', _("Comments"), $attrsTextarea);
-			
+
 			$form->addElement('text', 'start', _("Start Time"), $attrsText);
 			$form->addElement('text', 'end', _("End Time"), $attrsText);
 			$form->addElement('textarea', 'comment', _("Comments"), $attrsTextarea);
-			
+
 			$form->addRule('host_id', _("Required Field"), 'required');
 			$form->addRule('service_id', _("Required Field"), 'required');
 			$form->addRule('end', _("Required Field"), 'required');
 			$form->addRule('start', _("Required Field"), 'required');
-			$form->addRule('comment', _("Required Field"), 'required');	
-			
+			$form->addRule('comment', _("Required Field"), 'required');
+
 			$form->setDefaults($data);
-			
+
 			$subA =& $form->addElement('submit', 'submitA', _("Save"));
 			$res =& $form->addElement('reset', 'reset', _("Reset"));
-		  
+
 		  	if ((isset($_POST["submitA"]) && $_POST["submitA"]) && $form->validate())	{
 				if (!isset($_POST["persistant"]))
 					$_POST["persistant"] = 1;
 				if (!isset($_POST["comment"]))
 					$_POST["comment"] = 0;
-				AddSvcDowntime($_POST["host_id"], $_POST["service_id"],  $_POST["comment"], $_POST["start"], $_POST["end"], $_POST["persistant"]);
+				$ecObj->AddSvcDowntime($_POST["host_id"], $_POST["service_id"],  $_POST["comment"], $_POST["start"], $_POST["end"], $_POST["persistant"]);
 		    	require_once("viewServiceDowntime.php");
 			} else {
 				/*
@@ -143,15 +143,15 @@
 				 */
 				$tpl = new Smarty();
 				$tpl = initSmartyTpl($path, $tpl, "template/");
-		
+
 				/*
 				 * Apply a template definition
 				 */
 				$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 				$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
 				$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
-				$form->accept($renderer);		
-				$tpl->assign('form', $renderer->toArray());	
+				$form->accept($renderer);
+				$tpl->assign('form', $renderer->toArray());
 				$tpl->assign('o', $o);
 				$tpl->display("AddSvcDowntime.ihtml");
 		    }
--- www/include/monitoring/downtime/viewServiceDowntime.php
+++ www/include/monitoring/downtime/viewServiceDowntime.php
@@ -3,58 +3,80 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
-	
+
 	if (!isset($oreon))
 		exit();
-		
+
 	include_once $centreon_path."www/class/centreonGMT.class.php";
 	include("./include/common/autoNumLimit.php");
-	
+
+	if (isset($_POST["hostgroup"]))
+	  $hostgroup = $_POST["hostgroup"];
+	else if (isset($_GET["hostgroup"]))
+	   $hostgroup = $_GET["hostgroup"];
+	else if (isset($centreon->hostgroup) && $centreon->hostgroup)
+	   $hostgroup = $centreon->hostgroup;
+	else
+	   $hostgroup = 0;
+
+	if (isset($_POST["search_host"]))
+		$host_name = $_POST["search_host"];
+	else if (isset($_GET["search_host"]))
+		$host_name = $_GET["search_host"];
+	else
+		$host_name = NULL;
+
+	if (isset($_POST["search_output"]))
+		$search_output = $_POST["search_output"];
+	else if (isset($_GET["search_output"]))
+		$search_output = $_GET["search_output"];
+	else
+		$search_output = NULL;
+
 	/*
 	 * Init GMT class
 	 */
-	
 	$centreonGMT = new CentreonGMT($pearDB);
 	$centreonGMT->getMyGMTFromSession(session_id(), $pearDB);
-		
+
 	$ndo_base_prefix = getNDOPrefix();
 	include_once("./class/centreonDB.class.php");
-	
+
 	$pearDBndo = new CentreonDB("ndo");
-	
-	
+
+
 	/*
 	 * Smarty template Init
 	 */
@@ -71,64 +93,50 @@
 	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p);
 
 	$tab_downtime_svc = array();
-	
-	/* Pagination Services */
-	if ($is_admin) {
-		$rq3 = "SELECT COUNT(*) " .
-				"FROM ".$ndo_base_prefix."scheduleddowntime dtm, ".$ndo_base_prefix."objects obj " .
-				"WHERE obj.name1 IS NOT NULL " .
-				"AND obj.name2 IS NOT NULL " .
-				"AND obj.object_id = dtm.object_id " .
-				"AND dtm.scheduled_end_time > '".date("Y-m-d G:i:s", time())."'";				
-	} else {
-		$rq3 =	"SELECT COUNT(*) " .
-				"FROM ".$ndo_base_prefix."scheduleddowntime dtm, ".$ndo_base_prefix."objects obj, centreon_acl " .
-				"WHERE obj.name1 IS NOT NULL " .
-				"AND obj.name2 IS NOT NULL " .
-				"AND obj.object_id = dtm.object_id " .
-				"AND obj.name1 = centreon_acl.host_name " . 
-				"AND obj.name2 = centreon_acl.service_description " . 
-				"AND centreon_acl.group_id IN (".$oreon->user->access->getAccessGroupsString().") " .
-				"AND dtm.scheduled_end_time > '".date("Y-m-d G:i:s", time())."'";				
-	}
-	$DBRES =& $pearDBndo->query($rq3);
-	$rows =& $DBRES->fetchRow();
-	$rows = $rows['COUNT(*)'];	
-	
-	include("./include/common/checkPagination.php");
-	
+
 	/*
 	 * Service Downtimes
 	 */
-	if ($is_admin) 
-		$rq2 =	"SELECT dtm.internal_downtime_id, unix_timestamp(dtm.entry_time), dtm.duration, dtm.author_name, dtm.comment_data, dtm.is_fixed, unix_timestamp(dtm.scheduled_start_time) AS scheduled_start_time, unix_timestamp(dtm.scheduled_end_time) AS scheduled_end_time, obj.name1 host_name, obj.name2 service_description, was_started " .
+	if ($is_admin)
+		$rq2 =	"SELECT SQL_CALC_FOUND_ROWS dtm.internal_downtime_id, unix_timestamp(dtm.entry_time), dtm.duration, dtm.author_name, dtm.comment_data, dtm.is_fixed, unix_timestamp(dtm.scheduled_start_time) AS scheduled_start_time, unix_timestamp(dtm.scheduled_end_time) AS scheduled_end_time, obj.name1 host_name, obj.name2 service_description, was_started " .
 				"FROM ".$ndo_base_prefix."scheduleddowntime dtm, ".$ndo_base_prefix."objects obj " .
 				"WHERE obj.name1 IS NOT NULL " .
 				"AND obj.name2 IS NOT NULL " .
 				"AND obj.object_id = dtm.object_id " .
+				(isset($host_name) && $host_name != "" ? "AND obj.name1 LIKE '%$host_name%' " : "") .
+				(isset($search_output) && $search_output != "" ? "AND dtm.comment_data LIKE '%$search_output%' " : "") .
 				"AND dtm.scheduled_end_time > '".date("Y-m-d G:i:s", time())."' " .
-				"ORDER BY dtm.actual_start_time DESC " . 
+				"ORDER BY dtm.actual_start_time DESC " .
 				"LIMIT ".$num * $limit.", ".$limit;
 	else
-		$rq2 =	"SELECT dtm.internal_downtime_id, unix_timestamp(dtm.entry_time), dtm.duration, dtm.author_name, dtm.comment_data, dtm.is_fixed, unix_timestamp(dtm.scheduled_start_time) AS scheduled_start_time, unix_timestamp(dtm.scheduled_end_time) AS scheduled_end_time, obj.name1 host_name, obj.name2 service_description, was_started " .
+		$rq2 =	"SELECT SQL_CALC_FOUND_ROWS dtm.internal_downtime_id, unix_timestamp(dtm.entry_time), dtm.duration, dtm.author_name, dtm.comment_data, dtm.is_fixed, unix_timestamp(dtm.scheduled_start_time) AS scheduled_start_time, unix_timestamp(dtm.scheduled_end_time) AS scheduled_end_time, obj.name1 host_name, obj.name2 service_description, was_started " .
 				"FROM ".$ndo_base_prefix."scheduleddowntime dtm, ".$ndo_base_prefix."objects obj, centreon_acl " .
 				"WHERE obj.name1 IS NOT NULL " .
 				"AND obj.name2 IS NOT NULL " .
 				"AND obj.object_id = dtm.object_id " .
-				"AND obj.name1 = centreon_acl.host_name " . 
-				"AND obj.name2 = centreon_acl.service_description " . 
+				"AND obj.name1 = centreon_acl.host_name " .
+				(isset($host_name) && $host_name != "" ? "AND obj.name1 LIKE '%$host_name%' " : "") .
+				(isset($search_output) && $search_output != "" ? "AND dtm.comment_data LIKE '%$search_output%' " : "") .
+				"AND obj.name2 = centreon_acl.service_description " .
 				"AND centreon_acl.group_id IN (".$oreon->user->access->getAccessGroupsString().") " .
 				"AND dtm.scheduled_end_time > '".date("Y-m-d G:i:s", time())."' " .
 				"ORDER BY dtm.actual_start_time DESC " .
 				"LIMIT ".$num * $limit.", ".$limit;
-				
+
 	$DBRESULT_NDO =& $pearDBndo->query($rq2);
 	for ($i = 0; $data =& $DBRESULT_NDO->fetchRow(); $i++) {
 		$tab_downtime_svc[$i] = $data;
 		$tab_downtime_svc[$i]["scheduled_start_time"] = $centreonGMT->getDate("m/d/Y H:i" , $tab_downtime_svc[$i]["scheduled_start_time"])." ";
 		$tab_downtime_svc[$i]["scheduled_end_time"] = $centreonGMT->getDate("m/d/Y H:i" , $tab_downtime_svc[$i]["scheduled_end_time"])." ";
 	}
-	unset($data);	
+	unset($data);
+
+	/*
+	 * Number Rows
+	 */
+	$rows = $pearDBndo->numberRows();
+	include("./include/common/checkPagination.php");
+
 
 	$en = array("0" => _("No"), "1" => _("Yes"));
 	foreach ($tab_downtime_svc as $key => $value) {
@@ -140,17 +148,17 @@
 	 */
 	$form->addElement('hidden', 'p');
 	$tab = array ("p" => $p);
-	$form->setDefaults($tab);			
-	
-	if ($oreon->user->access->checkAction("service_schedule_downtime")) 
+	$form->setDefaults($tab);
+
+	if ($oreon->user->access->checkAction("service_schedule_downtime"))
 		$tpl->assign('msgs', array ("addL"=>"?p=".$p."&o=as", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
-		
-	
+
+
 	$tpl->assign("p", $p);
-		
+
 	$tpl->assign("tab_downtime_svc", $tab_downtime_svc);
 	$tpl->assign("nb_downtime_svc", count($tab_downtime_svc));
-	
+
 	$tpl->assign("dtm_host_name", _("Host Name"));
 	$tpl->assign("dtm_service_descr", _("Services"));
 	$tpl->assign("dtm_start_time", _("start Time"));
@@ -161,15 +169,34 @@
 	$tpl->assign("dtm_duration", _("Duration"));
 	$tpl->assign("dtm_started", _("Started"));
 	$tpl->assign("dtm_service_downtime", _("Services Downtimes"));
-	
+
 	$tpl->assign("secondes", _("s"));
-	
-	$tpl->assign("no_svc_dtm", _("No downtime scheduled for services"));	
+
+	$tpl->assign("no_svc_dtm", _("No downtime scheduled for services"));
 	$tpl->assign("view_host_dtm", _("View downtimes of hosts"));
 	$tpl->assign("host_dtm_link", "./main.php?p=".$p."&o=vh");
 	$tpl->assign("delete", _("Delete"));
 	$tpl->assign("limit", $limit);
-	
+
+	$tpl->assign("Host", _("Host Name"));
+	$tpl->assign("Output", _("Output"));
+	$tpl->assign("user", _("Utilisateurs"));
+	$tpl->assign('Hostgroup', _("Hostgroup"));
+	$tpl->assign('Search', _("Search"));
+	$tpl->assign("search_output", $search_output);
+	$tpl->assign('search_host', $host_name);
+
+	$DBRESULT =& $pearDBndo->query("SELECT hostgroup_id, alias FROM ".$ndo_base_prefix."hostgroups ORDER BY alias");
+	$options = "<option value='0'></options>";
+	while ($data =& $DBRESULT->fetchRow()) {
+        $options .= "<option value='".$data["hostgroup_id"]."' ".(($hostgroup == $data["hostgroup_id"]) ? 'selected' : "").">".$data["alias"]."</option>";
+    }
+    $DBRESULT->free();
+
+	$tpl->assign('hostgroup', $options);
+	unset($options);
+
+
 	$renderer =& new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
