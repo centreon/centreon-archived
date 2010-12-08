50d665964f105e4cd301068d1f5a5372779a37ac
Sylvestre Ho <shotamchay@merethis.com>
2010-12-08 13:45:59 +0000

    missing backquote
    git-svn-id: http://svn.centreon.com/trunk/centreon@11262 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configServers/DB-Func.php
+++ www/include/configuration/configServers/DB-Func.php
@@ -114,7 +114,7 @@
 		global $form, $pearDB, $oreon;
 		if (!count($ret))
 			$ret = $form->getSubmitValues();
-		$rq = "INSERT INTO `nagios_server` (`name` , `localhost` , `ns_ip_address`, `ssh_port`, `monitoring_engine`, nagios_bin`, `nagiostats_bin` , `init_script`, `nagios_perfdata` , `ssh_private_key`, `ns_activate`) ";
+		$rq = "INSERT INTO `nagios_server` (`name` , `localhost` , `ns_ip_address`, `ssh_port`, `monitoring_engine`, `nagios_bin`, `nagiostats_bin` , `init_script`, `nagios_perfdata` , `ssh_private_key`, `ns_activate`) ";
 		$rq .= "VALUES (";
 		isset($ret["name"]) && $ret["name"] != NULL ? $rq .= "'".htmlentities(trim($ret["name"]), ENT_QUOTES, "UTF-8")."', " : $rq .= "NULL, ";
 		isset($ret["localhost"]["localhost"]) && $ret["localhost"]["localhost"] != NULL ? $rq .= "'".htmlentities($ret["localhost"]["localhost"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "NULL, ";
