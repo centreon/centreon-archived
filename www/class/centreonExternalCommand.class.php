aaac71043a8d6e53102f96ce42b87fd46c169acd
Sylvestre Ho <shotamchay@merethis.com>
2010-12-09 08:54:48 +0000

    removes hardcoded paths (/etc/centreon)
    git-svn-id: http://svn.centreon.com/trunk/centreon@11269 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonExternalCommand.class.php
+++ www/class/centreonExternalCommand.class.php
@@ -36,7 +36,8 @@
  *
  */
 
-require_once "/etc/centreon/centreon.conf.php";
+require_once "@CENTREON_ETC@/centreon.conf.php";
+//require_once "/etc/centreon/centreon.conf.php";
 require_once $centreon_path . "/www/class/centreonDB.class.php";
 require_once $centreon_path . "/www/include/common/common-Func.php";
 
--- www/include/configuration/configObject/command/formArguments.php
+++ www/include/configuration/configObject/command/formArguments.php
@@ -3,69 +3,70 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  */
- 
-	require_once("/etc/centreon/centreon.conf.php");
+
+    require_once("@CENTREON_ETC@/centreon.conf.php");
+	//require_once("/etc/centreon/centreon.conf.php");
 	require_once($centreon_path . "/www/class/centreonSession.class.php");
 	require_once($centreon_path . "/www/class/centreon.class.php");
-	
+
 	session_start();
-		
+
 	$centreon = $_SESSION['centreon'];
 	if (!isset($centreon))
 		exit();
-	
+
 	require_once "HTML/QuickForm.php";
 	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
-		
+
 	$args = array();
 	$str = "";
-	$nb_arg = 0;	
+	$nb_arg = 0;
 	if (isset($_GET['cmd_line']) && $_GET['cmd_line']) {
-		$str = str_replace("\$", "@DOLLAR@", $_GET['cmd_line']);		
-		$nb_arg = preg_match_all("/@DOLLAR@ARG([0-9]+)@DOLLAR@/", $str, $matches);	
+		$str = str_replace("\$", "@DOLLAR@", $_GET['cmd_line']);
+		$nb_arg = preg_match_all("/@DOLLAR@ARG([0-9]+)@DOLLAR@/", $str, $matches);
 	}
-	
+
 	if (isset($_GET['textArea']) && $_GET['textArea']) {
-		$tab = split(";;;", $_GET['textArea']);		
-		foreach ($tab as $key=>$value) {			
-			$tab2 = split(" : ", $value, 2);			
+		$tab = split(";;;", $_GET['textArea']);
+		foreach ($tab as $key=>$value) {
+			$tab2 = split(" : ", $value, 2);
 			$index = str_replace("ARG", "", $tab2[0]);
 			if (isset($tab2[0]) && $tab2[0])
 				$args[$index] = $tab2[1];
 		}
 	}
-			
+
 	/* FORM */
-	
+
 $path = "$centreon_path/www/include/configuration/configObject/command/";
 
 $attrsText 		= array("size"=>"30");
@@ -103,11 +104,11 @@ $tpl->assign('nb_arg', $nb_arg);
 $dummyTab = array();
 $defaultDesc = array();
 
-for ($i = 1; $i <= $nb_arg; $i++) {	
+for ($i = 1; $i <= $nb_arg; $i++) {
 	$dummyTab[$i] = $matches[1][$i - 1];
 	$defaultDesc[$i] = "";
 	if (isset($args[$dummyTab[$i]]) && $args[$dummyTab[$i]])
-		$defaultDesc[$i] = $args[$dummyTab[$i]];	
+		$defaultDesc[$i] = $args[$dummyTab[$i]];
 }
 $tpl->assign('dummyTab', $dummyTab);
 $tpl->assign('defaultDesc', $defaultDesc);
