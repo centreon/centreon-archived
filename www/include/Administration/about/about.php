16804e5b75c9bdc9afaa2219b9992663d50b6396
Sylvestre Ho <shotamchay@merethis.com>
2010-12-10 15:34:28 +0000

    Christoph Ziemann appears twice ;)
    git-svn-id: http://svn.centreon.com/trunk/centreon@11277 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/Administration/about/about.php
+++ www/include/Administration/about/about.php
@@ -3,44 +3,44 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 	if (!isset($oreon))
 		exit();
 
 	$DBRESULT =& $pearDB->query("SELECT `value` FROM `informations` WHERE `key` = 'version' LIMIT 1");
 	$release =& $DBRESULT->fetchRow();
-	
+
 ?><center>
 <div style="width:100%;align:center;">
 	<div style="width:700px;padding:20px;background-color:#FFFFFF;border:1px #CDCDCD solid;-moz-border-radius:4px;">
@@ -53,62 +53,62 @@
 		<br />
 		<table>
 			<tr>
-				<td width="25">&nbsp;</td>	
+				<td width="25">&nbsp;</td>
 				<td>-&nbsp;<a href="mailto:jmathis@centreon.com">Julien Mathis</a></td>
 			</tr>
 			<tr>
-				<td width="25">&nbsp;</td>	
+				<td width="25">&nbsp;</td>
 				<td>-&nbsp;<a href="mailto:rlemerlus@centreon.com">Romain Le Merlus</a></td>
 			</tr>
 		</table>
 		<br><br><h3><b><?php echo _("Developers"); ?> :</b></h3><br />
 		<table>
 			<tr>
-				<td width="25">&nbsp;</td>	
+				<td width="25">&nbsp;</td>
 				<td>Maximilien Bersoult</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Romain Bertholon</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Nicolas Cordier</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Damien Duponchelle</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Cedrick Facon</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Nikolaus Filus</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Sylvestre Ho</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Matthieu Kermagoret</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Laurent Pinsivy</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Mathavarajan Sugumaran</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Cedric Temple</td>
 			</tr>
-			<tr>	
-				<td>&nbsp;</td>	
+			<tr>
+				<td>&nbsp;</td>
 				<td>Guillaume Watteeux</td>
 			</tr>
 		</table>
@@ -131,7 +131,7 @@
 				<tr>
 					<td>Vincent Carpentier</td>
 					<td>Christoph Ziemann</td>
-				</tr>				
+				</tr>
 				<tr>
 					<td>Jean Marc Grisar</td>
 					<td>Florin Grosu</td>
@@ -174,7 +174,7 @@
 				</tr>
 				<tr>
 					<td>Massimiliano Ziccardi</td>
-					<td>Christoph Ziemann</td>
+					<td></td>
 				</tr>
 				<tr>
 					<td colspan="2"><?php print _("And the others..."); ?></td>
