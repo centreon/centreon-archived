3c318aa5b6a1faa372ad747b7d900f7f38e8b7ac
Julien Mathis <jmathis@merethis.com>
2011-01-07 18:29:20 +0000

    improve GMT class
    git-svn-id: http://svn.centreon.com/trunk/centreon@11466 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonGMT.class.php
+++ www/class/centreonGMT.class.php
@@ -3,51 +3,51 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 class CentreonGMT{
 
 	var $listGTM;
 	var $myGMT;
 	var $use;
-	
+
 	function CentreonGMT($DB){
 		/*
 		 * Define Table of GMT line
 		 */
 		$this->listGTM = array(null => null);
-		
+
 		$this->listGTM['-12'] = -12;
 		$this->listGTM['-11'] = -11;
 		$this->listGTM['-10'] = -10;
@@ -73,37 +73,37 @@ class CentreonGMT{
 		$this->listGTM['10'] = 10;
 		$this->listGTM['11'] = 11;
 		$this->listGTM['12'] = 12;
-		
+
 		/*
 		 * Flag activ / inactiv
 		 */
 		$this->use = $this->checkGMTStatus($DB);
 	}
-	
+
 	function checkGMTStatus($DB) {
 		global $pearDB;
-		
+
 		if (!isset($pearDB) && isset($DB))
 			$pearDB = $DB;
-		
+
 		$DBRESULT =& $pearDB->query("SELECT * FROM options WHERE `key` = 'enable_gmt'");
 		$result =& $DBRESULT->fetchRow();
 		return ($result["value"]);
 	}
-	
+
 	function used(){
 		return $this->use;
 	}
-	
+
 	function setMyGMT($value){
 		if (!isset($value))
-			$this->myGMT = $value;	
+			$this->myGMT = $value;
 	}
-	
+
 	function getGMTList() {
 		return $this->listGTM;
 	}
-	
+
 	function getMyGMT(){
 		return $this->myGMT;
 	}
@@ -114,73 +114,77 @@ class CentreonGMT{
 			$gmt = "+$gmt";
 		return $gmt;
 	}
-	
+
 	function getDate($format, $date, $gmt = NULL) {
+		if ($date == "N/A") {
+			return $date;
+		}
 		/*
 		 * Specify special GMT
 		 */
-		if (!isset($gmt))
+		if (!isset($gmt)) {
 			$gmt = $this->myGMT;
-		
+		}
+
 		if ($this->use) {
 			if (isset($date) && isset($gmt)) {
 				$date += $gmt * 60 * 60;
 				return date($format, $date);
 			} else {
-				return "";	
+				return "";
 			}
 		} else {
 			return date($format, $date);
 		}
 	}
-	
+
 	function getUTCDate($date, $gmt = NULL) {
 		/*
 		 * Specify special GMT
 		 */
 		if (!isset($gmt))
 			$gmt = $this->myGMT;
-		
+
 		if ($this->use) {
 			if (isset($date) && isset($gmt)) {
 				$date += -1 * ($gmt * 60 * 60);
 				return $date;
 			} else {
-				return "";	
-			}			
+				return "";
+			}
 		} else {
 			return $date;
 		}
 	}
-	
+
 	function getDelaySecondsForRRD($gmt) {
 		$str = "";
 		if ($gmt) {
 			if ($gmt > 0)
 				$str .= "+";
 		} else {
-			return "";	
+			return "";
 		}
 	}
-	
+
 	function getMyGMTFromSession($sid = NULL, $DB){
 		global $pearDB;
-		
+
 		if (!isset($pearDB) && isset($DB))
 			$pearDB = $DB;
-		
+
 		if (!isset($sid))
 			return 0;
-		
+
 		$DBRESULT =& $pearDB->query("SELECT `contact_location` FROM `contact`, `session` " .
 									"WHERE `session`.`user_id` = `contact`.`contact_id` " .
 									"AND `session_id` = '$sid' LIMIT 1");
-		if (PEAR::isError($DBRESULT)) {		
+		if (PEAR::isError($DBRESULT)) {
 			$this->myGMT = 0;
 		}
 		$info =& $DBRESULT->fetchRow();
 		$DBRESULT->free();
 		$this->myGMT = $info["contact_location"];
 	}
-	
+
 }
