d20597bbc77d55811b026aec846a7af306be87cd
Julien Mathis <jmathis@merethis.com>
2011-01-14 17:55:42 +0000

    fix #2135
    git-svn-id: http://svn.centreon.com/trunk/centreon@11499 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.1.10_to_2.2.0-b1.sql
+++ www/install/sql/centreon/Update-DB-2.1.10_to_2.2.0-b1.sql
@@ -106,9 +106,10 @@ ALTER TABLE `cfg_nagios` ADD `check_for_orphaned_hosts` enum('0','1','2') defaul
 ALTER TABLE `cfg_nagios` ADD `external_command_buffer_slots` INT(11) DEFAULT NULL ;
 ALTER TABLE `cfg_nagios` CHANGE service_reaper_frequency check_result_reaper_frequency INT(11);
 ALTER TABLE `cfg_nagios` CHANGE `translate_passive_host_checks` `translate_passive_host_checks` INT(11) DEFAULT NULL ;
-ALTER TABLE `cfg_nagios` ADD use_aggressive_host_checking enum('0','1','2') default NULL;
+ALTER TABLE `cfg_nagios` ADD use_aggressive_host_checking enum('0','1','2') default 0;
 
 ALTER TABLE `cfg_nagios` DROP COLUMN aggregate_status_updates;
+ALTER TABLE `cfg_nagios` DROP COLUMN use_agressive_host_checking;
 
 CREATE TABLE `command_arg_description` (
 	`cmd_id` INT( 11 ) NOT NULL ,
