ebb7036db43968b5b07484832e4f0766c36e4c51
Julien Mathis <jmathis@merethis.com>
2011-01-26 10:29:52 +0000

    reduce reactivity time for poller status detection
    git-svn-id: http://svn.centreon.com/trunk/centreon@11522 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/monitoring/status/TopCounter/xml/ndo/statusCounter.php
+++ www/include/monitoring/status/TopCounter/xml/ndo/statusCounter.php
@@ -212,14 +212,14 @@
 		/*
 		 * Running
 		 */
-		if ($status != 2 && $ndo["is_currently_running"] == 0 && (time() - $ndo["last_update"] >= $timeUnit * 2)) {
+		if ($status != 2 && $ndo["is_currently_running"] == 0 && (time() - $ndo["last_update"] >= $timeUnit / 5)) {
 			$status = 1;
 			if ($pollerListInError != "") {
 				$pollerListInError .= ", ";
 			}
 			$pollerListInError .= $ndo["instance_name"];
 		}
-		if ($ndo["is_currently_running"] == 0 && (time() - $ndo["last_update"] >= $timeUnit * 4)) {
+		if ($ndo["is_currently_running"] == 0 && (time() - $ndo["last_update"] >= $timeUnit / 4)) {
 			$status = 2;
 			if ($pollerListInError != "") {
 				$pollerListInError .= ", ";
@@ -242,7 +242,7 @@
             }
             $inactivInstance .= $ndo["instance_name"]." [".(time() - $ndo["last_update"])."s / ".($timeUnit * 2)."s]";
 		}
-		
+
 	}
 	$DBRESULT->free();
 	$error = "Pollers $pollerListInError not running.";
