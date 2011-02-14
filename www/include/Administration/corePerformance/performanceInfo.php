31c4dd5107a04ad68e3a309e699cc22b701374d0
Julien Mathis <jmathis@merethis.com>
2011-02-14 16:07:27 +0000

    * Change copyight * add svn:keywords
    git-svn-id: http://svn.centreon.com/trunk/centreon@11682 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/Administration/corePerformance/performanceInfo.php
+++ www/include/Administration/corePerformance/performanceInfo.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2005-2010 MERETHIS
+ * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
@@ -31,8 +31,8 @@
  *
  * For more information : contact@centreon.com
  *
- * SVN : $URL: http://svn.centreon.com/trunk/centreon/www/include/Administration/SchedulePerformance/performanceInfo.php $
- * SVN : $Id: performanceInfo.php 10025 2010-02-19 14:29:02Z jmathis $
+ * SVN : $URL$
+ * SVN : $Id$
  *
  */
 
