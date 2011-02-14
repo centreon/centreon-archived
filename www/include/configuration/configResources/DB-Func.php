348d201531db8c64668121846c5c96f51cecd05b
Sylvestre Ho <shotamchay@merethis.com>
2011-02-14 15:49:55 +0000

    removes htmlentities on insert
    git-svn-id: http://svn.centreon.com/trunk/centreon@11681 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configResources/DB-Func.php
+++ www/include/configuration/configResources/DB-Func.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2010 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
@@ -44,13 +44,13 @@
 		$id = NULL;
 		if (isset($form))
 			$id = $form->getSubmitValue('resource_id');
-		$DBRESULT = $pearDB->query("SELECT resource_name, resource_id FROM cfg_resource WHERE resource_name = '".htmlentities($name, ENT_QUOTES, "UTF-8")."'");
+		$DBRESULT = $pearDB->query("SELECT resource_name, resource_id FROM cfg_resource WHERE resource_name = '".$pearDB->escape($name)."'");
 		$res = $DBRESULT->fetchRow();
 		#Modif case
-		if ($DBRESULT->numRows() >= 1 && $res["resource_id"] == $id)	
+		if ($DBRESULT->numRows() >= 1 && $res["resource_id"] == $id)
 			return true;
 		#Duplicate entry
-		else if ($DBRESULT->numRows() >= 1 && $res["resource_id"] != $id)	
+		else if ($DBRESULT->numRows() >= 1 && $res["resource_id"] != $id)
 			return false;
 		else
 			return true;
@@ -62,19 +62,19 @@
 			$DBRESULT = $pearDB->query("DELETE FROM cfg_resource WHERE resource_id = '".$key."'");
 		}
 	}
-	
+
 	function enableResourceInDB ($resource_id = null)	{
 		if (!$resource_id) exit();
 		global $pearDB;
-		$DBRESULT = $pearDB->query("UPDATE cfg_resource SET resource_activate = '1' WHERE resource_id = '".$resource_id."'");		
+		$DBRESULT = $pearDB->query("UPDATE cfg_resource SET resource_activate = '1' WHERE resource_id = '".$resource_id."'");
 	}
-	
+
 	function disableResourceInDB ($resource_id = null)	{
 		if (!$resource_id) return;
 		global $pearDB;
 		$DBRESULT = $pearDB->query("UPDATE cfg_resource SET resource_activate = '0' WHERE resource_id = '".$resource_id."'");
 	}
-	
+
 	function multipleResourceInDB ($DBRESULT = array(), $nbrDup = array())	{
 		foreach($DBRESULT as $key=>$value)	{
 			global $pearDB;
@@ -93,31 +93,31 @@
 			}
 		}
 	}
-	
+
 	function updateResourceInDB ($resource_id = NULL)	{
 		if (!$resource_id) return;
 		updateResource($resource_id);
 	}
-	
+
 	function updateResource($resource_id)	{
 		if (!$resource_id) return;
 		global $form, $pearDB;
 		$ret = array();
 		$ret = $form->getSubmitValues();
 		$rq = "UPDATE cfg_resource ";
-		$rq .= "SET resource_name = '".htmlentities($ret["resource_name"], ENT_QUOTES, "UTF-8")."', " .
-				"resource_line = '".htmlentities($ret["resource_line"], ENT_QUOTES, "UTF-8")."', " .
-				"resource_comment= '".htmlentities($ret["resource_comment"], ENT_QUOTES, "UTF-8")."', " .
+		$rq .= "SET resource_name = '".$pearDB->escape($ret["resource_name"])."', " .
+				"resource_line = '".$pearDB->escpae($ret["resource_line"])."', " .
+				"resource_comment= '".$pearDB->escape($ret["resource_comment"])."', " .
 				"resource_activate= '".$ret["resource_activate"]["resource_activate"]."' " .
 				"WHERE resource_id = '".$resource_id."'";
 		$DBRESULT = $pearDB->query($rq);
 	}
-	
+
 	function insertResourceInDB ()	{
 		$resource_id = insertResource();
 		return ($resource_id);
 	}
-	
+
 	function insertResource($ret = array())	{
 		global $form, $pearDB;
 		if (!count($ret))
@@ -125,9 +125,9 @@
 		$rq = "INSERT INTO cfg_resource ";
 		$rq .= "(resource_name, resource_line, resource_comment, resource_activate) ";
 		$rq .= "VALUES (";
-		isset($ret["resource_name"]) && $ret["resource_name"] != NULL ? $rq .= "'".htmlentities($ret["resource_name"], ENT_QUOTES, "UTF-8")."', " : $rq .= "NULL, ";
-		isset($ret["resource_line"]) && $ret["resource_line"] != NULL ? $rq .= "'".htmlentities($ret["resource_line"], ENT_QUOTES, "UTF-8")."', " : $rq .= "NULL, ";
-		isset($ret["resource_comment"]) && $ret["resource_comment"] != NULL ? $rq .= "'".htmlentities($ret["resource_comment"], ENT_QUOTES, "UTF-8")."', " : $rq .= "NULL, ";
+		isset($ret["resource_name"]) && $ret["resource_name"] != NULL ? $rq .= "'".$pearDB->escape($ret["resource_name"])."', " : $rq .= "NULL, ";
+		isset($ret["resource_line"]) && $ret["resource_line"] != NULL ? $rq .= "'".$pearDB->escape($ret["resource_line"])."', " : $rq .= "NULL, ";
+		isset($ret["resource_comment"]) && $ret["resource_comment"] != NULL ? $rq .= "'".$pearDB->escpae($ret["resource_comment"])."', " : $rq .= "NULL, ";
 		isset($ret["resource_activate"]["resource_activate"]) && $ret["resource_activate"]["resource_activate"] != NULL ? $rq .= "'".$ret["resource_activate"]["resource_activate"]."'" : $rq .= "NULL";
 		$rq .= ")";
 		$DBRESULT = $pearDB->query($rq);
