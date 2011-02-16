b082f95cfd9fa7a7b5c26c5959538f90c20279c4
Julien Mathis <jmathis@merethis.com>
2011-02-16 23:23:19 +0000

    Add help file
    git-svn-id: http://svn.centreon.com/trunk/centreon@11706 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/include/configuration/configDowntime/help.php
@@ -0,0 +1,10 @@
+<?php
+$help = array();
+$help["mc_update"] = dgettext("help", "Choose the update mode for the below field: incremental adds the selected values, replacement overwrites the original values.");
+
+/*
+ * Host Configuration
+ */
+$help["downtime_name"] = dgettext("help", "The name of the recurrent downtime rule.");
+
+?>
