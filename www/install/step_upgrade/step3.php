0a6c6214c866380edda3812fe8b79cba414e08a0
Julien Mathis <jmathis@merethis.com>
2011-02-17 08:30:52 +0000

    git-svn-id: http://svn.centreon.com/trunk/centreon@11711 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/step_upgrade/step3.php
+++ www/install/step_upgrade/step3.php
@@ -3,50 +3,50 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	include_once $centreon_path . "/www/class/centreonDB.class.php";
-    
+
 	$pearDB 	= new CentreonDB();
 	$pearDBO 	= new CentreonDB("centstorage");
 	$pearDBndo 	= new CentreonDB("ndo");
-		
+
 	$DBRESULT = $pearDB->query("SELECT `value` FROM `informations` WHERE `key` = 'version'");
 	$version = $DBRESULT->fetchRow();
 
 	$DBRESULT = $pearDB->query("SELECT db_name, db_prefix, db_user, db_pass, db_host FROM cfg_ndo2db LIMIT 1");
-	if (PEAR::isError($DBRESULT))
+	if (PEAR::isError($DBRESULT)) {
 		print "DB Error : ".$DBRESULT->getDebugInfo()."<br />";
 	$confNDO = $DBRESULT->fetchRow();
 	$DBRESULT->free();
@@ -115,7 +115,7 @@ aff_header("Centreon Upgrade Wizard", "Verifying Configuration", 3);	?>
           		echo '<b><span class="go">OK</font></b>';
 			else {
 				echo '<b><span class="warning">Warning: xmlwriter.so not loaded in php.ini</font></b>';
-				$return_false = 1;	
+				$return_false = 1;
 			}	?>
 		</td>
   </tr>
@@ -137,7 +137,7 @@ aff_header("Centreon Upgrade Wizard", "Verifying Configuration", 3);	?>
           		echo '<b><span class="go">OK</font></b>';
 			else {
 				echo '<b><span class="stop">Critical: php-posix functions are not installed</font></b>';
-				$return_false = 1;	
+				$return_false = 1;
 			}?>
 		</td>
   </tr>
