82330b750aca32eb7850d1bee2cf5340ea8b7500
Julien Mathis <jmathis@merethis.com>
2011-02-17 12:53:12 +0000

    * disable full purge of data_bin table * remove the display of the number data by metrics
    git-svn-id: http://svn.centreon.com/trunk/centreon@11726 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/centStorage/viewMetrics.php
+++ www/include/options/centStorage/viewMetrics.php
@@ -3,48 +3,48 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
 		exit();
-		
+
 	if ((isset($_POST["o1"]) && $_POST["o1"]) || (isset($_POST["o2"]) && $_POST["o2"])){
 		if ((defined($_POST["o1"]) && $_POST["o1"] == "rg") || (defined($_POST["o2"]) && $_POST["o2"] == "rg")){
 			$selected = $_POST["select"];
 			foreach ($selected as $key => $value){
 				$DBRESULT = $pearDBO->query("UPDATE index_data SET `must_be_rebuild` = '1' WHERE id = '".$key."'");
-			}	
+			}
 		} else if ((defined($_POST["o1"]) && $_POST["o1"] == "nrg") || (defined($_POST["o2"]) && $_POST["o2"] == "nrg")){
 			$selected = $_POST["select"];
 			foreach ($selected as $key => $value){
@@ -55,7 +55,6 @@
 			foreach ($selected as $key => $value){
 				$DBRESULT = $pearDBO->query("SELECT * FROM metrics WHERE `metric_id` = '".$key."'");
 				while ($metrics = $DBRESULT->fetchRow()){
-					$DBRESULT2 = $pearDBO->query("DELETE FROM data_bin WHERE `id_metric` = '".$metrics['metric_id']."'");
 					$DBRESULT2 = $pearDBO->query("DELETE FROM metrics WHERE `metric_id` = '".$metrics['metric_id']."'");
 				}
 			}
@@ -81,26 +80,23 @@
 			}
 		}
 	}
-		
+
 	$search_string = "";
 	if (isset($search) && $search)
 		$search_string = " WHERE `host_name` LIKE '%$search%' OR `service_description` LIKE '%$search%'";
-	
+
 	$DBRESULT = $pearDBO->query("SELECT COUNT(*) FROM metrics WHERE index_id = '".$_GET["index_id"]."'");
 	$tmp = $DBRESULT->fetchRow();
 	$rows = $tmp["COUNT(*)"];
-			
+
 	$tab_class = array("0" => "list_one", "1" => "list_two");
-	$storage_type = array(0 => "RRDTool", 2 => "RRDTool & MySQL");	
-	$yesOrNo = array(NULL => "No", 0 => "No", 1 => "Yes", 2 => "Rebuilding");	
-	
+	$storage_type = array(0 => "RRDTool", 2 => "RRDTool & MySQL");
+	$yesOrNo = array(NULL => "No", 0 => "No", 1 => "Yes", 2 => "Rebuilding");
+
 	$DBRESULT2 = $pearDBO->query("SELECT * FROM metrics WHERE index_id = '".$_GET["index_id"]."'");
 	unset($data);
 	for ($im = 0;$metrics = $DBRESULT2->fetchRow();$im++){
 		$metric = array();
-		$DBRESULT3 = $pearDBO->query("SELECT COUNT(*) FROM data_bin WHERE id_metric = '".$metrics["metric_id"]."'");
-		$nb_value = $DBRESULT3->fetchRow();
-		$metric["nb"] = $nb_value["COUNT(*)"];	
 		$metric["metric_id"] = $metrics["metric_id"];
 		$metric["class"] = $tab_class[$im % 2];
 		$metric["metric_name"] = $metrics["metric_name"];
@@ -124,8 +120,8 @@
 	$tpl = initSmartyTpl($path, $tpl);
 
 	$form = new HTML_QuickForm('form', 'POST', "?p=".$p);
-	
-	## Toolbar select 
+
+	## Toolbar select
 
 	?>
 	<script type="text/javascript">
@@ -153,7 +149,7 @@
 				"");
 	$form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "rg"=>_("Rebuild RRD Database"), "nrg"=>_("Stop rebuilding RRD Databases"), "ed"=>_("Empty all Service Data"), "hg"=>_("Hide graphs of selected Services"), "nhg"=>_("Stop hiding graphs of selected Services"), "lk"=>_("Lock Services"), "nlk"=>_("Unlock Services")), $attrs1);
 	$form->setDefaults(array('o1' => NULL));
-		
+
 	$attrs2 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1) {" .
@@ -181,7 +177,7 @@
 	$o2 = $form->getElement('o2');
 	$o2->setValue(NULL);
 	$o2->setSelected(NULL);
-	
+
 	$tpl->assign('limit', $limit);
 
 	$tpl->assign("p", $p);
@@ -197,11 +193,11 @@
 	$tpl->assign("NumberOfValues", _("Number of values"));
 	$tpl->assign("Hidden", _("Hidden"));
 	$tpl->assign("Locked", _("Locked"));
-	
+
 	$tpl->assign("data", $data);
-	
+
 	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);	
+	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
     $tpl->display("viewMetrics.ihtml");
 ?>
