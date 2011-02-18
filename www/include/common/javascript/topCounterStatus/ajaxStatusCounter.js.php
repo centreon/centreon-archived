fa813f7ce47fcb1ef6d7aab2ac5dd04f018b0bf8
Julien Mathis <jmathis@merethis.com>
2011-02-18 18:16:59 +0000

    Add dynamic path for ajax engine. Broker integration
    git-svn-id: http://svn.centreon.com/trunk/centreon@11741 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/common/javascript/topCounterStatus/ajaxStatusCounter.js.php
+++ www/include/common/javascript/topCounterStatus/ajaxStatusCounter.js.php
@@ -1,8 +1,8 @@
 /*
  * Centreon is developped with GPL Licence 2.0 :
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
- * Developped by : Julien Mathis - Romain Le Merlus 
- * 
+ * Developped by : Julien Mathis - Romain Le Merlus
+ *
  * The Software is provided to you AS IS and WITH ALL FAULTS.
  * Centreon makes no representation and gives no warranty whatsoever,
  * whether express or implied, and without limitation, with regard to the quality,
@@ -10,13 +10,28 @@
  * In no event will Centreon be liable for any direct, indirect, punitive, special,
  * incidental or consequential damages however they may arise and even if Centreon has
  * been previously advised of the possibility of such damages.
- * 
+ *
  * For information : contact@centreon.com
  */
+<?php
 
+	require_once "@CENTREON_ETC@/centreon.conf.php";
+
+    require_once $centreon_path . "www/class/centreonSession.class.php";
+    require_once $centreon_path . "www/class/centreon.class.php";
+
+	session_start();
+
+    if (!isset($_SESSION['centreon'])) {
+		exit();
+	}
+
+	$centreon = $_SESSION['centreon'];
+
+?>
 // JavaScript Document
 
-var _adrrsearchC = "./include/monitoring/status/TopCounter/xml/ndo/statusCounter.php";
+var _adrrsearchC = "./include/monitoring/status/TopCounter/xml/<?php global $centreon; print $centreon->broker->getBroker(); ?>/statusCounter.php";
 
 function getXhrC(){
 	if (window.XMLHttpRequest) {
@@ -29,7 +44,7 @@ function getXhrC(){
            	} catch (e) {
                 var xhrC = new ActiveXObject("Microsoft.XMLHTTP");
            	}
-	} else { 
+	} else {
 		// XMLHttpRequest non support2 par le navigateur
 		alert("Your Browser doesn't support XMLHTTPRequest object...");
 	   	var xhrC = false;
@@ -39,17 +54,17 @@ function getXhrC(){
 
 function reloadStatusCounter(_relaod_time, _sid){
 
-	_form=document.getElementById('AjaxBankBasic');		       
+	_form=document.getElementById('AjaxBankBasic');
 	_version=_form.version.value;
 
 	var xhrC = getXhrC();
 	// On defini ce qu'on va faire quand on aura la reponse
 	xhrC.onreadystatechange = function()
-	{	
+	{
 		// On ne fait quelque chose que si on a tout recu et que le serveur est ok
-		if (xhrC && xhrC.readyState == 4 && xhrC.status == 200 && xhrC.responseXML) {		
+		if (xhrC && xhrC.readyState == 4 && xhrC.status == 200 && xhrC.responseXML) {
 			reponseC = xhrC.responseXML.documentElement;
-			
+
 			// Get stats
 			var stats = reponseC.getElementsByTagName("s");
 			for (var i = 0 ; i < stats.length ; i++) {
@@ -68,7 +83,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				var _statistic_host_down 		= stat.getElementsByTagName("d")[0].firstChild.nodeValue;
 				var _statistic_host_unreachable = stat.getElementsByTagName("un2")[0].firstChild.nodeValue;
 				var _statistic_host_pending 	= stat.getElementsByTagName("p2")[0].firstChild.nodeValue;
-	
+
 				/*
 					 * host
 					 */
@@ -85,29 +100,29 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkHostup.href = 'main.php?p=20103&o=h&search=';
 					_linkHostup.appendChild(_text_host_up);
 					document.getElementById('host_up').appendChild(_linkHostup);
-	
+
 					document.getElementById('host_down').innerHTML = '';
 					var _text_host_down = document.createTextNode(_statistic_host_down);
 					var _linkHostProblem = document.createElement("a");
 				  	_linkHostProblem.href = 'main.php?p=20103&o=hpb&search=';
 					_linkHostProblem.appendChild(_text_host_down);
 					document.getElementById('host_down').appendChild(_linkHostProblem);
-	
+
 					document.getElementById('host_unreachable').innerHTML = '';//_statistic_host_unreachable;
 					var _text_host_unreachable = document.createTextNode(_statistic_host_unreachable);
 					var _linkHostunreachable = document.createElement("a");
 				  	_linkHostunreachable.href = 'main.php?p=20103&o=hpb&search=';
 					_linkHostunreachable.appendChild(_text_host_unreachable);
 					document.getElementById('host_unreachable').appendChild(_linkHostunreachable);
-	
+
 					document.getElementById('host_pending').innerHTML = '';//_statistic_host_pending;
 					var _text_host_pending = document.createTextNode(_statistic_host_pending);
 					var _linkHostpending = document.createElement("a");
 				  	_linkHostpending.href = 'main.php?p=20103&o=hpb&search=';
 					_linkHostpending.appendChild(_text_host_pending);
 					document.getElementById('host_pending').appendChild(_linkHostpending);
-	
-					/* 
+
+					/*
 					 * svc
 					 */
 					document.getElementById('services').innerHTML = '';
@@ -116,7 +131,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkservice_total.href = 'main.php?p=20201&o=svc&search=';
 					_linkservice_total.appendChild(_text_total_service);
 					document.getElementById('services').appendChild(_linkservice_total);
-					
+
 					// Ok service Stats
 					document.getElementById('service_ok').innerHTML = '';
 					var _text_service_ok = document.createTextNode(_statistic_service_ok);
@@ -124,7 +139,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkservice_ok.href = 'main.php?p=2020101&o=svc_ok&search=';
 					_linkservice_ok.appendChild(_text_service_ok);
 					document.getElementById('service_ok').appendChild(_linkservice_ok);
-					
+
 					// Warning service stats
 					document.getElementById('service_warning').innerHTML = '';
 					var _text_service_warning = document.createTextNode(_statistic_service_warningU+"/"+_statistic_service_warning);
@@ -132,7 +147,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkservice_warning.href = 'main.php?p=2020102&o=svc_warning&search=';
 					_linkservice_warning.appendChild(_text_service_warning);
 					document.getElementById('service_warning').appendChild(_linkservice_warning);
-					
+
 					// Critcal Service Stats
 					document.getElementById('service_critical').innerHTML = '';
 					var _text_service_critical = document.createTextNode(_statistic_service_criticalU+"/"+_statistic_service_critical);
@@ -140,7 +155,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkservice_critical.href = 'main.php?p=2020103&o=svc_critical&search=';
 					_linkservice_critical.appendChild(_text_service_critical);
 					document.getElementById('service_critical').appendChild(_linkservice_critical);
-										
+
 					// Unknown Service Stats
 					document.getElementById('service_unknown').innerHTML = '';
 					var _text_service_unknown = document.createTextNode(_statistic_service_unknownU+"/"+_statistic_service_unknown);
@@ -148,7 +163,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 				  	_linkservice_unknown.href = 'main.php?p=2020104&o=svc_unknown&search=';
 					_linkservice_unknown.appendChild(_text_service_unknown);
 					document.getElementById('service_unknown').appendChild(_linkservice_unknown);
-					
+
 					// Pending Services Stats
 					document.getElementById('service_pending').innerHTML = '';
 					var _text_service_pending = document.createTextNode(_statistic_service_pending);
@@ -157,7 +172,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 					_linkservice_pending.appendChild(_text_service_pending);
 					document.getElementById('service_pending').appendChild(_linkservice_pending);
 			}
-			
+
 			// Get Poller Statistics
 			if (document.getElementById('img_pollingState')) {
 				var statPoller = reponseC.getElementsByTagName("m");
@@ -169,11 +184,11 @@ function reloadStatusCounter(_relaod_time, _sid){
 					var _error_pollingState = statp.getElementsByTagName("errorPstt")[0].firstChild.nodeValue;
 					var _error_latency = statp.getElementsByTagName("errorLtc")[0].firstChild.nodeValue;
 					var _error_activity = statp.getElementsByTagName("errorAct")[0].firstChild.nodeValue;
-					
+
 					document.getElementById("img_pollingState").title = _error_pollingState;
 					document.getElementById("img_latency").title = _error_latency;
 					document.getElementById("img_activity").title = _error_activity;
-					
+
 					if (_statistic_latency == 0) {
 						document.getElementById("latency").style.backgroundColor = "";
 					} else if (_statistic_latency == 1) {
@@ -181,7 +196,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 					} else if (_statistic_latency == 2) {
 						document.getElementById("latency").style.backgroundColor = "red";
 					}
-					
+
 					if (_statistic_activity == 0) {
 						document.getElementById("activity").style.backgroundColor = "";
 					} else if (_statistic_activity == 1) {
@@ -189,7 +204,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 					} else if (_statistic_activity == 2) {
 						document.getElementById("activity").style.backgroundColor = "red";
 					}
-	
+
 					if (_statistic_pollingState == 0) {
 						document.getElementById("pollingState").style.backgroundColor = "";
 					} else if (_statistic_pollingState == 1) {
@@ -197,7 +212,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 					} else if (_statistic_pollingState == 2) {
 						document.getElementById("pollingState").style.backgroundColor = "red";
 					}
-					
+
 					//alert("Latence : "+_statistic_latency);
 				}
 			}
@@ -208,7 +223,7 @@ function reloadStatusCounter(_relaod_time, _sid){
 	xhrC.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
 	xhrC.send("sid="+_sid+"&version="+_version+"&session_expire="+(_relaod_time/1000));
 	//setQuickTip();
-	setTimeout('reloadStatusCounter("'+ _relaod_time +'","'+ _sid +'")', _relaod_time);	
+	setTimeout('reloadStatusCounter("'+ _relaod_time +'","'+ _sid +'")', _relaod_time);
 }
 
 function setQuickTip()
