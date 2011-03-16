b93cd905ef9d67750cc58fba66ae3e15ae476897
Julien Mathis <jmathis@merethis.com>
2011-03-16 10:12:40 +0000

    not ready for beta 5 :)
    git-svn-id: http://svn.centreon.com/trunk/centreon@11840 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.3.0-b3_to_2.3.0-b4.sql
+++ www/install/sql/centreon/Update-DB-2.3.0-b3_to_2.3.0-b4.sql
@@ -22,4 +22,45 @@ DELETE FROM topology_JS WHERE id_page IN (
 '2020103',
 '2020104');
 
+
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_ack_svc', '1');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_dwt_duration', '3600');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_ack_active_checks', '1');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_ack_persistent', '1');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_ack_notify', '0');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_ack_sticky', '1');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_dwt_fixed', '1');
+INSERT INTO `options` (`key`, `value`) VALUES('monitoring_dwt_svc', '1');
+
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Centreon-Broker', NULL, 609, NULL, NULL, 11, NULL, NULL, '0', '0', '1', NULL, NULL, NULL);
+INSERT INTO `topology` (`topology_id`, `topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`) VALUES(NULL, 'Configuration', './img/icones/16x16/text_code_colored.gif', 609, 60904, 40, 11, './include/configuration/configCentreonBroker/centreon-broker.php', NULL, '0', '0', '1', NULL, NULL, NULL);
+INSERT INTO `topology_JS` (`id_page`, `o`, `PathName_js`, `Init`) VALUES (60904, 'c', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_page`, `o`, `PathName_js`, `Init`) VALUES (60904, 'a', './include/common/javascript/changetab.js', 'initChangeTab');
+INSERT INTO `topology_JS` (`id_page`, `o`, `PathName_js`, `Init`) VALUES (60904, 'w', './include/common/javascript/changetab.js', 'initChangeTab');
+
+-- Tables for config Centreon-broker
+CREATE TABLE cfg_centreonbroker (
+	config_id INT NOT NULL AUTO_INCREMENT,
+	config_name VARCHAR(100) NOT NULL,
+	config_activate ENUM('0', '1') DEFAULT '0',
+	ns_nagios_server INT NOT NULL,
+	PRIMARY KEY (`config_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE cfg_centreonbroker_info (
+	config_id INT NOT NULL,
+	config_key VARCHAR(50) NOT NULL,
+	config_value VARCHAR(255) NOT NULL,
+	config_group VARCHAR (50) NOT NULL,
+	config_group_id INT DEFAULT NULL,
+	KEY cfg_centreonbroker_info_idx01 (config_id),
+	KEY cfg_centreonbroker_info_idx02 (config_id, config_group)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `cfg_centreonbroker_info`
+  ADD CONSTRAINT `cfg_centreonbroker_info_ibfk_01` FOREIGN KEY (`config_id`) REFERENCES `cfg_centreonbroker` (`config_id`) ON DELETE CASCADE;
+  
+-- Add column for Centreon Broker configuration path in pollers
+ALTER TABLE `nagios_server` ADD COLUMN `centreonbroker_cfg_path` VARCHAR(255) AFTER `nagios_perfdata`;
+
 UPDATE `informations` SET `value` = '2.3.0-b4' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.3.0-b3' LIMIT 1;
