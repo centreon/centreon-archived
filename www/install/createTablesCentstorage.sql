08d303f82abd272aa64b7f303df0dd8f9962052e
Julien Mathis <jmathis@merethis.com>
2011-03-17 18:08:25 +0000

    move centreon_acl table into cenrtstorage DB
    git-svn-id: http://svn.centreon.com/trunk/centreon@11868 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/createTablesCentstorage.sql
+++ www/install/createTablesCentstorage.sql
@@ -36,6 +36,27 @@ CREATE TABLE IF NOT EXISTS `config` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `centreon_acl`
+--
+
+CREATE TABLE IF NOT EXISTS `centreon_acl` (
+  `id` int(11) NOT NULL auto_increment,
+  `host_id` int(11) default NULL,
+  `host_name` varchar(255) default NULL,
+  `service_id` int(11) default NULL,
+  `service_description` varchar(255) default NULL,
+  `group_id` int(11) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `host_name` (`host_name`),
+  KEY `service_description` (`service_description`),
+  KEY `group_id_by_name` (`host_name`(70),`service_description`(120),`group_id`),
+  KEY `group_id_by_id` (`host_id`,`service_id`,`group_id`),
+  KEY `group_id_for_host` (`host_name`,`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `data_bin`
 --
 
