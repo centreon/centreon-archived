43acf5d636c4c228de32392f96355f89e63945e6
Julien Mathis <jmathis@merethis.com>
2011-04-05 17:34:06 +0000

    fix syntax problem with preg_split
    git-svn-id: http://svn.centreon.com/trunk/centreon@12026 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/doc/getImage.php
+++ www/include/doc/getImage.php
@@ -3,39 +3,39 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (function_exists("filter_get")) {
 		$img = filter_var($_GET["img"]);
 		$lang = filter_var($_GET["lang"]);
@@ -46,14 +46,14 @@
 		$version = htmlentities($_GET["version"], ENT_QUOTES, "UTF-8");
 	}
 
-	$tab_images = preg_split("/", $img);
+	$tab_images = preg_split("/\//", $img);
 	foreach ($tab_images as $value)
 		$image = $value;
-	
+
 	header("Content-Type: image/png");
-	
+
 	if (file_exists("../../../doc/".$version."/".$lang."/images/".$image)){
-		$img = fopen("../../../doc/".$version."/".$lang."/images/".$image, "r");	
+		$img = fopen("../../../doc/".$version."/".$lang."/images/".$image, "r");
 		if (isset($img) && $img)
 			while ($line = fgets($img))
 				print $line;
--- www/include/doc/index.php
+++ www/include/doc/index.php
@@ -3,46 +3,46 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
-		exit(); 
+		exit();
 
 	/*
 	 * Filter Args
 	 */
-		 
+
 	if (function_exists("filter_var")) {
 		$page = filter_var($_GET["page"], FILTER_SANITIZE_SPECIAL_CHARS);
 	} else if (function_exists("filter_get")) {
@@ -54,15 +54,15 @@
 	if (strstr($page, "http:"))
 		header("Location: $page");
 
-	$tab_pages = preg_split("/", $page);
+	$tab_pages = preg_split("/\//", $page);
 	foreach ($tab_pages as $value)
 		$page = $value;
 
 	if (!file_exists("../doc/".$oreon->user->get_version()."/".$oreon->user->get_lang()."/"))
 		$lang = "en_US";
-	else 
+	else
 		$lang = $oreon->user->get_lang();
-		
+
 	if (preg_match("/png/i", $page)) {
 		print "<img src=\"./include/doc/getImage.php?lang=".$oreon->user->get_lang()."&version=".$oreon->user->get_version()."&img=images/".$page."\" />" ;
 		exit ;
@@ -72,18 +72,18 @@
 	unset($path);
 
 	$path = "./include/doc/";
-		
+
 	/*
 	 * Smarty template Init
 	 */
 	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl, "./");	
-	
+	$tpl = initSmartyTpl($path, $tpl, "./");
+
 	$flag_begin = 0;
 	$flag_end = 0;
 	print "<div style='padding=20px'>";
 
-	if ($doc = fopen("../doc/".$oreon->user->get_version()."/".$lang."/".$page, "r"))	
+	if ($doc = fopen("../doc/".$oreon->user->get_version()."/".$lang."/".$page, "r"))
 		while ($line = fgets($doc)){
 			if ($flag_begin && !$flag_end){
 				$line = preg_replace("/href\=\"/", "href=\"./main.php?p=$p&doc=1&page=", $line);
