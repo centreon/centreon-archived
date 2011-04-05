3064dbc2f170b80270c24233d800be9dbe69b00c
Julien Mathis <jmathis@merethis.com>
2011-04-05 15:20:36 +0000

    remove hardcoded path
    git-svn-id: http://svn.centreon.com/trunk/centreon@12013 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configCentreonBroker/xml/loggerXml.php
+++ www/include/configuration/configCentreonBroker/xml/loggerXml.php
@@ -36,7 +36,7 @@
  *
  */
 
-    include_once("/etc/centreon/centreon.conf.php");
+    include_once("@CENTREON_ETC@/centreon.conf.php");
 
 	require_once $centreon_path . "/www/class/centreonDB.class.php";
 	require_once $centreon_path . "/www/class/centreonXML.class.php";
