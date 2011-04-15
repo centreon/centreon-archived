e28f2a9bb682aa938cc4a9e316ddd2e72a1d3e16
Sylvestre Ho <shotamchay@merethis.com>
2011-04-15 08:17:14 +0000

    fix query
    git-svn-id: http://svn.centreon.com/trunk/centreon@12108 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/Administration/corePerformance/performanceInfo.php
+++ www/include/Administration/corePerformance/performanceInfo.php
@@ -83,11 +83,12 @@
 	 * Get Poller List
 	 */
 	$tab_nagios_server = array();
-	$DBRESULT = $pearDB->query("SELECT n.id, ndomod.instance_name, n.name " .
-								"FROM `cfg_ndomod` ndomod, `nagios_server` n " .
-								"WHERE ndomod.activate = '1' AND n.ns_activate = '1' " .
-								"AND ndomod.ns_nagios_server = n.id AND n.id = '".$pearDB->escape($selectedPoller)."' " .
+	$DBRESULT = $pearDB->query("SELECT n.id, n.name " .
+								"FROM `nagios_server` n " .
+								"WHERE n.ns_activate = '1' " .
+								"AND n.id = '".$pearDB->escape($selectedPoller)."' " .
 								"ORDER BY n.localhost DESC");
+
 	$perfInfo = array();
 	$empty_stats = array();
 	$emptyPage = 1;
