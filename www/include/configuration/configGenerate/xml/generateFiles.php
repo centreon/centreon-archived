130d9708a4881eac43ded063622b87c02ae659a3
Sylvestre Ho <shotamchay@merethis.com>
2011-04-19 12:22:12 +0000

    + GPL header
    git-svn-id: http://svn.centreon.com/trunk/centreon@12133 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configGenerate/xml/generateFiles.php
+++ www/include/configuration/configGenerate/xml/generateFiles.php
@@ -1,4 +1,42 @@
 <?php
+/*
+ * Copyright 2005-2011 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ * SVN : $URL:$
+ * SVN : $Id:$
+ *
+ */
+
+require_once "@CENTREON_ETC@/centreon.conf.php";
 
 if (!isset($_POST['poller']) || !isset($_POST['comment']) || !isset($_POST['debug']) || !isset($_POST['sid'])) {
     exit;
@@ -76,8 +114,6 @@ function printDebug($xml)
     $xml->endElement();
     return $returnCode;
 }
-
-require_once "@CENTREON_ETC@/centreon.conf.php";
 
 $path = $centreon_path . "www/include/configuration/configGenerate/";
 $nagiosCFGPath = $centreon_path . "filesGeneration/nagiosCFG/";
@@ -164,7 +200,7 @@ try {
                  */
                 if ($files = glob($path . "metaService/*.php")) {
                     foreach ($files as $filename) {
-                        include($filename);
+                        include $filename;
                     }
                 }
             }
@@ -175,7 +211,7 @@ try {
             foreach ($oreon->modules as $key => $value) {
                 if ($value["gen"] && $files = glob($centreon_path . "www/modules/".$key."/generate_files/*.php")) {
                     foreach ($files as $filename) {
-                        require_once ($filename);
+                        include $filename;
                     }
                 }
             }
