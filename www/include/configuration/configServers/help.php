6c2ca932b030cf94b56c9196e05808206a35f084
Sylvestre Ho <shotamchay@merethis.com>
2011-04-22 14:35:17 +0000

    + help tooltip
    git-svn-id: http://svn.centreon.com/trunk/centreon@12146 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configServers/help.php
+++ www/include/configuration/configServers/help.php
@@ -2,14 +2,14 @@
 $help = array();
 $help['name'] = dgettext("help", "Used for identifying the poller");
 $help['ns_ip_address'] = dgettext("help", "IP address of the poller");
-$help['localhost'] = dgettext("help", "");
-$help['is_default'] = dgettext("help", "");
+$help['localhost'] = dgettext("help", "Whether the poller is local");
+$help['is_default'] = dgettext("help", "Main poller");
 $help['ssh_port'] = dgettext("help", "SSH port of the remote poller");
 $help['monitoring_engine'] = dgettext("help", "Scheduling engine");
-$help['init_script'] = dgettext("help", "");
-$help['nagios_bin'] = dgettext("help", "");
-$help['nagiostats_bin'] = dgettext("help", "");
-$help['nagios_perfdata'] = dgettext("help", "");
-$help['centreonbroker_cfg_path'] = dgettext("help", "");
-$help['ns_activate'] = dgettext("help", "");
+$help['init_script'] = dgettext("help", "Path of init script of the scheduler");
+$help['nagios_bin'] = dgettext("help", "Path of binary of the scheduler");
+$help['nagiostats_bin'] = dgettext("help", "Path of stats binary of the scheduler");
+$help['nagios_perfdata'] = dgettext("help", "Perfdata script");
+$help['centreonbroker_cfg_path'] = dgettext("help", "Path of the configuration file for Centreon Broker");
+$help['ns_activate'] = dgettext("help", "Enable or disable poller");
 ?>
