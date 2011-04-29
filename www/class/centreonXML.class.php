17edd4750af7e0ceb3dc4abbbe30404bf135ae14
wistof <wistof@free.fr>
2011-04-29 12:59:51 +0000

    Bug #2313 :  'All Nagios Servers' generation config with debug create a Apache Segmentation fault
    git-svn-id: http://svn.centreon.com/trunk/centreon@12161 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonXML.class.php
+++ www/class/centreonXML.class.php
@@ -89,16 +89,17 @@ class CentreonXML {
      */
     protected function is_utf8($string)
     {
-        return preg_match('%^(?:
-                  [\x09\x0A\x0D\x20-\x7E]
-                | [\xC2-\xDF][\x80-\xBF]
-                |  \xE0[\xA0-\xBF][\x80-\xBF]
-                | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}
-                |  \xED[\x80-\x9F][\x80-\xBF]
-                |  \xF0[\x90-\xBF][\x80-\xBF]{2}
-                | [\xF1-\xF3][\x80-\xBF]{3}
-                |  \xF4[\x80-\x8F][\x80-\xBF]{2}
-            )*$%xs', $string);
+		$res = 0;
+        $res += preg_match('%^(?:[\x09\x0A\x0D\x20-\x7E])*$%xs', $string);
+        $res += preg_match('%^(?:[\xC2-\xDF][\x80-\xBF])*$%xs', $string);
+        $res += preg_match('%^(?:\xE0[\xA0-\xBF][\x80-\xBF])*$%xs', $string);
+        $res += preg_match('%^(?:[\xE1-\xEC\xEE\xEF][\x80-\xBF]{2})*$%xs', $string);
+        $res += preg_match('%^(?:\xED[\x80-\x9F][\x80-\xBF])*$%xs', $string);
+        $res += preg_match('%^(?:\xF0[\x90-\xBF][\x80-\xBF]{2})*$%xs', $string);
+        $res += preg_match('%^(?:[\xF1-\xF3][\x80-\xBF]{3})*$%xs', $string);
+        $res += preg_match('%^(?:\xF4[\x80-\x8F][\x80-\xBF]{2})*$%xs', $string);
+        	
+		return $res;			
     }
 
     /*
