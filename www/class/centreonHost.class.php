ffdd9a1cebdd78a6885729015d23ad253d71bb9d
Sylvestre Ho <shotamchay@merethis.com>
2011-04-29 09:22:32 +0000

    better coding style
    git-svn-id: http://svn.centreon.com/trunk/centreon@12160 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonHost.class.php
+++ www/class/centreonHost.class.php
@@ -39,122 +39,180 @@
  /*
   *  Class that contains various methods for managing hosts
   */
- class CentreonHost {
- 	private $DB;
+ class CentreonHost
+ {
+ 	protected $db;
 
 
  	/**
- 	 *
  	 * Constructor
- 	 * @param $pearDB
+ 	 *
+ 	 * @param CentreonDB $db
+ 	 * @return void
  	 */
- 	function CentreonHost($pearDB) {
- 		$this->DB = $pearDB;
+ 	function __construct($db)
+ 	{
+ 		$this->db = $db;
  	}
 
 	/**
-	 *
 	 * Method that returns a hostname from host_id
-	 * @param unknown_type $host_id
+	 *
+	 * @param int $host_id
+	 * @return string
 	 */
  	public function getHostName($host_id)
  	{
  		static $hosts = array();
 
  		if (!isset($hosts[$host_id])) {
-     	    $rq = "SELECT host_name FROM host WHERE host_id = '".$this->DB->escape($host_id)."' LIMIT 1";
-     		$DBRES = $this->DB->query($rq);
-     		if (!$DBRES->numRows())
-     			return NULL;
-     		$row = $DBRES->fetchRow();
-     		$hosts[$host_id] = $row['host_name'];
+     	    $rq = "SELECT host_name
+     	    	   FROM host
+     	    	   WHERE host_id = ".$this->db->escape($host_id)."
+     	    	   LIMIT 1";
+     		$res = $this->db->query($rq);
+     		if ($res->numRows()) {
+     		    $row = $res->fetchRow();
+     		    $hosts[$host_id] = $row['host_name'];
+     		}
  		}
  		if (isset($hosts[$host_id])) {
  		    return $hosts[$host_id];
  		}
- 		return "";
+ 		return null;
  	}
 
  	/**
- 	 *
  	 * Method that returns a host alias from host_id
- 	 * @param $host_id
+ 	 *
+ 	 * @param int $host_id
+ 	 * @return string
  	 */
- 	public function getHostAlias($host_id) {
- 		$rq = "SELECT host_alias FROM host WHERE host_id = '".$host_id."' LIMIT 1";
- 		$DBRES = $this->DB->query($rq);
- 		if (!$DBRES->numRows())
- 			return NULL;
- 		$row = $DBRES->fetchRow();
- 		return $row['host_alias'];
+ 	public function getHostAlias($host_id)
+ 	{
+ 	    static $aliasTab = array();
+
+ 	    if (!isset($aliasTab[$host_id])) {
+     	    $rq = "SELECT host_alias
+     	    	   FROM host
+     	    	   WHERE host_id = ".$this->db->escape($host_id)."
+     	    	   LIMIT 1";
+     		$res = $this->db->query($rq);
+     		if ($res->numRows()) {
+     		    $row = $res->fetchRow();
+     		    $aliasTab[$host_id] = $row['host_alias'];
+     		}
+ 	    }
+ 	    if (isset($aliasTab[$host_id])) {
+ 	        return $aliasTab[$host_id];
+ 	    }
+ 		return null;
  	}
 
  	/**
- 	 *
  	 * Method that returns a host address from host_id
- 	 * @param unknown_type $host_id
+ 	 *
+ 	 * @param int $host_id
+ 	 * @return string
  	 */
- 	public function getHostAddress($host_id) {
- 		$rq = "SELECT host_address FROM host WHERE host_id = '".$host_id."' LIMIT 1";
- 		$DBRES = $this->DB->query($rq);
- 		if (!$DBRES->numRows())
- 			return NULL;
- 		$row = $DBRES->fetchRow();
- 		return $row['host_address'];
+ 	public function getHostAddress($host_id)
+ 	{
+ 		static $addrTab = array();
+
+ 		if (!isset($addrTab[$host_id])) {
+     	    $rq = "SELECT host_address
+     	    	   FROM host
+     	    	   WHERE host_id = ".$this->db->escape($host_id)."
+     	    	   LIMIT 1";
+     		$res = $this->db->query($rq);
+     		if ($res->numRows()) {
+     			$row = $res->fetchRow();
+     			$addrTab[$host_id] = $row['host_address'];
+     		}
+ 		}
+ 		if (isset($addrTab[$host_id])) {
+ 		    return $addrTab[$host_id];
+ 		}
+ 		return null;
  	}
 
  	/**
+ 	 * Method that returns the id of a host
  	 *
- 	 *  Method that returns the id of a host
- 	 * @param varchar $host_name
+ 	 * @param string $host_name
+ 	 * @return int
  	 */
- 	public function getHostId($host_name) {
- 		$rq = "SELECT host_id FROM host WHERE host_name = '".$host_name."' LIMIT 1";
- 		$DBRES = $this->DB->query($rq);
- 		if (!$DBRES->numRows())
- 			return NULL;
- 		$row = $DBRES->fetchRow();
- 		return $row['host_id'];
+ 	public function getHostId($host_name)
+ 	{
+ 		static $ids = array();
+
+ 		if (!isset($ids[$host_name])) {
+     	    $rq = "SELECT host_id
+     	    	   FROM host
+     	    	   WHERE host_name = '".$this->db->escape($host_name)."'
+     	    	   LIMIT 1";
+     		$res = $this->db->query($rq);
+     		if ($res->numRows()) {
+     		    $row = $res->fetchRow();
+     		    $ids[$host_name] = $row['host_id'];
+     		}
+ 		}
+ 		if (isset($ids[$host_name])) {
+ 		    return $ids[$host_name];
+ 		}
+ 		return null;
  	}
 
  	/**
- 	 *
  	 * Check illegal char defined into nagios.cfg file
- 	 * @param varchar $host_name
+ 	 *
+ 	 * @param string $host_name
+ 	 * @param int $poller_id
+ 	 * @return string
  	 */
- 	public function checkIllegalChar($host_name, $poller_id = null) {
- 		$DBRESULT = $this->DB->query("SELECT illegal_object_name_chars FROM cfg_nagios");
-		while ($data = $DBRESULT->fetchRow()) {
+ 	public function checkIllegalChar($host_name, $poller_id = null)
+ 	{
+ 		$res = $this->db->query("SELECT illegal_object_name_chars FROM cfg_nagios");
+		while ($data = $res->fetchRow()) {
 			$tab = str_split(html_entity_decode($data['illegal_object_name_chars'], ENT_QUOTES, "UTF-8"));
 			foreach ($tab as $char) {
 				$host_name = str_replace($char, "", $host_name);
 			}
 		}
-		$DBRESULT->free();
+		$res->free();
 		return $host_name;
  	}
 
  	/**
+ 	 * Method that returns the poller id that monitors the host
  	 *
- 	 *  Method that returns the poller id that monitors the host
  	 * @param int $host_id
+ 	 * @return int
  	 */
- 	public function getHostPollerId($host_id) {
- 		$rq = "SELECT nagios_server_id FROM ns_host_relation WHERE host_host_id = '".$host_id."' LIMIT 1";
- 		$DBRES = $this->DB->query($rq);
- 		if (!$DBRES->numRows())
- 			return NULL;
- 		$row = $DBRES->fetchRow();
+ 	public function getHostPollerId($host_id)
+ 	{
+ 		$rq = "SELECT nagios_server_id
+ 		       FROM ns_host_relation
+ 		       WHERE host_host_id = ".$this->db->escape($host_id)."
+ 		       LIMIT 1";
+ 		$res = $this->db->query($rq);
+ 		if (!$res->numRows()) {
+ 			return null;
+ 		}
+ 		$row = $res->fetchRow();
  		return $row['nagios_server_id'];
  	}
 
  	/**
- 	 *
  	 * Returns a string that replaces on demand macros by their values
- 	 * @param unknown_type $hostParam
- 	 * @param unknown_type $string
+ 	 *
+ 	 * @param mixed $hostParam
+ 	 * @param string $string
+ 	 * @param int $antiLoop
+ 	 * @return string
  	 */
- 	public function replaceMacroInString($hostParam, $string, $antiLoop = null) {
+ 	public function replaceMacroInString($hostParam, $string, $antiLoop = null)
+ 	{
 		if (is_numeric($hostParam)) {
  	        $host_id = $hostParam;
 		} elseif (is_string($hostParam)) {
@@ -163,21 +221,25 @@
 		    return $string;
 		}
 		$rq = "SELECT host_register FROM host WHERE host_id = '".$host_id."' LIMIT 1";
-        $DBRESULT = $this->DB->query($rq);
-        if (!$DBRESULT->numRows())
+        $res = $this->db->query($rq);
+        if (!$res->numRows()) {
         	return $string;
-        $row = $DBRESULT->fetchRow();
+        }
+        $row = $res->fetchRow();
 
         /*
          * replace if not template
          */
         if ($row['host_register'] == 1) {
-			if (strpos($string, "\$HOSTADDRESS$"))
+			if (strpos($string, "\$HOSTADDRESS$")) {
 	 			$string = str_replace("\$HOSTADDRESS\$", $this->getHostAddress($host_id), $string);
-			if (strpos($string, "\$HOSTNAME$"))
+			}
+			if (strpos($string, "\$HOSTNAME$")) {
 	 			$string = str_replace("\$HOSTNAME\$", $this->getHostName($host_id), $string);
-			if (strpos($string, "\$HOSTALIAS$"))
+			}
+			if (strpos($string, "\$HOSTALIAS$")) {
 	 			$string = str_replace("\$HOSTALIAS\$", $this->getHostAlias($host_id), $string);
+			}
         }
         unset($row);
 
@@ -187,7 +249,7 @@
  		$i = 0;
  		while (isset($matches[1][$i])) {
  			$rq = "SELECT host_macro_value FROM on_demand_macro_host WHERE host_host_id = '".$host_id."' AND host_macro_name LIKE '".$matches[1][$i]."'";
- 			$DBRES = $this->DB->query($rq);
+ 			$DBRES = $this->db->query($rq);
 	 		while ($row = $DBRES->fetchRow()) {
 	 			$string = str_replace($matches[1][$i], $row['host_macro_value'], $string);
 	 		}
@@ -195,7 +257,7 @@
  		}
  		if ($i) {
 	 		$rq2 = "SELECT host_tpl_id FROM host_template_relation WHERE host_host_id = '".$host_id."' ORDER BY `order`";
-	 		$DBRES2 = $this->DB->query($rq2);
+	 		$DBRES2 = $this->db->query($rq2);
 	 		while ($row2 = $DBRES2->fetchRow()) {
 	 		    if (!isset($antiLoop) || !$antiLoop) {
 	 		        $string = $this->replaceMacroInString($row2['host_tpl_id'], $string, $row2['host_tpl_id']);
