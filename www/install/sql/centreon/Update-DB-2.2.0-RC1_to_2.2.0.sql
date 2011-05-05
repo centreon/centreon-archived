ffdac6a43f851cf97cbb637a051e422155200704
Sylvestre Ho <shotamchay@merethis.com>
2011-05-05 14:21:54 +0000

    prepare sql upgrade
    git-svn-id: http://svn.centreon.com/trunk/centreon@12196 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.2.0-RC1_to_2.2.0.sql
+++ www/install/sql/centreon/Update-DB-2.2.0-RC1_to_2.2.0.sql
@@ -2,5 +2,8 @@ ALTER TABLE `traps` ADD `traps_advanced_treatment` enum('0','1') default '0' AFT
 
 -- Delete the Directories page for Media
 DELETE FROM `topology` WHERE `topology_page` = 5010202;
- 
-UPDATE `informations` SET `value` = '2.2.0' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.2.0-RC1' LIMIT 1;
+
+-- Add the default engine
+INSERT INTO `options` (`key`, `value`) VALUES ('monitoring_engine', 'NAGIOS'); 
+
+UPDATE `informations` SET `value` = '2.2.0-RC2' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.2.0-RC1' LIMIT 1;
