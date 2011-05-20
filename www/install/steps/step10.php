9228a194d29aa818c9ed118c66d58d810175816a
Julien Mathis <jmathis@merethis.com>
2011-05-20 08:57:30 +0000

    remove warning during configuration file write
    git-svn-id: http://svn.centreon.com/trunk/centreon@12256 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/steps/step10.php
+++ www/install/steps/step10.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 if (isset($_POST["goto"]) && strcmp($_POST["goto"], "Back")){
@@ -77,9 +77,9 @@ aff_header("Centreon Setup Wizard", "Centreon Configuration File", 10);	?>
   	<tr>
 		<td><b>Generate Centreon configuration file</b></td>
 		<td align="right"><?php
-		
+
 			$_SESSION["pwdOreonDB"] = str_replace("\$", "\\\$", $_SESSION["pwdOreonDB"]);
-		
+
 			$file[0] = "<?php\n";
 			$file[1] = "/*\n";
 			$file[2] = " * Centreon is developped with GPL Licence 2.0 :\n";
@@ -110,9 +110,9 @@ aff_header("Centreon Setup Wizard", "Centreon Configuration File", 10);	?>
 			$file[27] = "/* Centreon Path */\n";
 			$file[28] = "\$centreon_path='".$conf_centreon["centreon_dir"]."';\n";
 			$file[29] = "?>";
-			
+
 			if ($fd = fopen($conf_centreon["centreon_etc"]."centreon.conf.php", "w"))	{
-				for ($i = 0; $file[$i]; $i++)
+				for ($i = 0; isset($file[$i]) && $file[$i]; $i++)
 					fwrite ($fd, $file[$i]);
 				fclose ($fd);
 				echo '<b><span class="go">OK</b>';
@@ -138,7 +138,7 @@ aff_header("Centreon Setup Wizard", "Centreon Configuration File", 10);	?>
 			$file_pm[] = "\$mysql_database_ods = \"". $_SESSION["nameOdsDB"] . "\";\n";
 			$file_pm[] = "1;\n";
 			if ($fd = fopen($conf_centreon["centreon_etc"]."/conf.pm", "w"))	{
-				for ($i = 0; $file_pm[$i]; $i++)
+				for ($i = 0; isset($file_pm[$i]) && $file_pm[$i]; $i++)
 					fwrite ($fd, $file_pm[$i]);
 				fclose ($fd);
 				echo '<b><span class="go">OK</b>';
