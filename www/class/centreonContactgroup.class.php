390505e7054908307a629026a9c34f4fa8a1df0f
Sylvestre Ho <shotamchay@merethis.com>
2011-07-11 08:55:55 +0000

    + new method for contactgroup class
    git-svn-id: http://svn.centreon.com/trunk/centreon@12367 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonContactgroup.class.php
+++ www/class/centreonContactgroup.class.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 /**
@@ -42,20 +42,20 @@
 class CentreonContactgroup
 {
     private $db;
-    
+
     /**
      * Constructor
-     * 
+     *
      * @param CentreonDB $pearDB
      */
     public function __construct($pearDB)
     {
         $this->db = $pearDB;
     }
-    
+
     /**
      * Get the list of contactgroups with his id, or his name for a ldap groups if is not sync in database
-     * 
+     *
      * @param bool $withLdap if include LDAP group
      */
     public function getListContactgroup($withLdap = false)
@@ -72,7 +72,7 @@ class CentreonContactgroup
     		$contactgroups[$contactgroup["cg_id"]] = $contactgroup["cg_name"];
     	}
     	$res->free();
-    	
+
     	$query = "SELECT `value` FROM `options` WHERE `key` = 'ldap_auth_enable'";
     	$res = $this->db->query($query);
     	$row = $res->fetchRow();
@@ -81,13 +81,13 @@ class CentreonContactgroup
     	} else {
     	    $ldapEnable = false;
     	}
-    	
+
     	if ($withLdap && $ldapEnable) {
         	/* ContactGroup from LDAP */
         	$ldap = new CentreonLDAP($this->db, null);
         	$ldap->connect();
         	$cg_ldap = $ldap->listOfGroups();
-        	
+
     	    /* Merge contactgroup from ldap and from db */
         	foreach ($cg_ldap as $cg_name) {
         	    if (false === array_search($cg_name, $contactgroups)) {
@@ -95,13 +95,13 @@ class CentreonContactgroup
         	    }
         	}
     	}
-    	
+
     	return $contactgroups;
     }
-    
+
     /**
      * Insert the ldap groups in table contactgroups
-     * 
+     *
      * @param string $cg_name The ldap group name
      * @return int The contactgroup id or 0 if error
      */
@@ -134,5 +134,24 @@ class CentreonContactgroup
         }
         $row = $res->fetchRow();
         return $row['cg_id'];
+    }
+
+    /**
+     * Get contact group name from contact group id
+     *
+     * @param int $cgId
+     * @return string
+     * @throws Exception
+     */
+    public function getNameFromCgId($cgId)
+    {
+        $query = "SELECT cg_name FROM contactgroup WHERE cg_id = " . $this->db->escape($cgId) . " LIMIT 1";
+        $res = $this->db->query($query);
+        if ($res->numRows()) {
+            $row = $res->fetchRow();
+            return $row['cg_name'];
+        } else {
+            throw Exception('No contact group name found');
+        }
     }
 }
