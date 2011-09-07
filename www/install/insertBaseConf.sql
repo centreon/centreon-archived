3c392941924a9b4c4682e43a1fa6f235c75ae54f
Julien Mathis <jmathis@merethis.com>
2011-09-07 15:17:54 +0000

    prepare 2.3.0-RC3
    git-svn-id: http://svn.centreon.com/trunk/centreon@12467 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.3.0-RC2');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.3.0-RC3');
 
 --
 -- Contenu de la table `nagios_server`
