5144d03bc1b16420d8d02cb4e5a91297d1e42e0b
Julien Mathis <jmathis@merethis.com>
2011-09-07 16:41:33 +0000

    fix problem with contacts fix problems with topology_js
    git-svn-id: http://svn.centreon.com/trunk/centreon@12469 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.3.0-RC2_to_2.3.0-RC3.sql
+++ www/install/sql/centreon/Update-DB-2.3.0-RC2_to_2.3.0-RC3.sql
@@ -24,4 +24,7 @@ UPDATE `nagios_server` SET `centreonbroker_cfg_path` = '/etc/centreon-broker', `
 --
 UPDATE `topology_JS` SET `PathName_js` = './include/common/javascript/scriptaculous/jsProgressBarHandler.js ' WHERE `PathName_js` = './include/common/javascript/scriptaculous/s2.js ';
 
+UPDATE contact SET contact_enable_notifications = '1';
+UPDATE contact SET contact_register = '1';
+
 UPDATE `informations` SET `value` = '2.3.0-RC3' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.3.0-RC2' LIMIT 1;
