2650183f218a424172713975eb2a2ca201039aad
Mat Sugumaran <msugumaran@merethis.com>
2011-09-15 12:23:48 +0000

    centstorage update from 2.3.0-RC3 to 2.3.0-RC4
    git-svn-id: http://svn.centreon.com/trunk/centreon@12498 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/centstorage/Update-CSTG-2.3.0-RC3_to_2.3.0-RC4.sql
@@ -0,0 +1,2 @@
+ALTER TABLE  `hoststateevents` CHANGE  `hoststateevents_id`  `hoststateevent_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
+ALTER TABLE  `servicestateevents` CHANGE  `servicestateevents_id`  `servicestateevent_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
