4dc2d22c735d1f33de1514337999b5e38cb2d97e
Sylvestre Ho <shotamchay@merethis.com>
2011-09-20 13:14:19 +0000

    status field: enum to tinyint
    git-svn-id: http://svn.centreon.com/trunk/centreon@12523 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/installBroker.sql
+++ www/install/installBroker.sql
@@ -618,7 +618,7 @@ CREATE TABLE IF NOT EXISTS logs (
   retry int default NULL,
   service_description varchar(255) default NULL,
   service_id int default NULL,
-  status enum('0', '1', '2', '3', '4') default NULL,
+  status tinyint(4) default NULL,
   type smallint default NULL,
 
   PRIMARY KEY (log_id),
