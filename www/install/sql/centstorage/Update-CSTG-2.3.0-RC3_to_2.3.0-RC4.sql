8c1a779c9c290010223cbfaa8bff92cc10d70623
Sylvestre Ho <shotamchay@merethis.com>
2011-09-20 13:13:03 +0000

    status field: enum to tinyint
    git-svn-id: http://svn.centreon.com/trunk/centreon@12522 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centstorage/Update-CSTG-2.3.0-RC3_to_2.3.0-RC4.sql
+++ www/install/sql/centstorage/Update-CSTG-2.3.0-RC3_to_2.3.0-RC4.sql
@@ -2,3 +2,4 @@ ALTER TABLE  `hoststateevents` CHANGE  `hoststateevents_id`  `hoststateevent_id`
 ALTER TABLE  `servicestateevents` CHANGE  `servicestateevents_id`  `servicestateevent_id` INT( 11 ) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `hoststateevents` ADD `ack_time` INT NULL DEFAULT NULL;
 ALTER TABLE `servicestateevents` ADD `ack_time` INT NULL DEFAULT NULL;
+ALTER TABLE  `logs` CHANGE  `status`  `status` TINYINT( 4 );
