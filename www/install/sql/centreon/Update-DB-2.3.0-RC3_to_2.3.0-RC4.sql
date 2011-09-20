e8e3327ed27a6e5b3e09f86f7a5add32427b0b2e
Julien Mathis <jmathis@merethis.com>
2011-09-20 08:23:44 +0000

    fix #2624
    git-svn-id: http://svn.centreon.com/trunk/centreon@12515 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.3.0-RC3_to_2.3.0-RC4.sql
+++ www/install/sql/centreon/Update-DB-2.3.0-RC3_to_2.3.0-RC4.sql
@@ -1,1 +1,7 @@
+
+DELETE FROM topology WHERE topology_url LIKE './include/monitoring/mysql_log/viewLog.php' AND topology_name LIKE 'All Logs' AND topology_show = '0';
+DELETE FROM topology WHERE topology_url LIKE './include/monitoring/mysql_log/viewErrors.php' AND topology_name LIKE 'Warnings' AND topology_show = '0';
+DELETE FROM topology WHERE topology_page = '20313' OR topology_parent = '20313';
+DELETE FROM topology WHERE topology_page = '20312' OR topology_parent = '20312';
+
 UPDATE `informations` SET `value` = '2.3.0-RC4' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.3.0-RC3' LIMIT 1;
