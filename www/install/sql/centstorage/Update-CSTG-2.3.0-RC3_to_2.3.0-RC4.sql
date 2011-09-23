beb01e3520e4a46968e022b75730ff2e7a1f4fc4
Sylvestre Ho <shotamchay@merethis.com>
2011-09-23 13:59:59 +0000

    Bug #2647 Remove UNIQUE constraint in hosts table
    git-svn-id: http://svn.centreon.com/trunk/centreon@12532 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centstorage/Update-CSTG-2.3.0-RC3_to_2.3.0-RC4.sql
+++ www/install/sql/centstorage/Update-CSTG-2.3.0-RC3_to_2.3.0-RC4.sql
@@ -3,3 +3,7 @@ ALTER TABLE  `servicestateevents` CHANGE  `servicestateevents_id`  `servicestate
 ALTER TABLE `hoststateevents` ADD `ack_time` INT NULL DEFAULT NULL;
 ALTER TABLE `servicestateevents` ADD `ack_time` INT NULL DEFAULT NULL;
 ALTER TABLE  `logs` CHANGE  `status`  `status` TINYINT( 4 );
+
+ALTER TABLE `hosts` DROP FOREIGN KEY hosts_ibfk_1;
+ALTER TABLE `hosts` DROP INDEX instance_id;
+ALTER TABLE `hosts` ADD FOREIGN KEY (instance_id) REFERENCES instances (instance_id);
