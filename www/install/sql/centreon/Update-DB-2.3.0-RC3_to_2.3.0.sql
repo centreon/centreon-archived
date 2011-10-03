0eb4a4b29b0db8cb2a70ceca444ecb80c5149387
Maximilien Bersoult <mbersoult@merethis.com>
2011-10-03 10:05:50 +0000

    * Ticket #2650 : Change display order for configuration Centreont Broker ; Thank to raphael tr
    git-svn-id: http://svn.centreon.com/trunk/centreon@12559 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.3.0-RC3_to_2.3.0.sql
+++ www/install/sql/centreon/Update-DB-2.3.0-RC3_to_2.3.0.sql
@@ -8,4 +8,19 @@ INSERT INTO options (`key`, `value`) VALUES ('centstorage', '1');
 
 ALTER TABLE `acl_topology` ADD `acl_comments` text DEFAULT NULL AFTER acl_topo_alias ;
 
+--
+-- Update order for Perfdata generator for Centreon Broker configuration
+--
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '6' WHERE `cb_type_field_relation`.`cb_type_id` =14 AND `cb_type_field_relation`.`cb_field_id` =8;
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '7' WHERE `cb_type_field_relation`.`cb_type_id` =14 AND `cb_type_field_relation`.`cb_field_id` =9;
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '8' WHERE `cb_type_field_relation`.`cb_type_id` =14 AND `cb_type_field_relation`.`cb_field_id` =10;
+
+--
+-- Update order for Broker SQL for Centreon Broker configuration
+--
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '4' WHERE `cb_type_field_relation`.`cb_type_id` =16 AND `cb_type_field_relation`.`cb_field_id` =8;
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '5' WHERE `cb_type_field_relation`.`cb_type_id` =16 AND `cb_type_field_relation`.`cb_field_id` =9;
+UPDATE `centreon`.`cb_type_field_relation` SET `order_display` = '6' WHERE `cb_type_field_relation`.`cb_type_id` =16 AND `cb_type_field_relation`.`cb_field_id` =10;
+
+
 UPDATE `informations` SET `value` = '2.3.0' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.3.0-RC3' LIMIT 1;
