072c0f1ff9ea4f20ce6b1adb9bd1d2f11e36e5f6
Mat Sugumaran <msugumaran@merethis.com>
2011-11-02 15:39:58 +0000

    bug #2716 fix and adding indexes for tables hoststateevents and servicestateevents
    git-svn-id: http://svn.centreon.com/trunk/centreon@12657 6bcd3966-0018-0410-8128-fd23d134de7e

---
+++ www/install/sql/centstorage/Update-CSTG-2.3.1_to_2.3.2.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `hoststateevents` ADD UNIQUE (`host_id` , `start_time`);
+ALTER TABLE `hoststateevents` ADD INDEX ( `end_time` ); 
+ALTER TABLE `hoststateevents` ADD INDEX ( `start_time` ); 
+
+ALTER TABLE `servicestateevents` ADD UNIQUE (`host_id` , `service_id` , `start_time`);
+ALTER TABLE `servicestateevents` ADD INDEX ( `end_time` );
+ALTER TABLE `servicestateevents` ADD INDEX ( `start_time` );
+
+ALTER TABLE `hoststateevents` DROP FOREIGN KEY `hoststateevents_ibfk_1`;
+ALTER TABLE `servicestateevents` DROP FOREIGN KEY `servicestateevents_ibfk_1`;
