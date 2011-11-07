1fdd5dc3f3aa22a7bfb0b15ff89f2fe88f86da1a
Sylvestre Ho <shotamchay@merethis.com>
2011-11-07 16:36:31 +0000

    Bug #2600 change text
    git-svn-id: http://svn.centreon.com/trunk/centreon@12678 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/steps/step6.php
+++ www/install/steps/step6.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 if (!isset($_SESSION["nameOreonDB"])) {
@@ -80,11 +80,11 @@ if (isset($passwd_error) && $passwd_error)
     <td align="right"><input type="password" name="pwdOreonDB2" value="<?php if (isset($_SESSION["pwdOreonDB2"])) print $_SESSION["pwdOreonDB2"]; ?>"></td>
   </tr>
   <tr>
-    <td><b>Database location</b> (it's MySQL Server IP address. localhost if blank)</td>
+    <td><b>Database location</b> (it's MySQL Server IP address. Default: localhost)</td>
     <td align="right"><input type="text" name="dbLocation" value="<?php if (isset($_SESSION["dbLocation"])) print $_SESSION["dbLocation"]; ?>"></td>
   </tr>
   <tr>
-    <td><b>Centreon Web Interface location</b> (localhost if blank)</td>
+    <td><b>Centreon Web Interface location</b> (Default: localhost)</td>
     <td align="right"><input type="text" name="nagiosLocation" value="<?php if (isset($_SESSION["nagiosLocation"])) print $_SESSION["nagiosLocation"]; ?>"></td>
   </tr>
   <tr>
