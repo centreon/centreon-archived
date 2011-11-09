c8e2bb9f67b72cf01fe502ff80fff9335fdcf00e
Sylvestre Ho <shotamchay@merethis.com>
2011-11-09 10:58:36 +0000

    Bug #2731 Selectting pluginlists to next page directory reset to base dir
    git-svn-id: http://svn.centreon.com/trunk/centreon@12691 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configPlugins/listPlugins.php
+++ www/include/configuration/configPlugins/listPlugins.php
@@ -3,58 +3,62 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
 		exit();
-		
+
 	include("./include/common/autoNumLimit.php");
 
 	# start quickSearch form
 	include_once("./include/common/quickSearch.php");
-	
-	if (isset($_POST["plugin_dir"]) && $_POST["plugin_dir"])
+
+	if (isset($_POST["plugin_dir"]) && $_POST["plugin_dir"]) {
 		$dir = $_POST["plugin_dir"];
-	else
+	} elseif (isset($_GET['template']) && $_GET['template']) {
+	    $dir = $_GET['template'];
+	} else {
 		$dir = "";
-	
+	}
+    $template = $dir;
+
 	if (isset($_GET["create"]) && $_GET["create"])
 		mkdir(str_replace("//", "/", $oreon->optGen["nagios_path_plugins"].$dir.$_GET["new_dir"]));
-	
+
 	$plugin_list = return_plugin_list($dir, $search);
 	$plugin_dir = return_plugin_dir("");
-	
+
 	$rows = count($plugin_list);
 
 	include("./include/common/checkPagination.php");
@@ -62,7 +66,7 @@
 	# Smarty template Init
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
-	
+
 	# start header menu
 	$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
 	$tpl->assign("headerMenu_name", _("Name"));
@@ -71,11 +75,11 @@
 	$tpl->assign("headerMenu_date", _("Last modified"));
 
 	# List of elements - Depends on different criteria
-	
+
 	$form = new HTML_QuickForm('form', 'POST', "?p=".$p);
 	# Different style between each lines
 	$style = "one";
-	
+
 	# Fill a tab with a mutlidimensionnal Array we put in $tpl
 	$elemArr = array();
 	$i = 0;
@@ -85,17 +89,17 @@
 	foreach ($plugin_list as $name => $path) {
 		if (!$search || ($search && stristr($name, $search))) {
 			if ($i >= $begin && $i < $end){
-				$cmd["command_id"] = 1;				
+				$cmd["command_id"] = 1;
 				$path = str_replace('#BR#', "\\n", $path);
 				$path = str_replace('#T#', "\\t", $path);
 				$path = str_replace('#R#', "\\r", $path);
 				$path = str_replace('#S#', "/", $path);
 				$path = str_replace('#BS#', "\\", $path);
-				
+
 				$tab = stat($oreon->optGen["nagios_path_plugins"].$dir.$path);
 				$mdate = date("d-m-Y H:i", $tab["mtime"]);
-				
-				$elemArr[$i_real] = array("MenuClass"=>"list_".$style, 								
+
+				$elemArr[$i_real] = array("MenuClass"=>"list_".$style,
 								"RowMenu_name" => substr($name, 1),
 								"RowMenu_num" => $i,
 								"RowMenu_size" => round(filesize($oreon->optGen["nagios_path_plugins"].$dir.$path) / 1024,2),
@@ -108,22 +112,23 @@
 		}
 	}
 	$tpl->assign("elemArr", $elemArr);
-	
+
 	#Different messages we put in the template
 	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
 	$form->addElement('select', 'plugin_dir', "Directory", $plugin_dir, array("onChange" => "this.form.submit('')"));
-	
+    $form->setDefaults(array('plugin_dir' => $dir));
+
 	# Form2
-	
+
 	$form2 = new HTML_QuickForm('form', 'POST', "?p=".$p);
 	$form2->addElement('submit', 'create', _("Create"));
 	$form2->addElement('text', 'new_dir', _("Create New Directory"));
 	$file = $form2->addElement('file', 'filename', _("File (zip, tar or cfg)"));
 	$form2->addElement('submit', 'load', _("Load"));
-	
+
 	if (isset($_GET["filename"]))
 		print $_GET["filename"];
-	
+
 	if ($form2->validate()) {
 		$ret = $form2->getSubmitValues();
 		$fDataz = array();
@@ -145,15 +150,15 @@
 	$attrs1 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o1'].selectedIndex == 1 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-				" 	setO(this.form.elements['o1'].value); submit();} " .				
+				" 	setO(this.form.elements['o1'].value); submit();} " .
 				"");
 	$form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "d"=>_("Delete")), $attrs1);
 	$form->setDefaults(array('o1' => NULL));
-		
+
 	$attrs2 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-				" 	setO(this.form.elements['o2'].value); submit();} " .				
+				" 	setO(this.form.elements['o2'].value); submit();} " .
 				"");
     $form->addElement('select', 'o2', NULL, array(NULL=>_("More actions..."), "d"=>_("Delete")), $attrs2);
 	$form->setDefaults(array('o2' => NULL));
@@ -165,21 +170,21 @@
 	$o2 = $form->getElement('o2');
 	$o2->setValue(NULL);
 	$o2->setSelected(NULL);
-	
+
 	$tpl->assign('limit', $limit);
-	
+
 	#
 	##Apply a template definition
 	#
-	
+
 	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 	$renderer2 = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	
-	$form->accept($renderer);	
-	$form2->accept($renderer2);	
+
+	$form->accept($renderer);
+	$form2->accept($renderer2);
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->assign('form2', $renderer2->toArray());
 	$tpl->assign('p', $p);
-		
-	$tpl->display("listPlugins.ihtml");	
+
+	$tpl->display("listPlugins.ihtml");
 ?>
