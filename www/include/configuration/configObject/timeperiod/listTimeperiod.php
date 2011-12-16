46916f5ed5b90ec6455c5c7b9cd0e4b4d6b1f20a
Sylvestre Ho <shotamchay@merethis.com>
2011-12-16 14:38:01 +0000

    Bug #2837 Show resulting Timeperiod
    git-svn-id: http://svn.centreon.com/trunk/centreon@12791 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/timeperiod/listTimeperiod.php
+++ www/include/configuration/configObject/timeperiod/listTimeperiod.php
@@ -3,54 +3,54 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (!isset($oreon))
 		exit();
-		
+
 	include("./include/common/autoNumLimit.php");
-	
+
 	/*
 	 * start quickSearch form
 	 */
 	$advanced_search = 0;
 	include_once("./include/common/quickSearch.php");
-	
+
 	$SearchTool = NULL;
 	if (isset($search) && $search)
 		$SearchTool = " WHERE tp_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%'";
-	
+
 	$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM timeperiod $SearchTool");
 	$tmp = $DBRESULT->fetchRow();
 	$rows = $tmp["COUNT(*)"];
@@ -62,9 +62,9 @@
 	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
-	
+
 	/* Access level */
-	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r'; 
+	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
 	$tpl->assign('mode_access', $lvl_access);
 
 	/*
@@ -74,40 +74,41 @@
 	$tpl->assign("headerMenu_name", _("Name"));
 	$tpl->assign("headerMenu_desc", _("Description"));
 	$tpl->assign("headerMenu_options", _("Options"));
-	
+
 	/*
 	 * Timeperiod list
 	 */
 	$DBRESULT = $pearDB->query("SELECT tp_id, tp_name, tp_alias FROM timeperiod $SearchTool ORDER BY tp_name LIMIT ".$num * $limit.", ".$limit);
-	
+
 	$search = tidySearchKey($search, $advanced_search);
-	
+
 	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
 	/*
 	 * Different style between each lines
 	 */
 	$style = "one";
-	
+
 	/*
 	 * Fill a tab with a mutlidimensionnal Array we put in $tpl
 	 */
 	$elemArr = array();	for ($i = 0; $timeperiod = $DBRESULT->fetchRow(); $i++) {
 		$moptions = "";
-		$selectedElements = $form->addElement('checkbox', "select[".$timeperiod['tp_id']."]");	
+		$selectedElements = $form->addElement('checkbox', "select[".$timeperiod['tp_id']."]");
 		$moptions .= "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$timeperiod['tp_id']."]'></input>";
-		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+		$elemArr[$i] = array("MenuClass"=>"list_".$style,
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$timeperiod["tp_name"],
 						"RowMenu_link"=>"?p=".$p."&o=c&tp_id=".$timeperiod['tp_id'],
 						"RowMenu_desc"=>$timeperiod["tp_alias"],
-						"RowMenu_options"=>$moptions);
+						"RowMenu_options"=>$moptions,
+						"resultingLink" => "?p=".$p."&o=s&tp_id=".$timeperiod['tp_id']);
 		$style != "two" ? $style = "two" : $style = "one";	}
 	$tpl->assign("elemArr", $elemArr);
 	/*
 	 * Different messages we put in the template
 	 */
 	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
-	
+
 	/*
 	 * Toolbar select
 	 */
@@ -129,7 +130,7 @@
 				"");
 	$form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete")), $attrs1);
 	$form->setDefaults(array('o1' => NULL));
-		
+
 	$attrs2 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
@@ -149,14 +150,14 @@
 	$o2 = $form->getElement('o2');
 	$o2->setValue(NULL);
 	$o2->setSelected(NULL);
-	
+
 	$tpl->assign('limit', $limit);
 
 	/*
 	 * Apply a template definition
 	 */
 	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);	
+	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listTimeperiod.ihtml");
 ?>
---
+++ www/include/configuration/configObject/timeperiod/renderTimeperiod.php
@@ -0,0 +1,162 @@
+<?php
+/*
+ * Copyright 2005-2011 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+if (!isset($centreon)) {
+    exit();
+}
+
+isset($_GET["tp_id"]) ? $tpG = $_GET["tp_id"] : $tpG = null;
+isset($_POST["tp_id"]) ? $tpP = $_POST["tp_id"] : $tpP = null;
+$tpG ? $tp_id = $tpG : $tp_id = $tpP;
+$path = "./include/configuration/configObject/timeperiod/";
+require_once $path."DB-Func.php";
+require_once "./include/common/common-Func.php";
+require_once $centreon_path . "www/class/centreonTimeperiodRenderer.class.php";
+$imgpath = "./include/common/javascript/scriptaculous/images/bramus/";
+$imgs = scandir($imgpath);
+$t = null;
+if ($tp_id) {
+    $t = new CentreonTimePeriodRenderer($pearDB,$tp_id,1);
+    $t->timeBars();
+}
+$query = "SELECT tp_name, tp_id FROM timeperiod";
+$DBRESULT = $pearDB->query($query);
+$tplist[0] = _("Select Timeperiod...");
+while ($row = $DBRESULT->fetchRow()) {
+    $tplist[$row['tp_id']] = $row['tp_name'];
+}
+$form = new HTML_QuickForm('form', 'POST', "?p=".$p."&o=s");
+$attrs1 = array('onchange'=>"javascript: setTP(this.form.elements['tp_id'].value); submit();");
+$form->addElement('select', 'tp_id', null, $tplist, $attrs1);
+$form->setDefaults(array('tp_id' => null));
+$tpel = $form->getElement('tp_id');
+if ($tp_id) {
+    $tpel->setValue($tp_id);
+    $tpel->setSelected($tp_id);
+}
+
+$attrsTextLong  = array("size"=>"55");
+$form->addElement('header', 'title',_("Resulting Time Period with inclusions"));
+$form->addElement('header', 'information',_("General Information"));
+$form->addElement('header', 'notification',_("Time Range"));
+$form->addElement('header', 'exception', _("Exception List"));
+$form->addElement('text', 'tp_name', _("Timeperiod Name"), $attrsTextLong);
+$form->addElement('text', 'tp_alias', _("Timeperiod Alias"), $attrsTextLong);
+$form->addElement('text', 'tp_sunday', _("Sunday"), $attrsTextLong);
+$form->addElement('text', 'tp_monday', _("Monday"), $attrsTextLong);
+$form->addElement('text', 'tp_tuesday', _("Tuesday"), $attrsTextLong);
+$form->addElement('text', 'tp_wednesday', _("Wednesday"), $attrsTextLong);
+$form->addElement('text', 'tp_thursday', _("Thursday"), $attrsTextLong);
+$form->addElement('text', 'tp_friday', _("Friday"), $attrsTextLong);
+$form->addElement('text', 'tp_saturday', _("Saturday"), $attrsTextLong);
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
+$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+$form->accept($renderer);
+$labels = array(
+					'unset_timerange'    => _('Unset Timerange'),
+                    'included_timerange' => _('Included Timerange'),
+                    'excluded_timerange' => _('Excluded Timerange'),
+                    'timerange_overlaps' => _('Timerange Overlaps'),
+                    'hover_for_info' 	 => _('Hover on timeline to see more information'),
+                    'no_tp_selected'	 => _('No time period selected')
+                );
+$tpl->assign('labels', $labels);
+$tpl->assign('form', $renderer->toArray());
+$tpl->assign('tpId', $tp_id);
+$tpl->assign('tp', $t);
+$tpl->assign('path', $path);
+$tpl->display("renderTimeperiod.ihtml");
+?>
+<script type="text/javascript">
+var tipDiv;
+document.onload = genToolTip();
+
+/**
+ * Set Time period
+ */
+function setTP(_i) {
+	document.forms['form'].elements['tp_id'].value = _i;
+}
+
+/**
+ * The tool tip is created and referenced as a global object
+ */
+function genToolTip() {
+	if (document.createElement) {
+		tipDiv = document.createElement('div');
+		document.body.appendChild(tipDiv);
+		tipDiv.appendChild(document.createTextNode('initial text'));
+		tipDiv.className = 'toolTip';
+		tipDiv.style.display = 'none';
+	}
+}
+
+/**
+ * Show tooltip
+ */
+function showTip(e, txt) {
+	if ( tipDiv ) {
+		var e = e || window.event;
+		var xy = cursorPos(e);
+		tipDiv.firstChild.data = txt;
+		tipDiv.style.left = (xy[0] + 5) + 'px';
+		tipDiv.style.top = (xy[1] + 15) + 'px';
+		tipDiv.style.display = '';
+	}
+}
+
+/**
+ * Hide tooltip
+ */
+function hideTip() {
+	if ( tipDiv ) {
+		tipDiv.style.display = 'none';
+	}
+}
+
+/**
+ * Based on quirskmode 'get cursor position' script
+ */
+function cursorPos(e){
+	if (e.pageX || e.pageY) {
+		return [ e.pageX, e.pageY ];
+	} else if (e.clientX || e.clientY) {
+		return [
+				e.clientX + document.body.scrollLeft,
+				e.clientY + document.body.scrollTop
+               ];
+	}
+}
+</script>
