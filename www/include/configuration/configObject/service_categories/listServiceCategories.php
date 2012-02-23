e5d0cc911c1541809f4456509d9a2378e4ce59d4
Julien Mathis <jmathis@merethis.com>
2012-02-23 12:15:55 +0000

    fix #3084
    git-svn-id: http://svn.centreon.com/trunk/centreon@12917 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/service_categories/listServiceCategories.php
+++ www/include/configuration/configObject/service_categories/listServiceCategories.php
@@ -3,42 +3,42 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (!isset($oreon))
 		exit();
-		
+
 	include("./include/common/autoNumLimit.php");
 
 	/*
@@ -46,7 +46,7 @@
 	 */
 	$advanced_search = 0;
 	include_once("./include/common/quickSearch.php");
-	
+
 	$SearchTool = NULL;
 	if (isset($search) && $search)
 		$SearchTool = "WHERE (sc_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' OR sc_description LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%')";
@@ -57,15 +57,15 @@
 	$rows = $tmp["COUNT(*)"];
 
 	include("./include/common/checkPagination.php");
-	
+
 	/*
 	 * Smarty template Init
 	 */
 	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
-	
+
 	/* Access level */
-	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r'; 
+	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
 	$tpl->assign('mode_access', $lvl_access);
 
 	/*
@@ -76,22 +76,22 @@
 	$tpl->assign("headerMenu_status", _("Status"));
 	$tpl->assign("headerMenu_linked_svc", _("Number of linked services"));
 	$tpl->assign("headerMenu_options", _("Options"));
-	
+
 	/*
 	 * Services Categories Lists
-	 */ 
-	
+	 */
+
 	$DBRESULT = $pearDB->query("SELECT * FROM service_categories $SearchTool ORDER BY sc_name LIMIT ".$num * $limit.", ".$limit);
 
 	$search = tidySearchKey($search, $advanced_search);
 
 	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
-	
+
 	/*
 	 * Different style between each lines
 	 */
 	$style = "one";
-	
+
 	/*
 	 * Fill a tab with a mutlidimensionnal Array we put in $tpl
 	 */
@@ -100,33 +100,33 @@
 		$moptions = "";
 		$DBRESULT2 = $pearDB->query("SELECT COUNT(*) FROM `service_categories_relation` WHERE `sc_id` = '".$sc['sc_id']."'");
 		$nb_svc = $DBRESULT2->fetchRow();
-		
+
 		$selectedElements = $form->addElement('checkbox', "select[".$sc['sc_id']."]");
-		
+
 		if ($sc["sc_activate"])
 			$moptions .= "<a href='main.php?p=".$p."&sc_id=".$sc['sc_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
 		else
 			$moptions .= "<a href='main.php?p=".$p."&sc_id=".$sc['sc_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;";
 		$moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$sc['sc_id']."]'></input>";
-				
-		$elemArr[$i] = array("MenuClass"=>"list_".$style,
-							"RowMenu_select"=>$selectedElements->toHtml(),
-							"sc_name"=>htmlentities($sc["sc_name"]),
-							"sc_link"=>"?p=".$p."&o=c&sc_id=".$sc['sc_id'],
-							"sc_description"=>htmlentities($sc["sc_description"]),
-							"svc_linked"=>$nb_svc["COUNT(*)"],
-							"sc_activated"=>$sc["sc_activate"] ? _("Enabled") : _("Disabled"),
-							"RowMenu_options"=>$moptions);
-		$style != "two" ? $style = "two" : $style = "one";	
+
+		$elemArr[$i] = array("MenuClass" => "list_".$style,
+							"RowMenu_select" => $selectedElements->toHtml(),
+							"sc_name" => htmlentities($sc["sc_name"], ENT_QUOTES, "UTF-8"),
+							"sc_link" => "?p=".$p."&o=c&sc_id=".$sc['sc_id'],
+							"sc_description" => htmlentities($sc["sc_description"], ENT_QUOTES, "UTF-8"),
+							"svc_linked" => $nb_svc["COUNT(*)"],
+							"sc_activated" => $sc["sc_activate"] ? _("Enabled") : _("Disabled"),
+							"RowMenu_options" => $moptions);
+		$style != "two" ? $style = "two" : $style = "one";
 	}
 	$tpl->assign("elemArr", $elemArr);
-	
+
 	/*
 	 * Different messages we put in the template
 	 */
-	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add")));
-	
+	$tpl->assign('msg', array ("addL" => "?p=".$p."&o=a", "addT" => _("Add")));
+
 	?>
 	<script type="text/javascript">
 	function setO(_i) {
@@ -145,7 +145,7 @@
 				"this.form.elements['o1'].selectedIndex = 0");
 	$form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "mc"=>_("Massive Change"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs1);
 	$form->setDefaults(array('o1' => NULL));
-		
+
 	$attrs2 = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
@@ -165,7 +165,7 @@
 	$o2 = $form->getElement('o2');
 	$o2->setValue(NULL);
 	$o2->setSelected(NULL);
-	
+
 	$tpl->assign('limit', $limit);
 
 	/*
