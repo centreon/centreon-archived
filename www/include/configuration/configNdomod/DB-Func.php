b01b2fd267cc87ab09ead26eae65e0f44291a388
Julien Mathis <jmathis@merethis.com>
2012-04-25 13:13:15 +0000

    remove use of instance_name field in the ndmod cfg file. This information is deprecated since 2.2. TODO : think to remove sql field.
    git-svn-id: http://svn.centreon.com/trunk/centreon@13017 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configNdomod/DB-Func.php
+++ www/include/configuration/configNdomod/DB-Func.php
@@ -3,42 +3,42 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (!isset($oreon))
 		exit();
-	
+
 	function testExistence ($name = NULL)	{
 		global $pearDB, $form;
 		$id = NULL;
@@ -46,33 +46,33 @@
 			$id = $form->getSubmitValue('id');
 		$DBRESULT = $pearDB->query("SELECT description, id FROM cfg_ndomod WHERE `description` = '".htmlentities($name, ENT_QUOTES, "UTF-8")."'");
 		$ndomod = $DBRESULT->fetchRow();
-		if ($DBRESULT->numRows() >= 1 && $ndomod["id"] == $id)#Modif case	
+		if ($DBRESULT->numRows() >= 1 && $ndomod["id"] == $id)#Modif case
 			return true;
 		else if ($DBRESULT->numRows() >= 1 && $ndomod["id"] != $id)#Duplicate entry
 			return false;
 		else
 			return true;
-	}	
-	
+	}
+
 	function enableNdomodInDB ($id = null)	{
 		if (!$id) return;
 		global $pearDB, $oreon;
 		$DBRESULT = $pearDB->query("UPDATE `cfg_ndomod` SET `activate` = '1' WHERE id = '".$id."'");
 	}
-	
+
 	function disableNdomodInDB ($id = null)	{
 		if (!$id) return;
 		global $pearDB,$oreon;
 		$DBRESULT = $pearDB->query("UPDATE `cfg_ndomod` SET `activate` = '0' WHERE id = '".$id."'");
 	}
-	
+
 	function deleteNdomodInDB ($ndomod = array())	{
 		global $pearDB;
 		foreach($ndomod as $key => $value)	{
 			$DBRESULT = $pearDB->query("DELETE FROM `cfg_ndomod` WHERE id = '".$key."'");
 		}
 	}
-	
+
 	function multipleNdomodInDB ($ndomod = array(), $nbrDup = array())	{
 		foreach($ndomod as $key => $value)	{
 			global $pearDB;
@@ -94,22 +94,22 @@
 			}
 		}
 	}
-	
+
 	function updateNdomodInDB ($id = NULL)	{
 		if (!$id) return;
 		updateNdomod($id);
-	}	
-	
+	}
+
 	function insertNdomodInDB ()	{
 		$id = insertNdomod();
 		return ($id);
 	}
-	
+
 	function insertNdomod($ret = array())	{
 		global $form, $pearDB, $oreon;
 		if (!count($ret))
 			$ret = $form->getSubmitValues();
-		
+
 		/*
 		 * Get Nagios Server List
 		 */
@@ -118,15 +118,14 @@
 		while ($nagios_server = $DBRESULT->fetchRow())
 			$nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
 		$DBRESULT->free();
-		
+
 		$rq = "INSERT INTO `cfg_ndomod` (" .
-				"`description` , `ns_nagios_server` , `instance_name` , `output_type` , `output` , `buffer_file` , " .
+				"`description` , `ns_nagios_server` , `output_type` , `output` , `buffer_file` , " .
 				"`tcp_port` , `output_buffer_items`, `file_rotation_interval` , `file_rotation_command` , `file_rotation_timeout` , `reconnect_interval` , " .
 				"`reconnect_warning_interval` , `data_processing_options` , `config_output_options`, `activate`) ";
 		$rq .= "VALUES (";
 		isset($ret["description"]) && $ret["description"] != NULL ? $rq .= "'".htmlentities($ret["description"], ENT_QUOTES, "UTF-8")."', " : $rq .= "NULL, ";
 		isset($ret["ns_nagios_server"]) && $ret["ns_nagios_server"] != NULL ? $rq .= "'".htmlentities($ret["ns_nagios_server"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "NULL, ";
-        $rq .= "'".$nagios_servers[$ret["ns_nagios_server"]]."', ";
        	isset($ret["output_type"]) && $ret["output_type"] != NULL ? $rq .= "'".htmlentities($ret["output_type"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "NULL, ";
         isset($ret["output"]) && $ret["output"] != NULL ? $rq .= "'".htmlentities($ret["output"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "NULL, ";
         isset($ret["buffer_file"]) && $ret["buffer_file"] != NULL ? $rq .= "'".htmlentities($ret["buffer_file"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "NULL, ";
@@ -146,12 +145,12 @@
 		$DBRESULT->free();
 		return ($ndomod_id["MAX(id)"]);
 	}
-	
+
 	function updateNdomod($id = null)	{
 		global $form, $pearDB;
-		if (!$id) 
+		if (!$id)
 			return;
-		
+
 		/*
 		 * Get Nagios Server List
 		 */
@@ -160,15 +159,13 @@
 		while ($nagios_server = $DBRESULT->fetchRow())
 			$nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
 		$DBRESULT->free();
-		
+
 		$ret = array();
 		$ret = $form->getSubmitValues();
 		$rq = "UPDATE `cfg_ndomod` SET ";
         isset($ret["description"]) && $ret["description"] != NULL ? $rq .= "description = '".htmlentities($ret["description"], ENT_QUOTES, "UTF-8")."', " : $rq .= "description = NULL, ";
         isset($ret["ns_nagios_server"]) && $ret["ns_nagios_server"] != NULL ? $rq .= "ns_nagios_server = '".htmlentities($ret["ns_nagios_server"], ENT_QUOTES, "UTF-8")."', " : $rq .= "ns_nagios_server = NULL, ";
-		$rq .= "instance_name = '".$nagios_servers[$ret["ns_nagios_server"]]."', ";
-		//isset($ret["instance_name"]) && $ret["instance_name"] != NULL ? $rq .= "instance_name = '".htmlentities($ret["instance_name"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "instance_name = NULL, ";
-        isset($ret["output_type"]) && $ret["output_type"] != NULL ? $rq .= "output_type = '".htmlentities($ret["output_type"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "output_type = NULL, ";
+		isset($ret["output_type"]) && $ret["output_type"] != NULL ? $rq .= "output_type = '".htmlentities($ret["output_type"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "output_type = NULL, ";
        	isset($ret["output"]) && $ret["output"] != NULL ? $rq .= "output = '".htmlentities($ret["output"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "output = NULL, ";
         isset($ret["buffer_file"]) && $ret["buffer_file"] != NULL ? $rq .= "buffer_file = '".htmlentities($ret["buffer_file"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "buffer_file = NULL, ";
         isset($ret["tcp_port"]) && $ret["tcp_port"] != NULL ? $rq .= "tcp_port = '".htmlentities($ret["tcp_port"], ENT_QUOTES, "UTF-8")."',  " : $rq .= "tcp_port = NULL, ";
--- www/include/configuration/configNdomod/listNdomod.php
+++ www/include/configuration/configNdomod/listNdomod.php
@@ -3,60 +3,60 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
- 
+
 	if (!isset($oreon))
 		exit();
-		
+
 	include("./include/common/autoNumLimit.php");
 
 	# start quickSearch form
 	$advanced_search = 0;
 	include_once("./include/common/quickSearch.php");
 	# end quickSearch form
-	
+
 	$SearchTool = NULL;
 	if (isset($search) && $search)
 		$SearchTool = " WHERE description LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' ";
-	
+
 	$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM `cfg_ndomod` $SearchTool");
 
 	$tmp = $DBRESULT->fetchRow();
 	$rows = $tmp["COUNT(*)"];
 
 	/*
-	 * nagios servers comes from DB 
+	 * nagios servers comes from DB
 	 */
 	$nagios_servers = array();
 	$DBRESULT = $pearDB->query("SELECT * FROM nagios_server ORDER BY name");
@@ -73,57 +73,55 @@
 	$tpl = initSmartyTpl($path, $tpl);
 
 	/* Access level */
-	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r'; 
+	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
 	$tpl->assign('mode_access', $lvl_access);
-	
+
 	/*
 	 * start header menu
 	 */
 	$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
 	$tpl->assign("headerMenu_name", _("Name"));
-	$tpl->assign("headerMenu_instance_name", _("Instance Name"));
 	$tpl->assign("headerMenu_desc", _("Requester"));
 	$tpl->assign("headerMenu_interface_type", _("Interface Type"));
 	$tpl->assign("headerMenu_status", _("Status"));
 	$tpl->assign("headerMenu_options", _("Options"));
-	
+
 	/*
 	 * Nagios list
 	 */
-	
-		
+
+
 	$rq = "SELECT id, description, instance_name, ns_nagios_server, activate, output_type FROM cfg_ndomod $SearchTool ORDER BY description LIMIT ".$num * $limit.", ".$limit;
 	$DBRESULT = $pearDB->query($rq);
-	
+
 	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
 
 	/*
 	 * Different style between each lines
 	 */
 	$style = "one";
-	
+
 	/*
 	 * Fill a tab with a mutlidimensionnal Array we put in $tpl
 	 */
 	$elemArr = array();
-	for ($i = 0; $config = $DBRESULT->fetchRow(); $i++) {		
-		$selectedElements = $form->addElement('checkbox', "select[".$config['id']."]");	
+	for ($i = 0; $config = $DBRESULT->fetchRow(); $i++) {
+		$selectedElements = $form->addElement('checkbox', "select[".$config['id']."]");
 		$moptions = "";
 		if ($config["activate"])
 			$moptions .= "<a href='main.php?p=".$p."&id=".$config['id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_previous.gif' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
 		else
 			$moptions .= "<a href='main.php?p=".$p."&id=".$config['id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icones/16x16/element_next.gif' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
 		$moptions .= "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$config['id']."]'></input>";
-		$elemArr[$i] = array("MenuClass"=>"list_".$style, 
+		$elemArr[$i] = array("MenuClass"=>"list_".$style,
 						"RowMenu_select"=>$selectedElements->toHtml(),
 						"RowMenu_name"=>$config["description"],
-						"RowMenu_instance_name"=>$config["instance_name"],
 						"RowMenu_interface_type"=>$config["output_type"],
 						"RowMenu_link"=>"?p=".$p."&o=c&id=".$config['id'],
 						"RowMenu_desc"=>substr($nagios_servers[$config["ns_nagios_server"]], 0, 40),
 						"RowMenu_status"=>$config["activate"] ? _("Enabled") : _("Disabled"),
 						"RowMenu_options"=>$moptions);
-		$style != "two" ? $style = "two" : $style = "one";	
+		$style != "two" ? $style = "two" : $style = "one";
 	}
 	$tpl->assign("elemArr", $elemArr);
 	/*
@@ -132,7 +130,7 @@
 	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
 
 	/*
-	 * Toolbar select 
+	 * Toolbar select
 	 */
 	?>
 	<script type="text/javascript">
@@ -149,12 +147,12 @@
 				" 	setO(this.form.elements['o1'].value); submit();} " .
 				"else if (this.form.elements['o1'].selectedIndex == 3) {" .
 				" 	setO(this.form.elements['o1'].value); submit();} " .
-				"");	  
+				"");
     $form->addElement('select', 'o1', NULL, array(NULL=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete")), $attrs);
 	$form->setDefaults(array('o1' => NULL));
 	$o1 = $form->getElement('o1');
 	$o1->setValue(NULL);
-	
+
 	$attrs = array(
 		'onchange'=>"javascript: " .
 				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
@@ -169,14 +167,14 @@
 
 	$o2 = $form->getElement('o2');
 	$o2->setValue(NULL);
-	
+
 	$tpl->assign('limit', $limit);
 
 	/*
 	 * Apply a template definition
 	 */
 	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);	
+	$form->accept($renderer);
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->display("listNdomod.ihtml");
 ?>
