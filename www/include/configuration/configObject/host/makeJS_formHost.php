4e635f1d87e33d7e185c57f8f5f670f0c7319954
Alexandru Vilau <avilau@merethis.com>
2012-07-25 08:17:56 +0000

    Fixes #2398; Multiple IP addresses drop down list not implemented yet;
    git-svn-id: http://svn.centreon.com/trunk/centreon@13214 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/host/makeJS_formHost.php
+++ www/include/configuration/configObject/host/makeJS_formHost.php
@@ -390,6 +390,39 @@ function displayExistingMacroHost(max){
 	document.getElementById('hiddenMacInput').value = globalj;
 }
 
+/**
+ * Resolves host address
+ * 
+ * callback(err, ipAddress){
+ *      // Code...
+ * }
+ */
+function resolveHostNameToAddress(hostName, callback) {
+    if(window.XMLHttpRequest) {  // Latest browsers
+        var xhr = new XMLHttpRequest();
+    } else if(window.ActiveXObject) {  // Internet Explorer
+        try {
+            var xhr = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            var xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    } else { // XMLHttpRequest not supported
+        alert("Ajax requests are disabled");
+        return;
+    }
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if(xhr.status == 200){
+                callback(false, xhr.responseText);
+            } else {
+                callback(true, undefined);
+            }
+        }
+    }
+    xhr.open("GET", "./include/configuration/configObject/host/resolveHostName.php?hostName=" + encodeURIComponent(hostName) + "&sid=" + encodeURIComponent(sid));
+    xhr.send(null);
+}
+
 /*
  * Global variables
  */
