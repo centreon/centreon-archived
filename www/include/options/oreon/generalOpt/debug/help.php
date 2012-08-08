d39ff58acfed86181214cd5451205a7a7582388c
Sylvestre Ho <shotamchay@merethis.com>
2012-08-08 08:16:28 +0000

    Refs #1995; Text added for: * Administration -> Options -> SNMP,RRDTool,Debug
    git-svn-id: http://svn.centreon.com/trunk/centreon@13282 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/oreon/generalOpt/debug/help.php
+++ www/include/options/oreon/generalOpt/debug/help.php
@@ -5,10 +5,10 @@ $help = array();
  * Debug
  */
 
-$help['tip_logs_directory'] = dgettext('help', 'Logs Directory.');
-$help['tip_authentication_debug'] = dgettext('help', 'Authentication debug.');
-$help['tip_nagios_import_debug'] = dgettext('help', 'Nagios Import debug.');
-$help['tip_rrdtool_debug'] = dgettext('help', 'RRDTool debug.');
-$help['tip_ldap_user_import_debug'] = dgettext('help', 'LDAP User Import debug.');
-$help['tip_sql_debug'] = dgettext('help', 'SQL debug.');
+$help['tip_logs_directory'] = dgettext('help', 'Directory of log files.');
+$help['tip_authentication_debug'] = dgettext('help', 'Enables authentication debug.');
+$help['tip_nagios_import_debug'] = dgettext('help', 'Enables Nagios import debug.');
+$help['tip_rrdtool_debug'] = dgettext('help', 'Enables RRDTool debug.');
+$help['tip_ldap_user_import_debug'] = dgettext('help', 'Enables LDAP user import debug.');
+$help['tip_sql_debug'] = dgettext('help', 'Enables SQL debug.');
 
--- www/include/options/oreon/generalOpt/snmp/help.php
+++ www/include/options/oreon/generalOpt/snmp/help.php
@@ -5,14 +5,14 @@ $help = array();
  * snmpd
  */
 
-$help['tip_global_community'] = dgettext('help', 'Global Community.');
-$help['tip_version'] = dgettext('help', 'Version.');
+$help['tip_global_community'] = dgettext('help', 'SNMP global community.');
+$help['tip_version'] = dgettext('help', 'SNMP version.');
 
 /**
  * snmptrapd
  */
 
-$help['tip_directory_of_traps_configuration_files'] = dgettext('help', 'Directory of traps configuration files.');
-$help['tip_snmpttconvertmib_directory+binary'] = dgettext('help', 'snmpttconvertmib Directory + Binary.');
+$help['tip_directory_of_traps_configuration_files'] = dgettext('help', 'Directory of trap configuration files.');
+$help['tip_snmpttconvertmib_directory+binary'] = dgettext('help', 'snmpttconvertmib binary with complete path.');
 $help['tip_snmptt_log_file'] = dgettext('help', 'SNMPTT log file.');
 $help['tip_perl_library_directory'] = dgettext('help', 'Perl library directory.');
