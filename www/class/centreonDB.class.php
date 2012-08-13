d779e0331e4e51e347531efab1fabb34e7fb9958
Alexandru Vilau <avilau@merethis.com>
2012-08-13 15:12:56 +0000

    Fixes  #3537; Valid HTML now outputted when database server offline
    git-svn-id: http://svn.centreon.com/trunk/centreon@13328 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/class/centreonDB.class.php
+++ www/class/centreonDB.class.php
@@ -128,12 +128,20 @@ class CentreonDB
      */
     protected function displayConnectionErrorPage($msg = null)
     {
-		echo "<img src='./img/centreon.gif'><br/>";
-		if ($msg) {
-            echo "<b>" . $msg . "</b>";
-		} else {
-		    echo "<b>" . _("Connection failed, please contact your administrator") . "</b>";
+		if (!$msg) {
+            $msg = _("Connection failed, please contact your administrator");
 		}
+        echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+              <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+                <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <title>Centreon</title>
+              </head>
+                <body>
+                  <img src="./img/centreon.gif" alt="Centreon"/><br/>
+                  <b>' . $msg . '</b>
+                </body>
+              </html>';
 		exit;
 	}
 
