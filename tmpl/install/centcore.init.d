3b4739e67a949b1934f27b6575c164c48c7117ba
Quentin Garnier <qgarnier@merethis.com>
2012-08-20 14:49:56 +0000

    Enhancement #3556 [centcore] Reload configuration file
    git-svn-id: http://svn.centreon.com/trunk/centreon@13363 6bcd3966-0018-0410-8128-fd23d134de7e

--- tmpl/install/centcore.init.d
+++ tmpl/install/centcore.init.d
@@ -1,215 +1,112 @@
-#!/bin/sh
-################################################################################
-# Copyright 2005-2011 MERETHIS
-# Centreon is developped by : Julien Mathis and Romain Le Merlus under
-# GPL Licence 2.0.
-# 
-# This program is free software; you can redistribute it and/or modify it under 
-# the terms of the GNU General Public License as published by the Free Software 
-# Foundation ; either version 2 of the License.
-# 
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
-# this program; if not, see <http://www.gnu.org/licenses>.
-# 
-# Linking this program statically or dynamically with other modules is making a 
-# combined work based on this program. Thus, the terms and conditions of the GNU 
-# General Public License cover the whole combination.
-# 
-# As a special exception, the copyright holders of this program give MERETHIS 
-# permission to link this program with independent modules to produce an executable, 
-# regardless of the license terms of these independent modules, and to copy and 
-# distribute the resulting executable under terms of MERETHIS choice, provided that 
-# MERETHIS also meet, for each linked independent module, the terms  and conditions 
-# of the license of that module. An independent module is a module which is not 
-# derived from this program. If you modify this program, you may extend this 
-# exception to your version of the program, but you are not obliged to do so. If you
-# do not wish to do so, delete this exception statement from your version.
-# 
-# For more information : contact@centreon.com
-# 
-# SVN : $URL$
-# SVN : $Id$
+#! /bin/bash
 #
-####################################################################################
+# crond          Start/Stop the centcore daemon.
 #
-# Script init
-#
-### BEGIN INIT INFO Suse
-# Provides:       centcore
-# Required-Start:
-# Required-Stop:
-# Default-Start:  3 5
-# Default-Stop: 0 1 6
-# Description:    Start the centore high-Availability Engine
-### END INIT INFO
-
-### BEGIN INIT INFO Redhat
-# chkconfig: - 70 30
-# description: Centreon Core
+# chkconfig: 2345 70 30
+# description: centcore is a Centreon program that manage pollers
 # processname: centcore
-# config:
-# pidfile:
-### END INIT INFO
+# config: @CENTREON_ETC@
+# pidfile: ${RunDir}/centcore.pid
 
-status_centcore () {
-    if test ! -f $RunFile; then
-		echo "No lock file found in $RunFile"
-		return 1
-    fi
-    PID=`head -n 1 $RunFile`
-    if ps -p $PID; then
-		return 0
-    else
-		return 1
-    fi
-    return 1
-}
+# Source function library.
+. /etc/init.d/functions
+  
+binary=@CENTCORE_BINDIR@/centcore
+servicename=$(basename "$0")
+user=@NAGIOS_USER@
+timeout=60
 
+pidfile=${RunDir}/centcore.pid
 
-killproc_centcore () {
-    if test ! -f $RunFile; then
-		echo "No lock file found in $RunFile"
-		return 1
-    fi    
-    PID=`head -n 1 $RunFile`
-    kill -s INT $PID
-}
-
-# Create RunDir if not exit
-rundir_exist() {
-[ -e ${centstorageRunDir} ] || \
-        install -d -o@NAGIOS_USER@ -m750 ${centstorageRunDir}
-}
-
-# Source function library
-# Solaris doesn't have an rc.d directory, so do a test first
-if [ -f /etc/rc.d/init.d/functions ]; then
-    . /etc/rc.d/init.d/functions
-elif [ -f /etc/init.d/functions ]; then
-    . /etc/init.d/functions
+# Check if we can find the binary.
+if [ ! -x $binary ]; then
+    echo -n $"Starting $servicename.";
+    failure $"Executable file $binary not found. Exiting."
+    echo
+    exit 2
 fi
 
-prefix=@CENTREON_DIR@/
-exec_prefix=${prefix}
-Bin=@CENTCORE_BINDIR@/centcore
-CfgFile=@CENTREON_ETC@/conf.pm
-VarDir=${prefix}var/
-LogDir=@CENTREON_LOG@
-RunDir=@CENTREON_RUNDIR@
-RunFile=${RunDir}/centcore.pid
-DemLog=${LogDir}/centcore.log
-LockDir=/var/lock/subsys
-LockFile=centcore
-NICE=5    
-
-# Check that centcore exists.
-if [ ! -f $Bin ]; then
-    echo "Executable file $Bin not found.  Exiting."
-    exit 1
-fi
-
-# Check that centcore.cfg exists.
-if [ ! -f $CfgFile ]; then
-    echo "Configuration file $CfgFile not found.  Exiting."
-    exit 1
-fi
-          
-# See how we were called.
-case "$1" in
-
-    start)
-	    # Check lock file
-	    if test -f $RunFile; then
-		echo "Error : $RunFile already Exists."
-		ISRUNNING=`ps -edf | grep $Bin | grep -v grep | wc -l `
-		if [ $ISRUNNING = 0 ]
-		    then
-		    echo "But no centcore process runnig"
-		    rm -f $RunFile
-		    echo "Removing centcore pid file"
-		else 
-		    exit 1
-		fi
-	    fi
-	    # Test if running directory exist.
-	    rundir_exist
-	    echo "Starting Centcore"
-	    (su - @NAGIOS_USER@ -c "nice -n $NICE $Bin >> $DemLog 2>&1") &
-	    for i in `seq 20` ; do
-            if status_centcore > /dev/null; then
-                break
-            else
-                echo -n '.'
-                sleep 1
-            fi
-        done
-        if status_centcore > /dev/null; then
-                echo ' done.'
-                exit 0
-        else
-                echo ''
-                echo 'Warning - centcore did not start in time'
-                exit 1
+start() {
+	echo -n $"Starting $servicename: "
+        if [ -e "$pidfile" ] && [ -n "$(cat $pidfile)" ] && [ -e "/proc/`cat $pidfile`" ]; then
+                echo -n $"cannot start $servicename: $servicename is already running.";
+                failure $"cannot start $servicename: $servicename already running.";
+                echo
+                return 1
         fi
-	    if [ -d $LockDir ]; then 
-	    	touch $LockDir/$LockFile; 
-	    fi
-	    exit 0
-    ;;
-    
-    stop)
-	    echo "Stopping Centcore"
-	    killproc_centcore centcore
-	    
-	    echo -n 'Waiting for centcore to exit .'
-	    for i in `seq 20` ; do
-			if status_centcore > /dev/null; then
-			    echo -n '.'
-			    sleep 1
-			else
-			    break
-			fi
-	    done
-	    if status_centcore > /dev/null; then
-			echo ''
-			echo 'Warning - running centcore did not exit in time'
-		    else
-			echo ' done.'
-	    fi
-    ;;
-    
-    status)
-	    status_centcore centcore
-    ;;
-    
-    restart)
-	    $0 stop
-	    $0 start
-    ;;
-    
-    reload|force-reload)
-    if test ! -f $RunFile; then
-		$0 start
-    else
-		PID=`head -n 1 $RunFile`
-		if status_centcore > /dev/null; then
-		    killproc_centcore centcore -HUP
-		    echo "done"
-		else
-		    $0 stop
-		    $0 start
-		fi
-    fi
-    ;;
-    
-    *)
-	    echo "Usage: centcore {start|stop|restart|reload|force-reload|status}"
-    	exit 1
-    ;;
-    
+        if [ ! -e "$pidfile" ] ; then
+                pid=$(pidofproc $binary)
+                if [ -n "$pid" ] ; then
+                        echo -n $"cannot start $servicename: $servicename is already running.";
+                        failure $"cannot start $servicename: $servicename already running.";
+                        echo
+                        return 1
+                fi
+        fi
+
+	if [ "$(id -u -n)" = "$user" ] ; then
+		daemon ''$binary' > /dev/null 2>&1 &'
+	else
+		daemon --user $user ''$binary' > /dev/null 2>&1 &'
+	fi
+	pid=$(pidofproc $binary)
+	RETVAL=$?
+	echo $pid > $pidfile
+	success $"service launched"
+	echo
+	return $RETVAL
+}
+
+stop() {
+	echo -n $"Stopping $servicename: "
+        if [ ! -e "$pidfile" ] || [ -z "$(cat $pidfile)" ] ; then
+                killproc -d $timeout "$binary"
+        else
+                killproc -p "$pidfile" -d $timeout "$binary"
+        fi
+        RETVAL=$?
+        echo
+        return $RETVAL
+}	
+
+rhstatus() {
+	status -p "$pidfile" "$binary"
+}	
+
+restart() {
+  	stop
+	start
+}	
+
+reload() {
+	echo -n $"Reloading $servicename daemon configuration: "
+    killproc -p "$pidfile" "$binary" -HUP
+    RETVAL=$?
+    echo
+    return $RETVAL
+}	
+
+case "$1" in
+  start)
+  	start
+	;;
+  stop)
+  	stop
+	;;
+  restart)
+  	restart
+	;;
+  reload)
+  	reload
+	;;
+  status)
+  	rhstatus
+	;;
+  condrestart)
+  	[ -f /var/lock/subsys/centcore ] && restart || :
+	;;
+  *)
+	echo $"Usage: $0 {start|stop|status|reload|restart|condrestart}"
+	exit 1
 esac
-# End of this script
+
