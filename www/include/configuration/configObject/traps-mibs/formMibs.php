7e401a6e38da1ff164440adb5c6ebebaa731cc7a
Maximilien Bersoult <mbersoult@merethis.com>
2012-08-21 08:22:34 +0000

    * Fix #3540 : Security issues when uploading mib file
    git-svn-id: http://svn.centreon.com/trunk/centreon@13371 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configObject/traps-mibs/formMibs.php
+++ www/include/configuration/configObject/traps-mibs/formMibs.php
@@ -115,20 +115,19 @@
 			/*
 			 * Upload File
 			 */
-			$fileObj->moveUploadedFile("/tmp/");
 			$values = $fileObj->getValue();
-			$stdout = shell_exec("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=/tmp/".$values["name"]." --out=/tmp/".$values["name"].".conf 2>&1");
+			$stdout = shell_exec("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=".$values["tmp_name"]." --out=".$values["tmp_name"].".conf 2>&1");
 			if ($debug)
-				print ("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=/tmp/".$values["name"]." --out=/tmp/".$values["name"].".conf 2>&1");
+				print ("export LD_LIBRARY_PATH=".$oreon->optGen["perl_library_path"]." && export MIBS=ALL && ".$oreon->optGen["snmpttconvertmib_path_bin"]." --in=".$values["tmp_name"]." --out=".$values["tmp_name"].".conf 2>&1");
 
 			$msg .= "<br />".str_replace ("\n", "<br />", $stdout);
 			$msg .= "<br />Moving traps in DataBase...";
 
 			if ($debug)
-				print("@CENTPLUGINSTRAPS_BINDIR@/centFillTrapDB -f /tmp/".$values["name"].".conf -m ".htmlentities($ret["mnftr"], ENT_QUOTES, "UTF-8")." 2>&1");
+				print("@CENTPLUGINSTRAPS_BINDIR@/centFillTrapDB -f '".$values["tmp_name"].".conf' -m ".htmlentities($ret["mnftr"], ENT_QUOTES, "UTF-8")." 2>&1");
 
-			$stdout = shell_exec("@CENTPLUGINSTRAPS_BINDIR@/centFillTrapDB -f /tmp/".$values["name"].".conf -m ".htmlentities($ret["mnftr"], ENT_QUOTES, "UTF-8")." 2>&1");
-			shell_exec("rm /tmp/".$values["name"].".conf /tmp/".$values["name"]);
+			$stdout = shell_exec("@CENTPLUGINSTRAPS_BINDIR@/centFillTrapDB -f '".$values["tmp_name"].".conf' -m ".htmlentities($ret["mnftr"], ENT_QUOTES, "UTF-8")." 2>&1");
+			unlink($values['tmp_name'] . '.conf');
 			$msg .= "<br />".str_replace ("\n", "<br />", $stdout);
 			$msg .= "<br />Generate Traps configuration files from Monitoring Engine configuration form!";
 			if ($msg)
