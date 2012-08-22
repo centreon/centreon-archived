9dccf2acb4e27d329fb6b2d2291a15acc14576c8
Lionel Assepo <lassepo@merethis.com>
2012-08-22 12:01:44 +0000

    fixes #3572
    git-svn-id: http://svn.centreon.com/trunk/centreon@13388 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/eventLogs/GetXmlLog.php
+++ www/include/eventLogs/GetXmlLog.php
@@ -857,7 +857,7 @@
 	 */
 	$buffer->writeElement("d", _("Day"), 0);
 	$buffer->writeElement("t", _("Time"), 0);
-	$buffer->writeElement("h", _("Host/Service"), 0);
+	$buffer->writeElement("h", _("Host Status"), 0);
 	$buffer->writeElement("sc", _("Service Status"), 0);
 	$buffer->writeElement("T", _("Type"), 0);
 	$buffer->writeElement("R", _("Retry"), 0);
