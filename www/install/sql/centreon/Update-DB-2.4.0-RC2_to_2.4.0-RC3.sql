f2c1fb5192480c073e0fc16f65002a868e874561
Lionel Assepo <lassepo@merethis.com>
2012-08-29 09:35:13 +0000

    fixes #3587 : Added Mr Charles Judith patches
    git-svn-id: http://svn.centreon.com/trunk/centreon@13452 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/sql/centreon/Update-DB-2.4.0-RC2_to_2.4.0-RC3.sql
+++ www/install/sql/centreon/Update-DB-2.4.0-RC2_to_2.4.0-RC3.sql
@@ -6,3 +6,21 @@ UPDATE `connector` SET `command_line` = '$USER3$/centreon_connector_perl' WHERE 
 UPDATE `connector` SET `command_line` = '$USER3$/centreon_connector_ssh' WHERE `command_line` = '$USER2$/centreon_connector_ssh';
 
 UPDATE `informations` SET `value` = '2.4.0-RC3' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.4.0-RC2' LIMIT 1;
+
+UPDATE cb_field SET description = 'Port to listen on (empty host) or to connect to (with host filled).' WHERE fieldname = 'port';
+UPDATE cb_field SET description = 'IP address or hostname of the host to connect to (leave blank for listening mode).' WHERE fieldname = 'host';
+UPDATE cb_field SET description = 'Trusted CA''s certificate.' WHERE fieldname = 'ca_certificate';
+UPDATE cb_field SET description = 'Private key file path when TLS encryption is used.' WHERE fieldname = 'private_key';
+UPDATE cb_field SET description = 'Public certificate file path when TLS encryption is used.' WHERE fieldname = 'public_cert';
+UPDATE cb_field SET description = 'Enable TLS encryption.' WHERE fieldname = 'tls';
+UPDATE cb_field SET description = 'IP address or hostname of the database server.' WHERE fieldname = 'db_host';
+UPDATE cb_field SET description = 'User for connect to database' WHERE fieldname = 'db_user';
+UPDATE cb_field SET description = 'Used password for connect to the database.' WHERE fieldname = 'db_password';
+UPDATE cb_field SET description = 'Database name.' WHERE fieldname = 'db_name';
+UPDATE cb_field SET description = 'Path to the file.' WHERE fieldname = 'path';
+UPDATE cb_field SET description = 'Serialization protocol.' WHERE fieldname = 'protocol';
+UPDATE cb_field SET description = 'Metrics RRD Directory, for example /var/lib/centreon/metrics' WHERE fieldname = 'metrics_path';
+UPDATE cb_field SET description = 'Status RRD Directory, for example /var/lib/centreon/status' WHERE fieldname = 'status_path';
+UPDATE cb_field SET description = 'Target DBMS.' WHERE fieldname = 'db_type';
+UPDATE cb_field SET description = 'Interval Length in seconds.' WHERE fieldname = 'interval';
+UPDATE cb_field SET description = 'RRD storage duration in seconds.' WHERE fieldname = 'length';
