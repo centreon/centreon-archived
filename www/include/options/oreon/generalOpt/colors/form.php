37f2747a33e2b5a285ea674586464e8a0e5691bb
Maximilien Bersoult <mbersoult@merethis.com>
2012-09-11 10:10:57 +0000

    * Fix bad translation in javascript
    git-svn-id: http://svn.centreon.com/trunk/centreon@13505 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/options/oreon/generalOpt/colors/form.php
+++ www/include/options/oreon/generalOpt/colors/form.php
@@ -3,37 +3,37 @@
  * Copyright 2005-2011 MERETHIS
  * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
- * This program is free software; you can redistribute it and/or modify it under 
- * the terms of the GNU General Public License as published by the Free Software 
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along with 
+ *
+ * You should have received a copy of the GNU General Public License along with
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
- * Linking this program statically or dynamically with other modules is making a 
- * combined work based on this program. Thus, the terms and conditions of the GNU 
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
  * General Public License cover the whole combination.
- * 
- * As a special exception, the copyright holders of this program give MERETHIS 
- * permission to link this program with independent modules to produce an executable, 
- * regardless of the license terms of these independent modules, and to copy and 
- * distribute the resulting executable under terms of MERETHIS choice, provided that 
- * MERETHIS also meet, for each linked independent module, the terms  and conditions 
- * of the license of that module. An independent module is a module which is not 
- * derived from this program. If you modify this program, you may extend this 
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  * SVN : $URL$
  * SVN : $Id$
- * 
+ *
  */
 
 	if (!isset($oreon))
@@ -53,12 +53,12 @@
 	 */
 	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
 	$form->addElement('header', 'title', _("Modify General Options"));
-	
+
 	$form->addElement('header', 'host', _("Hosts status colors"));
 	$form->addElement('header', 'service', _("Services status colors"));
 	$form->addElement('header', 'misc', _("Miscelleneous"));
 	$form->addElement('header', 'hostspec', _("Specifics for hosts"));
-	
+
 	$TabColorNameAndLang = array("color_up"=>_("Host UP Color"),
                             	"color_down"=>_("Host DOWN Color"),
                             	"color_unreachable"=>_("Host UNREACHABLE Color"),
@@ -136,7 +136,7 @@
 				hcolor = bckcolor.substr(1,6);
 			}
 		}
-		Modalbox.show('./include/common/javascript/color_picker_mb.php?name='+name, { title: " . _('Pick a color') . ", width: width, height: height , afterLoad: function(){cp_init(t, hcolor);} });
+		Modalbox.show('./include/common/javascript/color_picker_mb.php?name='+name, { title: \"" . _('Pick a color') . "\", width: width, height: height , afterLoad: function(){cp_init(t, hcolor);} });
 	}
 </script>");
 	/*
@@ -172,7 +172,7 @@
 	$tpl->assign('form', $renderer->toArray());
 	$tpl->assign('o', $o);
 	$tpl->assign("genOpt_colors_properties", _("Status Color Properties"));
-        
+
 	// prepare help texts
 	$helptext = "";
 	include_once("help.php");
@@ -180,7 +180,7 @@
 		$helptext .= '<span style="display:none" id="help:'.$key.'">'.$text.'</span>'."\n";
 	}
 	$tpl->assign("helptext", $helptext);
-        
+
 	$tpl->assign('valid', $valid);
 	$tpl->display("form.ihtml");
 ?>
