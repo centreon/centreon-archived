6c2ebdb566bbc38445fe81f540ca902f021d6f45
Sylvestre Ho <shotamchay@merethis.com>
2012-11-09 13:13:31 +0000

    prepare 2.4.0-RC7
    git-svn-id: http://svn.centreon.com/trunk/centreon@13733 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.4.0-RC6');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.4.0-RC7');
 
 --
 -- Contenu de la table `contact`
