6ea0e132e89c0f191caa1c454cf041f84bf1e3f5
Antoine Nguyen <anguyen@merethis.com>
2012-11-21 12:04:47 +0000

    Indentation update.
    git-svn-id: http://svn.centreon.com/trunk/centreon@13800 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configServers/formServers.php
+++ www/include/configuration/configServers/formServers.php
@@ -36,192 +36,192 @@
  *
  */
 
-	if (!isset($oreon)) {
-		exit();
-	}
+if (!isset($oreon)) {
+    exit();
+}
 
-	/*
-	 * Database retrieve information for Nagios
-	 */
-	$nagios = array();
-	if (($o == "c" || $o == "w") && $server_id)	{
-		$DBRESULT = $pearDB->query("SELECT * FROM `nagios_server` WHERE `id` = '".$server_id."' LIMIT 1");
-		# Set base value
-		$cfg_server = array_map("myDecode", $DBRESULT->fetchRow());
-		$DBRESULT->free();
-	}
+/*
+ * Database retrieve information for Nagios
+ */
+$nagios = array();
+if (($o == "c" || $o == "w") && $server_id) {
+    $DBRESULT = $pearDB->query("SELECT * FROM `nagios_server` WHERE `id` = '".$server_id."' LIMIT 1");
+    # Set base value
+    $cfg_server = array_map("myDecode", $DBRESULT->fetchRow());
+    $DBRESULT->free();
+}
 
-	/*
-	 * nagios servers comes from DB
-	 */
-	$nagios_servers = array();
-	$DBRESULT = $pearDB->query("SELECT * FROM `nagios_server` ORDER BY name");
-	while($nagios_server = $DBRESULT->fetchRow()) {
-		$nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
-	}
-	$DBRESULT->free();
+/*
+ * nagios servers comes from DB
+ */
+$nagios_servers = array();
+$DBRESULT = $pearDB->query("SELECT * FROM `nagios_server` ORDER BY name");
+while($nagios_server = $DBRESULT->fetchRow()) {
+    $nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
+}
+$DBRESULT->free();
 
-	$attrsText		= array("size"=>"30");
-	$attrsText2 	= array("size"=>"50");
-	$attrsText3 	= array("size"=>"5");
-	$attrsTextarea 	= array("rows"=>"5", "cols"=>"40");
+$attrsText      = array("size"=>"30");
+$attrsText2     = array("size"=>"50");
+$attrsText3     = array("size"=>"5");
+$attrsTextarea  = array("rows"=>"5", "cols"=>"40");
 
-	/*
-	 * Form begin
-	 */
-	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
-	if ($o == "a")
-		$form->addElement('header', 'title', _("Add a poller"));
-	else if ($o == "c")
-		$form->addElement('header', 'title', _("Modify a poller Configuration"));
-	else if ($o == "w")
-		$form->addElement('header', 'title', _("View a poller Configuration"));
+/*
+ * Form begin
+ */
+$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
+if ($o == "a")
+    $form->addElement('header', 'title', _("Add a poller"));
+else if ($o == "c")
+    $form->addElement('header', 'title', _("Modify a poller Configuration"));
+else if ($o == "w")
+    $form->addElement('header', 'title', _("View a poller Configuration"));
 
-	/*
-	 * Headers
-	 */
-	$form->addElement('header', 'Server_Informations', _("Server Information"));
-	$form->addElement('header', 'SSH_Informations', _("SSH Information"));
-	$form->addElement('header', 'Nagios_Informations', _("Monitoring Engine Information"));
-	$form->addElement('header', 'Misc', _("Miscelleneous"));
-	$form->addElement('header', 'SNMPTT', _("SNMPTT Traps Collector"));
-	$form->addElement('select', 'monitoring_engine', _("Engine"), array("CENGINE" => "Centreon Engine", "ICINGA" => "Icinga", "NAGIOS" => "Nagios", "SHINKEN" => "Shinken"));
+/*
+ * Headers
+ */
+$form->addElement('header', 'Server_Informations', _("Server Information"));
+$form->addElement('header', 'SSH_Informations', _("SSH Information"));
+$form->addElement('header', 'Nagios_Informations', _("Monitoring Engine Information"));
+$form->addElement('header', 'Misc', _("Miscelleneous"));
+$form->addElement('header', 'SNMPTT', _("SNMPTT Traps Collector"));
+$form->addElement('select', 'monitoring_engine', _("Engine"), array("CENGINE" => "Centreon Engine", "ICINGA" => "Icinga", "NAGIOS" => "Nagios", "SHINKEN" => "Shinken"));
 
-	/*
-	 * Nagios Configuration basic information
-	 */
-	$form->addElement('header', 'information', _("Satellite configuration"));
-	$form->addElement('text', 'name', _("Poller Name"), $attrsText);
-	$form->addElement('text', 'ns_ip_address', _("IP Address"), $attrsText);
-	$form->addElement('text', 'init_script', _("Monitoring Engine Init Script"), $attrsText);
+/*
+ * Nagios Configuration basic information
+ */
+$form->addElement('header', 'information', _("Satellite configuration"));
+$form->addElement('text', 'name', _("Poller Name"), $attrsText);
+$form->addElement('text', 'ns_ip_address', _("IP Address"), $attrsText);
+$form->addElement('text', 'init_script', _("Monitoring Engine Init Script"), $attrsText);
 
-	$form->addElement('text', 'nagios_bin', _("Monitoring Engine Binary"), $attrsText2);
-	$form->addElement('text', 'nagiostats_bin', _("Monitoring Engine Statistics Binary"), $attrsText2);
-	$form->addElement('text', 'nagios_perfdata', _("Perfdata file"), $attrsText2);
+$form->addElement('text', 'nagios_bin', _("Monitoring Engine Binary"), $attrsText2);
+$form->addElement('text', 'nagiostats_bin', _("Monitoring Engine Statistics Binary"), $attrsText2);
+$form->addElement('text', 'nagios_perfdata', _("Perfdata file"), $attrsText2);
 
-	$form->addElement('text', 'ssh_private_key', _("SSH Private key"), $attrsText2);
-	$form->addElement('text', 'ssh_port', _("SSH port"), $attrsText3);
+$form->addElement('text', 'ssh_private_key', _("SSH Private key"), $attrsText2);
+$form->addElement('text', 'ssh_port', _("SSH port"), $attrsText3);
 
-	$Tab = array();
-	$Tab[] = HTML_QuickForm::createElement('radio', 'localhost', null, _("Yes"), '1');
-	$Tab[] = HTML_QuickForm::createElement('radio', 'localhost', null, _("No"), '0');
-	$form->addGroup($Tab, 'localhost', _("Localhost ?"), '&nbsp;');
+$Tab = array();
+$Tab[] = HTML_QuickForm::createElement('radio', 'localhost', null, _("Yes"), '1');
+$Tab[] = HTML_QuickForm::createElement('radio', 'localhost', null, _("No"), '0');
+$form->addGroup($Tab, 'localhost', _("Localhost ?"), '&nbsp;');
 
-	$Tab = array();
-	$Tab[] = HTML_QuickForm::createElement('radio', 'is_default', null, _("Yes"), '1');
-	$Tab[] = HTML_QuickForm::createElement('radio', 'is_default', null, _("No"), '0');
-	$form->addGroup($Tab, 'is_default', _("Is default poller ?"), '&nbsp;');
+$Tab = array();
+$Tab[] = HTML_QuickForm::createElement('radio', 'is_default', null, _("Yes"), '1');
+$Tab[] = HTML_QuickForm::createElement('radio', 'is_default', null, _("No"), '0');
+$form->addGroup($Tab, 'is_default', _("Is default poller ?"), '&nbsp;');
 
-	$Tab = array();
-	$Tab[] = HTML_QuickForm::createElement('radio', 'ns_activate', null, _("Enabled"), '1');
-	$Tab[] = HTML_QuickForm::createElement('radio', 'ns_activate', null, _("Disabled"), '0');
-	$form->addGroup($Tab, 'ns_activate', _("Status"), '&nbsp;');
+$Tab = array();
+$Tab[] = HTML_QuickForm::createElement('radio', 'ns_activate', null, _("Enabled"), '1');
+$Tab[] = HTML_QuickForm::createElement('radio', 'ns_activate', null, _("Disabled"), '0');
+$form->addGroup($Tab, 'ns_activate', _("Status"), '&nbsp;');
 
-	/*
-	 * Centreon Broker
-	 */
-	$form->addElement('header', 'CentreonBroker', _("Centreon Broker"));
-	$form->addElement('text', 'centreonbroker_cfg_path', _("Centreon Broker configuration path"), $attrsText2);
-	$form->addElement('text', 'centreonbroker_module_path', _("Centreon Broker modules path"), $attrsText2);
+/*
+ * Centreon Broker
+ */
+$form->addElement('header', 'CentreonBroker', _("Centreon Broker"));
+$form->addElement('text', 'centreonbroker_cfg_path', _("Centreon Broker configuration path"), $attrsText2);
+$form->addElement('text', 'centreonbroker_module_path', _("Centreon Broker modules path"), $attrsText2);
 
-	/*
-	 * SNMPTT
-	 */
-	$form->addElement('text', 'init_script_snmptt', _("SNMPTT init script path"), $attrsText2);
+/*
+ * SNMPTT
+ */
+$form->addElement('text', 'init_script_snmptt', _("SNMPTT init script path"), $attrsText2);
 
-	/*
-	 * Set Default Values
-	 */
-	if (isset($_GET["o"]) && $_GET["o"] == 'a'){
-		$form->setDefaults(array(
-		"name" => '',
-		"localhost" => '0',
-		"ns_ip_address" => "127.0.0.1",
-		"nagios_bin" => "/usr/sbin/nagios",
-		"nagiostats_bin" => "/usr/bin/nagiostats",
-		"monitoring_engine"  =>  $centreon->optGen["monitoring_engine"],
-		"init_script" => "/etc/init.d/nagios",
-		"ns_activate" => '1',
-		"is_default"  =>  '0',
-		"ssh_port"  =>  '22',
-		"ssh_private_key"  =>  '~/.ssh/rsa.id',
-		"nagios_perfdata"  =>  "/var/log/nagios/service-perfdata",
-		"centreonbroker_cfg_path" => "/etc/centreon/broker",
-		"init_script" => "/etc/init.d/snmptt",
-		"init_script_snmptt" => isset($centreon->optGen["init_script_snmptt"]) ? $centreon->optGen["init_script_snmptt"] : ''));
-	} else {
-		if (isset($cfg_server)) {
-			$form->setDefaults($cfg_server);
-		}
-	}
-	$form->addElement('hidden', 'id');
-	$redirect = $form->addElement('hidden', 'o');
-	$redirect->setValue($o);
+/*
+ * Set Default Values
+ */
+if (isset($_GET["o"]) && $_GET["o"] == 'a'){
+    $form->setDefaults(array(
+    "name" => '',
+    "localhost" => '0',
+    "ns_ip_address" => "127.0.0.1",
+    "nagios_bin" => "/usr/sbin/nagios",
+    "nagiostats_bin" => "/usr/bin/nagiostats",
+    "monitoring_engine"  =>  $centreon->optGen["monitoring_engine"],
+    "init_script" => "/etc/init.d/nagios",
+    "ns_activate" => '1',
+    "is_default"  =>  '0',
+    "ssh_port"  =>  '22',
+    "ssh_private_key"  =>  '~/.ssh/rsa.id',
+    "nagios_perfdata"  =>  "/var/log/nagios/service-perfdata",
+    "centreonbroker_cfg_path" => "/etc/centreon/broker",
+    "init_script" => "/etc/init.d/snmptt",
+    "init_script_snmptt" => isset($centreon->optGen["init_script_snmptt"]) ? $centreon->optGen["init_script_snmptt"] : ''));
+} else {
+    if (isset($cfg_server)) {
+        $form->setDefaults($cfg_server);
+    }
+}
+$form->addElement('hidden', 'id');
+$redirect = $form->addElement('hidden', 'o');
+$redirect->setValue($o);
 
-	/*
-	 * Form Rules
-	 */
-	$form->addRule('nagios_name', _("Name is already in use"), 'exist');
+/*
+ * Form Rules
+ */
+$form->addRule('nagios_name', _("Name is already in use"), 'exist');
 
-	/*
-	 * Smarty template Init
-	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl);
+/*
+ * Smarty template Init
+ */
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
 
-	if ($o == "w")	{
-		/*
-		 * Just watch a nagios information
-		 */
-		if ($centreon->user->access->page($p) != 2)
-			$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&id=".$server_id."'"));
-	    $form->setDefaults($nagios);
-		$form->freeze();
-	} else if ($o == "c")	{
-		/*
-		 * Modify a nagios information
-		 */
-		$subC = $form->addElement('submit', 'submitC', _("Save"));
-		$res = $form->addElement('reset', 'reset', _("Reset"));
-	    $form->setDefaults($nagios);
-	} else if ($o == "a")	{
-		/*
-		 * Add a nagios information
-		 */
-		$subA = $form->addElement('submit', 'submitA', _("Save"));
-		$res = $form->addElement('reset', 'reset', _("Reset"));
-	}
+if ($o == "w")  {
+    /*
+     * Just watch a nagios information
+     */
+    if ($centreon->user->access->page($p) != 2)
+        $form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&id=".$server_id."'"));
+    $form->setDefaults($nagios);
+    $form->freeze();
+} else if ($o == "c")   {
+    /*
+     * Modify a nagios information
+     */
+    $subC = $form->addElement('submit', 'submitC', _("Save"));
+    $res = $form->addElement('reset', 'reset', _("Reset"));
+    $form->setDefaults($nagios);
+} else if ($o == "a")   {
+    /*
+     * Add a nagios information
+     */
+    $subA = $form->addElement('submit', 'submitA', _("Save"));
+    $res = $form->addElement('reset', 'reset', _("Reset"));
+}
 
-	$valid = false;
-	if ($form->validate())	{
-		$nagiosObj = $form->getElement('id');
-		if ($form->getSubmitValue("submitA"))
-			insertServerInDB();
-		else if ($form->getSubmitValue("submitC"))
-			updateServerInDB($nagiosObj->getValue());
-		$o = NULL;
-		$valid = true;
-	}
-	if ($valid)
-		require_once($path."listServers.php");
-	else	{
-		/*
-		 * Apply a template definition
-		 */
-		$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-		$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
-		$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
-		$form->accept($renderer);
-		$tpl->assign('form', $renderer->toArray());
-		$tpl->assign('o', $o);
+$valid = false;
+if ($form->validate())  {
+    $nagiosObj = $form->getElement('id');
+    if ($form->getSubmitValue("submitA"))
+        insertServerInDB();
+    else if ($form->getSubmitValue("submitC"))
+        updateServerInDB($nagiosObj->getValue());
+    $o = NULL;
+    $valid = true;
+}
+if ($valid)
+    require_once($path."listServers.php");
+else    {
+    /*
+     * Apply a template definition
+     */
+    $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+    $renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
+    $renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
+    $form->accept($renderer);
+    $tpl->assign('form', $renderer->toArray());
+    $tpl->assign('o', $o);
 
-		include_once("help.php");
+    include_once("help.php");
 
-		$helptext = "";
-        foreach ($help as $key => $text) {
-            $helptext .= '<span style="display:none" id="help:'.$key.'">'.$text.'</span>'."\n";
-        }
-        $tpl->assign("helptext", $helptext);
-		$tpl->display("formServers.ihtml");
-	}
+    $helptext = "";
+    foreach ($help as $key => $text) {
+        $helptext .= '<span style="display:none" id="help:'.$key.'">'.$text.'</span>'."\n";
+    }
+    $tpl->assign("helptext", $helptext);
+    $tpl->display("formServers.ihtml");
+}
 ?>
