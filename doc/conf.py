0d24337d9095a6ff4dda79d453ca58e00d1d5775
Antoine Nguyen <anguyen@merethis.com>
2012-11-26 14:20:55 +0000

    *readthedocs* compatibility.
    git-svn-id: http://svn.centreon.com/trunk/centreon@13819 6bcd3966-0018-0410-8128-fd23d134de7e

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,8 @@
 
 import sys, os
 
+on_centreon_rtd = os.environ.get('CENTREON_RTD', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -89,17 +91,20 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+if on_centreon_rtd:
+	# The theme to use for HTML and HTML Help pages.  See the documentation for
+	# a list of builtin themes.
+	html_theme = 'centreon-documentation-sphinx-theme'
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
+	# Theme options are theme-specific and customize the look and feel of a theme
+	# further.  For a list of options available for each theme, see the
+	# documentation.
+	#html_theme_options = {}
 
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+	# Add any paths that contain custom themes here, relative to this directory.
+	html_theme_path = ['/srv/themes']
+else:
+	html_theme = 'default'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
