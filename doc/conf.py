cf3aa505847367554b879bec969ae737cdb22842
Antoine Nguyen <anguyen@merethis.com>
2012-11-26 14:36:34 +0000

    Using the right theme when *readthedocs* is in use.
    git-svn-id: http://svn.centreon.com/trunk/centreon@13820 6bcd3966-0018-0410-8128-fd23d134de7e

--- doc/conf.py
+++ doc/conf.py
@@ -94,7 +94,7 @@ pygments_style = 'sphinx'
 if on_centreon_rtd:
 	# The theme to use for HTML and HTML Help pages.  See the documentation for
 	# a list of builtin themes.
-	html_theme = 'centreon-documentation-sphinx-theme'
+	html_theme = 'centreon'
 
 	# Theme options are theme-specific and customize the look and feel of a theme
 	# further.  For a list of options available for each theme, see the
