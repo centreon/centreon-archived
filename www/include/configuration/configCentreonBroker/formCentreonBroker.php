9a1fc38d2b2c490641ac62bd5bfec1e61019e7b7
Antoine Nguyen <anguyen@merethis.com>
2012-12-05 15:19:07 +0000

    Indentation cleanup.
    git-svn-id: http://svn.centreon.com/trunk/centreon@13902 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/include/configuration/configCentreonBroker/formCentreonBroker.php
+++ www/include/configuration/configCentreonBroker/formCentreonBroker.php
@@ -36,153 +36,153 @@
  *
  */
 
-	if (!isset($oreon)) {
-		exit();
-	}
+if (!isset($oreon)) {
+    exit();
+}
 
-	$cbObj = new CentreonConfigCentreonBroker($pearDB);
+$cbObj = new CentreonConfigCentreonBroker($pearDB);
 
-	/*
-	 * nagios servers comes from DB
-	 */
-	$nagios_servers = array();
-	$DBRESULT = $pearDB->query("SELECT * FROM nagios_server ORDER BY name");
-	while($nagios_server = $DBRESULT->fetchRow()) {
-		$nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
-	}
-	$DBRESULT->free();
+/*
+ * nagios servers comes from DB
+ */
+$nagios_servers = array();
+$DBRESULT = $pearDB->query("SELECT * FROM nagios_server ORDER BY name");
+while($nagios_server = $DBRESULT->fetchRow()) {
+    $nagios_servers[$nagios_server["id"]] = $nagios_server["name"];
+}
+$DBRESULT->free();
 
-	##########################################################
-	# Var information to format the element
-	#
-	$attrsText		= array("size"=>"30");
-	$attrsText2 	= array("size"=>"50");
-	$attrsText3 	= array("size"=>"10");
-	$attrsTextarea 	= array("rows"=>"5", "cols"=>"40");
+/*
+ * Var information to format the element
+ */
+$attrsText      = array("size"=>"30");
+$attrsText2     = array("size"=>"50");
+$attrsText3     = array("size"=>"10");
+$attrsTextarea  = array("rows"=>"5", "cols"=>"40");
 
-	/*
-	 * Form begin
-	 */
-	$form = new HTML_QuickForm('Form', 'post', "?p=".$p, '', array('onsubmit' => 'return formValidate()'));
-	if ($o == "a") {
-		$form->addElement('header', 'title', _("Add a Centreon-Broker Configuration"));
-	} else if ($o == "c") {
-		$form->addElement('header', 'title', _("Modify a Centreon-Broker Configuration"));
-	} else if ($o == "w") {
-		$form->addElement('header', 'title', _("View a Centreon-Broker Configuration"));
-	}
+/*
+ * Form begin
+ */
+$form = new HTML_QuickForm('Form', 'post', "?p=".$p, '', array('onsubmit' => 'return formValidate()'));
+if ($o == "a") {
+    $form->addElement('header', 'title', _("Add a Centreon-Broker Configuration"));
+} else if ($o == "c") {
+    $form->addElement('header', 'title', _("Modify a Centreon-Broker Configuration"));
+} else if ($o == "w") {
+    $form->addElement('header', 'title', _("View a Centreon-Broker Configuration"));
+}
 
-	/*
-	 * Smarty template Init
-	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl);
+/*
+ * Smarty template Init
+ */
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
 
-	/*
-	 * TAB 1 - General informations
-	 */
-	$tpl->assign('centreonbroker_configuration', _("Centreon-Broker Informations"));
-	$form->addElement('header', 'information', _("Centreon-Broker configuration"));
-	$form->addElement('text', 'name', _("Name"), $attrsText);
-	$form->addElement('text', 'filename', _("Config file name"), $attrsText);
-	$form->addElement('select', 'ns_nagios_server', _("Requester"), $nagios_servers);
-        $form->addElement('text', 'event_queue_max_size', _('Event Queue Max Size'), $attrsText);
-        
-	$status = array();
-	$status[] = HTML_QuickForm::createElement('radio', 'activate', null, _("Enabled"), 1);
-	$status[] = HTML_QuickForm::createElement('radio', 'activate', null, _("Disabled"), 0);
-	$form->addGroup($status, 'activate', _("Status"), '&nbsp;');
+/*
+ * TAB 1 - General informations
+ */
+$tpl->assign('centreonbroker_configuration', _("Centreon-Broker Informations"));
+$form->addElement('header', 'information', _("Centreon-Broker configuration"));
+$form->addElement('text', 'name', _("Name"), $attrsText);
+$form->addElement('text', 'filename', _("Config file name"), $attrsText);
+$form->addElement('select', 'ns_nagios_server', _("Requester"), $nagios_servers);
+    $form->addElement('text', 'event_queue_max_size', _('Event Queue Max Size'), $attrsText);
+    
+$status = array();
+$status[] = HTML_QuickForm::createElement('radio', 'activate', null, _("Enabled"), 1);
+$status[] = HTML_QuickForm::createElement('radio', 'activate', null, _("Disabled"), 0);
+$form->addGroup($status, 'activate', _("Status"), '&nbsp;');
 
-	$tags = $cbObj->getTags();
+$tags = $cbObj->getTags();
 
-	$tabs = array();
-	foreach ($tags as $tagId => $tag) {
-	     $tabs[] = array('id' => $tag, 'name' => _("Centreon-Broker " . ucfirst($tag)), 'link' => _("Add"), 'nb' => 0, 'blocks' => $cbObj->getListConfigBlock($tagId), 'forms' => array());
-	}
+$tabs = array();
+foreach ($tags as $tagId => $tag) {
+    $tabs[] = array('id' => $tag, 'name' => _("Centreon-Broker " . ucfirst($tag)), 'link' => _("Add"), 'nb' => 0, 'blocks' => $cbObj->getListConfigBlock($tagId), 'forms' => array());
+}
 
-	/*
-	 * Default values
-	 */
-	if (isset($_GET["o"]) && $_GET["o"] == 'a'){
-		$form->setDefaults(array(
-    		"name" => '',
-    		"activate" => '1'
-		));
-		$tpl->assign('config_id', 0);
-	} else {
-		if (isset($_GET['id']) && $_GET['id'] != 0) {
-		    $id = $_GET['id'];
-		    $tpl->assign('config_id', $id);
-			$form->setDefaults(getCentreonBrokerInformation($id));
-			/*
-			 * Get informations for modify
-			 */
-			$nbTabs = count($tabs);
-			for ($i = 0; $i < $nbTabs; $i++) {
-			    $tabs[$i]['forms'] = $cbObj->getForms($id, $tabs[$i]['id'], $p, $tpl);
-			    $tabs[$i]['helps'] = $cbObj->getHelps($id, $tabs[$i]['id']);
-			    $tabs[$i]['nb'] = count($tabs[$i]['forms']);
-			}
-		}
-	}
-	$form->addElement('hidden', 'id');
-	$redirect = $form->addElement('hidden', 'o');
-	$redirect->setValue($o);
+/*
+ * Default values
+ */
+if (isset($_GET["o"]) && $_GET["o"] == 'a'){
+    $form->setDefaults(array(
+        "name" => '',
+        "activate" => '1'
+    ));
+    $tpl->assign('config_id', 0);
+} else {
+    if (isset($_GET['id']) && $_GET['id'] != 0) {
+        $id = $_GET['id'];
+        $tpl->assign('config_id', $id);
+        $form->setDefaults(getCentreonBrokerInformation($id));
+        /*
+         * Get informations for modify
+         */
+        $nbTabs = count($tabs);
+        for ($i = 0; $i < $nbTabs; $i++) {
+            $tabs[$i]['forms'] = $cbObj->getForms($id, $tabs[$i]['id'], $p, $tpl);
+            $tabs[$i]['helps'] = $cbObj->getHelps($id, $tabs[$i]['id']);
+            $tabs[$i]['nb'] = count($tabs[$i]['forms']);
+        }
+    }
+}
+$form->addElement('hidden', 'id');
+$redirect = $form->addElement('hidden', 'o');
+$redirect->setValue($o);
 
-	/*
-	 * Form Rules
-	 */
-	$form->registerRule('exist', 'callback', 'testExistence');
-	$form->addRule('name', _("Mandatory name"), 'required');
-	$form->addRule('name', _("Name is already in use"), 'exist');
-	$form->addRule('filename', _("Mandatory filename"), 'required');
-    $form->addRule('event_queue_max_size', _('Value must be numeric'), 'numeric');
+/*
+ * Form Rules
+ */
+$form->registerRule('exist', 'callback', 'testExistence');
+$form->addRule('name', _("Mandatory name"), 'required');
+$form->addRule('name', _("Name is already in use"), 'exist');
+$form->addRule('filename', _("Mandatory filename"), 'required');
+$form->addRule('event_queue_max_size', _('Value must be numeric'), 'numeric');
 
-	if ($o == "w")	{
-		if ($centreon->user->access->page($p) != 2) {
-			$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&id=".$ndo2db_id."'"));
-		}
-		$form->freeze();
-	} else if ($o == "c")	{
-	    /*
-	     * Modify a Centreon Broker information
-	     */
-		$subC = $form->addElement('submit', 'submitC', _("Save"));
-		$res = $form->addElement('reset', 'reset', _("Reset"));
-	} else if ($o == "a")	{
-	    /*
-	     * Add a nagios information
-	     */
-		$subA = $form->addElement('submit', 'submitA', _("Save"));
-		$res = $form->addElement('reset', 'reset', _("Reset"));
-	}
+if ($o == "w")  {
+    if ($centreon->user->access->page($p) != 2) {
+        $form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&id=".$ndo2db_id."'"));
+    }
+    $form->freeze();
+} else if ($o == "c")   {
+    /*
+     * Modify a Centreon Broker information
+     */
+    $subC = $form->addElement('submit', 'submitC', _("Save"));
+    $res = $form->addElement('reset', 'reset', _("Reset"));
+} else if ($o == "a")   {
+    /*
+     * Add a nagios information
+     */
+    $subA = $form->addElement('submit', 'submitA', _("Save"));
+    $res = $form->addElement('reset', 'reset', _("Reset"));
+}
 
-	$valid = false;
-	if ($form->validate())	{
-		$nagiosObj = $form->getElement('id');
-		if ($form->getSubmitValue("submitA")) {
-			$cbObj->insertConfig($_POST);
-		} else if ($form->getSubmitValue("submitC")) {
-		    $cbObj->updateConfig($_POST['id'], $_POST);
-		}
-		$o = NULL;
-		$valid = true;
-	}
-	if ($valid)
-		require_once($path."listCentreonBroker.php");
-	else	{
-		/*
-		 * Apply a template definition
-		 */
-		$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-		$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
-		$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
-		$form->accept($renderer);
-		$tpl->assign('form', $renderer->toArray());
-		$tpl->assign('o', $o);
-		$tpl->assign('p', $p);
-		$tpl->assign('sort1', _("General"));
-		$tpl->assign('tabs', $tabs);
-		$tpl->display("formCentreonBroker.ihtml");
-	}
+$valid = false;
+if ($form->validate())  {
+    $nagiosObj = $form->getElement('id');
+    if ($form->getSubmitValue("submitA")) {
+        $cbObj->insertConfig($_POST);
+    } else if ($form->getSubmitValue("submitC")) {
+        $cbObj->updateConfig($_POST['id'], $_POST);
+    }
+    $o = NULL;
+    $valid = true;
+}
+if ($valid)
+    require_once($path."listCentreonBroker.php");
+else {
+    /*
+     * Apply a template definition
+     */
+    $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+    $renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
+    $renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
+    $form->accept($renderer);
+    $tpl->assign('form', $renderer->toArray());
+    $tpl->assign('o', $o);
+    $tpl->assign('p', $p);
+    $tpl->assign('sort1', _("General"));
+    $tpl->assign('tabs', $tabs);
+    $tpl->display("formCentreonBroker.ihtml");
+}
 ?>
