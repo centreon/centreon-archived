66f0f51e74136b383ec7cf6ea051fd7643e47c05
Alexandru Vilau <avilau@merethis.com>
2012-12-13 15:44:45 +0000

    Refs #3752; Synchronized schema in createTables.sql with database update in Update-DB-2.4.0-RC6_to_2.4.0-RC7.sql
    git-svn-id: http://svn.centreon.com/trunk/centreon@13990 6bcd3966-0018-0410-8128-fd23d134de7e

--- www/install/createTables.sql
+++ www/install/createTables.sql
@@ -810,7 +810,7 @@ CREATE TABLE `contact` (
   `contact_js_effects` enum('0','1') DEFAULT '0',
   `contact_location` int(11) DEFAULT '0',
   `contact_oreon` enum('0','1') DEFAULT NULL,
-  `contact_enable_notifications` enum('0','1','2') DEFAULT '0',
+  `contact_enable_notifications` enum('0','1','2') DEFAULT '2',
   `contact_template_id` int(11) DEFAULT NULL,
   `contact_admin` enum('0','1') DEFAULT '0',
   `contact_type_msg` enum('txt','html','pdf') DEFAULT 'txt',
