1d430062d54d9a9a1d1144663b316b5402430288
Sylvestre Ho <sho@merethis.com>
2013-02-14 10:26:21 +0100

    fix #4049; remove duplicate <All logs>

--- www/install/sql/centreon/Update-DB-2.4.0_to_2.4.1.sql
+++ www/install/sql/centreon/Update-DB-2.4.0_to_2.4.1.sql
@@ -1,5 +1,4 @@
 -- Ldap group by auth ressource
 ALTER TABLE contactgroup ADD COLUMN ar_id INT;
-DELETE FROM `topology` WHERE `topology_page` = 203 AND `topology_url` = './include/monitoring/mysql_log/viewLog.php';
-
+DELETE FROM `topology` WHERE `topology_name` = 'All Logs' AND `topology_url` = './include/monitoring/mysql_log/viewLog.php';
 UPDATE `informations` SET `value` = '2.4.1' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.4.0' LIMIT 1;
