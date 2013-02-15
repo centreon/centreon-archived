225268068c62c727510cab830b0cf5c15c7de8eb
Julien Mathis <jmathis@merethis.com>
2013-02-15 17:31:15 +0100

    fix #4218

--- www/install/sql/centreon/Update-DB-2.4.0_to_2.4.1.sql
+++ www/install/sql/centreon/Update-DB-2.4.0_to_2.4.1.sql
@@ -5,4 +5,7 @@ DELETE FROM `topology` WHERE `topology_name` = 'All Logs' AND `topology_url` = '
 -- Add Centreon Connector path
 ALTER TABLE nagios_server ADD COLUMN centreonconnector_path VARCHAR(255) DEFAULT NULL AFTER centreonbroker_module_path;
 
+-- Change aggressive_host_checking options
+UPDATE cfg_nagios SET `use_aggressive_host_checking` = '0';
+
 UPDATE `informations` SET `value` = '2.4.1' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.4.0' LIMIT 1;
