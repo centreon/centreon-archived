4470fdcab1fd07fa1ea8f6ecd41896319b84ee0f
Quentin Garnier <qgarnier@merethis.com>
2013-03-19 17:51:40 +0100

    Fix #5140

---
+++ lib/perl/centstorage/CentstorageLib.pm
@@ -0,0 +1,195 @@
+
+package centstorage::CentstorageLib;
+use File::Basename;
+
+my $read_size = 1*1024*1024*10; # 10Mo
+
+sub start_or_not {
+	my ($centreon_db_centreon) = @_;
+	my $status = 1;
+	
+	my ($status2, $stmt) = $centreon_db_centreon->query("SELECT value FROM options WHERE `key` = 'centstorage' LIMIT 1");
+	my $data = $stmt->fetchrow_hashref();
+	if (defined($data) && int($data->{'value'}) == 0) {
+		$status = 0;
+	}
+	return $status;
+}
+
+sub get_main_perfdata_file {
+	my ($centreon_db_centreon) = @_;
+	my $filename;
+
+	my ($status, $stmt) = $centreon_db_centreon->query("SELECT `nagios_perfdata` FROM `nagios_server` WHERE `localhost` = '1'");
+	my $data = $stmt->fetchrow_hashref();
+	if (defined($data)) {
+		$filename = $data->{'nagios_perfdata'};
+	}
+	return ($status, $filename);
+}
+
+sub check_pool_old_perfdata_file {
+	my ($main_filename, $num_pool) = @_;
+
+	my @files = ();
+	for (my $i = 0; $i < $num_pool; $i++) {
+		if (-e $main_filename . "_" . $i . ".bckp_read") {
+			push @files, $main_filename . "_" . $i . ".bckp_read";	
+		}
+		if (-e $main_filename . "_" . $i . ".bckp") {
+			push @files, $main_filename . "_" . $i . ".bckp";	
+		}
+	}
+
+	if (-e $main_filename . "_read") {
+		push @files, $main_filename . "_read";
+	}
+	return \@files;
+}
+
+sub call_pool_rebuild {
+	my ($line, $pool_pipes, $routing_services, $roundrobin_pool_current, $pool_childs, $rebuild_progress, $rebuild_pool_choosen) = @_;
+
+	# Send Info
+	my ($method, $host_name, $service_description) = split(/\t/, $line);
+	my $pool_choosen;
+	if (!defined($routing_services->{$host_name . ";" . $service_description})) {
+		my $pool_num = $$roundrobin_pool_current % $pool_childs;
+		$$roundrobin_pool_current++;
+		$routing_services->{$host_name . ";" . $service_description} = $pool_num;
+	}
+	$pool_choosen = $routing_services->{$host_name . ";" . $service_description};	
+	for ($i = 0; $i < $pool_childs; $i++) {
+		if ($i == $pool_choosen) {
+			my $fh = $pool_pipes->{$i}->{'writer_two'};
+			# It's when you loose a pool. You have to know
+			$$rebuild_progress = 1;
+			$$rebuild_pool_choosen = $pool_choosen;
+			print $fh "REBUILDBEGIN\t$host_name\t$service_description\n";
+		} else {
+			my $fh = $pool_pipes->{$i}->{'writer_two'};
+			print $fh "REBUILDBEGIN\n";
+		}
+	}
+}
+
+sub call_pool_rebuild_finish {
+	my ($pool_pipes, $pool_childs, $delete_pipes, $rebuild_progress, $rebuild_pool_choosen) = @_;
+	my $fh;
+
+	$$rebuild_progress = 0;
+	$$rebuild_pool_choosen = -1;
+	for ($i = 0; $i < $pool_childs; $i++) {
+		if ($rebuild_pool_choosen != $i) {
+			$fh = $pool_pipes->{$i}->{'writer_two'};
+			print $fh "REBUILDFINISH\n";
+		}
+	}
+	$fh = $delete_pipes->{'writer_two'};
+	print $fh "REBUILDFINISH\n";
+}
+
+sub call_pool_rename_clean {
+	my ($line, $pool_pipes, $routing_services, $roundrobin_pool_current, $pool_childs) = @_;
+	
+	my ($method, $old_host_name, $old_service_description, $new_host_name, $new_service_description) = split(/\t/, $line);
+	my $pool_choosen;
+	if (!defined($routing_services->{$old_host_name . ";" . $old_service_description})) {
+		# There is no for the old name. Can go back
+		my $fh = $pool_pipes->{$routing_services->{$new_host_name . ";" . $new_service_description}}->{'writer_two'};
+		print $fh "RENAMEFINISH\t" . $new_host_name . "\t" . $new_service_description . "\n";
+		return ;
+	}
+	# Send to clean
+	my $fh = $pool_pipes->{$routing_services->{$old_host_name . ";" . $old_service_description}}->{'writer_two'};
+	print $fh "RENAMECLEAN\t" . $old_host_name . "\t" . $old_service_description . "\t" . $new_host_name . "\t" . $new_service_description . "\n";
+}
+
+sub call_pool_rename_finish {
+	my ($line, $pool_pipes, $routing_services, $roundrobin_pool_current, $pool_childs) = @_;
+
+	my ($method, $new_host_name, $new_service_description) = split(/\t/, $line);
+	if (defined($routing_services->{$new_host_name . ";" . $new_service_description})) {
+		my $fh = $pool_pipes->{$routing_services->{$new_host_name . ";" . $new_service_description}}->{'writer_two'};
+		print $fh "RENAMEFINISH\t" . $new_host_name . "\t" . $new_service_description . "\n";
+		return ;
+	}
+}
+
+sub call_pool_delete_clean {
+	my ($line, $pool_pipes, $routing_services, $roundrobin_pool_current, $pool_childs) = @_;
+
+	my ($method, $host_name, $service_description) = split(/\t/, $line);
+	if (!defined($routing_services->{$host_name . ";" . $service_description})) {
+		# No cache. so we return
+		return ;
+	}
+	my $fh = $pool_pipes->{$routing_services->{$host_name . ";" . $service_description}}->{'writer_two'};
+	print $fh "$line\n";
+}
+
+sub can_write {
+	my $file = $_[0];
+	my $dir = dirname($file);
+	
+	return 0 if (-d $dir && ! -w $dir);
+	return 0 if (-e $file && ! -w $file);
+	return 1;
+}
+
+sub get_line_file {
+	my ($fh, $datas, $readed) = @_;
+	my $line;
+	my $size = scalar(@$datas);
+
+	return (1, shift(@$datas)) if ($size > 1);
+	while ((my $eof = sysread($fh, $line, $read_size))) {
+		my @result = split("\n", $line);
+		if ($line =~ /\n$/) {
+			push @result, "";
+		}
+		if ($size == 1) {
+			$$datas[0] .= shift(@result);
+		}
+		push @$datas, @result;
+		$$readed += $eof;
+		$size = scalar(@$datas);
+		if ($size > 1) {
+			return (1, shift(@$datas));
+		}
+	}
+	return (1, shift(@$datas)) if ($size > 1);
+	return -1;
+}
+
+sub get_line_pipe {
+	my ($fh, $datas, $read_done) = @_;
+	my $line;
+	my $size = scalar(@$datas);
+
+	if ($size > 1) {
+		return (1, shift(@$datas));
+	} elsif ($size == 1 && $$read_done == 1) {
+		return 0;
+	}
+	while ((my $eof = sysread($fh, $line, 10000))) {
+		$$read_done = 1;
+		my @result = split("\n", $line);
+		if ($line =~ /\n$/) {
+			push @result, "";
+		}
+		if ($size == 1) {
+			$$datas[0] .= shift(@result);
+		}
+		push @$datas, @result;
+		$size = scalar(@$datas);
+		if ($size > 1) {
+			return (1, shift(@$datas));
+		} else {
+			return 0;
+		}
+	}
+	return -1;
+}
+
+1;
