dcef0b0e7075679424c90e5d8933e381b07d8d6d
Sylvestre Ho <sho@merethis.com>
2013-03-26 13:54:33 +0100

    refs #4299; dynamic ndo prefix

--- www/install/sql/brocker/Update-NDO-2.4.1_to_2.4.2.sql
+++
@@ -1,3 +0,0 @@
--- Ticket #4299 --
-ALTER TABLE nagios_acknowledgements ADD INDEX idx_reporting_ack (object_id, acknowledgement_id, entry_time);
-ALTER TABLE nagios_downtimehistory ADD INDEX idx_reporting_donwtime (was_started, actual_start_time, actual_end_time);
