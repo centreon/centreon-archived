961f3a34986d69acb6f057419e6c1e0df2128fc8
Sylvestre Ho <sho@merethis.com>
2013-05-27 10:59:42 +0200

    fixes #4340; remove unnecessary warning messages

--- www/include/configuration/configGenerate/xml/moveFiles.php
+++ www/include/configuration/configGenerate/xml/moveFiles.php
@@ -121,7 +121,7 @@ try {
         $DBRESULT_imgs = $pearDB->query("SELECT `dir_alias`, `img_path` FROM `view_img`, `view_img_dir`, `view_img_dir_relation` WHERE dir_dir_parent_id = dir_id AND img_img_id = img_id");
         while ($images = $DBRESULT_imgs->fetchrow()){
             if (!is_dir($oreon->optGen["nagios_path_img"]."/".$images["dir_alias"])) {
-                $mkdirResult = mkdir($oreon->optGen["nagios_path_img"]."/".$images["dir_alias"]);
+                $mkdirResult = @mkdir($oreon->optGen["nagios_path_img"]."/".$images["dir_alias"]);
             }
             if (file_exists($centreon_path."www/img/media/".$images["dir_alias"]."/".$images["img_path"]))  {
                 $copyResult = @copy($centreon_path."www/img/media/".$images["dir_alias"]."/".$images["img_path"], $oreon->optGen["nagios_path_img"]."/".$images["dir_alias"]."/".$images["img_path"]);
