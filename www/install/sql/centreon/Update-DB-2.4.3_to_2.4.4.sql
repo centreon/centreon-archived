ca72de28450ecf772f44cf612ba50f14315acba8
Lionel Assepo <lassepo@merethis.com>
2013-07-02 14:58:14 +0200

    refs #4536

--- www/install/sql/centreon/Update-DB-2.4.3_to_2.4.4.sql
+++ www/install/sql/centreon/Update-DB-2.4.3_to_2.4.4.sql
@@ -1,5 +1,5 @@
 -- Add size to max for CPU Graph template
-UPDATE `service` SET `service_alias` = 'Swap' WHERE `service_description` = 'SNMP-Linux-Swap';
+UPDATE `service` SET `service_alias` = 'Swap' WHERE `service_description` = 'SNMP-Linux-Swap' AND `service_alias` = 'Memory';
 
 -- Add possibility to lock a service template
 ALTER TABLE `service` ADD `service_locked` BOOLEAN DEFAULT 0 AFTER `command_command_id_arg2`;
