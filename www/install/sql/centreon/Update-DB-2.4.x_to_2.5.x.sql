53df452d520548482327c11767fc7393643e5d4b
Lionel Assepo <lassepo@merethis.com>
2013-07-02 17:14:13 +0200

    closes #4440 closes #4586

--- www/install/sql/centreon/Update-DB-2.4.x_to_2.5.x.sql
+++ www/install/sql/centreon/Update-DB-2.4.x_to_2.5.x.sql
@@ -10,6 +10,7 @@ ALTER TABLE `traps` ADD COLUMN `traps_routing_value` varchar(255) DEFAULT NULL A
 ALTER TABLE `traps` ADD COLUMN `traps_advanced_treatment_default` enum('0','1') DEFAULT '0' AFTER `traps_advanced_treatment`;
 
 ALTER TABLE `cfg_nagios` ADD COLUMN `use_setpgid` enum('0','1','2') DEFAULT NULL AFTER `enable_environment_macros`;
+ALTER TABLE `cfg_nagios` ADD COLUMN `use_check_result_path` enum('0','1') DEFAULT '0' AFTER `check_result_path`;
 UPDATE `cfg_nagios` SET `use_setpgid` = '2';
 
 
@@ -55,10 +56,27 @@ UPDATE `topology` SET `readonly` = '0' WHERE `topology_parent` = '608' AND `topo
 ALTER TABLE  `traps` CHANGE  `traps_args`  `traps_args` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
 -- ticket #4536
-UPDATE `service` SET `service_alias` = 'Swap' WHERE `service_description` = 'SNMP-Linux-Swap';
+UPDATE `service` SET `service_alias` = 'Swap' WHERE `service_description` = 'SNMP-Linux-Swap' AND `service_alias` = 'Memory';
 
 -- Ticket #4201
 INSERT INTO `cb_list_values` (`cb_list_id`, `value_name`, `value_value`) VALUE (2, 'BBDO Protocol', 'bbdo');
+
+-- Ticket #4440
+INSERT INTO cb_field(`cb_field_id`, `fieldname`, `displayname`, `description`, `fieldtype`, `external`)
+       values(42, 'store_in_data_bin', 'Store in data_bin', ' It should be enabled to control whether or not Centreon Broker should insert performance data in the data_bin table.', 'radio', NULL);
+INSERT INTO cb_type_field_relation(`cb_type_id`, `cb_field_id`, `is_required`, `order_display`)
+       values(14, 42, 1, 9); 
+INSERT INTO cb_list(`cb_list_id`, `cb_field_id`, `default_value`)
+       values(1, 42, 'yes');
+
+-- Ticket #4586
+INSERT INTO cb_field(`cb_field_id`, `fieldname`, `displayname`, `description`, `fieldtype`, `external`)
+       values(43, 'insert_in_index_data', 'Insert in index data', 'Whether or not Broker should create entries in the index_data table. This process should be done by Centreon and this option should only be enabled by advanced users knowing what they\'re doing', 'radio', NULL); 
+INSERT INTO cb_type_field_relation(`cb_type_id`, `cb_field_id`, `is_required`, `order_display`)
+       values(14, 43, 1, 10);
+INSERT INTO cb_list(`cb_list_id`, `cb_field_id`, `default_value`)
+       values(1, 43, 'yes');   
+
 
 -- Add option to not inherit host contacts and contactgroups Ticket #4498
 ALTER TABLE `service` ADD COLUMN `service_inherit_contacts_from_host` enum('0','1') DEFAULT '1' AFTER `service_notifications_enabled`;
