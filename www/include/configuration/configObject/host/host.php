2165949e7c816def9e80a6b8f4d4e7c4aeaa55df
Julien Mathis <jmathis@merethis.com>
2013-09-17 16:08:20 +0200

    improve source code

--- www/include/configuration/configObject/host/host.php
+++ www/include/configuration/configObject/host/host.php
@@ -31,74 +31,77 @@
  *
  * For more information : contact@centreon.com
  *
- * SVN : $URL$
- * SVN : $Id$
- *
  */
 
-	if (!isset($oreon))
-		exit ();
+if (!isset($oreon)) {
+    exit ();
+}
 
-	isset($_GET["host_id"]) ? $hG = $_GET["host_id"] : $hG = NULL;
-	isset($_POST["host_id"]) ? $hP = $_POST["host_id"] : $hP = NULL;
-	$hG ? $host_id = $hG : $host_id = $hP;
+isset($_GET["host_id"]) ? $hG = $_GET["host_id"] : $hG = NULL;
+isset($_POST["host_id"]) ? $hP = $_POST["host_id"] : $hP = NULL;
+$hG ? $host_id = $hG : $host_id = $hP;
 
-	isset($_GET["select"]) ? $cG = $_GET["select"] : $cG = NULL;
-	isset($_POST["select"]) ? $cP = $_POST["select"] : $cP = NULL;
-	$cG ? $select = $cG : $select = $cP;
+isset($_GET["select"]) ? $cG = $_GET["select"] : $cG = NULL;
+isset($_POST["select"]) ? $cP = $_POST["select"] : $cP = NULL;
+$cG ? $select = $cG : $select = $cP;
 
-	isset($_GET["dupNbr"]) ? $cG = $_GET["dupNbr"] : $cG = NULL;
-	isset($_POST["dupNbr"]) ? $cP = $_POST["dupNbr"] : $cP = NULL;
-	$cG ? $dupNbr = $cG : $dupNbr = $cP;
+isset($_GET["dupNbr"]) ? $cG = $_GET["dupNbr"] : $cG = NULL;
+isset($_POST["dupNbr"]) ? $cP = $_POST["dupNbr"] : $cP = NULL;
+$cG ? $dupNbr = $cG : $dupNbr = $cP;
 
-	/*
-	 * Pear library
-	 */
-	require_once "HTML/QuickForm.php";
-	require_once 'HTML/QuickForm/advmultiselect.php';
-	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+/*
+ * Pear library
+ */
+require_once "HTML/QuickForm.php";
+require_once 'HTML/QuickForm/advmultiselect.php';
+require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
 
-	/*
-	 * Path to the configuration dir
-	 */
-	global $path;
-	$path = "./include/configuration/configObject/host/";
+/*
+ * Path to the configuration dir
+ */
+global $path;
 
-	/*
-	 * PHP functions
-	 */
-	require_once $path."DB-Func.php";
-	require_once "./include/common/common-Func.php";
+$path = "./include/configuration/configObject/host/";
 
-	if (isset($_POST["o1"]) && isset($_POST["o2"])){
-		if ($_POST["o1"] != "")
-			$o = $_POST["o1"];
-		if ($_POST["o2"] != "")
-			$o = $_POST["o2"];
-	}
+/*
+ * PHP functions
+ */
+require_once $path."DB-Func.php";
+require_once "./include/common/common-Func.php";
 
-	/* Set the real page */
-	if ($ret['topology_page'] != "" && $p != $ret['topology_page'])
-		$p = $ret['topology_page'];
+if (isset($_POST["o1"]) && isset($_POST["o2"])){
+    if ($_POST["o1"] != "") {
+        $o = $_POST["o1"];
+    }
+    if ($_POST["o2"] != "") {
+        $o = $_POST["o2"];
+    }
+ }
 
-    $acl = $oreon->user->access;
-    $dbmon = $oreon->broker->getBroker() == 'broker' ? new CentreonDB('centstorage') : new CentreonDB('ndo');
-    $aclDbName = $acl->getNameDBAcl($oreon->broker->getBroker());
-    $hgs = $acl->getHostGroupAclConf(null, $oreon->broker->getBroker());
-    $aclHostString = $acl->getHostsString('ID', $dbmon);
-    $aclPollerString = $acl->getPollerString();
+/* Set the real page */
+if ($ret['topology_page'] != "" && $p != $ret['topology_page']) {
+    $p = $ret['topology_page'];
+ }
 
-	switch ($o)	{
-		case "a" 	: require_once($path."formHost.php"); break; #Add a host
-		case "w" 	: require_once($path."formHost.php"); break; #Watch a host
-		case "c" 	: require_once($path."formHost.php"); break; #Modify a host
-		case "mc" 	: require_once($path."formHost.php"); break; # Massive Change
-		case "s" 	: enableHostInDB($host_id); require_once($path."listHost.php"); break; #Activate a host
-		case "ms" 	: enableHostInDB(NULL, isset($select) ? $select : array()); require_once($path."listHost.php"); break;
-		case "u" 	: disableHostInDB($host_id); require_once($path."listHost.php"); break; #Desactivate a host
-		case "mu" 	: disableHostInDB(NULL, isset($select) ? $select : array()); require_once($path."listHost.php"); break;
-		case "m" 	: multipleHostInDB(isset($select) ? $select : array(), $dupNbr); require_once($path."listHost.php"); break; #Duplicate n hosts
-		case "d" 	: deleteHostInDB(isset($select) ? $select : array()); require_once($path."listHost.php"); break; #Delete n hosts
-		default 	: require_once($path."listHost.php"); break;
-	}
+$acl = $oreon->user->access;
+$dbmon = $oreon->broker->getBroker() == 'broker' ? new CentreonDB('centstorage') : new CentreonDB('ndo');
+$aclDbName = $acl->getNameDBAcl($oreon->broker->getBroker());
+$hgs = $acl->getHostGroupAclConf(null, $oreon->broker->getBroker());
+$aclHostString = $acl->getHostsString('ID', $dbmon);
+$aclPollerString = $acl->getPollerString();
+
+switch ($o)	{
+ case "a" 	: require_once($path."formHost.php"); break; #Add a host
+ case "w" 	: require_once($path."formHost.php"); break; #Watch a host
+ case "c" 	: require_once($path."formHost.php"); break; #Modify a host
+ case "mc" 	: require_once($path."formHost.php"); break; # Massive Change
+ case "s" 	: enableHostInDB($host_id); require_once($path."listHost.php"); break; #Activate a host
+ case "ms" 	: enableHostInDB(NULL, isset($select) ? $select : array()); require_once($path."listHost.php"); break;
+ case "u" 	: disableHostInDB($host_id); require_once($path."listHost.php"); break; #Desactivate a host
+ case "mu" 	: disableHostInDB(NULL, isset($select) ? $select : array()); require_once($path."listHost.php"); break;
+ case "m" 	: multipleHostInDB(isset($select) ? $select : array(), $dupNbr); require_once($path."listHost.php"); break; #Duplicate n hosts
+ case "d" 	: deleteHostInDB(isset($select) ? $select : array()); require_once($path."listHost.php"); break; #Delete n hosts
+ default 	: require_once($path."listHost.php"); break;
+}
+
 ?>
