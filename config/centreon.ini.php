27a9f7469914297ff91438c47a16677231613cb4
Julien Mathis <julien.mathis@gmail.com>
2014-01-06 15:26:13 +0100

    add options for configuration file

--- config/centreon.ini.php
+++ config/centreon.ini.php
@@ -1,6 +1,24 @@
 <?php
 
-require_once "/etc/centreon/centreon.conf.php";
+/* 
+ * MySQL Connection
+ */
+$conf_centreon['hostCentreon'] = "localhost";
+$conf_centreon['hostCentstorage'] = "localhost";
+$conf_centreon['user'] = "centreon";
+$conf_centreon['password'] = "XKvg2oj19CW4";
+$conf_centreon['db'] = "centreon";
+$conf_centreon['dbcstg'] = "centreon_storage";
+$conf_centreon['port'] = "3306";
+
+
+ini_set("include_path", ini_get("include_path").":/srv/centreon-3/www/class");
+
+/* path to classes */
+$classdir='/srv/centreon-3/class';
+
+/* Centreon Path */
+$centreon_path='/srv/centreon-3/';
 
 /*
  * Enable Errors for PHP Notice
