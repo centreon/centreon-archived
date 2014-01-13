5de0316d0a4f0d3787415dda11c2238272ed4c8e
Maximilien Bersoult <mbersoult@merethis.com>
2014-01-13 15:24:59 +0100

    * Refs #5112 : Add class for configuration * Add template of configuration file

---
+++ application/class/Centreon/Core/Config.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * Copyright 2005-2014 MERETHIS
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give MERETHIS
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of MERETHIS choice, provided that
+ * MERETHIS also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+namespace Centreon\Core;
+
+/**
+ * Class for Centreon Configuration
+ *
+ * @see http://www.php.net/manual/en/class.pdo.php
+ * @authors Maximilien Bersoult
+ * @package Centreon
+ * @subpackage Core
+ */
+class Config
+{
+    const FILE_GROUP = array('db_centreon', 'db_storage');
+    private $config = null;
+
+    /**
+     * Constructor
+     *
+     * @param $filename string The configuration filename, this file is in ini format
+     * @throws \Centreon\Core\Exception The configuration file is not readable
+     */
+    public function __construct($filename)
+    {
+        if (is_readable($filename)) {
+            throw new Exception("The configuration file is not readable.");
+        }
+        $this->config = parse_ini_file($filename);
+    }
+
+    /**
+     * Load configuration from Centreon database
+     */
+    public function loadFromDb()
+    {
+        $di = Di::getDefault();
+        /* @Todo test if in cache and load from cache */
+    }
+
+    /**
+     * Get a configuration value
+     *
+     * @param $group string The group of configuration
+     * @param $var string The variable name
+     * @param $default mixed The default value if the variable doesn't exists
+     * @return mixed
+     */
+    public function get($group, $var, $default=null)
+    {
+        if (isset($this->config[$group]) && isset($this->config[$group][$var])) {
+            return $this->config[$group][$var];
+        }
+        return $default;
+    }
+
+    /**
+     * Set a configuration variable
+     *
+     * @param $group string The group of configuration
+     * @param $var string The variable name
+     * @param $value mixed The value to store
+     * @throws The group is not permit for store in database
+     */
+    public function set($group, $var, $value)
+    {
+        if (in_array($group, self::FILE_GROUP)) {
+            throw new Exception("This configuration group is not permit.");
+        }
+        /* @Todo save into DB */
+        /* @Todo update cache */
+        if (false === isset($this->config[$group)) {
+            $this->config[$group] = array();
+        }
+        $this->config[$group][$var] = $value;
+    }
+}
