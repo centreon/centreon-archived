31c55c4a239840ab0a4ff294b432e271b0dd3dd6
Lionel Assepo <lassepo@merethis.com>
2014-01-16 17:57:15 +0100

    refs #5102

--- tests/Centreon/Core/TemplateTest.php
+++ tests/Centreon/Core/TemplateTest.php
@@ -33,6 +33,46 @@ class TemplateTest extends \PHPUnit_Framework_TestCase
     
     public function testTmpl()
     {
+        $expectedTemplate = <<<'EOD'
+<html>
+    
+    <head>
+        <title>Centreon - Home</title>
+                    </head>
+    
+    <body>
         
+        <div id="appLayout">
+            
+            <div id="appHeader">
+                My Header
+            </div>
+
+            <div id="appBody">
+                <div id="appLeftPanel">
+    My Menu
+</div>
+                <div id="appRightPanel">
+    My Content
+</div>
+            </div>
+
+            <div id="appFooter">
+                My Footer
+            </div>
+            
+        </div>
+        
+    </body>
+    
+</html>
+EOD;
+
+        $config = new Config(DATA_DIR . '/test-template.ini');
+        $di = new Di();
+        $di->setShared('config', $config);
+        $tpl = new Template();
+        $printedResult = $tpl->fetch('home/home.tpl');
+        $this->assertEquals($expectedTemplate, $printedResult);
     }
 }
