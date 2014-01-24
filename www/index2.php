d9dfdcc9e247d9431b1e6531ca2e475ee668a057
Maximilien Bersoult <mbersoult@merethis.com>
2014-01-24 10:53:11 +0100

    * Change bootstrap and prepare index

--- www/index2.php
+++ www/index2.php
@@ -1,2 +1,9 @@
 <?php
-    require_once "../bootstrap.php";
+require_once "../bootstrap.php";
+
+new \Centreon\Core\Session();
+session_start();
+
+/* Dispatch route */
+$router = Di::getDefault()->get('router');
+$router->dispatch();
