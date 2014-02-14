2de217b4ff4fc2d237159a31d514fe6cee1fba47
Maximilien Bersoult <mbersoult@merethis.com>
2014-02-14 15:38:49 +0100

    * Refs #5134, #5135, #5160, #5161, #5162, #5165, #5170, #5171, #5172 : Fix search for listing

--- application/class/Smarty/function.datatable.php
+++ application/class/Smarty/function.datatable.php
@@ -14,12 +14,14 @@ function smarty_function_datatable($params, $smarty)
     $smarty->assign('objectAddUrl', $params['objectAddUrl']);
     
     $datatableParameters = \Centreon\Core\Datatable::getConfiguration($params['object']);
+    $datatableParameters['nbFixedTr'] = 1;
     
     // Process Column
     $dCol = array();
     $depth = \Centreon\Repository\Repository::array_depth($datatableParameters['column']);
     foreach ($datatableParameters['column'] as $columnLabel => $columnContent) {
         if (is_array($columnContent)) {
+            $datatableParameters['nbFixedTr'] = 2;
             $dCol['firstLevel'][$columnLabel]['lab'] = $columnLabel;
             $dCol['firstLevel'][$columnLabel]['att'] = 'colspan="'.count($columnContent).'"';
             foreach ($columnContent as $subColumnLabel => $subColumnName) {
