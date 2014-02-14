55f1aeba3f2324fea801d7606a9e7e735dcfa1f8
Sylvestre Ho <sho@merethis.com>
2014-02-14 15:18:25 +0100

    fix menu table structure

--- www/install/sql/centreon/Update-DB-2.x_to_3.0.0.sql
+++ www/install/sql/centreon/Update-DB-2.x_to_3.0.0.sql
@@ -26,7 +26,7 @@ CREATE TABLE `menus` (
     `menu_id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
     `parent_id` int(11),
-    `url` varchar(255) NOT NULL,
+    `url` varchar(255) DEFAULT NULL,
     `icon_class`varchar(100),
     `icon` varchar(255),
     `bgcolor` varchar(55),
@@ -37,6 +37,19 @@ CREATE TABLE `menus` (
     CONSTRAINT `fk_menu_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `menus` (`menu_id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 --
+
+-- Ticket #5193
+CREATE TABLE `acl_routes` (
+    `acl_route_id` int(11) NOT NULL AUTO_INCREMENT,
+    `route` varchar(255) NOT NULL, 
+    `permission` int(11) NOT NULL,
+    `acl_group_id` int(11),
+    PRIMARY KEY (`acl_route_id`),
+    KEY `acl_group_id` (`acl_group_id`),
+    CONSTRAINT `fk_acl_route_group_id` FOREIGN KEY (`acl_group_id`) REFERENCES `acl_groups`(`acl_group_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+
 
 -- New session table
 DROP TABLE `session`;
