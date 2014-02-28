d8699e5d6ed3c59150e7e55d2368aa3dfa526297
Julien Mathis <jmathis@merethis.com>
2014-02-28 17:34:29 +0100

    fix  #5305

--- www/include/configuration/configObject/connector/help.php
+++ www/include/configuration/configObject/connector/help.php
@@ -3,6 +3,6 @@ $help = array();
 
 $help["connector_name"] = dgettext("help", "Name which will be used for identifying the connector.");
 $help["connector_description"] = dgettext("help", "A short description of the connector.");
-$help["command_line"] = dgettext("help", 'This will be executed by Centreon Engine, note that this line contains macros that will be replaced before execution. e.g: $USER3$/centreon_connector_perl');
+$help["command_line"] = dgettext("help", 'The connector binary that centreon-engine will launch.');
 $help["connector_status"] = dgettext("help", "Whether or not the connector is enabled.");
 ?>
