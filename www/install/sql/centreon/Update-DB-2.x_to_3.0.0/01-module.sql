cfb8b5430d33f379f8e7e836cfbb88a9a1392bc8
Lionel Assepo <lassepo@merethis.com>
2014-03-17 17:27:33 +0100

    refs #5329

---
+++ www/install/sql/centreon/Update-DB-2.x_to_3.0.0/01-module.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `module` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=latin1;
+
