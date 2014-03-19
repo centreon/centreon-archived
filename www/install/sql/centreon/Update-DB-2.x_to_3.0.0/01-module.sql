35b25ed4cdef66a84e20d62d83e04d830df769a0
Maximilien Bersoult <mbersoult@merethis.com>
2014-03-19 16:25:33 +0100

    * Fix charset and typo

--- www/install/sql/centreon/Update-DB-2.x_to_3.0.0/01-module.sql
+++ www/install/sql/centreon/Update-DB-2.x_to_3.0.0/01-module.sql
@@ -2,5 +2,5 @@ CREATE TABLE `module` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
