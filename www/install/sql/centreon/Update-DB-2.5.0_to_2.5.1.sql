0b92fd0beb59d09cf2396252e528dcd080852c80
Sylvestre Ho <sho@merethis.com>
2014-03-27 17:31:46 +0100

    fixes #5374;

--- www/install/sql/centreon/Update-DB-2.5.0_to_2.5.1.sql
+++ www/install/sql/centreon/Update-DB-2.5.0_to_2.5.1.sql
@@ -6,7 +6,8 @@ ALTER TABLE `nagios_server` CHANGE `init_script_snmptt` `init_script_centreontra
 UPDATE nagios_server SET init_script_centreontrapd = "/etc/init.d/centreontrapd" WHERE init_script_centreontrapd = "/etc/init.d/snmptt";
 
 -- #5374
-UPDATE topology SET readonly = '0' WHERE topology_page = 60104;
+UPDATE topology SET readonly = '0' WHERE topology_page IN (60104, 60106, 60216);
+UPDATE topology SET readonly = '1' WHERE topology_page IN (60804, 60708);
 
 -- #5399
 INSERT INTO `topology_JS` (`id_page`, `o`, `PathName_js`, `Init`) VALUES (601,NULL,'./include/common/javascript/jquery/plugins/sheepit/jquery.sheepItPlugin.min.js',NULL);
