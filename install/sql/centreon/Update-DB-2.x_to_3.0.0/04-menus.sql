7dc70973347e1e4e9480dcb1464ffefcf66cb74a
Lionel Assepo <lassepo@merethis.com>
2014-04-22 16:48:14 +0200

    Change 'menus' table structure

--- install/sql/centreon/Update-DB-2.x_to_3.0.0/04-menus.sql
+++ install/sql/centreon/Update-DB-2.x_to_3.0.0/04-menus.sql
@@ -1,17 +1,19 @@
 -- Menu with for routing
 -- @TODO See if icon is a relation to a image table
 CREATE TABLE `menus` (
-    `menu_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `short_name` VARCHAR(255) NOT NULL,
-    `parent_id` INT UNSIGNED,
-    `url` VARCHAR(255) DEFAULT NULL,
-    `icon_class` VARCHAR(100),
-    `icon` VARCHAR(255),
-    `bgcolor` VARCHAR(55),
-    `is_module` BOOLEAN DEFAULT 0,
-    `menu_order` TINYINT(5) DEFAULT 0,
-    PRIMARY KEY(`menu_id`),
-    KEY `parent_id` (`parent_id`),
-    CONSTRAINT `fk_menu_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `menus` (`menu_id`) ON DELETE SET NULL
+  `menu_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `short_name` varchar(255) NOT NULL,
+  `parent_id` int unsigned DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `icon_class` varchar(100) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL,
+  `bgcolor` varchar(55) DEFAULT NULL,
+  `module_id` int(10) unsigned NOT NULL,
+  `menu_order` tinyint(5) DEFAULT '0',
+  PRIMARY KEY (`menu_id`),
+  KEY `parent_id` (`parent_id`),
+  KEY `fk_menus_1_idx` (`module_id`),
+  CONSTRAINT `fk_menus_1` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_menu_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `menus` (`menu_id`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
