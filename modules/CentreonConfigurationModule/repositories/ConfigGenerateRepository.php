d19bfe1b44068bb1d834896d5b2bb92f14dd64b5
Julien Mathis <jmathis@merethis.com>
2014-05-06 18:11:39 +0200

    add basic generator engine for hosts

--- modules/CentreonConfigurationModule/repositories/ConfigGenerateRepository.php
+++ modules/CentreonConfigurationModule/repositories/ConfigGenerateRepository.php
@@ -86,6 +86,9 @@ class ConfigGenerateRepository
         /* Templates config files */
         HosttemplateRepository::generateHostTemplates($this->filesDir, $poller_id, $this->path, "/objects/hostTemplates.cfg");
 
+        /* Monitoring Object files */
+        HostRepository::generateHosts($this->filesDir, $poller_id, $this->path, "/objects/hosts.cfg");
+
         /* Generate Main File */
         ConfigGenerateMainRepository::generateMainFile($this->filesDir, $poller_id, $this->path, "/centengine.cfg");
 
