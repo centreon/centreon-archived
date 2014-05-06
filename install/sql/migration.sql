5a7a989a3395f91c033a052210ef8b3a72f15011
Lionel Assepo <lassepo@merethis.com>
2014-05-06 15:29:38 +0200

    refs #5511
    Add migration.sql file to be called before the propel migration

---
+++ install/sql/migration.sql
@@ -0,0 +1,16 @@
+ALTER TABLE custom_view_user_relation DROP FOREIGN KEY fk_custom_views_usergroup_id;
+ALTER TABLE custom_view_user_relation DROP FOREIGN KEY fk_custom_views_user_id;
+ALTER TABLE custom_view_user_relation DROP FOREIGN KEY fk_custom_view_user_id;
+ALTER TABLE `centreon`.`custom_view_user_relation` CHANGE COLUMN `user_id` `user_id` INT(11) NOT NULL , ADD PRIMARY KEY (`custom_view_id`, `user_id`);
+DROP TABLE `session`;
+CREATE TABLE `session` (
+    `session_id` VARCHAR(255) NOT NULL,
+    `user_id` INT NOT NULL,
+    `session_start_time` INT NOT NULL,
+    `last_reload` INT NOT NULL,
+    `ip_address` VARCHAR(45) NOT NULL,
+    `route` VARCHAR(255) NOT NULL,
+    `update_acl` BOOLEAN DEFAULT 0,
+    PRIMARY KEY(`session_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE widget_preferences DROP FOREIGN KEY fk_widget_view_id;
