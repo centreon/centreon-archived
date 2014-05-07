73af98160037f53ddbd4c37d73f01fbf116d247a
Lionel Assepo <lassepo@merethis.com>
2014-05-07 10:14:44 +0200

    refs 5414

---
+++ modules/CentreonHomeModule/install/Installer.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CentreonHome\Install;
+
+/**
+ * 
+ */
+class Installer extends \Centreon\Custom\Module\AbstractModule
+{
+    /**
+     * 
+     * @param type $moduleInfo
+     */
+    public function __construct($moduleDirectory, $moduleInfo)
+    {
+        parent::__construct($moduleDirectory, $moduleInfo);
+    }
+    
+    /**
+     * 
+     */
+    public function customInstall()
+    {
+        
+    }
+    
+    /**
+     * 
+     */
+    public function customRemove()
+    {
+        
+    }
+}
