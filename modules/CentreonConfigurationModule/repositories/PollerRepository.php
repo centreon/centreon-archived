d64673d85b3efaea0d0a2b1885aacba6759f4ce9
Lionel Assepo <lassepo@merethis.com>
2014-05-14 12:09:01 +0200

    refs #5538

--- modules/CentreonConfigurationModule/repositories/PollerRepository.php
+++ modules/CentreonConfigurationModule/repositories/PollerRepository.php
@@ -46,12 +46,6 @@ class PollerRepository extends \CentreonConfiguration\Repository\Repository
      *
      * @var string
      */
-    public static $tableName = 'nagios_server';
-    
-    /**
-     *
-     * @var string
-     */
     public static $objectName = 'Poller';
     
     /**
@@ -62,8 +56,14 @@ class PollerRepository extends \CentreonConfiguration\Repository\Repository
         '<input id="allPoller" class="allPoller" type="checkbox">' => 'id',
         'Name' => 'name',
         'Ip Address' => 'ns_ip_address',
-        'Last restart' => 'last_restart',
-        'Engine name' => 'monitoring_engine',
+        'Localhost' => 'last_restart',
+        'Is Running' => 'is_currently_running',
+        'Has changed' => 'hasChanged',
+        'PID' => 'process_id',
+        'Start time' => 'start_time',
+        'Last Update' => 'last_restart',
+        'Version' => 'version',
+        'Default' => 'ns_activate',
         'Status' => 'ns_activate'
     );
     
@@ -101,10 +101,44 @@ class PollerRepository extends \CentreonConfiguration\Repository\Repository
                 'linkName' => '::name::'
             )
         ),
+        'program_start_time' => array(
+            'type' => 'date',
+            'parameters' => array(
+                'date' => 'd/m/Y H:i:s'
+            )
+        ),
         'last_restart' => array(
             'type' => 'date',
             'parameters' => array(
                 'date' => 'd/m/Y H:i:s'
+            )
+        ),
+        'localhost' => array(
+            'type' => 'select',
+            'parameters' => array(
+                '0' => 'No',
+                '1' => 'Yes'
+            )
+        ),
+        'hasChanged' => array(
+            'type' => 'select',
+            'parameters' => array(
+                '0' => 'No',
+                '1' => 'Yes'
+            )
+        ),
+        'is_currently_running' => array(
+            'type' => 'select',
+            'parameters' => array(
+                '0' => 'No',
+                '1' => 'Yes'
+            )
+        ),
+        'is_default' => array(
+            'type' => 'select',
+            'parameters' => array(
+                '0' => 'No',
+                '1' => 'Yes'
             )
         ),
         'ns_activate' => array(
@@ -124,8 +158,34 @@ class PollerRepository extends \CentreonConfiguration\Repository\Repository
         'none',
         'text',
         'text',
+        array(
+            'select' => array(
+                'Yes' => '1',
+                'No' => '0'
+            )
+        ),
+        array(
+            'select' => array(
+                'Yes' => '1',
+                'No' => '0'
+            )
+        ),
+        array(
+            'select' => array(
+                'Yes' => '1',
+                'No' => '0'
+            )
+        ),
         'none',
         'none',
+        'none',
+        'text',
+        array(
+            'select' => array(
+                'Yes' => '1',
+                'No' => '0'
+            )
+        ),
         array(
             'select' => array(
                 'Enabled' => '1',
@@ -151,4 +211,164 @@ class PollerRepository extends \CentreonConfiguration\Repository\Repository
             )
         ),
     );
+    
+    /**
+     * 
+     * @param array $params
+     * @return array
+     */
+    public static function getDatasForDatatable($params)
+    {
+        // Get centreon DB and centreon storage DB connection
+        $di = \Centreon\Internal\Di::getDefault();
+        $dbconn = $di->get('db_centreon');
+        $dbconnStorage = $di->get('db_storage');
+        
+        // Get List of Nagios Servers
+        $sqlNagiosServer = "SELECT SQL_CALC_FOUND_ROWS id, name, localhost, is_default, last_restart, ns_ip_address, ns_activate FROM nagios_server "
+            . ""
+            . "ORDER BY name";
+        $stmtNagiosServer = $dbconn->query($sqlNagiosServer);
+        $resultNagiosServer = $stmtNagiosServer->fetchAll(\PDO::FETCH_ASSOC);
+        
+        
+        $sqlBroker = "SELECT start_time AS program_start_time, running AS is_currently_running, pid AS process_id, instance_id, name AS instance_name , last_alive FROM instances";
+        $stmtBroker = $dbconnStorage->query($sqlBroker);
+        $resultBroker = $stmtBroker->fetchAll(\PDO::FETCH_ASSOC);
+        
+        
+        $pollerNumber = count($resultNagiosServer);
+        $sqlBroker2 = "SELECT DISTINCT instance_id, version AS program_version, engine AS program_name, name AS instance_name FROM instances LIMIT $pollerNumber";
+        $stmtBroker2 = $dbconnStorage->query($sqlBroker2);
+        $resultBroker2 = $stmtBroker2->fetchAll(\PDO::FETCH_ASSOC);
+        
+        
+        // Build Up the line
+        foreach ($resultNagiosServer as &$nagiosServer) {
+            foreach ($resultBroker as $key => $broker) {
+                if ($broker['instance_name'] == $nagiosServer['name']) {
+                    $nagiosServer = array_merge($nagiosServer, $broker);
+                    unset($resultBroker[$key]);
+                }
+            }
+            foreach ($resultBroker2 as $key => $broker2) {
+                if ($broker2['instance_name'] == $nagiosServer['name']) {
+                    $nagiosServer = array_merge($nagiosServer, $broker2);
+                    unset($resultBroker2[$key]);
+                }
+            }
+            $nagiosServer['hasChanged'] = self::checkChangeState($nagiosServer['id'], $nagiosServer['last_restart']);
+        }
+        
+        $countTab = count($resultNagiosServer);
+        $objectTab = array();
+        for ($i=0; $i<$countTab; $i++) {
+            $objectTab[] = array(
+                static::$objectName,
+                static::$moduleName
+            );
+        }
+        
+        static::formatDatas($resultNagiosServer);
+        
+        return self::arrayValuesRecursive(
+            \array_values(
+                \Centreon\Internal\Datatable::removeUnwantedFields(
+                    static::$moduleName,
+                    static::$objectName,
+                    \array_map(
+                        "\\Centreon\\Internal\\Datatable::castResult",
+                        $resultNagiosServer,
+                        $objectTab
+                    )
+                )
+            )
+        );
+    }
+    
+    /**
+     * 
+     * @param array $resultSet
+     */
+    public static function formatDatas(&$resultSet)
+    {
+        $cache = $resultSet;
+        $resultSet = array();
+        
+        $nbPoller = count($cache);
+        for ($i=0; $i < $nbPoller; $i++) {
+            $resultSet[$i]['id'] = $cache[$i]['id'];
+            $resultSet[$i]['name'] = $cache[$i]['name'];
+            $resultSet[$i]['ns_ip_address'] = $cache[$i]['ns_ip_address'];
+            $resultSet[$i]['localhost'] = $cache[$i]['localhost'];
+            $resultSet[$i]['is_currently_running'] = $cache[$i]['is_currently_running'];
+            $resultSet[$i]['hasChanged'] = $cache[$i]['hasChanged'];
+            $resultSet[$i]['process_id'] = $cache[$i]['process_id'];
+            $resultSet[$i]['program_start_time'] = $cache[$i]['program_start_time'];
+            $resultSet[$i]['last_restart'] = $cache[$i]['last_restart'];
+            $resultSet[$i]['version'] = $cache[$i]['program_name'] . ' ' . $cache[$i]['program_version'];
+            $resultSet[$i]['is_default'] = $cache[$i]['is_default'];
+            $resultSet[$i]['ns_activate'] = $cache[$i]['ns_activate'];
+        }
+    }
+    
+    /**
+	 *
+	 * Check if a service or an host has been
+	 * changed for a specific poller.
+	 * @param unknown_type $poller_id
+	 * @param unknown_type $last_restart
+	 * @return number
+	 */
+	public static function checkChangeState($poller_id, $last_restart)
+    {
+        if (!isset($last_restart) || $last_restart == "") {
+			return 0;
+		}
+        
+        // Get centreon DB and centreon storage DB connection
+        $di = \Centreon\Internal\Di::getDefault();
+        $dbconnStorage = $di->get('db_storage');
+
+		$request = "SELECT *
+						FROM log_action
+						WHERE
+							action_log_date > $last_restart AND
+							((object_type = 'host' AND
+							object_id IN (
+								SELECT host_host_id
+								FROM centreon.ns_host_relation
+								WHERE nagios_server_id = '$poller_id'
+							)) OR
+							(object_type = 'service') AND
+							object_id IN (
+								SELECT service_service_id
+								FROM centreon.ns_host_relation nhr, centreon.host_service_relation hsr
+								WHERE nagios_server_id = '$poller_id' AND hsr.host_host_id = nhr.host_host_id
+						))";
+		$DBRESULT = $dbconnStorage->query($request);
+		if ($DBRESULT->rowCount()) {
+			return 1;
+		}
+		return 0;
+	}
+    
+    /**
+     * 
+     * @param array $params
+     * @return integer
+     */
+    public static function getTotalRecordsForDatatable($params)
+    {
+        // Get centreon DB and centreon storage DB connection
+        $di = \Centreon\Internal\Di::getDefault();
+        $dbconn = $di->get('db_centreon');
+        
+        // 
+        $sqlCalNagiosServer = "SELECT COUNT(`id`) as nb_poller FROM `nagios_server`";
+        $stmtCalNagiosServer = $dbconn->query($sqlCalNagiosServer);
+        $resultCalNagiosServer = $stmtCalNagiosServer->fetchAll(\PDO::FETCH_ASSOC);
+        
+        return $resultCalNagiosServer[0]['nb_poller'];
+    }
 }
