e41237202dd8e9198e2d5c645b70327c82f68a04
Lionel Assepo <lassepo@merethis.com>
2014-06-13 11:42:45 +0200

    refs #5538
        * Add new informations in the listing

--- modules/CentreonConfigurationModule/internal/PollerDatatable.php
+++ modules/CentreonConfigurationModule/internal/PollerDatatable.php
@@ -36,7 +36,7 @@
 
 namespace CentreonConfiguration\Internal;
 
-use \Centreon\Internal\Datatable\Datasource\CentreonDb;
+use \Centreon\Internal\Utils\Datetime;
 
 /**
  * Description of PollerDatatable
@@ -115,7 +115,23 @@ class PollerDatatable extends \Centreon\Internal\ExperimentalDatatable
             'type' => 'string',
             'visible' => true,
         ),
-        /*array (
+        array (
+            'title' => 'Localhost',
+            'name' => 'localhost',
+            'data' => 'localhost',
+            'orderable' => true,
+            'searchable' => true,
+            'type' => 'string',
+            'visible' => true,
+            'cast' => array(
+                'type' => 'select',
+                'parameters' => array(
+                    '0' => 'No',
+                    '1' => 'Yes'
+                )
+            ),
+        ),
+        array (
             'title' => 'Is Running',
             'name' => 'is_currently_running',
             'data' => 'is_currently_running',
@@ -123,6 +139,14 @@ class PollerDatatable extends \Centreon\Internal\ExperimentalDatatable
             'searchable' => true,
             'type' => 'string',
             'visible' => true,
+            'source' => 'other',
+            'cast' => array(
+                'type' => 'select',
+                'parameters' => array(
+                    '0' => '<span class="label label-danger">No</span>',
+                    '1' => '<span class="label label-success">Yes</span>'
+                )
+            ),
         ),
         array (
             'title' => 'Has changed',
@@ -132,43 +156,70 @@ class PollerDatatable extends \Centreon\Internal\ExperimentalDatatable
             'searchable' => true,
             'type' => 'string',
             'visible' => true,
+            'source' => 'other',
+            'cast' => array(
+                'type' => 'select',
+                'parameters' => array(
+                    '0' => 'No',
+                    '1' => '<span class="label label-warning">Yes</span>'
+                )
+            ),
         ),
         array (
             'title' => 'Start time',
-            'name' => 'start_time',
-            'data' => 'start_time',
+            'name' => 'program_start_time',
+            'data' => 'program_start_time',
             'orderable' => true,
             'searchable' => true,
             'type' => 'string',
             'visible' => true,
+            'source' => 'other'
         ),
         array (
-            'title' => 'Last Update',
+            'title' => 'Last Restart',
             'name' => 'last_restart',
             'data' => 'last_restart',
             'orderable' => true,
             'searchable' => true,
             'type' => 'string',
-            'visible' => true,
+            'visible' => false,
         ),
         array (
             'title' => 'Version',
-            'name' => 'version',
-            'data' => 'version',
+            'name' => 'program_version',
+            'data' => 'program_version',
             'orderable' => true,
             'searchable' => true,
             'type' => 'string',
             'visible' => true,
+            'source' => 'other'
         ),
         array (
             'title' => 'Last Update',
-            'name' => 'last_restart',
-            'data' => 'last_restart',
+            'name' => 'last_alive',
+            'data' => 'last_alive',
             'orderable' => true,
             'searchable' => true,
             'type' => 'string',
             'visible' => true,
-        ),*/
+            'source' => 'other'
+        ),
+        array (
+            'title' => 'Default',
+            'name' => 'is_default',
+            'data' => 'is_default',
+            'orderable' => true,
+            'searchable' => true,
+            'type' => 'string',
+            'visible' => true,
+            'cast' => array(
+                'type' => 'select',
+                'parameters' => array(
+                    '0' => 'No',
+                    '1' => 'Yes'
+                )
+            ),
+        ),
         array (
             'title' => 'Status',
             'name' => 'ns_activate',
@@ -194,5 +245,71 @@ class PollerDatatable extends \Centreon\Internal\ExperimentalDatatable
     public function __construct($params, $objectModelClass = '')
     {
         parent::__construct($params, $objectModelClass);
+    }
+    
+    /**
+     * 
+     * @param type $resultSet
+     */
+    public static function addAdditionnalDatas(&$resultSet)
+    {
+        // Get datatabases connections
+        $di = \Centreon\Internal\Di::getDefault();
+        $dbconn = $di->get('db_centreon');
+        $dbconnStorage = $di->get('db_storage');
+        
+        $sqlBroker = "SELECT start_time AS program_start_time, running AS is_currently_running, instance_id, name AS instance_name , last_alive FROM instances";
+        $stmtBroker = $dbconnStorage->query($sqlBroker);
+        $resultBroker = $stmtBroker->fetchAll(\PDO::FETCH_ASSOC);
+        
+        
+        $pollerNumber = count($resultSet);
+        $sqlBroker2 = "SELECT DISTINCT instance_id, version AS program_version, engine AS program_name, name AS instance_name FROM instances LIMIT $pollerNumber";
+        $stmtBroker2 = $dbconnStorage->query($sqlBroker2);
+        $resultBroker2 = $stmtBroker2->fetchAll(\PDO::FETCH_ASSOC);
+        
+        
+        // Build Up the line
+        foreach ($resultSet as &$nagiosServer) {
+            foreach ($resultBroker as $key => $broker) {
+                if ($broker['instance_name'] == $nagiosServer['name']) {
+                    $nagiosServer = array_merge($nagiosServer, $broker);
+                    unset($resultBroker[$key]);
+                }
+            }
+            foreach ($resultBroker2 as $key => $broker2) {
+                if ($broker2['instance_name'] == $nagiosServer['name']) {
+                    $nagiosServer = array_merge($nagiosServer, $broker2);
+                    unset($resultBroker2[$key]);
+                }
+            }
+            $nagiosServer['hasChanged'] = \CentreonConfiguration\Repository\PollerRepository::checkChangeState($nagiosServer['id'], $nagiosServer['last_restart']);
+        }
+    }
+    
+    /**
+     * 
+     * @param type $resultSet
+     */
+    protected function formatDatas(&$resultSet)
+    {
+        foreach ($resultSet as &$myPollerSet) {
+            $myPollerSet['program_version'] = $myPollerSet['program_name'] . ' ' . $myPollerSet['program_version'];
+            $myPollerSet['last_restart'] = Datetime::humanReadable(
+                $myPollerSet['last_restart'],
+                Datetime::PRECISION_FORMAT,
+                2
+            );
+            $myPollerSet['last_alive'] = Datetime::humanReadable(
+                $myPollerSet['last_alive'],
+                Datetime::PRECISION_FORMAT,
+                2
+            );
+            $myPollerSet['program_start_time'] = Datetime::humanReadable(
+                $myPollerSet['program_start_time'],
+                Datetime::PRECISION_FORMAT,
+                2
+            );
+        }
     }
 }
