2f7d0cff3eebb7d124331be7890b671dbc522287
Remi Werquin <rwerquin@merethis.com>
2014-07-28 15:18:51 +0200

    no sql update

--- www/install/sql/centreon/Update-DB-2.5.1_to_2.5.2.sql
+++ www/install/sql/centreon/Update-DB-2.5.1_to_2.5.2.sql
@@ -1,5 +1,3 @@
 
-ALTER TABLE `nagios_server` CHANGE `init_script_snmptt` `init_script_centreontrapd` VARCHAR(255);
-
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.5.2' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.5.1' LIMIT 1;
