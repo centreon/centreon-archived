3b29ea36142d3ed6568100847eac8033dd678c72
Sylvestre Ho <sho@merethis.com>
2014-09-10 09:13:51 +0200

    fix syntax error

--- core/internal/Form/Generator.php
+++ core/internal/Form/Generator.php
@@ -177,8 +177,8 @@ class Generator
                     . 'f.type, f.help, f.help_url, f.advanced, mandatory, parent_field, child_actions '
                     . 'FROM cfg_forms_fields f, cfg_forms_blocks_fields_relations bfr '
                     . 'WHERE bfr.block_id='.$block['block_id'].' '
-		    . 'AND bfr.field_id = f.field_id ' .
-		    . "AND bfr.product_version = '" . $this->productVersion . "' " .
+		    . 'AND bfr.field_id = f.field_id ' 
+		    . "AND bfr.product_version = '" . $this->productVersion . "' " 
                     . 'ORDER BY rank ASC';
                 
                 $this->formComponents[$section['name']][$block['name']] = array();
