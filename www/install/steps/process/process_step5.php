075d7555a9a153c7bef8ca7b0dd21dfbbdf05335
Maximilien Bersoult <mbersoult@merethis.com>
2014-10-21 09:28:08 +0200

    * Fix #5496 : Better regex for validate email during installation

--- www/install/steps/process/process_step5.php
+++ www/install/steps/process/process_step5.php
@@ -6,7 +6,7 @@ foreach ($_POST as $key => $value) {
 $mandatoryFields = array('ADMIN_PASSWORD', 'confirm_password', 'firstname', 'lastname', 'email');
 $strError = '';
 
-$emailRegexp = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[_a-z0-9-]+(\.[_a-z0-9-]+)*$/';
+$emailRegexp = "/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
 if (!preg_match($emailRegexp, $_POST['email'])) {
     $strError .= 'jQuery("input[name=email]").next().html("Invalid E-mail");';
 }
