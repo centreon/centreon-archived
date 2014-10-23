5179f27c82258e40e9aff53443b18f29527a1608
Maximilien Bersoult <mbersoult@merethis.com>
2014-10-23 10:18:58 +0200

    * Save custom Centreon Broker value when add a poller

--- modules/CentreonBrokerModule/repositories/BrokerRepository.php
+++ modules/CentreonBrokerModule/repositories/BrokerRepository.php
@@ -36,6 +36,7 @@
 namespace CentreonBroker\Repository;
 
 use Centreon\Internal\Di;
+use CentreonConfiguration\Internal\Poller\Template\Manager as PollerTemplateManager;
 
 /**
  * @author Sylvestre Ho <sho@merethis.com>
@@ -89,6 +90,128 @@ class BrokerRepository
         $stmt->bindParam(':broker_module_directory', $arr['broker_module_directory'], \PDO::PARAM_STR);
         $stmt->bindParam(':broker_logs_directory', $arr['broker_logs_directory'], \PDO::PARAM_STR);
         $stmt->bindParam(':broker_data_directory', $arr['broker_data_directory'], \PDO::PARAM_STR);
+        $stmt->execute();
+
+        /* Save extract params */
+        $listTpl = PollerTemplateManager::buildTemplatesList();
+        $tmpl = $params['poller_tmpl'];
+        if (!isset($listTpl[$tmpl])) {
+            return;
+        }
+        $fileTpl = $listTpl[$tmpl]->getBrokerPath();
+        $information = json_decode(file_get_contents($fileTpl), true);
+        $listType = array('output', 'input', 'logger');
+        /* setup */
+        foreach ($information['content']['broker']['setup']  as $setup) {
+            /* mode */
+            foreach ($setup['params']['mode'] as $mode) {
+                /* type */
+                foreach ($mode as $type => $config) {
+                    /* @todo one peer retention */
+                    if ($type == 'normal') {
+                        /* module */
+                        foreach ($config as $module) {
+                            $configId = static::insertConfig($pollerId, $module['general']['name']);
+                            foreach ($listType as $type) {
+                                if (isset($module[$type])) {
+                                    $groupNb = 1;
+                                    foreach ($module[$type] as $typeInfo) {
+                                        /* Key */
+                                        foreach ($typeInfo as $key => $value) {
+                                            if (preg_match("/%([\w_]+)%/", $value, $matches)) {
+                                                if (isset($params[$matches[1]])) {
+                                                    $parsedValue = preg_replace("/%([\w_]+)%/", $params[$matches[1]], $value);
+                                                    static::insertUserInfo($configId, $type, $groupNb, $key, $parsedValue);
+                                                }
+                                            }
+                                        }
+                                        $groupNb++;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Add a configuration for a module
+     *
+     * @param int $pollerId The poller id
+     * @param string $configName The configuration module name
+     */
+    public static function insertConfig($pollerId, $configName) {
+        $dbconn = Di::getDefault()->get('db_centreon');
+        /* Test if the configuration is in database */
+        $query = "SELECT config_id
+            FROM cfg_centreonbroker
+            WHERE poller_id = :poller_id
+            AND config_name = :config_name";
+        $stmt = $dbconn->prepare($query);
+        $stmt->bindParam(':poller_id', $pollerId, \PDO::PARAM_INT);
+        $stmt->bindParam(':config_name', $configName, \PDO::PARAM_STR);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        $stmt->closeCursor();
+        if (false !== $row) {
+            return $row['config_id'];
+        }
+        $queryInsert = "INSERT INTO cfg_centreonbroker
+            (poller_id, config_name) VALUES
+            (:poller_id, :config_name)";
+        $stmt = $dbconn->prepare($queryInsert);
+        $stmt->bindParam(':poller_id', $pollerId, \PDO::PARAM_INT);
+        $stmt->bindParam(':config_name', $configName, \PDO::PARAM_STR);
+        $stmt->execute();
+        return static::insertConfig($pollerId, $configName);
+    }
+
+    /**
+     * Add or update a custom information for Centreon Broker
+     *
+     * @param int $pollerId The poller id
+     * @param string $group The group name
+     * @param int $groupId The group id
+     * @param string $key The configuration name
+     * @param string $value The configuration value
+     */
+    public static function insertUserInfo($configId, $group, $groupId, $key, $value)
+    {
+        $dbconn = Di::getDefault()->get('db_centreon');
+        /* Test if the information is already in database */
+        $query = "SELECT COUNT(*) as nb
+            FROM cfg_centreonbroker_info
+            WHERE config_id = :config_id
+                AND config_key = :config_key
+                AND config_group = :config_group
+                AND config_group_id = :config_group_id";
+        $stmt = $dbconn->prepare($query);
+        $stmt->bindParam(':config_id', $configId, \PDO::PARAM_INT);
+        $stmt->bindParam(':config_key', $key, \PDO::PARAM_STR);
+        $stmt->bindParam(':config_group', $group, \PDO::PARAM_STR);
+        $stmt->bindParam(':config_group_id', $groupId, \PDO::PARAM_INT);
+        $stmt->execute();
+        $row = $stmt->fetch();
+        if ($row['nb'] > 0) {
+            $query = "UPDATE cfg_centreonbroker_info SET
+                config_key = :config_key,
+                config_group = :config_group,
+                config_group_id = :config_group_id,
+                config_value = :config_value,
+                WHERE config_id = :config_id";
+        } else {
+            $query = "INSERT INTO cfg_centreonbroker_info
+                (config_id, config_key, config_value, config_group, config_group_id) VALUES
+                (:config_id, :config_key, :config_value, :config_group, :config_group_id)";
+        }
+        $stmt = $dbconn->prepare($query);
+        $stmt->bindParam(':config_id', $configId, \PDO::PARAM_INT);
+        $stmt->bindParam(':config_key', $key, \PDO::PARAM_STR);
+        $stmt->bindParam(':config_value', $value, \PDO::PARAM_STR);
+        $stmt->bindParam(':config_group', $group, \PDO::PARAM_STR);
+        $stmt->bindParam(':config_group_id', $groupId, \PDO::PARAM_INT);
         $stmt->execute();
     }
 
