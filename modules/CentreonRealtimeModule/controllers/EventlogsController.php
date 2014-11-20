474dc6bb6429f40e270cc4a81ecaf8c469cb3573
Sylvestre Ho <sho@merethis.com>
2014-11-20 11:22:02 +0100

    init router

--- modules/CentreonRealtimeModule/controllers/EventlogsController.php
+++ modules/CentreonRealtimeModule/controllers/EventlogsController.php
@@ -125,6 +125,8 @@ class EventlogsController extends \Centreon\Internal\Controller
      */
     public function getLastNbEventsOfHostAction()
     {
+        $router = Di::getDefault()->get('router');
+
         $requestParams = $this->getParams('named');
         $hostId = $requestParams['host_id'];
         $lastNb = $requestParams['last_nb'];
