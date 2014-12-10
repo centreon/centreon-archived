3846a80b06f1c5bfe16c344c4e4b99ddce7909ec
Lionel Assepo <lassepo@merethis.com>
2014-12-10 09:59:53 +0100

    * Fix wrong author name ;)

--- modules/CentreonConfigurationModule/api/rest/ServiceApi.php
+++ modules/CentreonConfigurationModule/api/rest/ServiceApi.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Copyright 2005-2014 MERETHIS
- * Centreon is developped by : Lionel Assepo and Romain Le Merlus under
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
