4b9cac7b10c7a1b52ca2a9707de957f597bba96f
Sylvestre Ho <sho@merethis.com>
2015-01-22 16:44:45 +0100

    refs #6098; changing the format seems to be impactful, lets change the authentication part instead

--- www/include/configuration/configObject/contact/DB-Func.php
+++ www/include/configuration/configObject/contact/DB-Func.php
@@ -360,8 +360,8 @@
 		$rq .= "NULL, ";
 		isset($ret["timeperiod_tp_id"]) && $ret["timeperiod_tp_id"] != NULL ? $rq .= "'".$ret["timeperiod_tp_id"]."', ": $rq .= "NULL, ";
 		isset($ret["timeperiod_tp_id2"]) && $ret["timeperiod_tp_id2"] != NULL ? $rq .= "'".$ret["timeperiod_tp_id2"]."', ": $rq .= "NULL, ";
-		isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".$pearDB->escape($ret["contact_name"])."', ": $rq .= "NULL, ";
-		isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".$pearDB->escape($ret["contact_alias"])."', ": $rq .= "NULL, ";
+		isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".htmlentities($ret["contact_name"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+		isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".htmlentities($ret["contact_alias"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
 		isset($ret["contact_autologin_key"]) && $ret["contact_autologin_key"] != NULL ? $rq .= "'".htmlentities($ret["contact_autologin_key"], ENT_QUOTES)."', ": $rq .= "NULL, ";
 		if ($encryptType == 1)
 			isset($ret["contact_passwd"]) && $ret["contact_passwd"] != NULL ? $rq .= "'".md5($ret["contact_passwd"])."', ": $rq .= "NULL, ";
@@ -495,9 +495,9 @@
 		# If we are doing a MC, we don't have to set name and alias field
 		if (!$from_MC)	{
 			$rq .= "contact_name = ";
-			isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".$pearDB->escape($ret["contact_name"])."', ": $rq .= "NULL, ";
+			isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".htmlentities($ret["contact_name"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
 			$rq .= "contact_alias = ";
-			isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".$pearDB->escape($ret["contact_alias"])."', ": $rq .= "NULL, ";
+			isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".htmlentities($ret["contact_alias"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
 			$rq.= "contact_autologin_key = ";
 			isset($ret["contact_autologin_key"]) && $ret["contact_autologin_key"] != NULL ? $rq .= "'".htmlentities($ret["contact_autologin_key"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
 		}
