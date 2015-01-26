1e3181f42f67ab94495ccc427bcbdcdb143c3df6
Julien Mathis <jmathis@merethis.com>
2015-01-26 18:14:18 +0100

    fix #5794

--- www/install/sql/centreon/Update-DB-2.5.x_to_2.6.0.sql
+++ www/install/sql/centreon/Update-DB-2.5.x_to_2.6.0.sql
@@ -3,5 +3,7 @@ ALTER TABLE `traps` ADD COLUMN `traps_downtime` enum('0','1','2') DEFAULT '0' AF
 ALTER TABLE `traps` ADD COLUMN `traps_output_transform` varchar(255) DEFAULT NULL AFTER `traps_downtime`;
 ALTER TABLE `traps` ADD COLUMN `traps_routing_filter_services` varchar(255) DEFAULT NULL AFTER `traps_routing_value`;
 
+ALTER TABLE auth_ressource_info CHANGE ari_value ari_value VARCHAR(1024);
+
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.6.0' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.5.x' LIMIT 1;
