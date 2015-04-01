df21b0566b364b0a99a2b0640de98c38441c1fe2
Lionel Assepo <lassepo@merethis.com>
2015-04-01 17:15:40 +0200

    * Add support for Global Resources

---
+++ modules/CentreonConfigurationModule/controllers/ResourceController.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Copyright 2005-2014 CENTREON
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give CENTREON
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of CENTREON choice, provided that
+ * CENTREON also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : resource@centreon.com
+ *
+ */
+
+namespace CentreonConfiguration\Controllers;
+
+use Centreon\Controllers\FormController;
+
+class ResourceController extends FormController
+{
+    protected $objectDisplayName = 'Resource';
+    public static $objectName = 'resource';
+    public static $enableDisableFieldName = 'enabled';
+    protected $objectBaseUrl = '/centreon-configuration/resource';
+    protected $datatableObject = '\CentreonConfiguration\Internal\ResourceDatatable';
+    protected $objectClass = '\CentreonConfiguration\Models\Resource';
+    protected $repository = '\CentreonConfiguration\Repository\ResourceRepository';    
+    public static $isDisableable = true;
+    
+    public static $relationMap = array(
+        'resource_pollers' => '\CentreonConfiguration\Models\Relation\Resource\Poller'
+    );
+
+    /**
+     * Commands for specific resource
+     *
+     * @method get
+     * @route /resource/[i:id]/poller
+     */
+    public function commandsForResourceAction()
+    {
+        parent::getRelations(static::$relationMap['resource_pollers']);
+    }
+}
---
+++ modules/CentreonConfigurationModule/internal/ResourceDatatable.php
@@ -0,0 +1,155 @@
+<?php
+
+/*
+ * Copyright 2005-2014 CENTREON
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ * 
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation ; either version 2 of the License.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with 
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ * 
+ * Linking this program statically or dynamically with other modules is making a 
+ * combined work based on this program. Thus, the terms and conditions of the GNU 
+ * General Public License cover the whole combination.
+ * 
+ * As a special exception, the copyright holders of this program give CENTREON 
+ * permission to link this program with independent modules to produce an executable, 
+ * regardless of the license terms of these independent modules, and to copy and 
+ * distribute the resulting executable under terms of CENTREON choice, provided that 
+ * CENTREON also meet, for each linked independent module, the terms  and conditions 
+ * of the license of that module. An independent module is a module which is not 
+ * derived from this program. If you modify this program, you may extend this 
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ * 
+ * For more information : contact@centreon.com
+ * 
+ */
+
+namespace CentreonConfiguration\Internal;
+
+use Centreon\Internal\Datatable\Datasource\CentreonDb;
+use Centreon\Internal\Datatable;
+
+/**
+ * Description of ResourceDatatable
+ *
+ * @author lionel
+ */
+class ResourceDatatable extends Datatable
+{
+    protected static $dataprovider = '\Centreon\Internal\Datatable\Dataprovider\CentreonDb';
+    
+    /**
+     *
+     * @var type 
+     */
+    protected static $datasource = '\CentreonConfiguration\Models\Resource';
+    
+    /**
+     *
+     * @var type 
+     */
+    protected static $rowIdColumn = array('id' => 'resource_id', 'name' => 'resource_name');
+    
+    /**
+     *
+     * @var array 
+     */
+    protected static $configuration = array(
+        'autowidth' => true,
+        'order' => array(
+            array('resource_name', 'asc')
+        ),
+        'stateSave' => true,
+        'paging' => true,
+    );
+    
+    /**
+     *
+     * @var array 
+     */
+    public static $columns = array(
+        array (
+            'title' => "Id",
+            'name' => 'resource_id',
+            'data' => 'resource_id',
+            'orderable' => true,
+            'searchable' => false,
+            'type' => 'string',
+            'visible' => false,
+        ),
+        array (
+            'title' => 'Name',
+            'name' => 'resource_name',
+            'data' => 'resource_name',
+            'orderable' => true,
+            'searchable' => true,
+            'searchLabel' => 'resource',
+            'type' => 'string',
+            'visible' => true,
+            'cast' => array(
+                'type' => 'url',
+                'parameters' => array(
+                    'route' => '/centreon-configuration/resource/[i:id]',
+                    'routeParams' => array(
+                        'id' => '::resource_id::'
+                    ),
+                    'linkName' => '::resource_name::'
+                )
+            )
+        ),
+        array (
+            'title' => 'Values',
+            'name' => 'resource_line',
+            'data' => 'resource_line',
+            'orderable' => true,
+            'searchable' => true,
+            'type' => 'string',
+            'visible' => true,
+        ),
+        array (
+            'title' => 'Description',
+            'name' => 'resource_comment',
+            'data' => 'resource_comment',
+            'orderable' => true,
+            'searchable' => true,
+            'type' => 'string',
+            'visible' => true,
+        ),
+        array (
+            'title' => 'Status',
+            'name' => 'resource_activate',
+            'data' => 'resource_activate',
+            'orderable' => true,
+            'searchable' => true,
+            'type' => 'string',
+            'visible' => true,
+            'cast' => array(
+            'type' => 'select',
+                'parameters' =>array(
+                    '0' => '<span class="label label-danger">Disabled</span>',
+                    '1' => '<span class="label label-success">Enabled</span>',
+                )
+            )
+        ),
+    );
+    
+    /**
+     * 
+     * @param array $params
+     */
+    public function __construct($params, $objectModelClass = '')
+    {
+        parent::__construct($params, $objectModelClass);
+    }
+
+}
