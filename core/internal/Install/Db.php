225671d1fa7b98cd5bbee5aa4d094cc87e5b4aff
Kevin Duret <kduret@centreon.com>
2015-04-14 08:23:04 +0200

    Add function to remove folder, to avoid error on install

--- core/internal/Install/Db.php
+++ core/internal/Install/Db.php
@@ -185,7 +185,28 @@ class Db
             unlink($fileList[$i]);
         }
         
-        rmdir($targetFolder);
+        self::deleteFolder($targetFolder);
+    }
+
+    /**
+     *
+     * @param string $path
+     */
+    private static function deleteFolder($path)
+    {
+        if (is_dir($path) === true) {
+            $files = array_diff(scandir($path), array('.', '..'));
+
+            foreach ($files as $file) {
+                Delete(realpath($path) . '/' . $file);
+            }
+
+            return rmdir($path);
+        } else if (is_file($path) === true) {
+            return unlink($path);
+        }
+
+        return false;
     }
     
     /**
