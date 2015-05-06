3e30fd05a34b8acaec332e88a2c7256fb95cee4b
Julien Mathis <jmathis@centreon.com>
2015-05-06 17:06:02 +0200

    use class ico-16 for BAM listing

--- modules/CentreonBamModule/repositories/BusinessActivityRepository.php
+++ modules/CentreonBamModule/repositories/BusinessActivityRepository.php
@@ -91,7 +91,7 @@ class BusinessActivityRepository extends FormRepository
             $imgSrc = $router->getPathFor('/uploads/[*:image][png|jpg|gif|jpeg:format]', $routeAttr);
             $finalRoute .= '<img src="'.$imgSrc.'" style="width:16px;height:16px;">&nbsp;';
         } elseif (is_null($baIconResult['filename'])/* && !is_null($tplResult['host_tpl_id'])*/) {
-            $finalRoute .= "<i class='icon-BAM'></i>&nbsp;";
+            $finalRoute .= "<i class='icon-BAM ico-16'></i>&nbsp;";
         }
         
         return $finalRoute;
