9becbdc83387b49d551d4f8a7afb8f0a3ee12aac
Kevin Duret <kduret@centreon.com>
2015-05-07 09:03:00 +0200

    Improve bam datatable display

--- modules/CentreonBamModule/controllers/BusinessActivityRealtimeController.php
+++ modules/CentreonBamModule/controllers/BusinessActivityRealtimeController.php
@@ -75,8 +75,7 @@ class BusinessActivityRealtimeController extends Controller
     {
         $router = Di::getDefault()->get('router');
         /* Load css */
-        $this->tpl->addCss('jquery.dataTables.min.css')
-            ->addCss('dataTables.tableTools.min.css')
+        $this->tpl->addCss('dataTables.tableTools.min.css')
             ->addCss('dataTables.colVis.min.css')
             ->addCss('dataTables.colReorder.min.css')
             ->addCss('dataTables.bootstrap.css')
