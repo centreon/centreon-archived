7c516b50c0391fa9193ecc443ad361e796ce8a15
Julien Mathis <jmathis@centreon.com>
2015-05-12 15:55:20 +0200

    prepare 2.6.1 version

--- www/install/sql/centreon/Update-DB-2.6.0_to_2.6.1.sql
+++ www/install/sql/centreon/Update-DB-2.6.0_to_2.6.1.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `cfg_nagios` ADD COLUMN `log_pid` enum('0','1') DEFAULT '1';
   
+-- Change version of Centreon
+UPDATE `informations` SET `value` = '2.6.1' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.6.0' LIMIT 1;
