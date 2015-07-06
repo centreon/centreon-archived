55ad88c26d9147c0c31bd4a936e935a24578fc84
Maximilien Bersoult <mbersoult@merethis.com>
2015-07-06 14:26:25 +0200

    * [WEB-505] Add Illegal Charaters validator for Centreon Engine objects

---
+++ core/internal/Form/Validators/IllegalChars.php
@@ -0,0 +1,63 @@
+<?php
+/*
+ * Copyright 2005-2015 CENTREON
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give CENTREON
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of CENTREON choice, provided that
+ * CENTREON also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+namespace Centreon\Internal\Form\Validators;
+
+/**
+ * @author Maximilien Bersoult <mbersoult@centreon.com>
+ *
+ * @package Centreon
+ * @subpackage Core
+ */
+class IllegalChars extends ForbiddenChar
+{
+    /**
+     * Validate function for illegal characters
+     *
+     * Use the object illegal characters constant
+     *
+     * Validator params
+     *   - None
+     *
+     * @param string $value The value to validate
+     * @param array $params The list of parameters for this validator
+     * @return array
+     */
+    public function validate($value, $params = array()) {
+        $params['characters'] = CENTREON_ILLEGAL_CHAR_OBJ;
+        return parent::validate($value, $params);
+    }
+}
+
