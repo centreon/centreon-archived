77afdcba093cedca817ca7779af8afbabe3d05df
Quentin Delance <qdelance@centreon.com>
2015-07-08 12:23:50 +0200

    No need to hardcode init_data.sh anymore

--- hardcode_init_data.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp init_data.sh init_data_hardcoded.sh
-sed -i 's/\$_HOSTSNMPVERSION\$/v2c/' init_data_hardcoded.sh
-sed -i 's/\$_HOSTSNMPCOMMUNITY\$/public/' init_data_hardcoded.sh
-sed -i 's#\$USER1\$#/usr/lib/nagios/plugins#' init_data_hardcoded.sh
