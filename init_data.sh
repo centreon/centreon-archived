681fdf43bc8eb3f9c672a13312f74db2fa2865d6
Kevin Duret <kduret@centreon.com>
2015-07-09 10:53:22 +0200

    Finally, restart broker is mandatory

--- init_data.sh
+++ init_data.sh
@@ -130,4 +130,7 @@ echo " ==== Generating Configuration ==== "
 ./external/bin/centreonConsole centreon-configuration:Config:generate --poller=central
 ./external/bin/centreonConsole centreon-configuration:Config:move --poller=central
 
+echo " ==== Restarting Engine and Broker ==== "
+./external/bin/centreonConsole centreon-configuration:Config:apply --poller=central --action=restart
+
 echo " ==== That's all for now, we've already suffered so much to do it, please applause ==== "
