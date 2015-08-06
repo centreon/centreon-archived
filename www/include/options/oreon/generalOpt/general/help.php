bb1df23fa2fe147ecefb7a633dc3dd4254bf2259
Julien Mathis <jmathis@centreon.com>
2015-08-06 14:07:07 +0200

    fix #3708

--- www/include/options/oreon/generalOpt/general/help.php
+++ www/include/options/oreon/generalOpt/general/help.php
@@ -61,7 +61,7 @@ $help['tip_default_timezone'] = dgettext('help', 'Default host timezone.');
 /**
  * SSO
  */
-$help['sso_enable'] = dgettext('help', 'Whether SSO authentication is enabled.');
+$help['sso_enable'] = dgettext('help', 'Whether SSO authentication is enabled. SSO feature have only to be enabled in a secured and dedicated environment for SSO. Direct access to Centreon UI from users have to be disabled.');
 $help['sso_mode'] = dgettext('help', 'Authentication can be solely based on SSO or it can work with both SSO and local authentication systems.');
 $help['sso_trusted_clients'] = dgettext('help', 'IP/DNS of trusted clients. Use coma as delimiter in case of multiple clients.');
 $help['sso_header_username'] = dgettext('help', 'The header variable that will be used as login. i.e: $_SERVER[\'HTTP_AUTH_USER\']');
