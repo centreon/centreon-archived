822a89010247058f0d9a027b3acf26b2d3b41544
Julien Mathis <jmathis@centreon.com>
2015-09-18 19:00:18 +0200

    refs #3787

--- www/include/monitoring/status/monitoringHost.php
+++ www/include/monitoring/status/monitoringHost.php
@@ -31,118 +31,94 @@
  *
  * For more information : contact@centreon.com
  *
- * SVN : $URL$
- * SVN : $Id$
- *
  */
 
-	if (!isset($oreon)) {
-		exit();
+if (!isset($centreon)) {
+	exit();
+}
+
+require_once './class/centreonDuration.class.php';
+include_once "./include/monitoring/common-Func.php";
+include_once "./include/monitoring/external_cmd/cmd.php";
+
+/*
+ * Init Continue Value
+ */
+$continue = true;
+
+/*
+ * Pear library
+ */
+require_once "HTML/QuickForm.php";
+require_once 'HTML/QuickForm/advmultiselect.php';
+require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+
+$path = "./include/monitoring/status/Hosts/";
+$path_hg = "./include/monitoring/status/HostGroups/";
+
+$pathRoot = "./include/monitoring/";
+$pathDetails = "./include/monitoring/objectDetails/";
+$pathTools = "./include/tools/";
+
+if (!isset($_GET["cmd"]) && isset($_POST["cmd"])) {
+	$param = $_POST;
+} else {
+	$param = $_GET;
+}
+
+if (isset($param["cmd"]) && $param["cmd"] == 14 && isset($param["author"]) && isset($param["en"]) && $param["en"] == 1) {
+	if (!isset($param["sticky"])) {
+		$param["sticky"] = 0;
 	}
+	if (!isset($param["notify"])) {
+		$param["notify"] = 0;
+	}
+	if (!isset($param["persistent"])) {
+		$param["persistent"] = 0;
+	}
+	if (!isset($param["ackhostservice"])) {
+		$param["ackhostservice"] = 0;
+	}
+	acknowledgeHost($param);
+} else if (isset($param["cmd"]) && $param["cmd"] == 14 && isset($param["author"]) && isset($param["en"]) && $param["en"] == 0) {
+	acknowledgeHostDisable();
+}
 
-	require_once './class/centreonDuration.class.php';
-	include_once "./include/monitoring/common-Func.php";
-	include_once "./include/monitoring/external_cmd/cmd.php";
+if (isset($param["cmd"]) && $param["cmd"] == 16 && isset($param["output"])) {
+	submitHostPassiveCheck();
+}
 
+if ($min){
+	switch ($o)	{
+		default : require_once($pathTools."tools.php"); break;
+	}
+} else {
 	/*
-	 * Init Continue Value
+	 * Now route to pages or Actions
 	 */
-	$continue = true;
-
-	/*
-	 * Pear library
-	 */
-	require_once "HTML/QuickForm.php";
-	require_once 'HTML/QuickForm/advmultiselect.php';
-	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
-
-	$path = "./include/monitoring/status/Hosts/";
-	$path_hg = "./include/monitoring/status/HostGroups/";
-
-	$pathRoot = "./include/monitoring/";
-	$pathDetails = "./include/monitoring/objectDetails/";
-	$pathTools = "./include/tools/";
-
-	if (!isset($_GET["cmd"]) && isset($_POST["cmd"])) {
-		$param = $_POST;
-	} else {
-		$param = $_GET;
-	}
-
-	if (isset($param["cmd"]) && $param["cmd"] == 14 && isset($param["author"]) && isset($param["en"]) && $param["en"] == 1) {
-		if (!isset($param["sticky"])) {
-			$param["sticky"] = 0;
-		}
-		if (!isset($param["notify"])) {
-			$param["notify"] = 0;
-		}
-		if (!isset($param["persistent"])) {
-			$param["persistent"] = 0;
-		}
-		if (!isset($param["ackhostservice"])) {
-			$param["ackhostservice"] = 0;
-		}
-		acknowledgeHost($param);
-	} else if (isset($param["cmd"]) && $param["cmd"] == 14 && isset($param["author"]) && isset($param["en"]) && $param["en"] == 0) {
-		acknowledgeHostDisable();
-	}
-
-	if (isset($param["cmd"]) && $param["cmd"] == 16 && isset($param["output"])) {
-		submitHostPassiveCheck();
-	}
-
-	if ($min){
+	if ($continue) {
 		switch ($o)	{
-			default : require_once($pathTools."tools.php"); break;
-		}
-	} else {
-		if ($centreon->broker->getBroker() != "broker") {
-			$pearDBndo = new CentreonDB("ndo");
-
-			/*
-			 * Check NDO connection
-			 */
-			if (preg_match("/error/", $pearDBndo->toString(), $str) || preg_match("/failed/", $pearDBndo->toString(), $str)) {
-				print "<div class='msg'>"._("Connection Error to NDO DataBase ! \n")."</div>";
-				$continue = false;
-			}
-
-			/*
-			 * Check table ACL exists
-			 */
-			if ($err_msg = table_not_exists("centreon_acl")) {
-				print "<div class='msg'>"._("Warning: ").$err_msg."</div>";
-				$continue = false;
-			}
-		}
-
-		/*
-		 * Now route to pages or Actions
-		 */
-		if ($continue) {
-			switch ($o)	{
-				case "h" 	:
-					require_once($path."host.php");
-					break;
-				case "hpb" 	:
-					require_once($path."host.php");
-					break;
-				case "h_unhandled" 	:
-					require_once($path."host.php");
-					break;
-				case "hd" 	:
-					require_once($pathDetails."hostDetails.php");
-					break;
-				case "hpc" 		:
-					require_once("./include/monitoring/submitPassivResults/hostPassiveCheck.php");
-					break;
-				case "hak" 	:
-					require_once($pathRoot."acknowlegement/hostAcknowledge.php");
-					break;
-				default 	:
-					require_once($path."host.php");
-					break;
-			}
+			case "h" 	:
+				require_once($path."host.php");
+				break;
+			case "hpb" 	:
+				require_once($path."host.php");
+				break;
+			case "h_unhandled" 	:
+				require_once($path."host.php");
+				break;
+			case "hd" 	:
+				require_once($pathDetails."hostDetails.php");
+				break;
+			case "hpc" 		:
+				require_once("./include/monitoring/submitPassivResults/hostPassiveCheck.php");
+				break;
+			case "hak" 	:
+				require_once($pathRoot."acknowlegement/hostAcknowledge.php");
+				break;
+			default 	:
+				require_once($path."host.php");
+				break;
 		}
 	}
-?>
+}
