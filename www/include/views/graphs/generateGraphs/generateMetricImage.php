829590c2745ba99e7646ebcaf1318c3fea6de13e
Julien Mathis <jmathis@centreon.com>
2015-10-07 19:21:12 +0200

    clean source code

--- www/include/views/graphs/generateGraphs/generateMetricImage.php
+++ www/include/views/graphs/generateGraphs/generateMetricImage.php
@@ -31,80 +31,75 @@
  *
  * For more information : contact@centreon.com
  *
- * SVN : $URL$
- * SVN : $Id$
- *
  */
 
-	/**
-	 * Include config file
-	 */
-	include "@CENTREON_ETC@/centreon.conf.php";
+/**
+ * Include config file
+ */
+include "@CENTREON_ETC@/centreon.conf.php";
 
-	require_once "$centreon_path/www/class/centreonGraph.class.php";
+require_once "$centreon_path/www/class/centreonGraph.class.php";
 
-	/**
-	 * Create XML Request Objects
-	 */
-	$obj = new CentreonGraph($_GET["session_id"], $_GET["index"], 0, 1);
+/**
+ * Create XML Request Objects
+ */
+$obj = new CentreonGraph($_GET["session_id"], $_GET["index"], 0, 1);
 
-	if (isset($obj->session_id) && CentreonSession::checkSession($obj->session_id, $obj->DB)) {
-		;
-	} else {
-		$obj->displayError();
-	}
+if (isset($obj->session_id) && CentreonSession::checkSession($obj->session_id, $obj->DB)) {
+	;
+} else {
+	$obj->displayError();
+}
 
-	require_once $centreon_path."www/include/common/common-Func.php";
+require_once $centreon_path."www/include/common/common-Func.php";
 
-	/**
-	 * Set One curve
-	 **/
-	$obj->onecurve = true;
+/**
+ * Set One curve
+ **/
+$obj->onecurve = true;
 
-	/**
-	 * Set metric id
-	 */
-	if (isset($_GET["metric"])) {
-		$obj->setMetricList($_GET["metric"]);
-	}
+/**
+ * Set metric id
+ */
+if (isset($_GET["metric"])) {
+	$obj->setMetricList($_GET["metric"]);
+}
 
-	/**
-	 * Set arguments from GET
-	 */
-	$obj->setRRDOption("start", $obj->checkArgument("start", $_GET, time() - (60*60*48)) );
-	$obj->setRRDOption("end",   $obj->checkArgument("end", $_GET, time()) );
+/**
+ * Set arguments from GET
+ */
+$obj->setRRDOption("start", $obj->checkArgument("start", $_GET, time() - (60*60*48)) );
+$obj->setRRDOption("end",   $obj->checkArgument("end", $_GET, time()) );
 
- 	$obj->GMT->getMyGMTFromSession($obj->session_id, $pearDB);
+$obj->GMT->getMyGMTFromSession($obj->session_id, $pearDB);
 
-	/**
-	 * Template Management
-	 */
- 	if (isset($_GET["template_id"])) {
-		$obj->setTemplate($_GET["template_id"]);
- 	} else {
- 		$obj->setTemplate();
- 	}
+/**
+ * Template Management
+ */
+if (isset($_GET["template_id"])) {
+	$obj->setTemplate($_GET["template_id"]);
+} else {
+	$obj->setTemplate();
+}
 
-	$obj->init();
-	if (isset($_GET["flagperiod"])) {
-		$obj->setCommandLineTimeLimit($_GET["flagperiod"]);
-	}
+$obj->init();
+if (isset($_GET["flagperiod"])) {
+	$obj->setCommandLineTimeLimit($_GET["flagperiod"]);
+}
 
-	$obj->initCurveList();
+$obj->initCurveList();
 
-	/**
-	 * Comment time
-	 */
-	$obj->setOption("comment_time");
+/**
+ * Comment time
+ */
+$obj->setOption("comment_time");
 
-	/**
-	 * Create Legende
-	 */
-	$obj->createLegend();
+/**
+ * Create Legende
+ */
+$obj->createLegend();
 
-
-	/**
-	 * Display Images Binary Data
-	 */
-	$obj->displayImageFlow();
-?>
+/**
+ * Display Images Binary Data
+ */
+$obj->displayImageFlow();
