02a2afd61a73c4ece161032110044cb7d3ce8e51
Julien Mathis <jmathis@centreon.com>
2015-10-09 16:20:46 +0200

    remove old centcore options

--- www/include/options/oreon/generalOpt/centcore/centcore.php
+++ www/include/options/oreon/generalOpt/centcore/centcore.php
@@ -56,8 +56,6 @@ $form->addElement('header', 'title', _("Modify Centcore options"));
 /*
  * Centcore Options
  */
-$form->addElement('checkbox', 'enable_perfdata_sync', _("Enable Perfdata Synchronisation"));
-$form->addElement('checkbox', 'enable_logs_sync', _("Enable Logs Synchronisation"));
 $form->addElement('checkbox', 'enable_broker_stats', _("Enable Broker Statistics Collection"));
 $form->addElement('text', 'centcore_cmd_timeout', _("Timeout value for Centcore commands"), $attrsText2);
 $form->addRule('centcore_cmd_timeout', _('Must be a number'), 'numeric');
