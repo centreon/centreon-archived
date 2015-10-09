448ec2ff1b82b971ba8a874145769b5725ecd102
Julien Mathis <jmathis@centreon.com>
2015-10-09 16:21:09 +0200

    remove grouping pattern

--- www/include/configuration/configObject/host_template_model/listHostTemplateModel.php
+++ www/include/configuration/configObject/host_template_model/listHostTemplateModel.php
@@ -31,240 +31,199 @@
  *
  * For more information : contact@centreon.com
  *
- * SVN : $URL$
- * SVN : $Id$
- *
  */
 
-	if (!isset($oreon)) {
-		exit();
+if (!isset($oreon)) {
+	exit();
+}
+
+include("./include/common/autoNumLimit.php");
+
+/*
+ * Init Host Method
+ */
+$host_method = new CentreonHost($pearDB);
+$mediaObj = new CentreonMedia($pearDB);
+
+/*
+ * Get Extended informations
+ */
+$ehiCache = array();
+$DBRESULT = $pearDB->query("SELECT ehi_icon_image, host_host_id FROM extended_host_information");
+while ($ehi = $DBRESULT->fetchRow()) {
+	$ehiCache[$ehi["host_host_id"]] = $ehi["ehi_icon_image"];
+}
+$DBRESULT->free();
+
+$search = '';
+if (isset($_POST['searchHT']) && $_POST['searchHT']) {
+    $search = $_POST['searchHT'];
+	$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM host WHERE (host_name LIKE '%".CentreonDB::escape($search)."%' OR host_alias LIKE '%".CentreonDB::escape($search)."%') AND host_register = '0'");
+} else {
+	$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM host WHERE host_register = '0'");
+}
+$tmp = $DBRESULT->fetchRow();
+$rows = $tmp["COUNT(*)"];
+
+include("./include/common/checkPagination.php");
+
+/*
+ * Smarty template Init
+ */
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
+
+/* Access level */
+($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
+$tpl->assign('mode_access', $lvl_access);
+
+/*
+ * start header menu
+ */
+$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
+$tpl->assign("headerMenu_name", _("Host Templates name"));
+$tpl->assign("headerMenu_desc", _("Description"));
+$tpl->assign("headerMenu_svChilds", _("Linked Services Templates"));
+$tpl->assign("headerMenu_parent", _("Parent Templates"));
+$tpl->assign("headerMenu_status", _("Status"));
+$tpl->assign("headerMenu_options", _("Options"));
+
+/*
+ * Host Template list
+ */
+if ($search) {
+	$rq = "SELECT host_id, host_name, host_alias, host_activate, host_template_model_htm_id FROM host WHERE (host_name LIKE '%".CentreonDB::escape($search)."%' OR host_alias LIKE '%".CentreonDB::escape($search)."%') AND host_register = '0' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit;
+} else {
+	$rq = "SELECT host_id, host_name, host_alias, host_activate, host_template_model_htm_id FROM host WHERE host_register = '0' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit;
+}
+$DBRESULT = $pearDB->query($rq);
+
+$search = tidySearchKey($search, $advanced_search);
+
+$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
+
+/* Different style between each lines */
+$style = "one";
+
+/* Fill a tab with a mutlidimensionnal Array we put in $tpl */
+$elemArr = array();
+for ($i = 0; $host = $DBRESULT->fetchRow(); $i++) {
+    $moptions = "";
+    $selectedElements = $form->addElement('checkbox', "select[".$host['host_id']."]");
+    if (isset($lockedElements[$host['host_id']])) {
+        $selectedElements->setAttribute('disabled', 'disabled');
+    } else {
+        if ($host["host_activate"]) {
+            $moptions .= "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/eye_inactive.png' class='ico-14' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
+        } else {
+            $moptions .= "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/eye_active.png' class='ico-14' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
+        }
+        $moptions .= "&nbsp;";
+        $moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$host['host_id']."]'></input>";
+    }
+    # If the name of our Host Model is in the Template definition, we have to catch it, whatever the level of it :-)
+    if (!$host["host_name"]) {
+        $host["host_name"] = getMyHostName($host["host_template_model_htm_id"]);
+    }
+
+    /* TPL List */
+    $tplArr = array();
+    $tplStr = NULL;
+
+	$tplArr = getMyHostMultipleTemplateModels($host['host_id']);
+	if (count($tplArr)) {
+		$firstTpl = 1;
+		foreach($tplArr as $key =>$value) {
+			if ($firstTpl) {
+				$tplStr .= "<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
+				$firstTpl = 0;
+			} else {
+				$tplStr .= "&nbsp;|&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
+			}
+		}
 	}
 
-	include("./include/common/autoNumLimit.php");
-
 	/*
-	 * Init Host Method
+	 * Check icon
 	 */
-	$host_method = new CentreonHost($pearDB);
-	$mediaObj = new CentreonMedia($pearDB);
-
-	/*
-	 * Get Extended informations
-	 */
-	$ehiCache = array();
-	$DBRESULT = $pearDB->query("SELECT ehi_icon_image, host_host_id FROM extended_host_information");
-	while ($ehi = $DBRESULT->fetchRow()) {
-		$ehiCache[$ehi["host_host_id"]] = $ehi["ehi_icon_image"];
-	}
-	$DBRESULT->free();
-
-    $search = '';
-	if (isset($_POST['searchHT']) && $_POST['searchHT']) {
-        $search = $_POST['searchHT'];
-		$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM host WHERE (host_name LIKE '%".CentreonDB::escape($search)."%' OR host_alias LIKE '%".CentreonDB::escape($search)."%') AND host_register = '0'");
+	if ((isset($ehiCache[$host["host_id"]]) && $ehiCache[$host["host_id"]])) {
+		$host_icone = "./img/media/" . $mediaObj->getFilename($ehiCache[$host["host_id"]]);
+	} else if ($icone = $host_method->replaceMacroInString($host["host_id"], getMyHostExtendedInfoImage($host["host_id"], "ehi_icon_image", 1))) {
+		$host_icone = "./img/media/" . $icone;
 	} else {
-		$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM host WHERE host_register = '0'");
-	}
-	$tmp = $DBRESULT->fetchRow();
-	$rows = $tmp["COUNT(*)"];
-
-	include("./include/common/checkPagination.php");
-
-	/*
-	 * Smarty template Init
-	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl);
-
-	/* Access level */
-	($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
-	$tpl->assign('mode_access', $lvl_access);
-
-	/*
-	 * start header menu
-	 */
-	$tpl->assign("headerMenu_icone", "<img src='./img/icones/16x16/pin_red.gif'>");
-	$tpl->assign("headerMenu_name", _("Host Templates name"));
-	$tpl->assign("headerMenu_desc", _("Description"));
-	$tpl->assign("headerMenu_svChilds", _("Linked Services Templates"));
-	$tpl->assign("headerMenu_parent", _("Parent Templates"));
-	$tpl->assign("headerMenu_status", _("Status"));
-	$tpl->assign("headerMenu_options", _("Options"));
-
-	/*
-	 * Host Template list
-	 */
-	if ($search) {
-		$rq = "SELECT host_id, host_name, host_alias, host_activate, host_template_model_htm_id FROM host WHERE (host_name LIKE '%".CentreonDB::escape($search)."%' OR host_alias LIKE '%".CentreonDB::escape($search)."%') AND host_register = '0' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit;
-	} else {
-		$rq = "SELECT host_id, host_name, host_alias, host_activate, host_template_model_htm_id FROM host WHERE host_register = '0' ORDER BY host_name LIMIT ".$num * $limit.", ".$limit;
-	}
-	$DBRESULT = $pearDB->query($rq);
-
-	$search = tidySearchKey($search, $advanced_search);
-
-	$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
-	#Different style between each lines
-	$style = "one";
-	#Fill a tab with a mutlidimensionnal Array we put in $tpl
-	$elemArr = array();
-	for ($i = 0; $host = $DBRESULT->fetchRow(); $i++) {
-            $moptions = "";
-            $selectedElements = $form->addElement('checkbox', "select[".$host['host_id']."]");
-            if (isset($lockedElements[$host['host_id']])) {
-                $selectedElements->setAttribute('disabled', 'disabled');
-            } else {
-                if ($host["host_activate"]) {
-                    $moptions .= "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/eye_inactive.png' class='ico-14' border='0' alt='"._("Disabled")."'></a>&nbsp;&nbsp;";
-                } else {
-                    $moptions .= "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/eye_active.png' class='ico-14' border='0' alt='"._("Enabled")."'></a>&nbsp;&nbsp;";
-                }
-                $moptions .= "&nbsp;";
-                $moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$host['host_id']."]'></input>";
-            }
-            # If the name of our Host Model is in the Template definition, we have to catch it, whatever the level of it :-)
-            if (!$host["host_name"]) {
-                $host["host_name"] = getMyHostName($host["host_template_model_htm_id"]);
-            }
-
-            /* TPL List */
-            $tplArr = array();
-            $tplStr = NULL;
-
-		$tplArr = getMyHostMultipleTemplateModels($host['host_id']);
-		if (count($tplArr)) {
-			$firstTpl = 1;
-			foreach($tplArr as $key =>$value) {
-				if ($firstTpl) {
-					$tplStr .= "<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
-					$firstTpl = 0;
-				} else {
-					$tplStr .= "&nbsp;|&nbsp;<a href='main.php?p=60103&o=c&host_id=".$key."'>".$value."</a>";
-				}
-			}
-		}
-
-		/*
-		 * Check icon
-		 */
-		if ((isset($ehiCache[$host["host_id"]]) && $ehiCache[$host["host_id"]])) {
-			$host_icone = "./img/media/" . $mediaObj->getFilename($ehiCache[$host["host_id"]]);
-		} else if ($icone = $host_method->replaceMacroInString($host["host_id"], getMyHostExtendedInfoImage($host["host_id"], "ehi_icon_image", 1))) {
-			$host_icone = "./img/media/" . $icone;
-		} else {
-			$host_icone = "./img/icons/host.png";
-		}
-
-		/*
-		 * Service List
-		 */
-		$svArr = array();
-		$svStr = NULL;
-		$svArr = getMyHostServices($host['host_id']);
-		$elemArr[$i] = array("MenuClass" => "list_".$style,
-						"RowMenu_select" => $selectedElements->toHtml(),
-						"RowMenu_name" => $host["host_name"],
-						"RowMenu_link" => "?p=".$p."&o=c&host_id=".$host['host_id'],
-						"RowMenu_desc" => $host["host_alias"],
-						"RowMenu_icone" => $host_icone,
-						"RowMenu_svChilds" => count($svArr),
-						"RowMenu_parent" => $tplStr,
-						"RowMenu_status" => $host["host_activate"] ? _("Enabled") : _("Disabled"),
-						"RowMenu_options" => $moptions);
-		$style != "two" ? $style = "two" : $style = "one";
+		$host_icone = "./img/icons/host.png";
 	}
 
 	/*
-	 * Header title for same name - Ajust pattern lenght with (0, 4) param
+	 * Service List
 	 */
-	$pattern = null;
-	for ($i = 0; $i < count($elemArr); $i++) {
-		# Searching for a pattern wich n+1 elem
-		if (isset($elemArr[$i+1]["RowMenu_name"]) && strstr($elemArr[$i+1]["RowMenu_name"], substr($elemArr[$i]["RowMenu_name"], 0, 4)) && !$pattern)	{
-			for ($j = 0; isset($elemArr[$i]["RowMenu_name"][$j]); $j++)	{
-				if (isset($elemArr[$i+1]["RowMenu_name"][$j]) && $elemArr[$i+1]["RowMenu_name"][$j] == $elemArr[$i]["RowMenu_name"][$j])
-					;
-				else
-					break;
-			}
-			$pattern = substr($elemArr[$i]["RowMenu_name"], 0, $j);
-	                if ($pos = strrpos($pattern, "_") && $pos > 3 && $j > $pos+2) {
-			  $pattern = substr($pattern, 0, $pos);
-			} elseif ($pos = strrpos($pattern, "-") && $pos > 3 && $j > $pos+2) {
-                          $pattern = substr($pattern, 0, $pos);
-			}
-		}
-		if ($pattern && strstr($elemArr[$i]["RowMenu_name"], $pattern)) {
-			$elemArr[$i]["pattern"] = $pattern;
-		} else {
-			$elemArr[$i]["pattern"] = null;
-			$pattern = null;
-			if (isset($elemArr[$i+1]["RowMenu_name"]) && strstr($elemArr[$i+1]["RowMenu_name"], substr($elemArr[$i]["RowMenu_name"], 0, 4)) && !$pattern)	{
-				for ($j = 0; isset($elemArr[$i]["RowMenu_name"][$j]); $j++)	{
-					if (isset($elemArr[$i+1]["RowMenu_name"][$j]) && $elemArr[$i+1]["RowMenu_name"][$j] == $elemArr[$i]["RowMenu_name"][$j])
-						;
-					else
-						break;
-				}
-				$pattern = substr($elemArr[$i]["RowMenu_name"], 0, $j);
-				$elemArr[$i]["pattern"] = $pattern;
-			}
-		}
-	}
-	$tpl->assign("elemArr", $elemArr);
-	#Different messages we put in the template
-	$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
+	$svArr = array();
+	$svStr = NULL;
+	$svArr = getMyHostServices($host['host_id']);
+	$elemArr[$i] = array("MenuClass" => "list_".$style,
+					"RowMenu_select" => $selectedElements->toHtml(),
+					"RowMenu_name" => $host["host_name"],
+					"RowMenu_link" => "?p=".$p."&o=c&host_id=".$host['host_id'],
+					"RowMenu_desc" => $host["host_alias"],
+					"RowMenu_icone" => $host_icone,
+					"RowMenu_svChilds" => count($svArr),
+					"RowMenu_parent" => $tplStr,
+					"RowMenu_status" => $host["host_activate"] ? _("Enabled") : _("Disabled"),
+					"RowMenu_options" => $moptions);
+	$style != "two" ? $style = "two" : $style = "one";
+}
+$tpl->assign("elemArr", $elemArr);
 
-	#
-	## Toolbar select
-	#
-	?>
-	<script type="text/javascript">
-	function setO(_i) {
-		document.forms['form'].elements['o'].value = _i;
-	}
-	</SCRIPT>
-	<?php
-	$attrs1 = array(
-		'onchange'=>"javascript: " .
-				"if (this.form.elements['o1'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
-				" 	setO(this.form.elements['o1'].value); submit();} " .
-				"else if (this.form.elements['o1'].selectedIndex == 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-				" 	setO(this.form.elements['o1'].value); submit();} " .
-				"else if (this.form.elements['o1'].selectedIndex == 3 || this.form.elements['o1'].selectedIndex == 4 ||this.form.elements['o1'].selectedIndex == 5){" .
-				" 	setO(this.form.elements['o1'].value); submit();} " .
-				"this.form.elements['o1'].selectedIndex = 0");
-	$form->addElement('select', 'o1', null, array(null => _("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "mc"=>_("Massive Change"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs1);
-	$form->setDefaults(array('o1' => null));
+/* Different messages we put in the template */
+$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
 
-	$attrs2 = array(
-		'onchange'=>"javascript: " .
-				"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
-				" 	setO(this.form.elements['o2'].value); submit();} " .
-				"else if (this.form.elements['o2'].selectedIndex == 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-				" 	setO(this.form.elements['o2'].value); submit();} " .
-				"else if (this.form.elements['o2'].selectedIndex == 3 || this.form.elements['o2'].selectedIndex == 4 ||this.form.elements['o2'].selectedIndex == 5){" .
-				" 	setO(this.form.elements['o2'].value); submit();} " .
-				"this.form.elements['o1'].selectedIndex = 0");
-    $form->addElement('select', 'o2', null, array(null => _("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "mc"=>_("Massive Change"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs2);
-	$form->setDefaults(array('o2' => null));
-
-	$o1 = $form->getElement('o1');
-	$o1->setValue(null);
-	$o1->setSelected(null);
-
-	$o2 = $form->getElement('o2');
-	$o2->setValue(null);
-	$o2->setSelected(null);
-
-	$tpl->assign('limit', $limit);
-
-	#
-	##Apply a template definition
-	#
-	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);
-	$tpl->assign('form', $renderer->toArray());
-    $tpl->assign('searchHT', $search);
-	$tpl->display("listHostTemplateModel.ihtml");
+#
+## Toolbar select
+#
 ?>
+<script type="text/javascript">
+function setO(_i) {
+	document.forms['form'].elements['o'].value = _i;
+}
+</SCRIPT>
+<?php
+$attrs1 = array(
+	'onchange'=>"javascript: " .
+			"if (this.form.elements['o1'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
+			" 	setO(this.form.elements['o1'].value); submit();} " .
+			"else if (this.form.elements['o1'].selectedIndex == 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
+			" 	setO(this.form.elements['o1'].value); submit();} " .
+			"else if (this.form.elements['o1'].selectedIndex == 3 || this.form.elements['o1'].selectedIndex == 4 ||this.form.elements['o1'].selectedIndex == 5){" .
+			" 	setO(this.form.elements['o1'].value); submit();} " .
+			"this.form.elements['o1'].selectedIndex = 0");
+$form->addElement('select', 'o1', null, array(null => _("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "mc"=>_("Massive Change"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs1);
+$form->setDefaults(array('o1' => null));
+
+$attrs2 = array(
+	'onchange'=>"javascript: " .
+			"if (this.form.elements['o2'].selectedIndex == 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
+			" 	setO(this.form.elements['o2'].value); submit();} " .
+			"else if (this.form.elements['o2'].selectedIndex == 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
+			" 	setO(this.form.elements['o2'].value); submit();} " .
+			"else if (this.form.elements['o2'].selectedIndex == 3 || this.form.elements['o2'].selectedIndex == 4 ||this.form.elements['o2'].selectedIndex == 5){" .
+			" 	setO(this.form.elements['o2'].value); submit();} " .
+			"this.form.elements['o1'].selectedIndex = 0");
+$form->addElement('select', 'o2', null, array(null => _("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete"), "mc"=>_("Massive Change"), "ms"=>_("Enable"), "mu"=>_("Disable")), $attrs2);
+$form->setDefaults(array('o2' => null));
+
+$o1 = $form->getElement('o1');
+$o1->setValue(null);
+$o1->setSelected(null);
+
+$o2 = $form->getElement('o2');
+$o2->setValue(null);
+$o2->setSelected(null);
+
+$tpl->assign('limit', $limit);
+
+$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+$form->accept($renderer);
+$tpl->assign('form', $renderer->toArray());
+$tpl->assign('searchHT', $search);
+$tpl->display("listHostTemplateModel.ihtml");
+
