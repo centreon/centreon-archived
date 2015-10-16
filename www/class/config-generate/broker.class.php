906db420f4e955e07563d61f77f72ff50956c4d2
Kevin Duret <kduret@centreon.com>
2015-10-16 13:56:07 +0200

    #3832 generate automatically failover, temporary and stats outputs

--- www/class/config-generate/broker.class.php
+++ www/class/config-generate/broker.class.php
@@ -12,7 +12,10 @@ class Broker extends AbstractObjectXML {
         config_write_timestamp,
         config_write_thread_id,
         ns_nagios_server,
+        stats_activate,
+        correlation_activate,
         event_queue_max_size,
+        retention_path,
         command_file
     ';
     protected $attributes_select_parameters = '
@@ -37,6 +40,7 @@ class Broker extends AbstractObjectXML {
     protected $stmt_broker = null;
     protected $stmt_broker_parameters = null;
     protected $stmt_engine_parameters = null;
+    protected $max_config_group_id = null;
     
     private function generate($poller_id) {
         if (is_null($this->stmt_broker)) {
@@ -48,6 +52,10 @@ class Broker extends AbstractObjectXML {
         }
         $this->stmt_broker->bindParam(':poller_id', $poller_id, PDO::PARAM_INT);
         $this->stmt_broker->execute();
+
+        if (is_null($this->max_config_group_id)) {
+            $this->getMaxConfigGroupId();
+        }
 
         $this->getEngineParameters($poller_id);
 
@@ -65,7 +73,12 @@ class Broker extends AbstractObjectXML {
             $this->generate_filename = $row['config_filename'];
             $object = array();
 
-            // Base parameters
+            $config_name = $row['config_name'];
+            $retention_path = $row['retention_path'];
+            $stats_activate = $row['stats_activate'];
+            $correlation_activate = $row['correlation_activate'];
+
+            # Base parameters
             $object['instance'] = $this->engine['id'];
             $object['instance_name'] = $this->engine['name'];
             $object['module_directory'] = $this->engine['broker_modules_path'];
@@ -74,15 +87,16 @@ class Broker extends AbstractObjectXML {
             $object['event_queue_max_size'] = $row['event_queue_max_size'];
             $object['command_file'] = $row['command_file'];
 
-            // Flow parameters
             $this->stmt_broker_parameters->bindParam(':config_id', $row['config_id'], PDO::PARAM_INT);
             $this->stmt_broker_parameters->execute();
             $resultParameters = $this->stmt_broker_parameters->fetchAll(PDO::FETCH_GROUP|PDO::FETCH_ASSOC);
 
-            $org = array();
-            $previousValue = '';
-            $count = 0;
+            $failover_config_group_id = $this->max_config_group_id + 1;
             foreach ($resultParameters as $key => $value) {
+                $failover = false;
+                $output_name = null;
+
+                # Flow parameters
                 foreach ($value as $subvalue) {
                     if (in_array($subvalue['config_key'], $this->exclude_parameters) || trim($subvalue['config_value']) == '') {
                         continue;
@@ -91,10 +105,47 @@ class Broker extends AbstractObjectXML {
                     } else {
                         $object[$subvalue['config_group_id']][$key][$subvalue['config_key']] = $subvalue['config_value'];
                     }
+                    
+                    if ($key == 'output' && $subvalue['config_key'] == 'type' && $subvalue['config_value'] != 'file') {
+                        $failover = true;
+                    } else if ($key == 'output' && $subvalue['config_key'] == 'name') {
+                        $output_name = $subvalue['config_value'];
+                    }
+                }
+
+                # Failover parameters
+                if ($failover == true && !is_null($output_name)) {
+                    $object[$failover_config_group_id][$key] = array(
+                        'type' => 'file',
+                        'name' => $retention_path . '/' . $config_name . '_' . $output_name . '.retention',
+                        'protocol' => 'bbdo',
+                        'compression' => 'auto',
+                        'max_size' => '104857600'
+                    );
+                    $failover_config_group_id++;
                 }
             }
 
-            // Generate file
+            # Temporary parameters
+            $object[1]['temporary'] = array(
+                'type' => 'file',
+                'name' => $config_name . '-temporary',
+                'path' => $retention_path . '/' . $config_name . '.temporary',
+                'protocol' => 'bbdo',
+                'compression' => 'auto',
+                'max_size' => '104857600'
+            );
+
+            # Stats parameters
+            if ($stats_activate == '1') {
+                $object[1]['stats'] = array(
+                    'type' => 'stats',
+                    'name' => $config_name . '-stats',
+                    'fifo' => $retention_path . '/' . $config_name . '.temporary',
+                );
+            }
+
+            # Generate file
             $this->generateFile($object, true, 'centreonBroker');
             $this->writeFile($this->backend_instance->getPath());
         }
@@ -116,6 +167,20 @@ class Broker extends AbstractObjectXML {
             $this->engine['id'] = $row['id'];
             $this->engine['name'] = $row['name'];
             $this->engine['broker_modules_path'] = $row['centreonbroker_module_path'];
+        } catch (Exception $e) {
+            throw new Exception('Exception received : ' .  $e->getMessage() . "\n");
+        }
+    }
+
+    private function getMaxConfigGroupId() {
+        $stmt = $this->backend_instance->db->prepare("SELECT
+              MAX(config_group_id) as max_config_group_id
+            FROM cfg_centreonbroker_info
+            ");
+        $stmt->execute();
+        try {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->max_config_group_id = $row['max_config_group_id'];
         } catch (Exception $e) {
             throw new Exception('Exception received : ' .  $e->getMessage() . "\n");
         }
