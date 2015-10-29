42ab9329f672860c06d27634225515a069ecd547
Maximilien Bersoult <mbersoult@centreon.com>
2015-10-29 13:52:58 +0100

    Fix #3866 : Change the name of the forge in installation

--- www/install/steps/step8.php
+++ www/install/steps/step8.php
@@ -51,7 +51,7 @@ $contents .= '
         <li>'._('Official website').': <a href="http://www.centreon.com">www.centreon.com</a></li>
         <li>'._('Forum').': <a href="http://forum.centreon.com">forum.centreon.com</a></li>
         <li>'._('Documentation').': <a href="http://documentation.centreon.com">documentation.centreon.com</a></li>
-        <li>'._('Bug Tracker').': <a href="https://github.com/centreon/centreon/issues">forge.centreon.com</a></li>
+        <li>'._('Bug Tracker').': <a href="https://github.com/centreon/centreon/issues">github.com/centreon/centreon</a></li>
     </ul>';
 $contents .= _('For professional support subscription please contact the <a href="http://support.centreon.com">Centreon Support Center</a>.');
 
