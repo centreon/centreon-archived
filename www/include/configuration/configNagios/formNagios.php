22490f48e3ead163ac7a46bc1d25bfff41647be9
Toufik MECHOUET <tmechouet@merethis.com>
2015-11-05 17:50:26 +0100

    Fix multiple issues in poller wizard

--- www/include/configuration/configNagios/formNagios.php
+++ www/include/configuration/configNagios/formNagios.php
@@ -746,13 +746,11 @@
 		$o = "w";
 		if ($centreon->user->access->page($p) != 2)
 			$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&nagios_id=".$nagiosObj->getValue()."'"));
-		$form->freeze();
 		$valid = true;
 	}
-
-	$action = $form->getSubmitValue("action");
-	if ($valid && $action["action"]) {
-		require_once($path."listNagios.php");
+        
+	if ($valid) {
+            require_once($path."listNagios.php");
 	} else {
 		/*
 		 * Apply a template definition
