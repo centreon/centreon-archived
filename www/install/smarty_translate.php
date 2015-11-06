352a2f65e38fac1a4f96a7aa4542b7aa0f929529
Laurent Pinsivy <lpinsivy@gmail.com>
2015-11-06 10:36:00 +0100

    add php file for smarty translation

---
+++ www/install/smarty_translate.php
@@ -0,0 +1,521 @@
+<?php
+/* /tmp/centreon-2.7.x/www//include/monitoring/comments/template/serviceComments.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/comments/template/hostComments.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("More actions...");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Host Name");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Services");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Start Time");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("End Time");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Duration");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Author");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Comments");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Started");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Fixed");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Started");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("Actual End");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("No downtime scheduled for services");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/downtime/template/listDowntime.ihtml */
+_("More actions...");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/Services/templates/serviceGrid.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/ServicesHostGroups/templates/serviceGrid.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/Meta/templates/metaService.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/HostGroups/templates/hostGroup.ihtml */
+_("Poller");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/Scheduling/templates/serviceSchedule.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/monitoring/status/ServicesServiceGroups/templates/serviceGrid.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/eventLogs/viewLogEngine.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/eventLogs/viewLog.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/eventLogs/viewLog.ihtml */
+_("Status");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/virtualMetrics/listVirtualMetrics.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/virtualMetrics/listVirtualMetrics.ihtml */
+_("Virtual metrics");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/virtualMetrics/listVirtualMetrics.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/graphTemplates/listGraphTemplates.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/graphTemplates/listGraphTemplates.ihtml */
+_("Graph template");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/graphTemplates/listGraphTemplates.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/componentTemplates/listComponentTemplates.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/componentTemplates/listComponentTemplates.ihtml */
+_("Curve");
+
+/* /tmp/centreon-2.7.x/www//include/views/graphs/componentTemplates/listComponentTemplates.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/common/templates/clone.ihtml */
+_(" + Add a new entry");
+
+/* /tmp/centreon-2.7.x/www//include/common/templates/clone.ihtml */
+_("Nothing here, use the \"Add\" button");
+
+/* /tmp/centreon-2.7.x/www//include/common/templates/cloneMacro.ihtml */
+_(" + Add a new entry");
+
+/* /tmp/centreon-2.7.x/www//include/common/templates/cloneMacro.ihtml */
+_("Nothing here, use the \"Add\" button");
+
+/* /tmp/centreon-2.7.x/www//include/Administration/configChangelog/viewLogs.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configGenerate/formGenerateFiles.ihtml */
+_("Poller");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configGenerate/formGenerateFiles.ihtml */
+_("You must set a poller for generate configuration.");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_categories/listServiceCategories.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_categories/listServiceCategories.ihtml */
+_("Service category");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_categories/listServiceCategories.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_template_model/listHostTemplateModel.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_template_model/listHostTemplateModel.ihtml */
+_("Host template");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_template_model/listHostTemplateModel.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/command/listCommand.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/command/listCommand.ihtml */
+_("Command");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/command/listCommand.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact_template_model/listContactTemplateModel.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact_template_model/listContactTemplateModel.ihtml */
+_("Contact template");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact_template_model/listContactTemplateModel.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_dependency/listServiceDependency.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_dependency/listServiceDependency.ihtml */
+_("Service dependency");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_dependency/listServiceDependency.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact/listContact.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact/listContact.ihtml */
+_("Contact");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contact/listContact.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/escalation/listEscalation.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/escalation/listEscalation.ihtml */
+_("Escalation");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/escalation/listEscalation.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contactgroup/listContactGroup.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contactgroup/listContactGroup.ihtml */
+_("Contact group");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/contactgroup/listContactGroup.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/meta_service/listMetaService.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/meta_service/listMetaService.ihtml */
+_("Meta Services");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/meta_service/listMetaService.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/listTraps.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/listTraps.ihtml */
+_("Snmp Traps");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/listTraps.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/formTraps.ihtml */
+_("Misc");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/formTraps.ihtml */
+_("seconds");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps/formTraps.ihtml */
+_("seconds");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup_dependency/listServiceGroupDependency.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup_dependency/listServiceGroupDependency.ihtml */
+_("Servicegroup dependency");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup_dependency/listServiceGroupDependency.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup/listHostGroup.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup/listHostGroup.ihtml */
+_("Hostgroup");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup/listHostGroup.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/metaservice_dependency/listMetaServiceDependency.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/metaservice_dependency/listMetaServiceDependency.ihtml */
+_("Meta-service dependency");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/metaservice_dependency/listMetaServiceDependency.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_categories/listHostCategories.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_categories/listHostCategories.ihtml */
+_("Host category");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_categories/listHostCategories.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup_dependency/listHostGroupDependency.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup_dependency/listHostGroupDependency.ihtml */
+_("Hostgroup dependency");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/hostgroup_dependency/listHostGroupDependency.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_template_model/listServiceTemplateModel.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_template_model/listServiceTemplateModel.ihtml */
+_("Service template");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service_template_model/listServiceTemplateModel.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/timeperiod/listTimeperiod.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/timeperiod/listTimeperiod.ihtml */
+_("Timeperiod");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/timeperiod/listTimeperiod.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/service/listService.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-manufacturer/listMnftr.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-manufacturer/listMnftr.ihtml */
+_("Snmp trap manufacturer");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-manufacturer/listMnftr.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_dependency/listHostDependency.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_dependency/listHostDependency.ihtml */
+_("Host dependency");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host_dependency/listHostDependency.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-groups/listGroups.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-groups/listGroups.ihtml */
+_("Snmp trap Groups");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/traps-groups/listGroups.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup/listServiceGroup.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup/listServiceGroup.ihtml */
+_("Servicegroup");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/servicegroup/listServiceGroup.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configObject/host/listHost.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configDowntime/listDowntime.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configDowntime/listDowntime.ihtml */
+_("Downtime");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configDowntime/listDowntime.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configResources/listResources.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configResources/listResources.ihtml */
+_("Resource");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configResources/listResources.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/listCentreonBroker.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/listCentreonBroker.ihtml */
+_("Centreon Broker");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/listCentreonBroker.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/wizard.ihtml */
+_("Previous");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/wizard.ihtml */
+_("Next");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/wizard.ihtml */
+_("Finish");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configCentreonBroker/wizard/finish.ihtml */
+_("Back to Centreon Broker configuration menu");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configNagios/listNagios.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configNagios/listNagios.ihtml */
+_("Monitoring engine config");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configNagios/listNagios.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configServers/listServers.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configServers/listServers.ihtml */
+_("Poller");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configServers/listServers.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configServers/listServers.ihtml */
+_("Actions");
+
+/* /tmp/centreon-2.7.x/www//include/configuration/configServers/formServers.ihtml */
+_("Post-Restart command");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Add view");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("This view name is required");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Edit view");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("This view name is required");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Delete view");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Set default");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Share view");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Viewers");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Viewers group");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Add viewer / viewer group");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Add widget");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Title");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Version");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Description");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Author");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Rotation");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Deleting this view might impact other users. Are you sure you want to do it?");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Delete");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Cancel");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Set this view as your default view");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Save");
+
+/* /tmp/centreon-2.7.x/www//include/home/customViews/index.ihtml */
+_("Cancel");
+
+/* /tmp/centreon-2.7.x/www//include/options/media/images/listImg.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/media/images/listImg.ihtml */
+_("Media");
+
+/* /tmp/centreon-2.7.x/www//include/options/media/images/listImg.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/resourcesACL/listsResourcesAccess.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/resourcesACL/listsResourcesAccess.ihtml */
+_("ACL Resource");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/resourcesACL/listsResourcesAccess.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/menusACL/listsMenusAccess.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/menusACL/listsMenusAccess.ihtml */
+_("ACL Menu");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/menusACL/listsMenusAccess.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/actionsACL/listsActionsAccess.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/actionsACL/listsActionsAccess.ihtml */
+_("ACL Actions");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/actionsACL/listsActionsAccess.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/groupsACL/listGroupConfig.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/groupsACL/listGroupConfig.ihtml */
+_("ACL Group");
+
+/* /tmp/centreon-2.7.x/www//include/options/accessLists/groupsACL/listGroupConfig.ihtml */
+_("Search");
+
+/* /tmp/centreon-2.7.x/www//include/options/centStorage/viewData.ihtml */
+_("Filters");
+
+/* /tmp/centreon-2.7.x/www//include/options/oreon/generalOpt/centcore/centcore.ihtml */
+_("Centcore properties");
+
+/* /tmp/centreon-2.7.x/www//include/options/oreon/generalOpt/centcore/centcore.ihtml */
+_("Centcore Options");
+
+/* /tmp/centreon-2.7.x/www//include/options/oreon/generalOpt/centcore/centcore.ihtml */
+_("seconds");
+
+/* /tmp/centreon-2.7.x/www//include/options/oreon/myAccount/formMyAccount.ihtml */
+_("UI notifications");
+
+?>
