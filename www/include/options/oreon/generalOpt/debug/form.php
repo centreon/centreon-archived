6c614ee10028f6f6588f6d81bce6fa3c9efd425d
Maximilien Bersoult <mbersoult@centreon.com>
2015-11-08 11:39:51 +0100

    Apply some style to Administration pages

--- www/include/options/oreon/generalOpt/debug/form.php
+++ www/include/options/oreon/generalOpt/debug/form.php
@@ -132,7 +132,7 @@ if (!$form->validate() && isset($_POST["gopt_id"])) {
     print("<div class='msg' align='center'>"._("Impossible to validate, one or more field is incorrect")."</div>");
 }
 
-$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=debug'"));
+$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=debug'", 'class' => 'btc bt_info'));
 
     // prepare help texts
 $helptext = "";
