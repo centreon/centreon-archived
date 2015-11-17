fa9250481d5f7b3502aefea74d63d347a893d889
Julien Mathis <jmathis@centreon.com>
2015-11-17 18:57:30 +0100

    remove icone column

--- www/include/options/session/connected_user.php
+++ www/include/options/session/connected_user.php
@@ -31,82 +31,78 @@
  * 
  * For more information : contact@centreon.com
  * 
- * SVN : $URL$
- * SVN : $Id$
- * 
  */
 
-	if (!isset($oreon)) {
-		exit();
-	}
-	
-	$path = "./include/options/session/";	
-	
-	require_once "./include/common/common-Func.php";
-	require_once "./class/centreonMsg.class.php";
-    session_start();
-	$sid = $_GET['session'];
-	if (isset($_GET["o"]) && $_GET["o"] == "k"){
-		$pearDB->query("DELETE FROM session WHERE session_id = '".$pearDB->escape($sid)."'");
-		$msg = new CentreonMsg();
-		$msg->setTextStyle("bold");
-		$msg->setText(_("User kicked"));
-		$msg->setTimeOut("3");
-	}	
+if (!isset($centreon)) {
+	exit();
+}
 
-	/*
-	 * Smarty template Init
-	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl);
+$path = "./include/options/session/";	
 
-	$session_data = array();
-	$res = $pearDB->query("SELECT session.*, contact_name, contact_admin FROM session, contact WHERE contact_id = user_id ORDER BY contact_name, contact_admin");
-	for ($cpt = 0;$r = $res->fetchRow();$cpt++){
+require_once "./include/common/common-Func.php";
+require_once "./class/centreonMsg.class.php";
+session_start();
+$sid = $_GET['session'];
+if (isset($_GET["o"]) && $_GET["o"] == "k"){
+	$pearDB->query("DELETE FROM session WHERE session_id = '".$pearDB->escape($sid)."'");
+	$msg = new CentreonMsg();
+	$msg->setTextStyle("bold");
+	$msg->setText(_("User kicked"));
+	$msg->setTimeOut("3");
+}	
 
-		$session_data[$cpt] = array();
-		if ($cpt % 2) {
-			$session_data[$cpt]["class"] = "list_one";
-		} else {
-			$session_data[$cpt]["class"] = "list_two";
-		} 
-		
-		$session_data[$cpt]["user_id"] = $r["user_id"];
-		$session_data[$cpt]["user_alias"] = $r["contact_name"];
-		$session_data[$cpt]["admin"] = $r["contact_admin"];
-		$session_data[$cpt]["ip_address"] = $r["ip_address"];
-		$session_data[$cpt]["last_reload"] = date("H:i:s", $r["last_reload"]);
-		
-		$resCP = $pearDB->query("SELECT topology_name, topology_icone, topology_page, topology_url_opt FROM topology WHERE topology_page = '".$r["current_page"]."'");
-		$rCP = $resCP->fetchRow();
-		
-		$session_data[$cpt]["current_page"] = $r["current_page"].$rCP["topology_url_opt"];
-        if ($rCP['topology_name'] != '') {
-		    $session_data[$cpt]["topology_name"] = _($rCP["topology_name"]);
-        } else {
-		    $session_data[$cpt]["topology_name"] = $rCP["topology_name"];
-        }
-		if ($rCP["topology_icone"]) {
-			$session_data[$cpt]["topology_icone"] = "<img src='".$rCP["topology_icone"]."'>";
-        } else {
-			$session_data[$cpt]["topology_icone"] = "&nbsp;";
-        }
-        if ($centreon->user->admin) {
-		  $session_data[$cpt]["actions"] = "<a href='./main.php?p=$p&o=k&session=" . $r['session_id'] . "'><img src='./img/icones/16x16/flash.gif' border='0' alt='"._("Kick User")."' title='"._("Kick User")."'></a>";
-        } else {
-            $session_data[$cpt]["actions"] = "";
-        }
-	}
+/*
+ * Smarty template Init
+ */
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
+
+$session_data = array();
+$res = $pearDB->query("SELECT session.*, contact_name, contact_admin FROM session, contact WHERE contact_id = user_id ORDER BY contact_name, contact_admin");
+for ($cpt = 0;$r = $res->fetchRow();$cpt++){
+
+	$session_data[$cpt] = array();
+	if ($cpt % 2) {
+		$session_data[$cpt]["class"] = "list_one";
+	} else {
+		$session_data[$cpt]["class"] = "list_two";
+	} 
 	
-	if (isset($msg)) {
-		$tpl->assign("msg", $msg);
-	}
-			
-	$tpl->assign("session_data", $session_data);
-	$tpl->assign("wi_user", _("Users"));
-	$tpl->assign("wi_where", _("Position"));
-	$tpl->assign("wi_last_req", _("Last request"));
-	$tpl->assign("distant_location", _("IP Address"));
+	$session_data[$cpt]["user_id"] = $r["user_id"];
+	$session_data[$cpt]["user_alias"] = $r["contact_name"];
+	$session_data[$cpt]["admin"] = $r["contact_admin"];
+	$session_data[$cpt]["ip_address"] = $r["ip_address"];
+	$session_data[$cpt]["last_reload"] = date("H:i:s", $r["last_reload"]);
 	
-	$tpl->display("connected_user.ihtml");
-?>
+	$resCP = $pearDB->query("SELECT topology_name, topology_icone, topology_page, topology_url_opt FROM topology WHERE topology_page = '".$r["current_page"]."'");
+	$rCP = $resCP->fetchRow();
+	
+	$session_data[$cpt]["current_page"] = $r["current_page"].$rCP["topology_url_opt"];
+    if ($rCP['topology_name'] != '') {
+	    $session_data[$cpt]["topology_name"] = _($rCP["topology_name"]);
+    } else {
+	    $session_data[$cpt]["topology_name"] = $rCP["topology_name"];
+    }
+	if ($rCP["topology_icone"]) {
+		$session_data[$cpt]["topology_icone"] = "<img src='".$rCP["topology_icone"]."'>";
+    } else {
+		$session_data[$cpt]["topology_icone"] = "&nbsp;";
+    }
+    if ($centreon->user->admin) {
+	  $session_data[$cpt]["actions"] = "<a href='./main.php?p=$p&o=k&session=" . $r['session_id'] . "'><img src='./img/icones/16x16/flash.gif' border='0' alt='"._("Kick User")."' title='"._("Kick User")."'></a>";
+    } else {
+        $session_data[$cpt]["actions"] = "";
+    }
+}
+
+if (isset($msg)) {
+	$tpl->assign("msg", $msg);
+}
+		
+$tpl->assign("session_data", $session_data);
+$tpl->assign("wi_user", _("Users"));
+$tpl->assign("wi_where", _("Position"));
+$tpl->assign("wi_last_req", _("Last request"));
+$tpl->assign("distant_location", _("IP Address"));
+
+$tpl->display("connected_user.ihtml");
