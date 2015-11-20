a9de11f56b8cee3b926f6cf71232c5cf4a89e423
Lionel Assepo <lassepo@centreon.com>
2015-11-20 13:23:28 +0100

    * Set right value for macro password ifor service

--- www/class/centreonService.class.php
+++ www/class/centreonService.class.php
@@ -465,11 +465,21 @@ class CentreonService
                 if (preg_match('/\$_SERVICE(.*)\$$/', $row['svc_macro_name'], $matches)) {
                     $arr[$i]['macroInput_#index#'] = $matches[1];
                     $arr[$i]['macroValue_#index#'] = $row['svc_macro_value'];
-                    $arr[$i]['macroPassword_#index#'] = $row['is_password'] ? 1 : NULL;
+                    
+                    $valPassword = '0';
+                    if (isset($row['is_password'])) {
+                        if ($row['is_password'] === '1') {
+                            $valPassword = '1';
+                        } else {
+                           $valPassword = null;
+                        }
+                    }
+                    $arr[$i]['macroPassword_#index#'] = $valPassword;
+
                     $arr[$i]['macroDescription_#index#'] = $row['description'];
                     $arr[$i]['macroDescription'] = $row['description'];
                     if(!is_null($template)){
-                        $arr[$i]['macroTpl_#index#'] = "Serivce template : ".$template['service_description'];
+                        $arr[$i]['macroTpl_#index#'] = "Service template : ".$template['service_description'];
                     }
                     $i++;
                 }
