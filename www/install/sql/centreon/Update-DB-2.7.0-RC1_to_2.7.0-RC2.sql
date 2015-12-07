2c6fb00236d78d78541149e611f98e61a4cfa2c1
Julien Mathis <jmathis@centreon.com>
2015-12-07 13:27:49 +0100

    prepare RC2

---
+++ www/install/sql/centreon/Update-DB-2.7.0-RC1_to_2.7.0-RC2.sql
@@ -0,0 +1,4 @@
+
+
+-- Change version of Centreon
+UPDATE `informations` SET `value` = '2.7.0-RC2' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.7.0-RC1' LIMIT 1;
