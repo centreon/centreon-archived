0a289aecb0f3e63d99cc14eb05126e1f0c42cbfd
Julien Mathis <jmathis@centreon.com>
2015-12-09 14:42:49 +0100

    fix #3938

--- www/install/sql/centreon/Update-DB-2.7.0-RC1_to_2.7.0-RC2.sql
+++ www/install/sql/centreon/Update-DB-2.7.0-RC1_to_2.7.0-RC2.sql
@@ -14,5 +14,8 @@ UPDATE on_demand_macro_service SET svc_macro_value = REPLACE(svc_macro_value, '&
 -- Add changetab for hostDetails page in Monitoring
 INSERT INTO `topology_JS` (`id_t_js`, `id_page`, `o`, `PathName_js`, `Init`) VALUES (NULL,20202,NULL,'./include/common/javascript/changetab.js','initChangeTab');
 
+-- Add discovery command menu
+INSERT INTO `topology` (`topology_name`, `topology_icone`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`, `topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`, `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`) VALUES ('Discovery','./img/icones/16x16/gear_view.gif',608,60807,21,1,'./include/configuration/configObject/command/command.php','&type=4','0','0','1',NULL,NULL,NULL,'0');
+
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.7.0-RC2' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.7.0-RC1' LIMIT 1;
