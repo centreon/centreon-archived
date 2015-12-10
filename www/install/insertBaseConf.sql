4e26a07cc1d438ab314a0c7321e05ec5c9a2ff4e
Toufik MECHOUET <mtoufik77dz@yahoo.fr>
2015-12-10 15:10:01 +0100

    #3953 [2.7.0-RC2] nonexistent path Images Directory

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -147,7 +147,8 @@ INSERT INTO `options` (`key`, `value`) VALUES
 ('tactical_service_limit', '100'),
 ('tactical_refresh_interval', '20'), 
 ('index_data', '0'), 
-('interval_length', '60');
+('interval_length', '60'),
+('nagios_path_img','@INSTALL_DIR_CENTREON@/www/img/media/');
 
 --
 -- Contenu de la table `giv_components_template`
