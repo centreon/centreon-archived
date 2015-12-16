240e832f0a17b410aac3910ecf1ea4274fb3f966
Kevin Duret <kduret@centreon.com>
2015-12-16 11:28:31 +0100

    Fix topology for monitoring host and services menus

--- www/install/sql/centreon/Update-DB-2.7.0-RC2_to_2.7.0.sql
+++ www/install/sql/centreon/Update-DB-2.7.0-RC2_to_2.7.0.sql
@@ -6,5 +6,9 @@ WHERE NOT EXISTS (SELECT `key` FROM `options` WHERE `key` = 'nagios_path_img' ) 
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.7.0' WHERE CONVERT( `informations`.`key` USING utf8 )  = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.7.0-RC2' LIMIT 1;
 
---Delete multiple topology, exept once
-DELETE FROM `topology` WHERE `topology_page` = '60807' AND topology_id in (SELECT * FROM (SELECT topology_id FROM `topology` WHERE `topology_page` = '60807' LIMIT 1, 100) as t);
+-- Delete multiple topology, exept once
+DELETE FROM `topology` WHERE `topology_page` = '60807' AND topology_id IN (SELECT * FROM (SELECT topology_id FROM `topology` WHERE `topology_page` = '60807' LIMIT 1, 100) as t);
+
+-- Delete url opt on service and host topologies
+UPDATE `topology` SET `topology_url_opt` = NULL WHERE `topology_page` = '20201' OR `topology_page` = '20202';
+
