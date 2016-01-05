b506c12d9413f58e37de6b035383aff0e00241b8
Benoît Sauveton <bsauveton@centreon.com>
2016-01-05 16:34:00 +0100

    #4012 applying resizeTd to description + improve the code

--- www/include/common/javascript/resize_td.js
+++ www/include/common/javascript/resize_td.js
@@ -18,19 +18,21 @@
 
 function setOverflowDivToTitle(elemA){
 
+
     jQuery(elemA).contents().filter(function() {
         return this.nodeType === 3;
     }).each(function() {
         //this.nodeValue = jQuery.trim(this.nodeValue); code here if you want to apply some modification to the node value
     }).wrap('<span class="unWrapedElement"></span>');
-    
+    jQuery(elemA).wrapInner('<div style="display:inline-block" ></div>');
     
     jQuery(elemA).each(function(idx, elem){
         var elementWith = jQuery(elem).width();
-        var elementContentWith = elem.scrollWidth;
+        var elementContentWith = jQuery(elem).children( ":first-child" ).width();
+        var wrapper = jQuery(elem).children( ":first-child" );
         if(elementWith < elementContentWith){
-            var elemOldText = jQuery(elem).text();
-            var elemOldHtml = jQuery(elem).html();
+            var elemOldText = jQuery(elem).children( ":first-child" ).text();
+            var elemOldHtml = jQuery(elem).children( ":first-child" ).html();
             var newHtml = jQuery('<div></div>');
             var popin = jQuery('<div></div>',{html : elemOldHtml, style : 'position:relative;width:700px;word-wrap:break-word;'}).appendTo(jQuery(elem));
             var newSpan = jQuery('<span></span>',{
@@ -45,16 +47,39 @@ function setOverflowDivToTitle(elemA){
             });
             
             newSpan.css('visibility','inherit');
-
-            jQuery(elem).children().each(function (idx, el) {
-                tmpWidth += jQuery(el).outerWidth(true);
-                if (tmpWidth < elementWith) {
-                    newHtml.append(jQuery(el).clone());
-                }
-            });
+            
+            if(wrapper.children().length > 1){
+                wrapper.children().each(function (idx, el) {
+                    tmpWidth += jQuery(el).outerWidth(true);
+                    if (tmpWidth < elementWith) {
+                        newHtml.append(jQuery(el).clone());
+                    }
+                });
+            }else{
+                var childtext = wrapper.children().text();
+                var textLenght = childtext.length;
+                var maxTextLenght = elementWith / (elementContentWith / textLenght);
+                var finalText = TextAbstract(wrapper.children().text(),maxTextLenght);
+                var clone = wrapper.children().clone().text(finalText);
+                newHtml.append(clone);
+            }
             newHtml.append(newSpan);
             jQuery(elem).empty().append(newHtml);
         }
     });
 
+}
+
+
+function TextAbstract(text, length) {
+    if (text == null) {
+        return "";
+    }
+    if (text.length <= length) {
+        return text;
+    }
+    text = text.substring(0, length);
+    last = text.lastIndexOf(" ");
+    text = text.substring(0, last);
+    return text;
 }
