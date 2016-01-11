f796e5df5ddc06b60d0a0d1366770e0b01e74f08
Benoît Sauveton <bsauveton@centreon.com>
2016-01-11 11:54:06 +0100

    correction of pagination

--- www/include/common/autoNumLimit.php
+++ www/include/common/autoNumLimit.php
@@ -35,35 +35,38 @@
  * SVN : $Id$
  * 
  */
-	if (!isset($oreon))
-		exit();
+if (!isset($centreon))
+    exit();
 
-	if (isset($_POST["limit"]) && $_POST["limit"])
-		$limit = $_POST["limit"];
-	else if (isset($_GET["limit"]))
-		$limit = $_GET["limit"];
-	else if (!isset($_POST["limit"]) && !isset($_GET["limit"]) && isset($oreon->historyLimit[$url]))
-		$limit = $oreon->historyLimit[$url];
-	else {
-		if (($p >= 200 && $p < 300) || ($p >= 20000 && $p < 30000)){
-			$DBRESULT = $pearDB->query("SELECT * FROM `options` WHERE `key` = 'maxViewMonitoring'");
-			$gopt = $DBRESULT->fetchRow();		
-			$limit = myDecode($gopt["value"]);
-		} else {
-			$DBRESULT = $pearDB->query("SELECT * FROM `options` WHERE `key` = 'maxViewConfiguration'");
-			$gopt = $DBRESULT->fetchRow();		
-			$limit = myDecode($gopt["value"]);
-		}
-	}
+if (isset($_POST["limit"]) && $_POST["limit"])
+    $limit = $_POST["limit"];
+else if (isset($_GET["limit"]))
+    $limit = $_GET["limit"];
+else if (!isset($_POST["limit"]) && !isset($_GET["limit"]) && isset($centreon->historyLimit[$url]))
+    $limit = $centreon->historyLimit[$url];
+else {
+    if (($p >= 200 && $p < 300) || ($p >= 20000 && $p < 30000)){
+        $DBRESULT = $pearDB->query("SELECT * FROM `options` WHERE `key` = 'maxViewMonitoring'");
+        $gopt = $DBRESULT->fetchRow();		
+        $limit = myDecode($gopt["value"]);
+    } else {
+        $DBRESULT = $pearDB->query("SELECT * FROM `options` WHERE `key` = 'maxViewConfiguration'");
+        $gopt = $DBRESULT->fetchRow();		
+        $limit = myDecode($gopt["value"]);
+    }
+}
 
-	if (isset($_POST["num"]) && $_POST["num"])
-		$num = $_POST["num"];
-	else if (isset($_GET["num"]) && $_GET["num"])
-		$num = $_GET["num"];
-	else if (!isset($_POST["num"]) && !isset($_GET["num"]) && isset($oreon->historyPage[$url]))
-		$num = $oreon->historyPage[$url];
-	else 
-		$num = 0;
-	
-	global $search;
+if (!empty($centreon->historyLimit) && $limit != $centreon->historyLimit[$url]) {
+    $num = 0;
+} else if (isset($_POST["num"]) && $_POST["num"]) {
+    $num = $_POST["num"];
+} else if (isset($_GET["num"]) && $_GET["num"]) {
+    $num = $_GET["num"];
+} else if (!isset($_POST["num"]) && !isset($_GET["num"]) && isset($centreon->historyPage[$url])) {
+    $num = $centreon->historyPage[$url];
+} else {
+    $num = 0;
+}
+
+global $search;
 ?>
