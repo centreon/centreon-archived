8b11f5fdb6965c6d0819774e463a7c997991e4f9
Kevin Duret <kduret@centreon.com>
2016-01-21 17:09:39 +0100

    #4105 Force insert_in_index_data to 1

--- www/install/sql/centreon/Update-DB-2.7.1_to_2.7.2.sql
+++ www/install/sql/centreon/Update-DB-2.7.1_to_2.7.2.sql
@@ -3,3 +3,7 @@ UPDATE `informations` SET `value` = '2.7.2' WHERE CONVERT( `informations`.`key` 
 
 ALTER TABLE escalation ADD COLUMN host_inheritance_to_services tinyint(1) DEFAULT 0 NOT NULL;
 ALTER TABLE escalation ADD COLUMN hostgroup_inheritance_to_services tinyint(1) DEFAULT 0 NOT NULL;
+
+UPDATE options SET options.value = '1' WHERE options.key = 'index_data';
+UPDATE cfg_centreonbroker_info SET config_value = '1' WHERE config_key = 'insert_in_index_data';
+
