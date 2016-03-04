78aa4ee03067ba934dc9918025b125d6a27e3a89
Kevin Duret <kduret@centreon.com>
2016-03-04 15:04:03 +0100

    Prepare version 2.8

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.7.1');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.0');
 
 --
 -- Contenu de la table `contact`
