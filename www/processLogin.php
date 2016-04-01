624570ad191f1a5d9d137d8e999cddb2b14f1ff3
Julien Mathis <jmathis@centreon.com>
2016-04-01 15:18:24 +0200

    improve source code

--- www/processLogin.php
+++ www/processLogin.php
@@ -37,23 +37,21 @@ if (isset($_POST["centreon_token"])
     || (isset($_GET["autologin"]) && $_GET["autologin"] && isset($_GET["p"]) && $_GET["autologin"] && isset($generalOptions["enable_autologin"]) && $generalOptions["enable_autologin"])
     || (isset($_POST["autologin"]) && $_POST["autologin"] && isset($_POST["p"]) && isset($generalOptions["enable_autologin"]) && $generalOptions["enable_autologin"])
     || (!isset($generalOptions['sso_enable']) || $generalOptions['sso_enable'] == 1)) {
-
     /*
      * Init log class
      */
     $CentreonLog = new CentreonUserLog(-1, $pearDB);
 
     if (isset($_POST['p'])) {
-        $_GET["p"] = $_POST["p"];			
+        $_GET["p"] = $_POST["p"];
     }
 
     /*
      * Check first for Autologin or Get Authentication
      */
     isset($_GET["autologin"]) ? $autologin = $_GET["autologin"] : $autologin = 0;
-    isset($_GET["useralias"]) ? $useraliasG = $_GET["useralias"] : $useraliasG = NULL;
-    isset($_GET["password"]) ? $passwordG = $_GET["password"] : $passwordG = NULL;
-    
+    isset($_GET["useralias"]) ? $useraliasG = $_GET["useralias"] : $useraliasG = null;
+    isset($_GET["password"]) ? $passwordG = $_GET["password"] : $passwordG = null;
     
     $useraliasP = null;
     $passwordP = null;
@@ -77,14 +75,13 @@ if (isset($_POST["centreon_token"])
     $centreonAuth = new CentreonAuthSSO($useralias, $password, $autologin, $pearDB, $CentreonLog, $encryptType, $token, $generalOptions);
 
     if ($centreonAuth->passwdOk == 1) {
-
         $centreon = new Centreon($centreonAuth->userInfos, $generalOptions["nagios_version"]);
         $_SESSION["centreon"] = $centreon;
         $pearDB->query("INSERT INTO `session` (`session_id` , `user_id` , `current_page` , `last_reload`, `ip_address`) VALUES ('".session_id()."', '".$centreon->user->user_id."', '1', '".time()."', '".$_SERVER["REMOTE_ADDR"]."')");
-        if (!isset($_POST["submit"]))	{
-            $args = NULL;
+        if (!isset($_POST["submit"])) {
+            $args = null;
             foreach ($_GET as $key => $value) {
-                $args ? $args .= "&".$key."=".$value : $args = $key."=".$value;					
+                $args ? $args .= "&".$key."=".$value : $args = $key."=".$value;
             }
             header("Location: ./main.php?".$args."");
         } else {
@@ -92,6 +89,6 @@ if (isset($_POST["centreon_token"])
         }
         $connect = true;
     } else {
-        $connect = false;	    	
+        $connect = false;
     }
 }
