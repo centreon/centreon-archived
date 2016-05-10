f3a183b7b766bac781f5eb0035c72feba6f82f86
Julien Mathis <jmathis@centreon.com>
2016-05-10 11:18:46 +0200

    clean source code

--- www/include/options/oreon/myAccount/DB-Func.php
+++ www/include/options/oreon/myAccount/DB-Func.php
@@ -31,122 +31,120 @@
  * 
  * For more information : contact@centreon.com
  * 
- * SVN : $URL$
- * SVN : $Id$
- * 
  */
 
-	function testExistence ($name = NULL)	{
-		global $pearDB, $form, $oreon;
-		
-		$DBRESULT = $pearDB->query("SELECT contact_name, contact_id FROM contact WHERE contact_name = '".htmlentities($name, ENT_QUOTES, "UTF-8")."'");
-		$contact = $DBRESULT->fetchRow();
-		/*
-		 * Modif case
-		 */
-		if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] == $oreon->user->get_id())	
-			return true;
-		
-		/*
-		 * Duplicate entry
-		 */
-		else if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] != $oreon->user->get_id())
-			return false;
-		else
-			return true;
-	}	
+function testExistence ($name = NULL)	{
+	global $pearDB, $form, $oreon;
 	
-	function testAliasExistence ($alias = NULL)	{
-		global $pearDB, $form, $oreon;
-		
-		$DBRESULT = $pearDB->query("SELECT contact_alias, contact_id FROM contact WHERE contact_alias = '".htmlentities($alias, ENT_QUOTES, "UTF-8")."'");
-		$contact = $DBRESULT->fetchRow();
-		
-		/*
-		 * Modif case
-		 */
-		if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] == $oreon->user->get_id())	
-			return true;
+	$DBRESULT = $pearDB->query("SELECT contact_name, contact_id FROM contact WHERE contact_name = '".htmlentities($name, ENT_QUOTES, "UTF-8")."'");
+	$contact = $DBRESULT->fetchRow();
+	/*
+	 * Modif case
+	 */
+	if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] == $oreon->user->get_id()) {
+		return true;
+	} else if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] != $oreon->user->get_id()) {
 		/*
 		 * Duplicate entry
 		 */
-		else if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] != $oreon->user->get_id())
-			return false;
-		else
-			return true;
+		return false;
+	} else {
+		return true;
 	}
-	
-        function updateNotificationOptions($contact_id) {
-            global $form, $pearDB;
+}	
 
-            $pearDB->query("DELETE FROM contact_param 
-                WHERE cp_contact_id = ".$pearDB->escape($contact_id)."
-                AND cp_key LIKE 'monitoring%notification%'");
-            $data = $form->getSubmitValues();
-            foreach ($data as $k => $v) {
-                if (preg_match("/^monitoring_(host|svc)_notification/", $k)) {
-                        $pearDB->query("INSERT INTO contact_param (cp_key, cp_value, cp_contact_id) 
-                            VALUES ('".$pearDB->escape($k)."', '1', ".$pearDB->escape($contact_id).")");
-                } elseif (preg_match("/^monitoring_sound/", $k)) {
-                        $pearDB->query("INSERT INTO contact_param (cp_key, cp_value, cp_contact_id) 
-                            VALUES ('".$pearDB->escape($k)."', '".$pearDB->escape($v)."', ".$pearDB->escape($contact_id).")");
-                }
-            }
-            unset($_SESSION['centreon_notification_preferences']);
+function testAliasExistence ($alias = NULL)	{
+	global $pearDB, $form, $oreon;
+	
+	$DBRESULT = $pearDB->query("SELECT contact_alias, contact_id FROM contact WHERE contact_alias = '".htmlentities($alias, ENT_QUOTES, "UTF-8")."'");
+	$contact = $DBRESULT->fetchRow();
+	
+	/*
+	 * Modif case
+	 */
+	if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] == $oreon->user->get_id()) 
+		return true;
+	} else if ($DBRESULT->numRows() >= 1 && $contact["contact_id"] != $oreon->user->get_id())
+		/*
+		 * Duplicate entry
+		 */
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function updateNotificationOptions($contact_id) {
+    global $form, $pearDB;
+
+    $pearDB->query("DELETE FROM contact_param 
+        WHERE cp_contact_id = ".$pearDB->escape($contact_id)."
+        AND cp_key LIKE 'monitoring%notification%'");
+    $data = $form->getSubmitValues();
+    foreach ($data as $k => $v) {
+        if (preg_match("/^monitoring_(host|svc)_notification/", $k)) {
+            $pearDB->query("INSERT INTO contact_param (cp_key, cp_value, cp_contact_id) VALUES ('".$pearDB->escape($k)."', '1', ".$pearDB->escape($contact_id).")");
+        } elseif (preg_match("/^monitoring_sound/", $k)) {
+            $pearDB->query("INSERT INTO contact_param (cp_key, cp_value, cp_contact_id) VALUES ('".$pearDB->escape($k)."', '".$pearDB->escape($v)."', ".$pearDB->escape($contact_id).")");
         }
+    }
+    unset($_SESSION['centreon_notification_preferences']);
+}
 
-	function updateContactInDB ($contact_id = NULL)	{
-		if (!$contact_id) 
-			return;
-		updateContact($contact_id);
-                updateNotificationOptions($contact_id);
+function updateContactInDB ($contact_id = NULL)	{
+	if (!$contact_id) {
+		return;
+	}
+	updateContact($contact_id);
+    updateNotificationOptions($contact_id);
+}
+
+function updateContact($contact_id = null)	{
+	global $form, $pearDB, $oreon, $encryptType;
+	
+	if (!$contact_id) {
+		return;
 	}
 	
-	function updateContact($contact_id = null)	{
-		global $form, $pearDB, $oreon, $encryptType;
-		
-		if (!$contact_id) 
-			return;
-		
-		$ret = array();
-		$ret = $form->getSubmitValues();
-		$rq = "UPDATE contact SET ";
-		$rq .= "contact_name = ";
-		isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".htmlentities($ret["contact_name"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		$rq .= "contact_alias = ";
-		isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".htmlentities($ret["contact_alias"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		
-		if (isset($ret["contact_passwd"]) && $ret["contact_passwd"]) {
-			if ($encryptType == 1)
-				$rq .= "contact_passwd = '".md5($ret["contact_passwd"])."', ";	
-			else if ($encryptType == 2)
-				$rq .= "contact_passwd = '".sha1($ret["contact_passwd"])."', ";	
-			else
-				$rq .= "contact_passwd = '".md5($ret["contact_passwd"])."', ";				
+	$ret = array();
+	$ret = $form->getSubmitValues();
+	$rq = "UPDATE contact SET ";
+	$rq .= "contact_name = ";
+	isset($ret["contact_name"]) && $ret["contact_name"] != NULL ? $rq .= "'".htmlentities($ret["contact_name"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	$rq .= "contact_alias = ";
+	isset($ret["contact_alias"]) && $ret["contact_alias"] != NULL ? $rq .= "'".htmlentities($ret["contact_alias"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	
+	if (isset($ret["contact_passwd"]) && $ret["contact_passwd"]) {
+		if ($encryptType == 1) {
+			$rq .= "contact_passwd = '".md5($ret["contact_passwd"])."', ";	
+		} else if ($encryptType == 2) {
+			$rq .= "contact_passwd = '".sha1($ret["contact_passwd"])."', ";	
+		} else {
+			$rq .= "contact_passwd = '".md5($ret["contact_passwd"])."', ";				
 		}
-        
-        $rq .=	"contact_location = ";
-		isset($ret["contact_location"]) && $ret["contact_location"] != NULL ? $rq .= "'".htmlentities($ret["contact_location"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		$rq .=	"contact_lang = ";
-		isset($ret["contact_lang"]) && $ret["contact_lang"] != NULL ? $rq .= "'".htmlentities($ret["contact_lang"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		$rq .= "contact_email = ";
-		isset($ret["contact_email"]) && $ret["contact_email"] != NULL ? $rq .= "'".htmlentities($ret["contact_email"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		$rq .= "contact_pager = ";
-		isset($ret["contact_pager"]) && $ret["contact_pager"] != NULL ? $rq .= "'".htmlentities($ret["contact_pager"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
-		$rq .= "contact_js_effects = ";
-		isset($ret["contact_js_effects"]) ? $rq .= "'1', ": $rq .= "'0', ";
-                $rq .= "contact_autologin_key = ";
-                $rq .= isset($ret["contact_autologin_key"]) ? "'".$pearDB->escape($ret['contact_autologin_key'])."'" : "''";
-		$rq .= "WHERE contact_id = '".$contact_id."'";
-		$DBRESULT = $pearDB->query($rq);
-		
-		/*
-		 * Update user object..
-		 */
-		$oreon->user->name = $ret["contact_name"];
-		$oreon->user->alias = $ret["contact_alias"];
-		$oreon->user->lang = $ret["contact_lang"];
-		$oreon->user->email = $ret["contact_email"];
-                $oreon->user->setToken(isset($ret["contact_autologin_key"]) ? $ret['contact_autologin_key'] : "''");
 	}
-?>
+    
+    $rq .=	"contact_location = ";
+	isset($ret["contact_location"]) && $ret["contact_location"] != NULL ? $rq .= "'".htmlentities($ret["contact_location"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	$rq .=	"contact_lang = ";
+	isset($ret["contact_lang"]) && $ret["contact_lang"] != NULL ? $rq .= "'".htmlentities($ret["contact_lang"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	$rq .= "contact_email = ";
+	isset($ret["contact_email"]) && $ret["contact_email"] != NULL ? $rq .= "'".htmlentities($ret["contact_email"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	$rq .= "contact_pager = ";
+	isset($ret["contact_pager"]) && $ret["contact_pager"] != NULL ? $rq .= "'".htmlentities($ret["contact_pager"], ENT_QUOTES, "UTF-8")."', ": $rq .= "NULL, ";
+	$rq .= "contact_js_effects = ";
+	isset($ret["contact_js_effects"]) ? $rq .= "'1', ": $rq .= "'0', ";
+    $rq .= "contact_autologin_key = ";
+    $rq .= isset($ret["contact_autologin_key"]) ? "'".$pearDB->escape($ret['contact_autologin_key'])."'" : "''";
+	$rq .= "WHERE contact_id = '".$contact_id."'";
+	$DBRESULT = $pearDB->query($rq);
+	
+	/*
+	 * Update user object..
+	 */
+	$oreon->user->name = $ret["contact_name"];
+	$oreon->user->alias = $ret["contact_alias"];
+	$oreon->user->lang = $ret["contact_lang"];
+	$oreon->user->email = $ret["contact_email"];
+    $oreon->user->setToken(isset($ret["contact_autologin_key"]) ? $ret['contact_autologin_key'] : "''");
+}
--- www/include/options/oreon/myAccount/formMyAccount.php
+++ www/include/options/oreon/myAccount/formMyAccount.php
@@ -31,167 +31,166 @@
  * 
  * For more information : contact@centreon.com
  * 
- * SVN : $URL$
- * SVN : $Id$
- * 
  */
 
-	if (!isset ($oreon))
-		exit ();
+if (!isset ($centreon)) {
+	exit ();
+}
+
+// Pear library
+require_once "HTML/QuickForm.php";
+require_once 'HTML/QuickForm/advmultiselect.php';
+require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+
+require_once "./include/common/common-Func.php";
+
+$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
 	
-	// Pear library
-	require_once "HTML/QuickForm.php";
-	require_once 'HTML/QuickForm/advmultiselect.php';
-	require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
+// Path to the configuration dir
+$path = "./include/options/oreon/myAccount/";
 
-	require_once "./include/common/common-Func.php";
-	
-	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
-		
-	// Path to the configuration dir
-	$path = "./include/options/oreon/myAccount/";
-	
-	// PHP Functions
-	require_once $path."DB-Func.php";
-	
-	/*
-	 * Database retrieve information for the User
-	 */
-	$cct = array();
-	if ($o == "c")	{	
-		$DBRESULT = $pearDB->query("SELECT contact_id, contact_name, contact_alias, contact_lang, 
-                                                contact_email, contact_pager, contact_js_effects, contact_autologin_key 
-                                            FROM contact 
-                                            WHERE contact_id = '".$oreon->user->get_id()."' LIMIT 1");
-		// Set base value
-		$cct = array_map("myDecode", $DBRESULT->fetchRow());
-                $res = $pearDB->query("SELECT cp_key, cp_value 
-                                       FROM contact_param 
-                                       WHERE cp_contact_id = '".$pearDB->escape($oreon->user->get_id())."'");
-                while ($row = $res->fetchRow()) {
-                    $cct[$row['cp_key']] = $row['cp_value'];
-                }
-	}
-        
-	/*
-	 * Database retrieve information for differents elements list we need on the page
-	 *
-	 * Langs -> $langs Array
-         */
-	$langs = array();
-	$langs = getLangs();	
-	$attrsText 		= array("size"=>"35");
+// PHP Functions
+require_once $path."DB-Func.php";
 
-	$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
-	$form->addElement('header', 'title', _("Change my settings"));
+/*
+ * Database retrieve information for the User
+ */
+$cct = array();
+if ($o == "c")	{	
+	$DBRESULT = $pearDB->query("SELECT contact_id, contact_name, contact_alias, contact_lang, 
+                                            contact_email, contact_pager, contact_js_effects, contact_autologin_key 
+                                        FROM contact 
+                                        WHERE contact_id = '".$centreon->user->get_id()."' LIMIT 1");
+	// Set base value
+	$cct = array_map("myDecode", $DBRESULT->fetchRow());
+    $res = $pearDB->query("SELECT cp_key, cp_value 
+                           FROM contact_param 
+                           WHERE cp_contact_id = '".$pearDB->escape($centreon->user->get_id())."'");
+    while ($row = $res->fetchRow()) {
+        $cct[$row['cp_key']] = $row['cp_value'];
+    }
+}
+    
+/*
+ * Database retrieve information for differents elements list we need on the page
+ *
+ * Langs -> $langs Array
+     */
+$langs = array();
+$langs = getLangs();	
+$attrsText 		= array("size"=>"35");
 
-	$form->addElement('header', 'information', _("General Information"));
-	$form->addElement('text', 'contact_name', _("Name"), $attrsText);
-	$form->addElement('text', 'contact_alias', _("Alias / Login"), $attrsText);
-	$form->addElement('text', 'contact_email', _("Email"), $attrsText);
-	$form->addElement('text', 'contact_pager', _("Pager"), $attrsText);
-	$form->addElement('password', 'contact_passwd', _("Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd1", "onFocus" => "resetPwdType(this);"));
-	$form->addElement('password', 'contact_passwd2', _("Confirm Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd2", "onFocus" => "resetPwdType(this);"));
-        $form->addElement('button','contact_gen_passwd',_("Generate"), array('onclick'=>'generatePassword("passwd");', 'class' => 'btc bt_info'));
-        $form->addElement('text', 'contact_autologin_key', _("Autologin Key"), array("size" => "30", "id" => "aKey"));
-        $form->addElement('button','contact_gen_akey',_("Generate"), array( 'onclick' => 'generatePassword("aKey");', 'class' => 'btc bt_info'));
-        $form->addElement('select', 'contact_lang', _("Language"), $langs);
-        $form->addElement('checkbox', 'contact_js_effects', _("Animation effects"), null, $attrsText);
+$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
+$form->addElement('header', 'title', _("Change my settings"));
+
+$form->addElement('header', 'information', _("General Information"));
+$form->addElement('text', 'contact_name', _("Name"), $attrsText);
+$form->addElement('text', 'contact_alias', _("Alias / Login"), $attrsText);
+$form->addElement('text', 'contact_email', _("Email"), $attrsText);
+$form->addElement('text', 'contact_pager', _("Pager"), $attrsText);
+$form->addElement('password', 'contact_passwd', _("Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd1", "onFocus" => "resetPwdType(this);"));
+$form->addElement('password', 'contact_passwd2', _("Confirm Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd2", "onFocus" => "resetPwdType(this);"));
+$form->addElement('button','contact_gen_passwd',_("Generate"), array('onclick'=>'generatePassword("passwd");', 'class' => 'btc bt_info'));
+$form->addElement('text', 'contact_autologin_key', _("Autologin Key"), array("size" => "30", "id" => "aKey"));
+$form->addElement('button','contact_gen_akey',_("Generate"), array( 'onclick' => 'generatePassword("aKey");', 'class' => 'btc bt_info'));
+$form->addElement('select', 'contact_lang', _("Language"), $langs);
+$form->addElement('checkbox', 'contact_js_effects', _("Animation effects"), null, $attrsText);
 
 
-        $form->addElement('checkbox', 'monitoring_host_notification_0', _('Show Up status'));
-        $form->addElement('checkbox', 'monitoring_host_notification_1', _('Show Down status'));
-        $form->addElement('checkbox', 'monitoring_host_notification_2', _('Show Unreachable status'));
-        $form->addElement('checkbox', 'monitoring_svc_notification_0', _('Show OK status'));
-        $form->addElement('checkbox', 'monitoring_svc_notification_1', _('Show Warning status'));
-        $form->addElement('checkbox', 'monitoring_svc_notification_2', _('Show Critical status'));
-        $form->addElement('checkbox', 'monitoring_svc_notification_3', _('Show Unknown status'));
+$form->addElement('checkbox', 'monitoring_host_notification_0', _('Show Up status'));
+$form->addElement('checkbox', 'monitoring_host_notification_1', _('Show Down status'));
+$form->addElement('checkbox', 'monitoring_host_notification_2', _('Show Unreachable status'));
+$form->addElement('checkbox', 'monitoring_svc_notification_0', _('Show OK status'));
+$form->addElement('checkbox', 'monitoring_svc_notification_1', _('Show Warning status'));
+$form->addElement('checkbox', 'monitoring_svc_notification_2', _('Show Critical status'));
+$form->addElement('checkbox', 'monitoring_svc_notification_3', _('Show Unknown status'));
 
-        $sound_files = scandir(_CENTREON_PATH_."www/sounds/");
-        $sounds = array(null => null);
-        foreach ($sound_files as $f) {
-            if($f == "." || $f == "..") {
-                continue;
-            }
-            $info = pathinfo($f);
-            $fname = basename($f, ".".$info['extension']);
-            $sounds[$fname] = $fname;
-        }
-        $form->addElement('select', 'monitoring_sound_host_notification_0', _("Sound for Up status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_host_notification_1', _("Sound for Down status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_host_notification_2', _("Sound for Unreachable status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_svc_notification_0', _("Sound for OK status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_svc_notification_1', _("Sound for Warning status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_svc_notification_2', _("Sound for Critical status"), $sounds);
-        $form->addElement('select', 'monitoring_sound_svc_notification_3', _("Sound for Unknown status"), $sounds);
-        
-        $attrTimezones = array(
-            'datasourceOrigin' => 'ajax',
-            'availableDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=list',
-            'defaultDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=defaultValues&target=contact&field=contact_location&id=' . $oreon->user->get_id(),
-            'multiple' => false,
-            'linkedObject' => 'centreonGMT'
-        );
-        $form->addElement('select2', 'contact_location', _("Timezone / Location"), array(), $attrTimezones);
+$sound_files = scandir(_CENTREON_PATH_."www/sounds/");
+$sounds = array(null => null);
+foreach ($sound_files as $f) {
+    if($f == "." || $f == "..") {
+        continue;
+    }
+    $info = pathinfo($f);
+    $fname = basename($f, ".".$info['extension']);
+    $sounds[$fname] = $fname;
+}
+$form->addElement('select', 'monitoring_sound_host_notification_0', _("Sound for Up status"), $sounds);
+$form->addElement('select', 'monitoring_sound_host_notification_1', _("Sound for Down status"), $sounds);
+$form->addElement('select', 'monitoring_sound_host_notification_2', _("Sound for Unreachable status"), $sounds);
+$form->addElement('select', 'monitoring_sound_svc_notification_0', _("Sound for OK status"), $sounds);
+$form->addElement('select', 'monitoring_sound_svc_notification_1', _("Sound for Warning status"), $sounds);
+$form->addElement('select', 'monitoring_sound_svc_notification_2', _("Sound for Critical status"), $sounds);
+$form->addElement('select', 'monitoring_sound_svc_notification_3', _("Sound for Unknown status"), $sounds);
+    
+$attrTimezones = array(
+    'datasourceOrigin' => 'ajax',
+    'availableDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=list',
+    'defaultDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=defaultValues&target=contact&field=contact_location&id=' . $centreon->user->get_id(),
+    'multiple' => false,
+    'linkedObject' => 'centreonGMT'
+);
+$form->addElement('select2', 'contact_location', _("Timezone / Location"), array(), $attrTimezones);
 
-	$redirect = $form->addElement('hidden', 'o');
-	$redirect->setValue($o);
-	
-	function myReplace()	{
-		global $form;
-		$ret = $form->getSubmitValues();
-		return (str_replace(" ", "_", $ret["contact_name"]));
-	}
-	$form->applyFilter('__ALL__', 'myTrim');
-	$form->applyFilter('contact_name', 'myReplace');
-	$form->addRule('contact_name', _("Compulsory name"), 'required');
-	$form->addRule('contact_alias', _("Compulsory alias"), 'required');
-	$form->addRule('contact_email', _("Valid Email"), 'required');
-	$form->addRule(array('contact_passwd', 'contact_passwd2'), _("Passwords do not match"), 'compare');
-	$form->registerRule('exist', 'callback', 'testExistence');
-	$form->addRule('contact_name', _("Name already in use"), 'exist');
-	$form->registerRule('existAlias', 'callback', 'testAliasExistence');
-	$form->addRule('contact_alias', _("Name already in use"), 'existAlias');
-	$form->setRequiredNote("<font style='color: red;'>*</font>"._("Required fields"));
-	
-	// Smarty template Init
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl);
+$redirect = $form->addElement('hidden', 'o');
+$redirect->setValue($o);
 
-	// Modify a contact information
-	if ($o == "c")	{
-		$subC = $form->addElement('submit', 'submitC', _("Save"), array("class" => "btc bt_success"));
-		$res = $form->addElement('reset', 'reset', _("Reset"), array("class" => "btc bt_default"));
-	    $form->setDefaults($cct);
-	}
-	
-	if ($form->validate())	{
-		updateContactInDB($oreon->user->get_id());
-		if ($form->getSubmitValue("contact_passwd"))
-			$oreon->user->passwd = md5($form->getSubmitValue("contact_passwd"));
-		$o = NULL;
-		$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c'", 'class' => 'btc bt_info'));
-		$form->freeze();
-	}
-	//Apply a template definition	
-	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
-	$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
-	$form->accept($renderer);	
-	$tpl->assign('form', $renderer->toArray());	
-	$tpl->assign('o', $o);		
-	$tpl->display("formMyAccount.ihtml");
+function myReplace()	{
+	global $form;
+	$ret = $form->getSubmitValues();
+	return (str_replace(" ", "_", $ret["contact_name"]));
+}
+$form->applyFilter('__ALL__', 'myTrim');
+$form->applyFilter('contact_name', 'myReplace');
+$form->addRule('contact_name', _("Compulsory name"), 'required');
+$form->addRule('contact_alias', _("Compulsory alias"), 'required');
+$form->addRule('contact_email', _("Valid Email"), 'required');
+$form->addRule(array('contact_passwd', 'contact_passwd2'), _("Passwords do not match"), 'compare');
+$form->registerRule('exist', 'callback', 'testExistence');
+$form->addRule('contact_name', _("Name already in use"), 'exist');
+$form->registerRule('existAlias', 'callback', 'testAliasExistence');
+$form->addRule('contact_alias', _("Name already in use"), 'existAlias');
+$form->setRequiredNote("<font style='color: red;'>*</font>"._("Required fields"));
+
+// Smarty template Init
+$tpl = new Smarty();
+$tpl = initSmartyTpl($path, $tpl);
+
+// Modify a contact information
+if ($o == "c")	{
+	$subC = $form->addElement('submit', 'submitC', _("Save"), array("class" => "btc bt_success"));
+	$res = $form->addElement('reset', 'reset', _("Reset"), array("class" => "btc bt_default"));
+    $form->setDefaults($cct);
+}
+
+if ($form->validate())	{
+	updateContactInDB($centreon->user->get_id());
+	if ($form->getSubmitValue("contact_passwd"))
+		$centreon->user->passwd = md5($form->getSubmitValue("contact_passwd"));
+	$o = NULL;
+	$form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c'", 'class' => 'btc bt_info'));
+	$form->freeze();
+}
+
+//Apply a template definition	
+$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+$renderer->setRequiredTemplate('{$label}&nbsp;<font color="red" size="1">*</font>');
+$renderer->setErrorTemplate('<font color="red">{$error}</font><br />{$html}');
+$form->accept($renderer);	
+$tpl->assign('form', $renderer->toArray());	
+$tpl->assign('o', $o);		
+$tpl->display("formMyAccount.ihtml");
 ?>
 <script type='text/javascript' src='./include/common/javascript/keygen.js'></script>
 <script type="text/javascript">
-    jQuery(function() {
-        jQuery("select[name*='_notification_']").change(function() {
-            if (jQuery(this).val()) {
-  			    var snd = new buzz.sound("sounds/"+jQuery(this).val(), {
-                    formats: [ "ogg", "mp3" ]
- 			    });
-			}
-            snd.play();
-        });
+jQuery(function() {
+    jQuery("select[name*='_notification_']").change(function() {
+        if (jQuery(this).val()) {
+			    var snd = new buzz.sound("sounds/"+jQuery(this).val(), {
+                formats: [ "ogg", "mp3" ]
+			    });
+		}
+        snd.play();
     });
+});
 </script>
