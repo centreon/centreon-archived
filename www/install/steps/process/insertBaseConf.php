2340bff131c29ba40caa465b7ec0161161b2c109
Julien Mathis <jmathis@centreon.com>
2016-06-20 15:36:28 +0200

    fix problem for inserting base conf

--- www/install/steps/process/insertBaseConf.php
+++ www/install/steps/process/insertBaseConf.php
@@ -50,12 +50,8 @@ mysql_select_db($_SESSION['CONFIGURATION_DB']);
 splitQueries('../../insertMacros.sql', ';', null, '../../tmp/insertMacros');
 splitQueries('../../insertCommands.sql', ';', null, '../../tmp/insertCommands.sql');
 splitQueries('../../insertTimeperiods.sql', ';', null, '../../tmp/insertTimeperiods.sql');
-if (isset($_SESSION['MONITORING_ENGINE']) && is_file('../../var/baseconf/'.$_SESSION['MONITORING_ENGINE'].'.sql')) {
-    splitQueries('../../var/baseconf/'.$_SESSION['MONITORING_ENGINE'].'.sql', ';', null, '../../tmp/'.$_SESSION['MONITORING_ENGINE']);
-}
-if (isset($_SESSION['BROKER_MODULE']) && is_file('../../var/baseconf/'.$_SESSION['BROKER_MODULE'].'.sql')) {
-    splitQueries('../../var/baseconf/'.$_SESSION['BROKER_MODULE'].'.sql', ';', null, '../../tmp/'.$_SESSION['BROKER_MODULE']);
-}
+splitQueries('../../var/baseconf/centreon-engine.sql', ';', null, '../../tmp/centrepn-engine.sql');
+splitQueries('../../var/baseconf/centreon-broker.sql', ';', null, '../../tmp/centreon-broker.sql');
 splitQueries('../../insertTopology.sql', ';', null, '../../tmp/insertTopology');
 splitQueries('../../insertBaseConf.sql', ';', null, '../../tmp/insertBaseConf');
 
