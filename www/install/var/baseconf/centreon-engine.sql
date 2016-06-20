c70247c04768bc13ed1b52398723178b398de940
Julien Mathis <jmathis@centreon.com>
2016-06-20 16:34:04 +0200

    change description of macro $USER1$

--- www/install/var/baseconf/centreon-engine.sql
+++ www/install/var/baseconf/centreon-engine.sql
@@ -7,8 +7,8 @@ INSERT INTO `cfg_nagios` (`nagios_id`, `nagios_name`, `log_file`, `cfg_dir`, `ob
 INSERT INTO `cfg_nagios_broker_module` (`cfg_nagios_id`, `broker_module`) VALUES (1, '@CENTREON_ENGINE_LIB@/externalcmd.so');
 
 INSERT INTO `cfg_resource` (`resource_id`, `resource_name`, `resource_line`, `resource_comment`, `resource_activate`) VALUES
-(1, '$USER1$', '@MONITORINGENGINE_PLUGIN@', 'Centreon Nagios Plugins Path', '1'),
-(2, '$CENTREONPLUGINS$', '@CENTREONPLUGINS@', 'Centreon Plugin Path', '1');
+(1, '$USER1$', '@MONITORINGENGINE_PLUGIN@', 'Nagios Plugins Path', '1'),
+(2, '$CENTREONPLUGINS$', '@CENTREONPLUGINS@', 'Centreon Plugins Path', '1');
 
 INSERT INTO `cfg_resource_instance_relations` (`resource_id`, `instance_id` ) VALUES (1, 1), (2, 1);
 INSERT INTO `options` (`key`, `value`) VALUES ('cengine_path_connectors','@CENTREON_ENGINE_CONNECTORS@/');
