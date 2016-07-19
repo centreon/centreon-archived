f7aea3d6413e2900b238964b4eda264475eebc5d
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-07-19 13:53:17 +0200

    Fix style of www/include/options/media/images/listImg.php.

--- www/include/options/media/images/listImg.php
+++ www/include/options/media/images/listImg.php
@@ -43,9 +43,17 @@ include_once("./class/centreonUtils.class.php");
 $search = '';
 if (isset($_POST['searchM']) && $_POST['searchM']) {
     $search = $_POST['searchM'];
-    $res = $pearDB->query("SELECT COUNT(*) FROM view_img, view_img_dir, view_img_dir_relation WHERE (img_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' OR dir_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%') AND img_img_id = img_id AND dir_dir_parent_id = dir_id");
+    $res = $pearDB->query(
+        "SELECT COUNT(*) FROM view_img, view_img_dir, view_img_dir_relation "
+        . "WHERE (img_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%' "
+        . "OR dir_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%') "
+        . "AND img_img_id = img_id AND dir_dir_parent_id = dir_id"
+    );
 } else {
-    $res = $pearDB->query("SELECT COUNT(*) FROM view_img, view_img_dir, view_img_dir_relation WHERE img_img_id = img_id AND dir_dir_parent_id = dir_id");
+    $res = $pearDB->query(
+        "SELECT COUNT(*) FROM view_img, view_img_dir, view_img_dir_relation "
+        . "WHERE img_img_id = img_id AND dir_dir_parent_id = dir_id"
+    );
 }
 $tmp = $res->fetchRow();
 $rows = $tmp["COUNT(*)"];
@@ -66,10 +74,23 @@ $tpl->assign("headerMenu_desc", _("Directory"));
 $tpl->assign("headerMenu_img", _("Image"));
 $tpl->assign("headerMenu_comment", _("Comment"));
 
-if ($search)
-    $rq = "SELECT * FROM view_img_dir LEFT JOIN view_img_dir_relation ON dir_dir_parent_id = dir_id LEFT JOIN view_img ON img_img_id = img_id WHERE (img_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%'  OR dir_name LIKE '%".htmlentities($search, ENT_QUOTES, "UTF-8")."%') ORDER BY dir_alias, img_name LIMIT ".$num * $limit.", ".$limit;
-else
-    $rq = "SELECT * FROM view_img_dir LEFT JOIN view_img_dir_relation ON dir_dir_parent_id = dir_id LEFT JOIN view_img ON img_img_id = img_id ORDER BY dir_alias, img_name LIMIT ".$num * $limit.", ".$limit;
+if ($search) {
+    $rq =
+        "SELECT * FROM view_img_dir "
+        . "LEFT JOIN view_img_dir_relation ON dir_dir_parent_id = dir_id "
+        . "LEFT JOIN view_img ON img_img_id = img_id "
+        . "WHERE (img_name LIKE '%" . htmlentities($search, ENT_QUOTES, "UTF-8") . "%' "
+        . "OR dir_name LIKE '%" . htmlentities($search, ENT_QUOTES, "UTF-8") . "%') "
+        . "ORDER BY dir_alias, img_name "
+        . "LIMIT " . $num * $limit . ", " . $limit;
+} else {
+    $rq =
+        "SELECT * FROM view_img_dir "
+        . "LEFT JOIN view_img_dir_relation ON dir_dir_parent_id = dir_id "
+        . "LEFT JOIN view_img ON img_img_id = img_id "
+        . "ORDER BY dir_alias, img_name "
+        . "LIMIT " . $num * $limit . ", " . $limit;
+}
 $res = $pearDB->query($rq);
 
 $form = new HTML_QuickForm('form', 'POST', "?p=".$p);
@@ -92,18 +113,27 @@ for ($i = 0; $elem = $res->fetchRow(); $i++) {
     }
 
     if ($elem['img_id']) {
-        if (isset($search) && $search)
+        if (isset($search) && $search) {
             $searchOpt = "&search=".$search;
-        else
+        } else {
             $searchOpt = "";
+        }
         $selectedImgElem = $form->addElement('checkbox', "select[".$elem['dir_id']."-".$elem['img_id']."]");
-        $rowOpt = array("RowMenu_select"=>$selectedImgElem->toHtml(),
-                "RowMenu_ImgLink"=>"?p=".$p."&o=ci&img_id=".$elem['img_id'],
-                "RowMenu_DirLink"=>"?p=".$p."&o=cd&dir_id=".$elem['dir_id'],
-                "RowMenu_dir"=>$elem["dir_name"],
-                "RowMenu_img"=>CentreonUtils::escapeSecure(html_entity_decode($elem["dir_alias"]."/".$elem["img_path"], ENT_QUOTES, "UTF-8")),
-                "RowMenu_name"=>CentreonUtils::escapeSecure(html_entity_decode($elem["img_name"], ENT_QUOTES, "UTF-8")),
-                "RowMenu_comment"=>CentreonUtils::escapeSecure(html_entity_decode($elem["img_comment"], ENT_QUOTES, "UTF-8")));
+        $rowOpt = array(
+            "RowMenu_select" => $selectedImgElem->toHtml(),
+            "RowMenu_ImgLink" => "?p=".$p."&o=ci&img_id=".$elem['img_id'],
+            "RowMenu_DirLink" => "?p=".$p."&o=cd&dir_id=".$elem['dir_id'],
+            "RowMenu_dir" => $elem["dir_name"],
+            "RowMenu_img" => CentreonUtils::escapeSecure(
+                html_entity_decode($elem["dir_alias"]."/".$elem["img_path"], ENT_QUOTES, "UTF-8")
+            ),
+            "RowMenu_name" => CentreonUtils::escapeSecure(
+                html_entity_decode($elem["img_name"], ENT_QUOTES, "UTF-8")
+            ),
+            "RowMenu_comment" => CentreonUtils::escapeSecure(
+                html_entity_decode($elem["img_comment"], ENT_QUOTES, "UTF-8")
+            )
+        );
         $elemArr[$elem['dir_id']]["elem"][$i] = $rowOpt;
         $elemArr[$elem['dir_id']]["head"]["counter"]++;
     }
@@ -113,7 +143,14 @@ $tpl->assign("elemArr", $elemArr);
 /*
  * Different messages we put in the template
  */
-$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
+$tpl->assign(
+    'msg',
+    array(
+        "addL" => "?p=" . $p . "&o=a",
+        "addT" => _("Add"),
+        "delConfirm" => _("Do you confirm the deletion ?")
+    )
+);
 
 ?>
 <SCRIPT LANGUAGE="JavaScript">
@@ -152,20 +189,20 @@ function setSubNodes(theElement, like) {
 
 </SCRIPT>
 <?php
-$actions = array(NULL=>_("More actions"), "d"=>_("Delete"), "m"=>_("Move images"));
-$form->addElement('select', 'o1', NULL, $actions, array('onchange'=>"javascript:submitO('o1');"));
-$form->addElement('select', 'o2', NULL, $actions, array('onchange'=>"javascript:submitO('o2');"));
-$form->setDefaults(array('o1' => NULL));
-$form->setDefaults(array('o2' => NULL));
+$actions = array(null=>_("More actions"), "d"=>_("Delete"), "m"=>_("Move images"));
+$form->addElement('select', 'o1', null, $actions, array('onchange'=>"javascript:submitO('o1');"));
+$form->addElement('select', 'o2', null, $actions, array('onchange'=>"javascript:submitO('o2');"));
+$form->setDefaults(array('o1' => null));
+$form->setDefaults(array('o2' => null));
 
 
 $o1 = $form->getElement('o1');
-$o1->setValue(NULL);
-$o1->setSelected(NULL);
+$o1->setValue(null);
+$o1->setSelected(null);
 
 $o2 = $form->getElement('o2');
-$o2->setValue(NULL);
-$o2->setSelected(NULL);
+$o2->setValue(null);
+$o2->setSelected(null);
 
 $tpl->assign('limit', $limit);
 $tpl->assign('p', $p);
