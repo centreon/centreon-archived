01af9f52327ff2c13d355daefe747c5b6368cd8d
Julien Mathis <jmathis@merethis.com>
2016-07-26 22:56:21 +0200

    improve coding style

--- www/include/common/pagination.php
+++ www/include/common/pagination.php
@@ -35,210 +35,226 @@
  * SVN : $Id$
  *
  */
-	global $oreon;
+    global $oreon;
 
-	if (!isset($oreon))
-		exit();
+if (!isset($oreon)) {
+    exit();
+}
 
-	global $num, $limit, $search, $url, $pearDB, $search_type_service, $search_type_host, $host_name, $hostgroup, $rows, $p, $gopt, $pagination, $poller, $template, $search_output, $search_service;
+    global $num, $limit, $search, $url, $pearDB, $search_type_service, $search_type_host, $host_name, $hostgroup, $rows, $p, $gopt, $pagination, $poller, $template, $search_output, $search_service;
 
-        $type = isset($_REQUEST["type"]) ? $_REQUEST["type"] : NULL;
-	isset($_GET["o"]) ? $o = $_GET["o"] : $o = NULL;
+        $type = isset($_REQUEST["type"]) ? $_REQUEST["type"] : null;
+    isset($_GET["o"]) ? $o = $_GET["o"] : $o = null;
 
-	if (isset($_GET["num"]))
-		$num = $_GET["num"];
-	else if (!isset($_GET["num"]) && isset($oreon->historyPage[$url]) && $oreon->historyPage[$url])
-		$num = $oreon->historyPage[$url];
-	else
-		$num = 0;
+if (isset($_GET["num"])) {
+    $num = $_GET["num"];
+} elseif (!isset($_GET["num"]) && isset($oreon->historyPage[$url]) && $oreon->historyPage[$url]) {
+    $num = $oreon->historyPage[$url];
+} else {
+    $num = 0;
+}
 
-	$num = mysql_real_escape_string($num);
+    $num = mysql_real_escape_string($num);
 
-	$tab_order = array("sort_asc" => "sort_desc", "sort_desc" => "sort_asc");
+    $tab_order = array("sort_asc" => "sort_desc", "sort_desc" => "sort_asc");
 
-	if (isset($_GET["search_type_service"])){
-		$search_type_service = $_GET["search_type_service"];
-		$oreon->search_type_service = $_GET["search_type_service"];
-	} else if (isset($oreon->search_type_service))
-		 $search_type_service = $oreon->search_type_service;
-	else
-		$search_type_service = NULL;
+if (isset($_GET["search_type_service"])) {
+    $search_type_service = $_GET["search_type_service"];
+    $oreon->search_type_service = $_GET["search_type_service"];
+} elseif (isset($oreon->search_type_service)) {
+         $search_type_service = $oreon->search_type_service;
+} else {
+    $search_type_service = null;
+}
 
-	if (isset($_GET["search_type_host"])){
-		$search_type_host = $_GET["search_type_host"];
-		$oreon->search_type_host = $_GET["search_type_host"];
-	} else if (isset($oreon->search_type_host))
-		 $search_type_host = $oreon->search_type_host;
-	else
-		$search_type_host = NULL;
+if (isset($_GET["search_type_host"])) {
+    $search_type_host = $_GET["search_type_host"];
+    $oreon->search_type_host = $_GET["search_type_host"];
+} elseif (isset($oreon->search_type_host)) {
+         $search_type_host = $oreon->search_type_host;
+} else {
+    $search_type_host = null;
+}
 
-	if (!isset($_GET["search_type_host"]) && !isset($oreon->search_type_host) && !isset($_GET["search_type_service"]) && !isset($oreon->search_type_service)){
-		$search_type_host = 1;
-		$oreon->search_type_host = 1;
-		$search_type_service = 1;
-		$oreon->search_type_service = 1;
-	}
+if (!isset($_GET["search_type_host"]) && !isset($oreon->search_type_host) && !isset($_GET["search_type_service"]) && !isset($oreon->search_type_service)) {
+    $search_type_host = 1;
+    $oreon->search_type_host = 1;
+    $search_type_service = 1;
+    $oreon->search_type_service = 1;
+}
 
-	$url_var = "";
-	if (isset($search_type_service)) {
-		$url_var .= "&search_type_service=" . $search_type_service;
-	}
-	if (isset($search_type_host)) {
-		$url_var .= "&search_type_host=" . $search_type_host;
-	}
-	if (isset($hostgroup)) {
-		$url_var .= "&hostgroup=" . $hostgroup;
-	}
-	if (isset($host_name)) {
-		$url_var .= "&search_host=" . $host_name;
-	}
-	if (isset($search_service)) {
-		$url_var .= "&search_service=" . $search_service;
-	}
-	if (isset($search_output) && $search_output != "") {
-		$url_var .= "&search_output=" . $search_output;
-	}
+    $url_var = "";
+if (isset($search_type_service)) {
+    $url_var .= "&search_type_service=" . $search_type_service;
+}
+if (isset($search_type_host)) {
+    $url_var .= "&search_type_host=" . $search_type_host;
+}
+if (isset($hostgroup)) {
+    $url_var .= "&hostgroup=" . $hostgroup;
+}
+if (isset($host_name)) {
+    $url_var .= "&search_host=" . $host_name;
+}
+if (isset($search_service)) {
+    $url_var .= "&search_service=" . $search_service;
+}
+if (isset($search_output) && $search_output != "") {
+    $url_var .= "&search_output=" . $search_output;
+}
 
-	if (isset($_GET["order"])){
-		$url_var .= "&order=".$_GET["order"];
-		$order = $_GET["order"];
-	}
-	if (isset($_GET["sort_types"])){
-		$url_var .= "&sort_types=".$_GET["sort_types"];
-		$sort_type = $_GET["sort_types"];
-	}
+if (isset($_GET["order"])) {
+    $url_var .= "&order=".$_GET["order"];
+    $order = $_GET["order"];
+}
+if (isset($_GET["sort_types"])) {
+    $url_var .= "&sort_types=".$_GET["sort_types"];
+    $sort_type = $_GET["sort_types"];
+}
 
-	/* Fix for downtime */
-	if (isset($_REQUEST['view_all'])) {
-	    $url_var .= "&view_all=" . $_REQUEST['view_all'];
-	}
-	
-	if (isset($_REQUEST['view_downtime_cycle'])) {
-		$url_var .= "&view_downtime_cycle=" . $_REQUEST['view_downtime_cycle'];
-	}
-	if (isset($_REQUEST['search_author'])) {
-	    $url_var .= "&search_author=" . $_REQUEST['search_author'];
-	}
+    /* Fix for downtime */
+if (isset($_REQUEST['view_all'])) {
+    $url_var .= "&view_all=" . $_REQUEST['view_all'];
+}
+    
+if (isset($_REQUEST['view_downtime_cycle'])) {
+    $url_var .= "&view_downtime_cycle=" . $_REQUEST['view_downtime_cycle'];
+}
+if (isset($_REQUEST['search_author'])) {
+    $url_var .= "&search_author=" . $_REQUEST['search_author'];
+}
 
-	/* Fix for status in service configuration */
-	if (isset($_REQUEST['status'])) {
-	    $url_var .= '&status=' . $_REQUEST['status'];
-	}
+    /* Fix for status in service configuration */
+if (isset($_REQUEST['status'])) {
+    $url_var .= '&status=' . $_REQUEST['status'];
+}
     
     /* Fix for status in service configuration */
-	if (isset($_REQUEST['hostgroups'])) {
-	    $url_var .= '&hostgroups=' . $_REQUEST['hostgroups'];
-	}
+if (isset($_REQUEST['hostgroups'])) {
+    $url_var .= '&hostgroups=' . $_REQUEST['hostgroups'];
+}
 
-	/*
+    /*
 	 * Smarty template Init
 	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl($path, $tpl, "./include/common/");
+    $tpl = new Smarty();
+    $tpl = initSmartyTpl($path, $tpl, "./include/common/");
 
-	$page_max = ceil($rows / $limit);
-	if ($num >= $page_max && $rows) {
-		$num = $page_max - 1;
-	}
+    $page_max = ceil($rows / $limit);
+if ($num >= $page_max && $rows) {
+    $num = $page_max - 1;
+}
 
-	$pageArr = array();
-	$istart = 0;
-	for ($i = 5, $istart = $num; $istart && $i > 0; $i--)
-		$istart--;
-	for ($i2 = 0, $iend = $num; ( $iend <  ($rows / $limit - 1)) && ( $i2 < (5 + $i)); $i2++)
-		$iend++;
+    $pageArr = array();
+    $istart = 0;
+for ($i = 5, $istart = $num; $istart && $i > 0; $i--) {
+    $istart--;
+}
+for ($i2 = 0, $iend = $num; ( $iend <  ($rows / $limit - 1)) && ( $i2 < (5 + $i)); $i2++) {
+    $iend++;
+}
 
-	if ($rows != 0) {
-
-		for ($i = $istart; $i <= $iend; $i++)
-			$pageArr[$i] = array("url_page"=>"./main.php?p=".$p."&num=$i&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o . $url_var, "label_page"=>"<b>".($i +1)."</b>","num"=> $i);
-
-		if ($i > 1)
-			$tpl->assign("pageArr", $pageArr);
-
-		$tpl->assign("num", $num);
-		$tpl->assign("first", _("First page"));
-		$tpl->assign("previous", _("Previous page"));
-		$tpl->assign("next", _("Next page"));
-		$tpl->assign("last", _("Last page"));
-
-		if (($prev = $num - 1) >= 0)
-			$tpl->assign('pagePrev', ("./main.php?p=".$p."&num=$prev&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
-
-		if (($next = $num + 1) < ($rows/$limit))
-			$tpl->assign('pageNext', ("./main.php?p=".$p."&num=$next&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
-
-		$pageNumber = ceil($rows / $limit);
-		if (($rows / $limit) > 0)
-			$tpl->assign('pageNumber', ($num +1)."/".$pageNumber);
-		else
-			$tpl->assign('pageNumber', ($num)."/".$pageNumber);
-
-		if ($page_max > 5 && $num != 0)
-			$tpl->assign('firstPage', ("./main.php?p=".$p."&num=0&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
-		if ($page_max > 5 && $num != ($pageNumber-1))
-			$tpl->assign('lastPage', ("./main.php?p=".$p."&num=".($pageNumber-1)."&limit=".$limit."&template=$template&poller=".$poller."&search=".$search."&type=".$type."&o=" . $o .$url_var));
-
-		/*
-		 * Select field to change the number of row on the page
-		 */
-		for ($i = 10; $i <= 100; $i = $i +10)
-			$select[$i]=$i;
-		if (isset($gopt[$pagination]) && $gopt[$pagination])
-			$select[$gopt[$pagination]] = $gopt[$pagination];
-		if (isset($rows) && $rows)
-			$select[$rows] = $rows;
-		ksort($select);
-    } else {
-       for ($i = 10; $i <= 100; $i = $i +10)
-			$select[$i]=$i; 
+if ($rows != 0) {
+    for ($i = $istart; $i <= $iend; $i++) {
+        $pageArr[$i] = array("url_page"=>"./main.php?p=".$p."&num=$i&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o . $url_var, "label_page"=>"<b>".($i +1)."</b>","num"=> $i);
     }
 
-	?><script type="text/javascript">
-	function setL(_this){
-		var _l = document.getElementsByName('l');
-		document.forms['form'].elements['limit'].value = _this;
-		_l[0].value = _this;
-		_l[1].value = _this;
-	}
-	</SCRIPT>
-	<?php
-	$form = new HTML_QuickForm('select_form', 'GET', "?p=".$p."&search_type_service=" . $search_type_service."&search_type_host=" . $search_type_host);
-	$selLim = $form->addElement('select', 'l', _("Rows"), $select, array("onChange" => "setL(this.value);  this.form.submit()"));
-	$selLim->setSelected($limit);
+    if ($i > 1) {
+        $tpl->assign("pageArr", $pageArr);
+    }
 
-	/*
+    $tpl->assign("num", $num);
+    $tpl->assign("first", _("First page"));
+    $tpl->assign("previous", _("Previous page"));
+    $tpl->assign("next", _("Next page"));
+    $tpl->assign("last", _("Last page"));
+
+    if (($prev = $num - 1) >= 0) {
+        $tpl->assign('pagePrev', ("./main.php?p=".$p."&num=$prev&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
+    }
+
+    if (($next = $num + 1) < ($rows/$limit)) {
+        $tpl->assign('pageNext', ("./main.php?p=".$p."&num=$next&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
+    }
+
+    $pageNumber = ceil($rows / $limit);
+    if (($rows / $limit) > 0) {
+        $tpl->assign('pageNumber', ($num +1)."/".$pageNumber);
+    } else {
+        $tpl->assign('pageNumber', ($num)."/".$pageNumber);
+    }
+
+    if ($page_max > 5 && $num != 0) {
+        $tpl->assign('firstPage', ("./main.php?p=".$p."&num=0&limit=".$limit."&poller=".$poller."&template=$template&search=".$search."&type=".$type."&o=" . $o .$url_var));
+    }
+    if ($page_max > 5 && $num != ($pageNumber-1)) {
+        $tpl->assign('lastPage', ("./main.php?p=".$p."&num=".($pageNumber-1)."&limit=".$limit."&template=$template&poller=".$poller."&search=".$search."&type=".$type."&o=" . $o .$url_var));
+    }
+
+    /*
+ * Select field to change the number of row on the page
+ */
+    for ($i = 10; $i <= 100; $i = $i +10) {
+        $select[$i]=$i;
+    }
+    if (isset($gopt[$pagination]) && $gopt[$pagination]) {
+        $select[$gopt[$pagination]] = $gopt[$pagination];
+    }
+    if (isset($rows) && $rows) {
+        $select[$rows] = $rows;
+    }
+    ksort($select);
+} else {
+    for ($i = 10; $i <= 100; $i = $i +10) {
+        $select[$i]=$i;
+    }
+}
+
+    ?><script type="text/javascript">
+    function setL(_this){
+        var _l = document.getElementsByName('l');
+        document.forms['form'].elements['limit'].value = _this;
+        _l[0].value = _this;
+        _l[1].value = _this;
+    }
+    </SCRIPT>
+    <?php
+    $form = new HTML_QuickForm('select_form', 'GET', "?p=".$p."&search_type_service=" . $search_type_service."&search_type_host=" . $search_type_host);
+    $selLim = $form->addElement('select', 'l', _("Rows"), $select, array("onChange" => "setL(this.value);  this.form.submit()"));
+    $selLim->setSelected($limit);
+
+    /*
 	 * Element we need when we reload the page
 	 */
-	$form->addElement('hidden', 'p');
-	$form->addElement('hidden', 'search');
-	$form->addElement('hidden', 'num');
-	$form->addElement('hidden', 'order');
-	$form->addElement('hidden', 'type');
-	$form->addElement('hidden', 'sort_types');
-	$form->setDefaults(array("p" => $p, "search" => $search, "num"=>$num));
+    $form->addElement('hidden', 'p');
+    $form->addElement('hidden', 'search');
+    $form->addElement('hidden', 'num');
+    $form->addElement('hidden', 'order');
+    $form->addElement('hidden', 'type');
+    $form->addElement('hidden', 'sort_types');
+    $form->setDefaults(array("p" => $p, "search" => $search, "num"=>$num));
 
-	/*
+    /*
 	 * Init QuickForm
 	 */
-	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form->accept($renderer);
+    $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+    $form->accept($renderer);
 
-        if (isset($_GET['host_name'])) {
-            $host_name = $_GET['host_name'];
-        } elseif (!isset($host_name) || $host_name == "") {
-            $host_name = null;
-        }
-	isset($_GET["status"]) ? $status = $_GET["status"] : $status = NULL;
+    if (isset($_GET['host_name'])) {
+        $host_name = $_GET['host_name'];
+    } elseif (!isset($host_name) || $host_name == "") {
+        $host_name = null;
+    }
+    isset($_GET["status"]) ? $status = $_GET["status"] : $status = null;
 
-	$tpl->assign("host_name", $host_name);
-	$tpl->assign("status", $status);
-	$tpl->assign("limite", $limite);
-	$tpl->assign("begin", $num);
-	$tpl->assign("end", $limit);
-	$tpl->assign("pagin_page", _("Page"));
-	$tpl->assign("order", $_GET["order"]);
-	$tpl->assign("tab_order", $tab_order);
-	$tpl->assign('form', $renderer->toArray());
-	$tpl->display("pagination.ihtml");
+    $tpl->assign("host_name", $host_name);
+    $tpl->assign("status", $status);
+    $tpl->assign("limite", $limite);
+    $tpl->assign("begin", $num);
+    $tpl->assign("end", $limit);
+    $tpl->assign("pagin_page", _("Page"));
+    $tpl->assign("order", $_GET["order"]);
+    $tpl->assign("tab_order", $tab_order);
+    $tpl->assign('form', $renderer->toArray());
+    $tpl->display("pagination.ihtml");
 ?>
--- www/include/common/quickSearch.php
+++ www/include/common/quickSearch.php
@@ -35,119 +35,120 @@
  * SVN : $Id$
  *
  */
-	global $search;
+    global $search;
 
-	if (!isset($oreon)) {
-		exit();
-	}
+if (!isset($oreon)) {
+    exit();
+}
 
-	/*
+    /*
 	 * Init Flag
 	 */
-	$displayHSOptions = 0;
+    $displayHSOptions = 0;
 
-	$form_search = new HTML_QuickForm('quickSearchForm', 'POST', "?p=".$p."&o=".$o);
-	if (isset($_POST["search"])) {
-		$search = $_POST["search"];
-	} elseif (isset($_GET["search"])) {
-        $search = $_GET["search"];
-	} elseif (isset($oreon->historySearch[$url])) {
-		$search = $oreon->historySearch[$url];
-	} else {
-		$search = null;
-	}
+    $form_search = new HTML_QuickForm('quickSearchForm', 'POST', "?p=".$p."&o=".$o);
+if (isset($_POST["search"])) {
+    $search = $_POST["search"];
+} elseif (isset($_GET["search"])) {
+    $search = $_GET["search"];
+} elseif (isset($oreon->historySearch[$url])) {
+    $search = $oreon->historySearch[$url];
+} else {
+    $search = null;
+}
 
     $searchRaw = $search;
-	$search = mysql_real_escape_string($search);
+    $search = mysql_real_escape_string($search);
 
-	if (!isset($search_service)) {
-		$search_service = "";
-		$search_serviceRaw = "";
-	} else {
-		$search_serviceRaw = $search_service;
-	    $search_service = mysql_real_escape_string($search_service);
-	}
+if (!isset($search_service)) {
+    $search_service = "";
+    $search_serviceRaw = "";
+} else {
+    $search_serviceRaw = $search_service;
+    $search_service = mysql_real_escape_string($search_service);
+}
 
-	if (isset($search) && $search) {
-		if ($p == "4" || $p == "402" || $p == "203")
-			$attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_active_host", "style" => "padding-top:1px;padding-bottom:1px;");
-		else
-			$attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_active", "style" => "padding-top:1px;padding-bottom:1px;");
-	} else {
-		if ($p == "4" || $p == "402" || $p == "203")
-			$attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_host", "style" => "padding-top:1px;padding-bottom:1px;");
-		else
-			$attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input", "style" => "padding-top:1px;padding-bottom:1px;");
-	}
+if (isset($search) && $search) {
+    if ($p == "4" || $p == "402" || $p == "203") {
+        $attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_active_host", "style" => "padding-top:1px;padding-bottom:1px;");
+    } else {
+        $attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_active", "style" => "padding-top:1px;padding-bottom:1px;");
+    }
+} else {
+    if ($p == "4" || $p == "402" || $p == "203") {
+        $attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input_host", "style" => "padding-top:1px;padding-bottom:1px;");
+    } else {
+        $attrsText = array("size"=>"15", "id"=>"input_search", "class"=>"search_input", "style" => "padding-top:1px;padding-bottom:1px;");
+    }
+}
 
-	if (isset($search_service) && $search_service) {
-		$attrsText2 = array("size"=>"15", "id"=>"input_service", "class"=>"search_input_active_service", "style" => "padding-top:1px;padding-bottom:1px;", "title" => _("Service Description Search Key"));
-	} else {
-		$attrsText2 = array("size"=>"15", "id"=>"input_service", "class"=>"search_input_service", "style" => "padding-top:1px;padding-bottom:1px;", "title" => _("Service Description Search Key"));
-	}
-	$attrsText["title"] = _("Search");
-	$attrsSubmit = array("style"=>"display:none;");
+if (isset($search_service) && $search_service) {
+    $attrsText2 = array("size"=>"15", "id"=>"input_service", "class"=>"search_input_active_service", "style" => "padding-top:1px;padding-bottom:1px;", "title" => _("Service Description Search Key"));
+} else {
+    $attrsText2 = array("size"=>"15", "id"=>"input_service", "class"=>"search_input_service", "style" => "padding-top:1px;padding-bottom:1px;", "title" => _("Service Description Search Key"));
+}
+    $attrsText["title"] = _("Search");
+    $attrsSubmit = array("style"=>"display:none;");
 
-	if (!isset($limit)) {
-		$limit = 20;
-	}
+if (!isset($limit)) {
+    $limit = 20;
+}
 
-	$tab = array ("search"              => $searchRaw,
-				  "search_service"      => $search_serviceRaw,
-				  "p"                   => $p,
-				  "o"                   => $o,
-				  "limit"               => $limit,
-				  "search_type_host"    => 1,
-				  "search_type_service" => 1);
+    $tab = array ("search"              => $searchRaw,
+                  "search_service"      => $search_serviceRaw,
+                  "p"                   => $p,
+                  "o"                   => $o,
+                  "limit"               => $limit,
+                  "search_type_host"    => 1,
+                  "search_type_service" => 1);
 
-	$form_search->addElement('text', 'search', _("Quick Search"), $attrsText);
-	if (isset($FlagSearchService) && $FlagSearchService) {
-		$form_search->addElement('text', 'search_service', _("Quick Search"), $attrsText2);
-	}
-	$form_search->addElement('submit', 'submit', _("Go"), $attrsSubmit);
-	$form_search->addElement('hidden', 'p');
-	$form_search->addElement('hidden', 'limit');
-	$form_search->addElement('hidden', 'list');
-	//$form_search->addElement('hidden', 'o', $o);
+    $form_search->addElement('text', 'search', _("Quick Search"), $attrsText);
+    if (isset($FlagSearchService) && $FlagSearchService) {
+        $form_search->addElement('text', 'search_service', _("Quick Search"), $attrsText2);
+    }
+    $form_search->addElement('submit', 'submit', _("Go"), $attrsSubmit);
+    $form_search->addElement('hidden', 'p');
+    $form_search->addElement('hidden', 'limit');
+    $form_search->addElement('hidden', 'list');
+    //$form_search->addElement('hidden', 'o', $o);
 
-	/*
+    /*
 	 * Add specific options for search in commands
 	 */
-	if ($p == '608' || $p == '60801' || $p == '60802' || $p == '60803' || $p == '60106') {
-	    $form_search->addElement('hidden', 'type');
-	    $tab['type'] = 2;
-	    if (isset($type)) {
-	        $tab['type'] = $type;
-	    }
-	}
+    if ($p == '608' || $p == '60801' || $p == '60802' || $p == '60803' || $p == '60106') {
+        $form_search->addElement('hidden', 'type');
+        $tab['type'] = 2;
+        if (isset($type)) {
+            $tab['type'] = $type;
+        }
+    }
 
-	$tabQuickSearch = array(602 => 1, 60201 => 1, 2020201 => 1, 2020202 => 1, 2020203 => 1, 202 => 1,
-							2 => 1, 2020101 => 1, 20203 => 1, 2020301 => 1, 2020302 => 1, 2020303 => 1, 20208 => 1,
-							2020801 => 1, 2020802 => 1, 2020803 => 1, 20211 => 1,
-							2021101 => 1, 2021102 => 1, 2021103 => 1);
+    $tabQuickSearch = array(602 => 1, 60201 => 1, 2020201 => 1, 2020202 => 1, 2020203 => 1, 202 => 1,
+                            2 => 1, 2020101 => 1, 20203 => 1, 2020301 => 1, 2020302 => 1, 2020303 => 1, 20208 => 1,
+                            2020801 => 1, 2020802 => 1, 2020803 => 1, 20211 => 1,
+                            2021101 => 1, 2021102 => 1, 2021103 => 1);
 
 
-	if (isset($tabQuickSearch[$p])) {
-		$form_search->addElement('advcheckbox', 'search_type_host', 	_("host"), 	'', 'class=mini_checkbox');
-		$form_search->addElement('advcheckbox', 'search_type_service', 	_("service"), 	'', 'class=mini_checkbox');
-		$displayHSOptions = 1;
-	}
-	$form_search->setDefaults($tab);
+    if (isset($tabQuickSearch[$p])) {
+        $form_search->addElement('advcheckbox', 'search_type_host', _("host"), '', 'class=mini_checkbox');
+        $form_search->addElement('advcheckbox', 'search_type_service', _("service"), '', 'class=mini_checkbox');
+        $displayHSOptions = 1;
+    }
+    $form_search->setDefaults($tab);
 
-	/*
+    /*
 	 * Render with a smarty template
 	 */
-	$tpl = new Smarty();
-	$tpl = initSmartyTpl("./include/common/", $tpl);
+    $tpl = new Smarty();
+    $tpl = initSmartyTpl("./include/common/", $tpl);
 
-	$renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
-	$form_search->accept($renderer);
-	$tpl->assign('form_search', $renderer->toArray());
-	$tpl->assign('p', $p);
-	$tpl->assign("displayHSOptions", $displayHSOptions);
-	$tpl->assign("cleanSearch", _("Reset filters"));
-	if (isset($FlagSearchService) && $FlagSearchService) {
-		$tpl->assign("FlagSearchService", $FlagSearchService);
-	}
-	$tpl->display("quickSearch.ihtml");
-?>
+    $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
+    $form_search->accept($renderer);
+    $tpl->assign('form_search', $renderer->toArray());
+    $tpl->assign('p', $p);
+    $tpl->assign("displayHSOptions", $displayHSOptions);
+    $tpl->assign("cleanSearch", _("Reset filters"));
+    if (isset($FlagSearchService) && $FlagSearchService) {
+        $tpl->assign("FlagSearchService", $FlagSearchService);
+    }
+    $tpl->display("quickSearch.ihtml");
