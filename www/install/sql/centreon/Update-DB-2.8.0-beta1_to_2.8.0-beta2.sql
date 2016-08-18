78224a1590b021f384ee393b63ca87e4226ae3b1
Kevin Duret <kduret@centreon.com>
2016-08-18 14:46:16 +0200

    add backup menu in topology

--- www/install/sql/centreon/Update-DB-2.8.0-beta1_to_2.8.0-beta2.sql
+++ www/install/sql/centreon/Update-DB-2.8.0-beta1_to_2.8.0-beta2.sql
@@ -7,3 +7,14 @@ INNER JOIN cb_field cbf ON cbfr.cb_field_id=cbf.cb_field_id
 INNER JOIN cb_type cbt ON cbfr.cb_type_id=cbt.cb_type_id
 AND cbf.fieldname = 'failover'
 AND cbt.type_shortname = 'graphite';
+
+-- Inset Centreon Backup menu in topology
+INSERT INTO `topology` (
+`topology_id`, `topology_name`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`,
+`topology_url`, `topology_url_opt`, `topology_popup`, `topology_modules`, `topology_show`,
+`topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`
+) VALUES (
+NULL,'Backup',501,50165,90,1,
+'./include/Administration/backup/formBackup.php','&o=c','0','0','1',
+NULL,NULL,NULL,'1'
+);
