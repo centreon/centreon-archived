75b70eb5201416e32b1c915d5513c600ef0d753b
Rabaa Ridene <rridene@centreon.com>
2016-08-18 11:38:20 +0200

    #MON-803 : add collapse on broker config's form

--- www/include/common/javascript/centreon/brokerConfig.js
+++ www/include/common/javascript/centreon/brokerConfig.js
@@ -1,5 +1,6 @@
-jQuery(document).ready(function(){
+function centreonCollapse() {
     var tbody = jQuery(".collapse-wrapper");
+    var tab = jQuery(".tab");
 
     tbody.find(".list_lvl_1").addClass("elem-header");
     tbody.find(".list_one").addClass("elem-toCollapse");
@@ -9,24 +10,24 @@ jQuery(document).ready(function(){
     tbody.eq(0).find(".list_lvl_1").addClass("open");
     tbody.find(".list_lvl_1").slice(1).addClass("close");
 
-    tbody.each(function() {
-        var elem = jQuery(this).find('.list_lvl_1');
-        var nextElemChildren = elem.parent().siblings().find('.elem-toCollapse');
+        tbody.each(function() {
+            var elem = jQuery(this).find('.list_lvl_1');
+            var nextElemChildren = elem.parent().siblings().find('.elem-toCollapse');
 
-        elem.on('click', function() {
+            elem.on('click', function() {
 
-            var elemChildren = jQuery(this).siblings('.elem-toCollapse');
+                var elemChildren = jQuery(this).siblings('.elem-toCollapse');
 
-            if(elemChildren.is(':visible')) {
-                elemChildren.hide();
-                jQuery(this).removeClass('open').addClass('close');
-            }
-            else {
-                jQuery(this).addClass('open').removeClass('close');
-                tbody.eq(0).find(".list_lvl_1").removeClass("open").addClass('close');
-                nextElemChildren.hide();
-                elemChildren.show(); 
-            }
+                if(elemChildren.is(':visible')) {
+                    elemChildren.hide();
+                    jQuery(this).removeClass('open').addClass('close');
+                }
+                else {
+                    jQuery(this).addClass('open').removeClass('close');
+                    tbody.eq(0).find(".list_lvl_1").removeClass("open").addClass('close');
+                    nextElemChildren.hide();
+                    elemChildren.show();
+                }
+            });
         });
-    })
-});
+};
