2db65cbaa278eb7660974f6703ad90835b1509b6
Loic Laurent <loic.lau@gmail.com>
2016-08-19 14:38:25 +0200

    * fix jQuery plugin

--- www/include/common/javascript/centreon/popin.js
+++ www/include/common/javascript/centreon/popin.js
@@ -135,9 +135,8 @@
       if (typeof options === 'string') {
         methodReturn = data[options].apply(data, args);
       }
-      
-      return (methodReturn === undefined) ? $set : methodReturn;
     });
+    return (methodReturn === undefined) ? $set : methodReturn;
   };
   
   $.fn.centreonPopin.defaults = {
