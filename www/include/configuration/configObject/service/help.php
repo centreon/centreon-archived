2258fb1bc4b5c9fb58e623c473b17bb8640a95b6
Julien Mathis <jmathis@centreon.com>
2016-08-30 13:33:20 +0200

    improve notification configuration options in service form

--- www/include/configuration/configObject/service/help.php
+++ www/include/configuration/configObject/service/help.php
@@ -57,6 +57,7 @@ $help["passive_checks_enabled"] = dgettext("help", "Enable or disable passive ch
 
 $help["notifications_enabled"] = dgettext("help", "Specify whether or not notifications for this service are enabled.");
 $help["contact_additive_inheritance"] = dgettext("help", "When enabled, the contact definition will not override the definitions on template levels, it will be appended instead.");
+$help["cg_additive_inheritance"] = dgettext("help", "When enabled, the contactgroup definition will not override the definitions on template levels, it will be appended instead.");
 $help["contacts"] = dgettext("help", "This is a list of contacts that should be notified whenever there are problems (or recoveries) with this host. Useful if you want notifications to go to just a few people and don't want to configure contact groups. You must specify at least one contact or contact group in each host definition (or indirectly through its template).");
 $help["inherit_contacts_from_host"] = dgettext("help", "Specify whether or not the service will inherit host's contacts and contactgroups (if no contacts or contactgroups are defined for the service).");
 $help["use_only_contacts_from_host"] = dgettext("help", "To fill");
