4429a58b1b6e360ad0ca88e4616c2b41588b8275
Lionel Assepo <lassepo@centreon.com>
2016-09-12 16:36:08 +0200

    * #4680 : Don't display graph if more than 15 metrics.               Add button in graph pages to force display of graph which have more than 15 metrics

--- www/include/views/graphs/javascript/centreon-graph.js
+++ www/include/views/graphs/javascript/centreon-graph.js
@@ -10,6 +10,7 @@
     this.chartSvg = null;
     this.chartData = null;
     this.refreshEvent = null;
+    this.forceDisplay = false;
     parseInterval = settings.interval.match(/(\d+)([a-z]+)/i);
     this.interval = {
       number: parseInterval[1],
@@ -147,6 +148,7 @@
         }
       };
       var parsedData = this.buildMetricData(data);
+      
       axis = jQuery.extend({}, axis, parsedData.axis);
       if (axis.hasOwnProperty('y2')) {
         axis.y2.tick = {
@@ -154,12 +156,26 @@
         };
       }
       
+      if (data.data.length > 5) {
+          datasToAppend = {
+            x: parsedData.data.x,
+            columns: [],
+            names: {},
+            types: {},
+            colors: {},
+            regions: {},
+            empty: { label: { text: "Too much metrics, the chart can't be displayed" } }
+          }
+      } else {
+          datasToAppend = parsedData.data;
+      }
+      
       this.chart = c3.generate({
         bindto: '#' + this.$elem.attr('id'),
         size: {
           height: this.settings.height
         },
-        data: parsedData.data,
+        data: datasToAppend,
         axis: axis,
         tooltip: {
           format: {
@@ -167,7 +183,7 @@
               return moment(x).format('YYYY-MM-DD HH:mm:ss');
             },
             value: function (value, ratio, id) {
-              /* Test if the curse is inversed */
+              /* Test if the curve is inversed */
               if (self.isInversed(id)) {
                 return self.inverseRoundTick(value);
               }
@@ -181,6 +197,15 @@
         },
         regions: self.buildRegions(data)
       });
+      
+      if (data.data.length > 15) {
+          jQuery("#display-graph-" + self.id).css('display', 'block');
+          jQuery("#display-graph-" + self.id).on('click', function (e){
+              self.chart.load(parsedData.data)
+              self.chart.regions(self.buildRegions(data));
+              jQuery(this).css('display', 'none');
+          });
+      }
     },
     /**
      * Load data from rest api in ajax
@@ -212,10 +237,8 @@
               self.buildStatusData(data[0])
             );
           } else {
-            self.chart.load(
-              self.buildMetricData(data[0]).data
-            );
-            self.chart.regions(self.buildRegions(data[0]));
+              self.chart.load(self.buildMetricData(data[0]).data);
+              self.chart.regions(self.buildRegions(data[0]));
           }
         }
       });
@@ -237,8 +260,10 @@
         names: {},
         types: {},
         colors: {},
-        regions: {}
+        regions: {},
+        empty: { label: { text: "There's no data" } }
       };
+      
       var units = {};
       var axis = {};
       var column;
@@ -253,25 +278,26 @@
         return time * 1000;
       });
       times.unshift('times');
-      data.columns.push(times);
-      for (i = 0; i < dataRaw.data.length; i++) {
-        name = 'data' + (i + 1);
-        column = dataRaw.data[i].data;
-        column.unshift(name);
-        data.columns.push(column);
-        legend = dataRaw.data[i].label;
-        if (dataRaw.data[i].unit) {
-          legend += '(' + dataRaw.data[i].unit + ')';
-          if (units.hasOwnProperty(dataRaw.data[i].unit) === false) {
-            units[dataRaw.data[i].unit] = name;
-          }
-        }
-        data.names[name] = legend;
-        data.types[name] = convertType.hasOwnProperty(dataRaw.data[i].type) !== -1 ?
-          convertType[dataRaw.data[i].type] : dataRaw.data[i].type;
-        data.colors[name] = dataRaw.data[i].color;
-      }
       
+        data.columns.push(times);
+        for (i = 0; i < dataRaw.data.length; i++) {
+          name = 'data' + (i + 1);
+          column = dataRaw.data[i].data;
+          column.unshift(name);
+          data.columns.push(column);
+          legend = dataRaw.data[i].label;
+          if (dataRaw.data[i].unit) {
+            legend += '(' + dataRaw.data[i].unit + ')';
+            if (units.hasOwnProperty(dataRaw.data[i].unit) === false) {
+              units[dataRaw.data[i].unit] = name;
+            }
+          }
+          data.names[name] = legend;
+          data.types[name] = convertType.hasOwnProperty(dataRaw.data[i].type) !== -1 ?
+            convertType[dataRaw.data[i].type] : dataRaw.data[i].type;
+          data.colors[name] = dataRaw.data[i].color;
+        }
+
       if (Object.keys(units).length === 2) {
         axesName = 'y';
         for (unit in units) {
@@ -287,9 +313,9 @@
         }
         axis.y2.show = true;
       }
-      
+
       data.x = 'times';
-      
+
       /* Prepare threshold */
       if (this.settings.threshold && dataRaw.data.length === 1) {
         nbPoints = dataRaw.data[0].data.length;
@@ -318,7 +344,7 @@
           data.regions.warn = [{style: 'dashed'}];
         }
       }
-      
+
       /* Add group */
       data.groups = this.buildGroups(dataRaw);
       
