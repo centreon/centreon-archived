91a4db0734a7f26dffa8b170c9aee1400c8ad18a
Loic Laurent <loic.lau@gmail.com>
2016-09-22 15:22:49 +0200

    * init files

---
+++ features/DowntimeStartAndStopContext.feature
@@ -0,0 +1,7 @@
+Feature: Set service downtime
+  As a Centreon user
+  I want to be certain that the downtimes work correctly
+  To release quality products
+
+  Background:
+    Given I am logged in a Centreon server
---
+++ features/bootstrap/DowntimeStartAndStopContext.php
@@ -0,0 +1,17 @@
+<?php
+
+use Centreon\Test\Behat\CentreonContext;
+use Centreon\Test\Behat\DowntimeConfigurationPage;
+use Centreon\Test\Behat\DowntimeConfigurationListingPage;
+use Centreon\Test\Behat\MetaServiceConfigurationPage;
+use Centreon\Test\Behat\ServiceMonitoringDetailsPage;
+
+/**
+ * Defines application features from the specific context.
+ */
+class DowntimeStartAndStopContext extends CentreonContext
+{
+
+
+
+}
