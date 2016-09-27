e401d84aab2c77ea11fea62fee0ec01965a2900d
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-09-27 08:58:05 +0200

    Fix acceptance tests related to meta-services (they were improperly created).

--- features/bootstrap/MetaServicesApiContext.php
+++ features/bootstrap/MetaServicesApiContext.php
@@ -27,10 +27,11 @@ class MetaServicesApiContext extends CentreonContext
         $metaservicePage = new MetaServiceConfigurationPage($this);
         $metaservicePage->setProperties(array(
             'name' => $this->metaName,
-            'check_period' => 5,
+            'check_period' => '24x7',
             'max_check_attempts' => 1,
             'normal_check_interval' => 1,
-            'retry_check_interval' => 1));
+            'retry_check_interval' => 1
+        ));
         $metaservicePage->save();
         $this->restartAllPollers();
     }
