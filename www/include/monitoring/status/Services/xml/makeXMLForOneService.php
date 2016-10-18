2d224e24fe7ec5f0026651644934fffd1b7a203a
Kevin Duret <kduret@centreon.com>
2016-10-18 15:47:32 +0200

    fix perfdata in service details popup

--- www/include/monitoring/status/Services/xml/makeXMLForOneService.php
+++ www/include/monitoring/status/Services/xml/makeXMLForOneService.php
@@ -204,11 +204,11 @@ if ($data = $DBRESULT->fetchRow()) {
     }
 
     $tab_perf = preg_split("/\ /", $data["perfdata"]);
-    $perf_data = array();
+    $perf_data = array_slice($tab_perf,0,4);
     if(count($tab_perf) > 5) {
-    	$perf_data = array_slice($tab_perf,0,4);
-    	$perf_data[5] = "...";
+        $perf_data[5] = "...";
     }
+
     foreach ($perf_data as $val) {
         $obj->XML->startElement("performance_data");
         $obj->XML->writeElement("perf_data", CentreonUtils::escapeSecure($val));
