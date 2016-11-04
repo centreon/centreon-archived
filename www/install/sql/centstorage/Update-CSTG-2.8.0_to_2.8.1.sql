2ed117b4c5186f04d1b83f45d0d9506925802187
Kevin Duret <kduret@centreon.com>
2016-11-04 14:47:48 +0100

    Fix update of comments unique key

--- www/install/sql/centstorage/Update-CSTG-2.8.0_to_2.8.1.sql
+++
@@ -1,2 +0,0 @@
--- Update entry_time unique key in comments table
-ALTER TABLE `comments` DROP KEY `entry_time`, ADD UNIQUE KEY `entry_time` (`entry_time`,`host_id`,`service_id`, `instance_id`, `internal_id`);
