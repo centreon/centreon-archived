4e9c2b47242f405899ff2a2a211f0772dfebc23a
Matthieu Kermagoret <mkermagoret@centreon.com>
2016-11-29 18:16:51 +0100

    Working on Centreon Web 2.8.2.

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.1');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.2');
 
 --
 -- Contenu de la table `contact`
