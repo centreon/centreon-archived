c26e64882a04a516ed364ea178e452e18066916d
Jean-Baptiste <jb63@free.fr>
2016-11-30 10:24:30 +0100

    Oubli par rapport Pull request #4797
    Lien : https://github.com/centreon/centreon/pull/4797

--- www/install/sql/centreon/Update-DB-2.8.1_to_2.8.2.sql
+++ www/install/sql/centreon/Update-DB-2.8.1_to_2.8.2.sql
@@ -1,2 +1,4 @@
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.8.2' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.1' LIMIT 1;
+
+UPDATE `centreon`.`custom_view_user_relation` SET `deleted`='1' WHERE `is_owner`='1';
