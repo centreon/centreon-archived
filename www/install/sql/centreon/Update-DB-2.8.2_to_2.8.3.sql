aed9ba49bc5859a89a9d00e5b2427f731993cad7
Kevin Duret <kduret@centreon.com>
2017-01-09 15:03:27 +0100

    rename downtimes menu to recurrent donwtimes

--- www/install/sql/centreon/Update-DB-2.8.2_to_2.8.3.sql
+++ www/install/sql/centreon/Update-DB-2.8.2_to_2.8.3.sql
@@ -5,7 +5,10 @@ UPDATE `informations` SET `value` = '2.8.3' WHERE CONVERT( `informations`.`key` 
 DELETE FROM topology WHERE topology_page = 60216;
 UPDATE topology SET topology_page = 21003 WHERE topology_page = 60106;
 UPDATE topology SET topology_parent = 210 WHERE topology_page = 21003;
-UPDATE topology SET topology_url ='./include/monitoring/recurrentDowntime/downtime.php' WHERE topology_page = 21003;
+UPDATE topology SET topology_url = './include/monitoring/recurrentDowntime/downtime.php',
+topology_name = 'Recurrent downtimes',
+topology_order = 20
+WHERE topology_page = 21003;
 
 -- broker option
 DELETE FROM `options` WHERE `key` = 'broker';
