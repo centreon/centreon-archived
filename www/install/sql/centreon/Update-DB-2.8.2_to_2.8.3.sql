62d9778beaa0a659f509abf41ef931917112a325
Kevin Duret <kduret@centreon.com>
2017-01-09 11:51:21 +0100

    Remove relations between contact templates and contactgroups

--- www/install/sql/centreon/Update-DB-2.8.2_to_2.8.3.sql
+++ www/install/sql/centreon/Update-DB-2.8.2_to_2.8.3.sql
@@ -4,3 +4,9 @@ UPDATE `informations` SET `value` = '2.8.3' WHERE CONVERT( `informations`.`key` 
 -- broker option
 DELETE FROM `options` WHERE `key` = 'broker';
 INSERT INTO `options` (`key`, `value`) VALUES ('broker', 'broker');
+
+-- Remove relations between contact templates and contactgroups
+DELETE FROM contactgroup_contact_relation
+WHERE contact_contact_id IN (
+    SELECT contact_id FROM contact WHERE contact_register = '0'
+);
