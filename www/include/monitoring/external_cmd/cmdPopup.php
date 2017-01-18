9346b54414d46d991007b2dda588d6a28099d4c0
Julien Mathis <jmathis@centreon.com>
2017-01-18 16:08:31 +0100

    improve security on popup in the monitoring

--- www/include/monitoring/external_cmd/cmdPopup.php
+++ www/include/monitoring/external_cmd/cmdPopup.php
@@ -41,12 +41,13 @@ require_once _CENTREON_PATH_ . "www/class/centreonDB.class.php";
 require_once _CENTREON_PATH_ . "www/class/centreonGMT.class.php";
 
 session_start();
-if (!isset($obj->session_id) || !CentreonSession::checkSession($obj->session_id, $obj->DB)) {
-    exit;
-}
-$centreon = $_SESSION['centreon'];
 
 global $centreon, $pearDB;
+
+if (!CentreonSession::checkSession(session_id(), $pearDB)) {
+    exit();
+}
+$centreon = $_SESSION['centreon'];
 
 /*
  * Connect to DB
@@ -62,11 +63,6 @@ $centreonGMT->getMyGMTFromSession($sid, $pearDB);
 
 require_once _CENTREON_PATH_ . "www/include/common/common-Func.php";
 require_once _CENTREON_PATH_ . "www/include/monitoring/common-Func.php";
-
-if (!isset($centreon)) {
-    exit();
-}
-
 include_once _CENTREON_PATH_ . "www/include/monitoring/external_cmd/functionsPopup.php";
 
 if (isset($_GET["select"]) && isset($sid)) {
