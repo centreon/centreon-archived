19c45f98fb41ac557e304ad154dfa08c71978cbd
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-02-06 12:01:21 +0100

    Rework custom view scenarios and steps.

--- features/CustomViews.feature
+++ features/CustomViews.feature
@@ -6,37 +6,39 @@ Feature: Custom views
     Background:
         Given I am logged in a Centreon server with some widgets
 
-    Scenario: Create public share
-        Given a user sharing publicly a custom view
-        When another user wishes to add a new custom view
+    Scenario: Share public custom view
+        Given a publicly shared custom view
+        When a user wishes to add a new custom view
         Then he can add the shared view
         And he cannot modify the content of the shared view
 
     Scenario: Remove public share
-        Given a user sharing publicly a custom view
-        And another user is using this shared view
-        When this other user removes the shared view
+        Given a publicly shared custom view
+        And a user is using the shared view
+        When he removes the shared view
         Then the view is not visible anymore
         And the user can use it again
 
     Scenario: Share read-only custom view with users
-        Given a user sharing a view in read only to one or more other users
-        When the user want to add a new view
-        Then he can select this view shared with him without be able to modify his contents
+        Given a custom view shared in read only with a user
+        When the user wishes to add a new custom view
+        Then he can add the shared view
+        And he cannot modify the content of the shared view
 
     Scenario: Remove read-only custom view shared with users
-        Given a shared view in read only with a user
-        When I remove the view
+        Given a custom view shared in read only with a user
+        And the user is using the shared view
+        When he removes the shared view
         Then the view is not visible anymore
-        And I must be able to display later as long as this one is always shared with me
+        And the user can use it again
 
     Scenario: Update a read only custom view shared with users
-        Given a user sharing a view in read only to one or more other users
-        When the owner modifies this one
-        Then the impact is reflected on all the users displaying this one
+        Given a custom view shared in read only with a user
+        When the owner modifies the custom view
+        Then the changes are reflected on all users displaying the custom view
 
     Scenario: Delete a shared custom view
-        Given a user sharing a view in read only to one or more other users
-        And having been installed by users
-        When the owner remove the view
-        Then this view is removed for all the users using it
+        Given a custom view shared in read only with a user
+        And the user is using the shared view
+        When the owner removes the view
+        Then the view is removed for all users displaying the custom view
---
+++ features/bootstrap/CustomViewsContext.php
@@ -0,0 +1,137 @@
+<?php
+
+use Centreon\Test\Behat\CentreonContext;
+use Centreon\Test\Behat\CustomViewsPage;
+
+class CustomViewsContext extends CentreonContext
+{
+    private $customViewName;
+
+    /**
+     *  Build a new context.
+     */
+    public function __construct()
+    {
+        $this->customViewName = 'AcceptanceTestCustomView';
+    }
+
+    /**
+     *  @Given I am logged in a Centreon server with some widgets
+     */
+    public function iAmLoggedInCentreonWithWidgets()
+    {
+        /* $this->launchCentreonWebContainer('web_widgets'); */
+        /* $this->iAmLoggedIn(); */
+    }
+
+    /**
+     *  @Given a publicly shared custom view
+     */
+    public function aPubliclySharedCustomView()
+    {
+        /* $page = new CustomViewsPage($this); */
+        /* $page->createNewView($this->customViewName, 2, true); */
+        /* $page->addWidget('First widget', 'host-monitoring'); */
+        /* $page->addWidget('Second widget', 'service-monitoring'); */
+    }
+
+    /**
+     *  @Given a user is using the shared view
+     *  @Given the user is using the shared view
+     */
+    public function aUserIsUsingThisSharedView()
+    {
+        /* $page = new ContactConfigurationBranch($this); */
+    }
+
+    /**
+     *  @Given a custom view shared in read only with a user
+     */
+    public function aCustomViewSharedInReadOnlyWithAUser()
+    {
+        // XXX
+    }
+
+    /**
+     *  @When a user wishes to add a new custom view
+     *  @When the user wishes to add a new custom view
+     */
+    public function anotherUserWishesToAddANewCustomView()
+    {
+        // Nothing to do here, the view creation will be made
+        // with a single call, in the next step.
+    }
+
+    /**
+     *  @When he removes the shared view
+     */
+    public function thisOtherUserRemovesTheSharedView()
+    {
+        // XXX
+    }
+
+    /**
+     *  @When the owner modifies the custom view
+     */
+    public function theOwnerModifiesTheCustomView()
+    {
+        // XXX
+    }
+
+    /**
+     *  @When the owner removes the view
+     */
+    public function theOwnerRemovesTheView()
+    {
+        // XXX
+    }
+
+    /**
+     *  @Then he can add the shared view
+     */
+    public function heCanAddTheSharedView()
+    {
+        /* $page = new CustomViewsPage($this); */
+        /* $page->loadView($this->customViewName); */
+    }
+
+    /**
+     *  @Then he cannot modify the content of the shared view
+     */
+    public function heCannotModifyTheContentOfTheSharedView()
+    {
+        // XXX
+    }
+
+    /**
+     *  @Then the view is not visible anymore
+     */
+    public function theViewIsNotVisibleAnymore()
+    {
+        // XXX
+    }
+
+    /**
+     *  @Then the user can use it again
+     */
+    public function theUserCanUseItAgain()
+    {
+        // XXX
+    }
+
+    /**
+     *  @Then the changes are reflected on all users displaying the custom view
+     */
+    public function theChangesAreReflectedOnAllUsersDisplayingTheCustomView()
+    {
+        // XXX
+    }
+
+    /**
+     *  @Then the view is removed for all users displaying the custom view
+     */
+    public function theViewIsRemovedForAllUsersDisplayingTheCustomView()
+    {
+        // XXX
+    }
+}
