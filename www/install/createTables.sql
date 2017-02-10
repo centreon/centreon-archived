23d06ae11ac9ea1560428c19842e5bdd090a254b
Loic Laurent <loic.lau@gmail.com>
2017-02-10 10:13:19 +0100

    fix new column in install

--- www/install/createTables.sql
+++ www/install/createTables.sql
@@ -959,6 +959,8 @@ CREATE TABLE `custom_view_user_relation` (
   `usergroup_id` int(11) DEFAULT NULL,
   `locked` tinyint(6) DEFAULT '0',
   `is_owner` tinyint(6) DEFAULT '0',
+  `is_public` tinyint(6) DEFAULT '0',
+  `is_share` tinyint(6) DEFAULT '0',
   `is_consumed` int(1) NOT NULL DEFAULT 1,
   UNIQUE KEY `view_user_unique_index` (`custom_view_id`,`user_id`,`usergroup_id`),
   KEY `fk_custom_views_user_id` (`user_id`),
