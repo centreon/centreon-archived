3c42bcf6334eeae4cb80de0bb4050d4af5d6109d
Kevin Duret <kduret@centreon.com>
2017-02-21 14:50:13 +0100

    manage migration of custom views sharing

--- www/install/sql/centreon/Update-DB-2.8.4_to_2.8.5.sql
+++ www/install/sql/centreon/Update-DB-2.8.4_to_2.8.5.sql
@@ -14,6 +14,7 @@ INSERT INTO `widget_parameters_field_type` (`ft_typename`, `is_connector`) VALUE
 
 -- custom views share options
 ALTER TABLE custom_view_user_relation ADD is_share tinyint(1) NOT NULL DEFAULT 0 AFTER is_consumed;
+UPDATE custom_view_user_relation SET is_share = 1 WHERE is_owner = 0;
 
 -- Remove useless proxy option
 DELETE FROM options WHERE options.key = 'proxy_protocol';
