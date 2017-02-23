76e677d326c16a119cdf263d1fb68dad7ad633af
Lionel Assepo <lassepo@centreon.com>
2017-02-23 15:10:42 +0100

    * Fix error in acl_res_group_relations DELETE request

--- www/include/options/accessLists/groupsACL/DB-Func.php
+++ www/include/options/accessLists/groupsACL/DB-Func.php
@@ -384,9 +384,9 @@ function updateGroupResources($acl_group_id, $ret = array())
     }
 
     $query = 'DELETE '
-        . 'FROM acl_res_group_relations argr '
-        . 'JOIN acl_resources ar ON argr.acl_res_id = ar.acl_res_id '
-        . 'AND argr.acl_group_id = ' . $acl_group_id . ' '
+        . 'FROM acl_res_group_relations '
+        . 'JOIN acl_resources ar ON acl_res_group_relations.acl_res_id = ar.acl_res_id '
+        . 'AND acl_res_group_relations.acl_group_id = ' . $acl_group_id . ' '
         . 'AND ar.locked = 0 ';
     $pearDB->query($query);
     if (isset($_POST["resourceAccess"])) {
