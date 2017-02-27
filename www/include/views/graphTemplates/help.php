4e8aae94b282c758a5accb293a0bfbefa8d5f713
Maximilien Bersoult <mbersoult@centreon.com>
2017-02-27 16:15:54 +0100

    Explain usage of area color for curve template
    * Improve help text for area color
    * Fix english language
    
    Ref: #5033

--- www/include/views/graphTemplates/help.php
+++ www/include/views/graphTemplates/help.php
@@ -6,8 +6,8 @@ $help = array();
  */
 $help["tip_template_name"] = dgettext("help", "Name of graph template.");
 $help["tip_vertical_label"] = dgettext("help", "Vertical Label (Y-axis).");
-$help["tip_width"] = dgettext("help", "Width of grid. Use for export the chart.");
-$help["tip_height"] = dgettext("help", "Height of grid. Use for export the chart.");
+$help["tip_width"] = dgettext("help", "Width of grid. Used to export the chart.");
+$help["tip_height"] = dgettext("help", "Height of grid. Used to export the chart.");
 $help["tip_lower_limit"] = dgettext("help", "Lower limit of grid.");
 $help["tip_upper_limit"] = dgettext("help", "Upper limit of grid.");
 $help["tip_base"] = dgettext("help", "Base value.");
