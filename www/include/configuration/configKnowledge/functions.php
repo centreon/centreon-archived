42585e84de40bd4af8b7adba2fc34001e8ce25ac
Kevin Duret <kduret@centreon.com>
2017-03-30 17:11:16 +0200

    missing midiawiki config check

--- www/include/configuration/configKnowledge/functions.php
+++ www/include/configuration/configKnowledge/functions.php
@@ -29,7 +29,6 @@ function versionCentreon($pearDB)
 
 function Mediawikiconfigexist($url)
 {
-    return true;
     $file_headers = @get_headers($url);
     if ($file_headers[0] == 'HTTP/1.1 404 Not Found') {
         return false;
