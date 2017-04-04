87850f637a4d579efd0b52235e7f2928e5bb85b6
Kevin Duret <kduret@centreon.com>
2017-04-04 16:06:30 +0200

    improve centreon kb to work with spaces

--- cron/centKnowledgeSynchronizer.php
+++ cron/centKnowledgeSynchronizer.php
@@ -1,214 +1,38 @@
 <?php
-/**
+/*
+ * Copyright 2005-2017 Centreon
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give Centreon
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of Centreon choice, provided that
+ * Centreon also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
  *
  */
 
-// Getting wiki configuration
-require_once "/etc/centreon/centreon.conf.php";
-$centreon_path = "/usr/share/centreon/";
-$module_path = $centreon_path . "www/class/centreon-knowledge/";
-require_once $centreon_path . "www/class/centreonDB.class.php";
-require_once $module_path . "procedures.class.php";
-require_once $module_path . "procedures_DB_Connector.class.php";
+require_once realpath(dirname(__FILE__) . "/../www/class/centreon-knowledge/wikiApi.class.php");
 
-$modules_path = $centreon_path . "www/include/configuration/configKnowledge/";
-require_once $modules_path . 'functions.php';
-
-// Initiate connexion
-$dbConnector = new CentreonDB();
-$conf = getWikiConfig($dbConnector);
-$WikiURL = $conf['kb_wiki_url'];
-
-// Define cron constants
-define('_WIKIURL_', $WikiURL);
-
-// Last time the cron has been executed
-$startTimestamp = time() - (3600*24);
-define('_STARTDATE_', date('Y-m-d', $startTimestamp).'T00:00:00Z');
-
-/**
- *
- * @param string $type
- * @return array
- */
-function getChangedPages()
-{
-    // Connecting to Mediawiki API
-    $apiUrl = _WIKIURL_ . '/api.php?format=json&action=query&list=recentchanges' .
-        '&rclimit=50&rcprop=title&rctype=new|edit';
-
-    // Sending request
-    $result = json_decode(file_get_contents($apiUrl));
-    return $result->query->recentchanges;
-}
-
-/**
- *
- * @param array $pages
- * @return array
- */
-function detectCentreonObjects()
-{
-    $pages = getChangedPages();
-
-    $hosts = array();
-    $hostsTemplates = array();
-    $services = array();
-    $servicesTemplates = array();
-
-    $nbPages = count($pages);
-    for ($i=0; $i<$nbPages; $i++)
-    {
-        $objectFlag = explode(':', $pages[$i]->title);
-        switch($objectFlag[0])
-        {
-            case 'Host':
-                if (!in_array($pages[$i]->title, $hosts))
-                {
-                    $hosts[] = $pages[$i]->title;
-                }
-                break;
-
-            case 'Host-Template':
-                if (!in_array($pages[$i]->title, $hostsTemplates))
-                {
-                    $hostsTemplates[] = $pages[$i]->title;
-                }
-                break;
-
-            case 'Service':
-                if (!in_array($pages[$i]->title, $services))
-                {
-                    $services[] = $pages[$i]->title;
-                }
-                break;
-
-            case 'Service-Template':
-                if (!in_array($pages[$i]->title, $servicesTemplates))
-                {
-                    $servicesTemplates[] = $pages[$i]->title;
-                }
-                break;
-
-            default:
-                continue;
-                break;
-        }
-    }
-    $centreonObjects = array(
-        'hosts' => $hosts,
-        'hostsTemplates' => $hostsTemplates,
-        'services' => $services,
-        'servicesTemplates' => $servicesTemplates,
-    );
-    return $centreonObjects;
-}
-
-/**
- *
- * @param CentreonDB $dbConnector
- * @param array $listOfObjects
- */
-function synchronizeWithCentreon($dbConnector)
-{
-    // Get all pages title that where changed
-    $listOfObjects = detectCentreonObjects();
-
-    foreach($listOfObjects as $categorie=>$object)
-    {
-        switch($categorie)
-        {
-            case 'hosts':
-                foreach($object as $entity) {
-                    $objName = substr($entity, 5);
-                    editLinkForHost($dbConnector, str_replace(' ', '_', $objName));
-                }
-                break;
-
-            case 'hostsTemplates':
-                foreach($object as $entity) {
-                    $objName = substr($entity, 14);
-                    editLinkForHost($dbConnector, str_replace(' ', '_', $objName));
-                }
-                break;
-
-            case 'services':
-                foreach($object as $entity) {
-                    $objName = explode(' ', $entity);
-                    editLinkForService($dbConnector, $objName);
-                }
-                break;
-
-            case 'servicesTemplates':
-                foreach($object as $entity) {
-                    $objName = substr($entity, 17);
-                    editLinkForService($dbConnector, str_replace(' ', '_', $objName));
-                }
-                break;
-        }
-    }
-}
-
-/**
- *
- * @param CentreonDB $dbConnector
- * @param string $hostName
- */
-function editLinkForHost($dbConnector, $hostName)
-{
-    $querySelect = "SELECT host_id FROM host WHERE host_name='$hostName'";
-    $resHost = $dbConnector->query($querySelect);
-    $tuple = $resHost->fetchRow();
-
-    $valueToAdd = './include/configuration/configKnowledge/proxy/proxy.php?host_name=$HOSTNAME$';
-    $queryUpdate = "UPDATE extended_host_information "
-        ."SET ehi_notes_url = '$valueToAdd' "
-        ."WHERE host_host_id = '".$tuple['host_id']."'";
-    $dbConnector->query($queryUpdate);
-}
-
-/**
- *
- * @param CentreonDB $dbConnector
- * @param string $serviceName
- */
-function editLinkForService($dbConnector, $objName)
-{
-    if (is_array($objName))
-    {
-        $temphostName = explode(':', array_shift($objName));
-        $hostName = $temphostName[1];
-        $serviceName = join(' ', $objName);
-        $querySelect = "SELECT service_id "
-            ."FROM service, host, host_service_relation "
-            ."WHERE service.service_description = '$serviceName' "
-            ."AND host.host_name='$hostName' "
-            ."AND host_service_relation.host_host_id = host.host_id "
-            ."AND host_service_relation.service_service_id = service.service_id";
-    }
-    else
-    {
-        $querySelect = "SELECT service_id FROM service WHERE service_description='$objName'";
-    }
-
-    $resService = $dbConnector->query($querySelect);
-    $tuple = $resService->fetchRow();
-
-    $valueToAdd = './include/configuration/configKnowledge/proxy/proxy.php?host_name=$HOSTNAME$&service_description=$SERVICEDESC$';
-    $queryUpdate = "UPDATE extended_service_information "
-        ."SET esi_notes_url = '$valueToAdd' "
-        ."WHERE service_service_id = '".$tuple['service_id']."'";
-    $dbConnector->query($queryUpdate);
-}
-
-
-/**
- *************************
- ******     MAIN     *****
- *************************
- */
-
-// Synchro with Centreon
-synchronizeWithCentreon($dbConnector);
-
-?>
+(New WikiApi)->synchronize();
--- www/class/centreon-knowledge/procedures_Proxy.class.php
+++ www/class/centreon-knowledge/procedures_Proxy.class.php
@@ -13,24 +13,25 @@
 
 require_once _CENTREON_PATH_ . '/www/include/configuration/configKnowledge/functions.php';
 
-class procedures_Proxy  {
-	private $DB;
+class procedures_Proxy
+{
+    private $DB;
     private $hflag;
     private $sflag;
     private $proc;
     public $url;
     private $wikiUrl;
 
-	public function __construct($pearDB, $db_prefix, $host_name, $service_description = null)
+    public function __construct($pearDB, $db_prefix, $host_name, $service_description = null)
     {
-		$this->DB = $pearDB;
-		$this->hflag = 0;
-		$this->sflag = 0;
+        $this->DB = $pearDB;
+        $this->hflag = 0;
+        $this->sflag = 0;
 
-		$conf = getWikiConfig($this->DB);
-		$this->wikiUrl = $conf['kb_wiki_url'];
-		$this->proc = new procedures(
-		    3,
+        $conf = getWikiConfig($this->DB);
+        $this->wikiUrl = $conf['kb_wiki_url'];
+        $this->proc = new procedures(
+            3,
             $conf['kb_db_name'],
             $conf['kb_db_user'],
             $conf['kb_db_host'],
@@ -39,150 +40,150 @@ class procedures_Proxy  {
             $conf['kb_db_prefix']
         );
 
-		if (isset($host_name)) {
-			if (isset($service_description)) {
+        if (isset($host_name)) {
+            if (isset($service_description)) {
                 $this->returnServiceWikiUrl($this->DB->escape($host_name), $this->DB->escape($service_description));
             } else {
                 $this->returnHostWikiUrl($this->DB->escape($host_name));
             }
-		}
-	}
+        }
+    }
 
-	private function returnHostWikiUrl($host_name)
+    private function returnHostWikiUrl($host_name)
     {
-		$this->proc->setHostInformations();
+        $this->proc->setHostInformations();
 
-		$procList = $this->proc->getProcedures();
+        $procList = $this->proc->getProcedures();
 
-		/*
-		 * Check if host has a procedure directly on Host
-		 */
-		if (isset($procList["Host:" . $host_name])) {
-			$this->url = $this->wikiUrl."/index.php?title=Host:".$host_name;
-			return ;
-		}
+        /*
+         * Check if host has a procedure directly on Host
+         */
+        if (isset($procList["Host_:_" . $host_name])) {
+            $this->url = $this->wikiUrl . "/index.php?title=Host_:_" . $host_name;
+            return;
+        }
 
-		/*
-		 * Check if host can get a procedure on templates
-		 */
-		$templates = $this->getHostTemplateList($host_name);
-		foreach ($templates as $tpl) {
-			if (isset($procList["Host-Template:" . $tpl])) {
-				$this->url = $this->wikiUrl . "/index.php?title=Host-Template:".$tpl;
-				return;
-			}
-		}
-	}
+        /*
+         * Check if host can get a procedure on templates
+         */
+        $templates = $this->getHostTemplateList($host_name);
+        foreach ($templates as $tpl) {
+            if (isset($procList["Host-Template_:_" . $tpl])) {
+                $this->url = $this->wikiUrl . "/index.php?title=Host-Template_:_" . $tpl;
+                return;
+            }
+        }
+    }
 
-	private function returnServiceWikiUrl($host_name, $service_description)
+    private function returnServiceWikiUrl($host_name, $service_description)
     {
-		if ($this->hflag != 0) {
+        if ($this->hflag != 0) {
             $this->proc->setHostInformations();
         }
-		$this->proc->setServiceInformations();
-		$this->sflag;
+        $this->proc->setServiceInformations();
+        $this->sflag;
 
-		$procList = $this->proc->getProcedures();
+        $procList = $this->proc->getProcedures();
 
-		/*
-		 * Check Service
-		 */
+        /*
+         * Check Service
+         */
         $service_description = str_replace(' ', '_', $service_description);
-        if (isset($procList["Service:" . trim($host_name."_".$service_description)])) {
-            $this->url = $this->wikiUrl . "/index.php?title=Service:".$host_name."_".$service_description;
+        if (isset($procList["Service_:_" . trim($host_name . "_" . $service_description)])) {
+            $this->url = $this->wikiUrl . "/index.php?title=Service_:_" . $host_name . "_" . $service_description;
             return;
-		}
+        }
 
-		/*
-		 * Check service Template
-		 */
-		$host_id = $this->getMyHostID($host_name);
-		$templates = $this->getMyServiceTemplateModels($this->getMyServicesID($host_id, $service_description));
-		foreach ($templates as $key => $value) {
-			if (isset($procList["Service-Template:".trim($value)])) {
-				$this->url = $this->wikiUrl . "/index.php?title=Service-Template:".$value;
-				return ;
-			}
-		}
-		$this->returnHostWikiUrl($host_name);
-	}
+        /*
+         * Check service Template
+         */
+        $host_id = $this->getMyHostID($host_name);
+        $templates = $this->getMyServiceTemplateModels($this->getMyServicesID($host_id, $service_description));
+        foreach ($templates as $key => $value) {
+            if (isset($procList["Service-Template_:_" . trim($value)])) {
+                $this->url = $this->wikiUrl . "/index.php?title=Service-Template_:_" . $value;
+                return;
+            }
+        }
+        $this->returnHostWikiUrl($host_name);
+    }
 
-	function getMyHostID($host_name = NULL)
+    function getMyHostID($host_name = NULL)
     {
-		$DBRESULT =& $this->DB->query("SELECT host_id FROM host WHERE host_name = '".$host_name."' LIMIT 1");
-		$row =& $DBRESULT->fetchRow();
-		if ($row["host_id"])
-			return $row["host_id"];
-	}
+        $DBRESULT =& $this->DB->query("SELECT host_id FROM host WHERE host_name = '" . $host_name . "' LIMIT 1");
+        $row =& $DBRESULT->fetchRow();
+        if ($row["host_id"])
+            return $row["host_id"];
+    }
 
-	function getMyServicesID($host_id, $service_description)
+    function getMyServicesID($host_id, $service_description)
     {
-		/*
-		 * Get Services attached to hosts
-		 */
-		$query = "SELECT service_id, service_description " .
+        /*
+         * Get Services attached to hosts
+         */
+        $query = "SELECT service_id, service_description " .
             "FROM service, host_service_relation hsr " .
             "WHERE hsr.host_host_id = '" . $host_id . "' " .
             "AND hsr.service_service_id = service_id " .
             "AND service_description = '" . $service_description . "' ";
-		$DBRESULT =& $this->DB->query($query);
-		while ($elem =& $DBRESULT->fetchRow())	{
-			return $elem["service_id"];
-		}
-		$DBRESULT->free();
+        $DBRESULT =& $this->DB->query($query);
+        while ($elem =& $DBRESULT->fetchRow()) {
+            return $elem["service_id"];
+        }
+        $DBRESULT->free();
 
-		/*
-		 * Get Services attached to hostgroups
-		 */
-		$query = "SELECT service_id, service_description " .
+        /*
+         * Get Services attached to hostgroups
+         */
+        $query = "SELECT service_id, service_description " .
             "FROM hostgroup_relation hgr, service, host_service_relation hsr " .
             "WHERE hgr.host_host_id = '" . $host_id . "' AND hsr.hostgroup_hg_id = hgr.hostgroup_hg_id " .
             "AND service_id = hsr.service_service_id " .
             "AND service_description = '" . $service_description . "' ";
-		$DBRESULT =& $this->DB->query($query);
-		while ($elem =& $DBRESULT->fetchRow()){
-			return $elem["service_id"];
-		}
-		$DBRESULT->free();
-	}
+        $DBRESULT =& $this->DB->query($query);
+        while ($elem =& $DBRESULT->fetchRow()) {
+            return $elem["service_id"];
+        }
+        $DBRESULT->free();
+    }
 
 
-	private function getHostTemplateList($host_name)
+    private function getHostTemplateList($host_name)
     {
-		$templates = array();
+        $templates = array();
 
-		$DBRESULT =& $this->DB->query("SELECT host_tpl_id FROM `host_template_relation`, `host` WHERE host_host_id = host_id AND host_name = '".$host_name."' ORDER BY `order`");
-		while($row =& $DBRESULT->fetchRow())	{
-			$DBRESULT2 =& $this->DB->query("SELECT host_name FROM host WHERE host_id = '".$row['host_tpl_id']."' LIMIT 1");
-			$hTpl =& $DBRESULT2->fetchRow();
-			$templates[$row['host_tpl_id']] = html_entity_decode($hTpl["host_name"], ENT_QUOTES);
-		}
-		return $templates;
-	}
+        $DBRESULT =& $this->DB->query("SELECT host_tpl_id FROM `host_template_relation`, `host` WHERE host_host_id = host_id AND host_name = '" . $host_name . "' ORDER BY `order`");
+        while ($row =& $DBRESULT->fetchRow()) {
+            $DBRESULT2 =& $this->DB->query("SELECT host_name FROM host WHERE host_id = '" . $row['host_tpl_id'] . "' LIMIT 1");
+            $hTpl =& $DBRESULT2->fetchRow();
+            $templates[$row['host_tpl_id']] = html_entity_decode($hTpl["host_name"], ENT_QUOTES);
+        }
+        return $templates;
+    }
 
-	private function getMyServiceTemplateModels($service_id)
+    private function getMyServiceTemplateModels($service_id)
     {
-		$tplArr = array();
+        $tplArr = array();
 
-		$DBRESULT =& $this->DB->query("SELECT service_description, service_template_model_stm_id FROM service WHERE service_id = '".$service_id."' LIMIT 1");
-		$row =& $DBRESULT->fetchRow();
-		$DBRESULT->free();
-		$service_id = $row["service_template_model_stm_id"];
-		if ($row["service_description"])
-			$tplArr[$service_id] = $row["service_description"];
-		while (1) {
-			$DBRESULT =& $this->DB->query("SELECT service_description, service_template_model_stm_id FROM service WHERE service_id = '".$service_id."' LIMIT 1");
-			$row =& $DBRESULT->fetchRow();
-			$DBRESULT->free();
-			if ($row["service_description"])
-				$tplArr[$service_id] = $row["service_description"];
-			else
-				break;
-			if ($row["service_template_model_stm_id"])
-				$service_id = $row["service_template_model_stm_id"];
-			else
-				break;
-		}
-		return ($tplArr);
-	}
+        $DBRESULT =& $this->DB->query("SELECT service_description, service_template_model_stm_id FROM service WHERE service_id = '" . $service_id . "' LIMIT 1");
+        $row =& $DBRESULT->fetchRow();
+        $DBRESULT->free();
+        $service_id = $row["service_template_model_stm_id"];
+        if ($row["service_description"])
+            $tplArr[$service_id] = $row["service_description"];
+        while (1) {
+            $DBRESULT =& $this->DB->query("SELECT service_description, service_template_model_stm_id FROM service WHERE service_id = '" . $service_id . "' LIMIT 1");
+            $row =& $DBRESULT->fetchRow();
+            $DBRESULT->free();
+            if ($row["service_description"])
+                $tplArr[$service_id] = $row["service_description"];
+            else
+                break;
+            if ($row["service_template_model_stm_id"])
+                $service_id = $row["service_template_model_stm_id"];
+            else
+                break;
+        }
+        return ($tplArr);
+    }
 }
