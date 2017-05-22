862dd2edb8d5162b76459b080a564e066c70d50b
Laurent Pinsivy <lpinsivy@gmail.com>
2017-05-22 15:07:56 +0200

    Update to 2.7.11

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -3,7 +3,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.7.10');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.7.11');
 
 --
 -- Contenu de la table `contact`
