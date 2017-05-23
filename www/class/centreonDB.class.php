cc2fb4d34259de04beb49984465531acfa7afa80
loiclau <loic.lau@gmail.com>
2017-05-23 14:14:13 +0200

    * add option pdo error mode

--- www/class/centreonDB.class.php
+++ www/class/centreonDB.class.php
@@ -84,7 +84,8 @@ class CentreonDB extends \PDO
             $this->options = array(
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_STATEMENT_CLASS => array('CentreonDBStatement', array($this)),
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             );
 
             /*
