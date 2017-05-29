8e02ce8a911a2b97405ccb405ad94492e720ad95
Kevin Duret <kduret@centreon.com>
2017-05-29 16:22:57 +0200

    init version is 2.8.9

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.8');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.9');
 
 --
 -- Contenu de la table `contact`
