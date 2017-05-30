3116ca470998304fa8b6e26e652558246cacbe51
Kevin Duret <kduret@centreon.com>
2017-05-30 14:10:36 +0200

    add reserved macro $HOSTTIMEZONE$

--- www/install/sql/centreon/Update-DB-2.8.9_to_2.8.10.sql
+++ www/install/sql/centreon/Update-DB-2.8.9_to_2.8.10.sql
@@ -4,3 +4,4 @@ UPDATE `informations` SET `value` = '2.8.10' WHERE CONVERT( `informations`.`key`
 DELETE FROM nagios_macro WHERE macro_name IN ('$_HOSTLOCATION$', '$_HOSTHOST_ID$', '$_SERVICESERVICE_ID$');
 INSERT INTO `nagios_macro` (`macro_name`) VALUES ('$HOSTID$');
 INSERT INTO `nagios_macro` (`macro_name`) VALUES ('$SERVICEID$');
+INSERT INTO `nagios_macro` (`macro_name`) VALUES ('$HOSTTIMEZONE$');
