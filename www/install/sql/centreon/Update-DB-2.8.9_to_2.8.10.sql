778f7e0ed15ce9e98392d001b29dc7421e235af4
Kevin Duret <kduret@centreon.com>
2017-05-30 11:33:57 +0200

    remove reserved macro $_HOSTHOST_ID$ and $_SERVICESERVICE_ID$

--- www/install/sql/centreon/Update-DB-2.8.9_to_2.8.10.sql
+++ www/install/sql/centreon/Update-DB-2.8.9_to_2.8.10.sql
@@ -1,4 +1,6 @@
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.8.10' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.9' LIMIT 1;
 
-DELETE FROM nagios_macro WHERE macro_name = '$_HOSTLOCATION$';
+DELETE FROM nagios_macro WHERE macro_name IN ('$_HOSTLOCATION$', '$_HOSTHOST_ID$', '$_SERVICESERVICE_ID$');
+INSERT INTO `nagios_macro` (`macro_name`) VALUES ('$HOSTID$');
+INSERT INTO `nagios_macro` (`macro_name`) VALUES ('$SERVICEID$');
