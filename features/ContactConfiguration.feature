7d003c74f077e21a1d519bda7fe5c1875d8e551f
mariegallardo <mgallardo@centreon.com>
2017-06-26 10:58:59 +0200

    The 'add' were deleted

--- features/ContactConfiguration.feature
+++ features/ContactConfiguration.feature
@@ -7,22 +7,6 @@ Feature: ContactConfiguration
         Given I am logged in a Centreon server
         Given a contact is configured
 
-    Scenario: Edit the name of a contact
-        When I configure the name of a contact
-        And I configure the alias of a contact
-        And I configure the email of a contact
-        And I configure the access of a contact
-        And I make a contact be an admin
-        And I configure the status of a contact
-        And I configure the DN of a contact
-        And I configure the host_notif_period
-        And I configure the service_notif_period
-        Then the name has changed on the contact page
-        And the alias has changed on the contact page
-        And the email has changed on the contact page
-        And the access has changed on the contact page
-        And the contact is now an admin
-        And the status has changed on the contact page
-        And the DN has changed
-        And the host_notif_period has changed
-        And the service_notif_period has changed
+    Scenario: Edit a contact
+        When I update contact properties
+        Then the contact properties are updated
--- features/bootstrap/ContactConfigurationContext.php
+++ features/bootstrap/ContactConfigurationContext.php
@@ -14,25 +14,28 @@ class ContactConfigurationContext extends CentreonContext
     private $contactDN;
     private $contactHostNotifPeriod;
     private $contactServiceNotifPeriod;
-    private $changedName;
-    private $changedAlias;
-    private $changedAddress;
     private $currentPage;
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->contactName = 'contactName';
-        $this->contactPassword ='contactPassword';
-        $this->contactAlias = 'contactAlias';
-        $this->contactAddress = 'contact@localhost';
-        $this->contactDN = 'contactDN';
-        $this->contactHostNotifPeriod = 'workhours';
-        $this->contactServiceNotifPeriod = 'nonworkhours';
-        $this->changedName = 'changedName';
-        $this->changedAlias = 'changedAlias';
-        $this->changedAddress = 'contact@localhost';
-    }
+    private $initialProperties = (array(
+        'name' => 'contactName',
+        'alias' => 'contactAlias',
+        'email' => 'contact@localhost',
+        'password' => 'contactpwd',
+        'password2' => 'contactpwd',
+        'admin' => 0,
+        'dn' => 'contactDN',
+        'host_notification_period' => 'workhours',
+        'service_notification_period' => 'nonworkhours'
+    ));
+    private $updatedProperties = (array(
+        'name' => 'modifiedName',
+        'alias' => 'modifiedAlias',
+        'email' => 'modified@localhost',
+        'admin' => 1,
+        'dn' => 'modifiedDn',
+        'host_notification_period' => '4',
+        'service_notification_period' => '3' 
+    ));
 
     /**
      * @Given a contact is configured
@@ -40,277 +43,37 @@ class ContactConfigurationContext extends CentreonContext
     public function aContactIsConfigured()
     {
         $this->currentPage = new ContactConfigurationPage($this);
-        $this->currentPage->setProperties(array(
-            'name' => $this->contactName,
-            'alias' => $this->contactAlias,
-            'email' => $this->contactAddress,
-            'password' => $this->contactPassword,
-            'password2' => $this->contactPassword,
-            'admin' => 0
-        ));
+        $this->currentPage->setProperties($this->initialProperties);
         $this->currentPage->save();
     }
 
     /**
-     * @When I configure the name of a contact
+     * @When I update contact properties
      */
-    public function iConfigureTheNameOfAContact()
+    public function iUpdateContactProperties()
     {
         $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->contactAlias);
-        $this->currentPage->setProperties(array(
-            'name' => $this->changedName
-        ));
+        $this->currentPage = $this->currentPage->inspect($this->initialProperties['alias']);
+        $this->currentPage->setProperties($this->updatedProperties);
         $this->currentPage->save();
     }
 
     /**
-      * @When I configure the alias of a contact
-      */
-    public function iConfigureTheAliasOfAContact()
+     * @Then the contact properties are updated
+     */ 
+    public function theContactPropertiesAreUpdated()
     {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->contactAlias);
-        $this->currentPage->setProperties(array(
-            'alias' => $this->changedAlias
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the email of a contact
-     */
-    public function iConfigureTheAddressOfAContact()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'email' => $this->changedAddress
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the access of a contact
-     */
-    public function iConfigureTheAccessOfAContact()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'access' => 1
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I make a contact be an admin
-     */
-    public function iMakeAContactBeAnAdmin()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'admin' => 0
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the status of a contact
-     */
-    public function iConfigureTheStatusOfAContact()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'status' => 1
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the DN of a contact
-     */
-    public function iConfigureTheDNOfAContact()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'dn' => $this->contactDN
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the host_notif_period
-     */
-    public function iConfigureTheHost_notif_period()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'host_notification_period' => $this->contactHostNotifPeriod
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @When I configure the service_notif_period
-     */
-    public function iConfigureTheService_notif_period()
-    {
-        $this->currentPage = new ContactConfigurationListingPage($this);
-        $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-        $this->currentPage->setProperties(array(
-            'service_notification_period' => $this->contactServiceNotifPeriod
-        ));
-        $this->currentPage->save();
-    }
-
-    /**
-     * @Then the name has changed on the contact page
-     */
-    public function theNameHasChangedOnTheContactPage()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['name'] == $this->changedName;
-            },
-            "The contact has not changed.",
-            30
-        );
-    }
-
-    /**
-     * @Then the alias has changed on the contact page
-     */
-    public function theAliasHasChangedOnTheContactPage()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                return $this->currentPage->getEntry($this->changedAlias);
-            },
-            "The alias has not changed.",
-            30
-        );
-
-    }
-
-    /**
-     * @Then the email has changed on the contact page
-     */
-    public function theAddressHasChangedOnTheContactPage()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['email'] == $this->changedAddress;
-            },
-            "The address has not changed.",
-            30
-        );
-
-    }
-
-    /**
-     * @Then the access has changed on the contact page
-     */
-    public function theAccessHasChangedOnTheContactPage()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['access'] == 'Enabled';
-            },
-            "The access has not changed.",
-            30
-        );
-    }
-
-    /**
-     * @Then the contact is now an admin
-     */
-    public function theContactIsNowAnAdmin()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['admin'] == 'No';
-            },
-            "The contact is not an admin.",
-            30
-        );
-    }
-
-    /**
-     * @Then the status has changed on the contact page
-     */
-    public function theStatusHasChangedOnTheContactPage()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['status'] == 'ENABLED';
-            },
-            "The status has not changed.",
-            30
-        );
-    }
-
-    /**
-     * @Then the DN has changed
-     */
-    public function theDNHasChanged()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $this->currentPage = $this->currentPage->inspect($this->changedAlias);
-                $object = $this->currentPage->getProperties($this->changedAlias);
-                return $object['dn'] == $this->contactDN;
-            },
-            "The DN has not changed.",
-            30
-        );
-    }
-
-    /**
-     * @Then the host_notif_period has changed
-     */
-    public function theHost_notif_periodHasChanged()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['host_notification_period'] == $this->contactHostNotifPeriod . ' ()'; 
-            },
-            "The host_notification_period has not changed",
-            30
-        );
-    }
-
-    /**
-     * @Then the service_notif_period has changed
-     */
-    public function theService_notif_periodHasChanged()
-    {
-        $this->spin(
-            function($context){
-                $this->currentPage = new ContactConfigurationListingPage($this);
-                $object = $this->currentPage->getEntry($this->changedAlias);
-                return $object['service_notification_period'] == $this->contactServiceNotifPeriod . ' ()';
-            },
-            "The service_notification_period has not changed",
-            30
-        );
+	$this->currentPage = new ContactConfigurationListingPage($this);
+	$this->currentPage = $this->currentPage->inspect($this->updatedProperties['alias']);
+	$object = $this->currentPage->getProperties();
+	$tableau = array();
+        foreach($this->updatedProperties as $key => $value) {
+	    if ($value != $object[$key]) {
+		$tableau[] = $key;
+	    }
+	}
+	if (count($tableau) > 0) {
+	    throw new \Exception("Some properties are not being updated : " . implode(',', $tableau));
+	}
     }
 }
