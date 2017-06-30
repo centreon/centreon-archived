4a6b80c920aecbf373f45d6ac842ff47a932fee8
Guillaume28 <guillaume-baldo@hotmail.fr>
2017-06-30 13:53:06 +0200

    Suppression of service_inherit_contact_from_host

--- www/class/config-generate/abstract/service.class.php
+++ www/class/config-generate/abstract/service.class.php
@@ -70,7 +70,6 @@ abstract class AbstractService extends AbstractObject {
         service_notification_options as notification_options,
         service_notifications_enabled as notifications_enabled,
         contact_additive_inheritance,
-        service_use_only_contacts_from_host,
         cg_additive_inheritance,
         service_first_notification_delay as first_notification_delay,
         service_recovery_notification_delay as recovery_notification_delay,
@@ -160,10 +159,6 @@ abstract class AbstractService extends AbstractObject {
     }
     
     protected function getContacts(&$service) {
-        if (isset($service['service_use_only_contacts_from_host']) && $service['service_use_only_contacts_from_host'] == 1) {
-            $service['contacts_cache'] = array();
-            $service['contacts'] = "";
-        } else {
             $contact = Contact::getInstance();
             $service['contacts_cache'] = &$contact->getContactForService($service['service_id']);
             $contact_result = '';
@@ -182,15 +177,10 @@ abstract class AbstractService extends AbstractObject {
                     $service['contacts'] = '+' . $service['contacts'];
                 }
         }
-        }
     }
     
     protected function getContactGroups(&$service)
-    {        
-         if (isset($service['service_use_only_contacts_from_host']) && $service['service_use_only_contacts_from_host'] == 1) {
-            $service['contact_groups_cache'] = array();
-            $service['contact_groups'] = "";
-        } else {
+    {
             $cg = Contactgroup::getInstance();
             $service['contact_groups_cache'] = &$cg->getCgForService($service['service_id']);
             $cg_result = '';
@@ -208,7 +198,6 @@ abstract class AbstractService extends AbstractObject {
                 if (!is_null($service['cg_additive_inheritance']) && $service['cg_additive_inheritance'] == 1) {
                     $service['contact_groups'] = '+' . $service['contact_groups'];
                 }
-        }
         }
     }
     
--- www/include/monitoring/common-Func.php
+++ www/include/monitoring/common-Func.php
@@ -221,10 +221,10 @@ function get_notified_infos_for_service($service_id, $host_id)
 
     // Get Service Notifications options
     $additive = false;
-    $DBRESULT = $pearDB->query("SELECT contact_additive_inheritance, cg_additive_inheritance, service_inherit_contacts_from_host
+    $DBRESULT = $pearDB->query("SELECT contact_additive_inheritance, cg_additive_inheritance, service_use_only_contacts_from_host
             FROM service WHERE service_id = '" . CentreonDB::escape($service_id) . "'");
     $serviceParam = $DBRESULT->fetchRow();
-    $inherit_from_host = $serviceParam["service_inherit_contacts_from_host"];
+    $inherit_from_host = $serviceParam["service_use_only_contacts_from_host"];
 
     $serviceStack[] = array(
         "service_id" => $service_id,
