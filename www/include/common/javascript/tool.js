07dba0db2f7b682d63686e47bce0f1bdd2726512
loiclau <loic.lau@gmail.com>
2017-07-04 18:14:55 +0200

    * fix disabled row style

--- www/include/common/javascript/tool.js
+++ www/include/common/javascript/tool.js
@@ -188,12 +188,12 @@ function atoi(s)
 
 function setDisabledRowStyle(img)
 {
-    $( document ).ready(function() {
+    jQuery( document ).ready(function() {
 		if (!img) {
 			var img = "enabled.png";
 		}
-		$('img[src$="enabled.png"]').each(function(index) {
-			$(this).parent().parent().addClass('row_disabled');
+        jQuery('img[src$="enabled.png"]').each(function(index) {
+            jQuery(this).parent().parent().parent().className('row_disabled');
 		});
 	});
 }
