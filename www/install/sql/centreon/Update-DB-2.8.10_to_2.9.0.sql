f7fde1c57a461525d9842de1f7f11463e802815f
Kevin Duret <kduret@centreon.com>
2017-07-05 14:33:27 +0200

    drop foreign key before drop column

--- www/install/sql/centreon/Update-DB-2.8.10_to_2.9.0.sql
+++ www/install/sql/centreon/Update-DB-2.8.10_to_2.9.0.sql
@@ -1,6 +1,7 @@
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '2.9.0' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.10' LIMIT 1;
 
+ALTER TABLE `extended_host_information` DROP FOREIGN KEY `extended_host_information_ibfk_3`;
 ALTER TABLE `extended_host_information` DROP COLUMN `ehi_vrml_image`;
 
 DELETE FROM topology_JS WHERE PathName_js LIKE '%aculous%';
