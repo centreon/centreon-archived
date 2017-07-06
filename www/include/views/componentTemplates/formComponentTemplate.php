a402a93ae088624def8d0654db303a9bd56c0505
loiclau <loic.lau@gmail.com>
2017-07-06 17:14:44 +0200

    * fix style componentTemplate

--- www/include/views/componentTemplates/formComponentTemplate.php
+++ www/include/views/componentTemplates/formComponentTemplate.php
@@ -49,18 +49,20 @@ $DBRESULT->closeCursor();
 
 $compo = array();
 if (($o == "c" || $o == "w") && $compo_id) {
-    $res = $pearDB->query("SELECT * FROM giv_components_template WHERE compo_id = '".$compo_id."' LIMIT 1");
+    $res = $pearDB->query("SELECT * FROM giv_components_template WHERE compo_id = '" . $compo_id . "' LIMIT 1");
     /*
 	 * Set base value
 	 */
     $tab = $res->fetchRow();
     $compo = array_map("myDecode", $tab);
-            unset($compo['host_id']);
-            unset($compo['service_id']);
+    unset($compo['host_id']);
+    unset($compo['service_id']);
     $res->closeCursor();
 
     $hs_data = array();
-    $res = $pearDBO->query("SELECT id FROM index_data WHERE host_id = '".$compo["host_id"]."' AND service_id = '".$compo["service_id"]."' LIMIT 1");
+    $query = "SELECT id FROM index_data WHERE host_id = '" . $compo["host_id"] .
+        "' AND service_id = '" . $compo["service_id"] . "' LIMIT 1";
+    $res = $pearDBO->query($query);
     $tab = $res->fetchRow();
     if (is_array($tab)) {
         $hs_data = array_map("myDecode", $tab);
@@ -85,11 +87,12 @@ $res->closeCursor();
  * List of known data sources
  */
 $datasources = array();
-$DBRESULT = $pearDBO->query("SELECT `metric_name`, `unit_name` FROM `metrics` GROUP BY `metric_name` ORDER BY `metric_name`");
+$query = "SELECT `metric_name`, `unit_name` FROM `metrics` GROUP BY `metric_name` ORDER BY `metric_name`";
+$DBRESULT = $pearDBO->query($query);
 while ($row = $DBRESULT->fetchRow()) {
     $datasources[$row["metric_name"]] = $row["metric_name"];
     if (isset($row["unit_name"]) && $row["unit_name"] != "") {
-         $datasources[$row["metric_name"]] .= " (".$row["unit_name"].")";
+        $datasources[$row["metric_name"]] .= " (" . $row["unit_name"] . ")";
     }
 }
 unset($row);
@@ -98,14 +101,19 @@ $DBRESULT->closeCursor();
 /*
  * Define Styles
  */
-$attrsText = array("size"=>"40");
-$attrsText2 = array("size"=>"10");
-$attrsTextarea = array("rows"=>"4", "cols"=>"60");
-$eTemplate = '<table><tr><td><div class="ams">{label_2}</div>{unselected}</td><td align="center">{add}<br /><br /><br />{remove}</td><td><div class="ams">{label_3}</div>{selected}</td></tr></table>';
+$attrsText = array("size" => "40");
+$attrsText2 = array("size" => "10");
+$attrsTextarea = array("rows" => "4", "cols" => "60");
+$eTemplate = '<table><tr><td><div class="ams">{label_2}</div>{unselected}</td><td align="center">{add}<br />' .
+    '<br /><br />{remove}</td><td><div class="ams">{label_3}</div>{selected}</td></tr></table>';
+
+$availableRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_service&action=list';
+$defaultRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_objects' .
+    '&action=defaultValues&target=graphCurve&field=host_id&id=' . $compo_id;
 $attrServices = array(
     'datasourceOrigin' => 'ajax',
-    'availableDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_service&action=list',
-    'defaultDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_objects&action=defaultValues&target=graphCurve&field=host_id&id=' . $compo_id,
+    'availableDatasetRoute' => $availableRoute,
+    'defaultDatasetRoute' => $defaultRoute,
     'linkedObject' => 'centreonService',
     'multiple' => false
 );
@@ -113,7 +121,7 @@ $attrServices = array(
 /*
  * Form begin
  */
-$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
+$form = new HTML_QuickForm('Form', 'post', "?p=" . $p);
 if ($o == "a") {
     $form->addElement('header', 'ftitle', _("Add a Data Source Template"));
 } elseif ($o == "c") {
@@ -144,10 +152,10 @@ $form->addElement('text', 'ds_name', _("Data Source Name"), $attrsText);
 $form->addElement('select', 'datasources', null, $datasources);
 
 $l_dsColorList = array(
-    "ds_color_line" => array( "label" => _("Line color"), "color" => "#0000FF"),
-    "ds_color_area" => array( "label" => _("Area color"), "color" => "#FFFFFF"),
-    "ds_color_area_warn" => array( "label" => _("Warning Area color"), "color" => $l_general_opt["color_warning"]),
-    "ds_color_area_crit" => array( "label" => _("Critical Area color"), "color" => $l_general_opt["color_critical"])
+    "ds_color_line" => array("label" => _("Line color"), "color" => "#0000FF"),
+    "ds_color_area" => array("label" => _("Area color"), "color" => "#FFFFFF"),
+    "ds_color_area_warn" => array("label" => _("Warning Area color"), "color" => $l_general_opt["color_warning"]),
+    "ds_color_area_crit" => array("label" => _("Critical Area color"), "color" => $l_general_opt["color_critical"])
 );
 
 foreach ($l_dsColorList as $l_dsColor => $l_dCData) {
@@ -156,17 +164,28 @@ foreach ($l_dsColorList as $l_dsColor => $l_dCData) {
     } else {
         $l_hxColor = $l_dCData["color"];
     }
-    $attColText = array("value"=>$l_hxColor,"size"=>"7","maxlength"=>"7","style"=>"text-align: center; font-size: 11px; font-family: 'Courier New';");
-    $attColText = array("value"=>$l_hxColor,"size"=>"7","maxlength"=>"7","style"=>"text-align: center;");
+    $attColText = array(
+        "value" => $l_hxColor,
+        "size" => "7",
+        "maxlength" => "7",
+        "style" => "text-align: center; font-size: 11px; font-family: 'Courier New';"
+    );
+    $attColText = array("value" => $l_hxColor, "size" => "7", "maxlength" => "7", "style" => "text-align: center;");
     $form->addElement('text', $l_dsColor, $l_dCData["label"], $attColText);
 
-    $attColAreaR = array("style"=>"width:50px; height:15px; background-color: ".$l_hxColor."; border-width:0px; padding-bottom:2px;");
-    $attColAreaW = array("style"=>"width:50px; height:15px; background-color: ".$l_hxColor."; border-width:0px; padding-bottom:2px;");
-    $form->addElement('button', $l_dsColor.'_color', "", $attColAreaW);
-    $form->addElement('button', $l_dsColor.'_read', "", $attColAreaR);
+    $attColAreaR = array(
+        "style" => "width:50px; height:15px; background-color: " . $l_hxColor .
+            "; border-width:0px; padding-bottom:2px;"
+    );
+    $attColAreaW = array(
+        "style" => "width:50px; height:15px; background-color: " . $l_hxColor .
+            "; border-width:0px; padding-bottom:2px;"
+    );
+    $form->addElement('button', $l_dsColor . '_color', "", $attColAreaW);
+    $form->addElement('button', $l_dsColor . '_read', "", $attColAreaR);
 }
 
-$attTransext = array("size"=>"2","maxlength"=>"3","style"=>"text-align: center;");
+$attTransext = array("size" => "2", "maxlength" => "3", "style" => "text-align: center;");
 $form->addElement('text', 'ds_transparency', _("Transparency"), $attTransext);
 
 $form->addElement('checkbox', 'ds_filled', _("Filling"));
@@ -178,10 +197,15 @@ $form->addElement('checkbox', 'ds_last', _("Print Last Value"));
 $form->addElement('checkbox', 'ds_total', _("Print Total Value"));
 $form->addElement('checkbox', 'ds_invert', _("Invert"));
 $form->addElement('checkbox', 'default_tpl1', _("Default Centreon Graph Template"));
-$form->addElement('select', 'ds_tickness', _("Thickness"), array("1"=>"1", "2"=>"2", "3"=>"3"));
+$form->addElement('select', 'ds_tickness', _("Thickness"), array("1" => "1", "2" => "2", "3" => "3"));
 $form->addElement('text', 'ds_legend', _("Legend Name"), $attrsText);
 $form->addElement('checkbox', 'ds_hidecurve', _("Display Only The Legend"));
-$form->addElement('select', 'ds_jumpline', _("Empty Line After This Legend"), array("0"=>"0", "1"=>"1", "2"=>"2", "3"=>"3"));
+$form->addElement(
+    'select',
+    'ds_jumpline',
+    _("Empty Line After This Legend"),
+    array("0" => "0", "1" => "1", "2" => "2", "3" => "3")
+);
 $form->addElement('textarea', 'comment', _("Comments"), $attrsTextarea);
 
 /*
@@ -236,7 +260,7 @@ $form->addRule('ds_color_area', _("Bad Format: start color by #"), 'checkColorFo
 $form->addRule('ds_color_area_warn', _("Bad Format: start color by #"), 'checkColorFormat');
 $form->addRule('ds_color_area_crit', _("Bad Format: start color by #"), 'checkColorFormat');
 
-$form->setRequiredNote("<font style='color: red;'>*</font>&nbsp;". _("Required fields"));
+$form->setRequiredNote("<font style='color: red;'>*</font>&nbsp;" . _("Required fields"));
 
 /*
  * Smarty template Init
@@ -248,7 +272,12 @@ if ($o == "w") {
     /*
 	 * Just watch
 	 */
-    $form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&compo_id=".$compo_id."'"));
+    $form->addElement(
+        "button",
+        "change",
+        _("Modify"),
+        array("onClick" => "javascript:window.location.href='?p=" . $p . "&o=c&compo_id=" . $compo_id . "'")
+    );
     $form->setDefaults($compo);
     $form->freeze();
 } elseif ($o == "c") {
@@ -256,68 +285,84 @@ if ($o == "w") {
 	 * Modify
 	 */
     $subC = $form->addElement('submit', 'submitC', _("Save"), array("class" => "btc bt_success"));
-    $res = $form->addElement('reset', 'reset', _("Reset"), array("onClick"=>"javascript:resetLists(".$compo["host_id"].",".$compo["index_id"].")", "class" => "btc bt_default"));
+    $res = $form->addElement('reset', 'reset', _("Reset"), array(
+        "onClick" => "javascript:resetLists(" . $compo["host_id"] . "," . $compo["index_id"] . ")",
+        "class" => "btc bt_default"
+    ));
     $form->setDefaults($compo);
 } elseif ($o == "a") {
     /*
 	 * Add
 	 */
     $subA = $form->addElement('submit', 'submitA', _("Save"), array("class" => "btc bt_success"));
-    $res = $form->addElement('reset', 'reset', _("Reset"), array("onClick"=>"javascript:resetLists(0,0)", "class" => "btc bt_default"));
+    $res = $form->addElement(
+        'reset',
+        'reset',
+        _("Reset"),
+        array("onClick" => "javascript:resetLists(0,0)", "class" => "btc bt_default")
+    );
     $form->setDefaults(array(
-                "ds_color_area" => "#FFFFFF",
-                "ds_color_area_warn" => "#F8C706",
-                "ds_color_area_crit" => "#F91E05",
-                "ds_color_line" => "#0000FF",
-                "ds_color_line_mode" => '0',
-                "ds_transparency" => "80",
-                "ds_average" => true,
-                "ds_last" => true));
+        "ds_color_area" => "#FFFFFF",
+        "ds_color_area_warn" => "#F8C706",
+        "ds_color_area_crit" => "#F91E05",
+        "ds_color_line" => "#0000FF",
+        "ds_color_line_mode" => '0',
+        "ds_transparency" => "80",
+        "ds_average" => true,
+        "ds_last" => true
+    ));
 }
 if ($o == "c" || $o == "a") {
-?>
-<script type='text/javascript'>
-    function insertValueQuery() {
-        var e_input = document.Form.ds_name;
-        var e_select = document.getElementById('sl_list_metrics');
-        var sd_o = e_select.selectedIndex;
-        if ( sd_o != 0) {
-            var chaineAj = '';
-            chaineAj = e_select.options[sd_o].text;
-            chaineAj = chaineAj.replace(/\s(\[[CV]DEF\]|)\s*$/,"");
-            e_input.value = chaineAj;
+    ?>
+    <script type='text/javascript'>
+        function insertValueQuery() {
+            var e_input = document.Form.ds_name;
+            var e_select = document.getElementById('sl_list_metrics');
+            var sd_o = e_select.selectedIndex;
+            if (sd_o != 0) {
+                var chaineAj = '';
+                chaineAj = e_select.options[sd_o].text;
+                chaineAj = chaineAj.replace(/\s(\[[CV]DEF\]|)\s*$/, "");
+                e_input.value = chaineAj;
+            }
         }
-    }
 
-function popup_color_picker(t,name) {
-    var width = 318 ;
-    var height = 314;
-    var hcolor = '000000';
-    var i_elem = document.getElementsByName(t+'_color').item(0);
-    if ( i_elem != null ) {
-        var bckcolor = i_elem.style.backgroundColor;
-        var exp = new RegExp('rgb','g');
-        if (exp.test(bckcolor)) {
-            exp = new RegExp('[0-9]+','g');
-            var tab_rgb = bckcolor.match(exp);
-            hcolor = dechex(parseInt(tab_rgb[0]))+dechex(parseInt(tab_rgb[1]))+dechex(parseInt(tab_rgb[2]));
-        } else {
-            hcolor = bckcolor.substr(1,6);
+        function popup_color_picker(t, name) {
+            var width = 318;
+            var height = 314;
+            var hcolor = '000000';
+            var i_elem = document.getElementsByName(t + '_color').item(0);
+            if (i_elem != null) {
+                var bckcolor = i_elem.style.backgroundColor;
+                var exp = new RegExp('rgb', 'g');
+                if (exp.test(bckcolor)) {
+                    exp = new RegExp('[0-9]+', 'g');
+                    var tab_rgb = bckcolor.match(exp);
+                    hcolor = dechex(parseInt(tab_rgb[0])) + dechex(parseInt(tab_rgb[1])) + dechex(parseInt(tab_rgb[2]));
+                } else {
+                    hcolor = bckcolor.substr(1, 6);
+                }
+            }
+            Modalbox.show('./include/common/javascript/color_picker_mb.php?name=' + name, {
+                title: "<?php echo _('Pick a color') ?>",
+                width: width,
+                height: height,
+                afterLoad: function () {
+                    cp_init(t, hcolor);
+                }
+            });
         }
-    }
-    Modalbox.show('./include/common/javascript/color_picker_mb.php?name='+name, { title:  "<?php echo _('Pick a color') ?>", width: width, height: height , afterLoad: function(){cp_init(t, hcolor);} });
+    </script><?php
 }
-</script><?php
-}
-$tpl->assign('msg', array ("changeL"=>"?p=".$p."&o=c&compo_id=".$compo_id, "changeT"=>_("Modify")));
+$tpl->assign('msg', array("changeL" => "?p=" . $p . "&o=c&compo_id=" . $compo_id, "changeT" => _("Modify")));
 
 $tpl->assign("sort1", _("Properties"));
 $tpl->assign("sort2", _("Graphs"));
-    // prepare help texts
+// prepare help texts
 $helptext = "";
 include_once("help.php");
 foreach ($help as $key => $text) {
-    $helptext .= '<span style="display:none" id="help:'.$key.'">'.$text.'</span>'."\n";
+    $helptext .= '<span style="display:none" id="help:' . $key . '">' . $text . '</span>' . "\n";
 }
 $tpl->assign("helptext", $helptext);
 
@@ -330,7 +375,12 @@ if ($form->validate()) {
         updateComponentTemplateInDB($compoObj->getValue());
     }
     $o = "w";
-    $form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c&compo_id=".$compoObj->getValue()."'"));
+    $form->addElement(
+        "button",
+        "change",
+        _("Modify"),
+        array("onClick" => "javascript:window.location.href='?p=" . $p . "&o=c&compo_id=" . $compoObj->getValue() . "'")
+    );
     $form->freeze();
     $valid = true;
 }
@@ -353,16 +403,20 @@ $vdef = 0; /* don't list VDEF in metrics list */
 
 include_once("./include/views/graphs/common/makeJS_formMetricsList.php");
 if ($o == "c" || $o == "w") {
-    isset($_POST["host_id"]) && $_POST["host_id"] != null ? $host_service_id=$_POST["host_id"]: $host_service_id=$compo["host_id"];
+    isset($_POST["host_id"]) && $_POST["host_id"] != null
+        ? $host_service_id = $_POST["host_id"]
+        : $host_service_id = $compo["host_id"];
 } elseif ($o == "a") {
-    isset($_POST["host_id"]) && $_POST["host_id"] != null ? $host_service_id=$_POST["host_id"]: $host_service_id=0;
+    isset($_POST["host_id"]) && $_POST["host_id"] != null
+        ? $host_service_id = $_POST["host_id"]
+        : $host_service_id = 0;
 }
 ?>
 
 <script type="text/javascript">
     update_select_list('<?php echo $host_service_id;?>');
 
-    jQuery("#host_id").on('change', function() {
+    jQuery("#host_id").on('change', function () {
         update_select_list(this.value);
     });
 </script>
--- www/include/views/componentTemplates/listComponentTemplates.php
+++ www/include/views/componentTemplates/listComponentTemplates.php
@@ -43,10 +43,10 @@ $SearchTool = null;
 $search = '';
 if (isset($_POST['searchCurve']) && $_POST['searchCurve']) {
     $search = $_POST['searchCurve'];
-    $SearchTool = " WHERE name LIKE '%".$search."%'";
+    $SearchTool = " WHERE name LIKE '%" . $search . "%'";
 }
 
-$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM giv_components_template".$SearchTool);
+$DBRESULT = $pearDB->query("SELECT COUNT(*) FROM giv_components_template" . $SearchTool);
 
 $tmp = $DBRESULT->fetchRow();
 $rows = $tmp["COUNT(*)"];
@@ -79,10 +79,15 @@ if ($SearchTool != null) {
     $ClWh1 = "WHERE host_id IS NULL";
     $ClWh2 = "WHERE gct.host_id = h.host_id";
 }
-$rq = "( SELECT compo_id, NULL as host_name, host_id, service_id, name, ds_stack, ds_order, ds_name, ds_color_line, ds_color_area, ds_filled, ds_legend, default_tpl1, ds_tickness, ds_transparency FROM giv_components_template $SearchTool $ClWh1 ) UNION ( SELECT compo_id, host_name, gct.host_id, gct.service_id, name, ds_stack, ds_order, ds_name, ds_color_line, ds_color_area, ds_filled, ds_legend, default_tpl1, ds_tickness, ds_transparency FROM giv_components_template AS gct, host AS h $SearchTool $ClWh2 ) ORDER BY host_name, name LIMIT ".$num * $limit.", ".$limit;
-$DBRESULT = $pearDB->query($rq);
+$query = "( SELECT compo_id, NULL as host_name, host_id, service_id, name, ds_stack, ds_order, ds_name, " .
+    "ds_color_line, ds_color_area, ds_filled, ds_legend, default_tpl1, ds_tickness, ds_transparency " .
+    "FROM giv_components_template $SearchTool $ClWh1 ) UNION ( SELECT compo_id, host_name, gct.host_id, " .
+    "gct.service_id, name, ds_stack, ds_order, ds_name, ds_color_line, ds_color_area, ds_filled, ds_legend, " .
+    "default_tpl1, ds_tickness, ds_transparency FROM giv_components_template AS gct, host AS h $SearchTool $ClWh2 ) " .
+    "ORDER BY host_name, name LIMIT " . $num * $limit . ", " . $limit;
+$DBRESULT = $pearDB->query($query);
 
-$form = new HTML_QuickForm('select_form', 'POST', "?p=".$p);
+$form = new HTML_QuickForm('select_form', 'POST', "?p=" . $p);
 
 /*
  * Different style between each lines
@@ -95,30 +100,38 @@ $style = "one";
 $yesOrNo = array(null => _("No"), 0 => _("No"), 1 => _("Yes"));
 $elemArr = array();
 for ($i = 0; $compo = $DBRESULT->fetchRow(); $i++) {
-    $selectedElements = $form->addElement('checkbox', "select[".$compo['compo_id']."]");
-    $moptions = "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$compo['compo_id']."]'></input>";
-    $titles = $pearDB->query("SELECT h.host_name FROM giv_components_template AS gct, host AS h WHERE gct.host_id = '".$compo["host_id"]."' AND gct.host_id = h.host_id");
+    $selectedElements = $form->addElement('checkbox', "select[" . $compo['compo_id'] . "]");
+    $moptions = "&nbsp;<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) " .
+        "event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) " .
+        "return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[" .
+        $compo['compo_id'] . "]' />";
+
+    $query = "SELECT h.host_name FROM giv_components_template AS gct, host AS h WHERE gct.host_id = '" .
+        $compo["host_id"] . "' AND gct.host_id = h.host_id";
+    $titles = $pearDB->query($query);
     if ($titles->rowCount()) {
         $title = $titles->fetchRow();
     } else {
-        $title = array("host_name"=>"Global");
+        $title = array("host_name" => "Global");
     }
     $titles->closeCursor();
-    $elemArr[$i] = array("MenuClass"=>"list_".$style,
-                        "title"=>$title["host_name"],
-                        "RowMenu_select"=>$selectedElements->toHtml(),
-                        "RowMenu_name"=>$compo["name"],
-                        "RowMenu_link"=>"?p=".$p."&o=c&compo_id=".$compo['compo_id'],
-                        "RowMenu_desc"=>$compo["ds_name"],
-                        "RowMenu_legend"=>$compo["ds_legend"],
-                        "RowMenu_stack"=>$yesOrNo[$compo["ds_stack"]],
-                        "RowMenu_order"=>$compo["ds_order"],
-                        "RowMenu_transp"=>$compo["ds_transparency"],
-                        "RowMenu_clrLine"=>$compo["ds_color_line"],
-                        "RowMenu_clrArea"=>$compo["ds_color_area"],
-                        "RowMenu_fill"=>$yesOrNo[$compo["ds_filled"]],
-                        "RowMenu_tickness"=>$compo["ds_tickness"],
-                        "RowMenu_options"=>$moptions);
+    $elemArr[$i] = array(
+        "MenuClass" => "list_" . $style,
+        "title" => $title["host_name"],
+        "RowMenu_select" => $selectedElements->toHtml(),
+        "RowMenu_name" => $compo["name"],
+        "RowMenu_link" => "?p=" . $p . "&o=c&compo_id=" . $compo['compo_id'],
+        "RowMenu_desc" => $compo["ds_name"],
+        "RowMenu_legend" => $compo["ds_legend"],
+        "RowMenu_stack" => $yesOrNo[$compo["ds_stack"]],
+        "RowMenu_order" => $compo["ds_order"],
+        "RowMenu_transp" => $compo["ds_transparency"],
+        "RowMenu_clrLine" => $compo["ds_color_line"],
+        "RowMenu_clrArea" => $compo["ds_color_area"],
+        "RowMenu_fill" => $yesOrNo[$compo["ds_filled"]],
+        "RowMenu_tickness" => $compo["ds_tickness"],
+        "RowMenu_options" => $moptions
+    );
     $style != "two" ? $style = "two" : $style = "one";
 }
 $tpl->assign("elemArr", $elemArr);
@@ -126,39 +139,60 @@ $tpl->assign("elemArr", $elemArr);
 /*
  * Different messages we put in the template
  */
-$tpl->assign('msg', array ("addL"=>"?p=".$p."&o=a", "addT"=>_("Add"), "delConfirm"=>_("Do you confirm the deletion ?")));
+$tpl->assign(
+    'msg',
+    array("addL" => "?p=" . $p . "&o=a", "addT" => _("Add"), "delConfirm" => _("Do you confirm the deletion ?"))
+);
 
 /*
  * Toolbar select
  */
 ?>
-<script type="text/javascript">
-function setO(_i) {
-    document.forms['form'].elements['o'].value = _i;
-}
-</script>
+    <script type="text/javascript">
+        function setO(_i) {
+            document.forms['form'].elements['o'].value = _i;
+        }
+    </script>
 <?php
 $attrs1 = array(
-    'onchange'=>"javascript: " .
-                "if (this.form.elements['o1'].selectedIndex === 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
-                " 	setO(this.form.elements['o1'].value); submit();} " .
-                "else if (this.form.elements['o1'].selectedIndex === 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-                " 	setO(this.form.elements['o1'].value); submit();} ".
-                "this.form.elements['o1'].selectedIndex = 0;".
-                "");
-$form->addElement('select', 'o1', null, array(null=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete")), $attrs1);
+    'onchange' => "javascript: " .
+        "if (this.form.elements['o1'].selectedIndex === 1 && confirm('" .
+        _("Do you confirm the duplication ?") . "')) {" .
+        " 	setO(this.form.elements['o1'].value); submit();} " .
+        "else if (this.form.elements['o1'].selectedIndex === 2 && confirm('" .
+        _("Do you confirm the deletion ?") . "')) {" .
+        " 	setO(this.form.elements['o1'].value); submit();} " .
+        "this.form.elements['o1'].selectedIndex = 0;" .
+        ""
+);
+$form->addElement(
+    'select',
+    'o1',
+    null,
+    array(null => _("More actions..."), "m" => _("Duplicate"), "d" => _("Delete")),
+    $attrs1
+);
 $o1 = $form->getElement('o1');
 $o1->setValue(null);
 
 $attrs = array(
-    'onchange'=>"javascript: " .
-                "if (this.form.elements['o2'].selectedIndex === 1 && confirm('"._("Do you confirm the duplication ?")."')) {" .
-                " 	setO(this.form.elements['o2'].value); submit();} " .
-                "else if (this.form.elements['o2'].selectedIndex === 2 && confirm('"._("Do you confirm the deletion ?")."')) {" .
-                " 	setO(this.form.elements['o2'].value); submit();} " .
-                "this.form.elements['o2'].selectedIndex = 0;".
-                "");
-$form->addElement('select', 'o2', null, array(null=>_("More actions..."), "m"=>_("Duplicate"), "d"=>_("Delete")), $attrs);
+    'onchange' => "javascript: " .
+        "if (this.form.elements['o2'].selectedIndex === 1 && confirm('" .
+        _("Do you confirm the duplication ?") . "')) {" .
+        " 	setO(this.form.elements['o2'].value); submit();} " .
+        "else if (this.form.elements['o2'].selectedIndex === 2 && confirm('" .
+        _("Do you confirm the deletion ?") . "')) {" .
+        " 	setO(this.form.elements['o2'].value); submit();} " .
+        "this.form.elements['o2'].selectedIndex = 0;" .
+        ""
+);
+$form->addElement(
+    'select',
+    'o2',
+    null,
+    array(null => _("More actions..."), "m" => _("Duplicate"), "d" => _("Delete")),
+    $attrs
+);
 $o2 = $form->getElement('o2');
 $o2->setValue(null);
 
