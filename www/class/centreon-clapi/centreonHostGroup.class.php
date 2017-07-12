83e6c2c8ecadffd6c1e5deed64e8a8368c3658d3
Kev <duret.kevin@gmail.com>
2017-07-12 15:28:20 +0200

    Update centreonHostGroup.class.php

--- www/class/centreon-clapi/centreonHostGroup.class.php
+++ www/class/centreon-clapi/centreonHostGroup.class.php
@@ -237,9 +237,12 @@ class CentreonHostGroup extends CentreonObject
      */
     public function export($filter_id=null, $filter_name=null)
     {
-        $filters = null;
-        if (!is_null($filter_id)) {
-            $filters = array('hg_id' => $filter_id);
+        $filters = array();
+        if (!is_null($filter_id) && $filter_id !== 0) {
+            $filters['hg_id'] = $filter_id;
+        }
+        if (!is_null($filter_name)) {
+            $filters['hg_name'] = $filter_name;
         }
         parent::export($filters);
         $relObj = new Centreon_Object_Relation_Host_Group_Host();
@@ -252,7 +255,9 @@ class CentreonHostGroup extends CentreonObject
             -1,
             0,
             $hgFieldName,
-            null
+            'ASC',
+            $filters,
+            'AND'
         );
         foreach ($elements as $element) {
             $this->api->export_filter('HOST', $element['host_id'], $element[$hFieldName]);
