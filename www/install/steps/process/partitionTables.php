b84df70069fd9b655362d9197971aae84c706707
loiclau <loic.lau@gmail.com>
2017-07-13 10:14:10 +0200

    * add new argument in install partition config

--- www/install/steps/process/partitionTables.php
+++ www/install/steps/process/partitionTables.php
@@ -45,6 +45,7 @@ define('PROCESS_ID', 'dbpartitioning');
 
 /* Create partitioned tables */
 $database = new CentreonDB('centstorage', 3, false);
+$centreonDb = new CentreonDB('centreon');
 $partEngine = new PartEngine();
 
 if (!$partEngine->isCompatible($database)) {
@@ -60,7 +61,11 @@ $tables = array(
 
 try {
     foreach ($tables as $table) {
-        $config = new Config($database, _CENTREON_PATH_ . '/config/partition.d/partitioning-' . $table . '.xml');
+        $config = new Config(
+            $database,
+            _CENTREON_PATH_ . '/config/partition.d/partitioning-' . $table . '.xml',
+            $centreonDb
+        );
         $mysqlTable = $config->getTable($table);
         $partEngine->createParts($mysqlTable, $database);
     }
