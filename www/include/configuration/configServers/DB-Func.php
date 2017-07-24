10ce6c2b39175ed1d509329db2a940a971ac201b
Jean-Baptiste <jb63@free.fr>
2017-07-24 17:20:15 +0200

    Enhancement for deletion status and adding servicegroups and hostgroups types (#5433)

--- www/include/configuration/configServers/DB-Func.php
+++ www/include/configuration/configServers/DB-Func.php
@@ -470,11 +470,17 @@ function checkChangeState($poller_id, $last_restart)
     }
 
     $query = "SELECT * FROM log_action WHERE action_log_date > $last_restart " .
-        "AND ((object_type = 'host' AND (action_type = 'd' OR object_id IN (SELECT host_host_id FROM " .
+        "AND ((object_type = 'host' AND ((action_type = 'd' AND object_id IN (SELECT host_id FROM hosts)) OR object_id IN (SELECT host_host_id FROM " .
         $conf_centreon['db'] . ".ns_host_relation WHERE nagios_server_id = '$poller_id'))) " .
-        "OR (object_type = 'service' AND (action_type = 'd' OR object_id IN (SELECT service_service_id FROM " .
+        "OR (object_type = 'service' AND ((action_type = 'd' AND object_id IN (SELECT service_id FROM services)) OR object_id IN (SELECT service_service_id FROM " .
         $conf_centreon['db'] . ".ns_host_relation nhr, " . $conf_centreon['db'] . ".host_service_relation hsr " .
-        "WHERE nagios_server_id = '$poller_id' AND hsr.host_host_id = nhr.host_host_id))))";
+        "WHERE nagios_server_id = '$poller_id' AND hsr.host_host_id = nhr.host_host_id)))" .
+        "OR (object_type = 'servicegroup' AND ((action_type = 'd' AND object_id IN (SELECT DISTINCT servicegroup_id FROM services_servicegroups)) OR object_id IN (SELECT DISTINCT servicegroup_sg_id FROM " .
+        $conf_centreon['db'] . ".servicegroup_relation sgr, " . $conf_centreon['db'] . ".ns_host_relation nhr " .
+        "WHERE sgr.host_host_id = nhr.host_host_id AND nhr.nagios_server_id = '$poller_id')))" .
+        "OR (object_type = 'hostgroup' AND ((action_type = 'd' AND object_id IN (SELECT DISTINCT hostgroup_id FROM hosts_hostgroups)) OR object_id IN (SELECT DISTINCT hr.hostgroup_hg_id FROM " .
+        $conf_centreon['db'] . ".hostgroup_relation hr, " . $conf_centreon['db'] . ".ns_host_relation nhr " .
+        "WHERE hr.host_host_id = nhr.host_host_id AND nhr.nagios_server_id = '$poller_id'))))";
     $dbResult = $pearDBO->query($query);
     if ($dbResult->rowCount()) {
         return 1;
