43411b06cd0ae73fbe5263b5c11c0049afaba9d3
loiclau <loic.lau@gmail.com>
2017-07-25 17:28:32 +0200

    * fix style myAccount

--- www/include/Administration/myAccount/formMyAccount.php
+++ www/include/Administration/myAccount/formMyAccount.php
@@ -44,34 +44,35 @@ require_once 'HTML/QuickForm/Renderer/ArraySmarty.php';
 
 require_once "./include/common/common-Func.php";
 
-$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
-    
+$form = new HTML_QuickForm('Form', 'post', "?p=" . $p);
+
 /*
  * Path to the configuration dir
  */
 $path = "./include/Administration/myAccount/";
 
 // PHP Functions
-require_once $path."DB-Func.php";
+require_once $path . "DB-Func.php";
 
 /*
  * Database retrieve information for the User
  */
 $cct = array();
 if ($o == "c") {
-    $DBRESULT = $pearDB->query("SELECT contact_id, contact_name, contact_alias, contact_lang, contact_email, contact_pager, contact_js_effects, contact_autologin_key, default_page, contact_auth_type 
-                                        FROM contact 
-                                        WHERE contact_id = '".$centreon->user->get_id()."'");
+    $query = "SELECT contact_id, contact_name, contact_alias, contact_lang, contact_email, contact_pager, " .
+        "contact_js_effects, contact_autologin_key, default_page, contact_auth_type " .
+        "FROM contact WHERE contact_id = '" . $centreon->user->get_id() . "'";
+    $DBRESULT = $pearDB->query($query);
     // Set base value
     $cct = array_map("myDecode", $DBRESULT->fetchRow());
     $res = $pearDB->query("SELECT cp_key, cp_value 
                            FROM contact_param 
-                           WHERE cp_contact_id = '".$pearDB->escape($centreon->user->get_id())."'");
+                           WHERE cp_contact_id = '" . $pearDB->escape($centreon->user->get_id()) . "'");
     while ($row = $res->fetchRow()) {
         $cct[$row['cp_key']] = $row['cp_value'];
     }
 }
-    
+
 /*
  * Database retrieve information for differents elements list we need on the page
  *
@@ -79,9 +80,9 @@ if ($o == "c") {
  */
 $langs = array();
 $langs = getLangs();
-$attrsText = array("size"=>"35");
+$attrsText = array("size" => "35");
 
-$form = new HTML_QuickForm('Form', 'post', "?p=".$p);
+$form = new HTML_QuickForm('Form', 'post', "?p=" . $p);
 $form->addElement('header', 'title', _("Change my settings"));
 
 $form->addElement('header', 'information', _("General Information"));
@@ -94,40 +95,70 @@ if ($cct["contact_auth_type"] != 'ldap') {
 $form->addElement('text', 'contact_email', _("Email"), $attrsText);
 $form->addElement('text', 'contact_pager', _("Pager"), $attrsText);
 if ($cct["contact_auth_type"] != 'ldap') {
-    $form->addElement('password', 'contact_passwd', _("Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd1", "onFocus" => "resetPwdType(this);"));
-    $form->addElement('password', 'contact_passwd2', _("Confirm Password"), array("size"=>"30", "autocomplete"=>"off", "id"=>"passwd2", "onFocus" => "resetPwdType(this);"));
-    $form->addElement('button', 'contact_gen_passwd', _("Generate"), array('onclick'=>'generatePassword("passwd");', 'class' => 'btc bt_info'));
+    $form->addElement(
+        'password',
+        'contact_passwd',
+        _("Password"),
+        array("size" => "30", "autocomplete" => "off", "id" => "passwd1", "onFocus" => "resetPwdType(this);")
+    );
+    $form->addElement(
+        'password',
+        'contact_passwd2',
+        _("Confirm Password"),
+        array("size" => "30", "autocomplete" => "off", "id" => "passwd2", "onFocus" => "resetPwdType(this);")
+    );
+    $form->addElement(
+        'button',
+        'contact_gen_passwd',
+        _("Generate"),
+        array('onclick' => 'generatePassword("passwd");', 'class' => 'btc bt_info')
+    );
 }
 $form->addElement('text', 'contact_autologin_key', _("Autologin Key"), array("size" => "30", "id" => "aKey"));
-$form->addElement('button', 'contact_gen_akey', _("Generate"), array( 'onclick' => 'generatePassword("aKey");', 'class' => 'btc bt_info'));
+$form->addElement(
+    'button',
+    'contact_gen_akey',
+    _("Generate"),
+    array('onclick' => 'generatePassword("aKey");', 'class' => 'btc bt_info')
+);
 $form->addElement('select', 'contact_lang', _("Language"), $langs);
 $form->addElement('checkbox', 'contact_js_effects', _("Animation effects"), null, $attrsText);
 
 
 /* ------------------------ Topoogy ---------------------------- */
 $pages = array();
-$DBRESULT1 = $pearDB->query("SELECT topology_page, topology_name, topology_parent FROM topology WHERE topology_parent IS NULL AND topology_show = '1' ORDER BY topology_order, topology_group");
+$query = "SELECT topology_page, topology_name, topology_parent FROM topology " .
+    "WHERE topology_parent IS NULL AND topology_show = '1' ORDER BY topology_order, topology_group";
+$DBRESULT1 = $pearDB->query($query);
 while ($topo1 = $DBRESULT1->fetchRow()) {
     if ($centreon->user->access->page($topo1["topology_page"]) == 1) {
         $pages[$topo1["topology_page"]] = _($topo1["topology_name"]);
     }
-
-    $DBRESULT2 = $pearDB->query("SELECT topology_page, topology_name, topology_parent FROM topology WHERE topology_parent = '".$topo1["topology_page"]."' AND topology_show = '1' ORDER BY topology_order");
+    $query = "SELECT topology_page, topology_name, topology_parent FROM topology " .
+        "WHERE topology_parent = '" . $topo1["topology_page"] . "' AND topology_show = '1' ORDER BY topology_order";
+    $DBRESULT2 = $pearDB->query($query);
     while ($topo2 = $DBRESULT2->fetchRow()) {
         if ($centreon->user->access->page($topo2["topology_page"]) == 1) {
-            $pages[$topo2["topology_page"]] = "  "._($topo1["topology_name"]) . " > " . _($topo2["topology_name"]);
+            $pages[$topo2["topology_page"]] = "  " . _($topo1["topology_name"]) . " > " . _($topo2["topology_name"]);
         }
-
-        $DBRESULT3 = $pearDB->query("SELECT topology_name, topology_parent, topology_page FROM topology WHERE topology_parent = '".$topo2["topology_page"]."' AND topology_page IS NOT NULL AND topology_show = '1' ORDER BY topology_group, topology_order");
+        $query = "SELECT topology_name, topology_parent, topology_page FROM topology " .
+            "WHERE topology_parent = '" . $topo2["topology_page"] . "' " .
+            "AND topology_page IS NOT NULL AND topology_show = '1' ORDER BY topology_group, topology_order";
+        $DBRESULT3 = $pearDB->query($query);
         while ($topo3 = $DBRESULT3->fetchRow()) {
             if ($centreon->user->access->page($topo3["topology_page"]) == 1) {
-                $pages[$topo3["topology_page"]] = "  "._($topo1["topology_name"]) . " > " . _($topo2["topology_name"]) . " > " . _($topo3["topology_name"]);
+                $pages[$topo3["topology_page"]] = "  " . _($topo1["topology_name"]) . " > " .
+                    _($topo2["topology_name"]) . " > " . _($topo3["topology_name"]);
             }
-
-            $DBRESULT4 = $pearDB->query("SELECT topology_name, topology_parent FROM topology WHERE topology_parent = '".$topo3["topology_page"]."' AND topology_page IS NOT NULL AND topology_show = '1' ORDER BY topology_order");
+            $query = "SELECT topology_name, topology_parent FROM topology " .
+                "WHERE topology_parent = '" . $topo3["topology_page"] . "' " .
+                "AND topology_page IS NOT NULL AND topology_show = '1' ORDER BY topology_order";
+            $DBRESULT4 = $pearDB->query($query);
             while ($topo4 = $DBRESULT4->fetchRow()) {
                 if ($centreon->user->access->page($topo4["topology_page"]) == 1) {
-                    $pages[$topo4["topology_page"]] = "  "._($topo1["topology_name"]) . " > " . _($topo2["topology_name"]) . " > " . _($topo3["topology_name"]) . " > ". _($topo4["topology_name"]);
+                    $pages[$topo4["topology_page"]] = "  " . _($topo1["topology_name"]) . " > " .
+                        _($topo2["topology_name"]) . " > " . _($topo3["topology_name"]) . " > " .
+                        _($topo4["topology_name"]);
                 }
             }
         }
@@ -143,14 +174,14 @@ $form->addElement('checkbox', 'monitoring_svc_notification_1', _('Show Warning s
 $form->addElement('checkbox', 'monitoring_svc_notification_2', _('Show Critical status'));
 $form->addElement('checkbox', 'monitoring_svc_notification_3', _('Show Unknown status'));
 
-$sound_files = scandir(_CENTREON_PATH_."www/sounds/");
+$sound_files = scandir(_CENTREON_PATH_ . "www/sounds/");
 $sounds = array(null => null);
 foreach ($sound_files as $f) {
     if ($f == "." || $f == "..") {
         continue;
     }
     $info = pathinfo($f);
-    $fname = basename($f, ".".$info['extension']);
+    $fname = basename($f, "." . $info['extension']);
     $sounds[$fname] = $fname;
 }
 $form->addElement('select', 'monitoring_sound_host_notification_0', _("Sound for Up status"), $sounds);
@@ -160,11 +191,15 @@ $form->addElement('select', 'monitoring_sound_svc_notification_0', _("Sound for 
 $form->addElement('select', 'monitoring_sound_svc_notification_1', _("Sound for Warning status"), $sounds);
 $form->addElement('select', 'monitoring_sound_svc_notification_2', _("Sound for Critical status"), $sounds);
 $form->addElement('select', 'monitoring_sound_svc_notification_3', _("Sound for Unknown status"), $sounds);
-    
+
+
+$availableRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=list';
+$defaultRoute = './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone' .
+    '&action=defaultValues&target=contact&field=contact_location&id=' . $centreon->user->get_id();
 $attrTimezones = array(
     'datasourceOrigin' => 'ajax',
-    'availableDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=list',
-    'defaultDatasetRoute' => './include/common/webServices/rest/internal.php?object=centreon_configuration_timezone&action=defaultValues&target=contact&field=contact_location&id=' . $centreon->user->get_id(),
+    'availableDatasetRoute' => $availableRoute,
+    'defaultDatasetRoute' => $defaultRoute,
     'multiple' => false,
     'linkedObject' => 'centreonGMT'
 );
@@ -179,6 +214,7 @@ function myReplace()
     $ret = $form->getSubmitValues();
     return (str_replace(" ", "_", $ret["contact_name"]));
 }
+
 $form->applyFilter('__ALL__', 'myTrim');
 $form->applyFilter('contact_name', 'myReplace');
 $form->addRule('contact_name', _("Compulsory name"), 'required');
@@ -189,7 +225,7 @@ $form->registerRule('exist', 'callback', 'testExistence');
 $form->addRule('contact_name', _("Name already in use"), 'exist');
 $form->registerRule('existAlias', 'callback', 'testAliasExistence');
 $form->addRule('contact_alias', _("Name already in use"), 'existAlias');
-$form->setRequiredNote("<font style='color: red;'>*</font>"._("Required fields"));
+$form->setRequiredNote("<font style='color: red;'>*</font>" . _("Required fields"));
 
 // Smarty template Init
 $tpl = new Smarty();
@@ -208,7 +244,12 @@ if ($form->validate()) {
         $centreon->user->passwd = md5($form->getSubmitValue("contact_passwd"));
     }
     $o = null;
-    $form->addElement("button", "change", _("Modify"), array("onClick"=>"javascript:window.location.href='?p=".$p."&o=c'", 'class' => 'btc bt_info'));
+    $form->addElement(
+        "button",
+        "change",
+        _("Modify"),
+        array("onClick" => "javascript:window.location.href='?p=" . $p . "&o=c'", 'class' => 'btc bt_info')
+    );
     $form->freeze();
 }
 
@@ -224,14 +265,14 @@ $tpl->display("formMyAccount.ihtml");
 ?>
 <script type='text/javascript' src='./include/common/javascript/keygen.js'></script>
 <script type="text/javascript">
-jQuery(function() {
-    jQuery("select[name*='_notification_']").change(function() {
-        if (jQuery(this).val()) {
-                var snd = new buzz.sound("sounds/"+jQuery(this).val(), {
-                formats: [ "ogg", "mp3" ]
+    jQuery(function () {
+        jQuery("select[name*='_notification_']").change(function () {
+            if (jQuery(this).val()) {
+                var snd = new buzz.sound("sounds/" + jQuery(this).val(), {
+                    formats: ["ogg", "mp3"]
                 });
-        }
-        snd.play();
+            }
+            snd.play();
+        });
     });
-});
 </script>
