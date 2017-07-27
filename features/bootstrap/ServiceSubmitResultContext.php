d6e28d783ccfd436abe957ed9e5f6bfba6f2856c
MatthieuMan <mmandoula@centreon.com>
2017-07-27 16:04:02 +0200

    ServiceSubmitResult acceptance test initialized

---
+++ features/bootstrap/ServiceSubmitResultContext.php
@@ -0,0 +1,34 @@
+<?php
+
+use Centreon\Test\Behat\CentreonContext;
+
+class ServiceSubmitResultContext extends CentreonContext
+{
+    protected $page;
+
+
+    /**
+     * @Given one passive service has been configured using arguments status and output exists
+     */
+    public function onePassiveServiceHasBeenConfiguredUsingArgumentsStatusAndOutputExists()
+    {
+        
+    }
+
+    /**
+     * @When I submit some result to this service
+     */
+    public function iSubmitSomeResultToThisService()
+    {
+        
+    }
+
+    /**
+     * @Then the values are set as wanted in Monitoring > Status details page
+     */
+    public function theValuesAreSetAsWantedInMonitoringStatusDetailsPage()
+    {
+        
+    }
+
+}
