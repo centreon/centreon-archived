41c821d13b7858a2194d6a4569a038e0b6a13e51
mariegallardo <mgallardo@centreon.com>
2017-07-27 16:50:20 +0200

    New Features HostTemplateBasicsOperations

--- features/bootstrap/HostTemplateBasicsOperationsContext.php
+++ features/bootstrap/HostTemplateBasicsOperationsContext.php
@@ -93,7 +93,7 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         'notify_on_recovery' => 1,
         'notify_on_flapping' => 1,
         'notify_on_downtime_scheduled' => 1,
-        'notify_on_none' => 1,
+        'notify_on_none' => 0,
         'notification_interval' => 17,
         'notification_period' => 'none',
         'first_notification_delay' => 4,
@@ -159,13 +159,16 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         'notify_on_recovery' => 1,
         'notify_on_flapping' => 1,
         'notify_on_downtime_scheduled' => 1,
-        'notify_on_none' => 1,
+        'notify_on_none' => 0,
         'notification_interval' => 17,
         'notification_period' => 'none',
         'first_notification_delay' => 4,
         'recovery_notification_delay' => 3,
         'service_templates' => 'serviceTemplate1Description',
-        'parent_host_categories' => 'hostCategory2Name',
+        'parent_host_categories' => array(
+            'hostCategory1Name',
+            'hostCategory2Name'
+        ),
         'obsess_over_host' => 2,
         'acknowledgement_timeout' => 2,
         'check_freshness' => 0,
@@ -186,7 +189,7 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         'action_url' => 'hostTemplateChangeActionUrl',
         'icon' => '       centreon (png)',
         'alt_icon' => 'hostTemplateChangeIcon',
-        'status_map_image' => '       centreon (png)',
+        'status_map_image' => '',
         '2d_coords' => '15,84',
         '3d_coords' => '15,84,76',
         'severity_level' => 'hostCategory1Name (2)',
@@ -194,7 +197,7 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         'comments' => 'hostTemplateChangeComments'
     );
 
-    protected $updatedProperties = array(
+    protected $update = array(
         'name' => 'hostTemplateNameChanged',
         'alias' => 'hostTemplateAliasChanged',
         'address' => 'hostTemplate@localhostChanged',
@@ -206,9 +209,9 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         ),
         'check_command' => 'check_https',
         'command_arguments' => 'hostTemplateCommandArgumentChanged',
-        /*'macros' => array(
+        'macros' => array(
             'HOSTTEMPLATEMACRONAMECHANGED' => '11'
-        ),*/
+        ),
         'check_period' => 'nonworkhours',
         'max_check_attempts' => 34,
         'normal_check_interval' => 9,
@@ -232,6 +235,76 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
         'recovery_notification_delay' => 8,
         'service_templates' => 'serviceTemplate2Description',
         'parent_host_categories' => 'hostCategory1Name',
+        'obsess_over_host' => 1,
+        'acknowledgement_timeout' => 0,
+        'check_freshness' => 1,
+        'freshness_threshold' => 15,
+        'flap_detection_enabled' => 0,
+        'low_flap_threshold' => 25,
+        'high_flap_threshold' => 34,
+        'retain_status_information' => 1,
+        'retain_non_status_information' => 1,
+        'stalking_option_on_up' => 0,
+        'stalking_option_on_down' => 1,
+        'stalking_option_on_unreachable' => 0,
+        'event_handler_enabled' => 1,
+        'event_handler' => 'check_http',
+        'event_handler_arguments' => 'event_handler_argumentsChanged',
+        'url' => 'hostTemplateChangeUrlChanged',
+        'notes' => 'hostTemplateChangeNotesChanged',
+        'action_url' => 'hostTemplateChangeActionUrlChanged',
+        'icon' => '',
+        'alt_icon' => 'hostTemplateChangeIconChanged',
+        'status_map_image' => '       centreon (png)',
+        '2d_coords' => '48,29',
+        '3d_coords' => '09,25,27',
+        'severity_level' => 'hostCategory2Name (13)',
+        'enabled' => 1,
+        'comments' => 'hostTemplateChangeCommentsChanged'
+    );
+
+    protected $updatedProperties = array(
+        'name' => 'hostTemplateNameChanged',
+        'alias' => 'hostTemplateAliasChanged',
+        'address' => 'hostTemplate@localhostChanged',
+        'snmp_community' => 'snmpChanged',
+        'snmp_version' => '3',
+        'location' => 'Europe/Paris',
+        'templates' => array(
+            'hostTemplate2Name'
+        ),
+        'check_command' => 'check_https',
+        'command_arguments' => 'hostTemplateCommandArgumentChanged',
+        'macros' => array(
+            'HOSTTEMPLATEMACRONAME' => '22',
+            'HOSTTEMPLATEMACRONAMECHANGED' => '11'
+        ),
+        'check_period' => 'nonworkhours',
+        'max_check_attempts' => 34,
+        'normal_check_interval' => 9,
+        'retry_check_interval' => 4,
+        'active_checks_enabled' => 1,
+        'passive_checks_enabled' => 1,
+        'notifications_enabled' => 0,
+        'contact_additive_inheritance' => 0,
+        'contacts' => 'contactName',
+        'contact_group_additive_inheritance' => 1,
+        'contact_groups' => 'contactGroupName',
+        'notify_on_down' => 0,
+        'notify_on_unreachable' => 0,
+        'notify_on_recovery' => 0,
+        'notify_on_flapping' => 0,
+        'notify_on_downtime_scheduled' => 0,
+        'notify_on_none' => 0,
+        'notification_interval' => 49,
+        'notification_period' => 'workhours',
+        'first_notification_delay' => 7,
+        'recovery_notification_delay' => 8,
+        'service_templates' => 'serviceTemplate2Description',
+        'parent_host_categories' => array(
+            'hostCategory1Name',
+            'hostCategory2Name'
+        ),
         'obsess_over_host' => 1,
         'acknowledgement_timeout' => 0,
         'check_freshness' => 1,
@@ -298,7 +371,7 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
     {
         $this->currentPage = new HostTemplateConfigurationListingPage($this);
         $this->currentPage = $this->currentPage->inspect($this->initialProperties['name']);
-        $this->currentPage->setProperties($this->updatedProperties);
+        $this->currentPage->setProperties($this->update);
         $this->currentPage->save();
     }
 
@@ -316,10 +389,12 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
                     $object = $this->currentPage->getProperties();
                     foreach ($this->updatedProperties as $key => $value) {
                         if ($value != $object[$key]) {
-                            var_dump($key);
-                            var_dump($value);
-                            var_dump($object[$key]);
-                            $this->tableau[] = $key;
+                            if (is_array($value)) {
+                                $value = implode(' ', $value);
+                            }
+                            if ($value != $object[$key]) {
+                                $this->tableau[] = $key;
+                            }
                         }
                     }
                     return count($this->tableau) == 0;
@@ -359,10 +434,12 @@ class HostTemplateBasicsOperationsContext extends CentreonContext
                     $object = $this->currentPage->getProperties();
                     foreach ($this->duplicatedProperties as $key => $value) {
                         if ($value != $object[$key]) {
-                            var_dump($key);
-                            var_dump($value);
-                            var_dump($object[$key]);
-                            $this->tableau[] = $key;
+                            if (is_array($value)) {
+                                $value = implode(' ', $value);
+                            }
+                            if ($value != $object[$key]) {
+                                $this->tableau[] = $key;
+                            }
                         }
                     }
                     return count($this->tableau) == 0;
