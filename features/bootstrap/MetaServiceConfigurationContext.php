0acaa4477ab7fc22cad73b93e2906edb99d6dfd8
mariegallardo <mgallardo@centreon.com>
2017-08-03 15:52:52 +0200

    Feature enhanced

--- features/bootstrap/MetaServiceConfigurationContext.php
+++ features/bootstrap/MetaServiceConfigurationContext.php
@@ -53,7 +53,10 @@ class MetaServiceConfigurationContext extends CentreonContext
         'normal_check_interval' => 10,
         'retry_check_interval' => 20,
         'notification_enabled' => 2,
-        'contacts' => 'Guest',
+        'contacts' => array(
+            'Guest',
+            'User'
+        ),
         'contact_groups' => 'Supervisors',
         'notification_interval' => 12,
         'notification_period' => '24x7',
