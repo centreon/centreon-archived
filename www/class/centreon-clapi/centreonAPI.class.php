3ad4052f2af186c3e2d4ca0d594fa7dacf88b409
Kevin Duret <kduret@centreon.com>
2017-08-17 15:20:44 +0200

    fix rest api acceptance tests

--- www/class/centreon-clapi/centreonAPI.class.php
+++ www/class/centreon-clapi/centreonAPI.class.php
@@ -366,12 +366,10 @@ class CentreonAPI
      */
     protected function requireLibs($object)
     {
-
-
         if ($object != "") {
             if (isset($this->relationObject[$object]['class'])
                 && isset($this->relationObject[$object]['module'])
-                && !class_exists("Centreon" . $this->relationObject[$object])) {
+                && !class_exists("Centreon" . $this->relationObject[$object]['class'])) {
                 if ($this->relationObject[$object]['module'] == 'core') {
                     require_once "centreon" . $this->relationObject[$object]['class'] . ".class.php";
                 } else {
