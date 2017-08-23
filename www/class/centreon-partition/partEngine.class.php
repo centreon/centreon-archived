812478c9a18ce7bb310626ef285c8a55de5e31ce
cgagnaire <cgagnaire@centreon.com>
2017-08-23 15:12:09 +0200

    logging enhancement

--- www/class/centreon-partition/partEngine.class.php
+++ www/class/centreon-partition/partEngine.class.php
@@ -105,7 +105,7 @@ class PartEngine
             $day = "0" . $day;
         }
             
-        print "[".date(DATE_RFC822)."][updateParts] Create new part : " . ($ntime[5] + 1900) . $month . $day
+        print "[".date(DATE_RFC822)."][updateParts] Create new part for table " . $tableName . " : " . ($ntime[5] + 1900) . $month . $day
             . " - Range: $current_time\n";
 
         $partitionQuery = "PARTITION `p" . ($ntime[5] + 1900) . $month . $day
