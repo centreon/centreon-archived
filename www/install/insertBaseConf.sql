68536ca6f968c18c6497d9e91e4220a9a21d141e
Matthieu Kermagoret <mkermagoret@centreon.com>
2017-09-12 15:08:31 +0200

    chore(install): update version to 2.8.14 in install scripts.

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.13');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.14');
 
 --
 -- Contenu de la table `contact`
