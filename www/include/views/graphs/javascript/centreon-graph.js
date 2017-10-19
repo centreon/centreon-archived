4b75c8170f8173da16f258819f05cda9064174d2
Jean-Baptiste <jb63@free.fr>
2017-10-19 08:41:43 +0200

    fix(chart): fix extra legend on period change

--- www/include/views/graphs/javascript/centreon-graph.js
+++ www/include/views/graphs/javascript/centreon-graph.js
@@ -30,10 +30,6 @@
       });
     }
 
-    /* Prepare extra legends */
-    this.legendDiv = jQuery('<div>').addClass('chart-legends');
-    this.$elem.after(this.legendDiv);
-
     this.timeFormat = this.getTimeFormat();
 
     /* Color for status graph */
@@ -50,6 +46,10 @@
     ]);
 
     this.loadGraphId();
+
+    /* Prepare extra legends */
+    this.legendDiv = jQuery('<div>').addClass('chart-legends').attr('id', 'chart-legends-' + this.id);
+    this.$elem.after(this.legendDiv);
 
     /* Get start time and end time */
     times = this.getTimes();
@@ -275,7 +275,7 @@
           } else {
               self.chart.load(self.buildMetricData(data[0]).data);
               self.chart.regions(self.buildRegions(data[0]));
-              self.buildExtraLegend(data[0].legends);
+              self.buildExtraLegend(data[0].legends, data[0].service_id);
           }
         }
       });
@@ -808,12 +808,13 @@
      * Build for display the extra legends
      *
      * @param {String[]} legends - The list of legends to display
+     * @param {String} service_id - The host id and service id
      */
-    buildExtraLegend: function (legends) {
+    buildExtraLegend: function (legends, service_id) {
       var self = this;
       var i;
 
-      jQuery('.chart-legend').each(function (idx, el) {
+      jQuery('#chart-legends-' + service_id + ' .chart-legend').each(function (idx, el) {
         var legendName = jQuery(el).data('legend');
         if (!self.ids.hasOwnProperty(legendName)) {
           return true;
