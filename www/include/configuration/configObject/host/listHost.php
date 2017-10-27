b75fb46b8b8b3e2aa1f436024263b3253c0b2bd2
Laurent Pinsivy <lpinsivy@gmail.com>
2017-10-27 11:43:37 +0200

    Host search not saved when activate/desactivate a host

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -64,7 +64,9 @@ $DBRESULT->free();
 
 if (isset($_POST["searchH"])) {
     $search = $_POST["searchH"];
-    $_POST["search"] = $_POST["searchH"];
+    $centreon->historySearch[$url] = $search;
+} elseif (isset($_GET["searchH"])) {
+    $search = $_GET["searchH"];
     $centreon->historySearch[$url] = $search;
 } elseif (isset($centreon->historySearch[$url])) {
     $search = $centreon->historySearch[$url];
@@ -269,9 +271,9 @@ for ($i = 0; $host = $DBRESULT->fetchRow(); $i++) {
         $selectedElements = $form->addElement('checkbox', "select[".$host['host_id']."]");
 
         if ($host["host_activate"]) {
-            $moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/disabled.png' class='ico-14 margin_right' border='0' alt='"._("Disabled")."'></a>";
+            $moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=u&limit=".$limit."&num=".$num."&searchH=".$search."'><img src='img/icons/disabled.png' class='ico-14 margin_right' border='0' alt='"._("Disabled")."'></a>";
         } else {
-            $moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&search=".$search."'><img src='img/icons/enabled.png' class='ico-14 margin_right' border='0' alt='"._("Enabled")."'></a>";
+            $moptions = "<a href='main.php?p=".$p."&host_id=".$host['host_id']."&o=s&limit=".$limit."&num=".$num."&searchH=".$search."'><img src='img/icons/enabled.png' class='ico-14 margin_right' border='0' alt='"._("Enabled")."'></a>";
         }
 
         $moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57)) event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\" name='dupNbr[".$host['host_id']."]'></input>";
