81eb173682f049ed7759790e55c1b94b627a4dab
loiclau <loic.lau@gmail.com>
2017-12-01 10:56:09 +0100

    fix core

--- www/include/core/footer/footer.php
+++ www/include/core/footer/footer.php
@@ -40,121 +40,143 @@ if (!isset($centreon)) {
 require_once("./class/centreonData.class.php");
 
 if (!$min) {
-?>    
-<!-- Footer -->
-<div class="fullscreen-icon" title="Fullscreen" onclick="myToggleAll(0,true);" >
-    <img class="ico-16" id="fullscreenIcon" alt="FullScreen" src="./img/icons/fullscreen.png">
-</div> 
-<div id="clearfooter" style="height: 1px;"></div>
-<div id="footer">
-    <table cellpadding='0' cellspacing='0' width='100%' border='0' id="tfooter">
+    ?>
+    <!-- Footer -->
+    <div class="fullscreen-icon" title="Fullscreen" onclick="myToggleAll(0,true);">
+        <img class="ico-16" id="fullscreenIcon" alt="FullScreen" src="./img/icons/fullscreen.png">
+    </div>
+    <div id="clearfooter" style="height: 1px;"></div>
+    <div id="footer">
+        <table cellpadding='0' cellspacing='0' width='100%' border='0' id="tfooter">
             <tr>
-            <td>
-                <?php print _("Generated in ");
-                $time_end = microtime_float();
-                $now = $time_end - $time_start;
-                print round($now, 3) . " " . _("seconds"); ?>
-            </td>
+                <td>
+                    <?php print _("Generated in ");
+                    $time_end = microtime_float();
+                    $now = $time_end - $time_start;
+                    print round($now, 3) . " " . _("seconds"); ?>
+                </td>
                 <td align='center' class='copyRight'>
-                <a href='https://documentation.centreon.com' title='{$Documentation}' target='_blank'><?php echo _("Documentation"); ?></a> |
-                <a href="https://support.centreon.com" title="Centreon Support Access" target='_blank'>Centreon Support</a> | 
-                <a href="https://www.centreon.com" title='Centreon Services Overview' target='_blank'>Centreon</a> | 
-                <a href="https://github.com/centreon/centreon.git" title='Follow and Fork us on Github' target='_blank'>Github Project</a>  
-                    <?php if (isset($centreon->optGen["centreon_support_email"]) && $centreon->optGen["centreon_support_email"] != "") { ?>
-                | <a href='mailto:<?php print $oreon->optGen["centreon_support_email"]; ?>'><?php print _("Help Desk"); ?></a>
+                    <a href='https://documentation.centreon.com' title='{$Documentation}'
+                       target='_blank'><?php echo _("Documentation"); ?></a> |
+                    <a href="https://support.centreon.com" title="Centreon Support Access" target='_blank'>Centreon
+                        Support</a> |
+                    <a href="https://www.centreon.com" title='Centreon Services Overview' target='_blank'>Centreon</a> |
+                    <a href="https://github.com/centreon/centreon.git" title='Follow and Fork us on Github'
+                       target='_blank'>Github Project</a>
+                    <?php if (isset($centreon->optGen["centreon_support_email"]) &&
+                        $centreon->optGen["centreon_support_email"] != ""
+                    ) { ?>
+                        |
+                        <a href='mailto:<?php print $oreon->optGen["centreon_support_email"]; ?>'>
+                            <?php print _("Help Desk"); ?></a>
                     <?php } ?>
-            </td>
-            <td>Copyright &copy; 2005 - <?php echo date("Y"); ?></td>
-         </tr>
-    </table>
-</div>
-<?php
+                </td>
+                <td>Copyright &copy; 2005 - <?php echo date("Y"); ?></td>
+            </tr>
+        </table>
+    </div>
+    <?php
 }
 ?>
 
-<script type="text/javascript">
+    <script type="text/javascript">
 
-    // Centreon ToolTips
-    var centreonTooltip = new CentreonToolTip();
-    centreonTooltip.setTitle('<?php echo _("Help"); ?>');
-    centreonTooltip.render();
+        // Centreon ToolTips
+        var centreonTooltip = new CentreonToolTip();
+        centreonTooltip.setTitle('<?php echo _("Help"); ?>');
+        centreonTooltip.render();
 
-    function myToggleAll(duration,toggle){
-        if (toggle) {
-            //var i = document.getElementsByTagName("html")[0];
-            var i = document.documentElement;
-            if (
-                document.fullscreenElement ||
-                document.webkitFullscreenElement ||
-                document.mozFullScreenElement ||
-                document.msFullscreenElement
-            ) {
-                jQuery("#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor, #footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen").removeClass('tohide');
-                jQuery("#fullscreenIcon").attr("src","./img/icons/fullscreen.png");
-                jQuery('#contener').css({
-                    'height' : 'calc(100% - 170px)'
-                });
-                jQuery('#Tcontener').css({
-                   'margin-bottom': '0px'
-                });
-                if (document.exitFullscreen) {
-                    document.exitFullscreen();
-                } else if (document.msExitFullscreen) {
-                   document.msExitFullscreen();
-                } else if (document.mozCancelFullScreen) {
-                   document.mozCancelFullScreen();
-                } else if (document.webkitExitFullscreen) {
-                   document.webkitExitFullscreen();
-                }
-            } else {
-                jQuery("#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor, #footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen").addClass('tohide');
-                jQuery("#fullscreenIcon").attr("src","./img/icons/fullscreen_off.png");
-                jQuery('#contener').css({
-                    'height' : '100%'
-                });
-                jQuery('#Tcontener').css({
-                    'margin-bottom' : '0px'
-                });
-                // go full-screen
-                if (i.requestFullscreen) {
-                    i.requestFullscreen();
-                } else if (i.webkitRequestFullscreen) {
-                    i.webkitRequestFullscreen();
-                } else if (i.mozRequestFullScreen) {
-                    i.mozRequestFullScreen();
-                } else if (i.msRequestFullscreen) {
-                    i.msRequestFullscreen();
+        function myToggleAll(duration, toggle) {
+            if (toggle) {
+                //var i = document.getElementsByTagName("html")[0];
+                var i = document.documentElement;
+                if (
+                    document.fullscreenElement ||
+                    document.webkitFullscreenElement ||
+                    document.mozFullScreenElement ||
+                    document.msFullscreenElement
+                ) {
+                    jQuery("#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor, ".
+                    "#footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen"
+                ).
+                    removeClass('tohide');
+                    jQuery("#fullscreenIcon").attr("src", "./img/icons/fullscreen.png");
+                    jQuery('#contener').css({
+                        'height': 'calc(100% - 170px)'
+                    });
+                    jQuery('#Tcontener').css({
+                        'margin-bottom': '0px'
+                    });
+                    if (document.exitFullscreen) {
+                        document.exitFullscreen();
+                    } else if (document.msExitFullscreen) {
+                        document.msExitFullscreen();
+                    } else if (document.mozCancelFullScreen) {
+                        document.mozCancelFullScreen();
+                    } else if (document.webkitExitFullscreen) {
+                        document.webkitExitFullscreen();
+                    }
+                } else {
+                    jQuery(
+                        "#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor,".
+                    " #footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen"
+                ).
+                    addClass('tohide');
+                    jQuery("#fullscreenIcon").attr("src", "./img/icons/fullscreen_off.png");
+                    jQuery('#contener').css({
+                        'height': '100%'
+                    });
+                    jQuery('#Tcontener').css({
+                        'margin-bottom': '0px'
+                    });
+                    // go full-screen
+                    if (i.requestFullscreen) {
+                        i.requestFullscreen();
+                    } else if (i.webkitRequestFullscreen) {
+                        i.webkitRequestFullscreen();
+                    } else if (i.mozRequestFullScreen) {
+                        i.mozRequestFullScreen();
+                    } else if (i.msRequestFullscreen) {
+                        i.msRequestFullscreen();
+                    }
                 }
             }
         }
-    }
-    
-    document.addEventListener('webkitfullscreenchange', exitHandler, false);
-    document.addEventListener('mozfullscreenchange', exitHandler, false);
-    document.addEventListener('fullscreenchange', exitHandler, false);
-    document.addEventListener('MSFullscreenChange', exitHandler, false);
-    
-    function exitHandler() {
-        var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen || document.msFullscreenElement;
-        var event = state ? 'FullscreenOn' : 'FullscreenOff';
-        if (event === 'FullscreenOff') {
-            jQuery("#fullscreenIcon").attr("src","./img/icons/fullscreen.png");
-            jQuery("#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor, #footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen").removeClass('tohide');
-        }
-    }
 
-</script>
+        document.addEventListener('webkitfullscreenchange', exitHandler, false);
+        document.addEventListener('mozfullscreenchange', exitHandler, false);
+        document.addEventListener('fullscreenchange', exitHandler, false);
+        document.addEventListener('MSFullscreenChange', exitHandler, false);
+
+        function exitHandler() {
+            var state = document.fullScreen ||
+                document.mozFullScreen ||
+                document.webkitIsFullScreen ||
+                document.msFullscreenElement;
+            var event = state ? 'FullscreenOn' : 'FullscreenOff';
+            if (event === 'FullscreenOff') {
+                jQuery("#fullscreenIcon").attr("src", "./img/icons/fullscreen.png");
+                jQuery(
+                    "#actionBar, .pathWayBracket, .imgPathWay, .pathWay, hr, #QuickSearch, #menu1_bgcolor, ".
+                "#footer, #menu_1, #Tmenu , #menu_2, #menu_3, #header, .toHideInFullscreen"
+            ).
+                removeClass('tohide');
+            }
+        }
+
+    </script>
 <?php
 
 if ((isset($_GET["mini"]) && $_GET["mini"] == 1) ||
     (isset($_SESSION['fullScreen']) && isset($_SESSION['fullScreen']['value']) && $_SESSION['fullScreen']['value'])) {
-?>
+    ?>
     <script type="text/javascript">
-        myToggleAll(0,false);
+        myToggleAll(0, false);
     </script>
 <?php } else {
     if (!$centreon->user->showDiv("footer")) {
-?> <script type="text/javascript">new Effect.toggle('footer', 'blind', { duration : 0 });</script> <?php
+        ?>
+        <script type="text/javascript">new Effect.toggle('footer', 'blind', {duration: 0});</script> <?php
     }
 }
 
@@ -164,44 +186,44 @@ if ((isset($_GET["mini"]) && $_GET["mini"] == 1) ||
 $cdata = CentreonData::getInstance();
 $jsdata = $cdata->getJsData();
 foreach ($jsdata as $k => $val) {
-    echo "<span class=\"data hide\" id=\"".$k."\" data-".$k."=\"".$val."\"></span>";
+    echo "<span class=\"data hide\" id=\"" . $k . "\" data-" . $k . "=\"" . $val . "\"></span>";
 }
 
 ?>
 
-<script type='text/javascript'>
-jQuery(function() {
-    initWholePage();
-});
+    <script type='text/javascript'>
+        jQuery(function () {
+            initWholePage();
+        });
 
-/*
- * Init whole page
- */
-function initWholePage() {
-    setQuickSearchPosition();
-    jQuery().centreon_notify({
-        sid: "<?php echo session_id();?>", 
-        refresh_rate: <?php echo ($centreon->optGen['AjaxTimeReloadMonitoring'] * 1000);?>
-    });
-}
-
-/*
- * set quick search position
- */
-function setQuickSearchPosition() {
-    if (jQuery('#QuickSearch')) {
-        if (jQuery('#header').is(':visible')) {
-            jQuery('#QuickSearch').css({ top: '86px' });
-        } else {
-            jQuery('#QuickSearch').css({ top: '3px' });
+        /*
+         * Init whole page
+         */
+        function initWholePage() {
+            setQuickSearchPosition();
+            jQuery().centreon_notify({
+                sid: "<?php echo session_id();?>",
+                refresh_rate: <?php echo($centreon->optGen['AjaxTimeReloadMonitoring'] * 1000);?>
+            });
         }
-    }    
-    jQuery(".timepicker").timepicker();
-    jQuery(".datepicker").datepicker();
-}
-</script>
-</body>
-</html>
+
+        /*
+         * set quick search position
+         */
+        function setQuickSearchPosition() {
+            if (jQuery('#QuickSearch')) {
+                if (jQuery('#header').is(':visible')) {
+                    jQuery('#QuickSearch').css({top: '86px'});
+                } else {
+                    jQuery('#QuickSearch').css({top: '3px'});
+                }
+            }
+            jQuery(".timepicker").timepicker();
+            jQuery(".datepicker").datepicker();
+        }
+    </script>
+    </body>
+    </html>
 <?php
 
 /* 
