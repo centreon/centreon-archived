f1af89326c910688006e75f5d03d662e4d7f0ace
Sims24 <sbomm@centreon.com>
2017-12-30 15:07:34 +0100

    + finish checkbroker initial module (subject to change)

--- lib/perl/centreon/health/checkbroker.pm
+++ lib/perl/centreon/health/checkbroker.pm
@@ -1,139 +1,96 @@
-################################################################################
-# Copyright 2005-2013 Centreon
-# Centreon is developped by : Julien Mathis and Romain Le Merlus under
-# GPL Licence 2.0.
-# 
-# This program is free software; you can redistribute it and/or modify it under 
-# the terms of the GNU General Public License as published by the Free Software 
-# Foundation ; either version 2 of the License.
-# 
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along with 
-# this program; if not, see <http://www.gnu.org/licenses>.
-# 
-# Linking this program statically or dynamically with other modules is making a 
-# combined work based on this program. Thus, the terms and conditions of the GNU 
-# General Public License cover the whole combination.
-# 
-# As a special exception, the copyright holders of this program give Centreon 
-# permission to link this program with independent modules to produce an executable, 
-# regardless of the license terms of these independent modules, and to copy and 
-# distribute the resulting executable under terms of Centreon choice, provided that 
-# Centreon also meet, for each linked independent module, the terms  and conditions 
-# of the license of that module. An independent module is a module which is not 
-# derived from this program. If you modify this program, you may extend this 
-# exception to your version of the program, but you are not obliged to do so. If you
-# do not wish to do so, delete this exception statement from your version.
-# 
 #
-####################################################################################
+# Copyright 2017 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-package centreon::health::checksystems;
+package centreon::health::checkbroker;
 
 use strict;
 use warnings;
+use JSON;
+use POSIX qw(strftime);
 use centreon::common::misc;
 use centreon::health::ssh;
 
 sub new {
     my $class = shift;
     my $self = {};
-    $self->{cmd_system_health} = [];
     $self->{output} = {};
 
     bless $self, $class;
     return $self;
 }
 
-sub build_command_hash {
+sub json_parsing {
     my ($self, %options) = @_;
-
-    if ($options{medium} eq "snmp") {
-        $self->{cmd_system_health} = [ { cmd => "/usr/lib/centreon/plugins/centreon_linux_snmp.pl --plugin os::linux::snmp::plugin --mode cpu-detailed \\
-							 --hostname localhost \\
-							 --statefile-suffix='_diag-cpu' \\
-							 --filter-perfdata='^(?!(wait|guest|user|softirq|kernel|interrupt|guestnice|idle|steal|system|nice))' \\
-							 --snmp-community " . $options{community} ,
-                                         callback => \&centreon::health::ssh::ssh_callback,
-					 userdata => "cpu_usage" },
-                                       { cmd => "/usr/lib/centreon/plugins/centreon_linux_snmp.pl --plugin os::linux::snmp::plugin --mode load \\
-							 --hostname localhost \\
-							 --filter-perfdata='^(?!(load))' \\
-							 --snmp-community " . $options{community},
-                                         callback => \&centreon::health::ssh::ssh_callback,
-                                         userdata => "load" },
-                                       { cmd => "/usr/lib/centreon/plugins/centreon_linux_snmp.pl --plugin os::linux::snmp::plugin --mode memory \\
-							 --hostname localhost \\
-							 --filter-perfdata='^(?!(cached|buffer|used))' \\
-							 --snmp-community " . $options{community},
-                                         callback => \&centreon::health::ssh::ssh_callback,
-                                         userdata => "mem_usage" },
-                                       { cmd => "/usr/lib/centreon/plugins/centreon_linux_snmp.pl --plugin os::linux::snmp::plugin --mode swap \\
-							 --hostname localhost \\
-							 --filter-perfdata='^(?!(used))' \\
-							 --snmp-community " . $options{community},
-                                         callback => \&centreon::health::ssh::ssh_callback,
-                                         userdata => "swap_usage" },
-                                       { cmd => "/usr/lib/centreon/plugins/centreon_linux_snmp.pl --plugin os::linux::snmp::plugin --mode storage \\
-							 --hostname localhost \\
-							 --storage='^(?!(/dev/shm|/sys/fs/cgroup|/boot|/run.*))' --name --regexp \\
-							 --filter-perfdata='^(?!(used))' --statefile-suffix='_diag-storage' \\
-							 --verbose \\
-							 --snmp-community " . $options{community},
-                                        callback => \&centreon::health::ssh::ssh_callback,
-                                        userdata => "storage_usage" },
-                                     ];
-    } else {
-	return -1
-    }
-
-
-}
-
-sub get_remote_infos {
-    my ($self, %options) = @_;
-   
-    return centreon::health::ssh::new->main(host => $options{host}, port => $options{ssh_port}, command_pool => $self->{cmd_system_health}); 
     
-}
-     
-sub get_local_infos {
-    my ($self, %options) = @_;
-    my ($lerror, $stdout);    
-    
-    my $results;
-
-    foreach my $command (@{$self->{cmd_system_health}}) {
-        ($lerror, $stdout) = centreon::common::misc::backtick(command => $command->{cmd});
-	$results->{$command->{userdata}} = $stdout;
-	while ($stdout =~ m/Buffer creation/) {
-	    # Replay command to bypass cache creation
-	    sleep 1;
-	    ($lerror, $stdout) = centreon::common::misc::backtick(command => $command->{cmd});
-	    $results->{$command->{userdata}} = $stdout;
+    my $json_content = JSON->new->decode($options{json_content});
+    foreach my $key (keys $json_content) {
+	if ($key =~ m/^endpoint/) {
+	    foreach my $broker_metric (keys $json_content->{$key}) {
+		next if ($broker_metric !~ m/version|event_processing|last_connection|queued|state/);
+		$self->{output}->{$options{poller_name}}->{$options{file_name}}->{$broker_metric} = ($broker_metric =~ m/^last_connection/ && $json_content->{$key}->{$broker_metric} != -1) 
+										? strftime("%m/%d/%Y %H:%M:%S",localtime($json_content->{$key}->{$broker_metric}))
+										: $json_content->{$key}->{$broker_metric} ;
+	    }
+	} elsif ($key =~ m/version/) {
+	    $self->{output}->{$options{poller_name}}->{$options{file_name}}->{$key} = $json_content->{$key};
 	}
+
     }
 
-    return $results 
-    
+    return $self->{output} 
+
 }
 
 sub run {
     my $self = shift;
-    my ($server_list, $medium, $community, $logger) = @_;
+    my ($centreon_db, $server_list, $centreon_version, $logger) = @_;
+
+    my $sth;
 
     foreach my $server (keys $server_list) {
-	my $name = $server_list->{$server}->{name};
-	if ($server_list->{$server}->{localhost} eq "NO") {
-	    $self->{output}->{$name} = $self->get_remote_infos(host => $server_list->{$server}->{address}, ssh_port => $server_list->{$server}->{ssh_port});
-        } else {
-	    $self->{output}->{$name} = $self->get_local_infos(poller_name => $name);
-	}
+	$sth = $centreon_db->query("SELECT config_name, cache_directory 
+					FROM cfg_centreonbroker 
+					WHERE stats_activate='1' 
+					AND ns_nagios_server=".$centreon_db->quote($server)."");
+
+	if ($server_list->{$server}->{localhost} eq "YES") {
+            while (my $row = $sth->fetchrow_hashref()) {
+	        my ($lerror, $stdout) = centreon::common::misc::backtick(command => "cat " . $row->{cache_directory} . "/" . $row->{config_name} . "-stats.json");
+		$self->{output} = $self->json_parsing(json_content => $stdout,
+										     poller_name => $server_list->{$server}->{name},
+										     file_name => $row->{config_name}. "-stats.json");
+	    }
+	} else {
+            while (my $row = $sth->fetchrow_hashref()) {
+		my $stdout = centreon::health::ssh->new->main(host => $server_list->{$server}->{address},
+                                                              port => $server_list->{$server}->{ssh_port},
+                                                              userdata => $row->{cache_directory} . "/" . $row->{config_name} . "-stats.json",
+                                                              command => "cat " . $row->{cache_directory} . "/" . $row->{config_name} . "-stats.json");
+                $self->{output} = $self->json_parsing(json_content => $stdout,
+                                                                                     poller_name => $server_list->{$server}->{name},
+                                                                                       file_name => $row->{config_name}. "-stats.json");
+
+            }
+        }
+       
     }
-    
     return $self->{output}
 }
 
