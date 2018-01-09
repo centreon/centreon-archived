0a3092c054192faca9b75e1a691ae08d091c6994
loiclau <loic.lau@gmail.com>
2018-01-09 15:46:34 +0100

    fix(api) add quote for enum

--- www/api/class/centreon_statistics.class.php
+++ www/api/class/centreon_statistics.class.php
@@ -63,9 +63,9 @@ class CentreonStatistics extends CentreonWebService
     {
         $query = "SELECT COUNT(host_id) as nb_hosts, " .
             "(SELECT COUNT(service_id) FROM service " .
-            "WHERE service_activate = 1 AND service_register = 1) as nb_services, " .
-            "(SELECT COUNT(id) FROM nagios_server WHERE ns_activate = 1) as nb_pollers " .
-            "FROM host WHERE host_activate = 1 AND host_register = 1";
+            "WHERE service_activate = '1' AND service_register = '1') as nb_services, " .
+            "(SELECT COUNT(id) FROM nagios_server WHERE ns_activate = '1') as nb_pollers " .
+            "FROM host WHERE host_activate = '1' AND host_register = '1'";
         $dbResult = $this->pearDB->query($query);
         $data = $dbResult->fetchRow();
 
