f0020849a36376d9517d6275c9daa63a49e71e8e
loiclau <loic.lau@gmail.com>
2018-02-05 14:27:22 +0100

    fix(broker) use final input name. (#6042)

--- www/install/sql/centreon/Update-DB-2.8.17_to_2.8.18.sql
+++ www/install/sql/centreon/Update-DB-2.8.17_to_2.8.18.sql
@@ -7,36 +7,36 @@ ALTER TABLE `cb_type_field_relation` ADD COLUMN `jshook_name` VARCHAR(255) DEFAU
 ALTER TABLE `cb_type_field_relation` ADD COLUMN `jshook_arguments` VARCHAR(255) DEFAULT NULL;
 
 
-INSERT INTO `cb_module` (`name`, `libname`, `loading_pos`, `is_activated`) VALUES ('Lua', 'lua.so', 40,1);
+INSERT INTO `cb_module` (`name`, `libname`, `loading_pos`, `is_activated`) VALUES ('Generic', 'lua.so', 40,1);
 
 INSERT INTO `cb_type` (`type_name`, `type_shortname`, `cb_module_id`)
-VALUES ('Lua script', 'custom', (SELECT `cb_module_id` FROM `cb_module` WHERE `libname` = 'Lua.so'));
+VALUES ('Stream connector', 'lua', (SELECT `cb_module_id` FROM `cb_module` WHERE `libname` = 'lua.so'));
 
 INSERT INTO `cb_fieldgroup` (`groupname`, `displayname`, `multiple`, `group_parent_id`)
-VALUES ('metrics_lua', 'metrics Lua', 1, NULL);
+VALUES ('lua_parameters', 'lua parameters', 1, NULL);
 
 INSERT INTO `cb_field` (`fieldname`, `displayname`, `description`, `fieldtype`, `external`, `cb_fieldgroup_id`)
 VALUES
 ('path', 'Path', 'Path of the lua script.', 'text', NULL, NULL),
-('type', 'Type', 'Type of the metric.', 'select', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'metrics_lua')),
-('name', 'Name', 'Name of the metric.', 'text', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'metrics_lua')),
-('value', 'Value', 'Value of the metric.', 'text', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'metrics_lua'));
+('type', 'Type', 'Type of the metric.', 'select', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'lua_parameters')),
+('name', 'Name', 'Name of the metric.', 'text', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'lua_parameters')),
+('value', 'Value', 'Value of the metric.', 'text', NULL, (SELECT `cb_fieldgroup_id` FROM `cb_fieldgroup` WHERE `groupname` = 'lua_parameters'));
 
 INSERT INTO `cb_type_field_relation` (`cb_type_id`, `cb_field_id`, `is_required`, `order_display`, `jshook_name`, `jshook_arguments`)
 VALUES (
-(SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'),
+(SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'),
 (SELECT `cb_field_id` FROM `cb_field` WHERE `description` = 'Path of the lua script.'),
 1, 1, NULL, NULL),
-((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'),
+((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'),
 (SELECT `cb_field_id` FROM `cb_field` WHERE `description` = 'Category filter for flux in output'),
 0, 2, NULL, NULL),
-((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'),
+((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'),
 (SELECT `cb_field_id` FROM `cb_field` WHERE `description` = 'Type of the metric.'),
-0, 5, 'luaArguments', '{"target": "metrics_lua__value_%d"}'),
-((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'),
+0, 5, 'luaArguments', '{"target": "lua_parameters__value_%d"}'),
+((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'),
 (SELECT `cb_field_id` FROM `cb_field` WHERE `description` = 'Name of the metric.'),
 0, 4, NULL, NULL),
-((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'),
+((SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'),
 (SELECT `cb_field_id` FROM `cb_field` WHERE `description` = 'Value of the metric.'),
 0, 3, NULL, NULL);
 
@@ -56,4 +56,4 @@ VALUES
 );
 
 INSERT INTO `cb_tag_type_relation` (`cb_tag_id`, `cb_type_id`, `cb_type_uniq`)
- VALUES (1, (SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'custom'), 0);
+ VALUES (1, (SELECT `cb_type_id` FROM `cb_type` WHERE `type_shortname` = 'lua'), 0);
