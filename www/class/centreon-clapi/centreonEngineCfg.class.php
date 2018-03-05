24e4befdac5187fe0ac832a3d0f339251dbbee42
Guillaume28 <gbaldo@centreon.com>
2018-03-05 15:13:02 +0100

    fix(Engine): cfg export with filter

--- www/class/centreon-clapi/centreonEngineCfg.class.php
+++ www/class/centreon-clapi/centreonEngineCfg.class.php
@@ -284,8 +284,14 @@ class CentreonEngineCfg extends CentreonObject
      *
      * @return void
      */
-    public function export($filters = null)
+    public function export($filter_id = null, $filter_name = null)
     {
+        $filters = null;
+        if (!is_null($filter_id)) {
+            $filters = array(
+                $this->object->getPrimaryKey() => $filter_id
+                );
+        }
         $elements = $this->object->getList("*", -1, 0, null, null, $filters, "AND");
         $tpObj = new \Centreon_Object_Timeperiod($this->dependencyInjector);
         foreach ($elements as $element) {
