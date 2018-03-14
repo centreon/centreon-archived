383a4c202c01d8a274a1fc00c11dc937998e94f2
Stephane Chapron <schapron@centreon.com>
2018-03-14 16:33:48 +0100

    feat(date): Formatting date in Reporting Dashboard Host DatepickerResult

--- www/include/reporting/dashboard/viewHostLog.php
+++ www/include/reporting/dashboard/viewHostLog.php
@@ -118,9 +118,9 @@ if (isset($id) && $id != "NULL") {
     $tpl->assign("components_avg", array_pop($hostServicesStats));
     $tpl->assign("components", $hostServicesStats);
     $tpl->assign("period_name", _("From"));
-    $tpl->assign("date_start", date(_("d/m/Y H:i"), $start_date));
+    $tpl->assign("date_start", $start_date);
     $tpl->assign("to", _("to"));
-    $tpl->assign("date_end", date(_("d/m/Y H:i"), $end_date));
+    $tpl->assign("date_end", $end_date);
     $tpl->assign("period", $period);
     $tpl->assign("host_id", $id);
     $tpl->assign("Alert", _("Alert"));
