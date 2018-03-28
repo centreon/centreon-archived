1c456741a71a07c35895795da95382bad2a61551
Maximilien Bersoult <mbersoult@centreon.com>
2018-03-28 15:47:16 +0200

    feat(api): Split role between configuration and realtime
    An user can have a role to access to configuration API or realtime API.

--- www/api/class/centreon_proxy.class.php
+++ www/api/class/centreon_proxy.class.php
@@ -26,4 +26,17 @@ class CentreonProxy extends CentreonWebService
             'message' => $message
         );
     }
+
+    /**
+     * Authorize to access to the action
+     *
+     * @param string $action The action name
+     * @param array $user The current user
+     * @param boolean $isInternal If the api is call in internal
+     * @return boolean If the has access to the action
+     */
+    public function authorize($action, $user, $isInternal)
+    {
+        return true;
+    }
 }
