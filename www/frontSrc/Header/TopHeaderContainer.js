959819fea31a6c6622a1e4a0d1d249a70a30bf54
Ridene <rridene@centreon.com>
2018-03-30 17:30:16 +0200

    feat(new header): improve UX

--- www/frontSrc/Header/TopHeaderContainer.js
+++ www/frontSrc/Header/TopHeaderContainer.js
@@ -7,10 +7,13 @@ import TopHeader from './TopHeader'
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: '#88b917' },
+    primary: { main: '#88b917', dark: '#597F00' },
     secondary: { main: '#00bfb3' },
-    error: { main: '#e00b3d' },
-    warning: { main: '#ff9a13' },
+    error: { main: '#e00b3d', light: '#ff2c5b', lighter: '#FF8C88' },
+    warning: { main: '#ff9a13', light: '#ffb749' },
+    unreachable: { main: '#818285', light: '#a6a7a6' },
+    unknown: { main: '#bcbdc0', dark: '#95969a' },
+    pending: { main: '#2AD1D4' },
   },
   font: {
     openSans: "'Open Sans', Arial, Tahoma, Helvetica, Sans-Serif"
@@ -20,7 +23,7 @@ const theme = createMuiTheme({
       root: {
         height: '18px',
         fontSize: '0.9rem',
-        font: "'Open Sans', Arial, Tahoma, Helvetica, Sans-Serif",
+        fontFamily: "'Open Sans', Arial, Tahoma, Helvetica, Sans-Serif",
         paddingTop: '8px',
         paddingBottom: '8px',
       },
@@ -30,6 +33,11 @@ const theme = createMuiTheme({
         padding: '14px'
       }
     },
+    MuiButton: {
+      root: {
+        fontFamily: "'Open Sans', Arial, Tahoma, Helvetica, Sans-Serif"
+      }
+    }
   }
 });
 
