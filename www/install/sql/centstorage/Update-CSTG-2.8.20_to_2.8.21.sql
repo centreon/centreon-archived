d9d4cf259b44aae3c3d6f48ac9e548d92233a813
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-04-04 10:12:33 +0200

    Revert "fix(install): remove unused fields from the `instances` table"
    This reverts commit 7a92082ce8ba3dcad6ced1bc15228ece67a6d414.

--- www/install/sql/centstorage/Update-CSTG-2.8.20_to_2.8.21.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `instances` DROP COLUMN `address`;
-ALTER TABLE `instances` DROP COLUMN `daemon_mode`;
-ALTER TABLE `instances` DROP COLUMN `last_log_rotation`;
-ALTER TABLE `instances` DROP COLUMN `process_perfdata`;
