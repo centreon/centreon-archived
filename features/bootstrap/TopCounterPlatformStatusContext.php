84a4f24685903d1a3a51c9ba474c1948e59a00ca
Ridene <rridene@centreon.com>
2018-04-25 11:17:17 +0200

    test(at): init acceptance tests for poller behaviour

---
+++ features/bootstrap/TopCounterPlatformStatusContext.php
@@ -0,0 +1,14 @@
+<?php
+
+use Centreon\Test\Behat\CentreonContext;
+
+class TopCounterPlatformStatusContext extends CentreonContext
+{
+    /**
+     * @When I activate the new header
+     */
+    public function iActivateTheNewHeader()
+    {
+
+    }
+}
