0517a460842f83d51049ef8abca86a20e0deb37e
victorvassilev <acedweb@gmail.com>
2018-05-15 10:56:02 +0300

    Cp7 m24 migrate the installer and update (#6281)
    * refactor(install): fix upgrade process
    
    change MySQL library of php requirements and add a check for release notes file
    
    Resolves CP7M-24
    
    * refactor(install): add missing folder and cfg mock
    
    add missing folder `www/install/tmp` and add mock of `www/install/install.conf.php`
    
    Resolves CP7M-24
    
    * refactor(install): Fix smarty and smarty-plugins loading
    
    Resolves problem for unknown smarty plugin. Loads composer-managed smarty.

---
+++ www/install/install.conf.dist.php
@@ -0,0 +1,29 @@
+<?php
+$conf_centreon['centreon_dir'] = "/usr/share/centreon/";
+$conf_centreon['centreon_etc'] = "/etc/centreon/";
+$conf_centreon['centreon_dir_www'] = "/usr/share/centreon/www/";
+$conf_centreon['centreon_dir_rrd'] = "/var/lib/centreon";
+$conf_centreon['centreon_log'] = "/var/log/centreon";
+$conf_centreon['centreon_varlib'] = "/var/lib/centreon";
+$conf_centreon['centreon_group'] = "centreon";
+$conf_centreon['centreon_user'] = "centreon";
+$conf_centreon['rrdtool_dir'] = "/usr/bin/rrdtool";
+$conf_centreon['apache_user'] = "apache";
+$conf_centreon['apache_group'] = "apache";
+$conf_centreon['mail'] = "/bin/mail";
+$conf_centreon['broker_user'] = "centreon-broker";
+$conf_centreon['broker_group'] = "centreon-broker";
+$conf_centreon['broker_etc'] = "/etc/centreon-broker";
+$conf_centreon['broker_init_script'] = "cbd";
+$conf_centreon['monitoring_user'] = "centreon-engine";
+$conf_centreon['monitoring_group'] = "centreon-engine";
+$conf_centreon['monitoring_etc'] = "/etc/centreon-engine";
+$conf_centreon['monitoring_init_script'] = "centengine";
+$conf_centreon['monitoring_binary'] = "/usr/sbin/centengine";
+$conf_centreon['monitoring_varlog'] = "/var/log/centreon-engine";
+$conf_centreon['plugin_dir'] = "/usr/lib/nagios/plugins";
+$conf_centreon['centreon_engine_connectors'] = "/usr/lib64/centreon-connector";
+$conf_centreon['centreon_engine_lib'] = "/usr/lib64/centreon-engine";
+$conf_centreon['centreonbroker_cbmod'] = "/usr/lib64/nagios/cbmod.so";
+$conf_centreon['centreon_plugins'] = "/usr/lib/centreon/plugins";
+?>
