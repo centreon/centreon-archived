b74e58715af6a227d3a97f9e69dc8f941cef0146
Ridene <rridene@centreon.com>
2018-05-22 16:34:10 +0200

    fix(js): fix select all action on listing
    refs: #6193

--- www/include/common/javascript/tool.js
+++ www/include/common/javascript/tool.js
@@ -140,13 +140,19 @@ function toggleDisplay(id)
 function checkUncheckAll(theElement)
 {
     jQuery(theElement).parents('tr').nextAll().find('input[type=checkbox]').each(function() {
+
         if (theElement.checked && !jQuery(this).attr('checked')) {
             jQuery(this).attr('checked',true);
+            jQuery(this).prop( "checked", true);
+
             if (typeof(_selectedElem) != 'undefined') {
                 putInSelectedElem(jQuery(this).attr('id'));
             }
         } else if (!theElement.checked && jQuery(this).attr('checked')) {
+        	
             jQuery(this).attr('checked', false);
+            jQuery(this).prop( "checked", false);
+
             if (typeof(_selectedElem) != 'undefined') {
                 removeFromSelectedElem(jQuery(this).attr('id'));
             }
