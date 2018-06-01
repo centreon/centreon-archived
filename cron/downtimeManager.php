f424c0b0e5e3e57371a6b56f3dfe225b5b321b11
Matthieu Kermagoret <mkermagoret@centreon.com>
2018-06-01 11:08:45 +0200

    fix(build): use /usr/bin/env in shebang instead of direct php path.

--- cron/downtimeManager.php
+++ cron/downtimeManager.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+#!/usr/bin/env php
 <?php
 /**
  * Copyright 2005-2015 Centreon
@@ -130,4 +130,3 @@ $downtimeObj->sendCommands();
 
 # Purge downtime cache
 $downtimeObj->purgeCache();
-
