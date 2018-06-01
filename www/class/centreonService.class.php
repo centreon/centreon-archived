013f7263489f5d551d5fc94841f799ad802c2434
loiclau <loic.lau@gmail.com>
2018-06-01 14:14:27 +0200

    fix(api) fix select2 without default service (#6338)

--- www/class/centreonService.class.php
+++ www/class/centreonService.class.php
@@ -1050,13 +1050,17 @@ class CentreonService
 
         $hostIdList = array();
         $serviceIdList = array();
-        foreach ($values as $value) {
-            if (strpos($value, '-')) {
-                $tmpValue = explode('-', $value);
-                $hostIdList[] = $tmpValue[0];
-                $serviceIdList[] = $tmpValue[1];
-            } else {
-                $serviceIdList[] = $value;
+        if (!empty($values)) {
+            foreach ($values as $value) {
+                if (trim($value, '-') != '') {
+                    if ((strpos($value, '-') > 0)) {
+                        $tmpValue = explode('-', $value);
+                        $hostIdList[] = $tmpValue[0];
+                        $serviceIdList[] = $tmpValue[1];
+                    } else {
+                        $serviceIdList[] = $value;
+                    }
+                }
             }
         }
 
