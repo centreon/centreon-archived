292cf86bbaa31671e486cc3bad9263ee3e2e5803
sc979 <34628915+sc979@users.noreply.github.com>
2018-06-25 10:32:33 +0200

    fix(db): remove duplicate entries in centreon_acl table (#6366)
    * fix(DB): upgrade DB to correct a bug
    
    * fix(DB): centAcl close DB connection before the update is achieved
    
    * fix(acl): Add primary key and handle duplicate entries in centreon_acl
    
    * fix(sql): delete duplicate entries in centreon_acl table
    
    * feat(install): add base version to 2.8.24
    
    * doc(upgrade): add empty release notes for 2.8.24

---
+++ www/install/sql/centreon/Update-DB-2.8.24_to_18.9.0.sql
@@ -0,0 +1,2 @@
+-- Change version of Centreon
+UPDATE `informations` SET `value` = '18.9.0' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.24' LIMIT 1;
