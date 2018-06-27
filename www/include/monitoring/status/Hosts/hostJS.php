f2838be2c14f2e96bb5715e976bf88deab42cade
Valentin Hristov <vhr@users.noreply.github.com>
2018-06-27 17:51:38 +0300

    fix(ux): prevent memory leaks from a mainLoop and goM function
    Resolve CP7M-111

--- www/include/monitoring/status/Hosts/hostJS.php
+++ www/include/monitoring/status/Hosts/hostJS.php
@@ -206,7 +206,7 @@ if (isset($_GET["acknowledge"])) {
         }
         _oldInputFieldValue = _currentInputFieldValue;
 
-        setTimeout("mainLoopHost()", 250);
+        setTimeout(mainLoopHost, 250);
     }
 
     function initM(_time_reload, _sid, _o) {
@@ -264,7 +264,7 @@ if (isset($_GET["acknowledge"])) {
         }
 
         _lock = 0;
-        _timeoutID = cycleVisibilityChange('goM("' + _time_reload + '","' + _sid + '","' + _o + '")', _time_reload);
+        _timeoutID = cycleVisibilityChange(function(){goM(_time_reload, _sid, _o)}, _time_reload);
         _time_live = _time_reload;
         _on = 1;
 
