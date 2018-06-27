cc7f1a947f98bcebf351d6261ea41d65f006a4fe
Valentin Hristov <vhr@users.noreply.github.com>
2018-06-27 10:53:32 +0300

    fix(ux): use pure XMLHttpRequest to avoid a additional listener in keepAlive.js
    Resolve CP7M-111

--- www/include/common/javascript/keepAlive.js
+++ www/include/common/javascript/keepAlive.js
@@ -1,7 +1,6 @@
 /* Refresh session every 15 seconds*/
 setInterval(function(){
-    jQuery.ajax({
-        method: 'GET',
-        url: './api/internal.php?object=centreon_keepalive&action=keepalive'
-    });
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('GET', './api/internal.php?object=centreon_keepalive&action=keepalive', true);
+    xhttp.send();
 },15000);
