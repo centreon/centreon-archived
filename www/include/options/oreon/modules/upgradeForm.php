fdeb1fdfb3b8a8f6dadca056b2a97d7f5be31c08
V. Radev <v-radev@users.noreply.github.com>
2018-06-28 13:55:47 +0300

    feat(modules): add buttons to install and upgrade all (#6408)

--- www/include/options/oreon/modules/upgradeForm.php
+++ www/include/options/oreon/modules/upgradeForm.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -46,30 +46,53 @@ if (!isset($centreon)) {
 $tpl = new Smarty();
 $tpl = initSmartyTpl($path, $tpl);
 
-$tpl->assign("headerMenu_title", _("Module Information"));
-$tpl->assign("headerMenu_title2", _("Upgrade Information"));
-$tpl->assign("headerMenu_rname", _("Real name"));
-$tpl->assign("headerMenu_release", _("Release"));
-$tpl->assign("headerMenu_release_from", _("Base release"));
-$tpl->assign("headerMenu_release_to", _("Final release"));
-$tpl->assign("headerMenu_author", _("Author"));
-$tpl->assign("headerMenu_infos", _("Additional Information"));
-$tpl->assign("headerMenu_isinstalled", _("Installed"));
+$tpl->assign('headerMenu_title', _('Module Information'));
+$tpl->assign('headerMenu_title2', _('Upgrade Information'));
+$tpl->assign('headerMenu_rname', _('Real name'));
+$tpl->assign('headerMenu_release', _('Release'));
+$tpl->assign('headerMenu_release_from', _('Base release'));
+$tpl->assign('headerMenu_release_to', _('Final release'));
+$tpl->assign('headerMenu_author', _('Author'));
+$tpl->assign('headerMenu_infos', _('Additional Information'));
+$tpl->assign('headerMenu_isinstalled', _('Installed'));
+
+if (isset($_GET['action']) && $_GET['action'] == 'upgrade_all') {
+    $upgradableModules = $moduleInfoObj->getUpgradeableList();
+    $tpl->assign('output', _('All modules successfully upgraded.'));
+
+    foreach ($upgradableModules as $upgradableModule) {
+        $moduleUpgrader = $moduleFactory->newUpgrader($upgradableModule['name'], $upgradableModule['id']);
+
+        if ( !$moduleUpgrader->upgrade() ) {
+            $tpl->assign('output', _('Unable to upgrade module: ' . $upgradableModule['rname']));
+            break;
+        }
+    }
+
+    $centreon->creatModuleList($pearDB);
+    $centreon->user->access->updateTopologyStr();
+    $centreon->initHooks();
+
+    $tpl->assign('p', $p);
+    $tpl->display('modulesAction.tpl');
+    die;
+}
 
 if (is_null($name)) {
     $name = $moduleInfoObj->getNameById($id);
 }
-$moduleUpgrader = $moduleFactory->newUpgrader($name, $id);
 
+$moduleUpgrader = $moduleFactory->newUpgrader($name, $id);
 $moduleInfo = $moduleInfoObj->getConfiguration($name);
 $moduleInstalledInfo = $moduleInfoObj->getInstalledInformation($name);
 
 $elemArr = array();
-$form = new HTML_QuickFormCustom('Form', 'post', "?p=" . $p);
-$form->addElement('submit', 'list', _("Back"), array("class" => "btc bt_default"));
-$form->addElement('submit', 'upgrade', _("Upgrade"), array("class" => "btc bt_success"));
+$form = new HTML_QuickFormCustom('Form', 'post', "?p={$p}");
+$form->addElement('submit', 'list', _('Back'), array('class' => 'btc bt_default'));
+$form->addElement('submit', 'upgrade', _('Upgrade'), array('class' => 'btc bt_success'));
 $redirect = $form->addElement('hidden', 'o');
-$redirect->setValue("u");
+$redirect->setValue('u');
+
 if (!is_null($id)) {
     $hid_id = $form->addElement('hidden', 'id');
     $hid_id->setValue($id);
@@ -78,41 +101,43 @@ if (!is_null($id)) {
 $upgradeAvailable = false;
 $upgrade_infosTxt = false;
 
-if ($moduleInstalledInfo["mod_release"] != $moduleInfo["mod_release"]) {
+if ($moduleInstalledInfo['mod_release'] != $moduleInfo['mod_release']) {
     $upgradeAvailable = true;
 }
+
 if ($form->validate()) {
     $upgrade_ok = $moduleUpgrader->upgrade();
+
     if ($upgrade_ok) {
         $upgradeAvailable = false;
         $centreon->creatModuleList($pearDB);
         $centreon->user->access->updateTopologyStr();
         $centreon->initHooks();
     }
-    
+
     // @todo need to set default value of filename
     $filename = isset($filename) ? $filename : '';
-    
-    $upgradePath = _CENTREON_PATH_ . "www/modules/". $moduleInfo["name"] . "/UPGRADE/" . $filename;
 
-    if (is_dir($upgradePath . "/infos") && is_file($upgradePath . "/infos/infos.txt")) {
-        $infos_streams = file($upgradePath . "/infos/infos.txt");
-        $infos_streams = implode("<br />", $infos_streams);
+    $upgradePath = _CENTREON_PATH_ . 'www/modules/'. $moduleInfo['name'] . '/UPGRADE/' . $filename;
+
+    if (is_dir($upgradePath . '/infos') && is_file($upgradePath . '/infos/infos.txt')) {
+        $infos_streams = file($upgradePath . '/infos/infos.txt');
+        $infos_streams = implode('<br />', $infos_streams);
         $upgrade_infosTxt = $infos_streams;
     }
 }
 
 $module = array(
-    "upgrade_rname" => $moduleInfo["rname"],
-    "upgrade_release_from" => $moduleInstalledInfo["mod_release"],
-    "upgrade_release_to" => $moduleInfo["mod_release"],
-    "upgrade_author" => $moduleInfo["author"],
-    "upgrade_infos" =>$moduleInfo["infos"],
-    "upgrade_infosTxt" => $upgrade_infosTxt,
-    "upgrade_available" => $upgradeAvailable
+    'upgrade_rname'        => $moduleInfo['rname'],
+    'upgrade_release_from' => $moduleInstalledInfo['mod_release'],
+    'upgrade_release_to'   => $moduleInfo['mod_release'],
+    'upgrade_author'       => $moduleInfo['author'],
+    'upgrade_infos'        => $moduleInfo['infos'],
+    'upgrade_infosTxt'     => $upgrade_infosTxt,
+    'upgrade_available'    => $upgradeAvailable,
 );
 
-$tpl->assign("module", $module);
+$tpl->assign('module', $module);
 $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 $form->accept($renderer);
 $tpl->assign('form', $renderer->toArray());
@@ -120,4 +145,4 @@ $tpl->assign('form', $renderer->toArray());
 /**
  * Display form
  */
-$tpl->display("upgradeForm.tpl");
+$tpl->display('upgradeForm.tpl');
