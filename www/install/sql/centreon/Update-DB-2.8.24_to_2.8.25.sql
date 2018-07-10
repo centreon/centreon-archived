18b18650bdd6c416c548146bb3564a91d4936a4d
Kevin Duret <kduret@centreon.com>
2018-07-10 10:39:22 +0200

    chore(upgrade): upgrade version to 2.8.25

---
+++ www/install/sql/centreon/Update-DB-2.8.24_to_2.8.25.sql
@@ -0,0 +1,2 @@
+-- Change version of Centreon
+UPDATE `informations` SET `value` = '2.8.25' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.24' LIMIT 1;
