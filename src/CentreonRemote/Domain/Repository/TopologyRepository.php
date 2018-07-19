6f6d5523395192736c87d09294fd78edb7c6d92b
Victor Vassilev <victor.vassilev@stanga.net>
2018-07-19 12:37:01 +0300

    introduce new structure

---
+++ src/CentreonRemote/Domain/Repository/TopologyRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CentreonRemote\Domain\Repository;
+
+use Centreon\Domain\Repository\TopologyRepository as CentreonTopologyRepository;
+
+class TopologyRepository extends CentreonTopologyRepository
+{
+
+}
