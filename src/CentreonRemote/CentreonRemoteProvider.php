e3d0ead11338c18b8e7406da2c1ce00c72878aa5
Valentin Hristov <vhr@users.noreply.github.com>
2018-07-20 12:36:52 +0300

    feat(base): integrate DI in webservices (REST) API and CLAPI
    Resolve CP7M-202

--- src/CentreonRemote/CentreonRemoteProvider.php
+++ src/CentreonRemote/CentreonRemoteProvider.php
@@ -3,6 +3,8 @@ namespace CentreonRemote;
 
 use Pimple\ServiceProviderInterface;
 use Pimple\Container;
+use CentreonRemote\Application\Webservice;
+use CentreonRemote\Application\Clapi;
 
 class CentreonRemoteProvider implements ServiceProviderInterface
 {
@@ -12,8 +14,11 @@ class CentreonRemoteProvider implements ServiceProviderInterface
      * 
      * @param \Pimple\Container $pimple
      */
-    public function register(Container $pimple)
+    public function register(Container $pimple): void
     {
+        $pimple['centreon.webservice']->add(Webservice\CentreonRemoteServer::class);
+        $pimple['centreon.clapi']->add(Clapi\CentreonRemoteServer::class);
+
         // @todo register service here
     }
 }
