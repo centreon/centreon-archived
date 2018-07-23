3a37c5282306decbfc08b06b00e54381087e90cb
Valentin Hristov <vhr@users.noreply.github.com>
2018-07-23 14:33:15 +0300

    fix(remote): prevent loading of CentreonDB from WebService class
    Resolve CP7M-180

---
+++ src/Centreon/Infrastructure/Repository/ServiceEntityRepository.php
@@ -0,0 +1,28 @@
+<?php
+namespace Centreon\Infrastructure\Service;
+
+use ReflectionClass;
+use Psr\Container\ContainerInterface;
+use Centreon\Infrastructure\Service\CentreonClapiServiceInterface;
+use Centreon\Infrastructure\Service\Traits\ServiceContainerTrait;
+
+class CentreonClapiService implements ContainerInterface
+{
+
+    use ServiceContainerTrait;
+
+    public function add(string $object): void
+    {
+        $interface = CentreonClapiServiceInterface::class;
+        $hasInterface = (new ReflectionClass($object))
+            ->implementsInterface($interface)
+        ;
+
+        if ($hasInterface === false) {
+            throw new NotFoundException(sprintf('Object %s must implement %s', $object, $interface));
+        }
+
+        $name = strtolower($object::getName());
+        $this->objects[$name] = $object;
+    }
+}
