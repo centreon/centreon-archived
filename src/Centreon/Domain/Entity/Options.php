7ddf46f0d37d5e752f5bdb9cdc0e874e8ce87ecd
Valentin Hristov <vhr@users.noreply.github.com>
2018-07-23 17:21:01 +0300

    feat(remote): add ability in OptionsRepository to return array of Options entity
    Resolve CP7M-180

--- src/Centreon/Domain/Entity/Options.php
+++ src/Centreon/Domain/Entity/Options.php
@@ -7,4 +7,23 @@ class Options
     private $key;
     private $value;
 
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
 }
--- src/Centreon/Domain/Repository/OptionsRepository.php
+++ src/Centreon/Domain/Repository/OptionsRepository.php
@@ -2,15 +2,23 @@
 namespace Centreon\Domain\Repository;
 
 use Centreon\Infrastructure\CentreonLegacyDB\ServiceEntityRepository;
+use Centreon\Domain\Entity\Options;
+use PDO;
 
 class OptionsRepository extends ServiceEntityRepository
 {
 
+    /**
+     * Export options
+     * 
+     * @return \Centreon\Domain\Entity\Options[]
+     */
     public function export(): array
     {
         $sql = 'SELECT * FROM options';
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Options::class);
 
         $result = [];
 
