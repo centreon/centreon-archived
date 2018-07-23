cd8a5e7908fa48e0c38b5f5f40765bf1d9a2752b
Valentin Hristov <vhr@users.noreply.github.com>
2018-07-23 16:41:05 +0300

    feat(remote): add Options Entity and Repository classes
    Resolve CP7M-180

--- src/Centreon/Domain/Repository/OptionsRepository.php
+++ src/Centreon/Domain/Repository/OptionsRepository.php
@@ -1,9 +1,23 @@
 <?php
-
 namespace Centreon\Domain\Repository;
 
+use Centreon\Infrastructure\CentreonLegacyDB\ServiceEntityRepository;
 
-class TopologyRepository
+class OptionsRepository extends ServiceEntityRepository
 {
 
+    public function export(): array
+    {
+        $sql = 'SELECT * FROM options';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+
+        $result = [];
+
+        while ($row = $stmt->fetch()) {
+            $result[] = $row;
+        }
+
+        return $result;
+    }
 }
