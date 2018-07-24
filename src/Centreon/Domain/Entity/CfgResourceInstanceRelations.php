1d89b4944e3cbe9c8328670904965c4b3507fd65
Valentin Hristov <vhr@users.noreply.github.com>
2018-07-24 17:05:47 +0300

    feat(remote): add new Entity's classes
    Resolve CP7M-180

---
+++ src/Centreon/Domain/Entity/CfgResourceInstanceRelations.php
@@ -0,0 +1,40 @@
+<?php
+namespace Centreon\Domain\Entity;
+
+class CfgResourceInstanceRelations
+{
+
+    /**
+     * Relation with cfg_resource.id
+     * 
+     * @var int
+     */
+    private $resourceId;
+
+    /**
+     * Relation with nagios_server.id
+     * 
+     * @var int
+     */
+    private $instanceId;
+
+    public function setResourceId(int $resourceId): void
+    {
+        $this->resourceId = $resourceId;
+    }
+
+    public function getResourceId(): int
+    {
+        return $this->resourceId;
+    }
+
+    public function setInstanceId(int $instanceId): void
+    {
+        $this->instanceId = $instanceId;
+    }
+
+    public function getInstanceId(): int
+    {
+        return $this->instanceId;
+    }
+}
