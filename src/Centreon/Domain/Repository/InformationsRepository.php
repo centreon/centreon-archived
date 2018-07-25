d21860488932413878c4be6adf763ea10fedd0c4
Victor Vassilev <victor.vassilev@stanga.net>
2018-07-25 15:08:19 +0300

    initial services for limit action/notify master

---
+++ src/Centreon/Domain/Repository/InformationsRepository.php
@@ -0,0 +1,49 @@
+<?php
+namespace Centreon\Domain\Repository;
+
+use Centreon\Infrastructure\CentreonLegacyDB\ServiceEntityRepository;
+use Centreon\Domain\Entity\Informations;
+use PDO;
+
+class InformationsRepository extends ServiceEntityRepository
+{
+
+    /**
+     * Export options
+     * 
+     * @return \Centreon\Domain\Entity\Informations[]
+     */
+    public function getAll(): array
+    {
+        $sql = 'SELECT * FROM informations';
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Informations::class);
+
+        $result = [];
+
+        while ($row = $stmt->fetch()) {
+            $result[] = $row;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Find one by given key
+     * @param string $key
+     * @return Informations
+     */
+    public function getOneByKey($key): Informations
+    {
+        $sql = 'SELECT * FROM informations WHERE key = :key LIMIT 0,1';
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':key', $key, PDO::PARAM_STR);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_CLASS, Informations::class);
+        $result = $stmt->fetch();
+
+        return $result;
+    }
+
+}
--- src/CentreonRemote/Application/Clapi/CentreonRemoteServer.php
+++ src/CentreonRemote/Application/Clapi/CentreonRemoteServer.php
@@ -1,15 +1,25 @@
 <?php
+
 namespace CentreonRemote\Application\Clapi;
 
+use Centreon\Domain\Repository\TopologyRepository;
+use Centreon\Domain\Repository\OptionsRepository;
 use Pimple\Container;
 use Centreon\Infrastructure\Service\CentreonClapiServiceInterface;
+use ReflectionClass;
 
 class CentreonRemoteServer implements CentreonClapiServiceInterface
 {
 
+    /**
+     * @var Container
+     * todo: extract only services we need to avoid using whole container
+     */
+    private $di;
+
     public function __construct(Container $di)
     {
-        
+        $this->di = $di;
     }
 
     public static function getName() : string
@@ -23,5 +33,40 @@ class CentreonRemoteServer implements CentreonClapiServiceInterface
         echo "OK\n";
 
         return 200;
+    }
+
+    public function enableRemote($ip)
+    {
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            echo "Incorrect IP parameter, please pass `-v IP` of the master server\n"; return 200;
+        }
+
+        echo "Starting Centreon Remote enable: \n";
+
+        echo "\n Limiting Menu Access...";
+        $result = $this->getDi()['centreon.db-manager']->getRepository(TopologyRepository::class)->disableMenus();
+        echo ($result) ? 'Success' : 'Fail' . "\n";
+
+        echo "\n Limiting Actions...";
+        /**
+         * todo: add limiting actions
+         */
+        echo (false) ? 'Success' : 'Fail' . "\n";
+
+        echo "\n Notifying Master...";
+        $result = $this->getDi()['centreon.notifymaster']->pingMaster($ip);
+        echo (!empty($result['status']) && $result['status'] == 'success') ? 'Success' : 'Fail' . "\n";
+
+        echo "Centreon Remote enabling finished.";
+    }
+
+    public function disableRemote()
+    {
+        $this->getDi()['centreon.db-manager']->getRepository(TopologyRepository::class)->enableMenus();
+    }
+
+    public function getDi(): Container
+    {
+        return $this->di;
     }
 }
