40218db83f09f1e355fa29cce956e49250b1570c
v.radev <v.radev@live.com>
2018-08-06 12:12:00 +0300

    feat(remote): add remote data scaffold for tables

---
+++ src/CentreonRemote/todo/cfg_resource_instance_relations.php
@@ -0,0 +1,16 @@
+<?php
+
+// This table is used to link resources to a Centreon Engine.
+
+// TBD by cfg_resource.php
+
+// Each new Centreon Distant Poller must have those entries:
+// *************************** 1. row ***************************
+// resource_id: 1
+// instance_id: $ID (nagios_server.id)
+// *************************** 2. row ***************************
+// resource_id: 2
+// instance_id: $ID (nagios_server.id)
+// *************************** 3. row ***************************
+// resource_id: 3
+// instance_id: $ID (nagios_server.id)
