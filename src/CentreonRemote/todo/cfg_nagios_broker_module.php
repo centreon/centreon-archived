c85e78b72938a8e739a58b1dfda268949d38da8f
v.radev <v.radev@live.com>
2018-08-08 14:52:19 +0300

    feat(remote): add centreon remote data service separation

--- src/CentreonRemote/todo/cfg_nagios_broker_module.php
+++ src/CentreonRemote/todo/cfg_nagios_broker_module.php
@@ -1,22 +1,16 @@
 <?php
 
-// This table describes which C/C++ libraries will be launched by Centreon Engine on start.
-// Currently, there is only two possibilities:
-//  cbmod.so + configuration file
-//  externalcmd.so
+return function ($serverID, $pollerName) {
+    $pollerName = str_replace(' ', '_', $pollerName);
 
-// Notice: $pollerName is the name of poller without space (replaced by underscore character ‘_’).
-$pollerName = '';
-$data = [
-    [
-        'cfg_nagios_id' => '', // TODO relation id
-        'broker_module' => "/usr/lib64/nagios/cbmod.so /etc/centreon-broker/{$pollerName}-module.xml",
-    ],
-    [
-        'cfg_nagios_id' => '', // TODO relation id
-        'broker_module' => '/usr/lib64/centreon-engine/externalcmd.so',
-    ],
-];
-
-// Notice for later: Do not put the name of the configuration file
-// because this one is defined in another table for the Centreon Broker configuration.
+    return [
+        [
+            'cfg_nagios_id' => $serverID,
+            'broker_module' => "/usr/lib64/nagios/cbmod.so /etc/centreon-broker/{$pollerName}-module.xml",
+        ],
+        [
+            'cfg_nagios_id' => $serverID,
+            'broker_module' => '/usr/lib64/centreon-engine/externalcmd.so',
+        ],
+    ];
+};
