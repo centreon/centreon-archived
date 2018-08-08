def9e5f0e969567fcfcc3690b1f639aab5b42acd
v.radev <v.radev@live.com>
2018-08-08 14:52:19 +0300

    feat(remote): add centreon remote data service separation

--- src/CentreonRemote/todo/nagios_server.php
+++ src/CentreonRemote/todo/nagios_server.php
@@ -1,30 +1,26 @@
 <?php
 
-// This is a Centreon Poller/Remote
-
-// The only difference of configuration between a Centreon Central Server
-// and a Centreon Remote Server is the “Output IPv4” configuration
-// on “Central Broker” to forward all events received on Remote Server to the Centreon Central Server.
-
-return [
-    'name'                       => '', //TODO input
-    'localhost'                  => '0',
-    'is_default'                 => '0',
-    'ns_ip_address'              => '', //TODO input
-    'ns_activate'                => '1',
-    'ns_status'                  => '0',
-    'init_script'                => 'centengine',
-    'init_system'                => null,
-    'monitoring_engine'          => 'CENGINE',
-    'nagios_bin'                 => '/usr/sbin/centengine',
-    'nagiostats_bin'             => '/usr/sbin/centenginestats',
-    'nagios_perfdata'            => '/var/log/centreon-engine/service-perfdata',
-    'centreonbroker_cfg_path'    => '/etc/centreon-broker',
-    'centreonbroker_module_path' => '/usr/share/centreon/lib/centreon-broker',
-    'centreonconnector_path'     => '/usr/lib64/centreon-connector',
-    'ssh_port'                   => '22',
-    'ssh_private_key'            => null,
-    'init_script_centreontrapd'  => 'centreontrapd',
-    'snmp_trapd_path_conf'       => '/etc/snmp/centreon_traps/',
-    'centreonbroker_logs_path'   => '/var/log/centreon-broker/watchdog.log',
-];
+return function ($name, $ip) {
+    return [
+        'name'                       => $name,
+        'localhost'                  => '0',
+        'is_default'                 => '0',
+        'ns_ip_address'              => $ip,
+        'ns_activate'                => '1',
+        'ns_status'                  => '0',
+        'init_script'                => 'centengine',
+        'init_system'                => null,
+        'monitoring_engine'          => 'CENGINE',
+        'nagios_bin'                 => '/usr/sbin/centengine',
+        'nagiostats_bin'             => '/usr/sbin/centenginestats',
+        'nagios_perfdata'            => '/var/log/centreon-engine/service-perfdata',
+        'centreonbroker_cfg_path'    => '/etc/centreon-broker',
+        'centreonbroker_module_path' => '/usr/share/centreon/lib/centreon-broker',
+        'centreonconnector_path'     => '/usr/lib64/centreon-connector',
+        'ssh_port'                   => '22',
+        'ssh_private_key'            => null,
+        'init_script_centreontrapd'  => 'centreontrapd',
+        'snmp_trapd_path_conf'       => '/etc/snmp/centreon_traps/',
+        'centreonbroker_logs_path'   => '/var/log/centreon-broker/watchdog.log',
+    ];
+};
--- src/CentreonRemote/todo/service.php
+++ src/CentreonRemote/todo/service.php
@@ -28,67 +28,90 @@ class service
 
     public function insertRemoteConfiguration()
     {
-        $nagiosServerData = require_once 'nagios_server.php';
-        $nagiosServerData['name'] = $this->name;
-        $nagiosServerData['ns_ip_address'] = $this->ip;
-
-        $serverID = $this->db->insert('nagios_server', $nagiosServerData);
+        $serverID = $this->insertNagiosServer();
 
         if (!$serverID) {
             die(' ERROR at nagios_server ');//TODO
         }
 
-        $configNagiosData = require_once 'cfg_nagios.php';
-        $configNagiosData['nagios_name'] = $this->name;
-        $configNagiosData['nagios_server_id'] = $serverID;
-
-        $configID = $this->db->insert('cfg_nagios', $configNagiosData);
-
-        if (!$configID) {
+        if (!$this->insertConfigNagios($serverID)) {
             die(' ERROR at cfg_nagios ');//TODO
         }
 
-        $configNagiosBrokerData = require_once 'cfg_nagios_broker_module.php';
-        $configNagiosBrokerData[0]['cfg_nagios_id'] = $serverID;
-        $configNagiosBrokerData[1]['cfg_nagios_id'] = $serverID;
-
-        $configBrokerFirstID = $this->db->insert('cfg_nagios_broker_module', $configNagiosBrokerData[0]);
-        $configBrokerSecondID = $this->db->insert('cfg_nagios_broker_module', $configNagiosBrokerData[1]);
-
-        if (!$configBrokerFirstID || !$configBrokerSecondID) {
+        if ($this->insertConfigNagiosBroker($serverID)) {
             die(' ERROR at cfg_nagios_broker_module ');//TODO
         }
 
-        //TODO add some ids to data?
-        $configResourceData = require_once 'cfg_resource.php';
-        $configResourceID = $this->db->insert('cfg_resource', $configResourceData);
-
-        if (!$configResourceID) {
+        if (!$this->insertConfigResource()) {
             die(' ERROR at cfg_resource ');//TODO
         }
 
-        //TODO add some ids to data?
-        $configResourceRelationsData = require_once 'cfg_resource_instance_relations.php';
-        $configResourceInstanceID = $this->db->insert('cfg_resource_instance_relations', $configResourceRelationsData);
-
-        if (!$configResourceInstanceID) {
+        if (!$this->insertConfigResoureRelations()) {
             die(' ERROR at cfg_resource_instance_relations ');//TODO
         }
 
-        //TODO $pollerName
-        $configCentreonBrokerData = require_once 'cfg_centreonbroker.php';
-        $configCentreonBrokerID = $this->db->insert('cfg_centreonbroker', $configCentreonBrokerData);
-
-        if (!$configCentreonBrokerID) {
+        if (!$this->insertConfigCentreonBroker()) {
             die(' ERROR at cfg_centreonbroker ');//TODO
         }
 
-        //TODO some relation to the centreon broker id?
-        $configCentreonBrokerInfoData = require_once 'cfg_centreonbroker_info.php';
-        $configCentreonBrokerInfoID = $this->db->insert('cfg_centreonbroker_info', $configCentreonBrokerInfoData);
-
-        if (!$configCentreonBrokerInfoID) {
+        if (!$this->insertConfigCentreonBrokerInfo()) {
             die(' ERROR at cfg_centreonbroker_info ');//TODO
         }
+    }
+
+    private function insertNagiosServer()
+    {
+        $nagiosServerData = require_once 'nagios_server.php';
+
+        return $this->db->insert('nagios_server', $nagiosServerData($this->name, $this->ip));
+    }
+
+    private function insertConfigNagios($serverID)
+    {
+        $configNagiosData = require_once 'cfg_nagios.php';
+
+        return $this->db->insert('cfg_nagios', $configNagiosData($this->name, $serverID));
+    }
+
+    private function insertConfigNagiosBroker($serverID)
+    {
+        $configNagiosBrokerData = require_once 'cfg_nagios_broker_module.php';
+        $data = $configNagiosBrokerData($serverID, $this->name);
+
+        $configBrokerFirstID = $this->db->insert('cfg_nagios_broker_module', $data[0]);
+        $configBrokerSecondID = $this->db->insert('cfg_nagios_broker_module', $data[1]);
+
+        return $configBrokerFirstID && $configBrokerSecondID;
+    }
+
+    private function insertConfigResource()
+    {
+        //TODO add some ids to data?
+        $configResourceData = require_once 'cfg_resource.php';
+
+        return $this->db->insert('cfg_resource', $configResourceData);
+    }
+
+    private function insertConfigResoureRelations()
+    {
+        //TODO add some ids to data?
+        $configResourceRelationsData = require_once 'cfg_resource_instance_relations.php';
+
+        return $this->db->insert('cfg_resource_instance_relations', $configResourceRelationsData);
+    }
+
+    private function insertConfigCentreonBroker()
+    {
+        $configCentreonBrokerData = require_once 'cfg_centreonbroker.php';
+
+        return $this->db->insert('cfg_centreonbroker', $configCentreonBrokerData($this->name));
+    }
+
+    private function insertConfigCentreonBrokerInfo()
+    {
+        //TODO some relation to the centreon broker id?
+        $configCentreonBrokerInfoData = require_once 'cfg_centreonbroker_info.php';
+
+        return $this->db->insert('cfg_centreonbroker_info', $configCentreonBrokerInfoData);
     }
 }
