501e5b01df41baa82b519911f55f9dbd8ce97d72
v.radev <v.radev@live.com>
2018-08-08 14:52:19 +0300

    feat(remote): add centreon remote data service separation

--- src/CentreonRemote/todo/cfg_centreonbroker.php
+++ src/CentreonRemote/todo/cfg_centreonbroker.php
@@ -1,22 +1,20 @@
 <?php
 
-// To transfer collected data to the Centreon Central Server,
-// a Centreon distant Poller must have a Centreon, Broker module (cbmod.so) configuration.
+return function ($pollerName) {
+    $pollerName = str_replace(' ', '_', $pollerName);
 
-// Notice: $pollerName is the name of poller without space (replaced by underscore character ‘_’).
-$pollerName = '';
-
-return [
-    'config_name'            => "{$pollerName} module",
-    'config_filename'        => "{$pollerName}-module.xml",
-    'config_write_timestamp' => '1',
-    'config_write_thread_id' => '1',
-    'config_activate'        => '1',
-    'ns_nagios_server'       => '1',
-    'event_queue_max_size'   => '100000',
-    'command_file'           => '',
-    'cache_directory'        => '/var/lib/centreon-broker',
-    'stats_activate'         => '1',
-    'correlation_activate'   => '0',
-    'daemon'                 => '0',
-];
+    return [
+        'config_name'            => "{$pollerName} module",
+        'config_filename'        => "{$pollerName}-module.xml",
+        'config_write_timestamp' => '1',
+        'config_write_thread_id' => '1',
+        'config_activate'        => '1',
+        'ns_nagios_server'       => '1',
+        'event_queue_max_size'   => '100000',
+        'command_file'           => '',
+        'cache_directory'        => '/var/lib/centreon-broker',
+        'stats_activate'         => '1',
+        'correlation_activate'   => '0',
+        'daemon'                 => '0',
+    ];
+};
