7e8bf10d07bcd9b89aa2aca92976fdb6ddda4953
v.radev <v.radev@live.com>
2018-08-10 12:19:33 +0300

    feat(remote): add centreon remote service in container and load db adapter

--- src/Centreon/Infrastructure/CentreonLegacyDB/CentreonDBAdapter.php
+++ src/Centreon/Infrastructure/CentreonLegacyDB/CentreonDBAdapter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Centreon\Infrastructure\CentreonLegacyDB;
 
 use Centreon\Infrastructure\Service\Exception\NotFoundException;
@@ -8,10 +9,14 @@ use CentreonDB;
 class CentreonDBAdapter
 {
 
-    /**
-     * @var \CentreonDB
-     */
+    /** @var \CentreonDB */
     private $db;
+
+    private $count = 0;
+    private $error = false;
+    private $errorInfo = '';
+    private $query;
+    private $result;
 
     /**
      * Construct
@@ -25,7 +30,6 @@ class CentreonDBAdapter
 
     public function getRepository($repository): ServiceEntityRepository
     {
-
         $interface = ServiceEntityRepository::class;
         $ref = new ReflectionClass($repository);
         $hasInterface = $ref->isSubclassOf($interface);
@@ -35,6 +39,87 @@ class CentreonDBAdapter
         }
 
         $repositoryInstance = new $repository($this->db);
+
         return $repositoryInstance;
+    }
+
+    /**
+     * @param string $query
+     * @param array $params
+     *
+     * @return $this
+     */
+    public function query($query, $params = [])
+    {
+        $this->error = false;
+        $this->errorInfo = '';
+
+        $this->query = $this->db->prepare($query);
+
+        if (!$this->query){
+            throw new \Exception('Error at preparing the query.');
+        }
+
+        if (is_array($params) && !empty($params)){
+            $x = 1;
+
+            foreach ($params as $param) {
+                $this->query->bindValue($x, $param);
+                $x++;
+            }
+        }
+
+        if ($this->query->execute()){
+            $this->result = $this->query->fetchAll(\PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
+        } else {
+            $this->error = true;
+            $this->errorInfo = $this->query->errorInfo();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $table
+     * @param array $fields
+     *
+     * @return bool|int Last inserted ID
+     */
+    public function insert($table, array $fields)
+    {
+        $keys = array_keys($fields);
+        $values = '?' . str_repeat(", ?", count($fields) - 1);
+
+        $sql = "INSERT INTO {$table} (`". implode('`,`', $keys) ."`) VALUES ({$values})";
+
+        $this->query($sql, $fields);
+
+        return $this->passes() ? $this->db->lastInsertId() : false;
+    }
+
+    public function results()
+    {
+        return $this->result;
+    }
+
+    public function count()
+    {
+        return $this->count;
+    }
+
+    public function fails()
+    {
+        return $this->error;
+    }
+
+    public function passes()
+    {
+        return !$this->error;
+    }
+
+    public function errorInfo()
+    {
+        return $this->errorInfo;
     }
 }
