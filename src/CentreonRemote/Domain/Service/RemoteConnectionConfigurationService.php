f472da7f9644d26ae932b9f63610a0a9b82f9934
v.radev <v.radev@live.com>
2018-08-15 09:31:40 +0300

    refactor(remote): update remote connection sql data

--- src/CentreonRemote/Domain/Service/RemoteConnectionConfigurationService.php
+++ src/CentreonRemote/Domain/Service/RemoteConnectionConfigurationService.php
@@ -66,8 +66,6 @@ class RemoteConnectionConfigurationService
 
         $this->insertConfigCentreonBroker();
 
-        $this->insertConfigCentreonBrokerInfo();
-
         $this->getDbAdapter()->commit();
 
         return true;
@@ -100,43 +98,46 @@ class RemoteConnectionConfigurationService
 
     private function insertConfigResources($serverID)
     {
-        $configResourceData = $this->getResource('cfg_resource.php');
-        $configResourceRelationsData = $this->getResource('cfg_resource_instance_relations.php');
-        $configResourceData = $configResourceData();
+        $sql = 'SELECT `resource_id`, `resource_name` FROM `cfg_resource`';
+        $sql .= "WHERE `resource_name` IN('\$USER1$', '\$CENTREONPLUGINS$') ORDER BY `resource_id` ASC";
+        $this->getDbAdapter()->query($sql);
+        $results = $this->getDbAdapter()->results();
 
-        $resourceOneID = $this->insertWithAdapter('cfg_resource', $configResourceData[0]);
-        $resourceTwoID = $this->insertWithAdapter('cfg_resource', $configResourceData[1]);
-        $resourceThreeID = $this->insertWithAdapter('cfg_resource', $configResourceData[2]);
+        if (count($results) < 2) {
+            throw new \Exception('Resources records from `cfg_resource` could not be fetched.');
+        }
 
-        $relationData = [$resourceOneID, $resourceTwoID, $resourceThreeID];
-        $configResourceRelationsData = $configResourceRelationsData($relationData, $serverID);
+        if (
+            $results[0]->resource_name != '$USER1$' ||
+            $results[1]->resource_name != '$CENTREONPLUGINS$'
+        ) {
+            throw new \Exception('Resources records from `cfg_resource` are not as expected.');
+        }
 
-        $this->insertWithAdapter('cfg_resource_instance_relations', $configResourceRelationsData[0]);
-        $this->insertWithAdapter('cfg_resource_instance_relations', $configResourceRelationsData[1]);
-        $this->insertWithAdapter('cfg_resource_instance_relations', $configResourceRelationsData[2]);
+        $userResourceData = ['resource_id' => $results[0]->resource_id, 'instance_id' => $serverID];
+        $pluginResourceData = ['resource_id' => $results[1]->resource_id, 'instance_id' => $serverID];
 
-        return $resourceOneID && $resourceTwoID && $resourceThreeID;
+        $this->insertWithAdapter('cfg_resource_instance_relations', $userResourceData);
+        $this->insertWithAdapter('cfg_resource_instance_relations', $pluginResourceData);
+
+        return true;
     }
 
     private function insertConfigCentreonBroker()
     {
         $configCentreonBrokerData = $this->getResource('cfg_centreonbroker.php');
-
-        return $this->insertWithAdapter('cfg_centreonbroker', $configCentreonBrokerData($this->name));
-    }
-
-    private function insertConfigCentreonBrokerInfo()
-    {
-        //TODO some relation to the centreon broker id?
         $configCentreonBrokerInfoData = $this->getResource('cfg_centreonbroker_info.php');
 
-        foreach ($configCentreonBrokerInfoData() as $infoGroup) {
+        $configID = $this->insertWithAdapter('cfg_centreonbroker', $configCentreonBrokerData($this->name));
+
+        foreach ($configCentreonBrokerInfoData($configID) as $infoGroup) {
             foreach ($infoGroup as $row) {
+                $row['config_id'] = $configID;
                 $this->insertWithAdapter('cfg_centreonbroker_info', $row);
             }
         }
 
-        return true;
+        return $configID;
     }
 
     private function insertWithAdapter($table, array $data)
@@ -144,11 +145,6 @@ class RemoteConnectionConfigurationService
         try {
             $result = $this->getDbAdapter()->insert($table, $data);
         } catch(\Exception $e) {
-            $this->getDbAdapter()->rollBack();
-            throw new \Exception("Error inserting remote configuration. Rolling back. Table name: {$table}.");
-        }
-
-        if (!$result) {
             $this->getDbAdapter()->rollBack();
             throw new \Exception("Error inserting remote configuration. Rolling back. Table name: {$table}.");
         }
