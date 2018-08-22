6c5b244af632c7134657052fb14d9b63b99a6648
Adrien Morais <amorais@centreon.com>
2018-08-22 16:51:31 +0200

    chore(upgrade): bump version to 2.8.26

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.25');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.26');
 
 --
 -- Contenu de la table `contact`
