ee7a8829e5f87b7c9e8ad269fa0956cf8f2f474e
Laurent Pinsivy <lpinsivy@gmail.com>
2018-08-23 17:24:42 +0200

    Move poller statistics menu to administration (#6557)
    * enh(menu): change Poller Statistics menu to Administration on install
    * enh(menu): change Poller Statistics menu to Administration on upgrade to 18.9

--- www/install/sql/centreon/Update-DB-2.8.25_to_18.9.0.sql
+++ www/install/sql/centreon/Update-DB-2.8.25_to_18.9.0.sql
@@ -1,2 +1,20 @@
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '18.9.0' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '2.8.25' LIMIT 1;
+
+-- Move "Graphs" & "Broker Statistics" as "Server status" sub menu
+UPDATE topology SET topology_parent = '505' WHERE topology_page = '10205';
+UPDATE topology SET topology_page = '50501' WHERE topology_page = '10205' and topology_parent = '505';
+UPDATE topology SET topology_parent = '505' WHERE topology_page = '10201';
+UPDATE topology SET topology_page = '50502' WHERE topology_page = '10201' and topology_parent = '505';
+
+-- Remane "Graphs" menu to "Engine Statistics"
+UPDATE topology SET topology_name = 'Engine Statistics' WHERE topology_page = '50502';
+
+-- Rename "Server Status" to "Platform Status"
+UPDATE topology SET topology_name = 'Platform Status' WHERE topology_page = '505';
+
+-- Change default page of "Platform Status" menu to "Broker Statistics"
+UPDATE topology SET topology_url = './include/Administration/brokerPerformance/brokerPerformance.php' WHERE topology_page = '505' AND topology_name = 'Platform Status';
+
+-- Delete old entries
+DELETE FROM topology WHERE topology_page = '102';
