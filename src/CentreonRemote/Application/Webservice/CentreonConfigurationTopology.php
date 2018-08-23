07be3174799bc6bf9f8eba16aeca5a74da37dfd4
v.radev <v.radev@live.com>
2018-08-23 11:27:56 +0300

    feat(react): add api endpoint to check toplogy id

---
+++ src/CentreonRemote/Application/Webservice/CentreonConfigurationTopology.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace CentreonRemote\Application\Webservice;
+
+class CentreonConfigurationTopology extends CentreonWebServiceAbstract
+{
+
+    /**
+     * Name of web service object
+     *
+     * @return string
+     */
+    public static function getName(): string
+    {
+        return 'centreon_configuration_topology';
+    }
+
+    /**
+     * @SWG\Post(
+     *   path="/centreon/api/internal.php",
+     *   operationId="checkTopologyReactFlag",
+     *   @SWG\Parameter(
+     *       in="query",
+     *       name="object",
+     *       type="string",
+     *       description="the name of the API object class",
+     *       required=true,
+     *       enum="centreon_configuration_topology",
+     *   ),
+     *   @SWG\Parameter(
+     *       in="query",
+     *       name="action",
+     *       type="string",
+     *       description="the name of the action in the API class",
+     *       required=true,
+     *       enum="checkTopologyReactFlag",
+     *   ),
+     *   @SWG\Parameter(
+     *       in="formData",
+     *       name="topology_id",
+     *       description="the ID of the topology page",
+     *       required=true,
+     *       type="string",
+     *   ),
+     *   @SWG\Response(
+     *     response=200,
+     *     description="JSON with flag is_react for the topology"
+     *   )
+     * )
+     *
+     * Get if topology_id is using react
+     * @return string
+     * @throws \RestBadRequestException
+     */
+    public function postCheckTopologyReactFlag()
+    {
+        if (!isset($_POST['topology_id']) || !$_POST['topology_id']) {
+            throw new \RestBadRequestException('You need to send \'topology_id\' in the request.');
+        }
+        $topologyID = (int) $_POST['topology_id'];
+        $statement = $this->pearDB->prepare('SELECT `is_react` FROM `topology` WHERE `topology_id` = :id');
+        $statement->execute([':id' => $topologyID]);
+        $result = $statement->fetch();
+        if (!$result) {
+            throw new \RestBadRequestException('No topology found.');
+        }
+        return json_encode(['is_react' => (bool) $result['is_react']]);
+    }
+
+    /**
+     * Authorize to access to the action
+     *
+     * @param string $action The action name
+     * @param \CentreonUser $user The current user
+     * @param boolean $isInternal If the api is call in internal
+     *
+     * @return boolean If the user has access to the action
+     */
+    public function authorize($action, $user, $isInternal = false)
+    {
+        if (parent::authorize($action, $user, $isInternal)) {
+            return true;
+        }
+
+        return $user && $user->hasAccessRestApiConfiguration();
+    }
+}
