7740df08fc81b6bd39ec84d6b4c56d12f1e36062
wallforfry <wallforfry@gmail.com>
2018-08-30 13:34:12 +0200

    Add same behavior as SSO (Keycloak Only/Mixed) and black/whitelist of clients

--- www/include/core/login/login.php
+++ www/include/core/login/login.php
@@ -58,6 +58,9 @@ $release = $DBRESULT->fetchRow();
 $DBRESULT = $pearDB->query("SELECT `value` FROM `options` WHERE `key` = 'keycloak_enable' LIMIT 1");
 $keycloakEnabled = $DBRESULT->fetchRow()["value"];
 
+$DBRESULT = $pearDB->query("SELECT `value` FROM `options` WHERE `key` = 'keycloak_mode' LIMIT 1");
+$keycloakMode = $DBRESULT->fetchRow()["value"];
+
 /**
  * Defining Login Form
  */
@@ -127,6 +130,7 @@ $tpl->assign('loginMessages', $loginMessages);
 $tpl->assign('centreonVersion', 'v. '.$release['value']);
 $tpl->assign('currentDate', date("d/m/Y"));
 $tpl->assign('keycloakEnabled', $keycloakEnabled);
+$tpl->assign('keycloakMode', $keycloakMode);
 
 // Redirect User
 $redirect = filter_input(
