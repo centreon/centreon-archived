3bb33825d87104f28a845f0c7e98ff968caec021
Danijel Halupka <danijel@simple-task.com>
2018-08-30 16:15:08 +0200

    new

--- www/front_src/src/components/progressBar/index.js
+++ www/front_src/src/components/progressBar/index.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { history } from '../../store';
 
-const ProgressBar = ({links}) => {
-  return (
-    <div class="progress-bar">
-      <div class="progress-bar-wrapper">
-        <ul class="progress-bar-items">
-          {links
-            ? links.map(link => (
-                <li class="progress-bar-item" onClick={link.path ? link.path : null}>
+class ProgressBar extends Component {
+
+  goToPath = path => {
+    history.push(path);
+  };
+
+  render() {
+    const { links } = this.props;
+    return (
+      <div class="progress-bar">
+        <div class="progress-bar-wrapper">
+          <ul class="progress-bar-items">
+            {links
+              ? links.map(link => (
+                <li class="progress-bar-item" onClick={this.goToPath.bind(this, link.path)}>
                   <span class={'progress-bar-link ' + (link.active ? 'active' : '') + (link.prevActive ? ' prev' : '')}>
                     {link.number}
                   </span>
                 </li>
               ))
-            : null}
-        </ul>
+              : null}
+          </ul>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+const mapStateToProps = () => ({
+});
 
-export default ProgressBar;
+const mapDispatchToProps = {
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProgressBar);
--- www/front_src/src/route-components/pollerStep3/index.js
+++ www/front_src/src/route-components/pollerStep3/index.js
@@ -5,15 +5,10 @@ import routeMap from '../../route-maps';
 
 class PollerStepThreeRoute extends Component {
 
-  goToPath = path => {
-    const {history} = this.props;
-    history.push(path);
-  };
-
   links = [
-    {active: true, prevActive: true, number: 1, path: this.goToPath.bind(this, routeMap.serverConfigurationWizard)},
-    {active: true, prevActive: true, number: 2, path: this.goToPath.bind(this, routeMap.pollerStep1)},
-    {active: true, prevActive: true, number: 3, path: this.goToPath.bind(this, routeMap.pollerStep2)},
+    {active: true, prevActive: true, number: 1, path: routeMap.serverConfigurationWizard},
+    {active: true, prevActive: true, number: 2, path: routeMap.pollerStep1},
+    {active: true, prevActive: true, number: 3, path: routeMap.pollerStep2},
     {active: true, number: 4},
   ];
     
