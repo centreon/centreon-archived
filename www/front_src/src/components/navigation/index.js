053bf0791baed93598f145d350da7defa75e7cc6
Nikola Lovic <nikola.lovic@simple-task.com>
2018-09-17 17:30:38 +0200

    Sidebar scrollbar added

--- www/front_src/src/components/navigation/index.js
+++ www/front_src/src/components/navigation/index.js
@@ -106,144 +106,146 @@ class NavigationComponent extends Component {
     const pageId = this.props.location.search.split("=")[1];
     return (
       <nav class={"sidebar" + (active ? " active" : "")} id="sidebar">
-        <div class="sidebar-logo">
-          <a onClick={this.toggleNavigation.bind(this)}>
-            <img
-              class="sidebar-logo-image"
-              src={logo}
-              width="254"
-              height="57"
-              alt=""
-            />
-          </a>
-        </div>
-        <div class="sidebar-logo-mini">
-          <a onClick={this.toggleNavigation.bind(this)}>
-            <img
-              class="sidebar-logo-mini-image"
-              src={miniLogo}
-              width="23"
-              height="21"
-              alt=""
-            />
-          </a>
-        </div>
-        <ul class="menu menu-items list-unstyled components">
-          {menuItems.map((item, index) => {
-            return (
-              <li class={"menu-item" + (item.active ? " active" : "")}>
-                <a
-                  onClick={this.onSwitch.bind(this, index)}
-                  style={{ cursor: "pointer" }}
-                  class="menu-item-link dropdown-toggle"
-                >
-                  <span class={`iconmoon icon-${item.label.toLowerCase()}`}>
-                    <span class={"menu-item-name"}>{item.label}</span>
-                  </span>
-                </a>
-                <ul
-                  class="collapse collapsed-items list-unstyled"
-                  style={{ display: (item.toggled && !active) ? "block" : "none" }}
-                >
-                  {item.children
-                    ? Object.keys(item.children).map(menuKey => {
-                        let subItem = item.children[menuKey];
-                        return (
-                          <li
-                            class={
-                              "collapsed-item" +
-                              (subItem.collapsed ? " active" : "")
-                            }
-                          >
-                            {Object.keys(subItem.children).length > 0 ? (
-                              <a
-                                style={{ cursor: "pointer" }}
-                                onClick={this.collapseSubMenu.bind(
-                                  this,
-                                  menuKey,
-                                  index
-                                )}
-                                class="collapsed-level-item-link"
-                              >
-                                {subItem.label}
-                              </a>
-                            ) : (
-                              <Link
-                                className="collapsed-level-item-link img-none"
-                                to={routeMap.module + "?p=" + menuKey}
-                              >
-                                {subItem.label}
-                              </Link>
-                            )}
+        <div class="sidebar-inner">
+          <div class="sidebar-logo">
+            <a onClick={this.toggleNavigation.bind(this)}>
+              <img
+                class="sidebar-logo-image"
+                src={logo}
+                width="254"
+                height="57"
+                alt=""
+              />
+            </a>
+          </div>
+          <div class="sidebar-logo-mini">
+            <a onClick={this.toggleNavigation.bind(this)}>
+              <img
+                class="sidebar-logo-mini-image"
+                src={miniLogo}
+                width="23"
+                height="21"
+                alt=""
+              />
+            </a>
+          </div>
+          <ul class="menu menu-items list-unstyled components">
+            {menuItems.map((item, index) => {
+              return (
+                <li class={"menu-item" + (item.active ? " active" : "")}>
+                  <a
+                    onClick={this.onSwitch.bind(this, index)}
+                    style={{ cursor: "pointer" }}
+                    class="menu-item-link dropdown-toggle"
+                  >
+                    <span class={`iconmoon icon-${item.label.toLowerCase()}`}>
+                      <span class={"menu-item-name"}>{item.label}</span>
+                    </span>
+                  </a>
+                  <ul
+                    class="collapse collapsed-items list-unstyled"
+                    style={{ display: (item.toggled && !active) ? "block" : "none" }}
+                  >
+                    {item.children
+                      ? Object.keys(item.children).map(menuKey => {
+                          let subItem = item.children[menuKey];
+                          return (
+                            <li
+                              class={
+                                "collapsed-item" +
+                                (subItem.collapsed ? " active" : "")
+                              }
+                            >
+                              {Object.keys(subItem.children).length > 0 ? (
+                                <a
+                                  style={{ cursor: "pointer" }}
+                                  onClick={this.collapseSubMenu.bind(
+                                    this,
+                                    menuKey,
+                                    index
+                                  )}
+                                  class="collapsed-level-item-link"
+                                >
+                                  {subItem.label}
+                                </a>
+                              ) : (
+                                <Link
+                                  className="collapsed-level-item-link img-none"
+                                  to={routeMap.module + "?p=" + menuKey}
+                                >
+                                  {subItem.label}
+                                </Link>
+                              )}
 
-                            <ul class="collapse-level collapsed-level-items first-level list-unstyled">
-                              {Object.keys(subItem.children).length > 0
-                                ? Object.keys(subItem.children).map(
-                                    (key, idx) => (
-                                      <div>
-                                        <span class="collapsed-level-title">
-                                          {key}:{" "}
-                                        </span>
-                                        {Object.keys(subItem.children[key]).map(
-                                          (subKey, idx) => {
-                                            if (pageId == subKey) {
-                                              if (!initiallyCollapsed) {
-                                                this.collapseInitialSubMenu(
-                                                  menuKey,
-                                                  index
-                                                );
-                                                this.activateTopLevelMenu(
-                                                  index
-                                                );
-                                              }
-                                            }
-                                            return (
-                                              <li
-                                                class={
-                                                  "collapsed-level-item" +
-                                                  (pageId == subKey
-                                                    ? " active"
-                                                    : "")
-                                                }
-                                              >
-                                                <a
-                                                  onClick={this.goToPage.bind(
-                                                    this,
-                                                    routeMap.module +
-                                                      "?p=" +
-                                                      subKey,
+                              <ul class="collapse-level collapsed-level-items first-level list-unstyled">
+                                {Object.keys(subItem.children).length > 0
+                                  ? Object.keys(subItem.children).map(
+                                      (key, idx) => (
+                                        <div>
+                                          <span class="collapsed-level-title">
+                                            {key}:{" "}
+                                          </span>
+                                          {Object.keys(subItem.children[key]).map(
+                                            (subKey, idx) => {
+                                              if (pageId == subKey) {
+                                                if (!initiallyCollapsed) {
+                                                  this.collapseInitialSubMenu(
+                                                    menuKey,
                                                     index
-                                                  )}
-                                                  className="collapsed-level-item-link"
-                                                >
-                                                  {
-                                                    subItem.children[key][
-                                                      subKey
-                                                    ].label
+                                                  );
+                                                  this.activateTopLevelMenu(
+                                                    index
+                                                  );
+                                                }
+                                              }
+                                              return (
+                                                <li
+                                                  class={
+                                                    "collapsed-level-item" +
+                                                    (pageId == subKey
+                                                      ? " active"
+                                                      : "")
                                                   }
-                                                </a>
-                                              </li>
-                                            );
-                                          }
-                                        )}
-                                      </div>
+                                                >
+                                                  <a
+                                                    onClick={this.goToPage.bind(
+                                                      this,
+                                                      routeMap.module +
+                                                        "?p=" +
+                                                        subKey,
+                                                      index
+                                                    )}
+                                                    className="collapsed-level-item-link"
+                                                  >
+                                                    {
+                                                      subItem.children[key][
+                                                        subKey
+                                                      ].label
+                                                    }
+                                                  </a>
+                                                </li>
+                                              );
+                                            }
+                                          )}
+                                        </div>
+                                      )
                                     )
-                                  )
-                                : null}
-                            </ul>
-                          </li>
-                        );
-                      })
-                    : null}
-                </ul>
-              </li>
-            );
-          })}
-        </ul>
-        <span
-          class="toggle-sidebar"
-          onClick={this.toggleNavigation.bind(this)}
-        />
+                                  : null}
+                              </ul>
+                            </li>
+                          );
+                        })
+                      : null}
+                  </ul>
+                </li>
+              );
+            })}
+          </ul>
+          <span
+            class="toggle-sidebar"
+            onClick={this.toggleNavigation.bind(this)}
+          />
+        </div>
       </nav>
     );
   }
