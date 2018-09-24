b3f1e3931be72decc2decfadcd3a4637a3082c2c
victorvassilev <acedweb@gmail.com>
2018-09-24 18:40:12 +0300

    enh(ui): apply intervals on header refresh (#6638)
    * apply intervals on header refresh
    
    * PR feedback
    
    * fix indentation

--- www/front_src/src/components/header/index.js
+++ www/front_src/src/components/header/index.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import routeMap from "../../route-maps";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import {Link} from "react-router-dom";
+import {connect} from "react-redux";
 
 import "moment-timezone";
 import Moment from "moment";
@@ -50,45 +50,49 @@ class TopHeader extends Component {
   refreshInterval;
 
   setClock = () => {
-    this.clockService.get().then(({ data }) => {
+    this.clockService.get().then(({data}) => {
       this.setState({
         clockData: instantiateDate(data.timezone, data.locale, data.time)
       });
     });
   };
 
-  setRefreshInterval = () => {
-    this.refreshInterval = setInterval(this.setClock, 15000);
+  _updateHeaderData = () => {
+    this.pollerService.get().then(({data}) => {
+      this.setState({
+        pollerData: data
+      });
+    });
+    this.hostsService.get().then(({data}) => {
+      this.setState({
+        hostsData: data
+      });
+    });
+    this.servicesStatusService.get().then(({data}) => {
+      this.setState({
+        servicesStatusData: data
+      });
+    });
+    this.userService.get().then(({data}) => {
+      this.setState({
+        userData: data
+      });
+    });
   };
 
-
-
+  setRefreshInterval = () => {
+    this.refreshInterval = setInterval(() => {
+      this.setClock();
+      this._updateHeaderData();
+    }, 15000);
+  };
 
   componentWillUnmount = () => {
     clearInterval(this.refreshInterval);
   };
 
   UNSAFE_componentWillMount = () => {
-    this.pollerService.get().then(({ data }) => {
-      this.setState({
-        pollerData: data
-      });
-    });
-    this.hostsService.get().then(({ data }) => {
-      this.setState({
-        hostsData: data
-      });
-    });
-    this.servicesStatusService.get().then(({ data }) => {
-      this.setState({
-        servicesStatusData: data
-      });
-    });
-    this.userService.get().then(({ data }) => {
-      this.setState({
-        userData: data
-      });
-    });
+    this._updateHeaderData();
     this.setClock();
     this.setRefreshInterval();
   };
@@ -105,16 +109,17 @@ class TopHeader extends Component {
       <header class="header">
         <div class="header-icons">
           <div class="wrap wrap-left">
-            <PollerMenu data={pollerData} />
+            <PollerMenu data={pollerData}/>
           </div>
           <div class="wrap wrap-right">
-            <HostMenu data={hostsData} />
-            <ServiceStatusMenu data={servicesStatusData} />
-            <UserMenu data={userData} clockData={clockData} />
+            <HostMenu data={hostsData}/>
+            <ServiceStatusMenu data={servicesStatusData}/>
+            <UserMenu data={userData} clockData={clockData}/>
           </div>
         </div>
       </header>
     );
   };
 }
+
 export default connect(null, null)(TopHeader);
