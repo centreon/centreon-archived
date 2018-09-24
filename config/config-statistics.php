9fac2f6a30fa13810dae3d76414ede27d6a5ccb4
Loic Laurent <loic.lau@gmail.com>
2018-09-24 09:58:48 +0200

    clean data

--- config/config-statistics.php
+++ config/config-statistics.php
@@ -1,22 +1,3 @@
 <?php
-const STATS_PATH = '/tmp';
-const STATS_PREFIX = 'stats_';
-
-// Credentials
-const USERNAME = 'admin';
-const PASSWORD = 'centreon';
-
-// Sub route to authenticate
-const AUTH_URL = 'http://localhost/centreon/api/index.php?action=authenticate';
-
-// Route of the webservices
-const WS_ROUTE = 'http://localhost/centreon/api/index.php?object=centreon_statistics&action=';
-
-// webservices
-const INFOS_RESOURCE = 'platformInfo';
-const VERSIONNING_RESOURCE = 'version';
-const INFO_TIMEZONE = 'platformTimezone';
-const UUID_RESOURCE = 'CentreonUUID';
-
 // Url where the stats are sent.
 const CENTREON_STATS_URL = 'https://statistics.centreon.com';
