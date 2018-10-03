492475bead9f4a493e9d0119692fd432b41c8c30
Kevin Duret <duret.kevin@gmail.com>
2018-10-03 13:17:13 +0200

    fix(ui): add compatibility with ie11 (#6703)
    * fix(ui): add compatibility with ie11
    
    * update package-lock.json
    
    * User submenu IE fix
    
    * package.json sass watcher added
    
    * fix(ui): fix css of autologin button

--- www/front_src/webpack.config.js
+++ www/front_src/webpack.config.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = "production";
 var reactScriptsConfig = require("react-scripts/config/webpack.config.prod");
 module.exports = Object.assign({}, reactScriptsConfig, {
-  entry: __dirname + "/src/index.js",
+  entry: ["@babel/polyfill", __dirname + "/src/index.js"],
   output: Object.assign({}, reactScriptsConfig.output, {
     path: __dirname + "/../"
   })
