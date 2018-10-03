02deb0b836fbbee14c1fa8f9810284eb6742b75f
v.radev <v.radev@live.com>
2018-10-03 17:02:47 +0300

    fix(misc): remove object destructuring and fix centcore cmd call

--- www/front_src/src/route-components/pollerStep2/index.js
+++ www/front_src/src/route-components/pollerStep2/index.js
@@ -69,7 +69,7 @@ class PollerStepTwoRoute extends Component {
         <ProgressBar links={links} />
         <Form
           pollers={pollers}
-          initialValues={{...pollerData}}
+          initialValues={pollerData}
           onSubmit={this.handleSubmit.bind(this)}
         />
       </div>
