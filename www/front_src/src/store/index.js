2f097881c1a01a6ebc0a3214c67721de323fb443
Marko Kovac <marko.kovac@simple-task.com>
2018-10-04 13:59:38 +0200

    translations init

--- www/front_src/src/store/index.js
+++ www/front_src/src/store/index.js
@@ -1,6 +1,41 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { routerMiddleware } from "react-router-redux";
 import reducers from "../redux/reducers";
+import thunk from 'redux-thunk';
+import { loadTranslations, setLocale, syncTranslationWithStore } from 'react-redux-i18n';
+
+const translationsObject = {
+  en: {
+    application: {
+      title: 'Awesome app with i18n!',
+      hello: 'Hello, %{name}!'
+    },
+    date: {
+      long: 'MMMM Do, YYYY'
+    },
+    export: 'Export %{count} items',
+    export_0: 'Nothing to export',
+    export_1: 'Export %{count} item',
+    two_lines: 'Line 1<br />Line 2',
+    literal_two_lines: 'Line 1\
+Line 2'
+  },
+  nl: {
+    application: {
+      title: 'Toffe app met i18n!',
+      hello: 'Hallo, %{name}!'
+    },
+    date: {
+      long: 'D MMMM YYYY'
+    },
+    export: 'Exporteer %{count} dingen',
+    export_0: 'Niks te exporteren',
+    export_1: 'Exporteer %{count} ding',
+    two_lines: 'Regel 1<br />Regel 2',
+    literal_two_lines: 'Regel 1\
+Regel 2'
+  }
+};
 
 const createHistory =
   typeof document !== undefined
@@ -10,7 +45,10 @@ const createHistory =
 export const history = createHistory();
 
 const createAppStore = (options, initialState = {}) => {
-  const middlewares = [routerMiddleware(history)];
+  const middlewares = [
+    routerMiddleware(history), 
+    thunk,
+  ];
 
   const store = createStore(
     reducers,
@@ -20,6 +58,10 @@ const createAppStore = (options, initialState = {}) => {
       window.devToolsExtension ? window.devToolsExtension() : f => f
     )
   );
+
+  syncTranslationWithStore(store)
+  store.dispatch(loadTranslations(translationsObject));
+  store.dispatch(setLocale('en'));
 
   return store;
 };
