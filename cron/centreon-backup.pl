6f60867e821f043f56ca095f0c740af42e612fb6
Dimitar Kalinov <miteto@miteto.net>
2018-10-15 10:01:03 +0300

    fix(configuration) : add PHP configuration to cron, remove default php.ini
    - Add php.d folder to backup
    - Remove default php.ini from backup

--- cron/centreon-backup.pl
+++ cron/centreon-backup.pl
@@ -315,13 +315,6 @@ sub getPHPConfFile() {
 	my $result = `@PHP_BIN@ -r 'echo php_ini_loaded_file();'`;
 	push(@tab_php_ini, trim($result));
 
-	# Apache
-	if ( -e '/etc/opt/rh/rh-php71/php.ini') {
-	    push(@tab_php_ini, '/etc/opt/rh/rh-php71/php.ini');
-	} else {
-        print STDERR "Unable to get PHP configuration\n";
-    }
-
 	return @tab_php_ini;
 }
 
@@ -585,6 +578,11 @@ sub centralBackup() {
 			print STDERR "Unable to copy PHP configuration file\n";
 		}
 	}
+	# PHP configuration ini files
+	`cp -pr /etc/opt/rh/rh-php71/php.d/ $TEMP_CENTRAL_ETC_DIR/php/`;
+    if ($? ne 0) {
+        print STDERR "Unable to copy PHP configuration files\n";
+    }
 
 	#####################
 	# Get Centreon logs #
