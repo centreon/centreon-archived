85179926bb5ec1b9eacd93f302b30ec5a2f161d6
Laurent Calvet <lcalvet@centreon.com>
2018-10-17 10:57:20 +0200

    Add translation for button with label 'Add server with wizard' and create a php file with all React translations to use with messages.pot

---
+++ www/install/react_translation.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * Copyright 2005-2018 Centreon
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give Centreon
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of Centreon choice, provided that
+ * Centreon also meet, for each linked independent module, the terms and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+echo _("All pollers");
+echo _("Configure pollers");
+echo _("services");
+echo _("Critical services");
+echo _("Warning services");
+echo _("Unknown services");
+echo _("Ok services");
+echo _("Pending services");
+echo _("Edit profile");
+echo _("Copy autologin link");
+echo _("Server Configuration Wizard");
+echo _("Choose a server type");
+echo _("Add a Centreon Remote Server");
+echo _("Add a Centreon Poller");
+echo _("Remote Server Configuration");
+echo _("Create new Remote Server");
+echo _("Server Name");
+echo _("Server IP address");
+echo _("Database username");
+echo _("Database password");
+echo _("Centreon Central IP address, as seen by this server");
+echo _("Centreon Web Folder on Remote");
+echo _("Select a Remote Server");
+echo _("Select Pending Remote Links");
+echo _("Select IP");
+echo _("Select pollers to be attached to this new Remote Server");
+echo _("Finalizing Setup");
+echo _("Creating Export Task");
+echo _("Generating Export Files");
+echo _("Processing Remote Import/Configuration");
+echo _("The field is required");
+echo _("Not a valid IP address");
+echo _("Add server with wizard");
+echo _("Select linked Remote Server");
+echo _("as");
+echo _("Server Configuration");
+echo _("Attach poller to a server");
+echo _("Advanced: reverse Centreon Broker communication flow");
+echo _("%s is not valid.");
+echo _("You need to send '%s' in the request.");
+echo _("Some database poller updates are not active; check your configuration");
+echo _("Latency detected, check configuration for better optimization");
