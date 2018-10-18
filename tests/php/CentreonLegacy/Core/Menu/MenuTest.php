eff5b983142e8cbbb4473eb9959da0f11c9212f9
Marko Kovac <kovacmarko168@gmail.com>
2018-10-18 15:41:29 +0200

    main-menu has been changed to Main Menu (#6799)

--- tests/php/CentreonLegacy/Core/Menu/MenuTest.php
+++ tests/php/CentreonLegacy/Core/Menu/MenuTest.php
@@ -213,7 +213,7 @@ class MenuTest extends \PHPUnit_Framework_TestCase
                 "children" => array(
                     '_201' => array(
                         "children" => array(
-                            "main-menu" => array(
+                            "Main Menu" => array(
                                 '_20101' => array(
                                     "label" => "By host",
                                     "url" => "centreon/20101",
