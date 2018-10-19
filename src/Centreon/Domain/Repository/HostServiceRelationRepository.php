23471eb8da1d3e22dcd89371f799c82b5e317fee
Laurent Pinsivy <lpinsivy@gmail.com>
2018-10-19 16:23:09 +0200

    fix(remote): export BA hosts services relation (#6869)
    * fix(remote): export BA hosts services relation

--- src/Centreon/Domain/Repository/HostServiceRelationRepository.php
+++ src/Centreon/Domain/Repository/HostServiceRelationRepository.php
@@ -14,7 +14,7 @@ class HostServiceRelationRepository extends ServiceEntityRepository
      * @param int[] $pollerIds
      * @return array
      */
-    public function export(array $pollerIds): array
+    public function export(array $pollerIds,  array $ba = null): array
     {
         // prevent SQL exception
         if (!$pollerIds) {
@@ -24,6 +24,7 @@ class HostServiceRelationRepository extends ServiceEntityRepository
         $ids = join(',', $pollerIds);
 
         $sql = <<<SQL
+SELECT l.* FROM (
 SELECT
     t.*
 FROM host_service_relation AS t
@@ -33,6 +34,20 @@ INNER JOIN ns_host_relation AS hr ON hr.host_host_id = t.host_host_id OR hr.host
 WHERE hr.nagios_server_id IN ({$ids})
 GROUP BY t.hsr_id
 SQL;
+
+        // Extract BA services relations
+        if ($ba) {
+            foreach ($ba as $key => $val) {
+                $ba[$key] = "'ba_{$val}'";
+            }
+              $ba = implode(',', $ba);
+              $sql .= " UNION SELECT t2.*"
+                  . " FROM host_service_relation AS t2"
+                  . " INNER JOIN service s2 ON s2.service_id = t2.service_service_id"
+                  . " AND s2.service_description IN({$ba}) GROUP BY t2.service_service_id";
+        }
+        $sql .= ") AS l GROUP BY l.hsr_id";
+
         $stmt = $this->db->prepare($sql);
         $stmt->execute();
 
