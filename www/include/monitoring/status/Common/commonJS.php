1bf7ff5276bc1415d4e8eb8762bf555554d9bf0f
Laurent Calvet <lcalvet@centreon.com>
2018-12-12 14:11:39 +0100

    fix(monitoring service): Fix columns on list page (MON-3217)

--- www/include/monitoring/status/Common/commonJS.php
+++ www/include/monitoring/status/Common/commonJS.php
@@ -1001,7 +1001,11 @@ function monitoring_play()  {
     document.getElementById('JS_monitoring_pause_gray').style.display = 'none';
     document.getElementById('JS_monitoring_play_gray').style.display = 'block';
     _on = 1;
-    initM(<?php echo $tM?>,"<?php echo $sid?>","<?php echo $o?>");
+    // Allows to use the new status when click on the play button
+    if (typeof(_o) == "undefined") {
+        _o = "<?= $o ?>";
+    }
+    initM(<?php echo $tM?>, "<?php echo $sid?>", _o)
 }
 
 function monitoring_pause() {
