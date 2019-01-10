7ea32b775ad7c2da30767e4af79c88e2788f3648
Danijel Chalupka <danijel@simple-task.com>
2019-01-10 23:53:16 +0100

    top filters implemented into extensions page

--- www/front_src/src/route-components/extensions/index.js
+++ www/front_src/src/route-components/extensions/index.js
@@ -6,47 +6,71 @@ class ExtensionsRoute extends Component {
     
     return (
       <div>
-        <div className="container container-gray">
-          <Centreon.Wrapper>
-            <div className="container__row">
-              <div className="container__col-md-3 container__col-xs-12">
-                <Centreon.SearchLive label="Search:"/>
-              </div>
-              <div className="container__col-md-9 container__col-xs-12">
-                <div className="container__row">
-                  <div className="container__col-sm-6 container__col-xs-12">
-                    <div className="container__row">
-                      <Centreon.Switcher
-                        customClass="container__col-md-4 container__col-xs-4"
-                        switcherTitle="Status:"
-                        switcherStatus="Not installed"/>
-                      <Centreon.Switcher
-                        customClass="container__col-md-4 container__col-xs-4"
-                        switcherStatus="Installed"/>
-                      <Centreon.Switcher
-                        customClass="container__col-md-4 container__col-xs-4"
-                        switcherStatus="Update"/>
-                    </div>
-                  </div>
-                  <div className="container__col-sm-6 container__col-xs-12">
-                    <div className="container__row">
-                      <Centreon.Switcher
-                        customClass="container__col-sm-3 container__col-xs-4"
-                        switcherTitle="Type:"
-                        switcherStatus="Module"/>
-                      <Centreon.Switcher
-                        customClass="container__col-sm-3 container__col-xs-4"
-                        switcherStatus="Widget"/>
-                      <div className="container__col-sm-6 container__col-xs-4 center-vertical mt-1">
-                        <Centreon.Button label={"Clear Filters"} buttonType="bordered" color="black"/>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Centreon.Wrapper>
-        </div>
+        <Centreon.TopFilters
+            fullText={{
+                label: "Search:",
+                onChange: (a) => {
+                    console.log(a)
+                }
+            }}
+            switchers={[
+                [
+                    {
+                        customClass: "container__col-md-4 container__col-xs-4",
+                        switcherTitle: "Status:",
+                        switcherStatus: "Not installed",
+                        defaultValue:false,
+                        onChange:(value)=>{
+                            console.log(value)
+                        }
+                    },
+                    {
+                        customClass: "container__col-md-4 container__col-xs-4",
+                        switcherStatus: "Installed",
+                        defaultValue:false,
+                        onChange:(value)=>{
+                            console.log(value)
+                        }
+                    },
+                    {
+                        customClass: "container__col-md-4 container__col-xs-4",
+                        switcherStatus: "Update",
+                        defaultValue:false,
+                        onChange:(value)=>{
+                            console.log(value)
+                        }
+                    }
+                ],
+                [
+                    {
+                        customClass: "container__col-sm-3 container__col-xs-4",
+                        switcherTitle: "Type:",
+                        switcherStatus: "Module",
+                        defaultValue:false,
+                        onChange:(value)=>{
+                            console.log(value)
+                        }
+                    },
+                    {
+                        customClass: "container__col-sm-3 container__col-xs-4",
+                        switcherStatus: "Update",
+                        defaultValue:false,
+                        onChange:(value)=>{
+                            console.log(value)
+                        }
+                    },
+                    {
+                        button:true,
+                        label:"Clear Filters",
+                        color:"black",
+                        buttonType:"bordered",
+                        onClick: ()=>{
+                            console.log('Clear filters clicked')
+                        }
+                    }
+                ]
+            ]}
+        />
         <Centreon.Wrapper>
           <Centreon.Button label={"Update all"} buttonType="regular" customClass="mr-2" color="orange"/>
           <Centreon.Button label={"Install all"} buttonType="regular" customClass="mr-2" color="green"/>
