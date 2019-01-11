0a126b4760c9f8554f5a5922a86dda25e9aaf47f
Danijel Chalupka <danijel@simple-task.com>
2019-01-11 14:48:19 +0100

    extensions holders added

--- www/front_src/src/route-components/extensions/index.js
+++ www/front_src/src/route-components/extensions/index.js
@@ -1,198 +1,143 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import * as Centreon from '@centreon/react-components'
+import { connect } from 'react-redux';
 
 class ExtensionsRoute extends Component {
-  render() {
-    
+
+  state = {
+    widgetsActive: true,
+    modulesActive: true,
+    filters: {
+      search: null,
+
+    }
+  }
+
+  componentDidMount = () => {
+    this.getData();
+  }
+
+  onChange = (value, key) => {
+    const { filters } = this.state;
+    console.log(value, key)
+    this.setState({
+      filters: {
+        ...filters,
+        [key]: value
+      }
+    })
+  }
+
+  clearFilters = () => {
+    console.log('here')
+  }
+
+  getData = () => {
+    const { getAxiosData } = this.props;
+    getAxiosData({ url: `./api/internal.php?object=centreon_module&action=list`, propKey: 'extensions' })
+  }
+
+  render = () => {
+
+    const { remoteData } = this.props;
+    const { modulesActive, widgetsActive } = this.state;
+
+
     return (
       <div>
         <Centreon.TopFilters
-            fullText={{
-                label: "Search:",
-                onChange: (a) => {
-                    console.log(a)
-                }
-            }}
-            switchers={[
-                [
-                    {
-                        customClass: "container__col-md-4 container__col-xs-4",
-                        switcherTitle: "Status:",
-                        switcherStatus: "Not installed",
-                        defaultValue:false,
-                        onChange:(value)=>{
-                            console.log(value)
-                        }
-                    },
-                    {
-                        customClass: "container__col-md-4 container__col-xs-4",
-                        switcherStatus: "Installed",
-                        defaultValue:false,
-                        onChange:(value)=>{
-                            console.log(value)
-                        }
-                    },
-                    {
-                        customClass: "container__col-md-4 container__col-xs-4",
-                        switcherStatus: "Update",
-                        defaultValue:false,
-                        onChange:(value)=>{
-                            console.log(value)
-                        }
-                    }
-                ],
-                [
-                    {
-                        customClass: "container__col-sm-3 container__col-xs-4",
-                        switcherTitle: "Type:",
-                        switcherStatus: "Module",
-                        defaultValue:false,
-                        onChange:(value)=>{
-                            console.log(value)
-                        }
-                    },
-                    {
-                        customClass: "container__col-sm-3 container__col-xs-4",
-                        switcherStatus: "Update",
-                        defaultValue:false,
-                        onChange:(value)=>{
-                            console.log(value)
-                        }
-                    },
-                    {
-                        button:true,
-                        label:"Clear Filters",
-                        color:"black",
-                        buttonType:"bordered",
-                        onClick: ()=>{
-                            console.log('Clear filters clicked')
-                        }
-                    }
-                ]
-            ]}
+          fullText={{
+            label: "Search:",
+            filterKey: 'search'
+          }}
+          onChange={this.onChange.bind(this)}
+          switchers={[
+            [
+              {
+                customClass: "container__col-md-4 container__col-xs-4",
+                switcherTitle: "Status:",
+                switcherStatus: "Not installed",
+                defaultValue: false,
+                filterKey: 'not_installed'
+              },
+              {
+                customClass: "container__col-md-4 container__col-xs-4",
+                switcherStatus: "Installed",
+                defaultValue: false,
+                filterKey: 'installed'
+              },
+              {
+                customClass: "container__col-md-4 container__col-xs-4",
+                switcherStatus: "Update",
+                defaultValue: false,
+                filterKey: 'updated'
+              }
+            ],
+            [
+              {
+                customClass: "container__col-sm-3 container__col-xs-4",
+                switcherTitle: "Type:",
+                switcherStatus: "Module",
+                defaultValue: false,
+                filterKey: 'type_module'
+              },
+              {
+                customClass: "container__col-sm-3 container__col-xs-4",
+                switcherStatus: "Widget",
+                defaultValue: false,
+                filterKey: 'type_widget'
+              },
+              {
+                button: true,
+                label: "Clear Filters",
+                color: "black",
+                buttonType: "bordered",
+                onClick: this.clearFilters.bind(this)
+              }
+            ]
+          ]}
         />
         <Centreon.Wrapper>
-          <Centreon.Button label={"Update all"} buttonType="regular" customClass="mr-2" color="orange"/>
-          <Centreon.Button label={"Install all"} buttonType="regular" customClass="mr-2" color="green"/>
-          <Centreon.Button label={"Upload licence"} buttonType="regular" color="blue"/>
+          <Centreon.Button label={"Update all"} buttonType="regular" customClass="mr-2" color="orange" />
+          <Centreon.Button label={"Install all"} buttonType="regular" customClass="mr-2" color="green" />
+          <Centreon.Button label={"Upload licence"} buttonType="regular" color="blue" />
         </Centreon.Wrapper>
-        <Centreon.Wrapper>
-          <Centreon.HorizontalLineContent hrTitle="Modules"/>
-          <Centreon.Card>
-            <div className="container__row">
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem
-                  itemBorderColor="orange"
-                  itemFooterColor="red"
-                  itemFooterLabel="Licence expire 12/08/2018">
-                  <Centreon.IconInfo iconName="state"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Engine-status"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button
-                    buttonType="regular"
-                    color="orange"
-                    label="Available 3.1.5"
-                    iconActionType="update"/>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem
-                  itemBorderColor="green"
-                  itemFooterColor="orange"
-                  itemFooterLabel="Licence expire 12/08/2018">
-                  <Centreon.IconInfo iconName="state"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Engine-status"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="bordered" color="blue" label="Available 3.1.5"/>
-                  <Centreon.ButtonAction buttonActionType="delete" buttonIconType="delete"/>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem itemBorderColor="gray">
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Engine-status"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="regular" color="green"  label="Available 3.1.5"><Centreon.IconContent iconContentType="add"/></Centreon.Button>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem itemBorderColor="gray">
-                  <Centreon.IconInfo iconName="state"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Engine-status"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="regular" color="green"  label="Available 3.1.5"><Centreon.IconContent iconContentType="add"/></Centreon.Button>
-                </Centreon.CardItem>
-              </div>
-            </div>
-          </Centreon.Card>
-        </Centreon.Wrapper>
-        <Centreon.Wrapper>
-          <Centreon.HorizontalLineContent hrTitle="Widgets"/>
-          <Centreon.Card>
-            <div className="container__row">
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem
-                  itemBorderColor="orange"
-                  itemFooterColor="red"
-                  itemFooterLabel="Licence expire 12/08/2018">
-                  <Centreon.IconInfo iconName="puzzle"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Plugin pack manager"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button
-                    buttonType="regular"
-                    color="orange"
-                    label="Available 3.1.5"
-                    iconActionType="update"/>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem
-                  itemBorderColor="green"
-                  itemFooterColor="orange"
-                  itemFooterLabel="Licence expire 12/08/2018">
-                  <Centreon.IconInfo iconName="puzzle"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Plugin pack manager"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="bordered" color="blue" label="Available 3.1.5"/>
-                  <Centreon.ButtonAction buttonActionType="delete" buttonIconType="delete"/>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem itemBorderColor="gray">
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Plugin pack manager"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="regular" color="green"  label="Available 3.1.5"><Centreon.IconContent iconContentType="add"/></Centreon.Button>
-                </Centreon.CardItem>
-              </div>
-              <div className="container__col-md-3 container__col-sm-6 container__col-xs-12">
-                <Centreon.CardItem itemBorderColor="gray">
-                  <Centreon.IconInfo iconName="puzzle"/>
-                  <div className="custom-title-heading">
-                    <Centreon.Title icon="object" label="Engine-status"/>
-                    <Centreon.Subtitle label="by Centreon"/>
-                  </div>
-                  <Centreon.Button buttonType="regular" color="green"  label="Available 3.1.5"><Centreon.IconContent iconContentType="add"/></Centreon.Button>
-                </Centreon.CardItem>
-              </div>
-            </div>
-          </Centreon.Card>
-        </Centreon.Wrapper>
+        {
+          remoteData.extensions ? (
+            <React.Fragment>
+              {
+                remoteData.extensions.result.module && modulesActive ? (
+                  <Centreon.ExtensionsHolder title="Modules" entities={remoteData.extensions.result.module.entities} />
+                ) : null
+              }
+              {
+                remoteData.extensions.result.widget && widgetsActive ? (
+                  <Centreon.ExtensionsHolder title="Widgets" entities={remoteData.extensions.result.widget.entities} />
+                ) : null
+              }
+            </React.Fragment>
+          ) : null
+        }
+
       </div>
     )
   }
 }
 
-export default ExtensionsRoute;
+
+const mapStateToProps = ({ remoteData }) => ({
+  remoteData
+})
+
+
+const mapDispatchToProps = {
+  getAxiosData: (data) => {
+    return {
+      type: '@axios/GET_DATA',
+      ...data
+    }
+  }
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExtensionsRoute);
