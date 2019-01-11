5d6a1aa19f19b453d3377b3227d34dcb3abb4411
Valentin Hristov <vhr@users.noreply.github.com>
2019-01-11 10:16:09 +0200

    feat(API): add copyright header
    Resolve MON-3235

--- src/CentreonModule/Tests/Application/Webservice/CentreonModuleWebserviceTest.php
+++ src/CentreonModule/Tests/Application/Webservice/CentreonModuleWebserviceTest.php
@@ -1,4 +1,38 @@
 <?php
+/*
+ * Copyright 2005-2019 Centreon
+ * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * GPL Licence 2.0.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation ; either version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Linking this program statically or dynamically with other modules is making a
+ * combined work based on this program. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this program give Centreon
+ * permission to link this program with independent modules to produce an executable,
+ * regardless of the license terms of these independent modules, and to copy and
+ * distribute the resulting executable under terms of Centreon choice, provided that
+ * Centreon also meet, for each linked independent module, the terms  and conditions
+ * of the license of that module. An independent module is a module which is not
+ * derived from this program. If you modify this program, you may extend this
+ * exception to your version of the program, but you are not obliged to do so. If you
+ * do not wish to do so, delete this exception statement from your version.
+ *
+ * For more information : contact@centreon.com
+ *
+ *
+ */
 
 namespace CentreonModule\Tests\Application\Webservice;
 
@@ -23,28 +57,28 @@ class CentreonModuleWebserviceTest extends TestCase
         $container['centreon.module']
             ->method('getList')
             ->will($this->returnCallback(function () {
-                $funcArgs = func_get_args();
+                    $funcArgs = func_get_args();
 
-                // prepare filters
-                $funcArgs[0] = $funcArgs[0] === null ? '-' : $funcArgs[0];
-                $funcArgs[1] = $funcArgs[1] === true ? '1' : ($funcArgs[1] !== false ? '-' : '0');
-                $funcArgs[2] = $funcArgs[2] === true ? '1' : ($funcArgs[2] !== false ? '-' : '0');
-                $funcArgs[3] = $funcArgs[3] ? implode('|', $funcArgs[3]) : '-';
-                $name = implode(',', $funcArgs);
+                    // prepare filters
+                    $funcArgs[0] = $funcArgs[0] === null ? '-' : $funcArgs[0];
+                    $funcArgs[1] = $funcArgs[1] === true ? '1' : ($funcArgs[1] !== false ? '-' : '0');
+                    $funcArgs[2] = $funcArgs[2] === true ? '1' : ($funcArgs[2] !== false ? '-' : '0');
+                    $funcArgs[3] = $funcArgs[3] ? implode('|', $funcArgs[3]) : '-';
+                    $name = implode(',', $funcArgs);
 
-                $module = new Module;
-                $module->setId(ModuleSourceTest::$moduleName);
-                $module->setName($name);
-                $module->setAuthor('');
-                $module->setVersion('');
-                $module->setType(ModuleSource::TYPE);
+                    $module = new Module;
+                    $module->setId(ModuleSourceTest::$moduleName);
+                    $module->setName($name);
+                    $module->setAuthor('');
+                    $module->setVersion('');
+                    $module->setType(ModuleSource::TYPE);
 
-                return [
-                    ModuleSource::TYPE => [
-                        $module,
-                    ],
-                ];
-            }))
+                    return [
+                        ModuleSource::TYPE => [
+                            $module,
+                        ],
+                    ];
+                }))
         ;
 
         $this->webservice = $this->createPartialMock(CentreonModuleWebservice::class, [
@@ -64,8 +98,8 @@ class CentreonModuleWebserviceTest extends TestCase
         $this->webservice
             ->method('query')
             ->will($this->returnCallback(function() use (&$filters) {
-                return $filters;
-            }))
+                    return $filters;
+                }))
         ;
 
         $executeTest = function($controlJsonFile) {
@@ -121,8 +155,8 @@ class CentreonModuleWebserviceTest extends TestCase
             $user
                 ->method('hasAccessRestApiConfiguration')
                 ->will($this->returnCallback(function () {
-                    return true;
-                }))
+                        return true;
+                    }))
             ;
 
             $result = $this->webservice->authorize(null, $user);
