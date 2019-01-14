b0655f205b180faf8ba428369332d78ac0b7d9d7
Valentin Hristov <vhr@users.noreply.github.com>
2019-01-14 17:43:31 +0200

    feat(API): code style
    Resolve MON-3238

--- src/CentreonModule/Infrastructure/Service/CentreonModuleService.php
+++ src/CentreonModule/Infrastructure/Service/CentreonModuleService.php
@@ -58,8 +58,12 @@ class CentreonModuleService
         $this->initSources($services);
     }
 
-    public function getList(string $search = null, bool $installed = null, bool $updated = null, array $typeList = null) : array
-    {
+    public function getList(
+        string $search = null,
+        bool $installed = null,
+        bool $updated = null,
+        array $typeList = null
+    ) : array {
         $result = [];
 
         if ($typeList !== null && $typeList) {
