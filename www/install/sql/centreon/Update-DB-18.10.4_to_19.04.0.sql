dd54976f81c30d5e1d4d2488f8b2f2cd5eda0e60
Victor Vassilev <victor.vassilev@stanga.net>
2019-01-16 14:33:21 +0200

    update component index path

--- www/install/sql/centreon/Update-DB-18.10.4_to_19.04.0.sql
+++ www/install/sql/centreon/Update-DB-18.10.4_to_19.04.0.sql
@@ -2,4 +2,4 @@
 UPDATE `informations` SET `value` = '19.04.0' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.4' LIMIT 1;
 
 -- Add new Extensions Page entry
-INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_group`) VALUES ('Manager', '/extensions-manager/index.html', '1', '1', '507', '50709', 1);
+INSERT INTO `topology` (`topology_name`, `topology_url`, `readonly`, `is_react`, `topology_parent`, `topology_page`, `topology_group`) VALUES ('Manager', ' ./include/react-compiled/Manager/index.html', '1', '1', '507', '50709', 1);
