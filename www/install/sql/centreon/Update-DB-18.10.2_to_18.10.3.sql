9c3f90ea000516b9db9638cc3e7eaaf92ec579d4
Stéphane C.979 <34628915+sc979@users.noreply.github.com>
2019-01-16 08:52:39 +0000

    Mon 3281 set default language to browser (#7046)
    * fix(DP): avoid datepicker error when language is set to null
    
    * fix(lang): replace default language value from 'NULL' to 'browser'
    
    * enh(DP): supporting more localizations

--- www/install/sql/centreon/Update-DB-18.10.2_to_18.10.3.sql
+++ www/install/sql/centreon/Update-DB-18.10.2_to_18.10.3.sql
@@ -3,3 +3,7 @@ UPDATE `informations` SET `value` = '18.10.3' WHERE CONVERT( `informations`.`key
 
 -- Remove old progressbar lib
 DELETE FROM topology_JS WHERE PathName_js LIKE '%aculous%';
+
+-- Changing contact_lang default value to 'browser' and each of its value from NULL to 'browser'
+ALTER TABLE contact MODIFY `contact_lang` varchar(255) DEFAULT 'browser';
+UPDATE contact SET `contact_lang` = 'browser' WHERE `contact_lang` IS NULL;
