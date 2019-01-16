c989fbfa21b6f3457c9f1e1f8618e56f10936649
Dimitar Kalinov <miteto@miteto.net>
2019-01-16 10:55:01 +0200

    fix(CLAPI): check for "Reach API *" fields in user instead of "Reach … (#7117)
    * fix(CLAPI): check for "Reach API *" fields in user instead of "Reach Centreon Front-end" when using CLAPI
    
    - remove check for "contact_oreon" field
    - remove check for "contact_admin" field
    - add check whether at least on of reach_api/reach_api_rt field is active
    - fix warning in user form
    
    * fix(typo): do not use fetchRow() twice
    
    * fix(clapi): change check for permissions from reach_api* fields to contact_admin
    
    * fix(enh): move closeCursor() outside of "if" condition

--- www/class/centreon-clapi/centreonAPI.class.php
+++ www/class/centreon-clapi/centreonAPI.class.php
@@ -517,41 +517,41 @@ class CentreonAPI
         $DBRESULT = $this->DB->query("SELECT *
                  FROM contact
                  WHERE contact_alias = '" . $this->login . "'
-                 AND contact_activate = '1'
-                 AND contact_oreon = '1'");
+                 AND contact_activate = '1'");
+
         if ($DBRESULT->rowCount()) {
             $row = $DBRESULT->fetchRow();
 
-            if ($row['contact_admin'] == 1) {
-                $algo = $this->dependencyInjector['utils']->detectPassPattern($row['contact_passwd']);
-                if (!$algo) {
-                    if ($useSha1) {
-                        $row['contact_passwd'] = 'sha1__' . $row['contact_passwd'];
-                    } else {
-                        $row['contact_passwd'] = 'md5__' . $row['contact_passwd'];
-                    }
+            if ($row['contact_admin'] == 0) {
+                print "You don't have permissions for CLAPI.\n";
+                exit(1);
+            }
+
+            $algo = $this->dependencyInjector['utils']->detectPassPattern($row['contact_passwd']);
+            if (!$algo) {
+                if ($useSha1) {
+                    $row['contact_passwd'] = 'sha1__' . $row['contact_passwd'];
+                } else {
+                    $row['contact_passwd'] = 'md5__' . $row['contact_passwd'];
                 }
-                if ($row['contact_passwd'] == $pass) {
+            }
+            if ($row['contact_passwd'] == $pass) {
+                \CentreonClapi\CentreonUtils::setUserId($row['contact_id']);
+                return 1;
+            } elseif ($row['contact_auth_type'] == 'ldap') {
+                $CentreonLog = new \CentreonUserLog(-1, $this->DB);
+                $centreonAuth = new \CentreonAuthLDAP(
+                    $this->DB,
+                    $CentreonLog,
+                    $this->login,
+                    $this->password,
+                    $row,
+                    $row['ar_id']
+                );
+                if ($centreonAuth->checkPassword() == 1) {
                     \CentreonClapi\CentreonUtils::setUserId($row['contact_id']);
                     return 1;
-                } elseif ($row['contact_auth_type'] == 'ldap') {
-                    $CentreonLog = new \CentreonUserLog(-1, $this->DB);
-                    $centreonAuth = new \CentreonAuthLDAP(
-                        $this->DB,
-                        $CentreonLog,
-                        $this->login,
-                        $this->password,
-                        $row,
-                        $row['ar_id']
-                    );
-                    if ($centreonAuth->checkPassword() == 1) {
-                        \CentreonClapi\CentreonUtils::setUserId($row['contact_id']);
-                        return 1;
-                    }
                 }
-            } else {
-                print "Centreon CLAPI is for admin users only.\n";
-                exit(1);
             }
         }
         print "Invalid credentials.\n";
