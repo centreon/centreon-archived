6a61ace8fbc5c33164106b7ef4224dbb62fd2e3a
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-01-28 15:56:41 +0100

    chore(install): update to 18.10.4.

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '18.10.3');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '18.10.4');
 
 --
 -- Contenu de la table `contact`
