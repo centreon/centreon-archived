61a43d7d4d7e72ca1bb00c3987068f83e4b18246
Kevin Duret <kduret@centreon.com>
2019-02-27 17:26:30 +0100

    tmp

--- www/front_src/src/index.js
+++ www/front_src/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./App.css";
+//import "./App.css";
 import "loaders.css/loaders.min.css";
 import AppProvider from "./Provider";
 
--- www/front_src/webpack.config.js
+++ www/front_src/webpack.config.js
@@ -1,8 +1,88 @@
-process.env.NODE_ENV = "production";
-var reactScriptsConfig = require("react-scripts/config/webpack.config.prod");
-module.exports = Object.assign({}, reactScriptsConfig, {
-  entry: ["@babel/polyfill", __dirname + "/src/index.js"],
-  output: Object.assign({}, reactScriptsConfig.output, {
-    path: __dirname + "/../"
-  })
-});
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: [
+    "@babel/polyfill",
+    "./src/App.scss",
+    "./src/index.js"
+  ],
+  output: {
+    path: __dirname + "/..",
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    libraryTarget: 'umd',
+    library: '[name]',
+    umdNamedDefine: true,
+  },
+  optimization: {
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'static/css/[name].[contenthash:8].css',
+      chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+    }),
+  ],
+  module: {
+    rules: [
+      { parser: { system: false } },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            babelrc: true
+          },
+        }
+      },
+      {
+        test: /\.(c|sa|sc)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'resolve-url-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+        ],
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/img/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\/fonts\//,
+        loader: 'file-loader',
+        exclude: [/\.(js|jsx)$/, /\.(c|sa|sc)ss$/, /\.html$/, /\.json$/],
+        options: {
+          publicPath: '../../',
+          name: 'static/fonts/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\/img\//,
+        loader: 'file-loader',
+        exclude: [/\.(js|jsx)$/, /\.(c|sa|sc)ss$/, /\.html$/, /\.json$/],
+        options: {
+          publicPath: '../../',
+          name: 'static/img/[name].[hash:8].[ext]',
+        },
+      },
+    ]
+  },
+};
