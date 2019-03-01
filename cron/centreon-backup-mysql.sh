57139089f99aedf27ce6e5c84896a19dfdbb1c5c
root <pkrikorian@centreon.com>
2019-03-01 12:26:29 +0100

    fix(bug): partial backup didn't backup the right partition for data_bin and los

--- cron/centreon-backup-mysql.sh
+++ cron/centreon-backup-mysql.sh
@@ -236,7 +236,9 @@ $INIT_SCRIPT start
 ###
 output_log "Mount LVM snapshot"
 mkdir -p "$SNAPSHOT_MOUNT"
-mount /dev/$vg_name/dbbackup "$SNAPSHOT_MOUNT"
+TYPEFS_BACKUP=$(df -T "$datadir" | tail -1 | awk -F' ' '{print $(NF-5)}')
+[ "$TYPEFS_BACKUP"  = "xfs" ] && MNTOPTIONS="-o nouuid"
+mount $MNTOPTIONS /dev/$vg_name/dbbackup "$SNAPSHOT_MOUNT"
 if [ $? -eq 0 ]; then
     output_log "Device mounted successfully"
 else
@@ -253,12 +255,13 @@ concat_datadir=$(echo "$datadir" | sed "s#^${mount_point}##")
 ar_exclude_file=""
 last_save_time=$(cat "$SAVE_LAST_DIR/$SAVE_LAST_FILE")
 
-if [ $OPT_PARTIAL -eq 1 ] ; then
+if [ $OPT_PARTIAL -eq 1 ] && [ -n "$last_save_time" ] ; then
+    minutes=$((($save_timestamp - $last_save_time) / 60))
     for table in $PARTITION_NAME ; do
     	tmp_dir=$(dirname "$table")
     	tmp_name=$(basename "$table")
     	tmp_path=$(echo "$SNAPSHOT_MOUNT/$concat_datadir/$tmp_dir" | sed "s#/\+#/#g")
-    	for tmp_file in $(find "$tmp_path" -name "$tmp_name*" -type f); do
+    	for tmp_file in $(find "$tmp_path" -name "$tmp_name*" -mmin +$minutes -type f); do
     		ar_exclude_file="$ar_exclude_file \"$tmp_file\""
     	done
     done
