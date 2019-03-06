2ab75075aa99ed4e5643dd53a496dea8cb0354f3
Edyta SKIBINSKA <eskibinska@centreon.com>
2019-03-06 14:15:47 +0100

    enh(ui) : tooltip in folded menu hover ok

---
+++ www/front_src/src/components/tooltip/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from "react-redux";
+
+const Tooltip = ({x, y, label, toggled}) => (
+  <div className={`tooltip ${toggled ? ' ' : 'hidden'}`}
+    style={
+      {
+        top:y,
+        left:x
+      }
+    }
+  >{label}</div>
+)
+
+/*
+* to make tooltip work we need the following key value pairs in order to map it's state to props
+* x for x mouse position
+* y for y mouse position
+* label for text that will be shown into the tooltip
+* toogled (bool) to detect if the menu is folded or not - toggled false - menu is folded
+*/
+const mapStateToProps = ({tooltip}) => (
+  {
+    x:tooltip.x,
+    y:tooltip.y,
+    label:tooltip.label,
+    toggled:tooltip.toggled
+  }
+)
+
+export default connect(mapStateToProps, null)(Tooltip)
