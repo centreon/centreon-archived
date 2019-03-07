404eebf619d22ac0c018319f6223d92ccfde484b
Louis Sautier <sautier.louis@gmail.com>
2019-03-07 13:23:41 +0100

    Fix wording of messages related to recurring downtimes

--- www/include/monitoring/downtime/listDowntime.php
+++ www/include/monitoring/downtime/listDowntime.php
@@ -283,7 +283,7 @@ $tpl->assign("user", _("Users"));
 $tpl->assign('Hostgroup', _("Hostgroup"));
 $tpl->assign('Search', _("Search"));
 $tpl->assign("ViewAll", _("Display Finished Downtimes"));
-$tpl->assign("ViewDowntimeCycle", _("Display Downtime Cycle"));
+$tpl->assign("ViewDowntimeCycle", _("Display Recurring Downtimes"));
 $tpl->assign("Author", _("Author"));
 $tpl->assign("search_output", $search_output);
 $tpl->assign('search_host', $host_name);
