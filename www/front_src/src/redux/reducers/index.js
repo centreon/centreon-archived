dd414d5b69a54c40bf92d07845e8a759da9f2836
EdytaSki <41005916+EdytaSki@users.noreply.github.com>
2019-03-15 16:08:55 +0100

    enh(ui) : tooltip in folded menu hover ok (#7258)
    * enh(ui) : tooltip in folded menu hover ok
    
    * enh(UI) tooltip :: non teste
    
    * enh(UI)tooltip menu : traductions

--- www/front_src/src/redux/reducers/index.js
+++ www/front_src/src/redux/reducers/index.js
@@ -5,11 +5,13 @@ import { i18nReducer } from 'react-redux-i18n';
 import pollerWizardReducer from "./pollerWizardReducer";
 import navigationReducer from "./navigationReducer";
 import refreshReducer from "./refreshReducer";
+import tooltipReducer from './tooltipReducer';
 
 export default combineReducers({
   form: formReducer,
   pollerForm: pollerWizardReducer,
   i18n: i18nReducer,
   navigation: navigationReducer,
-  intervals: refreshReducer
+  intervals: refreshReducer,
+  tooltip: tooltipReducer
 });
