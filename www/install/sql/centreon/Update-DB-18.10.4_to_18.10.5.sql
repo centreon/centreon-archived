b8f70bd49d300ae6347b612f22ec5aaf5ef78e98
Laurent Pinsivy <lpinsivy@centreon.com>
2019-03-21 10:53:11 +0000

    fix(update): correct SQL update

--- www/install/sql/centreon/Update-DB-18.10.4_to_18.10.5.sql
+++
@@ -1,5 +0,0 @@
--- Change version of Centreon
-UPDATE `informations` SET `value` = '18.10.5' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.4' LIMIT 1;
-
--- Remove non existing entries
-DELETE FROM topology_JS WHERE id_page IN ('201', '2020301', '2020302', '5010103', '5010105');
---
+++ www/install/sql/centreon/Update-DB-18.10.5.sql
@@ -0,0 +1,2 @@
+-- Remove non existing entries
+DELETE FROM topology_JS WHERE id_page IN ('201', '2020301', '2020302', '5010103', '5010105');
