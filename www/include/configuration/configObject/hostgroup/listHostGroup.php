9503129d627cfa5105a1a999d204f210e668465f
Stéphane Chapron <schapron@centreon.com>
2019-03-25 16:51:05 +0100

    fix(ui): saving configuration > hosts filters

--- www/include/configuration/configObject/hostgroup/listHostGroup.php
+++ www/include/configuration/configObject/hostgroup/listHostGroup.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Copyright 2005-2019 Centreon
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -37,9 +37,9 @@ if (!isset($centreon)) {
     exit();
 }
 
-include_once("./class/centreonUtils.class.php");
+require_once "./class/centreonUtils.class.php";
 
-include("./include/common/autoNumLimit.php");
+require_once "./include/common/autoNumLimit.php";
 
 /*
  * Object init
@@ -75,15 +75,11 @@ if ($search) {
 $tpl = new Smarty();
 $tpl = initSmartyTpl($path, $tpl);
 
-//$centreon->cache->initHostGroupCache($pearDB);
-
-/* Access level */
+// Access level
 ($centreon->user->access->page($p) == 1) ? $lvl_access = 'w' : $lvl_access = 'r';
 $tpl->assign('mode_access', $lvl_access);
 
-/*
- * start header menu
- */
+// start header menu
 $tpl->assign("headerMenu_name", _("Name"));
 $tpl->assign("headerMenu_desc", _("Alias"));
 $tpl->assign("headerMenu_status", _("Status"));
@@ -91,49 +87,41 @@ $tpl->assign("headerMenu_hostAct", _("Enabled Hosts"));
 $tpl->assign("headerMenu_hostDeact", _("Disabled Hosts"));
 $tpl->assign("headerMenu_options", _("Options"));
 
-/*
- * Hostgroup list
- */
-$rq = "SELECT SQL_CALC_FOUND_ROWS hg_id, hg_name, hg_alias, hg_activate, hg_icon_image
-           FROM hostgroup
-           WHERE {$searchFilterQuery} hg_id NOT IN (SELECT hg_child_id FROM hostgroup_hg_relation) " .
+// Hostgroup list
+$rq = "SELECT SQL_CALC_FOUND_ROWS hg_id, hg_name, hg_alias, hg_activate, hg_icon_image " .
+    "FROM hostgroup " .
+    "WHERE {$searchFilterQuery} hg_id NOT IN (SELECT hg_child_id FROM hostgroup_hg_relation) " .
     $acl->queryBuilder('AND', 'hg_id', $hgString) .
-    " ORDER BY hg_name LIMIT " . $num * $limit . ", $limit";
+    " ORDER BY hg_name LIMIT " . $num * $limit . ", " . $limit;
 $DBRESULT = $pearDB->query($rq, $mainQueryParameters);
 
-/*
- * Pagination
- */
+// Pagination
 $rows = $pearDB->query("SELECT FOUND_ROWS()")->fetchColumn();
-include("./include/common/checkPagination.php");
+require_once "./include/common/checkPagination.php";
 
 $search = tidySearchKey($search, $advanced_search);
 
 $form = new HTML_QuickFormCustom('select_form', 'POST', "?p=" . $p);
-/*
- * Different style between each lines
- */
+// Different style between each lines
 $style = "one";
 
-/*
- * Fill a tab with a mutlidimensionnal Array we put in $tpl
- */
+// Fill a tab with a multidimensional Array we put in $tpl
 $elemArr = array();
 for ($i = 0; $hg = $DBRESULT->fetch(); $i++) {
     $selectedElements = $form->addElement('checkbox', "select[" . $hg['hg_id'] . "]");
-    $moptions = "";
+    $mOptions = "";
     if ($hg["hg_activate"]) {
-        $moptions .= "<a href='main.php?p=" . $p . "&hg_id=" . $hg['hg_id'] . "&o=u&limit=" . $limit
+        $mOptions .= "<a href='main.php?p=" . $p . "&hg_id=" . $hg['hg_id'] . "&o=u&limit=" . $limit
             . "&num=" . $num . "&search=" . $search
             . "'><img src='img/icons/disabled.png' class='ico-14 margin_right' border='0' alt='"
             . _("Disabled") . "'></a>";
     } else {
-        $moptions .= "<a href='main.php?p=" . $p . "&hg_id=" . $hg['hg_id'] . "&o=s&limit=" . $limit
+        $mOptions .= "<a href='main.php?p=" . $p . "&hg_id=" . $hg['hg_id'] . "&o=s&limit=" . $limit
             . "&num=" . $num . "&search=" . $search
             . "'><img src='img/icons/enabled.png' class='ico-14 margin_right' border='0' alt='"
             . _("Enabled") . "'></a>";
     }
-    $moptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57))"
+    $mOptions .= "<input onKeypress=\"if(event.keyCode > 31 && (event.keyCode < 45 || event.keyCode > 57))"
         . " event.returnValue = false; if(event.which > 31 && (event.which < 45 || event.which > 57)) return false;"
         . "\" maxlength=\"3\" size=\"3\" value='1' style=\"margin-bottom:0px;\""
         . " name='dupNbr[" . $hg['hg_id'] . "]'></input>";
@@ -141,10 +129,10 @@ for ($i = 0; $hg = $DBRESULT->fetch(); $i++) {
     /*
      * Check Nbr of Host / hg
      */
-    $nbrhostAct = array();
-    $nbrhostDeact = array();
-    $nbrhostgroupAct = array();
-    $nbrhostgroupDeact = array();
+    $nbrHostAct = array();
+    $nbrHostDeact = array();
+    $nbrHostgroupAct = array();
+    $nbrHostgroupDeact = array();
 
     $aclFrom = "";
     $aclCond = "";
@@ -152,23 +140,24 @@ for ($i = 0; $hg = $DBRESULT->fetch(); $i++) {
         $aclFrom = ", $aclDbName.centreon_acl acl ";
         $aclCond = " AND h.host_id = acl.host_id AND acl.group_id IN (" . $acl->getAccessGroupsString() . ") ";
     }
-    $rq = "SELECT h.host_id, h.host_activate
-               FROM hostgroup_relation hgr, host h $aclFrom
-               WHERE hostgroup_hg_id = '" . $hg['hg_id'] . "'
-               AND h.host_id = hgr.host_host_id
-               AND h.host_register = '1' $aclCond";
-    $DBRESULT2 = $pearDB->query($rq);
-    $nbrhostActArr = array();
-    $nbrhostDeactArr = array();
+    $DBRESULT2 = $pearDB->query(
+        "SELECT h.host_id, h.host_activate " .
+        "FROM hostgroup_relation hgr, host h " . $aclFrom . 
+        "WHERE hostgroup_hg_id = '" . $hg['hg_id'] . "' " . 
+        "AND h.host_id = hgr.host_host_id " .
+        "AND h.host_register = '1' " . $aclCond
+    );
+    $nbrHostActArr = array();
+    $nbrHostDeactArr = array();
     while ($row = $DBRESULT2->fetch()) {
         if ($row['host_activate']) {
-            $nbrhostActArr[$row['host_id']] = true;
+            $nbrHostActArr[$row['host_id']] = true;
         } else {
-            $nbrhostDeactArr[$row['host_id']] = true;
+            $nbrHostDeactArr[$row['host_id']] = true;
         }
     }
-    $nbrhostAct = count($nbrhostActArr);
-    $nbrhostDeact = count($nbrhostDeactArr);
+    $nbrHostAct = count($nbrHostActArr);
+    $nbrHostDeact = count($nbrHostDeactArr);
 
     if ($hg['hg_icon_image'] != "") {
         $hgIcone = "./img/media/" . $mediaObj->getFilename($hg['hg_icon_image']);
@@ -185,32 +174,33 @@ for ($i = 0; $hg = $DBRESULT->fetch(); $i++) {
             : CentreonUtils::escapeSecure(html_entity_decode($hg["hg_alias"]))),
         "RowMenu_status" => $hg["hg_activate"] ? _("Enabled") : _("Disabled"),
         "RowMenu_badge" => $hg["hg_activate"] ? "service_ok" : "service_critical",
-        "RowMenu_hostAct" => $nbrhostAct,
+        "RowMenu_hostAct" => $nbrHostAct,
         "RowMenu_icone" => $hgIcone,
-        "RowMenu_hostDeact" => $nbrhostDeact,
-        "RowMenu_options" => $moptions
+        "RowMenu_hostDeact" => $nbrHostDeact,
+        "RowMenu_options" => $mOptions
     );
-    /*
-     * Switch color line
-     */
+
+    // Switch color line
     $style != "two" ? $style = "two" : $style = "one";
 }
 $tpl->assign("elemArr", $elemArr);
 
-/*
- * Different messages we put in the template
- */
+// Different messages put in the template
 $tpl->assign(
     'msg',
-    array("addL" => "main.php?p=" . $p . "&o=a", "addT" => _("Add"), "delConfirm" => _("Do you confirm the deletion ?"))
+    array(
+        "addL" => "main.php?p=" . $p . "&o=a",
+        "addT" => _("Add"),
+        "delConfirm" => _("Do you confirm the deletion ?")
+    )
 );
 
 ?>
-    <script type="text/javascript">
-        function setO(_i) {
-            document.forms['form'].elements['o'].value = _i;
-        }
-    </script>
+<script type="text/javascript">
+    function setO(_i) {
+        document.forms['form'].elements['o'].value = _i;
+    }
+</script>
 <?php
 foreach (array('o1', 'o2') as $option) {
     $attrs1 = array(
@@ -246,9 +236,7 @@ foreach (array('o1', 'o2') as $option) {
 $tpl->assign('searchHg', $search);
 $tpl->assign('limit', $limit);
 
-/*
- * Apply a template definition
- */
+// Apply a template definition
 $renderer = new HTML_QuickForm_Renderer_ArraySmarty($tpl);
 $form->accept($renderer);
 $tpl->assign('form', $renderer->toArray());
