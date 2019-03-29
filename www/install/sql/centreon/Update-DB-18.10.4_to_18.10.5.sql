1493c4ca39b216377449fa5b6b785b1cebaa4a00
Laurent Pinsivy <lpinsivy@centreon.com>
2019-03-29 10:36:27 +0000

    enh(remote-server): update DB schema to configure HTTP method and TCP port
    Conflicts:
    	www/install/sql/centreon/Update-DB-19.04.0.sql

--- www/install/sql/centreon/Update-DB-18.10.4_to_18.10.5.sql
+++ www/install/sql/centreon/Update-DB-18.10.4_to_18.10.5.sql
@@ -3,3 +3,8 @@ UPDATE `informations` SET `value` = '18.10.5' WHERE CONVERT( `informations`.`key
 
 -- Remove non existing entries
 DELETE FROM topology_JS WHERE id_page IN ('201', '2020301', '2020302', '5010103', '5010105');
+
+-- Add HTTPS connexion to Remote Server
+ALTER TABLE remote_servers ADD COLUMN `http_method` enum('http','https') NOT NULL DEFAULT 'http';
+ALTER TABLE remote_servers ADD COLUMN `http_port` int(11) NULL DEFAULT NULL;
+ALTER TABLE remote_servers ADD COLUMN `no_check_certificate` enum('0','1') NOT NULL DEFAULT '1';
