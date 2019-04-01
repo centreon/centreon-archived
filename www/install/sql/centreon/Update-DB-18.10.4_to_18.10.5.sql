9b4281ddedf64f2c28b4bff7cd6949b8cf5c9bb0
Laurent Pinsivy <lpinsivy@centreon.com>
2019-04-01 10:48:17 +0100

    fix(install/upgrade): correct default value for remote_servers
    Conflicts:
    	www/install/sql/centreon/Update-DB-19.04.0.sql

--- www/install/sql/centreon/Update-DB-18.10.4_to_18.10.5.sql
+++ www/install/sql/centreon/Update-DB-18.10.4_to_18.10.5.sql
@@ -7,4 +7,4 @@ DELETE FROM topology_JS WHERE id_page IN ('201', '2020301', '2020302', '5010103'
 -- Add HTTPS connexion to Remote Server
 ALTER TABLE remote_servers ADD COLUMN `http_method` enum('http','https') NOT NULL DEFAULT 'http';
 ALTER TABLE remote_servers ADD COLUMN `http_port` int(11) NULL DEFAULT NULL;
-ALTER TABLE remote_servers ADD COLUMN `no_check_certificate` enum('0','1') NOT NULL DEFAULT '1';
+ALTER TABLE remote_servers ADD COLUMN `no_check_certificate` enum('0','1') NOT NULL DEFAULT '0';
