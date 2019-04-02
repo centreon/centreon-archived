93a0e0eb1a1e4c4654136dc616d9d8e6ae097654
Laurent Pinsivy <lpinsivy@centreon.com>
2019-04-02 13:54:43 +0100

    fix(update): remove unsued file

--- www/install/sql/centreon/Update-DB-18.10.4_to_19.04.0.sql
+++
@@ -1,5 +0,0 @@
--- Change version of Centreon
-UPDATE `informations` SET `value` = '19.04.0' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.4' LIMIT 1;
-
--- Remove unused options
-DELETE FROM options WHERE options.key IN ('rrdtool_title_font', 'rrdtool_title_fontsize', 'rrdtool_unit_font', 'rrdtool_unit_fontsize', 'rrdtool_axis_font', 'rrdtool_axis_fontsize', 'rrdtool_watermark_font', 'rrdtool_watermark_fontsize', 'rrdtool_legend_font', 'rrdtool_legend_fontsize');
