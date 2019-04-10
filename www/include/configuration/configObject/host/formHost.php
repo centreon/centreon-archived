9ceaf5ce58df4de93389f1cb7c72cf4e046b026f
Jérémy Delpierre <jdelpierre@users.noreply.github.com>
2019-04-10 14:43:12 +0200

    fix(host/form): change size (6 => 30) of input geo coordinates on host form (#7405)

--- www/include/configuration/configObject/host/formHost.php
+++ www/include/configuration/configObject/host/formHost.php
@@ -971,7 +971,7 @@ $form->addElement('select', 'ehi_statusmap_image', _("Status Map Image"), $extIm
 ));
 $form->addElement('text', 'ehi_2d_coords', _("2d Coords"), $attrsText2);
 $form->addElement('text', 'ehi_3d_coords', _("3d Coords"), $attrsText2);
-$form->addElement('text', 'geo_coords', _("Geo coordinates"), $attrsText2);
+$form->addElement('text', 'geo_coords', _("Geo coordinates"), $attrsText);
 
 if (!$centreon->user->admin && $o == "a") {
     $aclDeRoute = './include/common/webServices/rest/internal.php?object=centreon_administration_aclgroup'
