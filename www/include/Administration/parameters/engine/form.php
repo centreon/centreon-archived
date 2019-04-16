be7f9360694c400c919c1e71c09ca0db3b1ef42f
sc979 <34628915+sc979@users.noreply.github.com>
2019-04-16 12:03:13 +0200

    Update www/include/Administration/parameters/engine/form.php
    Co-Authored-By: loiclau <loic.lau@gmail.com>

--- www/include/Administration/parameters/engine/form.php
+++ www/include/Administration/parameters/engine/form.php
@@ -134,7 +134,6 @@ $form->registerRule('is_writable_file', 'callback', 'is_writable_file');
 $form->registerRule('is_writable_file_if_exist', 'callback', 'is_writable_file_if_exist');
 $form->registerRule('isNum', 'callback', 'isNum');
 
-// - Field is not added so no need for rule
 // $form->addRule('nagios_path', _("The directory isn't valid"), 'is_valid_path');
 // - Field is not added so no need for rule
 $form->addRule('nagios_path_plugins', _("The directory isn't valid"), 'is_valid_path');
