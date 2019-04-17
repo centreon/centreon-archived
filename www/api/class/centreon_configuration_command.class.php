1b743e24b556e8f241e8c1c382042f2cc972795d
sc979 <34628915+sc979@users.noreply.github.com>
2019-04-17 16:11:59 +0300

    Update www/api/class/centreon_configuration_command.class.php
    Co-Authored-By: victorvassilev <acedweb@gmail.com>

--- www/api/class/centreon_configuration_command.class.php
+++ www/api/class/centreon_configuration_command.class.php
@@ -106,7 +106,7 @@ class CentreonConfigurationCommand extends CentreonConfigurationObjects
 
         return array(
             'items' => $commandList,
-            'total' => (int) $stmt->rowCount()
+            'total' => (int)$stmt->rowCount()
         );
     }
 }
