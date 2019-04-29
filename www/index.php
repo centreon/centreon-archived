5606c31f0fe3ee541359ddf47da09a04c7bd69fc
Kevin Duret <kduret@centreon.com>
2019-04-29 10:23:32 +0200

    tmp

--- www/index.php
+++ www/index.php
@@ -98,43 +98,6 @@ $indexHtmlContent = preg_replace(
 );
 file_put_contents($indexHtmlPath, $indexHtmlContent);
 
- /*
-$staticExists = glob('static/css/*.css');
-$newPath = trim(explode('index.php', $_SERVER['REQUEST_URI'])[0], "/");
-
-if (!$staticExists) {
-    shell_exec('rm -rf ' . __DIR__ . '/static ');
-    shell_exec('cp -pR ' . __DIR__ . '/template '. __DIR__ . '/static');
-    $allCssFiles = glob('static/css/*');
-    $allJsFiles = glob('static/js/*');
-    $indexFile = glob('index.html');
-    $allFiles = array_merge($allCssFiles, $allJsFiles, $indexFile);
-    foreach ($allFiles as $file) {
-        $fc = file_get_contents($file);
-        $newCont = str_replace('_CENTREON_PATH_PLACEHOLDER_', $newPath, $fc);
-        file_put_contents($file, $newCont);
-    }
-} else {
-    $hashStatic = explode('static/css/main.', $staticExists[0]);
-    $hashTemplate = explode('template/css/main.', glob('template/css/*.css')[0]);
-    if (!isset($hashTemplate[1])
-        || $hashTemplate[1] !== $hashStatic
-    ) {
-        shell_exec('rm -rf ' . __DIR__ . '/static ');
-        shell_exec('cp -pR ' . __DIR__ . '/template '. __DIR__ . '/static');
-        $allCssFiles = glob('static/css/*');
-        $allJsFiles = glob('static/js/*');
-        $indexFile = glob('index.html');
-        $allFiles = array_merge($allCssFiles, $allJsFiles, $indexFile);
-        foreach ($allFiles as $file) {
-            $fc = file_get_contents($file);
-            $newCont = str_replace('_CENTREON_PATH_PLACEHOLDER_', $newPath, $fc);
-            file_put_contents($file, $newCont);
-        }
-    }
-}
-*/
-
 /*
  * Set PHP Session Expiration time
  */
