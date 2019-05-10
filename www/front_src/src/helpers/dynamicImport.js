e79746b2226b21f1731bfd4ef3fa82ceff0db3a9
Kevin Duret <kduret@centreon.com>
2019-05-10 11:20:50 +0200

    update dynamic import

--- www/front_src/src/helpers/dynamicImport.js
+++ www/front_src/src/helpers/dynamicImport.js
@@ -1,14 +1,15 @@
-import System from '../../../../node_modules/systemjs/dist/system.js'; // IIFE format so it's imported on window
-import systemCss from "systemjs-plugin-css"; // used to import css in <head>
+import '../../../../node_modules/systemjs/dist/system.js'; // IIFE format so it's imported on window
+import systemCss from 'systemjs-plugin-css'; // used to import css in <head>
 
 // this function allows to import dynamically js and css using systemjs
 // it is compatible with IE, Edge, firefox and chrome
-export const dynamicImport = (basename, parameters) => {
+export function dynamicImport(basename, parameters) {
 
   return new Promise(async (resolve, reject) => {
 
     if (!parameters.js) {
-      return reject(new Error('dynamic import should contains js parameter.'));
+      console.error(new Error('dynamic import should contains js parameter.'));
+      return null;
     }
 
     try {
@@ -18,12 +19,16 @@ export const dynamicImport = (basename, parameters) => {
       }
 
       // check external component in memory to avoid to reimport it
-      const vector = "$centreonExternalModule$" + parameters.js.replace(/(^\.?\/)|(\.js)/g, '').replace(/\//g, '$');
+      const vector = '$centreonExternalModule$' + parameters.js.replace(/(^\.?\/)|(\.js)/g, '').replace(/\//g, '$');
       if (typeof(window[vector]) === "object") {
         return resolve(window[vector]);
       } else {
         const module = await(window.System.import(basename + parameters.js));
-        window[vector] = module;
+        if (module.default && typeof(module.default) === 'object') { // named umd export
+          window[vector] = module.default;
+        } else { // unnamed umd export or systemjs export
+          window[vector] = module;
+        }
         return resolve(window[vector]);
       }
     } catch (error) {
