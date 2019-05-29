c4a9f7df9f08f8674dc5f3a8c848c1d59d88e173
Danijel Chalupka <danijel@simple-task.com>
2019-05-29 16:58:10 +0200

    navigation action optimized for new api

--- www/front_src/src/redux/actions/navigationActions.js
+++ www/front_src/src/redux/actions/navigationActions.js
@@ -41,7 +41,7 @@ export const fetchNavigationData = () => {
       ).get();
 
       let pageIds = [];
-      for(let levelOne of data.result){
+      for(let levelOne of data){
         pageIds.push(levelOne.page);
         for(let levelTwo of levelOne.children){
           pageIds.push(levelTwo.page);
