3343339a42acaa2c026d44815d62bd3e418f4269
Valentin Hristov <vhr@users.noreply.github.com>
2019-06-04 10:44:25 +0300

    feat(base): add Symfony validator as a service and pagination service
    Resolve BAM-538

--- src/Centreon/ServiceProvider.php
+++ src/Centreon/ServiceProvider.php
@@ -54,6 +54,9 @@ use Centreon\Domain\Service\BrokerConfigurationService;
 use Centreon\Domain\Repository\CfgCentreonBrokerRepository;
 use Centreon\Domain\Repository\CfgCentreonBrokerInfoRepository;
 use CentreonClapi\CentreonACL;
+use Centreon\Application\Validation;
+use Symfony\Component\Validator;
+use Symfony\Component\Validator\Constraints;
 
 class ServiceProvider implements AutoloadServiceProviderInterface
 {
@@ -62,6 +65,7 @@ class ServiceProvider implements AutoloadServiceProviderInterface
     const MENU_WEBSERVICE = 'centreon.menu.webservice';
 
     //services
+    const CENTREON_PAGINATION = 'centreon.pagination';
     const CENTREON_I18N_SERVICE = 'centreon.i18n_service';
     const CENTREON_FRONTEND_COMPONENT_SERVICE = 'centreon.frontend_component_service';
     const CENTREON_BROKER_CONFIGURATION_SERVICE = 'centreon.broker_configuration_service';
@@ -77,6 +81,9 @@ class ServiceProvider implements AutoloadServiceProviderInterface
     const CENTREON_EVENT_DISPATCHER = 'centreon.event_dispatcher';
     const CENTREON_USER = 'centreon.user';
     const YML_CONFIG = 'yml.config';
+    const CENTREON_EVENT_DISPATCHER = 'centreon.event_dispatcher';
+    const CENTREON_VALIDATOR_FACTORY = 'centreon.validator_factory';
+    const CENTREON_VALIDATOR_TRANSLATOR = 'centreon.validator_translator';
 
     /**
      * Register Centreon services
@@ -140,6 +147,17 @@ class ServiceProvider implements AutoloadServiceProviderInterface
 
             $locator = new ServiceLocator($container, $services);
             $service = new CentreonDBManagerService($locator);
+
+            return $service;
+        };
+
+        $pimple[static::CENTREON_PAGINATION] = function(Container $container): Service\CentreonPaginationService {
+            $service = new Service\CentreonPaginationService(
+                new ServiceLocator(
+                    $container,
+                    Service\CentreonPaginationService::dependencies()
+                )
+            );
 
             return $service;
         };
@@ -218,6 +236,38 @@ class ServiceProvider implements AutoloadServiceProviderInterface
             );
 
             return $eventDispatcher;
+        };
+
+        $this->registerValidator($pimple);
+    }
+
+    /**
+     * Register services related with validation
+     *
+     * @param \Pimple\Container $pimple
+     */
+    public function registerValidator(Container $pimple): void
+    {
+        $pimple[static::VALIDATOR] = function(Container $container): Validator\Validator\ValidatorInterface {
+            return Validator\Validation::createValidatorBuilder()
+                    ->addMethodMapping('loadValidatorMetadata')
+                    ->setConstraintValidatorFactory($container[ServiceProvider::CENTREON_VALIDATOR_FACTORY])
+                    ->setTranslator($container[ServiceProvider::CENTREON_VALIDATOR_TRANSLATOR])
+                    ->getValidator();
+        };
+
+        $pimple[static::CENTREON_VALIDATOR_FACTORY] = function(Container $container): Validation\CentreonValidatorFactory {
+            $service = new Validation\CentreonValidatorFactory($container);
+
+            return $service;
+        };
+
+        $pimple[static::CENTREON_VALIDATOR_TRANSLATOR] = function(Container $container): Validation\CentreonValidatorTranslator {
+            return new Validation\CentreonValidatorTranslator;
+        };
+
+        $pimple[static::VALIDATOR_EXPRESSION] = function(): Constraints\ExpressionValidator {
+            return new Constraints\ExpressionValidator();
         };
     }
 
