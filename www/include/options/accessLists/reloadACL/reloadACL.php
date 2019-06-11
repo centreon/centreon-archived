a9989b3c7a8f98035579ff33cd41a2157887f14f
schapron <schapron@centreon.com>
2019-06-11 09:42:38 +0200

    update the refresh icon in the reloadACL page

--- www/include/options/accessLists/reloadACL/reloadACL.php
+++ www/include/options/accessLists/reloadACL/reloadACL.php
@@ -119,7 +119,7 @@ while ($r = $res->fetch()) {
         $session_data[$cpt]["current_page"] = $r["current_page"] . $rCP["topology_url_opt"];
         $session_data[$cpt]["topology_name"] = _($rCP["topology_name"]);
         $session_data[$cpt]["actions"] = "<a href='./main.php?p=" . $p . "&o=r'>" .
-            "<img src='./img/icones/16x16/refresh.gif' border='0' alt='"._("Reload ACL")."' " .
+            "<img src='./img/icons/refresh.png' border='0' alt='"._("Reload ACL")."' " .
             "title='"._("Reload ACL")."'></a>";
         $selectedElements = $form->addElement('checkbox', "select[" . $r['user_id'] . "]");
         $session_data[$cpt]["checkbox"] = $selectedElements->toHtml();
