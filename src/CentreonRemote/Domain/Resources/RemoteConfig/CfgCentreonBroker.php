42dd60a36bd9a47abe6024c6d997bdcbf735886d
CPbN <cpbnfo@gmail.com>
2019-06-21 08:09:40 +0200

    Rename default Remote Server configuration files

--- src/CentreonRemote/Domain/Resources/RemoteConfig/CfgCentreonBroker.php
+++ src/CentreonRemote/Domain/Resources/RemoteConfig/CfgCentreonBroker.php
@@ -22,7 +22,7 @@ class CfgCentreonBroker
         return [
             'broker' => [
                 'config_name'            => "{$configName}-broker",
-                'config_filename'        => "{$configName}-broker.xml",
+                'config_filename'        => "central-broker.xml",
                 'config_write_timestamp' => '0',
                 'config_write_thread_id' => '0',
                 'config_activate'        => '1',
@@ -36,7 +36,7 @@ class CfgCentreonBroker
             ],
             'module' => [
                 'config_name'            => "{$configName}-module",
-                'config_filename'        => "{$configName}-module.xml",
+                'config_filename'        => "central-module.xml",
                 'config_write_timestamp' => '0',
                 'config_write_thread_id' => '0',
                 'config_activate'        => '1',
@@ -50,7 +50,7 @@ class CfgCentreonBroker
             ],
             'rrd' => [
                 'config_name'            => "{$configName}-rrd",
-                'config_filename'        => "{$configName}-rrd.xml",
+                'config_filename'        => "central-rrd.xml",
                 'config_write_timestamp' => '0',
                 'config_write_thread_id' => '0',
                 'config_activate'        => '1',
