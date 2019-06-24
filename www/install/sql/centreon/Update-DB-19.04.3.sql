5dd90945e01dcbfecf31fc339630f0ec664a6c07
CPbN <cpbnfo@gmail.com>
2019-06-24 22:35:21 +0200

    Move to 19.10.0

--- www/install/sql/centreon/Update-DB-19.04.3.sql
+++
@@ -1,8 +0,0 @@
--- Update topology of service grid
-UPDATE topology SET topology_url_opt = '&o=svcOV_pb' WHERE topology_page = 20204;
-
--- Update topology of service by host group
-UPDATE topology SET topology_url_opt = '&o=svcOVHG_pb' WHERE topology_page = 20209;
-
--- Update topology of service by service group
-UPDATE topology SET topology_url_opt = '&o=svcOVSG_pb' WHERE topology_page = 20212;
--- www/install/sql/centreon/Update-DB-19.10.0.sql
+++ www/install/sql/centreon/Update-DB-19.10.0.sql
@@ -60,3 +60,11 @@ ALTER TABLE `widget_models` MODIFY COLUMN `description` TEXT NOT NULL;
 ALTER TABLE `modules_informations` DROP COLUMN `lang_files`;
 ALTER TABLE `modules_informations` DROP COLUMN `sql_files`;
 ALTER TABLE `modules_informations` DROP COLUMN `php_files`;
+
+--
+-- Update topology of service grid / by host group / by service group
+--
+UPDATE topology SET topology_url_opt = '&o=svcOV_pb' WHERE topology_page = 20204;
+UPDATE topology SET topology_url_opt = '&o=svcOVHG_pb' WHERE topology_page = 20209;
+UPDATE topology SET topology_url_opt = '&o=svcOVSG_pb' WHERE topology_page = 20212;
+
