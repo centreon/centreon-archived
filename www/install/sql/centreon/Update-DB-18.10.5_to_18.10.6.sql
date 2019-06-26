451dd541720187025780fc85740658674161ad8e
Laurent Pinsivy <lpinsivy@centreon.com>
2019-06-26 09:40:29 +0100

    fix(install): correct SQL install script name PR #7610

--- www/install/sql/centreon/Update-DB-18.10.5_to_18.10.6.sql
+++
@@ -1,5 +0,0 @@
--- Change version of Centreon
-UPDATE `informations` SET `value` = '18.10.6' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.5' LIMIT 1;
-
--- Change traps_execution_command from varchar(255) to text
-ALTER TABLE `traps` MODIFY COLUMN `traps_execution_command` text DEFAULT NULL;
