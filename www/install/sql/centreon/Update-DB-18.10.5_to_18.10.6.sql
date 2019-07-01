89119d3c86c3ca99d15c767f774d5104e26c6fef
Laurent Pinsivy <lpinsivy@centreon.com>
2019-07-01 12:16:26 +0100

    fix(remote-server): store FQDN (fix #7615)

--- www/install/sql/centreon/Update-DB-18.10.5_to_18.10.6.sql
+++ www/install/sql/centreon/Update-DB-18.10.5_to_18.10.6.sql
@@ -3,3 +3,8 @@ UPDATE `informations` SET `value` = '18.10.6' WHERE CONVERT( `informations`.`key
 
 -- Change traps_execution_command from varchar(255) to text
 ALTER TABLE `traps` MODIFY COLUMN `traps_execution_command` text DEFAULT NULL;
+
+--
+-- Change IP field from varchar(16) to varchar(255)
+--
+ALTER TABLE `remote_servers` MODIFY COLUMN `ip` VARCHAR(255) NOT NULL;
