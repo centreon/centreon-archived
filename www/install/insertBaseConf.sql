55823137a5c49bb5c9805cc0d5f66bfcd60545d7
Adrien Morais <amorais@centreon.com>
2019-07-15 16:09:09 +0200

    chore(build): New version released 18.10.6

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '18.10.5');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '18.10.6');
 
 --
 -- Contenu de la table `contact`
