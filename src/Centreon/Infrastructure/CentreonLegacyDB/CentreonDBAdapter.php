7c9a652145e895bba507639b9213aee2b727e643
Laurent Pinsivy <lpinsivy@centreon.com>
2019-07-18 10:30:24 +0200

    enh(DB): add bulk insert method

--- src/Centreon/Infrastructure/CentreonLegacyDB/CentreonDBAdapter.php
+++ src/Centreon/Infrastructure/CentreonLegacyDB/CentreonDBAdapter.php
@@ -8,6 +8,7 @@ use CentreonDB;
 
 class CentreonDBAdapter
 {
+    const BULK_SIZE = 10000;
 
     /** @var \CentreonDB */
     private $db;
@@ -131,6 +132,109 @@ class CentreonDBAdapter
             $stmt->execute();
         } catch (\Exception $e) {
             throw new \Exception('Query failed. ' . $e->getMessage());
+        }
+
+        return $this->db->lastInsertId();
+    }
+
+    /**
+     * Generate value template place holders
+     * 
+     * @param string $text
+     * @param int $count
+     * @param string $separator
+     * 
+     * @return string
+     */
+    public function placeHolders($text, $count = 0, $separator = ",")
+    {
+        $result = array();
+        if ($count > 0) {
+            for ($x = 0; $x < $count; $x++) {
+                $result[] = $text;
+            }
+        }
+    
+        return implode($separator, $result);
+    }
+
+    /**
+     * Insert data using bulk
+     * 
+     * @param string $table
+     * @param array $data
+     *
+     * @throws \Exception
+     *
+     * @return int Last inserted ID
+     */
+    public function insertBulk($table, array $data)
+    {
+        if (!$table && !$data) {
+            throw new \Exception("The argument can't be empty");
+        }
+
+        //Define needed vars
+        $rowsSQL = array();
+        $columnNames = array();
+        $toBind = array();
+        $params = array();
+        $i = 0;
+
+        foreach ($data as $row) {
+            $params = array();
+            $columnNames = array_keys($row);
+            foreach ($row as $columnName => $columnValue) {
+                $param = ":" . $columnName . $i;
+                $params[] = $param;
+                $toBind[$param] = $columnValue; 
+            }
+            $rowsSQL[] = "(" . implode(", ", $params) . ")";
+            $i++;
+
+            if ($i >= BULK_SIZE) {
+                //Construct SQL statement
+                $sql = "INSERT INTO `$table` (`" . implode("`, `", $columnNames) . "`) VALUES " . implode(", ", $rowsSQL);
+
+                //Prepare PDO statement.
+                $stmt = $this->db->prepare($sql);
+
+                //Bind values.
+                foreach ($toBind as $param => $val) {
+                    $stmt->bindValue($param, $val);
+                }
+
+                try {
+                    $stmt->execute();
+
+                    //Reset values
+                    $i = 0;
+                    $rowsSQL = array();
+                    $toBind = array();
+                } catch (\Exception $e) {
+                    throw new \Exception('Query failed. ' . $e->getMessage());
+                }
+            }
+        }
+
+        if ($rowsSQL) {
+            //Construct SQL statement
+            $rowsSQL[] = "(" . implode(", ", $params) . ")";
+            $sql = "INSERT INTO `$table` (`" . implode("`, `", $columnNames) . "`) VALUES " . implode(", ", $rowsSQL);
+
+            //Prepare PDO statement.
+            $stmt = $this->db->prepare($sql);
+
+            //Bind values.
+            foreach ($toBind as $param => $val) {
+                $stmt->bindValue($param, $val);
+            }
+
+            try {
+                $stmt->execute();
+            } catch (\Exception $e) {
+                throw new \Exception('Query failed. ' . $e->getMessage());
+            }
         }
 
         return $this->db->lastInsertId();
