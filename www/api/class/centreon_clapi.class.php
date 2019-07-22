psamecentreon/centreon

fix(chore): codestyle ...
Resolve BAM-12

www/api/class/centreon_clapi.class.php
@@ -239,7 +239,8 @@ class CentreonClapi extends CentreonWebService implements CentreonWebServiceDiIn
      * Removes carriage returns from $item if string
      * @param $item variable to check
      */
-    private function clearCarriageReturns(&$item) {
+    private function clearCarriageReturns(&$item)
+    {
         $item = (is_string($item)) ? str_replace(array("\n", "\t", "\r", "<br/>"), '', $item) : $item;
     }
 }

centreon/centreon-archived

Conflicts:
	www/api/class/centreon_clapi.class.php

www/api/class/centreon_clapi.class.php
@@ -240,7 +240,8 @@ class CentreonClapi extends CentreonWebService implements CentreonWebServiceDiIn
      * Removes carriage returns from $item if string
      * @param mixed &$item variable to check
      */
-    private function customAlterString(&$item) {
+    private function customAlterString(&$item)
+    {
         $item = (is_string($item)) ? str_replace(array("\n", "\t", "\r", "<br/>"), '', $item) : $item;
     }
 }