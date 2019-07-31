b1786f05d3759b15d1835abb54a88ff3f5abae54
Valentin Hristov <vhr@users.noreply.github.com>
2019-07-31 17:25:59 +0300

    fix(test): cover legacy service provider
    Resolve BAM-573, BAM-607

--- src/CentreonLegacy/ServiceProvider.php
+++ src/CentreonLegacy/ServiceProvider.php
@@ -102,11 +102,20 @@ class ServiceProvider implements AutoloadServiceProviderInterface
         };
     }
 
+    /**
+     * @param Container $pimple
+     */
     protected function registerRestHttp(Container $pimple)
     {
         $pimple[static::CENTREON_REST_HTTP] = function (Container $container) {
+<<<<<<< HEAD
             return function ($contentType = 'application/json', $logFile = null) {
                 return new \CentreonRestHttp($contentType, $logFile);
+=======
+            return function($contentType = 'application/json', $logFile = null) {
+                // @codeCoverageIgnoreStart
+                return new \CentreonRestHttp($contentType, $logFile); // @codeCoverageIgnoreEnd
+>>>>>>> fix(test): cover legacy service provider
             };
         };
     }
