040b128e0c41885dc26a01dc6c59603e9bdc23d0
aclotudrzal <aclotudrzal@centreon.com>
2019-08-01 15:06:38 +0100

    Add the Severity connector's name to the database

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -879,6 +879,7 @@ INSERT INTO `widget_parameters_field_type` (`ft_typename`, `is_connector`) VALUE
                                            ('serviceMulti', 1),
                                            ('pollerMulti', 1),
                                            ('serviceGroupMulti', 1),
+                                           ('serviceSeverityMulti', 1),
                                            ('integer', 0);
 
 INSERT INTO timezone (`timezone_name`, `timezone_offset`, `timezone_dst_offset`) VALUES
