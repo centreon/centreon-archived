8b4911c8fe45ae30dcf00cce4728b5fc418948ca
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-08-02 09:11:21 +0200

    chore(install): remove pre-2.8.0-beta1 update scripts.

--- www/install/sql/centreon/Update-DB-2.6.1.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `cfg_nagios` ADD COLUMN `log_pid` enum('0','1') DEFAULT '1';
-
--- Ticket #6035 : remove local module into centreon borker.
-DELETE FROM cb_module WHERE name = 'local';
-
-INSERT INTO `widget_parameters_field_type` (`ft_typename`, `is_connector`) VALUES
-                                           ('ba', 1),
-                                           ('bv', 1);
