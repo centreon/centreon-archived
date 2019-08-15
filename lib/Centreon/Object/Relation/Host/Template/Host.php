acf98c0b1c812a09f520f8361a9ad5af7d97d927
Dimitar Kalinov <dkalinoff@gmail.com>
2019-08-15 11:09:26 +0300

    fix(host templates): use method for deleting services in DB-Func

--- lib/Centreon/Object/Relation/Host/Template/Host.php
+++ lib/Centreon/Object/Relation/Host/Template/Host.php
@@ -82,34 +82,8 @@ class Centreon_Object_Relation_Host_Template_Host extends Centreon_Object_Relati
         $this->db->beginTransaction();
         try {
             parent::delete($fkey, $skey);
-            // Delete linked services :
-            // First get service IDs
-            $stmt = $this->db->prepare(
-                'SELECT service_service_id FROM host_service_relation WHERE host_host_id = :host_id'
-            );
-            $stmt->bindParam(':host_id', $fkey, PDO::PARAM_INT);
-            $stmt->execute();
-            $services = [];
-            foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $services[] = $row['service_service_id'];
-            }
-
-            if (!empty($services)) {
-                $serviceIds = [];
-                foreach ($services as $key => $service) {
-                    $serviceIds[] = ':id_' . $key;
-                }
-                $stmt = $this->db->prepare(
-                    'DELETE FROM host_service_relation
-                     WHERE host_host_id = :host_id
-                     AND service_service_id IN ( ' . implode(',', $serviceIds) . ' )'
-                );
-                $stmt->bindParam(':host_id', $skey, PDO::PARAM_INT);
-                foreach ($services as $key => $service) {
-                    $stmt->bindParam(':id_' . $key, $services[$key], PDO::PARAM_INT);
-                }
-                $stmt->execute();
-            }
+            require_once "./../../../../../../www/include/configuration/configObject/host/DB-Func.php";
+            deleteHostServiceMultiTemplate($skey, $fkey, array());
             $this->db->commit();
         } catch (\PDOException $e) {
             $this->db->rollBack();
