4dde48883d88ffcd94c31f83ce1a498d92f78342
schapron <schapron@centreon.com>
2019-08-19 14:31:58 +0200

    rework conditional logic

--- www/class/centreon-partition/partEngine.class.php
+++ www/class/centreon-partition/partEngine.class.php
@@ -622,7 +622,12 @@ class PartEngine
         $dbResult = $db->query("SELECT plugin_status FROM INFORMATION_SCHEMA.PLUGINS WHERE plugin_name = 'partition'");
         $config = $dbResult->fetch();
         $dbResult->closeCursor();
-        if ($config["plugin_status"] != "ACTIVE") {
+        if ($config["plugin_status"] == "ACTIVE") {
+            unset($config);
+
+            return true;
+        }
+        elseif (empty($config["plugin_status"])) {
             // as the plugin "partition" was deprecated in mysql 5.9
             // and as it was removed from mysql 8 and replaced by the native partitioning one,
             // we need to check the current version and db before failing this step
@@ -638,11 +643,9 @@ class PartEngine
 
                 return true;
             }
-            return false;
         }
-        unset($config);
 
-        return true;
+        return false;
     }
 
     /**
