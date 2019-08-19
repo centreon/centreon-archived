b503ad49520107a12f608152ccec187da2abe044
Laurent Calvet <laurent@callapa.com>
2019-08-19 14:51:43 +0200

    Add the Symfony PHPUnit bridge

--- tests/php/Centreon/Domain/Entity/EntityCreatorTest.php
+++ tests/php/Centreon/Domain/Entity/EntityCreatorTest.php
@@ -5,23 +5,22 @@ namespace Tests\Centreon\Domain\Entity;
 use Centreon\Domain\Entity\EntityCreator;
 use Centreon\Domain\Monitoring\Host;
 use PHPUnit\Framework\TestCase;
-use JMS\Serializer\Annotation\Groups;
 
 class EntityCreatorTest extends TestCase
 {
-
     public function testCreateByArray()
     {
         $now = new \DateTime();
 
         $data = [
-            ['host_id' => 1, 'last_notification' => $now->getTimestamp()],
+            'host_id' => 1,
+            'last_notification' => $now->getTimestamp()
         ];
         $host = EntityCreator::createEntityByArray(
             Host::class,
             $data
         );
         $this->assertEquals(1, $host->getId());
-        $this->assertEquals($host->getLastNotification(), $now);
+        $this->assertEquals($host->getLastNotification()->getTimestamp(), $now->getTimestamp());
     }
 }
