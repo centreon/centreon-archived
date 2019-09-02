3123874eb07233cb5372429be279aa686dbf92ca
Loic Laurent <loic.lau@gmail.com>
2019-09-02 15:54:45 +0200

    update for legacy

--- www/install/sql/centreon/Update-DB-19.10.0-beta.3.sql
+++ www/install/sql/centreon/Update-DB-19.10.0-beta.3.sql
@@ -6,3 +6,5 @@ ALTER TABLE nagios_server ADD COLUMN `remote_server_centcore_ssh_proxy` enum('0'
 -- Add severity preference on host-monitoring widget
 INSERT INTO `widget_parameters_field_type` (`ft_typename`, `is_connector`) VALUES
 ('hostSeverityMulti', 1);
+
+INSERT INTO `options` (`key`, `value`) VALUES ('inheritance_mode', 1);
