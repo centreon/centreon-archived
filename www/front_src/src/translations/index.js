addaca76f4b7466f20e266a32bcc2d9c0b23e630
Kevin Duret <kduret@centreon.com>
2019-09-04 10:24:05 +0200

    fix(front): redirect to login page when user is unauthorized (#7687)

--- www/front_src/src/translations/index.js
+++ www/front_src/src/translations/index.js
@@ -5,17 +5,24 @@ const translationService = axios("internal.php?object=centreon_i18n&action=trans
 const userService = axios("internal.php?object=centreon_topcounter&action=user");
 
 export default function setTranslations(store, callback) {
-    const localePromise = userService.get();
-    const translationsPromise = translationService.get();
-    Promise
-        .all([localePromise, translationsPromise])
-        .then(response => {
-            let locale = response[0].data.locale;
-            locale = locale != null ? locale.slice(0,2) : navigator.language;
-            const translations = response[1].data
-            syncTranslationWithStore(store);
-            store.dispatch(loadTranslations(translations));
-            store.dispatch(setLocale(locale));
-            callback();
-        });
+  const localePromise = userService.get();
+  const translationsPromise = translationService.get();
+
+  Promise
+    .all([localePromise, translationsPromise])
+    .then((response) => {
+      let locale = response[0].data.locale;
+      locale = locale !== null ? locale.slice(0,2) : navigator.language;
+      const translations = response[1].data;
+      syncTranslationWithStore(store);
+      store.dispatch(loadTranslations(translations));
+      store.dispatch(setLocale(locale));
+      callback();
+    })
+    .catch((error) => {
+      if (error.response.status === 401) {
+        // redirect to login page
+        window.location.href = 'index.php?disconnect=1';
+      }
+    });
 };
