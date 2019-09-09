cd0a33c757c3e15188fc0315f244d4277b60758a
Kevin Duret <kduret@centreon.com>
2019-09-09 16:53:38 +0200

    feat(front): add additional poller wizard field in poller form

--- www/front_src/src/components/forms/poller/PollerFormStepTwo.js
+++ www/front_src/src/components/forms/poller/PollerFormStepTwo.js
@@ -5,43 +5,112 @@
 
 import React, { Component } from 'react';
 import { Field, reduxForm as connectForm } from 'redux-form';
-import { Translate, I18n } from 'react-redux-i18n';
 import styles from '../../../styles/partials/form/_form.scss';
-import SelectField from '../../form-fields/SelectField';
-import CheckboxField from '../../form-fields/CheckboxField';
+import SelectField from "../../form-fields/SelectField";
+import CheckboxField from "../../form-fields/CheckboxField";
+import Select from "react-select";
+import fieldHoc from "../../form-fields/hoc";
 
 class PollerFormStepTwo extends Component {
+  state = {
+    selectedMaster: null,
+    selectedAdditionals: [],
+  };
+
+  /**
+   * Get available additionals remote server
+   * ==> all remote servers except selected master
+   */
+  getAvailableAdditionals = () => {
+    const { pollers } = this.props;
+    const { selectedMaster } = this.state;
+
+    const availableAdditionals = pollers.filter((poller) => {
+      if (poller.id !== selectedMaster) {
+        return true;
+      }
+    });
+
+    return availableAdditionals;
+  }
+
+  handleChangeMaster = (event, value) => {
+    const { change } = this.props;
+    const { selectedAdditionals } = this.state;
+
+    // remove selected additional if it's already the master
+    const filteredAdditionals = value
+      ? selectedAdditionals.filter((additional) => {
+          if (additional.value !== value) {
+            return true;
+          }
+        })
+      : [];
+
+    // update field value (mandatory cause it is connected to redux-form)
+    change('linked_remote_slaves', filteredAdditionals);
+
+    this.setState({
+      selectedMaster: value,
+      selectedAdditionals: filteredAdditionals,
+    });
+  }
+
+  handleChangeAdditionals = (event, values) => {
+    this.setState({
+      selectedAdditionals: values
+    });
+  }
+
   render() {
     const { error, handleSubmit, onSubmit, pollers } = this.props;
+    const { selectedMaster, selectedAdditionals } = this.state;
+
+    const availableAdditionals = this.getAvailableAdditionals();
 
     return (
-      <div className={styles['form-wrapper']}>
-        <div className={styles['form-inner']}>
-          <div className={styles['form-heading']}>
-            <h2 className={styles['form-title']}>
-              <Translate value="Attach poller to a server" />
-            </h2>
-          </div>
-          <form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles["form-wrapper"]}>
+        <div className={styles["form-inner"]}>
+          <form autocomplete="off" onSubmit={handleSubmit(onSubmit)}>
             {pollers ? (
-              <Field
-                name="linked_remote"
-                component={SelectField}
-                options={[
-                  {
-                    disabled: true,
-                    selected: true,
-                    text: `${I18n.t('Select a Remote Server')}`,
-                    value: '',
-                  },
-                ].concat(
-                  pollers.map((c) => ({
-                    value: c.id,
-                    label: c.name,
-                    text: c.name,
-                  })),
-                )}
-              />
+              <>
+                <h2 className={styles["form-title"]}>
+                  <Translate value="Attach poller to a master remote server"/>
+                </h2>
+                <Field
+                  name="linked_remote_master"
+                  component={SelectField}
+                  options={[
+                    {
+                      selected: true,
+                      text: '',
+                      value: null,
+                    }
+                  ].concat(
+                    pollers.map(c => ({ value: c.id, label: c.name, text:c.name }))
+                  )}
+                  onChange={this.handleChangeMaster}
+                />
+              </>
+            ) : null}
+            {selectedMaster && pollers.length >= 2 ? (
+              <>
+                <h2 className={styles["form-title"]}>
+                  <Translate value="Attach poller to additional remote servers"/>
+                </h2>
+                <div className={styles["form-item"]}>
+                  <Field
+                    name="linked_remote_slaves"
+                    component={fieldHoc(Select)}
+                    options={availableAdditionals.map(remote => ({
+                      value: remote.id,
+                      label: remote.name
+                    }))}
+                    isMulti
+                    onChange={this.handleChangeAdditionals}
+                  />
+                </div>
+              </>
             ) : null}
             <Field
               name="open_broker_flow"
