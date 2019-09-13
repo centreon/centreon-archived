ef351340e13f73c8751e46b33dccf2bc15821b8c
Kevin Duret <kduret@centreon.com>
2019-09-13 11:18:22 +0200

    move to beta.3

--- www/install/sql/centreon/Update-DB-19.10.0-beta.3.sql
+++ www/install/sql/centreon/Update-DB-19.10.0-beta.3.sql
@@ -6,3 +6,11 @@ ALTER TABLE nagios_server ADD COLUMN `remote_server_centcore_ssh_proxy` enum('0'
 -- Add severity preference on host-monitoring widget
 INSERT INTO `widget_parameters_field_type` (`ft_typename`, `is_connector`) VALUES
 ('hostSeverityMulti', 1);
+
+-- Remove broker correlation mechanism
+ALTER TABLE `cfg_centreonbroker` DROP COLUMN `correlation_activate`;
+DELETE FROM `cb_field` WHERE
+  `displayname` = 'Correlation file'
+  OR `description` LIKE 'File where correlation%'
+  OR `displayname` = 'Correlation passive';
+DELETE FROM `cb_type` WHERE `type_shortname` = 'correlation';
