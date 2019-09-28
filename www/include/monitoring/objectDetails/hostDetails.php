03350384f7c2c1f6c5608dd325fbf981af9c68c6
Kevin Duret <kduret@centreon.com>
2019-09-28 08:03:51 +0200

    fix(sec): remove xss injection of service output in host form (#7865)

--- www/include/monitoring/objectDetails/hostDetails.php
+++ www/include/monitoring/objectDetails/hostDetails.php
@@ -211,7 +211,10 @@ if (!$is_admin && !$haveAccess) {
             $row['line_class'] = $class;
 
             /* Split the plugin_output */
-            $outputLines = explode("\n", $row['plugin_output']);
+            $outputLines = explode(
+                "\n",
+                htmlentities($row['plugin_output'], ENT_QUOTES, 'UTF-8')
+            );
             $row['short_output'] = $outputLines[0];
             $row["hnl"] = CentreonUtils::escapeSecure(urlencode($row["host_name"]));
             $row["sdl"] = CentreonUtils::escapeSecure(urlencode($row["service_description"]));
