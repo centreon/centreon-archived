a48b30c2f3c7c2f79563523a4ce1f95f2bec3d8e
Laurent Pinsivy <lpinsivy@centreon.com>
2019-10-03 14:34:45 +0200

    enh(ceip): improve message during web install/update

--- www/install/step_upgrade/step5.php
+++ www/install/step_upgrade/step5.php
@@ -76,15 +76,16 @@ if ($stat === false) {
                         <tr>
                             <td class=\'formValue\'>
                                 <div class=\'md-checkbox md-checkbox-inline\'>
-                                    <input id=\'send_statistics\' value=\'1\' name=\'send_statistics\' type=\'checkbox\' checked=\'checked\'/>
+                                    <input id=\'send_statistics\' value=\'1\' name=\'send_statistics\' type=\'checkbox\' checked=\'checked\' type=\'hidden\'/>
                                     <label class=\'empty-label\' for=\'send_statistics\'></label>
                                 </div>
                             </td>
                             <td class=\'formlabel\'>
-                                <p style="text-align:justify">I agree to participate to the Centreon Customer Experience
-                                    Improvement Program whereby anonymous information about the usage of this server
-                                    may be sent to Centreon. This information will solely be used to improve the
-                                    software user experience. I will be able to opt-out at anytime.
+                                <p style="text-align:justify">Centreon uses a telemetry system and a Centreon Customer Experience
+                                Improvement Program whereby anonymous information about the usage of this server
+                                may be sent to Centreon. This information will solely be used to improve the
+                                software user experience. You will be able to opt-out at any time about CEIP program
+                                through administration menu.
                                     Refer to
                                     <a target="_blank" style="text-decoration: underline" 
                                     href="http://ceip.centreon.com/">ceip.centreon.com</a>
