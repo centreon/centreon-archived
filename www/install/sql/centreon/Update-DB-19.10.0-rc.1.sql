d1cfc03c2a50e573d001532316b4f10563fcf651
Laurent Pinsivy <lpinsivy@centreon.com>
2019-10-03 11:54:02 +0200

    fix(install): move SQL update to Centreon 19.10 RC.1

---
+++ www/install/sql/centreon/Update-DB-19.10.0-rc.1.sql
@@ -0,0 +1,7 @@
+-- Create rs_poller_relation for the additional relationship between poller and remote servers
+CREATE TABLE IF NOT EXISTS `rs_poller_relation` (
+  `remote_server_id` int(11) NOT NULL,
+  `poller_server_id` int(11) NOT NULL,
+  KEY `remote_server_id` (`remote_server_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Relation Table For centreon pollers and remote servers';
+
