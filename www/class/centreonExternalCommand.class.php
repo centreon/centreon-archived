d2ac2e609b0c3ad4e080fe8c261d5d7c8f9f53db
Laurent Pinsivy <lpinsivy@centreon.com>
2019-10-04 11:43:57 +0200

    fix(externalcmd): create centcore file by action (#6985)

--- www/class/centreonExternalCommand.class.php
+++ www/class/centreonExternalCommand.class.php
@@ -132,7 +132,7 @@ class CentreonExternalCommand
             if ($this->debug) {
                 print "COMMAND BEFORE SEND: $str_remote";
             }
-            $result = file_put_contents($varlib . '/centcore.cmd', $str_remote, FILE_APPEND);
+            $result = file_put_contents($varlib . '/centcore/' . microtime(true) . '-externalcommand.cmd', $str_remote, FILE_APPEND);
             $return_remote = ($result !== false) ? 0 : 1;
         }
 
