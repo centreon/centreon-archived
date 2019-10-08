134ec1d46de0c6d17dca90944472acd456434a78
Kevin Duret <kduret@centreon.com>
2019-10-08 15:41:46 +0200

    migrate duration column to bigint

--- www/install/sql/centstorage/Update-CSTG-19.10.0-rc.2.sql
+++ www/install/sql/centstorage/Update-CSTG-19.10.0-rc.2.sql
@@ -1,2 +1,3 @@
--- migrate downtimes.end_time column from int(11) to bigint(20)
+-- migrate downtimes.end_time & downtimes.duration columns from int(11) to bigint(20)
 ALTER TABLE `downtimes` MODIFY COLUMN `end_time` BIGINT(20) NULL DEFAULT NULL;
+ALTER TABLE `downtimes` MODIFY COLUMN `duration` BIGINT(20) NULL DEFAULT NULL;
