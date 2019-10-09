32d959d83613618aadba26f594f8746a367a55db
sc979 <34628915+sc979@users.noreply.github.com>
2019-10-09 17:46:26 +0200

    fix(UI): contact list using escapeSecure method (#7947)
    * fix(UI): contact list using escapeSecure method

--- www/class/centreonUtils.class.php
+++ www/class/centreonUtils.class.php
@@ -345,7 +345,7 @@ class CentreonUtils
                     ENT_QUOTES,
                     "UTF-8"
                 );
-                return preg_replace("/[" . $pattern . "]/", "", $stringToEscape);
+                return str_replace(str_split($pattern), "", $stringToEscape);
         }
     }
 
