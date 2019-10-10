3f6b5182d4738333418688683c538e2d8df9c4b3
Kevin Duret <kduret@centreon.com>
2019-10-10 11:25:06 +0200

    remove upgrade of bigint columns

--- www/install/sql/centstorage/Update-CSTG-19.10.0-rc.2.sql
+++ www/install/sql/centstorage/Update-CSTG-19.10.0-rc.2.sql
@@ -1,7 +1,3 @@
--- migrate downtimes.end_time & downtimes.duration columns from int(11) to bigint(20)
-ALTER TABLE `downtimes` MODIFY COLUMN `end_time` BIGINT(20) NULL DEFAULT NULL;
-ALTER TABLE `downtimes` MODIFY COLUMN `duration` BIGINT(20) NULL DEFAULT NULL;
-
 -- Remove useless reporting tables
 ALTER TABLE log_archive_host
   DROP COLUMN UPTimeAverageAck,
