12332ae54411a905d13a1b2ad1782aadf0f2d74c
Laurent Calvet <lcalvet@centreon.com>
2019-10-11 23:00:05 +0200

    Update src/Centreon/Infrastructure/Monitoring/MonitoringRepositoryRDB.php
    Co-Authored-By: Kevin Duret <kduret@centreon.com>

--- src/Centreon/Infrastructure/Monitoring/MonitoringRepositoryRDB.php
+++ src/Centreon/Infrastructure/Monitoring/MonitoringRepositoryRDB.php
@@ -513,7 +513,7 @@ final class MonitoringRepositoryRDB extends AbstractRepositoryDRB implements Mon
                 INNER JOIN `:db`.`acl_groups` acg
                   ON acg.acl_group_id = acl.group_id
                   AND acg.acl_group_activate = \'1\'
-                  AND acg.acl_group_id IN (' . $this->accessGroupIdToString($this->accessGroups). ') ';
+                  AND acg.acl_group_id IN (' . $this->accessGroupIdToString($this->accessGroups) . ') ';
 
         $request =
             'SELECT DISTINCT srv.*
