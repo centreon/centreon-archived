centreon/centreon-archived

commit f41e5408e4eb8ddcfa376808574b90b4e850a860

fix(monitoring service): Fix columns on the list page (#6984)

www/include/monitoring/status/Services/service.php
@@ -486,9 +486,8 @@ $tpl->display("service.ihtml");
 
     jQuery(function () {
         preInit();
-        /* Disable to prevent double Ajax call*/
-        //updateSelect();
     });
+
     function preInit()
     {
         _keyPrefix = '<?php echo $keyPrefix; ?>';
@@ -544,7 +543,5 @@ $tpl->display("service.ihtml");
     function statusServices(value, isInit)
     {
         _o = value;
-        window.clearTimeout(_timeoutID);
-        initM(_tm, _sid, _o);
     }
 </script>

www/include/monitoring/status/Common/commonJS.php
@@ -990,7 +990,12 @@ function monitoring_play()  {
     document.getElementById('JS_monitoring_pause_gray').style.display = 'none';
     document.getElementById('JS_monitoring_play_gray').style.display = 'block';
     _on = 1;
-    initM(<?php echo $tM?>,"<?php echo $sid?>","<?php echo $o?>");
+
+    // Allows to use the new status when click on the play button
+    if (typeof(_o) == "undefined") {
+        _o = "<?= $o ?>";
+    }
+    initM(<?php echo $tM?>, "<?php echo $sid?>", _o);
 }
 
 function monitoring_pause() {

www/include/monitoring/status/Services/serviceJS.php
@@ -32,7 +32,6 @@
  * For more information : contact@centreon.com
  *
  */
-
 if (!isset($centreon)) {
     exit();
 }
@@ -170,7 +169,6 @@ var _criticality_id = 0;
     }
 
     function initM(_time_reload,_sid,_o){
-
         // INIT Select objects
         construct_selecteList_ndo_instance('instance_selected');
         construct_HostGroupSelectList('hostgroups_selected');
@@ -201,7 +199,6 @@ var _criticality_id = 0;
             _criticality_id = document.getElementById("critFilter").value;
             viewDebugInfo('service criticality: '+document.getElementById("critFilter").value);
         }
-
         if (_first){
             mainLoop();
             _first = 0;
@@ -213,6 +210,9 @@ var _criticality_id = 0;
     }
 
     function goM(_time_reload,_sid,_o){
+        if (_on == 0) {
+            return;
+        }
         _lock = 1;
         var proc = new Transformation();
 
@@ -223,10 +223,10 @@ var _criticality_id = 0;
             document.getElementById("output_search").value = _output_search;
             _counter += 1;
         }
-        
+
         var statusService = jQuery.trim(jQuery('#statusService').val());
         var statusFilter = jQuery.trim(jQuery('#statusFilter').val());
-               
+
         proc.setCallback(monitoringCallBack);
         proc.setXml(_addrXML+"?"+'&search='+_search+'&search_host='+_host_search+'&search_output='+_output_search+'&num='+_num+'&limit='+_limit+'&sort_type='+_sort_type+'&order='+_order+'&date_time_format_status='+_date_time_format_status+'&o='+_o+'&p='+_p+'&host_name=<?php echo $host_name; ?>'+'&nc='+_nc+'&criticality='+_criticality_id+'&statusService='+statusService+'&statusFilter='+statusFilter+"&sSetOrderInMemory="+sSetOrderInMemory);
         proc.setXslt(_addrXSL);
@@ -235,7 +235,11 @@ var _criticality_id = 0;
         }
 
         _lock = 0;
+        if (_timeoutID) { // Kill next execution if in queue
+            clearTimeout(_timeoutID);
+        }
         _timeoutID = cycleVisibilityChange('goM("'+ _time_reload +'","'+ _sid +'","'+_o+'")', _time_reload);
+
         _time_live = _time_reload;
         _on = 1;
 

commit 0edfbfcfbbf970f0558a99650e692f75617b7b1a

Fix status details not reloading when filter changed, #7306
The problem was introduced by f41e5408e4eb8ddcfa376808574b90b4e850a860

www/include/monitoring/status/Services/service.php
@@ -543,5 +543,7 @@ $tpl->display("service.ihtml");
     function statusServices(value, isInit)
     {
         _o = value;
+        window.clearTimeout(_timeoutID);
+        initM(_tm, _sid, _o);
     }
 </script>