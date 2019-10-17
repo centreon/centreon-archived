239ab5e2e1136c7d1f47e3d0b658f2f14b99e6c5
CPbN <40244829+CPbN@users.noreply.github.com>
2019-10-17 08:06:45 +0200

    Move to 19.10.2

--- www/install/sql/centreon/Update-DB-19.10.0-beta.1.sql
+++ www/install/sql/centreon/Update-DB-19.10.0-beta.1.sql
@@ -75,10 +75,3 @@ ALTER TABLE ods_view_details MODIFY metric_id int(11);
 
 -- Add trap filter
 ALTER TABLE `traps` MODIFY COLUMN `traps_exec_interval_type` ENUM('0','1','2','3') NULL DEFAULT '0';
-
---
--- Update topology of service grid / by host group / by service group
---
-UPDATE topology SET topology_url_opt = '&o=svcOV_pb' WHERE topology_page = 20204;
-UPDATE topology SET topology_url_opt = '&o=svcOVHG_pb' WHERE topology_page = 20209;
-UPDATE topology SET topology_url_opt = '&o=svcOVSG_pb' WHERE topology_page = 20212;
