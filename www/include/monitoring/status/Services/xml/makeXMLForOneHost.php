56493938752f2038f9fc467ec65b6e2ac0e7a98d
schapron <schapron@centreon.com>
2019-10-30 11:36:27 +0100

    fix(secu): sanitize makeXMLForOneHost.php

--- www/include/monitoring/status/Services/xml/makeXMLForOneHost.php
+++ www/include/monitoring/status/Services/xml/makeXMLForOneHost.php
@@ -62,15 +62,7 @@ $centreonlang->bindLang();
 /*
  * Check Arguments From GET tab
  */
-$host_id = $obj->checkArgument("host_id", $_GET, 0);
-$enable = $obj->checkArgument("enable", $_GET, "");
-$disable = $obj->checkArgument("disable", $_GET, "disable");
-$dateFormat = $obj->checkArgument("date_time_format_status", $_GET, "Y/m/d H:i:s");
-
-$host_id = filter_var(
-    $host_id ?? null,
-    FILTER_VALIDATE_INT
-);
+$host_id = filter_input(INPUT_GET, 'host_id', FILTER_VALIDATE_INT, array('options' => array('default' =>  false)));
 
 if ($host_id === false) {
     print _("Bad host ID");
