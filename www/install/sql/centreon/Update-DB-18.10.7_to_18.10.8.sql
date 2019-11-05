7369aa8308163e44e9aed64c6bdf5933a3d96d07
loiclau <loic.lau@gmail.com>
2019-11-05 14:33:36 +0100

    fix(sql) fix default contact_autologin_key value

--- www/install/sql/centreon/Update-DB-18.10.7_to_18.10.8.sql
+++ www/install/sql/centreon/Update-DB-18.10.7_to_18.10.8.sql
@@ -1,2 +1,5 @@
 -- Change version of Centreon
 UPDATE `informations` SET `value` = '18.10.8' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.7' LIMIT 1;
+
+-- fix default contact_autologin_key
+UPDATE `contact` SET `contact_autologin_key` = NULL WHERE `contact_autologin_key` ='';
