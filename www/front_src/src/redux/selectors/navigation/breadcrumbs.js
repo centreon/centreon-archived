087a1c302993d1c24cf888ef076b53d36822a959
Kevin Duret <kduret@centreon.com>
2019-11-12 09:07:14 +0100

    fix(breacrumb): fix breacrumb url for parent's levels

--- www/front_src/src/redux/selectors/navigation/breadcrumbs.js
+++ www/front_src/src/redux/selectors/navigation/breadcrumbs.js
@@ -1,15 +1,32 @@
 import { createSelector } from 'reselect';
 
 /**
+ * get URL from legacy or react pages
+ * @param {Object} item
+ * @return {String} build URL
+ */
+function getUrl(item) {
+  return item.is_react
+    ? item.url
+    : `/main.php?p=${item.page}${item.options !== null ? item.options : ''}`;
+}
+
+/**
  * loop on each group/child to get first url
  * @param {Object} item
  * @return {String|undefined} first url found
  */
 function findFirstUrl(item) {
+  if (item.url) {
+    return getUrl(item);
+  }
+
   if (item.groups) {
     const groupWithUrl = item.groups.find(findFirstUrl);
 
-    return groupWithUrl && groupWithUrl.children ? getFirstUrlInChildren(groupWithUrl) : undefined;
+    return groupWithUrl && groupWithUrl.children
+      ? getFirstUrlInChildren(groupWithUrl)
+      : undefined;
   }
 
   return item.children ? getFirstUrlInChildren(item) : undefined;
@@ -25,25 +42,8 @@ function getFirstUrlInChildren(item) {
     return undefined;
   }
 
-  const childWithUrl = item.children.find((child) => child.url);
-
-  if (childWithUrl) {
-    return getUrl(childWithUrl);
-  }
-
-  if (item.children) {
-    const childWithUrl = item.children.find(findFirstUrl);
-    return childWithUrl ? findFirstUrl(childWithUrl) : undefined;
-  }
-}
-
-/**
- * get URL from legacy or react pages
- * @param {Object} item
- * @return {String} build URL
- */
-function getUrl(item) {
-  return item.is_react ? item.url : `/main.php?p=${item.page}${item.options !== null ? item.options : ''}`;
+  const childrenWithUrl = item.children.find(findFirstUrl);
+  return childrenWithUrl ? findFirstUrl(childrenWithUrl) : undefined;
 }
 
 /**
@@ -52,24 +52,24 @@ function getUrl(item) {
  * @return {Object|null} breadcrumb step information
  */
 function getBreadcrumbStep(item) {
-    const availableUrl = item.url ? getUrl(item) : findFirstUrl(item);
-    return availableUrl
-      ? {
+  const availableUrl = item.url ? getUrl(item) : findFirstUrl(item);
+  return availableUrl
+    ? {
         label: item.label,
         link: availableUrl,
       }
-      : null;
+    : null;
 }
 
 const getNavigationItems = (state) => state.navigation.items;
 
-export const breadcrumbsSelector = createSelector(
+const breadcrumbsSelector = createSelector(
   getNavigationItems,
   (navItems) => {
-    let breadcrumbs = {};
+    const breadcrumbs = {};
 
     // build level 1 breadcrumbs
-    navItems.map((itemLvl1) => {
+    navItems.forEach((itemLvl1) => {
       const stepLvl1 = getBreadcrumbStep(itemLvl1);
       if (stepLvl1 === null) {
         return;
@@ -78,12 +78,12 @@ export const breadcrumbsSelector = createSelector(
         {
           label: stepLvl1.label,
           link: stepLvl1.link,
-        }
+        },
       ];
 
       // build level 2 breadcrumbs
       if (itemLvl1.children) {
-        itemLvl1.children.map((itemLvl2) => {
+        itemLvl1.children.forEach((itemLvl2) => {
           const stepLvl2 = getBreadcrumbStep(itemLvl2);
           if (stepLvl2 === null) {
             return;
@@ -101,9 +101,9 @@ export const breadcrumbsSelector = createSelector(
 
           // build level 3 breadcrumbs
           if (itemLvl2.groups) {
-            itemLvl2.groups.map((groupLvl3) => {
+            itemLvl2.groups.forEach((groupLvl3) => {
               if (groupLvl3.children) {
-                groupLvl3.children.map((itemLvl3) => {
+                groupLvl3.children.forEach((itemLvl3) => {
                   const stepLvl3 = getBreadcrumbStep(itemLvl3);
                   if (stepLvl3 === null) {
                     return;
@@ -133,3 +133,5 @@ export const breadcrumbsSelector = createSelector(
     return breadcrumbs;
   },
 );
+
+export default breadcrumbsSelector;
