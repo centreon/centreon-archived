9a7c075700ed6912566480269582df39f60f25c2
schapron <schapron@centreon.com>
2019-11-13 17:29:36 +0100

    fix(ldap): correct the LDAP DN during the upgrade

--- www/install/php/Update-19.10.2.php
+++ www/install/php/Update-19.10.2.php
@@ -45,3 +45,28 @@ try {
         "UPGRADE : 19.10.2 Unable to delete ldap auto-imported users with empty contact_name"
     );
 }
+
+// correct the DN of manually imported users from an LDAP
+try {
+    // finding the data of contacts linked to an LDAP
+    $stmt = $this->pearDB->query(
+        "SELECT contact_id, contact_name, contact_ldap_dn FROM contact WHERE ar_id is NOT NULL"
+    );
+    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+        // removing the double slashes if needed and saving the corrected data
+        if (strpos($row['contact_ldap_dn'], "\\\\")) {
+            $newDn = str_replace("\\\\", "\\", $row['contact_ldap_dn']);
+            $updateDB = $this->pearDB->prepare(
+                "UPDATE contact SET contact_ldap_dn = :newDn WHERE contact_id = :contactId"
+            );
+            $updateDB->bindValue(':newDn', $newDn, \PDO::PARAM_STR);
+            $updateDB->bindValue(':contactId', $row['contact_id'], \PDO::PARAM_INT);
+            $updateDB->execute();
+        }
+    }
+} catch (\PDOException $e) {
+    $centreonLog->insertLog(
+        2,
+        "UPGRADE : 19.10.2 Unable to correct the LDAP DN data"
+    );
+}
