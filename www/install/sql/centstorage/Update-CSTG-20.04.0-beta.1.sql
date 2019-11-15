cae8ac3cd69d9b3f18af9741fbd762003cc38d7d
Laurent Calvet <laurent@callapa.com>
2019-11-15 22:13:24 +0100

    Added change unique key

--- www/install/sql/centstorage/Update-CSTG-20.04.0-beta.1.sql
+++ www/install/sql/centstorage/Update-CSTG-20.04.0-beta.1.sql
@@ -1,2 +1,8 @@
-ALTER TABLE `comments` MODIFY COLUMN `service_id` int(11) NOT NULL;
-ALTER TABLE `downtimes` MODIFY COLUMN `service_id` int(11) NOT NULL;
+UPDATE `comments` SET `service_id` = 0 WHERE `service_id` IS NULL;
+ALTER TABLE `comments` MODIFY COLUMN `service_id` int(11) NOT NULL DEFAULT 0;
+
+UPDATE `downtimes` SET `service_id` = 0 WHERE `service_id` IS NULL;
+ALTER TABLE `downtimes`
+    MODIFY COLUMN `service_id` int(11) NOT NULL DEFAULT 0,
+    DROP INDEX `entry_time`,
+    ADD UNIQUE KEY `entry_time` (`entry_time`, `host_id`, `service_id`, `instance_id`, `internal_id`);
