70213b393c796454d6251ecdd005d19bad0ffeff
loiclau <loic.lau@gmail.com>
2019-11-18 10:21:27 +0100

    fix(sql) add missing column for engine configuration (#8125)

--- www/install/sql/centreon/Update-DB-18.10.7_to_18.10.8.sql
+++ www/install/sql/centreon/Update-DB-18.10.7_to_18.10.8.sql
@@ -3,3 +3,7 @@ UPDATE `informations` SET `value` = '18.10.8' WHERE CONVERT( `informations`.`key
 
 -- fix default contact_autologin_key
 UPDATE `contact` SET `contact_autologin_key` = NULL WHERE `contact_autologin_key` ='';
+
+-- add columns for engine configuration
+ALTER TABLE `cfg_nagios` ADD COLUMN `enable_macros_filter` ENUM('0', '1') DEFAULT '0';
+ALTER TABLE `cfg_nagios` ADD COLUMN `macros_filter` TEXT DEFAULT '';
