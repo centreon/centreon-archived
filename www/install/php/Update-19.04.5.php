352ba99344e8f37b130d9e3b056b9d891c0f82f1
schapron <schapron@centreon.com>
2019-11-26 09:42:54 +0100

    fix(upgrade): moving scipt to next minor

--- www/install/php/Update-19.04.5.php
+++ www/install/php/Update-19.04.5.php
@@ -47,35 +47,3 @@ try {
         "UPGRADE : 19.04.5 Unable to modify contact autologin key default value"
     );
 }
-
-// remove LDAP users missing contact name
-// these users have been added using the auto-import LDAP feature and will be re-imported at their next login.
-try {
-    $pearDB->query('DELETE FROM contact WHERE contact_name = ""');
-} catch (\PDOException $e) {
-    $centreonLog->insertLog(
-        2,
-        "UPGRADE : 19.04.5 Unable to delete LDAP auto-imported users with empty contact_name"
-    );
-}
-
-// correct the DN of manually imported users from an LDAP
-try {
-    // finding the data of contacts linked to an LDAP
-    $stmt = $pearDB->query("SELECT contact_id, contact_name, contact_ldap_dn FROM contact WHERE ar_id is NOT NULL");
-    $updateDB = $pearDB->prepare("UPDATE contact SET contact_ldap_dn = :newDn WHERE contact_id = :contactId");
-    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-        // removing the double slashes if needed and saving the corrected data
-        if (strpos($row['contact_ldap_dn'], "\\\\")) {
-            $newDn = str_replace("\\\\", "\\", $row['contact_ldap_dn']);
-            $updateDB->bindValue(':newDn', $newDn, \PDO::PARAM_STR);
-            $updateDB->bindValue(':contactId', $row['contact_id'], \PDO::PARAM_INT);
-            $updateDB->execute();
-        }
-    }
-} catch (\PDOException $e) {
-    $centreonLog->insertLog(
-        2,
-        "UPGRADE : 19.04.5 Unable to correct the LDAP DN data"
-    );
-}
