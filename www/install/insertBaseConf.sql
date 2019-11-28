8213b909ddb7e8a42108eae577fea11edc7a2c63
Adrien Morais <31647811+adr-mo@users.noreply.github.com>
2019-11-28 20:02:49 +0100

    fix(topology) Correct URL options for service pages (#8164)
    * chore(build): update version to 19.10.4
    
    * fix(topology) Correct URL options for service pages

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '19.10.3');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '19.10.4');
 
 --
 -- Contenu de la table `contact`
