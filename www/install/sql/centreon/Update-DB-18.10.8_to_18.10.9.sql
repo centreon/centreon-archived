6a717e7578ba338e5dcaa9e62c90ff11ef4b9006
Matthieu Kermagoret <mkermagoret@centreon.com>
2019-12-02 11:14:21 +0100

    chore(install): update version to 18.10.9.

---
+++ www/install/sql/centreon/Update-DB-18.10.8_to_18.10.9.sql
@@ -0,0 +1,2 @@
+-- Change version of Centreon
+UPDATE `informations` SET `value` = '18.10.9' WHERE CONVERT( `informations`.`key` USING utf8 ) = 'version' AND CONVERT ( `informations`.`value` USING utf8 ) = '18.10.8' LIMIT 1;
