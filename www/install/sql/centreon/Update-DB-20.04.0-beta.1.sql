c35333758f99fc7e20dc5da6c5d9729bd37f0298
Loic Laurent <loic.lau@gmail.com>
2019-12-10 09:18:07 +0100

    update comment

--- www/install/sql/centreon/Update-DB-20.04.0-beta.1.sql
+++ www/install/sql/centreon/Update-DB-20.04.0-beta.1.sql
@@ -11,7 +11,7 @@ UPDATE topology SET topology_url_opt = '&o=svcOV_pb' WHERE topology_page = 20204
 UPDATE topology SET topology_url_opt = '&o=svcOVHG_pb' WHERE topology_page = 20209;
 UPDATE topology SET topology_url_opt = '&o=svcOVSG_pb' WHERE topology_page = 20212;
 
--- Update topology of service grid / by host group / by service group
+-- Update nagios_server to add gorgone connection
 ALTER TABLE `nagios_server` ADD `gorgone_communication_type` enum('1','2') NOT NULL DEFAULT '1' AFTER `centreonconnector_path`;
 ALTER TABLE `nagios_server` CHANGE `ssh_port` `gorgone_port` INT(11) NULL;
 ALTER TABLE `nagios_server` CHANGE `remote_server_centcore_ssh_proxy` `remote_server_use_as_proxy` enum('0','1') NOT NULL DEFAULT '1';
