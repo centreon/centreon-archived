68708a73da605afd94e76241bf45187d75b39648
loiclau <loic.lau@gmail.com>
2019-12-10 09:13:39 +0100

    Update www/include/configuration/configServers/formServers.php
    Co-Authored-By: Colin Gagnaire <cgagnaire@centreon.com>

--- www/include/configuration/configServers/formServers.php
+++ www/include/configuration/configServers/formServers.php
@@ -220,7 +220,7 @@ if (strcmp($serverType, 'poller') ==  0) {
     $form->addElement('select2', 'remote_additional_id', _('Attach additional Remote Servers'), array(), $attrPoller2);
     $tab = [];
     $tab[] = $form->createElement('radio', 'remote_server_use_as_proxy', null, _("Yes"), '1');
-    $tab[] = $form->createElement('radio', 'is_proxy', null, _("No"), '0');
+    $tab[] = $form->createElement('radio', 'remote_server_use_as_proxy', null, _("No"), '0');
     $form->addGroup($tab, 'ish_proxy', _("Use the Remote Server as a proxy"), '&nbsp;');
 }
 $form->addElement('text', 'nagios_bin', _("Monitoring Engine Binary"), $attrsText2);
