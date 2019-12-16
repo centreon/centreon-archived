b33304e9f9f31a3686956736432675d8b6769ebf
Loic Laurent <loic.lau@gmail.com>
2019-12-16 10:16:31 +0100

    update admin-param-centcore form

--- www/install/sql/centreon/Update-DB-20.04.0-beta.1.sql
+++ www/install/sql/centreon/Update-DB-20.04.0-beta.1.sql
@@ -14,4 +14,4 @@ UPDATE topology SET topology_url_opt = '&o=svcOVSG_pb' WHERE topology_page = 202
 
 UPDATE options SET `key` = 'gorgone_illegal_characters' WHERE `key` = 'centcore_illegal_characters';
 UPDATE options SET `key` = 'gorgone_cmd_timeout' WHERE `key` = 'centcore_cmd_timeout';
-UPDATE topology SET topology_url_opt = '&o=gorgone' WHERE topology_page = 50117;
+UPDATE topology SET topology_url_opt = '&o=gorgone', topology_name = 'Gorgone' WHERE topology_page = 50117;
