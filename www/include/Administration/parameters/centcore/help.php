6c5ea2e5a9610cf171102334ff8fbc2eef69cbfc
Loic Laurent <loic.lau@gmail.com>
2019-12-18 09:49:39 +0100

    clean unused option enable_perfdata_sync

--- www/include/Administration/parameters/centcore/help.php
+++ www/include/Administration/parameters/centcore/help.php
@@ -4,10 +4,6 @@ $help = array();
 /**
  * Centcore Settings
  */
-$help['tip_enable_perfdata_sync'] = dgettext(
-    'help',
-    'Enable Perfdata synchronisation between poller and Central Server operated by Centcore'
-);
 $help['tip_enable_logs_sync'] = dgettext(
     'help',
     'Enable Monitoring Engine Logs synchronisation between poller and Central Server operated by Centcore'
