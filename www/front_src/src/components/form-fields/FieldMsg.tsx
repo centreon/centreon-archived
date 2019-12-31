e697997509dcc0388e774a2fb3473253e31a5b33
tdarneix <tdarneix@centreon.com>
2019-12-31 10:44:05 +0100

    Convert FieldMsg to typescript

--- www/front_src/src/components/form-fields/FieldMsg.tsx
+++ www/front_src/src/components/form-fields/FieldMsg.tsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prop-types */
 
-import React from 'react';
+import React, {ReactNode} from 'react';
 import classnames from 'classnames';
 import styles from '../../styles/partials/form/_form.scss';
+
+interface Props {
+  className: string;
+  children: ReactNode;
+  isError: boolean;
+  tagName: ReactNode;
+  restProps: object;
+}
 
 const FieldMsg = ({
   className,
@@ -11,7 +19,7 @@ const FieldMsg = ({
   isError,
   tagName: FieldMsgTagName,
   ...restProps
-}) => (
+}: Props) => (
   <FieldMsgTagName
     className={classnames(styles.field__msg, styles[className], {
       [styles['field__msg--error']]: !!isError,
