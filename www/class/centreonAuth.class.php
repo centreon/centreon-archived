77581f53c4dece724421d30c4cc62acc19683380
CPbN <40244829+CPbN@users.noreply.github.com>
2020-01-09 13:18:01 +0100

    enh(sec) Improve authentication messages in login.log (#7943)

--- www/class/centreonAuth.class.php
+++ www/class/centreonAuth.class.php
@@ -306,32 +306,32 @@ class CentreonAuth
                 }
             }
 
-            if ($this->userInfos["contact_oreon"]
-                || ($this->userInfos["contact_oreon"] == 0 && $this->source == 'API')
-            ) {
-                /*
-                 * Check password matching
-                 */
-                $this->getCryptFunction();
-                $this->checkPassword($password, $token);
-
-                if ($this->passwdOk == 1) {
+            /*
+             * Check password matching
+             */
+            $this->getCryptFunction();
+            $this->checkPassword($password, $token);
+            if ($this->passwdOk == 1) {
+                if ($this->userInfos["contact_oreon"]
+                    || ($this->userInfos["contact_oreon"] == 0 && $this->source == 'API')
+                ) {
                     $this->CentreonLog->setUID($this->userInfos["contact_id"]);
                     $this->CentreonLog->insertLog(
                         1,
-                        "[".$this->source."] Contact '" . $username . "' logged in - IP : " . $_SERVER["REMOTE_ADDR"]
+                        "[" . $this->source . "] [" . $_SERVER["REMOTE_ADDR"] . "] Authentication succeeded for '" . $username . "'"
                     );
                 } else {
                     $this->CentreonLog->insertLog(
                         1,
-                        "Contact '" . $username . "' doesn't match with password"
+                        "[" . $this->source . "] [" . $_SERVER["REMOTE_ADDR"] . "] '" . $username . "' is not allowed to reach Centreon"
                     );
                     $this->error = _('Your credentials are incorrect.');
                 }
             } else {
+                //  Take care before modifying this message pattern as it may break tools such as fail2ban
                 $this->CentreonLog->insertLog(
                     1,
-                    "[".$this->source."] Contact '" . $username . "' is not enable for reaching centreon"
+                    "[" . $this->source . "] [" . $_SERVER["REMOTE_ADDR"] . "] Authentication failed for '" . $username . "'"
                 );
                 $this->error = _('Your credentials are incorrect.');
             }
@@ -366,10 +366,11 @@ class CentreonAuth
                 }
             }
         } else {
-            if ($this->debug) {
+            if (strlen($username) > 0) {
+                //  Take care before modifying this message pattern as it may break tools such as fail2ban
                 $this->CentreonLog->insertLog(
                     1,
-                    "[".$this->source."] No contact found with this login : '$username'"
+                    "[" . $this->source . "] [" . $_SERVER["REMOTE_ADDR"] . "] Authentication failed for '" . $username . "' : not found"
                 );
             }
             $this->error = _('Your credentials are incorrect.');
