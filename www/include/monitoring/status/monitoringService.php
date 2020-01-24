6b9daeee2a9c3b3b2244c8a098986215e522bf68
Laurent Pinsivy <lpinsivy@centreon.com>
2020-01-24 12:03:07 +0100

    fix(code): scheduling queue console does not exist anymore

--- www/include/monitoring/status/monitoringService.php
+++ www/include/monitoring/status/monitoringService.php
@@ -117,7 +117,6 @@ $pathDetails = "./include/monitoring/objectDetails/";
 $svc_path = $path . "Services/";
 $hg_path = $path . "ServicesHostGroups/";
 $sg_path = $path . "ServicesServiceGroups/";
-$path_sch = $path . "Scheduling/";
 
 if ($continue) {
     switch ($o) {
@@ -197,12 +196,6 @@ if ($continue) {
             break;
         case "svcSumHG":
             require_once $hg_path . "serviceSummaryByHG.php";
-            break;
-        /*
-         * Scheduling Queue
-         */
-        case "svcSch":
-            require_once $path_sch . "serviceSchedule.php";
             break;
         default:
             require_once $svc_path . "service.php";
