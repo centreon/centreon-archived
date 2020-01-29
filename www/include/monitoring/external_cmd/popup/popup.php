a71086ecf32cae13194411322241fa67c0bb9000
sc979 <34628915+sc979@users.noreply.github.com>
2020-01-29 16:23:37 +0100

    Update www/include/monitoring/external_cmd/popup/popup.php

--- www/include/monitoring/external_cmd/popup/popup.php
+++ www/include/monitoring/external_cmd/popup/popup.php
@@ -61,7 +61,7 @@ if (!isset($centreon) ||
 $sid = session_id();
 if (isset($sid)) {
     $res = $pearDB->prepare("SELECT * FROM `session` WHERE `session_id` = :sid");
-    $res->bindValue(':sid', $sid, PDO::PARAM_STR);
+    $res->bindValue(':sid', session_id(), PDO::PARAM_STR);
     $res->execute();
     if (!$session = $res->fetch()) {
         exit();
