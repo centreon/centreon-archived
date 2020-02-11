ea53ac23a9b7b8560d3678773bac1f03e0392d51
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-02-11 09:35:17 +0100

    chore(install): update version to 19.04.10.

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '19.04.9');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '19.04.10');
 
 --
 -- Contenu de la table `contact`
