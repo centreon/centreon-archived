5589ef5477089112e72463e455ae225ed3e51349
Laurent Pinsivy <lpinsivy@centreon.com>
2020-02-11 11:11:36 +0100

    enh(mobile): add third level menu in mobile (#8320)

--- www/include/common/mobile_menu.php
+++ www/include/common/mobile_menu.php
@@ -12,11 +12,30 @@
             <?php if (!empty($subMenu['children'])) : ?>
                 <ul class="nav-items nav-expand-content">
                 <?php foreach ($subMenu['children'] as $index2 => $subMenu2) : ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="main.php?p=<?= substr($index2, 1) ?>">
-                            <?= $subMenu2['label'] ?>
-                        </a>
-                    </li>
+                    <?php if (!empty($subMenu2['children'])) : ?>
+                        <li class="nav-item nav-expand">
+                            <a class="nav-link nav-expand-link" href="#">
+                                <?= $subMenu2['label'] ?>
+                            </a>
+                            <ul class="nav-items nav-expand-content">
+                            <?php foreach ($subMenu2['children'] as $childrens) : ?>
+                                <?php foreach ($childrens as $index3 => $subMenu3) : ?>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="main.php?p=<?= substr($index3, 1) . $subMenu3['options'] ?>">
+                                            <?= $subMenu3['label'] ?>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            <?php endforeach; ?>
+                            </ul>
+                        </li>
+                    <?php else : ?>
+                        <li class="nav-item">
+                            <a class="nav-link" href="main.php?p=<?= substr($index2, 1) ?>">
+                                <?= $subMenu2['label'] ?>
+                            </a>
+                        </li>
+                    <?php endif; ?>
                 <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
