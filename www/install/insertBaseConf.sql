4a41ed6f226c72dc9ab8bcba4c36a0e6195317ae
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-02-11 09:16:15 +0100

    chore(install): update version to 2.8.32.

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.31');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '2.8.32');
 
 --
 -- Contenu de la table `contact`
