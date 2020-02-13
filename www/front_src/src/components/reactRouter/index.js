2fcc1f1f061a3dfd4b9f41bb677747b1b2b19f1f
Bruno d'Auria <bdauria@centreon.com>
2020-02-13 16:07:13 +0100

    Refactor reactRouter
    Co-authored-by: Kevin Duret <duret.kevin@gmail.com>

--- www/front_src/src/components/reactRouter/index.js
+++ www/front_src/src/components/reactRouter/index.js
@@ -2,46 +2,45 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable react/prop-types */
 
-import React, { Component, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import reactRoutes from '../../route-maps';
 import { dynamicImport } from '../../helpers/dynamicImport';
-import centreonAxios from '../../axios';
 import NotAllowedPage from '../../route-components/notAllowedPage';
 import BreadcrumbWrapper from '../breadcrumbWrapper';
 import styles from '../../styles/partials/_content.scss';
 import { allowedPagesSelector } from '../../redux/selectors/navigation/allowedPages';
 
-// class to manage internal react pages
-class ReactRouter extends Component {
-  getLoadableComponents = () => {
-    const { history, isNavigationFetched, allowedPages, pages } = this.props;
-    const basename = history.createHref({
-      pathname: '/',
-      search: '',
-      hash: '',
-    });
-    const LoadableComponents = [];
+const LoadableComponents = ({
+  history,
+  isNavigationFetched,
+  allowedPages,
+  pages,
+}) => {
+  const basename = history.createHref({
+    pathname: '/',
+    search: '',
+    hash: '',
+  });
 
-    // wait acl to add authorized routes
-    if (!isNavigationFetched) {
-      return LoadableComponents;
-    }
+  // wait acl to add authorized routes
+  if (!isNavigationFetched) {
+    return null;
+  }
 
-    for (const [path, parameter] of Object.entries(pages)) {
-      // check if each acl route contains external page
-      // eg: a user which have access to /configuration will have access to /configuration/hosts
-      let isAllowed = false;
-      for (const route of allowedPages) {
-        if (path.includes(route)) {
-          isAllowed = true;
-        }
-      }
+  const pageEntries = Object.entries(pages);
+  const isAllowedPage = (path) =>
+    allowedPages.find((allowedPage) => path.includes(allowedPage));
 
-      if (isAllowed) {
+  const loadablePages = pageEntries.filter(([path]) => isAllowedPage(path));
+
+  return (
+    <>
+      {loadablePages.map(([path, parameter]) => {
         const Page = React.lazy(() => dynamicImport(basename, parameter));
-        LoadableComponents.push(
+
+        return (
           <Route
             key={path}
             path={path}
@@ -49,62 +48,73 @@ class ReactRouter extends Component {
             render={(renderProps) => (
               <div className={styles['react-page']}>
                 <BreadcrumbWrapper path={path}>
-                  <Page centreonAxios={centreonAxios} {...renderProps} />
+                  <Page {...renderProps} />
                 </BreadcrumbWrapper>
               </div>
             )}
-          />,
+          />
         );
-      }
-    }
+      })}
+    </>
+  );
+};
 
-    return LoadableComponents;
-  };
-
-  render() {
-    const { isNavigationFetched, allowedPages, fetched } = this.props;
-
-    if (!isNavigationFetched || !fetched) {
-      return null;
-    }
-
-    const LoadableComponents = this.getLoadableComponents();
-
-    return (
-      <Suspense fallback={null}>
-        <Switch>
-          {reactRoutes.map(({ path, comp: Comp, ...rest }) => (
-            <Route
-              key={path}
-              path={path}
-              exact
-              render={(renderProps) => (
-                <div className={styles['react-page']}>
-                  {allowedPages.includes(path) ? (
-                    <BreadcrumbWrapper path={path}>
-                      <Comp {...renderProps} />
-                    </BreadcrumbWrapper>
-                  ) : (
-                    <NotAllowedPage {...renderProps} />
-                  )}
-                </div>
-              )}
-              {...rest}
-            />
-          ))}
-          {LoadableComponents}
-          {fetched && (
-            <Route
-              component={
-                NotAllowedPage // wait external components are fetched to avoid quick display of "not allowed" page
-              }
-            />
-          )}
-        </Switch>
-      </Suspense>
-    );
+// Component to manage internal react pages
+const ReactRouter = ({
+  isNavigationFetched,
+  allowedPages,
+  fetched,
+  history,
+  pages,
+}) => {
+  if (!isNavigationFetched || !fetched) {
+    return null;
   }
-}
+
+  console.log('reactRouter rendered');
+
+  console.log(`Pages: ${JSON.stringify(pages)}`);
+
+
+  return (
+    <Suspense fallback={null}>
+      <Switch>
+        {reactRoutes.map(({ path, comp: Comp, ...rest }) => (
+          <Route
+            key={path}
+            path={path}
+            exact
+            render={(renderProps) => (
+              <div className={styles['react-page']}>
+                {allowedPages.includes(path) ? (
+                  <BreadcrumbWrapper path={path}>
+                    <Comp {...renderProps} />
+                  </BreadcrumbWrapper>
+                ) : (
+                  <NotAllowedPage {...renderProps} />
+                )}
+              </div>
+            )}
+            {...rest}
+          />
+        ))}
+        <LoadableComponents
+          history={history}
+          isNavigationFetched={isNavigationFetched}
+          allowedPages={allowedPages}
+          pages={pages}
+        />
+        {fetched && (
+          <Route
+            component={
+              NotAllowedPage // wait external components are fetched to avoid quick display of "not allowed" page
+            }
+          />
+        )}
+      </Switch>
+    </Suspense>
+  );
+};
 
 const mapStateToProps = (state) => ({
   isNavigationFetched: state.navigation.fetched,
