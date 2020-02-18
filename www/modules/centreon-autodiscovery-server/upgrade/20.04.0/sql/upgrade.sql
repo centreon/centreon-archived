b99d7f56d6c96f2372205169bae5c5dc553ce5fd
Laurent Calvet <laurent@callapa.com>
2020-02-18 18:15:17 +0100

    Remove module files

--- www/modules/centreon-autodiscovery-server/upgrade/20.04.0/sql/upgrade.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE `mod_host_disco_provider_mapping`;
-ALTER TABLE `mod_host_disco_provider` DROP COLUMN test_option;
-ALTER TABLE `mod_host_disco_provider` ADD `attributes` TEXT DEFAULT NULL NULL;
-ALTER TABLE `mod_host_disco_provider` ADD need_proxy TINYINT UNSIGNED DEFAULT 0 NOT NULL;
-ALTER TABLE `mod_host_disco_provider` DROP COLUMN default_template;
-ALTER TABLE `mod_host_disco_provider` ADD host_template_id INT NULL;
-
