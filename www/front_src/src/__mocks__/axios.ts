e28e836c170b3b2d392dc3ac6fba7bf7ba7b016d
Bruno d'Auria <bdauria@centreon.com>
2020-02-26 17:36:35 +0100

    Update packages

--- www/front_src/src/__mocks__/axios.ts
+++ www/front_src/src/__mocks__/axios.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-ignore */
+
 const mockAxios = jest.genMockFromModule('axios');
 
+// @ts-ignore
 mockAxios.create = jest.fn(() => mockAxios);
+
+// @ts-ignore
 mockAxios.CancelToken = {
   source: () => ({
     token: {},
