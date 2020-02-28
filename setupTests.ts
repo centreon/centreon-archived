a1b12aeaa5853e165001c76c26e396b9aa677089
Bruno d'Auria <bdauria@centreon.com>
2020-02-28 10:16:58 +0100

    Remove jest config

--- setupTests.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-
-// eslint-disable-next-line @typescript-eslint/no-namespace
-declare namespace jest {
-  interface Matchers<R> {
-    toBeInTheDocument(): R;
-  }
-}
