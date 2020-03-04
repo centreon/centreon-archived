13f7a8fcabed0c8bbd6fa097bf1a305b15f3fa7e
Kevin Duret <kduret@centreon.com>
2020-03-04 16:05:44 +0100

    fix coding style

--- src/Centreon/Infrastructure/RequestParameters/SqlRequestParametersTranslator.php
+++ src/Centreon/Infrastructure/RequestParameters/SqlRequestParametersTranslator.php
@@ -183,7 +183,8 @@ class SqlRequestParametersTranslator
      * @return string Part of the database query.
      * @throws RequestParametersTranslatorException
      */
-    private function createQueryOnKeyValue(string $key, $valueOrArray): string{
+    private function createQueryOnKeyValue(string $key, $valueOrArray): string
+    {
         if ($this->requestParameters->getConcordanceStrictMode() === RequestParameters::CONCORDANCE_MODE_STRICT
             && !key_exists($key, $this->concordanceArray)
         ) {
