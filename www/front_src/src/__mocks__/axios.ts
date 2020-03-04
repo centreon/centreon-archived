9c57da659bf791b9ea3a18d63fb91f4f304af817
Kevin Duret <kduret@centreon.com>
2020-03-04 17:03:05 +0100

    fix coding style

--- www/front_src/src/__mocks__/axios.ts
+++ www/front_src/src/__mocks__/axios.ts
@@ -7,6 +7,7 @@ mockAxios.create = jest.fn(() => mockAxios);
 
 // @ts-ignore
 mockAxios.CancelToken = {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   source: () => ({
     token: {},
     cancel: jest.fn(),
