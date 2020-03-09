433be3dd075a94bed30464e6080ec10794b7665e
schapron <schapron@centreon.com>
2020-03-09 10:24:23 +0100

    fix: correct method and condition

--- install.sh
+++ install.sh
@@ -226,14 +226,14 @@ echo -e "\t$(gettext "Check mandatory gorgone service status")"
 echo -e "$line"
 
 yes_no_default "$(gettext "Is the Gorgone module already installed?")"
-if [ "$?" -nq 0 ] ; then
+if [ "$?" -ne 0 ] ; then
     echo_failure "$(gettext "Gorgone is required.\nPlease install it before launching this script")\n" "$fail"
     echo -e "\n$(gettext "Please read the documentation to manage the Gorgone daemon installation")"
     echo -e "\t$(gettext "Available on github") : https://github.com/centreon/centreon-gorgone"
     echo -e "\t$(gettext "or on the centreon documentation") : https://documentation.centreon.com/\n"
     exit 1
 fi
-locate_centreon_gorgone
+locate_gorgone_varlib
 check_gorgone_user
 check_gorgone_group
 
