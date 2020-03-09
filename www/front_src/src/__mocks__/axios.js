db5e7cd2810c0f9d223c99b72acb114b901647f0
Bruno d'Auria <bdauria@centreon.com>
2020-03-09 22:18:57 +0100

    use plain JavaScript for axios mock

--- www/front_src/src/__mocks__/axios.js
+++ www/front_src/src/__mocks__/axios.js
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
-
 const mockAxios = jest.genMockFromModule('axios');
 
-// @ts-ignore
 mockAxios.create = jest.fn(() => mockAxios);
 
-// @ts-ignore
 mockAxios.CancelToken = {
   source: () => ({
     token: {},
