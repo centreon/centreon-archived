ed9d5f4e4411048fdb22bf24a61b9f2fe24b4a96
Laurent Pinsivy <lpinsivy@centreon.com>
2020-03-16 12:00:11 +0100

    enh(code): use filter_var instead of regexp

--- www/class/centreon-clapi/centreonInstance.class.php
+++ www/class/centreon-clapi/centreonInstance.class.php
@@ -137,9 +137,8 @@ class CentreonInstance extends CentreonObject
         }
 
         // Check IPv6, IPv4 and FQDN format
-        if ($params[1] == 'ns_ip_address' && !preg_match('/^([0-9a-fA-F]{4}|0)(\:([0-9a-fA-F]{4}|0)){7}$/', $params[2])
-            && !(preg_match('/^[0-9\.]+$/', $params[2]) && preg_match('/^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){4}$/', $params[2]))
-            && !(!preg_match('/^[0-9\.]+$/', $params[2]) && preg_match('/^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]+)\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/', $params[2]))
+        if ($params[1] == 'ns_ip_address' && !filter_var($params[2], FILTER_VALIDATE_DOMAIN)
+            && !filter_var($params[2], FILTER_VALIDATE_IP)
         ) {
             throw new CentreonClapiException(self::INCORRECTIPADDRESS);
         }
