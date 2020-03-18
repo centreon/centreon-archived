52a0c463d392c270a13c847cbde9b7a0e53dc70a
sc979 <34628915+sc979@users.noreply.github.com>
2020-03-18 10:48:28 +0100

    correct default value

--- www/include/reporting/dashboard/xmlInformations/GetXmlServiceGroup.php
+++ www/include/reporting/dashboard/xmlInformations/GetXmlServiceGroup.php
@@ -35,7 +35,7 @@
 $stateType = 'service';
 require_once realpath(dirname(__FILE__) . "/initXmlFeed.php");
 
-$color = array_filter($_GET['color'] ?? false, function($oneColor) {
+$color = array_filter($_GET['color'] ?? [], function($oneColor) {
     return filter_var($oneColor, FILTER_VALIDATE_REGEXP, [
         'options' => [
             'regexp' => "/^#[[:xdigit:]]{6}$/"
