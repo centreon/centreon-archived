b6b2b158d18b15dea89f3b80204b83b7a2eda14a
CRubin <54900686+CoralieRubin@users.noreply.github.com>
2020-03-20 17:00:49 +0100

    Update www/front_src/src/components/breadcrumbWrapper/Breadcrumb.jsx
    Co-Authored-By: Tom Darneix <tomdar87@outlook.com>

--- www/front_src/src/components/breadcrumbWrapper/Breadcrumb.jsx
+++ www/front_src/src/components/breadcrumbWrapper/Breadcrumb.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+import { makeStyles, Breadcrumbs} from '@material-ui/core';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 import BreadcrumbLink from './Link';
