804d698d94368ea76201c0a97b4723cd47e27719
Lalie <crubin@centreon.com>
2020-03-24 09:43:41 +0100

    delete index.stories and move Breadcrumb index and link in breadcrumbWrapper

---
+++ www/front_src/src/components/breadcrumbWrapper/Breadcrumb.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+
+import { makeStyles, Breadcrumbs } from '@material-ui/core/styles';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+import BreadcrumbLink from './Link';
+
+const useStyles = makeStyles({
+  breadcrumb: {
+    padding: '4px 16px',
+  },
+  item: {
+    display: 'flex',
+  },
+});
+
+const Breadcrumb = ({ breadcrumbs }) => {
+  const classes = useStyles();
+
+  return (
+    <Breadcrumbs
+      classes={{ root: classes.breadcrumb, li: classes.item }}
+      separator={<NavigateNextIcon fontSize="small" />}
+      aria-label="Breadcrumb"
+    >
+      {breadcrumbs &&
+        breadcrumbs.map((breadcrumb, index) => (
+          <BreadcrumbLink
+            key={`${breadcrumb.label}${breadcrumb.index}`}
+            breadcrumb={breadcrumb}
+            index={index}
+            count={breadcrumbs.length}
+          />
+        ))}
+    </Breadcrumbs>
+  );
+};
+
+Breadcrumb.propTypes = {
+  breadcrumbs: PropTypes.arrayOf(PropTypes.shape).isRequired,
+};
+
+export default Breadcrumb;
