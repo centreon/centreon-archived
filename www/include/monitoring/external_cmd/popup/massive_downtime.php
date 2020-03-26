0abb9bddb1dafc605f507a3aa7350c4c9c4dda34
sc979 <34628915+sc979@users.noreply.github.com>
2020-03-26 12:00:38 +0100

    style

--- www/include/monitoring/external_cmd/popup/massive_downtime.php
+++ www/include/monitoring/external_cmd/popup/massive_downtime.php
@@ -154,8 +154,9 @@ if (
     && $centreon->optGen['monitoring_dwt_duration']
 ) {
     $defaultDuration = $centreon->optGen['monitoring_dwt_duration'];
-    if (isset($centreon->optGen['monitoring_dwt_duration_scale']) &&
-        $centreon->optGen['monitoring_dwt_duration_scale']
+    if (
+        isset($centreon->optGen['monitoring_dwt_duration_scale'])
+        && $centreon->optGen['monitoring_dwt_duration_scale']
     ) {
         $defaultScale = $centreon->optGen['monitoring_dwt_duration_scale'];
     }
