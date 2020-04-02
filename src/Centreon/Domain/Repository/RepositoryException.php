c63e87f7534d706c7cff3d03c89e616bfb08d620
Laurent Calvet <laurent@callapa.com>
2020-04-02 09:47:08 +0200

    Reverses unnecessary changes

--- src/Centreon/Domain/Repository/RepositoryException.php
+++ src/Centreon/Domain/Repository/RepositoryException.php
@@ -25,8 +25,4 @@ use Throwable;
 
 class RepositoryException extends \Exception
 {
-    public function __construct ($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2005 - 2020 Centreon (https://www.centreon.com/)
+ * Copyright 2016 Centreon
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * For more information : contact@centreon.com
- *
  */
 
 // mock path constants to redirect to base centreon directory
