fcdd54b5674a5a29b51250b6b87591a61eff824a
Matthieu Kermagoret <mkermagoret@centreon.com>
2020-04-14 12:05:48 +0200

    fix(install): make all supported upgrade scripts compatible with strict mode.

--- www/install/sql/centreon/Update-DB-19.10.2.sql
+++ www/install/sql/centreon/Update-DB-19.10.2.sql
@@ -1,2 +1,4 @@
 --new updated field of pollers-
+SET SESSION innodb_strict_mode=OFF;
 ALTER TABLE `nagios_server` ADD COLUMN `updated` enum('0','1') NOT NULL DEFAULT '0';
+SET SESSION innodb_strict_mode=ON;
