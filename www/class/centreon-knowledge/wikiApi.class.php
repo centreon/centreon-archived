24c51881924857a7a887f762392848f08216c3c8
garnier-quentin <garnier.quentin@gmail.com>
2020-04-14 16:59:55 +0200

    Fix #8124 #7790: pr reviews

--- www/class/centreon-knowledge/wikiApi.class.php
+++ www/class/centreon-knowledge/wikiApi.class.php
@@ -271,11 +271,9 @@ class WikiApi
                     $postfields['apfrom'] = $result->{'continue'}->apcontinue;
                     $continue = true;
                 }
-            } else {
-                if (isset($result->{'query-continue'}->allpages->apcontinue)) {
-                    $postfields['apfrom'] = $result->{'query-continue'}->allpages->apcontinue;
-                    $continue = true;
-                }
+            } else if (isset($result->{'query-continue'}->allpages->apcontinue)) {
+                $postfields['apfrom'] = $result->{'query-continue'}->allpages->apcontinue;
+                $continue = true;
             }
         } while ($continue === true);
 
