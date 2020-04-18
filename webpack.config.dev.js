e847b648e8a57090df5c436f618f00b7469b70a5
Bruno d'Auria <bdauria@centreon.com>
2020-04-18 12:25:12 +0200

    chore(dev): Setup webpack-dev-server with HMR

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,29 @@
 const merge = require('webpack-merge');
-const LiveReloadPlugin = require('webpack-livereload-plugin');
 const path = require('path');
 
 const devConfig = require('@centreon/frontend-core/webpack/patch/dev');
 const baseConfig = require('./webpack.config');
 
+const devServerPort = 9090;
+
 module.exports = merge(baseConfig, devConfig, {
+  output: {
+    publicPath: `http://localhost:${devServerPort}/static/`,
+  },
   resolve: {
     alias: {
       react: path.resolve('./node_modules/react'),
+      'react-dom': '@hot-loader/react-dom',
       'react-router-dom': path.resolve('./node_modules/react-router-dom'),
       '@material-ui/core': path.resolve('./node_modules/@material-ui/core'),
     },
   },
-  plugins: [new LiveReloadPlugin({ appendScriptTag: true })],
+  devServer: {
+    contentBase: path.resolve(`${__dirname}/www/modules/`),
+    compress: true,
+    port: devServerPort,
+    hot: true,
+    watchContentBase: true,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const merge = require('webpack-merge');
 const path = require('path');
 
@@ -6,7 +7,11 @@ const baseConfig = require('@centreon/frontend-core/webpack/base');
 const extractCssConfig = require('@centreon/frontend-core/webpack/patch/extractCss');
 
 module.exports = merge(baseConfig, extractCssConfig, {
-  entry: ['@babel/polyfill', './www/front_src/src/index.js'],
+  entry: [
+    'react-hot-loader/patch',
+    '@babel/polyfill',
+    './www/front_src/src/index.js',
+  ],
   output: {
     path: path.resolve(`${__dirname}/www/static`),
     publicPath: './static/',
@@ -20,21 +25,23 @@ module.exports = merge(baseConfig, extractCssConfig, {
   },
   plugins: [
     new HtmlWebpackPlugin({
+      alwaysWriteToDisk: true,
       template: './www/front_src/public/index.html',
       filename: '../index.html',
     }),
+    new HtmlWebpackHarddiskPlugin(),
   ],
   module: {
     rules: [
       { parser: { system: false } },
       {
-        test: /fonts(\\|\/).+\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /fonts(\\|\/).+\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?|\.(bmp|png|jpg|jpeg|gif|svg)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: 'url-loader',
             options: {
               name: '[name].[hash:8].[ext]',
-              publicPath: './',
+              limit: 10000,
             },
           },
         ],
@@ -42,18 +49,6 @@ module.exports = merge(baseConfig, extractCssConfig, {
       {
         test: /\.icon.svg$/,
         use: ['@svgr/webpack'],
-      },
-      {
-        test: /\.(bmp|png|jpg|jpeg|gif|svg)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000,
-              name: '[name].[hash:8].[ext]',
-            },
-          },
-        ],
       },
     ],
   },
