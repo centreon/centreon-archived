a506c45d3eef751cb2a5c5e93390569b8598b642
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2020-04-30 14:48:01 +0200

    fix(monitoring): change SQL Request to get ServiceGroups as non-admin user (#8529)

--- www/include/monitoring/status/Common/commonJS.php
+++ www/include/monitoring/status/Common/commonJS.php
@@ -427,11 +427,8 @@ if (!$centreon->user->access->admin) {
     $query = "SELECT DISTINCT sg.sg_alias, sg.sg_name AS name
                 FROM servicegroup sg, acl_resources_sg_relations arsr
                 WHERE sg.sg_id = arsr.sg_id
-                    AND arsr.acl_res_id IN (".$centreon->user->access->getResourceGroupsString().")
-                    AND sg.sg_activate = '1'
-                    AND sg.sg_id in (SELECT servicegroup_sg_id
-                FROM servicegroup_relation
-                WHERE service_service_id IN (".$centreon->user->access->getServicesString("ID", $acldb)."))";
+                    AND arsr.acl_res_id IN (" . $centreon->user->access->getResourceGroupsString() . ")
+                    AND sg.sg_activate = '1'";
     $DBRESULT = $pearDB->query($query);
     while ($data = $DBRESULT->fetchRow()) {
         $sgBrk[$data["name"]] = 1;
