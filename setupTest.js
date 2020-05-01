3484a34cb6bdd14bd02356e44f9bceb651f43963
Bruno d'Auria <bdauria@centreon.com>
2020-05-01 12:14:42 +0200

    Remove react-transition-group config

--- setupTest.js
+++ setupTest.js
@@ -1,8 +1,3 @@
-import { config } from 'react-transition-group';
-
-// This prevents jsdom from maitaining the Select listbox open after clicking on an item
-config.disabled = true;
-
 document.createRange = () => ({
   setStart: () => {},
   setEnd: () => {},
