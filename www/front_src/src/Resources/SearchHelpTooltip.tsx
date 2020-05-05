51a4743cc80be1638752473d5618491d603be4d2
Kevin Duret <kduret@centreon.com>
2020-05-05 10:26:01 +0200

    fix security issue with target _blank

--- www/front_src/src/Resources/SearchHelpTooltip.tsx
+++ www/front_src/src/Resources/SearchHelpTooltip.tsx
@@ -81,7 +81,7 @@ const Content = ({ onClose }: ContentProps): JSX.Element => {
         <i>
           <b>{`${labelTips}: `}</b>
           {`${labelGetRegexHelp} `}
-          <Link href="https://regex101.com" target="_blank">
+          <Link href="https://regex101.com" target="_blank" rel="noopener">
             regex101.com
           </Link>
         </i>
