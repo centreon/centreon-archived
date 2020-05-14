1bc9eb794f3314eb8ea26f45953ce6db0481a6dd
Kevin Duret <kduret@centreon.com>
2020-05-14 08:47:33 +0200

    fix(APIv1): fix authorize check on webservices added directly in container (#8697)
    Refs: BAM-1005

--- src/Centreon/Infrastructure/Webservice/WebserviceAutorizeRestApiInterface.php
+++ src/Centreon/Infrastructure/Webservice/WebserviceAutorizeRestApiInterface.php
@@ -38,4 +38,13 @@ namespace Centreon\Infrastructure\Webservice;
 
 interface WebserviceAutorizeRestApiInterface
 {
+    /**
+     * Authorize to access to the action
+     *
+     * @param string $action The action name
+     * @param \CentreonUser $user The current user
+     * @param boolean $isInternal If the api is call in internal
+     * @return boolean If the user has access to the action
+     */
+    public function authorize($action, $user, $isInternal = false);
 }
