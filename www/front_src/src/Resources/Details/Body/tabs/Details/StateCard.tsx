8fb434011fb4589444e665d696a9f5e805b8e190
Bruno d'Auria <bdauria@centreon.com>
2020-06-01 11:14:42 +0200

    feat(ui): Add Downtime and Acknowledge comments in Events view details panel (#8755)

--- www/front_src/src/Resources/Details/Body/tabs/Details/StateCard.tsx
+++ www/front_src/src/Resources/Details/Body/tabs/Details/StateCard.tsx
@@ -1,11 +1,40 @@
 import * as React from 'react';
 
-import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
+import { labelComment } from '../../../../translatedLabels';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 2fr auto',
+    gridTemplateAreas: ` 
+      'content-title content chip'
+      'comment-title comment chip'
+      `,
+    gridGap: theme.spacing(2),
+  },
+  contentTitle: {
+    gridArea: 'content-title',
+  },
+  content: {
+    gridArea: 'content',
+  },
+  chip: {
+    gridArea: 'chip',
+  },
+  commentTitle: {
+    gridArea: 'comment-title',
+  },
+  comment: {
+    gridArea: 'comment',
+  },
+}));
 
 interface Props {
   title: string;
   contentLines: Array<string>;
   chip: JSX.Element;
+  commentLine: string;
 }
 
 const Line = (line): JSX.Element => (
@@ -14,21 +43,37 @@ const Line = (line): JSX.Element => (
   </Typography>
 );
 
-const StateCard = ({ title, contentLines, chip }: Props): JSX.Element => {
+const StateCard = ({
+  title,
+  contentLines,
+  commentLine,
+  chip,
+}: Props): JSX.Element => {
+  const classes = useStyles();
+
   return (
     <Card>
       <CardContent>
-        <Grid container spacing={4} alignItems="center">
-          <Grid item>
-            <Typography variant="subtitle2" color="textSecondary">
-              {title}
-            </Typography>
-          </Grid>
-          <Grid item style={{ flexGrow: 1 }}>
-            {contentLines.map(Line)}
-          </Grid>
-          <Grid item>{chip}</Grid>
-        </Grid>
+        <div className={classes.container}>
+          <Typography
+            className={classes.contentTitle}
+            variant="subtitle2"
+            color="textSecondary"
+          >
+            {title}
+          </Typography>
+          <div className={classes.content}>{contentLines.map(Line)}</div>
+
+          <Typography
+            className={classes.commentTitle}
+            variant="subtitle2"
+            color="textSecondary"
+          >
+            {labelComment}
+          </Typography>
+          <div className={classes.comment}>{Line(commentLine)}</div>
+          <div className={classes.chip}>{chip}</div>
+        </div>
       </CardContent>
     </Card>
   );
