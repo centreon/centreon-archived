ad0e786e8fbbc84a3d064ab755b1f39b162fa293
Loic Laurent <loic.lau@gmail.com>
2020-06-02 14:28:40 +0200

    bind query

--- www/include/home/customViews/triggers/loadServiceFromHost.php
+++ www/include/home/customViews/triggers/loadServiceFromHost.php
@@ -70,19 +70,21 @@ try {
         );
         $sql = "SELECT service_id, service_description
         		FROM service s, host_service_relation hsr
-        		WHERE hsr.host_host_id = " . (int)$hostId . "
+        		WHERE hsr.host_host_id = :hostId
         		AND hsr.service_service_id = s.service_id ";
         $sql .= $aclString;
         $sql .= " UNION ";
         $sql .= " SELECT service_id, service_description
         		FROM service s, host_service_relation hsr, hostgroup_relation hgr
         		WHERE hsr.hostgroup_hg_id = hgr.hostgroup_hg_id
-        		AND hgr.host_host_id = " . (int)$hostId . "
+        		AND hgr.host_host_id = :hostId
         		AND hsr.service_service_id = s.service_id ";
         $sql .= $aclString;
         $sql .= " ORDER BY service_description ";
-        $res = $db->query($sql);
-        while ($row = $res->fetchRow()) {
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':hostId', $hostId, PDO::PARAM_INT);
+        $stmt->execute();
+        while ($row = $stmt->fetchRow()) {
             $xml->startElement('option');
             $xml->writeElement('id', $row['service_id']);
             $xml->writeElement('label', $row['service_description']);
