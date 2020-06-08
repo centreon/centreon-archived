75a631f25e5f57c09d05d674577eeb1cb1426294
loiclau <loic.lau@gmail.com>
2020-06-08 15:33:18 +0200

    Update www/include/configuration/configObject/meta_service/formMetaService.php
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/include/configuration/configObject/meta_service/formMetaService.php
+++ www/include/configuration/configObject/meta_service/formMetaService.php
@@ -253,7 +253,7 @@ $form->addElement('textarea', 'meta_comment', _("Comments"), $attrsTextarea);
 
 $form->registerRule('validate_geo_coords', 'function', 'validateGeoCoords');
 $form->addElement('text', 'geo_coords', _("Geo coordinates"), $attrsText);
-$form->addRule('geo_coords', _("geo coords not valid"), 'validate_geo_coords');
+$form->addRule('geo_coords', _("geo coords are not valid"), 'validate_geo_coords');
 
 $form->addElement('hidden', 'meta_id');
 $redirect = $form->addElement('hidden', 'o');
