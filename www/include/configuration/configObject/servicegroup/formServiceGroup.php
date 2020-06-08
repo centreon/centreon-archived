16f93b2d13ec1e17568313cb65fbdf2a7ccad63d
loiclau <loic.lau@gmail.com>
2020-06-08 15:33:05 +0200

    Update www/include/configuration/configObject/servicegroup/formServiceGroup.php
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/include/configuration/configObject/servicegroup/formServiceGroup.php
+++ www/include/configuration/configObject/servicegroup/formServiceGroup.php
@@ -111,7 +111,7 @@ $form->addElement('text', 'sg_alias', _("Description"), $attrsText);
 
 $form->registerRule('validate_geo_coords', 'function', 'validateGeoCoords');
 $form->addElement('text', 'geo_coords', _("Geo coordinates"), $attrsText);
-$form->addRule('geo_coords', _("geo coords not valid"), 'validate_geo_coords');
+$form->addRule('geo_coords', _("geo coords are not valid"), 'validate_geo_coords');
 
 $form->addElement('header', 'relation', _("Relations"));
 
