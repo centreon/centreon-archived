1a25994af51918e897f1d250a48a7c407fd9e3e4
Kevin Duret <kduret@centreon.com>
2020-06-17 09:49:57 +0200

    Update src/Centreon/Domain/Acknowledgement/AcknowledgementService.php

--- src/Centreon/Domain/Acknowledgement/AcknowledgementService.php
+++ src/Centreon/Domain/Acknowledgement/AcknowledgementService.php
@@ -164,7 +164,7 @@ class AcknowledgementService extends AbstractCentreonService implements Acknowle
         $this->engineService->addHostAcknowledgement($acknowledgement, $host);
 
         if ($acknowledgement->isWithServices()) {
-            $services = $this->monitoringRepository->findServicesByHostWithoutParameters($host->getId());
+            $services = $this->monitoringRepository->findServicesByHostWithoutRequestParameters($host->getId());
             foreach ($services as $service) {
                 $service->setHost($host);
                 $this->engineService->addServiceAcknowledgement($acknowledgement, $service);
