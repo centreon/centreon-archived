c6aa957bb3880c73b0680037d7b0d1ba0c455b04
sc979 <34628915+sc979@users.noreply.github.com>
2020-07-01 12:24:11 +0200

    Update www/include/configuration/configKnowledge/display-hosts.php
    Co-authored-by: Adrien Morais <31647811+adr-mo@users.noreply.github.com>

--- www/include/configuration/configKnowledge/display-hosts.php
+++ www/include/configuration/configKnowledge/display-hosts.php
@@ -151,7 +151,7 @@ try {
     $rows = $pearDB->query("SELECT FOUND_ROWS()")->fetchColumn();
 
     $selection = [];
-    while ($data = $statement->fetch()) {
+    while ($data = $statement->fetch(PDO::FETCH_ASSOC)) {
         if ($data["host_register"] == 1) {
             $selection[$data["host_name"]] = $data["host_id"];
         }
