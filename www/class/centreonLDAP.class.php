73249ee2c91df5e7a94f4236ab6a91551843f7ca
schapron <schapron@centreon.com>
2020-07-03 17:47:12 +0200

    correct message alignment to be consistent with other messages

--- www/class/centreonLDAP.class.php
+++ www/class/centreonLDAP.class.php
@@ -796,7 +796,7 @@ class CentreonLDAP
     private function debug($msg)
     {
         if ($this->debugImport) {
-            error_log("[" . date("d/m/Y H:i") . "]" . $msg . "\n", 3, $this->debugPath . "ldapsearch.log");
+            error_log("[" . date("d/m/Y H:i") . "] " . $msg . "\n", 3, $this->debugPath . "ldapsearch.log");
         }
     }
 
