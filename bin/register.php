aee3c149566f19f4fb851215cde8f35eb0a4cfc1
jeremyjaouen <jjaouen@centreon.com>
2020-07-20 15:50:56 +0200

    basic proxy conf

--- bin/register.php
+++ bin/register.php
@@ -4,22 +4,23 @@
  * Get script params and assign them
  */
 $opt = getopt('u:p:t:h:', ["help::","proxy:"]);
+$helpMessage = PHP_EOL;
+$helpMessage .= "Global Options:" . PHP_EOL;
+$helpMessage .= PHP_EOL;
+$helpMessage .= "  -u <mandatory>              username of your centreon-web account" . PHP_EOL;
+$helpMessage .= "  -p <mandatory>              password of your centreon-web account" . PHP_EOL;
+$helpMessage .= "  -t <mandatory>              the server type you want to register:" . PHP_EOL;
+$helpMessage .= "            0: Central" . PHP_EOL;
+$helpMessage .= "            1: Poller" . PHP_EOL;
+$helpMessage .= "            2: Remote Server" . PHP_EOL;
+$helpMessage .= "            3: Map Server" . PHP_EOL;
+$helpMessage .= "            4: MBI Server" . PHP_EOL;
+$helpMessage .= "  -h <mandatory>              URL of your Central platform" . PHP_EOL;
+$helpMessage .= "  --help <optional>           get informations about the parameters available" . PHP_EOL;
+$helpMessage .= "  --proxy <optional>          if using a proxy, provide this parameter" . PHP_EOL;
 
 if (isset($opt['help'])) {
-    echo PHP_EOL;
-    echo "Global Options:" . PHP_EOL;
-    echo PHP_EOL;
-    echo "  -u <mandatory>              username of your centreon-web account" . PHP_EOL;
-    echo "  -p <mandatory>              password of your centreon-web account" . PHP_EOL;
-    echo "  -t <mandatory>              the server type you want to register:" . PHP_EOL;
-    echo "            0: Central" . PHP_EOL;
-    echo "            1: Poller" . PHP_EOL;
-    echo "            2: Remote Server" . PHP_EOL;
-    echo "            3: Map Server" . PHP_EOL;
-    echo "            4: MBI Server" . PHP_EOL;
-    echo "  -h <mandatory>              URL of your Central platform" . PHP_EOL;
-    echo "  --help <optional>           get informations about the parameters available" . PHP_EOL;
-    echo "  --proxy <optional>          if using a proxy, provide this parameter" . PHP_EOL;
+    echo $helpMessage;
     exit;
 }
 
@@ -31,7 +32,7 @@ try {
         $centralIp = $opt['h'];
     } else {
         throw new \InvalidArgumentException(
-            'missing parameter: -u -p -t -h are mandatories, use --help for further informations'
+            'missing parameter: -u -p -t -h are mandatories, use --help for further informations' . $helpMessage
         );
     }
 } catch (\InvalidArgumentException $e) {
@@ -39,11 +40,21 @@ try {
     exit;
 }
 
-
 if (!in_array($serverType, [0,1,2,3,4])) {
     echo '-t must be one of those value' . PHP_EOL;
     echo '0 => Central, 1 => Poller, 2 => Remote Server, 3 => Map Server, 4 => MBI Server';
     exit;
+}
+
+if (isset($opt['proxy'])) {
+    $proxy = preg_split("/[:@]/", $opt['proxy']);
+
+    $proxyInfo['username'] = $proxy[0];
+    $proxyInfo['password'] = $proxy[1];
+    $proxyInfo['host'] = $proxy[2];
+    $proxyInfo['port'] = (int) $proxy[3];
+    var_dump($proxyInfo);
+    die();
 }
 
 /**
@@ -79,8 +90,6 @@ $loginUrl .= '/centreon/api/' . $version . '/login';
 
 $ch = curl_init($loginUrl);
 curl_setopt($ch, CURLOPT_POST, 1);
-// curl_setopt($ch, CURLOPT_PROXY, 'proxy.int.centreon.com');
-// curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $credentials);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
