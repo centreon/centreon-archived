1b2f98a2f03a1728f7a69059418705a6146f80cf
Kevin Duret <kduret@centreon.com>
2020-07-20 13:50:23 +0200

    feat(api): manage user filter order (#8854)
    Refs: MON-4890

--- src/Centreon/Domain/Filter/Interfaces/FilterRepositoryInterface.php
+++ src/Centreon/Domain/Filter/Interfaces/FilterRepositoryInterface.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace Centreon\Domain\Filter\Interfaces;
 
 use Centreon\Domain\Filter\Filter;
+use Centreon\Domain\Filter\FilterException;
 
 interface FilterRepositoryInterface
 {
@@ -39,21 +40,19 @@ interface FilterRepositoryInterface
      * Update filter.
      *
      * @param Filter $filter
-     * @return int created filter id
+     * @return void
      * @throws FilterException
      */
-    public function updateFilter(Filter $filter): int;
+    public function updateFilter(Filter $filter): void;
 
     /**
      * Delete filter.
      *
-     * @param int $userId current user id
-     * @param string $pageName page name
-     * @param int $filterId Filter id to search
+     * @param Filter $filter
      * @return void
      * @throws FilterException
      */
-    public function deleteFilterByUserId(int $userId, string $pageName, int $filterId): void;
+    public function deleteFilter(Filter $filter): void;
 
     /**
      * Find filters linked to a user id using request parameters.
--- www/install/createTables.sql
+++ www/install/createTables.sql
@@ -2321,6 +2321,7 @@ CREATE TABLE `user_filter` (
     `user_id` int(11) NOT NULL,
     `page_name` varchar(255) NOT NULL,
     `criterias` text,
+    `order` int(11) NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `filter_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
