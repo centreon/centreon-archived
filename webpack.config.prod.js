8f0203f3fad0ec5695510fcbf0b6433dd09ff401
Bruno d'Auria <bdauria@centreon.com>
2020-07-22 09:29:20 +0200

    Fix build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const baseConfig = require('./webpack.config');
