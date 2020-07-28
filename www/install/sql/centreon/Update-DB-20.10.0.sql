288b95c03f6eb025fbcbd4b4383869be795e5cc8
schapron <schapron@centreon.com>
2020-07-28 19:03:13 +0200

    move upgrade script in PHP file to get the server address

--- www/install/sql/centreon/Update-DB-20.10.0.sql
+++ www/install/sql/centreon/Update-DB-20.10.0.sql
@@ -9,25 +9,3 @@ CREATE TABLE IF NOT EXISTS `user_filter` (
     PRIMARY KEY (`id`),
     CONSTRAINT `filter_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `contact` (`contact_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Create platform_topology table
-CREATE TABLE `platform_topology` (
-    `ip_address` varchar(255) NOT NULL,
-    `hostname` varchar(255) NOT NULL,
-    `server_type` tinyint(1) NOT NULL DEFAULT 0,
-    `parent` varchar(255),
-    PRIMARY KEY (`ip_address`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Insert the current platform into the platform_topology table
-INSERT INTO `platform_topology` (
-    `ip_address`,
-    `hostname`,
-    `server_type`,
-    `parent`
-) VALUES (
-    (SELECT `ns_ip_address` FROM nagios_server WHERE localhost = '1'),
-    (SELECT `name` FROM nagios_server WHERE localhost = '1'),
-    0,
-    NULL
-);
