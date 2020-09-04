39d1309996d61f914817277ff0d1ba1ee40151be
schapron <schapron@centreon.com>
2020-09-04 15:04:21 +0200

    feat(menu): add remote's central credentials page

--- www/install/sql/centreon/Update-DB-20.10.0.sql
+++ www/install/sql/centreon/Update-DB-20.10.0.sql
@@ -13,3 +13,16 @@ CREATE TABLE IF NOT EXISTS `user_filter` (
 -- Rename and move events view menu
 UPDATE `topology` SET `topology_name` = 'Resources Status', `topology_url` = '/monitoring/resources', `topology_parent` = 2, `topology_page` = 200 WHERE `topology_page` = 104;
 UPDATE `contact` SET `default_page` = 200 WHERE `default_page` = 104;
+
+-- Create a new menu page related to remote. Hidden by default on a Central
+INSERT INTO `topology`(
+    `topology_name`, `topology_parent`, `topology_page`, `topology_order`, `topology_group`,
+    `topology_url`, `topology_url_opt`,
+    `topology_popup`, `topology_modules`, `topology_show`,
+    `topology_style_class`, `topology_style_id`, `topology_OnClick`, `readonly`
+) VALUES (
+    'Remote access', 501, 50120, 25, 1,
+    './include/Administration/parameters/parameters.php', '&o=remote',
+    '0', '0', '0',
+    NULL, NULL, NULL, '1'
+);
