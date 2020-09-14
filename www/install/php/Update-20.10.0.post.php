9cd2aac826fdd4a8f440fd4fbb5c9a9bc978cdf5
Kevin Duret <kduret@centreon.com>
2020-09-14 11:39:29 +0200

    enh(acl): give access to resource status page on existing acl (#8982)
    Refs: MON-5992

--- www/install/php/Update-20.10.0.post.php
+++ www/install/php/Update-20.10.0.post.php
@@ -86,6 +86,49 @@ try {
             )
         ");
     }
+  
+    // migrate resource status menu acl
+    $errorMessage = "Unable to update acl of resource status page.";
+
+    $topologyAclQuery = $pearDB->query(
+        "SELECT DISTINCT(tr1.acl_topo_id)
+        FROM acl_topology_relations tr1
+        WHERE tr1.acl_topo_id NOT IN (
+            SELECT tr2.acl_topo_id
+            FROM acl_topology_relations tr2, topology t2
+            WHERE tr2.topology_topology_id = t2.topology_id
+            AND t2.topology_page = 200
+        )
+        AND tr1.acl_topo_id IN (
+            SELECT tr3.acl_topo_id
+            FROM acl_topology_relations tr3, topology t3
+            WHERE tr3.topology_topology_id = t3.topology_id
+            AND t3.topology_page IN (20201, 20202)
+        )"
+    );
+
+    $resourceStatusQuery = $pearDB->query(
+        "SELECT topology_id FROM topology WHERE topology_page = 200"
+    );
+    if ($resourceStatusPage = $resourceStatusQuery->fetch()) {
+        $stmt = $pearDB->prepare("
+            INSERT INTO `acl_topology_relations` (
+                `topology_topology_id`,
+                `acl_topo_id`,
+                `access_right`
+            ) VALUES (
+                :topology_id,
+                :acl_topology_id,
+                1
+            )
+        ");
+
+        while ($row = $topologyAclQuery->fetch()) {
+            $stmt->bindValue(':topology_id', $resourceStatusPage['topology_id'], \PDO::PARAM_INT);
+            $stmt->bindValue(':acl_topology_id', $row['acl_topo_id'], \PDO::PARAM_INT);
+            $stmt->execute();
+        }
+    }
 
     $pearDB->commit();
     $errorMessage = "";
