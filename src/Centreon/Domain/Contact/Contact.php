faa853d53a5b5025724e2fd2d6750be42dc1fd11
Kevin Duret <kduret@centreon.com>
2020-09-22 10:38:59 +0200

    fix(api): fix timeline with recurrent downtime (no contact id)

--- src/Centreon/Domain/Contact/Contact.php
+++ src/Centreon/Domain/Contact/Contact.php
@@ -64,7 +64,7 @@ class Contact implements UserInterface, ContactInterface
     public const DEFAULT_CHARSET = 'UTF-8';
 
     /**
-     * @var int Id of contact
+     * @var int|null Id of contact
      */
     private $id;
 
@@ -144,18 +144,18 @@ class Contact implements UserInterface, ContactInterface
     private $locale;
 
     /**
-     * @return int
+     * @return int|null
      */
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
 
     /**
-     * @param int $id
+     * @param int|null $id
      * @return self
      */
-    public function setId(int $id): self
+    public function setId(?int $id): self
     {
         $this->id = $id;
         return $this;
