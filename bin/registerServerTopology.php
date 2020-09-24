71e84c60cbe37c1cd8a93fb693e27afcabeeaa21
sc979 <34628915+sc979@users.noreply.github.com>
2020-09-24 17:01:50 +0200

    enh msg

--- bin/registerServerTopology.php
+++ bin/registerServerTopology.php
@@ -247,7 +247,7 @@ if (isRemote($serverType)) {
 
     //check if e remote is register on server
     if (hasRemoteChild()) {
-        exit(formatResponseMessage(401, 'Central cannot be convert to Remote', 'Unauthorized'));
+        exit(formatResponseMessage(401, 'Central cannot be converted to Remote', 'Unauthorized'));
     }
 
     //prepare db credential
