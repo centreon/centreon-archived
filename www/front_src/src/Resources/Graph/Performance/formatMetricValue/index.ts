9dda5fa5f084ab2ab05aa09e1077610cf22afb4e
Bruno d'Auria <bdauria@centreon.com>
2020-09-25 10:56:55 +0200

    Update www/front_src/src/Resources/Graph/Performance/formatMetricValue/index.ts
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>

--- www/front_src/src/Resources/Graph/Performance/formatMetricValue/index.ts
+++ www/front_src/src/Resources/Graph/Performance/formatMetricValue/index.ts
@@ -13,7 +13,7 @@ const formatMetricValue = ({ value, unit, base = 1000 }): string => {
 
   const base1024 = base2Units.includes(unit) || base === 1024;
 
-  const formatSuffx = base1024 ? ' ib' : 'a';
+  const formatSuffix = base1024 ? ' ib' : 'a';
 
   return numeral(value)
     .format(`0.[00]${formatSuffx}`)
