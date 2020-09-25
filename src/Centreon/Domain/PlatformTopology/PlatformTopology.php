3e23afa06b918c857de4cf7dc48e8f3d74289588
schapron <schapron@centreon.com>
2020-09-25 11:10:50 +0200

    reorder methods

--- src/Centreon/Domain/PlatformTopology/PlatformTopology.php
+++ src/Centreon/Domain/PlatformTopology/PlatformTopology.php
@@ -138,107 +138,346 @@ class PlatformTopology
     private $apiSelfSignedCertificate;
 
     /**
-     * data retrieved from 'informations' table
+     * data retrieved from 'options' table
      * @var string|null
      */
     private $apiProxyHost;
 
     /**
-     * data retrieved from 'informations' table
+     * data retrieved from 'options' table
      * @var int|null
      */
     private $apiProxyPort;
 
     /**
-     * data retrieved from 'informations' table
+     * data retrieved from 'options' table
      * @var string|null
      */
     private $apiProxyUsername;
 
     /**
-     * data retrieved from 'informations' table
+     * data retrieved from 'options' table
      * @var string|null
      */
     private $apiProxyCredentials;
 
-    public function getApiProxyHost(): ?string
+    /**
+     * Validate address consistency
+     *
+     * @param string|null $address the address to be tested
+     *
+     * @return string|null
+     */
+    private function checkIpAddress(?string $address): ?string
     {
-        return $this->apiProxyHost;
+        // Check for valid IPv4 or IPv6 IP
+        // or not sent address (in the case of Central's "parent_address")
+        if (null === $address || false !== filter_var($address, FILTER_VALIDATE_IP)) {
+            return $address;
+        }
+
+        // check for DNS to be resolved
+        if (false === filter_var(gethostbyname($address), FILTER_VALIDATE_IP)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    _("The address of '%s' is not valid"),
+                    $this->getName()
+                )
+            );
+        }
+
+        return $address;
     }
 
     /**
-     * @param string $path
-     * @return $this
+     * @param int|null $port
+     * @return int
      */
-    public function setApiProxyHost(string $path): self
+    public function checkPortConsistency(?int $port): int
     {
-        $path = filter_var($path, FILTER_SANITIZE_STRING);
-        if (empty($path)) {
+        // checking
+        $port = filter_var($port, FILTER_VALIDATE_INT);
+        if (false === $port || 1 > $port || $port > 65536) {
             throw new InvalidArgumentException(
-                _("Central's platform path is not consistent. Please check the 'Remote Access' form")
+                _("Central platform's data are not consistent. Please check the 'Remote Access' form")
             );
         }
-        $this->apiProxyHost = $path;
+        return $port;
+    }
+
+    /**
+     * @param string $encryptedKey
+     * @return string
+     */
+    public function decryptApiCredentials(string $encryptedKey): string
+    {
+        // first key
+        $path = __DIR__ . "/../../../../";
+        require_once $path . "/src/Security/Encryption.php";
+        if (file_exists($path . '/.env.local.php')) {
+            $localEnv = @include $path . '/.env.local.php';
+        }
+
+        // second key
+        if (empty($localEnv) || !isset($localEnv['APP_SECRET'])) {
+            throw new InvalidArgumentException(
+                _("Unable to find the encryption key. Please check the '.env.local.php' file")
+            );
+        }
+        $secondKey = base64_encode('api_remote_credentials');
+
+        try {
+            $centreonEncryption = new Encryption();
+            $centreonEncryption->setFirstKey($localEnv['APP_SECRET'])->setSecondKey($secondKey);
+            return $centreonEncryption->decrypt($encryptedKey);
+        } catch (throwable $e) {
+            throw new InvalidArgumentException(
+                _("Unable to decipher central's credentials. Please check the credentials in the 'Remote Access' form")
+            );
+        }
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return $this
+     */
+    public function setId(?int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getParentAddress(): ?string
+    {
+        return $this->parentAddress;
+    }
+
+    /**
+     * @param string|null $parentAddress
+     *
+     * @return $this
+     */
+    public function setParentAddress(?string $parentAddress): self
+    {
+        if (null !== $parentAddress && $this->getType() === static::TYPE_CENTRAL) {
+            throw new InvalidArgumentException(_("Cannot use parent address on a Central server type"));
+        }
+        $this->parentAddress = $this->checkIpAddress($parentAddress);
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type server type: central, poller, remote, map or mbi
+     *
+     * @return $this
+     */
+    public function setType(string $type): self
+    {
+        $type = strtolower($type);
+
+        // Check if the server_type is available
+        if (!in_array($type, static::AVAILABLE_TYPES)) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    _("The platform type of '%s'@'%s' is not consistent"),
+                    $this->getName(),
+                    $this->getAddress()
+                )
+            );
+        }
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     * @return $this
+     */
+    public function setName(?string $name): self
+    {
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        if (empty($name)) {
+            throw new InvalidArgumentException(
+                _('The name of the platform is not consistent')
+            );
+        }
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string|null $address
+     *
+     * @return $this
+     */
+    public function setAddress(?string $address): self
+    {
+        $this->address = $this->checkIpAddress($address);
+        return $this;
+    }
+
+    /**
+     * @return integer|null
+     */
+    public function getParentId(): ?int
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param int|null $parentId
+     *
+     * @return $this
+     */
+    public function setParentId(?int $parentId): self
+    {
+        if (null !== $parentId && $this->getType() === static::TYPE_CENTRAL) {
+            throw new InvalidArgumentException(_("Cannot set parent id to a central server"));
+        }
+        $this->parentId = $parentId;
         return $this;
     }
 
     /**
      * @return int|null
      */
-    public function getApiProxyPort(): ?int
+    public function getServerId(): ?int
     {
-        return $this->apiProxyPort;
+        return $this->serverId;
     }
 
     /**
-     * @param int $port
+     * @param int|null $serverId nagios_server ID
+     * @return PlatformTopology
+     */
+    public function setServerId(?int $serverId): self
+    {
+        $this->serverId = $serverId;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLinkedToAnotherServer(): bool
+    {
+        return $this->isLinkedToAnotherServer;
+    }
+
+    /**
+     * @param bool $isLinked
      * @return $this
      */
-    public function setApiProxyPort(int $port): self
+    public function setLinkedToAnotherServer(bool $isLinked): self
     {
-        $this->apiProxyPort = $this->checkPortConsistency($port);
+        $this->isLinkedToAnotherServer = $isLinked;
         return $this;
     }
 
     /**
      * @return string|null
      */
-    public function getApiProxyUsername(): ?string
+    public function getIsRemote(): ?string
     {
-        return $this->apiProxyUsername;
+        return $this->isRemote;
+    }
+
+    /**
+     * @param string $type
+     * @return $this
+     */
+    public function setIsRemote(string $type): self
+    {
+        $this->isRemote = $type;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getAuthorizedMaster(): ?string
+    {
+        return $this->authorizedMaster;
+    }
+
+    /**
+     * @param string $address
+     * @return $this
+     */
+    public function setAuthorizedMaster(string $address): self
+    {
+        $this->authorizedMaster = $address;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getApiUsername(): ?string
+    {
+        return $this->apiUsername;
     }
 
     /**
      * @param string $username
      * @return $this
      */
-    public function setApiProxyUsername(string $username): self
+    public function setApiUsername(string $username): self
     {
-        $username = filter_var($username, FILTER_SANITIZE_STRING);
-        if (empty($username)) {
-            throw new InvalidArgumentException(
-                _("Central platform's data are not consistent. Please check the 'Remote Access' form")
-            );
-        }
-        $this->apiProxyUsername = $username;
+        $this->apiUsername = $username;
         return $this;
     }
 
     /**
      * @return string|null
      */
-    public function getApiProxyCredentials(): ?string
+    public function getApiCredentials(): ?string
     {
-        return $this->apiProxyCredentials;
+        return $this->apiCredentials;
     }
 
     /**
-     * @param string $encrypted
+     * @param string $encryptedKey
      * @return $this
      */
-    public function setApiProxyCredentials(string $encrypted): self
+    public function setApiCredentials(string $encryptedKey): self
     {
-        $this->apiCredentials = $this->decryptApiCredentials($encrypted);
+        $this->apiCredentials = $this->decryptApiCredentials($encryptedKey);
         return $this;
     }
 
@@ -331,323 +570,84 @@ class PlatformTopology
         return $this;
     }
 
-    /**
-     * @param int|null $port
-     * @return int
-     */
-    public function checkPortConsistency(?int $port): int
+    public function getApiProxyHost(): ?string
     {
-        // checking
-        $port = filter_var($port, FILTER_VALIDATE_INT);
-        if (false === $port || 1 > $port || $port > 65536) {
+        return $this->apiProxyHost;
+    }
+
+    /**
+     * @param string $path
+     * @return $this
+     */
+    public function setApiProxyHost(string $path): self
+    {
+        $path = filter_var($path, FILTER_SANITIZE_STRING);
+        if (empty($path)) {
             throw new InvalidArgumentException(
-                _("Central platform's data are not consistent. Please check the 'Remote Access' form")
+                _("Central's platform path is not consistent. Please check the 'Remote Access' form")
             );
         }
-        return $port;
+        $this->apiProxyHost = $path;
+        return $this;
     }
 
     /**
-     * @return string|null
+     * @return int|null
      */
-    public function getIsRemote(): ?string
+    public function getApiProxyPort(): ?int
     {
-        return $this->isRemote;
+        return $this->apiProxyPort;
     }
 
     /**
-     * @param string $type
+     * @param int $port
      * @return $this
      */
-    public function setIsRemote(string $type): self
+    public function setApiProxyPort(int $port): self
     {
-        $this->isRemote = $type;
+        $this->apiProxyPort = $this->checkPortConsistency($port);
         return $this;
     }
 
     /**
      * @return string|null
      */
-    public function getAuthorizedMaster(): ?string
+    public function getApiProxyUsername(): ?string
     {
-        return $this->authorizedMaster;
-    }
-
-    /**
-     * @param string $address
-     * @return $this
-     */
-    public function setAuthorizedMaster(string $address): self
-    {
-        $this->authorizedMaster = $address;
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getApiUsername(): ?string
-    {
-        return $this->apiUsername;
+        return $this->apiProxyUsername;
     }
 
     /**
      * @param string $username
      * @return $this
      */
-    public function setApiUsername(string $username): self
+    public function setApiProxyUsername(string $username): self
     {
-        $this->apiUsername = $username;
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getApiCredentials(): ?string
-    {
-        return $this->apiCredentials;
-    }
-
-    /**
-     * @param string $encryptedKey
-     * @return $this
-     */
-    public function setApiCredentials(string $encryptedKey): self
-    {
-        $this->apiCredentials = $this->decryptApiCredentials($encryptedKey);
-        return $this;
-    }
-
-    /**
-     * @param string $encryptedKey
-     * @return string
-     */
-    public function decryptApiCredentials(string $encryptedKey): string
-    {
-        // first key
-        $path = __DIR__ . "/../../../../";
-        require_once $path . "/src/Security/Encryption.php";
-        if (file_exists($path . '/.env.local.php')) {
-            $localEnv = @include $path . '/.env.local.php';
-        }
-
-        // second key
-        if (empty($localEnv) || !isset($localEnv['APP_SECRET'])) {
+        $username = filter_var($username, FILTER_SANITIZE_STRING);
+        if (empty($username)) {
             throw new InvalidArgumentException(
-                _("Unable to find the encryption key. Please check the '.env.local.php' file")
+                _("Central platform's data are not consistent. Please check the 'Remote Access' form")
             );
         }
-        $secondKey = base64_encode('api_remote_credentials');
-
-        try {
-            $centreonEncryption = new Encryption();
-            $centreonEncryption->setFirstKey($localEnv['APP_SECRET'])->setSecondKey($secondKey);
-            return $centreonEncryption->decrypt($encryptedKey);
-        } catch (throwable $e) {
-            throw new InvalidArgumentException(
-                _("Unable to decipher central's credentials. Please check the credentials in the 'Remote Access' form")
-            );
-        }
-    }
-
-    /**
-     * @return bool
-     */
-    public function isLinkedToAnotherServer(): bool
-    {
-        return $this->isLinkedToAnotherServer;
-    }
-
-    /**
-     * @param bool $isLinked
-     * @return $this
-     */
-    public function setLinkedToAnotherServer(bool $isLinked): self
-    {
-        $this->isLinkedToAnotherServer = $isLinked;
-        return $this;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param int|null $id
-     * @return $this
-     */
-    public function setId(?int $id): self
-    {
-        $this->id = $id;
+        $this->apiProxyUsername = $username;
         return $this;
     }
 
     /**
      * @return string|null
      */
-    public function getParentAddress(): ?string
+    public function getApiProxyCredentials(): ?string
     {
-        return $this->parentAddress;
+        return $this->apiProxyCredentials;
     }
 
     /**
-     * @param string|null $parentAddress
-     *
+     * @param string $encrypted
      * @return $this
      */
-    public function setParentAddress(?string $parentAddress): self
+    public function setApiProxyCredentials(string $encrypted): self
     {
-        if (null !== $parentAddress && $this->getType() === static::TYPE_CENTRAL) {
-            throw new InvalidArgumentException(_("Cannot use parent address on a Central server type"));
-        }
-        $this->parentAddress = $this->checkIpAddress($parentAddress);
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param string $type server type: central, poller, remote, map or mbi
-     *
-     * @return $this
-     */
-    public function setType(string $type): self
-    {
-        $type = strtolower($type);
-
-        // Check if the server_type is available
-        if (!in_array($type, static::AVAILABLE_TYPES)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    _("The platform type of '%s'@'%s' is not consistent"),
-                    $this->getName(),
-                    $this->getAddress()
-                )
-            );
-        }
-        $this->type = $type;
-        return $this;
-    }
-
-    /**
-     * Validate address consistency
-     *
-     * @param string|null $address the address to be tested
-     *
-     * @return string|null
-     */
-    private function checkIpAddress(?string $address): ?string
-    {
-        // Check for valid IPv4 or IPv6 IP
-        // or not sent address (in the case of Central's "parent_address")
-        if (null === $address || false !== filter_var($address, FILTER_VALIDATE_IP)) {
-            return $address;
-        }
-
-        // check for DNS to be resolved
-        if (false === filter_var(gethostbyname($address), FILTER_VALIDATE_IP)) {
-            throw new InvalidArgumentException(
-                sprintf(
-                    _("The address of '%s' is not valid"),
-                    $this->getName()
-                )
-            );
-        }
-
-        return $address;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param string|null $name
-     * @return $this
-     */
-    public function setName(?string $name): self
-    {
-        $name = filter_var($name, FILTER_SANITIZE_STRING);
-        if (empty($name)) {
-            throw new InvalidArgumentException(
-                _('The name of the platform is not consistent')
-            );
-        }
-        $this->name = $name;
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getAddress(): ?string
-    {
-        return $this->address;
-    }
-
-    /**
-     * @param string|null $address
-     *
-     * @return $this
-     */
-    public function setAddress(?string $address): self
-    {
-        $this->address = $this->checkIpAddress($address);
-        return $this;
-    }
-
-    /**
-     * @return integer|null
-     */
-    public function getParentId(): ?int
-    {
-        return $this->parentId;
-    }
-
-    /**
-     * @param int|null $parentId
-     *
-     * @return $this
-     */
-    public function setParentId(?int $parentId): self
-    {
-        if (null !== $parentId && $this->getType() === static::TYPE_CENTRAL) {
-            throw new InvalidArgumentException(_("Cannot set parent id to a central server"));
-        }
-        $this->parentId = $parentId;
-        return $this;
-    }
-
-    /**
-     * @return int|null
-     */
-    public function getServerId(): ?int
-    {
-        return $this->serverId;
-    }
-
-    /**
-     * @param int|null $serverId nagios_server ID
-     * @return PlatformTopology
-     */
-    public function setServerId(?int $serverId): self
-    {
-        $this->serverId = $serverId;
+        $this->apiCredentials = $this->decryptApiCredentials($encrypted);
         return $this;
     }
 }
