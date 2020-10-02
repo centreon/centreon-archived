722125e37e45b01dd4d10d3d04a2553b3657ca20
sc979 <34628915+sc979@users.noreply.github.com>
2020-10-02 15:37:42 +0200

    fix(upgrade): correct typo in 20.10.beta2 (#9072)

--- www/install/php/Update-20.10.0-beta.2.php
+++ www/install/php/Update-20.10.0-beta.2.php
@@ -23,7 +23,7 @@ include_once __DIR__ . "/../../class/centreonLog.class.php";
 $centreonLog = new CentreonLog();
 
 //error specific content
-$versionOfTheUpgrade = 'UPGRADE - 20.10.0-beta.1 : ';
+$versionOfTheUpgrade = 'UPGRADE - 20.10.0-beta.2 : ';
 
 /**
  * Queries needing exception management and rollback if failing
@@ -41,7 +41,7 @@ try {
         'keycloak_trusted_clients', 'keycloak_blacklist_clients')"
     );
 
-    $keycloak[];
+    $keycloak = [];
     while ($row = $result->fetch()) {
         $keycloak[$row['key']] = $row['value'];
     }
