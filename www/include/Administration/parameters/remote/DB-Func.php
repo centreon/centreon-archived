50554a69b60366d02fa1ad0b779bdd8cace9207f
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2020-10-13 19:30:44 +0200

    Update DB-Func.php

--- www/include/Administration/parameters/remote/DB-Func.php
+++ www/include/Administration/parameters/remote/DB-Func.php
@@ -19,7 +19,8 @@
  *
  */
 
-function validateApiPort($port) {
+function validateApiPort($port) 
+{
     $port = filter_var($port, FILTER_VALIDATE_INT);
 
     if ($port === false || $port < 1 || $port > 65335) {
