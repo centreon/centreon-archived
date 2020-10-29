3ae0b1eb169b980681a7bad8d62d45892a36aed2
tuntoja <58987095+tuntoja@users.noreply.github.com>
2020-10-29 19:06:33 +0100

    fix(install): set version to 20.10.0 (#9166) (#9170)

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.0-beta.2');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.0');
 
 --
 -- Contenu de la table `contact`
