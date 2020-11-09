bdc9eb4247582b133640082aaaae12ac09a84096
Bruno d'Auria <bdauria@centreon.com>
2020-11-09 17:36:59 +0100

    Implement URL column

---
+++ www/front_src/src/Resources/Listing/columns/Url.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import IconLink from '@material-ui/icons/Link';
+
+import { IconButton, ComponentColumnProps } from '@centreon/ui';
+
+import { path, isNil } from 'ramda';
+import { labelUrl } from '../../translatedLabels';
+
+const UrlColumn = ({ row }: ComponentColumnProps): JSX.Element | null => {
+  const endpoint = path<string | undefined>(
+    ['links', 'externals', 'notes'],
+    row,
+  );
+
+  if (isNil(endpoint)) {
+    return null;
+  }
+
+  return (
+    <a href={endpoint}>
+      <IconButton
+        title={labelUrl}
+        ariaLabel={labelUrl}
+        onClick={(): void => {
+          // nothing to do
+        }}
+      >
+        <IconLink fontSize="small" />
+      </IconButton>
+    </a>
+  );
+};
+
+export default UrlColumn;
