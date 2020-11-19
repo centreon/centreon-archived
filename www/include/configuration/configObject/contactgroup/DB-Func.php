b14344c7c305c8664a62fec9c1f0d30dc7605b2e
loiclau <loic.lau@gmail.com>
2020-11-19 17:20:28 +0100

    Update www/include/configuration/configObject/contactgroup/DB-Func.php

--- www/include/configuration/configObject/contactgroup/DB-Func.php
+++ www/include/configuration/configObject/contactgroup/DB-Func.php
@@ -257,7 +257,8 @@ function updateContactGroup($cgId = null, $params = array())
     $cgName = $centreon->checkIllegalChar(filter_var($ret["cg_name"], FILTER_SANITIZE_STRING));
     $cgAlias = filter_var($ret["cg_alias"], FILTER_SANITIZE_STRING);
     $cgComment = filter_var($ret["cg_comment"], FILTER_SANITIZE_STRING);
-    $cgActivate = filter_var($ret["cg_activate"]["cg_activate"], FILTER_SANITIZE_STRING) ?: '1'; //enum
+    $cgActivate = filter_var($ret["cg_activate"]["cg_activate"], FILTER_SANITIZE_STRING); //enum
+    $cgActivate = ($cgActivate === '0' ? '0' : '1');
 
     $stmt = $pearDB->prepare(
         "UPDATE `contactgroup` SET `cg_name` = :cgName, `cg_alias` = :cgAlias, `cg_comment` = :cgComment, " .
