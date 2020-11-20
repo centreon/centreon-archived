75bd1f6c4c84008227930d12fbf49875ddadad15
jeremyjaouen <jjaouen@centreon.com>
2020-11-20 10:50:34 +0100

    update comparison

--- www/include/options/media/images/DB-Func.php
+++ www/include/options/media/images/DB-Func.php
@@ -492,7 +492,7 @@ function removeRecursiveTempDirectory(string $dir): void
     if (is_dir($dir)) {
         $objects = scandir($dir);
         foreach ($objects as $object) {
-            if ($object != "." && $object != "..") {
+            if ($object !== "." && $object !== "..") {
                 if (is_dir($dir . DIRECTORY_SEPARATOR . $object) && !is_link($dir . DIRECTORY_SEPARATOR . $object)) {
                     removeRecursiveTempDirectory($dir . DIRECTORY_SEPARATOR . $object);
                 } else {
