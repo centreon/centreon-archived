6ee3a82e6ae393014a1f8733bffeb66ff221edf0
Jeremy Delpierre <jdelpierre@centreon.com>
2020-11-23 11:22:05 +0100

    add webpack build production on start dev env

--- tests/end_to_end/cypress/start-env.sh
+++ tests/end_to_end/cypress/start-env.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-cd cypress
+cd ../../
+npm run build
+
+cd tests/end_to_end/cypress
 docker-compose pull
 docker-compose up -d --force-recreate
 docker exec -it centreon-web-only sh -c "rm -rf /usr/share/centreon/www/install"
