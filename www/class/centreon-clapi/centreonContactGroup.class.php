fd320e5886fea540ba76c28ac1a1e8f8c04259f7
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2020-11-23 09:37:06 +0100

    fix(clapi): Correctly Export/Import LDAP ContactGroup (#9224)

--- www/class/centreon-clapi/centreonContactGroup.class.php
+++ www/class/centreon-clapi/centreonContactGroup.class.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Copyright 2005-2015 CENTREON
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Copyright 2005-2020 CENTREON
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -73,6 +73,22 @@ class CentreonContactGroup extends CentreonObject
         $this->action = "CG";
         $this->nbOfCompulsoryParams = count($this->insertParams);
         $this->activateField = "cg_activate";
+    }
+
+    /**
+     * Get contact group ID
+     *
+     * @param string|null $contactGroupName
+     * @return int
+     * @throws CentreonClapiException
+     */
+    public function getContactGroupID($contactGroupName = null)
+    {
+        $cgIds = $this->object->getIdByParameter($this->object->getUniqueLabelField(), array($contactGroupName));
+        if (count($cgIds) !== 1) {
+            throw new CentreonClapiException("Unknown contact group: " . $contactGroupName);
+        }
+        return (int) $cgIds[0];
     }
 
     /**
