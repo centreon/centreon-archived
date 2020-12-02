e382fa92c87ac4b25bb5b22b47d48970d749654a
sc979 <34628915+sc979@users.noreply.github.com>
2020-12-02 18:53:26 +0100

    fix(secu): check page_limit consistency (#9267)

--- www/api/class/centreon_configuration_hostcategory.class.php
+++ www/api/class/centreon_configuration_hostcategory.class.php
@@ -1,7 +1,8 @@
 <?php
+
 /*
- * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Copyright 2005-2020 Centreon
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -33,9 +34,8 @@
  *
  */
 
-
 require_once _CENTREON_PATH_ . "/www/class/centreonDB.class.php";
-require_once dirname(__FILE__) . "/centreon_configuration_objects.class.php";
+require_once __DIR__ . "/centreon_configuration_objects.class.php";
 
 class CentreonConfigurationHostcategory extends CentreonConfigurationObjects
 {
@@ -112,8 +112,12 @@ class CentreonConfigurationHostcategory extends CentreonConfigurationObjects
         $queryHostCategory .= 'ORDER BY hc.hc_name ';
 
         if (isset($this->arguments['page_limit']) && isset($this->arguments['page'])) {
-            if (!is_numeric($this->arguments['page']) || !is_numeric($this->arguments['page_limit'])) {
-                throw new \RestBadRequestException('Error, limit must be numerical');
+            if (
+                !is_numeric($this->arguments['page'])
+                || !is_numeric($this->arguments['page_limit'])
+                || $this->arguments['page_limit'] < 1
+            ) {
+                throw new \RestBadRequestException('Error, limit must be an integer greater than zero');
             }
             $offset = ($this->arguments['page'] - 1) * $this->arguments['page_limit'];
             $queryHostCategory .= 'LIMIT :offset, :limit';
