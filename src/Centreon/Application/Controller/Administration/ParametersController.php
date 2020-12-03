f1e8f0612853dbd80fec2aeb28b894f34d975a27
Adrien Morais <amorais@centreon.com>
2020-12-03 12:06:47 +0100

    fix coding style issue

--- src/Centreon/Application/Controller/Administration/ParametersController.php
+++ src/Centreon/Application/Controller/Administration/ParametersController.php
@@ -23,7 +23,6 @@ declare(strict_types=1);
 
 namespace Centreon\Application\Controller\Administration;
 
-
 use Centreon\Application\Controller\AbstractController;
 use Centreon\Domain\Option\Interfaces\OptionServiceInterface;
 use FOS\RestBundle\View\View;
