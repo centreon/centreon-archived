2708c74092e3043d40975ea07655cf5ffc75628d
jeremyjaouen <jjaouen@centreon.com>
2020-12-14 07:43:47 +0100

    basic registration

--- bin/registerServerTopology.sh
+++ bin/registerServerTopology.sh
@@ -59,8 +59,7 @@ function parse_command_options() {
 
 
 #========= begin of function set_variable()
-function set_variable()
-{
+function set_variable() {
   local varname=$1
   shift
   if [ -z "${!varname}" ]; then
@@ -99,28 +98,28 @@ function get_api_token() {
 # log "DEBUG" "This is a DEBUG_LOG_LEVEL message"
 # log "INFO" "This is a INFO_LOG_LEVEL message"
 #
-function log(){
- TIMESTAMP=$(date --rfc-3339=seconds)
+function log() {
+  TIMESTAMP=$(date --rfc-3339=seconds)
 
- if [[ -z "${1}" || -z "${2}" ]]
- then
-	echo "${TIMESTAMP} - ERROR : Missing argument"
-	echo "${TIMESTAMP} - ERROR : Usage log \"INFO\" \"Message log\" "
-	exit 1
- fi
+  if [[ -z "${1}" || -z "${2}" ]]
+  then
+    echo "${TIMESTAMP} - ERROR : Missing argument"
+    echo "${TIMESTAMP} - ERROR : Usage log \"INFO\" \"Message log\" "
+    exit 1
+  fi
 
- # get the message log level
- log_message_level="${1}"
+  # get the message log level
+  log_message_level="${1}"
 
- # get the log message
- log_message="${2}"
+  # get the log message
+  log_message="${2}"
 
- # check if the log_message_level is greater than the runtime_log_level
- [[ ${SUPPORTED_LOG_LEVEL[$log_message_level]} ]] || return 1
+  # check if the log_message_level is greater than the runtime_log_level
+  [[ ${SUPPORTED_LOG_LEVEL[$log_message_level]} ]] || return 1
 
- (( ${SUPPORTED_LOG_LEVEL[$log_message_level]} < ${SUPPORTED_LOG_LEVEL[$runtime_log_level]} )) && return 2
+  (( ${SUPPORTED_LOG_LEVEL[$log_message_level]} < ${SUPPORTED_LOG_LEVEL[$runtime_log_level]} )) && return 2
 
- echo -e "${TIMESTAMP} - $log_message_level - $log_message"
+  echo -e "${TIMESTAMP} - $log_message_level - $log_message"
 
 }
 #========= end of function log()
@@ -151,40 +150,40 @@ function parse_fqdn() {
 #========= begin of function usage()
 function usage() {
   cat << EOF
-This script will register a platform (CURRENT NODE) on another (TARGET NODE).
-If you register a CURRENT NODE on a TARGET NODE that is already linked to a Central,
-your informations will automatically be forwarded to the Central.
-If you register a Remote Server, this script will automatically convert your CURRENT NODE in Remote Server.
-After executing the script, please use the wizard on your Central to complete your installation.
+  This script will register a platform (CURRENT NODE) on another (TARGET NODE).
+  If you register a CURRENT NODE on a TARGET NODE that is already linked to a Central,
+  your informations will automatically be forwarded to the Central.
+  If you register a Remote Server, this script will automatically convert your CURRENT NODE in Remote Server.
+  After executing the script, please use the wizard on your Central to complete your installation.
 
-Global Options:
-  -u <mandatory>              username of your centreon-web account on the TARGET NODE.
-  -h <mandatory>              URL of the TARGET NODE
-  -t <mandatory>              the server type you want to register (CURRENT NODE):
-            - Poller
-            - Remote
-            - MAP
-            - MBI
-  -n <mandatory>              name of the CURRENT NODE that will be displayed on the TARGET NODE
+  Global Options:
+    -u <mandatory>              username of your centreon-web account on the TARGET NODE.
+    -h <mandatory>              URL of the TARGET NODE
+    -t <mandatory>              the server type you want to register (CURRENT NODE):
+              - Poller
+              - Remote
+              - MAP
+              - MBI
+    -n <mandatory>              name of the CURRENT NODE that will be displayed on the TARGET NODE
 
-  --help <optional>           get information about the parameters available
-  --root <optional>           your Centreon root path on TARGET NODE (by default "centreon")
-  --node-address <optional>   provide your FQDN or IP of the CURRENT NODE. FQDN must be resolvable on the TARGET NODE
-  --insecure <optional>       allow self-signed certificate
-  --template <optional>       provide the path of a register topology configuration file to automate the script
-             - API_USERNAME             <mandatory> string
-             - API_PASSWORD             <mandatory> string
-             - CURRENT_NODE_TYPE        <mandatory> string
-             - TARGET_NODE_ADDRESS      <mandatory> string (PARENT NODE ADDRESS)
-             - CURRENT_NODE_NAME        <mandatory> string (CURRENT NODE NAME)
-             - PROXY_USAGE              <mandatory> boolean
-             - ROOT_CENTREON_FOLDER     <optional> string (CENTRAL ROOT CENTREON FOLDER)
-             - CURRENT_NODE_ADDRESS     <optional> string (CURRENT NODE IP OR FQDN)
-             - INSECURE                 <optional> boolean
-             - PROXY_HOST               <optional> string
-             - PROXY_PORT               <optional> integer
-             - PROXY_USERNAME           <optional> string
-             - PROXY_PASSWORD           <optional> string
+    --help <optional>           get information about the parameters available
+    --root <optional>           your Centreon root path on TARGET NODE (by default "centreon")
+    --node-address <optional>   provide your FQDN or IP of the CURRENT NODE. FQDN must be resolvable on the TARGET NODE
+    --insecure <optional>       allow self-signed certificate
+    --template <optional>       provide the path of a register topology configuration file to automate the script
+              - API_USERNAME             <mandatory> string
+              - API_PASSWORD             <mandatory> string
+              - CURRENT_NODE_TYPE        <mandatory> string
+              - TARGET_NODE_ADDRESS      <mandatory> string (PARENT NODE ADDRESS)
+              - CURRENT_NODE_NAME        <mandatory> string (CURRENT NODE NAME)
+              - PROXY_USAGE              <mandatory> boolean
+              - ROOT_CENTREON_FOLDER     <optional> string (CENTRAL ROOT CENTREON FOLDER)
+              - CURRENT_NODE_ADDRESS     <optional> string (CURRENT NODE IP OR FQDN)
+              - INSECURE                 <optional> boolean
+              - PROXY_HOST               <optional> string
+              - PROXY_PORT               <optional> integer
+              - PROXY_USERNAME           <optional> string
+              - PROXY_PASSWORD           <optional> string
 
 EOF
 }
@@ -227,6 +226,57 @@ function set_proxy_parameters() {
 #========= end of set_proxy_parameters()
 
 
+#========= begin of get_current_node_ip()
+function get_current_node_ip() {
+  CURRENT_NODE_ADDRESS=$(hostname -I)
+
+  ips=$(echo $CURRENT_NODE_ADDRESS | tr " " "\n")
+  echo "Which IP do you want to use as CURRENT NODE IP ?"
+  select addr in $ips
+  do
+    CURRENT_NODE_ADDRESS="$addr"
+    return 0
+  done
+}
+#========= end of get_current_node_ip()
+
+
+#========= begin of prepare_register_payload()
+function prepare_register_payload() {
+  PAYLOAD='{"name":"'"${CURRENT_NODE_NAME}"'","hostname":"'"${HOSTNAME}"'","type":"'"${CURRENT_NODE_TYPE}"'","address":"'"${CURRENT_NODE_ADDRESS}"'","parent_address":"'"${PARSED_URL[HOST]}"'"}'
+
+cat << EOD
+
+Summary of the information that will be send:
+
+Api Connection:
+username: ${USERNAME_API}
+password: ******
+target server: ${PARSED_URL[HOST]}
+
+Pending Registration Server:
+name: ${CURRENT_NODE_NAME}
+hostname: ${HOSTNAME}
+type: ${CURRENT_NODE_TYPE}
+address: ${CURRENT_NODE_ADDRESS}
+
+
+EOD
+}
+#========= end of prepare_register_payload()
+
+
+#========= begin of register_server()
+function register_server() {
+  API_RESPONSE=$(curl -v -X POST -H "Content-Type: application/json" -H "X-AUTH-TOKEN: ${API_TOKEN}"\
+    -d "${PAYLOAD}" \
+    "${TARGET_NODE_ADDRESS}/centreon/api/latest/platform/topology")
+  IS_REGISTER=$(echo ${API_RESPONSE} |grep "201 Created")
+  echo ${IS_REGISTER}
+}
+#========= begin of register_server()
+
+
 ###########################################################
 #                                                         #
 #                    SCRIPT EXECUTION                     #
@@ -242,13 +292,13 @@ get_api_password
 # Ask for Proxy
 set_proxy_parameters
 
+# Ask for IP to use
+get_current_node_ip
 # Get the API TARGET Token
 get_api_token "$API_TARGET_PASSWORD"
 
-# Ask for IP to use
-
-# Prepare Payload
-
-# Display Summary
+# Prepare Payload & Display Summary
+prepare_register_payload
 
 # Send cURL to POST Register
+register_server
