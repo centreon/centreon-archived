67428ec643ca61c268dd0863752a148a4ec3dbfb
Kevin Duret <kduret@centreon.com>
2020-12-14 11:05:25 +0100

    fix warning

--- tests/php/Centreon/Application/Controller/PlatformTopologyControllerTest.php
+++ tests/php/Centreon/Application/Controller/PlatformTopologyControllerTest.php
@@ -239,10 +239,6 @@ class PlatformTopologyControllerTest extends TestCase
             ->method('getContent')
             ->willReturn($this->goodJsonPlatform);
 
-        $this->platformTopologyService->expects($this->any())
-            ->method('addPlatformToTopology')
-            ->willReturn(null);
-
         $platformTopologyController = new PlatformTopologyController($this->platformTopologyService);
         $platformTopologyController->setContainer($this->container);
 
