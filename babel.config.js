8c956cb0f717af9a9dd506f5f6ce9283f4a83b88
Thebarda <tomdar87@outlook.com>
2020-12-21 09:48:29 +0100

    Fix react-refresh

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
+const isDevelopment =
+  process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test';
+
+const plugins = isDevelopment ? ['react-refresh/babel'] : [];
+
 module.exports = {
   extends: '@centreon/frontend-core/babel/typescript',
-  plugins: ['react-refresh/babel'],
+  plugins,
 };
