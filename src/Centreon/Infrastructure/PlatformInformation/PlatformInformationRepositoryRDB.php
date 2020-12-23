cfd87dbd0202b2a5cd48eec7a5fed118f1d3393f
jeremyjaouen <jjaouen@centreon.com>
2020-12-23 08:39:40 +0100

    add update method in repository

--- src/Centreon/Infrastructure/PlatformInformation/PlatformInformationRepositoryRDB.php
+++ src/Centreon/Infrastructure/PlatformInformation/PlatformInformationRepositoryRDB.php
@@ -23,10 +23,11 @@ declare(strict_types=1);
 namespace Centreon\Infrastructure\PlatformInformation;
 
 use Centreon\Domain\Entity\EntityCreator;
-use Centreon\Domain\PlatformInformation\PlatformInformation;
-use Centreon\Domain\PlatformInformation\Interfaces\PlatformInformationRepositoryInterface;
 use Centreon\Infrastructure\DatabaseConnection;
+use Centreon\Domain\PlatformInformation\PlatformInformation;
 use Centreon\Infrastructure\Repository\AbstractRepositoryDRB;
+use Centreon\Domain\PlatformInformation\Interfaces\PlatformInformationRepositoryInterface;
+use Centreon\Domain\Repository\RepositoryException;
 
 /**
  * This class is designed to manage the repository of the platform topology requests
@@ -62,6 +63,9 @@ class PlatformInformationRepositoryRDB extends AbstractRepositoryDRB implements 
                 if ('authorizedMaster' === $row['key']) {
                     $row['key'] = 'centralServerAddress';
                 }
+                if ('apiCredentials' === $row['key']) {
+                    $row['key'] = 'encryptedApiCredentials';
+                }
                 // Converting each camelCase key as snake_case
                 $key = strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $row['key']));
                 $result[$key] = $row['value'];
@@ -79,5 +83,90 @@ class PlatformInformationRepositoryRDB extends AbstractRepositoryDRB implements 
         }
 
         return $platformInformation;
+    }
+
+    public function updatePlatformInformation(PlatformInformation $platformInformation): ?PlatformInformation
+    {
+        try {
+            $this->db->beginTransaction();
+            $deleteQuery = $this->db->prepare($this->translateDbName('DELETE FROM `:db`.informations'));
+            $deleteQuery->execute();
+            $insertQuery =  'INSERT INTO `:db`.informations (`key`, `value`) VALUES ';
+
+            $queryParameters = [];
+            if ($platformInformation->getVersion() !== null) {
+                $queryParameters[':version'] = [
+                    \PDO::PARAM_STR => $platformInformation->getVersion()
+                ];
+                $insertQuery .= "('version', :version), ";
+            }
+            if ($platformInformation->getAppKey() !== null) {
+                $queryParameters[':appKey'] = [
+                    \PDO::PARAM_STR => $platformInformation->getAppKey()
+                ];
+                $insertQuery .= "('appKey', :appKey), ";
+            }
+            if ($platformInformation->isRemote() === true) {
+                $insertQuery .= "('isRemote', 'yes'),  ('isCentral', 'no'), ";
+
+                if ($platformInformation->getCentralServerAddress() !== null) {
+                    $queryParameters[':authorizedMaster'] = [
+                        \PDO::PARAM_STR => $platformInformation->getCentralServerAddress()
+                    ];
+                    $insertQuery .= "('authorizedMaster', :authorizedMaster), ";
+                }
+                if ($platformInformation->getApiUsername() !== null) {
+                    $queryParameters[':apiUsername'] = [
+                        \PDO::PARAM_STR => $platformInformation->getApiUsername()
+                    ];
+                    $insertQuery .= "('apiUsername', :apiUsername), ";
+                }
+                if ($platformInformation->getEncryptedApiCredentials() !== null) {
+                    $queryParameters[':apiCredentials'] = [
+                        \PDO::PARAM_STR => $platformInformation->getEncryptedApiCredentials()
+                    ];
+                    $insertQuery .= "('apiCredentials', :apiCredentials), ";
+                }
+                if ($platformInformation->getApiScheme() !== null) {
+                    $queryParameters[':apiScheme'] = [
+                        \PDO::PARAM_STR => $platformInformation->getApiScheme()
+                    ];
+                    $insertQuery .= "('apiScheme', :apiScheme), ";
+                }
+                if ($platformInformation->getApiPort() !== null) {
+                    $queryParameters[':apiPort'] = [
+                        \PDO::PARAM_INT => $platformInformation->getApiPort()
+                    ];
+                    $insertQuery .= "('apiPort', :apiPort), ";
+                }
+                if ($platformInformation->getApiPath() !== null) {
+                    $queryParameters[':apiPath'] = [
+                        \PDO::PARAM_STR => $platformInformation->getApiPath()
+                    ];
+                    $insertQuery .= "('apiPath', :apiPath), ";
+                }
+                if ($platformInformation->hasApiPeerValidation() === true) {
+                    $insertQuery .= "('apiPeerValidation', 'yes')";
+                } else {
+                    $insertQuery .= "('apiPeerValidation', 'no')";
+                }
+            } else {
+                $insertQuery .= "('isCentral', 'yes'),  ('isRemote', 'no')";
+            }
+
+            $statement = $this->db->prepare($this->translateDbName($insertQuery));
+            foreach ($queryParameters as $token => $bindParams) {
+                foreach ($bindParams as $paramType => $paramValue) {
+                    $statement->bindValue($token, $paramValue, $paramType);
+                }
+            }
+            $statement->execute();
+        } catch ( \Exception $ex) {
+            $this->db->rollBack();
+            throw new RepositoryException('An error occured while updating the platform');
+        }
+        $this->db->commit();
+
+        return $this->findPlatformInformation();
     }
 }
