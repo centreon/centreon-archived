a0c8bd7b5b86de1eab0cb402b91bce717da1ae77
jeremyjaouen <jjaouen@centreon.com>
2020-12-29 10:09:31 +0100

    basic unit test

---
+++ tests/php/Centreon/Domain/PlatformInformation/PlatformInformationServiceTest.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * Copyright 2005 - 2020 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+namespace Tests\Centreon\Domain\PlatformInformation;
+
+use Centreon\Domain\PlatformInformation\Interfaces\PlatformInformationRepositoryInterface;
+use Centreon\Domain\PlatformInformation\PlatformInformation;
+use Centreon\Domain\PlatformInformation\PlatformInformationService;
+use Centreon\Domain\RemoteServer\Interfaces\RemoteServerServiceInterface;
+use PHPUnit\Framework\TestCase;
+
+class PlatformInformationServiceTest extends TestCase
+{
+    /**
+     * Basic Mock Informations
+     *
+     * @var PlatformInformation
+     */
+    private $baseInformation;
+
+    /**
+     * Information for a remote conversion
+     *
+     * @var PlatformInformation
+     */
+    private $informationRemote;
+
+    /**
+     * Information for a central conversion
+     *
+     * @var PlatformInformation
+     */
+    private $informationCentral;
+
+    /**
+     * @var PlatformInformationRepositoryInterface&MockObject $platformInformationRepository
+     */
+    private $platformInformationRepository;
+
+    /**
+     * @var RemoteServerServiceInterface&MockObject $remoteServerService
+     */
+    private $remoteServerService;
+
+    protected function setUp(): void
+    {
+        $this->baseInformation = (new PlatformInformation())
+            ->setVersion('21.04.0')
+            ->setAppKey('xxXxxXXxXXxxx')
+            ->setIsCentral(true)
+            ->setIsRemote(false);
+
+        $this->informationRemote = (new PlatformInformation())
+            ->setIsRemote(true)
+            ->setApiUsername('admin')
+            ->setApiCredentials('centreon')
+            ->setApiScheme('http')
+            ->setApiPort(80)
+            ->setApiPath('path')
+            ->setCentralServerAddress('192.168.0.1');
+
+        $this->informationCentral = (new PlatformInformation())
+            ->setIsCentral(true);
+
+        $this->platformInformationRepository = $this->createMock(PlatformInformationRepositoryInterface::class);
+        $this->remoteServerService = $this->createMock(RemoteServerServiceInterface::class);
+    }
+
+    public function testUpdatePlatformSuccess(): void
+    {
+        $this->platformInformationRepository
+            ->expects('once')
+            ->method('findPlatformInformation')
+            ->willReturn($this->baseInformation);
+
+        $this->platformInformationRepository
+            ->expects('once')
+            ->method('updatePlatformInformation')
+            ->willReturn($this->informationRemote);
+
+        $platformInformationService = new PlatformInformationService(
+            $this->platformInformationRepository,
+            $this->remoteServerService
+        );
+
+        $expectedResult = (new PlatformInformation())
+            ->setIsRemote(true)
+            ->setApiUsername('admin')
+            ->setApiCredentials('centreon')
+            ->setApiScheme('http')
+            ->setApiPort(80)
+            ->setApiPath('path')
+            ->setCentralServerAddress('192.168.0.1');
+
+        $this->assertInstanceOf(
+            PlatformInformation::class,
+            $platformInformationService->updatePlatformInformation($this->informationRemote)
+        );
+
+        $this->assertEquals($expectedResult, $platformInformationService->getInformation());
+    }
+}
