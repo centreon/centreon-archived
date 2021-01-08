daff0e8666527f2242400e333de2d2fbec7dbe5b
jeremyjaouen <jjaouen@centreon.com>
2021-01-08 09:53:46 +0100

    handle the remote conversion, and update template

--- bin/registerServerTopology.sh
+++ bin/registerServerTopology.sh
@@ -10,9 +10,10 @@ ROOT_CENTREON_FOLDER=""
 INSECURE=""
 TEMPLATE_FILE=""
 API_TOKEN=""
+REMOTE_API_TOKEN=""
 RESPONSE_MESSAGE=""
 
-declare -A SUPPORTED_LOG_LEVEL=([DEBUG]=0 [INFO]=1 [WARNING]=2 [ERROR]=3)
+declare -A SUPPORTED_LOG_LEVEL=([INFO]=0 [ERROR]=1)
 declare -A PARSED_URL=([SCHEME]="http" [HOST]="" [PORT]="80")
 runtime_log_level="INFO"
 
@@ -30,7 +31,6 @@ function parse_command_options() {
     case $1 in
         -t|--type)
           set_variable "CURRENT_NODE_TYPE" "$2"
-          # TODO: If Remote call endpoint to register remote
           shift 2
           ;;
         -n|--name)
@@ -107,11 +107,16 @@ function set_variable() {
 # Get the X-AUTH-TOKEN used in register request
 function get_api_token() {
   API_RESPONSE=$(curl -s -X POST ${INSECURE:+--insecure} -H "Content-Type: application/json" \
-    -d '{"security":{"credentials":{"login":"'"${API_USERNAME}"'", "password":"'"$1"'"}}}' \
-    "${TARGET_NODE_ADDRESS}/${ROOT_CENTREON_FOLDER}/api/latest/login")
+    -d '{"security":{"credentials":{"login":"'"$2"'", "password":"'"$3"'"}}}' \
+    "$1/$4/api/latest/login")
 
   API_TOKEN=$( echo "${API_RESPONSE}" | grep -o '"token":"[^"]*' | cut -d'"' -f4)
 
+  if [[ ! $API_RESPONSE ]];
+    then
+      log "ERROR" "Couldn't connect to $1"
+      exit 1
+  fi
   if [[ ! $API_TOKEN ]];
   then
     log "ERROR" "${API_RESPONSE}"
@@ -126,10 +131,10 @@ function get_api_token() {
 # with timestamp
 #
 # usage:
-# log "$LOG_LEVEL" "$message" ($LOG_LEVEL = DEBUG|INFO|WARNING|ERROR)
+# log "$LOG_LEVEL" "$message" ($LOG_LEVEL = INFO|ERROR)
 #
 # example:
-# log "DEBUG" "This is a DEBUG_LOG_LEVEL message"
+# log "ERROR" "This is a ERROR_LOG_LEVEL message"
 # log "INFO" "This is a INFO_LOG_LEVEL message"
 #
 function log() {
@@ -138,7 +143,7 @@ function log() {
   if [[ -z "${1}" || -z "${2}" ]]
   then
     echo "${TIMESTAMP} - ERROR : Missing argument"
-    echo "${TIMESTAMP} - ERROR : Usage log \"INFO\" \"Message log\" "
+    echo "${TIMESTAMP} - ERROR : Usage log \"TYPE\" \"Message log\" "
     exit 1
   fi
 
@@ -161,22 +166,30 @@ function log() {
 #========= begin of function parse_fqdn()
 # Parse the -h flag to explode it as SCHEME HOST PORT
 function parse_fqdn() {
-  PARSED_URL[SCHEME]="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
-  # remove the protocol
-  url="$(echo ${1/${PARSED_URL[SCHEME]}/})"
   # extract the user (if any)
-  userpass="$(echo $url | grep @ | cut -d@ -f1)"
+  userpass="$(echo $1 | grep @ | cut -d@ -f1)"
   pass="$(echo $userpass | grep : | cut -d: -f2)"
   if [ -n "$pass" ]; then
     user="$(echo $userpass | grep : | cut -d: -f1)"
   else
       user=$userpass
   fi
+  url="$(echo ${1/${userpass}"@"/})"
+
+  # extract the Scheme
+  SCHEME="$(echo $url | grep :// | cut -d: -f1)"
+  if [ -n "$SCHEME" ]; then
+    PARSED_URL[SCHEME]=$SCHEME;
+  fi
 
   # extract the host
-  PARSED_URL[HOST]="$(echo ${url/$user@/} | cut -d: -f1)"
-  # by request - try to extract the port
-  PARSED_URL[PORT]="$(echo ${url/PARSED_URL[HOST]/} | grep : | cut -d: -f2)"
+  PARSED_URL[HOST]="$(echo ${url/${PARSED_URL[SCHEME]}"://"/} | cut -d: -f1)"
+
+  # extract the port
+  PORT="$(echo ${url/PARSED_URL[HOST]/} | grep : | cut -d: -f3)"
+  if [ -n "$PORT" ]; then
+    PARSED_URL[PORT]=$PORT
+  fi
 }
 #========= end of function parse_fqdn()
 
@@ -215,6 +228,17 @@ function usage() {
               - ROOT_CENTREON_FOLDER     <optional> string (CENTRAL ROOT CENTREON FOLDER)
               - INSECURE                 <optional> boolean
 
+              Additional Properties for Remote:
+              - API_CURRENT_NODE_USERNAME           <mandatory> string
+              - API_CURRENT_NODE_PASSWORD           <mandatory> string
+              - API_CURRENT_NODE_PROTOCOL           <mandatory> string
+              - API_CURRENT_NODE_PORT               <mandatory> string
+              - API_CURRENT_NODE_CENTREON_FOLDER    <mandatory> string
+              - PROXY_PORT=
+              - PROXY_HOST=
+              - PROXY_USERNAME=
+              - PROXY_PASSWORD=
+
 EOF
 }
 #========= end of function usage()
@@ -223,15 +247,16 @@ EOF
 #========= begin of get_api_password()
 function get_api_password() {
   stty -echo
-  echo "${TARGET_NODE_ADDRESS} : Please enter your password"
+  echo "$1 : Please enter your password:"
   read -r API_TARGET_PASSWORD
   stty echo
 }
 #========= end of get_api_password()
 
+
 #========= begin of get_current_node_ip()
 function get_current_node_ip() {
-  CURRENT_NODE_ADDRESS=$(hostname -I)
+  CURRENT_NODE_ADDRESS=$(hostname -I | xargs)
 
   ips=($CURRENT_NODE_ADDRESS)
   count_available_ips=${#ips[@]}
@@ -294,6 +319,7 @@ EOD
 #========= begin of register_server()
 # Send the request to register the server
 function register_server() {
+  # We set IFS to \n to correctly parse and extract HTTP Code and message
   IFS=$'\n' API_RESPONSE=($(curl -s -X POST ${INSECURE:+--insecure} -i -H "Content-Type: application/json" -H "X-AUTH-TOKEN: ${API_TOKEN}" \
     -d "${PAYLOAD}" \
     "${TARGET_NODE_ADDRESS}/${ROOT_CENTREON_FOLDER}/api/latest/platform/topology" | grep -E "(HTTP/|message)"))
@@ -304,12 +330,12 @@ function register_server() {
   if [[ $HTTP_CODE == "201" ]];
   then
     log "INFO" "The CURRENT NODE ${CURRENT_NODE_TYPE}: '${CURRENT_NODE_NAME}@${CURRENT_NODE_ADDRESS}' linked to TARGET NODE: ${TARGET_NODE_ADDRESS} has been added"
-  elif [[ $RESPONSE_MESSAGE != "" ]];
+  elif [[  -n $RESPONSE_MESSAGE ]];
   then
     log "ERROR" "${RESPONSE_MESSAGE}"
     exit 1
   else
-    log "ERROR" "An error occurred while contacting the API using: '${TARGET_NODE_ADDRESS}/centreon/api/latest/platform/topology' "
+    log "ERROR" "An error occurred while contacting the API using: '${TARGET_NODE_ADDRESS}/${ROOT_CENTREON_FOLDER}/api/latest/platform/topology' "
     exit 1
   fi
 }
@@ -318,16 +344,16 @@ function register_server() {
 
 #========= begin of set_variable_from_template()
 function set_variable_from_template() {
-
   source "$1"
 
   if [[ ! $API_USERNAME \
+    || ! $API_TARGET_PASSWORD \
     || ! $CURRENT_NODE_TYPE \
     || ! $TARGET_NODE_ADDRESS \
     || ! $CURRENT_NODE_NAME \
     || ! $CURRENT_NODE_ADDRESS \
   ]]; then
-    log "ERROR" "Missing Parameters: please fill all the template's mandatory fields"
+    log "ERROR" "Missing Parameters: please fill all the template's mandatory parameters"
     usage
     exit 1
   else
@@ -335,9 +361,124 @@ function set_variable_from_template() {
   fi
 
   PAYLOAD='{"name":"'"${CURRENT_NODE_NAME}"'","hostname":"'"${HOSTNAME}"'","type":"'"${CURRENT_NODE_TYPE}"'","address":"'"${CURRENT_NODE_ADDRESS}"'","parent_address":"'"${PARSED_URL[HOST]}"'"}'
+  REMOTE_PAYLOAD=
 }
 #========= end of set_variable_from_template()
 
+
+#========= begin of convert_to_remote()
+function convert_to_remote() {
+  # set default variables
+  API_CURRENT_NODE_PROTOCOL="http"
+  API_CURRENT_NODE_PORT="80"
+  API_CURRENT_NODE_CENTREON_FOLDER="centreon"
+
+  # if no template are used, ask for information
+  if [[ ! $TEMPLATE_FILE ]]; then
+    set_remote_parameters_manually
+  fi
+
+  # set peerValidation information
+  if [ -n $INSECURE ]; then
+    PEER_VALIDATION='"peerValidation": false'
+  else
+    PEER_VALIDATION='"peerValidation": true'
+  fi
+  # get token of Remote API
+  get_api_token "$CURRENT_NODE_ADDRESS" "$API_CURRENT_NODE_USERNAME" "$API_CURRENT_NODE_PASSWORD" "$API_CURRENT_NODE_CENTREON_FOLDER"
+  # send request to update informations and convert remote
+  request_to_remote
+}
+#========= end of convert_to_remote()
+
+#========= begin of request_to_remote()
+function request_to_remote() {
+  # Prepare Proxy Payload
+   # Check si Proxy Host remplis
+  if [[ -n $PROXY_HOST ]]; then
+    PROXY_PAYLOAD=', "proxy":{"host":"'"${PROXY_HOST}"'","port":'"${PROXY_PORT}"
+    if [[ -n $PROXY_USERNAME ]]; then
+      PROXY_PAYLOAD="${PROXY_PAYLOAD}"',"user":"'"${PROXY_USERNAME}"'","password":"'"${PROXY_PASSWORD}"'"'
+    fi
+    PROXY_PAYLOAD="${PROXY_PAYLOAD}"'}'
+  fi
+
+  # Prepare Remote Payload
+  REMOTE_PAYLOAD='{"isRemote":true,"centralServerAddress":"'"${TARGET_NODE_ADDRESS}"'","apiUsername":"'"${API_USERNAME}"'","apiCredentials":"'"${API_TARGET_PASSWORD}"'","apiScheme":"'"${PARSED_URL[SCHEME]}"'","apiPort":'"${PARSED_URL[PORT]}"',"apiPath":"'"${ROOT_CENTREON_FOLDER}"'",'"${PEER_VALIDATION}"
+  if [[ -n PROXY_PAYLOAD ]]; then
+    REMOTE_PAYLOAD="${REMOTE_PAYLOAD}""${PROXY_PAYLOAD}"
+  fi
+  REMOTE_PAYLOAD="${REMOTE_PAYLOAD}}"
+
+  #get response
+  IFS=$'\n' REMOTE_API_RESPONSE=($(curl -s -X PATCH ${INSECURE:+--insecure} -i -H "Content-Type: application/json" -H "X-AUTH-TOKEN: ${API_TOKEN}" \
+    -d "${REMOTE_PAYLOAD}" \
+    "${API_CURRENT_NODE_PROTOCOL}://${CURRENT_NODE_ADDRESS}:${API_CURRENT_NODE_PORT}/${ROOT_CENTREON_FOLDER}/api/latest/platform" | grep -E "(HTTP/|message)"))
+
+  HTTP_CODE="$(echo ${REMOTE_API_RESPONSE[0]} | cut -d ' ' -f2)"
+  RESPONSE_MESSAGE=${REMOTE_API_RESPONSE[1]}
+
+  if [[ $HTTP_CODE == "204" ]];
+  then
+    log "INFO" "The CURRENT NODE ${CURRENT_NODE_TYPE}: '${CURRENT_NODE_NAME}@${CURRENT_NODE_ADDRESS}' has been converted successfully."
+  elif [[ $RESPONSE_MESSAGE != "" ]];
+  then
+    log "ERROR" "${RESPONSE_MESSAGE}"
+    exit 1
+  else
+    log "ERROR" "An error occurred while contacting the API using: '${CURRENT_NODE_ADDRESS}/${ROOT_CENTREON_FOLDER}/api/latest/platform' "
+    exit 1
+  fi
+}
+
+function set_remote_parameters_manually() {
+    # ask information to connect to Remote API
+    echo "A few more information are required to convert your platform into Remote : "
+    echo "${CURRENT_NODE_ADDRESS} : Please enter your username:"
+    read -r API_CURRENT_NODE_USERNAME
+    get_api_password "$CURRENT_NODE_ADDRESS"
+    API_CURRENT_NODE_PASSWORD=$API_TARGET_PASSWORD
+    echo "${CURRENT_NODE_ADDRESS} : Protocol [http]:"
+    read -r INPUT_PROTOCOL
+    echo "${CURRENT_NODE_ADDRESS} : Port [80]:"
+    read -r INPUT_PORT
+    echo "${CURRENT_NODE_ADDRESS} : centreon root folder [centreon]:"
+    read -r INPUT_CENTREON_FOLDER
+    if [[ -n $INPUT_PROTOCOL ]]; then
+      API_CURRENT_NODE_PROTOCOL=$INPUT_PROTOCOL
+    fi
+    if [[ -n $INPUT_PORT ]]; then
+      API_CURRENT_NODE_PORT=$INPUT_PORT
+    fi
+    if [[ -n $INPUT_PORT ]]; then
+      API_CURRENT_NODE_CENTREON_FOLDER=$INPUT_CENTREON_FOLDER
+    fi
+      # Set Proxy informations
+    echo "Are you using a proxy ? (y/n)"
+    read -r PROXY_USAGE
+    if [[ $PROXY_USAGE == 'y' || $PROXY_USAGE == true ]]; then
+      PROXY_PORT="3128"
+      echo 'enter your proxy Host:'
+      read -r PROXY_HOST
+      echo 'enter your proxy Port [3128]:'
+      read -r INPUT_PROXY_PORT
+      if [[ -n $INPUT_PROXY_PORT ]]; then
+        PROXY_PORT=$INPUT_PROXY_PORT
+      fi
+
+      echo 'Are you using a username/password ? (y/n)'
+      read -r PROXY_CREDENTIALS
+      if [[ $PROXY_CREDENTIALS == 'y' ]]; then
+        echo 'enter your username:'
+        read -r PROXY_USERNAME
+        stty -echo
+        echo 'enter your password:'
+        read -r PROXY_PASSWORD
+        stty echo
+      fi
+    fi
+}
+#========= end of convert_to_remote()
 ###########################################################
 #                                                         #
 #                    SCRIPT EXECUTION                     #
@@ -350,7 +491,7 @@ parse_command_options "$@"
 if [[ ! $TEMPLATE_FILE ]];
 then
   # Ask for API TARGET Password
-  get_api_password
+  get_api_password "$TARGET_NODE_ADDRESS"
 
   if [[ ! $CURRENT_NODE_ADDRESS ]];
   then
@@ -365,8 +506,12 @@ then
   ROOT_CENTREON_FOLDER="centreon"
 fi
 
+if [[ $CURRENT_NODE_TYPE == 'remote' ]]; then
+  convert_to_remote
+fi
+
 # Get the API TARGET Token
-get_api_token "$API_TARGET_PASSWORD"
+get_api_token "$TARGET_NODE_ADDRESS" "$API_USERNAME" "$API_TARGET_PASSWORD" "$ROOT_CENTREON_FOLDER"
 
 # Send cURL to POST Register
 register_server
