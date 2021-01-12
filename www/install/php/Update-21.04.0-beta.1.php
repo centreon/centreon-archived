7b43cd62b3a128c44ca47bcf5322e13407a78192
Loic Laurent <loic.lau@gmail.com>
2021-01-12 17:13:29 +0100

    update

--- www/install/php/Update-21.04.0-beta.1.php
+++ www/install/php/Update-21.04.0-beta.1.php
@@ -29,51 +29,47 @@ $versionOfTheUpgrade = 'UPGRADE - 21.04.0-beta.1 : ';
  * Queries needing exception management and rollback if failing
  */
 try {
-    $tables = [
-        'dependency_hostChild_relation',
-        'dependency_hostParent_relation',
-        'dependency_hostgroupChild_relation',
-        'dependency_hostgroupParent_relation',
-        'dependency_metaserviceChild_relation',
-        'dependency_metaserviceParent_relation',
-        'dependency_serviceChild_relation',
-        'dependency_serviceParent_relation',
-        'dependency_servicegroupChild_relation',
-        'dependency_servicegroupParent_relation'
-    ];
-    $pearDB->beginTransaction();
-
-    foreach ($tables as $table) {
-        $errorMessage = "Unable to check $tables dependencies";
-        $result = $pearDB->query(
-            "SELECT COUNT( * ) AS duplicate, host_host_id, service_service_id, dependency_dep_id
-        FROM {$tables}
-        GROUP BY host_host_id, dependency_dep_id, service_service_id"
-        );
-        while ($row = $result->fetch()) {
-            if ($row['duplicate'] > 1) {
-                $depId = $row['dependency_dep_id'];
-                $hostId = $row['host_host_id'];
-                $serviceId = $row['service_service_id'];
-
-                $errorMessage = "Unable to delete $tables";
-                $pearDB->query(
-                    "DELETE FROM {$tables}
-                WHERE dependency_dep_id = " . $depId . "
-                AND host_host_id =  " . $hostId . "
-                AND service_service_id = " . $serviceId
-                );
-                $pearDB->commit();
-
-                $errorMessage = "Unable to insert $tables";
-                $pearDB->query(
-                    "INSERT INTO {$tables}
-                VALUES ( " . $depId . ", " . $serviceId . ", " . $hostId . ")"
-                );
-                $pearDB->commit();
-            }
-        }
-    }
+    $errorMessage = "Unable to update dependency_hostChild_relation";
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_hostChild_relation`
+        ADD UNIQUE (`dependency_dep_id`, `host_host_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_hostParent_relation`
+        ADD UNIQUE (`dependency_dep_id`, `host_host_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_hostgroupChild_relation`
+        ADD UNIQUE (`dependency_dep_id`, `hostgroup_hg_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_hostgroupParent_relation`
+        ADD UNIQUE (`dependency_dep_id`, `hostgroup_hg_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_metaserviceChild_relation`
+        ADD UNIQUE (`dependency_dep_id`, `meta_service_meta_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_metaserviceParent_relation`
+        ADD UNIQUE (`dependency_dep_id`, `meta_service_meta_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_serviceChild_relation`
+        ADD UNIQUE (`dependency_dep_id`, `service_service_id`, `host_host_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_serviceParent_relation`
+        ADD UNIQUE (`dependency_dep_id`, `service_service_id`, `host_host_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_servicegroupChild_relation`
+        ADD UNIQUE (`dependency_dep_id`, `servicegroup_sg_id`)"
+    );
+    $pearDB->query(
+        "ALTER IGNORE TABLE `dependency_servicegroupParent_relation`
+        ADD UNIQUE (`dependency_dep_id`, `servicegroup_sg_id`)"
+    );
     $errorMessage = "";
 } catch (\Exception $e) {
     $pearDB->rollBack();
