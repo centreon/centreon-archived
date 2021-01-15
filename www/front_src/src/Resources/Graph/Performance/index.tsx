99a6c7bd57d4da717797de434e870f4d3127940a
Bruno d'Auria <bdauria@centreon.com>
2021-01-15 09:00:46 +0100

    fix(Resource-status/Services): only fetch services with graph data when in graph mode and fix tooltip position #9384

--- www/front_src/src/Resources/Graph/Performance/index.tsx
+++ www/front_src/src/Resources/Graph/Performance/index.tsx
@@ -29,8 +29,8 @@ interface Props {
   resource: Resource | ResourceDetails;
   timeline?: Array<TimelineEvent>;
   onAddComment?: (commentParameters: CommentParameters) => void;
-  tooltipX?: number;
-  onTooltipDisplay?: (x?: number) => void;
+  tooltipPosition?: [number, number];
+  onTooltipDisplay?: (position?: [number, number]) => void;
 }
 
 const useStyles = makeStyles<Theme, Pick<Props, 'graphHeight'>>((theme) => ({
@@ -64,7 +64,7 @@ const PerformanceGraph = ({
   toggableLegend = false,
   eventAnnotationsActive = false,
   timeline,
-  tooltipX,
+  tooltipPosition,
   onTooltipDisplay,
   resource,
   onAddComment,
@@ -156,7 +156,7 @@ const PerformanceGraph = ({
             xAxisTickFormat={xAxisTickFormat}
             timeline={timeline}
             onTooltipDisplay={onTooltipDisplay}
-            tooltipX={tooltipX}
+            tooltipPosition={tooltipPosition}
             resource={resource}
             onAddComment={onAddComment}
             eventAnnotationsActive={eventAnnotationsActive}
