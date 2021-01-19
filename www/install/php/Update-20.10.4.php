9b95b407c5598ba589cae0e1d978f50aedcb2dd1
schapron <schapron@centreon.com>
2021-01-19 18:34:58 +0100

    concatenate sql and php upgrade script

--- www/install/php/Update-20.10.4.php
+++ www/install/php/Update-20.10.4.php
@@ -21,26 +21,43 @@
 include_once __DIR__ . "/../../class/centreonLog.class.php";
 
 // error specific content
-$versionOfTheUpgrade = 'UPGRADE - 20.10.4.post : ';
+$versionOfTheUpgrade = 'UPGRADE - 20.10.4 : ';
 $errorMessage = '';
 
 // Part requiring rollback management
 try {
     // Platform_topology refacto
-    $pearDB->beginTransaction();
     $errorMessage = "Unable to add pending column to platform_topology table";
+    $statement = $pearDB->query(
+        "SELECT COLUMN_DEFAULT
+        FROM information_schema.COLUMNS
+        WHERE TABLE_SCHEMA = 'centreon'
+          AND TABLE_NAME = 'platform_topology'
+          AND COLUMN_NAME = 'pending'"
+    );
+    if ($statement->fetch(\PDO::FETCH_ASSOC) === false) {
+        //Create the new column
+        $pearDB->query(
+            "ALTER TABLE `platform_topology` ADD COLUMN `pending` enum('0','1') DEFAULT ('1') AFTER `parent_id`"
+        );
+    }
+
+    $pearDB->beginTransaction();
+    $errorMessage = "Unable to update pending state to platform_topology table";
     $registeredPlatforms = "";
     // find registered platforms in monitoring table
-    $pearDB->query(
+    $statement = $pearDB->query(
         "SELECT id FROM `platform_topology`
-        WHERE `type` NOT IN ('central', 'remote', 'poller') OR server_id IS NOT NULL"
+        WHERE `type` NOT IN ('central', 'remote', 'poller') OR `server_id` IS NOT NULL"
     );
-    if (($result = $statement->fetch(\PDO::FETCH_ASSOC)) !== false) {
-        if (empty($result)) {
-            throw new Exception('Cannot find the monitoring platform in platform_topology table');
-        }
-        $registeredPlatforms = implode(', ', $result);
+    while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+        $result[] = $row['id'];
     }
+    if (empty($result)) {
+        throw new Exception('Cannot find the monitoring platform in platform_topology table');
+    }
+    $registeredPlatforms = implode(', ', $result);
+
     $pearDB->query(
         "UPDATE `platform_topology` SET `pending` = '0'
         WHERE id IN ($registeredPlatforms)"
--- www/install/sql/centreon/Update-DB-20.10.4.sql
+++
@@ -1,2 +0,0 @@
--- Add new column
-ALTER TABLE `platform_topology` ADD COLUMN `pending` enum('0','1') DEFAULT ('1') AFTER `parent_id`;
