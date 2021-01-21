600761422453f7c6fd03b5fe51ca2267f7a49493
loiclau <loic.lau@gmail.com>
2021-01-21 14:20:38 +0100

    Update www/install/php/Update-20.10.4.php
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/install/php/Update-20.10.4.php
+++ www/install/php/Update-20.10.4.php
@@ -23,7 +23,7 @@ include_once __DIR__ . "/../../class/centreonLog.class.php";
 $centreonLog = new CentreonLog();
 
 //error specific content
-$versionOfTheUpgrade = 'UPGRADE - 21.10.4 : ';
+$versionOfTheUpgrade = 'UPGRADE - 20.10.4 : ';
 
 /**
  * Queries needing exception management and rollback if failing
