64a5f5a4ead98e5d4698cf33f2096771e88816ac
Loic Laurent <loic.lau@gmail.com>
2021-01-22 11:03:36 +0100

    complete media path

--- src/Centreon/Domain/HostConfiguration/UseCase/V21/HostSeverity/FindHostSeverities.php
+++ src/Centreon/Domain/HostConfiguration/UseCase/V21/HostSeverity/FindHostSeverities.php
@@ -24,44 +24,84 @@ namespace Centreon\Domain\HostConfiguration\UseCase\V21\HostSeverity;
 
 use Centreon\Domain\HostConfiguration\Exception\HostSeverityException;
 use Centreon\Domain\HostConfiguration\Interfaces\HostSeverityReadRepositoryInterface;
+use Centreon\Domain\HostConfiguration\Model\HostSeverity;
 
 /**
- * This class is designed to represent a use case to find all host severities.
+ * This class is designed to represent a use case to find all host groups
  *
- * @package Centreon\Domain\HostConfiguration\UseCase\V21
+ * @package Centreon\Domain\HostConfiguration\UseCase\V21\HostGroup
  */
 class FindHostSeverities
 {
     /**
-     * @var HostSeverityReadRepositoryInterface
+     * @var HostGroupReadRepositoryInterface
      */
-    private $repository;
-
+    private $hostSeverityReadRepository;
+    
     /**
-     * FindHostSeverities constructor.
-     *
-     * @param HostSeverityReadRepositoryInterface $repository
+     * @var string|null
      */
-    public function __construct(HostSeverityReadRepositoryInterface $repository)
+    private $mediaPath;
+    
+    /**
+     * @param HostSeverityReadRepositoryInterface $configurationReadRepository
+     */
+    public function __construct(HostSeverityReadRepositoryInterface $configurationReadRepository)
     {
-        $this->repository = $repository;
+        $this->hostSeverityReadRepository = $configurationReadRepository;
     }
-
+    
+    /**
+     * @param string|null $mediaPath
+     * @return FindHostSeverities
+     */
+    public function setMediaPath(?string $mediaPath): FindHostSeverities
+    {
+        $this->mediaPath = $mediaPath;
+        return $this;
+    }
+    
     /**
      * Execute the use case for which this class was designed.
      *
      * @return FindHostSeveritiesResponse
-     * @throws \Exception
+     * @throws \Assert\AssertionFailedException
+     * @throws HostSeverityException
      */
     public function execute(): FindHostSeveritiesResponse
     {
-        $response = new FindHostSeveritiesResponse();
         try {
-            $hostSeverities = $this->repository->findHostSeverities();
-            $response->setHostSeverities($hostSeverities);
-        } catch (\Throwable $ex) {
-            HostSeverityException::searchHostSeveritiesException($ex);
+            $hostSeverities = $this->hostSeverityReadRepository->findHostSeverities();
+        } catch (\Exception $ex) {
+            throw HostSeverityException::searchHostSeveritiesException($ex);
         }
+        $this->updateMediaPaths($hostSeverities);
+        $response = new FindHostSeveritiesResponse();
+        $response->setHostSeverities($hostSeverities);
         return $response;
+    }
+    
+    /**
+     * Updated all media paths for all host groups.
+     *
+     * @param HostSeverity[] $hostSeverities
+     * @throws \Assert\AssertionFailedException
+     */
+    private function updateMediaPaths(array $hostSeverities): void
+    {
+        if ($this->mediaPath !== null) {
+            foreach ($hostSeverities as $hostSeverity) {
+                $icon = $hostSeverity->getIcon();
+                if (
+                    $icon !== null
+                    && $icon->getPath() !== null
+                    && substr($icon->getPath(), 0, strlen($this->mediaPath)) !== $this->mediaPath
+                ) {
+                    $icon->setPath(
+                        $this->mediaPath . DIRECTORY_SEPARATOR . $icon->getPath()
+                    );
+                }
+            }
+        }
     }
 }
