f6897bb4b6661412b22c89d1a8a309021589e9d6
Kevin Duret <kduret@centreon.com>
2021-02-09 11:41:48 +0100

    test

--- src/Centreon/LegacyContainer.php
+++ src/Centreon/LegacyContainer.php
@@ -1,6 +1,7 @@
 <?php
+
 /*
- * Copyright 2005 - 2019 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +30,7 @@ use Pimple\Container;
  *
  * @package Centreon
  */
-class LegacyContainer
+class LegacyContainer 
 {
     /**
      * @var Container
