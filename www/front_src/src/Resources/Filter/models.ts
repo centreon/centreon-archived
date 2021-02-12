d8e23d07b01e016e2df6ab8c2ddf6c6cf32d16e9
Bruno d'Auria <bdauria@centreon.com>
2021-02-12 09:33:37 +0100

    feat(Resource-status/Filter): implement dynamic filter display (#9450)

--- www/front_src/src/Resources/Filter/models.ts
+++ www/front_src/src/Resources/Filter/models.ts
@@ -1,32 +1,77 @@
-export interface CriteriaValue {
-  id: number | string;
-  name: string;
-}
+import { isNil } from 'ramda';
 
-export interface Criterias {
-  resourceTypes: Array<CriteriaValue>;
-  states: Array<CriteriaValue>;
-  statuses: Array<CriteriaValue>;
-  hostGroups: Array<CriteriaValue>;
-  serviceGroups: Array<CriteriaValue>;
-  search?: string;
-}
+import {
+  labelAll,
+  labelNewFilter,
+  labelUnhandledProblems,
+  labelResourceProblems,
+} from '../translatedLabels';
+
+import getDefaultCriterias from './Criterias/default';
+import {
+  Criteria,
+  criticalStatus,
+  downStatus,
+  selectableResourceTypes,
+  selectableStates,
+  selectableStatuses,
+  unhandledState,
+  unknownStatus,
+  warningStatus,
+} from './Criterias/models';
 
 export interface Filter {
-  id: string | number;
-  name: string;
-  criterias: Criterias;
-}
-
-export interface RawCriteria {
-  name: string;
-  object_type?: string;
-  type: string;
-  value?: Array<CriteriaValue> | string | boolean;
-}
-
-export interface RawFilter {
   id: number | string;
   name: string;
-  criterias: Array<RawCriteria>;
+  criterias: Array<Criteria>;
 }
+
+const allFilter = {
+  id: 'all',
+  name: labelAll,
+  criterias: getDefaultCriterias(),
+};
+
+const newFilter = {
+  id: '',
+  name: labelNewFilter,
+} as Filter;
+
+const unhandledProblemsFilter: Filter = {
+  id: 'unhandled_problems',
+  name: labelUnhandledProblems,
+  criterias: getDefaultCriterias({
+    states: [unhandledState],
+    statuses: [warningStatus, downStatus, criticalStatus, unknownStatus],
+  }),
+};
+
+const resourceProblemsFilter: Filter = {
+  id: 'resource_problems',
+  name: labelResourceProblems,
+  criterias: getDefaultCriterias({
+    statuses: [warningStatus, downStatus, criticalStatus, unknownStatus],
+  }),
+};
+
+const standardFilterById = {
+  resource_problems: resourceProblemsFilter,
+  all: allFilter,
+  unhandled_problems: unhandledProblemsFilter,
+};
+
+const isCustom = ({ id }: Filter): boolean => {
+  return isNil(standardFilterById[id]);
+};
+
+export {
+  allFilter,
+  unhandledProblemsFilter,
+  resourceProblemsFilter,
+  newFilter,
+  selectableResourceTypes,
+  selectableStates,
+  selectableStatuses,
+  standardFilterById,
+  isCustom,
+};
