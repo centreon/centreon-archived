cb2115c9ba5a9e3ec84280724606d15165531da2
chgautier <cgautier@centreon.com>
2021-02-26 16:58:22 +0100

    chore(install):update version to 20.10.5

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.4');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.5');
 
 --
 -- Contenu de la table `contact`
