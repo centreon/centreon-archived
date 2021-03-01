122abbab70a8d1886bdda1c547532c70ff4f4190
Kevin Duret <kduret@centreon.com>
2021-03-01 16:32:14 +0100

    add transaction

--- src/Security/Infrastructure/Repository/AuthenticationRepository.php
+++ src/Security/Infrastructure/Repository/AuthenticationRepository.php
@@ -32,43 +32,7 @@ use Security\Domain\Authentication\Model\ProviderToken;
 use Security\Encryption;
 
 /*
-CREATE TABLE `security_token` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token` varchar(255) NOT NULL,
-  `creation_date` datetime NOT NULL,
-  `expiration_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
-CREATE TABLE `security_authentication_tokens` (
-  `session_token_id` int(11) NOT NULL,
-  `token_id` int(11) NOT NULL,
-  `token_refresh_id` int(11) DEFAULT NULL,
-  `provider_configuration_id` int(11) NOT NULL,
-  PRIMARY KEY (`session_token_id`),
-  KEY `security_authentication_session_tokens_id_fk` (`session_token_id`),
-  KEY `security_authentication_tokens_id_fk` (`token_id`),
-  KEY `security_authentication_tokens_refresh_id_fk` (`token_refresh_id`),
-  KEY `security_authentication_tokens_configuration_id_fk` (`provider_configuration_id`),
-  CONSTRAINT `security_authentication_session_tokens_id_fk` FOREIGN KEY (`session_token_id`)
-  REFERENCES `session` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `security_authentication_tokens_configuration_id_fk` FOREIGN KEY (`provider_configuration_id`)
-  REFERENCES `provider_configuration` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `security_authentication_tokens_id_fk` FOREIGN KEY (`token_id`)
-  REFERENCES `security_token` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `security_authentication_tokens_refresh_id__fk` FOREIGN KEY (`token_refresh_id`)
-  REFERENCES `security_token` (`id`) ON DELETE SET NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
-
-CREATE TABLE `provider_configuration` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `provider_name` varchar(255) NOT NULL,
-  `provider_configuration_name` varchar(255) NOT NULL,
-  `configuration` text NOT NULL,
-  `isActive` BOOLEAN NOT NULL DEFAULT 1,
-  `isForced` BOOLEAN NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
 */
 
 /**
@@ -103,13 +67,28 @@ class AuthenticationRepository extends AbstractRepositoryDRB implements Authenti
         ProviderToken $providerToken,
         ?ProviderToken $providerRefreshToken
     ): void {
-        $this->deleteExistingToken($contactId);
-        $this->insertTokens(
-            $sessionToken,
-            $providerConfigurationId,
-            $contactId, $providerToken,
-            $providerRefreshToken
-        );
+        // We avoid to start again a database transaction
+        $isAlreadyInTransaction = $this->db->inTransaction();
+        if (!$isAlreadyInTransaction) {
+            $this->db->beginTransaction();
+        }
+        try {
+            $this->deleteExistingToken($contactId);
+            $this->insertTokens(
+                $sessionToken,
+                $providerConfigurationId,
+                $contactId, $providerToken,
+                $providerRefreshToken
+            );
+            if (!$isAlreadyInTransaction) {
+                $this->db->commit();
+            }
+        } catch (\Exception $e) {
+            if (!$isAlreadyInTransaction) {
+                $this->db->rollBack();
+            }
+            throw $e;
+        }
     }
 
     /**
@@ -130,7 +109,7 @@ class AuthenticationRepository extends AbstractRepositoryDRB implements Authenti
         $getSessionStatement->execute();
 
         // If any token found, also delete them from security_token
-        if(($result = $getSessionStatement->fetch(\PDO::FETCH_ASSOC)) !== false) {
+        if (($result = $getSessionStatement->fetch(\PDO::FETCH_ASSOC)) !== false) {
             $session = $result['session_id'];
             $deleteSecurityTokenStatement = $this->db->prepare(
                 $this->translateDbName(
@@ -152,6 +131,16 @@ class AuthenticationRepository extends AbstractRepositoryDRB implements Authenti
         }
     }
 
+    /**
+     * Insert session, security and refresh tokens
+     *
+     * @param string $sessionToken
+     * @param integer $providerConfigurationId
+     * @param integer $contactId
+     * @param ProviderToken $providerToken
+     * @param ProviderToken|null $providerRefreshToken
+     * @return void
+     */
     private function insertTokens(
         string $sessionToken,
         int $providerConfigurationId,
