1878fa47180d09c1455ac4707795426815624438
Kevin Duret <kduret@centreon.com>
2021-03-01 16:28:32 +0100

    create trables on install/upgrade

--- www/install/createTables.sql
+++ www/install/createTables.sql
@@ -2356,6 +2356,46 @@ CREATE TABLE `platform_topology` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 COMMENT='Registration and parent relation Table used to set the platform topology';
 
+-- Create authentication tables
+
+CREATE TABLE `security_token` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(255) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  `expiration_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `provider_configuration` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `provider_name` varchar(255) NOT NULL,
+  `provider_configuration_name` varchar(255) NOT NULL,
+  `configuration` text NOT NULL,
+  `isActive` BOOLEAN NOT NULL DEFAULT 1,
+  `isForced` BOOLEAN NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
+CREATE TABLE `security_authentication_tokens` (
+  `session_token_id` int(11) NOT NULL,
+  `token_id` int(11) NOT NULL,
+  `token_refresh_id` int(11) DEFAULT NULL,
+  `provider_configuration_id` int(11) NOT NULL,
+  PRIMARY KEY (`session_token_id`),
+  KEY `security_authentication_session_tokens_id_fk` (`session_token_id`),
+  KEY `security_authentication_tokens_id_fk` (`token_id`),
+  KEY `security_authentication_tokens_refresh_id_fk` (`token_refresh_id`),
+  KEY `security_authentication_tokens_configuration_id_fk` (`provider_configuration_id`),
+  CONSTRAINT `security_authentication_session_tokens_id_fk` FOREIGN KEY (`session_token_id`)
+  REFERENCES `session` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `security_authentication_tokens_configuration_id_fk` FOREIGN KEY (`provider_configuration_id`)
+  REFERENCES `provider_configuration` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `security_authentication_tokens_id_fk` FOREIGN KEY (`token_id`)
+  REFERENCES `security_token` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `security_authentication_tokens_refresh_id__fk` FOREIGN KEY (`token_refresh_id`)
+  REFERENCES `security_token` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
