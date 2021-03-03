37fc877018b31a72166d9d215a555254aec81a56
Loic Laurent <loic.lau@gmail.com>
2021-03-03 16:28:42 +0100

    sanitize generate

--- www/class/config-generate/broker.class.php
+++ www/class/config-generate/broker.class.php
@@ -197,8 +197,8 @@ class Broker extends AbstractObjectJSON
             $resultParameters = $this->stmt_broker_parameters->fetchAll(PDO::FETCH_GROUP | PDO::FETCH_ASSOC);
 
             //logger
-            $object['log']['directory'] = (string) $row['log_directory'];
-            $object['log']['filename'] = (string) $row['log_filename'];
+            $object['log']['directory'] = filter_var($row['log_directory'], FILTER_SANITIZE_STRING);
+            $object['log']['filename'] = filter_var($row['log_filename'], FILTER_SANITIZE_STRING);
             $object['log']['max_size'] = filter_var($row['log_max_size'], FILTER_VALIDATE_INT);
             $this->getLogsValues();
             $logs = $this->cacheLogValue[$poller_id];
