9e9e0da2172485ae16a9848b88d9fd5e1f146a0f
Jeremy Delpierre <jdelpierre@centreon.com>
2021-03-09 18:30:19 +0100

    feat(test): resource feature refacto

--- tests/e2e/cypress/integration/Resources-status/resourceCommon.ts
+++ tests/e2e/cypress/integration/Resources-status/resourceCommon.ts
@@ -1,19 +1,34 @@
-const containerStateFilter = 'div[aria-label="State filter"]';
-const defaultfilterValue = 'unhandled_problems';
+const containerStateFilter = '[aria-label="State filter"]';
 const labelClearFilter = 'Clear';
+const inputSearch = 'input[placeholder="Search"]';
+const serviceName = 'Ping';
+const searchValue = `s.description:${serviceName}`;
 
-const selectFilterOnInput = (filterValue = defaultfilterValue) => {
-  const inputFilter = 'input[aria-label="MuiSelect-nativeInput"]';
-
-  cy.get(inputFilter).should('not.be.empty');
-
+const selectFilterOnInput = (filterValue) => {
   cy.get(containerStateFilter).should('be.visible').click();
   cy.get(`li[data-value="${filterValue}"]`).click();
 };
 
+const pageAuthorizedToUser = () =>
+  cy.get('nav[aria-label="Breadcrumb"]').should('be.visible');
+
+const toggleCriteras = () =>
+  cy.get('[aria-label="Show criterias filters"]').click();
+
+const isServiceDisplayOnTable = () =>
+  cy.get('table').contains(serviceName).should('be.visible');
+
+const isInputContainServiceName = () =>
+  cy.get(inputSearch).should('contain.value', serviceName);
+
 export {
   containerStateFilter,
-  defaultfilterValue,
+  inputSearch,
+  searchValue,
   labelClearFilter,
   selectFilterOnInput,
+  pageAuthorizedToUser,
+  toggleCriteras,
+  isServiceDisplayOnTable,
+  isInputContainServiceName,
 };
--- tests/e2e/cypress/integration/Resources-status/resourceStatus.ts
+++ tests/e2e/cypress/integration/Resources-status/resourceStatus.ts
@@ -2,111 +2,90 @@ import { Given, And, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
 import {
   containerStateFilter,
+  inputSearch,
+  searchValue,
   selectFilterOnInput,
   labelClearFilter,
+  pageAuthorizedToUser,
+  toggleCriteras,
+  isServiceDisplayOnTable,
+  isInputContainServiceName,
 } from './resourceCommon';
-
-const serviceName = 'Ping';
-const resourcesRegexApiUrl = /\/api\/.+\/monitoring\/resources?/;
 
 Given('a valid centreon user account', () =>
   cy.getCookie('PHPSESSID').should('exist'),
 );
 And('there is a page with a list of resources under monitoring', () =>
-  cy.get('table.MuiTable-root th p.MuiTypography-root').contains('Resource'),
+  cy.get('table').should('be.visible'),
 );
-And('the user can access this page', () =>
-  cy.get('nav[aria-label="Breadcrumb"]').should('be.visible'),
-);
+And('the user can access this page', () => pageAuthorizedToUser());
 And('there is a filters menu on this page', () =>
-  cy
-    .get('div.MuiButtonBase-root')
-    .find('button')
-    .contains('Search')
-    .should('be.visible'),
+  cy.get('button').contains('Search').should('be.visible'),
 );
 
 // Scenario: User first access to the page
-When('the user accesses the page for the first time', () => {
-  cy.get('ol[class="MuiBreadcrumbs-ol"]')
-    .find('li')
-    .each(($li) => $li.text() === 'Resources Status');
-});
-Then('a default filter is applied', () => selectFilterOnInput());
+When('the user accesses the page for the first time', () =>
+  pageAuthorizedToUser(),
+);
+Then('a default filter is applied', () =>
+  selectFilterOnInput('unhandled_problems'),
+);
 
 // Scenario: User can choose from predefined filters
-When('the user clicks on the predefined filters selection', () => {
-  selectFilterOnInput('resource_problems');
-
-  cy.get('svg[aria-label="Show criterias filters"]')
-    .click()
-    .then(() => cy.get('div.MuiAccordionDetails-root').should('be.visible'));
-});
+When('the user clicks on the predefined filters selection', () =>
+  cy.get(containerStateFilter).click(),
+);
 Then('the predefined filters should be listed', () => {
-  cy.get('div.MuiAutocomplete-inputRoot')
-    .find('.MuiChip-label')
-    .its('length')
-    .should('be.gte', 1);
+  cy.get('ul')
+    .children('li')
+    .should('contain', 'Resource problems')
+    .and('contain', 'All')
+    .and('contain', 'Unhandled problems');
 });
 
 // Scenario: User resets applied filters
-Given('filters already applied', () => {
-  cy.get('.MuiButton-label').contains(labelClearFilter).should('be.visible');
-});
+Given('filters already applied', () => cy.get('[data-value="all"]').click());
 When('user clicks on Clear button', () => {
-  cy.get('.MuiButton-label').contains(labelClearFilter).click();
+  toggleCriteras();
+  cy.get('button').contains(labelClearFilter).should('be.visible').click();
 });
 Then('all selected filters should be reset to their default value', () => {
-  const inputFilter = 'input[aria-label="MuiSelect-nativeInput"]';
-
-  cy.get(containerStateFilter).should('be.visible');
-  cy.get(inputFilter).should('not.be.empty');
+  cy.get(containerStateFilter).should('be.visible').and('contain', 'All');
 });
-And('search filter should be emptied', () => {
-  cy.get('div.MuiAutocomplete-inputRoot .MuiChip-label').should(
-    'have.length',
-    0,
-  );
-});
+And('search filter should be emptied', () =>
+  cy.get(inputSearch).should('be.empty'),
+);
 
 // Scenario: User applies filter(s)
-Given('the user has selected filters', () => true);
-And('the user has input a search pattern', () => {
-  cy.get('.MuiAccordionSummary-content input.MuiInputBase-input')
-    .should('have.attr', 'aria-invalid', 'false')
-    .type(`s.description:${serviceName}`);
-});
+Given('the user has selected filters', () =>
+  cy.get(containerStateFilter).contains('All'),
+);
+And('the user has input a search pattern', () =>
+  cy.get(inputSearch).type(searchValue),
+);
 When('the user clicks on the SEARCH button', () => {
-  cy.intercept('GET', resourcesRegexApiUrl).as('searchForTerms');
-  cy.get('.MuiButton-label').contains('Search').click();
+  cy.intercept('GET', /\/api\/.+\/monitoring\/resources?/).as('searchForTerms');
+  cy.saveLocalStorage();
+  cy.get('button').contains('Search').click();
 });
 
 Then(
   'only resources matching the user selected filters should be shown in the result',
-  () => {
-    cy.wait('@searchForTerms');
-    cy.saveLocalStorage();
-    cy.get('p.MuiTypography-root').contains(serviceName);
-  },
+  () => isServiceDisplayOnTable(),
 );
 
 // Scenario: Selected filters are retained when leaving the page
-Given('a set of filters applied to the resources list', () => {
-  return true;
-});
+Given('a set of filters applied to the resources list', () =>
+  isInputContainServiceName(),
+);
 When('the user leaves the page', () => {
-  cy.visitCentreon('/centreon/main.php?p=103');
-  cy.get('iframe#main-content').should('be.visible');
+  cy.visitCentreon('/centreon/main.php?p=50104&o=c'); // User account page
+  cy.contains('My Account');
 });
 Then('the set of filters should be retained on his next visit', () => {
+  cy.restoreLocalStorage();
   cy.visitCentreon('/centreon/monitoring/resources');
 
-  cy.restoreLocalStorage();
-  cy.intercept('GET', resourcesRegexApiUrl).as('filtersSavedRequest');
-
-  cy.wait('@filtersSavedRequest');
-
-  cy.get(containerStateFilter).should('be.visible');
-
-  cy.get('p.MuiTypography-root').contains(serviceName);
+  isInputContainServiceName();
+  isServiceDisplayOnTable();
 });
