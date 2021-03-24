0da961c89f9a33d2457ad2437c1fe74a6c21d789
Kevin Duret <kduret@centreon.com>
2021-03-24 10:49:49 +0100

    fix linting and condition

--- www/lib/HTML/QuickForm/HTML_QuickFormCustom.php
+++ www/lib/HTML/QuickForm/HTML_QuickFormCustom.php
@@ -87,9 +87,9 @@ class HTML_QuickFormCustom extends HTML_QuickForm
     {
         $token = bin2hex(openssl_random_pseudo_bytes(16));
 
-        if (false === isset($_SESSION['x-centreon-token']) &&
-            (isset($_SESSION['x-centreon-token']) &&
-                false === is_array($_SESSION['x-centreon-token']))
+        if (
+            !isset($_SESSION['x-centreon-token'])
+            || (isset($_SESSION['x-centreon-token']) && !is_array($_SESSION['x-centreon-token']))
         ) {
             $_SESSION['x-centreon-token'] = array();
             $_SESSION['x-centreon-token-generated-at'] = array();
