d452d765d0c0241cc3b9235d2066c4521ec5978e
karimsx <karim.squalli@outlook.fr>
2021-03-26 14:36:39 +0100

    add rollup outpput directory

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
+const { resolve } = require('path')
 
-// https://vitejs.dev/config/
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
   plugins: [reactRefresh()],
-  
 
-
-})
+  build: {
+    manifest: true,
+    minify: 'esbuild',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'www/front_src/src/index.html')
+      },
+    },
+    target: 'es2015',
+    outDir: './www/static',
+  },
+});
--- www/front_src/src/dist/assets/pick.70e729da.js
+++
@@ -1,1 +0,0 @@
-import{bf as r}from"./index.eda796bc.js";var n=r((function(r,n){for(var a={},e=0;e<r.length;)r[e]in n&&(a[r[e]]=n[r[e]]),e+=1;return a}));export{n as p};
