83b4659d118659d5e8f9e68a48a08ca22a502529
Bruno d’Auria <bdauria@centreon.com>
2021-03-29 16:44:19 +0200

    Fix time format

--- www/front_src/src/Resources/dateTime.ts
+++ www/front_src/src/Resources/dateTime.ts
@@ -8,14 +8,14 @@ interface ParseAndFormatProps {
 const parseAndFormat = ({ isoDate, to }: ParseAndFormatProps): string =>
   moment.parseZone(isoDate).format(to);
 
-const getFormattedDateTime = (isoDate: string): string =>
-  parseAndFormat({ isoDate, to: 'MM/DD/YYYY HH:mm' });
+const getFormattedDateTime = (isoDate): string =>
+  parseAndFormat({ isoDate, to: 'L LT' });
 
-const getFormattedDate = (isoDate: string): string =>
-  parseAndFormat({ isoDate, to: 'MM/DD/YYYY' });
+const getFormattedDate = (isoDate): string =>
+  parseAndFormat({ isoDate, to: 'L' });
 
-const getFormattedTime = (isoDate: string): string =>
-  parseAndFormat({ isoDate, to: 'HH:mm' });
+const getFormattedTime = (isoDate): string =>
+  parseAndFormat({ isoDate, to: 'LT' });
 
 export {
   parseAndFormat,
