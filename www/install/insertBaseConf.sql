1620a26fc6d82d948ac8ab0a0b68e2a4f6f09754
Kevin Duret <kduret@centreon.com>
2021-03-30 16:43:03 +0200

    chore(install): update version to 20.10.7

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.5');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.7');
 
 --
 -- Contenu de la table `contact`
