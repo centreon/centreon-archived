94624b57f04f1728e87519938318969abec8324d
Thebarda <tdarneix@centreon.com>
2021-03-31 17:06:32 +0200

    fix eslint errors

--- www/front_src/src/Resources/Graph/Performance/models.ts
+++ www/front_src/src/Resources/Graph/Performance/models.ts
@@ -44,11 +44,6 @@ export interface Line {
   legend: string | null;
 }
 
-export interface AdjustTimePeriodProps {
-  start: Date;
-  end: Date;
-}
-
 export enum GraphOptionId {
   displayTooltips = 'displayTooltips',
   displayEvents = 'displayEvents',
