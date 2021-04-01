cb2b5df81bc9832dc89fdc82a65b344c7628df1b
Adrien Morais <amorais@centreon.com>
2021-04-01 15:04:57 +0200

    feedback: construct SELECT statement before to avoid code duplication

--- src/Centreon/Infrastructure/MetaServiceConfiguration/Repository/MetaServiceConfigurationRepositoryRDB.php
+++ src/Centreon/Infrastructure/MetaServiceConfiguration/Repository/MetaServiceConfigurationRepositoryRDB.php
@@ -106,62 +106,35 @@ class MetaServiceConfigurationRepositoryRDB extends AbstractRepositoryDRB implem
                 }
             }
         );
-        if ($contactId === null) {
-            $request = $this->translateDbName(
-                "SELECT SQL_CALC_FOUND_ROWS
-                    ms.meta_id,
-                    ms.meta_name,
-                    ms.meta_display,
-                    ms.data_source_type,
-                    CASE
-                        WHEN ms.data_source_type = 0 THEN 'gauge'
-                        WHEN ms.data_source_type = 1 THEN 'counter'
-                        WHEN ms.data_source_type = 2 THEN 'derive'
-                        WHEN ms.data_source_type = 3 THEN 'absolute'
-                    END AS `data_source_type`,
-                    ms.meta_select_mode,
-                    ms.regexp_str,
-                    ms.metric,
-                    ms.warning,
-                    ms.critical,
-                    ms.meta_activate,
-                    ms.calcul_type,
-                    CASE
-                        WHEN ms.calcul_type = 'AVE' THEN 'average'
-                        WHEN ms.calcul_type = 'SOM' THEN 'sum'
-                        WHEN ms.calcul_type = 'MIN' THEN 'minimum'
-                        WHEN ms.calcul_type = 'MAX' THEN 'maximum'
-                    END AS `calculation_type`
-                FROM `:db`.meta_service ms"
-            );
-        } else {
-            $request = $this->translateDbName(
-                "SELECT SQL_CALC_FOUND_ROWS
-                    ms.meta_id,
-                    ms.meta_name,
-                    ms.meta_display,
-                    ms.data_source_type,
-                    CASE
-                        WHEN ms.data_source_type = 0 THEN 'gauge'
-                        WHEN ms.data_source_type = 1 THEN 'counter'
-                        WHEN ms.data_source_type = 2 THEN 'derive'
-                        WHEN ms.data_source_type = 3 THEN 'absolute'
-                    END AS `data_source_type`,
-                    ms.meta_select_mode,
-                    ms.regexp_str,
-                    ms.metric,
-                    ms.warning,
-                    ms.critical,
-                    ms.meta_activate,
-                    ms.calcul_type,
-                    CASE
-                        WHEN ms.calcul_type = 'AVE' THEN 'average'
-                        WHEN ms.calcul_type = 'SOM' THEN 'sum'
-                        WHEN ms.calcul_type = 'MIN' THEN 'minimum'
-                        WHEN ms.calcul_type = 'MAX' THEN 'maximum'
-                    END AS `calculation_type`
-                FROM `:db`.meta_service ms
-                INNER JOIN `:db`.acl_resources_meta_relations armr
+
+        $request = "SELECT SQL_CALC_FOUND_ROWS
+            ms.meta_id,
+            ms.meta_name,
+            ms.meta_display,
+            ms.data_source_type,
+            CASE
+                WHEN ms.data_source_type = 0 THEN 'gauge'
+                WHEN ms.data_source_type = 1 THEN 'counter'
+                WHEN ms.data_source_type = 2 THEN 'derive'
+                WHEN ms.data_source_type = 3 THEN 'absolute'
+            END AS `data_source_type`,
+            ms.meta_select_mode,
+            ms.regexp_str,
+            ms.metric,
+            ms.warning,
+            ms.critical,
+            ms.meta_activate,
+            ms.calcul_type,
+            CASE
+                WHEN ms.calcul_type = 'AVE' THEN 'average'
+                WHEN ms.calcul_type = 'SOM' THEN 'sum'
+                WHEN ms.calcul_type = 'MIN' THEN 'minimum'
+                WHEN ms.calcul_type = 'MAX' THEN 'maximum'
+            END AS `calculation_type`
+        FROM `:db`.meta_service ms";
+
+        if ($contactId !== null) {
+            $request .= " INNER JOIN `:db`.acl_resources_meta_relations armr
                     ON ms.meta_id = armr.meta_id
                 INNER JOIN `:db`.acl_resources res
                     ON armr.acl_res_id = res.acl_res_id
@@ -176,8 +149,7 @@ class MetaServiceConfigurationRepositoryRDB extends AbstractRepositoryDRB implem
                     ON ag.acl_group_id = agcgr.acl_group_id
                 LEFT JOIN `:db`.contactgroup_contact_relation cgcr
                     ON cgcr.contactgroup_cg_id = agcgr.cg_cg_id
-                    AND cgcr.contact_contact_id = :contact_id"
-            );
+                    AND cgcr.contact_contact_id = :contact_id";
         }
 
         // Search
@@ -192,7 +164,7 @@ class MetaServiceConfigurationRepositoryRDB extends AbstractRepositoryDRB implem
 
         // Pagination
         $request .= $this->sqlRequestTranslator->translatePaginationToSql();
-        $statement = $this->db->prepare($request);
+        $statement = $this->db->prepare($this->translateDbName($request));
 
         foreach ($this->sqlRequestTranslator->getSearchValues() as $key => $data) {
             $type = key($data);
@@ -244,64 +216,34 @@ class MetaServiceConfigurationRepositoryRDB extends AbstractRepositoryDRB implem
      */
     private function findByIdRequest(int $metaId, ?int $contactId): ?MetaServiceConfiguration
     {
-        if ($contactId === null) {
-            $statement = $this->db->prepare(
-                $this->translateDbName(
-                    "SELECT ms.meta_id,
-                        ms.meta_name,
-                        ms.meta_display,
-                        ms.data_source_type,
-                        CASE
-                            WHEN ms.data_source_type = 0 THEN 'gauge'
-                            WHEN ms.data_source_type = 1 THEN 'counter'
-                            WHEN ms.data_source_type = 2 THEN 'derive'
-                            WHEN ms.data_source_type = 3 THEN 'absolute'
-                        END AS `data_source_type`,
-                        ms.meta_select_mode,
-                        ms.regexp_str,
-                        ms.metric,
-                        ms.warning,
-                        ms.critical,
-                        ms.meta_activate,
-                        ms.calcul_type,
-                        CASE
-                            WHEN ms.calcul_type = 'AVE' THEN 'average'
-                            WHEN ms.calcul_type = 'SOM' THEN 'sum'
-                            WHEN ms.calcul_type = 'MIN' THEN 'minimum'
-                            WHEN ms.calcul_type = 'MAX' THEN 'maximum'
-                        END AS `calculation_type`
-                    FROM `:db`.meta_service ms
-                    WHERE meta_id = :id"
-                )
-            );
-        } else {
-            $statement = $this->db->prepare(
-                $this->translateDbName(
-                    "SELECT ms.meta_id,
-                        ms.meta_name,
-                        ms.meta_display,
-                        ms.data_source_type,
-                        CASE
-                            WHEN ms.data_source_type = 0 THEN 'gauge'
-                            WHEN ms.data_source_type = 1 THEN 'counter'
-                            WHEN ms.data_source_type = 2 THEN 'derive'
-                            WHEN ms.data_source_type = 3 THEN 'absolute'
-                        END AS `data_source_type`,
-                        ms.meta_select_mode,
-                        ms.regexp_str,
-                        ms.metric,
-                        ms.warning,
-                        ms.critical,
-                        ms.meta_activate,
-                        ms.calcul_type,
-                        CASE
-                            WHEN ms.calcul_type = 'AVE' THEN 'average'
-                            WHEN ms.calcul_type = 'SOM' THEN 'sum'
-                            WHEN ms.calcul_type = 'MIN' THEN 'minimum'
-                            WHEN ms.calcul_type = 'MAX' THEN 'maximum'
-                        END AS `calculation_type`
-                    FROM `:db`.meta_service ms
-                    INNER JOIN `:db`.acl_resources_meta_relations armr
+        $request = "SELECT ms.meta_id,
+            ms.meta_name,
+            ms.meta_display,
+            ms.data_source_type,
+            CASE
+                WHEN ms.data_source_type = 0 THEN 'gauge'
+                WHEN ms.data_source_type = 1 THEN 'counter'
+                WHEN ms.data_source_type = 2 THEN 'derive'
+                WHEN ms.data_source_type = 3 THEN 'absolute'
+            END AS `data_source_type`,
+            ms.meta_select_mode,
+            ms.regexp_str,
+            ms.metric,
+            ms.warning,
+            ms.critical,
+            ms.meta_activate,
+            ms.calcul_type,
+            CASE
+                WHEN ms.calcul_type = 'AVE' THEN 'average'
+                WHEN ms.calcul_type = 'SOM' THEN 'sum'
+                WHEN ms.calcul_type = 'MIN' THEN 'minimum'
+                WHEN ms.calcul_type = 'MAX' THEN 'maximum'
+            END AS `calculation_type`
+        FROM `:db`.meta_service ms
+        WHERE meta_id = :id";
+
+        if ($contactId !== null) {
+                $request .= " INNER JOIN `:db`.acl_resources_meta_relations armr
                         ON ms.meta_id = armr.meta_id
                     INNER JOIN `:db`.acl_resources res
                         ON armr.acl_res_id = res.acl_res_id
@@ -316,13 +258,16 @@ class MetaServiceConfigurationRepositoryRDB extends AbstractRepositoryDRB implem
                     LEFT JOIN `:db`.contactgroup_contact_relation cgcr
                         ON cgcr.contactgroup_cg_id = agcgr.cg_cg_id
                     WHERE ms.meta_id = :id
-                        AND (agcr.contact_contact_id = :contact_id OR cgcr.contact_contact_id = :contact_id)"
-                )
-            );
+                        AND (agcr.contact_contact_id = :contact_id OR cgcr.contact_contact_id = :contact_id)";
+        }
+
+        $statement = $this->db->prepare($this->translateDbName($request));
+        $statement->bindValue(':id', $metaId, \PDO::PARAM_INT);
+
+        if ($contactId !== null) {
             $statement->bindValue(':contact_id', $contactId, \PDO::PARAM_INT);
         }
 
-        $statement->bindValue(':id', $metaId, \PDO::PARAM_INT);
         $statement->execute();
 
         if (($result = $statement->fetch(\PDO::FETCH_ASSOC)) !== false) {
