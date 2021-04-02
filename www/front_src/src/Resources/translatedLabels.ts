edbe8a25c104d70daf6647a56154d923193060a1
Bruno d'Auria <bdauria@centreon.com>
2021-04-02 09:54:04 +0200

    fix(Resource-status): Various fixes (#9645)

--- www/front_src/src/Resources/translatedLabels.ts
+++ www/front_src/src/Resources/translatedLabels.ts
@@ -204,6 +204,9 @@ export const labelGraphOptions = 'Graph options';
 export const labelDisplayTooltips = 'Display metric values tooltip';
 export const labelAction = 'Action';
 export const labelNotes = 'Notes';
-export const labelChecksDisabled = 'Checks disabled';
+export const labelChecksDisabled = 'All checks are disabled';
+export const labelOnlyPassiveChecksEnabled = 'Only passive checks are enabled';
 export const labelNotificationDisabled = 'Notifications disabled';
 export const labelNotificationEnabled = 'Notifications enabled';
+export const labelSelectAtLeastOneColumn =
+  'At least one column must be selected';
