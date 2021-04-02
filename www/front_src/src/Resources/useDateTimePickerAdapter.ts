39a5acb35161d05d4244c3ae06ff540f6ae8bba7
Thebarda <tdarneix@centreon.com>
2021-04-02 22:34:17 +0200

    Fix picker adapter that cause crash and bad timezone display

--- www/front_src/src/Resources/useDateTimePickerAdapter.ts
+++ www/front_src/src/Resources/useDateTimePickerAdapter.ts
@@ -15,12 +15,27 @@ const useDateTimePickerAdapter = (): typeof DayjsAdapter => {
     }
 
     public date(value): dayjs.Dayjs {
-      // We wrap the dayjs + timezone instance with dayjs to avoid some infinite state update from Material Picker inputs
-      return dayjs(dayjs(value).locale(locale).tz(timezone));
+      return dayjs(value).locale(locale);
     }
 
     public startOfMonth(date: dayjs.Dayjs) {
       return dayjs(date.tz()).startOf('month');
+    }
+
+    public isEqual = (value, comparing) => {
+      if (value === null && comparing === null) {
+        return true;
+      }
+
+      return dayjs(value).isSame(dayjs(comparing), 'minute');
+    };
+
+    public getHours(date) {
+      return date.locale(locale).tz(timezone).hour();
+    }
+
+    public setHours(date: dayjs.Dayjs, count: number) {
+      return date.locale(locale).tz(timezone).set('hour', count);
     }
   }
 
