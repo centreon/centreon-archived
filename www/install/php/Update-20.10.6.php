460c1863bdbdc97192a218188312dbcff2134ede
sc979 <34628915+sc979@users.noreply.github.com>
2021-04-06 19:20:00 +0200

    fix(chore): remove 20.10.7 and enhance 20.10.6 upgrade (#9657)

--- www/install/php/Update-20.10.6.php
+++ www/install/php/Update-20.10.6.php
@@ -20,14 +20,11 @@
  */
 
 include_once __DIR__ . "/../../class/centreonLog.class.php";
-$centreonLog = new CentreonLog();
 
 //error specific content
 $versionOfTheUpgrade = 'UPGRADE - 20.10.6 : ';
+$errorMessage = '';
 
-/**
- * Queries needing exception management and rollback if failing
- */
 try {
     //engine postpone
     if ($pearDB->isColumnExist('cfg_nagios', 'postpone_notification_to_timeperiod')) {
@@ -35,9 +32,54 @@ try {
         $errorMessage = 'Impossible to drop postpone_notification_to_timeperiod from fg_nagios';
         $pearDB->query('ALTER TABLE `cfg_nagios` DROP COLUMN `postpone_notification_to_timeperiod`');
     }
-    $errorMessage = "";
+
+    // Platform_topology refacto
+    if (0 === $pearDB->isColumnExist('platform_topology', 'pending')) {
+        //Create the new column
+        $errorMessage = "Unable to add pending column to platform_topology table";
+        $pearDB->query(
+            "ALTER TABLE `platform_topology` ADD COLUMN `pending` enum('0','1') DEFAULT ('1') AFTER `parent_id`"
+        );
+    }
+    $errorMessage = '';
 } catch (\Exception $e) {
-    $centreonLog->insertLog(
+    (new CentreonLog())->insertLog(
+        4,
+        $versionOfTheUpgrade . $errorMessage .
+        " - Code : " . (int)$e->getCode() .
+        " - Error : " . $e->getMessage() .
+        " - Trace : " . $e->getTraceAsString()
+    );
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int)$e->getCode(), $e);
+}
+
+/**
+ * Queries needing exception management and rollback if failing
+ */
+try {
+    $pearDB->beginTransaction();
+    $errorMessage = "Unable to update pending state on platform_topology table";
+    // find registered platforms
+    $statement = $pearDB->query(
+        "SELECT id FROM `platform_topology`
+        WHERE `type` NOT IN ('central', 'remote', 'poller') OR `server_id` IS NOT NULL"
+    );
+    while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+        $result[] = $row['id'];
+    }
+    if (empty($result)) {
+        throw new Exception('Cannot find the monitoring platform in platform_topology table');
+    }
+    $registeredPlatforms = implode(', ', $result);
+
+    $pearDB->query(
+        "UPDATE `platform_topology` SET `pending` = '0'
+        WHERE id IN ($registeredPlatforms)"
+    );
+    $pearDB->commit();
+} catch (\Exception $e) {
+    $pearDB->rollBack();
+    (new CentreonLog())->insertLog(
         4,
         $versionOfTheUpgrade . $errorMessage .
         " - Code : " . (int)$e->getCode() .
