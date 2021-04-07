820c559e7265bfeff743687bc94fdfdcacfe5ad8
sc979 <34628915+sc979@users.noreply.github.com>
2021-04-07 16:14:06 +0200

    fix(secu): Check user ACL on extensions' XHR (#9663)

--- src/CentreonModule/Application/Webservice/CentreonModuleWebservice.php
+++ src/CentreonModule/Application/Webservice/CentreonModuleWebservice.php
@@ -52,6 +52,23 @@ class CentreonModuleWebservice extends Webservice\WebServiceAbstract implements
 {
 
     /**
+     * Authorize to access to the action
+     *
+     * @param string $action The action name
+     * @param \CentreonUser $user The current user
+     * @param boolean $isInternal If the api is call in internal
+     *
+     * @return boolean If the user has access to the action
+     */
+    public function authorize($action, $user, $isInternal = false)
+    {
+        if (!$user->admin && $user->access->page('50709') === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
      * @OA\Get(
      *   path="/internal.php?object=centreon_module&action=list",
      *   description="Get list of modules and widgets",
