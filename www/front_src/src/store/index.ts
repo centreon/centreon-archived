ed5f234d63a7b872dcc53aec1f7258ba953c8e04
karimsx <karim.squalli@outlook.fr>
2021-04-08 17:46:30 +0200

    update more file to correct extension

--- www/front_src/src/store/index.ts
+++ www/front_src/src/store/index.ts
@@ -25,8 +25,8 @@ const createAppStore = (initialState = {}) => {
   ];
 
   const composeEnhancers =
-    typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    typeof window === 'object' && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
       : compose;
 
   const enhancer = composeEnhancers(applyMiddleware(...middlewares));
