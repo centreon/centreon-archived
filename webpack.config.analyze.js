7eca44d8c7f13d488e4f0debbb3b1a62853d83ef
karimsx <karim.squalli@outlook.fr>
2021-04-22 14:12:42 +0200

    update package json remove webpack old config

--- webpack.config.analyze.js
+++
@@ -1,8 +0,0 @@
-const { merge } = require('webpack-merge');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-const baseConfig = require('./webpack.config');
-
-module.exports = merge(baseConfig, {
-  plugins: [new BundleAnalyzerPlugin()],
-});
