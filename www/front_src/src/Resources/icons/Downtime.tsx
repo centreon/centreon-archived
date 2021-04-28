dd1980f28994607d540293cafe106c7b18d44d8c
karimsx <karim.squalli@outlook.fr>
2021-04-28 13:26:11 +0200

    update downtime icon

--- www/front_src/src/Resources/icons/Downtime.tsx
+++ www/front_src/src/Resources/icons/Downtime.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { SvgIcon, SvgIconProps } from '@material-ui/core';
+import { Icon, makeStyles, SvgIcon, SvgIconProps } from '@material-ui/core';
 
-const Downtime = (props: SvgIconProps): JSX.Element => (
-  <div> to be loaded vite </div>
-);
+import DowntimeIcon from './downtime.icon.svg';
 
+const useStyles = makeStyles({
+  iconRoot: {
+    textAlign: 'center',
+  },
+  imageIcon: {
+    height: '100%',
+  },
+});
+
+const Downtime = (props: SvgIconProps): JSX.Element => {
+  const classes = useStyles();
+
+  return (
+    <Icon classes={{ root: classes.iconRoot }}>
+      <img alt="Downtime" className={classes.imageIcon} src={DowntimeIcon} />
+    </Icon>
+  );
+};
 export default Downtime;
