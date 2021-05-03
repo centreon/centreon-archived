b1681766645b1aae4b1a2745c78bb6a9c065d807
Laurent Pinsivy <lpinsivy@centreon.com>
2021-05-03 09:51:58 +0200

    enh(update): link meta host to localhost poller

---
+++ www/install/sql/centreon/Update-DB-20.10.8.sql
@@ -0,0 +1,7 @@
+-- Add missing link between _Module_Meta and localhost poller
+INSERT INTO ns_host_relation(`nagios_server_id`, `host_host_id`)
+VALUES(
+    (SELECT id FROM nagios_server WHERE localhost = '1'),
+    (SELECT host_id FROM host WHERE host_name = '_Module_Meta')
+)
+ON DUPLICATE KEY UPDATE nagios_server_id = (SELECT id FROM nagios_server WHERE localhost = '1');
