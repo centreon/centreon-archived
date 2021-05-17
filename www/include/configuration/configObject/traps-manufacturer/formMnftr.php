b97396cee1314c956ae92955ce408aff6cb2ac93
jeremyjaouen <jjaouen@centreon.com>
2021-05-17 14:30:48 +0200

    style header block

--- www/include/configuration/configObject/traps-manufacturer/formMnftr.php
+++ www/include/configuration/configObject/traps-manufacturer/formMnftr.php
@@ -1,24 +1,25 @@
 <?php
+
 /*
  * Copyright 2005-2021 Centreon
  * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under 
  * the terms of the GNU General Public License as published by the Free Software 
  * Foundation ; either version 2 of the License.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
  * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with 
  * this program; if not, see <http://www.gnu.org/licenses>.
- * 
+ *
  * Linking this program statically or dynamically with other modules is making a 
  * combined work based on this program. Thus, the terms and conditions of the GNU 
  * General Public License cover the whole combination.
- * 
+ *
  * As a special exception, the copyright holders of this program give Centreon 
  * permission to link this program with independent modules to produce an executable, 
  * regardless of the license terms of these independent modules, and to copy and 
@@ -28,9 +29,9 @@
  * derived from this program. If you modify this program, you may extend this 
  * exception to your version of the program, but you are not obliged to do so. If you
  * do not wish to do so, delete this exception statement from your version.
- * 
+ *
  * For more information : contact@centreon.com
- * 
+ *
  */
 
 #
