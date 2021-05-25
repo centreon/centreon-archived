81b73361479b9384a5470211cce64c52749e766b
Thomas Untoja <tuntoja@centreon.com>
2021-05-25 09:46:13 +0200

    chore(install): update to version 20.04.14

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.13');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.14');
 
 --
 -- Contenu de la table `contact`
