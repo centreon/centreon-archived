5c303c7066a2dc383c170cf1e23e5ce7c8f4ed1b
Laurent Pinsivy <lpinsivy@centreon.com>
2021-05-26 15:42:37 +0200

    fix(update): insert module meta relation if exist (#9869)
    * fix(update): insert module meta relation if exist
    
    * Update www/install/php/Update-21.04.2.php
    
    * Update www/install/php/Update-21.04.2.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * Update www/install/php/Update-21.04.2.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * fix(update): correct script
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/install/php/Update-21.04.2.php
+++ www/install/php/Update-21.04.2.php
@@ -18,3 +18,53 @@
  * For more information : contact@centreon.com
  *
  */
+
+include_once __DIR__ . "/../../class/centreonLog.class.php";
+$centreonLog = new CentreonLog();
+
+//error specific content
+$versionOfTheUpgrade = 'UPGRADE - 21.04.2: ';
+
+$pearDB = new CentreonDB('centreon', 3, false);
+
+/**
+ * Query with transaction
+ */
+try {
+    $pearDB->beginTransaction();
+    /**
+     * Retreive Meta Host Id
+     */
+    $statement = $pearDB->query(
+        "SELECT `host_id` FROM `host` WHERE `host_name` = '_Module_Meta'"
+    );
+
+    /*
+     * Add missing relation
+     */
+    if ($moduleMeta = $statement->fetch()) {
+        $moduleMetaId = $moduleMeta['host_id'];
+        $errorMessage = "Unable to add relation between Module Meta and default poller.";
+        $statement = $pearDB->prepare(
+            "INSERT INTO ns_host_relation(`nagios_server_id`, `host_host_id`)
+            VALUES(
+                (SELECT id FROM nagios_server WHERE localhost = '1'),
+                (:moduleMetaId)
+            )
+            ON DUPLICATE KEY UPDATE nagios_server_id = nagios_server_id"
+        );
+        $statement->bindValue(':moduleMetaId', (int) $moduleMetaId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+    $pearDB->commit();
+} catch (\Exception $e) {
+    $pearDB->rollBack();
+    $centreonLog->insertLog(
+        4,
+        $versionOfTheUpgrade . $errorMessage .
+        " - Code : " . (int)$e->getCode() .
+        " - Error : " . $e->getMessage() .
+        " - Trace : " . $e->getTraceAsString()
+    );
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int)$e->getCode(), $e);
+}
