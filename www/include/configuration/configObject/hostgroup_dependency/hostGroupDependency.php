7d3bdf4e0cdfc802cde604510470c034b66fd659
Laurent Calvet <lcalvet@centreon.com>
2021-06-07 09:20:26 +0200

    Update www/include/configuration/configObject/hostgroup_dependency/hostGroupDependency.php
    Co-authored-by: Adrien Morais <31647811+adr-mo@users.noreply.github.com>

--- www/include/configuration/configObject/hostgroup_dependency/hostGroupDependency.php
+++ www/include/configuration/configObject/hostgroup_dependency/hostGroupDependency.php
@@ -1,8 +1,8 @@
 <?php
 
 /*
- * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
+ * Copyright 2005-2021 Centreon
+ * Centreon is developed by : Julien Mathis and Romain Le Merlus under
  * GPL Licence 2.0.
  *
  * This program is free software; you can redistribute it and/or modify it under
