ba49a8cf059284b712440a58ab9b494cb68cde28
Tom Darneix <tomdar87@outlook.com>
2021-06-24 18:24:59 +0200

    Add the label

--- www/front_src/src/Resources/translatedLabels.ts
+++ www/front_src/src/Resources/translatedLabels.ts
@@ -216,3 +216,5 @@ export const labelMaxDuration1Year = 'The duration must be lesser than a year';
 export const labelAsDisplayed = 'As displayed';
 export const labelMediumSize = 'Medium size';
 export const labelSmallSize = 'Small size';
+export const labelClickHereToDisplayAllLines =
+  'Click here to display all the lines';
