d07866d48b0fd26e966379abf2e27eb2e9bf3607
Bruno d'Auria <bdauria@centreon.com>
2021-06-25 09:05:40 +0200

    Fix timeout to RTL

--- setupTest.js
+++ setupTest.js
@@ -1,7 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { configure } from '@testing-library/dom';
 
-jest.setTimeout(10000);
+const timeout = 10000;
+
+configure({
+  asyncUtilTimeout: timeout,
+});
+
+jest.setTimeout(timeout);
 
 document.createRange = () => ({
   setStart: () => {},
