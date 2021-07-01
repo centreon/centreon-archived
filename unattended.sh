176823e197c98d50ca2642600da06376ed9eaaec
Thomas Untoja <tuntoja@centreon.com>
2021-07-01 11:12:16 +0200

    fix(unattended): added support for centos 8.3 and upper centos 8.x)

--- unattended.sh
+++ unattended.sh
@@ -347,7 +347,7 @@ function set_required_prerequisite() {
 			subscription-manager repos --enable codeready-builder-for-rhel-8-x86_64-rpms
 			;;
 
-		centos-release-8.3* | centos-linux-release* | centos-stream-release*)
+		centos-release-8.[3-9]* | centos-linux-release* | centos-stream-release*)
 			BASE_PACKAGES=(dnf-plugins-core epel-release)
 			$PKG_MGR config-manager --set-enabled powertools
 			;;
