286ff13f59fdb33d88bf1a9ed2e8c77660f872ae
Julien Mathis <jmathis@centreon.com>
2021-07-12 15:58:08 +0200

    empty color files

--- www/Themes/Centreon-2/Color/blue_css.php
+++ www/Themes/Centreon-2/Color/blue_css.php
@@ -37,47 +37,4 @@ require_once realpath(dirname(__FILE__) . "/../../../../config/centreon.config.p
 
 header("Content-type: text/css");
 
-$color1 = "#F2F2F2";
-$color2 = "#666666";
-$color3 = "#E3A385";
-$color4 = "#d5dfeb";
-$color5 = "#ced3ed";
-$color6 = "#BFD0E2";
-$color7 = "#AAAAAA";
-$color8 = "#D1DCEB";
-$color9 = "#FFFFFF";
-$color10 = "#AAAAEE";
-$color11 = "#592bed";
-$color12 = "#242af6";
-
-$color13 = "#5e5e5e";
-$color14 = "#E8AB5C";
-
-$menu1_bgcolor = "#009fdf";
-$menu2_bgcolor = "#009fdf";
-
-$colorHeader_1 = "#cfedf9";
-$colorGradient_2 = "#02bbff";
-
-$footerline2 = "#dedede";
-
-$color_list_1 = "#f8fdff";
-$color_list_1_hover = "#FFF";
-
-$color_list_2 = "#f0fbff";
-$color_list_2_hover = "#FFF";
-$color_list_3 = "#fada83";
-$color_list_3_hover = "#bada83";
-$color_list_4 = "#fdc11e";
-$color_list_4_hover = "#bdc11e";
-$color_list_up = "#B2F867";
-$color_list_up_hover = "#B2A867";
-$color_list_down = "#ffbbbb";
-$color_list_down_hover = "#dfbbbb";
-
-$menu1_bgimg = "#009fdf";
-
-$menu2_color = "#c1ecff";
-$bg_image_header = "../../Images/bg_header_grayblue.gif";
-
 require_once _CENTREON_PATH_ . "www/Themes/Centreon-2/color_css.php";
--- www/Themes/Centreon-2/Color/green_css.php
+++ www/Themes/Centreon-2/Color/green_css.php
@@ -37,46 +37,4 @@ require_once realpath(dirname(__FILE__) . "/../../../../config/centreon.config.p
 
 header("Content-type: text/css");
 
-$color1 = "#F2F2F2";
-$color2 = "#666666";
-$color3 = "#E3A385";
-$color4 = "#d5dfeb";
-$color5 = "#ced3ed";
-$color6 = "#BFD0E2";
-$color7 = "#AAAAAA";
-$color8 = "#D1DCEB";
-$color9 = "#FFFFFF";
-$color10 = "#AAAAEE";
-$color11 = "#592bed";
-$color12 = "#242af6";
-
-$color13 = "#5e5e5e";
-$color14 = "#E8AB5C";
-
-$menu1_bgcolor = "#ADC7A1";
-$menu2_bgcolor = "#43B02A";
-
-$colorHeader_1 = "#cfedf9";
-
-$footerline2 = "#dedede";
-
-$color_list_1 = "#F7FAFF";
-$color_list_1_hover = "#FDF0D5";
-
-$color_list_2 = "#EDF4FF";
-$color_list_2_hover = "#FDF0D5";
-$color_list_3 = "#fada83";
-$color_list_3_hover = "#bada83";
-$color_list_4 = "#fdc11e";
-$color_list_4_hover = "#bdc11e";
-$color_list_up = "#B2F867";
-$color_list_up_hover = "#B2A867";
-$color_list_down = "#ffbbbb";
-$color_list_down_hover = "#dfbbbb";
-
-$bg_image_header = "../Images/bg_header_green.gif";
-$menu1_bgimg = "#43B02A";
-
-$menu2_color = "#e7ffb4";
-
 require_once _CENTREON_PATH_ . "www/Themes/Centreon-2/color_css.php";
