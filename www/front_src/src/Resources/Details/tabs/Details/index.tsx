b2e9ab3b42b9b2c42835e62182d7e7543f1a79aa
Tom Darneix <tomdar87@outlook.com>
2021-07-16 12:51:07 +0200

    Fix eslint errors

--- www/front_src/src/Resources/Details/tabs/Details/index.tsx
+++ www/front_src/src/Resources/Details/tabs/Details/index.tsx
@@ -4,42 +4,17 @@ import { isNil, isEmpty } from 'ramda';
 import { useTranslation } from 'react-i18next';
 import { ParentSize } from '@visx/visx';
 
-import {
-  Grid,
-  Typography,
-  styled,
-  Tooltip,
-  IconButton,
-  makeStyles,
-} from '@material-ui/core';
+import { Grid, styled, makeStyles } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
-import IconCopyFile from '@material-ui/icons/FileCopy';
 
-import {
-  useSnackbar,
-  Severity,
-  useLocaleDateTimeFormat,
-  copyToClipboard,
-} from '@centreon/ui';
+import { useLocaleDateTimeFormat } from '@centreon/ui';
 
-import {
-  labelCopy,
-  labelCommand,
-  labelCommandCopied,
-  labelSomethingWentWrong,
-} from '../../../translatedLabels';
 import { ResourceDetails } from '../../models';
 
-import Card from './Card';
 import DetailsCard from './DetailsCard';
 import getDetailCardLines from './DetailsCard/cards';
-import CommandWithArguments from './CommandLine';
 
 const useStyles = makeStyles((theme) => ({
-  details: {
-    display: 'grid',
-    gridRowGap: theme.spacing(2),
-  },
   loadingSkeleton: {
     display: 'grid',
     gridRowGap: theme.spacing(2),
@@ -70,9 +45,6 @@ interface Props {
 const DetailsTab = ({ details }: Props): JSX.Element => {
   const { t } = useTranslation();
   const { toDateTime } = useLocaleDateTimeFormat();
-  const classes = useStyles();
-
-  const { showMessage } = useSnackbar();
 
   if (isNil(details)) {
     return <LoadingSkeleton />;
