9e3fa81be4bdef93242d1e29d1c119d50046b2c6
Charles Gautier <cgautier@centreon.com>
2021-07-23 11:33:15 +0200

    chore(install): update version to 21.04.4

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.3');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.4');
 
 --
 -- Contenu de la table `contact`
