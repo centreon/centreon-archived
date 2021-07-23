0299c46bb51c7b255eef39eb8fdb08b4aa4ebfaa
Charles Gautier <cgautier@centreon.com>
2021-07-23 11:36:31 +0200

    chore(install): update version to 20.04.17

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.16');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.17');
 
 --
 -- Contenu de la table `contact`
