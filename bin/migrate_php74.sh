710ab516e59c449b63282e04701c2b073c4f46ae
Kevin Duret <kduret@centreon.com>
2021-07-27 14:45:41 +0200

    update script

--- bin/migrate_php74.sh
+++ bin/migrate_php74.sh
@@ -33,11 +33,11 @@
 
 function usage() {
     cat <<EOF
-This script aims to upgrade the php library from 7.2 to 7.4.
+This script aims to upgrade the php library from 7.3 to 7.4.
 This script will:
     - Install remi repository
     - Install dependencies needed for the upgrade
-    - Copy existing files from the php 7.2 to php 7.4 configuration directory
+    - Copy existing files from the php 7.3 to php 7.4 configuration directory
     - Setup the system to run by default the php 7.4 version
 Usage: ./${0##*/}
 Parameters:
@@ -63,12 +63,6 @@ function upgrade_rhel7() {
         https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm \
         https://rpms.remirepo.net/enterprise/remi-release-7.rpm \
         yum-utils
-<<<<<<< HEAD
-    yum-config-manager --enable remi-php74
-    ;;
-  '8')
-    # CentOS 8 specific part
-=======
     yum-config-manager -q --enable remi-php74
     yum install -q -y \
         php74 \
@@ -88,55 +82,28 @@ function upgrade_rhel7() {
         php74-php-ioncube-loader \
         php74-php-pecl-gnupg
 
-    info "Copying php-fpm configuration from 7.2 to 7.4"
-    \cp /etc/opt/rh/rh-php72/php-fpm.d/*.conf /etc/opt/remi/php74/php-fpm.d/
+    info "Copying php-fpm configuration from 7.3 to 7.4"
+    \cp /etc/opt/rh/rh-php73/php-fpm.d/*.conf /etc/opt/remi/php74/php-fpm.d/
 
-    info "Copying php configuration from 7.2 to 7.4"
-    cp /etc/opt/rh/rh-php72/php.d/50-centreon.ini /etc/opt/remi/php74/php.d/50-centreon.ini
+    info "Copying php configuration from 7.3 to 7.4"
+    cp /etc/opt/rh/rh-php73/php.d/50-centreon.ini /etc/opt/remi/php74/php.d/50-centreon.ini
 
     info "Configuring system to use new PHP 7.4 binary"
-    mv /opt/rh/rh-php72/root/usr/bin/php{,.backup}
-    ln -s /opt/remi/php74/root/usr/bin/php /opt/rh/rh-php72/root/usr/bin/php
-    systemctl -q stop rh-php72-php-fpm
-    systemctl -q disable rh-php72-php-fpm
+    mv /opt/rh/rh-php3/root/usr/bin/php{,.backup}
+    ln -s /opt/remi/php74/root/usr/bin/php /opt/rh/rh-php73/root/usr/bin/php
+    systemctl -q stop rh-php73-php-fpm
+    systemctl -q disable rh-php73-php-fpm
     systemctl -q start php74-php-fpm
     systemctl -q enable php74-php-fpm
 }
 
 function upgrade_rhel8() {
     info "Installing dependencies for PHP 7.4"
->>>>>>> 6469bebabd (improve script)
     dnf install -q -y \
         https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm \
         https://rpms.remirepo.net/enterprise/remi-release-8.rpm
     dnf module reset php
     dnf module install php:remi-7.4
-<<<<<<< HEAD
-    ;;
-  *)
-    error_and_exit "This unattended installation script only supports CentOS 7 and CentOS 8."
-    ;;
-esac
-
-info "Installing dependencies for PHP 7.4"
-yum install -q -y \
-    php74 \
-    php74-php-cli \
-    php74-php-pdo \
-    php74-php-mysqlnd \
-    php74-php-gd \
-    php74-php-xml \
-    php74-php-mbstring \
-    php74-php-ldap \
-    php74-php-snmp \
-    php74-php-intl \
-    php74-php-fpm \
-    php74-php-curl \
-    php74-php-zip \
-    php74-php-pear \
-    php74-ioncube-loader \
-    php74-php-pecl-gnupg
-=======
     dnf install -q -y \
         php-cli \
         php-pdo \
@@ -154,13 +121,12 @@ yum install -q -y \
         php-ioncube-loader \
         php-pecl-gnupg
 
-    info "Backuping ioncube configuration of php 7.2"
+    info "Backuping ioncube configuration of php 7.3"
     mv /etc/php.d/10-ioncube_loader.ini{,.backup}
 
     info "Restarting php-fpm"
     systemctl -q restart php-fpm
 }
->>>>>>> 6469bebabd (improve script)
 
 if [ \! -e /etc/redhat-release ] ; then
   error_and_exit "This script can only be executed on CentOS 7"
