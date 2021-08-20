2cc469c2fa503e3c6d256eea69d7c286f96d2ece
Bruno d'Auria <bdauria@centreon.com>
2021-08-20 11:31:13 +0200

    Fixes

--- tests/e2e/cypress/support/database.ts
+++ tests/e2e/cypress/support/database.ts
@@ -49,45 +49,37 @@ let configCheckStepCount = 0;
 
 const checkThatConfigurationIsExported = (): void => {
   cy.log('Checking that configuration is exported');
-  cy.exec(`bash -c "whoami"`)
-    .then((res) => {
-      cy.log(res.stdout);
-      return cy.exec(
-        `bash -c "docker exec -i ${Cypress.env(
-          'dockerName',
-        )} bash -c date -r /etc/centreon-engine/hosts.cfg"`,
+  cy.exec(
+    `docker exec -i ${Cypress.env(
+      'dockerName',
+    )} date -r /etc/centreon-engine/hosts.cfg"`,
+  ).then(({ stdout }): Cypress.Chainable<null> | null => {
+    configCheckStepCount += 1;
+
+    const twoMinutes = 2000;
+    const exported =
+      new Date().getTime() - new Date(stdout).getTime() < twoMinutes;
+
+    cy.log(stdout);
+    cy.log('Configuration exported', exported);
+    cy.log('Configuration export check step count', configCheckStepCount);
+
+    if (exported) {
+      return null;
+    }
+
+    if (configCheckStepCount < maxSteps) {
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait(stepWaitingTime, { log: false });
+
+      // return cy.wrap(null)
+      return applyConfigurationViaClapi().then(() =>
+        checkThatConfigurationIsExported(),
       );
-    })
-    .then(({ stdout }): Cypress.Chainable<null> | null => {
-      // let configurationExported = false;
-      configCheckStepCount += 1;
+    }
 
-      const twoMinutes = 5000;
-      const exported =
-        new Date().getTime() - new Date(stdout).getTime() < twoMinutes;
-
-      // configurationExported = exported as boolean;
-
-      cy.log(stdout);
-      cy.log('Configuration exported', exported);
-      cy.log('Configuration export check step count', configCheckStepCount);
-
-      if (exported) {
-        return null;
-      }
-
-      if (configCheckStepCount < maxSteps) {
-        // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.wait(stepWaitingTime, { log: false });
-
-        // return cy.wrap(null)
-        return applyConfigurationViaClapi().then(() =>
-          checkThatConfigurationIsExported(),
-        );
-      }
-
-      throw new Error(`No configuration export after ${timeout}ms`);
-    });
+    throw new Error(`No configuration export after ${timeout}ms`);
+  });
 };
 
 export {
