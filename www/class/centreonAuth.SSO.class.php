d6461f465c75341109b76bede4343d729c00bb02
Laurent Pinsivy <lpinsivy@centreon.com>
2021-08-31 16:31:37 +0200

    fix(openid): fixed http method for userinfo endpoint - #10044

--- www/class/centreonAuth.SSO.class.php
+++ www/class/centreonAuth.SSO.class.php
@@ -482,7 +482,7 @@ class CentreonAuthSSO extends CentreonAuth
         try {
             $result = $restHttp->call(
                 $url,
-                'POST',
+                'GET',
                 $data,
                 [$authentication],
                 true,
