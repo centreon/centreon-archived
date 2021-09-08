612917898b0f2833f382e37716f2df61f9062b3a
Laurent Pinsivy <lpinsivy@centreon.com>
2021-09-08 16:39:59 +0200

    enh(openid): Allow to do not defined redirect URL

--- www/class/centreonAuth.SSO.class.php
+++ www/class/centreonAuth.SSO.class.php
@@ -104,7 +104,7 @@ class CentreonAuthSSO extends CentreonAuth
             $clientSecret = $this->ssoOptions['openid_connect_client_secret'];
             if (empty($this->ssoOptions['openid_connect_redirect_url'])) {
                 $redirectNoEncode = '{$scheme}://{$hostname}:{$port}'
-                . $this->ssoOptions['oreon_web_path'] . 'index.php';
+                . rtim($this->ssoOptions['oreon_web_path'], "/") . "/" . 'index.php';
             } else {
                 $redirectNoEncode = $this->ssoOptions['openid_connect_redirect_url'];
             }
