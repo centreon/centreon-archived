4d2719101c986bc6cb79c5beeda8957aee3d75a9
Jérémy Jaouen <jjaouen@centreon.com>
2021-09-09 09:20:52 +0200

    update upgrade version

--- www/install/php/Update-21.04.4.php
+++ www/install/php/Update-21.04.4.php
@@ -22,7 +22,7 @@ include_once __DIR__ . "/../../class/centreonLog.class.php";
 $centreonLog = new CentreonLog();
 
 //error specific content
-$versionOfTheUpgrade = 'UPGRADE - 21.10.0-beta.1: ';
+$versionOfTheUpgrade = 'UPGRADE - 21.04.4:';
 
 $pearDB = new CentreonDB('centreon', 3, false);
 
