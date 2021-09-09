a38e4aa0fe1a34e7e4c15f007610930880fd601b
Bruno d'Auria <bdauria@centreon.com>
2021-09-09 15:59:46 +0200

    Update www/front_src/src/components/forms/poller/PollerFormStepTwo.jsx
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>

--- www/front_src/src/components/forms/poller/PollerFormStepTwo.jsx
+++ www/front_src/src/components/forms/poller/PollerFormStepTwo.jsx
@@ -146,7 +146,7 @@ class PollerFormStepTwo extends Component {
               </Button>
             </div>
             {error ? (
-              <Typography style={{ color: '#d0021b' }} variant="body2">
+              <Typography color="error" variant="body2">
                 {error.message}
               </Typography>
             ) : null}
