22c9b42ff817f7768378ffd1752af90a786f2462
Tom Darneix <tomdar87@outlook.com>
2021-09-15 17:17:45 +0200

    Fix test

--- tests/e2e/cypress/integration/Resources-status/03-top-counter-pollers/index.ts
+++ tests/e2e/cypress/integration/Resources-status/03-top-counter-pollers/index.ts
@@ -3,6 +3,7 @@ import { When, Then } from 'cypress-cucumber-preprocessor/steps';
 When(
   'I click on the pollers icon and I click on the configuration button',
   () => {
+    cy.contains('Unhandled problems');
     cy.get('[aria-label="Pollers"]').click();
     cy.get('button').contains('Configure pollers').click();
   },
