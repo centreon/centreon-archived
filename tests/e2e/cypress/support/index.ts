3a0ee718d86972711240df71e3b1618ff6931e97
Tom Darneix <tomdar87@outlook.com>
2021-09-15 15:19:30 +0200

    Add login page check up when re-trying a test

--- tests/e2e/cypress/support/index.ts
+++ tests/e2e/cypress/support/index.ts
@@ -24,12 +24,23 @@ before(() => {
   cy.exec(`npx wait-on ${Cypress.config().baseUrl}`).then(() => {
     cy.visit(`${Cypress.config().baseUrl}`);
 
-    cy.fixture('users/admin.json').then((userAdmin) => {
-      cy.get('input[placeholder="Login"]').type(userAdmin.login);
-      cy.get('input[placeholder="Password"]').type(userAdmin.password);
-    });
+    cy.contains('Connect').then((c) => {
+      const element = c?.[0] as HTMLInputElement | undefined;
 
-    cy.get('form').submit();
+      const isNotInLoginPage =
+        element === undefined || element?.getAttribute('value') !== 'Connect';
+
+      if (isNotInLoginPage) {
+        return;
+      }
+
+      cy.fixture('users/admin.json').then((userAdmin) => {
+        cy.get('input[placeholder="Login"]').type(userAdmin.login);
+        cy.get('input[placeholder="Password"]').type(userAdmin.password);
+      });
+
+      cy.get('form').submit();
+    });
   });
 
   Cypress.Cookies.defaults({
