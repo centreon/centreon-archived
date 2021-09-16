042c6b8644b62654624af37b9a97dc01faf6fc71
Tom Darneix <tomdar87@outlook.com>
2021-09-16 22:51:37 +0200

    Add checkboxes forr persistent and sticky

--- www/front_src/src/Resources/Actions/Resource/Acknowledge/index.tsx
+++ www/front_src/src/Resources/Actions/Resource/Acknowledge/index.tsx
@@ -19,13 +19,23 @@ import DialogAcknowledge from './Dialog';
 
 const validationSchema = Yup.object().shape({
   comment: Yup.string().required(labelRequired),
+  is_sticky: Yup.boolean(),
   notify: Yup.boolean(),
+  persistent: Yup.boolean(),
 });
 
 interface Props {
-  onClose;
-  onSuccess;
+  onClose: () => void;
+  onSuccess: () => void;
   resources: Array<Resource>;
+}
+
+export interface AcknowledgeFormValues {
+  acknowledgeAttachedResources: boolean;
+  comment?: string;
+  isSticky: boolean;
+  notify: boolean;
+  persistent: boolean;
 }
 
 const AcknowledgeForm = ({
@@ -45,13 +55,15 @@ const AcknowledgeForm = ({
     request: acknowledgeResources,
   });
 
-  const form = useFormik({
+  const form = useFormik<AcknowledgeFormValues>({
     initialValues: {
       acknowledgeAttachedResources: false,
       comment: undefined,
+      isSticky: false,
       notify: false,
+      persistent: false,
     },
-    onSubmit: (values) => {
+    onSubmit: (values): void => {
       sendAcknowledgeResources({
         params: values,
         resources,
