97d80915e0c4b1f11298de4b35e73e5665a5f7fb
Bruno d'Auria <bdauria@centreon.com>
2021-09-17 13:56:20 +0200

    use use_deprecated_pages flag

--- www/front_src/src/Provider/index.tsx
+++ www/front_src/src/Provider/index.tsx
@@ -114,6 +114,7 @@ const AppProvider = (): JSX.Element | null => {
             locale: retrievedUser.locale || 'en',
             name: retrievedUser.name,
             timezone: retrievedUser.timezone,
+            use_deprecated_pages: retrievedUser.use_deprecatedPages,
           });
           setDowntime({
             default_duration: parseInt(
