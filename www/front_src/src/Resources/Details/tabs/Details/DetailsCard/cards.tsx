76da5711180125e8012e3039dca03aa0a796fe3f
Jordan CARO <jcaro@centreon.com>
2021-09-24 17:32:44 +0200

    rename field by shouldBeDisplayed

--- www/front_src/src/Resources/Details/tabs/Details/DetailsCard/cards.tsx
+++ www/front_src/src/Resources/Details/tabs/Details/DetailsCard/cards.tsx
@@ -41,9 +41,9 @@ import CommandLineCard from './CommandLineCard';
 
 export interface DetailCardLine {
   active?: boolean;
-  field?: string | number | boolean | Array<unknown>;
   isCustomCard?: boolean;
   line: JSX.Element;
+  shouldBeDisplayed?: string | number | boolean | Array<unknown>;
   title: string;
   xs?: 6 | 12;
 }
@@ -67,7 +67,6 @@ const getDetailCardLines = ({
 
   return [
     {
-      field: details.information,
       isCustomCard: true,
       line: (
         <ExpandableCard
@@ -76,116 +75,116 @@ const getDetailCardLines = ({
           title={t(labelStatusInformation)}
         />
       ),
+      shouldBeDisplayed: details.information,
       title: labelStatusInformation,
       xs: 12,
     },
     {
-      field: details.downtimes,
       isCustomCard: true,
       line: <DowntimesCard details={details} />,
+      shouldBeDisplayed: details.downtimes,
       title: labelDowntimeDuration,
       xs: 12,
     },
     {
-      field: details.acknowledgement ? true : undefined,
       isCustomCard: true,
       line: <AcknowledgementCard details={details} />,
+      shouldBeDisplayed: details.acknowledgement ? true : undefined,
       title: labelAcknowledgement,
       xs: 12,
     },
     {
-      field: details.fqdn,
       line: <DetailsLine line={details.fqdn} />,
+      shouldBeDisplayed: details.fqdn,
       title: labelFqdn,
       xs: 12,
     },
     {
-      field: details.alias,
       line: <DetailsLine line={details.alias} />,
+      shouldBeDisplayed: details.alias,
       title: labelAlias,
     },
     {
-      field: details.monitoring_server_name,
       line: <DetailsLine line={details.monitoring_server_name} />,
+      shouldBeDisplayed: details.monitoring_server_name,
       title: labelMonitoringServer,
     },
     {
-      field: details.timezone,
       line: <DetailsLine line={details.timezone} />,
+      shouldBeDisplayed: details.timezone,
       title: labelTimezone,
     },
     {
-      field: details.duration,
       line: <DetailsLine line={`${details.duration} - ${details.tries}`} />,
+      shouldBeDisplayed: details.duration,
       title: labelCurrentStateDuration,
     },
     {
-      field: details.last_status_change,
       line: <DetailsLine line={toDateTime(details.last_status_change)} />,
+      shouldBeDisplayed: details.last_status_change,
       title: labelLastStateChange,
     },
     {
-      field:
+      line: <DetailsLine line={toDateTime(details.last_time_with_no_issue)} />,
+      shouldBeDisplayed:
         details.last_time_with_no_issue &&
         !equals(details.status.severity_code, SeverityCode.Ok),
-      line: <DetailsLine line={toDateTime(details.last_time_with_no_issue)} />,
       title: labelLastCheckWithOkStatus,
     },
     {
-      field: details.last_check,
       line: <DetailsLine line={toDateTime(details.last_check)} />,
+      shouldBeDisplayed: details.last_check,
       title: labelLastCheck,
     },
     {
-      field: displayChecksIcon ? true : undefined,
       line: (
         <ChecksIcon {...pick(['active_checks', 'passive_checks'], details)} />
       ),
+      shouldBeDisplayed: displayChecksIcon ? true : undefined,
       title: labelCheck,
     },
     {
-      field: details.next_check,
       line: <DetailsLine line={toDateTime(details.next_check)} />,
+      shouldBeDisplayed: details.next_check,
       title: labelNextCheck,
     },
     {
-      field: details.execution_time,
       line: <DetailsLine line={`${details.execution_time} s`} />,
+      shouldBeDisplayed: details.execution_time,
       title: labelCheckDuration,
     },
     {
-      field: details.latency,
       line: <DetailsLine line={`${details.latency} s`} />,
+      shouldBeDisplayed: details.latency,
       title: labelLatency,
     },
     {
-      field: details.percent_state_change,
       line: <PercentStateChangeCard details={details} />,
+      shouldBeDisplayed: details.percent_state_change,
       title: labelPercentStateChange,
     },
     {
-      field: details.last_notification,
       line: <DetailsLine line={toDateTime(details.last_notification)} />,
+      shouldBeDisplayed: details.last_notification,
       title: labelLastNotification,
     },
     {
-      field: details.notification_number,
       line: <DetailsLine line={details.notification_number.toString()} />,
+      shouldBeDisplayed: details.notification_number,
       title: labelCurrentNotificationNumber,
     },
     {
-      field: details.calculation_type,
       line: <DetailsLine line={details.calculation_type} />,
+      shouldBeDisplayed: details.calculation_type,
       title: labelCalculationType,
     },
     {
-      field: details.groups,
       line: <Groups details={details} />,
+      shouldBeDisplayed: details.groups,
       title: labelGroups,
       xs: 12,
     },
     {
-      field: details.performance_data,
       isCustomCard: true,
       line: (
         <ExpandableCard
@@ -193,13 +192,14 @@ const getDetailCardLines = ({
           title={t(labelPerformanceData)}
         />
       ),
+      shouldBeDisplayed: details.performance_data,
       title: labelPerformanceData,
       xs: 12,
     },
     {
-      field: details.command_line,
       isCustomCard: true,
       line: <CommandLineCard details={details} />,
+      shouldBeDisplayed: details.command_line,
       title: labelCommand,
       xs: 12,
     },
