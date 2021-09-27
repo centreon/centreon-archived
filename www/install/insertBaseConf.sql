c6841dd3b868988ed71daa58eeaf629ba3e66f0f
tuntoja <58987095+tuntoja@users.noreply.github.com>
2021-09-27 19:01:51 +0200

    chore(install): update to version 20.04.19 (#10201)

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.18');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.04.19');
 
 --
 -- Contenu de la table `contact`
