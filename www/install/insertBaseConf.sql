67b83bcb7f0c8a9407a0cbba56b86f92e63b73d5
tuntoja <58987095+tuntoja@users.noreply.github.com>
2021-09-28 12:01:58 +0200

    chore(install): update to version 21.04.6 (#10203)
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.5');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.6');
 
 --
 -- Contenu de la table `contact`
