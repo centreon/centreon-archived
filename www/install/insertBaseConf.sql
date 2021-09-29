912511fe522d854db545990c15194d8ae5eeb98a
tuntoja <58987095+tuntoja@users.noreply.github.com>
2021-09-29 11:32:19 +0200

    chore(install): upgrade to version 20.10.12 (#10202)

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.11');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.12');
 
 --
 -- Contenu de la table `contact`
