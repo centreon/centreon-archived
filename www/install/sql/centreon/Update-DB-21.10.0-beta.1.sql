1589804a598a69aec996eb8dc9179184a9ce2ef0
alaunois <alaunois@centreon.com>
2021-09-29 11:48:20 +0200

    Mon 10852 add tls common name on broker config (#10195)
    * ehn(conf) add tls_hostname to brocker input/output IPV4 (install)
    
    * enh(conf) add help translation
    
    * ehn(conf) add tls_hostname to brocker input/output IPV4 (update 21.10)

--- www/install/sql/centreon/Update-DB-21.10.0-beta.1.sql
+++ www/install/sql/centreon/Update-DB-21.10.0-beta.1.sql
@@ -54,3 +54,10 @@ ALTER TABLE `session` MODIFY `last_reload` BIGINT UNSIGNED;
 
 -- Add one-click export button column to contact
 ALTER TABLE `contact` ADD COLUMN `enable_one_click_export` enum('0','1') DEFAULT '0';
+
+-- Add TLS hostname in config brocker input/outputs IPV4
+INSERT INTO `cb_field` (`cb_field_id`, `fieldname`, `displayname`, `description`, `fieldtype`, `external`) VALUES
+(76, 'tls_hostname', 'TLS Host name', 'Expected TLS certificate common name (CN) - leave blank if unsure.', 'text', NULL);
+
+INSERT INTO `cb_type_field_relation` (`cb_type_id`, `cb_field_id`, `is_required`, `order_display`) VALUES
+(3, 76, 0, 5);
