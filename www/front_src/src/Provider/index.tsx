1c5e8b3d117b0beb4bd121a854f3a769f522123b
Bruno d'Auria <bdauria@centreon.com>
2021-10-12 14:42:27 +0200

    Fix ACL

--- www/front_src/src/Provider/index.tsx
+++ www/front_src/src/Provider/index.tsx
@@ -13,6 +13,7 @@ import {
   useUser,
   Context,
   useRefreshInterval,
+  useDowntime,
 } from '@centreon/ui-context';
 
 import App from '../App';
@@ -25,7 +26,6 @@ import {
   parametersEndpoint,
 } from './endpoint';
 import { DefaultParameters } from './models';
-import useDowntime from './useDowntime';
 
 const store = createStore();
 
