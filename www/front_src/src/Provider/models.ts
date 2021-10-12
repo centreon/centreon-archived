b4640a7f009e8347981eb306a3b52cbe6ac4728b
Bruno d'Auria <bdauria@centreon.com>
2021-10-12 15:49:24 +0200

    Update models

--- www/front_src/src/Provider/models.ts
+++ www/front_src/src/Provider/models.ts
@@ -1,43 +1,9 @@
 import { KeyValuePair } from 'ramda';
 
-export interface User {
-  alias: string;
-  locale: string;
-  name: string;
-  timezone: string;
-}
-
-export type UserContext = {
-  acl: Acl;
-  downtime: Downtime;
-  refreshInterval: number;
-} & User;
-
-export interface ActionAcl {
-  acknowledgement: boolean;
-  check: boolean;
-  disacknowledgement: boolean;
-  downtime: boolean;
-  submit_status: boolean;
-}
-
-export interface Actions {
-  host: ActionAcl;
-  service: ActionAcl;
-}
-
-interface Acl {
-  actions: Actions;
-}
-
-type Translation = KeyValuePair<string, string>;
-export type Translations = KeyValuePair<string, Translation>;
-
 export interface DefaultParameters {
   monitoring_default_downtime_duration: string;
   monitoring_default_refresh_interval: string;
 }
 
-export interface Downtime {
-  default_duration: number;
-}
+type Translation = KeyValuePair<string, string>;
+export type Translations = KeyValuePair<string, Translation>;
