527e0a25db55f5c0391a9a9f2451d4e0acaf12df
Adrien Morais <amorais@centreon.com>
2021-10-12 14:43:19 +0200

    kduret feedbacks: return type re-added

--- www/class/centreonACL.class.php
+++ www/class/centreonACL.class.php
@@ -2455,6 +2455,7 @@ class CentreonACL
      * @access public
      * @param array $options
      * @param boolean $localOnly Indicates if only local contactgroups should be searched
+     * @return void
      */
     public function getContactGroupAclConf(array $options = [], bool $localOnly = true)
     {
