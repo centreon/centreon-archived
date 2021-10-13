92224230bd4c624e0fcebaa878d502ba7f3cd3b1
Bruno d'Auria <bdauria@centreon.com>
2021-10-13 09:33:35 +0200

    Mock ACL endpoint

--- www/front_src/src/Provider/endpoint.ts
+++ www/front_src/src/Provider/endpoint.ts
@@ -3,6 +3,8 @@ const translationEndpoint = `${legacyEndpoint}?object=centreon_i18n&action=trans
 const baseEndpoint = './api/beta';
 const userEndpoint = `${baseEndpoint}/configuration/users/current/parameters`;
 const parametersEndpoint = `${baseEndpoint}/administration/parameters`;
-const aclEndpoint = `${baseEndpoint}/users/acl/actions`;
+// const aclEndpoint = `${baseEndpoint}/users/acl/actions`;
+
+const aclEndpoint = 'http://localhost:6000/mock/acl';
 
 export { parametersEndpoint, translationEndpoint, aclEndpoint, userEndpoint };
