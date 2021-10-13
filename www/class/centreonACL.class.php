20d7e6c756224b3658cb329c03b2300763f005d7
Adrien Morais <31647811+adr-mo@users.noreply.github.com>
2021-10-13 09:47:20 +0200

    fix(customViews): list ldap groups when concerned by shared views (#10286)
    * fix(customViews): list ldap groups when concerned by shared views
    
    * enhance phpdoc block and typehinting
    
    * kduret feedbacks: return type re-added
    
    * Update www/class/centreonACL.class.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/class/centreonACL.class.php
+++ www/class/centreonACL.class.php
@@ -2449,9 +2449,10 @@ class CentreonACL
      *
      * @access public
      * @param array $options
-     * @return void
+     * @param boolean $localOnly Indicates if only local contactgroups should be searched
+     * @return mixed[]
      */
-    public function getContactGroupAclConf($options = array(), $localOnly = true)
+    public function getContactGroupAclConf(array $options = [], bool $localOnly = true)
     {
         $request = $this->constructRequest($options, true);
 
