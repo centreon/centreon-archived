7623b55763ba6a02424981d29ee1a6ef9f30fe3d
Bruno d'Auria <bdauria@centreon.com>
2021-10-13 09:37:05 +0200

    Change port

--- www/front_src/src/Provider/endpoint.ts
+++ www/front_src/src/Provider/endpoint.ts
@@ -5,6 +5,6 @@ const userEndpoint = `${baseEndpoint}/configuration/users/current/parameters`;
 const parametersEndpoint = `${baseEndpoint}/administration/parameters`;
 // const aclEndpoint = `${baseEndpoint}/users/acl/actions`;
 
-const aclEndpoint = 'http://localhost:6000/mock/acl';
+const aclEndpoint = 'http://localhost:5151/mock/acl';
 
 export { parametersEndpoint, translationEndpoint, aclEndpoint, userEndpoint };
