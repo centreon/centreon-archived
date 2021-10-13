326b5d1904f57a922804d3ae324180e53a129bcd
Bruno d'Auria <bdauria@centreon.com>
2021-10-13 12:53:01 +0200

    Restore API

--- www/front_src/src/Provider/endpoint.ts
+++ www/front_src/src/Provider/endpoint.ts
@@ -3,8 +3,6 @@ const translationEndpoint = `${legacyEndpoint}?object=centreon_i18n&action=trans
 const baseEndpoint = './api/beta';
 const userEndpoint = `${baseEndpoint}/configuration/users/current/parameters`;
 const parametersEndpoint = `${baseEndpoint}/administration/parameters`;
-// const aclEndpoint = `${baseEndpoint}/users/acl/actions`;
-
-const aclEndpoint = 'http://localhost:5151/mock/acl';
+const aclEndpoint = `${baseEndpoint}/users/acl/actions`;
 
 export { parametersEndpoint, translationEndpoint, aclEndpoint, userEndpoint };
