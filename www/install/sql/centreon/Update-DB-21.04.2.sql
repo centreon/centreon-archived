441731356549910dc0b6730f2910fcc7c0ecba62
alaunois <alaunois@centreon.com>
2021-10-15 10:40:06 +0200

    Revert "fix(sql) move sql update to new file for 21.04.7 (#10226)"
    This reverts commit bcb381e9e60768077f88195b4bee1f93c7fa757b.

--- www/install/sql/centreon/Update-DB-21.04.2.sql
+++ www/install/sql/centreon/Update-DB-21.04.2.sql
@@ -1,2 +1,9 @@
 -- Delete obsolete topologies
 DELETE FROM `topology` WHERE `topology_page` IN (6090901, 6090902);
+
+-- Add TLS hostname in config brocker input/outputs IPV4
+INSERT INTO `cb_field` (`cb_field_id`, `fieldname`, `displayname`, `description`, `fieldtype`, `external`) VALUES
+(76, 'tls_hostname', 'TLS Host name', 'Expected TLS certificate common name (CN) - leave blank if unsure.', 'text', NULL);
+
+INSERT INTO `cb_type_field_relation` (`cb_type_id`, `cb_field_id`, `is_required`, `order_display`) VALUES
+(3, 76, 0, 5);
