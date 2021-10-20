cef6a7dc981458ce792bd1ccdef6793f689a866e
Laurent Pinsivy <lpinsivy@centreon.com>
2021-10-20 17:13:20 +0200

    fix(openid): fix error after Centreon update

--- www/class/centreonAuth.SSO.class.php
+++ www/class/centreonAuth.SSO.class.php
@@ -100,6 +100,11 @@ class CentreonAuthSSO extends CentreonAuth
             $this->source = "OpenId";
 
             # Get configured values
+            if (!empty($this->ssoOptions['openid_connect_client_basic_auth'])) {
+                $clientBasicAuth = $this->ssoOptions['openid_connect_client_basic_auth'];
+            } else {
+                $clientBasicAuth = false;
+            }
             $clientBasicAuth = $this->ssoOptions['openid_connect_client_basic_auth'];
             $clientId = $this->ssoOptions['openid_connect_client_id'];
             $clientSecret = $this->ssoOptions['openid_connect_client_secret'];
