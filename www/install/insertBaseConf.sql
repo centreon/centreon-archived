1f966b0e7d4316f537cb16ecfe0579a74e7f1179
Kevin Duret <kduret@centreon.com>
2021-10-25 09:13:45 +0200

    fix fresh install version

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.0-beta.2');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.0-rc.1');
 
 --
 -- Contenu de la table `contact`
