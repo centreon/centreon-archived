52b6eddb7b989f04f498b1bf76ed70941888c9d2
tuntoja <58987095+tuntoja@users.noreply.github.com>
2021-11-05 11:10:25 +0100

    chore(install): update version 21.10.1 (#10391)

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.0');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.1');
 
 --
 -- Contenu de la table `contact`
