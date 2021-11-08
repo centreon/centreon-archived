34e72d372d8774c1fab4ac87b16afbee04bbcd2c
Laurent Calvet <lcalvet@centreon.com>
2021-11-08 13:42:38 +0100

    fix(depedencies) Fix PHP warnings on dependencies (#10364)

--- www/include/configuration/configObject/hostgroup_dependency/hostGroupDependency.php
+++ www/include/configuration/configObject/hostgroup_dependency/hostGroupDependency.php
@@ -38,6 +38,11 @@ if (!isset($centreon)) {
     exit();
 }
 
+const ADD_DEPENDENCY = 'a';
+const WATCH_DEPENDENCY = 'w';
+const MODIFY_DEPENDENCY = 'c';
+const DUPLICATE_DEPENDENCY = 'm';
+const DELETE_DEPENDENCY = 'd';
 /*
  * Path to the configuration dir
  */
@@ -49,7 +54,7 @@ $path = "./include/configuration/configObject/hostgroup_dependency/";
 require_once $path . "DB-Func.php";
 require_once "./include/common/common-Func.php";
 
-$dep_id = filter_var(
+$depId = filter_var(
     $_GET['dep_id'] ?? $_POST['dep_id'] ?? null,
     FILTER_VALIDATE_INT
 );
@@ -74,12 +79,12 @@ $hgs = $acl->getHostGroupAclConf(null, 'broker');
 $hgstring = CentreonUtils::toStringWithQuotes($hgs);
 
 switch ($o) {
-    case "a": # Add a Dependency
-    case "w": # Watch a Dependency
-    case "c": # Modify a Dependency
+    case ADD_DEPENDENCY:
+    case WATCH_DEPENDENCY:
+    case MODIFY_DEPENDENCY:
         require_once($path . "formHostGroupDependency.php");
         break;
-    case "m": # Duplicate n Dependencies
+    case DUPLICATE_DEPENDENCY:
         purgeOutdatedCSRFTokens();
         if (isCSRFTokenValid()) {
             purgeCSRFToken();
@@ -92,7 +97,7 @@ switch ($o) {
         }
         require_once($path . "listHostGroupDependency.php");
         break;
-    case "d": # Delete n Dependency
+    case DELETE_DEPENDENCY:
         purgeOutdatedCSRFTokens();
         if (isCSRFTokenValid()) {
             purgeCSRFToken();
