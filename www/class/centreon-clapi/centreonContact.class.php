40e21e8866a034b3629832a76b93c4e8ddcd6333
AssanDiomande <adiomande-ext@centreon.com>
2021-11-10 16:47:56 +0100

    fixed Jenkis errors

--- www/class/centreon-clapi/centreonContact.class.php
+++ www/class/centreon-clapi/centreonContact.class.php
@@ -405,9 +405,8 @@ class CentreonContact extends CentreonObject
                             'reach_api',
                             'reach_api_rt',
                             'default_page',
-                            'ar_id',
                             'show_deprecated_pages',
-                            'enable_one_click_export',
+                            'enable_one_click_export'
                         ]
                     )
                 ) {
