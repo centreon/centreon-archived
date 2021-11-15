49c7e98735c368806cffdead2432905184423a3e
Kevin Duret <kduret@centreon.com>
2021-11-15 13:39:39 +0100

    fix build

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,8 @@ module.exports = merge(baseConfig, extractCssConfig, {
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
       filename: path.resolve(`${__dirname}`, 'www', 'index.html'),
+      // the default function (md4) is not supported by OpenSSL by default starting in Node 17
+      hashFunction: 'xxhash64',
       template: './www/front_src/public/index.html',
     }),
     new HtmlWebpackHarddiskPlugin(),
