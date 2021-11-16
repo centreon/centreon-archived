f0be915e9ce926df8da384e155f330346db6efbf
Jérémy Jaouen <jjaouen@centreon.com>
2021-11-16 10:51:47 +0100

    move create table script into 22.04 file

--- www/install/php/Update-22.04.0-beta.1.php
+++ www/install/php/Update-22.04.0-beta.1.php
@@ -18,3 +18,40 @@
  * For more information : contact@centreon.com
  *
  */
+
+include_once __DIR__ . "/../../class/centreonLog.class.php";
+$centreonLog = new CentreonLog();
+
+$versionOfTheUpgrade = 'UPGRADE - 22.04.0-beta.1: ';
+
+$pearDB = new CentreonDB();
+
+try {
+    $pearDB->beginTransaction();
+    $errorMessage = "Unable to check if table 'password_security_policy' exists";
+    $dbResult = $pearDB->query("SHOW TABLES LIKE 'password_security_policy'");
+    if ($dbResult->fetch()) {
+        $errorMessage = "Unable to create table 'password_security_policy'";
+        $pearDB->query(
+            "CREATE TABLE `password_security_policy` (
+            `password_length` int(11) UNSIGNED NOT NULL DEFAULT 12,
+            `uppercase_characters` enum('0', '1') NOT NULL DEFAULT '1',
+            `lowercase_characters` enum('0', '1') NOT NULL DEFAULT '1',
+            `integer_characters` enum('0', '1') NOT NULL DEFAULT '1',
+            `special_characters` enum('0', '1') NOT NULL DEFAULT '1',
+            `attempts` int(11) UNSIGNED NOT NULL DEFAULT 5,
+            `blocking_duration` int(11) UNSIGNED NOT NULL DEFAULT 900,
+            `password_expiration` int(11) UNSIGNED NOT NULL DEFAULT 7776000,
+            `delay_before_new_password` int(11) UNSIGNED NOT NULL DEFAULT 3600)"
+        );
+
+        $errorMessage = "Unable to create insert default configuration in 'password_security_policy'";
+        $pearDB->query("INSERT INTO `password_security_policy`
+        (`password_length`, `uppercase_characters`, `lowercase_characters`, `integer_characters`,
+        `special_characters`, `attempts`, `blocking_duration`, `password_expiration`, `delay_before_new_password`)
+        VALUES (12, '1', '1', '1', '1', 5, 900, 7776000, 3600)");
+    }
+    $pearDB->commit();
+} catch (Exception $e) {
+    $pearDB->rollback();
+}
