2d53a9fac9290da926590bef41496f8e2b273e3a
Bruno d'Auria <bdauria@centreon.com>
2021-11-18 10:06:23 +0100

    Fix Downtime time parsing

--- www/front_src/src/Resources/Actions/Resource/Downtime/Dialog.tsx
+++ www/front_src/src/Resources/Actions/Resource/Downtime/Dialog.tsx
@@ -12,14 +12,23 @@ import dayjs from 'dayjs';
 //   TimePickerProps,
 // } from '@material-ui/pickers';
 
-import { DatePicker, LocalizationProvider, MuiPickersAdapter } from '@mui/lab';
+import {
+  DatePicker,
+  LocalizationProvider,
+  MuiPickersAdapter,
+  TimePicker,
+} from '@mui/lab';
 import {
   Checkbox,
   FormControlLabel,
   FormHelperText,
   Grid,
   Alert,
+  StandardTextFieldProps,
+  TextFieldProps,
+  useTheme,
 } from '@mui/material';
+import { Box } from '@mui/system';
 
 import { Dialog, TextField, SelectField } from '@centreon/ui';
 import { useUserContext } from '@centreon/ui-context';
@@ -101,8 +110,7 @@ const DialogDowntime = ({
   const { getDowntimeDeniedTypeAlert, canDowntimeServices } = useAclQuery();
   const [isPickerOpened, setIsPickerOpened] = React.useState(false);
   const { locale, timezone } = useUserContext();
-
-  // const { Adapter } = useDateTimePickerAdapter();
+  const theme = useTheme();
 
   const { Adapter } = useDateTimePickerAdapter({ locale, tz: timezone });
 
@@ -118,14 +126,29 @@ const DialogDowntime = ({
 
   const deniedTypeAlert = getDowntimeDeniedTypeAlert(resources);
 
-  const renderInput = (props): JSX.Element => <TextField {...props} />;
+  const renderInput =
+    (field: string) =>
+    ({ inputRef, inputProps, InputProps }: TextFieldProps): JSX.Element =>
+      (
+        <TextField
+          EndAdornment={(): JSX.Element => <>{InputProps?.endAdornment}</>}
+          error={errors[field]}
+          inputProps={{
+            ...inputProps,
+            ref: inputRef,
+            style: { padding: theme.spacing(1) },
+          }}
+        />
+      );
 
   const handleInputChange =
-    (field: string) =>
+    ({ field, format = undefined }: { field: string; format?: string }) =>
     (newValue: dayjs.Dayjs | null, keyBoardValue: string | undefined): void => {
-      const value = dayjs(isPickerOpened ? newValue : keyBoardValue).locale(
-        locale,
-      );
+      console.log(isPickerOpened, dayjs(keyBoardValue, 'H:mm'));
+      const value = dayjs(
+        isPickerOpened ? newValue : keyBoardValue,
+        format,
+      ).locale(locale);
 
       changeDate(field)(value);
     };
@@ -147,186 +170,127 @@ const DialogDowntime = ({
         dateAdapter={Adapter}
         locale={locale.substring(0, 2)}
       >
-        <Grid container direction="column" spacing={1}>
-          <Grid item>
-            <FormHelperText>{t(labelFrom)}</FormHelperText>
-            <Grid container direction="row" spacing={1}>
-              <Grid item style={{ width: 240 }}>
-                <DatePicker<dayjs.Dayjs>
-                  label={t(labelStartDate)}
-                  renderInput={renderInput}
-                  value={values.dateStart}
-                  onChange={handleInputChange('dateStart')}
-                  onClose={(): void => setIsPickerOpened(false)}
-                  onOpen={(): void => setIsPickerOpened(true)}
-                />
-                {/* <KeyboardDatePicker
-                  KeyboardButtonProps={{
-                    'aria-label': t(labelChangeStartDate),
-                  }}
-                  aria-label={t(labelStartDate)}
-                  error={errors?.dateStart !== undefined}
-                  helperText={errors?.dateStart}
-                  inputMode="text"
-                  maxDate={maxEndDate}
-                  value={values.dateStart}
-                  onChange={changeDate('dateStart')}
-                  {...datePickerProps}
-                /> */}
-              </Grid>
-              <Grid item style={{ width: 200 }}>
-                <DatePicker<dayjs.Dayjs>
-                  label="Date desktop"
-                  renderInput={renderInput}
-                  value={values.dateStart}
-                  onChange={handleInputChange('dateStart')}
-                  onClose={(): void => setIsPickerOpened(false)}
-                  onOpen={(): void => setIsPickerOpened(true)}
-                />
-                {/* <KeyboardTimePicker
-                  KeyboardButtonProps={{
-                    'aria-label': t(labelChangeStartTime),
-                  }}
-                  ampm={isMeridianFormat(values.timeStart)}
-                  aria-label={t(labelStartTime)}
-                  error={errors?.timeStart !== undefined}
-                  helperText={errors?.timeStart}
-                  value={values.timeStart}
-                  onChange={changeDate('timeStart')}
-                  {...timePickerProps}
-                /> */}
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <FormHelperText>{t(labelTo)}</FormHelperText>
-            <Grid container direction="row" spacing={1}>
-              <Grid item style={{ width: 240 }}>
-                <DatePicker<dayjs.Dayjs>
-                  label="Date desktop"
-                  renderInput={renderInput}
-                  value={values.dateStart}
-                  onChange={handleInputChange('dateStart')}
-                  onClose={(): void => setIsPickerOpened(false)}
-                  onOpen={(): void => setIsPickerOpened(true)}
-                />
-                {/* <KeyboardDatePicker
-                  KeyboardButtonProps={{
-                    'aria-label': t(labelChangeEndDate),
-                  }}
-                  aria-label={t(labelEndDate)}
-                  error={errors?.dateEnd !== undefined}
-                  helperText={errors?.dateEnd}
-                  value={values.dateEnd}
-                  onChange={changeDate('dateEnd')}
-                  {...datePickerProps}
-                /> */}
-              </Grid>
-              <Grid item style={{ width: 200 }}>
-                <DatePicker<dayjs.Dayjs>
-                  label="Date desktop"
-                  renderInput={renderInput}
-                  value={values.dateStart}
-                  onChange={handleInputChange('dateStart')}
-                  onClose={(): void => setIsPickerOpened(false)}
-                  onOpen={(): void => setIsPickerOpened(true)}
-                />
-                {/* <KeyboardTimePicker
-                  KeyboardButtonProps={{
-                    'aria-label': t(labelChangeEndTime),
-                  }}
-                  ampm={isMeridianFormat(values.timeEnd)}
-                  aria-label={t(labelEndTime)}
-                  disableToolbar={not(isMeridianFormat(values.timeEnd))}
-                  error={errors?.timeEnd !== undefined}
-                  helperText={errors?.timeEnd}
-                  value={values.timeEnd}
-                  onChange={changeDate('timeEnd')}
-                  {...timePickerProps}
-                /> */}
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
+        <Box
+          sx={{
+            display: 'grid',
+            gap: 1,
+
+            gridAutoColumns: 'auto',
+            gridAutoRows: 'auto',
+            gridTemplateColumns: '1.25fr 1fr',
+          }}
+        >
+          <FormHelperText sx={{ gridColumn: 'span 2' }}>
+            {t(labelFrom)}
+          </FormHelperText>
+          <DatePicker<dayjs.Dayjs>
+            aria-label={t(labelStartDate)}
+            maxDate={dayjs(maxEndDate)}
+            renderInput={renderInput('dateStart')}
+            value={values.dateStart}
+            onChange={handleInputChange({ field: 'dateStart' })}
+            onClose={(): void => setIsPickerOpened(false)}
+            onOpen={(): void => setIsPickerOpened(true)}
+          />
+          <TimePicker<dayjs.Dayjs>
+            aria-label={t(labelStartTime)}
+            renderInput={renderInput('timeStart')}
+            value={values.timeStart}
+            onChange={handleInputChange({
+              field: 'timeStart',
+              format: 'HH:mm',
+            })}
+            onClose={(): void => setIsPickerOpened(false)}
+            onOpen={(): void => setIsPickerOpened(true)}
+          />
+          <FormHelperText sx={{ gridColumn: 'span 2' }}>
+            {t(labelTo)}
+          </FormHelperText>
+          <DatePicker<dayjs.Dayjs>
+            aria-label={t(labelEndDate)}
+            renderInput={renderInput('dateEnd')}
+            value={values.endDate}
+            onChange={handleInputChange({ field: 'dateEnd' })}
+            onClose={(): void => setIsPickerOpened(false)}
+            onOpen={(): void => setIsPickerOpened(true)}
+          />
+          <TimePicker<dayjs.Dayjs>
+            aria-label={t(labelEndTime)}
+            renderInput={renderInput('timeEnd')}
+            value={values.endTime}
+            onChange={handleInputChange({ field: 'timeEnd', format: 'HH:mm' })}
+            onClose={(): void => setIsPickerOpened(false)}
+            onOpen={(): void => setIsPickerOpened(true)}
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={values.fixed}
+                color="primary"
+                inputProps={{ 'aria-label': t(labelFixed) }}
+                size="small"
+                onChange={handleChange('fixed')}
+              />
+            }
+            label={t(labelFixed) as string}
+          />
+          <FormHelperText sx={{ gridColumn: 'span 2' }}>
+            {t(labelDuration)}
+          </FormHelperText>
+          <TextField
+            disabled={values.fixed}
+            error={errors?.duration?.value}
+            type="number"
+            value={values.duration.value}
+            onChange={handleChange('duration.value')}
+          />
+          <SelectField
+            disabled={values.fixed}
+            options={[
+              {
+                id: 'seconds',
+                name: t(labelSeconds),
+              },
+              {
+                id: 'minutes',
+                name: t(labelMinutes),
+              },
+              {
+                id: 'hours',
+                name: t(labelHours),
+              },
+            ]}
+            selectedOptionId={values.duration.unit}
+            onChange={handleChange('duration.unit')}
+          />
+          <TextField
+            fullWidth
+            multiline
+            error={errors?.comment}
+            label={t(labelComment)}
+            rows={3}
+            sx={{ gridColumn: 'span 2' }}
+            value={values.comment}
+            onChange={handleChange('comment')}
+          />
+          {hasHosts && (
             <FormControlLabel
               control={
                 <Checkbox
-                  checked={values.fixed}
+                  checked={
+                    canDowntimeServices() && values.downtimeAttachedResources
+                  }
                   color="primary"
-                  inputProps={{ 'aria-label': t(labelFixed) }}
+                  disabled={!canDowntimeServices()}
+                  inputProps={{ 'aria-label': labelSetDowntimeOnServices }}
                   size="small"
-                  onChange={handleChange('fixed')}
+                  onChange={handleChange('downtimeAttachedResources')}
                 />
               }
-              label={t(labelFixed) as string}
+              label={t(labelSetDowntimeOnServices) as string}
             />
-          </Grid>
-          <Grid item>
-            <FormHelperText>{t(labelDuration)}</FormHelperText>
-            <Grid container direction="row" spacing={1}>
-              <Grid item style={{ width: 150 }}>
-                <TextField
-                  disabled={values.fixed}
-                  error={errors?.duration?.value}
-                  type="number"
-                  value={values.duration.value}
-                  onChange={handleChange('duration.value')}
-                />
-              </Grid>
-              <Grid item style={{ width: 150 }}>
-                <SelectField
-                  disabled={values.fixed}
-                  options={[
-                    {
-                      id: 'seconds',
-                      name: t(labelSeconds),
-                    },
-                    {
-                      id: 'minutes',
-                      name: t(labelMinutes),
-                    },
-                    {
-                      id: 'hours',
-                      name: t(labelHours),
-                    },
-                  ]}
-                  selectedOptionId={values.duration.unit}
-                  onChange={handleChange('duration.unit')}
-                />
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item>
-            <TextField
-              fullWidth
-              multiline
-              error={errors?.comment}
-              label={t(labelComment)}
-              rows={3}
-              value={values.comment}
-              onChange={handleChange('comment')}
-            />
-          </Grid>
-          {hasHosts && (
-            <Grid item>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={
-                      canDowntimeServices() && values.downtimeAttachedResources
-                    }
-                    color="primary"
-                    disabled={!canDowntimeServices()}
-                    inputProps={{ 'aria-label': labelSetDowntimeOnServices }}
-                    size="small"
-                    onChange={handleChange('downtimeAttachedResources')}
-                  />
-                }
-                label={t(labelSetDowntimeOnServices) as string}
-              />
-            </Grid>
           )}
-        </Grid>
+        </Box>
       </LocalizationProvider>
     </Dialog>
   );
