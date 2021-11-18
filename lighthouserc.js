de743d4682ae93c6777e7248a4072b5d05438157
Tom Darneix <tomdar87@outlook.com>
2021-11-18 12:29:02 +0100

    Improve Lighthouse CI configuration

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,31 +1,26 @@
+const baseUrl = 'http://localhost:4000/';
+
 module.exports = {
+  baseUrl,
   ci: {
     assert: {
-      assertions: {
-        // Disable PWA metrics that might fail
-        'apple-touch-icon': ['off'],
-        'installable-manifest': ['off'],
-        'maskable-icon': ['off'],
-        'service-worker': ['off'],
-        'splash-screen': ['off'],
-        'themed-omnibox': ['off'],
-      },
-      preset: 'lighthouse:recommended',
+      preset: 'lighthouse:no-pwa',
     },
     collect: {
       isSinglePageApplication: true,
       numberOfRuns: 1,
       puppeteerScript: './puppeteer-script.js',
       settings: {
+        configPath:
+          './node_modules/lighthouse/lighthouse-core/config/lr-desktop-config.js',
         onlyCategories: [
           'performance',
           'accessibility',
           'best-practices',
           'seo',
         ],
-        preset: 'desktop',
       },
-      url: 'http://localhost:4000/centreon/monitoring/resources',
+      url: `${baseUrl}centreon/monitoring/resources`,
     },
     upload: {
       reportFilenamePattern: '.lighthouseci/lighthouseci-index.%%EXTENSION%%',
--- puppeteer-script.js
+++ puppeteer-script.js
@@ -1,7 +1,9 @@
+const { baseUrl } = require('./lighthouserc');
+
 module.exports = async (browser) => {
   // launch browser for LHCI
   const page = await browser.newPage();
-  await page.goto('http://localhost:4000/');
+  await page.goto(baseUrl);
   await page.type('[name="useralias"]', 'admin');
   await page.type('[name="password"]', 'centreon');
   await page.click('[type="submit"]');
