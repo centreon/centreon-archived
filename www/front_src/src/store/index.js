a9540134f02aa8578cdabd29103e7eb498a16fac
Tom Darneix <tomdar87@outlook.com>
2021-12-07 12:10:54 +0100

    Remove connected router from redux

--- www/front_src/src/store/index.js
+++ www/front_src/src/store/index.js
@@ -1,23 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import { routerMiddleware } from 'connected-react-router';
 import { batchDispatchMiddleware } from 'redux-batched-actions';
 import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
 
 import createRootReducer from '../redux/reducers';
 
-export const history = createBrowserHistory({
-  basename: document.baseURI.replace(window.location.origin, ''),
-});
-
 const createAppStore = (initialState = {}) => {
-  const middlewares = [
-    routerMiddleware(history),
-    thunk,
-    batchDispatchMiddleware,
-  ];
+  const middlewares = [thunk, batchDispatchMiddleware];
 
   const composeEnhancers =
     typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -26,7 +16,7 @@ const createAppStore = (initialState = {}) => {
 
   const enhancer = composeEnhancers(applyMiddleware(...middlewares));
 
-  const store = createStore(createRootReducer(history), initialState, enhancer);
+  const store = createStore(createRootReducer(), initialState, enhancer);
 
   return store;
 };
