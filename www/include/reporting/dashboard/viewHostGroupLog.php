1e397e724b9dad3f700321db4e008cb6b27a7742
AssanDiomande <adiomande-ext@centreon.com>
2021-12-09 14:58:30 +0100

    kept timeperiod after refresh

--- www/include/reporting/dashboard/viewHostGroupLog.php
+++ www/include/reporting/dashboard/viewHostGroupLog.php
@@ -68,14 +68,14 @@ $hostsGroupRoute = array(
     'multiple' => false,
     'linkedObject' => 'centreonHostgroups',
     'availableDatasetRoute' =>
-        './include/common/webServices/rest/internal.php?object=centreon_configuration_hostgroup&action=list',
+    './api/internal.php?object=centreon_configuration_hostgroup&action=list',
     'defaultDatasetRoute' =>
-        './api/internal.php?object=centreon_configuration_hostgroup'
-        . '&action=defaultValues&target=service&field=service_hgPars&id=' . $id,
+    './api/internal.php?object=centreon_configuration_hostgroup'
+    . '&action=defaultValues&target=service&field=service_hgPars&id=' . $id,
 );
-$hostGroupSelectBox = $formHostGroup->addElement(
+$hostGroupSelectBox = $formPeriod->addElement(
     'select2',
-    'item',
+    'itemElement',
     _("Host Group"),
     [],
     $hostsGroupRoute
