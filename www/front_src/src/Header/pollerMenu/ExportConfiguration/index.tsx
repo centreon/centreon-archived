0467bda3d863436e0005bca4aff306d643b7a92e
Tom Darneix <tomdar87@outlook.com>
2021-12-13 20:25:54 +0100

    chore(core): Fix centreon-frontend breaking changes (#10488)
    * Update centreon-frontend
    
    * Fix breaking changes

--- www/front_src/src/Header/pollerMenu/ExportConfiguration/index.tsx
+++ www/front_src/src/Header/pollerMenu/ExportConfiguration/index.tsx
@@ -66,7 +66,9 @@ const ExportConfiguration = ({
 
   const confirmExportAndReload = (): void => {
     showInfoMessage(t(labelExportingAndReloadingTheConfiguration));
-    sendRequest(exportAndReloadConfigurationEndpoint).then(() => {
+    sendRequest({
+      endpoint: exportAndReloadConfigurationEndpoint,
+    }).then(() => {
       showSuccessMessage(t(labelConfigurationExportedAndReloaded));
     });
     closeConfirmDialog();
