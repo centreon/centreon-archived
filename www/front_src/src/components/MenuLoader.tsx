217cb57e5b8e10658db9965dcb4ba47a26fd00da
Tom Darneix <tomdar87@outlook.com>
2021-12-14 10:34:58 +0100

    Improve skeleton on top counter

--- www/front_src/src/components/MenuLoader.tsx
+++ www/front_src/src/components/MenuLoader.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import { useTheme, makeStyles, alpha } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 
+import { LoadingSkeleton } from '@centreon/centreon-frontend/packages/centreon-ui/src';
+
 const useStyles = makeStyles((theme) => ({
   skeleton: {
     backgroundColor: alpha(theme.palette.grey[50], 0.4),
@@ -19,10 +21,10 @@ const MenuLoader = ({ width = 15 }: Props): JSX.Element => {
   const classes = useStyles();
 
   return (
-    <Skeleton
-      animation="wave"
+    <LoadingSkeleton
       className={classes.skeleton}
-      height={theme.spacing(5)}
+      height={theme.spacing(5.5)}
+      variant="text"
       width={theme.spacing(width)}
     />
   );
