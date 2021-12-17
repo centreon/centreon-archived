3243bca61d3363321d51599e3ea58b978c99bfcc
Tom Darneix <tomdar87@outlook.com>
2021-12-17 22:35:05 +0100

    Adapt tests

--- www/front_src/src/Main/index.test.tsx
+++ www/front_src/src/Main/index.test.tsx
@@ -92,12 +92,19 @@ describe('Main', () => {
 
   it('displays the login page when the path is "/login"', async () => {
     window.history.pushState({}, '', '/login');
-    mockedAxios.get.mockResolvedValueOnce({
-      data: {
-        available_version: null,
-        installed_version: '21.10.1',
-      },
-    });
+    mockedAxios.get
+      .mockResolvedValueOnce({
+        data: {
+          available_version: null,
+          installed_version: '21.10.1',
+        },
+      })
+      .mockResolvedValueOnce({
+        data: retrievedTranslations,
+      })
+      .mockRejectedValueOnce({
+        response: { status: 403 },
+      });
 
     renderMain();
 
@@ -110,11 +117,25 @@ describe('Main', () => {
       );
     });
 
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        translationEndpoint,
+        cancelTokenRequestParam,
+      );
+    });
+
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        userEndpoint,
+        cancelTokenRequestParam,
+      );
+    });
+
     expect(screen.getByLabelText(labelCentreonLogo)).toBeInTheDocument();
     expect(screen.getByLabelText(labelAlias)).toBeInTheDocument();
     expect(screen.getByLabelText(labelPassword)).toBeInTheDocument();
     expect(screen.getByLabelText(labelLogin)).toBeInTheDocument();
-    expect(screen.getByText('v21.10.1')).toBeInTheDocument();
+    expect(screen.getByText('v. 21.10.1')).toBeInTheDocument();
     expect(screen.getByText('Copyright © 2005 - 2020')).toBeInTheDocument();
   });
 
@@ -122,13 +143,16 @@ describe('Main', () => {
     window.history.pushState({}, '', '/');
     mockedAxios.get
       .mockResolvedValueOnce({
-        data: retrievedUser,
-      })
-      .mockResolvedValueOnce({
         data: {
           available_version: '21.10.1',
           installed_version: null,
         },
+      })
+      .mockResolvedValueOnce({
+        data: retrievedTranslations,
+      })
+      .mockResolvedValueOnce({
+        data: retrievedUser,
       });
 
     renderMain();
@@ -158,13 +182,16 @@ describe('Main', () => {
     window.history.pushState({}, '', '/');
     mockedAxios.get
       .mockResolvedValueOnce({
-        data: retrievedUser,
-      })
-      .mockResolvedValueOnce({
         data: {
           available_version: '21.10.1',
           installed_version: '21.10.0',
         },
+      })
+      .mockResolvedValueOnce({
+        data: retrievedTranslations,
+      })
+      .mockResolvedValueOnce({
+        data: retrievedUser,
       });
 
     renderMain();
@@ -194,13 +221,16 @@ describe('Main', () => {
     window.history.pushState({}, '', '/');
     mockedAxios.get
       .mockResolvedValueOnce({
-        data: retrievedUser,
-      })
-      .mockResolvedValueOnce({
         data: {
           available_version: null,
           installed_version: '21.10.0',
         },
+      })
+      .mockResolvedValueOnce({
+        data: retrievedTranslations,
+      })
+      .mockResolvedValueOnce({
+        data: retrievedUser,
       })
       .mockResolvedValueOnce({
         data: retrievedNavigation,
@@ -213,9 +243,6 @@ describe('Main', () => {
       })
       .mockResolvedValueOnce({
         data: retrievedActionsAcl,
-      })
-      .mockResolvedValueOnce({
-        data: retrievedTranslations,
       });
 
     renderMain();
