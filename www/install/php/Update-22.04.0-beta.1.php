da907319963ff8b5af7da42a0daeccbadc3747c0
AssanDiomande <adiomande-ext@centreon.com>
2021-12-20 17:39:48 +0100

    fix(broker) : update broker retry interval default value and description (22.04) (#10478)
    * change retry_interval to 15
    
    * try to set retry interval to 15 sec
    
    * restored clapi config and help files
    
    * set retry interval default value to 30 sec by using the form
    
    * updated help box for broker
    
    * add text for retry_interval default value
    
    * update install script
    
    * update php script
    
    * Delete Update-21.10.2.php
    
    * changed files
    
    * updated 22.04 script
    
    * updated text
    
    * Update Update-22.04.0-beta.1.php
    
    * Update lang/fr_FR.UTF-8/LC_MESSAGES/help.po
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * updated files
    
    * add blank line
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/install/php/Update-22.04.0-beta.1.php
+++ www/install/php/Update-22.04.0-beta.1.php
@@ -19,18 +19,18 @@
  *
  */
 
+
 include_once __DIR__ . "/../../class/centreonLog.class.php";
 $centreonLog = new CentreonLog();
 
 //error specific content
-$versionOfTheUpgrade = 'UPGRADE - 21.10.2: ';
+$versionOfTheUpgrade = 'UPGRADE - 22.04.0-beta-1: ';
 
 /**
  * Query with transaction
  */
 try {
     $errorMessage = 'Impossible to add "contact_js_effects" column to "contact" table';
-
     if (!$pearDB->isColumnExist('contact', 'contact_js_effects')) {
         $pearDB->query(
             "ALTER TABLE `contact`
@@ -38,6 +38,16 @@ try {
             AFTER `contact_comment`"
         );
     }
+
+    $errorMessage = 'Unable to delete logger entry in cb_tag';
+    $statement = $pearDB->query("DELETE FROM cb_tag WHERE tagname = 'logger'");
+
+    $errorMessage = 'Unable to update the description in cb_field';
+    $statement = $pearDB->query("
+        UPDATE cb_field
+        SET `description` = 'Time in seconds to wait between each connection attempt (Default value: 30s).'
+        WHERE `cb_field_id` = 31
+    ");
 } catch (\Exception $e) {
     $centreonLog->insertLog(
         4,
