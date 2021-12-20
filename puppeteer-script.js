b5e73f7a8c4a9ea407554bb4c85e041523cc51b7
Tom Darneix <tomdar87@outlook.com>
2021-12-20 09:26:05 +0100

    Update puppeteer script

--- puppeteer-script.js
+++ puppeteer-script.js
@@ -4,8 +4,9 @@ module.exports = async (browser) => {
   // launch browser for LHCI
   const page = await browser.newPage();
   await page.goto(baseUrl);
-  await page.type('[name="useralias"]', 'admin');
-  await page.type('[name="password"]', 'centreon');
+  await page.waitForSelector('input[aria-label="Alias"]');
+  await page.type('input[aria-label="Alias"]', 'admin');
+  await page.type('input[aria-label="Password"]', 'centreon');
   await page.click('[type="submit"]');
   await page.waitForNavigation();
   // close session for next run
