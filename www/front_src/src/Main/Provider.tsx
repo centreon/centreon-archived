441080c9097107bb25f7a8500680f5250fd7dc9f
Tom Darneix <tomdar87@outlook.com>
2021-12-21 17:31:45 +0100

    Fix redirection on 404

--- www/front_src/src/Main/Provider.tsx
+++ www/front_src/src/Main/Provider.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider as JotaiProvider } from 'jotai';
+import { startsWith, tail } from 'ramda';
 
 import { ThemeProvider } from '@centreon/ui';
 
@@ -14,6 +15,20 @@ const Provider = ({ children }: Props): JSX.Element => {
     (document
       .getElementsByTagName('base')[0]
       ?.getAttribute('href') as string) || '';
+
+  React.useEffect(() => {
+    const pathStartWithBasename = startsWith(
+      basename,
+      window.location.pathname,
+    );
+
+    if (pathStartWithBasename) {
+      return;
+    }
+
+    const path = tail(window.location.pathname);
+    window.location.href = `${basename}${path}`;
+  }, []);
 
   return (
     <Router basename={basename}>
