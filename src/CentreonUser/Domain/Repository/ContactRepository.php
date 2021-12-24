bd4babd2b6909de745fb2fa42e2ef06b892f2bd9
AssanDiomande <adiomande-ext@centreon.com>
2021-12-24 14:18:39 +0100

    add int[] to ContactRepository.php

--- src/CentreonUser/Domain/Repository/ContactRepository.php
+++ src/CentreonUser/Domain/Repository/ContactRepository.php
@@ -32,7 +32,7 @@ class ContactRepository extends ServiceEntityRepository
     /**
      * Check list of IDs
      *
-     * @param array<int,int> $ids
+     * @param int[] $ids
      * @return bool
      */
     public function checkListOfIds(array $ids): bool
