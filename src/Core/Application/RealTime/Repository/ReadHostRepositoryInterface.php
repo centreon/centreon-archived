63fda0272cefd2a5e262d998c1129caf5636c64a
Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
2021-12-30 21:17:50 +0100

    enh(resource): apply new architecture to host detail (#10496)
    * refacto(host): apply new architecture
    
    * code-review: first iteration of feedbacks
    
    * Update config/packages/Centreon.yaml
    
    * code-review: iteration 2
    
    * code-review: iteration 3
    
    * feedbacks: new iteration
    
    * code-review: last review done
    
    * code-review: JJA feedback
    
    * update phpDoc
    
    * add phpdoc for constructor
    
    * add phpDoc
    
    * review code + phpstan errors + phpcs errors
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

---
+++ src/Core/Application/RealTime/Repository/ReadHostRepositoryInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Application\RealTime\Repository;
+
+use Core\Domain\RealTime\Model\Host;
+
+interface ReadHostRepositoryInterface
+{
+    /**
+     * Find Host without ACL
+     *
+     * @param int $hostId
+     * @return Host|null
+     */
+    public function findHostById(int $hostId): ?Host;
+
+    /**
+     * Find Host regarding user ACL
+     *
+     * @param int $hostId
+     * @param int[] $accessGroupIds
+     * @return Host|null
+     */
+    public function findHostByIdAndAccessGroupIds(int $hostId, array $accessGroupIds): ?Host;
+}
---
+++ src/Core/Infrastructure/RealTime/Api/Hypermedia/HypermediaProviderInterface.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+declare(strict_types=1);
+
+namespace Core\Infrastructure\RealTime\Api\Hypermedia;
+
+interface HypermediaProviderInterface
+{
+    /**
+     * @var mixed $data
+     * @return bool
+     */
+    public function isValidFor(mixed $data): bool;
+
+    /**
+     * @var mixed $data
+     * @return string|null
+     */
+    public function createForConfiguration(mixed $data): ?string;
+
+    /**
+     * @var mixed $data
+     * @return string|null
+     */
+    public function createForReporting(mixed $data): ?string;
+
+    /**
+     * @var mixed $data
+     * @return string|null
+     */
+    public function createForEventLog(mixed $data): ?string;
+
+    /**
+     * @param mixed $data
+     * @return string
+     */
+    public function createForTimelineEndpoint(mixed $data): string;
+}
