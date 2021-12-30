9b673b28a04e3a258aa4f04f65137de640870e38
Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
2021-12-30 21:17:50 +0100

    enh(resource): apply new architecture to host detail (#10496)
    * refacto(host): apply new architecture
    
    * code-review: first iteration of feedbacks
    
    * Update config/packages/Centreon.yaml
    
    * code-review: iteration 2
    
    * code-review: iteration 3
    
    * feedbacks: new iteration
    
    * code-review: last review done
    
    * code-review: JJA feedback
    
    * update phpDoc
    
    * add phpdoc for constructor
    
    * add phpDoc
    
    * review code + phpstan errors + phpcs errors
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

---
+++ src/Core/Infrastructure/RealTime/Api/Hypermedia/HypermediaProviderTrait.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+declare(strict_types=1);
+
+namespace Core\Infrastructure\RealTime\Api\Hypermedia;
+
+trait HypermediaProviderTrait
+{
+    /**
+     * Get base URI
+     *
+     * @return string
+     */
+    protected function getBaseUri(): string
+    {
+        $baseUri = '';
+        if (
+            isset($_SERVER['REQUEST_URI'])
+            && preg_match(
+                '/^(.+)\/((api|widgets|modules|include)\/|main(\.get)?\.php).+/',
+                $_SERVER['REQUEST_URI'],
+                $matches
+            )
+        ) {
+            $baseUri = $matches[1];
+        }
+        return $baseUri;
+    }
+}
