4dd1c2cc3c548ab222cd3cbb862cdce632a98788
Tom Darneix <tomdar87@outlook.com>
2022-01-04 14:45:49 +0100

    Fix version to 22.04

--- www/front_src/src/Authentication/api/endpoints.ts
+++ www/front_src/src/Authentication/api/endpoints.ts
@@ -1,3 +1,3 @@
-const baseEndpoint = './api/latest';
+const baseEndpoint = './api/v22.04';
 
 export const securityPolicyEndpoint = `${baseEndpoint}/administration/authentication/local/security-policy`;
