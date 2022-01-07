7b35882bdc24f0ecd833b3c07257ffffea8ec639
Kevin Duret <kduret@centreon.com>
2022-01-07 15:06:27 +0100

    fix linting

--- api/index.php
+++ api/index.php
@@ -22,7 +22,7 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
+require dirname(__DIR__) . '/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
