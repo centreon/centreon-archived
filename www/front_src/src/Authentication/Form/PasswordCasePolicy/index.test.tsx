6ee4168cc09e5f8fafd8a1a30e156a53743c97e7
Tom Darneix <tomdar87@outlook.com>
2022-01-11 10:40:41 +0100

    feat(UI): Add password policy form (#10462)
    * doc(api): add openapi documentation for security policy
    
    * Add topology
    
    * Add react route
    
    * Add password case policy
    
    * Refactor button content
    
    * Start to implement TimeInput
    
    * Fix tine input value
    
    * Add can reuse passwords switch
    
    * Add adapters
    
    * Support null value for password expiration
    
    * Rename validation schema file
    
    * Refactor time inputs to one component
    
    * Add time before set new password inputs
    
    * memoize timeInput change function
    
    * Update form container UI
    
    * Add password blocking field
    
    * Remove irrevelant sub-folders
    
    * Implement form sending
    
    * Add some memoization
    
    * Use even more Ramda functions
    
    * Add loading skeleton
    
    * Add password case policy tests
    
    * Add tests
    
    * Add duration plugin to tests setup
    
    * Add translations
    
    * Update dependencies
    
    * Add confirm dialog before reset the form
    
    * Fix base endpoint
    
    * Add more space between elements
    
    * attempt to fix conflict
    
    * Remove minutes and second about time before set new pwd
    
    * Remove second input for blocking time
    
    * Improve translations
    
    * Fix centreon-ui import
    
    * Fix getData signatures
    
    * Update @centreon/centreon-frontend to use a branch
    
    * Use the new LoadingSkeleton component
    
    * Fix eslint errors
    
    * Fix eslint error
    
    * fix package-lock
    
    * Use develop branch
    
    * Remove duplicated translations
    
    * Remove duplication
    
    * Migrate authentication page to new MUI version
    
    * Remove unused import
    
    * Fix skeleton
    
    * Fix skeleton
    
    * Fix version to 22.04
    
    * Fix paper container height
    
    * Use container from Mui
    
    * Do not reset on submit
    
    * Fix label
    
    * Invert strength bar progress for blocking attempt
    
    * Fix tests and bugs
    
    * Add max value on units and rename max and min value properties
    
    * Normalize duration according to a maximum value
    
    * Remove ser files
    
    * Fix eslint errors
    
    * Fix test
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

---
+++ www/front_src/src/Authentication/Form/PasswordCasePolicy/index.test.tsx
@@ -0,0 +1,174 @@
+import * as React from 'react';
+
+import userEvent from '@testing-library/user-event';
+import { Formik } from 'formik';
+
+import { render, RenderResult, screen, waitFor } from '@centreon/ui';
+
+import { SecurityPolicy } from '../../models';
+import useValidationSchema from '../../useValidationSchema';
+import {
+  labelForceToUseLowerCase,
+  labelForceToUseNumbers,
+  labelForceToUseSpecialCharacters,
+  labelForceToUseUpperCase,
+  labelGood,
+  labelMaximum128Characters,
+  labelMinimum8Characters,
+  labelPasswordCasePolicy,
+  labelPasswordLength,
+  labelRequired,
+  labelStrong,
+  labelWeak,
+} from '../../translatedLabels';
+import {
+  defaultSecurityPolicy,
+  defaultSecurityPolicyWithNullValues,
+} from '../defaults';
+
+import PasswordCasePolicy from '.';
+
+const noOp = jest.fn();
+
+interface Props {
+  initialValues: SecurityPolicy;
+}
+
+const TestComponent = ({ initialValues }: Props): JSX.Element => {
+  const validationSchema = useValidationSchema();
+
+  return (
+    <Formik<SecurityPolicy>
+      enableReinitialize
+      validateOnBlur
+      validateOnMount
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={noOp}
+    >
+      {(): JSX.Element => <PasswordCasePolicy />}
+    </Formik>
+  );
+};
+
+const renderPasswordCasePolicy = (
+  initialValues: SecurityPolicy = defaultSecurityPolicy,
+): RenderResult => render(<TestComponent initialValues={initialValues} />);
+
+describe('Password case policy', () => {
+  it('renders the password case policy fields with values', async () => {
+    renderPasswordCasePolicy();
+
+    await waitFor(() => {
+      expect(screen.getByText(labelPasswordCasePolicy)).toBeInTheDocument();
+    });
+
+    expect(screen.getByLabelText(labelPasswordLength)).toHaveValue(12);
+    expect(
+      screen.getAllByLabelText(labelForceToUseLowerCase)[0],
+    ).toBeInTheDocument();
+    expect(
+      screen.getAllByLabelText(labelForceToUseUpperCase)[0],
+    ).toBeInTheDocument();
+    expect(
+      screen.getAllByLabelText(labelForceToUseNumbers)[0],
+    ).toBeInTheDocument();
+    expect(
+      screen.getAllByLabelText(labelForceToUseSpecialCharacters)[0],
+    ).toBeInTheDocument();
+    expect(screen.getByText(labelStrong)).toBeInTheDocument();
+  });
+
+  it('changes the password minimum length value when "45" is typed in the input', async () => {
+    renderPasswordCasePolicy();
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}45',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByLabelText(labelPasswordLength)).toHaveValue(45);
+    });
+  });
+  it('displays an error message when password minimum length input is emptied', async () => {
+    renderPasswordCasePolicy();
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelRequired)).toBeInTheDocument();
+    });
+  });
+
+  it('displays an error message when "7" is typed in the password minimum length input', async () => {
+    renderPasswordCasePolicy();
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}7',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelMinimum8Characters)).toBeInTheDocument();
+    });
+  });
+
+  it('displays an error message when "129" is typed in the password minimum length input', async () => {
+    renderPasswordCasePolicy();
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}129',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelMaximum128Characters)).toBeInTheDocument();
+    });
+  });
+
+  it('displays "Strong" when all cases buttons are selected', async () => {
+    renderPasswordCasePolicy(defaultSecurityPolicyWithNullValues);
+
+    userEvent.click(screen.getAllByLabelText(labelForceToUseLowerCase)[0]);
+    userEvent.click(screen.getAllByLabelText(labelForceToUseUpperCase)[0]);
+    userEvent.click(screen.getAllByLabelText(labelForceToUseNumbers)[0]);
+    userEvent.click(
+      screen.getAllByLabelText(labelForceToUseSpecialCharacters)[0],
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelStrong)).toBeInTheDocument();
+    });
+  });
+
+  it('displays "Good" when only 3 cases buttons are selected', async () => {
+    renderPasswordCasePolicy(defaultSecurityPolicyWithNullValues);
+
+    userEvent.click(screen.getAllByLabelText(labelForceToUseLowerCase)[0]);
+    userEvent.click(screen.getAllByLabelText(labelForceToUseUpperCase)[0]);
+    userEvent.click(
+      screen.getAllByLabelText(labelForceToUseSpecialCharacters)[0],
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelGood)).toBeInTheDocument();
+    });
+  });
+
+  it('displays "Weak" when only 2 cases buttons are selected', async () => {
+    renderPasswordCasePolicy(defaultSecurityPolicyWithNullValues);
+
+    userEvent.click(screen.getAllByLabelText(labelForceToUseNumbers)[0]);
+    userEvent.click(
+      screen.getAllByLabelText(labelForceToUseSpecialCharacters)[0],
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelWeak)).toBeInTheDocument();
+    });
+  });
+});
---
+++ www/front_src/src/Authentication/index.test.tsx
@@ -0,0 +1,200 @@
+import * as React from 'react';
+
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
+
+import { RenderResult, render, screen, waitFor } from '@centreon/ui';
+
+import {
+  labelReset,
+  labelDefinePasswordSecurityPolicy,
+  labelDoYouWantToResetTheForm,
+  labelNumberOfAttemptsBeforeBlockingNewAttempts,
+  labelPasswordBlockingPolicy,
+  labelPasswordCasePolicy,
+  labelPasswordExpirationPolicy,
+  labelPasswordLength,
+  labelResetTheForm,
+  labelSave,
+} from './translatedLabels';
+import {
+  defaultSecurityPolicyAPI,
+  retrievedSecurityPolicyAPI,
+} from './Form/defaults';
+import { securityPolicyEndpoint } from './api/endpoints';
+import { SecurityPolicyAPI } from './models';
+
+import Authentication from '.';
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const cancelTokenRequestParam = { cancelToken: {} };
+
+const cancelTokenPutParams = {
+  ...cancelTokenRequestParam,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+};
+
+const renderAuthentication = (): RenderResult => render(<Authentication />);
+
+const mockGetSecurityPolicy = (securityPolicy: SecurityPolicyAPI): void => {
+  mockedAxios.get.mockReset();
+  mockedAxios.get.mockResolvedValue({
+    data: securityPolicy,
+  });
+};
+
+describe('Authentication', () => {
+  beforeEach(() => {
+    mockedAxios.put.mockReset();
+    mockedAxios.put.mockResolvedValue({
+      data: {},
+    });
+  });
+
+  it('updates the retrieved form recommended values and send the data when the "Save" button is clicked', async () => {
+    mockGetSecurityPolicy(defaultSecurityPolicyAPI);
+    renderAuthentication();
+
+    expect(
+      screen.getByText(labelDefinePasswordSecurityPolicy),
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        securityPolicyEndpoint,
+        cancelTokenRequestParam,
+      );
+    });
+
+    expect(screen.getByText(labelPasswordCasePolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordExpirationPolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordBlockingPolicy)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText(labelSave)).not.toBeDisabled();
+    });
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}45',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelSave)).not.toBeDisabled();
+    });
+
+    userEvent.click(screen.getByText(labelSave));
+
+    await waitFor(() => {
+      expect(mockedAxios.put).toHaveBeenCalledWith(
+        securityPolicyEndpoint,
+        {
+          ...defaultSecurityPolicyAPI,
+          password_min_length: 45,
+        },
+        cancelTokenPutParams,
+      );
+    });
+  });
+
+  it('updates the retrieved form recommended values and reset the form to the inital values', async () => {
+    mockGetSecurityPolicy(defaultSecurityPolicyAPI);
+    renderAuthentication();
+
+    expect(
+      screen.getByText(labelDefinePasswordSecurityPolicy),
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        securityPolicyEndpoint,
+        cancelTokenRequestParam,
+      );
+    });
+
+    expect(screen.getByText(labelPasswordCasePolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordExpirationPolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordBlockingPolicy)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText(labelReset)).toBeDisabled();
+    });
+
+    userEvent.type(
+      screen.getByLabelText(labelPasswordLength),
+      '{selectall}{backspace}45',
+    );
+
+    userEvent.type(
+      screen.getByLabelText(labelNumberOfAttemptsBeforeBlockingNewAttempts),
+      '{selectall}{backspace}8',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelReset)).not.toBeDisabled();
+    });
+
+    userEvent.click(screen.getByText(labelReset));
+
+    await waitFor(() => {
+      expect(screen.getByText(labelResetTheForm)).toBeInTheDocument();
+    });
+
+    expect(screen.getByText(labelDoYouWantToResetTheForm)).toBeInTheDocument();
+
+    userEvent.click(screen.getAllByText(labelReset)[1]);
+
+    await waitFor(() => {
+      expect(screen.getByLabelText(labelPasswordLength)).toHaveValue(12);
+    });
+  });
+
+  it('updates the retrieved form values and send the data when the "Save" button is clicked', async () => {
+    mockGetSecurityPolicy(retrievedSecurityPolicyAPI);
+    renderAuthentication();
+
+    expect(
+      screen.getByText(labelDefinePasswordSecurityPolicy),
+    ).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(mockedAxios.get).toHaveBeenCalledWith(
+        securityPolicyEndpoint,
+        cancelTokenRequestParam,
+      );
+    });
+
+    expect(screen.getByText(labelPasswordCasePolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordExpirationPolicy)).toBeInTheDocument();
+    expect(screen.getByText(labelPasswordBlockingPolicy)).toBeInTheDocument();
+
+    await waitFor(() => {
+      expect(screen.getByText(labelSave)).toBeDisabled();
+    });
+
+    userEvent.type(
+      screen.getByLabelText(labelNumberOfAttemptsBeforeBlockingNewAttempts),
+      '{selectall}{backspace}2',
+    );
+
+    await waitFor(() => {
+      expect(screen.getByText(labelSave)).not.toBeDisabled();
+    });
+
+    userEvent.click(screen.getByText(labelSave));
+
+    await waitFor(() => {
+      expect(mockedAxios.put).toHaveBeenCalledWith(
+        securityPolicyEndpoint,
+        {
+          ...retrievedSecurityPolicyAPI,
+          attempts: 2,
+        },
+        cancelTokenPutParams,
+      );
+    });
+  });
+});
