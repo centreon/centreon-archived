kseckinc/centreon

Refacto and added functionality to update a host

src/Centreon/Infrastructure/HostConfiguration/Repository/HostConfigurationRepositoryRDB.php
@@ -764,4 +764,89 @@ class HostConfigurationRepositoryRDB extends AbstractRepositoryDRB implements Ho
             }
         }
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function updateHost(Host $host): void
+    {
+        $isInTransaction = $this->db->inTransaction();
+        if (!$isInTransaction) {
+            $this->db->beginTransaction();
+        }
+        try {
+            $request = $this->translateDbName(
+                'UPDATE `:db`.host SET
+                    host_name = :name, host_alias = :alias, host_address = :ip_address, host_comment = :comment, 
+                    geo_coords = :geo_coords, host_activate = :is_activate, host_register = :host_register, 
+                    host_active_checks_enabled = :active_check_status,
+                    host_passive_checks_enabled = :passive_check_status, host_checks_enabled = :check_status,
+                    host_obsess_over_host = :obsess_over_status, host_check_freshness = :freshness_check_status,
+                    host_event_handler_enabled = :event_handler_status, 
+                    host_flap_detection_enabled = :flap_detection_status, host_process_perf_data = :process_perf_status,
+                    host_retain_status_information = :retain_status_information, 
+                    host_retain_nonstatus_information = :retain_nonstatus_information,
+                    host_notifications_enabled = :notifications_status
+                    WHERE host_id = :id'
+            );
+
+            $statement = $this->db->prepare($request);
+            $statement->bindValue(':id', $host->getId(), \PDO::PARAM_INT);
+            $statement->bindValue(':name', $host->getName(), \PDO::PARAM_STR);
+            $statement->bindValue(':alias', $host->getAlias(), \PDO::PARAM_STR);
+            $statement->bindValue(':ip_address', $host->getIpAddress(), \PDO::PARAM_STR);
+            $statement->bindValue(':comment', $host->getComment(), \PDO::PARAM_STR);
+            $statement->bindValue(':geo_coords', $host->getGeoCoords(), \PDO::PARAM_STR);
+            $statement->bindValue(':is_activate', $host->isActivated(), \PDO::PARAM_STR);
+            $statement->bindValue(':host_register', '1', \PDO::PARAM_STR);
+            $statement->bindValue(':active_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':passive_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':obsess_over_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':freshness_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':event_handler_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':flap_detection_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':process_perf_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':retain_status_information', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':retain_nonstatus_information', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':notifications_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->execute();
+            $this->updateMonitoringServerRelation($host->getId(), $host->getMonitoringServer()->getId());
+            if (!$isInTransaction) {
+                $this->db->commit();
+            }
+        } catch (\Throwable $ex) {
+            if (!$isInTransaction) {
+                $this->db->rollBack();
+            }
+            throw $ex;
+        }
+    }
+
+    /**
+     * Add or update the link between a host and a monitoring server to which it is linked.
+     *
+     * @param int $hostId
+     * @param int $monitoringServerId
+     */
+    private function updateMonitoringServerRelation(int $hostId, int $monitoringServerId): void
+    {
+        $request = $this->translateDbName(
+            "DELETE FROM `:db`.ns_host_relation
+            WHERE nagios_server_id = :monitoring_server_id AND host_host_id = :host_id"
+        );
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':monitoring_server_id', $monitoringServerId, \PDO::PARAM_INT);
+        $statement->bindValue(':host_id', $hostId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        $request = $this->translateDbName(
+            "INSERT INTO `:db`.ns_host_relation
+            (nagios_server_id, host_host_id) VALUES (:monitoring_server_id, :host_id)"
+        );
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':monitoring_server_id', $monitoringServerId, \PDO::PARAM_INT);
+        $statement->bindValue(':host_id', $hostId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }

centreon/centreon-archived

src/Centreon/Infrastructure/HostConfiguration/Repository/HostConfigurationRepositoryRDB.php
@@ -764,4 +764,89 @@ class HostConfigurationRepositoryRDB extends AbstractRepositoryDRB implements Ho
             }
         }
     }
+
+    /**
+     * @inheritDoc
+     */
+    public function updateHost(Host $host): void
+    {
+        $isAlreadyInTransaction = $this->db->inTransaction();
+        if (!$isAlreadyInTransaction) {
+            $this->db->beginTransaction();
+        }
+        try {
+            $request = $this->translateDbName(
+                'UPDATE `:db`.host SET
+                    host_name = :name, host_alias = :alias, host_address = :ip_address, host_comment = :comment, 
+                    geo_coords = :geo_coords, host_activate = :is_activate, host_register = :host_register, 
+                    host_active_checks_enabled = :active_check_status,
+                    host_passive_checks_enabled = :passive_check_status, host_checks_enabled = :check_status,
+                    host_obsess_over_host = :obsess_over_status, host_check_freshness = :freshness_check_status,
+                    host_event_handler_enabled = :event_handler_status, 
+                    host_flap_detection_enabled = :flap_detection_status, host_process_perf_data = :process_perf_status,
+                    host_retain_status_information = :retain_status_information, 
+                    host_retain_nonstatus_information = :retain_nonstatus_information,
+                    host_notifications_enabled = :notifications_status
+                    WHERE host_id = :id'
+            );
+
+            $statement = $this->db->prepare($request);
+            $statement->bindValue(':id', $host->getId(), \PDO::PARAM_INT);
+            $statement->bindValue(':name', $host->getName(), \PDO::PARAM_STR);
+            $statement->bindValue(':alias', $host->getAlias(), \PDO::PARAM_STR);
+            $statement->bindValue(':ip_address', $host->getIpAddress(), \PDO::PARAM_STR);
+            $statement->bindValue(':comment', $host->getComment(), \PDO::PARAM_STR);
+            $statement->bindValue(':geo_coords', $host->getGeoCoords(), \PDO::PARAM_STR);
+            $statement->bindValue(':is_activate', $host->isActivated(), \PDO::PARAM_STR);
+            $statement->bindValue(':host_register', '1', \PDO::PARAM_STR);
+            $statement->bindValue(':active_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':passive_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':obsess_over_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':freshness_check_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':event_handler_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':flap_detection_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':process_perf_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':retain_status_information', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':retain_nonstatus_information', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->bindValue(':notifications_status', Host::OPTION_DEFAULT, \PDO::PARAM_STR);
+            $statement->execute();
+            $this->updateMonitoringServerRelation($host->getId(), $host->getMonitoringServer()->getId());
+            if (!$isAlreadyInTransaction) {
+                $this->db->commit();
+            }
+        } catch (\Throwable $ex) {
+            if (!$isAlreadyInTransaction) {
+                $this->db->rollBack();
+            }
+            throw $ex;
+        }
+    }
+
+    /**
+     * Add or update the link between a host and a monitoring server to which it is linked.
+     *
+     * @param int $hostId
+     * @param int $monitoringServerId
+     */
+    private function updateMonitoringServerRelation(int $hostId, int $monitoringServerId): void
+    {
+        $request = $this->translateDbName(
+            "DELETE FROM `:db`.ns_host_relation
+            WHERE nagios_server_id = :monitoring_server_id AND host_host_id = :host_id"
+        );
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':monitoring_server_id', $monitoringServerId, \PDO::PARAM_INT);
+        $statement->bindValue(':host_id', $hostId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        $request = $this->translateDbName(
+            "INSERT INTO `:db`.ns_host_relation
+            (nagios_server_id, host_host_id) VALUES (:monitoring_server_id, :host_id)"
+        );
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':monitoring_server_id', $monitoringServerId, \PDO::PARAM_INT);
+        $statement->bindValue(':host_id', $hostId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
 }