7f11a415e59b392ed9311dab5400cb3bd3211070
Tom Darneix <tomdar87@outlook.com>
2022-01-19 21:53:00 +0000

    Fix copyright

--- www/front_src/src/Footer/Copyright.tsx
+++ www/front_src/src/Footer/Copyright.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { makeStyles, Typography } from '@material-ui/core';
 
-import { useLocaleDateTimeFormat } from '@centreon/ui';
+import { useUserContext } from '@centreon/ui-context';
 
 const useStyles = makeStyles({
   copyright: {
@@ -13,16 +13,20 @@ const useStyles = makeStyles({
 
 const Copyright = (): JSX.Element => {
   const classes = useStyles();
-  const { format } = useLocaleDateTimeFormat();
+  const { timezone } = useUserContext();
 
-  const year = format({
-    date: new Date(),
-    formatString: 'YYYY',
+  const now = new Date();
+
+  const year = now.toLocaleDateString('fr', {
+    timeZone: timezone,
+    year: 'numeric',
   });
+
+  const label = `Copyright © 2005 - ${year}`;
 
   return (
     <Typography className={classes.copyright} variant="body2">
-      Copyright © 2005 - {year}
+      {label}
     </Typography>
   );
 };
