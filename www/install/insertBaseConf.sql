533307093b51242988b134a2d3cfbb87b815fa2f
Jérémy Jaouen <jjaouen@centreon.com>
2022-01-25 07:27:41 +0100

    fix pipeline

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.10');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.04.11');
 
 --
 -- Contenu de la table `contact`
