33e778ee69c526c549d24a29a65103e95e32b0d2
jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
2022-01-25 12:52:29 +0100

    enh(autologin): verify that autologin is different than password (#10585)

--- www/include/configuration/configObject/contact/formContact.php
+++ www/include/configuration/configObject/contact/formContact.php
@@ -713,6 +713,9 @@ if ($o != "mc") {
     }
 
     $form->addRule(array('contact_passwd', 'contact_passwd2'), _("Passwords do not match"), 'compare');
+    if ($o === ADD_CONTACT || $o === MODIFY_CONTACT) {
+        $form->addFormRule('validateAutologin');
+    }
     $form->registerRule('exist', 'callback', 'testContactExistence');
     $form->addRule('contact_name', "<font style='color: red;'>*</font>&nbsp;" . _("Contact already exists"), 'exist');
     $form->registerRule('existAlias', 'callback', 'testAliasExistence');
