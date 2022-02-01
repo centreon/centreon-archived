c8540708de0d980147c644caff447c40c15be978
Adrien Morais <amorais@centreon.com>
2022-02-01 16:22:49 +0100

    feedbacks from kdu applied

--- www/include/reporting/dashboard/initReport.php
+++ www/include/reporting/dashboard/initReport.php
@@ -244,32 +244,31 @@ $lcaSvcstr    = $centreon->user->access->getServicesString("ID", $pearDBO);
 /*
  * setting variables for link with services
  */
-$period_choice = (isset($_POST["period_choice"]))
-    ? filter_var($_POST["period_choice"], FILTER_SANITIZE_STRING)
-    : "preset";
+$period_choice = filter_var(
+    $_POST["period_choice"] ?? "preset",
+    FILTER_SANITIZE_STRING
+);
 
 $period = '';
-if (isset($_POST['period'])) {
-    $period = filter_var($_POST['period'], FILTER_SANITIZE_STRING);
-} elseif (isset($_GET['period'])) {
-    $period = filter_var($_GET['period'], FILTER_SANITIZE_STRING);
-}
+
+$period = filter_var(
+    $_POST['period'] ?? $_GET['period'],
+    FILTER_SANITIZE_STRING
+);
 
 $get_date_start = '';
 
-if (isset($_POST['StartDate'])) {
-    $get_date_start = filter_var($_POST['StartDate'], FILTER_SANITIZE_STRING);
-} elseif (isset($_GET['StartDate'])) {
-    $get_date_start = filter_var($_GET['StartDate'], FILTER_SANITIZE_STRING);
-}
+$get_date_start = filter_var(
+    $_POST['StartDate'] ?? $_GET['StartDate'],
+    FILTER_SANITIZE_STRING
+);
 
 $get_date_end = '';
 
-if (isset($_POST['EndDate'])) {
-    $get_date_end = filter_var($_POST['EndDate'], FILTER_SANITIZE_STRING);
-} elseif (isset($_GET['EndDate'])) {
-    $get_date_end = filter_var($_GET['EndDate'], FILTER_SANITIZE_STRING);
-}
+$get_date_start = filter_var(
+    $_POST['EndDate'] ?? $_GET['EndDate'],
+    FILTER_SANITIZE_STRING
+);
 
 if ($get_date_start == "" && $get_date_end == "" && $period == "") {
     $period = "yesterday";
