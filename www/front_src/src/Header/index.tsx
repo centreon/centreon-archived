402c5b7a09c94acc63d1ede036622a53ba46f934
vagrant <vagrant@ip-10-0-2-15.eu-west-1.compute.internal>
2022-02-01 13:53:41 +0000

    Replace Header scss module with material makeStyles-composant header-

--- www/front_src/src/Header/index.tsx
+++ www/front_src/src/Header/index.tsx
@@ -1,25 +1,40 @@
 import * as React from 'react';
 
-import classnames from 'classnames';
+import { makeStyles } from '@mui/styles';
 
 import Hook from '../components/Hook';
 
-import styles from './header.scss';
 import PollerMenu from './PollerMenu';
-import UserMenu from './userMenu';
-import ServiceStatusCounter from './RessourceStatusCounter/Service';
 import HostStatusCounter from './RessourceStatusCounter/Host';
+import ServiceStatusCounter from './RessourceStatusCounter/Service';
+import UserMenu from './userMenu';
 
 const HookComponent = Hook as unknown as (props) => JSX.Element;
 
+const useStyles = makeStyles({
+  container: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  header: {
+    background: '#232f39',
+  },
+  rightContainer: {
+    display: 'flex',
+  },
+});
+
 const Header = (): JSX.Element => {
+  const classes = useStyles();
+
   return (
-    <header className={styles.header}>
-      <div className={styles['header-icons']}>
-        <div className={classnames(styles.wrap, styles['wrap-left'])}>
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <div>
           <PollerMenu />
         </div>
-        <div className={classnames(styles.wrap, styles['wrap-right'])}>
+        <div className={classes.rightContainer}>
           <HookComponent path="/header/topCounter" />
           <HostStatusCounter />
           <ServiceStatusCounter />
