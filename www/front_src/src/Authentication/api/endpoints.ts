2d7b3f2696f360a37346636c82de962965cb537e
Tom Darneix <tomdar87@outlook.com>
2022-02-03 21:12:24 +0000

    Remove mock base endpoint

--- www/front_src/src/Authentication/api/endpoints.ts
+++ www/front_src/src/Authentication/api/endpoints.ts
@@ -1,5 +1,4 @@
 const baseEndpoint = './api/latest';
 
 export const securityPolicyEndpoint = `${baseEndpoint}/administration/authentication/providers/local`;
-export const contactsEndpoint =
-  'http://localhost:5003/centreon/api/latest/configuration/users';
+export const contactsEndpoint = `${baseEndpoint}/configuration/users`;
