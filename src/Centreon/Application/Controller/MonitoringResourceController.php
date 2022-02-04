38897e767ba572327e3ec0cdcd4527f54af3c100
jcaro <jcaro@centreon.com>
2022-02-04 12:08:36 +0100

    enh(UI): Rework detail panel chip to add action on hover Chip (#10528)
    * Resize Chip like in Listing
    
    * Add hovered condition to display or not icon actions
    
    * Uncomment code to push pr in draft mode
    
    * Remove isHovered prop on Cards
    
    * Add isHovered behavior on DetailCard Groups
    
    * Add Hovered on Chip - Icons to set filter and redirect link to HG configuration
    
    * Add onHover beahvior only on Chip Hovered
    
    * Add icon clickable and execute function
    
    * Add htmlColor attribute for IconButton
    
    * Fiw minmaxWidht on Chip
    
    * Remove props unused
    
    * Add tooltips and chips to improve Detail Card Groups behavior
    
    * enh(resource): add backend part
    
    * First step to change card to custom card
    
    * Fix displaying  Chip into tile if we have mmore five Chips HG-SG
    
    * Add hook useHistory to use navigate for redirect to configuration page for host/service group
    
    * Add new interface Group to extend NamedEntity with new props
    
    * Add redirection link  to configuration page for HG and SG
    
    * Export interface Group
    
    * Add GroupChip component to code refacto
    
    * Refacto GroupChip component
    
    * Refacto code
    
    * Fix issue from mui migration to V5
    
    * Regenerate package-lock
    
    * Remove scope from centreon-frontend path
    
    * Remove scope from centreon-frontend path into tsconfig.json
    
    * rollback
    
    * Regenerate config files without @centreon for centreon frontend
    
    * Remove path "@centreon"
    
    * Fix issues with last changes
    
    * FIx issue with pr merged about Module and LicenseModule
    
    * Add new file for component GroupChip and regenerate packa-lock
    
    * Fix Eslint issues
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/GroupChip.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/GroupChip.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Extract mouse event into specific functions
    
    * Fix issues
    
    * Remove ThemeProvider by Module
    
    * Change ThemeProvider by Module
    
    * Fix path from centreon-frontend
    
    * Fix eslint issue and import GroupChip in render of Groups
    
    * FIx  unit test and fix displaying icons on Hover
    
    * Fix eslint issue
    
    * Remove unused style and rename class style
    
    * Remove unused style in Groups file
    
    * enable eslint react/jsx-key
    
    * Update src/Centreon/Domain/Contact/Contact.php
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/GroupChip.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/Groups.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Remove style unused and extract function in specific variable
    
    * Add class for icon label color
    
    * Add style for chip label color , return transparent or not
    
    * Extract groups to specific variable and Fix unit test
    
    * fix eslint issues
    
    * Add class for Typography
    
    * Fix ieslint issue Unexpected constant condition in property chipLabelColor
    
    * Add condition class to state for display chipLabel
    
    * Rename interface for component GroupChip
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/GroupChip.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update www/front_src/src/Resources/Details/tabs/Details/DetailsCard/GroupChip.tsx
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    Co-authored-by: Adrien Morais <amorais@centreon.com>
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- src/Centreon/Application/Controller/MonitoringResourceController.php
+++ src/Centreon/Application/Controller/MonitoringResourceController.php
@@ -45,6 +45,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Centreon\Domain\Monitoring\Interfaces\ResourceServiceInterface;
 use Centreon\Domain\Monitoring\Serializer\ResourceExclusionStrategy;
 use Centreon\Domain\Monitoring\Interfaces\MonitoringServiceInterface;
+use Centreon\Domain\Monitoring\ResourceGroup;
 use Centreon\Domain\RequestParameters\Interfaces\RequestParametersInterface;
 
 /**
@@ -90,6 +91,9 @@ class MonitoringResourceController extends AbstractController
     private const HOST_REPORTING_URI = '/main.php?p=307&host={resource_id}';
     private const SERVICE_REPORTING_URI =
         '/main.php?p=30702&period=yesterday&start=&end=&host_id={parent_resource_id}&item={resource_id}';
+
+    private const HOSTGROUP_CONFIGURATION_URI = '/main.php?p=60102&o=c&hg_id={resource_group_id}';
+    private const SERVICEGROUP_CONFIGURATION_URI = '/main.php?p=60203&o=c&sg_id={resource_group_id}';
 
     private const RESOURCE_LISTING_URI = '/monitoring/resources';
 
@@ -578,6 +582,48 @@ class MonitoringResourceController extends AbstractController
     }
 
     /**
+     * This function adds to the group the redirection URI to the configuration
+     *
+     * @param ResourceEntity $resource
+     * @param Contact $contact
+     * @return void
+     */
+    private function provideResourceGroupInternalUris(ResourceEntity $resource, Contact $contact): void
+    {
+        if ($resource->getType() === ResourceEntity::TYPE_HOST) {
+            if (
+                $contact->hasTopologyRole(Contact::ROLE_CONFIGURATION_HOSTS_HOST_GROUPS_READ_WRITE)
+                || $contact->hasTopologyRole(Contact::ROLE_CONFIGURATION_HOSTS_HOST_GROUPS_READ)
+                || $contact->isAdmin()
+            ) {
+                foreach ($resource->getGroups() as $group) {
+                    $group->setConfigurationUri(
+                        $this->generateResourceGroupConfigurationUri(
+                            $group,
+                            self::HOSTGROUP_CONFIGURATION_URI
+                        )
+                    );
+                }
+            }
+        } elseif ($resource->getType() === ResourceEntity::TYPE_SERVICE) {
+            if (
+                $contact->hasTopologyRole(Contact::ROLE_CONFIGURATION_SERVICES_SERVICE_GROUPS_READ_WRITE)
+                || $contact->hasTopologyRole(Contact::ROLE_CONFIGURATION_SERVICES_SERVICE_GROUPS_READ)
+                || $contact->isAdmin()
+            ) {
+                foreach ($resource->getGroups() as $group) {
+                    $group->setConfigurationUri(
+                        $this->generateResourceGroupConfigurationUri(
+                            $group,
+                            self::SERVICEGROUP_CONFIGURATION_URI
+                        )
+                    );
+                }
+            }
+        }
+    }
+
+    /**
      * Generate full uri from relative path
      *
      * @param ResourceEntity $resource
@@ -594,6 +640,19 @@ class MonitoringResourceController extends AbstractController
             $relativeUri = str_replace('{parent_resource_id}', (string) $resource->getParent()->getId(), $relativeUri);
         }
 
+        return $this->getBaseUri() . $relativeUri;
+    }
+
+    /**
+     * Generate full uri from relative path for ResourceGroup
+     *
+     * @param ResourceGroup $group
+     * @param string $relativeUri
+     * @return string
+     */
+    private function generateResourceGroupConfigurationUri(ResourceGroup $group, string $relativeUri): string
+    {
+        $relativeUri = str_replace('{resource_group_id}', (string) $group->getId(), $relativeUri);
         return $this->getBaseUri() . $relativeUri;
     }
 
