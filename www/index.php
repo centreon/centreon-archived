4586e0b5ad38b16b8a75bd8c2216ebd6d18e8c44
Jérémy Jaouen <jjaouen@centreon.com>
2022-02-07 15:34:02 +0100

    move processLogin into index.php

--- www/index.php
+++ www/index.php
@@ -35,18 +35,15 @@
  */
 
 require_once realpath(__DIR__ . '/../config/centreon.config.php');
+require_once __DIR__ . '/../bootstrap.php';
 
 $etc = _CENTREON_ETC_;
+const AUTOLOGIN_FIELDS = array('autologin' , 'useralias', 'token');
 
-define('SMARTY_DIR', realpath('../vendor/smarty/smarty/libs/') . '/');
-
-ini_set('display_errors', 'Off');
-
-require_once $classdir . "/centreon.class.php";
-require_once $classdir . "/centreonSession.class.php";
-require_once $classdir . "/centreonAuth.SSO.class.php";
-require_once $classdir . "/centreonLog.class.php";
-require_once $classdir . "/centreonDB.class.php";
+require_once __DIR__ . "/class/centreonSession.class.php";
+require_once __DIR__ . "/class/centreonAuth.SSO.class.php";
+require_once __DIR__ . "/class/centreonLog.class.php";
+require_once __DIR__ . "/class/centreonDB.class.php";
 
 /*
  * Get auth type
@@ -67,17 +64,160 @@ CentreonSession::start();
  */
 if (isset($_SESSION["centreon"])) {
     $centreon = &$_SESSION["centreon"];
-    header('Location: main.php');
+    header('Location: index.html');
 }
 
 /*
  * Check PHP version
  *
- *  Centreon >= 18.10 doesn't support PHP < 7.1
+ *  Centreon >= 22.04 doesn't support PHP < 8.0
  *
  */
-if (version_compare(phpversion(), '7.1') < 0) {
-    echo "<div class='msg'> PHP version is < 7.1. Please Upgrade PHP</div>";
+if (version_compare(phpversion(), '8.0') < 0) {
+    echo "<div class='msg'> PHP version is < 8.0. Please Upgrade PHP</div>";
 } else {
-    include_once "./include/core/login/login.php";
+    if (
+        isset($_POST["centreon_token"])
+        || (isset($_GET["autologin"]) && $_GET["autologin"]
+            && isset($generalOptions["enable_autologin"])
+            && $generalOptions["enable_autologin"])
+        || (isset($_POST["autologin"]) && $_POST["autologin"]
+            && isset($generalOptions["enable_autologin"])
+            && $generalOptions["enable_autologin"])
+        || (!isset($generalOptions['sso_enable']) || $generalOptions['sso_enable'] == 1)
+        || (!isset($generalOptions['openid_connect_enable']) || $generalOptions['openid_connect_enable'] == 1)
+    ) {
+        if (isset($_POST['p'])) {
+            $_GET["p"] = $_POST["p"];
+        }
+
+        if (isset($_POST['min'])) {
+            $_GET["min"] = $_POST["min"];
+        }
+        /*
+        * Init log class
+        */
+        $CentreonLog = new CentreonUserLog(-1, $pearDB);
+
+        /*
+        * Check first for Autologin or Get Authentication
+        */
+        $autologin = isset($_GET["autologin"]) ? $_GET["autologin"] : CentreonAuthSSO::AUTOLOGIN_DISABLE;
+        $useraliasG = isset($_GET["useralias"]) ? $_GET["useralias"] : null;
+
+        $useraliasP = null;
+        $passwordP = null;
+
+        $useralias = $useraliasG ?? $useraliasP;
+        $password = $passwordG ?? $passwordP;
+
+        $token = "";
+        if (isset($_REQUEST['token']) && $_REQUEST['token']) {
+            $token = $_REQUEST['token'];
+        }
+
+        if (!isset($encryptType)) {
+            $encryptType = CentreonAuthSSO::ENCRYPT_MD5;
+        }
+
+        $centreonAuth = new CentreonAuthSSO(
+            $dependencyInjector,
+            $useralias,
+            $password,
+            $autologin,
+            $pearDB,
+            $CentreonLog,
+            $encryptType,
+            $token,
+            $generalOptions
+        );
+        if ($centreonAuth->passwdOk == 1) {
+            $centreon = new Centreon($centreonAuth->userInfos);
+            // security fix - regenerate the sid after the login to prevent session fixation
+            session_regenerate_id(true);
+            $_SESSION["centreon"] = $centreon;
+            // saving session data in the DB
+            $query = "INSERT INTO `session` (`session_id` , `user_id` , `current_page` , `last_reload`, `ip_address`) "
+                . "VALUES (?, ?, ?, ?, ?)";
+            $dbResult = $pearDB->prepare($query);
+            $pearDB->execute(
+                $dbResult,
+                array(session_id(), $centreon->user->user_id, '1', time(), $_SERVER["REMOTE_ADDR"])
+            );
+
+            // saving session token in security_token
+            $expirationSessionDelay = 120;
+            $delayStatement = $pearDB->prepare("SELECT value FROM options WHERE `key` = 'session_expire'");
+            $delayStatement->execute();
+            if (($result = $delayStatement->fetch(\PDO::FETCH_ASSOC)) !== false) {
+                $expirationSessionDelay = $result['value'];
+            }
+            $securityTokenStatement = $pearDB->prepare(
+                "INSERT INTO security_token (`token`, `creation_date`, `expiration_date`) " .
+                "VALUES (:token, :createdAt, :expireAt)"
+            );
+            $securityTokenStatement->bindValue(":token", session_id(), \PDO::PARAM_STR);
+            $securityTokenStatement->bindValue(':createdAt', (new \DateTime())->getTimestamp(), \PDO::PARAM_INT);
+            $securityTokenStatement->bindValue(
+                ':expireAt',
+                (new \DateTime())->add(new \DateInterval('PT' . $expirationSessionDelay . 'M'))->getTimestamp(),
+                \PDO::PARAM_INT
+            );
+            $securityTokenStatement->execute();
+
+            //saving session in security_authentication_tokens
+            $providerTokenId = (int) $pearDB->lastInsertId();
+
+            $configurationStatement = $pearDB->query("SELECT id from provider_configuration WHERE name='local'");
+            if (($result = $configurationStatement->fetch(\PDO::FETCH_ASSOC)) !== false) {
+                $configurationId = (int) $result['id'];
+            } else {
+                throw new \Exception('No local provider found');
+            }
+            $securityAuthenticationTokenStatement = $pearDB->prepare(
+                "INSERT INTO security_authentication_tokens " .
+                "(`token`, `provider_token_id`, `provider_configuration_id`, `user_id`) VALUES " .
+                "(:token, :providerTokenId, :providerConfigurationId, :userId)"
+            );
+            $securityAuthenticationTokenStatement->bindValue(':token', session_id(), \PDO::PARAM_STR);
+            $securityAuthenticationTokenStatement->bindValue(':providerTokenId', $providerTokenId, \PDO::PARAM_INT);
+            $securityAuthenticationTokenStatement->bindValue(
+                ':providerConfigurationId',
+                $configurationId,
+                \PDO::PARAM_INT
+            );
+            $securityAuthenticationTokenStatement->bindValue(':userId', $centreon->user->user_id, \PDO::PARAM_INT);
+            $securityAuthenticationTokenStatement->execute();
+            $headerRedirection = "./main.php";
+            if (!empty($_GET["p"])) {
+                $headerRedirection .= "?p=" . $_GET["p"];
+                unset($_GET["p"]);
+                foreach ($_GET as $parameter => $value) {
+                    if (!in_array($parameter, AUTOLOGIN_FIELDS)) {
+                        $headerRedirection .= '&' . $parameter . '=' . $value;
+                    }
+                }
+            } elseif (isset($centreon->user->default_page) && $centreon->user->default_page != '') {
+                // get more details about the default page
+                $stmt = $pearDB->prepare(
+                    "SELECT topology_url, is_react FROM topology WHERE topology_page = ? LIMIT 0, 1"
+                );
+                $pearDB->execute($stmt, [$centreon->user->default_page]);
+
+                if ($stmt->rowCount() && ($topologyData = $stmt->fetch()) && $topologyData['is_react']) {
+                    // redirect to the react path
+                    $headerRedirection = '.' . $topologyData['topology_url'];
+                } else {
+                    $headerRedirection .= "?p=" . $centreon->user->default_page;
+
+                    if (isset($_GET["min"]) && $_GET["min"] == '1') {
+                        $headerRedirection .= '&min=1';
+                    }
+                }
+            }
+            header("Location: " . $headerRedirection);
+        } else {
+            header("Location: index.html");
+        }
+    }
 }
