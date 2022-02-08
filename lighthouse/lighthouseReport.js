1436efc4b1b9fb1080326aa8684c6cb0722f27f9
Tom Darneix <tomdar87@outlook.com>
2022-02-08 18:33:45 +0100

    Improve report

--- lighthouse/lighthouseReport.js
+++ lighthouse/lighthouseReport.js
@@ -5,6 +5,21 @@ import { startFlow } from 'lighthouse/lighthouse-core/fraggle-rock/api.js';
 
 const baseUrl = 'http://localhost:4000/centreon/';
 
+const screenEmulation = {
+  deviceScaleFactor: 1,
+  disabled: false,
+  height: 720,
+  mobile: false,
+  width: 1280,
+};
+
+const baseConfigContext = {
+  settingsOverrides: {
+    formFactor: 'desktop',
+    screenEmulation,
+  },
+};
+
 const createReportFile = (report) => {
   const lighthouseFolderExists = fs.existsSync('report');
 
@@ -15,6 +30,46 @@ const createReportFile = (report) => {
   fs.writeFileSync('report/lighthouseci-index.html', report);
 };
 
+const generateReportForLoginPage = async ({ flow, page }) => {
+  await flow.navigate(`${baseUrl}login`, { stepName: 'Login Cold navigation' });
+
+  await flow.navigate(`${baseUrl}login`, {
+    stepName: 'Login Warm navigation',
+  });
+
+  await flow.snapshot({ stepName: 'Login Snapshot' });
+
+  await page.waitForSelector('input[aria-label="Alias"]');
+
+  await flow.startTimespan({ stepName: 'Type login credentials' });
+  await page.type('input[aria-label="Alias"]', 'admin');
+  await page.type('input[aria-label="Password"]', 'Centreon!2021');
+  await page.click('button[aria-label="Connect"]');
+  await flow.endTimespan();
+};
+
+const generateReportForResourceStatusPage = async ({ flow, page }) => {
+  await page.setCacheEnabled(false);
+
+  await flow.navigate(`${baseUrl}monitoring/resources`, {
+    stepName: 'Resource Status Cold navigation',
+  });
+
+  await page.setCacheEnabled(true);
+
+  await flow.navigate(`${baseUrl}monitoring/resources`, {
+    stepName: 'Resource Status Warm navigation',
+  });
+
+  await flow.snapshot({ stepName: 'Resource Status Snapshot' });
+
+  await page.waitForSelector('input[placeholder="Search"]');
+
+  await flow.startTimespan({ stepName: 'Type search query' });
+  await page.type('input[placeholder="Search"]', 'Centreon');
+  await flow.endTimespan();
+};
+
 const captureReport = async () => {
   const browser = await puppeteer.launch({
     headless: true,
@@ -22,9 +77,13 @@ const captureReport = async () => {
   const page = await browser.newPage();
 
   const flow = await startFlow(page, {
-    name: 'Visit login page with cold navigation',
+    configContext: baseConfigContext,
+    name: 'Centreon Web pages',
   });
-  await flow.navigate(`${baseUrl}login`);
+
+  await generateReportForLoginPage({ flow, page });
+
+  await generateReportForResourceStatusPage({ flow, page });
 
   await browser.close();
 
