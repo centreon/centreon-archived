9ca646a3b0913c2ea4b63b08f2525e5411c36592
Adrien Morais <amorais@centreon.com>
2022-02-08 16:45:06 +0100

    fix(MonitoringResourceController): re-add constants to the class

--- src/Centreon/Application/Controller/MonitoringResourceController.php
+++ src/Centreon/Application/Controller/MonitoringResourceController.php
@@ -98,6 +98,8 @@ class MonitoringResourceController extends AbstractController
 
     private const HOSTGROUP_CONFIGURATION_URI = '/main.php?p=60102&o=c&hg_id={resource_group_id}';
     private const SERVICEGROUP_CONFIGURATION_URI = '/main.php?p=60203&o=c&sg_id={resource_group_id}';
+    private const SERVICE_REDIRECT_URL_ENDPOINT = 'centreon_application_monitoring_resource_redirect_url_service';
+    private const HOST_REDIRECT_URL_ENDPOINT = 'centreon_application_monitoring_resource_redirect_url_host';
 
     private const RESOURCE_LISTING_URI = '/monitoring/resources';
 
