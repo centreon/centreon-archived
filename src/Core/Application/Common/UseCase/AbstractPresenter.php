028ba9634355b0fa231241514a483ee1d19fc68f
Kevin Duret <kduret@centreon.com>
2022-02-15 13:23:58 +0100

    fix phpstan issues

--- src/Core/Application/Common/UseCase/AbstractPresenter.php
+++ src/Core/Application/Common/UseCase/AbstractPresenter.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2005 - 2021 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,6 @@
  * For more information : contact@centreon.com
  *
  */
-
 declare(strict_types=1);
 
 namespace Core\Application\Common\UseCase;
@@ -43,7 +42,7 @@ abstract class AbstractPresenter implements PresenterInterface
     }
 
     /**
-     * @param mixed $data
+     * @inheritDoc
      */
     public function present(mixed $data): void
     {
