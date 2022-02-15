6a389f17f1a31dc0709e921d5248ed6d7cf091db
Tom Darneix <tomdar87@outlook.com>
2022-02-15 09:12:21 +0000

    Remove debug url

--- www/front_src/src/Login/api/endpoint.ts
+++ www/front_src/src/Login/api/endpoint.ts
@@ -1,5 +1,4 @@
 import { baseEndpoint } from '../../api/endpoint';
 
-export const loginEndpoint = `http://localhost:5003/centreon/api/latest/authentication/providers/configurations/local`;
+export const loginEndpoint = `${baseEndpoint}/authentication/providers/configurations/local`;
 export const platformVersionsEndpoint = `${baseEndpoint}/platform/versions`;
-// http://localhost:5003/centreon/api/latest/authentication/providers/configurations/local
