a115acc601a513dec1e24143bf54349944614a8c
Laurent Calvet <lcalvet@centreon.com>
2022-02-16 11:22:42 +0100

    Add update script to clean host macros duplicated

--- www/install/php/Update-21.10.4.php
+++ www/install/php/Update-21.10.4.php
@@ -42,7 +42,14 @@ try {
 
     $errorMessage  = 'Unable to delete logger entry in cb_tag';
     $statement = $pearDB->query("DELETE FROM cb_tag WHERE tagname = 'logger'");
+    $pearDB->commit();
 
+    $errorMessage = 'Cannot purge host macros';
+    $cache = loadHosts($pearDB);
+    $pearDB->beginTransaction();
+    foreach ($cache as $hostId => $value) {
+        cleanDuplicateHostMacros($pearDB, $centreonLog, $cache, (int) $hostId);
+    }
     $pearDB->commit();
 } catch (\Exception $e) {
     if ($pearDB->inTransaction()) {
@@ -56,4 +63,102 @@ try {
         " - Trace : " . $e->getTraceAsString()
     );
     throw new \Exception($versionOfTheUpgrade . $errorMessage, (int)$e->getCode(), $e);
+}
+
+/**
+ * @param CentreonDb $db
+ * @return array<int, array<string, mixed>>
+ */
+function loadHosts(CentreonDb $db): array
+{
+    $stmt = $db->prepare('SELECT host_id, host_name FROM host');
+    $stmt->execute();
+    $cache = $stmt->fetchAll(PDO::FETCH_GROUP | PDO::FETCH_UNIQUE | PDO::FETCH_ASSOC);
+
+    $stmt = $db->prepare(
+        'SELECT host_host_id, host_tpl_id 
+         FROM host_template_relation ORDER BY `host_host_id`, `order` ASC'
+    );
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $cache[$row['host_host_id']]['htpl'][] = $row['host_tpl_id'];
+    }
+
+    $stmt = $db->prepare(
+        'SELECT host_macro_id, host_host_id, host_macro_name, host_macro_value
+        FROM on_demand_macro_host'
+    );
+    $stmt->execute();
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $cache[$row['host_host_id']]['macros'][$row['host_macro_name']] = $row;
+    }
+
+    return $cache;
+}
+
+/**
+ * @param CentreonDb $db
+ * @param CentreonLog $centreonLog
+ * @param array<int, array<string, mixed>> $cache
+ * @param int $srcHostId
+ */
+function cleanDuplicateHostMacros(
+    CentreonDb $db,
+    CentreonLog $centreonLog,
+    array $cache,
+    int $srcHostId
+): void {
+    global $versionOfTheUpgrade;
+
+    if (!isset($cache[$srcHostId]['htpl']) || !isset($cache[$srcHostId]['macros'])) {
+        return ;
+    }
+
+    $loop = [];
+    $stack = [];
+
+    $macros = $cache[$srcHostId]['macros'];
+    $stack = $cache[$srcHostId]['htpl'];
+    while (($hostId = array_shift($stack)) !== null) {
+        if (isset($loop[$hostId])) {
+            continue;
+        }
+        $loop[$hostId] = 1;
+
+        foreach ($macros as $macroName => $macro) {
+            if (isset($macro['checked']) && $macro['checked'] === 1) {
+                continue;
+            }
+
+            if (isset($cache[$hostId]['macros'][$macroName])) {
+                $macro['checked'] = 1;
+                if ($cache[$hostId]['macros'][$macroName]['host_macro_value'] === $macro['host_macro_value']) {
+                    $centreonLog->insertLog(
+                        4,
+                        $versionOfTheUpgrade . "host " . $cache[$hostId]['host_name'] . " delete macro " . $macroName
+                    );
+                    $db->query(
+                        'DELETE FROM on_demand_macro_host WHERE host_macro_id = ' . (int) $macro['host_macro_id']
+                    );
+                }
+            }
+        }
+
+        if (isset($cache[$hostId]['htpl'])) {
+            $stack = array_merge($cache[$hostId]['htpl'], $stack);
+        }
+    }
+
+    // clean empty macros with no macros inherited
+    foreach ($macros as $macroName => $macro) {
+        if (!isset($macro['checked']) && empty($macro['host_macro_value'])) {
+            $centreonLog->insertLog(
+                4,
+                $versionOfTheUpgrade . "host " . $cache[$srcHostId]['host_name'] . " delete macro " . $macroName
+            );
+            $db->query(
+                'DELETE FROM on_demand_macro_host WHERE host_macro_id = ' . (int) $macro['host_macro_id']
+            );
+        }
+    }
 }
