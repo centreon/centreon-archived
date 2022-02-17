f30eec850db3eb97b26f7a8c43e21579d0421aa5
Yassir <ybenboubker-ext@centreon.com>
2022-02-17 16:59:32 +0000

    optimisation de code Extension

--- www/front_src/src/Extensions/api/endpoint.ts
+++ www/front_src/src/Extensions/api/endpoint.ts
@@ -1,13 +1,56 @@
-interface Parameters {
+import { find, propEq } from 'ramda';
+
+import { SelectEntry } from '@centreon/ui';
+
+import { Criteria } from '../Filter/Criterias/models';
+
+interface Parameter {
   action: string;
   id: string;
   type: string;
 }
 
+interface ParameterWithFilter {
+  action: string;
+  criteriaStatus: Criteria | undefined;
+}
+
 const baseEndpoint = './api/internal.php?object=centreon_module&';
 
-const buildEndpoint = (parameters: Parameters): string => {
-  return `${baseEndpoint}action=${parameters.action}&id=${parameters.id}&type=${parameters.type}`;
+const buildEndPoint = ({ action, id, type }: Parameter): string => {
+  return `${baseEndpoint}action=${action}&id=${id}&type=${type}`;
 };
 
-export default buildEndpoint;
+const buildExtensionEndPoint = ({
+  action,
+  criteriaStatus,
+}: ParameterWithFilter): string => {
+  let params = `${baseEndpoint}action=${action}`;
+
+  if (!criteriaStatus || !criteriaStatus.value) {
+    return params;
+  }
+
+  const values = criteriaStatus.value as Array<SelectEntry>;
+
+  const installed = !!find(propEq('id', 'INSTALLED'), values);
+  const notInstalled = !!find(propEq('id', 'NOTINSTALLED'), values);
+  const updated = !!find(propEq('id', 'UPDATED'), values);
+  const outdated = !!find(propEq('id', 'OUTDATED'), values);
+
+  if (!updated && outdated) {
+    params += '&updated=false';
+  } else if (updated && !outdated) {
+    params += '&updated=true';
+  }
+
+  if (!installed && notInstalled) {
+    params += '&installed=false';
+  } else if (installed && !notInstalled) {
+    params += '&installed=true';
+  }
+
+  return params;
+};
+
+export { buildEndPoint, buildExtensionEndPoint };
