19e343d9679fb5ba8fa19dead8b93ca7cc502acd
Tom Darneix <tomdar87@outlook.com>
2022-02-18 20:43:19 +0100

    Fix UTC timezone (#10748)

--- www/front_src/src/Resources/useDateTimePickerAdapter.ts
+++ www/front_src/src/Resources/useDateTimePickerAdapter.ts
@@ -1,7 +1,7 @@
 /* eslint-disable class-methods-use-this */
 import DayjsAdapter from '@date-io/dayjs';
 import dayjs from 'dayjs';
-import { includes } from 'ramda';
+import { equals, includes } from 'ramda';
 
 import { useUserContext } from '@centreon/ui-context';
 import { useLocaleDateTimeFormat } from '@centreon/ui';
@@ -17,6 +17,23 @@ const useDateTimePickerAdapter = (): UseDateTimePickerAdapterProps => {
   const { locale, timezone } = useUserContext();
   const { format, toTime } = useLocaleDateTimeFormat();
 
+  const getLocalAndConfiguredTimezoneOffset = (): number => {
+    const now = new Date();
+    const currentTimezoneDate = new Date(now.toLocaleString('en-US'));
+    const destinationTimezoneDate = new Date(
+      now.toLocaleString('en-US', {
+        timeZone: timezone,
+      }),
+    );
+
+    return (
+      (currentTimezoneDate.getTime() - destinationTimezoneDate.getTime()) /
+      60 /
+      60 /
+      1000
+    );
+  };
+
   class Adapter extends DayjsAdapter {
     public format(date, formatString): string {
       return format({ date, formatString });
@@ -24,10 +41,6 @@ const useDateTimePickerAdapter = (): UseDateTimePickerAdapterProps => {
 
     public date(value): dayjs.Dayjs {
       return dayjs(value).locale(locale);
-    }
-
-    public startOfMonth(date: dayjs.Dayjs): dayjs.Dayjs {
-      return dayjs(date.tz()).startOf('month');
     }
 
     public isEqual = (value, comparing): boolean => {
@@ -42,9 +55,81 @@ const useDateTimePickerAdapter = (): UseDateTimePickerAdapterProps => {
       return date.locale(locale).tz(timezone).hour();
     }
 
-    public setHours(date: dayjs.Dayjs, count: number): dayjs.Dayjs {
-      return date.locale(locale).tz(timezone).set('hour', count);
-    }
+    public setHours = (date: dayjs.Dayjs, count: number): dayjs.Dayjs => {
+      if (equals(timezone, 'UTC')) {
+        return date
+          .tz(timezone)
+          .set('hour', count - getLocalAndConfiguredTimezoneOffset());
+      }
+
+      return date.tz(timezone).set('hour', count);
+    };
+
+    public isSameHour = (
+      date: dayjs.Dayjs,
+      comparing: dayjs.Dayjs,
+    ): boolean => {
+      return date.tz(timezone).isSame(comparing.tz(timezone), 'hour');
+    };
+
+    public isSameDay = (date: dayjs.Dayjs, comparing: dayjs.Dayjs): boolean => {
+      return date.tz(timezone).isSame(comparing.tz(timezone), 'day');
+    };
+
+    public startOfDay = (date: dayjs.Dayjs): dayjs.Dayjs => {
+      return date.tz(timezone).startOf('day') as dayjs.Dayjs;
+    };
+
+    public endOfDay = (date: dayjs.Dayjs): dayjs.Dayjs => {
+      return date.tz(timezone).startOf('day') as dayjs.Dayjs;
+    };
+
+    public startOfMonth = (date: dayjs.Dayjs): dayjs.Dayjs => {
+      return date.tz(timezone).endOf('month') as dayjs.Dayjs;
+    };
+
+    public endOfMonth = (date: dayjs.Dayjs): dayjs.Dayjs => {
+      return date.tz(timezone).endOf('month') as dayjs.Dayjs;
+    };
+
+    public isSameMonth = (
+      date: dayjs.Dayjs,
+      comparing: dayjs.Dayjs,
+    ): boolean => {
+      return date.tz(timezone).isSame(comparing.tz(timezone), 'month');
+    };
+
+    public getMonth = (date: dayjs.Dayjs): number => {
+      return date.tz(timezone).month();
+    };
+
+    public getWeekdays = (): Array<string> => {
+      const start = dayjs().locale(locale).tz(timezone).startOf('week');
+
+      return [0, 1, 2, 3, 4, 5, 6].map((diff) =>
+        this.format(start.add(diff, 'day'), 'dd'),
+      );
+    };
+
+    public mergeDateAndTime = (
+      date: dayjs.Dayjs,
+      time: dayjs.Dayjs,
+    ): dayjs.Dayjs => {
+      const dateWithTimezone = date.tz(timezone);
+      const timeWithTimezone = time.tz(timezone);
+
+      if (equals(timezone, 'UTC')) {
+        return dateWithTimezone
+          .add(timeWithTimezone.hour(), 'hour')
+          .add(timeWithTimezone.minute(), 'minute')
+          .add(timeWithTimezone.second(), 'second');
+      }
+
+      return dateWithTimezone
+        .hour(timeWithTimezone.hour())
+        .minute(timeWithTimezone.minute())
+        .second(timeWithTimezone.second()) as dayjs.Dayjs;
+    };
   }
 
   const isMeridianFormat = (date: Date): boolean => {
