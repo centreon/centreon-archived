bb460760e9bbb946ea0a87d6a925001384c63da0
Yassir <ybenboubker-ext@centreon.com>
2022-02-22 09:03:37 +0000

    add the Criterias Query to Filter of Extension.

---
+++ www/front_src/src/Extensions/Filter/filterAtoms.ts
@@ -0,0 +1,76 @@
+import { atom } from 'jotai';
+import { atomWithDefault } from 'jotai/utils';
+import { findIndex, lensPath, propEq, set as update } from 'ramda';
+
+import { Criteria } from './Criterias/models';
+import getDefaultCriterias from './Criterias/default';
+import { build, parse } from './Criterias/searchQueryLanguage';
+
+export const getFilterDefaultCriteriasDerivedAtom = atom(
+  () => (): Array<Criteria> => {
+    return getDefaultCriterias();
+  },
+);
+export const currentFilterCriteriasAtom = atomWithDefault<Array<Criteria>>(
+  (get) => get(getFilterDefaultCriteriasDerivedAtom)(),
+);
+
+export const appliedFilterCriteriasAtom = atomWithDefault<Array<Criteria>>(
+  (get) => get(getFilterDefaultCriteriasDerivedAtom)(),
+);
+
+export const searchAtom = atom('');
+export const sendingFilterAtom = atom(false);
+
+export const filterWithParsedSearchDerivedAtom = atom((get) => {
+  return parse(get(searchAtom));
+});
+
+export const getUpdatedFilterCriteriaDerivedAtom = atom(
+  (get) =>
+    ({ name, value }): Array<Criteria> => {
+      const index = findIndex(propEq('name', name))(
+        get(filterWithParsedSearchDerivedAtom),
+      );
+
+      const lens = lensPath([index, 'value']);
+
+      return update(lens, value, get(filterWithParsedSearchDerivedAtom));
+    },
+);
+
+export const applyFilterDerivedAtom = atom(
+  null,
+  (get, set, criterias: Array<Criteria>) => {
+    set(currentFilterCriteriasAtom, criterias);
+    set(appliedFilterCriteriasAtom, criterias);
+    set(searchAtom, build(criterias));
+  },
+);
+
+export const setFilterCriteriaDerivedAtom = atom(
+  null,
+  (get, set, { name, value, apply = false }) => {
+    const getUpdatedFilterCriteria = get(getUpdatedFilterCriteriaDerivedAtom);
+
+    const updatedFilter = getUpdatedFilterCriteria({ name, value });
+
+    set(searchAtom, build(updatedFilter));
+
+    if (apply) {
+      set(applyFilterDerivedAtom, updatedFilter);
+
+      return;
+    }
+
+    set(currentFilterCriteriasAtom, updatedFilter);
+  },
+);
+
+export const applyCurrentFilterDerivedAtom = atom(null, (get, set) => {
+  set(applyFilterDerivedAtom, get(filterWithParsedSearchDerivedAtom));
+});
+
+export const clearFilterDerivedAtom = atom(null, (_, set) => {
+  set(applyFilterDerivedAtom, getDefaultCriterias());
+});
