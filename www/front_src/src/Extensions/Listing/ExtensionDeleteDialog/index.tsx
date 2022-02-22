81f1a5e47fff1901b86c93568837f03721042e82
Yassir <ybenboubker-ext@centreon.com>
2022-02-22 10:07:55 +0000

    Extension add Listing

---
+++ www/front_src/src/Extensions/Listing/ExtensionDeleteDialog/index.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/prefer-stateless-function */
+
+import React from 'react';
+
+import { Typography, Alert } from '@mui/material';
+
+import { Dialog } from '@centreon/ui';
+
+const ExtensionDeletePopup = ({
+  deletingEntity,
+  onConfirm,
+  onCancel,
+}): JSX.Element => {
+  const confirmDelete = (): void => {
+    onConfirm(deletingEntity.id, deletingEntity.type);
+  };
+
+  return (
+    <Dialog
+      open
+      labelConfirm="Delete"
+      onCancel={onCancel}
+      onClose={onCancel}
+      onConfirm={confirmDelete}
+    >
+      <Typography variant="h6">{deletingEntity.description}</Typography>
+
+      <Alert severity="warning">
+        Do you want to delete this extension? This action will remove all
+        associated data.
+      </Alert>
+    </Dialog>
+  );
+};
+
+export default ExtensionDeletePopup;
