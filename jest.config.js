be37e851297d7526fd076b0fd660b851c1b2d8a3
Tom Darneix <tomdar87@outlook.com>
2022-02-22 15:40:45 +0000

    Adapt jest config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,6 @@ module.exports = mergeDeepRight(
   require('centreon-frontend/packages/frontend-config/jest'),
   {
     moduleNameMapper: {
-      '\\.(s?css|png|svg|jpg)$': 'identity-obj-proxy',
       'd3-array': '<rootDir>/node_modules/d3-array/dist/d3-array.min.js',
     },
     roots: ['<rootDir>/www/front_src/src/'],
