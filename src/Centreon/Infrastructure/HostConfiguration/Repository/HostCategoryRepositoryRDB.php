0f6fe9d7b484f802669ef4633301d47cefa30422
alaunois <alaunois@centreon.com>
2022-02-23 15:19:41 +0100

    enh(conf/host) handle groups/categories/templates/severity in host update (#10759)
    * enh(conf/host) Updating the host updates the host groups (#10610)
    
    * enh(host/macro): Updating the host updates the macros (#10677)
    
    * enh(host) handle templates in host update (#10603)
    
    * enh(conf/host) Updating the host updates the host groups (#10610)
    
    * enh(conf/host) Updating the host updates the host groups (#10610)
    
    * enh(host) handle templates in host update
    
    * update methods names
    
    * update methods names
    
    * remodel host template update
    
    * clean code
    
    * fix doc blocks
    
    * fix doc blocks
    
    * remove useless method, add translation
    
    * updating error messages
    
    * fix phpstan
    
    * fix phpstan
    
    * fix missing execute
    
    * fix rebase error
    
    Co-authored-by: Laurent Calvet <lcalvet@centreon.com>
    
    * enh(host) handle host categories in host update (#10650)
    
    * enh(hostdisco) handle hostcategories in host update
    
    * enh(host) handle host categories in host update
    
    * remove comments
    
    * fix doc blocks
    
    * modify method name
    
    * exclude host severities from host categories requests
    
    * Apply suggestion
    
    Co-authored-by: Laurent Calvet <lcalvet@centreon.com>
    
    * enh(host) handle host severity in host update (#10681)
    
    * enh(host) handle host severity in host update
    
    * fix host severity in host configuration model
    
    * fix severity on host add and update
    
    * Apply suggestion
    
    * remove host severity check before deletion
    
    * add findByHost in host severity
    
    Co-authored-by: Laurent Calvet <lcalvet@centreon.com>

--- src/Centreon/Infrastructure/HostConfiguration/Repository/HostCategoryRepositoryRDB.php
+++ src/Centreon/Infrastructure/HostConfiguration/Repository/HostCategoryRepositoryRDB.php
@@ -28,6 +28,7 @@ use Centreon\Domain\HostConfiguration\Exception\HostCategoryException;
 use Centreon\Domain\HostConfiguration\Interfaces\HostCategory\HostCategoryReadRepositoryInterface;
 use Centreon\Domain\HostConfiguration\Interfaces\HostCategory\HostCategoryWriteRepositoryInterface;
 use Centreon\Domain\HostConfiguration\Model\HostCategory;
+use Centreon\Domain\HostConfiguration\Host;
 use Centreon\Domain\RequestParameters\RequestParameters;
 use Centreon\Infrastructure\DatabaseConnection;
 use Centreon\Infrastructure\HostConfiguration\Repository\Model\HostCategoryFactoryRdb;
@@ -365,5 +366,30 @@ class HostCategoryRepositoryRDB extends AbstractRepositoryDRB implements
             return HostCategoryFactoryRdb::create($result);
         }
         return null;
+    }
+
+    /**
+     * Find HostCategories by host id
+     *
+     * @param Host $host
+     * @return HostCategory[]
+     */
+    public function findAllByHost(Host $host): array
+    {
+        $request = $this->translateDbName(
+            'SELECT * FROM `:db`.hostcategories hc
+            JOIN `:db`.hostcategories_relation hc_rel ON hc.hc_id = hc_rel.hostcategories_hc_id
+            WHERE hc_rel.host_host_id = :host_id
+            AND hc.level IS NULL'
+        );
+        $statement = $this->db->prepare($request);
+        $statement->bindValue(':host_id', $host->getId(), \PDO::PARAM_INT);
+        $statement->execute();
+
+        $hostCategories = [];
+        while (($record = $statement->fetch(\PDO::FETCH_ASSOC)) !== false) {
+            $hostCategories[] = HostCategoryFactoryRdb::create($record);
+        }
+        return $hostCategories;
     }
 }
