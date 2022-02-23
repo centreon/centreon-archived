da5f0dba12a8c3b40fd74d7ab2489a94b7ca1507
Kevin Duret <kduret@centreon.com>
2022-02-23 18:00:24 +0100

    enh(api): get password remaining time in legacy user endpoint

--- www/class/centreonContact.class.php
+++ www/class/centreonContact.class.php
@@ -388,6 +388,32 @@ class CentreonContact
     }
 
     /**
+     * Find last password creation date by contact id
+     *
+     * @param int $contactId
+     * @return \DateTimeImmutable|null
+     */
+    public function findLastPasswordCreationDate(int $contactId): ?\DateTimeImmutable
+    {
+        $creationDate = null;
+
+        $statement = $this->db->prepare(
+            "SELECT creation_date
+            FROM contact_password
+            WHERE contact_id = :contactId
+            ORDER BY creation_date DESC LIMIT 1"
+        );
+        $statement->bindValue(':contactId', $contactId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        if ($row = $statement->fetch()) {
+            $creationDate = (new \DateTimeImmutable())->setTimestamp((int) $row['creation_date']);
+        }
+
+        return $creationDate;
+    }
+
+    /**
      * Check if a user password respects configured policy when updated (delay, reuse)
      *
      * @param array<string,mixed> $passwordPolicy
@@ -398,16 +424,11 @@ class CentreonContact
      */
     private function respectPasswordChangePolicyOrFail(array $passwordPolicy, string $password, int $contactId): void
     {
-        $statement = $this->db->prepare(
-            "SELECT creation_date FROM contact_password "
-            . "WHERE contact_id = :contactId "
-            . "ORDER BY creation_date DESC LIMIT 1"
-        );
-        $statement->bindValue(':contactId', $contactId, \PDO::PARAM_INT);
-        $statement->execute();
-        if ($passwordCreationDate = $statement->fetchColumn()) {
+        $passwordCreationDate = $this->findLastPasswordCreationDate($contactId);
+
+        if ($passwordCreationDate !== null) {
             $delayBeforeNewPassword = (int) $passwordPolicy['delay_before_new_password'];
-            $isPasswordCanBeChanged = (int) $passwordCreationDate + $delayBeforeNewPassword < time();
+            $isPasswordCanBeChanged = $passwordCreationDate->getTimestamp() + $delayBeforeNewPassword < time();
             if (!$isPasswordCanBeChanged) {
                 throw new \Exception(
                     _("You can't change your password because the delay before changing password is not over.")
