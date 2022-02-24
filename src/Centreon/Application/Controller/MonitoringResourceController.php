2887ffdb6471f6a261cc0e7ea835076d379c3eb2
Kevin Duret <kduret@centreon.com>
2022-02-24 11:27:42 +0100

    fix(kb): fix kb links in resource status (#10781)
    Refs: MON-12517

--- src/Centreon/Application/Controller/MonitoringResourceController.php
+++ src/Centreon/Application/Controller/MonitoringResourceController.php
@@ -967,14 +967,12 @@ class MonitoringResourceController extends AbstractController
     /**
      * Endpoint: Replaces macros in the URL provided and redirect to it.
      *
-     * @param Request $request
      * @param int $hostId
      * @param int|null $serviceId
      * @param string $urlType
      * @return View
      */
     public function redirectResourceExternalLink(
-        Request $request,
         int $hostId,
         ?int $serviceId,
         string $urlType
@@ -1008,6 +1006,9 @@ class MonitoringResourceController extends AbstractController
                 return View::create(null, Response::HTTP_NOT_FOUND, []);
             }
         }
+
+        // replace relative url by absolute url
+        $redirectUrl = preg_replace('/^(\.\/)/', $this->getBaseUri() . '/', $redirectUrl);
 
         return View::createRedirect($redirectUrl);
     }
