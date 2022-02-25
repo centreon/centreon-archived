e99c44785b37280c6d678a49980b39989509952d
Jérémy Jaouen <jjaouen@centreon.com>
2022-02-25 13:03:13 +0100

    remove coma

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -1404,4 +1404,4 @@ VALUES (
   '{"trusted_client_addresses":[],"blacklist_client_addresses":[],"base_url":null,"authorization_endpoint":null,"token_endpoint":null,"introspection_token_endpoint":null,"userinfo_endpoint":null,"endsession_endpoint":null,"connection_scopes":[],"login_claim":null,"client_id":null,"client_secret":null,"authentication_type":"client_secret_post","verify_peer":true}',
   false,
   false
-),;
+);
