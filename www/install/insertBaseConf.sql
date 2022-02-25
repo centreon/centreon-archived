3212955706e2fe5c14a3604280d5c168ced01696
Jérémy Jaouen <jjaouen@centreon.com>
2022-02-25 11:25:21 +0100

    fresh install

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -1397,4 +1397,11 @@ VALUES (
   '{"password_security_policy": {"password_length": 12, "has_uppercase_characters": true, "has_lowercase_characters": true, "has_numbers": true, "has_special_characters": true, "attempts": 5, "blocking_duration": 900, "password_expiration_delay": 7776000, "delay_before_new_password": 3600, "can_reuse_passwords": false }}',
   true,
   true
-);
+),
+(
+  'openid',
+  'openid',
+  '{}',
+  false,
+  false
+),;
