975bad62bb86f9671a902a9c08dfae01c5ee8b2e
jcaro <jcaro@centreon.com>
2022-02-25 10:02:19 +0100

    enh(UI): PENDO - Add a unique identifier for css objects (#10668)

--- www/front_src/src/Resources/Graph/Performance/TimePeriods/index.tsx
+++ www/front_src/src/Resources/Graph/Performance/TimePeriods/index.tsx
@@ -109,6 +109,7 @@ const TimePeriodButtonGroup = ({
                       <Button
                         className={classes.button}
                         component="span"
+                        data-testid={largeName}
                         variant={
                           selectedTimePeriod?.id === id
                             ? 'contained'
