2a53aaeb1656ea2cad0ab5dfc0ac1e58c8b4871d
Yassir <ybenboubker-ext@centreon.com>
2022-03-02 16:27:49 +0000

    update serverConfigurationWizard on deleting Form-redux

--- www/front_src/src/PollerWizard/serverConfigurationWizard/index.tsx
+++ www/front_src/src/PollerWizard/serverConfigurationWizard/index.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 
-import { equals, isNil } from 'ramda';
+import { useTranslation } from 'react-i18next';
+import { equals } from 'ramda';
 
-import Form from '../forms/ServerConfigurationWizardForm';
+import { Button, Typography } from '@mui/material';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import Radio from '@mui/material/Radio';
+
+import {
+  labelAddACentreonPoller,
+  labelAddACentreonRemoteServer,
+} from '../translatedLabels';
 import { ServerType } from '../models';
+import { useStyles } from '../../styles/partials/form/PollerWizardStyle';
 
 interface Props {
   changeServerType: (type: ServerType) => void;
@@ -14,22 +25,64 @@ const ServerConfigurationWizard = ({
   changeServerType,
   goToNextStep,
 }: Props): JSX.Element => {
-  const handleSubmit = ({ server_type }): void => {
-    if (isNil(server_type)) {
-      return;
-    }
+  const classes = useStyles();
+  const { t } = useTranslation();
 
-    if (equals(server_type, '1')) {
+  const [serverType, setServerType] = React.useState<number>(1);
+
+  const handleSubmit = (): void => {
+    if (equals(serverType, 1)) {
       changeServerType(ServerType.Remote);
     }
-    if (equals(server_type, '2')) {
+    if (equals(serverType, 2)) {
       changeServerType(ServerType.Poller);
     }
 
     goToNextStep();
   };
 
-  return <Form onSubmit={handleSubmit} />;
+  const configurationTypes = [
+    {
+      label: labelAddACentreonRemoteServer,
+      value: 1,
+    },
+    {
+      label: labelAddACentreonPoller,
+      value: 2,
+    },
+  ];
+
+  return (
+    <div>
+      <div className={classes.formHeading}>
+        <Typography variant="h6">{t('Choose a server type')}</Typography>
+      </div>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <RadioGroup defaultValue="1" name="server_type">
+          {configurationTypes.map((type) => (
+            <FormControlLabel
+              checked={serverType === type.value}
+              control={<Radio color="primary" size="small" />}
+              key={type.value}
+              label={type.label}
+              value={type.value}
+              onClick={(): void => setServerType(type.value)}
+            />
+          ))}
+        </RadioGroup>
+        <div className={classes.formButton}>
+          <Button
+            color="primary"
+            size="small"
+            type="submit"
+            variant="contained"
+          >
+            {t('Next')}
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ServerConfigurationWizard;
