5233943de0e82d8b54c7178b41a1721e5c6448ac
Ayoub TAGHZOUT <ayoub.taghzout@goandev.net>
2022-03-02 14:48:52 +0100

    Fix PR change request

--- www/include/home/customViews/layouts/widgetScript.js
+++ www/include/home/customViews/layouts/widgetScript.js
@@ -61,7 +61,7 @@ jQuery(function () {
         jQuery(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
             .find(".portlet-header")
             .addClass("ui-widget-header ui-corner-all")
-            .prepend('\n' +
+            .prepend(
                 '<svg xmlns="http://www.w3.org/2000/svg" class="ui-icon ui-icon-refresh" height="18px" ' +
                 'viewBox="0 0 24 24" width="18px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/>' +
                 '<path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79' +
