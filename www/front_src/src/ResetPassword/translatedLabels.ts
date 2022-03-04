c6ae1820e3c2bbc5cd181a56c38f38fc0389e25d
Tom Darneix <tomdar87@outlook.com>
2022-03-04 12:12:11 +0100

    feat(UI): Renew password when expired (#10808)
    * feat(api): doc on update password endpoint
    
    * fix(conf) fix contact update when user is not admin (#10695)
    
    * enh(API): Interfaces for renew password use case (#10728)
    
    * fix present method signature
    
    * enh(api): implements infrastructure for renew password use case (#10736)
    
    * enh(api): finish implementation for renew password use case (#10752)
    
    * enh(backend): Refactor configuration security policy (#10756)
    
    * feat(login): apply blocking and expiration policy on login (#10770)
    
    Refs: MON-12420
    
    * enh(api): get password remaining time in legacy user endpoint (#10782)
    
    Refs: MON-12519
    
    * enh(Authentication): Add page to renew password (#10724)
    
    * Add basic code to manage password expiration status
    
    * Implement page to reset the password
    
    * Update centreon-frontend to specific branch
    
    * Adapt User context props
    
    * Adapt user context props
    
    * Add a divider between current password and new passwords
    
    * Remove debug url
    
    * Adds tests
    
    * Display warning when the password is about to expire
    
    * Load user when password is renewed
    
    * Revert some changes and add message about password expiration
    
    * Remove unused import
    
    * Remove prop for user context
    
    * Rename variables
    
    * Add translations
    
    * Fix test
    
    * Fix critical error
    
    * Fix password expiration display
    
    * Update www/front_src/src/Login/index.test.tsx
    
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>
    
    * Add login endpoint call when resetting password
    
    * Fix centreon logo path
    
    * set cookie header manually
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    Co-authored-by: alaunois <alaunois@centreon.com>
    Co-authored-by: Jérémy Jaouen <jjaouen@centreon.com>
    Co-authored-by: jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>

---
+++ www/front_src/src/ResetPassword/translatedLabels.ts
@@ -0,0 +1,10 @@
+export const labelResetYourPassword = 'Reset your password';
+export const labelRequired = 'Required';
+export const labelCurrentPassword = 'Current password';
+export const labelNewPassword = 'New password';
+export const labelNewPasswordConfirmation = 'New password confirmation';
+export const labelNewPasswordsMustMatch = 'New passwords must match';
+export const labelTheNewPasswordIstheSameAsTheOldPassword =
+  'The new password is the same as the old password';
+export const labelResetPassword = 'Reset password';
+export const labelPasswordRenewed = 'Password renewed';
