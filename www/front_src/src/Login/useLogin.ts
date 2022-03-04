c1e9505f2bf2079af3dac0366dda975f21b06703
Tom Darneix <tomdar87@outlook.com>
2022-03-04 12:12:11 +0100

    feat(UI): Renew password when expired (#10808)
    * feat(api): doc on update password endpoint
    
    * fix(conf) fix contact update when user is not admin (#10695)
    
    * enh(API): Interfaces for renew password use case (#10728)
    
    * fix present method signature
    
    * enh(api): implements infrastructure for renew password use case (#10736)
    
    * enh(api): finish implementation for renew password use case (#10752)
    
    * enh(backend): Refactor configuration security policy (#10756)
    
    * feat(login): apply blocking and expiration policy on login (#10770)
    
    Refs: MON-12420
    
    * enh(api): get password remaining time in legacy user endpoint (#10782)
    
    Refs: MON-12519
    
    * enh(Authentication): Add page to renew password (#10724)
    
    * Add basic code to manage password expiration status
    
    * Implement page to reset the password
    
    * Update centreon-frontend to specific branch
    
    * Adapt User context props
    
    * Adapt user context props
    
    * Add a divider between current password and new passwords
    
    * Remove debug url
    
    * Adds tests
    
    * Display warning when the password is about to expire
    
    * Load user when password is renewed
    
    * Revert some changes and add message about password expiration
    
    * Remove unused import
    
    * Remove prop for user context
    
    * Rename variables
    
    * Add translations
    
    * Fix test
    
    * Fix critical error
    
    * Fix password expiration display
    
    * Update www/front_src/src/Login/index.test.tsx
    
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>
    
    * Add login endpoint call when resetting password
    
    * Fix centreon logo path
    
    * set cookie header manually
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    Co-authored-by: alaunois <alaunois@centreon.com>
    Co-authored-by: Jérémy Jaouen <jjaouen@centreon.com>
    Co-authored-by: jeremyjaouen <61694165+jeremyjaouen@users.noreply.github.com>
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>

--- www/front_src/src/Login/useLogin.ts
+++ www/front_src/src/Login/useLogin.ts
@@ -4,22 +4,35 @@ import { useNavigate } from 'react-router-dom';
 import { FormikHelpers, FormikValues } from 'formik';
 import { useAtom } from 'jotai';
 import { useTranslation } from 'react-i18next';
+import { not, path, pathEq } from 'ramda';
+import { useUpdateAtom } from 'jotai/utils';
 
 import { useRequest, useSnackbar, getData } from '@centreon/ui';
 
 import { PlatformInstallationStatus } from '../api/models';
 import { platformInstallationStatusAtom } from '../platformInstallationStatusAtom';
 import useUser from '../Main/useUser';
+import { passwordResetInformationsAtom } from '../ResetPassword/passwordResetInformationsAtom';
+import routeMap from '../reactRoutes/routeMap';
 
 import postLogin from './api';
 import { platformVersionsDecoder, redirectDecoder } from './api/decoder';
-import { LoginFormValues, PlatformVersions, Redirect } from './models';
-import { labelLoginSucceeded } from './translatedLabels';
+import {
+  LoginFormValues,
+  PlatformVersions,
+  Redirect,
+  RedirectAPI,
+} from './models';
+import {
+  labelLoginSucceeded,
+  labelPasswordHasExpired,
+} from './translatedLabels';
 import { platformVersionsEndpoint } from './api/endpoint';
 
 interface UseLoginState {
   platformInstallationStatus: PlatformInstallationStatus | null;
   platformVersions: PlatformVersions | null;
+  sendLogin: (values) => Promise<Redirect>;
   submitLoginForm: (
     values: LoginFormValues,
     { setSubmitting }: Pick<FormikHelpers<FormikValues>, 'setSubmitting'>,
@@ -33,6 +46,7 @@ const useLogin = (): UseLoginState => {
 
   const { sendRequest: sendLogin } = useRequest<Redirect>({
     decoder: redirectDecoder,
+    httpCodesBypassErrorSnackbar: [401],
     request: postLogin,
   });
 
@@ -41,11 +55,42 @@ const useLogin = (): UseLoginState => {
     request: getData,
   });
 
-  const { showSuccessMessage } = useSnackbar();
+  const { showSuccessMessage, showWarningMessage, showErrorMessage } =
+    useSnackbar();
   const navigate = useNavigate();
   const loadUser = useUser(i18n.changeLanguage);
 
   const [platformInstallationStatus] = useAtom(platformInstallationStatusAtom);
+  const setPasswordResetInformations = useUpdateAtom(
+    passwordResetInformationsAtom,
+  );
+
+  const checkPasswordExpiration = React.useCallback(
+    ({ error, alias, setSubmitting }) => {
+      const isUserNotAllowed = pathEq(['response', 'status'], 401, error);
+
+      const { password_is_expired: passwordIsExpired } = path(
+        ['response', 'data'],
+        error,
+      ) as RedirectAPI;
+
+      if (isUserNotAllowed && not(passwordIsExpired)) {
+        setSubmitting(false);
+        showErrorMessage(
+          path(['response', 'data', 'message'], error) as string,
+        );
+
+        return;
+      }
+
+      setPasswordResetInformations({
+        alias,
+      });
+      navigate(routeMap.resetPassword);
+      showWarningMessage(t(labelPasswordHasExpired));
+    },
+    [],
+  );
 
   const submitLoginForm = (
     values: LoginFormValues,
@@ -59,10 +104,9 @@ const useLogin = (): UseLoginState => {
         showSuccessMessage(t(labelLoginSucceeded));
         loadUser()?.then(() => navigate(redirectUri));
       })
-      .catch(() => undefined)
-      .finally(() => {
-        setSubmitting(false);
-      });
+      .catch((error) =>
+        checkPasswordExpiration({ alias: values.alias, error, setSubmitting }),
+      );
   };
 
   const getBrowserLocale = (): string => navigator.language.slice(0, 2);
@@ -77,6 +121,7 @@ const useLogin = (): UseLoginState => {
   return {
     platformInstallationStatus,
     platformVersions,
+    sendLogin,
     submitLoginForm,
   };
 };
---
+++ www/front_src/src/ResetPassword/index.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+
+import { useTranslation } from 'react-i18next';
+import { useAtomValue } from 'jotai/utils';
+import { isNil, not } from 'ramda';
+import { useNavigate } from 'react-router';
+import { Formik } from 'formik';
+
+import { Paper, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+import routeMap from '../reactRoutes/routeMap';
+import Logo from '../Login/Logo';
+
+import { passwordResetInformationsAtom } from './passwordResetInformationsAtom';
+import { labelResetYourPassword } from './translatedLabels';
+import { ResetPasswordValues } from './models';
+import useResetPassword from './useResetPassword';
+import Form from './Form';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    alignItems: 'center',
+    backgroundColor: theme.palette.background.default,
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+    justifyContent: 'center',
+    rowGap: theme.spacing(2),
+    width: '100vw',
+  },
+  paper: {
+    padding: theme.spacing(4, 3),
+  },
+}));
+
+const initialValues = {
+  newPassword: '',
+  newPasswordConfirmation: '',
+  oldPassword: '',
+};
+
+const ResetPassword = (): JSX.Element | null => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const passwordResetInformations = useAtomValue(passwordResetInformationsAtom);
+
+  const { submitResetPassword, validationSchema } = useResetPassword();
+
+  React.useEffect(() => {
+    if (
+      not(isNil(passwordResetInformations)) &&
+      passwordResetInformations?.alias
+    ) {
+      return;
+    }
+
+    navigate(routeMap.login);
+  }, [passwordResetInformations]);
+
+  if (
+    isNil(passwordResetInformations) ||
+    not(passwordResetInformations?.alias)
+  ) {
+    return null;
+  }
+
+  return (
+    <div className={classes.container}>
+      <Logo />
+      <Paper className={classes.paper}>
+        <Typography variant="h4">{t(labelResetYourPassword)}</Typography>
+        <Formik<ResetPasswordValues>
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={submitResetPassword}
+        >
+          <Form />
+        </Formik>
+      </Paper>
+    </div>
+  );
+};
+
+export default ResetPassword;
