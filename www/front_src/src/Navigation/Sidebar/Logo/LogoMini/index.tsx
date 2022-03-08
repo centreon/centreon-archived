67ce80258c4ff8abcf25bc98d14cb92b7012c262
nouha <nelabrouki-ext@centreon.com>
2022-03-08 16:05:57 +0000

    adapt logo for menu

---
+++ www/front_src/src/Navigation/Sidebar/Logo/LogoMini/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { useAtomValue } from 'jotai/utils';
+import { equals } from 'ramda';
+
+import { ThemeMode, userAtom } from '@centreon/ui-context';
+
+import miniLogoDark from './Centreon_Logo_Noir_RVB_C.svg';
+import miniLogoLight from './Centreon_Logo_Blanc_C.svg';
+
+interface Props {
+  onClick?: () => void;
+}
+
+const MiniLogo = ({ onClick }: Props): JSX.Element => {
+  const { themeMode } = useAtomValue(userAtom);
+  const miniLogo = equals(themeMode, ThemeMode.light)
+    ? miniLogoDark
+    : miniLogoLight;
+
+  return (
+    <div aria-hidden onClick={onClick}>
+      <img alt="" height="52" src={miniLogo} width={25} />
+    </div>
+  );
+};
+
+export default MiniLogo;
---
+++ www/front_src/src/Navigation/Sidebar/Logo/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { useAtomValue } from 'jotai/utils';
+import { equals } from 'ramda';
+
+import { ThemeMode, userAtom } from '@centreon/ui-context';
+
+import logoDark from './Centreon_Logo_Noir_RVB.svg';
+import logoLight from './Centreon_Logo_Blanc.svg';
+
+interface Props {
+  onClick?: () => void;
+}
+
+const Logo = ({ onClick }: Props): JSX.Element => {
+  const { themeMode } = useAtomValue(userAtom);
+  const logo = equals(themeMode, ThemeMode.light) ? logoDark : logoLight;
+
+  return (
+    <div aria-hidden onClick={onClick}>
+      <img alt="" height="52" src={logo} width={120} />
+    </div>
+  );
+};
+
+export default Logo;
