27a2f2c5ab93b20520518b0391356a3aa27e8f1f
Tom Darneix <tomdar87@outlook.com>
2022-03-09 15:47:02 +0000

    Refactor Tabs main component

--- www/front_src/src/Authentication/index.tsx
+++ www/front_src/src/Authentication/index.tsx
@@ -5,13 +5,31 @@ import { useTranslation } from 'react-i18next';
 import { Box, Paper, Tab } from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 
-import { useMemoComponent } from '@centreon/ui';
-
 import { Provider } from './models';
 import LocalAuthentication from './Local';
 import { labelPasswordSecurityPolicy } from './Local/translatedLabels';
 import { labelOpenIDConnectConfiguration } from './Openid/translatedLabels';
 import OpenidConfiguration from './Openid';
+import WebSSOConfigurationForm from './WebSSO';
+import { labelWebSSOConfiguration } from './WebSSO/translatedLabels';
+
+const panels = [
+  {
+    Component: LocalAuthentication,
+    title: labelPasswordSecurityPolicy,
+    value: Provider.Local,
+  },
+  {
+    Component: OpenidConfiguration,
+    title: labelOpenIDConnectConfiguration,
+    value: Provider.Openid,
+  },
+  {
+    Component: WebSSOConfigurationForm,
+    title: labelWebSSOConfiguration,
+    value: Provider.WebSSO,
+  },
+];
 
 const Authentication = (): JSX.Element => {
   const { t } = useTranslation();
@@ -21,33 +39,33 @@ const Authentication = (): JSX.Element => {
     setTab(newTab);
   };
 
+  const tabs = React.useMemo(
+    () =>
+      panels.map(({ title, value }) => (
+        <Tab key={value} label={t(title)} value={value} />
+      )),
+    [],
+  );
+
+  const tabPanels = React.useMemo(
+    () =>
+      panels.map(({ Component, value }) => (
+        <TabPanel key={value} value={value}>
+          <Component />
+        </TabPanel>
+      )),
+    [],
+  );
+
   return (
     <Box>
       <TabContext value={tab}>
         <Paper>
           <TabList variant="fullWidth" onChange={changeTab}>
-            <Tab
-              label={t(labelPasswordSecurityPolicy)}
-              value={Provider.Local}
-            />
-            <Tab
-              label={t(labelOpenIDConnectConfiguration)}
-              value={Provider.Openid}
-            />
+            {tabs}
           </TabList>
         </Paper>
-        <TabPanel value={Provider.Local}>
-          {useMemoComponent({
-            Component: <LocalAuthentication />,
-            memoProps: [],
-          })}
-        </TabPanel>
-        <TabPanel value={Provider.Openid}>
-          {useMemoComponent({
-            Component: <OpenidConfiguration />,
-            memoProps: [],
-          })}
-        </TabPanel>
+        {tabPanels}
       </TabContext>
     </Box>
   );
