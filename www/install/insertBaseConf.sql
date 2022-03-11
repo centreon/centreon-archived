ba69e30e2e54c6621e28917149703bc2ff766f4f
tuntoja <58987095+tuntoja@users.noreply.github.com>
2022-03-11 14:14:28 +0100

    chore(install): update version to 21.10.5 (#10854)
    * chore(install): update version to 21.10.5
    
    * Remove last line

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.4');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.5');
 
 --
 -- Contenu de la table `contact`
