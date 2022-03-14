18d5a195b02c6e84fee77942552341f4b51f91cf
yassine <yayasssine@gmail.com>
2022-03-14 09:36:38 +0100

    indent code

--- www/include/configuration/configObject/host/listHost.php
+++ www/include/configuration/configObject/host/listHost.php
@@ -331,17 +331,22 @@ for ($i = 0; $host = $dbResult->fetch(); $i++) {
             $moptions = "<a href='main.php?p=$p&host_id={$host['host_id']}"
                 . "&o=u&limit=$limit&num=$num&searchH=$search"
                 . "&centreon_token=" . $centreonToken
-                . "'><svg xmlns='http://www.w3.org/2000/svg' class='ico-14-disabled margin_right' viewBox='0 0 22 22' >
-                <path d='M0 0h24v24H0z' fill='none'/>
-        <path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.42 0 8 3.58 8 8 0 1.85-.63 3.55-1.69 4.9z'/>
-        </svg></a>&nbsp;&nbsp;";
+                . "'>
+                        <svg xmlns='http://www.w3.org/2000/svg' class='ico-14-disabled margin_right' viewBox='0 0 22 22' >
+                            <path d='M0 0h24v24H0z' fill='none'/>
+                            <path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8 0-1.85.63-3.55 1.69-4.9L16.9 18.31C15.55 19.37 13.85 20 12 20zm6.31-3.1L7.1 5.69C8.45 4.63 10.15 4 12 4c4.42 0 8 3.58 8 8 0 1.85-.63 3.55-1.69 4.9z'/>
+                        </svg>
+                    </a>&nbsp;&nbsp;";
         } else {
             $moptions = "<a href='main.php?p=$p&host_id={$host['host_id']}"
                 . "&o=s&limit=$limit&num=$num&searchH=$search"
                 . "&centreon_token=" . $centreonToken
-                . "'><svg xmlns='http://www.w3.org/2000/svg' class='ico-14-enabled margin_right' viewBox='0 0 24 24' >
-    <path d='M0 0h24v24H0z' fill='none'/>
-    <path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/></svg></a>&nbsp;&nbsp;";
+                . "'>
+                        <svg xmlns='http://www.w3.org/2000/svg' class='ico-14-enabled margin_right' viewBox='0 0 24 24' >
+                            <path d='M0 0h24v24H0z' fill='none'/>
+                            <path d='M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z'/>
+                        </svg>
+                    </a>&nbsp;&nbsp;";
         }
 
         $moptions .= "<input onKeypress=\"if(event.keyCode > 31 && "
