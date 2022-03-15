e20e8c10516cc15afa03424dd164809e0e24bbfb
Laurent Calvet <lcalvet@centreon.com>
2022-03-15 10:53:54 +0100

    fix(ldap): Fix SQL queries (#10857)

--- www/include/Administration/parameters/ldap/list.php
+++ www/include/Administration/parameters/ldap/list.php
@@ -67,7 +67,9 @@ foreach ($list as $k => $v) {
         $enableLdap = 1;
     }
 }
-$pearDB->query("UPDATE options SET `value` = " . $enableLdap . " WHERE `key` = 'ldap_auth_enable'");
+$statement = $pearDB->prepare("UPDATE options SET `value` = :value WHERE `key` = 'ldap_auth_enable'");
+$statement->bindValue(':value', $enableLdap, PDO::PARAM_STR);
+$statement->execute();
 
 include "./include/common/checkPagination.php";
 $list = $ldapConf->getLdapConfigurationList($searchLdap, ($num * $limit), $limit);
