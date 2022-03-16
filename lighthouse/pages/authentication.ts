4e9cb20070702ce819b1ba98adb5327562dc606a
Tom Darneix <tomdar87@outlook.com>
2022-03-16 21:04:50 +0000

    Fix button selector

--- lighthouse/pages/authentication.ts
+++ lighthouse/pages/authentication.ts
@@ -4,7 +4,9 @@ export const generateReportForAuthenticationPage = async ({
   flow,
   page,
 }): Promise<void> => {
-  await flow.navigate(`${baseUrl}administration/authentication`, { stepName: 'Authentication Cold navigation' });
+  await flow.navigate(`${baseUrl}administration/authentication`, {
+    stepName: 'Authentication Cold navigation',
+  });
 
   await flow.navigate(`${baseUrl}administration/authentication`, {
     stepName: 'Authentication Warm navigation',
@@ -14,18 +16,20 @@ export const generateReportForAuthenticationPage = async ({
 
   await page.waitForSelector('input[aria-label="Password minimum length"]');
 
-  
   await flow.startTimespan({ stepName: 'Change letter case' });
-  await page.click('input[aria-label="Force to use lower case"]');
+  await page.click('button[aria-label="Force to use lower case"]');
   await flow.endTimespan();
-  
-  await page.click('input[aria-label="Force to use lower case"]');
-  
+
+  await page.click('button[aria-label="Force to use lower case"]');
+
   await flow.startTimespan({ stepName: 'Change tab' });
   await page.click('button[role="tab"]:nth-child(2)');
   await flow.endTimespan();
 
   await flow.startTimespan({ stepName: 'Type base URL' });
-  await page.type('input[aria-label="Password minimum length"]', 'http://localhost/');
+  await page.type(
+    'input[aria-label="Password minimum length"]',
+    'http://localhost/',
+  );
   await flow.endTimespan();
 };
