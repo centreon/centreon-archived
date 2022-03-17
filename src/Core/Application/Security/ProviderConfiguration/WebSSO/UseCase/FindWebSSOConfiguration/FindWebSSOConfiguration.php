518e24f8c9b954eab511607b8aa32a1a9b7b47ba
Jérémy Jaouen <jjaouen@centreon.com>
2022-03-17 14:39:29 +0100

    enh(api): add interfaces for get WebSSO configuration use case

---
+++ src/Core/Application/Security/ProviderConfiguration/WebSSO/UseCase/FindWebSSOConfiguration/FindWebSSOConfiguration.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Application\Security\ProviderConfiguration\WebSSO\UseCase\FindWebSSOConfiguration;
+
+use Core\Application\Common\UseCase\ErrorResponse;
+use Core\Application\Common\UseCase\NotFoundResponse;
+use Core\Domain\Security\ProviderConfiguration\WebSSO\Model\WebSSOConfiguration;
+use Core\Application\Security\ProviderConfiguration\WebSSO\Repository\ReadWebSSOConfigurationRepositoryInterface;
+
+class FindWebSSOConfiguration
+{
+    /**
+     * @param ReadWebSSOConfigurationRepositoryInterface $repository
+     */
+    public function __construct(private ReadWebSSOConfigurationRepositoryInterface $repository)
+    {
+    }
+
+    /**
+     * @param FindWebSSOConfigurationPresenterInterface $presenter
+     * @return void
+     */
+    public function __invoke(FindWebSSOConfigurationPresenterInterface $presenter): void
+    {
+        try {
+            $configuration = $this->repository->findConfiguration();
+        } catch (\Throwable $ex) {
+            $presenter->setResponseStatus(new ErrorResponse($ex->getMessage()));
+            return;
+        }
+
+        if ($configuration === null) {
+            $presenter->setResponseStatus(new NotFoundResponse('WebSSOConfiguration'));
+            return;
+        }
+
+        $presenter->present($this->createResponse($configuration));
+    }
+
+    /**
+     * @param WebSSOConfiguration $configuration
+     * @return FindWebSSOConfigurationResponse
+     */
+    private function createResponse(WebSSOConfiguration $configuration): FindWebSSOConfigurationResponse
+    {
+        return new FindWebSSOConfigurationResponse();
+    }
+}
---
+++ tests/php/Core/Application/Security/ProviderConfiguration/WebSSO/UseCase/FindWebSSOConfiguration/FindWebSSOConfigurationTest.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Core\Application\Security\ProviderConfiguration\WebSSO\UseCase\FindWebSSOConfiguration;
+
+use Core\Application\Common\UseCase\ErrorResponse;
+use Core\Application\Common\UseCase\NotFoundResponse;
+use Core\Infrastructure\Common\Presenter\PresenterFormatterInterface;
+use Core\Domain\Security\ProviderConfiguration\WebSSO\Model\WebSSOConfiguration;
+use Core\Application\Security\ProviderConfiguration\WebSSO\Repository\ReadWebSSOConfigurationRepositoryInterface;
+use Core\Application\Security\ProviderConfiguration\WebSSO\UseCase\FindWebSSOConfiguration\{
+    FindWebSSOConfiguration,
+    FindWebSSOConfigurationResponse
+};
+
+beforeEach(function () {
+    $this->repository = $this->createMock(ReadWebSSOConfigurationRepositoryInterface::class);
+    $this->presenterFormatter = $this->createMock(PresenterFormatterInterface::class);
+});
+
+it('should present a FindWebSSOConfigurationResponse when everything goes well', function () {
+    $configuration = new WebSSOConfiguration(
+        true,
+        false,
+        [],
+        [],
+        null,
+        null,
+        null,
+    );
+
+    $useCase = new FindWebSSOConfiguration($this->repository);
+    $presenter = new FindWebSSOConfigurationPresenterStub($this->presenterFormatter);
+
+    $this->repository
+        ->expects($this->once())
+        ->method('findConfiguration')
+        ->willReturn($configuration);
+
+    $useCase($presenter);
+    expect($presenter->response)->toBeInstanceOf(FindWebSSOConfigurationResponse::class);
+});
+
+it('should present a NotFoundResponse when no configuration are found in Data storage', function () {
+    $useCase = new FindWebSSOConfiguration($this->repository);
+    $presenter = new FindWebSSOConfigurationPresenterStub($this->presenterFormatter);
+
+    $this->repository
+        ->expects($this->once())
+        ->method('findConfiguration')
+        ->willReturn(null);
+
+    $useCase($presenter);
+    expect($presenter->getResponseStatus())->toBeInstanceOf(NotFoundResponse::class);
+    expect($presenter->getResponseStatus()?->getMessage())
+        ->toBe((new NotFoundResponse('WebSSOConfiguration'))->getMessage());
+});
+
+it('should present an ErrorResponse when an error occured during the finding process', function () {
+    $useCase = new FindWebSSOConfiguration($this->repository);
+    $presenter = new FindWebSSOConfigurationPresenterStub($this->presenterFormatter);
+    $exceptionMessage = 'An error occured';
+    $this->repository
+        ->expects($this->once())
+        ->method('findConfiguration')
+        ->willThrowException(new \Exception($exceptionMessage));
+
+    $useCase($presenter);
+    expect($presenter->getResponseStatus())->toBeInstanceOf(ErrorResponse::class);
+    expect($presenter->getResponseStatus()?->getMessage())->toBe($exceptionMessage);
+});
