7a34e04753054de9fe3975723d89f0b52500338b
Tom Darneix <tomdar87@outlook.com>
2022-03-23 15:30:54 +0000

    Replace react-beatiful-dnd by dnd-kit

--- www/front_src/src/Resources/Filter/Edit/index.tsx
+++ www/front_src/src/Resources/Filter/Edit/index.tsx
@@ -1,33 +1,32 @@
 import * as React from 'react';
 
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useTranslation } from 'react-i18next';
-import { move, isNil } from 'ramda';
+import { map, find, equals, path } from 'ramda';
 import { useUpdateAtom } from 'jotai/utils';
 import { useAtom } from 'jotai';
+import { rectIntersection } from '@dnd-kit/core';
+import { rectSortingStrategy } from '@dnd-kit/sortable';
 
-import { Typography, LinearProgress, Paper } from '@mui/material';
+import { Typography, LinearProgress, Stack } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
-import MoveIcon from '@mui/icons-material/UnfoldMore';
 
-import { MemoizedSectionPanel as SectionPanel, useRequest } from '@centreon/ui';
+import {
+  MemoizedSectionPanel as SectionPanel,
+  useRequest,
+  SortableItems,
+} from '@centreon/ui';
 
 import { labelEditFilters } from '../../translatedLabels';
 import { patchFilter } from '../api';
 import { customFiltersAtom, editPanelOpenAtom } from '../filterAtoms';
+import { Filter } from '../models';
+import { Criteria } from '../Criterias/models';
 
-import EditFilterCard from './EditFilterCard';
+import SortableContent from './SortableContent';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     width: '100%',
-  },
-  filterCard: {
-    alignItems: 'center',
-    display: 'grid',
-    gridGap: theme.spacing(2),
-    gridTemplateColumns: '1fr auto',
-    padding: theme.spacing(1),
   },
   filters: {
     display: 'grid',
@@ -64,23 +63,41 @@ const EditFiltersPanel = (): JSX.Element => {
     setEditPanelOpen(false);
   };
 
-  const onDragEnd = ({ draggableId, source, destination }): void => {
-    const id = Number(draggableId);
+  const dragEnd = (items: Array<string>, event): void => {
+    const reorderedCutomFilters = map((id) => {
+      const filter = find(
+        (customFilter) => equals(Number(customFilter.id), Number(id)),
+        customFilters,
+      ) as Filter;
 
-    if (isNil(destination)) {
-      return;
-    }
+      return {
+        ...filter,
+        order: items.indexOf(id),
+      };
+    }, items);
 
-    const reordedCustomFilters = move(
-      source.index,
-      destination.index,
-      customFilters,
-    );
+    setCustomFilters(reorderedCutomFilters);
 
-    setCustomFilters(reordedCustomFilters);
+    const activeId = path(['active', 'id'], event);
+    const destinationIndex = path(
+      ['active', 'data', 'current', 'sortable', 'index'],
+      event,
+    ) as number;
 
-    sendRequest({ id, order: destination.index });
+    sendRequest({ id: activeId, order: destinationIndex + 1 });
   };
+
+  const displayedFilters = map(
+    ({ id, ...other }) => ({ ...other, id: `${id}` }),
+    customFilters,
+  );
+
+  const RootComponent = React.useCallback(
+    ({ children }: RootComponentProps): JSX.Element => (
+      <Stack spacing={2}>{children}</Stack>
+    ),
+    [],
+  );
 
   const sections = [
     {
@@ -91,40 +108,20 @@ const EditFiltersPanel = (): JSX.Element => {
           <div className={classes.loadingIndicator}>
             {sending && <LinearProgress style={{ width: '100%' }} />}
           </div>
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="droppable">
-              {(droppable): JSX.Element => (
-                <div
-                  className={classes.filters}
-                  ref={droppable.innerRef}
-                  {...droppable.droppableProps}
-                >
-                  {customFilters?.map((filter, index) => (
-                    <Draggable
-                      draggableId={`${filter.id}`}
-                      index={index}
-                      key={filter.id}
-                    >
-                      {(draggable): JSX.Element => (
-                        <Paper
-                          square
-                          className={classes.filterCard}
-                          ref={draggable.innerRef}
-                          {...draggable.draggableProps}
-                        >
-                          <EditFilterCard filter={filter} />
-                          <div {...draggable.dragHandleProps}>
-                            <MoveIcon />
-                          </div>
-                        </Paper>
-                      )}
-                    </Draggable>
-                  ))}
-                  {droppable.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
+          <SortableItems<{
+            criterias: Array<Criteria>;
+            id: string;
+            name: string;
+          }>
+            updateSortableItemsOnItemsChange
+            Content={SortableContent}
+            RootComponent={RootComponent}
+            collisionDetection={rectIntersection}
+            itemProps={['criterias', 'id', 'name']}
+            items={displayedFilters}
+            sortingStrategy={rectSortingStrategy}
+            onDragEnd={dragEnd}
+          />
         </div>
       ),
     },
