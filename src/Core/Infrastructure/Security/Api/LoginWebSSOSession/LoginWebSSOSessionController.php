03ae3a3e73d371d8e4325bae63f6e9fd078b63b7
Jérémy Jaouen <jjaouen@centreon.com>
2022-03-24 07:42:50 +0100

    start implementing apache web sso

---
+++ src/Core/Infrastructure/Security/Api/LoginWebSSOSession/LoginWebSSOSessionController.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Core\Infrastructure\Security\Api\LoginWebSSOSession;
+
+use Symfony\Component\HttpFoundation\Request;
+use Centreon\Application\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class LoginWebSSOSessionController extends AbstractController
+{
+    /**
+     * @param $request Request
+     */
+    public function __invoke(Request $request)
+    {
+        
+    }
+}
