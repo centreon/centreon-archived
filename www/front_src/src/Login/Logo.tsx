cedd5c9282dcb5217541523ae2b34a1a4accabc3
nouha <nelabrouki-ext@centreon.com>
2022-04-04 09:40:58 +0000

    update logo for dark mode

--- www/front_src/src/Login/Logo.tsx
+++ www/front_src/src/Login/Logo.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react';
 
+import { useAtomValue } from 'jotai/utils';
+import { equals } from 'ramda';
 import { useTranslation } from 'react-i18next';
+import clsx from 'clsx';
 
 import { makeStyles } from '@mui/styles';
 
+import { ThemeMode, userAtom } from '@centreon/ui-context';
+
 import logoCentreon from '../assets/centreon.png';
+import logoDark from '../assets/Centreon_Logo_Blanc.svg';
 
 import { labelCentreonLogo } from './translatedLabels';
 
@@ -13,18 +19,27 @@ const useStyles = makeStyles({
     height: 'auto',
     width: 'auto',
   },
+  centreonLogoDark: {
+    height: 57,
+    width: 250,
+  },
 });
 
 const Logo = (): JSX.Element => {
   const classes = useStyles();
   const { t } = useTranslation();
+  const { themeMode } = useAtomValue(userAtom);
+  const logo = equals(themeMode, ThemeMode.light) ? logoCentreon : logoDark;
+  const isDarkMode = equals(themeMode, ThemeMode.dark);
 
   return (
     <img
       alt={t(labelCentreonLogo)}
       aria-label={t(labelCentreonLogo)}
-      className={classes.centreonLogo}
-      src={logoCentreon}
+      className={clsx(classes.centreonLogo, {
+        [classes.centreonLogoDark]: isDarkMode,
+      })}
+      src={logo}
     />
   );
 };
