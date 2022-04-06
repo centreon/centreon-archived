936c442e29908a88c14798a8f00982e25c8a86bc
ataghzout <97593248+ataghzout@users.noreply.github.com>
2022-04-06 12:27:41 +0000

    Refactoring dark mode (#10845)
    * fixing some dark mode issues and applying new colors
    
    * refactoring all variables hexa & adapt colors
    
    * adapt colors of all legacy component
    
    * change color legend of chart
    
    * changing radio buttons in dashboard pages
    
    * changing leftover checkbox to match the other ones
    
    * applying dark mode to performance's chart title
    
    * changing header title text color
    
    * add dark mode for disabled rows in list
    
    * changing butoon for rules configuration
    
    * change style checkbox service by host
    
    * fixing some dark mode issues and applying new colors
    
    * refactoring all variables hexa & adapt colors
    
    * adapt colors of all legacy component
    
    * change color legend of chart
    
    * changing radio buttons in dashboard pages
    
    * changing leftover checkbox to match the other ones
    
    * applying dark mode to performance's chart title
    
    * changing header title text color
    
    * add dark mode for disabled rows in list
    
    * changing butoon for rules configuration
    
    * change style checkbox service by host
    
    * fix(test): check properly checkbox
    
    * apply dark mode for graphs
    
    * Allows to activate the checkbox by clicking on the parent div (necessary to pass the TA)
    
    Co-authored-by: YAHIAOUI Hamza <hamza.yahiaoui@goandev.net>
    Co-authored-by: Ayoub TAGHZOUT <ayoub.taghzout@goandev.net>
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    Co-authored-by: Laurent Calvet <lcalvet@centreon.com>

--- features/bootstrap/ShowDisabledHostsServicesContext.php
+++ features/bootstrap/ShowDisabledHostsServicesContext.php
@@ -44,10 +44,7 @@ class ShowDisabledHostsServicesContext extends CentreonContext
      */
     public function iActivateTheVisibilityFilterOfDisabledHosts()
     {
-        $checkbox = $this->assertFind('named', array('id_or_name', 'statusHostFilter'));
-        $checkbox->click();
-        sleep(2);
-
+        $this->getSession()->evaluateScript("document.getElementById('statusHostFilter').checked = true");
         $search = $this->assertFind('named', array('id_or_name', 'Search'));
         $search->click();
         sleep(2);
