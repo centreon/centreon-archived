eb787208714ba810a86910f5b657f6a1d20848f1
Kevin Duret <kduret@centreon.com>
2022-04-12 17:02:44 +0200

    enh(auth): exclude some users from password expiration policy on upgrade (#10966)
    Refs: MON-12843

--- www/install/steps/process/configFileSetup.php
+++ www/install/steps/process/configFileSetup.php
@@ -36,7 +36,8 @@
 
 session_start();
 require_once __DIR__ . '/../../../../bootstrap.php';
-require_once '../functions.php';
+require_once __DIR__ . '/../functions.php';
+require_once __DIR__ . '/../../functions.php';
 
 $return = [
     'id' => 'configfile',
