49070918319390756a2f3bea69d313ecd15d1846
Tom Darneix <tomdar87@outlook.com>
2022-04-12 13:27:36 +0200

    fix(UI): Centreon is slowed down (#10902)
    * Remove old pendo script
    
    * Remove pendo script tag creation
    
    * Use Pendo as a React hook
    
    * Fix eslint
    
    * Remove useless pendo script call
    
    * Fix variable naming
    
    * Re-enable pendo script for iframe
    
    * Fix function name

--- www/front_src/src/Provider/index.tsx
+++ www/front_src/src/Provider/index.tsx
@@ -34,6 +34,7 @@ import {
   parametersEndpoint,
 } from './endpoint';
 import { DefaultParameters } from './models';
+import usePendo from './usePendo';
 
 dayjs.extend(localizedFormat);
 dayjs.extend(utcPlugin);
@@ -60,6 +61,7 @@ const AppProvider = (): JSX.Element | null => {
   const { sendRequest: getAcl } = useRequest<Actions>({
     request: getData,
   });
+  usePendo();
 
   const initializeI18n = ({ retrievedUser, retrievedTranslations }): void => {
     const locale = (retrievedUser.locale || navigator.language)?.slice(0, 2);
