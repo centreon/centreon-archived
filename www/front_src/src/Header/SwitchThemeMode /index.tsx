7a2b69b40bd9c0928e4d888a92ad501453493db5
nouha <nelabrouki-ext@centreon.com>
2022-04-15 08:47:29 +0000

    extract svg

--- www/front_src/src/Header/SwitchThemeMode /index.tsx
+++ www/front_src/src/Header/SwitchThemeMode /index.tsx
@@ -11,20 +11,15 @@ import makeStyles from '@mui/styles/makeStyles';
 import { userAtom, ThemeMode } from '@centreon/ui-context';
 import { patchData, useRequest } from '@centreon/ui';
 
+import svgSun from './images/sun.svg';
+import svgMoon from './images/moon.svg';
+
 interface StyleProps {
   darkModeSvg?: string;
   lightModeSvg?: string;
 }
 
-const svgMoon = `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-  'black',
-)}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`;
-
-const svgSun = `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="19" width="19" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-  'black',
-)}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`;
-
-const MaterialUISwitch = styled(Switch, {
+const ThemeModeSwitch = styled(Switch, {
   shouldForwardProp: (prop) =>
     !equals(prop, 'color') &&
     !equals(prop, 'lightModeSvg') &&
@@ -37,7 +32,7 @@ const MaterialUISwitch = styled(Switch, {
         opacity: 1,
       },
       '& .MuiSwitch-thumb:before': {
-        backgroundImage: darkModeSvg,
+        backgroundImage: `url(${darkModeSvg})`,
       },
       transform: 'translate(15px,-50%)',
     },
@@ -52,7 +47,7 @@ const MaterialUISwitch = styled(Switch, {
   },
   '& .MuiSwitch-thumb': {
     '&:before': {
-      backgroundImage: lightModeSvg,
+      backgroundImage: `url(${lightModeSvg})`,
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
       content: "''",
@@ -85,7 +80,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const SwitchThemeMode = (): JSX.Element => {
-  const props = { darkModeSvg: svgMoon, lightModeSvg: svgSun };
+  const props = {
+    darkModeSvg: svgMoon,
+    lightModeSvg: svgSun,
+  };
   const classes = useStyles();
   const { pathname } = useLocation();
 
@@ -116,7 +114,7 @@ const SwitchThemeMode = (): JSX.Element => {
 
   return (
     <div className={classes.container}>
-      <MaterialUISwitch
+      <ThemeModeSwitch
         checked={isDarkMode}
         {...props}
         onChange={switchThemeMode}
