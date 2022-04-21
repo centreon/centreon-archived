b35feb01c1e8299ff2e3ef45fe9eb2b5f8e3c853
jcaro <jcaro@centreon.com>
2022-04-21 17:34:49 +0200

    fix(UI): Fully handle default ack options (#10963)
    * fix(resource): fully handle default ack options
    
    * coding-style: patch centreon-bot issues found
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * feedback: apply kduret suggestions
    
    * fix(resource): fully handle default ack options
    
    * coding-style: patch centreon-bot issues found
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * Update src/Centreon/Application/Controller/Administration/ParametersController.php
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    
    * feedback: apply kduret suggestions
    
    * enh(parameters): reload page after saving monitoring parameters
    
    * Regenerate package-lock only for centreon-frontend
    
    * Add new props to handle ACK
    
    * Add new props in Form to ACK
    
    * Add translated labels
    
    * Fix initial values for ack in Form
    
    * Add force_active_checks in axios post request
    
    * Add unit test with the new options for acknowledgement
    
    * Remove option to force active checks in Form when set up acknowledgement but he is sent with default value in POST request
    
    * Fix Jotai version and regenerate package lock for Jotai
    
    * Fix issues on units test
    
    * enh(resource): handle force active check
    
    * fix some coding style on legacy
    
    * centreon-bot: fix error
    
    * Add translations into files messages.po and re-allign package.json with develop
    
    * Re-align package.json with develop
    
    * Update lang/fr_FR.UTF-8/LC_MESSAGES/messages.po
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update lang/es_ES.UTF-8/LC_MESSAGES/messages.po
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update lang/pt_PT.UTF-8/LC_MESSAGES/messages.po
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Update lang/pt_BR.UTF-8/LC_MESSAGES/messages.po
    
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>
    
    * Fix units test => remove test about ACK with services box
    
    * code-review : apply feedbacks
    
    * Remove checkbox about sticky and persistent option
    
    * Adapt unit test for Action ACK
    
    * Fix eslint issues
    
    Co-authored-by: Adrien Morais <amorais@centreon.com>
    Co-authored-by: Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>
    Co-authored-by: Kevin Duret <kduret@centreon.com>
    Co-authored-by: Tom Darneix <tomdar87@outlook.com>

--- www/front_src/src/Resources/Actions/index.test.tsx
+++ www/front_src/src/Resources/Actions/index.test.tsx
@@ -53,7 +53,6 @@ import {
   labelCritical,
   labelUnknown,
   labelAddComment,
-  labelPersistent,
   labelEndTime,
   labelEndDateGreaterThanStartDate,
   labelInvalidFormat,
@@ -94,9 +93,9 @@ const mockUser = {
 };
 const mockRefreshInterval = 15;
 const mockDowntime = {
-  default_duration: 7200,
-  default_fixed: true,
-  default_with_services: false,
+  duration: 7200,
+  fixed: true,
+  with_services: false,
 };
 const mockAcl = {
   actions: {
@@ -119,8 +118,11 @@ const mockAcl = {
   },
 };
 const mockAcknowledgement = {
+  force_active_checks: false,
+  notify: false,
   persistent: true,
-  sticky: false,
+  sticky: true,
+  with_services: true,
 };
 
 jest.mock('../icons/Downtime');
@@ -303,8 +305,7 @@ describe(Actions, () => {
   );
 
   it('sends an acknowledgement request when Resources are selected and the Ackowledgement action is clicked and confirmed', async () => {
-    const { getByText, getByLabelText, findByLabelText, getAllByText } =
-      renderActions();
+    const { getByText, findByLabelText, getAllByText } = renderActions();
 
     const selectedResources = [host, service];
 
@@ -315,11 +316,8 @@ describe(Actions, () => {
     fireEvent.click(getByText(labelAcknowledge));
 
     const notifyCheckbox = await findByLabelText(labelNotify);
-    const persistentCheckbox = await findByLabelText(labelPersistent);
 
     fireEvent.click(notifyCheckbox);
-    fireEvent.click(persistentCheckbox);
-    fireEvent.click(getByLabelText(labelAcknowledgeServices));
 
     mockedAxios.get.mockResolvedValueOnce({ data: {} });
     mockedAxios.post.mockResolvedValueOnce({});
@@ -332,9 +330,10 @@ describe(Actions, () => {
         {
           acknowledgement: {
             comment: labelAcknowledgedByAdmin,
+            force_active_checks: false,
             is_notify_contacts: true,
-            is_persistent_comment: false,
-            is_sticky: false,
+            is_persistent_comment: true,
+            is_sticky: true,
             with_services: true,
           },
 
