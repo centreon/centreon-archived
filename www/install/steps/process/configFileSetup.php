e774d41ba8033767222a3ede122aa8a65324919b
Kevin Duret <kduret@centreon.com>
2022-04-25 16:16:05 +0200

    fix(sec): update configuration file rights

--- www/install/steps/process/configFileSetup.php
+++ www/install/steps/process/configFileSetup.php
@@ -90,9 +90,6 @@ $centreonConfFile = $centreonEtcPath . '/centreon.conf.php';
 $contents = file_get_contents('../../var/configFileTemplate');
 $contents = str_replace(array_keys($macroReplacements), array_values($macroReplacements), $contents);
 file_put_contents($centreonConfFile, $contents);
-chmod($centreonConfFile, 0660);
-chown($centreonConfFile, 'apache');
-chgrp($centreonConfFile, 'apache');
 
 /**
  * conf.pm
@@ -101,9 +98,6 @@ $centreonConfPmFile = $centreonEtcPath . '/conf.pm';
 $contents = file_get_contents('../../var/configFilePmTemplate');
 $contents = str_replace(array_keys($macroReplacements), array_values($macroReplacements), $contents);
 file_put_contents($centreonConfPmFile, $contents);
-chmod($centreonConfPmFile, 0660);
-chown($centreonConfPmFile, 'centreon');
-chgrp($centreonConfPmFile, 'centreon');
 
 /**
  * Database configuration file
