505fbd077bb1e4b6198c7497519bc7887d217006
alaunois <alaunois@centreon.com>
2022-04-26 17:03:14 +0200

    fix typo

--- src/CentreonRemote/Domain/Resources/DefaultConfig/CfgNagiosLogger.php
+++ src/CentreonRemote/Domain/Resources/DefaultConfig/CfgNagiosLogger.php
@@ -17,19 +17,19 @@ class CfgNagiosLogger
         return [
             'cfg_nagios_id' => 1,
             'log_v2_logger' => 'file',
-            'log_level_functions' => 'error',
+            'log_level_functions' => 'err',
             'log_level_config' => 'info',
             'log_level_events' => 'info',
             'log_level_checks' => 'info',
-            'log_level_notifications' => 'error',
-            'log_level_eventbroker' => 'error',
-            'log_level_external_command' => 'error',
-            'log_level_commands' => 'error',
-            'log_level_downtimes' => 'error',
-            'log_level_comments' => 'error',
-            'log_level_macros' => 'error',
+            'log_level_notifications' => 'err',
+            'log_level_eventbroker' => 'err',
+            'log_level_external_command' => 'err',
+            'log_level_commands' => 'err',
+            'log_level_downtimes' => 'err',
+            'log_level_comments' => 'err',
+            'log_level_macros' => 'err',
             'log_level_process' => 'info',
-            'log_level_runtime' => 'error',
+            'log_level_runtime' => 'err',
         ];
     }
 }
