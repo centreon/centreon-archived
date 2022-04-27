60281cb26301236a9e1ff3dfd81857670ae3dd6b
YAHIAOUI Hamza <hamza.yahiaoui@goandev.net>
2022-04-27 03:36:09 +0000

    Sanitizing queries in update 2.8.0 file

--- www/install/php/Update-2.8.0.php
+++ www/install/php/Update-2.8.0.php
@@ -44,19 +44,25 @@ if (isset($pearDB)) {
     /* Check virtual host */
     $queryHost = 'SELECT host_id '
         . 'FROM host '
-        . 'WHERE host_register = "2" '
-        . 'AND host_name = "_Module_Meta" ';
-    $res = $pearDB->query($queryHost);
-    if ($res->rowCount()) {
-        $row = $res->fetchRow();
+        . 'WHERE host_register = :host_register '
+        . 'AND host_name = :host_name ';
+    $statement = $pearDB->prepare($queryHost);
+    $statement->bindValue(":host_register", '2');
+    $statement->bindValue(":host_name", '_Module_Meta');
+    $statement->execute();
+    if ($statement->rowCount()) {
+        $row = $statement->fetch(\PDO::FETCH_ASSOC);
         $hostId = $row['host_id'];
     } else {
         $query = 'INSERT INTO host (host_name, host_register) '
-            . 'VALUES ("_Module_Meta", "2") ';
-        $pearDB->query($query);
-        $res = $pearDB->query($queryHost);
-        if ($res->rowCount()) {
-            $row = $res->fetchRow();
+            . 'VALUES (:host_name, :host_register) ';
+        $insertStatement = $pearDB->prepare($query);
+        $insertStatement->bindValue(':host_name', '_Module_Meta');
+        $insertStatement->bindValue(':host_register', '2');
+        $insertStatement->execute();
+        $statement->execute();
+        if ($statement->rowCount()) {
+            $row = $statement->fetch(\PDO::FETCH_ASSOC);
             $hostId = $row['host_id'];
         }
     }
@@ -64,10 +70,13 @@ if (isset($pearDB)) {
     /* Check existing virtual services */
     $query = 'SELECT service_id, service_description '
         . 'FROM service '
-        . 'WHERE service_description LIKE "meta_%" '
-        . 'AND service_register = "2" ';
-    $res = $pearDB->query($query);
-    while ($row = $res->fetchRow()) {
+        . 'WHERE service_description LIKE :service_description '
+        . 'AND service_register = :service_register ';
+    $statement = $pearDB->prepare($query);
+    $statement->bindValue(":service_description", 'meta_%');
+    $statement->bindValue(":service_register", '2');
+    $statement->execute();
+    foreach ($statement->fetchAll(\PDO::FETCH_ASSOC) as $row) {
         if (preg_match('/meta_(\d+)/', $row['service_description'], $matches)) {
             $metaId = $matches[1];
             $virtualServices[$matches[1]]['service_id'] = $row['service_id'];
@@ -77,11 +86,15 @@ if (isset($pearDB)) {
     /* Check existing relations between virtual services and virtual host */
     $query = 'SELECT s.service_id, s.service_description '
         . 'FROM service s, host_service_relation hsr '
-        . 'WHERE hsr.host_host_id = "' . $hostId . '" '
-        . 'AND s.service_register = "2" '
-        . 'AND s.service_description LIKE "meta_%" ';
-    $res = $pearDB->query($query);
-    while ($row = $res->fetchRow()) {
+        . 'WHERE hsr.host_host_id = :host_id '
+        . 'AND s.service_register = :service_register '
+        . 'AND s.service_description LIKE :service_description ';
+    $statement = $pearDB->prepare($query);
+    $statement->bindValue(':host_id', (int)$hostId, \PDO::PARAM_INT);
+    $statement->bindValue(':service_register', '2');
+    $statement->bindValue(':service_description', 'meta_%');
+    $statement->execute();
+    foreach ($statement->fetchAll(\PDO::FETCH_ASSOC) as $row) {
         if (preg_match('/meta_(\d+)/', $row['service_description'], $matches)) {
             $metaId = $matches[1];
             $virtualServices[$matches[1]]['relation'] = true;
@@ -99,8 +112,11 @@ if (isset($pearDB)) {
         }
         if (!isset($virtualServices[$row['meta_id']]) || !isset($virtualServices[$row['meta_id']]['relation'])) {
             $query = 'INSERT INTO host_service_relation (host_host_id, service_service_id) '
-                . 'VALUES (' . $hostId . ',' . $serviceId . ') ';
-            $pearDB->query($query);
+                . 'VALUES (:host_id, :service_id) ';
+            $statement = $pearDB->prepare($query);
+            $statement->bindValue(':host_id', (int) $hostId, \PDO::PARAM_INT);
+            $statement->bindValue(':service_id', (int) $serviceId, \PDO::PARAM_INT);
+            $statement->execute();
         }
     }
 }
