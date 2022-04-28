cc62b9e4b08e4e1c9efdb91f6df8f2137d747f69
nouha <nelabrouki-ext@centreon.com>
2022-04-28 10:54:22 +0000

    configuration of cypress

---
+++ cypress/support/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { mount as cypressMount, MountReturn } from '@cypress/react';
+
+import { ThemeProvider } from '@centreon/ui';
+
+const mount = (
+  children: React.ReactElement,
+): Cypress.Chainable<MountReturn> => {
+  return cypressMount(<ThemeProvider>{children}</ThemeProvider>);
+};
+
+export { mount };
