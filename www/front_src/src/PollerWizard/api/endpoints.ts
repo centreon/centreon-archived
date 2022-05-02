4bbf21100e05c3d29d151abd7b9f52d8238a75ba
Tom Darneix <tomdar87@outlook.com>
2022-05-02 17:57:03 +0000

    Add another base endpoint constant

--- www/front_src/src/PollerWizard/api/endpoints.ts
+++ www/front_src/src/PollerWizard/api/endpoints.ts
@@ -1,8 +1,10 @@
 const legacyBaseEndpoint = './api/internal.php';
 
-export const pollerWaitListEndpoint = `${legacyBaseEndpoint}?object=centreon_configuration_remote&action=getPollerWaitList`;
-export const getPollersEndpoint = `${legacyBaseEndpoint}?object=centreon_configuration_remote&action=getRemotesList`;
-export const remoteServerWaitListEndpoint = `${legacyBaseEndpoint}?object=centreon_configuration_remote&action=getWaitList`;
-export const getRemoteServersEndpoint = `${legacyBaseEndpoint}?object=centreon_configuration_remote&action=getRemotesList`;
-export const wizardFormEndpoint = `${legacyBaseEndpoint}?object=centreon_configuration_remote&action=linkCentreonRemoteServer`;
+const baseRemoteConfigurationEndpoint = `${legacyBaseEndpoint}?action=centreon_configuration_remote`;
+
+export const pollerWaitListEndpoint = `${baseRemoteConfigurationEndpoint}&action=getPollerWaitList`;
+export const getPollersEndpoint = `${baseRemoteConfigurationEndpoint}&action=getRemotesList`;
+export const remoteServerWaitListEndpoint = `${baseRemoteConfigurationEndpoint}&action=getWaitList`;
+export const getRemoteServersEndpoint = `${baseRemoteConfigurationEndpoint}&action=getRemotesList`;
+export const wizardFormEndpoint = `${baseRemoteConfigurationEndpoint}&action=linkCentreonRemoteServer`;
 export const exportTaskEndpoint = `${legacyBaseEndpoint}?object=centreon_task_service&action=getTaskStatus`;
