dcbffe34a80d3e02f5c1e8595b81dae3dd020fce
Jordan CARO <jcaro@centreon.com>
2022-05-09 10:58:54 +0200

    Remove overflow property

--- www/front_src/src/Navigation/Sidebar/Menu/CollapsibleItems.tsx
+++ www/front_src/src/Navigation/Sidebar/Menu/CollapsibleItems.tsx
@@ -112,7 +112,6 @@ const useStyles = makeStyles((theme) => ({
       collapseScrollMaxWidth
         ? theme.spacing(collapseScrollMaxWidth)
         : theme.spacing(collapseWidth),
-    overflow: 'hidden',
     position: 'fixed',
     textOverflow: 'ellipsis',
     top: ({ currentTop }: StyleProps): number | undefined => currentTop,
