d2e07e10d4b504a4be1d8ef131b77d275aaf02cb
nouha <nelabrouki-ext@centreon.com>
2022-05-11 15:53:18 +0000

    visual testing for menu

---
+++ www/front_src/src/Navigation/Sidebar/index.cypress.test.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import { mount } from '../../../../../cypress/support';
+
+import SideBar from './index';
+
+describe('Visual testing for menu: ', () => {
+  beforeEach(() => {
+    cy.fixture('dataMenu').then((data) => {
+      mount(
+        <Router>
+          <SideBar navigationData={data.result} />
+        </Router>,
+      );
+    });
+  });
+  it('Initial menu', () => {
+    cy.get("[alt='mini logo']").should('be.visible');
+    cy.get('li')
+      .should('have.length', 2)
+      .each(($li) => {
+        cy.wrap($li).get('svg').should('be.visible');
+      });
+    cy.percySnapshot();
+  });
+
+  it('menu should expanded after clicking on logo', () => {
+    cy.get("[alt='mini logo']").click();
+    cy.get("[alt='logo']").should('be.visible');
+    cy.get('li')
+      .should('have.length', 2)
+      .each(($li, index) => {
+        cy.wrap($li).as('element').get('svg').should('be.visible');
+        if (index === 0) {
+          cy.get('@element').contains('Monitoring');
+        } else {
+          cy.get('@element').contains('Configuration');
+        }
+      });
+    cy.percySnapshot();
+  });
+
+  it('collapse should be visible when item is hovered and this item should be colored', () => {
+    cy.get("[alt='mini logo']").click();
+    cy.get('li').each(($ele) => {
+      if ($ele.find('[data-testid=NavigateNextIcon]').length > 0) {
+        cy.wrap($ele)
+          .find('[data-testid=NavigateNextIcon]')
+          .parent()
+          .trigger('mouseover');
+        cy.get('.MuiCollapse-root').should('be.visible');
+      }
+      cy.percySnapshot();
+    });
+  });
+
+  it('item of menu should be colored after double click', () => {
+    cy.get('li').first().as('element').trigger('mouseover');
+    cy.get('@element').trigger('dblclick');
+    cy.percySnapshot();
+  });
+
+  it('when item of collapse is clicked , the item and parent item should be colored', () => {
+    cy.get("[alt='mini logo']").click();
+    cy.get('li')
+      .first()
+      .each(($ele) => {
+        if ($ele.find('[data-testid=NavigateNextIcon]').length > 0) {
+          cy.wrap($ele)
+            .find('[data-testid=NavigateNextIcon]')
+            .parent()
+            .trigger('mouseover');
+          cy.get('.MuiCollapse-root').as('collapse').should('be.visible');
+          cy.get('@collapse')
+            .find('.MuiListItemText-root')
+            .first()
+            .trigger('mouseover')
+            .trigger('click');
+        }
+        cy.percySnapshot();
+      });
+  });
+});
