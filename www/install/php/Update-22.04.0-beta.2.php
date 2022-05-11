4bc63a681b66d811e08d12de1f45c05cd9bdd1f4
Kevin Duret <kduret@centreon.com>
2022-05-11 09:47:39 +0200

    fix(upgrade): move engine logger upgrade to beta 2

--- www/install/php/Update-22.04.0-beta.2.php
+++ www/install/php/Update-22.04.0-beta.2.php
@@ -18,3 +18,54 @@
  * For more information : contact@centreon.com
  *
  */
+
+require_once __DIR__ . '/../../class/centreonLog.class.php';
+
+$centreonLog = new CentreonLog();
+
+//error specific content
+$versionOfTheUpgrade = 'UPGRADE - 22.04.0-beta.2: ';
+$errorMessage = '';
+
+try {
+    // Centengine logger v2
+    if (
+        $pearDB->isColumnExist('cfg_nagios', 'log_archive_path') === 1
+        && $pearDB->isColumnExist('cfg_nagios', 'log_rotation_method') === 1
+        && $pearDB->isColumnExist('cfg_nagios', 'daemon_dumps_core') === 1
+    ) {
+        $errorMessage = "Unable to remove log_archive_path,log_rotation_method,daemon_dumps_core from cfg_nagios table";
+        $pearDB->query(
+            "ALTER TABLE `cfg_nagios`
+            DROP COLUMN `log_archive_path`,
+            DROP COLUMN `log_rotation_method`,
+            DROP COLUMN `daemon_dumps_core`"
+        );
+    }
+    if ($pearDB->isColumnExist('cfg_nagios', 'logger_version') !== 1) {
+        $errorMessage = "Unable to add logger_version to cfg_nagios table";
+        $pearDB->query(
+            "ALTER TABLE `cfg_nagios`
+            ADD COLUMN `logger_version` enum('log_v2_enabled', 'log_legacy_enabled') DEFAULT 'log_v2_enabled'"
+        );
+    }
+
+    $errorMessage = "Unable to update logger_version from cfg_nagios table";
+    $pearDB->query(
+        "UPDATE `cfg_nagios` set logger_version = 'log_legacy_enabled'"
+    );
+} catch (\Exception $e) {
+    if ($pearDB->inTransaction()) {
+        $pearDB->rollBack();
+    }
+
+    $centreonLog->insertLog(
+        4,
+        $versionOfTheUpgrade . $errorMessage .
+        " - Code : " . (int)$e->getCode() .
+        " - Error : " . $e->getMessage() .
+        " - Trace : " . $e->getTraceAsString()
+    );
+
+    throw new \Exception($versionOfTheUpgrade . $errorMessage, (int) $e->getCode(), $e);
+}
