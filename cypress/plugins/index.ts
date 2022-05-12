8d4ec058ee28ad5dfbf3b9bb2141f7a55c17c73e
nouha <nelabrouki-ext@centreon.com>
2022-05-12 10:12:42 +0000

    add cypress image snapshot

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable global-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
+const {
+  addMatchImageSnapshotPlugin,
+} = require('cypress-image-snapshot/plugin');
 
 module.exports = (on, config) => {
   if (config.testingType === 'component') {
+    addMatchImageSnapshotPlugin(on, config);
+
     const { startDevServer } = require('@cypress/webpack-dev-server');
 
     const webpackConfig = require('../../webpack.config.dev');
--- cypress/support/index.tsx
+++ cypress/support/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 import { mount as cypressMount, MountReturn } from '@cypress/react';
 
 import { ThemeProvider } from '@centreon/ui';
+
+addMatchImageSnapshotCommand({
+  capture: 'viewport',
+  customDiffConfig: { threshold: 0.1 },
+  failureThreshold: 0.03,
+  failureThresholdType: 'percent',
+});
 
 const mount = (
   children: React.ReactElement,
