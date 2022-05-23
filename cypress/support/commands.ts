11badb9926863a6258065ad1fa87cac7ecdebafd
nouha <nelabrouki-ext@centreon.com>
2022-05-23 17:01:13 +0000

    update snapshots path

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,6 +3,7 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand({
   capture: 'viewport',
   customDiffConfig: { threshold: 0.1 },
+  customSnapshotsDir: './cypress/visual-testing-snapshots',
   failureThreshold: 0.03,
   failureThresholdType: 'percent',
 });
