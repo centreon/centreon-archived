7a06696cef6901232a1f97821fed0191926cfd79
Zakaria Guennoune <83596451+zguennoune02@users.noreply.github.com>
2022-05-25 16:27:07 +0200

    Update centreon-deb-package.sh

--- ci/scripts/centreon-deb-package.sh
+++ ci/scripts/centreon-deb-package.sh
@@ -5,7 +5,7 @@ if [ -z "$VERSION" -o -z "$RELEASE" -o -z "$DISTRIB" ] ; then
   echo "You need to specify VERSION / RELEASE / DISTRIB variables"
   exit 1
 fi
-
+rm -rf /root/.cache/*
 echo "################################################## PACKAGING WEB ##################################################"
 
 AUTHOR="Luiz Costa"
@@ -14,13 +14,10 @@ AUTHOR_EMAIL="me@luizgustavo.pro.br"
 # fix version to debian format accept
 VERSION="$(echo $VERSION | sed 's/-/./g')"
 
-ls -lart
 tar czpf centreon-$VERSION.tar.gz centreon
 cd centreon/
 cp -rf ci/debian .
 sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
 debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -y -r "${DISTRIB}"
 debuild-pbuilder
-cd ../
-ls -lart
-#mv *.deb /src
+
