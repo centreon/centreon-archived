8aeaed415bfde2db6892770c205aa083c1e6cf71
Kevin Duret <kduret@centreon.com>
2022-05-25 09:53:05 +0200

    enh(monitoring): mechanism to switch repository (#11123)
    * enh(resources): add option to switch repositories in Resource Status
    
    * fix unit tests
    
    * Update src/Centreon/Infrastructure/Monitoring/Resource/ResourceRepositoryFactory.php
    
    Co-authored-by: Adrien Morais <amorais@centreon.com>
    Co-authored-by: Adrien Morais-Mestre <31647811+adr-mo@users.noreply.github.com>

--- tests/php/Centreon/Domain/Monitoring/ResourceServiceTest.php
+++ tests/php/Centreon/Domain/Monitoring/ResourceServiceTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2005 - 2020 Centreon (https://www.centreon.com/)
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@
 
 namespace Tests\Centreon\Domain\Monitoring;
 
-use PHPUnit\Framework\TestCase;
 use Centreon\Domain\Monitoring\Resource;
 use Centreon\Domain\Monitoring\ResourceFilter;
 use Centreon\Domain\Monitoring\ResourceService;
@@ -29,42 +28,34 @@ use Centreon\Domain\Monitoring\Interfaces\ResourceRepositoryInterface;
 use Centreon\Domain\Security\Interfaces\AccessGroupRepositoryInterface;
 use Centreon\Domain\Monitoring\Interfaces\MonitoringRepositoryInterface;
 
-class ResourceServiceTest extends TestCase
-{
-    /**
-     * @throws \Exception
-     */
-    public function testFindResources(): void
-    {
-        $hostResource = (new Resource())
-            ->setType('host')
-            ->setId(1)
-            ->setName('host1');
-        $serviceResource = (new Resource())
-            ->setType('service')
-            ->setId(1)
-            ->setName('service1')
-            ->setParent($hostResource);
+it('find resources and build uuids', function () {
+    $hostResource = (new Resource())
+        ->setType('host')
+        ->setId(1)
+        ->setName('host1');
+    $serviceResource = (new Resource())
+        ->setType('service')
+        ->setId(1)
+        ->setName('service1')
+        ->setParent($hostResource);
 
-        $resourceRepository = $this->createMock(ResourceRepositoryInterface::class);
-        $resourceRepository->expects(self::any())
-            ->method('findResources')
-            ->willReturn([$hostResource, $serviceResource]); // values returned for the all next tests
+    $resourceRepository = $this->createMock(ResourceRepositoryInterface::class);
+    $resourceRepository->expects($this->any())
+        ->method('findResources')
+        ->willReturn([$hostResource, $serviceResource]); // values returned for the all next tests
 
-        $monitoringRepository = $this->createMock(MonitoringRepositoryInterface::class);
+    $monitoringRepository = $this->createMock(MonitoringRepositoryInterface::class);
+    $accessGroup = $this->createMock(AccessGroupRepositoryInterface::class);
 
-        $accessGroup = $this->createMock(AccessGroupRepositoryInterface::class);
+    $resourceService = new ResourceService(
+        $monitoringRepository,
+        $accessGroup,
+        $resourceRepository
+    );
 
-        $resourceService = new ResourceService(
-            $resourceRepository,
-            $monitoringRepository,
-            $accessGroup
-        );
+    $resourcesFound = $resourceService->findResources(new ResourceFilter());
 
-        $resourcesFound = $resourceService->findResources(new ResourceFilter());
-
-        $this->assertCount(2, $resourcesFound);
-        $this->assertEquals('h1', $resourcesFound[0]->getUuid());
-        $this->assertEquals('h1-s1', $resourcesFound[1]->getUuid());
-    }
-}
+    expect($resourcesFound)->toHaveCount(2);
+    expect($resourcesFound[0]->getUuid())->toBe('h1');
+    expect($resourcesFound[1]->getUuid())->toBe('h1-s1');
+});
