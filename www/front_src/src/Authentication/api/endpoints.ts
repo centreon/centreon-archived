39d3c82930924fb38f60aa7724b487882ca97900
Tom Darneix <tomdar87@outlook.com>
2022-05-27 16:26:07 +0000

    Add temp endpoints

--- www/front_src/src/Authentication/api/endpoints.ts
+++ www/front_src/src/Authentication/api/endpoints.ts
@@ -6,3 +6,5 @@ export const authenticationProvidersEndpoint = (provider: Provider): string =>
   `${baseEndpoint}/administration/authentication/providers/${provider}`;
 export const contactsEndpoint = `${baseEndpoint}/configuration/users`;
 export const contactTemplatesEndpoint = `${baseEndpoint}/configuration/contacts/templates`;
+export const contactGroupsEndpoint = `http://localhost:5003/centreon/api/latest/configuration/contacts/groups`;
+export const accessGroupsEndpoint = `http://localhost:5003/centreon/api/latest/configuration/access-groups`;
