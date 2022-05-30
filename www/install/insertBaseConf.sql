b637489d85976e0f02d8b9e5dd0c97cfa9100e8f
Thomas Untoja <tuntoja@centreon.com>
2022-05-30 09:56:58 +0200

    chore(install): update version to 20.10.18

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.17');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '20.10.18');
 
 --
 -- Contenu de la table `contact`
