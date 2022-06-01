4862a65dea42030d7bb2c0cde1ef02b5c7f9dc86
Luiz Costa <me@luizgustavo.pro.br>
2022-06-01 16:58:03 +0100

    Add missing locale and api manual on Debian build

--- ci/scripts/centreon-deb-package.sh
+++ ci/scripts/centreon-deb-package.sh
@@ -13,12 +13,37 @@ AUTHOR_EMAIL="me@luizgustavo.pro.br"
 
 # fix version to debian format accept
 VERSION="$(echo $VERSION | sed 's/-/./g')"
+$MAJOR_VERSION="$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')"
 
 ls -lart
 tar czpf centreon-$VERSION.tar.gz centreon
 cd centreon/
+
+# Replace basic macros.
+find ./www/include/Administration/about -type f | xargs --delimiter='\n' sed -i -e "s/@COMMIT@/$COMMIT/g"
+
+# set locale
+apt -y upgrade && apt install -y php gettext
+mkdir -p www/locale/en_US.UTF-8/LC_MESSAGES
+php bin/centreon-translations.php en lang/fr_FR.UTF-8/LC_MESSAGES/messages.po www/locale/en_US.UTF-8/LC_MESSAGES/messages.ser
+for i in lang/* ; do
+  localefull=`basename $i`
+  langcode=`echo $localefull | cut -d _ -f 1`
+  mkdir -p "www/locale/$localefull/LC_MESSAGES"
+  msgfmt "lang/$localefull/LC_MESSAGES/messages.po" -o "www/locale/$localefull/LC_MESSAGES/messages.mo" || exit 1
+  msgfmt "lang/$localefull/LC_MESSAGES/help.po" -o "www/locale/$localefull/LC_MESSAGES/help.mo" || exit 1
+  php bin/centreon-translations.php "$langcode" "lang/$localefull/LC_MESSAGES/messages.po" "www/locale/$localefull/LC_MESSAGES/messages.ser"
+done
+rm -rf lang
+
+# Generate API documentation.
+apt install -y npm && sleep 30
+npm i redoc-cli
+~/node_modules/.bin/redoc-cli bundle --options.hideDownloadButton=true doc/API/centreon-api-v${MAJOR_VERSION}.yaml -o ../centreon-api-v${MAJOR_VERSION}.html
+
+# make debian config
 cp -rf ci/debian .
-sed -i "s/^centreon:version=.*$/centreon:version=$(echo $VERSION | egrep -o '^[0-9][0-9].[0-9][0-9]')/" debian/substvars
+sed -i "s/^centreon:version=.*$/centreon:version=${MAJOR_VERSION}/" debian/substvars
 debmake -f "${AUTHOR}" -e "${AUTHOR_EMAIL}" -u "$VERSION" -y -r "${DISTRIB}"
 debuild-pbuilder
 cd ../
