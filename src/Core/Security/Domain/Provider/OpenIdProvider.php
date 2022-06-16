f5d1464f6c8b95252439a4328cb504305f0fad8f
Jérémy Jaouen <jjaouen@centreon.com>
2022-06-16 08:11:27 +0200

    typo

--- src/Core/Security/Domain/Provider/OpenIdProvider.php
+++ src/Core/Security/Domain/Provider/OpenIdProvider.php
@@ -188,7 +188,7 @@ class OpenIdProvider implements OpenIdProviderInterface
         ]);
         if (empty($authorizationCode)) {
             $this->error(
-                'No authorization code return from external provider',
+                'No authorization code returned from external provider',
                 [
                     'provider' => Configuration::NAME
                 ]
