925841b72249f3f445d22985572670262d2ceacb
tcharles <tcharles@centreon.com>
2022-06-28 20:48:12 +0200

    remove duplicated code

--- www/class/centreonHostgroups.class.php
+++ www/class/centreonHostgroups.class.php
@@ -335,6 +335,7 @@ class CentreonHostgroups
         }
         
         $hostgroups = [];
+        // $values structure: ['1,2,3,4'], keeping the foreach in case it could have more than one index
         foreach ($values as $value) {
             $hostgroups = array_merge($hostgroups, explode(',', $value));
         }
@@ -363,16 +364,14 @@ class CentreonHostgroups
         // get list of selected hostgroups
         $listValues = '';
         $queryValues = array();
-
-        foreach ($values as $k => $v) {
-            //As it happens that $v could be like "X,Y" when two hostgroups are selected, we added a second foreach
-            $multiValues = explode(',', $v);
-            foreach ($multiValues as $item) {
-                $ids = explode('-', $item);
-                $listValues .= ':hgId_' . $ids[0] . ', ';
-                $queryValues['hgId_' . $ids[0]] = (int)$ids[0];
-            }
+        
+        foreach ($hostgroups as $item) {
+            // the below explode may not be useful 
+            $ids = explode('-', $item);
+            $listValues .= ':hgId_' . $ids[0] . ', ';
+            $queryValues['hgId_' . $ids[0]] = (int)$ids[0];
         }
+        
         $listValues = rtrim($listValues, ', ');
         $query = 'SELECT hg_id, hg_name FROM hostgroup WHERE hg_id IN (' . $listValues . ') ORDER BY hg_name ';
         $stmt = $this->DB->prepare($query);
