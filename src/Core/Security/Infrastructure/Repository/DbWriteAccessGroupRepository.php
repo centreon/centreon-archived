82a618fcc41f9042bb87752c28b4bfae2e5f4816
Jérémy Jaouen <jjaouen@centreon.com>
2022-06-28 17:25:05 +0200

    create empty repository

---
+++ src/Core/Security/Infrastructure/Repository/DbWriteAccessGroupRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+* Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* For more information : contact@centreon.com
+*
+*/
+
+declare(strict_types=1);
+
+namespace Core\Security\Infrastructure\Repository;
+
+use Centreon\Domain\Contact\Interfaces\ContactInterface;
+use Centreon\Infrastructure\Repository\AbstractRepositoryDRB;
+use Core\Security\Application\Repository\WriteAccessGroupRepositoryInterface;
+
+class DbWriteAccessGroupRepository extends AbstractRepositoryDRB implements WriteAccessGroupRepositoryInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function deleteAccessGroupsForUser(ContactInterface $user): void
+    {
+        //@todo: implements this method
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function insertAccessGroupsForUser(ContactInterface $user, array $accessGroups): void
+    {
+        //@todo: implements this method
+    }
+}
