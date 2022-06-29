909201e3ce578229fcd02af1a11633c06957d982
Jordan CARO <jcaro@centreon.com>
2022-06-29 16:12:08 +0200

    add endpoint to download file

--- www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
+++ www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
@@ -1,24 +1,39 @@
 import { useTranslation } from 'react-i18next';
+import { useAtomValue } from 'jotai';
+import { IconButton } from 'centreon-frontend/packages/centreon-ui/src';
+import { path } from 'ramda';
 
-import { Box, Link, Stack, Tooltip } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
 
 import { labelExportToCsv } from '../../../translatedLabels';
+import { detailsAtom } from '../../detailsAtoms';
 
 const ExportToCsv = (): JSX.Element => {
   const { t } = useTranslation();
 
+  const details = useAtomValue(detailsAtom);
+
+  const openInNewTab = (url): void => {
+    window.open(url, 'noopener', 'noreferrer');
+  };
+
+  const downloadTimelineFile = `${details?.links.endpoints.timeline}/download`;
+
+  const exportToCsv = (): void => {
+    openInNewTab(downloadTimelineFile);
+  };
+
   return (
-    <Box sx={{ width: '100%' }}>
+    <Box>
       <Stack sx={{ alignItems: 'center', padding: 1 }}>
-        <Tooltip title={t(labelExportToCsv)}>
-          <Link
-            data-testid={labelExportToCsv}
-            href="http://localhost:4000/centreon/api/latest/monitoring/hosts/14/services/25/timeline/download"
-          >
-            <FileDownloadIcon style={{ fontSize: 30 }} />
-          </Link>
-        </Tooltip>
+        <IconButton
+          data-testid={labelExportToCsv}
+          title={t(labelExportToCsv)}
+          onClick={exportToCsv}
+        >
+          <FileDownloadIcon style={{ fontSize: 30 }} />
+        </IconButton>
       </Stack>
     </Box>
   );
@@ -26,4 +41,6 @@ const ExportToCsv = (): JSX.Element => {
 
 export default ExportToCsv;
 
+// Snackbar
+// si href non ok => window open
 //     path: /monitoring/hosts/{hostId}/services/{serviceId}/timeline/download
