eec077678d9eb6f03d55cc1f0560ef001dafd489
Tamaz Cheishvili <tamazc@yahoo.com>
2022-07-05 12:23:29 +0200

    Improove csv file generation for graphs

--- www/include/views/graphs/exportData/ExportCSVServiceData.php
+++ www/include/views/graphs/exportData/ExportCSVServiceData.php
@@ -1,184 +1,52 @@
 <?php
-/*
- * Copyright 2005-2015 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
- * GPL Licence 2.0.
+
+/**
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation ; either version 2 of the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses>.
- *
- * Linking this program statically or dynamically with other modules is making a
- * combined work based on this program. Thus, the terms and conditions of the GNU
- * General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this program give Centreon
- * permission to link this program with independent modules to produce an executable,
- * regardless of the license terms of these independent modules, and to copy and
- * distribute the resulting executable under terms of Centreon choice, provided that
- * Centreon also meet, for each linked independent module, the terms  and conditions
- * of the license of that module. An independent module is a module which is not
- * derived from this program. If you modify this program, you may extend this
- * exception to your version of the program, but you are not obliged to do so. If you
- * do not wish to do so, delete this exception statement from your version.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * For more information : contact@centreon.com
- *
  */
 
-function get_error($str)
-{
-    echo $str . "<br />";
-    exit(0);
-}
+require_once realpath(dirname(__FILE__) . '/../../../../../config/centreon.config.php');
+include_once _CENTREON_PATH_ . 'www/class/centreonDB.class.php';
+include_once './functions.php';
 
-require_once realpath(dirname(__FILE__) . "/../../../../../config/centreon.config.php");
-include_once _CENTREON_PATH_ . "www/class/centreonDB.class.php";
+try {
+    //setup
+    $pearDB = new CentreonDB();
 
-$pearDB = new CentreonDB();
-$pearDBO = new CentreonDB("centstorage");
+    //checks
+    session_start();
+    session_write_close();
+    $sid = session_id();
+    checkSession($sid, $pearDB);
 
-session_start();
-session_write_close();
+    //retrieve required params for DB query
+    $chartId = $_GET['chartId'];
+    $indexParam = $_GET['index'] ?? $_POST['index'] ?? 0;
+    $startDate = extractDate($_GET['start']);
+    $endDate = extractDate($_GET['end']);
 
-$sid = session_id();
-if (isset($sid)) {
-    $res = $pearDB->query("SELECT * FROM session WHERE session_id = '" . $sid . "'");
-    if (!$session = $res->fetchRow()) {
-        get_error('bad session id');
-    }
-} else {
-    get_error('need session id !');
-}
+    //retrieve data from database
+    $pearDBO = new CentreonDB('centstorage');
+    $index = findIndex($chartId, $indexParam, $pearDBO);
+    $metrics = getMetricsByIndexId($index, $pearDBO);
+    $rows = getDataByMetrics($metrics, $startDate, $endDate, $pearDBO);
 
-$index = filter_var(
-    $_GET['index'] ?? $_POST['index'] ?? false,
-    FILTER_VALIDATE_INT
-);
-$period = filter_var(
-    $_GET['period'] ?? $_POST['period'] ?? 'today',
-    FILTER_SANITIZE_STRING
-);
-$start = filter_var(
-    $_GET['start'] ?? false,
-    FILTER_VALIDATE_INT
-);
-$end = filter_var(
-    $_GET['end'] ?? false,
-    FILTER_VALIDATE_INT
-);
-$chartId = filter_var(
-    $_GET['chartId'] ?? null,
-    FILTER_SANITIZE_STRING
-);
-
-if (!empty($chartId)) {
-    if (preg_match('/([0-9]+)_([0-9]+)/', $chartId, $matches)) {
-        // Should be allowed chartId matching int_int regexp
-        $hostId = (int)$matches[1];
-        $serviceId = (int)$matches[2];
-
-        // Making sure that splitted values are positive.
-        if ($hostId > 0 && $serviceId > 0) {
-            $query = 'SELECT id'
-                . ' FROM index_data'
-                . ' WHERE host_id = :hostId'
-                . ' AND service_id = :serviceId';
-
-            $stmt = $pearDBO->prepare($query);
-            $stmt->bindValue(':hostId', $hostId, \PDO::PARAM_INT);
-            $stmt->bindValue(':serviceId', $serviceId, \PDO::PARAM_INT);
-            $stmt->execute();
-            while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-                $index = $row['id'];
-            }
-        }
-    } else {
-        die('Resource not found');
-    }
-}
-if ($index !== false) {
-    $stmt = $pearDBO->prepare(
-        'SELECT host_name, service_description FROM index_data WHERE id = :index'
-    );
-    $stmt->bindValue(':index', $index, \PDO::PARAM_INT);
-    $stmt->execute();
-    while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-        $hName = $row["host_name"];
-        $sName = $row["service_description"];
-    }
-
-    header("Content-Type: application/csv-tab-delimited-table");
-    if (isset($hName) && isset($sName)) {
-        header("Content-disposition: filename=" . $hName . "_" . $sName . ".csv");
-    } else {
-        header("Content-disposition: filename=" . $index . ".csv");
-    }
-
-    if ($start === false || $end === false) {
-        die('Start or end time is not consistent or not an integer');
-    }
-
-    $listMetric = array();
-    $datas = array();
-    $listEmptyMetric = array();
-
-    $stmt = $pearDBO->prepare(
-        'SELECT DISTINCT metric_id, metric_name ' .
-        'FROM metrics, index_data ' .
-        'WHERE metrics.index_id = index_data.id AND id = :index ORDER BY metric_name'
-    );
-
-    $stmt->bindValue(':index', $index, \PDO::PARAM_INT);
-    $stmt->execute();
-
-    while ($indexData = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-        $listMetric[$indexData['metric_id']] = $indexData['metric_name'];
-        $listEmptyMetric[$indexData['metric_id']] = '';
-        $stmt2 = $pearDBO->prepare(
-            "SELECT ctime, `value` FROM data_bin WHERE id_metric = :metricId " .
-            "AND ctime >= :start AND ctime < :end"
-        );
-        $stmt2->bindValue(':start', $start, \PDO::PARAM_INT);
-        $stmt2->bindValue(':end', $end, \PDO::PARAM_INT);
-        $stmt2->bindValue(':metricId', $indexData['metric_id'], \PDO::PARAM_INT);
-        $stmt2->execute();
-        while ($data = $stmt2->fetch(\PDO::FETCH_ASSOC)) {
-            $datas[$data["ctime"]][$indexData["metric_id"]] = $data["value"];
-        }
-    }
-}
-
-// Order by timestamp
-ksort($datas);
-foreach ($datas as $key => $data) {
-    $datas[$key] = $data + $listEmptyMetric;
-    // Order by metric
-    ksort($datas[$key]);
-}
-
-print "time;humantime";
-if (count($listMetric)) {
-    ksort($listMetric);
-    print ";" . implode(';', $listMetric);
-}
-print "\n";
-
-foreach ($datas as $ctime => $tab) {
-    print $ctime . ";" . date("Y-m-d H:i:s", $ctime);
-    foreach ($tab as $metric_value) {
-        if ($metric_value !== '') {
-            printf(";%f", $metric_value);
-        } else {
-            print(";");
-        }
-    }
-    print "\n";
+    //dump results
+    $fileName = generateFileNameByIndex($index, $pearDBO);
+    sendDownloadResponse($fileName, ['time', 'humantime', ...$metrics], $rows);
+} catch (\Exception $e) {
+    sendErrorResponse($e->getMessage());
 }
---
+++ www/include/views/graphs/exportData/functions.php
@@ -0,0 +1,257 @@
+<?php
+
+/**
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For more information : contact@centreon.com
+ */
+
+declare(strict_types=1);
+
+/**
+ * Sends a json response with http code 400
+ *
+ * @param mixed $data
+ *
+ * @return void
+ */
+function sendErrorResponse(mixed $data): void
+{
+    header('HTTP/1.1 400 Bad Request', true, 400);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($data);
+}
+
+/**
+ * Sends download stream response to the client
+ *
+ * @param array<int,string>       $heads
+ * @param iterable<array<string>> $rows
+ */
+function sendDownloadResponse(string $fileName, array $heads, iterable $rows): void
+{
+    header('Content-Disposition: attachment;filename="' . $fileName . '";');
+    header('Content-Type: application/csv; charset=UTF-8');
+    header('Pragma: no-cache');
+
+    $f = fopen('php://output', 'w');
+
+    if ($f === false) {
+        throw new \RuntimeException('Unable to write content in output');
+    }
+
+    fputcsv($f, $heads, ';');
+
+    foreach ($rows as $row) {
+        fputcsv($f, $row, ';');
+    }
+
+    fclose($f);
+}
+
+function checkSession(mixed $sid, CentreonDB $pearDB): void
+{
+    if (!is_string($sid)) {
+        throw new \InvalidArgumentException('Session id is required');
+    }
+
+    $pearDB->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    $statement = $pearDB->prepare('SELECT COUNT(1) as session_counter FROM session WHERE session_id = :session_id');
+    $statement->bindParam(':session_id', $sid, \PDO::PARAM_STR);
+    $statement->execute();
+    $row = $statement->fetch();
+
+    $sessionExists = is_array($row) && array_key_exists('session_counter', $row) && (int) $row['session_counter'] === 1;
+
+    if (!$sessionExists) {
+        throw new \InvalidArgumentException('Bad session id');
+    }
+}
+
+/**
+ * @return array<string,int>
+ */
+function extractHostIdAndServiceIdFromChartId(string $chartId): array
+{
+    if ($chartId === '') {
+        throw new \InvalidArgumentException('Chart ID is missing');
+    }
+
+    if (preg_match('/([0-9]+)_([0-9]+)/', $chartId, $matches) !== 1) {
+        throw new \InvalidArgumentException('Unable to parse chart ID');
+    }
+
+    $hostId = (int) $matches[1];
+    $serviceId = (int) $matches[2];
+
+    if ($hostId <= 0 || $serviceId <= 0) {
+        throw new \InvalidArgumentException('Bad formatted chart ID');
+    }
+
+    return ['hostId' => $hostId, 'serviceId' => $serviceId];
+}
+
+function findIndexByHostIdAndServiceId(int $hostId, int $serviceId, CentreonDB $pearDBO): int
+{
+    $query = 'SELECT id FROM index_data WHERE host_id = :hostId AND service_id = :serviceId';
+    $stmt = $pearDBO->prepare($query);
+    $stmt->bindValue(':hostId', $hostId, \PDO::PARAM_INT);
+    $stmt->bindValue(':serviceId', $serviceId, \PDO::PARAM_INT);
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+
+    if (!is_array($row) || !array_key_exists('id', $row)) {
+        throw new \InvalidArgumentException('Resource not found');
+    }
+
+    return (int) $row['id'];
+}
+
+function findIndexByChartId(string $chartId, CentreonDB $pearDBO): int
+{
+    $hostAndServiceIds = extractHostIdAndServiceIdFromChartId($chartId);
+
+    return findIndexByHostIdAndServiceId($hostAndServiceIds['hostId'], $hostAndServiceIds['serviceId'], $pearDBO);
+}
+
+function generateFileNameByIndex(int $index, CentreonDB $pearDBO): string
+{
+    $stmt = $pearDBO->prepare('SELECT host_name, service_description FROM index_data WHERE id = :index');
+    $stmt->bindValue(':index', $index, \PDO::PARAM_INT);
+    $stmt->execute();
+    $row = $stmt->fetch();
+
+    if (!is_array($row) || !array_key_exists('host_name', $row)) {
+        throw new \InvalidArgumentException('host_name could not be found');
+    }
+
+    if (!array_key_exists('service_description', $row)) {
+        throw new \InvalidArgumentException('service_description could not be found');
+    }
+
+    $hostName = $row['host_name'];
+    $serviceDescription = $row['service_description'];
+
+    if ($hostName !== '' && $serviceDescription !== '') {
+        return sprintf('%s_%s.csv', $hostName, $serviceDescription);
+    }
+
+    return sprintf('%s.csv', $index);
+}
+
+/**
+ * @return array<string,string>
+ */
+function getMetricsByIndexId(int $indexId, CentreonDB $pearDBO): array
+{
+    $query = 'SELECT DISTINCT metric_id, metric_name  FROM metrics, index_data ';
+    $query .= ' WHERE metrics.index_id = index_data.id AND id = :index_id ORDER BY metric_id';
+    $stmt = $pearDBO->prepare($query);
+    $stmt->bindValue(':index_id', $indexId, \PDO::PARAM_INT);
+    $stmt->execute();
+
+    return $stmt->fetchAll(\PDO::FETCH_KEY_PAIR);
+}
+
+/**
+ * Retrieves raw data_bin with filters
+ *
+ * @param  array<int, string> $metrics
+ * @return iterable<array<string,string>>
+ */
+function getDataByMetrics(array $metrics, int $startDate, int $endDate, CentreonDB $pearDBO): iterable
+{
+    $pearDBO->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+    $pearDBO->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, true);
+
+    $columns = ['ctime AS time'];
+    foreach ($metrics as $metricId => $metricName) {
+        $columns[] = sprintf('AVG(CASE WHEN id_metric = %d THEN `value` end) AS %s', $metricId, $metricName);
+    }
+    $query = sprintf(
+        'SELECT %s FROM data_bin WHERE ctime >= :start AND ctime < :end GROUP BY time',
+        join(',', $columns)
+    );
+
+    $stmt = $pearDBO->prepare($query);
+    $stmt->bindValue(':start', $startDate, \PDO::PARAM_INT);
+    $stmt->bindValue(':end', $endDate, \PDO::PARAM_INT);
+    $stmt->execute();
+
+    foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $dataBin) {
+        yield formatDataBin($dataBin);
+    }
+
+    $stmt->closeCursor();
+}
+
+/**
+ * Formats raw data from data_bin table
+ *
+ * @param  array<string> $dataBin
+ * @return array<string>
+ */
+function formatDataBin(array $dataBin): array
+{
+    $formattedData = [
+        'time' => $dataBin['time'],
+        'humantime' => date('Y-m-d H:i:s', (int) $dataBin['time']),
+
+    ];
+
+    foreach ($dataBin as $columnName => $columnValue) {
+        if ($columnName !== 'time') {
+            $formattedData[$columnName] = sprintf('%f', $columnValue);
+        }
+    }
+
+    return $formattedData;
+}
+
+function extractDate(mixed $data): int
+{
+    $date = filter_var($data ?? 0, FILTER_VALIDATE_INT);
+
+    if (!is_int($date) || $date <= 0) {
+        throw new \InvalidArgumentException('Date is not consistent');
+    }
+
+    return $date;
+}
+
+/**
+ * Finds index with chartId in DB. If not $defaultIndex is returned
+ *
+ * @param string     $chartId      Chart identifier with pattern {hostId}_{serviceId}
+ * @param string     $defaultIndex default value for index. Would be returned if chartId handling fails
+ * @param CentreonDB $pearDBO      Database connection
+ *
+ * @return int
+ */
+function findIndex(string $chartId, string $defaultIndex, CentreonDB $pearDBO): int
+{
+    $sanitizedChartId = filter_var($chartId, FILTER_SANITIZE_STRING);
+    if (is_string($sanitizedChartId)) {
+        return findIndexByChartId($sanitizedChartId, $pearDBO);
+    }
+
+    $sanitizedDefaultIndex = filter_var($defaultIndex, FILTER_VALIDATE_INT);
+    if (is_int($sanitizedDefaultIndex)) {
+        return $sanitizedDefaultIndex;
+    }
+
+    return 0;
+}
