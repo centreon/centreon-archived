55ac468ada124343a5f0aa81a7a4dba297f83a2a
elmahdiabbassi <emabbassi-ext@centreon.com>
2022-07-15 10:54:07 +0100

    fix issues

--- www/include/configuration/configObject/host_dependency/DB-Func.php
+++ www/include/configuration/configObject/host_dependency/DB-Func.php
@@ -123,11 +123,11 @@ function multipleHostDependencyInDB($dependencies = array(), $nbrDup = array())
                         "WHERE dependency_dep_id = " . $key;
                     $dbResult = $pearDB->query($query);
                     $fields["dep_serviceChilds"] = "";
+                    $statement = $pearDB->prepare("INSERT INTO dependency_serviceChild_relation " .
+                        " VALUES (:max_dep_id, :service_id, :host_host_id)");
                     while ($service = $dbResult->fetch()) {
-                        $statement = $pearDB->prepare("INSERT INTO dependency_serviceChild_relation " .
-                            " VALUES (:max_dep_id, :service_service_id, :host_host_id)");
                         $statement->bindValue(':max_dep_id', (int)$maxId["MAX(dep_id)"], \PDO::PARAM_INT);
-                        $statement->bindValue(':service_service_id', (int)$service["service_service_id"], \PDO::PARAM_INT);
+                        $statement->bindValue(':service_id', (int)$service["service_service_id"], \PDO::PARAM_INT);
                         $statement->bindValue(':host_host_id', (int)$service["host_host_id"], \PDO::PARAM_INT);
                         $statement->execute();
 
@@ -139,9 +139,9 @@ function multipleHostDependencyInDB($dependencies = array(), $nbrDup = array())
                         "WHERE dependency_dep_id = '" . $key . "'";
                     $dbResult = $pearDB->query($query);
                     $fields["dep_hostParents"] = "";
+                    $statement = $pearDB->prepare("INSERT INTO dependency_hostParent_relation " .
+                        "VALUES (:max_dep_id, :host_host_id)");
                     while ($host = $dbResult->fetch()) {
-                        $statement = $pearDB->prepare("INSERT INTO dependency_hostParent_relation " .
-                            "VALUES (:max_dep_id, :host_host_id)");
                         $statement->bindValue(':max_dep_id', (int)$maxId["MAX(dep_id)"], \PDO::PARAM_INT);
                         $statement->bindValue(':host_host_id', (int)$host["host_host_id"], \PDO::PARAM_INT);
                         $statement->execute();
@@ -153,9 +153,9 @@ function multipleHostDependencyInDB($dependencies = array(), $nbrDup = array())
                         "WHERE dependency_dep_id = '" . $key . "'";
                     $dbResult = $pearDB->query($query);
                     $fields["dep_hostChilds"] = "";
+                    $statement = $pearDB->prepare("INSERT INTO dependency_hostChild_relation " .
+                        "VALUES (:max_dep_id, :host_host_id)");
                     while ($host = $dbResult->fetch()) {
-                        $statement = $pearDB->prepare("INSERT INTO dependency_hostChild_relation " .
-                            "VALUES (:max_dep_id, :host_host_id)");
                         $statement->bindValue(':max_dep_id', (int)$maxId["MAX(dep_id)"], \PDO::PARAM_INT);
                         $statement->bindValue(':host_host_id', (int)$host["host_host_id"], \PDO::PARAM_INT);
                         $statement->execute();
