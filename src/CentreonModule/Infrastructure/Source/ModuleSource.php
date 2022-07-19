4be4e8e885e8051cbf8e44d74c280a69d225cd82
Kevin Duret <kduret@centreon.com>
2022-07-19 16:33:21 +0200

    manage remove

--- src/CentreonModule/Infrastructure/Source/ModuleSource.php
+++ src/CentreonModule/Infrastructure/Source/ModuleSource.php
@@ -1,36 +1,21 @@
 <?php
+
 /*
- * Copyright 2005-2019 Centreon
- * Centreon is developped by : Julien Mathis and Romain Le Merlus under
- * GPL Licence 2.0.
+ * Copyright 2005 - 2022 Centreon (https://www.centreon.com/)
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation ; either version 2 of the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, see <http://www.gnu.org/licenses>.
- *
- * Linking this program statically or dynamically with other modules is making a
- * combined work based on this program. Thus, the terms and conditions of the GNU
- * General Public License cover the whole combination.
- *
- * As a special exception, the copyright holders of this program give Centreon
- * permission to link this program with independent modules to produce an executable,
- * regardless of the license terms of these independent modules, and to copy and
- * distribute the resulting executable under terms of Centreon choice, provided that
- * Centreon also meet, for each linked independent module, the terms  and conditions
- * of the license of that module. An independent module is a module which is not
- * derived from this program. If you modify this program, you may extend this
- * exception to your version of the program, but you are not obliged to do so. If you
- * do not wish to do so, delete this exception statement from your version.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  * For more information : contact@centreon.com
- *
  *
  */
 
@@ -48,7 +33,6 @@ class ModuleSource extends SourceAbstract
     public const PATH = 'www/modules/';
     public const PATH_WEB = 'modules/';
     public const CONFIG_FILE = 'conf.php';
-    public const LICENSE_FILE = 'license/merethis_lic.zl';
 
     /**
      * @var array<string,mixed>
@@ -79,9 +63,9 @@ class ModuleSource extends SourceAbstract
     }
 
     /**
-     * Install module
-     *
      * {@inheritDoc}
+     *
+     * Install module
      *
      * @throws \Exception
      */
@@ -89,20 +73,18 @@ class ModuleSource extends SourceAbstract
     {
         $this->installOrUpdateDependencies($id);
 
-        ($this->installer)($id)->install();
-
-        $this->initInfo();
-
-        return $this->getDetail($id);
+        return parent::install($id);
     }
 
     /**
-     * Remove module
-     *
      * {@inheritDoc}
+     *
+     * Remove module
      */
     public function remove(string $id): void
     {
+        $this->validateRemovalRequirementsOrFail($id);
+
         $recordId = $this->db
             ->getRepository(ModulesInformationsRepository::class)
             ->findIdByName($id)
@@ -112,9 +94,9 @@ class ModuleSource extends SourceAbstract
     }
 
     /**
-     * Update module
-     *
      * {@inheritDoc}
+     *
+     * Update module
      *
      * @throws \Exception
      */
@@ -306,7 +288,7 @@ class ModuleSource extends SourceAbstract
         foreach ($sortedDependencies as $dependency) {
             $dependencyDetails = $this->getDetail($dependency);
             if ($dependencyDetails === null) {
-                throw new \Exception('Cannot find details of module ' . $dependency);
+                throw ModuleException::cannotFindModuleDetails($dependency);
             }
 
             if (! $dependencyDetails->isInstalled()) {
@@ -340,7 +322,7 @@ class ModuleSource extends SourceAbstract
 
         $moduleDetails = $this->getDetail($moduleId);
         if ($moduleDetails === null) {
-            throw new \Exception('Module ' . $moduleId . ' is required');
+            throw ModuleException::moduleIsMissing($moduleId);
         }
 
         foreach ($moduleDetails->getDependencies() as $dependency) {
@@ -355,5 +337,28 @@ class ModuleSource extends SourceAbstract
         }
 
         return $dependencies;
+    }
+
+    /**
+     * Validate requirements before remove (dependencies)
+     *
+     * @param string $moduleId (example: centreon-license-manager)
+     *
+     * @throws \Exception
+     */
+    private function validateRemovalRequirementsOrFail(string $moduleId): void
+    {
+        $dependenciesToRemove = [];
+
+        $modules = $this->getList();
+        foreach ($modules as $module) {
+            if ($module->isInstalled() && in_array($moduleId, $module->getDependencies())) {
+                $dependenciesToRemove[] = $module->getName();
+            }
+        }
+
+        if (! empty($dependenciesToRemove)) {
+            throw ModuleException::modulesNeedToBeRemovedFirst($dependenciesToRemove);
+        }
     }
 }
