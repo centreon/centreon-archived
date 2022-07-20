ffa62de42aa8c8756c30cc0ba1fbcdfc41e2ea7d
nouha <nelabrouki-ext@centreon.com>
2022-07-20 11:09:16 +0000

    test

--- www/front_src/src/Header/index.tsx
+++ www/front_src/src/Header/index.tsx
@@ -59,7 +59,7 @@ const Header = (): JSX.Element => {
 
   return (
     <header className={classes.header} ref={headerRef}>
-      <div>testoooo</div>
+      <div>testoo</div>
       <div className={classes.container}>
         <div className={classes.pollerContainer}>
           <PollerMenu />
