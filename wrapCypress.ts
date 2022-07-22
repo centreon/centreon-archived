f028006fb00c87df99f9ad73688a56f796013c16
nouha <nelabrouki-ext@centreon.com>
2022-07-22 12:55:54 +0000

    update wrapper

--- wrapCypress.ts
+++ wrapCypress.ts
@@ -1,12 +1,30 @@
 const cypress = require('cypress');
 
-cypress.cli.parseRunArguments(process.argv.slice(2)).then((res) => {
-  return cypress
-    .run(res)
-    .then((testRes) => {
-      process.exit(testRes.totalFailed);
-    })
-    .catch((err) => {
-      process.exit(1);
-    });
-});
+// cypress.cli.parseRunArguments(process.argv.slice(2)).then((res) => {
+//   cypress
+//     .run(res)
+//     .then((testRes) => {
+//       return process.exit(testRes.totalFailed);
+//     })
+//     .catch((_) => {
+//       return process.exit(1);
+//     });
+// });
+
+cypress.cli
+  .parseRunArguments(process.argv.slice(2))
+  .then((runOptions) => {
+    cypress
+      .run(runOptions)
+      .then(async (results) => {
+        if (results.totalFailed > 0) {
+          process.exit(results.totalFailed);
+        }
+      })
+      .catch((error) => {
+        process.exit(1);
+      });
+  })
+  .catch((err) => {
+    process.exit(1);
+  });
