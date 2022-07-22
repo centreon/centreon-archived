b4a29322d9f2f5940faea3557549e5cc2ba3aa69
nouha <nelabrouki-ext@centreon.com>
2022-07-22 13:52:17 +0000

    fix test for jest

--- www/front_src/src/Header/userMenu/index.test.tsx
+++ www/front_src/src/Header/userMenu/index.test.tsx
@@ -109,7 +109,7 @@ describe('User Menu', () => {
     userEvent.click(screen.getByLabelText(labelProfile));
 
     await waitFor(() => {
-      expect(screen.getByText('admi')).toBeInTheDocument();
+      expect(screen.getByText('admin')).toBeInTheDocument();
     });
 
     await waitFor(() => {
