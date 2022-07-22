7a2890117748dc761537144e6ba6d3c5e0330c73
nouha <nelabrouki-ext@centreon.com>
2022-07-22 12:07:29 +0000

    update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -18,12 +18,6 @@ module.exports = defineConfig({
     specPattern: './www/front_src/src/**/*.cypress.spec.tsx',
     supportFile: './cypress/support/component.tsx',
   },
-  reporter: 'junit',
-  reporterOptions: {
-    jenkinsMode: true,
-    mochaFile: 'results/my-testoo.xml',
-    toConsole: true,
-  },
   video: true,
   videosFolder: 'cypress/results/videos',
 });
