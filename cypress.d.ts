dc4b021d76eb931ff60c095b759219feb707e2f7
nouha <nelabrouki-ext@centreon.com>
2022-07-25 08:23:59 +0000

    remove comments

--- cypress.d.ts
+++ cypress.d.ts
@@ -4,11 +4,6 @@ import { MemoryRouterProps } from 'react-router-dom';
 declare global {
   namespace Cypress {
     interface Chainable {
-      /**
-       * Mounts a React node
-       * @param component React Node to mount
-       * @param options Additional options to pass into mount
-       */
       mount: (
         component: React.ReactNode,
         options?: MountOptions & { routerProps?: MemoryRouterProps },
