bd7fe0c8c85ad5e549a3c44e16faa64756b66234
Tamaz Cheishvili <tamazc@yahoo.com>
2022-08-09 10:44:50 +0200

    Refactoring unit tests for DbReadResourceRepository

--- tests/php/Core/Resources/Infrastructure/Repository/DbReadResourceRepositoryTest.php
+++ tests/php/Core/Resources/Infrastructure/Repository/DbReadResourceRepositoryTest.php
@@ -21,105 +21,165 @@
 
 declare(strict_types=1);
 
-namespace Tests\Core\Resources\Infrastructure\Repository;
+namespace Tests\Core\Resources\Infrastructure\Repository\DbReadResourceRepository\FindResources;
 
 use Centreon\Domain\Monitoring\ResourceFilter;
 use Centreon\Domain\RequestParameters\Interfaces\RequestParametersInterface;
 use Centreon\Domain\RequestParameters\RequestParameters;
-use PHPUnit\Framework\TestCase;
-use Core\Resources\Infrastructure\Repository\DbReadResourceRepository;
 use Centreon\Infrastructure\DatabaseConnection;
 use Centreon\Infrastructure\RequestParameters\SqlRequestParametersTranslator;
 use Core\Domain\RealTime\Model\ResourceTypes\ServiceResourceType;
-use Centreon\Domain\Contact\Interfaces\ContactInterface;
+use Core\Resources\Infrastructure\Repository\DbReadResourceRepository;
 
-class DbReadResourceRepositoryTest extends TestCase
+function generateAccessGroupSubQuery(array $accessGroupIds): string
 {
-    private DbReadResourceRepository $repository;
-    private DatabaseConnection $dbConnection;
-    private SqlRequestParametersTranslator $paramsTranslator;
-    private ServiceResourceType $serviceResourceType;
-
-    /**
-     * @test
-     */
-    public function resourceListShouldBeEmptyWhenContactIsNotSet(): void
-    {
-        $this->createDbConnectionMock();
-        $this->createRequestParamTranslatorMock();
-        $this->createServiceResourceTypeMock();
-        $this->repository = new DbReadResourceRepository(
-            $this->dbConnection,
-            $this->paramsTranslator,
-            new \ArrayObject([$this->serviceResourceType])
-        );
-
-        $resources = $this->repository->findResources(new ResourceFilter());
-
-        $this->assertEquals([], $resources);
-    }
-
-    /**
-     * @test
-     */
-    public function resourceListShouldBeEmptyWhenContactIsNotAdmin(): void
-    {
-        $this->createDbConnectionMock();
-        $this->createRequestParamTranslatorMock();
-        $this->createServiceResourceTypeMock();
-        $this->repository = new DbReadResourceRepository(
-            $this->dbConnection,
-            $this->paramsTranslator,
-            new \ArrayObject([$this->serviceResourceType])
-        );
-        $this->repository->setContact($this->mockContact(false));
-
-        $resources = $this->repository->findResources(new ResourceFilter());
-
-        $this->assertEquals([], $resources);
-    }
-
-    private function createDbConnectionMock(): void
-    {
-        $this->dbConnection = $this->createMock(DatabaseConnection::class);
-    }
-
-    private function createRequestParamTranslatorMock(): void
-    {
-        $requestParams = $this->createMock(RequestParametersInterface::class);
-
-        $requestParams
-            ->expects($this->once())
-            ->method('setConcordanceStrictMode')
-            ->with(RequestParameters::CONCORDANCE_MODE_STRICT)
-            ->willReturn($requestParams);
-
-        $requestParams
-            ->expects($this->once())
-            ->method('setConcordanceErrorMode')
-            ->with(RequestParameters::CONCORDANCE_ERRMODE_SILENT)
-            ->willReturn($requestParams);
-
-        $this->paramsTranslator = $this->createMock(SqlRequestParametersTranslator::class);
-
-        $this->paramsTranslator
-            ->expects($this->once())
-            ->method('getRequestParameters')
-            ->willReturn($requestParams);
-    }
-
-    private function createServiceResourceTypeMock(): void
-    {
-        $this->serviceResourceType = $this->createMock(ServiceResourceType::class);
-    }
-
-    private function mockContact(bool $isAdmin): ContactInterface
-    {
-        $contact = $this->createMock(ContactInterface::class);
-        $contact
-            ->method('isAdmin')
-            ->willReturn($isAdmin);
-
-        return $contact;
-    }
+    return 'AND EXISTS (
+          SELECT 1 FROM `:dbstg`.centreon_acl acl WHERE
+              (
+                (resources.type = 0 AND resources.parent_id = acl.host_id AND resources.id = acl.service_id)
+                OR
+                (resources.type = 2 AND resources.parent_id = acl.host_id AND resources.id = acl.service_id)
+                OR
+                (resources.type = 1 AND resources.id = acl.host_id AND acl.service_id IS NULL)
+                OR
+                (resources.type = 4 AND resources.id = acl.service_id)
+              )
+              AND acl.group_id IN (' . implode(', ', $accessGroupIds) . ')
+          LIMIT 1
+        ) ';
 }
+
+function generateExpectedSQLQuery(array $accessGroupIds): string
+{
+    $accessGroupRequest = empty($accessGroupIds) ? '' :  generateAccessGroupSubQuery($accessGroupIds);
+
+    $request = 'SELECT SQL_CALC_FOUND_ROWS DISTINCT
+            resources.resource_id,
+            resources.name,
+            resources.alias,
+            resources.address,
+            resources.id,
+            resources.internal_id,
+            resources.parent_id,
+            resources.parent_name,
+            parent_resource.status AS `parent_status`,
+            parent_resource.alias AS `parent_alias`,
+            parent_resource.status_ordered AS `parent_status_ordered`,
+            parent_resource.address AS `parent_fqdn`,
+            severities.id AS `severity_id`,
+            severities.level AS `severity_level`,
+            severities.name AS `severity_name`,
+            severities.type AS `severity_type`,
+            severities.icon_id AS `severity_icon_id`,
+            resources.type,
+            resources.status,
+            resources.status_ordered,
+            resources.status_confirmed,
+            resources.in_downtime,
+            resources.acknowledged,
+            resources.passive_checks_enabled,
+            resources.active_checks_enabled,
+            resources.notifications_enabled,
+            resources.last_check,
+            resources.last_status_change,
+            resources.check_attempts,
+            resources.max_check_attempts,
+            resources.notes,
+            resources.notes_url,
+            resources.action_url,
+            resources.output,
+            resources.poller_id,
+            resources.has_graph,
+            instances.name AS `monitoring_server_name`,
+            resources.enabled,
+            resources.icon_id,
+            resources.severity_id
+        FROM `:dbstg`.`resources`
+        LEFT JOIN `:dbstg`.`resources` parent_resource
+            ON parent_resource.id = resources.parent_id
+        LEFT JOIN `:dbstg`.`severities`
+            ON `severities`.severity_id = `resources`.severity_id
+        LEFT JOIN `:dbstg`.`resources_tags` AS rtags
+            ON `rtags`.resource_id = `resources`.resource_id
+        INNER JOIN `:dbstg`.`instances`
+            ON `instances`.instance_id = `resources`.poller_id WHERE ' .
+        " resources.name NOT LIKE '\_Module\_%'
+            AND resources.parent_name NOT LIKE '\_Module\_BAM%'
+            AND resources.enabled = 1 AND resources.type != 3 " .
+        $accessGroupRequest .
+            'ORDER BY resources.status_ordered DESC, resources.name ASC';
+
+    return $request;
+}
+
+it('findResources method should fetch resources', function () {
+    $statement = $this->createMock(\PDOStatement::class);
+    $statement->method('fetchColumn')->willReturn(10);
+
+    $dbConnection = $this->createMock(DatabaseConnection::class);
+    $dbConnection->expects($this->once())->method('getStorageDbName')->willReturn(':dbstg');
+    $dbConnection->expects($this->once())->method('getCentreonDbName')->willReturn(':db');
+    $dbConnection->expects($this->once())->method('prepare')->with(generateExpectedSQLQuery([]))
+        ->willReturn($statement);
+    $dbConnection->expects($this->once())->method('query')->with('SELECT FOUND_ROWS()')
+        ->willReturn($statement);
+    $serviceResourceType = $this->createMock(ServiceResourceType::class);
+    $requestParams = $this->createMock(RequestParametersInterface::class);
+    $requestParams
+        ->expects($this->once())
+        ->method('setConcordanceStrictMode')
+        ->with(RequestParameters::CONCORDANCE_MODE_STRICT)
+        ->willReturn($requestParams);
+    $requestParams
+        ->expects($this->once())
+        ->method('setConcordanceErrorMode')
+        ->with(RequestParameters::CONCORDANCE_ERRMODE_SILENT)
+        ->willReturn($requestParams);
+    $paramsTranslator = $this->createMock(SqlRequestParametersTranslator::class);
+    $paramsTranslator->method('getRequestParameters')->willReturn($requestParams);
+    $repository = new DbReadResourceRepository(
+        $dbConnection,
+        $paramsTranslator,
+        new \ArrayObject([$serviceResourceType])
+    );
+
+    $resources = $repository->findResources(new ResourceFilter());
+
+    expect($resources)->toBe([]);
+});
+
+it('findResourcesByAccessGroupIds method should fetch resources', function () {
+    $statement = $this->createMock(\PDOStatement::class);
+    $statement->method('fetchColumn')->willReturn(10);
+
+    $dbConnection = $this->createMock(DatabaseConnection::class);
+    $dbConnection->expects($this->once())->method('getStorageDbName')->willReturn(':dbstg');
+    $dbConnection->expects($this->once())->method('getCentreonDbName')->willReturn(':db');
+    $dbConnection->expects($this->once())->method('prepare')->with(generateExpectedSQLQuery(['1', '4']))
+        ->willReturn($statement);
+    $dbConnection->expects($this->once())->method('query')->with('SELECT FOUND_ROWS()')
+        ->willReturn($statement);
+    $serviceResourceType = $this->createMock(ServiceResourceType::class);
+    $requestParams = $this->createMock(RequestParametersInterface::class);
+    $requestParams
+        ->expects($this->once())
+        ->method('setConcordanceStrictMode')
+        ->with(RequestParameters::CONCORDANCE_MODE_STRICT)
+        ->willReturn($requestParams);
+    $requestParams
+        ->expects($this->once())
+        ->method('setConcordanceErrorMode')
+        ->with(RequestParameters::CONCORDANCE_ERRMODE_SILENT)
+        ->willReturn($requestParams);
+    $paramsTranslator = $this->createMock(SqlRequestParametersTranslator::class);
+    $paramsTranslator->method('getRequestParameters')->willReturn($requestParams);
+    $repository = new DbReadResourceRepository(
+        $dbConnection,
+        $paramsTranslator,
+        new \ArrayObject([$serviceResourceType])
+    );
+
+    $resources = $repository->findResourcesByAccessGroupIds(new ResourceFilter(), ['1', '4']);
+
+    expect($resources)->toBe([]);
+});
