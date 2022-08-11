772888ab77fb5f6707c6fc40fb4b21d5aa258c40
jcaro <jcaro@centreon.com>
2022-08-11 17:56:49 +0200

    Update www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
    Co-authored-by: Bruno d'Auria <bdauria@centreon.com>

--- www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
+++ www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { useAtomValue } from 'jotai';
-import { IconButton } from 'centreon-frontend/packages/centreon-ui/src';
+import { IconButton } from '@centreon/ui';
 
 import { Box } from '@mui/material';
 import SaveAsImageIcon from '@mui/icons-material/SaveAlt';
