ddf30677eac9bdbdf78bc7f217eb9da768d5b932
alaunois <alaunois@centreon.com>
2022-08-11 10:13:37 +0200

    fix(conf) fix encoding in template service listing (#11558)
    * fix encoding
    
    * remove useless function

--- www/include/configuration/configObject/service_template_model/listServiceTemplateModel.php
+++ www/include/configuration/configObject/service_template_model/listServiceTemplateModel.php
@@ -49,10 +49,7 @@ include "./include/common/autoNumLimit.php";
 
 $o = "";
 
-$search = filter_var(
-    $_POST['searchST'] ?? $_GET['searchST'] ?? $centreon->historySearch[$url]['search'] ?? '',
-    FILTER_SANITIZE_STRING
-);
+$search =  htmlspecialchars($_POST['searchST'] ?? $_GET['searchST'] ?? $centreon->historySearch[$url]['search'] ?? '');
 
 $displayLocked = filter_var(
     $_POST['displayLocked'] ?? $_GET['displayLocked'] ?? 'off',
@@ -229,11 +226,11 @@ for ($i = 0; $service = $dbResult->fetch(); $i++) {
     $elemArr[$i] = array(
         "MenuClass" => "list_" . $style,
         "RowMenu_select" => $selectedElements->toHtml(),
-        "RowMenu_desc" => CentreonUtils::escapeSecure($service["service_description"]),
-        "RowMenu_alias" => CentreonUtils::escapeSecure($service["service_alias"]),
-        "RowMenu_parent" => CentreonUtils::escapeSecure($tplStr),
+        "RowMenu_desc" => htmlentities($service["service_description"]),
+        "RowMenu_alias" => htmlentities($service["service_alias"]),
+        "RowMenu_parent" => htmlentities($tplStr),
         "RowMenu_icon" => $svc_icon,
-        "RowMenu_retry" => CentreonUtils::escapeSecure(
+        "RowMenu_retry" => htmlentities(
             "$normal_check_interval $normal_units / $retry_check_interval $retry_units"
         ),
         "RowMenu_attempts" => getMyServiceField($service['service_id'], "service_max_check_attempts"),
