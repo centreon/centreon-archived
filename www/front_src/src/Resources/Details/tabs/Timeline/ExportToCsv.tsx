90e5c4119d5a345b3214ed9343522a0ae8f19387
Jordan CARO <jcaro@centreon.com>
2022-08-18 14:46:05 +0200

    Add start and end date in the path to download CSV file

--- www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
+++ www/front_src/src/Resources/Details/tabs/Timeline/ExportToCsv.tsx
@@ -7,15 +7,25 @@ import SaveAsImageIcon from '@mui/icons-material/SaveAlt';
 import { labelExportToCSV } from '../../../translatedLabels';
 import { detailsAtom } from '../../detailsAtoms';
 import ResourceActionButton from '../../../Actions/Resource/ResourceActionButton';
+import {
+  getDatesDerivedAtom,
+  selectedTimePeriodAtom,
+} from '../../../Graph/Performance/TimePeriods/timePeriodAtoms';
 
 const ExportToCsv = (): JSX.Element => {
   const { t } = useTranslation();
 
   const details = useAtomValue(detailsAtom);
-  const downloadTimelineFile = `${details?.links.endpoints.timeline}/download`;
+
+  const getIntervalDates = useAtomValue(getDatesDerivedAtom);
+  const selectedTimePeriod = useAtomValue(selectedTimePeriodAtom);
+
+  const [start, end] = getIntervalDates(selectedTimePeriod);
+
+  const timelineFileCsv = `${details?.links.endpoints.timeline}/download?search=start_date=${start}&end_date=${end}`;
 
   const exportToCsv = (): void => {
-    window.open(downloadTimelineFile, 'noopener', 'noreferrer');
+    window.open(timelineFileCsv, 'noopener', 'noreferrer');
   };
 
   return (
