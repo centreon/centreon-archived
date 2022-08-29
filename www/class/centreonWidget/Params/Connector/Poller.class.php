7bec9f3000ea8fb7fc807e4c93d051e53ce6f898
Stéphane Duret <sduret@centreon.com>
2022-08-29 13:54:35 +0200

    Update www/class/centreonWidget/Params/Connector/Poller.class.php
    Co-authored-by: tcharles <tcharles@centreon.com>

--- www/class/centreonWidget/Params/Connector/Poller.class.php
+++ www/class/centreonWidget/Params/Connector/Poller.class.php
@@ -89,7 +89,7 @@ class CentreonWidgetParamsConnectorPoller extends CentreonWidgetParamsList
                     );
                 }
 
-                $tab = [];
+                $tab = [null => null];
                 while (($record = $statement->fetch(\PDO::FETCH_ASSOC)) !== false) {
                     $tab[$record['id']] = $record['name'];
                 }
