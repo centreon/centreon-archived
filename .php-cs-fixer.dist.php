6c08a868761787e8ee75f704a9154c3e3a1c43a5
dmyios <diosypenko@centreon.com>
2022-08-30 10:54:06 +0200

    fix

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -19,10 +19,16 @@
  *
  */
 
-use Centreon\PhpCsFixer\PhpCsFixerHelper;
+use Centreon\PhpCsFixer\PhpCsFixerRuleSet;
+use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-  ->in(__DIR__ . '/src/Core');
+    ->in(__DIR__ . '/src/Core');
 
-return PhpCsFixerHelper::styles($finder);
+$rules = PhpCsFixerRuleSet::getRules();
+
+return (new Config())
+    ->setFinder($finder)
+    ->setRiskyAllowed(true)
+    ->setRules($rules);
