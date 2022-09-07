f74cbd2160b6023c6d7939d748fdd9f4b25abfeb
nouha <nelabrouki-ext@centreon.com>
2022-09-07 08:32:00 +0000

    fix eslint

--- www/front_src/src/Resources/Details/tabs/Timeline/index.tsx
+++ www/front_src/src/Resources/Details/tabs/Timeline/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { prop, isEmpty, path, isNil, equals } from 'ramda';
