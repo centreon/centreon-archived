150d2793b277c3a2ffb8589f18ee6c9ac4a6d38a
Tamaz Cheishvili <tamazc@yahoo.com>
2022-09-14 14:49:44 +0200

    Update unit test

--- tests/php/Centreon/Domain/Monitoring/CommandLineTraitTest.php
+++ tests/php/Centreon/Domain/Monitoring/CommandLineTraitTest.php
@@ -180,7 +180,7 @@ class CommandLineTraitTest extends TestCase
             . $this->replacementValue . ' -f extra options';
 
         $this->expectException(MonitoringServiceException::class);
-        $this->expectExceptionMessage('Macro passwords cannot be detected');
+        $this->expectExceptionMessage('Configuration has changed');
 
         $this->buildCommandLineFromConfiguration(
             $this->configurationCommand,
