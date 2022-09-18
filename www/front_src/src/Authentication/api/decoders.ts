f418af221b8fbd57f4d33d5890799f43c5649f0d
Mohamed El Meziani <melmeziani-ext@centreon.com>
2022-09-18 19:53:24 +0000

    update decoders

--- www/front_src/src/Authentication/api/decoders.ts
+++ www/front_src/src/Authentication/api/decoders.ts
@@ -2,9 +2,12 @@ import { JsonDecoder } from 'ts.data.json';
 
 import { PasswordExpiration, PasswordSecurityPolicy } from '../Local/models';
 import {
+  AuthConditions,
   AuthorizationRule,
+  Endpoint,
   NamedEntity,
   OpenidConfiguration,
+  RolesMapping,
 } from '../Openid/models';
 import { WebSSOConfiguration } from '../WebSSO/models';
 
@@ -58,6 +61,79 @@ const getNamedEntityDecoder = (
     title,
   );
 
+const endpoint = JsonDecoder.object<Endpoint>(
+  {
+    customEndpoint: JsonDecoder.string,
+    type: JsonDecoder.string,
+  },
+  'Endpoint',
+  {
+    customEndpoint: 'custom_endpoint',
+    type: 'type',
+  },
+);
+
+const relation = JsonDecoder.object<AuthorizationRule>(
+  {
+    accessGroup: getNamedEntityDecoder('Access group'),
+    claimValue: JsonDecoder.string,
+  },
+  'Relation',
+  {
+    accessGroup: 'access_group',
+    claimValue: 'claim_value',
+  },
+);
+
+// auth conditions
+
+const authConditions = JsonDecoder.object<AuthConditions>(
+  {
+    blacklistClientAddresses: JsonDecoder.array(
+      JsonDecoder.string,
+      'blacklist client addresses',
+    ),
+    conditionsAttributePath: JsonDecoder.string,
+    conditionsAuthorizedValues: JsonDecoder.array(
+      JsonDecoder.string,
+      'condition authorized value',
+    ),
+    enableConditionsOnIdentityProvider: JsonDecoder.boolean,
+    endpointTheConditionsAttributePathComeFrom: endpoint,
+    trustedClientAddresses: JsonDecoder.array(
+      JsonDecoder.string,
+      'trusted client addresses',
+    ),
+  },
+  'Authentification conditions',
+  {
+    blacklistClientAddresses: 'blacklist_client_addresses',
+    conditionsAttributePath: 'attribute_path',
+    conditionsAuthorizedValues: 'authorized_values',
+    enableConditionsOnIdentityProvider: 'is_enabled',
+    endpointTheConditionsAttributePathComeFrom: 'endpoint',
+    trustedClientAddresses: 'trusted_client_addresses',
+  },
+);
+
+const rolesMapping = JsonDecoder.object<RolesMapping>(
+  {
+    rolesApplyOnlyFirstRole: JsonDecoder.boolean,
+    rolesAttributePath: JsonDecoder.string,
+    rolesEndpoint: endpoint,
+    rolesIsEnabled: JsonDecoder.boolean,
+    rolesRelations: JsonDecoder.array(relation, 'Roles relation'),
+  },
+  'Roles mapping',
+  {
+    rolesApplyOnlyFirstRole: 'apply_only_first_role',
+    rolesAttributePath: 'attribute_path',
+    rolesEndpoint: 'endpoint',
+    rolesIsEnabled: 'is_enabled',
+    rolesRelations: 'relations',
+  },
+);
+
 const authorization = JsonDecoder.object<AuthorizationRule>(
   {
     accessGroup: getNamedEntityDecoder('Access group'),
@@ -74,18 +150,20 @@ export const openidConfigurationDecoder =
   JsonDecoder.object<OpenidConfiguration>(
     {
       authenticationType: JsonDecoder.nullable(JsonDecoder.string),
+      authentificationConditions: authConditions,
       authorizationEndpoint: JsonDecoder.nullable(JsonDecoder.string),
       authorizationRules: JsonDecoder.array(
         authorization,
         'Authorization relations',
       ),
       autoImport: JsonDecoder.boolean,
+
       baseUrl: JsonDecoder.nullable(JsonDecoder.string),
-      blacklistClientAddresses: JsonDecoder.array(
-        JsonDecoder.string,
-        'blacklist client addresses',
-      ),
-      claimName: JsonDecoder.nullable(JsonDecoder.string),
+      // blacklistClientAddresses: JsonDecoder.array(
+      //   JsonDecoder.string,
+      //   'blacklist client addresses',
+      // ),
+      // claimName: JsonDecoder.nullable(JsonDecoder.string),
       clientId: JsonDecoder.nullable(JsonDecoder.string),
       clientSecret: JsonDecoder.nullable(JsonDecoder.string),
       connectionScopes: JsonDecoder.array(
@@ -104,24 +182,46 @@ export const openidConfigurationDecoder =
       introspectionTokenEndpoint: JsonDecoder.nullable(JsonDecoder.string),
       isActive: JsonDecoder.boolean,
       isForced: JsonDecoder.boolean,
+
       loginClaim: JsonDecoder.nullable(JsonDecoder.string),
+
+      // rolesApplyOnlyFirstRole: JsonDecoder.boolean,
+      // rolesAttributePath: JsonDecoder.nullable(JsonDecoder.string),
+      // rolesEndpoint: endpoint,
+      // rolesIsEnabled: JsonDecoder.boolean,
+      // rolesRelations: JsonDecoder.array(
+      //   relation ,
+      //   "Roles relation"
+      // ),
+      // conditionsAttributePath: JsonDecoder.nullable(JsonDecoder.string),
+      // conditionsAuthorizedValues: JsonDecoder.array(
+      //   JsonDecoder.string,
+      //   'condition authorized value',
+      // ),
+      // endpointTheConditionsAttributePathComeFrom: endpoint,
+      // enableConditionsOnIdentityProvider : JsonDecoder.boolean
+      rolesMapping,
+
       tokenEndpoint: JsonDecoder.nullable(JsonDecoder.string),
-      trustedClientAddresses: JsonDecoder.array(
-        JsonDecoder.string,
-        'trusted client addresses',
-      ),
+
+      // trustedClientAddresses: JsonDecoder.array(
+      //   JsonDecoder.string,
+      //   'trusted client addresses',
+      // ),
       userinfoEndpoint: JsonDecoder.nullable(JsonDecoder.string),
       verifyPeer: JsonDecoder.boolean,
     },
     'Open ID Configuration',
     {
       authenticationType: 'authentication_type',
+      authentificationConditions: 'authentication_conditions',
       authorizationEndpoint: 'authorization_endpoint',
       authorizationRules: 'authorization_rules',
       autoImport: 'auto_import',
+
       baseUrl: 'base_url',
-      blacklistClientAddresses: 'blacklist_client_addresses',
-      claimName: 'claim_name',
+      // blacklistClientAddresses: 'blacklist_client_addresses',
+      // claimName: 'claim_name',
       clientId: 'client_id',
       clientSecret: 'client_secret',
       connectionScopes: 'connection_scopes',
@@ -134,8 +234,11 @@ export const openidConfigurationDecoder =
       isActive: 'is_active',
       isForced: 'is_forced',
       loginClaim: 'login_claim',
+
+      rolesMapping: 'roles_mapping',
+
       tokenEndpoint: 'token_endpoint',
-      trustedClientAddresses: 'trusted_client_addresses',
+      // trustedClientAddresses: 'trusted_client_addresses',
       userinfoEndpoint: 'userinfo_endpoint',
       verifyPeer: 'verify_peer',
     },
