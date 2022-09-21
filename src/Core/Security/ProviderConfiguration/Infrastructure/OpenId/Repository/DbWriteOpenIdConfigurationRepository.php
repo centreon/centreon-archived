5143d214ff972b7887f345b3a95dc5737336af0a
jeremyjaouen <jjaouen@centreon.com>
2022-09-21 08:45:40 +0200

    fix conflict

--- src/Core/Security/ProviderConfiguration/Infrastructure/OpenId/Repository/DbWriteOpenIdConfigurationRepository.php
+++ src/Core/Security/ProviderConfiguration/Infrastructure/OpenId/Repository/DbWriteOpenIdConfigurationRepository.php
@@ -73,7 +73,6 @@ class DbWriteOpenIdConfigurationRepository extends AbstractRepositoryDRB impleme
         $customConfiguration = $configuration->getCustomConfiguration();
         $authorizationRules = $customConfiguration->getAuthorizationRules();
 
-<<<<<<< HEAD
         if (!empty($authorizationRules)) {
             $this->info('Removing existing Authorization Rules');
             $this->deleteAuthorizationRules();
@@ -88,12 +87,6 @@ class DbWriteOpenIdConfigurationRepository extends AbstractRepositoryDRB impleme
             $this->info('Inserting new Authorization Rules');
             $this->insertContactGroupRelations($contactGroupRelations);
         }
-=======
-        $this->info('Removing existing Authorization Rules');
-        $this->deleteAuthorizationRules();
-        $this->info('Inserting new Authorization Rules');
-        $this->insertAuthorizationRules($authorizationRules);
->>>>>>> MON-14870
     }
 
     /**
