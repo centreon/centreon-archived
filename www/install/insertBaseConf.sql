a487b584e6358a14acceefa156e38d07207eacc8
tuntoja <58987095+tuntoja@users.noreply.github.com>
2022-10-07 09:08:39 +0200

    chore(release): merge hotfix-mon-15318 in 21.10.x (#11948)
    * update version to 21.10.12 in insertBaseConf
    
    * fixed issue where notification number < 0 before update (#11935)
    
    Co-authored-by: dmyios <diosypenko@centreon.com>
    
    Refs: MON-15318
    
    * add php update file for 21.10.12
    
    Co-authored-by: Kevin Duret <kduret@centreon.com>

--- www/install/insertBaseConf.sql
+++ www/install/insertBaseConf.sql
@@ -2,7 +2,7 @@
 -- Insert version
 --
 
-INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.11');
+INSERT INTO `informations` (`key` ,`value`) VALUES ('version', '21.10.12');
 
 --
 -- Contenu de la table `contact`
