e94d61279be4371c828321891eb6bc702094011d
Laurent Pinsivy <lpinsivy@centreon.com>
2022-10-13 15:00:32 +0200

    fix(code): fixe style

--- www/class/centreonStatistics.class.php
+++ www/class/centreonStatistics.class.php
@@ -198,48 +198,48 @@ class CentreonStatistics
         $query = "SELECT * FROM provider_configuration WHERE is_active = '1'";
         $result = $this->dbConfig->query($query);
         while ($row = $result->fetch()) {
-            $custom_configuration = json_decode($row['custom_configuration'], true);
+            $customConfiguration = json_decode($row['customConfiguration'], true);
             if ($row['type'] === 'local') {
-                $data['local'] = $custom_configuration['password_security_policy'];
+                $data['local'] = $customConfiguration['password_security_policy'];
             } elseif ($row['type'] === 'web-sso') {
                 $data['web-sso'] = [
                     'is_forced' => $row['is_forced'] ? true : false,
-                    'trusted_client_addresses' => count($custom_configuration['trusted_client_addresses']) ?? 0,
-                    'blacklist_client_addresses' => count($custom_configuration['blacklist_client_addresses']) ?? 0,
-                    'pattern_matching_login' => ($custom_configuration['pattern_matching_login'] ? true : false),
-                    'pattern_replace_login' => ($custom_configuration['pattern_replace_login'] ? true : false),
+                    'trusted_client_addresses' => count($customConfiguration['trusted_client_addresses']) ?? 0,
+                    'blacklist_client_addresses' => count($customConfiguration['blacklist_client_addresses']) ?? 0,
+                    'pattern_matching_login' => ($customConfiguration['pattern_matching_login'] ? true : false),
+                    'pattern_replace_login' => ($customConfiguration['pattern_replace_login'] ? true : false),
                 ];
             } elseif ($row['type'] === 'openid') {
-                $authentication_conditions = $custom_configuration['authentication_conditions'];
-                $groups_mapping = $custom_configuration['groups_mapping'];
-                $roles_mapping = $custom_configuration['roles_mapping'];
+                $authenticationConditions = $customConfiguration['authenticationConditions'];
+                $groupsMapping = $customConfiguration['groups_mapping'];
+                $rolesMapping = $customConfiguration['roles_mapping'];
                 $data['openid'] = [
                     'is_forced' => $row['is_forced'] ? true : false,
-                    'authentication_conditions' => [
-                        'is_enabled' => $authentication_conditions['is_enabled'] ? true : false,
+                    'authenticationConditions' => [
+                        'is_enabled' => $authenticationConditions['is_enabled'] ? true : false,
                         'trusted_client_addresses'
-                            => count($authentication_conditions['trusted_client_addresses']) ?? 0,
+                            => count($authenticationConditions['trusted_client_addresses']) ?? 0,
                         'blacklist_client_addresses'
-                            => count($authentication_conditions['blacklist_client_addresses']) ?? 0,
-                        'authorized_values' => count($authentication_conditions['authorized_values']) ?? 0,
+                            => count($authenticationConditions['blacklist_client_addresses']) ?? 0,
+                        'authorized_values' => count($authenticationConditions['authorized_values']) ?? 0,
                     ],
                     'groups_mapping' => [
-                        'is_enabled' => $groups_mapping['is_enabled'] ? true : false,
+                        'is_enabled' => $groupsMapping['is_enabled'] ? true : false,
                         'relations' => $cgRelations
                     ],
                     'roles_mapping' => [
-                        'is_enabled' => $roles_mapping['is_enabled'] ? true : false,
-                        'apply_only_first_role' => $roles_mapping['apply_only_first_role'] ? true : false,
+                        'is_enabled' => $rolesMapping['is_enabled'] ? true : false,
+                        'apply_only_first_role' => $rolesMapping['apply_only_first_role'] ? true : false,
                         'relations' => $aclRelations
                     ],
-                    'introspection_token_endpoint' => ($custom_configuration['introspection_token_endpoint']
+                    'introspection_token_endpoint' => ($customConfiguration['introspection_token_endpoint']
                         ? true : false),
-                    'userinfo_endpoint' => ($custom_configuration['userinfo_endpoint'] ? true : false),
-                    'endsession_endpoint' => ($custom_configuration['endsession_endpoint'] ? true : false),
-                    'connection_scopes' => count($custom_configuration['connection_scopes']) ?? 0,
-                    'authentication_type' => $custom_configuration['authentication_type'],
-                    'verify_peer' => ($custom_configuration['verify_peer'] ? true : false),
-                    'auto_import' => ($custom_configuration['auto_import'] ? true : false)
+                    'userinfo_endpoint' => ($customConfiguration['userinfo_endpoint'] ? true : false),
+                    'endsession_endpoint' => ($customConfiguration['endsession_endpoint'] ? true : false),
+                    'connection_scopes' => count($customConfiguration['connection_scopes']) ?? 0,
+                    'authentication_type' => $customConfiguration['authentication_type'],
+                    'verify_peer' => ($customConfiguration['verify_peer'] ? true : false),
+                    'auto_import' => ($customConfiguration['auto_import'] ? true : false)
                 ];
             }
         }
