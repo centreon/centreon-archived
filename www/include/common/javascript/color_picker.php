f51500411e0c7f85d44b3f19ad4f44b032bcb8a3
elmahdiabbassi <emabbassi-ext@centreon.com>
2022-10-19 17:06:38 +0100

    fix centreon-bot suggestions

--- www/include/common/javascript/color_picker.php
+++ www/include/common/javascript/color_picker.php
@@ -33,7 +33,6 @@
  * 
  * SVN : $URL$
  * SVN : $Id$
- * 
  */
 require_once __DIR__ . '/../../../class/HtmlAnalyzer.php';
 
@@ -69,8 +68,8 @@ $n = htmlspecialchars($n, ENT_QUOTES, 'UTF-8');
 $name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
 $title = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
 $hcolor = htmlspecialchars($hcolor, ENT_QUOTES, 'UTF-8');
-    $name1 = $n."";
-    $name2 = $n."_color";
+$name1 = $n . "";
+$name2 = $n . "_color";
 
 ?>
 <html>
