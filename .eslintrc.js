9c0a942d1543e4775d8b7c2cd7bc654314c543fe
Pascal Tassel <ptassel@centreon.com>
2022-10-24 08:13:31 +0000

    set ESLint rule trailingComma to none

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   extends:
     './node_modules/centreon-frontend/packages/frontend-config/eslint/react/typescript.eslintrc.js',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none'
+      }
+    ]
+  }
 };
