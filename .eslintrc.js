ef113d3fa3117062280e918a23aac96bf2e18d4e
Pascal Tassel <ptassel@centreon.com>
2022-10-24 09:55:18 +0000

    remove prettier rules from ESLint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   extends:
     './node_modules/centreon-frontend/packages/frontend-config/eslint/react/typescript.eslintrc.js',
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'none'
-      }
-    ]
-  }
 };
