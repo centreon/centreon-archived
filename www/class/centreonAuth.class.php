dbd219d884da568b557fafe84cddad60d4a78d3c
jeremyjaouen <jjaouen@centreon.com>
2022-11-03 08:45:27 +0100

    fix api v1 authentication

--- www/class/centreonAuth.class.php
+++ www/class/centreonAuth.class.php
@@ -233,6 +233,7 @@ class CentreonAuth
         ) {
             if (
                 array_key_exists('contact_oreon', $this->userInfos)
+                && $this->autologin === "1"
                 && $this->userInfos['contact_oreon'] !== '1'
             ) {
                 $this->passwdOk = 0;
